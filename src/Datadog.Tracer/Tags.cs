2253ebc9f8a413998a4e98c8448ca4cfa022a6c5
Bertrand Mermet <bertrand.mermet@datadoghq.com>
2017-10-16 04:51:42 -0700

    Address comments

--- src/Datadog.Tracer/Tags.cs
+++ src/Datadog.Tracer/Tags.cs
@@ -2,8 +2,8 @@
 {
     public static class Tags
     {
-        public const string ServiceName = "service-name";
-        public const string ResourceName = "resource-name";
-        public const string SpanType = "span-type";
+        public const string ServiceName = "service.name";
+        public const string ResourceName = "resource.name";
+        public const string SpanType = "span.type";
     }
 }
