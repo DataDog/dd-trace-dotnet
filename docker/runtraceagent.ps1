c2a04189e6c71f8b2913b1043cced52bc013c0af
Bertrand Mermet <bertrand.mermet@datadoghq.com>
2017-11-09 16:21:56 +0100

    WIP

--- docker/runtraceagent.ps1
+++ docker/runtraceagent.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest https://github.com/DataDog/datadog-trace-agent/releases/download/5.19.0/trace-agent-windows-5.19.0.exe -OutFile trace-agent-windows.exe
-Start-Process -NoNewWindow trace-agent-windows.exe "--ddconfig config.ini"
+Start-Process trace-agent-windows.exe "--ddconfig $PSScriptRoot\config.ini"
