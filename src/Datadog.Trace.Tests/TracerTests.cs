e8b69cfd4c0d51388a81210d6e373a88ec0f004f
Bertrand Mermet <bertrand.mermet@datadoghq.com>
2017-11-09 10:08:05 -0800

    Fix compile constants for NET45

--- src/Datadog.Trace.Tests/TracerTests.cs
+++ src/Datadog.Trace.Tests/TracerTests.cs
@@ -19,7 +19,7 @@ namespace Datadog.Trace.Tests
             var span = (Span)builder.Start();
 #if NETCOREAPP2_0
             Assert.Equal("testhost", span.ServiceName);
-#elif NET45
+#elif NET45_TESTS
             Assert.Equal("Datadog.Trace.Tests.Net45", span.ServiceName);
 #else
             Assert.Equal("Datadog.Trace.Tests", span.ServiceName);
