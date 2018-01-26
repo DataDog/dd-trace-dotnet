855d12a3bce952d083cc2a29ffca77a421e97c90
Bertrand Mermet <bertrand.mermet@datadoghq.com>
2018-01-26 14:14:40 +0100

    WIP

--- src/Datadog.Trace.SqlClient.Tests/SqlClientListenerTests.cs
+++ src/Datadog.Trace.SqlClient.Tests/SqlClientListenerTests.cs
@@ -10,6 +10,11 @@ namespace Datadog.Trace.SqlClient.Tests
         private static GlobalListener _globalListener;
         private static SqlClientListener _sqlListener;
 
+        private string connectionString = "Server=172.29.108.68;User Id=SA;Password=pass";
+
+        //// Provide the query string with a parameter placeholder.
+        // private string queryString =
+        //    "SELECT * from dbo.employee";
         public SqlClientListenerTests()
         {
             _sqlListener = new SqlClientListener(Tracer.Instance);
@@ -19,11 +24,11 @@ namespace Datadog.Trace.SqlClient.Tests
         [Fact]
         public void Test1()
         {
-            using (var connection = new SqlConnection())
+            using (var connection = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand("Select * from lol", connection);
 
-                // connection.Open();
+                connection.Open();
                 var reader = command.ExecuteReader();
                 reader.Close();
             }
