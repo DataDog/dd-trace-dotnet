c315b3f15b3865c24c4ff3cad571f329ab01dc3d
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-04-30 21:18:27 -0400

    delete projects that will not be used

--- src/Datadog.Trace/AssemblyInfo.cs
+++ src/Datadog.Trace/AssemblyInfo.cs
@@ -1,12 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Datadog.Trace.SqlClient")]
 [assembly: InternalsVisibleTo("Datadog.Trace.Tests")]
-[assembly: InternalsVisibleTo("Datadog.Trace.Tests.Net45")]
 [assembly: InternalsVisibleTo("Datadog.Trace.IntegrationTests")]
-[assembly: InternalsVisibleTo("Datadog.Trace.IntegrationTests.Net45")]
-[assembly: InternalsVisibleTo("Datadog.Trace.TestUtils")]
-[assembly: InternalsVisibleTo("Datadog.Trace.AspNetCore.Tests")]
-[assembly: InternalsVisibleTo("Datadog.Trace.AspNetCore10.Tests")]
-[assembly: InternalsVisibleTo("Datadog.Trace.SqlClient.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
