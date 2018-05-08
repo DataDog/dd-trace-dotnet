4727d7a3ff8b402f62ca0cd690bd5e536ecdf60f
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-05-08 11:28:39 -0400

    fix code style issues found by StyleCop

--- benchmarks/Datadog.Trace.Performance/Program.cs
+++ benchmarks/Datadog.Trace.Performance/Program.cs
@@ -1,30 +1,9 @@
 ﻿using System;
-using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using Datadog.Trace;
 
 namespace MyBenchmarks
 {
-    public class TracerBenchmarks
-    {
-        [Benchmark]
-        public void StartSpan()
-        {
-            using(var span = Tracer.Instance.StartSpan("Operation"))
-            {
-            }
-        }
-
-        [Benchmark]
-        public void StartScope()
-        {
-            using(var scope = Tracer.Instance.StartActive("Operation"))
-            {
-            }
-        }
-    }
-
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
