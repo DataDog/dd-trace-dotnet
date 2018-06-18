bacf8329278a2628a59408ee827dcf973233e4b0
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-06-18 10:38:26 -0400

    add missing xml-doc comments

--- src/Datadog.Trace.ClrProfiler.Managed/Instrumentation.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Instrumentation.cs
@@ -11,31 +11,21 @@ using System.Web;
 
 namespace Datadog.Trace.ClrProfiler
 {
+    /// <summary>
+    /// Provides instrumentation probes that can be injected into profiled code.
+    /// </summary>
     public static class Instrumentation
     {
         private static readonly ConcurrentDictionary<string, MetadataNames> MetadataLookup = new ConcurrentDictionary<string, MetadataNames>();
 
-        private static MetadataNames GetMetadataNames(IntPtr moduleId, uint methodToken)
-        {
-            var modulePathBuffer = new StringBuilder(512);
-            var typeNameBuffer = new StringBuilder(256);
-            var methodNameBuffer = new StringBuilder(256);
-
-            NativeMethods.GetMetadataNames(moduleId,
-                                           methodToken,
-                                           modulePathBuffer,
-                                           (ulong)modulePathBuffer.Capacity,
-                                           typeNameBuffer,
-                                           (ulong)typeNameBuffer.Capacity,
-                                           methodNameBuffer,
-                                           (ulong)methodNameBuffer.Capacity);
-
-            string module = System.IO.Path.GetFileName(modulePathBuffer.ToString());
-            string type = typeNameBuffer.ToString();
-            string method = methodNameBuffer.ToString();
-            return new MetadataNames(module, type, method);
-        }
-
+        /// <summary>
+        /// Called after an instrumented method is entered.
+        /// </summary>
+        /// <param name="integrationTypeValue">A <see cref="IntegrationType"/> tht indicated which integration is instrumenting this method.</param>
+        /// <param name="moduleId">The id of the module where the instrumented method is defined.</param>
+        /// <param name="methodToken">The <c>mdMemberDef</c> token of the instrumented method.</param>
+        /// <param name="args">An array with all the argumetns that were passed into the instrumented method. If it is an instance method, the first arguments is <c>this</c>.</param>
+        /// <returns>A <see cref="Scope"/> created to instrument the method.</returns>
         [System.Security.SecuritySafeCritical]
         public static object OnMethodEntered(int integrationTypeValue,
                                              ulong moduleId,
@@ -108,12 +98,15 @@ namespace Datadog.Trace.ClrProfiler
                     break;
             }
 
-
             // the return value will be left on the stack for the duration
             // of the instrumented method and passed into OnMethodExit()
             return scope;
         }
 
+        /// <summary>
+        /// Called before an instrumented method exits.
+        /// </summary>
+        /// <param name="args">The <see cref="Scope"/> that was created by <see cref="OnMethodEntered"/>.</param>
         [System.Security.SecuritySafeCritical]
         public static void OnMethodExit(object args)
         {
@@ -121,6 +114,12 @@ namespace Datadog.Trace.ClrProfiler
             scope?.Close();
         }
 
+        /// <summary>
+        /// Called before an instrumented method exits.
+        /// </summary>
+        /// <param name="args">The <see cref="Scope"/> that was created by <see cref="OnMethodEntered"/>.</param>
+        /// <param name="originalReturnValue">The value returned by the instrumented method.</param>
+        /// <returns>Returns the value that was originally returned by the instrumented method.</returns>
         [System.Security.SecuritySafeCritical]
         public static object OnMethodExit(object args, object originalReturnValue)
         {
@@ -128,15 +127,44 @@ namespace Datadog.Trace.ClrProfiler
             return originalReturnValue;
         }
 
+        /// <summary>
+        /// Determines whether tracing with Datadog's profiler is enabled.
+        /// </summary>
+        /// <returns><c>true</c> if profiling is enabled; <c>false</c> otherwise.</returns>
         public static bool IsProfilingEnabled()
         {
             string setting = ConfigurationManager.AppSettings["Datadog.Tracing:Enabled"];
             return !string.Equals(setting, bool.FalseString, StringComparison.InvariantCultureIgnoreCase);
         }
 
+        /// <summary>
+        /// Determines whether Datadog's profiler is currently attached.
+        /// </summary>
+        /// <returns><c>true</c> if the profiler is currentl attached; <c>false</c> otherwise.</returns>
         public static bool IsProfilerAttached()
         {
             return NativeMethods.IsProfilerAttached();
+        }
+
+        private static MetadataNames GetMetadataNames(IntPtr moduleId, uint methodToken)
+        {
+            var modulePathBuffer = new StringBuilder(512);
+            var typeNameBuffer = new StringBuilder(256);
+            var methodNameBuffer = new StringBuilder(256);
+
+            NativeMethods.GetMetadataNames(moduleId,
+                                           methodToken,
+                                           modulePathBuffer,
+                                           (ulong)modulePathBuffer.Capacity,
+                                           typeNameBuffer,
+                                           (ulong)typeNameBuffer.Capacity,
+                                           methodNameBuffer,
+                                           (ulong)methodNameBuffer.Capacity);
+
+            string module = System.IO.Path.GetFileName(modulePathBuffer.ToString());
+            string type = typeNameBuffer.ToString();
+            string method = methodNameBuffer.ToString();
+            return new MetadataNames(module, type, method);
         }
     }
 }
--- src/Datadog.Trace.ClrProfiler.Managed/MetadataNames.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/MetadataNames.cs
@@ -2,14 +2,32 @@ using System;
 
 namespace Datadog.Trace.ClrProfiler
 {
+    /// <summary>
+    /// Holds names obtained through the metadata interface.
+    /// </summary>
     public class MetadataNames
     {
+        /// <summary>
+        /// Gets the module's name.
+        /// </summary>
         public string ModuleName { get; }
 
+        /// <summary>
+        /// Gets the type's full name.
+        /// </summary>
         public string TypeName { get; }
 
+        /// <summary>
+        /// Gets the method's name.
+        /// </summary>
         public string MethodName { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MetadataNames"/> class.
+        /// </summary>
+        /// <param name="moduleName">The module's name.</param>
+        /// <param name="typeName">The type's name.</param>
+        /// <param name="methodName">The method's name.</param>
         public MetadataNames(string moduleName, string typeName, string methodName)
         {
             ModuleName = moduleName ?? throw new ArgumentNullException(nameof(moduleName));
