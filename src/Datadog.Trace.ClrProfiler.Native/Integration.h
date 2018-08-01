74a16ed827a595d69ea1eb36d68623de1aaef3f0
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-08-01 16:09:36 -0400

    create only once instance of `asp_net_mvc5_integration` and `all_integrations`

--- src/Datadog.Trace.ClrProfiler.Native/Integration.h
+++ src/Datadog.Trace.ClrProfiler.Native/Integration.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <corhlpr.h>
 #include "IntegrationType.h"
 
@@ -57,63 +58,6 @@ struct integration
     }
 };
 
-const integration asp_net_mvc5_integration
-    = ::integration(
-                    // integration_type
-                    IntegrationType_AspNetMvc5,
-                    // integration_name
-                    L"aspNetMvc5",
-                    // target_assembly_name
-                    L"System.Web.Mvc",
-                    // wrapper_assembly_name
-                    L"Datadog.Trace.ClrProfiler.Managed",
-                    // wrapper_type_name
-                    L"Datadog.Trace.ClrProfiler.Integrations.AspNetMvc5Integration",
-                    // method_replacements
-                    {
-                        method_replacement(
-                                           // caller_method_token: [System.Web.Mvc]System.Web.Mvc.Controller.BeginExecuteCore() ...
-                                           0x06000FA5,
-                                           // target_method_token: [System.Web.Mvc]System.Web.Mvc.Async.IAsyncActionInvoker.BeginInvokeAction()
-                                           0x06000CA1,
-                                           // wrapper_method_name
-                                           L"BeginInvokeAction",
-                                           // wrapper_method_signature
-                                           {
-                                               // calling convention
-                                               IMAGE_CEE_CS_CALLCONV_DEFAULT,
-                                               // parameter count
-                                               0x05,
-                                               // return type
-                                               ELEMENT_TYPE_OBJECT,
-                                               // parameter types
-                                               ELEMENT_TYPE_OBJECT,
-                                               ELEMENT_TYPE_OBJECT,
-                                               ELEMENT_TYPE_OBJECT,
-                                               ELEMENT_TYPE_OBJECT,
-                                               ELEMENT_TYPE_OBJECT,
-                                           }),
-                        method_replacement(
-                                           // caller_method_token: [System.Web.Mvc]System.Web.Mvc.Controller.BeginExecuteCore() ...
-                                           0x06000fA6,
-                                           // target_method_token: [System.Web.Mvc]System.Web.Mvc.Async.IAsyncActionInvoker.EndInvokeAction()
-                                           0x06000CA2,
-                                           // wrapper_method_name
-                                           L"EndInvokeAction",
-                                           // wrapper_method_signature
-                                           {
-                                               // calling convention
-                                               IMAGE_CEE_CS_CALLCONV_DEFAULT,
-                                               // parameter count
-                                               0x02,
-                                               // return type
-                                               ELEMENT_TYPE_BOOLEAN,
-                                               // parameter types
-                                               ELEMENT_TYPE_OBJECT,
-                                               ELEMENT_TYPE_OBJECT,
-                                           })
-                    });
+extern const integration asp_net_mvc5_integration;
 
-const std::vector<integration> all_integrations = {
-    asp_net_mvc5_integration,
-};
+extern const std::vector<integration> all_integrations;
