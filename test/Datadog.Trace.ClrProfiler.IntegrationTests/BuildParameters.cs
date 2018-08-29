be93805394794384251fc416b7c638f5a468f86d
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-08-29 16:53:45 -0400

    add integration test that sets env. variables and run console app, asserting that the profiler is attached and custom method replacement happened

---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/BuildParameters.cs
@@ -0,0 +1,27 @@
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class BuildParameters
+    {
+#if DEBUG
+        public const string Configuration = "Debug";
+#else
+        public const string Configuration = "Release";
+#endif
+
+#if NETFRAMEWORK
+        public const bool CoreClr = false;
+#else
+        public const bool CoreClr = true;
+#endif
+
+#if NET452
+        public const string TargetFramework = "net452";
+#elif NET46
+        public const string TargetFramework = "net46";
+#elif NET47
+        public const string TargetFramework = "net47";
+#elif NETCOREAPP2_0
+        public const string TargetFramework = "netcoreapp2.0";
+#endif
+    }
+}
---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/ConsoleCoreTests.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Datadog.Trace.TestHelpers;
+using Xunit;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class ConsoleCoreTests
+    {
+        [Fact]
+        public void ProfilerAttached()
+        {
+            // get path to native profiler dll
+            var platform = Environment.Is64BitProcess ? "x64" : "x86";
+            string profilerDllPath = Path.GetFullPath($"Datadog.Trace.ClrProfiler.Native.{BuildParameters.Configuration}-{platform}.dll");
+            Assert.True(File.Exists(profilerDllPath), $"Profiler DLL not found at {profilerDllPath}");
+
+            // get path to sample app that the profiler will attach to
+            string appPath = Path.GetFullPath(BuildParameters.CoreClr ? "Samples.ConsoleCore.dll" : "Samples.ConsoleCore.exe");
+            Assert.True(File.Exists(appPath), $"Application not found at {appPath}");
+
+            // get path to integration definitions
+            IEnumerable<string> integrationPaths = Directory.EnumerateFiles(".", "*.json");
+
+            string output;
+            int exitCode;
+
+            using (Process process = ProfilerHelper.StartProfiledProcess(
+                appPath,
+                BuildParameters.CoreClr,
+                integrationPaths,
+                Instrumentation.ProfilerClsid,
+                profilerDllPath))
+            {
+                output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                exitCode = process.ExitCode;
+            }
+
+            Assert.True(exitCode >= 0, $"Process exited with code {exitCode}");
+
+            string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            Assert.Contains("ProfilerAttached=True", lines);
+            Assert.DoesNotContain("Add(1,2)=3", lines);
+        }
+    }
+}
