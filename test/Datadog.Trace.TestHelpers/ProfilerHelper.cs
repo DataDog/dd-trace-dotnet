7f953de637bc2b2ee3da9f79829b3908627bcfec
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-08-28 20:37:40 -0400

    add helper method to start a .net process with the profiler attached

---
+++ test/Datadog.Trace.TestHelpers/ProfilerHelper.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Datadog.Trace.TestHelpers
+{
+    public class ProfilerHelper
+    {
+        public static Process StartProfiledProcess(
+            string appPath,
+            bool coreClr,
+            IEnumerable<string> integrationPaths,
+            string profilerClsid,
+            string profilerDllPath)
+        {
+            if (appPath == null)
+            {
+                throw new ArgumentNullException(nameof(appPath));
+            }
+
+            if (integrationPaths == null)
+            {
+                throw new ArgumentNullException(nameof(integrationPaths));
+            }
+
+            if (profilerClsid == null)
+            {
+                throw new ArgumentNullException(nameof(profilerClsid));
+            }
+
+            // TODO: use "CorFlags MyAssembly.exe /32BIT+" to force 32/64 bit process?
+            ProcessStartInfo startInfo;
+
+            if (coreClr)
+            {
+                // .NET Core
+                Environment.SetEnvironmentVariable("CORECLR_ENABLE_PROFILING", "1");
+                Environment.SetEnvironmentVariable("CORECLR_PROFILER", profilerClsid);
+                Environment.SetEnvironmentVariable("CORECLR_PROFILER_PATH", profilerDllPath);
+
+                startInfo = new ProcessStartInfo("dotnet", appPath);
+            }
+            else
+            {
+                // .NET Framework
+                Environment.SetEnvironmentVariable("COR_ENABLE_PROFILING", "1");
+                Environment.SetEnvironmentVariable("COR_PROFILER", profilerClsid);
+                Environment.SetEnvironmentVariable("COR_PROFILER_PATH", profilerDllPath);
+
+                startInfo = new ProcessStartInfo(appPath);
+            }
+
+            Environment.SetEnvironmentVariable("DATADOG_INTEGRATIONS", string.Join(";", integrationPaths));
+
+            // clear this one
+            Environment.SetEnvironmentVariable("DATADOG_PROFILER_PROCESSES", null);
+
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+
+            return Process.Start(startInfo);
+        }
+    }
+}
