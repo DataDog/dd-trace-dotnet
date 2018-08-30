5f47e1b7ee91615373a5d17be036d6bff064e91c
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-08-30 12:54:38 -0400

    output relevant environment variables to console

--- samples/Samples.ConsoleCore/Program.cs
+++ samples/Samples.ConsoleCore/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Samples.ConsoleCore
 {
@@ -11,6 +14,22 @@ namespace Samples.ConsoleCore
 
         private void Run()
         {
+            var prefixes = new[] { "COR_", "CORECLR_", "DATADOG_" };
+
+            var environmentVariables = from entry in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
+                                       from prefix in prefixes
+                                       let pair = new KeyValuePair<string, string>(
+                                           ((string)entry.Key).ToUpperInvariant(),
+                                           (string)entry.Value)
+                                       where pair.Key.StartsWith(prefix)
+                                       orderby pair.Key
+                                       select pair;
+
+            foreach (var envVar in environmentVariables)
+            {
+                Console.WriteLine($"{envVar.Key}={envVar.Value}");
+            }
+
             Console.WriteLine($"ProfilerAttached={Datadog.Trace.ClrProfiler.Instrumentation.ProfilerAttached}");
             Console.WriteLine($"Add(1,2)={new ExampleLibrary.Class1().Add(1, 2)}");
         }
