7a153dda68b7ac51f9b791b41102ad86bce504cf
Caleb Doxsey <caleb@datadoghq.com>
2018-09-07 07:53:17 -0700

    revert console framework changes

--- samples/Samples.ConsoleFramework/Program.cs
+++ samples/Samples.ConsoleFramework/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Data.SqlClient;
-using System.Linq;
-using Newtonsoft.Json;
 
 namespace Samples.ConsoleFramework
 {
@@ -10,37 +6,15 @@ namespace Samples.ConsoleFramework
     {
         private static void Main(string[] args)
         {
-            object output = new Program().Run();
-
-            var serializer = JsonSerializer.CreateDefault();
-            serializer.Formatting = Formatting.Indented;
-            serializer.Serialize(Console.Out, output);
-
-            Console.WriteLine();
+            new Program().Run();
         }
 
-        private object Run()
+        private void Run()
         {
-            var prefixes = new[] { "COR_", "CORECLR_", "DATADOG_" };
-
-            var environmentVariables = from entry in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
-                                       from prefix in prefixes
-                                       let key = ((string)entry.Key).ToUpperInvariant()
-                                       where key.StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase)
-                                       select new
-                                       {
-                                           Key = key,
-                                           entry.Value
-                                       };
-
-            var dictionary = environmentVariables.ToDictionary(v => v.Key, v => v.Value);
-            int addResult = new ExampleLibrary.Class1().Add(1, 2);
-
-            dictionary.Add("ProfilerAttached", Datadog.Trace.ClrProfiler.Instrumentation.ProfilerAttached);
-            dictionary.Add("AddResult", addResult);
-
-
-            return dictionary;
+            Console.WriteLine($"1 + 2 = {new ExampleLibrary.Class1().Add(1, 2)}");
+            Console.WriteLine($"1 * 2 = {new ExampleLibrary.Class1().Multiply(1, 2)}");
+            Console.WriteLine($"1 / 2 = {new ExampleLibrary.Class1().Divide(1, 2)}");
+            Console.ReadLine();
         }
     }
 }
