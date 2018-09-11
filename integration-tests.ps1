ac18efc8728299604e3b0427093516c7bffa110c
Caleb Doxsey <caleb@datadoghq.com>
2018-09-11 11:45:15 -0400

    use start-process

--- integration-tests.ps1
+++ integration-tests.ps1
@@ -3,7 +3,7 @@ Param(
   [string]$Configuration
 )
 
-docker-compose up --detach
+Start-Process "docker-compose" -ArgumentList "up","--detach" -NoNewWindow -Wait
 try
 {
   $Env:CONNECTION_STRING="Data Source=127.0.0.1,1433;Initial Catalog=Trace;Integrated Security=False;MultipleActiveResultSets=True;User Id=sa;Password=password!123"
@@ -17,5 +17,5 @@ try
 }
 finally
 {
-  docker-compose down
+  Start-Process "docker-compose" -ArgumentList "down" -NoNewWindow -Wait
 }
