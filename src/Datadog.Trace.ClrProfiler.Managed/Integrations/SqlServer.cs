7eea9653ed2b3a7ac22aa94591e65aa0060c7032
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-09-11 08:23:52 -0400

    cache the delegate for the dynamically generated method

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/SqlServer.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/SqlServer.cs
@@ -10,6 +10,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
     public static class SqlServer
     {
         private const string OperationName = "sqlserver.query";
+        private static Func<object, CommandBehavior, object> _executeReader;
 
         /// <summary>
         /// ExecuteReader traces any SQL call.
@@ -21,6 +22,14 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         {
             var command = (DbCommand)@this;
 
+            if (_executeReader == null)
+            {
+                _executeReader = DynamicMethodBuilder.CreateMethodCallDelegate<Func<object, CommandBehavior, object>>(
+                    command.GetType(),
+                    "ExecuteReader",
+                    isStatic: false);
+            }
+
             using (var scope = Tracer.Instance.StartActive(OperationName))
             {
                 // set the scope properties
@@ -31,12 +40,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
 
                 try
                 {
-                    var dynamicMethod = DynamicMethodBuilder.CreateMethodCallDelegate<Func<object, CommandBehavior, object>>(
-                        command.GetType(),
-                        "ExecuteReader",
-                        false);
-
-                    return dynamicMethod(command, (CommandBehavior)behavior);
+                    return _executeReader(command, (CommandBehavior)behavior);
                 }
                 catch (Exception ex)
                 {
