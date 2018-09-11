b9a3b68bd3c314a24b15d1f9572fcf53c8259387
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-09-10 21:20:12 -0400

    restore the stylecop suppression I removed earlier because it's stupid

--- test/GlobalSuppressions.cs
+++ test/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:EnableXmlDocumentationOutput", Justification = "Reviewed.")]
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code must not contain multiple whitespace in a row", Justification = "Reviewed. Used for pretty formatting.")]
