7aa02944f6c9c220e6c5be583dc67cfcc12bc5d8
Caleb Doxsey <caleb@datadoghq.com>
2018-09-11 11:07:45 -0400

    update integration tests

--- integration-tests.ps1
+++ integration-tests.ps1
@@ -1,27 +1,21 @@
+Param(
+  [string]$Platform,
+  [string]$Configuration
+)
+
 docker-compose up --detach
 try
 {
+  $Env:CONNECTION_STRING="Data Source=127.0.0.1,1433;Initial Catalog=Trace;Integrated Security=False;MultipleActiveResultSets=True;User Id=sa;Password=password!123"
+  $frameworks = @("net47")
+  $name = "Datadog.Trace.ClrProfiler.IntegrationTests"
 
-$Env:CONNECTION_STRING="Data Source=127.0.0.1,1433;Initial Catalog=Trace;Integrated Security=False;MultipleActiveResultSets=True;User Id=sa;Password=password!123"
-
-$platforms = @("x64", "x86")
-$configurations = @("Debug", "Release")
-$frameworks = @("net47")
-$name = "Datadog.Trace.ClrProfiler.IntegrationTests"
-
-foreach ($platform in $platforms) {
-    foreach ($configuration in $configurations) {
-        foreach ($framework in $frameworks) {
-            dotnet vstest \
-                /Platform:"$($platform)" \
-                /Logger:trx \
-                "test\\$($name)\\bin\\$($platform)\\$($configuration)\\$($framework)\\$($name).dll"
-        }
-    }
-}
-
+  foreach ($framework in $frameworks) {
+    $dll = Join-Path "test" $name "bin" $Platform $Configuration $framework "$($name).dll"
+    dotnet vstest /Platform:$Platform /Logger:trx $dll
+  }
 }
 finally
 {
-    docker-compose down
+  docker-compose down
 }
