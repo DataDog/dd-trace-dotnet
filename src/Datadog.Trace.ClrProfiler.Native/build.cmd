e5a868ae70e0fc2f53468187c46e36fc0d1a228e
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-09-17 08:58:51 -0400

    build scripts

--- src/Datadog.Trace.ClrProfiler.Native/build.cmd
+++ src/Datadog.Trace.ClrProfiler.Native/build.cmd
@@ -1,1 +1,9 @@
-msbuild /p:Configuration="%BuildType%" /p:BuildOS="Windows_NT" /p:BuildType="%BuildType%" /p:BuildArch="%BuildArch%" /p:CORECLR_PATH="%CORECLR_PATH%"
+@echo off
+
+SET CORECLR_PATH=../../lib/coreclr
+SET Output=Datadog.Trace.ClrProfiler.Native.dll
+
+SET CXX_FLAGS=--no-undefined -Wno-invalid-noreturn -fms-extensions -DPAL_STDCPP_COMPAT -DPLATFORM_WINDOWS -DBIT64=1 -DAMD64 -D_AMD64_ -D_WIN64 -std=c++14
+SET INCLUDES=-I %CORECLR_PATH%/src/pal/inc/rt -I %CORECLR_PATH%/src/pal/prebuilt/inc -I %CORECLR_PATH%/src/pal/inc -I %CORECLR_PATH%/src/inc
+
+clang++ -shared -o %Output% %CXX_FLAGS% %INCLUDES% *.cpp
--- src/Datadog.Trace.ClrProfiler.Native/build.sh
+++ src/Datadog.Trace.ClrProfiler.Native/build.sh
@@ -13,9 +13,9 @@ printf '  BuildType    : %s\n' "$BuildType"
 
 printf '  Building %s ... ' "$Output"
 
-CXX_FLAGS="$CXX_FLAGS --no-undefined -Wno-invalid-noreturn -fPIC -fms-extensions -DBIT64 -DPAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++11"
-INCLUDES="-I $CORECLR_PATH/src/pal/inc/rt -I $CORECLR_PATH/src/pal/prebuilt/inc -I $CORECLR_PATH/src/pal/inc -I $CORECLR_PATH/src/inc -I $CORECLR_PATH/bin/Product/$BuildOS.$BuildArch.$BuildType/inc"
+CXX_FLAGS="$CXX_FLAGS --no-undefined -Wno-invalid-noreturn -Wno-pragma-pack -fPIC -fms-extensions -DBIT64 -DPAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++17"
+INCLUDES="-I $CORECLR_PATH/src/pal/inc/rt -I $CORECLR_PATH/src/pal/prebuilt/inc -I $CORECLR_PATH/src/pal/inc -I $CORECLR_PATH/src/inc"
 
-clang++ -shared -o $Output $CXX_FLAGS $INCLUDES class_factory.cpp clr_helpers.cpp cor_profiler_base.cpp cor_profiler.cpp cs_holder.cpp il_rewriter.cpp il_rewriter_wrapper.cpp integration.cpp integration_loader.cpp interop.cpp metadata_builder.cpp util.cpp json.hpp
+clang++-6.0 -shared -o $Output $CXX_FLAGS $INCLUDES *.cpp
 
 printf 'Done.\n'
