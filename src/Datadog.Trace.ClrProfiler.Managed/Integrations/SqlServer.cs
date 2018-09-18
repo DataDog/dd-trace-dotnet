6a9c1cb0fa5ced9746cae4804c6b5871064976ce
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-09-18 17:21:30 -0400

    remove unused `using` directives

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/SqlServer.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/SqlServer.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
-using System.Linq;
 
 namespace Datadog.Trace.ClrProfiler.Integrations
 {
