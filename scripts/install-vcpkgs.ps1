97d63a95d67a9f5b66b6fd938b09080de49c8be5
Caleb Doxsey <caleb@datadoghq.com>
2018-09-19 12:16:41 -0400

    fix install script

--- scripts/install-vcpkgs.ps1
+++ scripts/install-vcpkgs.ps1
@@ -3,16 +3,26 @@ $vcpkgRoot = [IO.Path]::Combine($workspaceRoot, "vcpkg")
 $vcpkgExe = [IO.Path]::Combine($vcpkgRoot, "vcpkg.exe")
 
 
+function Run {
+    param(
+        [Parameter(Position=0)][string] $Name, 
+        [Parameter(Position=1)][String[]] $Args, 
+        [Parameter(Position=2)][string] $WorkingDirectory
+    )
+    $proc = Start-Process $Name -ArgumentList $Args -WorkingDirectory $WorkingDirectory -NoNewWindow -PassThru
+    $handle = $proc.Handle # cache proc.Handle
+    $proc.WaitForExit(60000)
+    return $proc
+}
+
+
 if (Test-Path $vcpkgRoot) {
     Write-Output "vcpkg repo found"
 } else {
     Write-Output "vcpkg repo not found, cloning"
-    $p = Start-Process "git" `
-        -ArgumentList "clone","https://github.com/Microsoft/vcpkg.git" `
-        -WorkingDirectory $workspaceRoot `
-        -NoNewWindow -PassThru
-    $p.WaitForExit()
+    $p = Run "git" "clone","https://github.com/Microsoft/vcpkg.git" $workspaceRoot
     if ($p.ExitCode -ne 0) {
+        Write-Output "failed to clone vcpkg repo: $($p.ExitCode)"
         Exit $p.ExitCode
     }
 }
@@ -21,12 +31,9 @@ if (Test-Path $vcpkgExe) {
     Write-Output "vcpkg.exe found"
 } else {
     Write-Output "vcpkg.exe not found, bootstrapping"
-    $p = Start-Process "cmd" `
-        -ArgumentList "/c","bootstrap-vcpkg.bat" `
-        -WorkingDirectory $vcpkgRoot `
-        -NoNewWindow -PassThru
-    $p.WaitForExit()
+    $p = Run "cmd" "/c","bootstrap-vcpkg.bat" $vcpkgRoot
     if ($p.ExitCode -ne 0) {
+        Write-Output "failed to bootstrap vcpkg: $($p.ExitCode)"
         Exit $p.ExitCode
     }
 }
@@ -37,12 +44,9 @@ $platforms = @("x86", "x64")
 foreach ($platform in $platforms) {
     foreach ($package in $packages) {
         Write-Output "installing $($package):$($platform)-windows-static"
-        $p = Start-Process $vcpkgExe `
-            -ArgumentList "install","$($package):$($platform)-windows-static" `
-            -WorkingDirectory $vcpkgRoot `
-            -NoNewWindow -PassThru
-        $p.WaitForExit()
+        $p = Run $vcpkgExe "install","$($package):$($platform)-windows-static" $vcpkgRoot
         if ($p.ExitCode -ne 0) {
+            Write-Output "failed to install $($package):$($platform): $($p.ExitCode)"
             Exit $p.ExitCode
         }
     }
