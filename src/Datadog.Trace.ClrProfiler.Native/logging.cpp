a5e579f4cede7e83fc3730d73d0cf1390e9c578e
Caleb Doxsey <caleb@datadoghq.com>
2018-09-19 17:04:37 -0400

    lock log initialization, support env variable

--- src/Datadog.Trace.ClrProfiler.Native/logging.cpp
+++ src/Datadog.Trace.ClrProfiler.Native/logging.cpp
@@ -1,14 +1,27 @@
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/stdout_sinks.h>
+#include <filesystem>
 
 #include "logging.h"
+#include "util.h"
 
 namespace trace {
 std::shared_ptr<spdlog::logger> GetLogger() {
+  static std::mutex mtx;
+
   auto logger = spdlog::get("dotnet-profiler");
   if (logger == nullptr) {
-    logger = spdlog::rotating_logger_mt("dotnet-profiler",
-                                        R"(C:\ProgramData\Datadog\logs)",
+    std::unique_lock<std::mutex> lck(mtx);
+
+    std::string logfile = R"(C:\ProgramData\Datadog\logs\profiler.log)";
+
+    const auto programdata = GetEnvironmentValue(L"PROGRAMDATA");
+    const auto dddir = std::filesystem::path(programdata).append("Datadog");
+    if (std::filesystem::exists(dddir)) {
+      logfile = dddir.string();
+    }
+
+    logger = spdlog::rotating_logger_mt("dotnet-profiler", logfile,
                                         1024 * 1024 * 5, 3);
   }
   return logger;
