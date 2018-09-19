d423c8dd8422bea3ac447086d41d4db04346678c
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-09-19 16:29:48 -0400

    clang-tidy told me to use raw string literal

--- src/Datadog.Trace.ClrProfiler.Native/logging.cpp
+++ src/Datadog.Trace.ClrProfiler.Native/logging.cpp
@@ -7,7 +7,8 @@ namespace trace {
 std::shared_ptr<spdlog::logger> GetLogger() {
   auto logger = spdlog::get("dotnet-profiler");
   if (logger == nullptr) {
-    logger = spdlog::rotating_logger_mt("dotnet-profiler", "C:\\ProgramData\\Datadog\\logs",
+    logger = spdlog::rotating_logger_mt("dotnet-profiler",
+                                        R"(C:\ProgramData\Datadog\logs)",
                                         1024 * 1024 * 5, 3);
   }
   return logger;
