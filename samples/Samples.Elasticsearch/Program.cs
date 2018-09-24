1f1e8e17d8a01fef58e575e75ac5e535ae7519ca
Caleb Doxsey <caleb@datadoghq.com>
2018-09-24 17:24:04 -0400

    wip

--- samples/Samples.Elasticsearch/Program.cs
+++ samples/Samples.Elasticsearch/Program.cs
@@ -9,11 +9,29 @@ namespace Samples.Elasticsearch
         static void Main(string[] args)
         {
             var localhost = new Uri("http://localhost:9200");
-            var settings = new ConnectionSettings(localhost);
+            var settings = new ConnectionSettings(localhost).DefaultIndex("elastic-net-example");
             var elastic = new ElasticClient(settings);
+
+            var createRes = elastic.CreateDocumentAsync(new Post
+            {
+                Id = 1,
+                Title = "Hello World",
+                Contents = "Hello World"
+            }).Result;
+            Console.WriteLine(createRes.DebugInformation);
+
+            var getRes = elastic.GetAsync<Post>(1).Result;
+            Console.WriteLine(getRes.DebugInformation);
 
             var res = elastic.ClusterHealth();
             Console.WriteLine(res.DebugInformation);
+        }
+
+        public class Post
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Contents { get; set; }
         }
     }
 }
