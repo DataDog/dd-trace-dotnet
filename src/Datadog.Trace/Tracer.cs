76e445e1e9eb52aa8a3eca70b0f15cb175132656
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-09-28 08:01:13 -0400

    move CreateConfigurationSource() because StyleCop

--- src/Datadog.Trace/Tracer.cs
+++ src/Datadog.Trace/Tracer.cs
@@ -45,27 +45,6 @@ namespace Datadog.Trace
             _scopeManager = new AsyncLocalScopeManager();
         }
 
-        private static IConfigurationSource CreateConfigurationSource()
-        {
-            // app.config (app local) > datadog.json (app local) > env (system-wide)
-            var configurationSource = new AggregateConfigurationSource();
-
-#if NET45 || NET46
-            configurationSource.AddSource(new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings));
-#endif
-
-            string jsonConfigurationFileName = configurationSource.GetString("DD_DOTNET_TRACER_CONFIGURATION_FILE") ??
-                                               Path.Combine(Environment.CurrentDirectory, "datadog.json");
-
-            if (File.Exists(jsonConfigurationFileName))
-            {
-                configurationSource.AddSource(JsonConfigurationSource.LoadFile(jsonConfigurationFileName));
-            }
-
-            configurationSource.AddSource(new EnvironmentConfigurationSource());
-            return configurationSource;
-        }
-
         /// <summary>
         /// Gets or sets the global tracer object
         /// </summary>
@@ -167,6 +146,27 @@ namespace Datadog.Trace
             var agentWriter = new AgentWriter(api);
             var tracer = new Tracer(agentWriter, serviceName, isDebugEnabled);
             return tracer;
+        }
+
+        internal static IConfigurationSource CreateConfigurationSource()
+        {
+            // app.config (app local) > datadog.json (app local) > env (system-wide)
+            var configurationSource = new AggregateConfigurationSource();
+
+#if NET45 || NET46
+            configurationSource.AddSource(new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings));
+#endif
+
+            string jsonConfigurationFileName = configurationSource.GetString("DD_DOTNET_TRACER_CONFIGURATION_FILE") ??
+                                               Path.Combine(Environment.CurrentDirectory, "datadog.json");
+
+            if (File.Exists(jsonConfigurationFileName))
+            {
+                configurationSource.AddSource(JsonConfigurationSource.LoadFile(jsonConfigurationFileName));
+            }
+
+            configurationSource.AddSource(new EnvironmentConfigurationSource());
+            return configurationSource;
         }
 
         private static Uri DefaultAgentUri()
