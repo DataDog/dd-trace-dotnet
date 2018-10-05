1497080e9c4f07bfc135c2704a653ae96f9acf85
Caleb Doxsey <caleb@datadoghq.com>
2018-10-05 11:25:51 -0400

    make install script compatible

--- scripts/install-vcpkgs.ps1
+++ scripts/install-vcpkgs.ps1
@@ -1,13 +1,17 @@
 $solutionRoot = [IO.Path]::GetFullPath(([IO.Path]::Combine($PSScriptRoot, "..")))
 $workspaceRoot = [IO.Path]::GetFullPath(([IO.Path]::Combine($solutionRoot, "..")))
 $vcpkgRoot = [IO.Path]::Combine($workspaceRoot, "vcpkg")
-$vcpkgExe = [IO.Path]::Combine($vcpkgRoot, "vcpkg.exe")
+if ($IsWindows) {
+    $vcpkgExe = [IO.Path]::Combine($vcpkgRoot, "vcpkg.exe")
+} else {
+    $vcpkgExe = [IO.Path]::Combine($vcpkgRoot, "vcpkg")
+}
 
 
 function Run {
     param(
-        [Parameter(Position=0)][string] $Name, 
-        [Parameter(Position=1)][String[]] $Args, 
+        [Parameter(Position=0)][string] $Name,
+        [Parameter(Position=1)][String[]] $Args,
         [Parameter(Position=2)][string] $WorkingDirectory
     )
     Write-Host "running $($Name) $($Args)"
@@ -33,20 +37,40 @@ if (Test-Path $vcpkgExe) {
     Write-Host "vcpkg.exe found"
 } else {
     Write-Host "vcpkg.exe not found, bootstrapping"
-    $p = Run "cmd" "/c","bootstrap-vcpkg.bat" $vcpkgRoot
-    if ($p.ExitCode -ne 0) {
-        Write-Host "failed to bootstrap vcpkg: $($p.ExitCode)"
-        Exit 1
+    if ($IsWindows) {
+        $p = Run "cmd" "/c","bootstrap-vcpkg.bat" $vcpkgRoot
+        if ($p.ExitCode -ne 0) {
+            Write-Host "failed to bootstrap vcpkg: $($p.ExitCode)"
+            Exit 1
+        }
+    } else {
+        $p = Run "sh" "bootstrap-vcpkg.sh" $vcpkgRoot
+        if ($p.ExitCode -ne 0) {
+            Write-Host "failed to bootstrap vcpkg: $($p.ExitCode)"
+            Exit 1
+        }
+
     }
 }
 
 $packages = @("nlohmann-json")
-$platforms = @("x86", "x64")
+if ($IsWindows) {
+    $platforms = @("x86", "x64")
+} else {
+    $platforms = @("x64")
+}
+if ($IsWindows) {
+    $os = "windows-static"
+} elseif ($IsMacOS) {
+    $os = "osx"
+} else {
+    $os = "linux"
+}
 
 foreach ($platform in $platforms) {
     foreach ($package in $packages) {
-        Write-Host "installing $($package):$($platform)-windows-static"
-        $p = Run $vcpkgExe "install","$($package):$($platform)-windows-static" $vcpkgRoot
+        Write-Host "installing $($package):$($platform)-$($os)"
+        $p = Run $vcpkgExe "install","$($package):$($platform)-$($os)" $vcpkgRoot
         if ($p.ExitCode -ne 0) {
             Write-Host "failed to install $($package):$($platform): $($p.ExitCode)"
             Exit 1
