373cdae511ce877b84accaeef942631ea30e9d5f
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-10-09 10:03:25 -0400

    add integration tests for ASP.NET Core MVC 2

---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetCoreMvc2Tests.cs
@@ -0,0 +1,92 @@
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class AspNetCoreMvc2Tests : TestHelper
+    {
+        private const int AgentPort = 59000;
+        private const int Port = 59001;
+
+        public AspNetCoreMvc2Tests(ITestOutputHelper output)
+            : base("AspNetCoreMvc2", output)
+        {
+        }
+
+        [Theory]
+        [Trait("Category", "EndToEnd")]
+        [InlineData("/")]
+        [InlineData("/api/delay/0")]
+        public void SubmitsTracesSelfHosted(string path)
+        {
+            using (var agent = new MockTracerAgent(AgentPort))
+            using (Process process = StartSample(AgentPort))
+            {
+                var wh = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+                process.OutputDataReceived += (sender, args) =>
+                                              {
+                                                  if (args.Data?.Contains("Now listening on:") == true)
+                                                  {
+                                                      wh.Set();
+                                                  }
+
+                                                  Output.WriteLine($"[webserver][stdout] {args.Data}");
+                                              };
+                process.BeginOutputReadLine();
+
+                process.ErrorDataReceived += (sender, args) =>
+                                             {
+                                                 Output.WriteLine($"[webserver][stderr] {args.Data}");
+                                             };
+                process.BeginErrorReadLine();
+
+                // wait for server to start
+                wh.WaitOne(5000);
+
+                try
+                {
+                    var request = WebRequest.Create($"http://localhost:{Port}{path}");
+                    using (var response = (HttpWebResponse)request.GetResponse())
+                    using (var stream = response.GetResponseStream())
+                    using (var reader = new StreamReader(stream))
+                    {
+                        Output.WriteLine($"[http] {response.StatusCode} {reader.ReadToEnd()}");
+                    }
+                }
+                catch (WebException wex)
+                {
+                    Output.WriteLine($"[http] exception: {wex}");
+                    if (wex.Response is HttpWebResponse response)
+                    {
+                        using (var stream = response.GetResponseStream())
+                        using (var reader = new StreamReader(stream))
+                        {
+                            Output.WriteLine($"[http] {response.StatusCode} {reader.ReadToEnd()}");
+                        }
+                    }
+                }
+
+                var spans = agent.WaitForSpans(1);
+                Assert.True(spans.Count > 0, "expected at least one span");
+
+                foreach (var span in spans)
+                {
+                    Assert.Equal(Integrations.AspNetWebApi2Integration.OperationName, span.Name);
+                    Assert.Equal(SpanTypes.Web, span.Type);
+                    Assert.Equal($"GET {path}", span.Resource);
+                }
+
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
