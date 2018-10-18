b03ab9704ce8b6b25b1774820c49a3d1c9e79296
Caleb Doxsey <caleb@datadoghq.com>
2018-10-18 11:50:51 -0400

    fix tests

---
+++ docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+$DIR/with-profiler-logs.bash \
+    wait-for-it redis:6379 -- \
+    wait-for-it elasticsearch:9200 -- \
+    wait-for-it sqlserver:1433 -- \
+    dotnet test --verbosity minimal $DIR/../test/Datadog.Trace.ClrProfiler.IntegrationTests/Datadog.Trace.ClrProfiler.IntegrationTests.csproj
---
+++ docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cd "$DIR/.."
+
+cd src/Datadog.Trace.ClrProfiler.Native
+mkdir -p obj/Debug/x64
+(cd obj/Debug/x64 && cmake ../../.. && make)
+
+mkdir -p bin/Debug/x64
+cp -f obj/Debug/x64/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/Datadog.Trace.ClrProfiler.Native.so
