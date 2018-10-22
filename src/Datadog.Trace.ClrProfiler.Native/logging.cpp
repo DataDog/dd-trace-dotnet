ebc1c81b0a443181fc03696a87ee4f3e38b19326
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-10-22 17:41:37 -0400

    add env variable that can disable logging, use separate path from Agent, add process id to filename

--- src/Datadog.Trace.ClrProfiler.Native/logging.cpp
+++ src/Datadog.Trace.ClrProfiler.Native/logging.cpp
@@ -1,5 +1,5 @@
-#include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/null_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 #include <filesystem>
 
 #include "logging.h"
@@ -14,21 +14,31 @@ std::shared_ptr<spdlog::logger> GetLogger() {
     std::unique_lock<std::mutex> lck(mtx);
 
     try {
-      auto programdata = GetEnvironmentValue(L"PROGRAMDATA");
-      if (programdata.empty()) {
-        programdata = L"C:\\ProgramData";
-      }
-      auto path = std::filesystem::path(programdata)
-                      .append("Datadog")
-                      .append("logs")
-                      .append("dotnet-profiler.log");
+      const auto logger_enabled =
+          GetEnvironmentValue(L"DD_DOTNET_TRACER_LOG_ENABLED");
 
-      if (!std::filesystem::exists(path.parent_path())) {
-        std::filesystem::create_directories(path.parent_path());
-      }
-      logger = spdlog::rotating_logger_mt("dotnet-profiler", path.string(),
-                                          1024 * 1024 * 5, 3);
+      if (logger_enabled == L"0") {
+        // disable logging, useful if the logger itself is crashing
+        logger = spdlog::create<spdlog::sinks::null_sink_st>("dotnet-profiler");
+      } else {
+        auto programdata = GetEnvironmentValue(L"PROGRAMDATA");
+        if (programdata.empty()) {
+          programdata = L"C:\\ProgramData";
+        }
 
+        DWORD process_id = GetCurrentProcessId();
+
+        auto path = std::filesystem::path(programdata)
+                        .append("Datadog .NET Tracer")
+                        .append("logs")
+                        .append("dotnet-profiler-" + std::to_string(process_id) + ".log");
+
+        if (!std::filesystem::exists(path.parent_path())) {
+          std::filesystem::create_directories(path.parent_path());
+        }
+        logger = spdlog::rotating_logger_mt("dotnet-profiler", path.string(),
+                                            1024 * 1024 * 5, 3);
+      }
     } catch (...) {
       logger = spdlog::create<spdlog::sinks::null_sink_st>("dotnet-profiler");
     }
