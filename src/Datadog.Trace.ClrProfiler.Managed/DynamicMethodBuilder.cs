0f5bf97a9d94df640d864e4f1869da19e2bdb186
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-10-25 17:15:23 -0400

    use `Sigil` NuGet package to validate emitted IL

--- src/Datadog.Trace.ClrProfiler.Managed/DynamicMethodBuilder.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/DynamicMethodBuilder.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using Sigil;
 
 namespace Datadog.Trace.ClrProfiler
 {
@@ -12,9 +13,9 @@ namespace Datadog.Trace.ClrProfiler
     /// </summary>
     /// <typeparam name="TDelegate">The type of delegate</typeparam>
     public static class DynamicMethodBuilder<TDelegate>
-            where TDelegate : Delegate
+        where TDelegate : Delegate
     {
-        private static ConcurrentDictionary<Key, TDelegate> _cached = new ConcurrentDictionary<Key, TDelegate>(new KeyComparer());
+        private static readonly ConcurrentDictionary<Key, TDelegate> _cached = new ConcurrentDictionary<Key, TDelegate>(new KeyComparer());
 
         /// <summary>
         /// Memoizes CreateMethodCallDelegate
@@ -31,21 +32,22 @@ namespace Datadog.Trace.ClrProfiler
             Type[] methodGenericArguments = null)
         {
             return _cached.GetOrAdd(
-                new Key(type, methodName, methodParameterTypes, methodGenericArguments),
-                key =>
-                {
-                    return CreateMethodCallDelegate(key.Type, key.MethodName, key.MethodParameterTypes, key.MethodGenericArguments);
-                });
+                                    new Key(type, methodName, methodParameterTypes, methodGenericArguments),
+                                    key => CreateMethodCallDelegate(
+                                                                    key.Type,
+                                                                    key.MethodName,
+                                                                    key.MethodParameterTypes,
+                                                                    key.MethodGenericArguments));
         }
 
         /// <summary>
         /// Creates a simple <see cref="DynamicMethod"/> using <see cref="System.Reflection.Emit"/> that
-        /// calls a method with the specified name and and parameter types.
+        /// calls a method with the specified name and parameter types.
         /// </summary>
-        /// <param name="type">The <see cref="Type"/> that contains the method.</param>
-        /// <param name="methodName">The name of the method.</param>
-        /// <param name="methodParameterTypes">optional types for the method parameters</param>
-        /// <param name="methodGenericArguments">optional generic type arguments for a generic method</param>
+        /// <param name="type">The <see cref="Type"/> that contains the method to call when the returned delegate is executed..</param>
+        /// <param name="methodName">The name of the method to call when the returned delegate is executed.</param>
+        /// <param name="methodParameterTypes">If not null, use method overload that matches the specified parameters.</param>
+        /// <param name="methodGenericArguments">If not null, use method overload that has the same number of generic arguments.</param>
         /// <returns>A <see cref="Delegate"/> that can be used to execute the dynamic method.</returns>
         public static TDelegate CreateMethodCallDelegate(
             Type type,
@@ -76,38 +78,42 @@ namespace Datadog.Trace.ClrProfiler
             }
 
             // find any method that matches by name and parameter types
-            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-            methods = methods.Where(m => m.Name == methodName).ToArray();
+            IEnumerable<MethodInfo> methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                                                  .Where(m => m.Name == methodName);
+
+            // if methodParameterTypes was specified, check for a method that matches
             if (methodParameterTypes != null)
             {
                 methods = methods.Where(m =>
-                {
-                    var ps = m.GetParameters();
-                    if (ps.Length != methodParameterTypes.Length)
-                    {
-                        return false;
-                    }
+                                        {
+                                            var ps = m.GetParameters();
+                                            if (ps.Length != methodParameterTypes.Length)
+                                            {
+                                                return false;
+                                            }
 
-                    for (var i = 0; i < ps.Length; i++)
-                    {
-                        var t1 = ps[i].ParameterType;
-                        var t2 = methodParameterTypes[i];
+                                            for (var i = 0; i < ps.Length; i++)
+                                            {
+                                                var t1 = ps[i].ParameterType;
+                                                var t2 = methodParameterTypes[i];
 
-                        // generics can be tricky to compare for type equality
-                        // so we will just check the namespace and name
-                        if (t1.Namespace != t2.Namespace || t1.Name != t2.Name)
-                        {
-                            return false;
-                        }
-                    }
+                                                // generics can be tricky to compare for type equality
+                                                // so we will just check the namespace and name
+                                                if (t1.Namespace != t2.Namespace || t1.Name != t2.Name)
+                                                {
+                                                    return false;
+                                                }
+                                            }
 
-                    return true;
-                }).ToArray();
+                                            return true;
+                                        });
             }
 
             if (methodGenericArguments != null)
             {
-                methods = methods.Where(m => m.IsGenericMethodDefinition).ToArray();
+                methods = methods.Where(m => m.IsGenericMethodDefinition &&
+                                             m.GetGenericArguments().Length == methodGenericArguments.Length)
+                                 .ToArray();
             }
 
             MethodInfo methodInfo = methods.FirstOrDefault();
@@ -123,41 +129,53 @@ namespace Datadog.Trace.ClrProfiler
                 methodInfo = methodInfo.MakeGenericMethod(methodGenericArguments);
             }
 
-            var dynamicMethod = new DynamicMethod(methodName, returnType, parameterTypes, type);
-            ILGenerator ilGenerator = dynamicMethod.GetILGenerator();
+            Type[] effectiveParameterTypes;
 
-            for (int argumentIndex = 0; argumentIndex < parameterTypes.Length; argumentIndex++)
+            IEnumerable<Type> reflectedParameterTypes = methodInfo.GetParameters()
+                                                                  .Select(p => p.ParameterType);
+            if (methodInfo.IsStatic)
             {
-                if (argumentIndex == 0)
+                effectiveParameterTypes = reflectedParameterTypes.ToArray();
+            }
+            else
+            {
+                // for instance methods, insert object's type as first element in array
+                effectiveParameterTypes = new[] { type }
+                                         .Concat(reflectedParameterTypes)
+                                         .ToArray();
+            }
+
+            Emit<TDelegate> dynamicMethod = Emit<TDelegate>.NewDynamicMethod(methodInfo.Name);
+
+            // load each argument and cast or unbox as necessary
+            for (ushort argumentIndex = 0; argumentIndex < parameterTypes.Length; argumentIndex++)
+            {
+                Type delegateParameterType = parameterTypes[argumentIndex];
+                Type underlyingParameterType = effectiveParameterTypes[argumentIndex];
+
+                dynamicMethod.LoadArgument(argumentIndex);
+
+                if (underlyingParameterType.IsValueType && delegateParameterType == typeof(object))
                 {
-                    ilGenerator.Emit(OpCodes.Ldarg_0);
+                    dynamicMethod.UnboxAny(underlyingParameterType);
                 }
-                else if (argumentIndex == 1)
+                else if (underlyingParameterType != delegateParameterType)
                 {
-                    ilGenerator.Emit(OpCodes.Ldarg_1);
-                }
-                else if (argumentIndex == 2)
-                {
-                    ilGenerator.Emit(OpCodes.Ldarg_2);
-                }
-                else if (argumentIndex == 3)
-                {
-                    ilGenerator.Emit(OpCodes.Ldarg_3);
-                }
-                else if (argumentIndex < 256)
-                {
-                    ilGenerator.Emit(OpCodes.Ldarg_S, (byte)argumentIndex);
-                }
-                else
-                {
-                    ilGenerator.Emit(OpCodes.Ldarg, argumentIndex);
+                    dynamicMethod.CastClass(underlyingParameterType);
                 }
             }
 
-            ilGenerator.Emit(methodInfo.IsVirtual ? OpCodes.Callvirt : OpCodes.Call, methodInfo);
-            ilGenerator.Emit(OpCodes.Ret);
+            if (methodInfo.IsVirtual)
+            {
+                dynamicMethod.CallVirtual(methodInfo);
+            }
+            else
+            {
+                dynamicMethod.Call(methodInfo);
+            }
 
-            return (TDelegate)dynamicMethod.CreateDelegate(delegateType);
+            dynamicMethod.Return();
+            return dynamicMethod.CreateDelegate();
         }
 
         private struct Key
