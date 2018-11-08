de0f585ec67fd360913aa34907890482cf0ea530
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-11-07 20:46:33 -0500

    add helper method `GetProfilerEnvironmentVariables()` to help troubleshoot profiler issues

--- src/Datadog.Trace.ClrProfiler.Managed/Instrumentation.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Instrumentation.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 // [assembly: System.Security.SecurityCritical]
@@ -36,5 +39,24 @@ namespace Datadog.Trace.ClrProfiler
         ///   <c>true</c> if the profiler is currently attached; <c>false</c> otherwise.
         /// </value>
         public static bool ProfilerAttached => _profilerAttached.Value;
+
+        /// <summary>
+        /// Gets environment variables related to Datadog's profiler, filtered by prefix.
+        /// </summary>
+        /// <returns>Environment variables related to Datadog's profiler.</returns>
+        public static IEnumerable<KeyValuePair<string, string>> GetProfilerEnvironmentVariables()
+        {
+            var prefixes = new[] { "COR_", "CORECLR_", "DD_", "DATADOG_" };
+
+            var envVars = from envVar in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
+                          from prefix in prefixes
+                          let key = (envVar.Key as string)?.ToUpperInvariant()
+                          let value = envVar.Value as string
+                          where key.StartsWith(prefix)
+                          orderby key
+                          select new KeyValuePair<string, string>(key, value);
+
+            return envVars;
+        }
     }
 }
