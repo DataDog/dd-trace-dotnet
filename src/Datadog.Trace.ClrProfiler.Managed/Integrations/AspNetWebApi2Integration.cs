d421aa266a425a0b2ba856ee66f42764f8595e18
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-11-15 12:43:12 -0500

    use async/awaot instead of `Span.Trace()` in Web API integration

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/AspNetWebApi2Integration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/AspNetWebApi2Integration.cs
@@ -19,13 +19,26 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         /// <summary>
         /// Calls the underlying ExecuteAsync and traces the request.
         /// </summary>
-        /// <param name="this">The Api Controller</param>
+        /// <param name="apiController">The Api Controller</param>
         /// <param name="controllerContext">The controller context for the call</param>
         /// <param name="cancellationTokenSource">The cancellation token source</param>
         /// <returns>A task with the result</returns>
-        public static object ExecuteAsync(object @this, object controllerContext, object cancellationTokenSource)
+        public static object ExecuteAsync(object apiController, object controllerContext, object cancellationTokenSource)
         {
-            Type controllerType = @this.GetType();
+            var cancellationToken = ((CancellationTokenSource)cancellationTokenSource).Token;
+            return ExecuteAsyncInternal(apiController, controllerContext, cancellationToken);
+        }
+
+        /// <summary>
+        /// Calls the underlying ExecuteAsync and traces the request.
+        /// </summary>
+        /// <param name="apiController">The Api Controller</param>
+        /// <param name="controllerContext">The controller context for the call</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <returns>A task with the result</returns>
+        private static async Task<HttpResponseMessage> ExecuteAsyncInternal(object apiController, object controllerContext, CancellationToken cancellationToken)
+        {
+            Type controllerType = apiController.GetType();
 
             // in some cases, ExecuteAsync() is an explicit interface implementation,
             // which is not public and has a different name, so try both
@@ -37,29 +50,26 @@ namespace Datadog.Trace.ClrProfiler.Integrations
 
             using (Scope scope = CreateScope(controllerContext))
             {
-                return scope.Span.Trace(
-                                        () =>
-                                        {
-                                            CancellationToken cancellationToken = ((CancellationTokenSource)cancellationTokenSource).Token;
-                                            return executeAsyncFunc(@this, controllerContext, cancellationToken);
-                                        },
-                                        onComplete: e =>
-                                        {
-                                            if (e != null)
-                                            {
-                                                scope.Span.SetException(e);
-                                            }
+                try
+                {
+                    var responseMessage = await executeAsyncFunc(apiController, controllerContext, cancellationToken).ConfigureAwait(false);
 
-                                            // some fields aren't set till after execution, so repopulate anything missing
-                                            UpdateSpan(controllerContext, scope.Span);
-                                            scope.Span.Finish();
-                                        });
+                    // some fields aren't set till after execution, so populate anything missing
+                    UpdateSpan(controllerContext, scope.Span);
+
+                    return responseMessage;
+                }
+                catch (Exception ex)
+                {
+                    scope.Span.SetException(ex);
+                    throw;
+                }
             }
         }
 
         private static Scope CreateScope(object controllerContext)
         {
-            var scope = Tracer.Instance.StartActive(OperationName, finishOnClose: false);
+            var scope = Tracer.Instance.StartActive(OperationName);
             UpdateSpan(controllerContext, scope.Span);
             return scope;
         }
