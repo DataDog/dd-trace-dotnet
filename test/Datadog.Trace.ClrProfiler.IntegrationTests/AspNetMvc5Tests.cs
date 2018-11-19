f8761d30ce88f9e29f0d81d7b46652b4c5760885
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-11-19 13:06:24 -0500

    use `HttpClient` instead of deprecated `WebRequest` or `WebClient`

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetMvc5Tests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetMvc5Tests.cs
@@ -1,7 +1,7 @@
 #if NET461
 
-using System.IO;
-using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Datadog.Trace.TestHelpers;
 using Xunit;
 using Xunit.Abstractions;
@@ -20,7 +20,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
         [Fact]
         [Trait("Category", "EndToEnd")]
-        public void SubmitsTraces()
+        public async Task SubmitsTraces()
         {
             using (var agent = new MockTracerAgent(AgentPort))
             {
@@ -29,32 +29,17 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                     // give IIS Express time to boot up
                     await Task.Delay(2000);
 
-                    try
-                    {
-                        var request = WebRequest.Create($"http://localhost:{Port}/api/environment");
-                        using (var response = (HttpWebResponse)request.GetResponse())
-                        using (var stream = response.GetResponseStream())
-                        using (var reader = new StreamReader(stream))
-                        {
-                            Output.WriteLine($"[http] {response.StatusCode} {reader.ReadToEnd()}");
-                        }
-                    }
-                    catch (WebException wex)
-                    {
-                        Output.WriteLine($"[http] exception: {wex}");
-                        if (wex.Response is HttpWebResponse response)
-                        {
-                            using (var stream = response.GetResponseStream())
-                            using (var reader = new StreamReader(stream))
-                            {
-                                Output.WriteLine($"[http] {response.StatusCode} {reader.ReadToEnd()}");
-                            }
-                        }
-                    }
+                    var httpClient = new HttpClient();
+                    HttpResponseMessage response = await httpClient.GetAsync($"http://localhost:{Port}/api/environment");
+                    response.EnsureSuccessStatusCode();
+
+                    string content = await response.Content.ReadAsStringAsync();
+                    Output.WriteLine($"[http] {response.StatusCode} {content}");
                 }
 
                 var spans = agent.WaitForSpans(1);
                 Assert.True(spans.Count > 0, "expected at least one span");
+
                 foreach (var span in spans)
                 {
                     Assert.Equal(Integrations.AspNetWebApi2Integration.OperationName, span.Name);
