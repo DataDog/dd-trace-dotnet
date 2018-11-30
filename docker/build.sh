0f591f33ea6e1ad6107ea95b3ff78f88297a5b4e
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-11-30 13:10:39 -0500

    fix target version when running integration tests on Linux with docker compose

--- docker/build.sh
+++ docker/build.sh
@@ -6,12 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR/.."
 
 for config in Debug Release ; do
-    for proj in Datadog.Trace Datadog.Trace.OpenTracing ; do
+    for proj in Datadog.Trace Datadog.Trace.ClrProfiler.Managed Datadog.Trace Datadog.Trace.OpenTracing ; do
         dotnet publish -f netstandard2.0 -c $config src/$proj/$proj.csproj
-    done
-
-    for proj in Datadog.Trace.ClrProfiler.Managed ; do
-        dotnet publish -f netcoreapp2.0 -c $config src/$proj/$proj.csproj
     done
 
     for sample in Samples.AspNetCoreMvc2 Samples.ConsoleCore Samples.Elasticsearch Samples.RedisCore Samples.SqlServer ; do
