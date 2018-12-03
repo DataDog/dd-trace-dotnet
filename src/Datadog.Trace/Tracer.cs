fa22c9708738d32e1c30fa04bbcf34bc04507b4f
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-12-03 15:17:48 -0500

    define constant `Tags.Env`

--- src/Datadog.Trace/Tracer.cs
+++ src/Datadog.Trace/Tracer.cs
@@ -153,7 +153,7 @@ namespace Datadog.Trace
             // automatically the "env" tag if environment variable is defined
             if (!string.IsNullOrWhiteSpace(env))
             {
-                span.SetTag("env", env);
+                span.SetTag(Tags.Env, env);
             }
 
             span.TraceContext.AddSpan(span);
