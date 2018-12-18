1b10fede94b129f92a1081436ba2388648dfb52a
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-12-18 14:01:36 -0500

    be more defensive when trying to access properties and fields, add tag for MongoDB collection

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/MongoDbIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/MongoDbIntegration.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading;
 
@@ -25,18 +26,18 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         public static object Execute(object wireProtocol, object connection, object cancellationTokenSource)
         {
             // TResult MongoDB.Driver.Core.WireProtocol.IWireProtocol<TResult>.Execute(IConnection connection, CancellationToken cancellationToken)
-            var cancellationToken = (cancellationTokenSource as CancellationTokenSource)?.Token ?? CancellationToken.None;
-            Type wireProtocolType = wireProtocol.GetType();
+            if (wireProtocol == null) { throw new ArgumentNullException(nameof(wireProtocol)); }
 
-            wireProtocol.TryGetFieldValue("_command", out object command);
-
-            wireProtocol.TryGetFieldValue("_databaseNamespace", out object databaseNamespace);
-            databaseNamespace.TryGetPropertyValue("DatabaseName", out string databaseName);
-
+            string databaseName = null;
             string host = null;
             string port = null;
 
-            if (connection.TryGetPropertyValue("EndPoint", out object endpoint))
+            if (wireProtocol.TryGetFieldValue("_databaseNamespace", out object databaseNamespace))
+            {
+                databaseNamespace?.TryGetPropertyValue("DatabaseName", out databaseName);
+            }
+
+            if (connection != null && connection.TryGetPropertyValue("EndPoint", out object endpoint))
             {
                 if (endpoint is IPEndPoint ipEndPoint)
                 {
@@ -51,24 +52,36 @@ namespace Datadog.Trace.ClrProfiler.Integrations
             }
 
             string operationName = null;
-            // string collectionName = null;
+            string collectionName = null;
             string query = null;
             string resourceName = null;
 
-            if (command != null)
+            if (wireProtocol.TryGetFieldValue("_command", out object command) && command != null)
             {
                 try
                 {
-                    command.TryCallMethod("GetElement", 0, out object firstElement);
-                    firstElement.TryGetPropertyValue("Name", out operationName);
-                    // firstElement.TryGetPropertyValue("Value", out object collectionNameObj);
-                    // collectionName = (string)collectionNameObj;
+                    // the name of the first element in the command BsonDocument will be the operation type (insert, delete, find, etc)
+                    // and its value is the collection name
+                    if (command.TryCallMethod("GetElement", 0, out object firstElement) && firstElement != null)
+                    {
+                        firstElement.TryGetPropertyValue("Name", out operationName);
 
+                        if (firstElement.TryGetPropertyValue("Value", out object collectionNameObj) && collectionNameObj != null)
+                        {
+                            collectionName = (string)collectionNameObj;
+                        }
+                    }
+
+                    // get the "query" element from the command BsonDocument, if it exists
                     if (command.TryCallMethod("Contains", "query", out bool found) && found)
                     {
-                        command.TryCallMethod("GetElement", "query", out object queryElement);
-                        queryElement.TryGetPropertyValue("Value", out object queryValue);
-                        query = queryValue.ToString();
+                        if (command.TryCallMethod("GetElement", "query", out object queryElement) && queryElement != null)
+                        {
+                            if (queryElement.TryGetPropertyValue("Value", out object queryValue) && queryValue != null)
+                            {
+                                query = queryValue.ToString();
+                            }
+                        }
                     }
                 }
                 catch
@@ -77,7 +90,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
                 }
 
                 string[] parts = { operationName, databaseName, query };
-                resourceName = string.Join(" ", parts);
+                resourceName = string.Join(" ", parts.Where(s => !string.IsNullOrWhiteSpace(s)));
             }
 
             Tracer tracer = Tracer.Instance;
@@ -85,21 +98,22 @@ namespace Datadog.Trace.ClrProfiler.Integrations
 
             var execute = DynamicMethodBuilder<Func<object, object, object, object>>
                .GetOrCreateMethodCallDelegate(
-                    wireProtocolType,
+                    wireProtocol.GetType(),
                     "Execute");
 
             using (var scope = tracer.StartActive(OperationName, serviceName: serviceName))
             {
                 scope.Span.Type = SpanTypes.MongoDB;
                 scope.Span.ResourceName = resourceName;
-                scope.Span.SetTag(Tags.MongoDbQuery, query);
                 scope.Span.SetTag(Tags.DbName, databaseName);
-                // scope.Span.SetTag("mongodb.collection", collectionName);
+                scope.Span.SetTag(Tags.MongoDbQuery, query);
+                scope.Span.SetTag(Tags.MongoDbCollection, collectionName);
                 scope.Span.SetTag(Tags.OutHost, host);
                 scope.Span.SetTag(Tags.OutPort, port);
 
                 try
                 {
+                    var cancellationToken = (cancellationTokenSource as CancellationTokenSource)?.Token ?? CancellationToken.None;
                     return execute(wireProtocol, connection, cancellationToken);
                 }
                 catch (Exception ex)
