d7314ec373741ef6b82e60c2009f07cb753b37a4
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-12-18 15:56:14 -0500

    moved all environment variable names to a single file and add new ones

---
+++ src/Datadog.Trace.ClrProfiler.Native/environment_variables.h
@@ -0,0 +1,28 @@
+#ifndef DD_CLR_PROFILER_ENVIRONMENT_VARIABLES_H_
+#define DD_CLR_PROFILER_ENVIRONMENT_VARIABLES_H_
+
+#include "string.h"  // NOLINT
+
+namespace trace::environment {
+
+const WSTRING tracing_enabled = "DD_TRACE_ENABLED"_W;
+
+const WSTRING debug_enabled = "DD_TRACE_DEBUG"_W;
+
+const WSTRING integrations_path = "DD_INTEGRATIONS"_W;
+
+const WSTRING process_names = "DD_PROFILER_PROCESSES"_W;
+
+const WSTRING agent_host = "DD_AGENT_HOST"_W;
+
+const WSTRING agent_port = "DD_TRACE_AGENT_PORT"_W;
+
+const WSTRING env = "DD_ENV"_W;
+
+const WSTRING service_name = "DD_SERVICE_NAME"_W;
+
+const WSTRING disabled_integrations = "DD_DISABLED_INTEGRATIONS"_W;
+
+}  // namespace trace::environment
+
+#endif
