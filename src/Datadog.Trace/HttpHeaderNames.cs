813d580123eada6c2cdf25159265aa39b3724b12
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-01-31 13:21:27 -0500

    add http header consant for sampling priority

--- src/Datadog.Trace/HttpHeaderNames.cs
+++ src/Datadog.Trace/HttpHeaderNames.cs
@@ -1,8 +1,9 @@
-﻿namespace Datadog.Trace
+namespace Datadog.Trace
 {
     internal static class HttpHeaderNames
     {
         public const string HttpHeaderTraceId = "x-datadog-trace-id";
         public const string HttpHeaderParentId = "x-datadog-parent-id";
+        public const string HttpHeaderSamplingPriority = "x-datadog-sampling-priority";
     }
 }
