a95a445d10951d42389e3f1b6af8341536852b90
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-02-08 13:39:44 -0500

    add string.TrimEnd(string) extension method and unit tests

--- src/Datadog.Trace/ExtensionMethods/StringExtensions.cs
+++ src/Datadog.Trace/ExtensionMethods/StringExtensions.cs
@@ -5,8 +5,25 @@ namespace Datadog.Trace.ExtensionMethods
 {
     internal static class StringExtensions
     {
+        /// <summary>
+        /// Removes the trailing occurrence of a substring from the current string.
+        /// </summary>
+        /// <param name="value">The original string.</param>
+        /// <param name="suffix">The string to remove from the end of <paramref name="value"/>.</param>
+        /// <returns>A new string with <paramref name="suffix"/> removed from the end, if found. Otherwise, <paramref name="value"/>.</returns>
+        public static string TrimEnd(this string value, string suffix)
+        {
+            if (value == null) { throw new ArgumentNullException(nameof(value)); }
+
+            return !string.IsNullOrEmpty(suffix) && value.EndsWith(suffix)
+                       ? value.Substring(0, value.Length - suffix.Length)
+                       : value;
+        }
+
         public static ulong? TryParseUInt64(this string value)
         {
+            if (value == null) { throw new ArgumentNullException(nameof(value)); }
+
             return ulong.TryParse(value, out var result)
                        ? result
                        : default;
@@ -14,6 +31,8 @@ namespace Datadog.Trace.ExtensionMethods
 
         public static ulong? TryParseUInt64(this string value, NumberStyles style, IFormatProvider provider)
         {
+            if (value == null) { throw new ArgumentNullException(nameof(value)); }
+
             return ulong.TryParse(value, style, provider, out var result)
                        ? result
                        : default;
