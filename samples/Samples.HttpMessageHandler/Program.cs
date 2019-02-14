a74fed982cf50506458a956999a12217ef29f4f7
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-02-14 17:35:55 -0500

    remove System.Reactive dependency so we can test in net45

--- samples/Samples.HttpMessageHandler/Program.cs
+++ samples/Samples.HttpMessageHandler/Program.cs
@@ -2,8 +2,8 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Samples.HttpMessageHandler
@@ -23,22 +23,18 @@ namespace Samples.HttpMessageHandler
                 listener.Prefixes.Add(Url);
                 listener.Start();
 
+                // handle http requests in a background thread
+                var listenerThread = new Thread(HandleHttpRequests);
+                listenerThread.Start(listener);
+
                 if (args.Length == 0 || args[0].Equals("HttpClient", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    // subscribe to http listener event
-                    Observable.FromAsync(listener.GetContextAsync)
-                              .Subscribe(async context => await HandleHttpRequest(context));
-
                     // send an http request using HttpClient
                     SendHttpClientRequest().GetAwaiter().GetResult();
                 }
 
                 if (args.Length == 0 || args[0].Equals("WebClient", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    // subscribe to http listener event
-                    Observable.FromAsync(listener.GetContextAsync)
-                              .Subscribe(async context => await HandleHttpRequest(context));
-
                     // send an http request using WebClient
                     SendWebClientRequest();
                 }
@@ -87,35 +83,50 @@ namespace Samples.HttpMessageHandler
             }
         }
 
-        private static async Task HandleHttpRequest(HttpListenerContext context)
+        private static void HandleHttpRequests(object state)
         {
-            Console.WriteLine("[HttpListener] received request");
+            var listener = (HttpListener)state;
 
-            // read request content and headers
-            using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+            while (listener.IsListening)
             {
-                string requestContent = await reader.ReadToEndAsync();
-                Console.WriteLine($"[HttpListener] request content: {requestContent}");
-
-                foreach (string headerName in context.Request.Headers)
+                try
                 {
-                    string headerValue = context.Request.Headers[headerName];
-                    Console.WriteLine($"[HttpListener] request header: {headerName}={headerValue}");
+                    var context = listener.GetContext();
+
+                    Console.WriteLine("[HttpListener] received request");
+
+                    // read request content and headers
+                    using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                    {
+                        string requestContent = reader.ReadToEnd();
+                        Console.WriteLine($"[HttpListener] request content: {requestContent}");
+
+                        foreach (string headerName in context.Request.Headers)
+                        {
+                            string headerValue = context.Request.Headers[headerName];
+                            Console.WriteLine($"[HttpListener] request header: {headerName}={headerValue}");
+                        }
+                    }
+
+                    var delay = TimeSpan.FromSeconds(1);
+                    Console.WriteLine($"[HttpListener] Faking it for {delay}");
+                    Thread.Sleep(delay);
+
+                    // write response content
+                    byte[] responseBytes = Utf8.GetBytes(ResponseContent);
+                    context.Response.ContentEncoding = Utf8;
+                    context.Response.ContentLength64 = responseBytes.Length;
+                    context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+
+                    // we must close the response
+                    context.Response.Close();
+                }
+                catch (HttpListenerException)
+                {
+                    // listener was stopped,
+                    // ignore to let the loop end and the method return
                 }
             }
-
-            var delay = TimeSpan.FromSeconds(1);
-            Console.WriteLine($"[HttpListener] Faking it for {delay}");
-            await Task.Delay(delay);
-
-            // write response content
-            byte[] responseBytes = Utf8.GetBytes(ResponseContent);
-            context.Response.ContentEncoding = Utf8;
-            context.Response.ContentLength64 = responseBytes.Length;
-            await context.Response.OutputStream.WriteAsync(responseBytes, 0, responseBytes.Length);
-
-            // we must close the response
-            context.Response.Close();
         }
     }
 }
