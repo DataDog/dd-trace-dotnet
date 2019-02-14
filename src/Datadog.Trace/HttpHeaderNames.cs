1f6675769bbccad1dc4133abb9b8961defc68e7e
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-02-14 15:25:11 -0500

    add http header comments

--- src/Datadog.Trace/HttpHeaderNames.cs
+++ src/Datadog.Trace/HttpHeaderNames.cs
@@ -1,14 +1,45 @@
+using System.Runtime.InteropServices;
+
 namespace Datadog.Trace
 {
     internal static class HttpHeaderNames
     {
+        /// <summary>
+        /// The language-specific tracer that generated this span.
+        /// Always ".NET" for the .NET Tracer.
+        /// </summary>
         public const string Language = "Datadog-Meta-Lang";
+
+        /// <summary>
+        /// The interpreter version for the given language, e.g. ".NET Framework 4.7.2" or ".NET Core 2.1".
+        /// The value of <see cref="RuntimeInformation.FrameworkDescription"/>.
+        /// </summary>
         public const string LanguageInterpreter = "Datadog-Meta-Lang-Interpreter";
+
+        /// <summary>
+        /// The version of the tracer that generated this span.
+        /// </summary>
         public const string TracerVersion = "Datadog-Meta-Tracer-Version";
 
+        /// <summary>
+        /// ID of a distributed trace.
+        /// </summary>
         public const string TraceId = "x-datadog-trace-id";
+
+        /// <summary>
+        /// ID of the parent span in a distributed trace.
+        /// </summary>
         public const string ParentId = "x-datadog-parent-id";
+
+        /// <summary>
+        /// Setting used to determine whether a trace should be sampled or not.
+        /// </summary>
         public const string SamplingPriority = "x-datadog-sampling-priority";
+
+        /// <summary>
+        /// If header is set to "false", tracing is disabled for that http request.
+        /// Tracing is enabled by default.
+        /// </summary>
         public const string TracingEnabled = "x-datadog-tracing-enabled";
     }
 }
