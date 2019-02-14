29177cfebfee8fa7438d49ed529c888b1fa68d1e
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-02-14 14:55:22 -0500

    clean up code

--- test/Datadog.Trace.TestHelpers/MockTracerAgent.cs
+++ test/Datadog.Trace.TestHelpers/MockTracerAgent.cs
@@ -23,10 +23,7 @@ namespace Datadog.Trace.TestHelpers
             Port = port;
         }
 
-        public int Port
-        {
-            get; private set;
-        }
+        public int Port { get; }
 
         public List<Span> GetSpans()
         {
