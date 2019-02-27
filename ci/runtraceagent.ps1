aefe37048a54d09eef6f5510a84ee89c07d9ea98
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-02-27 10:12:47 -0500

    Fix legacy web samples (#273)
    * move "legacy" ASP.NET projects into their own folder and add an empty `Directory.Build.props` so they don't share with other samples
    
    * enable automatically-generated bindings
    
    * set x64/x86 builds for Samples.AspNetMvc4
    
    * remove unused build and ci files
    
    * remove obsolete reference to projects in benchmarks folder
    
    * build all samples

--- ci/runtraceagent.ps1
+++
@@ -1,3 +0,0 @@
-# This script downloads the 5.19.0 version of the trace-agent and runs it in the background for the integration tests
-Invoke-WebRequest https://github.com/DataDog/datadog-trace-agent/releases/download/5.19.0/trace-agent-windows-5.19.0.exe -OutFile trace-agent-windows.exe
-Start-Process trace-agent-windows.exe "--ddconfig $PSScriptRoot\config.ini"
