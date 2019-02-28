2490a06ddd1e9dfaa281c4f525fb46316338df32
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-02-28 10:20:04 -0500

    assert either the first or last span as needed

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/HttpClientTests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/HttpClientTests.cs
@@ -34,16 +34,15 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 var traceId = GetHeader(processResult.StandardOutput, HttpHeaderNames.TraceId);
                 var parentSpanId = GetHeader(processResult.StandardOutput, HttpHeaderNames.ParentId);
 
-                foreach (var span in spans)
-                {
-                    Assert.Equal("http.request", span.Name);
-                    Assert.Equal("Samples.HttpMessageHandler-http-client", span.Service);
-                    Assert.Equal(SpanTypes.Http, span.Type);
-                    Assert.Equal(nameof(HttpMessageHandler), span.Tags[Tags.InstrumentationName]);
+                var firstSpan = spans.First();
+                Assert.Equal("http.request", firstSpan.Name);
+                Assert.Equal("Samples.HttpMessageHandler-http-client", firstSpan.Service);
+                Assert.Equal(SpanTypes.Http, firstSpan.Type);
+                Assert.Equal(nameof(HttpMessageHandler), firstSpan.Tags[Tags.InstrumentationName]);
 
-                    Assert.Equal(span.TraceId.ToString(CultureInfo.InvariantCulture), traceId);
-                    Assert.Equal(span.SpanId.ToString(CultureInfo.InvariantCulture), parentSpanId);
-                }
+                var lastSpan = spans.Last();
+                Assert.Equal(lastSpan.TraceId.ToString(CultureInfo.InvariantCulture), traceId);
+                Assert.Equal(lastSpan.SpanId.ToString(CultureInfo.InvariantCulture), parentSpanId);
             }
         }
 
@@ -91,16 +90,15 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 var parentSpanId = GetHeader(processResult.StandardOutput, HttpHeaderNames.ParentId);
 
                 // inspect the top-level span, underlying spans can be HttpMessageHandler in .NET Core
-                foreach (var span in spans.Take(1))
-                {
-                    Assert.Equal("http.request", span.Name);
-                    Assert.Equal("Samples.HttpMessageHandler-http-client", span.Service);
-                    Assert.Equal(SpanTypes.Http, span.Type);
-                    Assert.Equal(nameof(WebRequest), span.Tags[Tags.InstrumentationName]);
+                var firstSpan = spans.First();
+                Assert.Equal("http.request", firstSpan.Name);
+                Assert.Equal("Samples.HttpMessageHandler-http-client", firstSpan.Service);
+                Assert.Equal(SpanTypes.Http, firstSpan.Type);
+                Assert.Equal(nameof(WebRequest), firstSpan.Tags[Tags.InstrumentationName]);
 
-                    Assert.Equal(span.TraceId.ToString(CultureInfo.InvariantCulture), traceId);
-                    Assert.Equal(span.SpanId.ToString(CultureInfo.InvariantCulture), parentSpanId);
-                }
+                var lastSpan = spans.Last();
+                Assert.Equal(lastSpan.TraceId.ToString(CultureInfo.InvariantCulture), traceId);
+                Assert.Equal(lastSpan.SpanId.ToString(CultureInfo.InvariantCulture), parentSpanId);
             }
         }
 
