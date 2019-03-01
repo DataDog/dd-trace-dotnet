6af23b21c90bc7937bcc92a98e25d621e99c2328
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-03-01 13:25:40 -0500

    always return the scope, even on exception, so instrumentation can continue and it can be disposed

--- src/Datadog.Trace.ClrProfiler.Managed/ScopeHelper.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/ScopeHelper.cs
@@ -1,24 +1,37 @@
 using System;
 using Datadog.Trace.ExtensionMethods;
+using Datadog.Trace.Logging;
 
 namespace Datadog.Trace.ClrProfiler
 {
     internal static class ScopeHelper
     {
+        private static readonly ILog Log = LogProvider.GetLogger(typeof(ScopeHelper));
+
         public static Scope CreateOutboundHttpScope(string httpMethod, Uri requestUri, Type type)
         {
-            var tracer = Tracer.Instance;
-            string serviceName = $"{tracer.DefaultServiceName}-http-client";
-            string url = requestUri.OriginalString;
+            Scope scope = null;
 
-            var scope = tracer.StartActive("http.request", serviceName: serviceName);
-            var span = scope.Span;
-            span.Type = SpanTypes.Http;
-            span.ResourceName = httpMethod;
-            span.SetTag(Tags.HttpMethod, httpMethod);
-            span.SetTag(Tags.HttpUrl, url);
-            span.SetTag(Tags.InstrumentationName, type.Name.TrimEnd("Integration", StringComparison.OrdinalIgnoreCase));
+            try
+            {
+                var tracer = Tracer.Instance;
+                scope = tracer.StartActive("http.request");
+                var span = scope.Span;
 
+                span.Type = SpanTypes.Http;
+                span.ServiceName = $"{tracer.DefaultServiceName}-http-client";
+                span.ResourceName = httpMethod;
+                span.SetTag(Tags.HttpMethod, httpMethod);
+                span.SetTag(Tags.HttpUrl, requestUri.OriginalString);
+                span.SetTag(Tags.InstrumentationName, type.Name.TrimEnd("Integration", StringComparison.OrdinalIgnoreCase));
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorException("Error creating or populating scope.", ex);
+            }
+
+            // always returns the scope, even if it's null because we couldn't create it,
+            // or we couldn't populate it completely (some tags is better than no tags)
             return scope;
         }
     }
