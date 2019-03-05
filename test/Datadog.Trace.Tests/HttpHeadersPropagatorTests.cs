04f3d324641fa98c740f0ea40c9a1612c9ac00d5
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-03-05 16:58:13 -0500

    add tests to assert we do not propagate if traceId is zero

--- test/Datadog.Trace.Tests/HttpHeadersPropagatorTests.cs
+++ test/Datadog.Trace.Tests/HttpHeadersPropagatorTests.cs
@@ -1,12 +1,17 @@
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.Headers;
 using Xunit;
 
 namespace Datadog.Trace.Tests
 {
+    // TODO: for now, these tests cover all of this,
+    // but we should probably split them up into actual *unit* tests for:
+    // - HttpHeadersCollection wrapper over HttpHeaders (Get, Set, Add, Remove)
+    // - NameValueHeadersCollection wrapper over NameValueCollection (Get, Set, Add, Remove)
+    // - IHeadersCollection.InjectSpanContext() extension method
+    // - IHeadersCollection.ExtractSpanContext() extension method
     public class HttpHeadersPropagatorTests
     {
         [Fact]
@@ -16,10 +21,9 @@ namespace Datadog.Trace.Tests
             const int spanId = 7;
             const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
 
-            HttpRequestHeaders httpHeaders = new HttpRequestMessage().Headers;
+            IHeadersCollection headers = new HttpRequestMessage().Headers.Wrap();
             var context = new SpanContext(traceId, spanId, samplingPriority);
 
-            IHeadersCollection headers = httpHeaders.Wrap();
             headers.InjectSpanContext(context);
             var resultContext = headers.ExtractSpanContext();
 
@@ -29,22 +33,55 @@ namespace Datadog.Trace.Tests
         }
 
         [Fact]
+        public void HttpRequestMessage_InjectExtract_NoContext()
+        {
+            const int traceId = 0;
+            const int spanId = 7;
+            const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
+
+            IHeadersCollection headers = new HttpRequestMessage().Headers.Wrap();
+            var context = new SpanContext(traceId, spanId, samplingPriority);
+
+            headers.InjectSpanContext(context);
+            var resultContext = headers.ExtractSpanContext();
+
+            // traceId 0 should return a null context even if other values are set
+            Assert.Null(resultContext);
+        }
+
+        [Fact]
         public void WebRequest_InjectExtract_Identity()
         {
             const int traceId = 9;
             const int spanId = 7;
             const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
 
-            WebHeaderCollection webHeaders = WebRequest.CreateHttp("http://localhost").Headers;
+            IHeadersCollection headers = WebRequest.CreateHttp("http://localhost").Headers.Wrap();
             var context = new SpanContext(traceId, spanId, samplingPriority);
 
-            IHeadersCollection headers = webHeaders.Wrap();
             headers.InjectSpanContext(context);
             var resultContext = headers.ExtractSpanContext();
 
             Assert.Equal(context.SpanId, resultContext.SpanId);
             Assert.Equal(context.TraceId, resultContext.TraceId);
             Assert.Equal(context.SamplingPriority, resultContext.SamplingPriority);
+        }
+
+        [Fact]
+        public void WebRequest_InjectExtract_NoContext()
+        {
+            const int traceId = 0;
+            const int spanId = 7;
+            const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
+
+            IHeadersCollection headers = WebRequest.CreateHttp("http://localhost").Headers.Wrap();
+            var context = new SpanContext(traceId, spanId, samplingPriority);
+
+            headers.InjectSpanContext(context);
+            var resultContext = headers.ExtractSpanContext();
+
+            // traceId 0 should return a null context even if other values are set
+            Assert.Null(resultContext);
         }
     }
 }
