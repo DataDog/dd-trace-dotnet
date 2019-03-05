35fe26da1239be7ce452cd383c0f17f330f0c58c
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-03-05 17:56:21 -0500

    add separate tests for invalid traceId, spanId, and samplingPriority headers

--- test/Datadog.Trace.Tests/HeadersCollectionTests.cs
+++ test/Datadog.Trace.Tests/HeadersCollectionTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Net;
 using System.Net.Http;
 using Datadog.Trace.ExtensionMethods;
@@ -27,26 +28,10 @@ namespace Datadog.Trace.Tests
             headers.InjectSpanContext(context);
             var resultContext = headers.ExtractSpanContext();
 
+            Assert.NotNull(resultContext);
             Assert.Equal(context.SpanId, resultContext.SpanId);
             Assert.Equal(context.TraceId, resultContext.TraceId);
             Assert.Equal(context.SamplingPriority, resultContext.SamplingPriority);
-        }
-
-        [Fact]
-        public void HttpRequestMessage_InjectExtract_NoContext()
-        {
-            const int traceId = 0;
-            const int spanId = 7;
-            const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
-
-            IHeadersCollection headers = new HttpRequestMessage().Headers.Wrap();
-            var context = new SpanContext(traceId, spanId, samplingPriority);
-
-            headers.InjectSpanContext(context);
-            var resultContext = headers.ExtractSpanContext();
-
-            // traceId 0 should return a null context even if other values are set
-            Assert.Null(resultContext);
         }
 
         [Fact]
@@ -62,26 +47,79 @@ namespace Datadog.Trace.Tests
             headers.InjectSpanContext(context);
             var resultContext = headers.ExtractSpanContext();
 
+            Assert.NotNull(resultContext);
             Assert.Equal(context.SpanId, resultContext.SpanId);
             Assert.Equal(context.TraceId, resultContext.TraceId);
             Assert.Equal(context.SamplingPriority, resultContext.SamplingPriority);
         }
 
-        [Fact]
-        public void WebRequest_InjectExtract_NoContext()
+        [Theory]
+        [InlineData("0")]
+        [InlineData("-1")]
+        [InlineData("trace.id")]
+        public void Extract_InvalidTraceId(string traceId)
         {
-            const int traceId = 0;
-            const int spanId = 7;
-            const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
+            const string spanId = "7";
+            const string samplingPriority = "2";
 
-            IHeadersCollection headers = WebRequest.CreateHttp("http://localhost").Headers.Wrap();
-            var context = new SpanContext(traceId, spanId, samplingPriority);
-
-            headers.InjectSpanContext(context);
+            var headers = InjectContext(traceId, spanId, samplingPriority);
             var resultContext = headers.ExtractSpanContext();
 
-            // traceId 0 should return a null context even if other values are set
+            // invalid traceId should return a null context even if other values are set
             Assert.Null(resultContext);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("-1")]
+        [InlineData("span.id")]
+        public void Extract_InvalidSpanId(string spanId)
+        {
+            const ulong traceId = 9;
+            const SamplingPriority samplingPriority = SamplingPriority.UserKeep;
+
+            var headers = InjectContext(
+                traceId.ToString(CultureInfo.InvariantCulture),
+                spanId,
+                ((int)samplingPriority).ToString(CultureInfo.InvariantCulture));
+
+            var resultContext = headers.ExtractSpanContext();
+
+            Assert.NotNull(resultContext);
+            Assert.Equal(traceId, resultContext.TraceId);
+            Assert.Equal(default(ulong), resultContext.SpanId);
+            Assert.Equal(samplingPriority, resultContext.SamplingPriority);
+        }
+
+        [Theory]
+        [InlineData("-2")]
+        [InlineData("3")]
+        [InlineData("sampling.priority")]
+        public void Extract_InvalidSamplingPriority(string samplingPriority)
+        {
+            const ulong traceId = 9;
+            const ulong spanId = 7;
+
+            var headers = InjectContext(
+                traceId.ToString(CultureInfo.InvariantCulture),
+                spanId.ToString(CultureInfo.InvariantCulture),
+                samplingPriority);
+
+            var resultContext = headers.ExtractSpanContext();
+
+            Assert.NotNull(resultContext);
+            Assert.Equal(traceId, resultContext.TraceId);
+            Assert.Equal(spanId, resultContext.SpanId);
+            Assert.Null(resultContext.SamplingPriority);
+        }
+
+        private static IHeadersCollection InjectContext(string traceId, string spanId, string samplingPriority)
+        {
+            IHeadersCollection headers = new HttpRequestMessage().Headers.Wrap();
+            headers.Add(HttpHeaderNames.TraceId, traceId);
+            headers.Add(HttpHeaderNames.ParentId, spanId);
+            headers.Add(HttpHeaderNames.SamplingPriority, samplingPriority);
+            return headers;
         }
     }
 }
