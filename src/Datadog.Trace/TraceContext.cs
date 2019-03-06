e08baf5769d16198d42c01b94f173c87c9123821
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-03-06 13:37:54 -0500

    clean up code (readonly fields, rename _log, field order)

--- src/Datadog.Trace/TraceContext.cs
+++ src/Datadog.Trace/TraceContext.cs
@@ -1,21 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using Datadog.Trace.Logging;
 
 namespace Datadog.Trace
 {
     internal class TraceContext : ITraceContext
     {
-        private static ILog _log = LogProvider.For<TraceContext>();
+        private static readonly ILog Log = LogProvider.For<TraceContext>();
 
-        private object _lock = new object();
-        private IDatadogTracer _tracer;
-        private List<Span> _spans = new List<Span>();
+        private readonly object _lock = new object();
+        private readonly IDatadogTracer _tracer;
+        private readonly List<Span> _spans = new List<Span>();
+        private readonly DateTimeOffset _start;
+        private readonly Stopwatch _sw;
+
         private int _openSpans = 0;
-        private DateTimeOffset _start;
-        private Stopwatch _sw;
 
         public TraceContext(IDatadogTracer tracer)
         {
