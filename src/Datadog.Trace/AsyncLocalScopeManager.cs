bc9acf254fe3bc8b9a04e20af61af70f7fb62843
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-03-07 11:49:11 -0500

    clean up code

--- src/Datadog.Trace/AsyncLocalScopeManager.cs
+++ src/Datadog.Trace/AsyncLocalScopeManager.cs
@@ -4,36 +4,36 @@ namespace Datadog.Trace
 {
     internal class AsyncLocalScopeManager
     {
-        private static readonly ILog _log = LogProvider.For<AsyncLocalScopeManager>();
+        private static readonly ILog Log = LogProvider.For<AsyncLocalScopeManager>();
 
-        private readonly AsyncLocalCompat<Scope> _currentSpan = new AsyncLocalCompat<Scope>();
+        private readonly AsyncLocalCompat<Scope> _activeScope = new AsyncLocalCompat<Scope>();
 
-        public Scope Active => _currentSpan.Get();
+        public Scope Active => _activeScope.Get();
 
         public Scope Activate(Span span, bool finishOnClose = true)
         {
-            var current = _currentSpan.Get();
-            var scope = new Scope(current, span, this, finishOnClose);
-            _currentSpan.Set(scope);
+            var activeScope = _activeScope.Get();
+            var scope = new Scope(activeScope, span, this, finishOnClose);
+            _activeScope.Set(scope);
             return scope;
         }
 
         public void Close(Scope scope)
         {
-            var current = _currentSpan.Get();
-
-            if (current == null)
-            {
-                _log.Error("Trying to close a null scope.");
-                return;
-            }
+            var current = _activeScope.Get();
 
             if (current != scope)
             {
-                _log.Warn("Specified scope is not the active scope.");
+                Log.Warn("Specified scope is not the active scope.");
             }
 
-            _currentSpan.Set(current.Parent);
+            if (current == null)
+            {
+                Log.Error("Trying to close a null scope.");
+                return;
+            }
+
+            _activeScope.Set(current.Parent);
         }
     }
 }
