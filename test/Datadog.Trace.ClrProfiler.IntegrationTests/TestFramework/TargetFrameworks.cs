97174e3ecbce93b84937802d80429e5b9a229f05
colin-higgins <colin.higgins@datadoghq.com>
2019-04-15 17:38:06 -0400

    remove some scratchpad code

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/TestFramework/TargetFrameworks.cs
+++
@@ -1,9 +0,0 @@
-namespace Datadog.Trace.ClrProfiler.IntegrationTests.TestFramework
-{
-    public static class TargetFrameworks
-    {
-        public const string Net452 = "net452";
-        public const string Net461 = "net461";
-        public const string NetCoreApp21 = "netcoreapp2.1";
-    }
-}
