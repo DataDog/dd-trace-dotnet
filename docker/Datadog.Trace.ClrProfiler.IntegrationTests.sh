14b57915273e86d14e8cac3070dde81d6e30036f
colin-higgins <colin.higgins@datadoghq.com>
2019-04-17 13:36:06 -0400

    Add environment variable with default for mongo sample test

--- docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -7,5 +7,5 @@ $DIR/with-profiler-logs.bash \
     wait-for-it redis:6379 -- \
     wait-for-it elasticsearch:9200 -- \
     wait-for-it sqlserver:1433 -- \
-    wait-for-it mongo:27017 -- \
+    wait-for-it mongosample:27017 -- \
     dotnet test --verbosity minimal $DIR/../test/Datadog.Trace.ClrProfiler.IntegrationTests/Datadog.Trace.ClrProfiler.IntegrationTests.csproj
