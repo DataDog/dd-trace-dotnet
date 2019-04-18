92a3ac5118eb27b47df384785e633ab479f796aa
colin-higgins <colin.higgins@datadoghq.com>
2019-04-17 23:23:44 -0400

    add npg test file

---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/NpgSqlTests.cs
@@ -0,0 +1,36 @@
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class NpgSqlTests : TestHelper
+    {
+        public NpgSqlTests(ITestOutputHelper output)
+            : base("SqlServer", output)
+        {
+        }
+
+        [Fact]
+        [Trait("Category", "EndToEnd")]
+        public void SubmitsTraces()
+        {
+            int agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port))
+            {
+                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
+
+                var spans = agent.WaitForSpans(1);
+                Assert.True(spans.Count > 0, "expected at least one span");
+                foreach (var span in spans)
+                {
+                    Assert.Equal("sql-server.query", span.Name);
+                    Assert.Equal($"Samples.SqlServer-sql-server", span.Service);
+                    Assert.Equal(SpanTypes.Sql, span.Type);
+                }
+            }
+        }
+    }
+}
