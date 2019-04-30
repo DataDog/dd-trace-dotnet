66b074d8da45cf7ac398369fa43e7fb4e08b42ec
colin-higgins <colin.higgins@datadoghq.com>
2019-04-30 10:28:03 -0400

    newline and clarity echo

--- copy-profiler-assemblies.bat
+++ copy-profiler-assemblies.bat
@@ -3,7 +3,7 @@
 REM Makes copies of the profiler assemblies for single project use (relies on post-build-events-cpp.bat)
 REM Other assemblies can test against the profiler without locking the bin of the profiler project
 
-echo PostBuildEvents for %2%
+echo Copy profiler for %2%
 REM echo $(SolutionDir) is %1
 REM echo $(ProjectDir) is %2
 
