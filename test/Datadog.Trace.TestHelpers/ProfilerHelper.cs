599d7205d55bd0a8d674c1bf3fa97e74f688cfe4
colin-higgins <colin.higgins@datadoghq.com>
2019-05-15 16:34:47 -0400

    Delete some more code

--- test/Datadog.Trace.TestHelpers/ProfilerHelper.cs
+++ test/Datadog.Trace.TestHelpers/ProfilerHelper.cs
@@ -30,7 +30,7 @@ namespace Datadog.Trace.TestHelpers
             var applicationPath = environmentHelper.GetSampleApplicationPath();
 
             // clear all relevant environment variables to start with a clean slate
-            ClearProfilerEnvironmentVariables();
+            EnvironmentHelper.ClearProfilerEnvironmentVariables();
 
             ProcessStartInfo startInfo;
 
@@ -48,8 +48,6 @@ namespace Datadog.Trace.TestHelpers
                 environmentHelper.SetEnvironmentVariableDefaults(traceAgentPort, executableFileName, startInfo.EnvironmentVariables);
             }
 
-            SetSharedEnvironmentVariables(traceAgentPort, startInfo.EnvironmentVariables, integrationPaths);
-
             startInfo.UseShellExecute = false;
             startInfo.CreateNoWindow = true;
             startInfo.RedirectStandardOutput = true;
@@ -57,55 +55,6 @@ namespace Datadog.Trace.TestHelpers
             startInfo.RedirectStandardInput = redirectStandardInput;
 
             return Process.Start(startInfo);
-        }
-
-        public static void SetSharedEnvironmentVariables(int traceAgentPort, StringDictionary environmentVariables, IEnumerable<string> integrationPaths)
-        {
-            string integrations = string.Join(";", integrationPaths);
-            environmentVariables["DD_INTEGRATIONS"] = integrations;
-            environmentVariables["DD_TRACE_AGENT_HOSTNAME"] = "localhost";
-            environmentVariables["DD_TRACE_AGENT_PORT"] = traceAgentPort.ToString();
-
-            // for ASP.NET Core sample apps, set the server's port
-            environmentVariables["ASPNETCORE_URLS"] = $"http://localhost:{traceAgentPort}/";
-
-            foreach (var name in new string[] { "REDIS_HOST" })
-            {
-                var value = Environment.GetEnvironmentVariable(name);
-                if (!string.IsNullOrEmpty(value))
-                {
-                    environmentVariables[name] = value;
-                }
-            }
-        }
-
-        public static void ClearProfilerEnvironmentVariables()
-        {
-            var environmentVariables = new[]
-                                       {
-                                           // .NET Core
-                                           "CORECLR_ENABLE_PROFILING",
-                                           "CORECLR_PROFILER",
-                                           "CORECLR_PROFILER_PATH",
-                                           "CORECLR_PROFILER_PATH_32",
-                                           "CORECLR_PROFILER_PATH_64",
-
-                                           // .NET Framework
-                                           "COR_ENABLE_PROFILING",
-                                           "COR_PROFILER",
-                                           "COR_PROFILER_PATH",
-
-                                           // Datadog
-                                           "DD_PROFILER_PROCESSES",
-                                           "DD_INTEGRATIONS",
-                                           "DATADOG_PROFILER_PROCESSES",
-                                           "DATADOG_INTEGRATIONS",
-                                       };
-
-            foreach (string variable in environmentVariables)
-            {
-                Environment.SetEnvironmentVariable(variable, null);
-            }
         }
     }
 }
