1e5e290527356aee8f60d1ec47e290384b6edc71
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2019-05-31 16:04:12 -0400

    split HttpMessageHandlerIntegration into separate wrappers, use new opcode argument to emit the correct IL

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/HttpMessageHandlerIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/HttpMessageHandlerIntegration.cs
@@ -3,12 +3,13 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Datadog.Trace.ClrProfiler.Emit;
 using Datadog.Trace.ExtensionMethods;
 
 namespace Datadog.Trace.ClrProfiler.Integrations
 {
     /// <summary>
-    /// Tracer integration for HttpMessageHandler.
+    /// Tracer integration for HttpClientHandler.
     /// </summary>
     public static class HttpMessageHandlerIntegration
     {
@@ -26,46 +27,88 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         [InterceptMethod(
             TargetAssembly = "System.Net.Http",
             TargetType = "System.Net.Http.HttpMessageHandler",
+            TargetMethod = "SendAsync",
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
-        [InterceptMethod(
-            TargetAssembly = "System.Net.Http",
-            TargetType = "System.Net.Http.HttpClientHandler",
-            TargetMinimumVersion = Major4,
-            TargetMaximumVersion = Major4)]
-        public static object SendAsync(
+        public static object HttpMessageHandler_SendAsync(
             object handler,
             object request,
             object cancellationTokenSource,
             int opCode)
         {
-            // HttpMessageHandler
-            // Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            // original signature:
+            // Task<HttpResponseMessage> HttpMessageHandler.SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
             var tokenSource = cancellationTokenSource as CancellationTokenSource;
             var cancellationToken = tokenSource?.Token ?? CancellationToken.None;
+            var callOpCode = (OpCodeValue)opCode;
+            var handlerType = typeof(HttpMessageHandler); // Note the HttpMessageHandler to match the method call we replaced
+
+            var sendAsync = Emit.DynamicMethodBuilder<Func<HttpMessageHandler, HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>>>
+                                .GetOrCreateMethodCallDelegate(
+                                     handlerType,
+                                     "SendAsync",
+                                     callOpCode);
 
             return SendAsyncInternal(
+                sendAsync,
+                callOpCode == OpCodeValue.Call ? handlerType : handler.GetType(),
+                (HttpMessageHandler)handler,
+                (HttpRequestMessage)request,
+                cancellationToken);
+        }
+
+        /// <summary>
+        /// Instrumentation wrapper for <see cref="HttpMessageHandler.SendAsync"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="HttpMessageHandler"/> instance to instrument.</param>
+        /// <param name="request">The <see cref="HttpRequestMessage"/> that represents the current HTTP request.</param>
+        /// <param name="cancellationTokenSource">The <see cref="CancellationTokenSource"/> that can be used to cancel this <c>async</c> operation.</param>
+        /// <param name="opCode">The OpCode used in the original method call.</param>
+        /// <returns>Returns the value returned by the inner method call.</returns>
+        [InterceptMethod(
+            TargetAssembly = "System.Net.Http",
+            TargetType = "System.Net.Http.HttpClientHandler",
+            TargetMethod = "SendAsync",
+            TargetMinimumVersion = Major4,
+            TargetMaximumVersion = Major4)]
+        public static object HttpClientHandler_SendAsync(
+            object handler,
+            object request,
+            object cancellationTokenSource,
+            int opCode)
+        {
+            // original signature:
+            // Task<HttpResponseMessage> HttpClientHandler.SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            var tokenSource = cancellationTokenSource as CancellationTokenSource;
+            var cancellationToken = tokenSource?.Token ?? CancellationToken.None;
+            var callOpCode = (OpCodeValue)opCode;
+            var handlerType = typeof(HttpClientHandler); // Note the HttpClientHandler to match the method call we replaced
+
+            var sendAsync = Emit.DynamicMethodBuilder<Func<HttpMessageHandler, HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>>>
+                                .GetOrCreateMethodCallDelegate(
+                                     handlerType,
+                                     "SendAsync",
+                                     callOpCode);
+
+            return SendAsyncInternal(
+                sendAsync,
+                callOpCode == OpCodeValue.Call ? handlerType : handler.GetType(),
                 (HttpMessageHandler)handler,
                 (HttpRequestMessage)request,
                 cancellationToken);
         }
 
         private static async Task<HttpResponseMessage> SendAsyncInternal(
+            Func<HttpMessageHandler, HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> sendAsync,
+            Type handlerType,
             HttpMessageHandler handler,
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            var handlerType = handler.GetType();
-
-            var executeAsync = Emit.DynamicMethodBuilder<Func<HttpMessageHandler, HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>>>
-               .GetOrCreateMethodCallDelegate(
-                    handlerType,
-                    nameof(SendAsync));
-
-            if (handlerType.FullName != "System.Net.Http.HttpClientHandler" || !IsTracingEnabled(request))
+            if (!(handler is HttpClientHandler) || !IsTracingEnabled(request))
             {
                 // skip instrumentation
-                return await executeAsync(handler, request, cancellationToken).ConfigureAwait(false);
+                return await sendAsync(handler, request, cancellationToken).ConfigureAwait(false);
             }
 
             string httpMethod = request.Method?.Method;
@@ -76,11 +119,13 @@ namespace Datadog.Trace.ClrProfiler.Integrations
                 {
                     if (scope != null)
                     {
+                        scope.Span.SetTag("http-client-handler-type", handlerType.FullName);
+
                         // add distributed tracing headers to the HTTP request
                         SpanContextPropagator.Instance.Inject(scope.Span.Context, request.Headers.Wrap());
                     }
 
-                    HttpResponseMessage response = await executeAsync(handler, request, cancellationToken).ConfigureAwait(false);
+                    HttpResponseMessage response = await sendAsync(handler, request, cancellationToken).ConfigureAwait(false);
 
                     // this tag can only be set after the response is returned
                     scope?.Span.SetTag(Tags.HttpStatusCode, ((int)response.StatusCode).ToString());
