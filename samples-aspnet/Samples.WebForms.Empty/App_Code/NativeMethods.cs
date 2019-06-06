aaf24bdafdcac5d2fbcc44caa01b35d4dd01fa7f
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-06-06 19:19:56 -0400

    add code comment

--- samples-aspnet/Samples.WebForms.Empty/App_Code/NativeMethods.cs
+++ samples-aspnet/Samples.WebForms.Empty/App_Code/NativeMethods.cs
@@ -4,6 +4,10 @@ namespace Samples.WebForms.Empty
 {
     public static class NativeMethods
     {
+        /// <summary>
+        /// Allows us to determine if the profile is attached without
+        /// adding a reference to Datadog.Trace.ClrProfiler.Managed
+        /// </summary>
         [DllImport("Datadog.Trace.ClrProfiler.Native.dll")]
         public static extern bool IsProfilerAttached();
     }
