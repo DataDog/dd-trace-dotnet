52f05e4ad0d6eb77ad0d229935c23ba859e3cc8b
Colin Higgins <colin-higgins@users.noreply.github.com>
2019-06-12 10:13:30 -0400

    AdoNet type array (#394)

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/AdoNetIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/AdoNetIntegration.cs
@@ -28,13 +28,13 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         [InterceptMethod(
             TargetAssembly = "System.Data", // .NET Framework
             TargetType = "System.Data.Common.DbCommand",
-            TargetSignatureTypes = new[] { ClrNames.Ignore, ClrNames.Ignore },
+            TargetSignatureTypes = new[] { "System.Data.Common.DbDataReader", "System.Data.CommandBehavior" },
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
         [InterceptMethod(
             TargetAssembly = "System.Data.Common", // .NET Core
             TargetType = "System.Data.Common.DbCommand",
-            TargetSignatureTypes = new[] { ClrNames.Ignore, ClrNames.Ignore },
+            TargetSignatureTypes = new[] { "System.Data.Common.DbDataReader", "System.Data.CommandBehavior" },
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
         public static object ExecuteDbDataReader(object @this, int behavior, int opCode)
@@ -72,13 +72,13 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         [InterceptMethod(
             TargetAssembly = "System.Data", // .NET Framework
             TargetType = "System.Data.Common.DbCommand",
-            TargetSignatureTypes = new[] { ClrNames.Ignore, ClrNames.Ignore, ClrNames.Ignore },
+            TargetSignatureTypes = new[] { "System.Threading.Tasks.Task`1<System.Data.Common.DbDataReader>", "System.Data.CommandBehavior", "System.Threading.CancellationToken" },
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
         [InterceptMethod(
             TargetAssembly = "System.Data.Common", // .NET Core
             TargetType = "System.Data.Common.DbCommand",
-            TargetSignatureTypes = new[] { ClrNames.Ignore, ClrNames.Ignore, ClrNames.Ignore },
+            TargetSignatureTypes = new[] { "System.Threading.Tasks.Task`1<System.Data.Common.DbDataReader>", "System.Data.CommandBehavior", "System.Threading.CancellationToken" },
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
         public static object ExecuteDbDataReaderAsync(object @this, int behavior, object cancellationTokenSource, int opCode)
