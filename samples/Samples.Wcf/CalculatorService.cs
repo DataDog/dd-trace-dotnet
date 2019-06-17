1be970457d28fc56041c615aaa357b70152ab10d
colin-higgins <colin.higgins@datadoghq.com>
2019-06-17 13:05:55 -0400

    WCF explicit type array

---
+++ samples/Samples.Wcf/CalculatorService.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Samples.Wcf
+{
+    public class CalculatorService : ICalculator
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Received Add({0},{1})", n1, n2);
+            // Code added to write output to the console window.
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            Console.WriteLine("Received Divide({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+    }
+}
---
+++ samples/Samples.Wcf/ICalculator.cs
@@ -0,0 +1,17 @@
+using System.ServiceModel;
+
+namespace Samples.Wcf
+{
+    [ServiceContract(Namespace = "WcfSample")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
