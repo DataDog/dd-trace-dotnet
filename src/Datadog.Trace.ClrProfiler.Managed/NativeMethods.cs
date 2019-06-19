8f8b5ed91b0322de665bfd2fa7e2d9190447a611
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-06-19 13:12:26 -0400

    fix Instrumentation.ProfilerAttached on .NET Framework by including dll extension

--- src/Datadog.Trace.ClrProfiler.Managed/NativeMethods.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/NativeMethods.cs
@@ -1,10 +1,36 @@
+using System;
 using System.Runtime.InteropServices;
 
+// ReSharper disable MemberHidesStaticFromOuterClass
 namespace Datadog.Trace.ClrProfiler
 {
     internal static class NativeMethods
     {
-        [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        public static extern bool IsProfilerAttached();
+        public static bool IsProfilerAttached()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return Windows.IsProfilerAttached();
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return Linux.IsProfilerAttached();
+            }
+
+            throw new PlatformNotSupportedException();
+        }
+
+        private static class Windows
+        {
+            [DllImport("Datadog.Trace.ClrProfiler.Native.dll")]
+            public static extern bool IsProfilerAttached();
+        }
+
+        private static class Linux
+        {
+            [DllImport("Datadog.Trace.ClrProfiler.Native.so")]
+            public static extern bool IsProfilerAttached();
+        }
     }
 }
