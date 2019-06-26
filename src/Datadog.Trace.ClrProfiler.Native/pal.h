d71a0552e4db47cc65a1173eb4c7ddeff5fea28e
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-06-26 11:33:25 -0400

    fix null check

--- src/Datadog.Trace.ClrProfiler.Native/pal.h
+++ src/Datadog.Trace.ClrProfiler.Native/pal.h
@@ -33,7 +33,7 @@ inline WSTRING DatadogLogFilePath() {
   const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
   std::string program_data;
 
-  if (SUCCEEDED(result) && p_program_data > nullptr && length > 0) {
+  if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
     program_data = std::string(p_program_data);
   } else {
     program_data = R"(C:\ProgramData)";
