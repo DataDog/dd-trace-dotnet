c8a6fdc4b3d16e756f0ca1daba00f47263b52acf
colin-higgins <colin.higgins@datadoghq.com>
2019-07-10 09:12:14 -0400

    more wip

---
+++ docker/wait-for-test-suite-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+$DIR/with-profiler-logs.bash \
+    wait-for-it servicestackredis:6379 -- \
+    wait-for-it stackexchangeredis:6379 -- \
+    wait-for-it elasticsearch6:9200 -- \
+    wait-for-it elasticsearch5:9200 -- \
+    wait-for-it sqlserver:1433 -- \
+    wait-for-it mongo:27017 -- \
+    wait-for-it postgres:5432 -- \
