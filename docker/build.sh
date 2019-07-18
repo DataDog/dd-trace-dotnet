f5631dc27b3d8cd095591e45945cdb7b6448f2f1
colin-higgins <colin.higgins@datadoghq.com>
2019-07-18 12:23:06 -0400

    publish the console app

--- docker/build.sh
+++ docker/build.sh
@@ -14,11 +14,9 @@ for config in Debug Release ; do
         dotnet publish -f netcoreapp2.1 -c $config samples/$sample/$sample.csproj
     done
 
-	dotnet publish -f netcoreapp2.1 -c $config reproductions/OrleansCrash/OrleansCrash.csproj
-	
-	dotnet publish -f netcoreapp2.1 -c $config reproductions/DataDogThreadTest/DataDogThreadTest.csproj
-
-	dotnet publish -f netcoreapp2.1 -c $config reproductions/HttpMessageHandler.StackOverflow/HttpMessageHandler.StackOverflow.csproj
+    for reproduction in OrleansCrash DataDogThreadTest HttpMessageHandler.StackOverflow AspNetMvcCorePerformance ; do
+        dotnet publish -f netcoreapp2.1 -c $config reproductions/$sample/$sample.csproj
+    done
 
     dotnet msbuild Datadog.Trace.proj -t:RestoreAndBuildSamplesForPackageVersions
 
