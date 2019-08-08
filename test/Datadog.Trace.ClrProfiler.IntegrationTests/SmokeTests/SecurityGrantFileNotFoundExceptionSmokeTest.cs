9b0aa81ad1de6cd010648bd9bd270e96cc569ff8
Zach Montoya <zach.montoya@datadoghq.com>
2019-08-08 13:28:14 -0700

    Extend the timeout once more for SecurityGrantFileNotFoundExceptionSmokeTest.

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/SecurityGrantFileNotFoundExceptionSmokeTest.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/SecurityGrantFileNotFoundExceptionSmokeTest.cs
@@ -14,7 +14,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
     public class SecurityGrantFileNotFoundExceptionSmokeTest : SmokeTestBase
     {
         public SecurityGrantFileNotFoundExceptionSmokeTest(ITestOutputHelper output)
-            : base(output, "SecurityGrant.FileNotFoundException", maxTestRunSeconds: 30)
+            : base(output, "SecurityGrant.FileNotFoundException", maxTestRunSeconds: 60)
         {
         }
 
