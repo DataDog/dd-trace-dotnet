7469e763fa7c4fb56d1561a50e3ca7c9b8d5b143
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-08-22 17:46:45 -0400

    remove unused MethodBuilder.Start(Assembly, ...) delegate

--- src/Datadog.Trace.ClrProfiler.Managed/Emit/MethodBuilder.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Emit/MethodBuilder.cs
@@ -54,12 +54,6 @@ namespace Datadog.Trace.ClrProfiler.Emit
             _forceMethodDefResolve = false;
         }
 
-        public static MethodBuilder<TDelegate> Start(Assembly resolutionAssembly, int mdToken, int opCode, string methodName)
-        {
-            // This method is deprecated in favor of the newer ModuleVersionId lookup
-            return new MethodBuilder<TDelegate>(resolutionAssembly.ManifestModule, mdToken, opCode, methodName);
-        }
-
         public static MethodBuilder<TDelegate> Start(Guid moduleVersionId, int mdToken, int opCode, string methodName)
         {
             return new MethodBuilder<TDelegate>(moduleVersionId, mdToken, opCode, methodName);
