11d7514ddfc0b4040d8149f648c8d53784808c2d
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-08-27 18:09:25 -0400

    add object.GetInstrumentedInterface() extension method

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/Types/TypeExtensions.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/Types/TypeExtensions.cs
@@ -25,5 +25,28 @@ namespace Datadog.Trace.ClrProfiler
 
             return null;
         }
+
+        public static System.Type GetInstrumentedInterface(
+            this object runtimeObject,
+            string instrumentedInterfaceName)
+        {
+            if (runtimeObject == null)
+            {
+                return null;
+            }
+
+            var currentType = runtimeObject.GetType();
+            var interfaces = currentType.GetInterfaces();
+
+            foreach (var interfaceType in interfaces)
+            {
+                if ($"{interfaceType.Namespace}.{interfaceType.Name}" == instrumentedInterfaceName)
+                {
+                    return interfaceType;
+                }
+            }
+
+            return null;
+        }
     }
 }
