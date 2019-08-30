bc99f64e06ff2d59f80e6f188453a74168f7205e
Zach Montoya <zach.montoya@datadoghq.com>
2019-08-30 13:50:40 -0700

    Double the time-out for EntityFramework6xMdTokenLookupFailure to 120 seconds.

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/EntityFramework6xMdTokenLookupFailure.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/EntityFramework6xMdTokenLookupFailure.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.ClrProfiler.IntegrationTests.Helpers;
 using Xunit;
@@ -13,7 +15,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
     public class EntityFramework6xMdTokenLookupFailure : SmokeTestBase
     {
         public EntityFramework6xMdTokenLookupFailure(ITestOutputHelper output)
-            : base(output, "EntityFramework6x.MdTokenLookupFailure", maxTestRunSeconds: 60)
+            : base(output, "EntityFramework6x.MdTokenLookupFailure", maxTestRunSeconds: 120)
         {
         }
 
