d78a4cc1f7a92714e4c4aabef45c5dd7a086c4f5
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-08-30 13:04:54 -0400

    handle exceptions, add logging

--- src/Datadog.Trace/Containers/ContainerInfo.cs
+++ src/Datadog.Trace/Containers/ContainerInfo.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
+using Datadog.Trace.Logging;
 
 namespace Datadog.Trace.Containers
 {
@@ -18,6 +19,8 @@ namespace Datadog.Trace.Containers
         private const string ContainerIdRegex = @"^(?:\d+):(?:[^:]*):/?(?:.+/)([0-9a-f]{8}[-_][0-9a-f]{4}[-_][0-9a-f]{4}[-_][0-9a-f]{4}[-_][0-9a-f]{12}|[0-9a-f]{64}(?:\.scope)?)$";
 
         private static readonly Lazy<string> ContainerId = new Lazy<string>(GetContainerIdInternal, LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private static readonly ILog Log = LogProvider.GetCurrentClassLogger();
 
         /// <summary>
         /// Gets the id of the container executing the code.
@@ -56,10 +59,30 @@ namespace Datadog.Trace.Containers
 
         private static string GetContainerIdInternal()
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && File.Exists(ControlGroupsFilePath))
+            bool isLinux;
+
+            try
             {
-                IEnumerable<string> lines = File.ReadLines(ControlGroupsFilePath);
-                return ParseCgroupLines(lines);
+                isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+            }
+            catch (Exception ex)
+            {
+                Log.WarnException("Unable to determine OS. Will not report container id.", ex);
+                return null;
+            }
+
+            try
+            {
+                if (isLinux && File.Exists(ControlGroupsFilePath))
+                {
+                    var lines = File.ReadLines(ControlGroupsFilePath);
+                    return ParseCgroupLines(lines);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.WarnException("Error reading cgroup file. Will not report container id.", ex);
+                return null;
             }
 
             return null;
