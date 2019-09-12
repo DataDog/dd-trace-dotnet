fe15461ff31d88c799694ab4e9b568ea9b85670f
Zach Montoya <zach.montoya@datadoghq.com>
2019-09-11 20:31:58 -0700

    Run the dotnet publish command on Datadog.Trace.ClrProfiler.Managed to produce assets for integration tests and packaging, reducing dependency on <CopyLocalLockFileAssemblies> which is not well-documented.

--- docker/Datadog.Trace.ClrProfiler.Native.sh
+++ docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -5,10 +5,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd "$DIR/.."
 
+PUBLISH_OUTPUT="$( pwd )/src/bin/managed-publish/netstandard2.0"
+
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p obj/Debug/x64
 (cd obj/Debug/x64 && cmake ../../.. && make)
 
 mkdir -p bin/Debug/x64
 cp -f obj/Debug/x64/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/Datadog.Trace.ClrProfiler.Native.so
-cp -f -r ../Datadog.Trace.ClrProfiler.Managed/bin/Debug/* bin/Debug/x64/
+
+mkdir -p bin/Debug/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT/*.dll bin/Debug/x64/netstandard2.0/
+cp -f $PUBLISH_OUTPUT/*.pdb bin/Debug/x64/netstandard2.0/
--- docker/build.sh
+++ docker/build.sh
@@ -5,21 +5,27 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd "$DIR/.."
 
+PUBLISH_OUTPUT="$( pwd )/src/bin/managed-publish/netstandard2.0"
+mkdir -p "$PUBLISH_OUTPUT"
+
 dotnet build -c $buildConfiguration src/Datadog.Trace.ClrProfiler.Managed.Loader/Datadog.Trace.ClrProfiler.Managed.Loader.csproj
 
-for proj in Datadog.Trace Datadog.Trace.ClrProfiler.Managed Datadog.Trace.OpenTracing ; do
+for proj in Datadog.Trace Datadog.Trace.OpenTracing ; do
     dotnet publish -f netstandard2.0 -c $buildConfiguration src/$proj/$proj.csproj
 done
 
+dotnet publish -f netstandard2.0 -c $buildConfiguration src/Datadog.Trace.ClrProfiler.Managed/Datadog.Trace.ClrProfiler.Managed.csproj -o "$PUBLISH_OUTPUT"
+rm -r "$PUBLISH_OUTPUT"/runtimes
+
 for sample in Samples.AspNetCoreMvc2 Samples.Elasticsearch Samples.Elasticsearch.V5 Samples.ServiceStack.Redis Samples.StackExchange.Redis Samples.SqlServer Samples.MongoDB Samples.HttpMessageHandler Samples.Npgsql Samples.GraphQL ; do
-    dotnet publish -f netcoreapp2.1 -c $buildConfiguration samples/$sample/$sample.csproj
+    dotnet publish -f netcoreapp2.1 -c $buildConfiguration samples/$sample/$sample.csproj -p:Configuration=$buildConfiguration -p:ManagedProfilerOutputDirectory="$PUBLISH_OUTPUT"
 done
 
 for sample in OrleansCrash DataDogThreadTest HttpMessageHandler.StackOverflowException StackExchange.Redis.StackOverflowException AspNetMvcCorePerformance AssemblyLoad.FileNotFoundException TraceContext.InvalidOperationException ; do
-    dotnet publish -f netcoreapp2.1 -c $buildConfiguration reproductions/$sample/$sample.csproj
+    dotnet publish -f netcoreapp2.1 -c $buildConfiguration reproductions/$sample/$sample.csproj -p:Configuration=$buildConfiguration -p:ManagedProfilerOutputDirectory="$PUBLISH_OUTPUT"
 done
 
-dotnet msbuild Datadog.Trace.proj -t:RestoreAndBuildSamplesForPackageVersions -p:Configuration=$buildConfiguration
+dotnet msbuild Datadog.Trace.proj -t:RestoreAndBuildSamplesForPackageVersions -p:Configuration=$buildConfiguration -p:ManagedProfilerOutputDirectory="$PUBLISH_OUTPUT"
 
 for proj in Datadog.Trace.ClrProfiler.IntegrationTests ; do
     dotnet publish -f netcoreapp2.1 -c $buildConfiguration test/$proj/$proj.csproj
