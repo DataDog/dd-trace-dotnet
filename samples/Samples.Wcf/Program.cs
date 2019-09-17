3b20cd97740f9ebec220548e00ab2d86449d3c14
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-09-17 17:04:03 -0400

    modify Samples.Wcf to allow choosing between WsHttpBinding or NetTcpBinding

--- samples/Samples.Wcf/Program.cs
+++ samples/Samples.Wcf/Program.cs
@@ -1,30 +1,59 @@
 using System;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 
 namespace Samples.Wcf
 {
     public static class Program
     {
-        private static string WcfPort = "8585";
-        private static string WcfNamespace = "WcfSample";
+        private const string WcfPort = "8585";
+        private const string WcfNamespace = "WcfSample";
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // This sample is a work in progress
-            var baseAddress = new Uri($"http://localhost:{WcfPort}/{WcfNamespace}/");
+            Binding binding;
+            Uri baseAddress;
+
+            if (args.Length > 0 && args[0].Equals("WSHttpBinding", StringComparison.OrdinalIgnoreCase))
+            {
+                binding = new WSHttpBinding();
+                baseAddress = new Uri($"http://localhost:{WcfPort}/{WcfNamespace}/");
+            }
+            else if (args.Length > 0 && args[0].Equals("BasicHttpBinding", StringComparison.OrdinalIgnoreCase))
+            {
+                binding = new BasicHttpBinding();
+                baseAddress = new Uri($"http://localhost:{WcfPort}/{WcfNamespace}/");
+            }
+            else if (args.Length > 0 && args[0].Equals("NetTcpBinding", StringComparison.OrdinalIgnoreCase))
+            {
+                binding = new NetTcpBinding();
+                baseAddress = new Uri($"net.tcp://localhost:{WcfPort}/{WcfNamespace}/");
+            }
+            else
+            {
+                throw new Exception("Binding type required: WSHttpBinding, BasicHttpBinding, or NetTcpBinding");
+            }
+
             var selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
 
             try
             {
-                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+                selfHost.AddServiceEndpoint(typeof(ICalculator), binding, "CalculatorService");
 
-                ServiceMetadataBehavior smb = new ServiceMetadataBehavior
+                if (binding is WSHttpBinding || binding is BasicHttpBinding)
                 {
-                    HttpGetEnabled = true
-                };
+                    var smb = new ServiceMetadataBehavior { HttpGetEnabled = true };
+                    selfHost.Description.Behaviors.Add(smb);
+                }
 
-                selfHost.Description.Behaviors.Add(smb);
+                selfHost.Opening += (sender, eventArgs) => Console.Write("Opening... ");
+                selfHost.Opened += (sender, eventArgs) => Console.WriteLine("done.");
+                selfHost.Closing += (sender, eventArgs) => Console.Write("Closing... ");
+                selfHost.Closed += (sender, eventArgs) => Console.WriteLine("done.");
+                selfHost.Faulted += (sender, eventArgs) => Console.WriteLine("Faulted.");
+                selfHost.UnknownMessageReceived += (sender, eventArgs) => Console.WriteLine($"UnknownMessageReceived: {eventArgs.Message}");
 
                 selfHost.Open();
 
