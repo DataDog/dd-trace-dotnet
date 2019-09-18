664c8413e02a416659f0d1f50516a970168632cd
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-09-18 10:23:51 -0400

    clean up AdoNetIntegration

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/AdoNetIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/AdoNetIntegration.cs
@@ -4,8 +4,6 @@ using System.Data.Common;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.ClrProfiler.Emit;
-using Datadog.Trace.ClrProfiler.Helpers;
-using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.Logging;
 
 namespace Datadog.Trace.ClrProfiler.Integrations
@@ -15,6 +13,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
     /// </summary>
     public static class AdoNetIntegration
     {
+        // TODO: rename to DbCommandIntegration
         private const string IntegrationName = "AdoNet";
         private const string Major4 = "4";
         private const string FrameworkAssembly = "System.Data";
@@ -28,42 +27,36 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         /// <summary>
         /// Wrapper method that instruments <see cref="System.Data.Common.DbCommand.ExecuteDbDataReader"/>.
         /// </summary>
-        /// <param name="this">The <see cref="DbCommand"/> that is references by the "this" pointer in the instrumented method.</param>
+        /// <param name="command">The <see cref="DbCommand"/> that is references by the "this" pointer in the instrumented method.</param>
         /// <param name="behavior">A value from <see cref="CommandBehavior"/>.</param>
         /// <param name="opCode">The OpCode used in the original method call.</param>
         /// <param name="mdToken">The mdToken of the original method call.</param>
         /// <param name="moduleVersionPtr">A pointer to the module version GUID.</param>
         /// <returns>The value returned by the instrumented method.</returns>
         [InterceptMethod(
-            TargetAssemblies = new[] { FrameworkAssembly, CoreAssembly },
+            TargetAssemblies = new[] { AdoNetConstants.AssemblyNames.SystemData, AdoNetConstants.AssemblyNames.SystemDataCommon },
             TargetType = DbCommandTypeName,
-            TargetSignatureTypes = new[] { DbDataReaderTypeName, CommandBehaviorTypeName },
+            TargetSignatureTypes = new[] { DbDataReaderTypeName, AdoNetConstants.TypeNames.CommandBehavior },
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
         public static object ExecuteDbDataReader(
-            object @this,
+            object command,
             int behavior,
             int opCode,
             int mdToken,
             long moduleVersionPtr)
         {
-            if (@this == null)
-            {
-                throw new ArgumentNullException(nameof(@this));
-            }
-
-            Func<object, CommandBehavior, object> instrumentedMethod = null;
+            Func<DbCommand, CommandBehavior, DbDataReader> instrumentedMethod;
             var commandBehavior = (CommandBehavior)behavior;
 
             try
             {
-                var instrumentedType = typeof(DbCommand);
                 instrumentedMethod =
-                    MethodBuilder<Func<object, CommandBehavior, object>>
-                       .Start(moduleVersionPtr, mdToken, opCode, nameof(ExecuteDbDataReader))
-                       .WithConcreteType(instrumentedType)
+                    MethodBuilder<Func<DbCommand, CommandBehavior, DbDataReader>>
+                       .Start(moduleVersionPtr, mdToken, opCode, ExecuteDbDataReaderMethodName)
+                       .WithConcreteType(typeof(DbCommand))
                        .WithParameters(commandBehavior)
-                       .WithNamespaceAndNameFilters(DbDataReaderTypeName, CommandBehaviorTypeName)
+                       .WithNamespaceAndNameFilters(DbDataReaderTypeName, AdoNetConstants.TypeNames.CommandBehavior)
                        .Build();
             }
             catch (Exception ex)
@@ -79,11 +72,13 @@ namespace Datadog.Trace.ClrProfiler.Integrations
                 throw;
             }
 
-            using (var scope = CreateScope((DbCommand)@this))
+            var dbCommand = command as DbCommand;
+
+            using (var scope = ScopeFactory.CreateDbCommandScope(Tracer.Instance, dbCommand, IntegrationName))
             {
                 try
                 {
-                    return instrumentedMethod(@this, commandBehavior);
+                    return instrumentedMethod(dbCommand, commandBehavior);
                 }
                 catch (Exception ex)
                 {
@@ -96,7 +91,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         /// <summary>
         /// Wrapper method that instruments <see cref="System.Data.Common.DbCommand.ExecuteDbDataReader"/>.
         /// </summary>
-        /// <param name="this">The <see cref="DbCommand"/> that is references by the "this" pointer in the instrumented method.</param>
+        /// <param name="command">The <see cref="DbCommand"/> that is references by the "this" pointer in the instrumented method.</param>
         /// <param name="behavior">A value from <see cref="CommandBehavior"/>.</param>
         /// <param name="cancellationTokenSource">A cancellation token source that can be used to cancel the async operation.</param>
         /// <param name="opCode">The OpCode used in the original method call.</param>
@@ -104,13 +99,13 @@ namespace Datadog.Trace.ClrProfiler.Integrations
         /// <param name="moduleVersionPtr">A pointer to the module version GUID.</param>
         /// <returns>The value returned by the instrumented method.</returns>
         [InterceptMethod(
-            TargetAssemblies = new[] { FrameworkAssembly, CoreAssembly },
+            TargetAssemblies = new[] { AdoNetConstants.AssemblyNames.SystemData, AdoNetConstants.AssemblyNames.SystemDataCommon },
             TargetType = DbCommandTypeName,
-            TargetSignatureTypes = new[] { "System.Threading.Tasks.Task`1<System.Data.Common.DbDataReader>", CommandBehaviorTypeName, ClrNames.CancellationToken },
+            TargetSignatureTypes = new[] { "System.Threading.Tasks.Task`1<System.Data.Common.DbDataReader>", AdoNetConstants.TypeNames.CommandBehavior, ClrNames.CancellationToken },
             TargetMinimumVersion = Major4,
             TargetMaximumVersion = Major4)]
         public static object ExecuteDbDataReaderAsync(
-            object @this,
+            object command,
             int behavior,
             object cancellationTokenSource,
             int opCode,
@@ -124,19 +119,34 @@ namespace Datadog.Trace.ClrProfiler.Integrations
 
             var tokenSource = cancellationTokenSource as CancellationTokenSource;
             var cancellationToken = tokenSource?.Token ?? CancellationToken.None;
-            var instrumentedType = typeof(DbCommand);
-            var dataReaderType = typeof(DbDataReader);
-            var commandBehavior = (CommandBehavior)behavior;
-            Func<object, CommandBehavior, object, object> instrumentedMethod;
+
+            return ExecuteDbDataReaderAsyncInternal(
+                command as DbCommand,
+                (CommandBehavior)behavior,
+                cancellationToken,
+                opCode,
+                mdToken,
+                moduleVersionPtr);
+        }
+
+        private static async Task<DbDataReader> ExecuteDbDataReaderAsyncInternal(
+            DbCommand command,
+            CommandBehavior commandBehavior,
+            CancellationToken cancellationToken,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            Func<DbCommand, CommandBehavior, CancellationToken, Task<DbDataReader>> instrumentedMethod;
 
             try
             {
                 instrumentedMethod =
-                    MethodBuilder<Func<object, CommandBehavior, object, object>>
-                       .Start(moduleVersionPtr, mdToken, opCode, nameof(ExecuteDbDataReaderAsync))
-                       .WithConcreteType(instrumentedType)
+                    MethodBuilder<Func<DbCommand, CommandBehavior, CancellationToken, Task<DbDataReader>>>
+                       .Start(moduleVersionPtr, mdToken, opCode, ExecuteDbDataReaderAsyncMethodName)
+                       .WithConcreteType(typeof(DbCommand))
                        .WithParameters(commandBehavior, cancellationToken)
-                       .WithNamespaceAndNameFilters(ClrNames.GenericTask, CommandBehaviorTypeName, ClrNames.CancellationToken)
+                       .WithNamespaceAndNameFilters(ClrNames.GenericTask, AdoNetConstants.TypeNames.CommandBehavior, ClrNames.CancellationToken)
                        .Build();
             }
             catch (Exception ex)
@@ -152,96 +162,17 @@ namespace Datadog.Trace.ClrProfiler.Integrations
                 throw;
             }
 
-            return AsyncHelper.InvokeGenericTaskDelegate(
-                owningType: instrumentedType,
-                taskResultType: dataReaderType,
-                nameOfIntegrationMethod: nameof(ExecuteDbDataReaderAsyncInternal),
-                integrationType: typeof(AdoNetIntegration),
-                parametersToPass: new[] { @this, behavior, cancellationToken, instrumentedMethod });
-        }
-
-        private static async Task<T> ExecuteDbDataReaderAsyncInternal<T>(
-            DbCommand command,
-            CommandBehavior behavior,
-            CancellationToken cancellationToken,
-            Func<object, CommandBehavior, object, object> instrumentedMethod)
-        {
-            using (var scope = CreateScope(command))
+            using (var scope = ScopeFactory.CreateDbCommandScope(Tracer.Instance, command, IntegrationName))
             {
                 try
                 {
-                    var task = (Task<T>)instrumentedMethod(command, behavior, cancellationToken);
-                    return await task.ConfigureAwait(false);
+                    return await instrumentedMethod(command, commandBehavior, cancellationToken).ConfigureAwait(false);
                 }
                 catch (Exception ex)
                 {
                     scope?.Span.SetException(ex);
                     throw;
                 }
-            }
-        }
-
-        private static Scope CreateScope(DbCommand command)
-        {
-            Scope scope = null;
-
-            try
-            {
-                Tracer tracer = Tracer.Instance;
-
-                if (!tracer.Settings.IsIntegrationEnabled(IntegrationName))
-                {
-                    // integration disabled, don't create a scope, skip this trace
-                    return null;
-                }
-
-                string dbType = GetDbType(command.GetType().Name);
-
-                if (dbType == null)
-                {
-                    // don't create a scope, skip this trace
-                    return null;
-                }
-
-                string serviceName = $"{tracer.DefaultServiceName}-{dbType}";
-                string operationName = $"{dbType}.query";
-
-                scope = tracer.StartActive(operationName, serviceName: serviceName);
-                var span = scope.Span;
-                span.SetTag(Tags.DbType, dbType);
-                span.AddTagsFromDbCommand(command);
-
-                // set analytics sample rate if enabled
-                var analyticsSampleRate = tracer.Settings.GetIntegrationAnalyticsSampleRate(IntegrationName, enabledWithGlobalSetting: false);
-                span.SetMetric(Tags.Analytics, analyticsSampleRate);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Error creating or populating scope.");
-            }
-
-            return scope;
-        }
-
-        private static string GetDbType(string commandTypeName)
-        {
-            switch (commandTypeName)
-            {
-                case "SqlCommand":
-                    return "sql-server";
-                case "NpgsqlCommand":
-                    return "postgres";
-                case "InterceptableDbCommand":
-                case "ProfiledDbCommand":
-                    // don't create spans for these
-                    return null;
-                default:
-                    const string commandSuffix = "Command";
-
-                    // remove "Command" suffix if present
-                    return commandTypeName.EndsWith(commandSuffix)
-                               ? commandTypeName.Substring(0, commandTypeName.Length - commandSuffix.Length).ToLowerInvariant()
-                               : commandTypeName.ToLowerInvariant();
             }
         }
     }
