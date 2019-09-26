d16d3d0e9738d9e9c331c5520767c0d6c98b4fed
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-09-26 13:54:43 -0400

    intercept more method calls in AdoNetIntegration

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/AdoNetIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/AdoNetIntegration.cs
@@ -175,5 +175,221 @@ namespace Datadog.Trace.ClrProfiler.Integrations
                 }
             }
         }
+
+        [InterceptMethod(
+            TargetAssemblies = new[] { AdoNetConstants.AssemblyNames.SystemData, AdoNetConstants.AssemblyNames.SystemDataCommon },
+            TargetType = DbCommandTypeName,
+            TargetSignatureTypes = new[] { ClrNames.Int32 },
+            TargetMinimumVersion = Major4,
+            TargetMaximumVersion = Major4)]
+        public static int ExecuteNonQuery(
+            object command,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            Func<DbCommand, int> instrumentedMethod;
+
+            try
+            {
+                instrumentedMethod =
+                    MethodBuilder<Func<DbCommand, int>>
+                       .Start(moduleVersionPtr, mdToken, opCode, AdoNetConstants.MethodNames.ExecuteNonQuery)
+                       .WithConcreteType(typeof(DbCommand))
+                       .WithNamespaceAndNameFilters(ClrNames.Int32)
+                       .Build();
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorException($"Error resolving {DbCommandTypeName}.{AdoNetConstants.MethodNames.ExecuteNonQuery}(...)", ex);
+                throw;
+            }
+
+            var dbCommand = command as DbCommand;
+
+            using (var scope = ScopeFactory.CreateDbCommandScope(Tracer.Instance, dbCommand, IntegrationName))
+            {
+                try
+                {
+                    return instrumentedMethod(dbCommand);
+                }
+                catch (Exception ex)
+                {
+                    scope?.Span.SetException(ex);
+                    throw;
+                }
+            }
+        }
+
+        [InterceptMethod(
+            TargetAssemblies = new[] { AdoNetConstants.AssemblyNames.SystemData, AdoNetConstants.AssemblyNames.SystemDataCommon },
+            TargetType = DbCommandTypeName,
+            TargetSignatureTypes = new[] { "System.Threading.Tasks.Task`1<System.Int32>", ClrNames.CancellationToken },
+            TargetMinimumVersion = Major4,
+            TargetMaximumVersion = Major4)]
+        public static object ExecuteNonQueryAsync(
+            object command,
+            object cancellationTokenSource,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            var tokenSource = cancellationTokenSource as CancellationTokenSource;
+            var cancellationToken = tokenSource?.Token ?? CancellationToken.None;
+
+            return ExecuteNonQueryAsyncInternal(
+                command as DbCommand,
+                cancellationToken,
+                opCode,
+                mdToken,
+                moduleVersionPtr);
+        }
+
+        public static async Task<int> ExecuteNonQueryAsyncInternal(
+            DbCommand command,
+            CancellationToken cancellationToken,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            Func<DbCommand, CancellationToken, Task<int>> instrumentedMethod;
+
+            try
+            {
+                instrumentedMethod =
+                    MethodBuilder<Func<DbCommand, CancellationToken, Task<int>>>
+                       .Start(moduleVersionPtr, mdToken, opCode, AdoNetConstants.MethodNames.ExecuteNonQueryAsync)
+                       .WithConcreteType(typeof(DbCommand))
+                       .WithParameters(cancellationToken)
+                       .WithNamespaceAndNameFilters(ClrNames.GenericTask, ClrNames.CancellationToken)
+                       .Build();
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorException($"Error resolving {DbCommandTypeName}.{AdoNetConstants.MethodNames.ExecuteNonQueryAsync}(...)", ex);
+                throw;
+            }
+
+            using (var scope = ScopeFactory.CreateDbCommandScope(Tracer.Instance, command, IntegrationName))
+            {
+                try
+                {
+                    return await instrumentedMethod(command, cancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    scope?.Span.SetException(ex);
+                    throw;
+                }
+            }
+        }
+
+        [InterceptMethod(
+            TargetAssemblies = new[] { AdoNetConstants.AssemblyNames.SystemData, AdoNetConstants.AssemblyNames.SystemDataCommon },
+            TargetType = DbCommandTypeName,
+            TargetSignatureTypes = new[] { ClrNames.Object },
+            TargetMinimumVersion = Major4,
+            TargetMaximumVersion = Major4)]
+        public static object ExecuteScalar(
+            object command,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            Func<DbCommand, object> instrumentedMethod;
+
+            try
+            {
+                instrumentedMethod =
+                    MethodBuilder<Func<DbCommand, object>>
+                       .Start(moduleVersionPtr, mdToken, opCode, AdoNetConstants.MethodNames.ExecuteScalar)
+                       .WithConcreteType(typeof(DbCommand))
+                       .WithNamespaceAndNameFilters(ClrNames.Object)
+                       .Build();
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorException($"Error resolving {DbCommandTypeName}.{AdoNetConstants.MethodNames.ExecuteScalar}(...)", ex);
+                throw;
+            }
+
+            var dbCommand = command as DbCommand;
+
+            using (var scope = ScopeFactory.CreateDbCommandScope(Tracer.Instance, dbCommand, IntegrationName))
+            {
+                try
+                {
+                    return instrumentedMethod(dbCommand);
+                }
+                catch (Exception ex)
+                {
+                    scope?.Span.SetException(ex);
+                    throw;
+                }
+            }
+        }
+
+        [InterceptMethod(
+            TargetAssemblies = new[] { AdoNetConstants.AssemblyNames.SystemData, AdoNetConstants.AssemblyNames.SystemDataCommon },
+            TargetType = DbCommandTypeName,
+            TargetSignatureTypes = new[] { "System.Threading.Tasks.Task`1<System.Object>", ClrNames.CancellationToken },
+            TargetMinimumVersion = Major4,
+            TargetMaximumVersion = Major4)]
+        public static object ExecuteScalarAsync(
+            object command,
+            object cancellationTokenSource,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            var tokenSource = cancellationTokenSource as CancellationTokenSource;
+            var cancellationToken = tokenSource?.Token ?? CancellationToken.None;
+
+            return ExecuteScalarAsyncInternal(
+                command as DbCommand,
+                cancellationToken,
+                opCode,
+                mdToken,
+                moduleVersionPtr);
+        }
+
+        public static async Task<object> ExecuteScalarAsyncInternal(
+            DbCommand command,
+            CancellationToken cancellationToken,
+            int opCode,
+            int mdToken,
+            long moduleVersionPtr)
+        {
+            Func<DbCommand, CancellationToken, Task<object>> instrumentedMethod;
+
+            try
+            {
+                instrumentedMethod =
+                    MethodBuilder<Func<DbCommand, CancellationToken, Task<object>>>
+                       .Start(moduleVersionPtr, mdToken, opCode, AdoNetConstants.MethodNames.ExecuteScalarAsync)
+                       .WithConcreteType(typeof(DbCommand))
+                       .WithParameters(cancellationToken)
+                       .WithNamespaceAndNameFilters(ClrNames.GenericTask)
+                       .Build();
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorException($"Error resolving {DbCommandTypeName}.{AdoNetConstants.MethodNames.ExecuteScalarAsync}(...)", ex);
+                throw;
+            }
+
+            using (var scope = ScopeFactory.CreateDbCommandScope(Tracer.Instance, command, IntegrationName))
+            {
+                try
+                {
+                    return await instrumentedMethod(command, cancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    scope?.Span.SetException(ex);
+                    throw;
+                }
+            }
+        }
     }
 }
