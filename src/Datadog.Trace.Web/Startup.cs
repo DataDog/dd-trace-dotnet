167e953ee984b93db26bfd06630e1327dd6e04b3
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-09-27 13:46:37 -0400

    rename AspNetStartup to Startup

--- src/Datadog.Trace.Web/Startup.cs
+++ src/Datadog.Trace.Web/Startup.cs
@@ -1,14 +1,14 @@
 using System.Web;
 using Datadog.Trace.Web;
 
-[assembly: PreApplicationStartMethod(typeof(AspNetStartup), "Register")]
+[assembly: PreApplicationStartMethod(typeof(Startup), "Register")]
 
 namespace Datadog.Trace.Web
 {
     /// <summary>
     ///     Used as the target of a PreApplicationStartMethodAttribute on the assembly to load the TracingHttpModule into the pipeline
     /// </summary>
-    public static class AspNetStartup
+    public static class Startup
     {
         /// <summary>
         ///     Registers the TracingHttpModule at ASP.NET startup into the pipeline
