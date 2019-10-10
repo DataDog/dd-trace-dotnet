b76a59f5f14ba8f006583ba4b4c7f7c8531fe600
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-10-10 17:44:57 +0200

    add new integration tests for MySql and Npgsql

---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MySqlCommandTests.cs
@@ -0,0 +1,39 @@
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
+{
+    public class MySqlCommandTests : TestHelper
+    {
+        public MySqlCommandTests(ITestOutputHelper output)
+            : base("MySqlServer", output)
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(PackageVersions.SqlClient), MemberType = typeof(PackageVersions))]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public void SubmitsTraces(string packageVersion)
+        {
+            int agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port, packageVersion: packageVersion))
+            {
+                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
+
+                var spans = agent.WaitForSpans(count: 12, operationName: "sql-server.query");
+                Assert.True(spans.Count > 0, "expected at least one span");
+
+                foreach (var span in spans)
+                {
+                    Assert.Equal("sql-server.query", span.Name);
+                    Assert.Equal("Samples.MySql-sql-server", span.Service);
+                    Assert.Equal(SpanTypes.Sql, span.Type);
+                }
+            }
+        }
+    }
+}
