82f14425c5589e29479bf2fb04de7ef556d95205
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-10-10 17:41:03 +0200

    refactor MySql and Npgsql integrations to use the new db test hardness

--- samples/Samples.MySql/Program.cs
+++ samples/Samples.MySql/Program.cs
@@ -1,83 +1,64 @@
 using System;
-using Datadog.Trace.ClrProfiler;
+using System.Data.Common;
+using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using Samples.DatabaseHelper;
 
 namespace Samples.MySql
 {
-    class Program
+    internal static class Program
     {
-        private static string Host()
+        private static async Task Main()
         {
-            return Environment.GetEnvironmentVariable("MYSQL_HOST") ?? "localhost";
+            /* TODO: enable this after adding a MySql-specific integration
+            using (var connection = CreateConnection())
+            {
+                var testQueries = new RelationalDatabaseTestHarness<MySqlConnection, MySqlCommand, MySqlDataReader>(
+                    connection,
+                    command => command.ExecuteNonQuery(),
+                    command => command.ExecuteScalar(),
+                    command => command.ExecuteReader(),
+                    (command, behavior) => command.ExecuteReader(behavior),
+                    command => command.ExecuteNonQueryAsync(),
+                    command => command.ExecuteScalarAsync(),
+                    executeReaderAsync: null,
+                    executeReaderWithBehaviorAsync: null
+                );
+
+
+                await testQueries.RunAsync();
+            }
+            */
+
+            using (var connection = CreateConnection())
+            {
+                var testQueries = new RelationalDatabaseTestHarness<DbConnection, DbCommand, DbDataReader>(
+                    connection,
+                    command => command.ExecuteNonQuery(),
+                    command => command.ExecuteScalar(),
+                    command => command.ExecuteReader(),
+                    (command, behavior) => command.ExecuteReader(behavior),
+                    command => command.ExecuteNonQueryAsync(),
+                    command => command.ExecuteScalarAsync(),
+                    command => command.ExecuteReaderAsync(),
+                    (command, behavior) => command.ExecuteReaderAsync(behavior)
+                );
+
+                await testQueries.RunAsync();
+            }
         }
 
-        private static string ConnectionString(string database)
+        private static MySqlConnection CreateConnection()
         {
-            return $"server={Host()};user=mysqldb;password=mysqldb;port=3306;database={database}";
-        }
+            var connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
 
-        private static void Main(string[] args)
-        {
-            Console.WriteLine($"Profiler attached: {Instrumentation.ProfilerAttached}");
-            Console.WriteLine($"Platform: {(Environment.Is64BitProcess ? "x64" : "x32")}");
-            Console.WriteLine();
-
-            Console.WriteLine("Opening the connection.");
-
-            string connStr = ConnectionString("world");
-            var conn = new MySqlConnection(connStr);
-            conn.Open();
-
-            Console.WriteLine("Creating the table for the continents.");
-
-            // Create table
-            var tableCommand =
-                new MySqlCommand(
-                    "DROP TABLE IF EXISTS `continent`; CREATE TABLE continent (continent_id INT AUTO_INCREMENT, name VARCHAR(255) NOT NULL, PRIMARY KEY(continent_id));",
-                    conn);
-            tableCommand.ExecuteNonQuery();
-
-            Console.WriteLine("Creating the continents.");
-
-            // Create continents
-            MySqlCommand createContinent;
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('Africa');", conn);
-            createContinent.ExecuteNonQuery();
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('Antarctica');", conn);
-            createContinent.ExecuteNonQuery();
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('Asia');", conn);
-            createContinent.ExecuteNonQuery();
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('Australia');", conn);
-            createContinent.ExecuteNonQuery();
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('Europe');", conn);
-            createContinent.ExecuteNonQuery();
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('North America');", conn);
-            createContinent.ExecuteNonQuery();
-            createContinent = new MySqlCommand("INSERT INTO continent (name) VALUES ('South America');", conn);
-            createContinent.ExecuteNonQuery();
-
-            try
+            if (connectionString == null)
             {
-                Console.WriteLine("Beginning to read the continents.");
-                string sql = "SELECT continent_id, name FROM continent";
-                var readContinents = new MySqlCommand(sql, conn);
-                using (var rdr = readContinents.ExecuteReader())
-                {
-                    while (rdr.Read())
-                    {
-                        Console.WriteLine(rdr[0] + " -- " + rdr[1]);
-                    }
-                    rdr.Close();
-                }
-                Console.WriteLine("Done reading the continents.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
+                var host = Environment.GetEnvironmentVariable("MYSQL_HOST") ?? "localhost";
+                connectionString = $"server={host};user=mysqldb;password=mysqldb;port=3306;database=dotnet_test";
             }
 
-            conn.Close();
-            Console.WriteLine("Done setting up, inserting, and reading from MySQL.");
+            return new MySqlConnection(connectionString);
         }
     }
 }
