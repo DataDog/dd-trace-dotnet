ff20dd835f81018ee187c25d751436c6f30a504a
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-10-10 15:26:31 +0200

    fix span counts and filter by operation name

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/DbCommandTests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/DbCommandTests.cs
@@ -24,13 +24,13 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
             {
                 Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
 
-                var spans = agent.WaitForSpans(4);
+                var spans = agent.WaitForSpans(count: 12, operationName: "sql-server.query");
                 Assert.True(spans.Count > 0, "expected at least one span");
 
                 foreach (var span in spans)
                 {
                     Assert.Equal("sql-server.query", span.Name);
-                    Assert.Equal($"Samples.DbCommand-sql-server", span.Service);
+                    Assert.Equal("Samples.DbCommand-sql-server", span.Service);
                     Assert.Equal(SpanTypes.Sql, span.Type);
                 }
             }
