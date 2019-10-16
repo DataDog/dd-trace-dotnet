ccd01ffd0b34644c075eb26e87b838904d54320e
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-10-16 14:02:54 -0400

    add a root span for the entire sample execution

--- samples/Samples.SqlServer/Program.cs
+++ samples/Samples.SqlServer/Program.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
+using Datadog.Trace;
 using Samples.DatabaseHelper;
 
 namespace Samples.SqlServer
@@ -10,39 +14,42 @@ namespace Samples.SqlServer
     {
         private static async Task Main()
         {
-            using (var connection = CreateConnection())
+            using (var root = Tracer.Instance.StartActive("root"))
             {
-                var testQueries = new RelationalDatabaseTestHarness<SqlConnection, SqlCommand, SqlDataReader>(
-                    connection,
-                    command => command.ExecuteNonQuery(),
-                    command => command.ExecuteScalar(),
-                    command => command.ExecuteReader(),
-                    (command, behavior) => command.ExecuteReader(behavior),
-                    command => command.ExecuteNonQueryAsync(),
-                    command => command.ExecuteScalarAsync(),
-                    command => command.ExecuteReaderAsync(),
-                    (command, behavior) => command.ExecuteReaderAsync(behavior)
-                );
+                using (var connection = CreateConnection())
+                {
+                    var testQueries = new RelationalDatabaseTestHarness<SqlConnection, SqlCommand, SqlDataReader>(
+                        connection,
+                        command => command.ExecuteNonQuery(),
+                        command => command.ExecuteScalar(),
+                        command => command.ExecuteReader(),
+                        (command, behavior) => command.ExecuteReader(behavior),
+                        command => command.ExecuteNonQueryAsync(),
+                        command => command.ExecuteScalarAsync(),
+                        command => command.ExecuteReaderAsync(),
+                        (command, behavior) => command.ExecuteReaderAsync(behavior)
+                    );
 
 
-                await testQueries.RunAsync();
-            }
+                    await testQueries.RunAsync();
+                }
 
-            using (var connection = CreateConnection())
-            {
-                var testQueries = new RelationalDatabaseTestHarness<DbConnection, DbCommand, DbDataReader>(
-                    connection,
-                    command => command.ExecuteNonQuery(),
-                    command => command.ExecuteScalar(),
-                    command => command.ExecuteReader(),
-                    (command, behavior) => command.ExecuteReader(behavior),
-                    command => command.ExecuteNonQueryAsync(),
-                    command => command.ExecuteScalarAsync(),
-                    command => command.ExecuteReaderAsync(),
-                    (command, behavior) => command.ExecuteReaderAsync(behavior)
-                );
+                using (var connection = CreateConnection())
+                {
+                    var testQueries = new RelationalDatabaseTestHarness<DbConnection, DbCommand, DbDataReader>(
+                        connection,
+                        command => command.ExecuteNonQuery(),
+                        command => command.ExecuteScalar(),
+                        command => command.ExecuteReader(),
+                        (command, behavior) => command.ExecuteReader(behavior),
+                        command => command.ExecuteNonQueryAsync(),
+                        command => command.ExecuteScalarAsync(),
+                        command => command.ExecuteReaderAsync(),
+                        (command, behavior) => command.ExecuteReaderAsync(behavior)
+                    );
 
-                await testQueries.RunAsync();
+                    await testQueries.RunAsync();
+                }
             }
         }
 
