cc495e2136ad535635749c76b3cdfa9fa59c5215
Zach Montoya <zach.montoya@datadoghq.com>
2019-10-24 10:57:08 -0700

    Create awesome, simple NLog demo.

---
+++ demo/AutomaticTraceIdInjection/NLogExample/Program.cs
@@ -0,0 +1,23 @@
+using Datadog.Trace;
+using NLog;
+
+namespace NLogExample
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            using (MappedDiagnosticsLogicalContext.SetScoped("order-number", 1024))
+            {
+                using (var scope = Tracer.Instance.StartActive($"NLogExample - Main()"))
+                {
+                    Logger.Info("Message inside a trace.");
+                }
+
+                Logger.Info("Message outside a trace.");
+            }
+        }
+    }
+}
