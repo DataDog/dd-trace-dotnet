d269062186030966a77ede1a40f33c0fdac7076b
colin-higgins <developerspike@gmail.com>
2019-10-25 15:57:03 -0400

    Make clear comments for metrics

--- src/Datadog.Trace/Metrics.cs
+++ src/Datadog.Trace/Metrics.cs
@@ -4,12 +4,22 @@ namespace Datadog.Trace
     {
         public const string SamplingPriority = "_sampling_priority_v1";
 
+        /// <summary>
+        /// To be set when the agent determines the sampling rate for a trace
+        /// Read: Agent Priority Sample Rate
+        /// </summary>
         public const string SamplingAgentDecision = "_dd.agent_psr";
 
+        /// <summary>
+        /// To be set when a sampling rule is applied to a trace
+        /// Read: Sampling Rule Priority Sample Rate
+        /// </summary>
         public const string SamplingRuleDecision = "_dd.rule_psr";
 
+        /// <summary>
+        /// To be set when a rate limiter is applied to a trace.
+        /// Read: Rate Limiter Priority Sample Rate
+        /// </summary>
         public const string SamplingLimitDecision = "_dd.limit_psr";
-
-        public const string OriginKey = "_dd.origin";
     }
 }
