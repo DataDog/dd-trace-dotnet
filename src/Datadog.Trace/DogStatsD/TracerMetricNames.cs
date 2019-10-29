d12adc37d96665053d096a9e50490609901e6e97
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-10-29 14:09:08 -0400

    add metric names

---
+++ src/Datadog.Trace/DogStatsD/TracerMetricNames.cs
@@ -0,0 +1,73 @@
+namespace Datadog.Trace.DogStatsD
+{
+    internal static class TracerMetricNames
+    {
+        public static class Api
+        {
+            /// <summary>
+            /// Count: Total number of API requests made
+            /// </summary>
+            public const string RequestCount = "datadog.tracer.api.requests";
+
+            /// <summary>
+            /// Count: Count of API responses.
+            /// This metric has an additional tag of "status: {code}" to group the responses by the HTTP response code.
+            /// This is different from <seealso cref="ErrorCount"/> in that this is all HTTP responses
+            /// regardless of status code, and <seealso cref="ErrorCount"/> is exceptions raised from making an API call.
+            /// </summary>
+            public const string ResponseCountByStatusCode = "datadog.tracer.api.responses";
+
+            /// <summary>
+            /// Count: Total number of exceptions raised by API calls.
+            /// This is different from receiving a 4xx or 5xx response.
+            /// It is a "timeout error" or something from making the API call.
+            /// </summary>
+            public const string ErrorCount = "datadog.tracer.api.errors";
+        }
+
+        public static class Queue
+        {
+            /// <summary>
+            /// Count: Total number of traces pushed into the queue, where "accepted - dropped = total to be flushed"
+            /// </summary>
+            public const string PushedTraces = "datadog.tracer.queue.accepted";
+
+            /// <summary>
+            /// Count: Total number of spans pushed into the queue
+            /// </summary>
+            public const string PushedSpans = "datadog.tracer.queue.accepted_lengths";
+
+            /// <summary>
+            /// Count: Total size in bytes of traces pushed into the queue
+            /// </summary>
+            public const string PushedBytes = "datadog.tracer.queue.accepted_size";
+
+            /// <summary>
+            /// Count: Total number of traces dropped by the queue.
+            /// This is the number of traces attempted to write into the queue above the max
+            /// (e.g. more than 1k traces, we start dropping traces)
+            /// </summary>
+            public const string DroppedTraces = "datadog.tracer.queue.dropped";
+
+            /// <summary>
+            /// Gauge: Number of traces pulled from the queue for flushing (should be between zero and queue.max_length)
+            /// </summary>
+            public const string PoppedTraces = "datadog.tracer.queue.length";
+
+            /// <summary>
+            /// Gauge: Total number of spans pulled from the queue for flushing
+            /// </summary>
+            public const string PoppedSpans = "datadog.tracer.queue.spans";
+
+            /// <summary>
+            /// Gauge: Size in bytes of traces pulled from the queue for flushing
+            /// </summary>
+            public const string PoppedBytes = "datadog.tracer.queue.size";
+
+            /// <summary>
+            /// Gauge: The maximum number of traces buffered by the background writer (this is static at 1k for now)
+            /// </summary>
+            public const string BufferedTracesLimit = "datadog.tracer.queue.max_length";
+        }
+    }
+}
