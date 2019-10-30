98305fa26daa0c1df070d8d64c13d02e05d58cd4
colin-higgins <developerspike@gmail.com>
2019-10-29 22:10:13 -0400

    Sampling rule tests

---
+++ test/Datadog.Trace.Tests/Sampling/RuleBasedSamplerTests.cs
@@ -0,0 +1,80 @@
+using System;
+using Datadog.Trace.Sampling;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Sampling
+{
+    [Collection(nameof(Datadog.Trace.Tests.Sampling))]
+    public class RuleBasedSamplerTests
+    {
+        [Fact]
+        public void Keep_Everything_Rule()
+        {
+            var sampler = new RuleBasedSampler(new NoLimits());
+            sampler.RegisterRule(new RegexSamplingRule(1, "Allow_all", ".*", ".*"));
+            RunSamplerTest(
+                sampler,
+                500,
+                1,
+                0,
+                () => Tracer.Instance.StartActive(operationName: "test"));
+        }
+
+        [Fact]
+        public void Keep_Nothing_Rule()
+        {
+            var sampler = new RuleBasedSampler(new NoLimits());
+            sampler.RegisterRule(new RegexSamplingRule(0, "Allow_nothing", ".*", ".*"));
+            RunSamplerTest(
+                sampler,
+                500,
+                0,
+                0,
+                () => Tracer.Instance.StartActive(operationName: "test"));
+        }
+
+        private void RunSamplerTest(
+            ISampler sampler,
+            int iterations,
+            float expectedAutoKeepRate,
+            float acceptableVariance,
+            Func<Scope> scopeFactory)
+        {
+            var sampleSize = iterations;
+            var autoKeeps = 0;
+            while (sampleSize-- > 0)
+            {
+                using (var scope = scopeFactory())
+                {
+                    var priority = sampler.GetSamplingPriority(scope.Span);
+                    if (priority == SamplingPriority.AutoKeep)
+                    {
+                        autoKeeps++;
+                    }
+                }
+            }
+
+            var autoKeepRate = autoKeeps / (float)iterations;
+
+            var lowerLimit = expectedAutoKeepRate * (1 - acceptableVariance);
+            var upperLimit = expectedAutoKeepRate * (1 + acceptableVariance);
+
+            Assert.True(
+                autoKeepRate >= lowerLimit && autoKeepRate <= upperLimit,
+                $"Expected between {lowerLimit} and {upperLimit}, actual rate is {autoKeepRate}.");
+        }
+
+        private class NoLimits : IRateLimiter
+        {
+            public bool Allowed(ulong traceId)
+            {
+                return true;
+            }
+
+            public float GetEffectiveRate()
+            {
+                return 1;
+            }
+        }
+    }
+}
