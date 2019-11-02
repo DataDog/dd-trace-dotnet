3f9e219b2ce7bdb7da2c1f060e4febc00b6a4b83
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-11-01 22:34:47 -0400

    improve tests, add test for disabled metrics

--- test/Datadog.Trace.Tests/DogStatsDTests.cs
+++ test/Datadog.Trace.Tests/DogStatsDTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Immutable;
 using System.Threading;
 using Datadog.Trace.Agent;
 using Datadog.Trace.Configuration;
@@ -20,25 +22,24 @@ namespace Datadog.Trace.Tests
         }
 
         [Fact]
-        public void SendMetrics()
+        public void Dont_send_metrics_when_disabled()
         {
             var statsd = new Mock<IStatsd>();
-            var api = new Mock<IApi>();
-            var agentPort = TcpPortProvider.GetOpenPort();
+            var spans = SendSpan(tracerMetricsEnabled: false, statsd);
 
-            using (var agent = new MockTracerAgent(agentPort))
-            {
-                var settings = new TracerSettings();
-                var agentWriter = new AgentWriter(api.Object, statsd.Object);
-                var tracer = new Tracer(settings, agentWriter, sampler: null, scopeManager: null, statsd: null);
+            Assert.True(spans.Count == 1, "Expected one span");
 
-                using (var rootScope = tracer.StartActive("root"))
-                {
-                    Thread.Sleep(5);
-                }
+            // no methods should be called on the IStatsd
+            statsd.VerifyNoOtherCalls();
+        }
 
-                agent.WaitForSpans(1);
-            }
+        [Fact]
+        public void Send_metrics_when_enabled()
+        {
+            var statsd = new Mock<IStatsd>();
+            var spans = SendSpan(tracerMetricsEnabled: true, statsd);
+
+            Assert.True(spans.Count == 1, "Expected one span");
 
             // for a single trace, these methods are called once with a value of "1"
             statsd.Verify(
@@ -46,36 +47,74 @@ namespace Datadog.Trace.Tests
                 Times.Once());
 
             statsd.Verify(
-                s => s.Add<Statsd.Counting, int>(TracerMetricNames.Queue.EnqueuedSpans, 1, 1D, null),
+                s => s.Add<Statsd.Counting, int>(TracerMetricNames.Queue.EnqueuedSpans, 1, 1, null),
                 Times.Once());
 
             statsd.Verify(
-                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedTraces, 1, 1D, null),
+                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedTraces, 1, 1, null),
                 Times.Once());
 
             statsd.Verify(
-                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedSpans, 1, 1D, null),
+                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedSpans, 1, 1, null),
+                Times.Once());
+
+            statsd.Verify(
+                s => s.Add<Statsd.Counting, int>(TracerMetricNames.Api.Requests, 1, 1, null),
+                Times.Once());
+
+            statsd.Verify(
+                s => s.Add<Statsd.Counting, int>(TracerMetricNames.Api.Responses, 1, 1, "status:200"),
                 Times.Once());
 
             // these methods can be called multiple times with a "0" value (no more traces left)
+            /*
             statsd.Verify(
-                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedTraces, 0, 1D, null),
+                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedTraces, 0, 1, null),
                 Times.AtLeastOnce);
 
             statsd.Verify(
-                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedSpans, 0, 1D, null),
+                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.DequeuedSpans, 0, 1, null),
                 Times.AtLeastOnce());
+            */
 
-            // these methods can be called multiple times with a "1000" value (the max buffer size, constant)
+            // these method can be called multiple times with a "1000" value (the max buffer size, constant)
             statsd.Verify(
-                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.MaxCapacity, 1000, 1D, null),
+                s => s.Add<Statsd.Gauge, int>(TracerMetricNames.Queue.MaxCapacity, 1000, 1, null),
                 Times.AtLeastOnce());
 
             statsd.Verify(
                 s => s.Send(),
                 Times.AtLeastOnce());
 
+            // no other methods should be called on the IStatsd
             statsd.VerifyNoOtherCalls();
+        }
+
+        private static IImmutableList<MockTracerAgent.Span> SendSpan(bool tracerMetricsEnabled, Mock<IStatsd> statsd)
+        {
+            IImmutableList<MockTracerAgent.Span> spans;
+            var agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            {
+                var settings = new TracerSettings
+                               {
+                                   AgentUri = new Uri($"http://localhost:{agentPort}"),
+                                   TracerMetricsEnabled = tracerMetricsEnabled
+                               };
+
+                var tracer = new Tracer(settings, agentWriter: null, sampler: null, scopeManager: null, statsd: statsd.Object);
+
+                using (var scope = tracer.StartActive("root"))
+                {
+                    scope.Span.ResourceName = "resource";
+                    Thread.Sleep(5);
+                }
+
+                spans = agent.WaitForSpans(1);
+            }
+
+            return spans;
         }
     }
 }
