337af3658cf6919ebdc6b98428eebd045c955906
Zach Montoya <zach.montoya@datadoghq.com>
2019-11-25 15:52:19 -0800

    Profiler changes to require an exact version match on the managed profiler assembly.

--- src/Datadog.Trace.ClrProfiler.Native/clr_helpers.h
+++ src/Datadog.Trace.ClrProfiler.Native/clr_helpers.h
@@ -200,19 +200,21 @@ struct RuntimeInformation {
 struct AssemblyInfo {
   const AssemblyID id;
   const WSTRING name;
+  const ModuleID manifest_module_id;
   const AppDomainID app_domain_id;
   const WSTRING app_domain_name;
 
-  AssemblyInfo() : id(0), name(""_W), app_domain_id(0), app_domain_name(""_W) {}
+  AssemblyInfo() : id(0), name(""_W), manifest_module_id(0), app_domain_id(0), app_domain_name(""_W) {}
 
-  AssemblyInfo(AssemblyID id, WSTRING name, AppDomainID app_domain_id,
+  AssemblyInfo(AssemblyID id, WSTRING name, ModuleID manifest_module_id, AppDomainID app_domain_id,
                WSTRING app_domain_name)
       : id(id),
         name(name),
+        manifest_module_id(manifest_module_id),
         app_domain_id(app_domain_id),
         app_domain_name(app_domain_name) {}
 
-  bool is_valid() const { return id != 0; }
+  bool IsValid() const { return id != 0; }
 };
 
 struct AssemblyMetadata {
@@ -230,7 +232,7 @@ struct AssemblyMetadata {
         assembly_token(assembly_token),
         version(Version(major, minor, build, revision)) {}
 
-  bool is_valid() const { return module_id != 0; }
+  bool IsValid() const { return module_id != 0; }
 };
 
 struct ModuleInfo {
