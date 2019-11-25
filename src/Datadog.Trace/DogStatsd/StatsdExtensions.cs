45ea4b269bcb1ede6e01873622c7e0eaaa04f7f3
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-11-25 16:19:58 -0500

    add Statsd extension methods SendException() and SendWarning()

--- src/Datadog.Trace/DogStatsd/StatsdExtensions.cs
+++ src/Datadog.Trace/DogStatsd/StatsdExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Datadog.Trace.Vendors.StatsdClient;
 
 namespace Datadog.Trace.DogStatsd
@@ -13,6 +15,44 @@ namespace Datadog.Trace.DogStatsd
         public static IStatsd AppendSetGauge(this IStatsd statsd, string name, int value, double sampleRate = 1, string[] tags = null)
         {
             statsd?.Add<Statsd.Gauge, int>(name, value, sampleRate, tags);
+            return statsd;
+        }
+
+        public static IStatsd SendException(this IStatsd statsd, Exception exception, string source, string message, string[] tags = null)
+        {
+            if (statsd != null)
+            {
+                string[] exceptionTags =
+                {
+                    $"source:{source}",
+                    $"message:{message}",
+                    $"exception-type:{exception.GetType().FullName}",
+                    $"exception-message:{exception.Message}",
+                };
+
+                string[] allTags = exceptionTags.Concat(tags ?? Enumerable.Empty<string>()).ToArray();
+
+                statsd.Send<Statsd.Counting, int>(TracerMetricNames.Health.Exceptions, value: 1, sampleRate: 1, allTags);
+            }
+
+            return statsd;
+        }
+
+        public static IStatsd SendWarning(this IStatsd statsd, string source, string message, string[] tags = null)
+        {
+            if (statsd != null)
+            {
+                string[] warningTags =
+                {
+                    $"source:{source}",
+                    $"message:{message}"
+                };
+
+                string[] allTags = warningTags.Concat(tags ?? Enumerable.Empty<string>()).ToArray();
+
+                statsd.Send<Statsd.Counting, int>(TracerMetricNames.Health.Warnings, value: 1, sampleRate: 1, allTags);
+            }
+
             return statsd;
         }
     }
