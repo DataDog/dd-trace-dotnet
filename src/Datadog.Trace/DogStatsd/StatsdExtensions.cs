7b4c0dc6ddf80a80444f8e1c231affbdf8971f96
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-11-25 16:19:32 -0500

    add missing null checks

--- src/Datadog.Trace/DogStatsd/StatsdExtensions.cs
+++ src/Datadog.Trace/DogStatsd/StatsdExtensions.cs
@@ -6,13 +6,13 @@ namespace Datadog.Trace.DogStatsd
     {
         public static IStatsd AppendIncrementCount(this IStatsd statsd, string name, int value = 1, double sampleRate = 1, string[] tags = null)
         {
-            statsd.Add<Statsd.Counting, int>(name, value, sampleRate, tags);
+            statsd?.Add<Statsd.Counting, int>(name, value, sampleRate, tags);
             return statsd;
         }
 
         public static IStatsd AppendSetGauge(this IStatsd statsd, string name, int value, double sampleRate = 1, string[] tags = null)
         {
-            statsd.Add<Statsd.Gauge, int>(name, value, sampleRate, tags);
+            statsd?.Add<Statsd.Gauge, int>(name, value, sampleRate, tags);
             return statsd;
         }
     }
