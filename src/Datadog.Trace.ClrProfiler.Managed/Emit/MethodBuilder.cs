b081b57a2b7a8747cf2dfdf96e8c70f2594e1d66
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-11-25 16:20:49 -0500

    send exception and warning metrics

--- src/Datadog.Trace.ClrProfiler.Managed/Emit/MethodBuilder.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Emit/MethodBuilder.cs
@@ -6,7 +6,9 @@ using System.Reflection;
 using System.Reflection.Emit;
 using Datadog.Trace.ClrProfiler.Helpers;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.DogStatsd;
 using Datadog.Trace.Logging;
+using Datadog.Trace.Vendors.StatsdClient;
 using Sigil;
 
 namespace Datadog.Trace.ClrProfiler.Emit
@@ -417,6 +419,9 @@ namespace Datadog.Trace.ClrProfiler.Emit
         {
             var logDetail = $"mdToken {_mdToken} on {_concreteTypeName}.{_methodName} in {_resolutionModule?.FullyQualifiedName ?? "NULL"}, {_resolutionModule?.ModuleVersionId ?? _moduleVersionId}";
             Log.Warning($"Using fallback method matching ({logDetail})");
+
+            string[] tags = { $"instrumented-method:{_concreteTypeName}.{_methodName}" };
+            Tracer.Instance.Statsd?.SendWarning(source: nameof(MethodBuilder), message: "Using fallback method matching", tags);
 
             var methods =
                 _concreteType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/LoggerExtensions.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/LoggerExtensions.cs
@@ -1,5 +1,7 @@
 using System;
 using Datadog.Trace.ClrProfiler.Helpers;
+using Datadog.Trace.DogStatsd;
+using Datadog.Trace.Vendors.StatsdClient;
 
 namespace Datadog.Trace.ClrProfiler.Integrations
 {
@@ -15,7 +17,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
             string methodName,
             string instanceType = null,
             string[] relevantArguments = null)
-            {
+        {
             var instrumentedMethod = $"{instrumentedType}.{methodName}(...)";
 
             if (instanceType != null)
@@ -32,6 +34,9 @@ namespace Datadog.Trace.ClrProfiler.Integrations
             logger.Error(
                 exception,
                 $"Error (MVID: {moduleVersionId}, mdToken: {mdToken}, opCode: {opCode}) could not retrieve: {instrumentedMethod}");
-            }
+
+            string[] tags = { $"instrumented-method:{instrumentedMethod}" };
+            Tracer.Instance.Statsd?.SendException(exception, source: instrumentedType, message: "Error retrieving instrumented method", tags);
+        }
     }
 }
