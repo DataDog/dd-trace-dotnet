82643b50d22b5033fdcb8dc998f95cded7b9bb77
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-12-03 17:31:45 -0500

    add missing "using" for extension method that moved

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/AspNet/AspNetCoreMvc3Integration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/AspNet/AspNetCoreMvc3Integration.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Datadog.Trace.ClrProfiler.Emit;
 using Datadog.Trace.ClrProfiler.ExtensionMethods;
+using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.Headers;
 using Datadog.Trace.Logging;
 
