6974054706c68ec6d107741e642e1d1e85820ad0
Colin Higgins <colin-higgins@users.noreply.github.com>
2019-12-04 11:05:59 -0500

    Rework variance to make limiter tests less flaky (#560)
    * Rework variance to make limiter tests less flaky

--- test/Datadog.Trace.Tests/Sampling/RateLimiterTests.cs
+++ test/Datadog.Trace.Tests/Sampling/RateLimiterTests.cs
@@ -74,9 +74,10 @@ namespace Datadog.Trace.Tests.Sampling
             var totalMilliseconds = result.TimeElapsed.TotalMilliseconds;
 
             var expectedLimit = totalMilliseconds * actualIntervalLimit / 1_000;
-            var acceptableDifference = (actualIntervalLimit * 0.80);
-            var upperLimit = expectedLimit + acceptableDifference;
-            var lowerLimit = expectedLimit - acceptableDifference;
+
+            var acceptableVariance = (actualIntervalLimit * 1.0);
+            var upperLimit = expectedLimit + acceptableVariance;
+            var lowerLimit = expectedLimit - acceptableVariance;
 
             Assert.True(
                 result.TotalAllowed >= lowerLimit && result.TotalAllowed <= upperLimit,
@@ -88,9 +89,13 @@ namespace Datadog.Trace.Tests.Sampling
             var totalExpectedAllowed = 2 * actualIntervalLimit;
             var expectedRate = totalExpectedAllowed / (float)totalExpectedSent;
 
-            var maxPercentVariance = 0.35f;
-            var lowestRate = expectedRate - maxPercentVariance;
-            var highestRate = expectedRate + maxPercentVariance;
+            var lowestRate = expectedRate - 0.40f;
+            if (lowestRate < 0)
+            {
+                lowestRate = expectedRate / 2;
+            }
+
+            var highestRate = expectedRate + 0.40f;
 
             Assert.True(
                 result.ReportedRate >= lowestRate && result.ReportedRate <= highestRate,
