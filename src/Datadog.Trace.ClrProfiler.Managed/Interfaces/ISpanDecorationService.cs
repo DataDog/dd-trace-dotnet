56e19cb57c34275d0945e3595aa14c517b21b608
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-12-05 12:42:56 -0500

    remove last vestiges of the now-unused DecorationsServices and its breathren (DecorationSource, IntegrationDelegate, TagsProducer, etc)

--- src/Datadog.Trace.ClrProfiler.Managed/Interfaces/ISpanDecorationService.cs
+++
@@ -1,9 +0,0 @@
-using Datadog.Trace.Interfaces;
-
-namespace Datadog.Trace.ClrProfiler.Interfaces
-{
-    internal interface ISpanDecorationService
-    {
-        void Decorate(ISpan span, ISpanDecorationSource with);
-    }
-}
