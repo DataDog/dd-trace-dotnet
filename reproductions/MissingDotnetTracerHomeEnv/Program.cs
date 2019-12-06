66885e19877ff27fd96afd15666f2941197e5d81
Zach Montoya <zach.montoya@datadoghq.com>
2019-12-06 10:03:53 -0800

    Add MissingDotnetTracerHomeEnv to reproductions where DD_INTEGRATIONS is set but not DD_DOTNET_TRACER_HOME. No crash, but no instrumentation.

---
+++ reproductions/MissingDotnetTracerHomeEnv/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MissingDotnetTracerHomeEnv
+{
+    internal class Program
+    {
+        private static async Task<int> Main()
+        {
+            try
+            {
+                var baseAddress = new Uri("https://www.example.com/");
+                var regularHttpClient = new HttpClient { BaseAddress = baseAddress };
+                await regularHttpClient.GetAsync("default-handler");
+                Console.WriteLine("All is well!");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return (int)ExitCode.UnknownError;
+            }
+
+            return (int)ExitCode.Success;
+        }
+    }
+
+    enum ExitCode : int
+    {
+        Success = 0,
+        UnknownError = -10
+    }
+}
