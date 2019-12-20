ddd782e0719bd30363f926a9dae29f1fdfde6f0a
Zach Montoya <zach.montoya@datadoghq.com>
2019-12-20 12:26:11 -0700

    Remove function ElementTypeIsAlwaysValueType which is no longer necessary

--- src/Datadog.Trace.ClrProfiler.Native/clr_helpers.h
+++ src/Datadog.Trace.ClrProfiler.Native/clr_helpers.h
@@ -361,8 +361,6 @@ bool CreateAssemblyRefToMscorlib(
     const ComPtr<IMetaDataAssemblyEmit>& assembly_emit,
     mdAssemblyRef* mscorlib_ref);
 
-bool ElementTypeIsAlwaysValueType(CorElementType element_type);
-
 bool ReturnTypeIsValueTypeOrGeneric(const ComPtr<IMetaDataImport2>& metadata_import,
                       const ComPtr<IMetaDataEmit2>& metadata_emit,
                       const ComPtr<IMetaDataAssemblyEmit>& assembly_emit,
