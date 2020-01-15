ec924f8d693669f3b70efecb645b8fc1d6dea456
Zach Montoya <zach.montoya@datadoghq.com>
2020-01-15 14:48:58 -0800

    Apparently Samples.AspNetCoreWebApi31 generates spans when run in CI because for some reason the logger from Microsoft.AspNetCore.Mvc.Infrastructure.ControllerActionInvoker has a LogLevel of Information or more descriptive. Now, make the sample fail by using the Serilog example from issue #578.

--- samples/Samples.AspNetCoreWebApi31/Program.cs
+++ samples/Samples.AspNetCoreWebApi31/Program.cs
@@ -6,18 +6,41 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Samples.AspNetCoreWebApi31
 {
     public class Program
     {
+        // Following Serilog setup instructions for ASP.NET Core 3
+        // from https://nblumhardt.com/2019/10/serilog-in-aspnetcore-3/
+        // to recreate https://github.com/DataDog/dd-trace-dotnet/issues/578
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
+                .WriteTo.Console()
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
