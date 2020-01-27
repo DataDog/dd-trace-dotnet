af414c32f4ffc7a605f94f71384a5d50153370c7
Zach Montoya <zach.montoya@datadoghq.com>
2020-01-27 14:20:35 -0500

    Revise log4net sample to remove the order-number property after it is used.

--- customer-samples/AutomaticTraceIdInjection/Log4NetExample/Program.cs
+++ customer-samples/AutomaticTraceIdInjection/Log4NetExample/Program.cs
@@ -14,11 +14,18 @@ namespace Log4NetExample
             var logRepository = LogManager.GetRepository(typeof(Program).Assembly);
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
-            LogicalThreadContext.Properties["order-number"] = 1024;
-            log.Info("Message before a trace.");
-            using (var scope = Tracer.Instance.StartActive("Log4NetExample - Main()"))
+            try
             {
-                log.Info("Message during a trace.");
+                LogicalThreadContext.Properties["order-number"] = 1024;
+                log.Info("Message before a trace.");
+                using (var scope = Tracer.Instance.StartActive("Log4NetExample - Main()"))
+                {
+                    log.Info("Message during a trace.");
+                }
+            }
+            finally
+            {
+                LogicalThreadContext.Properties.Remove("order-number");
             }
 
             log.Info("Message after a trace.");
