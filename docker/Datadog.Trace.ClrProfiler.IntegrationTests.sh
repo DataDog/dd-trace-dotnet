32591654da3a97807a79bf5bfb9e377c0b27d38e
Zach Montoya <zach.montoya@datadoghq.com>
2020-02-01 12:55:27 -0800

    Run integration tests from the publish folder to make sure all target frameworks have testhost.dll

--- docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -4,10 +4,10 @@ set -euxo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../
 
 docker/with-profiler-logs.bash \
-    dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
+    dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
 
 docker/with-profiler-logs.bash \
-    dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
+    dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
 
 docker/with-profiler-logs.bash \
     wait-for-it servicestackredis:6379 -- \
@@ -17,4 +17,4 @@ docker/with-profiler-logs.bash \
     wait-for-it sqlserver:1433 -- \
     wait-for-it mongo:27017 -- \
     wait-for-it postgres:5432 -- \
-    dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results
+    dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results
