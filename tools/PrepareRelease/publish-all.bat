c9cc446ff8bf9ea9081746b1e275b26d4cc7d6f5
colin-higgins <developerspike@gmail.com>
2020-02-07 10:52:58 -0500

    fix publish script

--- tools/PrepareRelease/publish-all.bat
+++ tools/PrepareRelease/publish-all.bat
@@ -3,6 +3,7 @@ REM SET SOLUTION_DIR=C:\Github\dd-trace-dotnet
 
 SET TOOL_BUILD_CONFIG=Release
 SET INTEGRATIONS_PROJ=%SOLUTION_DIR%\src\Datadog.Trace.ClrProfiler.Managed\Datadog.Trace.ClrProfiler.Managed.csproj
+SET OUTPUT_DIR=%SOLUTION_DIR%\tools\PrepareRelease\bin\tracer-home
 
 dotnet publish %INTEGRATIONS_PROJ% -c %TOOL_BUILD_CONFIG% -f net45 -o "%OUTPUT_DIR%\net45"
 dotnet publish %INTEGRATIONS_PROJ% -c %TOOL_BUILD_CONFIG% -f net461 -o "%OUTPUT_DIR%\net461"
