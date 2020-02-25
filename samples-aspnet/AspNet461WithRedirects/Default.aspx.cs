3622dd1cc6b399700e1bc393301b3a5123690097
Zach Montoya <zach.montoya@datadoghq.com>
2020-02-24 21:40:52 -0800

    Add samples to replicate the issue in IIS. AspNet461 works fine, but AspNet461WithRedirects causes the issues by requiring an app-local System.Net.Http to be loaded.

---
+++ samples-aspnet/AspNet461WithRedirects/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNet461WithRedirects
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+
+            // Add dependency on System.Net.Http
+            try
+            {
+                var response = client.GetAsync("http://www.contoso.com/").GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
+    }
+}
---
+++ samples-aspnet/AspNet461WithRedirects/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNet461WithRedirects.Global" Language="C#" %>
