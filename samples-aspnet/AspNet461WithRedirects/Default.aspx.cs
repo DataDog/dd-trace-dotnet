df86b07d4245fbfb472601265c554b98eb65803f
Zach Montoya <zach.montoya@datadoghq.com>
2020-02-25 15:00:04 -0800

    Add Sigil binding redirect so now we have two Tracer dependencies that are forced to load app-domain local.

--- samples-aspnet/AspNet461WithRedirects/Default.aspx.cs
+++ samples-aspnet/AspNet461WithRedirects/Default.aspx.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Sigil;
 
 namespace AspNet461WithRedirects
 {
@@ -12,11 +13,13 @@ namespace AspNet461WithRedirects
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            var client = new HttpClient();
+            // Add dependency on Sigil
+            var sigilEnum = Sigil.OptimizationOptions.All;
 
             // Add dependency on System.Net.Http
             try
             {
+                var client = new HttpClient();
                 var response = client.GetAsync("http://www.contoso.com/").GetAwaiter().GetResult();
                 response.EnsureSuccessStatusCode();
             }
