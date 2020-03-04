54400775cfded2e316236c9057ce06eb7e086278
Colin Higgins <colin-higgins@users.noreply.github.com>
2020-03-04 14:50:23 -0500

    Version bump 1.13.4-prerelease (#670)

--- tools/Datadog.Core.Tools/TracerVersion.cs
+++ tools/Datadog.Core.Tools/TracerVersion.cs
@@ -18,7 +18,7 @@ namespace Datadog.Core.Tools
         /// <summary>
         /// The patch portion of the current version.
         /// </summary>
-        public const int Patch = 3;
+        public const int Patch = 4;
 
         /// <summary>
         /// Whether the current release is a pre-release
