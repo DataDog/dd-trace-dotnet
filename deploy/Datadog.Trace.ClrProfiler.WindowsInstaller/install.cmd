bc1384aedb919014bed72bdf7c5dfb664102f033
Zach Montoya <zach.montoya@datadoghq.com>
2020-03-26 11:22:46 -0700

    Modify install/uninstall scripts to better handle error conditions, and execute them via the Quiet Execution Custom Action so that standard out from the scripts can be recorded in the MSI log.

--- deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/install.cmd
+++ deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/install.cmd
@@ -1,1 +1,15 @@
-%systemroot%\system32\inetsrv\APPCMD set config /section:system.webServer/modules /+[name='DatadogTracingModule',type='Datadog.Trace.AspNet.TracingHttpModule,Datadog.Trace.AspNet,Version=1.0.0.0,Culture=neutral,PublicKeyToken=def86d061d0d2eeb',preCondition='managedHandler']
+@echo off
+setlocal EnableDelayedExpansion
+
+set DATADOG_APPCMD_CMDLINE=%systemroot%\system32\inetsrv\appcmd.exe set config /section:system.webServer/modules /+[name='DatadogTracingModule',type='Datadog.Trace.AspNet.TracingHttpModule,Datadog.Trace.AspNet,Version=1.0.0.0,Culture=neutral,PublicKeyToken=def86d061d0d2eeb']
+
+echo Executing install.cmd at %date% %time%
+IF EXIST %systemroot%\system32\inetsrv\appcmd.exe (
+    echo Running: %DATADOG_APPCMD_CMDLINE%
+    %DATADOG_APPCMD_CMDLINE% 2>&1
+) ELSE (
+    echo "%systemroot%\system32\inetsrv\appcmd.exe" doesn't exist
+)
+
+REM Always report success
+exit /b 0
--- deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/uninstall.cmd
+++ deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/uninstall.cmd
@@ -1,1 +1,15 @@
-%systemroot%\system32\inetsrv\APPCMD set config /section:system.webServer/modules /-[name='DatadogTracingModule']
+@echo off
+setlocal EnableDelayedExpansion
+
+set DATADOG_APPCMD_CMDLINE=%systemroot%\system32\inetsrv\appcmd.exe set config /section:system.webServer/modules /-[name='DatadogTracingModule']
+
+echo Executing uninstall.cmd at %date% %time%
+IF EXIST %systemroot%\system32\inetsrv\appcmd.exe (
+    echo Running: %DATADOG_APPCMD_CMDLINE%
+    %DATADOG_APPCMD_CMDLINE% 2>&1
+) ELSE (
+    echo "%systemroot%\system32\inetsrv\appcmd.exe" doesn't exist
+)
+
+REM Always report success
+exit /b 0
