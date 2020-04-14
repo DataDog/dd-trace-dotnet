12f61be9189a4c6329c77fd1531ccaa185a606e6
Zach Montoya <zach.montoya@datadoghq.com>
2020-04-14 09:48:29 -0700

    Try to use TcpListener to grab a port instead of using the previous logic.

--- tools/Datadog.Core.Tools/TcpPortProvider.cs
+++ tools/Datadog.Core.Tools/TcpPortProvider.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Datadog.Core.Tools
 {
@@ -15,64 +10,20 @@ namespace Datadog.Core.Tools
     /// </summary>
     public static class TcpPortProvider
     {
-        private static readonly object PortLock = new { };
-        private static readonly ConcurrentBag<int> ReturnedPorts = new ConcurrentBag<int>();
-        [ThreadStatic]
-        private static readonly int MinPort = GetStartingPort();
-
         public static int GetOpenPort()
         {
-            lock (PortLock)
+            TcpListener tcpListener = null;
+            try
             {
-                var usedPorts = GetUsedPorts();
-
-                for (int port = MinPort; port < ushort.MaxValue; port++)
-                {
-                    if (!ReturnedPorts.Contains(port) && !usedPorts.Contains(port))
-                    {
-                        // don't return a port that was previously returned,
-                        // even if it is not in use (it could still be used
-                        // by the code that is was returned to)
-                        ReturnedPorts.Add(port);
-                        return port;
-                    }
-                }
+                tcpListener = new TcpListener(IPAddress.Loopback, 0);
+                tcpListener.Start();
+                var port = ((IPEndPoint)tcpListener.LocalEndpoint).Port;
+                return port;
             }
-
-            throw new Exception("No open TCP port found.");
-        }
-
-        public static HashSet<int> GetUsedPorts()
-        {
-            var usedPorts = IPGlobalProperties.GetIPGlobalProperties()
-                                              .GetActiveTcpListeners()
-                                              .Select(ipEndPoint => ipEndPoint.Port);
-            usedPorts.Concat(IPGlobalProperties.GetIPGlobalProperties()
-                                               .GetActiveTcpConnections()
-                                               .Select(information => information.LocalEndPoint.Port));
-
-            return new HashSet<int>(usedPorts);
-        }
-
-        private static int GetStartingPort()
-        {
-            // pick a starting port from the ephemeral port range (49152 – 65535),
-            // use process and threads ids to try to get different ports for each thread.
-            // https://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
-            const int startPort = 49152;
-            const int endPort = 65535;
-            const int poolSize = endPort - startPort;
-            int hash = 17;
-
-            unchecked
+            finally
             {
-                hash = (hash * 23) + Process.GetCurrentProcess().Id.GetHashCode();
-                hash = (hash * 23) + AppDomain.CurrentDomain.Id.GetHashCode();
-                hash = (hash * 23) + Thread.CurrentThread.ManagedThreadId.GetHashCode();
+                tcpListener?.Stop();
             }
-
-            int offset = hash % poolSize;
-            return startPort + offset;
         }
     }
 }
