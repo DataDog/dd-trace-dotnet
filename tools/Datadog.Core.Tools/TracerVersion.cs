ee43c606c977d5510565a1b2a4d3ba814f45ab72
Zach Montoya <zach.montoya@datadoghq.com>
2020-04-20 10:51:46 -0700

    Bump version number to 1.16.1 (#710)

--- tools/Datadog.Core.Tools/TracerVersion.cs
+++ tools/Datadog.Core.Tools/TracerVersion.cs
@@ -18,7 +18,7 @@ namespace Datadog.Core.Tools
         /// <summary>
         /// The patch portion of the current version.
         /// </summary>
-        public const int Patch = 0;
+        public const int Patch = 1;
 
         /// <summary>
         /// Whether the current release is a pre-release
