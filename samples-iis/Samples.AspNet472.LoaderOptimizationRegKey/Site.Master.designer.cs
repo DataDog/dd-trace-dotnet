7eec97122187a6964325980f6799716118e3f039
Zach Montoya <zach.montoya@datadoghq.com>
2020-04-28 17:46:31 -0700

    Remove jquery, Modernizr, MSAjax, and Webforms scripts.

--- samples-iis/Samples.AspNet472.LoaderOptimizationRegKey/Site.Master.designer.cs
+++ samples-iis/Samples.AspNet472.LoaderOptimizationRegKey/Site.Master.designer.cs
@@ -15,15 +15,6 @@ namespace Samples.AspNet472.LoaderOptimizationRegKey
     {
 
         /// <summary>
-        /// HeadContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
