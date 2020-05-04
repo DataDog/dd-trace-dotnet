d567c5eba050afbb8413cab979120fb86e27c963
Colin Higgins <colin-higgins@users.noreply.github.com>
2020-05-04 17:26:02 -0400

    Version 1.16.2 bump (#717)
    * Version bump to 1.16.2
    
    * Clear directory pre publish to prevent stale artifact confusion

--- tools/PrepareRelease/publish-all.bat
+++ tools/PrepareRelease/publish-all.bat
@@ -5,6 +5,8 @@ SET TOOL_BUILD_CONFIG=Release
 SET INTEGRATIONS_PROJ=%SOLUTION_DIR%\src\Datadog.Trace.ClrProfiler.Managed\Datadog.Trace.ClrProfiler.Managed.csproj
 SET OUTPUT_DIR=%SOLUTION_DIR%\tools\PrepareRelease\bin\tracer-home
 
+RMDIR "%OUTPUT_DIR%" /S /Q
+
 dotnet publish %INTEGRATIONS_PROJ% -c %TOOL_BUILD_CONFIG% -f net45 -o "%OUTPUT_DIR%\net45"
 dotnet publish %INTEGRATIONS_PROJ% -c %TOOL_BUILD_CONFIG% -f net461 -o "%OUTPUT_DIR%\net461"
 dotnet publish %INTEGRATIONS_PROJ% -c %TOOL_BUILD_CONFIG% -f netstandard2.0 -o "%OUTPUT_DIR%\netstandard2.0"
