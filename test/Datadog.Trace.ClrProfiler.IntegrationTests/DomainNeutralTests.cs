ee5b56bec0e258f494f93b04902fb48b370def8d
Zach Montoya <zach.montoya@datadoghq.com>
2020-05-29 10:53:59 -0700

    Add sanity check test to make sure we don't crash when the managed profiler cannot be loaded and we invoke the new domain-neutral instrumentation path

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/DomainNeutralTests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/DomainNeutralTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Datadog.Core.Tools;
@@ -52,6 +54,29 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                     Assert.Equal(SpanTypes.Http, span.Type);
                     Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
                 }
+            }
+        }
+
+        [TargetFrameworkVersionsFact("net452;net461")]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public void DoesNotCrashInBadConfiguration()
+        {
+            // Set bad configuration
+            SetEnvironmentVariable("DD_DOTNET_TRACER_HOME", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            Assert.False(typeof(Instrumentation).Assembly.GlobalAssemblyCache, "Datadog.Trace.ClrProfiler.Managed was loaded from the GAC. Ensure that the assembly and its dependencies are not installed in the GAC when running this test.");
+
+            int agentPort = TcpPortProvider.GetOpenPort();
+            int httpPort = TcpPortProvider.GetOpenPort();
+
+            Output.WriteLine($"Assigning port {agentPort} for the agentPort.");
+            Output.WriteLine($"Assigning port {httpPort} for the httpPort.");
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port, arguments: $"HttpClient Port={httpPort}"))
+            {
+                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
             }
         }
     }
