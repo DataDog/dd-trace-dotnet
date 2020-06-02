2a14d07eae278439c06baa1a8db456eb47bb2ffe
Kevin Gosse <krix33@gmail.com>
2020-06-02 14:54:29 +0200

    Remove array allocation from NextUInt63

--- src/Datadog.Trace/ExtensionMethods/RandomExtensions.cs
+++ src/Datadog.Trace/ExtensionMethods/RandomExtensions.cs
@@ -6,10 +6,13 @@ namespace Datadog.Trace.ExtensionMethods
     {
         public static ulong NextUInt63(this Random rnd)
         {
-            // From https://stackoverflow.com/a/677390
-            var buffer = new byte[sizeof(ulong)];
-            rnd.NextBytes(buffer);
-            return BitConverter.ToUInt64(buffer, 0) & 0x7FFFFFFFFFFFFFFF;
+            long high = rnd.Next(int.MinValue, int.MaxValue);
+            long low = rnd.Next(int.MinValue, int.MaxValue);
+
+            // Concatenate both values, and truncate the 32 top bits from low
+            var value = high << 32 | (low & 0xFFFFFFFF);
+
+            return (ulong)value & 0x7FFFFFFFFFFFFFFF;
         }
     }
 }
