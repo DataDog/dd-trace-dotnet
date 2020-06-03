c1b74b7b1e19e018301e5e07f0ae0a4afe04c563
colin-higgins <developerspike@gmail.com>
2020-06-03 11:00:21 -0400

    Simplify

---
+++ samples/Samples.OpenTracing/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+namespace Samples.SqlServer
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            await Task.Delay(1);
+        }
+    }
+}
