8489df6cfa6c692c640c2d62e53a757a90117e92
colin-higgins <developerspike@gmail.com>
2020-06-03 11:10:36 -0400

    Remove a line

--- src/Datadog.Trace/Logging/DatadogLogging.cs
+++ src/Datadog.Trace/Logging/DatadogLogging.cs
@@ -102,7 +102,6 @@ namespace Datadog.Trace.Logging
             }
             catch
             {
-
                 try
                 {
                     message = string.Format(message, args);
