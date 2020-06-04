e852f2a9ff52b3bf6c587a4464e4f5f4ade73b2a
colin-higgins <developerspike@gmail.com>
2020-06-04 15:21:29 -0400

    save a little work

--- samples/Samples.OpenTracing/Program.cs
+++ samples/Samples.OpenTracing/Program.cs
@@ -1,15 +1,54 @@
-using System;
-using System.Data;
-using System.Data.Common;
-using System.Data.SqlClient;
+using System.Threading;
 using System.Threading.Tasks;
+using Datadog.Trace;
+using Datadog.Trace.OpenTracing;
+
 namespace Samples.SqlServer
 {
     internal static class Program
     {
         private static async Task Main()
         {
-            await Task.Delay(1);
+            var tracer = OpenTracingTracerFactory.WrapTracer(Tracer.Instance);
+
+            // Manually create a span for sort orders
+            using (var span = tracer.BuildSpan("manualtrace.sortorders").StartActive())
+            {
+                SortOrders();
+            }
+
+            await Task.Run(
+                () =>
+                {
+                    // Manually create a span for sort orders running asynchronously
+                    using (var span = tracer.BuildSpan("manualtrace.sortorders").StartActive())
+                    {
+                        SortOrders();
+                    }
+                });
+
+
+            // Manually create a span for sort orders
+            using (var span = tracer.BuildSpan("manualtrace.sortorders").StartActive())
+            {
+                SortOrders();
+            }
+
+            using (var parentScope = Tracer.Instance.StartActive("manual.sortorders"))
+            {
+                using (var childScope = Tracer.Instance.StartActive("manual.sortorders.childspan"))
+                {
+                    // to use tracer with ASP.NET Core dependency injection
+                    SortOrders();
+                }
+            }
+
+
+        }
+
+        private static void SortOrders()
+        {
+            Thread.Sleep(50);
         }
     }
 }
