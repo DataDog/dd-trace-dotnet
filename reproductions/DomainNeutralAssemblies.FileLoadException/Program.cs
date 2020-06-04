4a5ff2bba0a6ed6211f495abc7575c095006c16f
Zach Montoya <zach.montoya@datadoghq.com>
2020-06-03 19:05:37 -0700

    - Run the HttpBindingRedirect program on every TFM even though it will fail on net45x and net472+ - Add an Http NuGet example without binding redirects - Add a Newtonsoft.Json NuGet example with binding redirects

--- reproductions/DomainNeutralAssemblies.FileLoadException/Program.cs
+++ reproductions/DomainNeutralAssemblies.FileLoadException/Program.cs
@@ -17,16 +17,32 @@ namespace DomainNeutralAssemblies.FileLoadException
                 // First load the application that does not have bindingRedirect policies
                 // This will instrument at least one domain-neutral assembly with a
                 // domain-neutral version of Datadog.Trace.ClrProfiler.Managed.dll
-                CreateAndRunAppDomain("DomainNeutralAssemblies.App.NoBindingRedirects");
+                CreateAndRunAppDomain("DomainNeutralAssemblies.App.NoBindingRedirects", "DomainNeutralAssemblies.App.NoBindingRedirects");
 
-#if RUNHTTPNUGETWITHBINDINGREDIRECT
+                // Next load an application that does the same thing, still does not have
+                // bindingRedirect policies, but it uses the System.Net.Http NuGet package
+                // instead of the built-in version.
+                // TBD if this breaks in the non-GAC scenario
+                CreateAndRunAppDomain("DomainNeutralAssemblies.App.HttpNoBindingRedirects", "DomainNeutralAssemblies.App.HttpNoBindingRedirects");
+
+                // Next load the application that has a bindingRedirect policy on Newtonsoft.Json.
+                // This will cause a sharing violation when the domain-neutral assembly attempts
+                // to call into Datadog.Trace.ClrProfiler.Managed.dll because Datadog.Trace.ClrProfiler.Managed.dll
+                // can no longer be loaded shared with the bindingRedirect policy in place. This breaks
+                // the consistency check of all domain-neutral assemblies only depending on other
+                // domain-neutral assemblies
+                CreateAndRunAppDomain("DomainNeutralAssemblies.App.JsonNuGetRedirects", "DomainNeutralAssemblies.App.JsonNuGetRedirects");
+
                 // Next load the application that has a bindingRedirect policy on System.Net.Http.
                 // This will cause a sharing violation when the domain-neutral assembly attempts
                 // to call into Datadog.Trace.ClrProfiler.Managed.dll because Datadog.Trace.ClrProfiler.Managed.dll
                 // can no longer be loaded shared with the bindingRedirect policy in place. This breaks
                 // the consistency check of all domain-neutral assemblies only depending on other
                 // domain-neutral assemblies
-                CreateAndRunAppDomain("DomainNeutralAssemblies.App.HttpBindingRedirects");
+#if RUNHTTPNUGETWITHBINDINGREDIRECT
+                CreateAndRunAppDomain("DomainNeutralAssemblies.App.HttpBindingRedirects", "DomainNeutralAssemblies.App.HttpBindingRedirects");
+#else
+                CreateAndRunAppDomain("DomainNeutralAssemblies.App.HttpBindingRedirects", "EXPECT.FAILURE.DomainNeutralAssemblies.App.HttpBindingRedirects");
 #endif
             }
             catch (Exception ex)
@@ -46,34 +62,42 @@ namespace DomainNeutralAssemblies.FileLoadException
             }
         }
 
-        private static void CreateAndRunAppDomain(string appName)
+        private static void CreateAndRunAppDomain(string appName, string appDomainName)
         {
-            // Construct and initialize settings for a second AppDomain.
-            AppDomainSetup ads = new AppDomainSetup();
-            ads.ApplicationBase = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, appName);
+            try
+            {
+                // Construct and initialize settings for a second AppDomain.
+                AppDomainSetup ads = new AppDomainSetup();
+                ads.ApplicationBase = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, appName);
 
-            ads.DisallowBindingRedirects = false;
-            ads.DisallowCodeDownload = true;
-            ads.ConfigurationFile =
-                Path.Combine(ads.ApplicationBase, appName + ".exe.config");
+                ads.DisallowBindingRedirects = false;
+                ads.DisallowCodeDownload = true;
+                ads.ConfigurationFile =
+                    Path.Combine(ads.ApplicationBase, appName + ".exe.config");
 
-            string name = appName;
-            PermissionSet ps = new PermissionSet(PermissionState.Unrestricted);
-            System.AppDomain appDomain1 = System.AppDomain.CreateDomain(
-                name,
-                System.AppDomain.CurrentDomain.Evidence,
-                ads,
-                ps);
+                PermissionSet ps = new PermissionSet(PermissionState.Unrestricted);
+                System.AppDomain appDomain1 = System.AppDomain.CreateDomain(
+                    appDomainName,
+                    System.AppDomain.CurrentDomain.Evidence,
+                    ads,
+                    ps);
 
-            Console.WriteLine("**********************************************");
-            Console.WriteLine($"Starting code execution in AppDomain {name}");
-            appDomain1.ExecuteAssemblyByName(
-                appName,
-                new string[0]);
-            
-            Console.WriteLine($"Finished code execution in AppDomain {name}");
-            Console.WriteLine("**********************************************");
-            Console.WriteLine();
+                Console.WriteLine("**********************************************");
+                Console.WriteLine($"Starting code execution in AppDomain {appDomainName}");
+                appDomain1.ExecuteAssemblyByName(
+                    appName,
+                    new string[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+            finally
+            {
+                Console.WriteLine($"Finished code execution in AppDomain {appDomainName}");
+                Console.WriteLine("**********************************************");
+                Console.WriteLine();
+            }
         }
     }
 
