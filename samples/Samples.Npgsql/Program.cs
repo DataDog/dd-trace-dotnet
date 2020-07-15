32bd7eec4fae2b3f206cbdfb65ccb571a2975eb5
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-07-15 13:21:41 -0400

    add calls to the new overloads with a CancellationToken parameter

--- samples/Samples.Npgsql/Program.cs
+++ samples/Samples.Npgsql/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data;
 using System.Data.Common;
+using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace;
 using Npgsql;
@@ -12,6 +13,8 @@ namespace Samples.Npgsql
     {
         private static async Task Main()
         {
+            var cts = new CancellationTokenSource();
+
             using (var root = Tracer.Instance.StartActive("root"))
             {
                 using (var connection = CreateConnection())
@@ -25,12 +28,16 @@ namespace Samples.Npgsql
                         command => command.ExecuteReader(),
                         (command, behavior) => command.ExecuteReader(behavior),
                         command => command.ExecuteNonQueryAsync(),
+                        (command, ct) => command.ExecuteNonQueryAsync(ct),
                         command => command.ExecuteScalarAsync(),
+                        (command, ct) => command.ExecuteScalarAsync(ct),
                         command => command.ExecuteReaderAsync(),
-                        (command, behavior) => command.ExecuteReaderAsync(behavior)
+                        (command, behavior) => command.ExecuteReaderAsync(behavior),
+                        (command, ct) => command.ExecuteReaderAsync(ct),
+                        (command, behavior, ct) => command.ExecuteReaderAsync(behavior, ct)
                     );
 
-                    await testQueries.RunAsync("NpgsqlCommand");
+                    await testQueries.RunAsync("NpgsqlCommand", cts.Token);
                 }
 
                 await Task.Delay(100);
@@ -46,12 +53,16 @@ namespace Samples.Npgsql
                         command => new DbCommandWrapper(command).ExecuteReader(),
                         (command, behavior) => new DbCommandWrapper(command).ExecuteReader(behavior),
                         command => new DbCommandWrapper(command).ExecuteNonQueryAsync(),
+                        (command, ct) => new DbCommandWrapper(command).ExecuteNonQueryAsync(ct),
                         command => new DbCommandWrapper(command).ExecuteScalarAsync(),
+                        (command, ct) => new DbCommandWrapper(command).ExecuteScalarAsync(ct),
                         command => new DbCommandWrapper(command).ExecuteReaderAsync(),
-                        (command, behavior) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior)
+                        (command, behavior) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior),
+                        (command, ct) => new DbCommandWrapper(command).ExecuteReaderAsync(ct),
+                        (command, behavior, ct) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior, ct)
                     );
 
-                    await testQueries.RunAsync("DbCommandWrapper");
+                    await testQueries.RunAsync("DbCommandWrapper", cts.Token);
                 }
 
                 await Task.Delay(100);
@@ -66,12 +77,16 @@ namespace Samples.Npgsql
                         command => command.ExecuteReader(),
                         (command, behavior) => command.ExecuteReader(behavior),
                         command => command.ExecuteNonQueryAsync(),
+                        (command, ct) => command.ExecuteNonQueryAsync(ct),
                         command => command.ExecuteScalarAsync(),
+                        (command, ct) => command.ExecuteScalarAsync(ct),
                         command => command.ExecuteReaderAsync(),
-                        (command, behavior) => command.ExecuteReaderAsync(behavior)
+                        (command, behavior) => command.ExecuteReaderAsync(behavior),
+                        (command, ct) => command.ExecuteReaderAsync(ct),
+                        (command, behavior, ct) => command.ExecuteReaderAsync(behavior, ct)
                     );
 
-                    await testQueries.RunAsync("DbCommand");
+                    await testQueries.RunAsync("DbCommand", cts.Token);
                 }
 
                 await Task.Delay(100);
@@ -84,13 +99,17 @@ namespace Samples.Npgsql
                         command => command.ExecuteScalar(),
                         command => command.ExecuteReader(),
                         (command, behavior) => command.ExecuteReader(behavior),
-                        executeNonQueryAsync: null,
-                        executeScalarAsync: null,
-                        executeReaderAsync: null,
-                        executeReaderWithBehaviorAsync: null
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
                     );
 
-                    await testQueries.RunAsync("IDbCommand");
+                    await testQueries.RunAsync("IDbCommand", cts.Token);
                 }
             }
         }
--- samples/Samples.SqlServer/Program.cs
+++ samples/Samples.SqlServer/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace;
 using Samples.DatabaseHelper;
@@ -12,6 +13,8 @@ namespace Samples.SqlServer
     {
         private static async Task Main()
         {
+            var cts = new CancellationTokenSource();
+
             using (var root = Tracer.Instance.StartActive("root"))
             {
                 using (var connection = CreateConnection())
@@ -23,13 +26,17 @@ namespace Samples.SqlServer
                         command => command.ExecuteReader(),
                         (command, behavior) => command.ExecuteReader(behavior),
                         command => command.ExecuteNonQueryAsync(),
+                        (command, ct) => command.ExecuteNonQueryAsync(ct),
                         command => command.ExecuteScalarAsync(),
+                        (command, ct) => command.ExecuteScalarAsync(ct),
                         command => command.ExecuteReaderAsync(),
-                        (command, behavior) => command.ExecuteReaderAsync(behavior)
+                        (command, behavior) => command.ExecuteReaderAsync(behavior),
+                        (command, ct) => command.ExecuteReaderAsync(ct),
+                        (command, behavior, ct) => command.ExecuteReaderAsync(behavior, ct)
                     );
 
 
-                    await testQueries.RunAsync("SqlCommand");
+                    await testQueries.RunAsync("SqlCommand", cts.Token);
                 }
 
                 await Task.Delay(100);
@@ -45,12 +52,16 @@ namespace Samples.SqlServer
                         command => new DbCommandWrapper(command).ExecuteReader(),
                         (command, behavior) => new DbCommandWrapper(command).ExecuteReader(behavior),
                         command => new DbCommandWrapper(command).ExecuteNonQueryAsync(),
+                        (command, ct) => new DbCommandWrapper(command).ExecuteNonQueryAsync(ct),
                         command => new DbCommandWrapper(command).ExecuteScalarAsync(),
+                        (command, ct) => new DbCommandWrapper(command).ExecuteScalarAsync(ct),
                         command => new DbCommandWrapper(command).ExecuteReaderAsync(),
-                        (command, behavior) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior)
+                        (command, behavior) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior),
+                        (command, ct) => new DbCommandWrapper(command).ExecuteReaderAsync(ct),
+                        (command, behavior, ct) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior, ct)
                     );
 
-                    await testQueries.RunAsync("DbCommandWrapper");
+                    await testQueries.RunAsync("DbCommandWrapper", cts.Token);
                 }
 
                 await Task.Delay(100);
@@ -65,12 +76,16 @@ namespace Samples.SqlServer
                         command => command.ExecuteReader(),
                         (command, behavior) => command.ExecuteReader(behavior),
                         command => command.ExecuteNonQueryAsync(),
+                        (command, ct) => command.ExecuteNonQueryAsync(ct),
                         command => command.ExecuteScalarAsync(),
+                        (command, ct) => command.ExecuteScalarAsync(ct),
                         command => command.ExecuteReaderAsync(),
-                        (command, behavior) => command.ExecuteReaderAsync(behavior)
+                        (command, behavior) => command.ExecuteReaderAsync(behavior),
+                        (command, ct) => command.ExecuteReaderAsync(ct),
+                        (command, behavior, ct) => command.ExecuteReaderAsync(behavior, ct)
                     );
 
-                    await testQueries.RunAsync("DbCommand");
+                    await testQueries.RunAsync("DbCommand", cts.Token);
                 }
 
                 await Task.Delay(100);
@@ -83,13 +98,17 @@ namespace Samples.SqlServer
                         command => command.ExecuteScalar(),
                         command => command.ExecuteReader(),
                         (command, behavior) => command.ExecuteReader(behavior),
-                        executeNonQueryAsync: null,
-                        executeScalarAsync: null,
-                        executeReaderAsync: null,
-                        executeReaderWithBehaviorAsync: null
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
                     );
 
-                    await testQueries.RunAsync("IDbCommand");
+                    await testQueries.RunAsync("IDbCommand", cts.Token);
                 }
             }
         }
