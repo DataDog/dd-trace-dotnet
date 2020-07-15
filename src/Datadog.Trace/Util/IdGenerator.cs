a9370e2afa85744dc4179c15666bd54b52005cb7
Kevin Gosse <krix33@gmail.com>
2020-07-15 10:53:14 +0200

    Add a link to the documentation

--- src/Datadog.Trace/Util/IdGenerator.cs
+++ src/Datadog.Trace/Util/IdGenerator.cs
@@ -37,6 +37,8 @@ namespace Datadog.Trace.Util
                 // Resolution is too low, so if a bunch of threads are created at the same time,
                 // they'll all get the same seed.
                 // Instead, use a shared random instance to generate the seed
+                // The same approach was taken for System.Random on .net core:
+                // https://docs.microsoft.com/en-us/dotnet/api/system.random.-ctor?view=netcore-3.1
                 int seed;
 
                 lock (GlobalRandom)
