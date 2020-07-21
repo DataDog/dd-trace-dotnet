1902fa5a4cad6df09bc051dce42ce5843eab86eb
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-07-20 22:16:05 -0400

    bump version

--- tools/Datadog.Core.Tools/TracerVersion.cs
+++ tools/Datadog.Core.Tools/TracerVersion.cs
@@ -18,11 +18,11 @@ namespace Datadog.Core.Tools
         /// <summary>
         /// The patch portion of the current version.
         /// </summary>
-        public const int Patch = 3;
+        public const int Patch = 4;
 
         /// <summary>
         /// Whether the current release is a pre-release
         /// </summary>
-        public const bool IsPreRelease = false;
+        public const bool IsPreRelease = true;
     }
 }
