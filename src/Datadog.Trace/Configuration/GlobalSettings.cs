acb2b6d0129685062463a9ab9b00fdf76c63ad21
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-07-22 11:15:48 -0400

    use fallbacks when getting config file name

--- src/Datadog.Trace/Configuration/GlobalSettings.cs
+++ src/Datadog.Trace/Configuration/GlobalSettings.cs
@@ -89,16 +89,18 @@ namespace Datadog.Trace.Configuration
         internal static IConfigurationSource CreateDefaultConfigurationSource()
         {
             // env > AppSettings > datadog.json
-            var configurationSource = new CompositeConfigurationSource
+            var compositeSource = new CompositeConfigurationSource
                                       {
                                           new EnvironmentConfigurationSource()
                                       };
+
+            var fallbacksSource = new FallbacksConfigurationSource(compositeSource);
 
 #if !NETSTANDARD2_0
             try
             {
                 // on .NET Framework only, also read from app.config/web.config
-                configurationSource.Add(new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings));
+                compositeSource.Add(new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings));
             }
             catch
             {
@@ -128,13 +130,13 @@ namespace Datadog.Trace.Configuration
             {
                 currentDirectory ??= System.Environment.CurrentDirectory;
 
-                // if environment variable is not set, look for default file name in the current directory
-                var configurationFileName = configurationSource.GetStringWithFallbacks(ConfigurationKeys.ConfigurationFileName) ??
+                var configurationFileName = fallbacksSource.GetString(ConfigurationKeys.ConfigurationFileName) ??
+                                            // if path to config file is not set, look in default path
                                             Path.Combine(currentDirectory, "datadog.json");
 
                 if (File.Exists(configurationFileName))
                 {
-                    configurationSource.Add(JsonConfigurationSource.FromFile(configurationFileName));
+                    compositeSource.Add(JsonConfigurationSource.FromFile(configurationFileName));
                 }
             }
             catch
@@ -142,7 +144,7 @@ namespace Datadog.Trace.Configuration
                 // TODO: can we log yet?
             }
 
-            return configurationSource;
+            return compositeSource;
         }
     }
 }
