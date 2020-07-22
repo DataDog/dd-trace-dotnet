f7d2c1e1c9f60c825850b1128528687211635f69
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-07-22 10:55:56 -0400

    catch exceptions while loading settings

--- src/Datadog.Trace/Configuration/GlobalSettings.cs
+++ src/Datadog.Trace/Configuration/GlobalSettings.cs
@@ -90,35 +90,56 @@ namespace Datadog.Trace.Configuration
         {
             // env > AppSettings > datadog.json
             var configurationSource = new CompositeConfigurationSource
-            {
-                new EnvironmentConfigurationSource(),
+                                      {
+                                          new EnvironmentConfigurationSource()
+                                      };
 
 #if !NETSTANDARD2_0
+            try
+            {
                 // on .NET Framework only, also read from app.config/web.config
-                new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings)
+                configurationSource.Add(new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings));
+            }
+            catch
+            {
+                // TODO: can we log yet?
+            }
 #endif
 
             string currentDirectory = null;
 
 #if !NETSTANDARD2_0
-            // on .NET Framework only, use application's root folder
-            // as default path when looking for datadog.json
-            if (System.Web.Hosting.HostingEnvironment.IsHosted)
+            try
             {
-                currentDirectory = System.Web.Hosting.HostingEnvironment.MapPath("~") ?? string.Empty;
+                // on .NET Framework only, use application's root folder
+                // as default path when looking for datadog.json
+                if (System.Web.Hosting.HostingEnvironment.IsHosted)
+                {
+                    currentDirectory = System.Web.Hosting.HostingEnvironment.MapPath("~") ?? string.Empty;
+                }
+            }
+            catch
+            {
+                // TODO: can we log yet?
             }
 #endif
 
-            currentDirectory ??= System.Environment.CurrentDirectory;
-
-            // if environment variable is not set, look for default file name in the current directory
-            var configurationFileName = configurationSource.GetStringWithFallbacks(ConfigurationKeys.ConfigurationFileName) ??
-                                        Path.Combine(currentDirectory, "datadog.json");
-
-            if (Path.GetExtension(configurationFileName).ToUpperInvariant() == ".JSON" &&
-                File.Exists(configurationFileName))
+            try
             {
-                configurationSource.Add(JsonConfigurationSource.FromFile(configurationFileName));
+                currentDirectory ??= System.Environment.CurrentDirectory;
+
+                // if environment variable is not set, look for default file name in the current directory
+                var configurationFileName = configurationSource.GetStringWithFallbacks(ConfigurationKeys.ConfigurationFileName) ??
+                                            Path.Combine(currentDirectory, "datadog.json");
+
+                if (File.Exists(configurationFileName))
+                {
+                    configurationSource.Add(JsonConfigurationSource.FromFile(configurationFileName));
+                }
+            }
+            catch
+            {
+                // TODO: can we log yet?
             }
 
             return configurationSource;
