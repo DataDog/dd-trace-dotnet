57a2f01e450b350120a192bc889e8d5882c785b8
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-07-23 11:37:39 -0400

    try all caps for env var names as fallback on non-Windows platforms

--- src/Datadog.Trace/Configuration/EnvironmentConfigurationSource.cs
+++ src/Datadog.Trace/Configuration/EnvironmentConfigurationSource.cs
@@ -1,3 +1,4 @@
+using System;
 using Datadog.Trace.Util;
 
 namespace Datadog.Trace.Configuration
@@ -11,7 +12,19 @@ namespace Datadog.Trace.Configuration
         /// <inheritdoc />
         public override string GetString(string key)
         {
-            return EnvironmentHelpers.GetEnvironmentVariable(key);
+            var value = EnvironmentHelpers.GetEnvironmentVariable(key);
+
+            // note: avoid RuntimeInformation.IsOSPlatform(), it is was added in net471
+            if (value == null && Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                // Env var names are case-sensitive on non-Windows platforms.
+                // This fallback fixes an issue where users set an
+                // env var name like "DD_TRACE_ADONET_ENABLED"
+                // but key is "DD_TRACE_AdoNet_ENABLED".
+                return EnvironmentHelpers.GetEnvironmentVariable(key.ToUpperInvariant());
+            }
+
+            return value;
         }
     }
 }
