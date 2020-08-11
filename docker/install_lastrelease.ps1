8c6240b426267d34752878878a45aff979d01a21
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-08-11 17:13:12 +0200

    changes

--- docker/install_lastrelease.ps1
+++ docker/install_lastrelease.ps1
@@ -14,6 +14,18 @@ if ($isWindows)
     echo "Extracting windows.zip"
     Expand-Archive windows.zip -DestinationPath .\release
     Remove-Item windows.zip
+
+    [Environment]::SetEnvironmentVariable("DD_DOTNET_TRACER_HOME", "$(pwd)\release", "User")
+    [Environment]::SetEnvironmentVariable("DD_INTEGRATIONS", "$($env:DD_DOTNET_TRACER_HOME)\integrations.json", "User")
+
+    [Environment]::SetEnvironmentVariable("CORECLR_ENABLE_PROFILING", "1", "User")
+    [Environment]::SetEnvironmentVariable("CORECLR_PROFILER", "{846F5F1C-F9AE-4B07-969E-05C26BC060D8}", "User")
+    [Environment]::SetEnvironmentVariable("CORECLR_PROFILER_PATH", "$($env:DD_DOTNET_TRACER_HOME)\win-x64\Datadog.Trace.ClrProfiler.Native.dll", "User")
+
+    [Environment]::SetEnvironmentVariable("COR_ENABLE_PROFILING", "1", "User")
+    [Environment]::SetEnvironmentVariable("COR_PROFILER", "{846F5F1C-F9AE-4B07-969E-05C26BC060D8}", "User")
+    [Environment]::SetEnvironmentVariable("COR_PROFILER_PATH_32", "$($env:DD_DOTNET_TRACER_HOME)\win-x86\Datadog.Trace.ClrProfiler.Native.dll", "User")
+    [Environment]::SetEnvironmentVariable("COR_PROFILER_PATH_64", "$($env:DD_DOTNET_TRACER_HOME)\win-x64\Datadog.Trace.ClrProfiler.Native.dll", "User")
 } 
 else 
 {
@@ -22,5 +34,11 @@ else
     echo "Extracting linux.tar.gz"
     tar -xvzf linux.tar.gz -C ./release
     Remove-Item linux.tar.gz
-}
 
+    [Environment]::SetEnvironmentVariable("DD_DOTNET_TRACER_HOME", "$(pwd)/release", "User")
+    [Environment]::SetEnvironmentVariable("DD_INTEGRATIONS", "$($env:DD_DOTNET_TRACER_HOME)/integrations.json", "User")
+
+    [Environment]::SetEnvironmentVariable("CORECLR_ENABLE_PROFILING", "1", "User")
+    [Environment]::SetEnvironmentVariable("CORECLR_PROFILER", "{846F5F1C-F9AE-4B07-969E-05C26BC060D8}", "User")
+    [Environment]::SetEnvironmentVariable("CORECLR_PROFILER_PATH", "$($env:DD_DOTNET_TRACER_HOME)/Datadog.Trace.ClrProfiler.Native.so", "User")
+}
