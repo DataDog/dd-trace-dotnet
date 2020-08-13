4fb95d034a469d0c06eb7aab75f0312339891e96
Kevin Gosse <krix33@gmail.com>
2020-08-13 16:08:05 +0200

    Always use the default WebRequest for the http:// prefix

---
+++ test/Datadog.Trace.Tests/ApiWebRequestFactoryTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net;
+using System.Reflection;
+using Datadog.Trace.Agent;
+using Xunit;
+
+namespace Datadog.Trace.Tests
+{
+    public class ApiWebRequestFactoryTests
+    {
+        /// <summary>
+        /// This test ensures that the ApiWebRequestFactory behaves correctly when
+        /// a different type of WebRequest is assigned to the http:// prefix
+        /// </summary>
+        [Fact]
+        public void OverrideHttpPrefix()
+        {
+            // Couldn't find a way to "officially" unregister a prefix but that shouldn't stop us
+            var prefixListProperty = typeof(WebRequest).GetProperty("PrefixList", BindingFlags.Static | BindingFlags.NonPublic);
+            var oldPrefixList = prefixListProperty.GetValue(null);
+
+            WebRequest.RegisterPrefix("http://", new CustomWebRequestCreator());
+
+            // Make sure we properly hooked the WebRequest factory
+            Assert.IsType<FakeWebRequest>(WebRequest.Create("http://localhost/"));
+
+            try
+            {
+                var factory = new ApiWebRequestFactory();
+
+                var request = factory.Create(new Uri("http://localhost"));
+
+                Assert.NotNull(request);
+            }
+            finally
+            {
+                // Unregister the prefix
+                prefixListProperty.SetValue(null, oldPrefixList);
+            }
+
+            // Make sure we properly restored the old WebRequest factory
+            Assert.IsType<HttpWebRequest>(WebRequest.Create("http://localhost/"));
+        }
+
+        private class CustomWebRequestCreator : IWebRequestCreate
+        {
+            public WebRequest Create(Uri uri)
+            {
+                return new FakeWebRequest();
+            }
+        }
+
+        private class FakeWebRequest : WebRequest
+        {
+        }
+    }
+}
