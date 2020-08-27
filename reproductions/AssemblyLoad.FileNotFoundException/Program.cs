275ecc56fd491382fc5360995733c696882b1b0c
Kevin Gosse <krix33@gmail.com>
2020-08-27 17:00:07 +0200

    Add delay to all smoke tests to prevent SIGSEGV

--- reproductions/AssemblyLoad.FileNotFoundException/Program.cs
+++ reproductions/AssemblyLoad.FileNotFoundException/Program.cs
@@ -8,6 +8,12 @@ namespace AssemblyLoad.FileNotFoundException
     {
         private static async Task<int> Main()
         {
+#if NETCOREAPP2_1
+            // Add a delay to avoid a race condition on shutdown: https://github.com/dotnet/coreclr/pull/22712
+            // This would cause a segmentation fault on .net core 2.x
+            System.Threading.Thread.Sleep(5000);
+#endif
+
             try
             {
                 var baseAddress = new Uri("https://www.example.com/");
--- reproductions/NLog10LogsInjection.NullReferenceException/Program.cs
+++ reproductions/NLog10LogsInjection.NullReferenceException/Program.cs
@@ -11,6 +11,12 @@ namespace NLog10LogsInjection.NullReferenceException
 
         static int Main(string[] args)
         {
+#if NETCOREAPP2_1
+            // Add a delay to avoid a race condition on shutdown: https://github.com/dotnet/coreclr/pull/22712
+            // This would cause a segmentation fault on .net core 2.x
+            System.Threading.Thread.Sleep(5000);
+#endif
+
             using (var scope = Tracer.Instance.StartActive("Main"))
             {
                 Logger.Info("Message during a trace.");
