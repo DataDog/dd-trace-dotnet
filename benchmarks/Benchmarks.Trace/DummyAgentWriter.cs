d5b2daf224f1b7fcbc1dcad5e5384b7389f91c39
Kevin Gosse <krix33@gmail.com>
2020-08-31 16:09:34 +0200

    Fix compilation of benchmarks

--- benchmarks/Benchmarks.Trace/DummyAgentWriter.cs
+++ benchmarks/Benchmarks.Trace/DummyAgentWriter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Datadog.Trace;
 using Datadog.Trace.Agent;
@@ -11,6 +12,10 @@ namespace Benchmarks.Trace
         public Task FlushAndCloseAsync()
         {
             return Task.CompletedTask;
+        }
+
+        public void SetApiBaseEndpoint(Uri uri)
+        {
         }
 
         public Task FlushTracesAsync()
