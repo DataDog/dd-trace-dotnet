79daf9e37aa495607e4374a8fa29071caf9e05d2
Kevin Gosse <krix33@gmail.com>
2020-09-09 12:12:59 +0200

    Update docker scripts

--- docker/Datadog.Trace.ClrProfiler.Native.sh
+++ docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -5,7 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd "$DIR/.."
 
-PUBLISH_OUTPUT="$( pwd )/src/bin/managed-publish/netstandard2.0"
+PUBLISH_OUTPUT_NET2="$( pwd )/src/bin/managed-publish/netstandard2.0"
+PUBLISH_OUTPUT_NET31="$( pwd )/src/bin/managed-publish/netcoreapp3.1"
 
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p obj/Debug/x64
@@ -15,4 +16,7 @@ mkdir -p bin/Debug/x64
 cp -f obj/Debug/x64/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/Datadog.Trace.ClrProfiler.Native.so
 
 mkdir -p bin/Debug/x64/netstandard2.0
-cp -f $PUBLISH_OUTPUT/*.dll bin/Debug/x64/netstandard2.0/
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Debug/x64/netstandard2.0/
+
+mkdir -p bin/Debug/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Debug/x64/netcoreapp3.1/
