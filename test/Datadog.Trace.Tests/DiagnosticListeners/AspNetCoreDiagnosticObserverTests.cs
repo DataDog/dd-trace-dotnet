b173bb4f8bfece5605527a7bdad631e006f82748
Kevin Gosse <krix33@gmail.com>
2020-09-09 16:52:53 +0200

    Add unit tests for AspNetCoreDiagnosticObserver (#916)

---
+++ test/Datadog.Trace.Tests/DiagnosticListeners/AspNetCoreDiagnosticObserverTests.cs
@@ -0,0 +1,93 @@
+#if !NETFRAMEWORK
+using System;
+using System.Collections.Generic;
+using Datadog.Trace.Agent;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.DiagnosticListeners;
+using Datadog.Trace.Sampling;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Xunit;
+
+namespace Datadog.Trace.Tests.DiagnosticListeners
+{
+    public class AspNetCoreDiagnosticObserverTests
+    {
+        [Fact]
+        public void HttpRequestIn_PopulateSpan()
+        {
+            var tracer = GetTracer();
+
+            IObserver<KeyValuePair<string, object>> observer = new AspNetCoreDiagnosticObserver(tracer, new AspNetCoreDiagnosticOptions());
+
+            var context = new HostingApplication.Context { HttpContext = GetHttpContext() };
+
+            observer.OnNext(new KeyValuePair<string, object>("Microsoft.AspNetCore.Hosting.HttpRequestIn.Start", context));
+
+            var scope = tracer.ActiveScope;
+
+            Assert.NotNull(scope);
+
+            var span = scope.Span;
+
+            Assert.NotNull(span);
+
+            Assert.Equal("aspnet_core.request", span.OperationName);
+            Assert.Equal("aspnet_core", span.GetTag(Tags.InstrumentationName));
+            Assert.Equal(SpanTypes.Web, span.Type);
+            Assert.Equal("GET /home/?/action", span.ResourceName);
+            Assert.Equal(SpanKinds.Server, span.GetTag(Tags.SpanKind));
+            Assert.Equal("GET", span.GetTag(Tags.HttpMethod));
+            Assert.Equal("localhost", span.GetTag(Tags.HttpRequestHeadersHost));
+            Assert.Equal("http://localhost/home/1/action", span.GetTag(Tags.HttpUrl));
+            Assert.Equal(TracerConstants.Language, span.GetTag(Tags.Language));
+        }
+
+        [Fact]
+        public void HttpRequestIn_FilterRequest()
+        {
+            var tracer = GetTracer();
+            var httpContext = GetHttpContext();
+
+            var options = new AspNetCoreDiagnosticOptions();
+            options.IgnorePatterns.Add(h => object.ReferenceEquals(h, httpContext));
+
+            IObserver<KeyValuePair<string, object>> observer = new AspNetCoreDiagnosticObserver(tracer, options);
+
+            var context = new HostingApplication.Context { HttpContext = httpContext };
+
+            observer.OnNext(new KeyValuePair<string, object>("Microsoft.AspNetCore.Hosting.HttpRequestIn.Start", context));
+
+            var scope = tracer.ActiveScope;
+
+            Assert.Null(scope);
+        }
+
+        private static Tracer GetTracer()
+        {
+            var settings = new TracerSettings();
+            var writerMock = new Mock<IAgentWriter>();
+            var samplerMock = new Mock<ISampler>();
+
+            return new Tracer(settings, writerMock.Object, samplerMock.Object, scopeManager: null, statsd: null);
+        }
+
+        private static HttpContext GetHttpContext()
+        {
+            var httpContext = new DefaultHttpContext();
+
+            httpContext.Request.Headers.Add("hello", "hello");
+            httpContext.Request.Headers.Add("world", "world");
+
+            httpContext.Request.Host = new HostString("localhost");
+            httpContext.Request.Scheme = "http";
+            httpContext.Request.Path = "/home/1/action";
+            httpContext.Request.Method = "GET";
+
+            return httpContext;
+        }
+    }
+}
+
+#endif
