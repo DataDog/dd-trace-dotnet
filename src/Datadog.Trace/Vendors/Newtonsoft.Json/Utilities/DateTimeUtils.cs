2768fba25dab80f0a487f499e4933e455e8bfad3
Zach Montoya <zach.montoya@datadoghq.com>
2020-09-14 15:11:33 -0700

    Revert "Remove references to System.Xml.dll in vendored Newtonsoft.Json implementation"
    This reverts commit cb905aa2aae7563fd50685e7b2bbaeb38e4d6e85.

--- src/Datadog.Trace/Vendors/Newtonsoft.Json/Utilities/DateTimeUtils.cs
+++ src/Datadog.Trace/Vendors/Newtonsoft.Json/Utilities/DateTimeUtils.cs
@@ -63,7 +63,6 @@ namespace Datadog.Trace.Vendors.Newtonsoft.Json.Utilities
         }
 
 #if !(PORTABLE40 || PORTABLE) || NETSTANDARD1_3
-#if HAVE_XML_DOCUMENT
         public static XmlDateTimeSerializationMode ToSerializationMode(DateTimeKind kind)
         {
             switch (kind)
@@ -78,7 +77,6 @@ namespace Datadog.Trace.Vendors.Newtonsoft.Json.Utilities
                     throw MiscellaneousUtils.CreateArgumentOutOfRangeException(nameof(kind), kind, "Unexpected DateTimeKind value.");
             }
         }
-#endif
 #else
         public static string ToDateTimeFormat(DateTimeKind kind)
         {
