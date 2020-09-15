bd3164fea7e841739859b910c4fd71a36c20cc43
Kevin Gosse <krix33@gmail.com>
2020-09-15 16:32:05 +0200

    Stop using with-profiler-logs.bash

--- docker/with-profiler-logs.bash
+++ docker/with-profiler-logs.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
+mkdir -p /var/log/datadog/dotnet
+touch /var/log/datadog/dotnet/dotnet-profiler.log
+tail -f /var/log/datadog/dotnet/dotnet-profiler.log | awk '
+  /info/ {print "\033[32m" $0 "\033[39m"}
+  /warn/ {print "\033[31m" $0 "\033[39m"}
+' &
+
 eval "$@"
