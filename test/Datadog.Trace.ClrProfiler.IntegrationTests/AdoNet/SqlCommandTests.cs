5228fa47a7b506dc5534a7fbdb4663d900d67614
Zach Montoya <zach.montoya@datadoghq.com>
2020-09-21 18:33:35 -0700

    Update SqlCommand and NpgsqlCommand tests

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/SqlCommandTests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/SqlCommandTests.cs
@@ -22,10 +22,17 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
             // Note: The automatic instrumentation currently bails out on the generic wrappers.
             // Once this is implemented, this will add another 1 group for the direct assembly reference
             // and another 1 group for the netstandard assembly reference
+            // Note: The automatic instrumentation currently does not instrument on the generic wrappers
+            // due to an issue with constrained virtual method calls. This leads to an inconsistency where
+            // the .NET Core apps generate 4 more spans than .NET Framework apps (2 ExecuteReader calls *
+            // 2 interfaces: IDbCommand and IDbCommand-netstandard).
+            // Once this is fully supported, this will add another 2 complete groups instead to both net461 and netcoreappX.X.
 #if NET452
             var expectedSpanCount = 49; // 7 queries * 7 groups
-#else
+#elif NET461
             var expectedSpanCount = 77; // 7 queries * 11 groups
+#else
+            var expectedSpanCount = 81; // 7 queries * 11 groups + 4 spans from generic wrapper on .NET Core
 #endif
 
             const string dbType = "sql-server";
