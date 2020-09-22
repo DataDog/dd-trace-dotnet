731104d65fd95851047f865cd23ae36686ec1e0d
Kevin Gosse <krix33@gmail.com>
2020-09-22 12:57:51 +0200

    Disable traces for the httpclient used by the tracer

--- src/Datadog.Trace/Agent/HttpClientRequestFactory.cs
+++ src/Datadog.Trace/Agent/HttpClientRequestFactory.cs
@@ -6,7 +6,19 @@ namespace Datadog.Trace.Agent
 {
     internal class HttpClientRequestFactory : IApiRequestFactory
     {
-        private readonly HttpClient _client = new HttpClient();
+        private readonly HttpClient _client;
+
+        public HttpClientRequestFactory()
+        {
+            _client = new HttpClient();
+
+            // Default headers
+            _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.Language, ".NET");
+            _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.TracerVersion, TracerConstants.AssemblyVersion);
+
+            // don't add automatic instrumentation to requests from this HttpClient
+            _client.DefaultRequestHeaders.Add(HttpHeaderNames.TracingEnabled, "false");
+        }
 
         public IApiRequest Create(Uri endpoint)
         {
