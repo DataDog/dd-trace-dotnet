1296084ea072e853abb8a8df9ebc0a60bbd2ae8a
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-09-30 12:36:23 +0200

    Initial commit

---
+++ src/Datadog.Trace.Tools.Runner/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Datadog.Trace.Tools.Runner
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
