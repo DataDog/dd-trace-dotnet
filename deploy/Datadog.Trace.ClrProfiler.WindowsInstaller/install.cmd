d22eecd4041be932426948a7f6e78af1866ba2b3
Kevin Gosse <krix33@gmail.com>
2020-10-05 20:02:22 +0200

    Scope the HttpModule to .NET 4.0+ (#965)

--- deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/install.cmd
+++ deploy/Datadog.Trace.ClrProfiler.WindowsInstaller/install.cmd
@@ -3,7 +3,7 @@ setlocal
 
 echo Executing install.cmd at %date% %time%
 
-set DATADOG_APPCMD_CMDLINE=%systemroot%\system32\inetsrv\appcmd.exe set config /section:system.webServer/modules /+[name='DatadogTracingModule',type='Datadog.Trace.AspNet.TracingHttpModule,Datadog.Trace.AspNet,Version=1.0.0.0,Culture=neutral,PublicKeyToken=def86d061d0d2eeb']
+set DATADOG_APPCMD_CMDLINE=%systemroot%\system32\inetsrv\appcmd.exe set config /section:system.webServer/modules /+[name='DatadogTracingModule',type='Datadog.Trace.AspNet.TracingHttpModule,Datadog.Trace.AspNet,Version=1.0.0.0,Culture=neutral,PublicKeyToken=def86d061d0d2eeb',preCondition='managedHandler,runtimeVersion4.0']
 
 IF EXIST %systemroot%\system32\inetsrv\appcmd.exe (
     echo Attempting to install the Datadog ASP.NET HttpModule with %systemroot%\system32\inetsrv\appcmd.exe
