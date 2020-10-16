ebc381307a3108141a744b91efff7c638c948fc8
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-10-16 14:49:36 +0200

    Fixes runner pipeline in azure.

--- src/Datadog.Trace.Tools.Runner/linux-native-build.cmd
+++ src/Datadog.Trace.Tools.Runner/linux-native-build.cmd
@@ -2,7 +2,6 @@
 SETLOCAL
 
 echo *** Building Native ClrProfiler for linux-x64 ***
-set DOCKER_BUILDKIT=1
-docker build -f ..\..\docker\linux-build.dockerfile --target native-linux-binary --output type=local,dest=home/linux-x64 ..\..\
-
+docker build -f ..\..\docker\linux-build.dockerfile --target native-linux-binary --tag native-linux-binary --output type=local,dest=home/linux-x64 ..\..\
+docker run -it --rm -v %cd%\home\linux-x64:/home/linux-x64 native-linux-binary:latest
 ENDLOCAL
