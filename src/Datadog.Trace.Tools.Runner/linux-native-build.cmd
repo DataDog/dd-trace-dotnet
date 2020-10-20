ea1698f6411c977ac9fc57e98f7d05511cb80531
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-10-20 11:05:28 +0200

    Update solution and project for new directory structure.

--- src/Datadog.Trace.Tools.Runner/linux-native-build.cmd
+++ src/Datadog.Trace.Tools.Runner/linux-native-build.cmd
@@ -2,6 +2,6 @@
 SETLOCAL
 
 echo *** Building Native ClrProfiler for linux-x64 ***
-docker build -f ..\..\docker\linux-build.dockerfile --target native-linux-binary --tag native-linux-binary --output type=local,dest=home/linux-x64 --platform=linux ..\..\
+docker build -f ..\..\build\docker\linux-build.dockerfile --target native-linux-binary --tag native-linux-binary --output type=local,dest=home/linux-x64 --platform=linux ..\..\
 docker run -it --rm -v %cd%\home\linux-x64:/home/linux-x64 native-linux-binary:latest
 ENDLOCAL
