6b2bd0727d545505a2e4f9a6c9c84af10c60f45b
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-10-28 10:01:35 -0400

    add Samples.Microsoft.Data.SqlClient project

---
+++ test/test-applications/integrations/Samples.Microsoft.Data.SqlClient/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Samples.DatabaseHelper;
+using Microsoft.Data.SqlClient;
+
+namespace Samples.MicrosoftSqlServer
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var commandFactory = new DbCommandFactory();
+            var commandExecutor = new MicrosoftSqlCommandExecutor();
+            var cts = new CancellationTokenSource();
+
+            using (var connection = OpenConnection())
+            {
+                await RelationalDatabaseTestHarness.RunAllAsync<SqlCommand>(connection, commandFactory, commandExecutor, cts.Token);
+            }
+
+            // allow time to flush
+            await Task.Delay(2000, cts.Token);
+        }
+
+        private static SqlConnection OpenConnection()
+        {
+            int numAttempts = 3;
+            var connectionString = Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING") ??
+@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Connection Timeout=60";
+
+            for (int i = 0; i < numAttempts; i++)
+            {
+                SqlConnection connection = null;
+
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    return connection;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    connection?.Dispose();
+                }
+            }
+
+            throw new Exception($"Unable to open connection to connection string {connectionString} after {numAttempts} attempts");
+        }
+    }
+}
