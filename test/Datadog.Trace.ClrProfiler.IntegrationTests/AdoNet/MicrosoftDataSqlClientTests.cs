3d5c7e3fd059b4e9daccbbf1d612f5c2e83dad33
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-10-30 17:56:15 -0400

    fix expected service name

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MicrosoftDataSqlClientTests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MicrosoftDataSqlClientTests.cs
@@ -36,7 +36,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
 
             const string dbType = "sql-server";
             const string expectedOperationName = dbType + ".query";
-            const string expectedServiceName = "Samples.SqlServer-" + dbType;
+            const string expectedServiceName = "Samples.Microsoft.Data.SqlClient-" + dbType;
 
             // NOTE: opt into the additional instrumentation of calls into netstandard.dll
             SetEnvironmentVariable("DD_TRACE_NETSTANDARD_ENABLED", "true");
