0367250e95afba076e9cd35f972ad8c09dc450c8
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-11-02 16:41:19 -0500

    fix table names

--- test/test-applications/integrations/dependency-libs/Samples.DatabaseHelper/DbCommandFactory.cs
+++ test/test-applications/integrations/dependency-libs/Samples.DatabaseHelper/DbCommandFactory.cs
@@ -14,14 +14,14 @@ namespace Samples.DatabaseHelper
         public IDbCommand GetCreateTableCommand(IDbConnection connection)
         {
             var command = connection.CreateCommand();
-            command.CommandText = $"DROP TABLE IF EXISTS {_tableName}; CREATE TABLE {_tableName} (Id int PRIMARY KEY, Name varchar(100));";
+            command.CommandText = $"DROP TABLE IF EXISTS '{_tableName}'; CREATE TABLE '{_tableName}' (Id int PRIMARY KEY, Name varchar(100));";
             return command;
         }
 
         public IDbCommand GetInsertRowCommand(IDbConnection connection)
         {
             var command = connection.CreateCommand();
-            command.CommandText = $"INSERT INTO {_tableName} (Id, Name) VALUES (@Id, @Name);";
+            command.CommandText = $"INSERT INTO '{_tableName}' (Id, Name) VALUES (@Id, @Name);";
             command.AddParameterWithValue("Id", 1);
             command.AddParameterWithValue("Name", "Name1");
             return command;
@@ -30,7 +30,7 @@ namespace Samples.DatabaseHelper
         public IDbCommand GetUpdateRowCommand(IDbConnection connection)
         {
             var command = connection.CreateCommand();
-            command.CommandText = $"UPDATE {_tableName} SET Name=@Name WHERE Id=@Id;";
+            command.CommandText = $"UPDATE '{_tableName}' SET Name=@Name WHERE Id=@Id;";
             command.AddParameterWithValue("Name", "Name2");
             command.AddParameterWithValue("Id", 1);
             return command;
@@ -39,7 +39,7 @@ namespace Samples.DatabaseHelper
         public IDbCommand GetSelectScalarCommand(IDbConnection connection)
         {
             var command = connection.CreateCommand();
-            command.CommandText = $"SELECT Name FROM {_tableName} WHERE Id=@Id;";
+            command.CommandText = $"SELECT Name FROM '{_tableName}' WHERE Id=@Id;";
             command.AddParameterWithValue("Id", 1);
             return command;
         }
@@ -47,7 +47,7 @@ namespace Samples.DatabaseHelper
         public IDbCommand GetSelectRowCommand(IDbConnection connection)
         {
             var command = connection.CreateCommand();
-            command.CommandText = $"SELECT * FROM {_tableName} WHERE Id=@Id;";
+            command.CommandText = $"SELECT * FROM '{_tableName}' WHERE Id=@Id;";
             command.AddParameterWithValue("Id", 1);
             return command;
         }
@@ -55,7 +55,7 @@ namespace Samples.DatabaseHelper
         public IDbCommand GetDeleteRowCommand(IDbConnection connection)
         {
             var command = connection.CreateCommand();
-            command.CommandText = $"DELETE FROM {_tableName} WHERE Id=@Id;";
+            command.CommandText = $"DELETE FROM '{_tableName}' WHERE Id=@Id;";
             command.AddParameterWithValue("Id", 1);
             return command;
         }
