2a69d56d662013f112d9e54549b8fd450e87458b
Tony Redondo <tony.redondo@datadoghq.com>
2020-11-09 21:51:14 +0100

    Fix an exception when the ducktype chaining is used with a public struct (private structs works fine). (#1023)

--- test/Datadog.Trace.DuckTyping.Tests/StructTests.cs
+++ test/Datadog.Trace.DuckTyping.Tests/StructTests.cs
@@ -64,5 +64,69 @@ namespace Datadog.Trace.DuckTyping.Tests
         {
             public readonly string Value => "Hello World";
         }
+
+        [Fact]
+        public void DuckChainingStructInterfaceProxyTest()
+        {
+            PrivateDuckChainingTarget instance = new PrivateDuckChainingTarget();
+            IPrivateDuckChainingTarget proxy = instance.As<IPrivateDuckChainingTarget>();
+            Assert.Equal(instance.ChainingTestField.Name, proxy.ChainingTestField.Name);
+            Assert.Equal(instance.ChainingTest.Name, proxy.ChainingTest.Name);
+            Assert.Equal(instance.ChainingTestMethod().Name, proxy.ChainingTestMethod().Name);
+
+            PublicDuckChainingTarget instance2 = new PublicDuckChainingTarget();
+            IPrivateDuckChainingTarget proxy2 = instance2.As<IPrivateDuckChainingTarget>();
+            Assert.Equal(instance2.ChainingTestField.Name, proxy2.ChainingTestField.Name);
+            Assert.Equal(instance2.ChainingTest.Name, proxy2.ChainingTest.Name);
+            Assert.Equal(instance2.ChainingTestMethod().Name, proxy2.ChainingTestMethod().Name);
+        }
+
+        public interface IPrivateDuckChainingTarget
+        {
+            [Duck(Kind = DuckKind.Field)]
+            IPrivateTarget ChainingTestField { get; }
+
+            IPrivateTarget ChainingTest { get; }
+
+            IPrivateTarget ChainingTestMethod();
+        }
+
+        public interface IPrivateTarget
+        {
+            [Duck(Kind = DuckKind.Field)]
+            public string Name { get; }
+        }
+
+        private class PrivateDuckChainingTarget
+        {
+#pragma warning disable SA1401 // Fields must be private
+            public PrivateTarget ChainingTestField = new PrivateTarget { Name = "Hello World 1" };
+#pragma warning restore SA1401 // Fields must be private
+
+            public PrivateTarget ChainingTest => new PrivateTarget { Name = "Hello World 2" };
+
+            public PrivateTarget ChainingTestMethod() => new PrivateTarget { Name = "Hello World 3" };
+        }
+
+        private struct PrivateTarget
+        {
+            public string Name;
+        }
+
+        public class PublicDuckChainingTarget
+        {
+#pragma warning disable SA1401 // Fields must be private
+            public PublicTarget ChainingTestField = new PublicTarget { Name = "Hello World 1" };
+#pragma warning restore SA1401 // Fields must be private
+
+            public PublicTarget ChainingTest => new PublicTarget { Name = "Hello World 2" };
+
+            public PublicTarget ChainingTestMethod() => new PublicTarget { Name = "Hello World 3" };
+        }
+
+        public struct PublicTarget
+        {
+            public string Name;
+        }
     }
 }
