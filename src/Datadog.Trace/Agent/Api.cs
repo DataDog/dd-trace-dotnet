04d15e1b2b3a6e1226a5ed044aa2bc4d7475dcd8
Zach Montoya <zach.montoya@datadoghq.com>
2020-11-09 10:59:57 -0800

    Improve managed tracer logs (#1011)
    - Rename log files to dotnet-tracer-managed-*.log
    - Add Debug level logs to document how many traces we attempt to send to the Tracer and a confirmation of how many traces succeeded or failed to send
    - For exceptions when sending traces to trace agent, only log intermediate retry failures in Debug mode.

--- src/Datadog.Trace/Agent/Api.cs
+++ src/Datadog.Trace/Agent/Api.cs
@@ -61,6 +61,9 @@ namespace Datadog.Trace.Agent
             var retryCount = 1;
             var sleepDuration = 100; // in milliseconds
             var traceIds = GetUniqueTraceIds(traces);
+            var traceCount = traceIds.Count;
+
+            Log.Debug("Sending {0} traces to the DD agent", traceCount);
 
             var batch = _statsd?.StartBatch(initialCapacity: 2) ?? default;
 
@@ -69,7 +72,7 @@ namespace Datadog.Trace.Agent
                 var request = _apiRequestFactory.Create(_tracesEndpoint);
 
                 // Set additional headers
-                request.AddHeader(AgentHttpHeaderNames.TraceCount, traceIds.Count.ToString());
+                request.AddHeader(AgentHttpHeaderNames.TraceCount, traceCount.ToString());
                 if (_frameworkDescription != null)
                 {
                     request.AddHeader(AgentHttpHeaderNames.LanguageInterpreter, _frameworkDescription.Name);
@@ -94,6 +97,7 @@ namespace Datadog.Trace.Agent
                     if (ex.InnerException is InvalidOperationException ioe)
                     {
                         Log.Error(ex, "An error occurred while sending traces to the agent at {0}", _tracesEndpoint);
+                        Log.Error("Failed to send {0} traces to the DD agent", traceCount);
                         return false;
                     }
 #endif
@@ -107,8 +111,10 @@ namespace Datadog.Trace.Agent
                     if (retryCount >= retryLimit)
                     {
                         // stop retrying
-                        Log.Error(exception, "An error occurred while sending traces to the agent at {0}", _tracesEndpoint);
                         batch.Send();
+
+                        Log.Error(exception, "An error occurred while sending traces to the agent at {0}", _tracesEndpoint);
+                        Log.Error("Failed to send {0} traces to the DD agent", traceCount);
                         return false;
                     }
 
@@ -129,7 +135,7 @@ namespace Datadog.Trace.Agent
 
                     if (isSocketException)
                     {
-                        Log.Error(exception, "Unable to communicate with the trace agent at {0}", _tracesEndpoint);
+                        Log.Debug(exception, "Unable to communicate with the trace agent at {0}", _tracesEndpoint);
                         TracingProcessManager.TryForceTraceAgentRefresh();
                     }
 
@@ -149,6 +155,7 @@ namespace Datadog.Trace.Agent
                 }
 
                 batch.Send();
+                Log.Debug("Successfully sent {0} traces to the DD agent", traceCount);
                 return true;
             }
         }
