40eb64182f0b7e34a3200a73ec7a784f88ec48a1
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-11-10 19:39:56 +0100

    HttpClient integration refactor

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/HttpClient/HttpClientHandlerIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/HttpClient/HttpClientHandlerIntegration.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Datadog.Trace.ClrProfiler.CallTarget;
-using Datadog.Trace.DuckTyping;
-using Datadog.Trace.Headers;
 using Datadog.Trace.Tagging;
-
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-#pragma warning disable SA1600 // Elements must be documented
-#pragma warning disable SA1201 // Elements must appear in the correct order
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.HttpClient
 {
+    /// <summary>
+    /// System.Net.Http.HttpClientHandler calltarget instrumentation
+    /// </summary>
     [InstrumentMethod(
         Assembly = "System.Net.Http",
         Type = "System.Net.Http.HttpClientHandler",
@@ -23,8 +19,17 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.HttpClient
         MaximumVersion = "5.*.*")]
     public class HttpClientHandlerIntegration
     {
-        private const string IntegrationName = "HttpMessageHandler-CallTarget";
+        private const string IntegrationName = "HttpMessageHandler[CallTarget]";
 
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TRequest">Type of the request</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="requestMessage">HttpRequest message instance</param>
+        /// <param name="cancellationToken">CancellationToken value</param>
+        /// <returns>Calltarget state value</returns>
         public static CallTargetState OnMethodBegin<TTarget, TRequest>(TTarget instance, TRequest requestMessage, CancellationToken cancellationToken)
             where TRequest : IHttpRequestMessage
         {
@@ -46,6 +51,16 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.HttpClient
             return new CallTargetState(new IntegrationState(scope, tags));
         }
 
+        /// <summary>
+        /// OnAsyncMethodEnd callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TResponse">Type of the response, in an async scenario will be T of Task of T</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="responseMessage">HttpResponse message instance</param>
+        /// <param name="exception">Exception instance in case the original code threw an exception.</param>
+        /// <param name="state">Calltarget state value</param>
+        /// <returns>A response value, in an async scenario will be T of Task of T</returns>
         public static TResponse OnAsyncMethodEnd<TTarget, TResponse>(TTarget instance, TResponse responseMessage, Exception exception, CallTargetState state)
             where TResponse : IHttpResponseMessage
         {
@@ -113,74 +128,6 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.HttpClient
             {
                 Scope = scope;
                 Tags = tags;
-            }
-        }
-
-        /*
-         * Duck Typing interfaces
-         */
-        public interface IHttpRequestMessage
-        {
-            HttpMethodStruct Method { get; }
-
-            Uri RequestUri { get; }
-
-            IRequestHeaders Headers { get; }
-        }
-
-        [DuckCopy]
-        public struct HttpMethodStruct
-        {
-            public string Method;
-        }
-
-        public interface IRequestHeaders
-        {
-            bool TryGetValues(string name, out IEnumerable<string> values);
-
-            bool Remove(string name);
-
-            void Add(string name, string value);
-        }
-
-        public interface IHttpResponseMessage
-        {
-            int StatusCode { get; }
-        }
-
-        internal readonly struct HttpHeadersCollection : IHeadersCollection
-        {
-            private readonly IRequestHeaders _headers;
-
-            public HttpHeadersCollection(IRequestHeaders headers)
-            {
-                _headers = headers ?? throw new ArgumentNullException(nameof(headers));
-            }
-
-            public IEnumerable<string> GetValues(string name)
-            {
-                if (_headers.TryGetValues(name, out IEnumerable<string> values))
-                {
-                    return values;
-                }
-
-                return Enumerable.Empty<string>();
-            }
-
-            public void Set(string name, string value)
-            {
-                _headers.Remove(name);
-                _headers.Add(name, value);
-            }
-
-            public void Add(string name, string value)
-            {
-                _headers.Add(name, value);
-            }
-
-            public void Remove(string name)
-            {
-                _headers.Remove(name);
             }
         }
     }
