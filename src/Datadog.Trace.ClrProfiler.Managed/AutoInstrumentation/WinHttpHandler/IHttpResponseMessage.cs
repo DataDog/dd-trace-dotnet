8d903023e53035f2cc48906f269c84874484dbdc
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-11-11 18:11:32 +0100

    WinHttpHandler integration

---
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/WinHttpHandler/IHttpResponseMessage.cs
@@ -0,0 +1,13 @@
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.WinHttpHandler
+{
+    /// <summary>
+    /// HttpResponseMessage interface for ducktyping
+    /// </summary>
+    public interface IHttpResponseMessage
+    {
+        /// <summary>
+        /// Gets the status code of the http response
+        /// </summary>
+        int StatusCode { get; }
+    }
+}
---
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/WinHttpHandler/WinHttpHandlerIntegration.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Linq;
+using System.Threading;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Tagging;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.WinHttpHandler
+{
+    /// <summary>
+    /// System.Net.Http.SocketsHttpHandler calltarget instrumentation
+    /// </summary>
+    [InstrumentMethod(
+        Assembly = "System.Net.Http",
+        Type = "System.Net.Http.WinHttpHandler",
+        Method = "SendAsync",
+        ReturnTypeName = ClrNames.HttpResponseMessageTask,
+        ParametersTypesNames = new[] { ClrNames.HttpRequestMessage, ClrNames.CancellationToken },
+        MinimumVersion = "4.0.0",
+        MaximumVersion = "5.*.*")]
+    public class WinHttpHandlerIntegration
+    {
+        private const string IntegrationName = "HttpMessageHandler";
+
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TRequest">Type of the request</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="requestMessage">HttpRequest message instance</param>
+        /// <param name="cancellationToken">CancellationToken value</param>
+        /// <returns>Calltarget state value</returns>
+        public static CallTargetState OnMethodBegin<TTarget, TRequest>(TTarget instance, TRequest requestMessage, CancellationToken cancellationToken)
+            where TRequest : IHttpRequestMessage
+        {
+            Scope scope = null;
+            HttpTags tags = null;
+
+            if (IsTracingEnabled(requestMessage.Headers))
+            {
+                scope = ScopeFactory.CreateOutboundHttpScope(Tracer.Instance, requestMessage.Method.Method, requestMessage.RequestUri, IntegrationName, out tags);
+                if (scope != null)
+                {
+                    tags.HttpClientHandlerType = instance.GetType().FullName;
+
+                    // add distributed tracing headers to the HTTP request
+                    SpanContextPropagator.Instance.Inject(scope.Span.Context, new HttpHeadersCollection(requestMessage.Headers));
+                }
+            }
+
+            return new CallTargetState(new IntegrationState(scope, tags));
+        }
+
+        /// <summary>
+        /// OnAsyncMethodEnd callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TResponse">Type of the response, in an async scenario will be T of Task of T</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="responseMessage">HttpResponse message instance</param>
+        /// <param name="exception">Exception instance in case the original code threw an exception.</param>
+        /// <param name="state">Calltarget state value</param>
+        /// <returns>A response value, in an async scenario will be T of Task of T</returns>
+        public static TResponse OnAsyncMethodEnd<TTarget, TResponse>(TTarget instance, TResponse responseMessage, Exception exception, CallTargetState state)
+            where TResponse : IHttpResponseMessage
+        {
+            IntegrationState integrationState = (IntegrationState)state.State;
+            if (integrationState.Scope is null)
+            {
+                return responseMessage;
+            }
+
+            try
+            {
+                if (exception is null)
+                {
+                    if (integrationState.Tags != null)
+                    {
+                        integrationState.Tags.HttpStatusCode = HttpTags.ConvertStatusCodeToString(responseMessage.StatusCode);
+                    }
+                }
+                else
+                {
+                    integrationState.Scope.Span.SetException(exception);
+                }
+            }
+            finally
+            {
+                integrationState.Scope.Dispose();
+            }
+
+            return responseMessage;
+        }
+
+        private static bool IsTracingEnabled(IRequestHeaders headers)
+        {
+            if (!Tracer.Instance.Settings.IsOptInIntegrationEnabled("WinHttpHandler"))
+            {
+                return false;
+            }
+
+            if (headers.TryGetValues(HttpHeaderNames.TracingEnabled, out var headerValues))
+            {
+                if (headerValues is string[] arrayValues)
+                {
+                    for (var i = 0; i < arrayValues.Length; i++)
+                    {
+                        if (string.Equals(arrayValues[i], "false", StringComparison.OrdinalIgnoreCase))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+
+                if (headerValues != null && headerValues.Any(s => string.Equals(s, "false", StringComparison.OrdinalIgnoreCase)))
+                {
+                    // tracing is disabled for this request via http header
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private readonly struct IntegrationState
+        {
+            public readonly Scope Scope;
+            public readonly HttpTags Tags;
+
+            public IntegrationState(Scope scope, HttpTags tags)
+            {
+                Scope = scope;
+                Tags = tags;
+            }
+        }
+    }
+}
