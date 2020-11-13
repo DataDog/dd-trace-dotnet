81dbeb0ae7a8df5a78e4db4274e4923368ad452c
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-11-13 01:30:53 +0100

    remove indent to avoid stackoverflow

--- src/Datadog.Trace.ClrProfiler.Native/cor_profiler.cpp
+++ src/Datadog.Trace.ClrProfiler.Native/cor_profiler.cpp
@@ -1453,7 +1453,6 @@ std::string CorProfiler::GetILCodes(std::string title, ILRewriter* rewriter,
 
   const auto ehCount = rewriter->GetEHCount();
   const auto ehPtr = rewriter->GetEHPointer();
-  unsigned int indent = 0;
 
   for (ILInstr* cInstr = rewriter->GetILList()->m_pNext;
        cInstr != rewriter->GetILList(); cInstr = cInstr->m_pNext) {
@@ -1463,25 +1462,13 @@ std::string CorProfiler::GetILCodes(std::string title, ILRewriter* rewriter,
         const auto currentEH = ehPtr[i];
         if (currentEH.m_Flags == COR_ILEXCEPTION_CLAUSE_FINALLY) {
           if (currentEH.m_pTryBegin == cInstr) {
-            if (indent > 0) {
-              orig_sstream << std::setw(indent * 2) << " ";
-            }
             orig_sstream << ".try {" << std::endl;
-            indent++;
           }
           if (currentEH.m_pTryEnd == cInstr) {
-            indent--;
-            if (indent > 0) {
-              orig_sstream << std::setw(indent * 2) << " ";
-            }
             orig_sstream << "}" << std::endl;
           }
           if (currentEH.m_pHandlerBegin == cInstr) {
-            if (indent > 0) {
-              orig_sstream << std::setw(indent * 2) << " ";
-            }
-            orig_sstream << ".finally {" << std::endl;
-            indent++;
+            orig_sstream <<  ".finally {" << std::endl;
           }
         }
       }
@@ -1489,32 +1476,18 @@ std::string CorProfiler::GetILCodes(std::string title, ILRewriter* rewriter,
         const auto currentEH = ehPtr[i];
         if (currentEH.m_Flags == COR_ILEXCEPTION_CLAUSE_NONE) {
           if (currentEH.m_pTryBegin == cInstr) {
-            if (indent > 0) {
-              orig_sstream << std::setw(indent * 2) << " ";
-            }
             orig_sstream << ".try {" << std::endl;
-            indent++;
           }
           if (currentEH.m_pTryEnd == cInstr) {
-            indent--;
-            if (indent > 0) {
-              orig_sstream << std::setw(indent * 2) << " ";
-            }
             orig_sstream << "}" << std::endl;
           }
           if (currentEH.m_pHandlerBegin == cInstr) {
-            if (indent > 0) {
-              orig_sstream << std::setw(indent * 2) << " ";
-            }
             orig_sstream << ".catch {" << std::endl;
-            indent++;
           }
         }
       }
     }
-    if (indent > 0) {
-      orig_sstream << std::setw(indent * 2) << " ";
-    }
+
     orig_sstream << cInstr;
     orig_sstream << ": ";
     if (cInstr->m_opcode < opcodes_names.size()) {
@@ -1572,10 +1545,6 @@ std::string CorProfiler::GetILCodes(std::string title, ILRewriter* rewriter,
       for (unsigned int i = 0; i < ehCount; i++) {
         const auto currentEH = ehPtr[i];
         if (currentEH.m_pHandlerEnd == cInstr) {
-          indent--;
-          if (indent > 0) {
-            orig_sstream << std::setw(indent * 2) << " ";
-          }
           orig_sstream << "}" << std::endl;
         }
       }
