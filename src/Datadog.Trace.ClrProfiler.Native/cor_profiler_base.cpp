0a03b8f761553137a26a6cfeff285a49b9f5ff44
Tony Redondo <daniel.redondo@undefinedlabs.com>
2020-11-13 16:56:24 +0100

    revert some Debug's

--- src/Datadog.Trace.ClrProfiler.Native/cor_profiler_base.cpp
+++ src/Datadog.Trace.ClrProfiler.Native/cor_profiler_base.cpp
@@ -134,15 +134,11 @@ HRESULT STDMETHODCALLTYPE CorProfilerBase::JITCompilationFinished(
 
 HRESULT STDMETHODCALLTYPE CorProfilerBase::JITCachedFunctionSearchStarted(
     FunctionID functionId, BOOL *pbUseCachedFunction) {
-  Debug("CorProfilerBase::JITCachedFunctionSearchStarted: ", functionId, " / ",
-       *pbUseCachedFunction);
   return S_OK;
 }
 
 HRESULT STDMETHODCALLTYPE CorProfilerBase::JITCachedFunctionSearchFinished(
     FunctionID functionId, COR_PRF_JIT_CACHE result) {
-  Debug("CorProfilerBase::JITCachedFunctionSearchFinished: ", functionId, " / ",
-       result);
   return S_OK;
 }
 
@@ -154,7 +150,6 @@ CorProfilerBase::JITFunctionPitched(FunctionID functionId) {
 HRESULT STDMETHODCALLTYPE CorProfilerBase::JITInlining(FunctionID callerId,
                                                        FunctionID calleeId,
                                                        BOOL *pfShouldInline) {
-  Debug("JITInlining: ", callerId, " ", calleeId, " ", *pfShouldInline);
   return S_OK;
 }
 
