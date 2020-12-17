2bd903d5c63c638ec0748c32e1506b9ae217677e
colin-higgins <developerspike@gmail.com>
2020-12-16 20:50:49 -0500

    Make strategies const, though they are internal featurs for now.

--- src/Datadog.Trace/Agent/TransportStrategy.cs
+++ src/Datadog.Trace/Agent/TransportStrategy.cs
@@ -8,6 +8,9 @@ namespace Datadog.Trace.Agent
 {
     internal static class TransportStrategy
     {
+        public const string DatadogTcp = "DATADOG-TCP";
+        public const string DatadogNamedPipes = "DATADOG-NAMED-PIPES";
+
         private static readonly Vendors.Serilog.ILogger Log = DatadogLogging.For<Tracer>();
 
         public static IApiRequestFactory Get(TracerSettings settings)
@@ -16,10 +19,10 @@ namespace Datadog.Trace.Agent
 
             switch (strategy)
             {
-                case "DATADOG-TCP":
+                case DatadogTcp:
                     Log.Information("Using {0} for trace transport.", nameof(TcpStreamFactory));
                     return new HttpStreamRequestFactory(new TcpStreamFactory(settings.AgentUri.Host, settings.AgentUri.Port), new DatadogHttpClient());
-                case "DATADOG-NAMED-PIPES":
+                case DatadogNamedPipes:
                     Log.Information("Using {0} for trace transport, with pipe name {1} and timeout {2}ms.", nameof(NamedPipeClientStreamFactory), settings.TracesPipeName, settings.TracesPipeTimeoutMs);
                     return new HttpStreamRequestFactory(new NamedPipeClientStreamFactory(settings.TracesPipeName, settings.TracesPipeTimeoutMs), new DatadogHttpClient());
                 default:
