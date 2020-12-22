1fa1ffccd15ff7d149c0a6c30e8a5a632e2e3a99
colin-higgins <developerspike@gmail.com>
2020-12-22 17:21:22 -0500

    Remove debug code for payload size

--- src/Datadog.Trace/Agent/MessagePack/CachedSerializer.cs
+++ src/Datadog.Trace/Agent/MessagePack/CachedSerializer.cs
@@ -10,14 +10,6 @@ namespace Datadog.Trace.Agent.MessagePack
         internal static readonly CachedSerializer Instance = new CachedSerializer();
         private const int InitialBufferSize = 64 * 1024;
 
-#if DEBUG
-        /// <summary>
-        /// Set this variable to true in order to output the serialized traces to a file
-        /// </summary>
-        private readonly bool _writePayloadToFile = true;
-        private readonly string _payloadFilePath = @"C:\ProgramData\Datadog .NET Tracer\logs\";
-#endif
-
         private byte[] _buffer;
 
         public CachedSerializer()
@@ -42,17 +34,6 @@ namespace Datadog.Trace.Agent.MessagePack
                 }
 
                 int length = MessagePackSerializer.Serialize(ref buffer, 0, obj, resolver);
-
-#if DEBUG
-                if (_writePayloadToFile && length > 3)
-                {
-                    // If there are actual traces present, then write
-                    // Trim down to the actual length to write to file
-                    var trimmed = new byte[length];
-                    System.Array.Copy(buffer, 0, trimmed, 0, length);
-                    File.WriteAllBytes(Path.Combine(_payloadFilePath, $"payload-{System.DateTime.Now.Ticks}.txt"), trimmed);
-                }
-#endif
 
                 await stream.WriteAsync(buffer, 0, length).ConfigureAwait(false);
             }
