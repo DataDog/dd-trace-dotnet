b4123a15ec7eb602c800d3f638afaa19d222cb51
Tony Redondo <daniel.redondo@undefinedlabs.com>
2021-01-08 13:52:21 +0100

    Change based in the review.

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/XUnit/XUnitTestInvokerRunAsyncIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/XUnit/XUnitTestInvokerRunAsyncIntegration.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Reflection;
-using Datadog.Trace.Ci;
 using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.DuckTyping;
-using Datadog.Trace.ExtensionMethods;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
 {
@@ -24,12 +20,6 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
     {
         private const string IntegrationName = "XUnit";
 
-        static XUnitTestInvokerRunAsyncIntegration()
-        {
-            // Preload environment variables.
-            CIEnvironmentValues.DecorateSpan(null);
-        }
-
         /// <summary>
         /// OnMethodBegin callback
         /// </summary>
@@ -44,66 +34,16 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
             }
 
             TestInvokerStruct invokerInstance = instance.As<TestInvokerStruct>();
-
-            string testSuite = invokerInstance.TestClass.ToString();
-            string testName = invokerInstance.TestMethod.Name;
-
-            AssemblyName testInvokerAssemblyName = instance.GetType().Assembly.GetName();
-
-            Tracer tracer = Tracer.Instance;
-            string testFramework = "xUnit " + testInvokerAssemblyName.Version.ToString();
-
-            Scope scope = tracer.StartActive("xunit.test");
-            Span span = scope.Span;
-
-            span.Type = SpanTypes.Test;
-            span.SetMetric(Tags.Analytics, 1.0d);
-            span.SetTraceSamplingPriority(SamplingPriority.AutoKeep);
-            span.ResourceName = $"{testSuite}.{testName}";
-            span.SetTag(TestTags.Suite, testSuite);
-            span.SetTag(TestTags.Name, testName);
-            span.SetTag(TestTags.Framework, testFramework);
-            span.SetTag(TestTags.Type, TestTags.TypeTest);
-            CIEnvironmentValues.DecorateSpan(span);
-
-            var framework = FrameworkDescription.Instance;
-
-            span.SetTag(CommonTags.RuntimeName, framework.Name);
-            span.SetTag(CommonTags.RuntimeOSArchitecture, framework.OSArchitecture);
-            span.SetTag(CommonTags.RuntimeOSPlatform, framework.OSPlatform);
-            span.SetTag(CommonTags.RuntimeProcessArchitecture, framework.ProcessArchitecture);
-            span.SetTag(CommonTags.RuntimeVersion, framework.ProductVersion);
-
-            // Get test parameters
-            object[] testMethodArguments = invokerInstance.TestMethodArguments;
-            ParameterInfo[] methodParameters = invokerInstance.TestMethod.GetParameters();
-            if (methodParameters?.Length > 0 && testMethodArguments?.Length > 0)
+            TestRunnerStruct runnerInstance = new TestRunnerStruct
             {
-                for (int i = 0; i < methodParameters.Length; i++)
-                {
-                    if (i < testMethodArguments.Length)
-                    {
-                        span.SetTag($"{TestTags.Arguments}.{methodParameters[i].Name}", testMethodArguments[i]?.ToString() ?? "(null)");
-                    }
-                    else
-                    {
-                        span.SetTag($"{TestTags.Arguments}.{methodParameters[i].Name}", "(default)");
-                    }
-                }
-            }
+                Aggregator = invokerInstance.Aggregator,
+                TestCase = invokerInstance.TestCase,
+                TestClass = invokerInstance.TestClass,
+                TestMethod = invokerInstance.TestMethod,
+                TestMethodArguments = invokerInstance.TestMethodArguments
+            };
 
-            // Get traits
-            Dictionary<string, List<string>> traits = invokerInstance.TestCase.Traits;
-            if (traits.Count > 0)
-            {
-                foreach (KeyValuePair<string, List<string>> traitValue in traits)
-                {
-                    span.SetTag($"{TestTags.Traits}.{traitValue.Key}", string.Join(", ", traitValue.Value) ?? "(null)");
-                }
-            }
-
-            scope.Span.ResetStartTime();
-            return new CallTargetState(scope);
+            return new CallTargetState(XUnitIntegration.CreateScope(ref runnerInstance, instance.GetType()));
         }
 
         /// <summary>
@@ -149,27 +89,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
             if (scope != null)
             {
                 TestInvokerStruct invokerInstance = instance.As<TestInvokerStruct>();
-                exception ??= invokerInstance.Aggregator.ToException();
-
-                if (exception != null)
-                {
-                    if (exception.GetType().Name == "SkipException")
-                    {
-                        scope.Span.SetTag(TestTags.Status, TestTags.StatusSkip);
-                        scope.Span.SetTag(TestTags.SkipReason, exception.Message);
-                    }
-                    else
-                    {
-                        scope.Span.SetException(exception);
-                        scope.Span.SetTag(TestTags.Status, TestTags.StatusFail);
-                    }
-                }
-                else
-                {
-                    scope.Span.SetTag(TestTags.Status, TestTags.StatusPass);
-                }
-
-                scope.Dispose();
+                XUnitIntegration.FinishScope(scope, invokerInstance.Aggregator);
             }
 
             return returnValue;
--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/XUnit/XUnitTestRunnerRunAsyncIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/XUnit/XUnitTestRunnerRunAsyncIntegration.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using Datadog.Trace.Ci;
 using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.DuckTyping;
-using Datadog.Trace.ExtensionMethods;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
 {
@@ -24,12 +19,6 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
     {
         private const string IntegrationName = "XUnit";
 
-        static XUnitTestRunnerRunAsyncIntegration()
-        {
-            // Preload environment variables.
-            CIEnvironmentValues.DecorateSpan(null);
-        }
-
         /// <summary>
         /// OnMethodBegin callback
         /// </summary>
@@ -46,71 +35,10 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.XUnit
             TestRunnerStruct runnerInstance = instance.As<TestRunnerStruct>();
 
             // Skip test support
-            if (runnerInstance.SkipReason is null)
+            if (runnerInstance.SkipReason != null)
             {
-                return CallTargetState.GetDefault();
+                XUnitIntegration.CreateScope(ref runnerInstance, instance.GetType());
             }
-
-            string testSuite = runnerInstance.TestClass.ToString();
-            string testName = runnerInstance.TestMethod.Name;
-
-            AssemblyName testInvokerAssemblyName = instance.GetType().Assembly.GetName();
-
-            Tracer tracer = Tracer.Instance;
-            string testFramework = "xUnit " + testInvokerAssemblyName.Version.ToString();
-
-            using Scope scope = tracer.StartActive("xunit.test");
-            Span span = scope.Span;
-
-            span.Type = SpanTypes.Test;
-            span.SetMetric(Tags.Analytics, 1.0d);
-            span.SetTraceSamplingPriority(SamplingPriority.AutoKeep);
-            span.ResourceName = $"{testSuite}.{testName}";
-            span.SetTag(TestTags.Suite, testSuite);
-            span.SetTag(TestTags.Name, testName);
-            span.SetTag(TestTags.Framework, testFramework);
-            span.SetTag(TestTags.Type, TestTags.TypeTest);
-            CIEnvironmentValues.DecorateSpan(span);
-
-            var framework = FrameworkDescription.Instance;
-
-            span.SetTag(CommonTags.RuntimeName, framework.Name);
-            span.SetTag(CommonTags.RuntimeOSArchitecture, framework.OSArchitecture);
-            span.SetTag(CommonTags.RuntimeOSPlatform, framework.OSPlatform);
-            span.SetTag(CommonTags.RuntimeProcessArchitecture, framework.ProcessArchitecture);
-            span.SetTag(CommonTags.RuntimeVersion, framework.ProductVersion);
-
-            // Get test parameters
-            object[] testMethodArguments = runnerInstance.TestMethodArguments;
-            ParameterInfo[] methodParameters = runnerInstance.TestMethod.GetParameters();
-            if (methodParameters?.Length > 0 && testMethodArguments?.Length > 0)
-            {
-                for (int i = 0; i < methodParameters.Length; i++)
-                {
-                    if (i < testMethodArguments.Length)
-                    {
-                        span.SetTag($"{TestTags.Arguments}.{methodParameters[i].Name}", testMethodArguments[i]?.ToString() ?? "(null)");
-                    }
-                    else
-                    {
-                        span.SetTag($"{TestTags.Arguments}.{methodParameters[i].Name}", "(default)");
-                    }
-                }
-            }
-
-            // Get traits
-            Dictionary<string, List<string>> traits = runnerInstance.TestCase.Traits;
-            if (traits.Count > 0)
-            {
-                foreach (KeyValuePair<string, List<string>> traitValue in traits)
-                {
-                    span.SetTag($"{TestTags.Traits}.{traitValue.Key}", string.Join(", ", traitValue.Value) ?? "(null)");
-                }
-            }
-
-            span.SetTag(TestTags.Status, TestTags.StatusSkip);
-            span.SetTag(TestTags.SkipReason, runnerInstance.SkipReason);
-            span.Finish(TimeSpan.Zero);
 
             return CallTargetState.GetDefault();
         }
