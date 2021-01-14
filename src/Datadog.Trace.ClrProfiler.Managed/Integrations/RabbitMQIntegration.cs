5cf3b1395dea8969fce14480a2354a870cc0bf50
Andrew Lock <andrew.lock@datadoghq.com>
2021-01-14 12:42:39 +0000

    Fix RabbitMQ not using correct servicename

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/RabbitMQIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/RabbitMQIntegration.cs
@@ -826,7 +826,7 @@ namespace Datadog.Trace.ClrProfiler.Integrations
                 Span parent = tracer.ActiveScope?.Span;
 
                 tags = new RabbitMQTags(spanKind);
-                scope = tracer.StartActiveWithTags(OperationName, parent: parentContext, tags: tags, serviceName: $"{tracer.DefaultServiceName}-{IntegrationName}", startTime: startTime);
+                scope = tracer.StartActiveWithTags(OperationName, parent: parentContext, tags: tags, serviceName: $"{tracer.DefaultServiceName}-{ServiceName}", startTime: startTime);
                 var span = scope.Span;
 
                 span.Type = SpanTypes.Queue;
