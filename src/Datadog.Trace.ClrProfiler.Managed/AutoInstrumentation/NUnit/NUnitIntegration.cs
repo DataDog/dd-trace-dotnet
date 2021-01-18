e87bc3e6f4db9ba7e4ef06c4ef3342b07c9edbcb
Tony Redondo <daniel.redondo@undefinedlabs.com>
2021-01-18 10:27:08 +0100

    Fix CIApp compability by removing the Analyzed flag in the span.

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/NUnit/NUnitIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/NUnit/NUnitIntegration.cs
@@ -39,7 +39,6 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.NUnit
             Span span = scope.Span;
 
             span.Type = SpanTypes.Test;
-            span.SetMetric(Tags.Analytics, 1.0d);
             span.SetTraceSamplingPriority(SamplingPriority.AutoKeep);
             span.ResourceName = $"{testSuite}.{testName}";
             span.SetTag(TestTags.Suite, testSuite);
