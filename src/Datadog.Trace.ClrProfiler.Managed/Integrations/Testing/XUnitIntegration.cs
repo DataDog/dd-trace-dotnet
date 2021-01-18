f6138a18ec2bb2630351b0158bbe4691be5dbbb6
Tony Redondo <daniel.redondo@undefinedlabs.com>
2021-01-18 11:32:13 +0100

    Fix CIApp integrations by removing the Analyzed span tag.

--- src/Datadog.Trace.ClrProfiler.Managed/Integrations/Testing/XUnitIntegration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Integrations/Testing/XUnitIntegration.cs
@@ -415,7 +415,6 @@ namespace Datadog.Trace.ClrProfiler.Integrations.Testing
                 Span span = scope.Span;
 
                 span.Type = SpanTypes.Test;
-                span.SetMetric(Tags.Analytics, 1.0d);
                 span.SetTraceSamplingPriority(SamplingPriority.AutoKeep);
                 span.ResourceName = $"{testSuite}.{testName}";
                 span.SetTag(TestTags.Suite, testSuite);
