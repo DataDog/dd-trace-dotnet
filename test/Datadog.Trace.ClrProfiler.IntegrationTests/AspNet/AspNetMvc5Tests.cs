bbf061237bd0f66c1f03820cb00afbe3d86fbff0
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-02 11:40:03 +0000

    Enhance AspNet tests to check correct tags are added, and to test areas

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetMvc5Tests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetMvc5Tests.cs
@@ -1,5 +1,6 @@
 #if NET461
 
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
@@ -21,23 +22,30 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             _iisFixture.TryStartIis(this);
         }
 
+        public static TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>> Data =>
+            new TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>>
+            {
+                { "/DataDog/DogHouse/Woof", "GET /datadog/doghouse/woof", HttpStatusCode.OK, false, null, null, DatadogAreaTags() },
+                { "/Home/Index", "GET /home/index", HttpStatusCode.OK, false, null, null, HomeIndexTags() },
+                { "/delay/0", "GET /delay/{seconds}", HttpStatusCode.OK, false, null, null, DelayTags() },
+                { "/delay-async/0", "GET /delay-async/{seconds}", HttpStatusCode.OK, false, null, null, DelayAsyncTags() },
+                { "/badrequest", "GET /badrequest", HttpStatusCode.InternalServerError, true, null, null, BadRequestTags() },
+                { "/statuscode/201", "GET /statuscode/{value}", HttpStatusCode.Created, false, null, null, StatusCodeTags() },
+                { "/statuscode/503", "GET /statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.", StatusCodeTags() }
+            };
+
         [Theory]
         [Trait("Category", "EndToEnd")]
         [Trait("Integration", nameof(Integrations.AspNetMvcIntegration))]
-        [InlineData("/Home/Index", "GET", "/home/index", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/delay/0", "GET", "/delay/{seconds}", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/delay-async/0", "GET", "/delay-async/{seconds}", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/badrequest", "GET", "/badrequest", HttpStatusCode.InternalServerError, true, null, null)]
-        [InlineData("/statuscode/201", "GET", "/statuscode/{value}", HttpStatusCode.Created, false, null, null)]
-        [InlineData("/statuscode/503", "GET", "/statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.")]
+        [MemberData(nameof(Data))]
         public async Task SubmitsTraces(
             string path,
-            string expectedVerb,
-            string expectedResourceSuffix,
+            string expectedResourceName,
             HttpStatusCode expectedStatusCode,
             bool isError,
             string expectedErrorType,
-            string expectedErrorMessage)
+            string expectedErrorMessage,
+            Dictionary<string, string> expectedTags)
         {
             await AssertWebServerSpan(
                 path,
@@ -49,9 +57,61 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 expectedErrorMessage,
                 "web",
                 "aspnet-mvc.request",
-                $"{expectedVerb} {expectedResourceSuffix}",
-                "1.0.0");
+                expectedResourceName,
+                "1.0.0",
+                expectedTags);
         }
+
+        private static Dictionary<string, string> DatadogAreaTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "Datadog/{controller}/{action}/{id}" },
+                { Tags.AspNetController, "doghouse" },
+                { Tags.AspNetAction, "woof" },
+                { Tags.AspNetArea, "datadog" }
+            };
+
+        private static string DefaultRoute() => "{controller}/{action}/{id}";
+
+        private static Dictionary<string, string> HomeIndexTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, DefaultRoute() },
+                { Tags.AspNetController, "home" },
+                { Tags.AspNetAction, "index" }
+            };
+
+        private static Dictionary<string, string> DelayTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "delay/{seconds}" },
+                { Tags.AspNetController, "home" },
+                { Tags.AspNetAction, "delay" }
+            };
+
+        private static Dictionary<string, string> BadRequestTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, DefaultRoute() },
+                { Tags.AspNetController, "home" },
+                { Tags.AspNetAction, "badrequest" }
+            };
+
+        private static Dictionary<string, string> DelayAsyncTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "delay-async/{seconds}" },
+                { Tags.AspNetController, "home" },
+                { Tags.AspNetAction, "delayasync" }
+            };
+
+        private static Dictionary<string, string> StatusCodeTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, DefaultRoute() },
+                { Tags.AspNetController, "home" },
+                { Tags.AspNetAction, "statuscode" }
+            };
     }
 }
 
--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebApi2Tests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebApi2Tests.cs
@@ -1,5 +1,6 @@
 #if NET461
 
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
@@ -21,16 +22,22 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             _iisFixture.TryStartIis(this);
         }
 
+        public static TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>> Data =>
+            new TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>>
+            {
+                { "/api/environment", "GET api/environment", HttpStatusCode.OK, false, null, null, EnvironmentTags() },
+                { "/api/delay/0", "GET api/delay/{seconds}", HttpStatusCode.OK, false, null, null, DelayTags() },
+                { "/api/delay-async/0", "GET api/delay-async/{seconds}", HttpStatusCode.OK, false, null, null, DelayAsyncTags() },
+                { "/api/transient-failure/true", "GET api/transient-failure/{value}", HttpStatusCode.OK, false, null, null, TransientFailureTags() },
+                { "/api/transient-failure/false", "GET api/transient-failure/{value}", HttpStatusCode.InternalServerError, true, null, null, TransientFailureTags() },
+                { "/api/statuscode/201", "GET api/statuscode/{value}", HttpStatusCode.Created, false, null, null, StatusCodeTags() },
+                { "/api/statuscode/503", "GET api/statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.", StatusCodeTags() },
+            };
+
         [Theory]
         [Trait("Category", "EndToEnd")]
         [Trait("Integration", nameof(Integrations.AspNetWebApi2Integration))]
-        [InlineData("/api/environment", "GET api/environment", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/api/delay/0", "GET api/delay/{seconds}", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/api/delay-async/0", "GET api/delay-async/{seconds}", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/api/transient-failure/true", "GET api/transient-failure/{value}", HttpStatusCode.OK, false, null, null)]
-        [InlineData("/api/transient-failure/false", "GET api/transient-failure/{value}", HttpStatusCode.InternalServerError, true, null, null)]
-        [InlineData("/api/statuscode/201", "GET api/statuscode/{value}", HttpStatusCode.Created, false, null, null)]
-        [InlineData("/api/statuscode/503", "GET api/statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.")]
+        [MemberData(nameof(Data))]
         public async Task SubmitsTraces(
             string path,
             string expectedResourceName,
@@ -52,6 +59,46 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 expectedResourceName,
                 "1.0.0");
         }
+
+        private static Dictionary<string, string> EnvironmentTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "api/environment" },
+                { Tags.AspNetController, "api" },
+                { Tags.AspNetAction, "environment" },
+            };
+
+        private static Dictionary<string, string> TransientFailureTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "api/transient-failure/{value}" },
+                { Tags.AspNetController, "api" },
+                { Tags.AspNetAction, "transientfailure" }
+            };
+
+        private static Dictionary<string, string> DelayTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "api/delay/{seconds}" },
+                { Tags.AspNetController, "api" },
+                { Tags.AspNetAction, "delay" }
+            };
+
+        private static Dictionary<string, string> DelayAsyncTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "api/delay-async/{seconds}" },
+                { Tags.AspNetController, "api" },
+                { Tags.AspNetAction, "delayasync" }
+            };
+
+        private static Dictionary<string, string> StatusCodeTags() =>
+            new Dictionary<string, string>
+            {
+                { Tags.AspNetRoute, "api/statuscode/{value}" },
+                { Tags.AspNetController, "api" },
+                { Tags.AspNetAction, "statuscode" }
+            };
     }
 }
 
