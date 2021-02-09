691eabd9f5f9001857270ccc03fbe784779175f4
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2021-02-09 22:37:31 +0100

    Add base DBCommand async overloads

---
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/AdoNet/AdoNetDefinitions.cs
@@ -0,0 +1,26 @@
+﻿using static Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet.AdoNetClientInstrumentMethodAttribute;
+using static Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet.AdoNetConstants;
+
+/********************************************************************************
+ * Task<int> .ExecuteNonQueryAsync(CancellationToken)
+ ********************************************************************************/
+
+// Task<int> System.Data.Common.DBCommand.ExecuteNonQueryAsync(CancellationToken)
+[assembly: CommandExecuteNonQueryAsync(typeof(SystemDataClientData))]
+[assembly: CommandExecuteNonQueryAsync(typeof(SystemDataCommonClientData))]
+
+/********************************************************************************
+ * Task<DbDataReader> .ExecuteDbDataReaderAsync(CommandBehavior, CancellationToken)
+ ********************************************************************************/
+
+// Task<DbDataReader> System.Data.Common.DBCommand.ExecuteDbDataReaderAsync(CommandBehavior, CancellationToken)
+[assembly: CommandExecuteDbDataReaderWithBehaviorAsync(typeof(SystemDataClientData))]
+[assembly: CommandExecuteDbDataReaderWithBehaviorAsync(typeof(SystemDataCommonClientData))]
+
+/********************************************************************************
+ * Task<object> .ExecuteScalarAsync(CancellationToken)
+ ********************************************************************************/
+
+// Task<object> System.Data.Common.DBCommand.ExecuteScalarAsync(CancellationToken)
+[assembly: CommandExecuteScalarAsync(typeof(SystemDataClientData))]
+[assembly: CommandExecuteScalarAsync(typeof(SystemDataCommonClientData))]
