d0403803ef2ccc495ef06d40ed8b8d224d115526
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-09 09:36:52 +0000

    Use DD_TRACE_LOGGING_RATE instead of DD_LOGGING_RATE

--- src/Datadog.Trace/Configuration/ConfigurationKeys.cs
+++ src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -229,7 +229,7 @@ namespace Datadog.Trace.Configuration
         /// identical log messages, for Tracer log files.
         /// Default value is 60s. Setting to 0 disables rate limiting.
         /// </summary>
-        public const string LogRateLimit = "DD_LOGGING_RATE";
+        public const string LogRateLimit = "DD_TRACE_LOGGING_RATE";
 
         /// <summary>
         /// Configuration key for setting the path to the .NET Tracer native log file.
