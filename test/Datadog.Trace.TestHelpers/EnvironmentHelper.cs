7c35d74206e3da6a84ad3823d702723173ca60d2
Zach Montoya <zach.montoya@datadoghq.com>
2021-02-12 14:10:54 -0800

    Temporarily enable DD_TRACE_DEBUG_LOOKUP_FALLBACK so all CallSite instrumentation must go through the reflection lookup instead of the Module.ResolveMethod mechanism

--- test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
+++ test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -189,6 +189,9 @@ namespace Datadog.Trace.TestHelpers
             environmentVariables["DD_TRACE_AGENT_HOSTNAME"] = "127.0.0.1";
             environmentVariables["DD_TRACE_AGENT_PORT"] = agentPort.ToString();
 
+            // TODO: REMOVE
+            environmentVariables["DD_TRACE_DEBUG_LOOKUP_FALLBACK"] = "true";
+
             // for ASP.NET Core sample apps, set the server's port
             environmentVariables["ASPNETCORE_URLS"] = $"http://127.0.0.1:{aspNetCorePort}/";
 
