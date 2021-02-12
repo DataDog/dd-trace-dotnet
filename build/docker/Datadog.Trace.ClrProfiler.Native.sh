e5b0bdeeef09228680ad1feb8cac8605b6a224c3
Kevin Gosse <krix33@gmail.com>
2021-02-12 14:44:09 +0100

    Fix build configuration

--- build/docker/Datadog.Trace.ClrProfiler.Native.sh
+++ build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -12,11 +12,11 @@ cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
 (cd build && cmake ../ && make)
 
-mkdir -p bin/Debug/x64
-cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/Datadog.Trace.ClrProfiler.Native.so
+mkdir -p bin/Release/x64
+cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Release/x64/Datadog.Trace.ClrProfiler.Native.so
 
-mkdir -p bin/Debug/x64/netstandard2.0
-cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Debug/x64/netstandard2.0/
+mkdir -p bin/Release/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Release/x64/netstandard2.0/
 
-mkdir -p bin/Debug/x64/netcoreapp3.1
-cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Debug/x64/netcoreapp3.1/
+mkdir -p bin/Release/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Release/x64/netcoreapp3.1/
