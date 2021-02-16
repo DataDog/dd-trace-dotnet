1e0e519f2724884afb3d37d11bf6ce50c6ef85b7
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-16 16:31:39 +0000

    Add test for Tracer using DatadogTcp transport
    This test currently hangs if you call tracer.FlushAsync!

---
+++ test/Datadog.Trace.IntegrationTests/DatadogHttpClientTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Datadog.Core.Tools;
+using Datadog.Trace.Agent;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.IntegrationTests
+{
+    public class DatadogHttpClientTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public DatadogHttpClientTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void DatadogHttpClient_CanSendTracesToAgent()
+        {
+            var agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            {
+                agent.RequestDeserialized += (sender, args) =>
+                {
+                    _output.WriteLine($"Received {args.Value.Count} traces");
+                };
+
+                var settings = new TracerSettings
+                {
+                    AgentUri = new Uri($"http://localhost:{agent.Port}"),
+                    TracesTransport = TransportStrategy.DatadogTcp,
+                };
+                var tracer = new Tracer(settings);
+
+                using (var scope = tracer.StartActive("operationName"))
+                {
+                    scope.Span.ResourceName = "resourceName";
+                }
+
+                // When this is added in, the test deadlocks! :scream:
+                // await tracer.FlushAsync();
+
+                var spans = agent.WaitForSpans(1);
+                Assert.Equal(1, spans.Count);
+            }
+        }
+    }
+}
