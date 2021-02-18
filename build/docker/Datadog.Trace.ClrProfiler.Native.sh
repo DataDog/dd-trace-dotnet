3cf338f350d0762792a42b80d9b2416384b59c6c
Kevin Gosse <krix33@gmail.com>
2021-02-18 16:03:15 +0100

    Enable optimizations when building the profiler

--- build/docker/Datadog.Trace.ClrProfiler.Native.sh
+++ build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -10,7 +10,7 @@ PUBLISH_OUTPUT_NET31="$( pwd )/src/bin/managed-publish/netcoreapp3.1"
 
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
-(cd build && cmake ../ && make)
+(cd build && cmake ../ -DCMAKE_BUILD_TYPE=Release && make)
 
 mkdir -p bin/Release/x64
 cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Release/x64/Datadog.Trace.ClrProfiler.Native.so
