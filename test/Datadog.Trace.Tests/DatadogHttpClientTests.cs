8fb3399e049a1bca76b799fee0bd043a74d2ef40
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-24 13:19:31 +0000

    Add unit test for DatadogHttpClient response parsing

---
+++ test/Datadog.Trace.Tests/DatadogHttpClientTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Datadog.Trace.HttpOverStreams;
+using Datadog.Trace.HttpOverStreams.HttpContent;
+using Xunit;
+
+namespace Datadog.Trace.Tests
+{
+    public class DatadogHttpClientTests
+    {
+        [Fact]
+        public async Task DatadogHttpClient_CanParseResponse()
+        {
+            var client = new DatadogHttpClient();
+            var requestContent = new BufferContent(new ArraySegment<byte>(new byte[0]));
+            var htmlResponse = string.Join("\r\n", HtmlResponseLines());
+            using var requestStream = new MemoryStream();
+            using var responseStream = new MemoryStream(Encoding.UTF8.GetBytes(htmlResponse));
+
+            var request = new HttpRequest("POST", "localhost", string.Empty, new HttpHeaders(), requestContent);
+            var response = await client.SendAsync(request, requestStream, responseStream);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal("OK", response.ResponseMessage);
+            Assert.Equal("Test Server", response.Headers.GetValue(("Server")));
+            Assert.Equal(2, response.ContentLength);
+            Assert.Equal("application/json", response.ContentType);
+
+            var buffer = new byte[2];
+            await response.Content.CopyToAsync(buffer);
+            var content = Encoding.UTF8.GetString(buffer);
+            Assert.Equal("{}", content);
+        }
+
+        private static string[] HtmlResponseLines() =>
+        new[]
+        {
+            "HTTP/1.1 200 OK",
+            "Date: Mon, 27 Jul 2009 12:28:53 GMT",
+            "Server: Test Server",
+            "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT",
+            "Content-Length: 2",
+            "Content-Type: application/json",
+            string.Empty,
+            "{}"
+        };
+    }
+}
