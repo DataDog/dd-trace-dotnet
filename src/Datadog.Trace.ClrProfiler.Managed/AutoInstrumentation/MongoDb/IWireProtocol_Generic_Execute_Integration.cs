e6a5206662f88cd97bacdf8479d1906aa12af0d4
Kevin Gosse <krix33@gmail.com>
2021-02-24 14:23:12 +0100

    Fix integrations

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/MongoDb/IWireProtocol_Generic_Execute_Integration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/MongoDb/IWireProtocol_Generic_Execute_Integration.cs
@@ -6,13 +6,28 @@ using Datadog.Trace.ClrProfiler.Integrations;
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
 {
     /// <summary>
-    /// MongoDB.Driver.Core.WireProtocol.IWireProtocol instrumentation
+    /// MongoDB.Driver.Core.WireProtocol.IWireProtocol&lt;TResult&gt; instrumentation
     /// </summary>
     [MongoDbExecute(
-        typeName: "MongoDB.Driver.Core.WireProtocol.KillCursorsWireProtocol",
-        isGeneric: false)]
+        typeName: "MongoDB.Driver.Core.WireProtocol.CommandUsingQueryMessageWireProtocol`1",
+        isGeneric: true)]
+    [MongoDbExecute(
+        typeName: "MongoDB.Driver.Core.WireProtocol.CommandUsingCommandMessageWireProtocol`1",
+        isGeneric: true)]
+    [MongoDbExecute(
+        typeName: "MongoDB.Driver.Core.WireProtocol.CommandWireProtocol`1",
+        isGeneric: true)]
+    [MongoDbExecute(
+        typeName: "MongoDB.Driver.Core.WireProtocol.GetMoreWireProtocol`1",
+        isGeneric: true)]
+    [MongoDbExecute(
+        typeName: "MongoDB.Driver.Core.WireProtocol.QueryWireProtocol`1",
+        isGeneric: true)]
+    [MongoDbExecute(
+        typeName: "MongoDB.Driver.Core.WireProtocol.WriteWireProtocolBase`1",
+        isGeneric: true)]
     // ReSharper disable once InconsistentNaming
-    public class IWireProtocol_Execute_Integration
+    public class IWireProtocol_Generic_Execute_Integration
     {
         /// <summary>
         /// OnMethodBegin callback
@@ -33,17 +48,19 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
         /// OnAsyncMethodEnd callback
         /// </summary>
         /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TReturn">Type of the return value</typeparam>
         /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="returnValue">Return value</param>
         /// <param name="exception">Exception instance in case the original code threw an exception.</param>
         /// <param name="state">Calltarget state value</param>
         /// <returns>A response value, in an async scenario will be T of Task of T</returns>
-        public static CallTargetReturn OnMethodEnd<TTarget>(TTarget instance, Exception exception, CallTargetState state)
+        public static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception exception, CallTargetState state)
         {
             var scope = state.Scope;
 
             scope.DisposeWithException(exception);
 
-            return CallTargetReturn.GetDefault();
+            return new CallTargetReturn<TReturn>(returnValue);
         }
     }
 }
