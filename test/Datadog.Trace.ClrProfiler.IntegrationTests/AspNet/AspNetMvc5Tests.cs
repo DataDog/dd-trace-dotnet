ef3663dc336866341ce892947a0f3a67dc190dfe
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-26 16:54:07 +0000

    Update AspNetMvc5Tests to test additional cases
    Also fixes some broken tests

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetMvc5Tests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetMvc5Tests.cs
@@ -10,7 +10,7 @@ using Xunit.Abstractions;
 
 namespace Datadog.Trace.ClrProfiler.IntegrationTests
 {
-    [Collection("IisTests")]
+    [Collection(nameof(IisFixture))]
     public class AspNetMvc5TestsCallsite : AspNetMvc5Tests
     {
         public AspNetMvc5TestsCallsite(IisFixture iisFixture, ITestOutputHelper output)
@@ -19,7 +19,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         }
     }
 
-    [Collection("IisTests")]
+    [Collection(nameof(IisFixture))]
     public class AspNetMvc5TestsCallTargetNoInlining : AspNetMvc5Tests
     {
         public AspNetMvc5TestsCallTargetNoInlining(IisFixture iisFixture, ITestOutputHelper output)
@@ -28,7 +28,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         }
     }
 
-    [Collection("IisTests")]
+    [Collection(nameof(IisFixture))]
     public class AspNetMvc5TestsCallTarget : AspNetMvc5Tests
     {
         public AspNetMvc5TestsCallTarget(IisFixture iisFixture, ITestOutputHelper output)
@@ -51,17 +51,23 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             _iisFixture.TryStartIis(this);
         }
 
-        public static TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>> Data =>
-            new TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>>
-            {
-                { "/DataDog/DogHouse/Woof", "GET /datadog/doghouse/woof", HttpStatusCode.OK, false, null, null, DatadogAreaTags() },
-                { "/Home/Index", "GET /home/index", HttpStatusCode.OK, false, null, null, HomeIndexTags() },
-                { "/delay/0", "GET /delay/{seconds}", HttpStatusCode.OK, false, null, null, DelayTags() },
-                { "/delay-async/0", "GET /delay-async/{seconds}", HttpStatusCode.OK, false, null, null, DelayAsyncTags() },
-                { "/badrequest", "GET /badrequest", HttpStatusCode.InternalServerError, true, null, null, BadRequestTags() },
-                { "/statuscode/201", "GET /statuscode/{value}", HttpStatusCode.Created, false, null, null, StatusCodeTags() },
-                { "/statuscode/503", "GET /statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.", StatusCodeTags() }
-            };
+        public static TheoryData<string, string, HttpStatusCode, bool, string, string, SerializableDictionary> Data => new()
+        {
+            { "/DataDog/DogHouse", "GET /datadog/doghouse", HttpStatusCode.OK, false, null, null, DatadogAreaTags() },
+            { "/DataDog/DogHouse/Woof", "GET /datadog/doghouse/woof", HttpStatusCode.OK, false, null, null, DatadogAreaWoofTags() },
+            { "/", "GET /", HttpStatusCode.OK, false, null, null, HomeIndexTags() },
+            { "/Home", "GET /home", HttpStatusCode.OK, false, null, null, HomeIndexTags() },
+            { "/Home/Index", "GET /home/index", HttpStatusCode.OK, false, null, null, HomeIndexTags() },
+            { "/Home/Get", "GET /home/get", HttpStatusCode.InternalServerError, true, "System.ArgumentException", MissingParameterError(), HomeGetTags() },
+            { "/Home/Get/3", "GET /home/get/?", HttpStatusCode.OK, false, null, null, HomeGetTags() },
+            { "/delay/0", "GET /delay/{seconds}", HttpStatusCode.OK, false, null, null, DelayTags() },
+            { "/delay-async/0", "GET /delay-async/{seconds}", HttpStatusCode.OK, false, null, null, DelayAsyncTags() },
+            { "/delay-optional", "GET /delay-optional/{seconds}", HttpStatusCode.OK, false, null, null, DelayOptionalTags() },
+            { "/delay-optional/1", "GET /delay-optional/{seconds}", HttpStatusCode.OK, false, null, null, DelayOptionalTags() },
+            { "/badrequest", "GET /badrequest", HttpStatusCode.InternalServerError, true, "System.Exception", "Oops, it broke.", BadRequestTags() },
+            { "/statuscode/201", "GET /statuscode/{value}", HttpStatusCode.Created, false, null, null, StatusCodeTags() },
+            { "/statuscode/503", "GET /statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.", StatusCodeTags() },
+        };
 
         [Theory]
         [Trait("Category", "EndToEnd")]
@@ -74,7 +80,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             bool isError,
             string expectedErrorType,
             string expectedErrorMessage,
-            Dictionary<string, string> expectedTags)
+            SerializableDictionary expectedTags)
         {
             await AssertWebServerSpan(
                 path,
@@ -91,57 +97,75 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 expectedTags);
         }
 
-        private static Dictionary<string, string> DatadogAreaTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "Datadog/{controller}/{action}/{id}" },
-                { Tags.AspNetController, "doghouse" },
-                { Tags.AspNetAction, "woof" },
-                { Tags.AspNetArea, "datadog" }
-            };
+        private static SerializableDictionary DatadogAreaTags() => new()
+        {
+            { Tags.AspNetRoute, "Datadog/{controller}/{action}/{id}" },
+            { Tags.AspNetController, "doghouse" },
+            { Tags.AspNetAction, "index" },
+            { Tags.AspNetArea, "datadog" }
+        };
+
+        private static SerializableDictionary DatadogAreaWoofTags() => new()
+        {
+            { Tags.AspNetRoute, "Datadog/{controller}/{action}/{id}" },
+            { Tags.AspNetController, "doghouse" },
+            { Tags.AspNetAction, "woof" },
+            { Tags.AspNetArea, "datadog" }
+        };
 
         private static string DefaultRoute() => "{controller}/{action}/{id}";
 
-        private static Dictionary<string, string> HomeIndexTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, DefaultRoute() },
-                { Tags.AspNetController, "home" },
-                { Tags.AspNetAction, "index" }
-            };
+        private static SerializableDictionary HomeIndexTags() => new()
+        {
+            { Tags.AspNetRoute, DefaultRoute() },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "index" }
+        };
 
-        private static Dictionary<string, string> DelayTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "delay/{seconds}" },
-                { Tags.AspNetController, "home" },
-                { Tags.AspNetAction, "delay" }
-            };
+        private static SerializableDictionary HomeGetTags() => new()
+        {
+            { Tags.AspNetRoute, DefaultRoute() },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "get" }
+        };
 
-        private static Dictionary<string, string> BadRequestTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, DefaultRoute() },
-                { Tags.AspNetController, "home" },
-                { Tags.AspNetAction, "badrequest" }
-            };
+        private static SerializableDictionary DelayTags() => new()
+        {
+            { Tags.AspNetRoute, "delay/{seconds}" },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "delay" }
+        };
 
-        private static Dictionary<string, string> DelayAsyncTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "delay-async/{seconds}" },
-                { Tags.AspNetController, "home" },
-                { Tags.AspNetAction, "delayasync" }
-            };
+        private static SerializableDictionary DelayOptionalTags() => new()
+        {
+            { Tags.AspNetRoute, "delay-optional/{seconds}" },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "optional" }
+        };
 
-        private static Dictionary<string, string> StatusCodeTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, DefaultRoute() },
-                { Tags.AspNetController, "home" },
-                { Tags.AspNetAction, "statuscode" }
-            };
+        private static SerializableDictionary BadRequestTags() => new()
+        {
+            { Tags.AspNetRoute, "badrequest" },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "badrequest" }
+        };
+
+        private static SerializableDictionary DelayAsyncTags() => new()
+        {
+            { Tags.AspNetRoute, "delay-async/{seconds}" },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "delayasync" }
+        };
+
+        private static SerializableDictionary StatusCodeTags() => new()
+        {
+            { Tags.AspNetRoute, "statuscode/{value}" },
+            { Tags.AspNetController, "home" },
+            { Tags.AspNetAction, "statuscode" }
+        };
+
+        private static string MissingParameterError() => @"The parameters dictionary contains a null entry for parameter 'id' of non-nullable type 'System.Int32' for method 'System.Web.Mvc.ActionResult Get(Int32)' in 'Samples.AspNetMvc5.Controllers.HomeController'. An optional parameter must be a reference type, a nullable type, or be declared as an optional parameter.
+Parameter name: parameters";
     }
 }
-
 #endif
