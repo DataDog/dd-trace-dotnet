1c172f7a2ddb3b77afc44cb54cc089a14c98fa79
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-27 16:13:17 +0000

    Update AspNetWebApi2Tests to test additional cases
    Also fixes some broken tests

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebApi2Tests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebApi2Tests.cs
@@ -51,17 +51,19 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             _iisFixture.TryStartIis(this);
         }
 
-        public static TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>> Data =>
-            new TheoryData<string, string, HttpStatusCode, bool, string, string, Dictionary<string, string>>
-            {
-                { "/api/environment", "GET api/environment", HttpStatusCode.OK, false, null, null, EnvironmentTags() },
-                { "/api/delay/0", "GET api/delay/{seconds}", HttpStatusCode.OK, false, null, null, DelayTags() },
-                { "/api/delay-async/0", "GET api/delay-async/{seconds}", HttpStatusCode.OK, false, null, null, DelayAsyncTags() },
-                { "/api/transient-failure/true", "GET api/transient-failure/{value}", HttpStatusCode.OK, false, null, null, TransientFailureTags() },
-                { "/api/transient-failure/false", "GET api/transient-failure/{value}", HttpStatusCode.InternalServerError, true, null, null, TransientFailureTags() },
-                { "/api/statuscode/201", "GET api/statuscode/{value}", HttpStatusCode.Created, false, null, null, StatusCodeTags() },
-                { "/api/statuscode/503", "GET api/statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.", StatusCodeTags() },
-            };
+        public static TheoryData<string, string, HttpStatusCode, bool, string, string, SerializableDictionary> Data => new()
+        {
+            { "/api/environment", "GET api/environment", HttpStatusCode.OK, false, null, null, EnvironmentTags() },
+            { "/api/absolute-route", "GET api/absolute-route", HttpStatusCode.OK, false, null, null, AbsoluteRouteTags() },
+            { "/api/delay/0", "GET api/delay/{seconds}", HttpStatusCode.OK, false, null, null, DelayTags() },
+            { "/api/delay-optional", "GET api/delay-optional/{seconds}", HttpStatusCode.OK, false, null, null, DelayOptionalTags() },
+            { "/api/delay-optional/1", "GET api/delay-optional/{seconds}", HttpStatusCode.OK, false, null, null, DelayOptionalTags() },
+            { "/api/delay-async/0", "GET api/delay-async/{seconds}", HttpStatusCode.OK, false, null, null, DelayAsyncTags() },
+            { "/api/transient-failure/true", "GET api/transient-failure/{value}", HttpStatusCode.OK, false, null, null, TransientFailureTags() },
+            { "/api/transient-failure/false", "GET api/transient-failure/{value}", HttpStatusCode.InternalServerError, true, "System.ArgumentException", "Passed in value was not 'true': false", TransientFailureTags() },
+            { "/api/statuscode/201", "GET api/statuscode/{value}", HttpStatusCode.Created, false, null, null, StatusCodeTags() },
+            { "/api/statuscode/503", "GET api/statuscode/{value}", HttpStatusCode.ServiceUnavailable, true, null, "The HTTP response has status code 503.", StatusCodeTags() },
+        };
 
         [Theory]
         [Trait("Category", "EndToEnd")]
@@ -73,7 +75,8 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             HttpStatusCode expectedStatusCode,
             bool isError,
             string expectedErrorType,
-            string expectedErrorMessage)
+            string expectedErrorMessage,
+            SerializableDictionary expectedTags)
         {
             await AssertWebServerSpan(
                 path,
@@ -86,48 +89,49 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 "web",
                 "aspnet-webapi.request",
                 expectedResourceName,
-                "1.0.0");
+                "1.0.0",
+                expectedTags);
         }
 
-        private static Dictionary<string, string> EnvironmentTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "api/environment" },
-                { Tags.AspNetController, "api" },
-                { Tags.AspNetAction, "environment" },
-            };
+        private static SerializableDictionary EnvironmentTags() => new()
+        {
+            { Tags.AspNetRoute, "api/environment" },
+        };
 
-        private static Dictionary<string, string> TransientFailureTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "api/transient-failure/{value}" },
-                { Tags.AspNetController, "api" },
-                { Tags.AspNetAction, "transientfailure" }
-            };
+        private static SerializableDictionary AbsoluteRouteTags() => new()
+        {
+            { Tags.AspNetRoute, "api/absolute-route" },
+        };
 
-        private static Dictionary<string, string> DelayTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "api/delay/{seconds}" },
-                { Tags.AspNetController, "api" },
-                { Tags.AspNetAction, "delay" }
-            };
+        private static SerializableDictionary TransientFailureTags() => new()
+        {
+            { Tags.AspNetRoute, "api/transient-failure/{value}" },
+        };
 
-        private static Dictionary<string, string> DelayAsyncTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "api/delay-async/{seconds}" },
-                { Tags.AspNetController, "api" },
-                { Tags.AspNetAction, "delayasync" }
-            };
+        private static SerializableDictionary DelayHomeTags() => new()
+        {
+            { Tags.AspNetRoute, "delay/{seconds}" },
+        };
 
-        private static Dictionary<string, string> StatusCodeTags() =>
-            new Dictionary<string, string>
-            {
-                { Tags.AspNetRoute, "api/statuscode/{value}" },
-                { Tags.AspNetController, "api" },
-                { Tags.AspNetAction, "statuscode" }
-            };
+        private static SerializableDictionary DelayTags() => new()
+        {
+            { Tags.AspNetRoute, "api/delay/{seconds}" },
+        };
+
+        private static SerializableDictionary DelayOptionalTags() => new()
+        {
+            { Tags.AspNetRoute, "api/delay-optional/{seconds}" },
+        };
+
+        private static SerializableDictionary DelayAsyncTags() => new()
+        {
+            { Tags.AspNetRoute, "api/delay-async/{seconds}" },
+        };
+
+        private static SerializableDictionary StatusCodeTags() => new()
+        {
+            { Tags.AspNetRoute, "api/statuscode/{value}" },
+        };
     }
 }
 
--- test/test-applications/aspnet/Samples.AspNetMvc5/Controllers/ApiController.cs
+++ test/test-applications/aspnet/Samples.AspNetMvc5/Controllers/ApiController.cs
@@ -6,10 +6,11 @@ using System.Web.Http;
 
 namespace Samples.AspNetMvc5.Controllers
 {
+    [RoutePrefix("api")]
     public class ApiController : System.Web.Http.ApiController
     {
         [HttpGet]
-        [Route("api/delay/{seconds}")]
+        [Route("delay/{seconds}")]
         public IHttpActionResult Delay(int seconds)
         {
             Thread.Sleep(TimeSpan.FromSeconds(seconds));
@@ -17,29 +18,44 @@ namespace Samples.AspNetMvc5.Controllers
         }
 
         [HttpGet]
-        [Route("api/delay-async/{seconds}")]
+        [Route("delay-async/{seconds}")]
         public async Task<IHttpActionResult> DelayAsync(int seconds)
         {
             await Task.Delay(TimeSpan.FromSeconds(seconds)).ConfigureAwait(false);
             return Json(seconds);
         }
+        
+        [HttpGet]
+        [Route("delay-optional/{seconds?}")]
+        public IHttpActionResult Optional(int? seconds = null)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(seconds ?? 0));
+            return Json(seconds);
+        }
+        
+        [HttpGet]
+        [Route("~/api/absolute-route")]
+        public IHttpActionResult AbsoluteRoute()
+        {
+            return Ok();
+        }
 
         [HttpGet]
-        [Route("api/environment")]
+        [Route("environment")]
         public IHttpActionResult Environment()
         {
             return Json(System.Environment.GetEnvironmentVariables());
         }
 
         [HttpGet]
-        [Route("api/statuscode/{value}")]
+        [Route("statuscode/{value}")]
         public IHttpActionResult StatusCode(int value)
         {
             return this.Content((HttpStatusCode)value, "Status code set to " + value);
         }
 
         [HttpGet]
-        [Route("api/transient-failure/{value}")]
+        [Route("transient-failure/{value}")]
         public IHttpActionResult TransientFailure(string value)
         {
             bool success = bool.TryParse(value, out bool result) && result;
