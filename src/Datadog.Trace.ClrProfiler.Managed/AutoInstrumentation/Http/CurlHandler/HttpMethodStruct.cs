57ef32f9b9aba6453fc85ede77f345ade71184c6
Tony Redondo <tony.redondo@datadoghq.com>
2021-03-02 12:37:46 +0100

    Initial CurlHandler integration.

---
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/Http/CurlHandler/HttpMethodStruct.cs
@@ -0,0 +1,16 @@
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Http.CurlHandler
+{
+    /// <summary>
+    /// Http method struct copy target for ducktyping
+    /// </summary>
+    [DuckCopy]
+    public struct HttpMethodStruct
+    {
+        /// <summary>
+        /// Gets the http method in string
+        /// </summary>
+        public string Method;
+    }
+}
