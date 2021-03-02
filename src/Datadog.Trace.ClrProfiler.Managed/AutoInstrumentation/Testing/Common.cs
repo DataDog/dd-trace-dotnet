66b5c20ce89dd2036573ccc00ec24fe414e2bb41
Tony Redondo <tony.redondo@datadoghq.com>
2021-03-02 23:33:40 +0100

    Fix CiApp Tests ServiceName (#1244)
    * Fix Tests ServiceName
    
    * Changes DD_SERVICE_NAME to DD_SERVICE
    
    * Changes based on the review.
    
    * Fix ConfigurationSourceTests, due DD_SERVICE being set when testing DD_SERVICE_NAME.
    
    * Add missing DD_ENV to benchmark tests.

---
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/Testing/Common.cs
@@ -0,0 +1,20 @@
+using Datadog.Trace.Ci;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Testing
+{
+    internal static class Common
+    {
+        static Common()
+        {
+            TestTracer = Tracer.Instance;
+            ServiceName = TestTracer.DefaultServiceName;
+
+            // Preload environment variables.
+            CIEnvironmentValues.DecorateSpan(null);
+        }
+
+        internal static Tracer TestTracer { get; private set; }
+
+        internal static string ServiceName { get; private set; }
+    }
+}
