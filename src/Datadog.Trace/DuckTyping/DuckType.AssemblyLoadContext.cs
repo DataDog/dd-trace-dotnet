b72dbf95d6c1d4f2ec971979890ea0ee31ad5932
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2021-03-18 10:13:40 +0100

    Add source code documentation and custom constants.

--- src/Datadog.Trace/DuckTyping/DuckType.AssemblyLoadContext.cs
+++ src/Datadog.Trace/DuckTyping/DuckType.AssemblyLoadContext.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
-#if NETCOREAPP2_0_OR_GREATER
+#if HAVE_ASSEMBLYLOADCONTEXT
 using RuntimeLoader = System.Runtime.Loader;
 #endif
 
@@ -19,22 +19,52 @@ namespace Datadog.Trace.DuckTyping
     /// </summary>
     public static partial class DuckType
     {
+        /// <summary>
+        /// Contains the current module builders, the key is an object that can be an AssemblyLoadContext (NETCORE) or an Assembly (NETFRAMEWORK) instance.
+        /// The usage of the ConditionalWeakTable is due to the fact that an AssemblyLoadContext can be unloaded.
+        /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private static readonly ConditionalWeakTable<object, ModuleBuilder> ModuleBuilders = new ConditionalWeakTable<object, ModuleBuilder>();
 
+        /// <summary>
+        /// Count of emmited AssemblyBuilders.
+        /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private static long _assemblyCount = 0;
 
-#if NETCOREAPP2_2_OR_GREATER || NETSTANDARD2_0
+#if HAVE_ASSEMBLYLOADCONTEXT || IS_NETSTANDARD2_X
+        /// <summary>
+        /// Contains the `DefineDynamicAssemblyDelegate` created on the target context. The key is an AssemblyLoadContext object.
+        /// The usage of the ConditionalWeakTable is due to the fact that an AssemblyLoadContext can be unloaded.
+        /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private static readonly ConditionalWeakTable<object, DefineDynamicAssemblyDelegate> DefineDynamicAssemblies = new ConditionalWeakTable<object, DefineDynamicAssemblyDelegate>();
+
+        /// <summary>
+        /// Context where the DuckType class is loaded.
+        /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private static object _currentContext;
 
+        /// <summary>
+        /// Delegate used to call AssemblyBuilder.DefineDynamicAssembly method in a target context.
+        /// </summary>
+        /// <param name="name">Assembly name</param>
+        /// <returns>Assembly builder instance</returns>
         private delegate AssemblyBuilder DefineDynamicAssemblyDelegate(string name);
 
+        /// <summary>
+        /// Gets the target context where the Assembly builder should be defined given a target type
+        /// </summary>
+        /// <param name="targetType">Target type for ducktyping</param>
+        /// <returns>An AssemblyLoadContext object</returns>
         private static object GetTargetContext(Type targetType)
         {
+            // First we get the AssemblyLoadContext of the target type
+            // If the Type is a Generic type, we scan for the contexts of the generics parameters (eg: Task<T>, Dictioanry<TKey, TValue>)
+            // If we get different contexts we remove the target type context if the target type is from System.Private.CoreLib
+            // If only 1 context remains then we use that. (Context of T from Task<T>)
+            // If we still have multiple context, then we play safe and create a new isolated context for that combination (context of TKey being different to context of TValue in a Dictionary<TKey, TValue>)
             List<object> contexts = new List<object>();
             var targetContext = GetAssemblyLoadContext(targetType.Assembly ?? typeof(DuckType).Assembly);
             contexts.Add(targetContext);
@@ -64,6 +94,12 @@ namespace Datadog.Trace.DuckTyping
             return contexts[0];
         }
 
+        /// <summary>
+        /// Creates an Assembly builder with a name in a target context
+        /// </summary>
+        /// <param name="name">Name of the assembly</param>
+        /// <param name="targetContext">AssemblyLoadContext where the assembly is going to be defined.</param>
+        /// <returns>An AssemblyBuilder instance</returns>
         private static AssemblyBuilder CreateAssemblyBuilder(string name, object targetContext)
         {
             if (_currentContext == null)
@@ -73,6 +109,9 @@ namespace Datadog.Trace.DuckTyping
 
             if (targetContext != _currentContext)
             {
+                // if the target context is different from the current context we need to call the `AssemblyBuilder.DefineDynamicAssembly` from
+                // the target context, for that we load the `Datadog.Trace.AssemblyLoadContext` assembly into the target context and create a delegate
+                // to `Datadog.Trace.AssemblyLoadContext.AssemblyBuilderHelper.DefineDynamicAssembly` method and call it.
                 if (!DefineDynamicAssemblies.TryGetValue(targetContext, out var @delegate))
                 {
                     var loaderAssembly = LoadFromAssemblyPath(targetContext, typeof(AssemblyLoadContext.AssemblyBuilderHelper).Assembly.Location);
@@ -85,11 +124,15 @@ namespace Datadog.Trace.DuckTyping
                 return @delegate(name);
             }
 
+            // In the case we are in the same context we just call `AssemblyBuilder.DefineDynamicAssembly` from here.
             return AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(name), AssemblyBuilderAccess.Run);
         }
 #endif
 
-#if NETCOREAPP2_0_OR_GREATER
+#if HAVE_ASSEMBLYLOADCONTEXT
+        // ***
+        // Indirections to the AssemblyLoadContext methods.
+        // ***
         private static object GetAssemblyLoadContext(Assembly assembly)
         {
             return RuntimeLoader.AssemblyLoadContext.GetLoadContext(assembly);
@@ -105,6 +148,12 @@ namespace Datadog.Trace.DuckTyping
             return new RuntimeLoader.AssemblyLoadContext(name);
         }
 
+        /// <summary>
+        /// Gets the ModuleBuilder instance from a target type.
+        /// By resolving all the AssemblyLoadContext if is required.
+        /// </summary>
+        /// <param name="targetType">Target type for ducktyping</param>
+        /// <returns>ModuleBuilder instance</returns>
         private static ModuleBuilder GetModuleBuilder(Type targetType)
         {
             object targetContext = GetTargetContext(targetType);
@@ -117,7 +166,10 @@ namespace Datadog.Trace.DuckTyping
 
             return moduleBuilder;
         }
-#elif NETSTANDARD2_0
+#elif IS_NETSTANDARD2_X
+        // ***
+        // Proxies to the AssemblyLoadContext methods using delegates and fetchers.
+        // ***
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private const string AssemblyLoadContextTypeName = "System.Runtime.Loader.AssemblyLoadContext, System.Runtime.Loader";
 
@@ -155,6 +207,12 @@ namespace Datadog.Trace.DuckTyping
             return Activator.CreateInstance(_assemblyLoadContextType, name);
         }
 
+        /// <summary>
+        /// Gets the ModuleBuilder instance from a target type.
+        /// By resolving all the AssemblyLoadContext if is required.
+        /// </summary>
+        /// <param name="targetType">Target type for ducktyping</param>
+        /// <returns>ModuleBuilder instance</returns>
         private static ModuleBuilder GetModuleBuilder(Type targetType)
         {
             ModuleBuilder moduleBuilder;
@@ -188,6 +246,12 @@ namespace Datadog.Trace.DuckTyping
             return moduleBuilder;
         }
 
+        /// <summary>
+        /// Fallback method in case `System.Runtime.Loader.AssemblyLoadContext, System.Runtime.Loader` cannot be loaded.
+        /// </summary>
+        /// <param name="name">Name of the assembly</param>
+        /// <param name="targetType">Target type for ducktyping</param>
+        /// <returns>An AssemblyBuilder instance</returns>
         private static AssemblyBuilder CreateAssemblyBuilderFallback(string name, Type targetType)
         {
             return AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(name), AssemblyBuilderAccess.Run);
@@ -258,11 +322,22 @@ namespace Datadog.Trace.DuckTyping
             }
         }
 #else
+        /// <summary>
+        /// Creates an assembly builder with a Name. (.NET Framework / Non AssemblyLoadContext version)
+        /// </summary>
+        /// <param name="name">Name of the assembly</param>
+        /// <param name="targetType">Target type for ducktyping</param>
+        /// <returns>An AssemblyBuilder instance</returns>
         private static AssemblyBuilder CreateAssemblyBuilder(string name, Type targetType)
         {
             return AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(name), AssemblyBuilderAccess.Run);
         }
 
+        /// <summary>
+        /// Gets the ModuleBuilder instance from a target type.  (.NET Framework / Non AssemblyLoadContext version)
+        /// </summary>
+        /// <param name="targetType">Target type for ducktyping</param>
+        /// <returns>ModuleBuilder instance</returns>
         private static ModuleBuilder GetModuleBuilder(Type targetType)
         {
             object targetAssembly = targetType.Assembly ?? typeof(DuckType).Assembly;
