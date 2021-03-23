c0d872f4fb4deed6695d6faf8b17d9e00cdf47b5
Andrew Lock <andrew.lock@datadoghq.com>
2021-03-23 16:20:23 +0000

    Add a moving average keep rate calculator
    Uses a simple moving average implementation to calculate the rate at which traces were kept/dropped
    Handles up to uint.MaxValue traces in a given timespan (1s by default, averaged over 10s)

---
+++ src/Datadog.Trace/Agent/MovingAverageKeepRateCalculator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Datadog.Trace.Logging;
+
+namespace Datadog.Trace.Agent
+{
+    /// <summary>
+    /// Used to calculate the Trace Keep Rate, tracking the number of
+    /// traces kept and dropped that should have been sent to the backend.
+    /// Traces ignored due to sampling are not included in these rates.
+    /// </summary>
+    internal class MovingAverageKeepRateCalculator : IKeepRateCalculator
+    {
+        private const int DefaultKeepRate = 10;
+        private static readonly TimeSpan DefaultBucketDuration = TimeSpan.FromSeconds(1);
+        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<MovingAverageKeepRateCalculator>();
+
+        private readonly int _windowSize;
+        private readonly TimeSpan _bucketDuration;
+        private readonly uint[] _dropped;
+        private readonly uint[] _created;
+
+        private readonly CancellationTokenSource _cts = new();
+
+        private int _index = 0;
+        private long _totals = 0;
+
+        private long _latestDrops = 0;
+        private long _latestKeeps = 0;
+
+        internal MovingAverageKeepRateCalculator(int size, TimeSpan bucketDuration)
+        {
+            if (size < 0 || size > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), "Must be a value between 1 and 100");
+            }
+
+            _windowSize = size;
+            _bucketDuration = bucketDuration;
+            _dropped = new uint[size];
+            _created = new uint[size];
+
+            if (bucketDuration != Timeout.InfiniteTimeSpan)
+            {
+                Task.Run(UpdateBucketTaskLoopAsync)
+                    .ContinueWith(t => Log.Error(t.Exception, "Error in "), TaskContinuationOptions.OnlyOnFaulted);
+            }
+        }
+
+        public static MovingAverageKeepRateCalculator CreateDefaultKeepRateCalculator()
+            => new MovingAverageKeepRateCalculator(DefaultKeepRate, DefaultBucketDuration);
+
+        /// <summary>
+        /// Increment the number of kept traces
+        /// </summary>
+        public void IncrementKeeps(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            Interlocked.Add(ref _latestKeeps, count);
+        }
+
+        /// <summary>
+        /// Increment the number of dropped traces
+        /// </summary>
+        public void IncrementDrops(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            Interlocked.Add(ref _latestDrops, count);
+        }
+
+        /// <summary>
+        /// Get the current keep rate for traces
+        /// </summary>
+        public double GetKeepRate()
+        {
+            var totals = Interlocked.Read(ref _totals);
+            UnpackBits(totals, out var totalDropped, out var totalCreated);
+            if (totalCreated == 0)
+            {
+                return 0;
+            }
+
+            return 1 - ((double)totalDropped / totalCreated);
+        }
+
+        /// <summary>
+        /// Stop updating the buckets. The current Keep rate can continue to be read.
+        /// </summary>
+        public void CancelUpdates()
+        {
+            _cts.Cancel();
+        }
+
+        /// <summary>
+        /// Update the current rate. Internal for testing only. Should not be called in normal usage.
+        /// </summary>
+        internal void UpdateBucket()
+        {
+            var index = _index;
+            var previousDropped = _dropped[_index];
+            var previousCreated = _created[_index];
+
+            var latestDropped = Math.Min(Interlocked.Exchange(ref _latestDrops, 0), uint.MaxValue);
+            var latestCreated = Math.Min(Interlocked.Exchange(ref _latestKeeps, 0) + latestDropped, uint.MaxValue);
+
+            UnpackBits(_totals, out var previousTotalDropped, out var previousTotalCreated);
+
+            var newTotalDropped = (uint)Math.Min(((long)previousTotalDropped) - previousDropped + latestDropped, uint.MaxValue);
+            var newTotalCreated = (uint)Math.Min(((long)previousTotalCreated) - previousCreated + latestCreated, uint.MaxValue);
+
+            // Pack the totals into a single value we can read and write atomically
+            var packedTotal = PackBits(newTotalDropped, newTotalCreated);
+            Interlocked.Exchange(ref _totals, packedTotal);
+
+            _dropped[_index] = (uint)latestDropped;
+            _created[_index] = (uint)latestCreated;
+            _index = (index + 1) % _windowSize;
+        }
+
+        private async Task UpdateBucketTaskLoopAsync()
+        {
+            while (true)
+            {
+                if (_cts.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                UpdateBucket();
+
+                await Task.Delay(_bucketDuration, _cts.Token).ConfigureAwait(false);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private long PackBits(uint hits, uint total)
+        {
+            long result = hits;
+            result = result << 32;
+            return result | (long)total;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void UnpackBits(long bits, out uint hits, out uint total)
+        {
+            hits = (uint)(bits >> 32);
+            total = (uint)(bits & 0xffffffffL);
+        }
+    }
+}
---
+++ test/Datadog.Trace.Tests/Agent/MovingAverageKeepRateCalculatorTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Datadog.Trace.Agent;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Agent
+{
+    public class MovingAverageKeepRateCalculatorTests
+    {
+        [Theory]
+        [InlineData(0, 10, 0)]
+        [InlineData(6, 4, 0.6)]
+        [InlineData(9, 1, 0.9)]
+        [InlineData(1, 9, 0.1)]
+        [InlineData(10, 0, 1)]
+        [InlineData(100, 1, 0.99)]
+        public void Calculator_ShouldCalculateKeepRateCorrectly(int keep, int drop, double expected)
+        {
+            const int size = 5;
+
+            var calc = new MovingAverageKeepRateCalculator(size, Timeout.InfiniteTimeSpan);
+
+            calc.IncrementKeeps(keep);
+            calc.IncrementDrops(drop);
+            calc.UpdateBucket();
+
+            var actualRate = calc.GetKeepRate();
+
+            Assert.Equal(expected, actualRate, precision: 2);
+        }
+
+        [Theory]
+        [InlineData(1, 0.1, 0.2, 0.3, 0.4, 0.5)]
+        [InlineData(2, 0.1, 0.15, 0.25, 0.35, 0.45)]
+        [InlineData(3, 0.1, 0.15, 0.2, 0.3, 0.4)]
+        [InlineData(4, 0.1, 0.15, 0.2, 0.25, 0.35)]
+        [InlineData(5, 0.1, 0.15, 0.2, 0.25, 0.3)]
+        public void Calculator_ShouldUpdateRates_BasedOnBucketSize(
+            int size, double rate1, double rate2, double rate3, double rate4, double rate5)
+        {
+            var values = new[]
+            {
+                (keep: 1, drop: 9, expectedRate: rate1),
+                (keep: 2, drop: 8, expectedRate: rate2),
+                (keep: 3, drop: 7, expectedRate: rate3),
+                (keep: 4, drop: 6, expectedRate: rate4),
+                (keep: 5, drop: 5, expectedRate: rate5),
+            };
+
+            var calc = new MovingAverageKeepRateCalculator(size, Timeout.InfiniteTimeSpan);
+
+            foreach (var value in values)
+            {
+                calc.IncrementKeeps(value.keep);
+                calc.IncrementDrops(value.drop);
+
+                calc.UpdateBucket();
+
+                var actualRate = calc.GetKeepRate();
+
+                Assert.Equal(value.expectedRate, actualRate, precision: 2);
+            }
+        }
+
+        [Fact]
+        public async Task Calculator_ShouldAutomaticallyIncrementBuckets()
+        {
+            var bucketSize = 10;
+            const int milliseconds = 50;
+            var timespanIncrements = TimeSpan.FromMilliseconds(milliseconds);
+
+            var calc = new MovingAverageKeepRateCalculator(bucketSize, timespanIncrements);
+
+            // keep rate should be 0 before we add anything
+            Assert.Equal(expected: 0, actual: calc.GetKeepRate());
+
+            calc.IncrementKeeps(10);
+
+            await Task.Delay(milliseconds * 2);
+
+            Assert.Equal(expected: 1, actual: calc.GetKeepRate());
+
+            calc.IncrementDrops(2);
+            calc.IncrementKeeps(8);
+
+            await Task.Delay(milliseconds * 2);
+
+            Assert.Equal(expected: 0.9, calc.GetKeepRate(), precision: 2);
+
+            // the initial "keeps" should drop off the end at some point
+            var hitExpectedRate = false;
+            for (int i = 0; i < bucketSize; i++)
+            {
+                await Task.Delay(milliseconds);
+                var rate = calc.GetKeepRate();
+
+                // Should hit 0.8 at some point
+                if (Math.Abs(rate - 0.8) < 0.01)
+                {
+                    hitExpectedRate = true;
+                }
+            }
+
+            calc.CancelUpdates();
+
+            Assert.True(hitExpectedRate, "Should have seen a hit rate of 0.8");
+
+            var finalRate = calc.GetKeepRate();
+
+            Assert.Equal(expected: 0, finalRate);
+        }
+
+        [Fact]
+        public void Calculator_ShouldHandleOverflows()
+        {
+            const int size = 5;
+
+            var calc = new MovingAverageKeepRateCalculator(size, Timeout.InfiniteTimeSpan);
+
+            calc.IncrementKeeps(int.MaxValue);
+            calc.IncrementKeeps(1);
+            calc.UpdateBucket();
+
+            var actualRate = calc.GetKeepRate();
+
+            // should not be negative!
+            Assert.Equal(expected: 1, actualRate);
+        }
+    }
+}
