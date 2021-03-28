8983a5b213a4d64a22303b6fb6163875367992cb
Zach Montoya <zach.montoya@datadoghq.com>
2021-03-28 16:48:18 -0700

    Restore the publishing of managed profiler assets in the 'docker-compose run Profiler' step to fix the main integration-tests build

--- build/docker/Datadog.Trace.ClrProfiler.Native.sh
+++ build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -5,9 +5,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd "$DIR/../.."
 
+# TODO Remove this from the native build as it should be logically separate
+# This is unnecessary in the unified pipeline as it's already done via the package.sh script
+PUBLISH_OUTPUT_NET2="$( pwd )/src/bin/managed-publish/netstandard2.0"
+PUBLISH_OUTPUT_NET31="$( pwd )/src/bin/managed-publish/netcoreapp3.1"
+
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
 (cd build && cmake ../ -DCMAKE_BUILD_TYPE=Release && make)
 
 mkdir -p bin/Release/x64
 cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Release/x64/Datadog.Trace.ClrProfiler.Native.so
+
+mkdir -p bin/Release/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Release/x64/netstandard2.0/
+
+mkdir -p bin/Release/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Release/x64/netcoreapp3.1/
