9c86ecfc82ec9cc8b758ab9356f03d01f25634f0
macrogreg <macrogreg@live.com>
2021-03-30 00:09:48 -0700

    Add some comments for ExceptionAggregator.

--- src/Shared-Src/Datadog.Util/internal/ExceptionAggregator.cs
+++ src/Shared-Src/Datadog.Util/internal/ExceptionAggregator.cs
@@ -3,6 +3,58 @@ using System.Collections.Generic;
 
 namespace Datadog.Util
 {
+    /// <summary>
+    /// An exception aggregator for zero-or-more exceptions. See suggested usage pattern below.
+    /// * If NO exceptions are added:
+    ///    - No allocations are made.
+    ///    - <c>TryGetAggregatedException(..)</c> returns no exceptions.
+    ///    - <c>ThrowIfNotEmpty(..)</c> does not throw.
+    /// * If ONE exception is added:
+    ///    - One additional allocations are made (for the <c>ExceptionDispatchInfo</c>).
+    ///    - <c>TryGetAggregatedException(..)</c> returns the added exception instance.
+    ///    - <c>ThrowIfNotEmpty(..)</c> re-throws that particular exception preserving all stack traced (uses <c>ExceptionDispatchInfo</c>).
+    /// * If TWO or MORE exceptions are added:
+    ///    - Two additional allocation are made (for a <c>List{Exception}</c> and for an <c>AggregateException</c> that encapsulates that list).
+    ///    - <c>TryGetAggregatedException(..)</c> returns a new <c>AggregateException</c> instance which contains all the added exceptions.
+    ///    - <c>ThrowIfNotEmpty(..)</c> throws a new <c>AggregateException</c> instance which contains all the added exceptions.
+    ///    
+    /// Suggested usage pattern:
+    /// <code>
+    ///     class Example
+    ///     {
+    ///         void DoStuff()
+    ///         {
+    ///         var errors = new ExceptionAggregator();
+    ///     
+    ///         try
+    ///             {
+    ///                 // ...
+    ///             }
+    ///             catch (Exception ex)
+    ///             {
+    ///                 errors.Add(ex);
+    ///             }
+    ///     
+    ///             IEnumerable<object> items = null;
+    ///             foreach (object item in items)
+    ///             {
+    ///                 try
+    ///                 {
+    ///                     // process item
+    ///                 }
+    ///                 catch (Exception ex)
+    ///                 {
+    ///                     errors.Add(ex);
+    ///                 }
+    ///             }
+    ///     
+    ///             errors.ThrowIfNotEmpty();
+    ///         }
+    ///     }
+    /// </code>
+    /// </summary>
+    /// <remarks>This type is a <c>ref struct</c> rather than a simple <c>struct</c> because it is a non-imutable value type.
+    /// Being a <c>ref struct</c> prevents it from being unintentionally boxed (such a boxed copy may be torn-modified).</remarks>
     internal ref struct ExceptionAggregator
     {
         private object _aggregator;
