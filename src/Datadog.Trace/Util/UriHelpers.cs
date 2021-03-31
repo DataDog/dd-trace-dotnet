873c8a00339146f0df52b27ecda1c570d25ec99e
Andrew Lock <andrew.lock@datadoghq.com>
2021-03-31 16:59:05 +0100

    Update Uri cleaning algorithm to be more aggressive
    Replaces any hex-ish (alpha or `-`) string with `?`.
    If it's numbers and dashes only, always replaces
    If it contains alpha, must be at least 16 chars long and contain a number
    Added in back-compat way, that avoids changing internal (public) APIs

--- src/Datadog.Trace/Util/UriHelpers.cs
+++ src/Datadog.Trace/Util/UriHelpers.cs
@@ -7,7 +7,12 @@ namespace Datadog.Trace.Util
     {
         public static string CleanUri(Uri uri, bool removeScheme, bool tryRemoveIds)
         {
-            var path = tryRemoveIds ? GetCleanUriPath(uri.AbsolutePath) : uri.AbsolutePath;
+            return CleanUri(uri, removeScheme, tryRemoveIds, useLegacyIdCleaning: true);
+        }
+
+        public static string CleanUri(Uri uri, bool removeScheme, bool tryRemoveIds, bool useLegacyIdCleaning)
+        {
+            var path = tryRemoveIds ? GetCleanUriPath(uri.AbsolutePath, useLegacyIdCleaning) : uri.AbsolutePath;
 
             if (removeScheme)
             {
@@ -36,12 +41,16 @@ namespace Datadog.Trace.Util
         public static string CleanUriSegment(string absolutePath)
             => GetCleanUriPath(absolutePath);
 
-        public static string GetCleanUriPath(Uri uri)
-        {
-            return GetCleanUriPath(uri.AbsolutePath);
-        }
+        public static string GetCleanUriPath(Uri uri) =>
+            GetCleanUriPath(uri.AbsolutePath, useLegacyIdCleaning: true);
 
-        public static string GetCleanUriPath(string absolutePath)
+        public static string GetCleanUriPath(Uri uri, bool useLegacyIdCleaning) =>
+            GetCleanUriPath(uri.AbsolutePath, useLegacyIdCleaning);
+
+        public static string GetCleanUriPath(string absolutePath) =>
+            GetCleanUriPath(absolutePath, useLegacyIdCleaning: true);
+
+        public static string GetCleanUriPath(string absolutePath, bool useLegacyIdCleaning)
         {
             if (string.IsNullOrWhiteSpace(absolutePath) || (absolutePath.Length == 1 && absolutePath[0] == '/'))
             {
@@ -64,11 +73,13 @@ namespace Datadog.Trace.Util
                 ReadOnlySpan<char> segment = index == -1 ? nStart : nStart.Slice(0, index);
 
                 // replace path segments that look like numbers or guid
-                // GUID format N "d85b1407351d4694939203acc5870eb1" length: 32
-                // GUID format D "d85b1407-351d-4694-9392-03acc5870eb1" length: 36 with dashes in indices 8, 13, 18 and 23.
-                if (long.TryParse(segment, out _) ||
-                    (segment.Length == 32 && IsAGuid(segment, "N")) ||
-                    (segment.Length == 36 && IsAGuid(segment, "D")))
+                var isIdSegment = useLegacyIdCleaning
+                     ? (long.TryParse(segment, out _) ||
+                      (segment.Length == 32 && IsAGuid(segment, "N")) ||
+                      (segment.Length == 36 && IsAGuid(segment, "D")))
+                     : IsIdentifierSegment(segment);
+
+                if (isIdSegment)
                 {
                     sb.Append('?');
                 }
@@ -98,29 +109,55 @@ namespace Datadog.Trace.Util
             {
                 index = absolutePath.IndexOf('/', previousIndex);
 
-                string segment;
-
-                if (index == -1)
+                if (useLegacyIdCleaning)
                 {
-                    // Last segment
-                    segment = absolutePath.Substring(previousIndex);
+                    string segment;
+
+                    if (index == -1)
+                    {
+                        // Last segment
+                        segment = absolutePath.Substring(previousIndex);
+                    }
+                    else
+                    {
+                        segment = absolutePath.Substring(previousIndex, index - previousIndex);
+                    }
+
+                    // replace path segments that look like numbers or guid
+                    // GUID format N "d85b1407351d4694939203acc5870eb1" length: 32
+                    // GUID format D "d85b1407-351d-4694-9392-03acc5870eb1" length: 36 with dashes in indices 8, 13, 18 and 23.
+                    if (long.TryParse(segment, out _) ||
+                        (segment.Length == 32 && IsAGuid(segment, "N")) ||
+                        (segment.Length == 36 && IsAGuid(segment, "D")))
+                    {
+                        segment = "?";
+                    }
+
+                    sb.Append(segment);
                 }
                 else
                 {
-                    segment = absolutePath.Substring(previousIndex, index - previousIndex);
-                }
+                    int segmentLength;
 
-                // replace path segments that look like numbers or guid
-                // GUID format N "d85b1407351d4694939203acc5870eb1" length: 32
-                // GUID format D "d85b1407-351d-4694-9392-03acc5870eb1" length: 36 with dashes in indices 8, 13, 18 and 23.
-                if (long.TryParse(segment, out _) ||
-                    (segment.Length == 32 && IsAGuid(segment, "N")) ||
-                    (segment.Length == 36 && IsAGuid(segment, "D")))
-                {
-                    segment = "?";
-                }
+                    if (index == -1)
+                    {
+                        // Last segment
+                        segmentLength = absolutePath.Length - previousIndex;
+                    }
+                    else
+                    {
+                        segmentLength = index - previousIndex;
+                    }
 
-                sb.Append(segment);
+                    if (IsIdentifierSegment(absolutePath, previousIndex, segmentLength))
+                    {
+                        sb.Append('?');
+                    }
+                    else
+                    {
+                        sb.Append(absolutePath, previousIndex, segmentLength);
+                    }
+                }
 
                 if (index != -1)
                 {
@@ -134,6 +171,84 @@ namespace Datadog.Trace.Util
             return StringBuilderCache.GetStringAndRelease(sb);
 #endif
         }
+
+#if NETCOREAPP
+        internal static bool IsIdentifierSegment(ReadOnlySpan<char> segment)
+        {
+            if (segment.Length == 0)
+            {
+                return false;
+            }
+
+            var containsNumber = false;
+            foreach (var c in segment)
+            {
+                switch (c)
+                {
+                    case >= '0' and <= '9':
+                        containsNumber = true;
+                        continue;
+                    case >= 'a' and <= 'f':
+                    case >= 'A' and <= 'F':
+                        if (segment.Length < 16)
+                        {
+                            // don't be too aggressive replacing
+                            // short hex segments like "/a" or "/cab",
+                            // they are likely not ids
+                            return false;
+                        }
+
+                        continue;
+                    case '-':
+                        continue;
+                    default:
+                        return false;
+                }
+            }
+
+            return containsNumber;
+        }
+#else
+        private static bool IsIdentifierSegment(string absolutePath, int startIndex, int segmentLength)
+        {
+            if (segmentLength == 0)
+            {
+                return false;
+            }
+
+            int lastIndex = startIndex + segmentLength;
+            var containsNumber = false;
+
+            for (int index = startIndex; index < lastIndex && index < absolutePath.Length; index++)
+            {
+                char c = absolutePath[index];
+
+                switch (c)
+                {
+                    case >= '0' and <= '9':
+                        containsNumber = true;
+                        continue;
+                    case >= 'a' and <= 'f':
+                    case >= 'A' and <= 'F':
+                        if (segmentLength < 16)
+                        {
+                            // don't be too aggressive replacing
+                            // short hex segments like "/a" or "/cab",
+                            // they are likely not ids
+                            return false;
+                        }
+
+                        continue;
+                    case '-':
+                        continue;
+                    default:
+                        return false;
+                }
+            }
+
+            return containsNumber;
+        }
+#endif
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static bool IsAGuid(string segment, string format) => Guid.TryParseExact(segment, format, out _);
