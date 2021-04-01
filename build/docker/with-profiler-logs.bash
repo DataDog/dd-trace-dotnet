e7b8d2a6ed00e526535f656e2b5db00c4c58daf2
Andrew Lock <andrew.lock@datadoghq.com>
2021-04-01 09:41:04 +0100

    Try outputting log files after execution, as can't easily tail them

--- build/docker/with-profiler-logs.bash
+++ build/docker/with-profiler-logs.bash
@@ -2,10 +2,11 @@
 set -euxo pipefail
 
 mkdir -p /var/log/datadog/dotnet
-touch /var/log/datadog/dotnet/dotnet-tracer-native.log
-tail -f /var/log/datadog/dotnet/dotnet-tracer-native.log | awk '
-  /info/ {print "\033[32m" $0 "\033[39m"}
-  /warn/ {print "\033[31m" $0 "\033[39m"}
-' &
 
 eval "$@"
+
+cat /var/log/datadog/dotnet/dotnet-tracer-native* \
+| awk '
+  /info/ {print "\033[32m" $0 "\033[39m"}
+  /warn/ {print "\033[31m" $0 "\033[39m"}
+'
