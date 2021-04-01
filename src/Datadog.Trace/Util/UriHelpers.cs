a3c8bd6378e67c7b84057477f41467a533469e84
Andrew Lock <andrew.lock@datadoghq.com>
2021-04-01 14:08:38 +0100

    Simplify implementation

--- src/Datadog.Trace/Util/UriHelpers.cs
+++ src/Datadog.Trace/Util/UriHelpers.cs
@@ -57,27 +57,57 @@ namespace Datadog.Trace.Util
                 return absolutePath;
             }
 
-#if NETCOREAPP
-            ReadOnlySpan<char> absPath = absolutePath.AsSpan();
-
             // Sanitized url will be at worse as long as the original
-            var sb = StringBuilderCache.Acquire(absPath.Length);
+            var sb = StringBuilderCache.Acquire(absolutePath.Length);
+
+#if NETCOREAPP
+            var absolutePathSpan = absolutePath.AsSpan();
+#endif
 
             int previousIndex = 0;
             int index;
+            int segmentLength;
 
             do
             {
-                ReadOnlySpan<char> nStart = absPath.Slice(previousIndex);
-                index = nStart.IndexOf('/');
-                ReadOnlySpan<char> segment = index == -1 ? nStart : nStart.Slice(0, index);
+                index = absolutePath.IndexOf('/', previousIndex);
 
-                // replace path segments that look like numbers or guid
-                var isIdSegment = useLegacyIdCleaning
-                     ? (long.TryParse(segment, out _) ||
-                      (segment.Length == 32 && IsAGuid(segment, "N")) ||
-                      (segment.Length == 36 && IsAGuid(segment, "D")))
-                     : IsIdentifierSegment(segment);
+                if (index == -1)
+                {
+                    // Last segment
+                    segmentLength = absolutePath.Length - previousIndex;
+                }
+                else
+                {
+                    segmentLength = index - previousIndex;
+                }
+
+                bool isIdSegment;
+
+                if (useLegacyIdCleaning)
+                {
+#if NETCOREAPP
+                    ReadOnlySpan<char> segment =
+                        index == -1
+                            ? absolutePathSpan.Slice(previousIndex)
+                            : absolutePathSpan.Slice(previousIndex, segmentLength);
+
+                    isIdSegment = long.TryParse(segment, out _) ||
+                                  (segment.Length == 32 && IsAGuid(segment, "N")) ||
+                                  (segment.Length == 36 && IsAGuid(segment, "D"));
+#else
+                    var segment = index == -1
+                                      ? absolutePath.Substring(previousIndex)
+                                      : absolutePath.Substring(previousIndex, index - previousIndex);
+                    isIdSegment = long.TryParse(segment, out _) ||
+                                     (segment.Length == 32 && IsAGuid(segment, "N")) ||
+                                     (segment.Length == 36 && IsAGuid(segment, "D"));
+#endif
+                }
+                else
+                {
+                    isIdSegment = IsIdentifierSegment(absolutePath, previousIndex, segmentLength);
+                }
 
                 if (isIdSegment)
                 {
@@ -85,7 +115,7 @@ namespace Datadog.Trace.Util
                 }
                 else
                 {
-                    sb.Append(segment);
+                    sb.Append(absolutePath, previousIndex, segmentLength);
                 }
 
                 if (index != -1)
@@ -93,122 +123,13 @@ namespace Datadog.Trace.Util
                     sb.Append('/');
                 }
 
-                previousIndex += index + 1;
-            }
-            while (index != -1);
-
-            return StringBuilderCache.GetStringAndRelease(sb);
-#else
-            // Sanitized url will be at worse as long as the original
-            var sb = StringBuilderCache.Acquire(absolutePath.Length);
-
-            int previousIndex = 0;
-            int index = 0;
-
-            do
-            {
-                index = absolutePath.IndexOf('/', previousIndex);
-
-                if (useLegacyIdCleaning)
-                {
-                    string segment;
-
-                    if (index == -1)
-                    {
-                        // Last segment
-                        segment = absolutePath.Substring(previousIndex);
-                    }
-                    else
-                    {
-                        segment = absolutePath.Substring(previousIndex, index - previousIndex);
-                    }
-
-                    // replace path segments that look like numbers or guid
-                    // GUID format N "d85b1407351d4694939203acc5870eb1" length: 32
-                    // GUID format D "d85b1407-351d-4694-9392-03acc5870eb1" length: 36 with dashes in indices 8, 13, 18 and 23.
-                    if (long.TryParse(segment, out _) ||
-                        (segment.Length == 32 && IsAGuid(segment, "N")) ||
-                        (segment.Length == 36 && IsAGuid(segment, "D")))
-                    {
-                        segment = "?";
-                    }
-
-                    sb.Append(segment);
-                }
-                else
-                {
-                    int segmentLength;
-
-                    if (index == -1)
-                    {
-                        // Last segment
-                        segmentLength = absolutePath.Length - previousIndex;
-                    }
-                    else
-                    {
-                        segmentLength = index - previousIndex;
-                    }
-
-                    if (IsIdentifierSegment(absolutePath, previousIndex, segmentLength))
-                    {
-                        sb.Append('?');
-                    }
-                    else
-                    {
-                        sb.Append(absolutePath, previousIndex, segmentLength);
-                    }
-                }
-
-                if (index != -1)
-                {
-                    sb.Append("/");
-                }
-
                 previousIndex = index + 1;
             }
             while (index != -1);
 
             return StringBuilderCache.GetStringAndRelease(sb);
-#endif
         }
 
-#if NETCOREAPP
-        internal static bool IsIdentifierSegment(ReadOnlySpan<char> segment)
-        {
-            if (segment.Length == 0)
-            {
-                return false;
-            }
-
-            var containsNumber = false;
-            foreach (var c in segment)
-            {
-                switch (c)
-                {
-                    case >= '0' and <= '9':
-                        containsNumber = true;
-                        continue;
-                    case >= 'a' and <= 'f':
-                    case >= 'A' and <= 'F':
-                        if (segment.Length < 16)
-                        {
-                            // don't be too aggressive replacing
-                            // short hex segments like "/a" or "/cab",
-                            // they are likely not ids
-                            return false;
-                        }
-
-                        continue;
-                    case '-':
-                        continue;
-                    default:
-                        return false;
-                }
-            }
-
-            return containsNumber;
-        }
-#else
         private static bool IsIdentifierSegment(string absolutePath, int startIndex, int segmentLength)
         {
             if (segmentLength == 0)
@@ -248,7 +169,6 @@ namespace Datadog.Trace.Util
 
             return containsNumber;
         }
-#endif
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static bool IsAGuid(string segment, string format) => Guid.TryParseExact(segment, format, out _);
