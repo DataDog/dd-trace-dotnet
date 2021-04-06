2e3d3ac57ea92e7a97f9f2d78f8c31f2c7a7d666
Andrew Lock <andrew.lock@datadoghq.com>
2021-04-06 10:09:15 +0100

    Reenable -e
    For simplicity, reinstate fail on first error, and use trap to copy logs and crashdumps

--- build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.arm64.sh
+++ build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.arm64.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-set -uxo pipefail
+set -euxo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../../
 
 mkdir -p /var/log/datadog/dotnet
-touch /var/log/datadog/dotnet/dotnet-tracer-native.log
 
 #https://docs.microsoft.com/en-us/dotnet/core/diagnostics/dumps#collecting-dumps-on-crash
 export COMPlus_DbgEnableMiniDump=1
 export COMPlus_DbgMiniDumpType=4
 
+cleanup() {
+
+    # Collect run data
+    mkdir /project/build_data
+
+    cp /var/log/datadog/dotnet/* /project/build_data/
+    cp /tmp/coredump* /project/build_data/ 2>/dev/null || :
+}
+
+trap cleanup SIGINT SIGTERM EXIT
+
 dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
-st01=$?
 
 dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
-st02=$?
 
 wait-for-it servicestackredis:6379 -- \
 wait-for-it stackexchangeredis:6379 -- \
@@ -23,14 +31,3 @@ wait-for-it sqledge:1433 -- \
 wait-for-it mongo:27017 -- \
 wait-for-it postgres:5432 -- \
 dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results --TestCaseFilter:Category!=ArmUnsupported
-st03=$?
-
-# Collect run data
-mkdir /project/build_data
-
-cp /var/log/datadog/dotnet/* /project/build_data/
-cp /tmp/coredump* /project/build_data/ 2>/dev/null || :
-
-if [ $st01 -eq 1 ] || [ $st02 -eq 1 ] || [ $st03 -eq 1 ]; then
-    exit 1
-fi
--- build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -uxo pipefail
+set -euxo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../../
 
@@ -9,11 +9,20 @@ mkdir -p /var/log/datadog/dotnet
 export COMPlus_DbgEnableMiniDump=1
 export COMPlus_DbgMiniDumpType=4
 
+cleanup() {
+
+    # Collect run data
+    mkdir /project/build_data
+
+    cp /var/log/datadog/dotnet/* /project/build_data/
+    cp /tmp/coredump* /project/build_data/ 2>/dev/null || :
+}
+
+trap cleanup SIGINT SIGTERM EXIT
+
 dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
-st01=$?
 
 dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
-st02=$?
 
 wait-for-it servicestackredis:6379 -- \
 wait-for-it stackexchangeredis:6379 -- \
@@ -23,14 +32,3 @@ wait-for-it sqlserver:1433 -- \
 wait-for-it mongo:27017 -- \
 wait-for-it postgres:5432 -- \
 dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results
-st03=$?
-
-# Collect run data
-mkdir /project/build_data
-
-cp /var/log/datadog/dotnet/* /project/build_data/
-cp /tmp/coredump* /project/build_data/ 2>/dev/null || :
-
-if [ $st01 -eq 1 ] || [ $st02 -eq 1 ] || [ $st03 -eq 1 ]; then
-    exit 1
-fi
