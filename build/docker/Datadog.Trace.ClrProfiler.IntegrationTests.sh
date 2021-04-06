3e6ee7de4c6d7c06960bab31f4f5b45abb897ac0
Andrew Lock <andrew.lock@datadoghq.com>
2021-04-06 10:30:03 +0100

    Also publish coredumps

--- build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-set -euxo pipefail
+set -uxo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../../
 
 mkdir -p /var/log/datadog/dotnet
 
+#https://docs.microsoft.com/en-us/dotnet/core/diagnostics/dumps#collecting-dumps-on-crash
+export COMPlus_DbgEnableMiniDump=1
+export COMPlus_DbgMiniDumpType=4
+
 dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
+st01=$?
 
 dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
+st02=$?
 
 wait-for-it servicestackredis:6379 -- \
 wait-for-it stackexchangeredis:6379 -- \
@@ -17,6 +23,14 @@ wait-for-it sqlserver:1433 -- \
 wait-for-it mongo:27017 -- \
 wait-for-it postgres:5432 -- \
 dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results
+st03=$?
 
-mkdir /project/logs
-cp /var/log/datadog/dotnet/dotnet-tracer-native*.log /project/logs
+# Collect run data
+mkdir /project/build_data
+
+cp /var/log/datadog/dotnet/* /project/build_data/
+cp /tmp/coredump* /project/build_data/ 2>/dev/null || :
+
+if [ $st01 -eq 1 ] || [ $st02 -eq 1 ] || [ $st03 -eq 1 ]; then
+    exit 1
+fi
--- build/docker/with-profiler-logs.bash
+++ build/docker/with-profiler-logs.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -euxo pipefail
+set -uxo pipefail
 
 mkdir -p /var/log/datadog/dotnet
 
 eval "$@"
+exitVal=$?
 
 cat /var/log/datadog/dotnet/dotnet-tracer-native* \
 | awk '
   /info/ {print "\033[32m" $0 "\033[39m"}
   /warn/ {print "\033[31m" $0 "\033[39m"}
 '
+exit $exitVal
