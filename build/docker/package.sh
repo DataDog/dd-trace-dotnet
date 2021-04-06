3485343c6466124b73cc594f5b9c021d09d33a30
Andrew Lock <andrew.lock@datadoghq.com>
2021-04-06 09:36:07 +0100

    Revert "Revert "Add a consolidated multi-stage pipeline (#1320)" (#1335)"
    This reverts commit 45171ff893fce8c3488b40dc168d1d34e37d2ba6.

--- build/docker/package.sh
+++ build/docker/package.sh
@@ -4,11 +4,18 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 VERSION=1.25.2
 
-mkdir -p $DIR/../../deploy/linux
-cp $DIR/../../integrations.json $DIR/../../src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/
-cp $DIR/../../build/artifacts/createLogPath.sh $DIR/../../src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/
+cd "$DIR/../.."
+mkdir -p deploy/linux
+cp integrations.json src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/
+cp build/artifacts/createLogPath.sh src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/
 
-cd $DIR/../../deploy/linux
+mkdir -p src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/netstandard2.0
+cp src/bin/windows-tracer-home/netstandard2.0/*.dll src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/netstandard2.0/
+
+mkdir -p src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/netcoreapp3.1
+cp src/bin/windows-tracer-home/netcoreapp3.1/*.dll src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/netcoreapp3.1/
+
+cd deploy/linux
 for pkgtype in $PKGTYPES ; do
     fpm \
         -f \
