58dd1b02dfb8e90923a30513b1fbc6b9cee9b0d2
Zach Montoya <zach.montoya@datadoghq.com>
2021-04-07 09:03:04 -0700

    OTel Port: Fix double parsing
    Source: https://github.com/open-telemetry/opentelemetry-dotnet-instrumentation/pull/96 (#1349)

--- src/Datadog.Trace/Configuration/StringConfigurationSource.cs
+++ src/Datadog.Trace/Configuration/StringConfigurationSource.cs
@@ -1,5 +1,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Globalization;
 using Datadog.Trace.ExtensionMethods;
 
 namespace Datadog.Trace.Configuration
@@ -89,7 +90,7 @@ namespace Datadog.Trace.Configuration
         {
             string value = GetString(key);
 
-            return double.TryParse(value, out double result)
+            return double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out double result)
                        ? result
                        : (double?)null;
         }
