db0520da2b2e86ff89885c905f2e715589279fa8
Tony Redondo <tony.redondo@datadoghq.com>
2021-04-26 18:07:29 +0200

    Remove mutexes in favor to an atomic_ullong

--- src/Datadog.Trace.ClrProfiler.Native/stats.h
+++ src/Datadog.Trace.ClrProfiler.Native/stats.h
@@ -8,22 +8,17 @@
 namespace trace {
 
 class SWStat {
-  std::chrono::nanoseconds* _value;
-  std::mutex* _mutex;
+  std::atomic_ullong* _value;
   std::chrono::steady_clock::time_point _startTime;
 
  public:
-  SWStat(std::chrono::nanoseconds* value, std::mutex* mutex) {
+  SWStat(std::atomic_ullong* value) {
     _value = value;
-    _mutex = mutex;
     _startTime = std::chrono::steady_clock::now();
   }
   ~SWStat() {
-    auto increment = std::chrono::steady_clock::now() - _startTime;
-    {
-      std::lock_guard<std::mutex> guard(*_mutex);
-      *_value += increment;
-    }
+    auto increment = (std::chrono::steady_clock::now() - _startTime).count();
+    _value->fetch_add(increment);
   }
 };
 
@@ -31,14 +26,14 @@ class Stats : public Singleton<Stats> {
   friend class Singleton<Stats>;
 
  private:
-  std::chrono::nanoseconds callTargetRequestRejit;
-  std::chrono::nanoseconds callTargetRewriter;
-  std::chrono::nanoseconds jitInlining;
-  std::chrono::nanoseconds jitCompilationStarted;
-  std::chrono::nanoseconds moduleUnloadStarted;
-  std::chrono::nanoseconds moduleLoadFinished;
-  std::chrono::nanoseconds assemblyLoadFinished;
-  std::chrono::nanoseconds initialize;
+  std::atomic_ullong callTargetRequestRejit = {0};
+  std::atomic_ullong callTargetRewriter = {0};
+  std::atomic_ullong jitInlining = {0};
+  std::atomic_ullong jitCompilationStarted = {0};
+  std::atomic_ullong moduleUnloadStarted = {0};
+  std::atomic_ullong moduleLoadFinished = {0};
+  std::atomic_ullong assemblyLoadFinished = {0};
+  std::atomic_ullong initialize = {0};
 
   //
   std::atomic_uint callTargetRequestRejitCount = {0};
@@ -49,25 +44,15 @@ class Stats : public Singleton<Stats> {
   std::atomic_uint moduleLoadFinishedCount = {0};
   std::atomic_uint assemblyLoadFinishedCount = {0};
 
-  //
-  std::mutex callTargetRequestRejitMutex;
-  std::mutex callTargetRewriterMutex;
-  std::mutex jitInliningMutex;
-  std::mutex jitCompilationStartedMutex;
-  std::mutex moduleUnloadStartedMutex;
-  std::mutex moduleLoadFinishedMutex;
-  std::mutex assemblyLoadFinishedMutex;
-  std::mutex initializeMutex;
-
  public:
   Stats() {
-    callTargetRequestRejit = std::chrono::nanoseconds(0);
-    jitInlining = std::chrono::nanoseconds(0);
-    jitCompilationStarted = std::chrono::nanoseconds(0);
-    moduleUnloadStarted = std::chrono::nanoseconds(0);
-    moduleLoadFinished = std::chrono::nanoseconds(0);
-    assemblyLoadFinished = std::chrono::nanoseconds(0);
-    initialize = std::chrono::nanoseconds(0);
+    callTargetRequestRejit = 0;
+    jitInlining = 0;
+    jitCompilationStarted = 0;
+    moduleUnloadStarted = 0;
+    moduleLoadFinished = 0;
+    assemblyLoadFinished = 0;
+    initialize = 0;
 
     callTargetRequestRejitCount = 0;
     jitInliningCount = 0;
@@ -78,61 +63,60 @@ class Stats : public Singleton<Stats> {
   }
   SWStat CallTargetRequestRejitMeasure() {
     callTargetRequestRejitCount++;
-
-    return SWStat(&callTargetRequestRejit, &callTargetRequestRejitMutex);
+    return SWStat(&callTargetRequestRejit);
   }
   SWStat CallTargetRewriterCallbackMeasure() {
     callTargetRewriterCount++;
-    return SWStat(&callTargetRewriter, &callTargetRewriterMutex);
+    return SWStat(&callTargetRewriter);
   }
   SWStat JITInliningMeasure() {
     jitInliningCount++;
-    return SWStat(&jitInlining, &jitInliningMutex);
+    return SWStat(&jitInlining);
   }
   SWStat JITCompilationStartedMeasure() {
     jitCompilationStartedCount++;
-    return SWStat(&jitCompilationStarted, &jitCompilationStartedMutex);
+    return SWStat(&jitCompilationStarted);
   }
   SWStat ModuleUnloadStartedMeasure() {
     moduleUnloadStartedCount++;
-    return SWStat(&moduleUnloadStarted, &moduleUnloadStartedMutex);
+    return SWStat(&moduleUnloadStarted);
   }
   SWStat ModuleLoadFinishedMeasure() {
     moduleLoadFinishedCount++;
-    return SWStat(&moduleLoadFinished, &moduleLoadFinishedMutex);
+    return SWStat(&moduleLoadFinished);
   }
   SWStat AssemblyLoadFinishedMeasure() {
     assemblyLoadFinishedCount++;
-    return SWStat(&assemblyLoadFinished, &assemblyLoadFinishedMutex);
+    return SWStat(&assemblyLoadFinished);
   }
   SWStat InitializeMeasure() {
-      return SWStat(&initialize, &initializeMutex);
+      return SWStat(&initialize);
   }
   std::string ToString() {
     std::stringstream ss;
     ss << "[Initialize=";
-    ss << initialize.count() / 1000000 << "ms";
+    ss << initialize.load() / 1000000 << "ms";
     ss << ", ModuleLoadFinished=";
-    ss << moduleLoadFinished.count() / 1000000 << "ms"
-       << "/" << moduleLoadFinishedCount;
+    ss << moduleLoadFinished.load() / 1000000 << "ms"
+       << "/" << moduleLoadFinishedCount.load();
     ss << ", CallTargetRequestRejit=";
-    ss << callTargetRequestRejit.count() / 1000000 << "ms"
-       << "/" << callTargetRequestRejitCount;
+    ss << callTargetRequestRejit.load() / 1000000 << "ms"
+       << "/" << callTargetRequestRejitCount.load();
     ss << ", CallTargetRewriter=";
-    ss << callTargetRewriter.count() / 1000000 << "ms"
-       << "/" << callTargetRewriterCount;
+    ss << callTargetRewriter.load() / 1000000 << "ms"
+       << "/" << callTargetRewriterCount.load();
     ss << ", AssemblyLoadFinished=";
-    ss << assemblyLoadFinished.count() / 1000000 << "ms"
-       << "/" << assemblyLoadFinishedCount;
+    ss << assemblyLoadFinished.load() / 1000000 << "ms"
+       << "/" << assemblyLoadFinishedCount.load();
     ss << ", ModuleUnloadStarted=";
-    ss << moduleUnloadStarted.count() / 1000000 << "ms"
-       << "/" << moduleUnloadStartedCount;
+    ss << moduleUnloadStarted.load() / 1000000 << "ms"
+       << "/" << moduleUnloadStartedCount.load();
     ss << ", JitCompilationStarted=";
-    ss << jitCompilationStarted.count() / 1000000 << "ms"
-       << "/" << jitCompilationStartedCount;
+    ss << jitCompilationStarted.load() / 1000000 << "ms"
+       << "/" << jitCompilationStartedCount.load();
     ss << ", JitInlining=";
-    ss << jitInlining.count() / 1000000 << "ms"
-       << "/" << jitInliningCount;
+    ss << jitInlining.load() / 1000000 << "ms"
+       << "/" << jitInliningCount.load();
     ss << "]";
     return ss.str();
   }
