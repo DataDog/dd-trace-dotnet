edb215413b1c9d35b352462459f7be766d23339e
anna-git <anna@yafi.me>
2021-05-05 16:23:16 +0200

    3 integrations methods ready

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/Msmq/MessageQueue_ReceiveCurrent_Integration.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/Msmq/MessageQueue_ReceiveCurrent_Integration.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.DuckTyping;
 using Datadog.Trace.Logging;
 using Datadog.Trace.Tagging;
 
@@ -24,17 +25,18 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Msmq
     IntegrationName = IntegrationName)]
     public class MessageQueue_ReceiveCurrent_Integration
     {
-        private const string Command = "msmq.receive";
+        private const string CommandPeek = "msmq.peek";
+        private const string CommandConsume = "msmq.consume";
         private const string IntegrationName = nameof(IntegrationIds.Msmq);
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(MessageQueue_ReceiveCurrent_Integration));
 
         /// <summary>
         /// OnMethodBegin callback
         /// </summary>
-        /// <typeparam name="TMessageQueue">Generic tmessage queue</typeparam>
+        /// <typeparam name="TMessageQueue">Generic TMessageQueue</typeparam>
         /// <param name="instance">Instance value, aka `this` of the instrumented method, the message queue</param>
-        /// <param name="timeout">   A System.TimeSpan that indicates the time to wait until a new message is available for inspection.</param>
-        /// <param name="action">args</param>
+        /// <param name="timeout">A System.TimeSpan that indicates the time to wait until a new message is available for inspection.</param>
+        /// <param name="action">action type</param>
         /// <param name="cursorHandle">A System.Messaging.Cursor that maintains a specific position in the message queue.</param>
         /// <param name="messagePropertyFilter"> Controls and selects the properties that are retrieved when peeking or receiving messages from a message queue.</param>2
         /// <param name="messageQueueTransaction">transaction</param>
@@ -43,24 +45,25 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Msmq
         public static CallTargetState OnMethodBegin<TMessageQueue>(TMessageQueue instance, TimeSpan timeout, int action, object cursorHandle, object messagePropertyFilter, object messageQueueTransaction, object messageQueueTransactionType)
             where TMessageQueue : IMessageQueue
         {
-            Log.Information("receive on method begin");
-            // var scope = MsmqCommon.CreateScope(Tracer.Instance, Command, SpanKinds.Producer, instance.QueueName, instance.FormatName, instance.Label, instance.LastModifyTime, messageQueueTransaction != null, messageQueueTransactionType.ToString(), instance.Transactional, out MsmqTags tags);
-            return CallTargetState.GetDefault();
+            var scope = MsmqCommon.CreateScope(Tracer.Instance, action != 0 ? CommandPeek : CommandConsume, SpanKinds.Producer, instance.QueueName, instance.FormatName, instance.Label, instance.LastModifyTime, messageQueueTransaction != null, string.Empty, instance.Transactional, out MsmqTags tags);
+            return new CallTargetState(scope);
         }
 
         /// <summary>
         /// OnMethodEnd callback
         /// </summary>
         /// <typeparam name="TTarget">Type of the target</typeparam>
-        /// <typeparam name="TReturn">Type of the return</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
         /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="messageResult">message result</param>
         /// <param name="exception">Exception instance in case the original code threw an exception.</param>
         /// <param name="state">Calltarget state value</param>
         /// <returns>CallTargetReturn</returns>
-        public static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TTarget instance, Exception exception, CallTargetState state)
+        public static CallTargetReturn<TResult> OnMethodEnd<TTarget, TResult>(TTarget instance, TResult messageResult, Exception exception, CallTargetState state)
+            where TResult : IMessage
         {
             state.Scope.DisposeWithException(exception);
-            return CallTargetReturn<TReturn>.GetDefault();
+            return new CallTargetReturn<TResult>(messageResult);
         }
     }
 }
