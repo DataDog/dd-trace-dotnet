3b1df15c816098a5242b212daa1343d7578afb5b
colin-higgins <developerspike@gmail.com>
2021-05-05 09:13:37 -0400

    Oopsied an extra loop

--- src/Datadog.Trace.ClrProfiler.Managed/Helpers/HttpBypassHelper.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/Helpers/HttpBypassHelper.cs
@@ -8,15 +8,12 @@ namespace Datadog.Trace.ClrProfiler.Helpers
         {
             var url = requestUri.ToString();
 
-            for (var index = 0; index < patternsToSkip.Length; index++)
+            for (var y = 0; y < patternsToSkip.Length; y++)
             {
-                for (var y = 0; y < patternsToSkip.Length; y++)
+                var found = (url.Length - url.Replace(patternsToSkip[y], string.Empty).Length) / patternsToSkip[y].Length > 0;
+                if (found)
                 {
-                    var found = (url.Length - url.Replace(patternsToSkip[y], string.Empty).Length) / patternsToSkip[y].Length > 0;
-                    if (found)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
 
