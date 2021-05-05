94da7cff59fae12a07c345f09165a0fcf5b17df6
Kevin Gosse <krix33@gmail.com>
2021-05-05 13:04:20 +0200

    Try using test result directory

--- build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -26,12 +26,13 @@ cleanup() {
     mkdir /project/build_data
 
     cp /var/log/datadog/dotnet/* /project/build_data/
+    cp -R /var/log/datadog/cover /project/
     cp /tmp/coredump* /project/build_data/ 2>/dev/null || :
 }
 
 trap cleanup SIGINT SIGTERM EXIT
 
-dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
+dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results 
 
 dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
 
@@ -42,9 +43,7 @@ wait-for-it elasticsearch5:9200 -- \
 wait-for-it sqlserver:1433 -- \
 wait-for-it mongo:27017 -- \
 wait-for-it postgres:5432 -- \
-dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results
-
-cp -R /var/log/datadog/cover /project/
+dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=cobertura DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.IncludeDirectory=$(Build.SourcesDirectory)/test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/Release/netcoreapp3.1/profiler-lib/netcoreapp3.1 DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Exclude=[*]Datadog.Trace.Vendors.*      
 
 ls /var/log/datadog/cover
 ls /project/
