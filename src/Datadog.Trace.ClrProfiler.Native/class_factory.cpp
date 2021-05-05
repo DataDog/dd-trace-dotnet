dfe0cd87147ef30163f2928961e56d15eb254746
Tony Redondo <tony.redondo@datadoghq.com>
2021-05-05 16:21:44 +0200

    Fix for linux

--- src/Datadog.Trace.ClrProfiler.Native/class_factory.cpp
+++ src/Datadog.Trace.ClrProfiler.Native/class_factory.cpp
@@ -9,7 +9,7 @@
 
 std::string ConvertToString(const IID guid) {
   char szGuid[40] = {0};
-  sprintf_s(szGuid, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+  sprintf_s(szGuid, 40, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
           guid.Data1, guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1],
           guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5],
           guid.Data4[6], guid.Data4[7]);
