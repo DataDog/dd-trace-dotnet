3141ff39243df22fa7a99c9daaad7310c5b472a0
anna-git <anna,yafi@datadoghq.com>
2021-05-18 17:42:11 +0200

    Security sln
    Fixing props
    
    Fix double main error
    
    Fixing paths
    
    Integration tests project created
    
    Integrations test on blocked request for asp.net core

---
+++ test/test-applications/security/Samples.AspNetCore5/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Samples.AspNetCore5.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace Samples.AspNetCore5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var instrumentationType = Type.GetType("Datadog.Trace.ClrProfiler.Instrumentation, Datadog.Trace.ClrProfiler.Managed");
+            ViewBag.ProfilerAttached = instrumentationType?.GetProperty("ProfilerAttached", BindingFlags.Public | BindingFlags.Static)?.GetValue(null) ?? false;
+            ViewBag.TracerAssemblyLocation = Type.GetType("Datadog.Trace.Tracer, Datadog.Trace")?.Assembly.Location;
+            ViewBag.ClrProfilerAssemblyLocation = instrumentationType?.Assembly.Location;
+
+            var prefixes = new[] { "COR_", "CORECLR_", "DD_", "DATADOG_", "PW" };
+
+            var envVars = from envVar in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
+                          from prefix in prefixes
+                          let key = (envVar.Key as string)?.ToUpperInvariant()
+                          let value = envVar.Value as string
+                          where key.StartsWith(prefix)
+                          orderby key
+                          select new KeyValuePair<string, string>(key, value);
+
+            return View(envVars.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
