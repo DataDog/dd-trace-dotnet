2f6aacd6990b737df4fa5ccd2b049518d9266dbb
Andrew Lock <andrew.lock@datadoghq.com>
2021-05-25 09:21:38 +0100

    Add copyright headers

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/ModuleInitializerAttribute.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/ModuleInitializerAttribute.cs
@@ -1,4 +1,9 @@
-﻿#if !NET5_0
+﻿// <copyright file="ModuleInitializerAttribute.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if !NET5_0
 // ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
