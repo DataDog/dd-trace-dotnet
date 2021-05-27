222621dfff3b2be580e8d3243332acb65acb2727
Kevin Gosse <krix33@gmail.com>
2021-05-27 16:58:50 +0200

    Add log injection benchmarks

---
+++ benchmarks/Benchmarks.Trace/SerilogBenchmark.cs
@@ -0,0 +1,95 @@
+using BenchmarkDotNet.Attributes;
+using Datadog.Trace;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Logging;
+using Datadog.Trace.Logging.LogProviders;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Logger = Serilog.Core.Logger;
+
+namespace Benchmarks.Trace
+{
+    [MemoryDiagnoser]
+    public class SerilogBenchmark
+    {
+        private static readonly Logger Logger;
+        private static readonly Tracer LogInjectionTracer;
+        private static readonly Tracer BaselineTracer;
+
+        static SerilogBenchmark()
+        {
+            LogProvider.SetCurrentLogProvider(new SerilogLogProvider());
+
+            var logInjectionSettings = new TracerSettings
+            {
+                LogsInjectionEnabled = true,
+                Environment = "env",
+                ServiceVersion = "version"
+            };
+
+            LogInjectionTracer = new Tracer(logInjectionSettings, new DummyAgentWriter(), null, null, null);
+            Tracer.Instance = LogInjectionTracer;
+
+            var baselineSettings = new TracerSettings
+            {
+                LogsInjectionEnabled = false,
+                Environment = "env",
+                ServiceVersion = "version"
+            };
+
+            BaselineTracer = new Tracer(baselineSettings, new DummyAgentWriter(), null, null, null);
+            var formatter = new MessageTemplateTextFormatter("{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}{Properties}{NewLine}", null);
+
+            Logger = new LoggerConfiguration()
+                // Add Enrich.FromLogContext to emit Datadog properties
+                .Enrich.FromLogContext()
+                .WriteTo.Sink(new NullSink(formatter))
+                .CreateLogger();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Log()
+        {
+            using (BaselineTracer.StartActive("Test"))
+            {
+                using (BaselineTracer.StartActive("Child"))
+                {
+                    Logger.Information("Hello");
+                }
+            }
+        }
+
+        [Benchmark]
+        public void EnrichedLog()
+        {
+            using (LogInjectionTracer.StartActive("Test"))
+            {
+                using (LogInjectionTracer.StartActive("Child"))
+                {
+                    Logger.Information("Hello");
+                }
+            }
+        }
+
+        private class NullSink : ILogEventSink
+        {
+            private readonly MessageTemplateTextFormatter _formatter;
+
+            public NullSink(MessageTemplateTextFormatter formatter)
+            {
+                _formatter = formatter;
+            }
+
+            public void Emit(LogEvent logEvent)
+            {
+#if DEBUG
+                _formatter.Format(logEvent, System.Console.Out);
+#else
+                _formatter.Format(logEvent, System.IO.TextWriter.Null);
+#endif
+            }
+        }
+    }
+}
