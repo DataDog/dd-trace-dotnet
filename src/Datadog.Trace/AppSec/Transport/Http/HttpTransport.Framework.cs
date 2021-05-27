571efeb9d4aebcb0d6147a4f32ab90f0317055c3
anna-git <anna,yafi@datadoghq.com>
2021-05-27 11:39:41 +0200

    Tests for .net framework. Rebase and fixing builds. Adding copyright headers to security files after Rebase

--- src/Datadog.Trace/AppSec/Transport/Http/HttpTransport.Framework.cs
+++ src/Datadog.Trace/AppSec/Transport/Http/HttpTransport.Framework.cs
@@ -1,5 +1,9 @@
-#if NETFRAMEWORK
+// <copyright file="HttpTransport.Framework.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
 
+#if NETFRAMEWORK
 using System.Web;
 
 namespace Datadog.Trace.AppSec.Transport.Http
--- test/Datadog.Trace.Security.IntegrationTests/AspNetCore5.cs
+++ test/Datadog.Trace.Security.IntegrationTests/AspNetCore5.cs
@@ -1,3 +1,8 @@
+// <copyright file="AspNetCore5.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
 #if NET5_0
 using System;
 using System.Net;
@@ -7,7 +12,7 @@ using Xunit.Abstractions;
 
 namespace Datadog.Trace.Security.IntegrationTests
 {
-    public class AspNetCore5 : AspNetCoreBase
+    public class AspNetCore5 : AspNetCoreBase, IDisposable
     {
         public AspNetCore5(ITestOutputHelper outputHelper)
             : base("AspNetCore5", outputHelper)
@@ -28,14 +33,9 @@ namespace Datadog.Trace.Security.IntegrationTests
             Environment.SetEnvironmentVariable("DD_DOTNET_TRACER_HOME", "$(ProjectDir)$(OutputPath)profiler-lib");
             Environment.SetEnvironmentVariable("DD_INTEGRATIONS", "$(ProjectDir)$(OutputPath)profiler-lib\\integrations.json");
             Environment.SetEnvironmentVariable("CORECLR_PROFILER_PATH", "$(ProjectDir)$(OutputPath)profiler-lib\\Datadog.Trace.ClrProfiler.Native.dll");
-            var process = await RunTraceTestOnSelfHosted("/Home");
+            await RunOnSelfHosted();
             var (statusCode, _) = await SubmitRequest("/Home?arg=database()");
             Assert.Equal(expectedStatusCode, statusCode);
-            if (!process.HasExited)
-            {
-                process.Kill();
-                process.Dispose();
-            }
         }
     }
 }
