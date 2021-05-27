3169e46ff138ab89c5915d908cea1a3a9c05e322
anna-git <anna,yafi@datadoghq.com>
2021-05-27 11:39:41 +0200

    Tests for .net framework. Rebase and fixing builds. Adding copyright headers to security files after Rebase

--- src/Datadog.Trace/AppSec/AspNetCore/BlockingMiddleware.cs
+++ src/Datadog.Trace/AppSec/AspNetCore/BlockingMiddleware.cs
@@ -1,4 +1,10 @@
+// <copyright file="BlockingMiddleware.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
 #if !NETFRAMEWORK
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- test/Datadog.Trace.Security.IntegrationTests/AspNetCoreBase.cs
+++ test/Datadog.Trace.Security.IntegrationTests/AspNetCoreBase.cs
@@ -1,3 +1,8 @@
+// <copyright file="AspNetCoreBase.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -12,37 +17,103 @@ using Xunit.Abstractions;
 
 namespace Datadog.Trace.Security.IntegrationTests
 {
-    public class AspNetCoreBase
+    public class AspNetCoreBase : IDisposable
     {
         private readonly HttpClient httpClient;
-        private int aspNetCorePort;
+        private int httpPort;
+        private Process process;
 
-        public AspNetCoreBase(string sampleName, ITestOutputHelper outputHelper)
+        public AspNetCoreBase(string sampleName, ITestOutputHelper outputHelper, string samplesDir = null)
         {
             Output = outputHelper;
             httpClient = new HttpClient();
-            EnvironmentHelper = new EnvironmentHelper(sampleName, typeof(AspNetCoreBase), Output, samplesDirectory: "test/test-applications/security");
+            EnvironmentHelper = new EnvironmentHelper(sampleName, typeof(AspNetCoreBase), Output, samplesDirectory: samplesDir ?? "test/test-applications/security");
         }
 
         public EnvironmentHelper EnvironmentHelper { get; }
 
         protected ITestOutputHelper Output { get; }
 
-        public async Task<Process> RunTraceTestOnSelfHosted(string path)
+        public Task RunOnSelfHosted()
         {
             var agentPort = TcpPortProvider.GetOpenPort();
-            aspNetCorePort = TcpPortProvider.GetOpenPort();
+            httpPort = TcpPortProvider.GetOpenPort();
 
             using var agent = new MockTracerAgent(agentPort);
-            var process = StartSample(agent.Port, arguments: null, string.Empty, aspNetCorePort: aspNetCorePort);
+            return StartSample(agent.Port, arguments: null, aspNetCorePort: httpPort);
+        }
 
+        public Task RunOnIis(string path)
+        {
+            var initialAgentPort = TcpPortProvider.GetOpenPort();
+            var agent = new MockTracerAgent(initialAgentPort);
+            httpPort = TcpPortProvider.GetOpenPort();
+
+            var arguments = $"/clr:v4.0 /path:{EnvironmentHelper.GetSampleProjectDirectory()} /systray:false /port:{httpPort} /trace:verbose";
+            Output.WriteLine($"[webserver] starting {path} {string.Join(" ", arguments)}");
+
+            return StartSample(agent.Port, arguments, httpPort, iisExpress: true);
+        }
+
+        public void Dispose()
+        {
+            if (process != null && !process.HasExited)
+            {
+                process.Kill();
+                process.Dispose();
+            }
+        }
+
+        protected async Task<(HttpStatusCode StatusCode, string ResponseText)> SubmitRequest(string path)
+        {
+            var response = await httpClient.GetAsync($"http://localhost:{this.httpPort}{path}");
+            var responseText = await response.Content.ReadAsStringAsync();
+            Output.WriteLine($"[http] {response.StatusCode} {responseText}");
+            return (response.StatusCode, responseText);
+        }
+
+        private async Task StartSample(int traceAgentPort, string arguments, int? aspNetCorePort = null, string packageVersion = "", int? statsdPort = null, string framework = "", bool iisExpress = false, string path = "/Home")
+        {
+            var sampleAppPath = string.Empty;
+            // get path to sample app that the profiler will attach to
+            const int mstimeout = 5000;
+            var message = "Now listening on:";
+
+            if (iisExpress)
+            {
+                message = "IIS Express is running.";
+                sampleAppPath = EnvironmentHelper.GetSampleExecutionSource();
+            }
+            else
+            {
+                sampleAppPath = EnvironmentHelper.GetSampleApplicationPath(packageVersion, framework);
+            }
+
+            if (!File.Exists(sampleAppPath))
+            {
+                throw new Exception($"application not found: {sampleAppPath}");
+            }
+
+            // get full paths to integration definitions
+            var integrationPaths = Directory.EnumerateFiles(".", "*integrations.json").Select(Path.GetFullPath);
+
+            process = ProfilerHelper.StartProcessWithProfiler(
+                EnvironmentHelper.GetSampleExecutionSource(),
+                sampleAppPath,
+                EnvironmentHelper,
+                integrationPaths,
+                arguments,
+                traceAgentPort: traceAgentPort,
+                statsdPort: statsdPort,
+                aspNetCorePort: aspNetCorePort.GetValueOrDefault(5000));
+
+            // then wait server ready
             var wh = new EventWaitHandle(false, EventResetMode.AutoReset);
-
             process.OutputDataReceived += (sender, args) =>
             {
                 if (args.Data != null)
                 {
-                    if (args.Data.Contains("Now listening on:") || args.Data.Contains("Unable to start Kestrel"))
+                    if (args.Data.Contains(message))
                     {
                         wh.Set();
                     }
@@ -62,17 +133,20 @@ namespace Datadog.Trace.Security.IntegrationTests
 
             process.BeginErrorReadLine();
 
-            wh.WaitOne(5000);
+            wh.WaitOne(mstimeout);
 
             var maxMillisecondsToWait = 15_000;
             var intervalMilliseconds = 500;
             var intervals = maxMillisecondsToWait / intervalMilliseconds;
             var serverReady = false;
+            var responseText = string.Empty;
 
             // wait for server to be ready to receive requests
             while (intervals-- > 0)
             {
-                serverReady = (await SubmitRequest(path)).StatusCode == HttpStatusCode.OK;
+                var response = await SubmitRequest(path);
+                responseText = response.ResponseText;
+                serverReady = response.StatusCode == HttpStatusCode.OK;
 
                 if (serverReady)
                 {
@@ -84,50 +158,9 @@ namespace Datadog.Trace.Security.IntegrationTests
 
             if (!serverReady)
             {
-                throw new Exception("Couldn't verify the application is ready to receive requests.");
+                process.Kill();
+                throw new Exception($"Couldn't verify the application is ready to receive requests: {responseText}");
             }
-
-            var testStart = DateTime.Now;
-            return process;
-        }
-
-        public Process StartSample(int traceAgentPort, string arguments, string packageVersion, int aspNetCorePort, int? statsdPort = null, string framework = "")
-        {
-            // get path to sample app that the profiler will attach to
-            var sampleAppPath = EnvironmentHelper.GetSampleApplicationPath(packageVersion, framework);
-            if (!File.Exists(sampleAppPath))
-            {
-                throw new Exception($"application not found: {sampleAppPath}");
-            }
-
-            // get full paths to integration definitions
-            var integrationPaths = Directory.EnumerateFiles(".", "*integrations.json").Select(Path.GetFullPath);
-
-            return ProfilerHelper.StartProcessWithProfiler(
-                EnvironmentHelper.GetSampleExecutionSource(),
-                sampleAppPath,
-                EnvironmentHelper,
-                integrationPaths,
-                arguments,
-                traceAgentPort: traceAgentPort,
-                statsdPort: statsdPort,
-                aspNetCorePort: aspNetCorePort);
-        }
-
-        protected async Task SubmitRequests(params string[] paths)
-        {
-            foreach (var path in paths)
-            {
-                await SubmitRequest(path);
-            }
-        }
-
-        protected async Task<(HttpStatusCode StatusCode, string ResponseText)> SubmitRequest(string path)
-        {
-            var response = await httpClient.GetAsync($"http://localhost:{this.aspNetCorePort}{path}");
-            var responseText = await response.Content.ReadAsStringAsync();
-            Output.WriteLine($"[http] {response.StatusCode} {responseText}");
-            return (response.StatusCode, responseText);
         }
     }
 }
