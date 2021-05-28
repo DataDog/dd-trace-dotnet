a2e7509de695f6d94d0832271a881d9d84a41d74
Andrew Lock <andrew.lock@datadoghq.com>
2021-05-28 15:26:56 +0100

    Try setting VSToolsPath explicitly

--- build/_build/gitlab.bat
+++ build/_build/gitlab.bat
@@ -2,6 +2,7 @@ if not exist c:\mnt\ goto nomntdir
 @echo PARAMS %*
 
 call "%VSTUDIO_ROOT%\vc\auxiliary\build\vcvars64.bat"
+SET VSToolsPath=%VSTUDIO_ROOT%\MSBuild\Microsoft\Visual Studio\v16.0
 
 cd c:\mnt\
 dotnet run --project build/_build/_build.csproj -- Info Clean BuildTracerHome PackageTracerHome SignPackages --Artifacts "build-out\%CI_JOB_ID%"
