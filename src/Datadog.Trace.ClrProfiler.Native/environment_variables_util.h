01a531b668e88f5f41b6967ca4e86098bb3e3e4f
Kevin Gosse <krix33@gmail.com>
2021-06-02 15:23:13 +0200

    Enable calltarget instrumentation by default on all architectures

--- src/Datadog.Trace.ClrProfiler.Native/environment_variables_util.h
+++ src/Datadog.Trace.ClrProfiler.Native/environment_variables_util.h
@@ -47,14 +47,7 @@ bool EnableInlining(bool defaultValue) {
 }
 
 bool IsCallTargetEnabled() {
-#if defined(ARM64) || defined(ARM)
-  //
-  // If the architecture is ARM64 or ARM, we enable CallTarget instrumentation by default
-  //
   ToBooleanWithDefault(GetEnvironmentValue(environment::calltarget_enabled), true);
-#else
-  ToBooleanWithDefault(GetEnvironmentValue(environment::calltarget_enabled), false);
-#endif
 }
 
 bool IsDebugEnabled() {
