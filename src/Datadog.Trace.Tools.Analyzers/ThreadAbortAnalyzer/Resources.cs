0c2486702cb51e680883da78a805434d44a11a4b
Andrew Lock <andrew.lock@datadoghq.com>
2021-06-02 12:31:09 +0100

    Add missing file headers

--- src/Datadog.Trace.Tools.Analyzers/ThreadAbortAnalyzer/Resources.cs
+++ src/Datadog.Trace.Tools.Analyzers/ThreadAbortAnalyzer/Resources.cs
@@ -1,4 +1,9 @@
-﻿namespace Datadog.Trace.Tools.Analyzers.ThreadAbortAnalyzer
+﻿// <copyright file="Resources.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Tools.Analyzers.ThreadAbortAnalyzer
 {
     internal static class Resources
     {
