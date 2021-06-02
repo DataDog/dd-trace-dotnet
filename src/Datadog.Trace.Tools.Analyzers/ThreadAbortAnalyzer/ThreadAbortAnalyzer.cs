f850e551a87e232fee81b6e2bd9237e25184617d
Andrew Lock <andrew.lock@datadoghq.com>
2021-06-02 12:31:09 +0100

    Add missing file headers

--- src/Datadog.Trace.Tools.Analyzers/ThreadAbortAnalyzer/ThreadAbortAnalyzer.cs
+++ src/Datadog.Trace.Tools.Analyzers/ThreadAbortAnalyzer/ThreadAbortAnalyzer.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Immutable;
+﻿// <copyright file="ThreadAbortAnalyzer.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Immutable;
 using System.Diagnostics;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
