d5a0978d94583299f68434d0b37bd2d40780d507
anna-git <anna,yafi@datadoghq.com>
2021-06-04 17:33:02 +0200

    Merged and adaptations for net fmwk

---
+++ devtools-gac.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+SET fmk=net45
+SET mode=Release
+echo framework is %fmk%
+gacutil -u Datadog.Trace
+gacutil -u Datadog.Trace.ClrProfiler.Managed
+gacutil -u Datadog.Trace.AspNet
+gacutil -i %~dp0\src\Datadog.Trace\bin\%mode%\%fmk%\Datadog.Trace.dll
+gacutil -i %~dp0\src\Datadog.Trace.ClrProfiler.Managed\bin\%mode%\%fmk%\Datadog.Trace.ClrProfiler.Managed.dll
+gacutil -i %~dp0\src\Datadog.Trace.AspNet\bin\%mode%\net45\Datadog.Trace.AspNet.dll
