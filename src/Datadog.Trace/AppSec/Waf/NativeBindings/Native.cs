b25ff06379d4704c77db2955bef2be3dab952414
Robert Pickering <robert@sqreen.io>
2021-06-07 14:18:29 +0200

    Adapt to 32/64 bit WAF

--- src/Datadog.Trace/AppSec/Waf/NativeBindings/Native.cs
+++ src/Datadog.Trace/AppSec/Waf/NativeBindings/Native.cs
@@ -24,7 +24,7 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
         internal static extern void pw_clearRuleH(IntPtr wafHandle);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWRet pw_runH(IntPtr wafHandle, PWArgs64 parameters, ulong timeLeftInUs);
+        internal static extern PWRet pw_runH(IntPtr wafHandle, PWArgs parameters, ulong timeLeftInUs);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
         internal static extern void pw_freeReturn(PWRet output);
@@ -33,41 +33,41 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
         internal static extern IntPtr pw_initAdditiveH(IntPtr powerwafHandle);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWRet pw_runAdditive(IntPtr context, PWArgs64 newArgs, ulong timeLeftInUs);
+        internal static extern PWRet pw_runAdditive(IntPtr context, PWArgs newArgs, ulong timeLeftInUs);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
         internal static extern void pw_clearAdditive(IntPtr context);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_getInvalid();
+        internal static extern PWArgs pw_getInvalid();
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_createStringWithLength(string s, ulong length);
+        internal static extern PWArgs pw_createStringWithLength(string s, ulong length);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_createString(string s);
+        internal static extern PWArgs pw_createString(string s);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_createInt(long value);
+        internal static extern PWArgs pw_createInt(long value);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_createUint(ulong value);
+        internal static extern PWArgs pw_createUint(ulong value);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_createArray();
+        internal static extern PWArgs pw_createArray();
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern PWArgs64 pw_createMap();
+        internal static extern PWArgs pw_createMap();
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern bool pw_addArray(ref PWArgs64 array, PWArgs64 entry);
+        internal static extern bool pw_addArray(ref PWArgs array, PWArgs entry);
 
         // Setting entryNameLength to 0 will result in the entryName length being re-computed with strlen
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern bool pw_addMap(ref PWArgs64 map, string entryName, ulong entryNameLength, PWArgs64 entry);
+        internal static extern bool pw_addMap(ref PWArgs map, string entryName, ulong entryNameLength, PWArgs entry);
 
         [DllImport("Datadog.Trace.ClrProfiler.Native")]
-        internal static extern void pw_freeArg(ref PWArgs64 input);
+        internal static extern void pw_freeArg(ref PWArgs input);
 
 #pragma warning restore SA1300 // Element should begin with upper-case letter
     }
