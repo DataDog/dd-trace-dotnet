b5a3552bb6833d01c29f9a4b2c7c9277a45034b3
Andrew Lock <andrew.lock@datadoghq.com>
2021-06-17 10:03:36 +0100

    Add ASP.NET Core tests for .NET 5

---
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNetCore/AspNetCoreMvc50Tests.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="AspNetCoreMvc50Tests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if NET5_0
+
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests.AspNetCore
+{
+    public class AspNetCoreMvc50Tests : AspNetCoreMvcTestBase
+    {
+        public AspNetCoreMvc50Tests(ITestOutputHelper output)
+            : base("AspNetCoreMvc31", output, serviceVersion: "1.0.0")
+        {
+            // EnableDebugMode();
+        }
+
+        [Fact]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public async Task MeetsAllAspNetCoreMvcExpectations()
+        {
+            // No package versions are relevant because this is built-in
+            var spans = await RunTraceTestOnSelfHosted(string.Empty);
+
+            RunLogInjectionTest(spans);
+        }
+    }
+}
+#endif
