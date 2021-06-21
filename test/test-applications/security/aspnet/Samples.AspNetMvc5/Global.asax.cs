c75faed4e587bae51892ca23d8d982e4594f4448
Anna <anna.yafi@datadoghq.com>
2021-06-21 17:44:14 +0200

    Adding path parameters and disposing additive context in both frameworks (#1550)
    * Paths parameters to the waf for Asp.net Core
    
    * fixing additive context not being disposed
    
    * Additive context scoped  per request
    
    * Getting Path parameters for the framework too
    
    * Running the waf in call target and call site for .net framework
    
    * Little fixes
    
    * PR fixes
    
    * Fixing build
    
    * checking in sqreen.lib
    
    * Little fix, adapting native proj to reference package manager
    
    Passing non encoded query to the waf in .net framework
    
    Merging integrations.json

--- test/test-applications/security/aspnet/Samples.AspNetMvc5/Global.asax.cs
+++ test/test-applications/security/aspnet/Samples.AspNetMvc5/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Samples.AspNetMvc5.Data;
 
 namespace Samples.AspNetMvc5
 {
@@ -16,6 +18,11 @@ namespace Samples.AspNetMvc5
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+      
+            if (bool.TryParse(ConfigurationManager.AppSettings["CreateDb"], out bool res) && res)
+            {
+                DatabaseHelper.CreateAndFeedDatabase(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            }
         }
     }
 }
