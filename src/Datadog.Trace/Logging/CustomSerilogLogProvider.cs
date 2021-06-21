44004721a72195bce28efe9b7fc606a1dd57dbd9
Zach Montoya <zach.montoya@datadoghq.com>
2021-06-21 15:42:34 -0700

    Enhancements to allow the optimized CustomSerilogLogProvider to be used for older versions of Serilog that only support the PushProperties(ILogEventEnricher[]) API. Since the method requires an array, cache an array containing the enricher using a ConditionalWeakTable

--- src/Datadog.Trace/Logging/CustomSerilogLogProvider.cs
+++ src/Datadog.Trace/Logging/CustomSerilogLogProvider.cs
@@ -6,30 +6,47 @@
 using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Datadog.Trace.Logging.LogProviders;
 
 namespace Datadog.Trace.Logging
 {
     internal class CustomSerilogLogProvider : SerilogLogProvider, ILogProviderWithEnricher
     {
+        private static ConditionalWeakTable<object, object> _enricherToArrayConditionalWeakTable;
         private static Func<object, IDisposable> _pushMethod;
+        private static Func<object, IDisposable> _openContext;
 
         public CustomSerilogLogProvider()
         {
-            _pushMethod = GetPush();
+            _enricherToArrayConditionalWeakTable = new();
+
+            var logEnricherType = GetLogEnricherType();
+            if (GetPushMethodInfo() != null)
+            {
+                _openContext = _pushMethod = GeneratePushDelegate(GetPushMethodInfo(), logEnricherType);
+            }
+            else
+            {
+                _pushMethod = GeneratePushDelegate(GetPushPropertiesMethodInfo(), logEnricherType.MakeArrayType());
+                _openContext = (enricher) =>
+                {
+                    // Use a conditional weak table to cache a map between the enricher and an array of size 1 containing the enricher,
+                    // since the API requires an argument of type ILogEventEnricher[]
+                    object properties = _enricherToArrayConditionalWeakTable.GetValue(key: enricher, createValueCallback: ConditionalWeakTableCallback);
+                    return _pushMethod(properties);
+                };
+            }
         }
 
-        public IDisposable OpenContext(object enricher)
-        {
-            return _pushMethod(enricher);
-        }
+        public IDisposable OpenContext(object enricher) => _openContext(enricher);
 
         public ILogEnricher CreateEnricher() => new SerilogEnricher(this);
 
         internal static Type GetLogEnricherType() => Type.GetType("Serilog.Core.ILogEventEnricher, Serilog");
 
         internal static new bool IsLoggerAvailable() =>
-            SerilogLogProvider.IsLoggerAvailable() && GetPushMethodInfo() != null;
+            SerilogLogProvider.IsLoggerAvailable() && (GetPushMethodInfo() != null || GetPushPropertiesMethodInfo() != null);
 
         private static MethodInfo GetPushMethodInfo()
         {
@@ -37,18 +54,31 @@ namespace Datadog.Trace.Logging
             return ndcContextType?.GetMethod("Push", GetLogEnricherType());
         }
 
-        private static Func<object, IDisposable> GetPush()
+        private static MethodInfo GetPushPropertiesMethodInfo()
         {
-            var pushPropertyMethod = GetPushMethodInfo();
+            var ndcContextType = FindType("Serilog.Context.LogContext", new[] { "Serilog", "Serilog.FullNetFx" });
+            return ndcContextType?.GetMethod("PushProperties", GetLogEnricherType().MakeArrayType());
+        }
+
+        private static Func<object, IDisposable> GeneratePushDelegate(MethodInfo methodInfo, Type argumentTargetType)
+        {
             var enricherParam = Expression.Parameter(typeof(object), "enricher");
-            var castEnricherParam = Expression.Convert(enricherParam, GetLogEnricherType());
-            var pushMethodCall = Expression.Call(null, pushPropertyMethod, castEnricherParam);
+            var castEnricherParam = Expression.Convert(enricherParam, argumentTargetType);
+            var pushMethodCall = Expression.Call(null, methodInfo, castEnricherParam);
+
             var push = Expression.Lambda<Func<object, IDisposable>>(
                     pushMethodCall,
                     enricherParam)
                 .Compile();
 
             return push;
+        }
+
+        private static object ConditionalWeakTableCallback(object key)
+        {
+            var array = Array.CreateInstance(GetLogEnricherType(), 1);
+            array.SetValue(key, 0);
+            return array;
         }
     }
 }
