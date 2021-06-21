31e5782fb280ed8105aac6b2e114be5d2d516971
Tony Redondo <tonyredondo@gmail.com>
2021-06-21 21:05:35 +0200

    Revert "clang-format"

--- src/Shared-Src/Native/string.h
+++ src/Shared-Src/Native/string.h
@@ -13,36 +13,32 @@
 #define WStrLen(value) (size_t) std::char_traits<char16_t>::length(value)
 #endif
 
-namespace shared
-{
+namespace shared {
 
-typedef std::basic_string<WCHAR> WSTRING;
+	typedef std::basic_string<WCHAR> WSTRING;
 
 #ifndef MACOS
-typedef std::basic_stringstream<WCHAR> WSTRINGSTREAM;
+	typedef std::basic_stringstream<WCHAR> WSTRINGSTREAM;
 #endif
 
-std::string ToString(const std::string& str);
-std::string ToString(const char* str);
-std::string ToString(const uint64_t i);
-std::string ToString(const WSTRING& wstr);
+	std::string ToString(const std::string& str);
+	std::string ToString(const char* str);
+	std::string ToString(const uint64_t i);
+	std::string ToString(const WSTRING& wstr);
 
-WSTRING ToWSTRING(const std::string& str);
-WSTRING ToWSTRING(const uint64_t i);
+	WSTRING ToWSTRING(const std::string& str);
+	WSTRING ToWSTRING(const uint64_t i);
 
-template <typename TChar>
-std::basic_string<TChar> ReplaceString(std::basic_string<TChar> subject, const std::basic_string<TChar>& search,
-                                       const std::basic_string<TChar>& replace)
-{
-    size_t pos = 0;
-    while ((pos = subject.find(search, pos)) != std::basic_string<TChar>::npos)
-    {
-        subject.replace(pos, search.length(), replace);
-        pos += replace.length();
-    }
-    return subject;
-}
+	template <typename TChar>
+	std::basic_string<TChar> ReplaceString(std::basic_string<TChar> subject, const std::basic_string<TChar>& search, const std::basic_string<TChar>& replace) {
+		size_t pos = 0;
+		while ((pos = subject.find(search, pos)) != std::basic_string<TChar>::npos) {
+			subject.replace(pos, search.length(), replace);
+			pos += replace.length();
+		}
+		return subject;
+	}
 
-} // namespace shared
+}  // namespace trace
 
-#endif // DD_CLR_PROFILER_STRING_H_
+#endif  // DD_CLR_PROFILER_STRING_H_
--- src/Shared-Src/Native/util.cpp
+++ src/Shared-Src/Native/util.cpp
@@ -8,100 +8,100 @@
 
 namespace shared
 {
-template <typename Out>
-void Split(const WSTRING& s, wchar_t delim, Out result)
-{
-    size_t lpos = 0;
-    for (size_t i = 0; i < s.length(); i++)
+    template <typename Out>
+    void Split(const WSTRING& s, wchar_t delim, Out result)
     {
-        if (s[i] == delim)
+        size_t lpos = 0;
+        for (size_t i = 0; i < s.length(); i++)
         {
-            *(result++) = s.substr(lpos, (i - lpos));
-            lpos = i + 1;
+            if (s[i] == delim)
+            {
+                *(result++) = s.substr(lpos, (i - lpos));
+                lpos = i + 1;
+            }
         }
+        *(result++) = s.substr(lpos);
     }
-    *(result++) = s.substr(lpos);
-}
 
-std::vector<WSTRING> Split(const WSTRING& s, wchar_t delim)
-{
-    std::vector<WSTRING> elems;
-    Split(s, delim, std::back_inserter(elems));
-    return elems;
-}
-
-WSTRING Trim(const WSTRING& str)
-{
-    if (str.length() == 0)
+    std::vector<WSTRING> Split(const WSTRING& s, wchar_t delim)
     {
-        return WStr("");
+        std::vector<WSTRING> elems;
+        Split(s, delim, std::back_inserter(elems));
+        return elems;
     }
 
-    WSTRING trimmed = str;
-
-    auto lpos = trimmed.find_first_not_of(WStr(" \t"));
-    if (lpos != WSTRING::npos && lpos > 0)
+    WSTRING Trim(const WSTRING& str)
     {
-        trimmed = trimmed.substr(lpos);
+        if (str.length() == 0)
+        {
+            return WStr("");
+        }
+
+        WSTRING trimmed = str;
+
+        auto lpos = trimmed.find_first_not_of(WStr(" \t"));
+        if (lpos != WSTRING::npos && lpos > 0)
+        {
+            trimmed = trimmed.substr(lpos);
+        }
+
+        auto rpos = trimmed.find_last_not_of(WStr(" \t"));
+        if (rpos != WSTRING::npos)
+        {
+            trimmed = trimmed.substr(0, rpos + 1);
+        }
+
+        return trimmed;
     }
 
-    auto rpos = trimmed.find_last_not_of(WStr(" \t"));
-    if (rpos != WSTRING::npos)
+    WSTRING GetEnvironmentValue(const WSTRING& name)
     {
-        trimmed = trimmed.substr(0, rpos + 1);
-    }
-
-    return trimmed;
-}
-
-WSTRING GetEnvironmentValue(const WSTRING& name)
-{
 #ifdef _WIN32
-    const size_t max_buf_size = 4096;
-    WSTRING buf(max_buf_size, 0);
-    auto len = GetEnvironmentVariable((LPWSTR) name.data(), (LPWSTR) buf.data(), (DWORD)(buf.size()));
-    return Trim(buf.substr(0, len));
+        const size_t max_buf_size = 4096;
+        WSTRING buf(max_buf_size, 0);
+        auto len = GetEnvironmentVariable((LPWSTR)name.data(), (LPWSTR)buf.data(), (DWORD)(buf.size()));
+        return Trim(buf.substr(0, len));
 #else
-    auto cstr = std::getenv(ToString(name).c_str());
-    if (cstr == nullptr)
-    {
-        return WStr("");
-    }
-    std::string str(cstr);
-    auto wstr = ToWSTRING(str);
-    return Trim(wstr);
-#endif
-}
-
-std::vector<WSTRING> GetEnvironmentValues(const WSTRING& name, const wchar_t delim)
-{
-    std::vector<WSTRING> values;
-    for (auto s : Split(GetEnvironmentValue(name), delim))
-    {
-        s = Trim(s);
-        if (!s.empty())
+        auto cstr = std::getenv(ToString(name).c_str());
+        if (cstr == nullptr)
         {
-            values.push_back(s);
+            return WStr("");
         }
+        std::string str(cstr);
+        auto wstr = ToWSTRING(str);
+        return Trim(wstr);
+#endif
     }
-    return values;
-}
 
-std::vector<WSTRING> GetEnvironmentValues(const WSTRING& name)
-{
-    return GetEnvironmentValues(name, L';');
-}
-
-WSTRING WHexStr(const void* pData, int len)
-{
-    const unsigned char* data = (unsigned char*) pData;
-    WSTRING s(len * 2, ' ');
-    for (int i = 0; i < len; ++i)
+    std::vector<WSTRING> GetEnvironmentValues(const WSTRING& name, const wchar_t delim)
     {
-        s[2 * i] = HexMap[(data[i] & 0xF0) >> 4];
-        s[2 * i + 1] = HexMap[data[i] & 0x0F];
+        std::vector<WSTRING> values;
+        for (auto s : Split(GetEnvironmentValue(name), delim))
+        {
+            s = Trim(s);
+            if (!s.empty())
+            {
+                values.push_back(s);
+            }
+        }
+        return values;
     }
-    return s;
-}
 
-} // namespace shared
+    std::vector<WSTRING> GetEnvironmentValues(const WSTRING& name)
+    {
+        return GetEnvironmentValues(name, L';');
+    }
+
+    WSTRING WHexStr(const void* pData, int len)
+    {
+        const unsigned char* data = (unsigned char*)pData;
+        WSTRING s(len * 2, ' ');
+        for (int i = 0; i < len; ++i)
+        {
+            s[2 * i] = HexMap[(data[i] & 0xF0) >> 4];
+            s[2 * i + 1] = HexMap[data[i] & 0x0F];
+        }
+        return s;
+    }
+
+}  // namespace trace
