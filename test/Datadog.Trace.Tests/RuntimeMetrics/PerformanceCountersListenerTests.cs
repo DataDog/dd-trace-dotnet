8b66e9b2894fc051060858a15f900721748bbff9
Kevin Gosse <krix33@gmail.com>
2021-06-24 14:22:35 +0200

    Initialize performance counters asynchronously

--- test/Datadog.Trace.Tests/RuntimeMetrics/PerformanceCountersListenerTests.cs
+++ test/Datadog.Trace.Tests/RuntimeMetrics/PerformanceCountersListenerTests.cs
@@ -4,9 +4,14 @@
 // </copyright>
 
 #if NETFRAMEWORK
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Datadog.Trace.RuntimeMetrics;
 using Datadog.Trace.Vendors.StatsdClient;
+using FluentAssertions;
 using Moq;
+using Moq.Protected;
 using Xunit;
 
 namespace Datadog.Trace.Tests.RuntimeMetrics
@@ -14,11 +19,13 @@ namespace Datadog.Trace.Tests.RuntimeMetrics
     public class PerformanceCountersListenerTests
     {
         [Fact]
-        public void PushEvents()
+        public async Task PushEvents()
         {
             var statsd = new Mock<IDogStatsd>();
 
             using var listener = new PerformanceCountersListener(statsd.Object);
+
+            await listener.WaitForInitialization();
 
             listener.Refresh();
 
@@ -44,6 +51,53 @@ namespace Datadog.Trace.Tests.RuntimeMetrics
             statsd.Verify(s => s.Counter(MetricsNames.ContentionCount, It.IsAny<double>(), 1, null), Times.Once);
 
             statsd.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void AsynchronousInitialization()
+        {
+            var barrier = new Barrier(2);
+            void Callback()
+            {
+                barrier.SignalAndWait();
+                barrier.SignalAndWait();
+            }
+
+            var statsd = new Mock<IDogStatsd>();
+
+            using var listener = new TestPerformanceCounterListener(statsd.Object, Callback);
+
+            // The first SignalAndWait will deadlock if InitializePerformanceCounters is not called asynchronously
+            barrier.SignalAndWait();
+
+            listener.WaitForInitialization().IsCompleted.Should().BeFalse();
+
+            // Initialization is still pending, make sure Refresh doesn't throw
+            listener.Refresh();
+
+            // Nothing should have been pushed to statsd since counters are not initialized
+            statsd.VerifyNoOtherCalls();
+
+            // All done, free the thread and cleanup
+            barrier.SignalAndWait();
+        }
+
+        private class TestPerformanceCounterListener : PerformanceCountersListener
+        {
+            private readonly Action _callback;
+
+            public TestPerformanceCounterListener(IDogStatsd statsd, Action callback)
+                : base(statsd)
+            {
+                _callback = callback;
+            }
+
+            protected override void InitializePerformanceCounters()
+            {
+                _callback();
+
+                base.InitializePerformanceCounters();
+            }
         }
     }
 }
