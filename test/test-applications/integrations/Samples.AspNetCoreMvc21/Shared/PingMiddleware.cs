595e647c6caa5d9b028a06914f86fe8de3074120
Andrew Lock <andrew.lock@datadoghq.com>
2021-06-24 17:03:41 +0100

    Add tests for middleware-only branches and map branches

---
+++ test/test-applications/integrations/Samples.AspNetCoreMvc21/Shared/PingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Samples.AspNetCoreMvc.Shared
+{
+    public class PingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public PingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/ping"))
+            {
+                return context.Response.WriteAsync("pong");
+            }
+
+            return _next(context);
+        }
+    }
+}
