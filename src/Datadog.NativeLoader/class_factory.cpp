7af26c665f6a29892156f1630d7ff989a06bc636
Tony Redondo <tony.redondo@datadoghq.com>
2021-06-25 12:07:46 +0200

    Initial NativeLoader project structure.

---
+++ src/Datadog.NativeLoader/class_factory.cpp
@@ -0,0 +1,71 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full
+// license information.
+#include "class_factory.h"
+
+#include "logging.h"
+#include "cor_profiler.h"
+
+const IID IID_IUnknown = {0x00000000, 0x0000, 0x0000, {0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46}};
+const IID IID_IClassFactory = {0x00000001, 0x0000, 0x0000, {0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46}};
+
+ClassFactory::ClassFactory() : refCount(0)
+{
+    Debug("ClassFactory::.ctor");
+}
+
+ClassFactory::~ClassFactory()
+{
+}
+
+HRESULT STDMETHODCALLTYPE ClassFactory::QueryInterface(REFIID riid, void** ppvObject)
+{
+    Debug("ClassFactory::QueryInterface");
+    if (riid == IID_IUnknown || riid == IID_IClassFactory)
+    {
+        *ppvObject = this;
+        this->AddRef();
+        return S_OK;
+    }
+
+    *ppvObject = nullptr;
+    return E_NOINTERFACE;
+}
+
+ULONG STDMETHODCALLTYPE ClassFactory::AddRef()
+{
+    Debug("ClassFactory::AddRef");
+    return std::atomic_fetch_add(&this->refCount, 1) + 1;
+}
+
+ULONG STDMETHODCALLTYPE ClassFactory::Release()
+{
+    Debug("ClassFactory::Release");
+    int count = std::atomic_fetch_sub(&this->refCount, 1) - 1;
+    if (count <= 0)
+    {
+        delete this;
+    }
+
+    return count;
+}
+
+// profiler entry point
+HRESULT STDMETHODCALLTYPE ClassFactory::CreateInstance(IUnknown* pUnkOuter, REFIID riid, void** ppvObject)
+{
+    Debug("ClassFactory::CreateInstance");
+    if (pUnkOuter != nullptr)
+    {
+        *ppvObject = nullptr;
+        return CLASS_E_NOAGGREGATION;
+    }
+
+    auto profiler = new datadog::nativeloader::CorProfiler();
+    return profiler->QueryInterface(riid, ppvObject);
+}
+
+HRESULT STDMETHODCALLTYPE ClassFactory::LockServer(BOOL fLock)
+{
+    Debug("ClassFactory::LockServer");
+    return S_OK;
+}
---
+++ src/Datadog.NativeLoader/string.h
@@ -0,0 +1,30 @@
+#ifndef STRING_H_
+#define STRING_H_
+
+#include <corhlpr.h>
+#include <sstream>
+#include <string>
+
+#ifdef _WIN32
+#define WStr(value) L##value
+#define WStrLen(value) (size_t) wcslen(value)
+#else
+#define WStr(value) u##value
+#define WStrLen(value) (size_t) std::char_traits<char16_t>::length(value)
+#endif
+
+typedef std::basic_string<WCHAR> WSTRING;
+
+#ifndef MACOS
+typedef std::basic_stringstream<WCHAR> WSTRINGSTREAM;
+#endif
+
+std::string ToString(const std::string& str);
+std::string ToString(const char* str);
+std::string ToString(const uint64_t i);
+std::string ToString(const WSTRING& wstr);
+
+WSTRING ToWSTRING(const std::string& str);
+WSTRING ToWSTRING(const uint64_t i);
+
+#endif // STRING_H_
