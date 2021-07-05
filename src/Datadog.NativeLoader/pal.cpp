5e2817f8442e14aedb9164e582f20b469073cd0e
Tony Redondo <tony.redondo@datadoghq.com>
2021-07-05 23:30:18 +0200

    Add CMakeLists.txt

--- src/Datadog.NativeLoader/pal.cpp
+++ src/Datadog.NativeLoader/pal.cpp
@@ -1,6 +1,9 @@
 #include "pal.h"
 
+#if _WIN32
 #include <Windows.h>
+#endif
+
 #include "logging.h"
 
 namespace datadog
@@ -8,17 +11,31 @@ namespace datadog
 namespace nativeloader
 {
 
-    HINSTANCE LoadDynamicLibrary(std::string filePath)
+#if _WIN32
+    void* LoadDynamicLibrary(std::string filePath)
     {
         Debug("LoadLibrary: ", filePath);
         return LoadLibrary(ToWSTRING(filePath).c_str());
     }
 
-    void* GetExternalFunction(HINSTANCE instance, std::string funcName)
+    void* GetExternalFunction(void* instance, std::string funcName)
     {
         Debug("GetExternalFunction: ", funcName);
         return (void*)GetProcAddress(instance, funcName.c_str());
     }
+#else
+    void* LoadDynamicLibrary(std::string filePath)
+    {
+        Debug("LoadLibrary: ", filePath);
+        return nullptr;
+    }
+
+    void* GetExternalFunction(void* instance, std::string funcName)
+    {
+        Debug("GetExternalFunction: ", funcName);
+        return nullptr;
+    }
+#endif
 
 } // namespace nativeloader
 } // namespace datadog
