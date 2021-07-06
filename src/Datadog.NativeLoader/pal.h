2dd7edc3e7016fa37af295a617ec319b91d5147f
Tony Redondo <tony.redondo@datadoghq.com>
2021-07-06 17:03:34 +0200

    Add support for loader.conf

--- src/Datadog.NativeLoader/pal.h
+++ src/Datadog.NativeLoader/pal.h
@@ -12,6 +12,17 @@ namespace nativeloader
 
     void* GetExternalFunction(void* instance, std::string funcName);
 
+    // GetEnvironmentValue returns the environment variable value for the given
+    // name. Space is trimmed.
+    WSTRING GetEnvironmentValue(const WSTRING& name);
+
+    // GetEnvironmentValues returns environment variable values for the given name
+    // split by the delimiter. Space is trimmed and empty values are ignored.
+    std::vector<WSTRING> GetEnvironmentValues(const WSTRING& name, const wchar_t delim);
+
+    // GetEnvironmentValues calls GetEnvironmentValues with a semicolon delimiter.
+    std::vector<WSTRING> GetEnvironmentValues(const WSTRING& name);
+
 } // namespace nativeloader
 } // namespace datadog
 
