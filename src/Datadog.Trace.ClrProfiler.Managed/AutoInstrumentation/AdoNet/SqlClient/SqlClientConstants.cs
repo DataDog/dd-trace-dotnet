18cb990d2406c1c3e966e62f9442899c067eaf2a
Kevin Gosse <krix33@gmail.com>
2021-07-07 14:22:02 +0200

    Add support for Microsoft.Data.SqlClient 3.*

--- src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/AdoNet/SqlClient/SqlClientConstants.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/AutoInstrumentation/AdoNet/SqlClient/SqlClientConstants.cs
@@ -53,7 +53,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet.SqlClient
 
             public string MinimumVersion => "1.0.0";
 
-            public string MaximumVersion => "2.*.*";
+            public string MaximumVersion => "3.*.*";
 
             public string DataReaderType => "Microsoft.Data.SqlClient.SqlDataReader";
 
