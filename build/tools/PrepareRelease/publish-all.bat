2e5ce76f9af16167a30c5a284766e67cbe285b77
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-07-07 15:17:04 -0400

    fix "PrepareRelease msi" command (#1583)

--- build/tools/PrepareRelease/publish-all.bat
+++ build/tools/PrepareRelease/publish-all.bat
@@ -1,2 +1,1 @@
-RMDIR "%OUTPUT_DIR%" /S /Q
-dotnet msbuild "%SOLUTION_DIR%\Datadog.Trace.proj" /t:PublishManagedProfilerOnDisk /p:Configuration=Release;TracerHomeDirectory=%OUTPUT_DIR%
+%SOLUTION_DIR%\build.cmd Clean BuildTracerHome
