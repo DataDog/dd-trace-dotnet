18f34e19d0915fce09d1fa32478c0d09c880700e
Tony Redondo <tony.redondo@datadoghq.com>
2021-07-08 11:47:59 +0200

    Code refactoring

--- src/Datadog.NativeLoader/util.h
+++ src/Datadog.NativeLoader/util.h
@@ -7,43 +7,34 @@
 using namespace datadog::nativeloader;
 
 const std::string conf_filename = "loader.conf";
-#if _WIN32
-const std::string dynExtension = ".dll";
-#elif LINUX
-const std::string dynExtension = ".so";
-#elif MACOS
-const std::string dynExtension = ".dylib";
-#endif
 
 // Gets the profiler path
 static WSTRING GetProfilerPath()
 {
     WSTRING profiler_path;
-    profiler_path = GetEnvironmentValue(WStr("CORECLR_PROFILER_PATH"));
-    if (profiler_path.length() == 0)
-    {
-        profiler_path = GetEnvironmentValue(WStr("COR_PROFILER_PATH"));
-    }
 
 #if BIT64
-    if (profiler_path.length() == 0)
-    {
-        profiler_path = GetEnvironmentValue(WStr("CORECLR_PROFILER_PATH_64"));
-    }
+    profiler_path = GetEnvironmentValue(WStr("CORECLR_PROFILER_PATH_64"));
     if (profiler_path.length() == 0)
     {
         profiler_path = GetEnvironmentValue(WStr("COR_PROFILER_PATH_64"));
     }
 #else
-    if (profiler_path.length() == 0)
-    {
-        profiler_path = GetEnvironmentValue(WStr("CORECLR_PROFILER_PATH_32"));
-    }
+    profiler_path = GetEnvironmentValue(WStr("CORECLR_PROFILER_PATH_32"));
     if (profiler_path.length() == 0)
     {
         profiler_path = GetEnvironmentValue(WStr("COR_PROFILER_PATH_32"));
     }
 #endif
+
+    if (profiler_path.length() == 0)
+    {
+        profiler_path = GetEnvironmentValue(WStr("CORECLR_PROFILER_PATH"));
+    }
+    if (profiler_path.length() == 0)
+    {
+        profiler_path = GetEnvironmentValue(WStr("COR_PROFILER_PATH"));
+    }
 
     return profiler_path;
 }
