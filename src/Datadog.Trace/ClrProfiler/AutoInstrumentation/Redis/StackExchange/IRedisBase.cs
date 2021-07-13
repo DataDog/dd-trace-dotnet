85ac602e2acda390ced27375baae38ede3b8bad4
Zach Montoya <zach.montoya@datadoghq.com>
2021-07-13 14:02:17 -0700

    Add a unit test and the appropriate fixes so that we don't use named arguments in attribute declarations, unless they're builtin types.

--- src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Redis/StackExchange/IRedisBase.cs
+++ src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Redis/StackExchange/IRedisBase.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IRedisBase.cs" company="Datadog">
+// <copyright file="IRedisBase.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -18,7 +18,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Redis.StackExchange
         /// <summary>
         /// Gets multiplexer data structure
         /// </summary>
-        [Duck(Name = "multiplexer", Kind = DuckKind.Field)]
+        [DuckField(Name = "multiplexer")]
         public MultiplexerData Multiplexer { get; }
     }
 }
