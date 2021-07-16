d53cbf729ea07286a628f9a42c23ae6788b89e57
Zach Montoya <zach.montoya@datadoghq.com>
2021-07-16 11:28:38 -0700

    Re-do the CustomNLogLogProvider changes. The missing Set(string, object) API means we can't use our custom LogEnricher with this version of NLog, so override the IsLoggerAvailable check to bail out and not use the CustomNLogLogProvider.

--- src/Datadog.Trace/Logging/CustomNLogLogProvider.cs
+++ src/Datadog.Trace/Logging/CustomNLogLogProvider.cs
@@ -13,6 +13,9 @@ namespace Datadog.Trace.Logging
     {
         public ILogEnricher CreateEnricher() => new LogEnricher(this);
 
+        internal static new bool IsLoggerAvailable() =>
+            NLogLogProvider.IsLoggerAvailable() && IsSetObjectAvailable();
+
         protected override OpenMdc GetOpenMdcMethod()
         {
             // This is a copy/paste of the base GetOpenMdcMethod, but calling Set(string, object) instead of Set(string, string)
@@ -43,48 +46,29 @@ namespace Datadog.Trace.Logging
 
             var mdcContextType = FindType("NLog.MappedDiagnosticsContext", "NLog");
             var setMethod = mdcContextType.GetMethod("Set", typeof(string), typeof(object));
+            var removeMethod = mdcContextType.GetMethod("Remove", typeof(string));
+            var valueParam = Expression.Parameter(typeof(object), "value");
+            var setMethodCall = Expression.Call(null, setMethod, keyParam, valueParam);
+            var removeMethodCall = Expression.Call(null, removeMethod, keyParam);
 
-            if (setMethod != null)
+            var set = Expression
+                .Lambda<Action<string, object>>(setMethodCall, keyParam, valueParam)
+                .Compile();
+            var remove = Expression
+                .Lambda<Action<string>>(removeMethodCall, keyParam)
+                .Compile();
+
+            return (key, value, _) =>
             {
-                var removeMethod = mdcContextType.GetMethod("Remove", typeof(string));
-                var valueParam = Expression.Parameter(typeof(object), "value");
-                var setMethodCall = Expression.Call(null, setMethod, keyParam, valueParam);
-                var removeMethodCall = Expression.Call(null, removeMethod, keyParam);
+                set(key, value);
+                return new DisposableAction(() => remove(key));
+            };
+        }
 
-                var set = Expression
-                    .Lambda<Action<string, object>>(setMethodCall, keyParam, valueParam)
-                    .Compile();
-                var remove = Expression
-                    .Lambda<Action<string>>(removeMethodCall, keyParam)
-                    .Compile();
-
-                return (key, value, _) =>
-                {
-                    set(key, value);
-                    return new DisposableAction(() => remove(key));
-                };
-            }
-            else
-            {
-                setMethod = mdcContextType.GetMethod("Set", typeof(string), typeof(string));
-                var removeMethod = mdcContextType.GetMethod("Remove", typeof(string));
-                var valueParam = Expression.Parameter(typeof(string), "value");
-                var setMethodCall = Expression.Call(null, setMethod, keyParam, valueParam);
-                var removeMethodCall = Expression.Call(null, removeMethod, keyParam);
-
-                var set = Expression
-                    .Lambda<Action<string, string>>(setMethodCall, keyParam, valueParam)
-                    .Compile();
-                var remove = Expression
-                    .Lambda<Action<string>>(removeMethodCall, keyParam)
-                    .Compile();
-
-                return (key, value, _) =>
-                {
-                    set(key, value.ToString());
-                    return new DisposableAction(() => remove(key));
-                };
-            }
+        private static bool IsSetObjectAvailable()
+        {
+            var mdcContextType = FindType("NLog.MappedDiagnosticsContext", "NLog");
+            return mdcContextType.GetMethod("Set", typeof(string), typeof(object)) != null;
         }
     }
 }
