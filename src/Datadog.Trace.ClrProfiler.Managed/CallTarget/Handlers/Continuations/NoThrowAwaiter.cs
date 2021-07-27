eaf9db0bc956a1323eb5edc40ade802894140b31
Kevin Gosse <krix33@gmail.com>
2021-07-27 17:44:49 +0200

    Preserve the task cancelled state when using calltarget instrumentation

---
+++ src/Datadog.Trace.ClrProfiler.Managed/CallTarget/Handlers/Continuations/NoThrowAwaiter.cs
@@ -0,0 +1,34 @@
+// <copyright file="NoThrowAwaiter.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+#pragma warning disable SA1649 // File name must match first type name
+
+namespace Datadog.Trace.ClrProfiler.CallTarget.Handlers.Continuations
+{
+    internal struct NoThrowAwaiter : ICriticalNotifyCompletion
+    {
+        private readonly Task _task;
+
+        public NoThrowAwaiter(Task task)
+        {
+            _task = task;
+        }
+
+        public bool IsCompleted => _task.IsCompleted;
+
+        public NoThrowAwaiter GetAwaiter() => this;
+
+        public void GetResult()
+        {
+        }
+
+        public void OnCompleted(Action continuation) => _task.GetAwaiter().OnCompleted(continuation);
+
+        public void UnsafeOnCompleted(Action continuation) => OnCompleted(continuation);
+    }
+}
--- src/Datadog.Trace.ClrProfiler.Managed/CallTarget/Handlers/Continuations/TaskContinuationGenerator.cs
+++ src/Datadog.Trace.ClrProfiler.Managed/CallTarget/Handlers/Continuations/TaskContinuationGenerator.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Reflection.Emit;
 using System.Runtime.ExceptionServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Datadog.Trace.ClrProfiler.CallTarget.Handlers.Continuations
@@ -14,7 +13,6 @@ namespace Datadog.Trace.ClrProfiler.CallTarget.Handlers.Continuations
     internal class TaskContinuationGenerator<TIntegration, TTarget, TReturn> : ContinuationGenerator<TTarget, TReturn>
     {
         private static readonly Func<TTarget, object, Exception, CallTargetState, object> _continuation;
-        private static readonly Action<Task, object> _continuationAction;
 
         static TaskContinuationGenerator()
         {
@@ -23,8 +21,6 @@ namespace Datadog.Trace.ClrProfiler.CallTarget.Handlers.Continuations
             {
                 _continuation = (Func<TTarget, object, Exception, CallTargetState, object>)continuationMethod.CreateDelegate(typeof(Func<TTarget, object, Exception, CallTargetState, object>));
             }
-
-            _continuationAction = new Action<Task, object>(ContinuationAction);
         }
 
         public override TReturn SetContinuation(TTarget instance, TReturn returnValue, Exception exception, CallTargetState state)
@@ -47,37 +43,53 @@ namespace Datadog.Trace.ClrProfiler.CallTarget.Handlers.Continuations
                 return returnValue;
             }
 
-            var continuationState = new ContinuationGeneratorState<TTarget>(instance, state);
-            return ToTReturn(previousTask.ContinueWith(
-                _continuationAction,
-                continuationState,
-                CancellationToken.None,
-                TaskContinuationOptions.ExecuteSynchronously,
-                TaskScheduler.Current));
+            return ToTReturn(ContinuationAction(previousTask, instance, state));
         }
 
-        private static void ContinuationAction(Task previousTask, object state)
+        private static async Task ContinuationAction(Task previousTask, TTarget target, CallTargetState state)
         {
+            if (!previousTask.IsCompleted)
+            {
+                await new NoThrowAwaiter(previousTask);
+            }
+
+            Exception exception = null;
+
+            if (previousTask.Status == TaskStatus.Faulted)
+            {
+                exception = previousTask.Exception.GetBaseException();
+            }
+            else if (previousTask.Status == TaskStatus.Canceled)
+            {
+                try
+                {
+                    // The only supported way to extract the cancellation exception is to await the task
+                    await previousTask;
+                }
+                catch (Exception ex)
+                {
+                    exception = ex;
+                }
+            }
+
             try
             {
-                ContinuationGeneratorState<TTarget> contState = (ContinuationGeneratorState<TTarget>)state;
-
                 // *
                 // Calls the CallTarget integration continuation, exceptions here should never bubble up to the application
                 // *
-                _continuation(contState.Target, null, previousTask.Exception, contState.State);
+                _continuation(target, null, exception, state);
             }
             catch (Exception ex)
             {
                 IntegrationOptions<TIntegration, TTarget>.LogException(ex, "Exception occurred when calling the CallTarget integration continuation.");
             }
 
-            if (previousTask.Exception is not null)
+            // *
+            // If the original task throws an exception we rethrow it here.
+            // *
+            if (exception != null)
             {
-                // *
-                // If the original task throws an exception we rethrow it here.
-                // *
-                ExceptionDispatchInfo.Capture(previousTask.Exception.GetBaseException()).Throw();
+                ExceptionDispatchInfo.Capture(exception).Throw();
             }
         }
     }
