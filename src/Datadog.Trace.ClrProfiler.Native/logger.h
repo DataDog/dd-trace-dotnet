5c4522018e9f82d1c4e9ef415588e65e16df75d6
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2021-07-27 11:08:51 +0200

    Fix escaped character in the string

--- src/Datadog.Trace.ClrProfiler.Native/logger.h
+++ src/Datadog.Trace.ClrProfiler.Native/logger.h
@@ -16,7 +16,7 @@ struct TracerLoggerPolicy
 #ifdef _WIN32
     // this field will be removed once merged with the profiler in order to have
     // the same product folder name
-    inline static const WSTRING folder_path = WStr("Datadog .NET Tracer\logs");
+    inline static const WSTRING folder_path = WStr(R"(Datadog .NET Tracer\logs)");
 #endif
     inline static const std::string pattern = "%D %I:%M:%S.%e %p [%P|%t] [%l] %v";
     struct logging_environment
