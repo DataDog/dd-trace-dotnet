DataDog/dd-trace-dotnet

commit da76934108db4f65af03ca9497309703523e8a65

Add fix for log injection with log4net.Ext.Json versions < 2.0.9.1 (#1615)
Add fix for log4net.Ext.Json versions < 2.0.9.1 not supporting log4net.Core.IFixingRequired for special-case object rendering.

Background
The log4net.Ext.Json library is a prominent third-party library that can render log4net objects into JSON. To prepare the final JSON object, the StandardTypesDecorator does a type check against each value in the MDC dictionary and tries to render each object accordingly. If the value is a well-known type, then the idiomatic JSON value is written. If not, the decorator will render the value as a new JSON object and attempt to render each of the original object's members as properties on the new JSON object.

Bug
In log4net.Ext.Json versions < 2.0.9.1, the interface log4net.Core.IFixingRequired is not included in the set of types specifically recognized as a built-in type, so the custom object Log4NetEnricher+TracerProperty that we insert into the MDC dictionary is rendered as a new JSON object, not a string, which breaks trace-log correlation.

Fix
Before using our CustomLog4NetLogProvider (which utilizes our Log4NetEnricher for holding the current trace_id and span_id values for logs injection), check if the application is using log4net.Ext.Json version 2.0.9.1 or newer. If not, we'll fallback to the original liblog log provider implementation.

src/Datadog.Trace/Logging/CustomLog4NetLogProvider.cs
@@ -5,13 +5,19 @@
 
 using System;
 using System.Linq.Expressions;
+using System.Reflection;
 using Datadog.Trace.Logging.LogProviders;
 
 namespace Datadog.Trace.Logging
 {
     internal class CustomLog4NetLogProvider : Log4NetLogProvider, ILogProviderWithEnricher
     {
+        private static readonly Version SupportedExtJsonAssemblyVersion = new Version("2.0.9.1");
+
         public ILogEnricher CreateEnricher() => new Log4NetEnricher(this);
+
+        internal static new bool IsLoggerAvailable() =>
+            Log4NetLogProvider.IsLoggerAvailable() && ExtJsonAssemblySupported();
 
         protected override OpenMdc GetOpenMdcMethod()
         {
@@ -49,6 +55,41 @@ namespace Datadog.Trace.Logging
                 set(key, value);
                 return new DisposableAction(() => remove(key));
             };
+        }
+
+        /// <summary>
+        /// <para>
+        /// Dynamically loads the log4net.Ext.Json assembly and returns true 1) if the library is not found or 2) the library
+        /// is found and the version is &gt;= 2.0.9.1. Otherwise, this returns false.
+        /// </para>
+        ///
+        /// <para>
+        /// Background: The log4net.Ext.Json library is a prominent third-party library that can render log4net
+        /// objects into JSON. To prepare the final JSON object, the StandardTypesDecorator does a
+        /// type check against each value in the MDC dictionary and tries to render each object
+        /// accordingly. If the value is a well-known type, then the idiomatic JSON value is written.
+        /// If not, the decorator will render the value as a new JSON object and attempt to render
+        /// each of the original object's members as properties on the new JSON object.
+        /// </para>
+        ///
+        /// <para>
+        /// Bug: In log4net.Ext.Json versions &lt; 2.0.9.1, the interface log4net.Core.IFixingRequired
+        /// is not included in the set of types specifically recognized as a built-in type, so the
+        /// custom object Log4NetEnricher+TracerProperty that we insert into the MDC dictionary is
+        /// rendered as a new JSON object, not a string, which breaks trace-log correlation.
+        /// </para>
+        /// </summary>
+        /// <returns>true if the log4net.Ext.Json assembly is not found or if it is found and the version is &gt;= 2.0.9.1. Otherwise, false.</returns>
+        private static bool ExtJsonAssemblySupported()
+        {
+            Assembly extJsonAssembly = Assembly.Load("log4net.Ext.Json");
+            if (extJsonAssembly is not null)
+            {
+                return extJsonAssembly.GetName().Version >= SupportedExtJsonAssemblyVersion;
+            }
+
+            // log4net.Ext.Json not found, so there will be no compatibility issues
+            return true;
         }
     }
 }

commit 49b033c79cd7d8f0412deed8af41de3c167be102

Fix a bug introduced in https://github.com/DataDog/dd-trace-dotnet/pull/1615

src/Datadog.Trace/Logging/CustomLog4NetLogProvider.cs
@@ -82,10 +82,18 @@ namespace Datadog.Trace.Logging
         /// <returns>true if the log4net.Ext.Json assembly is not found or if it is found and the version is &gt;= 2.0.9.1. Otherwise, false.</returns>
         private static bool ExtJsonAssemblySupported()
         {
-            Assembly extJsonAssembly = Assembly.Load("log4net.Ext.Json");
-            if (extJsonAssembly is not null)
+            try
             {
-                return extJsonAssembly.GetName().Version >= SupportedExtJsonAssemblyVersion;
+                Assembly extJsonAssembly = Assembly.Load("log4net.Ext.Json");
+                if (extJsonAssembly is not null)
+                {
+                    return extJsonAssembly.GetName().Version >= SupportedExtJsonAssemblyVersion;
+                }
+            }
+            catch
+            {
+                // The Assembly load should only fail when the assembly cannot be found
+                // Fallback to default case
             }
 
             // log4net.Ext.Json not found, so there will be no compatibility issues