13bd8579ca5e7654ab0d541736a54a01a054ae01
Robert Pickering <robert@sqreen.io>
2021-07-29 17:45:57 +0200

    Implement PostAsJsonAsync in HttpStreamRequest

--- src/Datadog.Trace/Agent/Transports/HttpStreamRequest.cs
+++ src/Datadog.Trace/Agent/Transports/HttpStreamRequest.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Datadog.Trace.Abstractions;
 using Datadog.Trace.HttpOverStreams;
 using Datadog.Trace.HttpOverStreams.HttpContent;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
 
 namespace Datadog.Trace.Agent.Transports
 {
@@ -39,9 +40,36 @@ namespace Datadog.Trace.Agent.Transports
             _headers.Add(name, value);
         }
 
-        public Task<IApiResponse> PostAsJsonAsync(IEvent events)
+        public async Task<IApiResponse> PostAsJsonAsync(IEvent events)
         {
-            throw new NotImplementedException();
+            using (var bidirectionalStream = _streamFactory.GetBidirectionalStream())
+            {
+                using var memoryStream = new MemoryStream();
+                using var streamWriter = new StreamWriter(memoryStream);
+                var json = JsonConvert.SerializeObject(events);
+                streamWriter.Write(json);
+
+                var content = new BufferContent(new ArraySegment<byte>(memoryStream.GetBuffer()));
+                var request = new HttpRequest("POST", _uri.Host, _uri.PathAndQuery, _headers, content);
+
+                // send request, get response
+                var response = await _client.SendAsync(request, bidirectionalStream, bidirectionalStream).ConfigureAwait(false);
+
+                // Content-Length is required as we don't support chunked transfer
+                var contentLength = response.Content.Length;
+                if (!contentLength.HasValue)
+                {
+                    throw new Exception("Content-Length is required but was not provided");
+                }
+
+                // buffer the entire contents for now
+                var buffer = new byte[contentLength.Value];
+                var responseContentStream = new MemoryStream(buffer);
+                await response.Content.CopyToAsync(buffer).ConfigureAwait(false);
+                responseContentStream.Position = 0;
+
+                return new HttpStreamResponse(response.StatusCode, responseContentStream.Length, response.GetContentEncoding(), responseContentStream, response.Headers);
+            }
         }
 
         public async Task<IApiResponse> PostAsync(ArraySegment<byte> traces)
