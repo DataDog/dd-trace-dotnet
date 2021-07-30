0145b66cd610437af9b3ad79a25fed2edc8f712e
Andrew Lock <andrew.lock@datadoghq.com>
2021-07-30 09:33:52 +0100

    Add ILogger integration
    When a logger iterates the scopes for a log message we insert ourselves as the first entry. Note that we don't _really_ add a scope (by instrumenting `BeginScope()` or similar) as that would insert our scope in the "scope stack", and make us responsible for removing it at the appropriate time. The approach here is much simpler, and works for all types of instrumentation that are using the standard ILogger infrastructure
    
    This essentially works the same way as the "activity" ILogger integration does in the runtime (https://github.com/dotnet/runtime/blob/v5.0.0/src/libraries/Microsoft.Extensions.Logging/src/LoggerFactoryScopeProvider.cs#L36-L52)

---
+++ src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ILogger/DatadogLoggingScope.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="DatadogLoggingScope.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ILogger
+{
+    internal class DatadogLoggingScope : IReadOnlyList<KeyValuePair<string, object>>
+    {
+        private readonly string _service;
+        private readonly string _env;
+        private readonly string _version;
+        private readonly Tracer _tracer;
+
+        public DatadogLoggingScope()
+            : this(Tracer.Instance)
+        {
+        }
+
+        internal DatadogLoggingScope(Tracer tracer)
+        {
+            _tracer = tracer;
+            _service = tracer.DefaultServiceName ?? string.Empty;
+            _env = tracer.Settings.Environment ?? string.Empty;
+            _version = tracer.Settings.ServiceVersion ?? string.Empty;
+        }
+
+        public int Count => 4;
+
+        public KeyValuePair<string, object> this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return new KeyValuePair<string, object>("dd_service", _tracer.ActiveScope?.Span.ServiceName ?? _service);
+                }
+                else if (index == 1)
+                {
+                    return new KeyValuePair<string, object>("dd_env", _env);
+                }
+                else if (index == 2)
+                {
+                    return new KeyValuePair<string, object>("dd_version", _version);
+                }
+                else if (index == 3)
+                {
+                    return new KeyValuePair<string, object>("dd_trace_id", _tracer.ActiveScope?.Span.TraceId);
+                }
+
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        public override string ToString()
+        {
+            var span = _tracer.ActiveScope?.Span;
+            if (span is null)
+            {
+                return string.Empty;
+            }
+
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "dd_service:{0}, dd_env:{1}, dd_version:{2}, dd_trace_id:{3}",
+                span.ServiceName,
+                _env,
+                _version,
+                span.TraceId);
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            var span = _tracer.ActiveScope?.Span;
+            if (span is null)
+            {
+                yield break;
+            }
+
+            yield return new KeyValuePair<string, object>("dd_service", span.ServiceName);
+            yield return new KeyValuePair<string, object>("dd_env", _env);
+            yield return new KeyValuePair<string, object>("dd_version", _version);
+            yield return new KeyValuePair<string, object>("dd_trace_id", span.TraceId);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
