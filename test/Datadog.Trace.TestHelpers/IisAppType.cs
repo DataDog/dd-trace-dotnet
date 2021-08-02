e97e2bcbb6a640edd9a2f613ef412c7a966eeb5a
Anna <anna.yafi@datadoghq.com>
2021-08-02 19:18:47 +0200

    Adaptations to build

--- test/Datadog.Trace.TestHelpers/IisAppType.cs
+++ test/Datadog.Trace.TestHelpers/IisAppType.cs
@@ -1,9 +1,9 @@
-﻿// <copyright file="IisAppType.cs" company="Datadog">
+// <copyright file="IisAppType.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-namespace Datadog.Trace.ClrProfiler.IntegrationTests
+namespace Datadog.Trace.TestHelpers
 {
     public enum IisAppType
     {
