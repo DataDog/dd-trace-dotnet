9c3e3a3a0dd76a1929dcff679d4843199735c26c
Andrew Lock <andrew.lock@datadoghq.com>
2021-08-02 12:05:43 +0100

    Add HttpWebRequest integration from BeginGetRequestStream, BeginGetResponse, and EndGetResponse
    BeginGetRequestStream can be used in both .NET Core and .NET Framework, but
    BeginGetResponse and EndGetResponse are .NET Framework only, because the .NET Core
    implementation uses HttpClient under the covers (which we already instrument)

---
+++ src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Http/WebRequest/IHttpWebRequest.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IHttpWebRequest.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Http.WebRequest
+{
+    /// <summary>
+    /// Duck type interface for HttpWebRequest
+    /// </summary>
+    public interface IHttpWebRequest
+    {
+        /// <summary>
+        /// Gets the time the HttpWebRequest was created in Ticks (UTC)
+        /// </summary>
+        [Duck(Name = "m_StartTimestamp", Kind = DuckKind.Field)]
+        long RequestStartTicks { get; }
+    }
+}
