87df16e6db0508c018ba25d60f2ec4ad3eea6086
Tony Redondo <tony.redondo@datadoghq.com>
2021-08-05 09:40:22 +0200

    Crank native profiler fix (#1655)
    * Adds commit hash to the profiler path
    
    * Apply the commit_hash sub folder to other crank scenarios.
    
    * fix missing variable declaration.
    
    * Fail the script in case the crank process return an error ExitCode
    
    * Fail the throughput tests if a file doesn't exist
    
    (cherry picked from commit 6b5b26eeffc997fb456a2d1eee1bb824e185b268)
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- test/test-applications/throughput/Samples.AspNetCoreSimpleController/Program.cs
+++ test/test-applications/throughput/Samples.AspNetCoreSimpleController/Program.cs
@@ -16,6 +16,26 @@ namespace Samples.AspNetCoreSimpleController
         {
             Console.WriteLine(typeof(Datadog.Trace.ClrProfiler.Instrumentation).Assembly.FullName);
             Console.WriteLine();
+
+            if (Environment.GetEnvironmentVariable("COR_ENABLE_PROFILING") == "1" ||
+                Environment.GetEnvironmentVariable("CORECLR_ENABLE_PROFILING") == "1")
+            {
+                bool isAttached = IsProfilerAttached();
+                Console.WriteLine(" * Checking if the profiler is attached: {0}", isAttached);
+                if (!isAttached)
+                {
+                    Console.WriteLine("Error: Profiler is required and is not loaded.");
+                    Environment.Exit(1);
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine(" * Running without profiler.");
+            }
+
+            Console.WriteLine();
+
             CreateHostBuilder(args).Build().Run();
         }
 
