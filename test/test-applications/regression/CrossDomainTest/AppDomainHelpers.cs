ade7d271c6bc6f8d9d6831e5e4b089adffd4e33b
Andrew Lock <andrew.lock@datadoghq.com>
2021-08-06 11:56:43 +0100

    Add temporary CrossDomainTest regression test

---
+++ test/test-applications/regression/CrossDomainTest/AppDomainHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace CrossDomainTest
+{
+    public static class AppDomainHelpers
+    {
+        // Ensure the remote call takes long enough for the lease manager poll to occur.
+        // Even though we reset LifetimeServices.LeaseManagerPollTime to a shorter duration,
+        // the default value is 10 seconds so the first poll may not be affected by our modification
+        public static void SleepForLeaseManagerPollCallback() => Thread.Sleep(TimeSpan.FromSeconds(12));
+
+        public static void EmptyCallback()
+        {
+        }
+    }
+}
