6a1e850d6a957d6c52a0672143b7202f126a277d
Anna <anna.yafi@datadoghq.com>
2021-08-09 12:36:57 +0200

    Fixing missing fields for security events

--- src/Datadog.Trace/AppSec/EventModel/Attack.cs
+++ src/Datadog.Trace/AppSec/EventModel/Attack.cs
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
 using Datadog.Trace.Vendors.Newtonsoft.Json;
 
 namespace Datadog.Trace.AppSec.EventModel
@@ -24,19 +25,31 @@ namespace Datadog.Trace.AppSec.EventModel
         public static Attack From(Waf.ReturnTypes.Managed.Return result, Trace.Span span, Transport.ITransport transport)
         {
             var ruleMatch = result.ResultData.Filter[0];
+            var request = transport.Request();
+
             var attack = new Attack
             {
-                Context = new Context(),
+                EventId = Guid.NewGuid().ToString(),
+                Context = new Context()
+                {
+                    Http = new Http
+                    {
+                        Request = request,
+                        Response = transport.Response(result.Blocked)
+                    },
+                    Actor = new Actor { Ip = new Ip { Address = request.RemoteIp } }
+                },
                 Blocked = result.Blocked,
                 Rule = new Rule { Name = result.ResultData.Flow, Id = result.ResultData.Rule },
-                DetectedAt = System.DateTime.UtcNow,
+                DetectedAt = DateTime.UtcNow,
                 RuleMatch = new RuleMatch
                 {
                     Operator = ruleMatch.Operator,
                     OperatorValue = ruleMatch.OperatorValue,
                     Highlight = new string[] { ruleMatch.MatchStatus },
                     Parameters = new Parameter[] { new Parameter { Name = ruleMatch.BindingAccessor, Value = ruleMatch.ResolvedValue } }
-                }
+                },
+                Type = result.ResultData.Flow
             };
             if (span != null)
             {
@@ -44,14 +57,6 @@ namespace Datadog.Trace.AppSec.EventModel
                 attack.Context.Trace = new Span { Id = span.TraceId };
                 attack.Context.Service = new Service { Name = span.ServiceName };
             }
-
-            var request = transport.Request();
-            attack.Context.Http = new Http
-            {
-                Request = request,
-                Response = transport.Response(result.Blocked)
-            };
-            attack.Context.Actor = new Actor { Ip = new Ip { Address = request.RemoteIp } };
 
             return attack;
         }
