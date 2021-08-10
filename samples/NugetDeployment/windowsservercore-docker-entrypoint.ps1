2f0c4eb5af0219f6f7ba4f150fb516e4271eed8a
Zach Montoya <zach.montoya@datadoghq.com>
2021-08-10 16:35:58 -0700

    Use docker-compose file, which means that we can tidy up the dockerfiles and not need the docker-entrypoint workarounds

--- samples/NugetDeployment/windowsservercore-docker-entrypoint.ps1
+++
@@ -1,5 +0,0 @@
-# Workaround from https://github.com/docker/for-win/issues/1976#issuecomment-585423014
-# DNS Entries
-Write-Host("Configuring DNS entries...")
-.\windowsservercore-docker-fix-hosts.ps1
-.\HttpListenerExample.exe
