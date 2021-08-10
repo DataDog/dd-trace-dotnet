a6303c60daa4f2ed97dc5e6d2a455e56dda9fea4
Zach Montoya <zach.montoya@datadoghq.com>
2021-08-10 14:52:37 -0700

    Fix typo in windowsservercore-docker-entrypoint

--- samples/NugetDeployment/windowsservercore-docker-entrypoint.ps1
+++ samples/NugetDeployment/windowsservercore-docker-entrypoint.ps1
@@ -1,4 +1,4 @@
-# Workaround from # Workaround from https://github.com/docker/for-win/issues/1976#issuecomment-585423014
+# Workaround from https://github.com/docker/for-win/issues/1976#issuecomment-585423014
 # DNS Entries
 Write-Host("Configuring DNS entries...")
 .\windowsservercore-docker-fix-hosts.ps1
