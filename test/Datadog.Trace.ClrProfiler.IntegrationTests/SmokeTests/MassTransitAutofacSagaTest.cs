fe98e6ab4020b28d58a9792079e1e55dfb4b7fdf
Zach Montoya <zach.montoya@datadoghq.com>
2021-08-18 12:59:44 -0700

    Clean up using statements

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/MassTransitAutofacSagaTest.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/MassTransitAutofacSagaTest.cs
@@ -3,11 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
