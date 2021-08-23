b83f18e5ac6aaee0084fe967b33129cffa679ba7
Andrew Lock <andrew.lock@datadoghq.com>
2021-08-23 12:06:17 +0100

    Add required [Browsable(false)] and [EditorBrowsable(Never)] attributes to ilogger integration

--- src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ILogger/LoggerFactoryScopeProviderForEachScopeIntegration.cs
+++ src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ILogger/LoggerFactoryScopeProviderForEachScopeIntegration.cs
@@ -4,6 +4,7 @@
 // </copyright>
 
 using System;
+using System.ComponentModel;
 using Datadog.Trace.ClrProfiler.CallTarget;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ILogger
@@ -20,6 +21,8 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ILogger
         MinimumVersion = "2.0.0",
         MaximumVersion = "5.*.*",
         IntegrationName = LoggerIntegrationCommon.IntegrationName)]
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public class LoggerFactoryScopeProviderForEachScopeIntegration
     {
         /// <summary>
