b250d5199d7c4171303ccaa48809e385435a0963
Andrew Lock <andrew.lock@datadoghq.com>
2021-08-23 15:35:33 +0100

    Add Microsoft.Extensions.Logging.ILogger instrumentation (#1663)
    * Add LogsInjectionTestBase from #1637
    
    * Add ILogger integration
    
    When a logger iterates the scopes for a log message we insert ourselves as the first entry. Note that we don't _really_ add a scope (by instrumenting `BeginScope()` or similar) as that would insert our scope in the "scope stack", and make us responsible for removing it at the appropriate time. The approach here is much simpler, and works for all types of instrumentation that are using the standard ILogger infrastructure
    
    This essentially works the same way as the "activity" ILogger integration does in the runtime (https://github.com/dotnet/runtime/blob/v5.0.0/src/libraries/Microsoft.Extensions.Logging/src/LoggerFactoryScopeProvider.cs#L36-L52)
    
    * Update profiler skipped assembly prefixes
    
    We need to instrument Microsoft.Extensions.Logging.Abstractions now, so can't skip the Microsoft.Extensions prefix
    
    This adds most of them to the skip list, but there may be a better approach, e.g. an additional "don't skip" list?
    
    * Add LogsInjection aspnetcore sample
    
    * Add tests for ILogger logs injection
    
    * Update ILogger integration to inject dd_service etc even if we don't have a trace_id
    
    * Inject dd_span when a span is available
    
    Update tests to verify span_id is injected correctly
    
    * Update integrations.json after rebase
    
    * Convert DatadogLoggingScope indexer to switch expression
    
    * Add required [Browsable(false)] and [EditorBrowsable(Never)] attributes to ilogger integration

---
+++ src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ILogger/DatadogLoggingScope.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="DatadogLoggingScope.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ILogger
+{
+    internal class DatadogLoggingScope : IReadOnlyList<KeyValuePair<string, object>>
+    {
+        private readonly string _service;
+        private readonly string _env;
+        private readonly string _version;
+        private readonly Tracer _tracer;
+
+        public DatadogLoggingScope()
+            : this(Tracer.Instance)
+        {
+        }
+
+        internal DatadogLoggingScope(Tracer tracer)
+        {
+            _tracer = tracer;
+            _service = tracer.DefaultServiceName ?? string.Empty;
+            _env = tracer.Settings.Environment ?? string.Empty;
+            _version = tracer.Settings.ServiceVersion ?? string.Empty;
+        }
+
+        public int Count => 5;
+
+        public KeyValuePair<string, object> this[int index]
+        {
+            get
+            {
+                return index switch
+                {
+                    0 => new KeyValuePair<string, object>("dd_service", _tracer.ActiveScope?.Span.ServiceName ?? _service),
+                    1 => new KeyValuePair<string, object>("dd_env", _env),
+                    2 => new KeyValuePair<string, object>("dd_version", _version),
+                    3 => new KeyValuePair<string, object>("dd_trace_id", _tracer.ActiveScope?.Span.TraceId),
+                    4 => new KeyValuePair<string, object>("dd_span_id", _tracer.ActiveScope?.Span.SpanId),
+                    _ => throw new ArgumentOutOfRangeException(nameof(index))
+                };
+            }
+        }
+
+        public override string ToString()
+        {
+            var span = _tracer.ActiveScope?.Span;
+
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "dd_service:{0}, dd_env:{1}, dd_version:{2}, dd_trace_id:{3}, dd_span_id:{4}",
+                span?.ServiceName ?? _service,
+                _env,
+                _version,
+                _tracer.ActiveScope?.Span.TraceId,
+                _tracer.ActiveScope?.Span.SpanId);
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            var span = _tracer.ActiveScope?.Span;
+            yield return new KeyValuePair<string, object>("dd_service", span?.ServiceName ?? _service);
+            yield return new KeyValuePair<string, object>("dd_env", _env);
+            yield return new KeyValuePair<string, object>("dd_version", _version);
+
+            if (span is not null)
+            {
+                yield return new KeyValuePair<string, object>("dd_trace_id", span.TraceId);
+                yield return new KeyValuePair<string, object>("dd_span_id", span.SpanId);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ILogger/LoggerFactoryScopeProviderForEachScopeIntegration.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="LoggerFactoryScopeProviderForEachScopeIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ILogger
+{
+    /// <summary>
+    /// LoggerFactoryScopeProvider.ForEach&lt;TState&gt; calltarget instrumentation
+    /// </summary>
+    [InstrumentMethod(
+        AssemblyName = "Microsoft.Extensions.Logging",
+        TypeName = "Microsoft.Extensions.Logging.LoggerFactoryScopeProvider",
+        MethodName = "ForEachScope",
+        ReturnTypeName = ClrNames.Void,
+        ParameterTypeNames = new[] { "System.Action`2[System.Object,!!0]", "!!0" },
+        MinimumVersion = "2.0.0",
+        MaximumVersion = "5.*.*",
+        IntegrationName = LoggerIntegrationCommon.IntegrationName)]
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class LoggerFactoryScopeProviderForEachScopeIntegration
+    {
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TAction">The type of the action</typeparam>
+        /// <typeparam name="TState">The type of the state</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="callback">The callback to be invoked per scope</param>
+        /// <param name="state">The state to pass to the callback</param>
+        /// <returns>Calltarget state value</returns>
+        public static CallTargetState OnMethodBegin<TTarget, TAction, TState>(TTarget instance, TAction callback, TState state)
+        {
+            LoggerIntegrationCommon.AddScope(Tracer.Instance, callback, state);
+            return new CallTargetState(scope: null, state: null);
+        }
+
+        /// <summary>
+        /// OnMethodEnd callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="exception">Exception instance in case the original code threw an exception.</param>
+        /// <param name="state">Calltarget state value</param>
+        /// <returns>A response value, in an async scenario will be T of Task of T</returns>
+        public static CallTargetReturn OnMethodEnd<TTarget>(TTarget instance, Exception exception, CallTargetState state)
+        {
+            return CallTargetReturn.GetDefault();
+        }
+    }
+}
