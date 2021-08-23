a14ca1e4fa0836834850355f275dc38959ea0900
Andrew Lock <andrew.lock@datadoghq.com>
2021-08-23 11:16:38 +0100

    Use numeric status code in snapshots + a shorter name
    numeric values are (mostly) shorter than the long names, and every test is called SubmitTraces, so don't need to include that in the filenames

--- test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebApi2Tests.cs
+++ test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetWebApi2Tests.cs
@@ -148,11 +148,12 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
             var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
 
-            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath, statusCode);
+            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath, (int)statusCode);
 
             // Overriding the type name here as we have multiple test classes in the file
             // Ensures that we get nice file nesting in Solution Explorer
             await Verifier.Verify(spans, settings)
+                          .UseMethodName("_")
                           .UseTypeName(_testName);
         }
     }
