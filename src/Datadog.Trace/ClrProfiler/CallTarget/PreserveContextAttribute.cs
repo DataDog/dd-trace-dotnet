54f51c183697dab494270b4ee22d68a46d18a447
Kevin Gosse <krix33@gmail.com>
2021-08-27 12:26:33 +0200

    Address PR feedback

--- src/Datadog.Trace/ClrProfiler/CallTarget/PreserveContextAttribute.cs
+++ src/Datadog.Trace/ClrProfiler/CallTarget/PreserveContextAttribute.cs
@@ -7,6 +7,11 @@ using System;
 
 namespace Datadog.Trace.ClrProfiler.CallTarget
 {
+    /// <summary>
+    /// Apply on a calltarget async callback to indicate that the method
+    /// should execute under the current synchronization context/task scheduler.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
     internal class PreserveContextAttribute : Attribute
     {
     }
