64f2f58bb03fab99a2ebb241bd731de4732271cb
Andrew Lock <andrew.lock@datadoghq.com>
2021-09-10 14:49:37 +0100

    Vendor Serilog.Sinks.PeriodicBatching
    Not strictly vendored, as make various changes to remove Serilog dependency and update API a little (as we don't need to support as many scenarios).

---
+++ tracer/src/Datadog.Trace/Logging/DirectSubmission/Sink/PeriodicBatching/PortableTimer.cs
@@ -0,0 +1,147 @@
+﻿// <copyright file="PortableTimer.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+// Based on https://github.com/serilog/serilog-sinks-periodicbatching/blob/66a74768196758200bff67077167cde3a7e346d5/src/Serilog.Sinks.PeriodicBatching/Sinks/PeriodicBatching/PortableTimer.cs
+// Copyright 2013-2020 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Datadog.Trace.Logging.DirectSubmission.Sink.PeriodicBatching
+{
+    internal class PortableTimer : IDisposable
+    {
+        private readonly IDatadogLogger _logger = DatadogLogging.GetLoggerFor<PortableTimer>();
+        private readonly object _stateLock = new();
+
+        private readonly Func<CancellationToken, Task> _onTick;
+        private readonly CancellationTokenSource _cancel = new();
+        private readonly Timer _timer;
+
+        private bool _running;
+        private bool _disposed;
+
+        public PortableTimer(Func<CancellationToken, Task> onTick)
+        {
+            _onTick = onTick ?? throw new ArgumentNullException(nameof(onTick));
+
+            var restoreFlow = false;
+            try
+            {
+                if (!ExecutionContext.IsFlowSuppressed())
+                {
+                    ExecutionContext.SuppressFlow();
+                    restoreFlow = true;
+                    _timer = new Timer(_ => OnTick(), null, Timeout.Infinite, Timeout.Infinite);
+                }
+            }
+            finally
+            {
+                if (restoreFlow)
+                {
+                    ExecutionContext.RestoreFlow();
+                }
+            }
+        }
+
+        public void Start(TimeSpan interval)
+        {
+            if (interval < TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(interval));
+            }
+
+            lock (_stateLock)
+            {
+                if (_disposed)
+                {
+                    throw new ObjectDisposedException(nameof(PortableTimer));
+                }
+
+                _timer.Change(interval, Timeout.InfiniteTimeSpan);
+            }
+        }
+
+        private async void OnTick()
+        {
+            try
+            {
+                lock (_stateLock)
+                {
+                    if (_disposed)
+                    {
+                        return;
+                    }
+
+                    // There's a little bit of raciness here, but it's needed to support the
+                    // current API, which allows the tick handler to reenter and set the next interval.
+
+                    if (_running)
+                    {
+                        Monitor.Wait(_stateLock);
+
+                        if (_disposed)
+                        {
+                            return;
+                        }
+                    }
+
+                    _running = true;
+                }
+
+                if (!_cancel.Token.IsCancellationRequested)
+                {
+                    await _onTick(_cancel.Token);
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                _logger.Debug(ex, "The timer was canceled during invocation: {0}");
+            }
+            finally
+            {
+                lock (_stateLock)
+                {
+                    _running = false;
+                    Monitor.PulseAll(_stateLock);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _cancel.Cancel();
+
+            lock (_stateLock)
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+
+                while (_running)
+                {
+                    Monitor.Wait(_stateLock);
+                }
+
+                _timer.Dispose();
+                _disposed = true;
+            }
+        }
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/Logging/DirectSubmission/Sink/PeriodicBatching/BatchingSinkTests.cs
@@ -0,0 +1,128 @@
+﻿// <copyright file="BatchingSinkTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+// Based on https://github.com/serilog/serilog-sinks-periodicbatching/blob/66a74768196758200bff67077167cde3a7e346d5/test/Serilog.Sinks.PeriodicBatching.Tests/PeriodicBatchingSinkTests.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Datadog.Trace.Logging.DirectSubmission.Sink.PeriodicBatching;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Logging.DirectSubmission.Sink.PeriodicBatching
+{
+    public class BatchingSinkTests
+    {
+        private static readonly int TinyWaitMs = 200;
+        private static readonly int MicroWaitMs = 1;
+
+        // Some very, very approximate tests here :)
+
+        [Fact]
+        public void WhenAnEventIsEnqueuedItIsWrittenToABatch_OnFlush()
+        {
+            var pbs = new InMemoryBatchedSink(TimeSpan.Zero, new BatchingSinkOptions(batchSizeLimit: 2, periodMs: TinyWaitMs));
+            var evt = "Some event";
+            pbs.EnqueueLog(evt);
+            pbs.Dispose();
+
+            pbs.Batches.Count.Should().Be(1);
+            pbs.Batches[0].Count.Should().Be(1);
+            pbs.Batches[0][0].Should().BeSameAs(evt);
+            pbs.IsDisposed.Should().BeTrue();
+            pbs.WasCalledAfterDisposal.Should().BeFalse();
+        }
+
+        [Fact]
+        public void WhenAnEventIsEnqueuedItIsWrittenToABatch_OnTimer()
+        {
+            var pbs = new InMemoryBatchedSink(TimeSpan.Zero, new BatchingSinkOptions(batchSizeLimit: 2, periodMs: TinyWaitMs));
+            var evt = "Some event";
+            pbs.EnqueueLog(evt);
+            Thread.Sleep(TinyWaitMs + TinyWaitMs);
+            pbs.Stop();
+            pbs.Dispose();
+
+            pbs.Batches.Count.Should().Be(1);
+            pbs.IsDisposed.Should().BeTrue();
+            pbs.WasCalledAfterDisposal.Should().BeFalse();
+        }
+
+        [Fact]
+        public void WhenAnEventIsEnqueuedItIsWrittenToABatch_FlushWhileRunning()
+        {
+            var batchEmitDelay = TimeSpan.FromMilliseconds(TinyWaitMs + TinyWaitMs);
+            var pbs = new InMemoryBatchedSink(batchEmitDelay, new BatchingSinkOptions(batchSizeLimit: 2, periodMs: MicroWaitMs));
+
+            var evt = "Some event";
+            pbs.EnqueueLog(evt);
+            Thread.Sleep(TinyWaitMs);
+            pbs.Dispose();
+
+            pbs.Batches.Count.Should().Be(1);
+            pbs.IsDisposed.Should().BeTrue();
+            pbs.WasCalledAfterDisposal.Should().BeFalse();
+        }
+
+        internal class InMemoryBatchedSink : BatchingSink, IDisposable
+        {
+            private readonly TimeSpan _batchEmitDelay;
+            private readonly object _stateLock = new();
+            private bool _stopped;
+
+            public InMemoryBatchedSink(TimeSpan batchEmitDelay, BatchingSinkOptions sinkOptions)
+                : base(sinkOptions)
+            {
+                _batchEmitDelay = batchEmitDelay;
+            }
+
+            // Postmortem only
+            public bool WasCalledAfterDisposal { get; private set; }
+
+            public IList<IList<string>> Batches { get; } = new List<IList<string>>();
+
+            public bool IsDisposed { get; private set; }
+
+            public void Stop()
+            {
+                lock (_stateLock)
+                {
+                    _stopped = true;
+                }
+            }
+
+            protected override Task EmitBatch(Queue<string> events)
+            {
+                lock (_stateLock)
+                {
+                    if (_stopped)
+                    {
+                        return Task.FromResult(0);
+                    }
+
+                    if (IsDisposed)
+                    {
+                        WasCalledAfterDisposal = true;
+                    }
+
+                    Thread.Sleep(_batchEmitDelay);
+                    Batches.Add(events.ToList());
+                }
+
+                return Task.FromResult(0);
+            }
+
+            protected override void AdditionalDispose()
+            {
+                lock (_stateLock)
+                {
+                    IsDisposed = true;
+                }
+            }
+        }
+    }
+}
