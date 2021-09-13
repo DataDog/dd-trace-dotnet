37d295dcd8258bd312ad39ad0aac7abe1ab8cb21
Zach Montoya <zach.montoya@datadoghq.com>
2021-09-13 10:16:06 -0700

    Add a custom Web API 2 exception handler that creates a new span whenever an exception is encountered. This affects all routes that throw an exception (/api/transient-failure and new ones) so update snapshots

--- tracer/test/test-applications/aspnet/Samples.AspNetMvc5/App_Start/WebApiConfig.cs
+++ tracer/test/test-applications/aspnet/Samples.AspNetMvc5/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@ using Samples.AspNetMvc5.Handlers;
 using System;
 using System.Configuration;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 
 namespace Samples.AspNetMvc5
 {
@@ -19,6 +20,13 @@ namespace Samples.AspNetMvc5
 
             config.MapHttpAttributeRoutes();
 
+            // Replace default exception handler
+            config.Services.Replace(typeof(IExceptionHandler), new CustomTracingExceptionHandler());
+
+            // Add global message handler
+            config.MessageHandlers.Add(new PassThroughQuerySuccessMessageHandler());
+
+            // Convention-based routing.
             config.Routes.MapHttpRoute(
                 name: "ApiConventions",
                 routeTemplate: "api2/{action}/{value}",
@@ -28,6 +36,7 @@ namespace Samples.AspNetMvc5
                     value = RouteParameter.Optional
                 });
 
+            // Add a new /handler-api base path that will be handled by a per-route message handler
             config.Routes.MapHttpRoute(
                 name: "Route2",
                 routeTemplate: "handler-api/{controller}/{id}",
@@ -38,8 +47,6 @@ namespace Samples.AspNetMvc5
                 constraints: null,
                 handler: new TerminatingQuerySuccessMessageHandler()  // per-route message handler
             );
-
-            config.MessageHandlers.Add(new PassThroughQuerySuccessMessageHandler());  // global message handler
         }
     }
 }
---
+++ tracer/test/test-applications/aspnet/Samples.AspNetMvc5/Handlers/CustomTracingExceptionHandler.cs
@@ -0,0 +1,21 @@
+using Datadog.Trace;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace Samples.AspNetMvc5.Handlers
+{
+    public class CustomTracingExceptionHandler : ExceptionHandler
+    {
+        public override async Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            using (var scope = Tracer.Instance.StartActive("CustomTracingExceptionHandler.handle-async"))
+            {
+                // Set span kind of span to server to pass through server span filtering
+                scope.Span.SetTag(Tags.SpanKind, SpanKinds.Server);
+
+                await base.HandleAsync(context, cancellationToken);
+            }
+        }
+    }
+}
