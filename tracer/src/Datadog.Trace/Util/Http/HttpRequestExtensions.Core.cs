eac18022d5224ecd92b039b95b05bf6801dfa102
Robert Pickering <robert.pickering@datadoghq.com>
2021-09-16 17:44:58 +0200

    Used the parsed version of the query string (#1777)
    Used the parsed version of the query string
    
    APPSEC-1404
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/src/Datadog.Trace/Util/Http/HttpRequestExtensions.Core.cs
+++ tracer/src/Datadog.Trace/Util/Http/HttpRequestExtensions.Core.cs
@@ -20,7 +20,7 @@ namespace Datadog.Trace.Util.Http
         internal static Dictionary<string, object> PrepareArgsForWaf(this HttpRequest request, RouteData routeDatas = null)
         {
             var url = GetUrl(request);
-            var headersDic = new Dictionary<string, string>();
+            var headersDic = new Dictionary<string, string>(request.Headers.Keys.Count);
             foreach (var k in request.Headers.Keys)
             {
                 if (!k.Equals("cookie", System.StringComparison.OrdinalIgnoreCase))
@@ -29,17 +29,23 @@ namespace Datadog.Trace.Util.Http
                 }
             }
 
-            var cookiesDic = new Dictionary<string, string>();
+            var cookiesDic = new Dictionary<string, string>(request.Cookies.Keys.Count);
             foreach (var k in request.Cookies.Keys)
             {
                 cookiesDic.Add(k, request.Cookies[k]);
             }
 
-            var dict = new Dictionary<string, object>()
+            var queryStringDic = new Dictionary<string, List<string>>(request.Query.Count);
+            foreach (var kvp in request.Query)
+            {
+                queryStringDic.Add(kvp.Key, kvp.Value.ToList());
+            }
+
+            var dict = new Dictionary<string, object>
             {
                 { AddressesConstants.RequestMethod, request.Method },
                 { AddressesConstants.RequestUriRaw, url },
-                { AddressesConstants.RequestQuery, request.QueryString.ToString() },
+                { AddressesConstants.RequestQuery, queryStringDic },
                 { AddressesConstants.RequestHeaderNoCookies, headersDic },
                 { AddressesConstants.RequestCookies, cookiesDic },
             };
