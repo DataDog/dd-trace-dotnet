fb6dc201654ae262436562941c9f85e498cf4de6
Tony Redondo <tony.redondo@datadoghq.com>
2021-09-18 00:30:20 +0200

    Add a Custom Sampler.

--- tracer/src/Datadog.Trace/Ci/CITracer.cs
+++ tracer/src/Datadog.Trace/Ci/CITracer.cs
@@ -3,15 +3,33 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System.Collections.Generic;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.Sampling;
 
 namespace Datadog.Trace.Ci
 {
     internal class CITracer : Tracer, ILockedTracer
     {
         public CITracer(TracerSettings settings)
-            : base(settings, agentWriter: null, sampler: null, scopeManager: null, statsd: null)
+            : base(settings, agentWriter: null, sampler: new CISampler(), scopeManager: null, statsd: null)
         {
+        }
+
+        private class CISampler : ISampler
+        {
+            public SamplingPriority GetSamplingPriority(Span span)
+            {
+                return SamplingPriority.UserKeep;
+            }
+
+            public void RegisterRule(ISamplingRule rule)
+            {
+            }
+
+            public void SetDefaultSampleRates(IEnumerable<KeyValuePair<string, float>> sampleRates)
+            {
+            }
         }
     }
 }
