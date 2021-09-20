665465f53cf2c9a77c6f9496c5e8e5d1eb19881d
Tony Redondo <tony.redondo@datadoghq.com>
2021-09-20 10:35:49 +0200

    Refactor

--- tracer/src/Datadog.Trace/Ci/CITracer.cs
+++ tracer/src/Datadog.Trace/Ci/CITracer.cs
@@ -3,9 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System.Collections.Generic;
 using Datadog.Trace.Configuration;
-using Datadog.Trace.Sampling;
 
 namespace Datadog.Trace.Ci
 {
@@ -14,22 +12,6 @@ namespace Datadog.Trace.Ci
         public CITracer(TracerSettings settings)
             : base(settings, agentWriter: null, sampler: new CISampler(), scopeManager: null, statsd: null)
         {
-        }
-
-        private class CISampler : ISampler
-        {
-            public SamplingPriority GetSamplingPriority(Span span)
-            {
-                return SamplingPriority.UserKeep;
-            }
-
-            public void RegisterRule(ISamplingRule rule)
-            {
-            }
-
-            public void SetDefaultSampleRates(IEnumerable<KeyValuePair<string, float>> sampleRates)
-            {
-            }
         }
     }
 }
