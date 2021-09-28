bab2428e64886dfc495cadf1bac3357bb859e8e2
Zach Montoya <zach.montoya@datadoghq.com>
2021-09-28 09:00:21 -0700

    Add CallSite integration for log4net injection

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/Log4Net/ILoggingEvent.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/Log4Net/ILoggingEvent.cs
@@ -3,12 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System;
 using System.Collections;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Log4Net
 {
