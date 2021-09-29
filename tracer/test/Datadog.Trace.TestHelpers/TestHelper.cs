4194ca6dde36fc5f259d1d8232912ab919c9e3bc
Andrew Lock <andrew.lock@datadoghq.com>
2021-09-29 08:28:03 +0100

    Add integration tests for Log shipping
    Added a MockLogsIntake to support verifying logs are shipped correctly, and have the expected properties
    As part of this, updated the existing logs injection tests to allow running them on Linux too

--- tracer/test/Datadog.Trace.TestHelpers/TestHelper.cs
+++ tracer/test/Datadog.Trace.TestHelpers/TestHelper.cs
@@ -14,6 +14,7 @@ using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Threading;
 using System.Threading.Tasks;
+using Datadog.Trace.Configuration;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -347,6 +348,14 @@ namespace Datadog.Trace.TestHelpers
         protected void SetAppSecBlockingEnabled(bool appSecBlockingEnabled)
         {
             SetEnvironmentVariable(Configuration.ConfigurationKeys.AppSecBlockingEnabled, appSecBlockingEnabled ? "true" : "false");
+        }
+
+        protected void EnableDirectLogSubmission(int intakePort, string integrationName, string host = "integration_tests")
+        {
+            SetEnvironmentVariable(ConfigurationKeys.DirectLogSubmission.Host, host);
+            SetEnvironmentVariable(ConfigurationKeys.DirectLogSubmission.Url, $"http://127.0.0.1:{intakePort}");
+            SetEnvironmentVariable(ConfigurationKeys.DirectLogSubmission.EnabledIntegrations, integrationName);
+            SetEnvironmentVariable(ConfigurationKeys.ApiKey, "DUMMY_KEY_REQUIRED_FOR_DIRECT_SUBMISSION");
         }
 
         protected async Task<IImmutableList<MockTracerAgent.Span>> GetWebServerSpans(
---
+++ tracer/test/Datadog.Trace.Tests/MockLogsIntakeTests.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="MockLogsIntakeTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Datadog.Trace.Logging.DirectSubmission;
+using Datadog.Trace.Logging.DirectSubmission.Formatting;
+using Datadog.Trace.TestHelpers;
+using Datadog.Trace.Tests.Logging.DirectSubmission;
+using Datadog.Trace.Tests.Logging.DirectSubmission.Sink;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests
+{
+    public class MockLogsIntakeTests
+    {
+        [Fact]
+        public void CanDeserializeJson()
+        {
+            var serialized = "[{\"@t\":\"2021-09-28T14:47:02.6486114Z\",\"@m\":\"[ExcludeMessage]Building pipeline\",\"@i\":\"c5676978\",\"ddsource\":\"csharp\",\"ddservice\":\"LogsInjection.ILogger\",\"host\":\"integration_ilogger_tests\"}]";
+            using var ms = new MemoryStream(Encoding.UTF8.GetBytes(serialized));
+
+            var logs = MockLogsIntake.Log.DeserializeFromStream(ms);
+
+            logs.Should().NotBeNull();
+            var log = logs.Should().ContainSingle().Subject;
+            log.Message.Should().Be("[ExcludeMessage]Building pipeline");
+            log.EventId.Should().Be("c5676978");
+            log.Source.Should().Be("csharp");
+            log.Service.Should().Be("LogsInjection.ILogger");
+            log.Host.Should().Be("integration_ilogger_tests");
+        }
+
+        [Fact]
+        public void CanDeserializeFormattedLogs()
+        {
+            var formatter = SettingsHelper.GetFormatter();
+            var log1 = GetLog(formatter, "This is a {Value}", DirectSubmissionLogLevel.Warning, new Dictionary<string, string> { { "Value", "SomeValue" } });
+            var log2 = GetLog(formatter, "This is another template with some other value", DirectSubmissionLogLevel.Information, new Dictionary<string, string> { { "Value", "SomeOtherValue" } });
+
+            var serialized = $"[{log1},{log2}]";
+            using var ms = new MemoryStream(Encoding.UTF8.GetBytes(serialized));
+
+            var logs = MockLogsIntake.Log.DeserializeFromStream(ms);
+
+            logs.Should().NotBeNull();
+            logs.Should().HaveCount(2);
+        }
+
+        private static string GetLog(
+            LogFormatter formatter,
+            string message,
+            DirectSubmissionLogLevel level,
+            Dictionary<string, string> properties)
+        {
+            var sb = new StringBuilder();
+
+            formatter.FormatLog(
+                sb,
+                sb, // not used here
+                DateTime.UtcNow,
+                message,
+                null,
+                level.GetName(),
+                exception: null,
+                (writer, _) =>
+                {
+                    foreach (var pair in properties)
+                    {
+                        writer.WritePropertyName(pair.Key);
+                        writer.WriteValue(pair.Value);
+                    }
+
+                    return new LogPropertyRenderingDetails(false, false, false, false, message);
+                });
+            return sb.ToString();
+        }
+    }
+}
