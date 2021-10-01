da0c9d23dda91ee8d5b41b5d754c889e20070a28
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-10-01 12:33:58 -0400

    Call into datadog via reflection

--- tracer/test/test-applications/azure-functions/Samples.AzureFunctions.Version3.OutOfProcess/TimerFunction.cs
+++ tracer/test/test-applications/azure-functions/Samples.AzureFunctions.Version3.OutOfProcess/TimerFunction.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 
@@ -19,7 +21,16 @@ namespace Samples.AzureFunctions.Version3.OutOfProcess
         public async Task TriggerAllTimer([TimerTrigger(SecondsInterval)] TimerInfo myTimer)
         {
             // log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            await CallFunctionHttp("simple");
+            var datadogTraceDll = Assembly.Load("Datadog.Trace");
+            var datadogTracer = datadogTraceDll.GetType("Datadog.Trace.Tracer");
+            var method = datadogTracer.GetMethods().Where(m => m.Name == "StartActive").Single();
+            var instanceProperty = datadogTracer.GetProperties().Where(p => p.Name == "Instance").Single();
+            var tracerInstanceGetter = instanceProperty.GetGetMethod();
+            var tracerInstance = tracerInstanceGetter.Invoke(null, new object[0]);
+            var parameters = new object[] { "uber-reflection", null, null, null, false, true };
+            using (var scope = (IDisposable)method.Invoke(tracerInstance, parameters)) {
+                await CallFunctionHttp("simple");
+            }
         }
 
         private async Task<string> CallFunctionHttp(string path)
