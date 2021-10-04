67874690359dca2e66f6929141c7c55c7ea8e85a
Tony Redondo <tony.redondo@datadoghq.com>
2021-10-04 18:03:30 +0200

    fix test

--- tracer/test/Datadog.Trace.Tests/Tagging/TagsListTests.cs
+++ tracer/test/Datadog.Trace.Tests/Tagging/TagsListTests.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Datadog.Trace.Agent.MessagePack;
-using Datadog.Trace.ClrProfiler.Integrations.AdoNet;
 using Datadog.Trace.Tagging;
 using Datadog.Trace.Util;
 using Datadog.Trace.Vendors.MessagePack;
