92780516ae6c64caa648cef25cfa8f54c81a2572
Tony Redondo <tony.redondo@datadoghq.com>
2021-10-05 20:59:23 +0200

    Add PInvoke Map rewriting in all platforms to support native library renaming (#1809)
    * Add PInvoke Map rewriting in all platforms to support native library renaming.
    
    * Fallback to the fixed name when we can't extract the path (Installed in the GAC).
    
    * Fix native filename per platform.
    
    * Adds the filename checker project. (Test is still missing for next commit)
    
    * Changes.
    
    * Fix GetCLRProfilerPath function.
    Adds the NativeProfilerChecks test.
    
    * Fix non Windows build
    
    * Revert unnecessary changes.
    
    * Add try/catch on the Checker path.
    Change GetCLRPRofilerPath to detect the runtime type.
    
    * try to fix tests.
    
    * Adds conditional for the check
    
    * setup the test project for linux.
    
    * commenting to do some checks.
    
    * Build passing. Uncomment, initialization block.
    
    * Add ToString to check and simplify catch scenario.
    
    * comments
    
    * change
    
    * change std::filepath with a normal rfind.
    
    * changing algorithm to avoid std::filesystem
    
    * Remove validation based on the process name, all samples are running through the dotnet.exe process

---
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/NativeProfilerChecks.cs
@@ -0,0 +1,35 @@
+// <copyright file="NativeProfilerChecks.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.IO;
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests
+{
+    public class NativeProfilerChecks : TestHelper
+    {
+        public NativeProfilerChecks(ITestOutputHelper output)
+            : base(new EnvironmentHelper("Datadog.Trace.ClrProfiler.Native.Checks", typeof(TestHelper), output, samplesDirectory: Path.Combine("test", "test-applications", "instrumentation"), prependSamplesToAppName: false), output)
+        {
+            SetServiceVersion("1.0.0");
+        }
+
+        [Fact]
+        public void RunChecksProject()
+        {
+            SetCallTargetSettings(enableCallTarget: true);
+            SetEnvironmentVariable("DD_TRACE_DEBUG", "1");
+            int agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (var processResult = RunSampleAndWaitForExit(agentPort))
+            {
+                Assert.True(processResult.ExitCode == 0, $"Process exited with code {processResult.ExitCode}");
+            }
+        }
+    }
+}
---
+++ tracer/test/test-applications/instrumentation/Datadog.Trace.ClrProfiler.Native.Checks/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Datadog.Trace.ClrProfiler.Native.Checks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
