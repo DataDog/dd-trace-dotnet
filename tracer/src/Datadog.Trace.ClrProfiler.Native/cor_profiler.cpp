50b35c578e2d17c3b2504ddcfabce038f0ee1eb8
Tony Redondo <tony.redondo@datadoghq.com>
2021-10-05 11:43:46 +0200

    change std::filepath with a normal rfind.

--- tracer/src/Datadog.Trace.ClrProfiler.Native/cor_profiler.cpp
+++ tracer/src/Datadog.Trace.ClrProfiler.Native/cor_profiler.cpp
@@ -307,20 +307,17 @@ HRESULT STDMETHODCALLTYPE CorProfiler::Initialize(IUnknown* cor_profiler_info_un
     is_attached_.store(true);
     profiler = this;
 
-#ifndef _WIN32
+#ifndef _WIN32_
     if (IsDebugEnabled())
     {
         try
         {
             // This enable a path to assert over checks inside the profiler code.
             // Only in Debug mode and for the Datadog.Trace.ClrProfiler.Native.Checks process
-            auto process_name_path = std::filesystem::path(ToString(process_name));
-            // process_name_path.replace_extension("");
-
-            // if (process_name_path.string() == "Datadog.Trace.ClrProfiler.Native.Checks")
-            // {
+            if (process_name.rfind(WStr("Datadog.Trace.ClrProfiler.Native.Checks")) != std::string::npos)
+            {
                 // CheckFilenameDefinitions();
-            // }
+            }
         }
         catch (...)
         {
