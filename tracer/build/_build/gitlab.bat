826aa07df1d5080f968d6a7e0af080a88a917f6a
Kevin Gosse <krix33@gmail.com>
2021-10-13 11:05:05 +0200

    Add sanity check for _OR_GREATER compiler directives (#1895)
    * Add sanity check for _OR_GREATER compiler directives
    * Propagate the error code in gitlabs if the build failed

--- tracer/build/_build/gitlab.bat
+++ tracer/build/_build/gitlab.bat
@@ -6,6 +6,9 @@ SET VSToolsPath=%VSTUDIO_ROOT%\MSBuild\Microsoft\Visual Studio\v16.0
 
 cd c:\mnt\
 dotnet run --project tracer/build/_build/_build.csproj -- Info Clean BuildTracerHome PackageTracerHome ZipSymbols SignDlls SignMsiAndNupkg --Artifacts "build-out\%CI_JOB_ID%"
+
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
 goto :EOF
 
 :nomntdir
