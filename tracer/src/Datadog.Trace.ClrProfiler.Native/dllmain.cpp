61f6d8da940a4954e6ba02c9d10c989b25e3f6f6
Zach Montoya <zach.montoya@datadoghq.com>
2021-10-15 17:39:53 -0700

    GUID updates: - Native Loader (new loading mechanism) now only responds to the original Tracer GUID {846F5F1C-F9AE-4B07-969E-05C26BC060D8} - Native Tracer (old loading mechanism) responds to both old Tracer GUID and new GUID {50DA5EED-F1ED-B00B-1055-5AFE55A1ADE5} which will be called by the new Native Loader - Reset the beta MSI to use the original Tracer GUID as the CLSID

--- tracer/src/Datadog.Trace.ClrProfiler.Native/dllmain.cpp
+++ tracer/src/Datadog.Trace.ClrProfiler.Native/dllmain.cpp
@@ -24,7 +24,10 @@ extern "C"
         // {846F5F1C-F9AE-4B07-969E-05C26BC060D8}
         const GUID CLSID_CorProfiler = {0x846f5f1c, 0xf9ae, 0x4b07, {0x96, 0x9e, 0x5, 0xc2, 0x6b, 0xc0, 0x60, 0xd8}};
 
-        if (ppv == NULL || rclsid != CLSID_CorProfiler)
+        // {50DA5EED-F1ED-B00B-1055-5AFE55A1ADE5}
+        const GUID CLSID_New_CorProfiler = {0x50da5eed, 0xf1ed, 0xb00b, {0x10, 0x55, 0x5a, 0xfe, 0x55, 0xa1, 0xad, 0xe5}};
+
+        if (ppv == NULL || (rclsid != CLSID_CorProfiler && rclsid != CLSID_New_CorProfiler))
         {
             return E_FAIL;
         }
