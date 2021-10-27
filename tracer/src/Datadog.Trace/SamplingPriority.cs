8c715156e5b658439e82cf7840720fad184f1762
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-10-27 12:46:47 -0400

    update SamplingPriority comments

--- tracer/src/Datadog.Trace/SamplingPriority.cs
+++ tracer/src/Datadog.Trace/SamplingPriority.cs
@@ -6,32 +6,44 @@
 namespace Datadog.Trace
 {
     /// <summary>
-    /// A traces sampling priority determines whether is should be kept and stored.
+    /// Sampling "priorities" indicate whether a trace should be kept (sampled) or dropped (not sampled).
+    /// Trace statistics are computed based on all traces, even if they are sampled out.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Currently, all traces are still sent to the Agent (for stats computation, etc),
+    /// but this will change in future versions of the tracer if the Agent supports it.
+    /// </para>
+    /// <para>
+    /// Despite the name, there is no relative priority between the different values.
+    /// All the "keep" and "reject" values have the same weight, they only indicate where
+    /// the decision originated from.
+    /// </para>
+    /// </remarks>
     public enum SamplingPriority
     {
         /// <summary>
-        /// Explicitly ask the backend to not store a trace.
+        /// Trace should be dropped (not sampled) due to a user request through code or configuration (e.g. the rules sampler).
         /// </summary>
         UserReject = -1,
 
         /// <summary>
-        /// Used by the built-in sampler to inform the backend that a trace should be rejected and not stored.
+        /// Trace should be dropped (not sampled) due according to the built-in sampler.
         /// </summary>
         AutoReject = 0,
 
         /// <summary>
-        /// Used by the built-in sampler to inform the backend that a trace should be kept and stored.
+        /// Trace should be kept (sampled) due according to the built-in sampler.
         /// </summary>
         AutoKeep = 1,
 
         /// <summary>
-        /// Explicitly ask the backend to keep a trace.
+        /// Trace should be kept (sampled) due to a user request through code or configuration (e.g. the rules sampler).
         /// </summary>
         UserKeep = 2,
 
         /// <summary>
-        /// Explicitly ask the backend to keep a trace because it was part of an application security event.
+        /// Trace should be kept (sampled) due to an application security event.
         /// </summary>
         AppSecKeep = 4,
     }
