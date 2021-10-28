a31b145a3d4dbb6c2a7725e1f57b2ada34396468
Andrew Lock <andrew.lock@datadoghq.com>
2021-10-28 10:41:39 +0100

    Add a "LatestMajors" option for testing all major package versions (#1939)
    It's an in-between step between TestAllMinorPackageVersions, and only testing the default samples, and is now the default for the multi-api package tests
    
    In many cases, the results are the same as using the default samples, but it should give more test coverage in PRs generally

--- tracer/build/_build/GeneratePackageVersions/XunitStrategyFileGenerator.cs
+++ tracer/build/_build/GeneratePackageVersions/XunitStrategyFileGenerator.cs
@@ -33,14 +33,19 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 {
     [SuppressMessage(""StyleCop.CSharp.LayoutRules"", ""SA1516:Elements must be separated by blank line"", Justification = ""This is an auto-generated file."")]
     public class PackageVersions
-    {";
+    {
+#if TEST_ALL_MINOR_PACKAGE_VERSIONS
+        public static readonly bool IsAllMinorPackageVersions = true;
+#else
+        public static readonly bool IsAllMinorPackageVersions = false;
+#endif";
 
         private const string FooterConst =
 @"    }
 }";
 
         private const string BodyFormat =
-@"{1}        public static IEnumerable<object[]> {0} => PackageVersionsLatestMinors.{0};{2}";
+@"{1}        public static IEnumerable<object[]> {0} => IsAllMinorPackageVersions ? PackageVersionsLatestMinors.{0} : PackageVersionsLatestMajors.{0};{2}";
 
         private const string EndIfDirectiveConst =
             @"
