c94df06b4430dfd5dbfe6fa53eca842f1b160185
Zach Montoya <zach.montoya@datadoghq.com>
2021-11-01 14:34:43 -0700

    Add AssemblyLoadContext to logs for netcoreapp assembly builds

--- tracer/src/Datadog.Trace/Logging/DatadogLogging.cs
+++ tracer/src/Datadog.Trace/Logging/DatadogLogging.cs
@@ -87,6 +87,9 @@ namespace Datadog.Trace.Logging
                     loggerConfiguration.Enrich.WithProperty("MachineName", DomainMetadata.MachineName);
                     loggerConfiguration.Enrich.WithProperty("Process", $"[{DomainMetadata.ProcessId} {DomainMetadata.ProcessName}]");
                     loggerConfiguration.Enrich.WithProperty("AppDomain", $"[{DomainMetadata.AppDomainId} {DomainMetadata.AppDomainName}]");
+#if NETCOREAPP
+                    loggerConfiguration.Enrich.WithProperty("AssemblyLoadContext", $"[{System.Runtime.Loader.AssemblyLoadContext.GetLoadContext(typeof(DatadogLogging).Assembly).Name}]");
+#endif
                     loggerConfiguration.Enrich.WithProperty("TracerVersion", TracerConstants.AssemblyVersion);
                 }
                 catch
