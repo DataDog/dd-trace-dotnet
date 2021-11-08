57ae6018651c1faadb7c1c2d4cd0274d7224911a
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-11-08 15:31:34 -0500

    Use sensible default service name in AAS (Functions & WebApps)

--- tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
@@ -53,6 +53,11 @@ namespace Datadog.Trace.Configuration
                           // backwards compatibility for names used in the past
                           source?.GetString("DD_SERVICE_NAME");
 
+            if (AzureAppServices.Metadata.IsRelevant)
+            {
+                ServiceName = ServiceName ?? AzureAppServices.Metadata.SiteName;
+            }
+
             ServiceVersion = source?.GetString(ConfigurationKeys.ServiceVersion);
 
             TraceEnabled = source?.GetBool(ConfigurationKeys.TraceEnabled) ??
