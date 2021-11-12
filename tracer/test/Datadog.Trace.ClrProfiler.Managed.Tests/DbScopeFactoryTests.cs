295376510f48e5d8b2a7ce1f964ae05dce7ff961
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-11-12 17:59:03 -0500

    fix tests

--- tracer/test/Datadog.Trace.ClrProfiler.Managed.Tests/DbScopeFactoryTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.Managed.Tests/DbScopeFactoryTests.cs
@@ -16,64 +16,47 @@ namespace Datadog.Trace.ClrProfiler.Managed.Tests
 {
     public class DbScopeFactoryTests
     {
-        public static IEnumerable<object[]> GetDbCommandsWithIntegrationNames()
-        {
-            yield return new object[] { new System.Data.SqlClient.SqlCommand(),    nameof(IntegrationIds.SqlClient) };
-            yield return new object[] { new Microsoft.Data.SqlClient.SqlCommand(), nameof(IntegrationIds.SqlClient) };
-            yield return new object[] { new MySql.Data.MySqlClient.MySqlCommand(), nameof(IntegrationIds.MySql)     };
-            yield return new object[] { new Npgsql.NpgsqlCommand(),                nameof(IntegrationIds.Npgsql)    };
-        }
-
-        public static IEnumerable<object[]> GetDbCommandsWithDbTypes()
-        {
-            yield return new object[] { new System.Data.SqlClient.SqlCommand(),    DbType.SqlServer  };
-            yield return new object[] { new Microsoft.Data.SqlClient.SqlCommand(), DbType.SqlServer  };
-            yield return new object[] { new MySql.Data.MySqlClient.MySqlCommand(), DbType.MySql      };
-            yield return new object[] { new Npgsql.NpgsqlCommand(),                DbType.PostgreSql };
-        }
-
         public static IEnumerable<object[]> GetDbCommands()
         {
-            yield return new object[] { new System.Data.SqlClient.SqlCommand() };
-            yield return new object[] { new Microsoft.Data.SqlClient.SqlCommand() };
-            yield return new object[] { new MySql.Data.MySqlClient.MySqlCommand() };
-            yield return new object[] { new Npgsql.NpgsqlCommand() };
+            yield return new object[] { new System.Data.SqlClient.SqlCommand(),    nameof(IntegrationIds.SqlClient), DbType.SqlServer  };
+            yield return new object[] { new Microsoft.Data.SqlClient.SqlCommand(), nameof(IntegrationIds.SqlClient), DbType.SqlServer  };
+            yield return new object[] { new MySql.Data.MySqlClient.MySqlCommand(), nameof(IntegrationIds.MySql),     DbType.MySql      };
+            yield return new object[] { new Npgsql.NpgsqlCommand(),                nameof(IntegrationIds.Npgsql),    DbType.PostgreSql };
         }
 
         [Theory]
-        [MemberData(nameof(GetDbCommandsWithIntegrationNames))]
-        public void CreateDbCommandScope_DoesNotReturnNullForEnabledIntegration(IDbCommand command, string integrationName)
+        [MemberData(nameof(GetDbCommands))]
+        public void CreateDbCommandScope_ReturnsScopeForEnabledIntegration(IDbCommand command, string integrationName, string dbType)
         {
-            // Set up tracer
-            var collection = new NameValueCollection { { $"DD_TRACE_{integrationName}_ENABLED", "true" } };
-            IConfigurationSource source = new NameValueConfigurationSource(collection);
-            var tracerSettings = new TracerSettings(source);
-            var tracer = TracerHelper.Create(tracerSettings);
+            var tracer = CreateTracerWithIntegrationEnabled(integrationName, enabled: true);
 
             // Create scope
             using var scope = CreateDbCommandScope(tracer, command);
             Assert.NotNull(scope);
+            Assert.Equal(dbType, scope.Span.GetTag(Tags.DbType));
         }
 
         [Theory]
-        [MemberData(nameof(GetDbCommandsWithIntegrationNames))]
-        public void CreateDbCommandScope_ReturnsNullForDisabledIntegration(IDbCommand command, string integrationName)
+        [MemberData(nameof(GetDbCommands))]
+        public void CreateDbCommandScope_ReturnNullForDisabledIntegration(IDbCommand command, string integrationName, string dbType)
         {
-            // Set up tracer
-            var collection = new NameValueCollection { { $"DD_TRACE_{integrationName}_ENABLED", "false" } };
-            IConfigurationSource source = new NameValueConfigurationSource(collection);
-            var tracerSettings = new TracerSettings(source);
-            var tracer = TracerHelper.Create(tracerSettings);
+            // HACK: avoid analyzer warning about not using arguments
+            _ = dbType;
 
-            // Create scope (or not)
+            var tracer = CreateTracerWithIntegrationEnabled(integrationName, enabled: false);
+
+            // Create scope
             using var scope = CreateDbCommandScope(tracer, command);
             Assert.Null(scope);
         }
 
         [Theory]
-        [MemberData(nameof(GetDbCommandsWithDbTypes))]
-        public void CreateDbCommandScope_UsesReplacementServiceNameWhenProvided(IDbCommand command, string dbType)
+        [MemberData(nameof(GetDbCommands))]
+        public void CreateDbCommandScope_UsesReplacementServiceNameWhenProvided(IDbCommand command, string integrationName, string dbType)
         {
+            // HACK: avoid analyzer warning about not using arguments
+            _ = integrationName;
+
             // Set up tracer
             var collection = new NameValueCollection { { ConfigurationKeys.ServiceNameMappings, $"{dbType}:my-custom-type" } };
             IConfigurationSource source = new NameValueConfigurationSource(collection);
@@ -81,14 +64,18 @@ namespace Datadog.Trace.ClrProfiler.Managed.Tests
             var tracer = TracerHelper.Create(tracerSettings);
 
             // Create scope
-            using var outerScope = CreateDbCommandScope(tracer, command);
-            Assert.Equal("my-custom-type", outerScope.Span.ServiceName);
+            using var scope = CreateDbCommandScope(tracer, command);
+            Assert.Equal("my-custom-type", scope.Span.ServiceName);
         }
 
         [Theory]
         [MemberData(nameof(GetDbCommands))]
-        public void CreateDbCommandScope_IgnoresReplacementServiceNameWhenNotProvided(IDbCommand command)
+        public void CreateDbCommandScope_IgnoresReplacementServiceNameWhenNotProvided(IDbCommand command, string integrationName, string dbType)
         {
+            // HACK: avoid analyzer warning about not using arguments
+            _ = dbType;
+            _ = integrationName;
+
             // Set up tracer
             var collection = new NameValueCollection { { ConfigurationKeys.ServiceNameMappings, "something:my-custom-type" } };
             IConfigurationSource source = new NameValueConfigurationSource(collection);
@@ -96,21 +83,21 @@ namespace Datadog.Trace.ClrProfiler.Managed.Tests
             var tracer = TracerHelper.Create(tracerSettings);
 
             // Create scope
-            using var outerScope = CreateDbCommandScope(tracer, command);
-            Assert.NotEqual("my-custom-type", outerScope.Span.ServiceName);
+            using var scope = CreateDbCommandScope(tracer, command);
+            Assert.NotEqual("my-custom-type", scope.Span.ServiceName);
         }
 
         [Theory]
-        [InlineData("System.Data.SqlClient.SqlCommand", true, IntegrationIds.SqlClient, DbType.SqlServer)]
-        [InlineData("Microsoft.Data.SqlClient.SqlCommand", true, IntegrationIds.SqlClient, DbType.SqlServer)]
-        [InlineData("Npgsql.NpgsqlCommand", true, IntegrationIds.Npgsql, DbType.PostgreSql)]
-        [InlineData("MySql.Data.MySqlClient.MySqlCommand", true, IntegrationIds.MySql, DbType.MySql)]
-        [InlineData("MySqlConnector.MySqlCommand", true, IntegrationIds.MySql, DbType.MySql)]
-        [InlineData("Oracle.ManagedDataAccess.Client.OracleCommand", true, IntegrationIds.Oracle, DbType.Oracle)]
-        [InlineData("Oracle.DataAccess.Client.OracleCommand", true, IntegrationIds.Oracle, DbType.Oracle)]
-        [InlineData("System.Data.SQLite.SQLiteCommand", true, IntegrationIds.Sqlite, DbType.Sqlite)]
-        [InlineData("Microsoft.Data.Sqlite.SqliteCommand", true, IntegrationIds.Sqlite, DbType.Sqlite)]
-        [InlineData("UnknownCommand", false, null, null)]
+        [InlineData("System.Data.SqlClient.SqlCommand",              true,  IntegrationIds.SqlClient, DbType.SqlServer)]
+        [InlineData("Microsoft.Data.SqlClient.SqlCommand",           true,  IntegrationIds.SqlClient, DbType.SqlServer)]
+        [InlineData("Npgsql.NpgsqlCommand",                          true,  IntegrationIds.Npgsql,    DbType.PostgreSql)]
+        [InlineData("MySql.Data.MySqlClient.MySqlCommand",           true,  IntegrationIds.MySql,     DbType.MySql)]
+        [InlineData("MySqlConnector.MySqlCommand",                   true,  IntegrationIds.MySql,     DbType.MySql)]
+        [InlineData("Oracle.ManagedDataAccess.Client.OracleCommand", true,  IntegrationIds.Oracle,    DbType.Oracle)]
+        [InlineData("Oracle.DataAccess.Client.OracleCommand",        true,  IntegrationIds.Oracle,    DbType.Oracle)]
+        [InlineData("System.Data.SQLite.SQLiteCommand",              true,  IntegrationIds.Sqlite,    DbType.Sqlite)]
+        [InlineData("Microsoft.Data.Sqlite.SqliteCommand",           true,  IntegrationIds.Sqlite,    DbType.Sqlite)]
+        [InlineData("UnknownCommand",                                false, null,                     null)]
         internal void TryGetIntegrationDetails_CorrectNameGenerated(string commandTypeFullName, bool expectedReturn, IntegrationIds? expectedIntegrationId, string expectedDbType)
         {
             bool actualReturn = DbScopeFactory.TryGetIntegrationDetails(commandTypeFullName, out var actualIntegrationId, out var actualDbType);
@@ -119,12 +106,22 @@ namespace Datadog.Trace.ClrProfiler.Managed.Tests
             Assert.Equal(expectedDbType, actualDbType);
         }
 
+        private static Tracer CreateTracerWithIntegrationEnabled(string integrationName, bool enabled)
+        {
+            // Set up tracer
+            var tracerSettings = new TracerSettings();
+            tracerSettings.Integrations[integrationName].Enabled = enabled;
+            return TracerHelper.Create(tracerSettings);
+        }
+
         private static Scope CreateDbCommandScope(Tracer tracer, IDbCommand command)
         {
-            return (Scope)typeof(DbScopeFactory<>)
-                         .MakeGenericType(command.GetType())
-                         .GetMethod(nameof(DbScopeFactory<object>.CreateDbCommandScope))
-                        ?.Invoke(null, new object[] { tracer, command });
+            var methodName = nameof(DbScopeFactory.Cache<object>.CreateDbCommandScope);
+            var arguments = new object[] { tracer, command };
+
+            return (Scope)typeof(DbScopeFactory.Cache<>).MakeGenericType(command.GetType())
+                                                        .GetMethod(methodName)
+                                                       ?.Invoke(null, arguments);
         }
     }
 }
