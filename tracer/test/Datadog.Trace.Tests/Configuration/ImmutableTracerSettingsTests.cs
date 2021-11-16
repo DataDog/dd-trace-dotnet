2273156c0219d12073ab3854cae65419ffe756a0
Andrew Lock <andrew.lock@datadoghq.com>
2021-11-16 13:23:06 +0000

    Add unit test to confirm ImmutableTracerSettings contains all (most) of the properties on TracerSettings

--- tracer/test/Datadog.Trace.Tests/Configuration/ImmutableTracerSettingsTests.cs
+++ tracer/test/Datadog.Trace.Tests/Configuration/ImmutableTracerSettingsTests.cs
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System.Linq;
 using System.Reflection;
 using Datadog.Trace.Configuration;
 using FluentAssertions;
@@ -13,26 +14,48 @@ namespace Datadog.Trace.Tests.Configuration
 {
     public class ImmutableTracerSettingsTests
     {
+        private const BindingFlags Flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
+
+        // These properties are present on TracerSettings, but not on ImmutableTracerSettings
+        private static readonly string[] ExcludedProperties =
+        {
+            nameof(TracerSettings.DisabledIntegrationNames),
+            nameof(TracerSettings.DiagnosticSourceEnabled),
+        };
+
         [Fact]
         public void OnlyHasReadOnlyProperties()
         {
             var type = typeof(ImmutableTracerSettings);
 
-            var flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
-
             using var scope = new AssertionScope();
 
-            var properties = type.GetProperties(flags);
+            var properties = type.GetProperties(Flags);
             foreach (var propertyInfo in properties)
             {
                 propertyInfo.CanWrite.Should().BeFalse($"{propertyInfo.Name} should be read only");
             }
 
-            var fields = type.GetFields(flags);
+            var fields = type.GetFields(Flags);
             foreach (var field in fields)
             {
                 field.IsInitOnly.Should().BeTrue($"{field.Name} should be read only");
             }
+        }
+
+        [Fact]
+        public void HasSamePropertiesAsTracerSettings()
+        {
+            var mutableProperties = typeof(TracerSettings)
+                                   .GetProperties(Flags)
+                                   .Select(x => x.Name)
+                                   .Where(x => !ExcludedProperties.Contains(x));
+
+            var immutableProperties = typeof(ImmutableTracerSettings)
+                                     .GetProperties(Flags)
+                                     .Select(x => x.Name);
+
+            immutableProperties.Should().Contain(mutableProperties);
         }
     }
 }
