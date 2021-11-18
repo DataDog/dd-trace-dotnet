4153faab84949d48bcec08c96e9d237dad63f508
Andrew Lock <andrew.lock@datadoghq.com>
2021-11-18 10:38:53 +0000

    Don't throw in AgentWriterBenchmark (#2057)
    Currently we're throwing every time we flush in the `AgentWriterBenchmark`. That's obviously very expensive, variable, and makes for difficult comparisons.

--- tracer/test/benchmarks/Benchmarks.Trace/AgentWriterBenchmark.cs
+++ tracer/test/benchmarks/Benchmarks.Trace/AgentWriterBenchmark.cs
@@ -120,10 +120,7 @@ namespace Benchmarks.Trace
 
             public long ContentLength => 0;
 
-            public string GetHeader(string headerName)
-            {
-                throw new NotImplementedException();
-            }
+            public string GetHeader(string headerName) => string.Empty;
 
             public Task<string> ReadAsStringAsync()
             {
