0a06d987dacc98de68b8ce3074d5ef1486ed439b
Andrew Lock <andrew.lock@datadoghq.com>
2021-11-19 14:41:06 +0000

    Update analyzer package versions
    Upgrade projects to fix warnings

--- tracer/src/Datadog.Trace.Tools.Analyzers/ThreadAbortAnalyzer/Resources.cs
+++ tracer/src/Datadog.Trace.Tools.Analyzers/ThreadAbortAnalyzer/Resources.cs
@@ -8,7 +8,7 @@ namespace Datadog.Trace.Tools.Analyzers.ThreadAbortAnalyzer
     internal static class Resources
     {
         public const string Title = "Potential infinite loop on ThreadAbortException";
-        public const string Description = "While blocks are vulnerable to infinite loop on ThreadAbortException due to a bug in the runtime. The catch block should rethrow a ThreadAbortException, or use a finally block";
+        public const string Description = "While blocks are vulnerable to infinite loop on ThreadAbortException due to a bug in the runtime. The catch block should rethrow a ThreadAbortException, or use a finally block.";
         public const string MessageFormat = "Potential infinite loop - you should rethrow Exception in catch block";
         public const string CodeFixTitle = "Rethrow exception";
     }
