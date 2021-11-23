34c613d46c89b0a51e96618cd833084d08ddf2e6
Zach Montoya <zach.montoya@datadoghq.com>
2021-11-23 12:26:53 -0800

    [AppSec] Waf fixes: reading register for waf and unit tests for alpine (#2060) (#2082)
    * Remove musl suffix in TryLoadLibrary too
    * Don't read memory if no reg key could be read
    * typo fix
    * Remove logging info
    * simplify filter switch
    
    Co-authored-by: Anna <anna.yafi@datadoghq.com>

--- tracer/src/Datadog.Trace/AppSec/Waf/NativeBindings/WafNative.cs
+++ tracer/src/Datadog.Trace/AppSec/Waf/NativeBindings/WafNative.cs
@@ -488,25 +488,6 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
             return success;
         }
 
-        private static bool IsMuslBasedLinux()
-        {
-            var muslDistros = new[] { "alpine" };
-
-            var files =
-                Directory.GetFiles("/etc", "*release")
-                    .Concat(Directory.GetFiles("/etc", "*version"))
-                    .ToList();
-
-            if (File.Exists("/etc/issue"))
-            {
-                files.Add("/etc/issue");
-            }
-
-            return files
-                .Select(File.ReadAllText)
-                .Any(fileContents => muslDistros.Any(distroId => fileContents.ToLowerInvariant().Contains(distroId)));
-        }
-
         private static void GetLibNameAndRuntimeId(FrameworkDescription frameworkDescription, out string libName, out string runtimeId)
         {
             string runtimeIdPart1, libPrefix, libExt;
@@ -519,10 +500,7 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
                     libExt = "dylib";
                     break;
                 case OSPlatform.Linux:
-                    runtimeIdPart1 =
-                        IsMuslBasedLinux() ?
-                            "linux-musl" :
-                            "linux";
+                    runtimeIdPart1 = "linux";
                     libPrefix = "lib";
                     libExt = "so";
                     break;
