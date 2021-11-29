f22fa59242efc71f0e4a0b03b64f3c71cb6ea75b
Kevin Gosse <krix33@gmail.com>
2021-11-29 18:37:44 +0100

    Add unit tests

---
+++ tracer/test/Datadog.Trace.Tests/DistributedTracer/DistributedTracerTests.cs
@@ -0,0 +1,73 @@
+// <copyright file="DistributedTracerTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using Datadog.Trace.ClrProfiler;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Datadog.Trace.Tests.DistributedTracer
+{
+    [CollectionDefinition(nameof(DistributedTracerTests), DisableParallelization = true)]
+    [Collection(nameof(DistributedTracerTests))]
+    [DistributedTracerRestorer]
+    public class DistributedTracerTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void LockSamplingPriority(bool notifyDistributedTracer)
+        {
+            using var scope = Tracer.Instance.StartActive("Test");
+
+            var distributedTracer = new Mock<IDistributedTracer>();
+
+            ClrProfiler.DistributedTracer.SetInstanceOnlyForTests(distributedTracer.Object);
+
+            scope.Span.Context.TraceContext.LockSamplingPriority(notifyDistributedTracer);
+
+            Func<Times> expectedInvocations = notifyDistributedTracer ? Times.Once : Times.Never;
+
+            distributedTracer.Verify(t => t.LockSamplingPriority(), expectedInvocations);
+        }
+
+        [Fact]
+        public void GetSpanContext()
+        {
+            using var parentScope = Tracer.Instance.StartActive("Parent");
+
+            var distributedTracer = new Mock<IDistributedTracer>();
+
+            var spanContext = new SpanContext(1, 2, SamplingPriority.UserKeep);
+
+            distributedTracer.Setup(t => t.GetSpanContext()).Returns(spanContext);
+
+            ClrProfiler.DistributedTracer.SetInstanceOnlyForTests(distributedTracer.Object);
+
+            using var scope = Tracer.Instance.StartActive("Test");
+
+            distributedTracer.Verify(t => t.GetSpanContext(), Times.Once);
+            scope.Span.TraceId.Should().Be(spanContext.TraceId);
+            scope.Span.Context.TraceContext.SamplingPriority.Should().Be(spanContext.SamplingPriority);
+        }
+
+        [Fact]
+        public void SetSpanContext()
+        {
+            var distributedTracer = new Mock<IDistributedTracer>();
+
+            ClrProfiler.DistributedTracer.SetInstanceOnlyForTests(distributedTracer.Object);
+
+            using (var scope = Tracer.Instance.StartActive("Test"))
+            {
+                distributedTracer.Verify(t => t.SetSpanContext(scope.Span.Context), Times.Once);
+            }
+
+            distributedTracer.Verify(t => t.SetSpanContext(null), Times.Once);
+            distributedTracer.Verify(t => t.SetSpanContext(It.IsAny<SpanContext>()), Times.Exactly(2));
+        }
+    }
+}
