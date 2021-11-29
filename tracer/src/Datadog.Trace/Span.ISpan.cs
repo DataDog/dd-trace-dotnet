7c9b04370148b3a529f5304b43d5b15d6ef8366d
Zach Montoya <zach.montoya@datadoghq.com>
2021-11-29 14:59:54 -0800

    Apply suggestions from code review
    Apply inheritdoc in Span.ISpan.cs
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/src/Datadog.Trace/Span.ISpan.cs
+++ tracer/src/Datadog.Trace/Span.ISpan.cs
@@ -15,103 +15,63 @@ namespace Datadog.Trace
     /// </summary>
     public partial class Span : ISpan
     {
-        /// <summary>
-        /// Gets or sets operation name
-        /// </summary>
+        /// <inheritdoc />
         string ISpan.OperationName
         {
             get => OperationName;
             set => OperationName = value;
         }
 
-        /// <summary>
-        /// Gets or sets the resource name
-        /// </summary>
+        /// <inheritdoc />
         string ISpan.ResourceName
         {
             get => ResourceName;
             set => ResourceName = value;
         }
 
-        /// <summary>
-        /// Gets or sets the type of request this span represents (ex: web, db).
-        /// Not to be confused with span kind.
-        /// </summary>
-        /// <seealso cref="SpanTypes"/>
-        /// <summary>
-        /// Gets or sets the resource name
-        /// </summary>
+        /// <inheritdoc />
         string ISpan.Type
         {
             get => Type;
             set => Type = value;
         }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether this span represents an error
-        /// </summary>
+        /// <inheritdoc />
         bool ISpan.Error
         {
             get => Error;
             set => Error = value;
         }
 
-        /// <summary>
-        /// Gets or sets the service name.
-        /// </summary>
+        /// <inheritdoc />
         string ISpan.ServiceName
         {
             get => ServiceName;
             set => ServiceName = value;
         }
 
-        /// <summary>
-        /// Gets the trace's unique identifier.
-        /// </summary>
+        /// <inheritdoc />
         ulong ISpan.TraceId => TraceId;
 
-        /// <summary>
-        /// Gets the span's unique identifier.
-        /// </summary>
+        /// <inheritdoc />
         ulong ISpan.SpanId => SpanId;
 
-        /// <summary>
-        /// Gets the span's span context
-        /// </summary>
+        /// <inheritdoc />
         ISpanContext ISpan.Context => Context;
 
-        /// <summary>
-        /// Add a the specified tag to this span.
-        /// </summary>
-        /// <param name="key">The tag's key.</param>
-        /// <param name="value">The tag's value.</param>
-        /// <returns>This span to allow method chaining.</returns>
+        /// <inheritdoc />
         ISpan ISpan.SetTag(string key, string value) => SetTag(key, value);
 
-        /// <summary>
-        /// Record the end time of the span and flushes it to the backend.
-        /// After the span has been finished all modifications will be ignored.
-        /// </summary>
+        /// <inheritdoc />
         void ISpan.Finish() => Finish();
 
-        /// <summary>
-        /// Explicitly set the end time of the span and flushes it to the backend.
-        /// After the span has been finished all modifications will be ignored.
-        /// </summary>
-        /// <param name="finishTimestamp">Explicit value for the end time of the Span</param>
+        /// <inheritdoc />
         void ISpan.Finish(DateTimeOffset finishTimestamp) => Finish(finishTimestamp);
 
-        /// <summary>
-        /// Add the StackTrace and other exception metadata to the span
-        /// </summary>
-        /// <param name="exception">The exception.</param>
+        /// <inheritdoc />
         void ISpan.SetException(Exception exception) => SetException(exception);
 
-        /// <summary>
-        /// Gets the value (or default/null if the key is not a valid tag) of a tag with the key value passed
-        /// </summary>
-        /// <param name="key">The tag's key</param>
-        /// <returns> The value for the tag with the key specified, or null if the tag does not exist</returns>
+        /// <inheritdoc />
         string ISpan.GetTag(string key) => GetTag(key);
     }
 }
