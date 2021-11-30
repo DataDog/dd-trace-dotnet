9d284a955628579ca4d6c05a68c032d9af06270c
Andrew Lock <andrew.lock@datadoghq.com>
2021-11-30 15:33:45 +0000

    Add try-catch to LoggerFactoryIntegrationCommon helper
    In case there are any issues loading the assemblies etc

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/ILogger/DirectSubmission/LoggerFactoryIntegrationCommon.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/ILogger/DirectSubmission/LoggerFactoryIntegrationCommon.cs
@@ -22,48 +22,59 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger.DirectSu
 
         static LoggerFactoryIntegrationCommon()
         {
-            // The ILoggerProvider type is in a different assembly to the LoggerFactory, so go via the ILogger type
-            // returned by CreateLogger
-            var loggerFactoryType = typeof(TLoggerFactory);
-            var abstractionsAssembly = loggerFactoryType.GetMethod("CreateLogger")?.ReturnType.Assembly;
-            var iLoggerProviderType = abstractionsAssembly.GetType("Microsoft.Extensions.Logging.ILoggerProvider");
-            var iSupportExternalScopeType = abstractionsAssembly.GetType("Microsoft.Extensions.Logging.ISupportExternalScope");
-
-            if (iSupportExternalScopeType is null)
+            try
             {
-                // ISupportExternalScope is only available in v2.1+
-                // We can just duck type ILoggerProvider directly
-                ProviderInterfaces = iLoggerProviderType;
+                // The ILoggerProvider type is in a different assembly to the LoggerFactory, so go via the ILogger type
+                // returned by CreateLogger
+                var loggerFactoryType = typeof(TLoggerFactory);
+                var abstractionsAssembly = loggerFactoryType.GetMethod("CreateLogger").ReturnType.Assembly;
+                var iLoggerProviderType = abstractionsAssembly.GetType("Microsoft.Extensions.Logging.ILoggerProvider");
+                var iSupportExternalScopeType = abstractionsAssembly.GetType("Microsoft.Extensions.Logging.ISupportExternalScope");
+
+                if (iSupportExternalScopeType is null)
+                {
+                    // ISupportExternalScope is only available in v2.1+
+                    // We can just duck type ILoggerProvider directly
+                    ProviderInterfaces = iLoggerProviderType;
+                }
+
+                // We need to implement both ILoggerProvider and ISupportExternalScope
+                // because LoggerFactory uses pattern matching to check if we implement the latter
+                // Duck Typing can currently only implement a single interface, so emit
+                // a new interface that implements both ILoggerProvider and ISupportExternalScope
+                // and duck cast to that
+                var thisAssembly = typeof(DirectSubmissionLoggerProvider).Assembly;
+                var assemblyName = new AssemblyName("DirectLogSubmissionILoggerFactoryAssembly") { Version = thisAssembly.GetName().Version };
+
+                var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+                var moduleBuilder = (ModuleBuilder)assemblyBuilder.DefineDynamicModule("MainModule");
+
+                var typeBuilder = moduleBuilder.DefineType(
+                    "DirectSubmissionLoggerProviderProxy",
+                    TypeAttributes.Interface | TypeAttributes.Public | TypeAttributes.Abstract,
+                    parent: null,
+                    interfaces: new[] { iLoggerProviderType, iSupportExternalScopeType });
+
+                ProviderInterfaces = typeBuilder.CreateTypeInfo().AsType();
             }
-
-            // We need to implement both ILoggerProvider and ISupportExternalScope
-            // because LoggerFactory uses pattern matching to check if we implement the latter
-            // Duck Typing can currently only implement a single interface, so emit
-            // a new interface that implements both ILoggerProvider and ISupportExternalScope
-            // and duck cast to that
-            var thisAssembly = typeof(DirectSubmissionLoggerProvider).Assembly;
-            var assemblyName = new AssemblyName("DirectLogSubmissionILoggerFactoryAssembly")
+            catch (Exception ex)
             {
-                Version = thisAssembly.GetName().Version
-            };
-
-            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
-            var moduleBuilder = (ModuleBuilder)assemblyBuilder.DefineDynamicModule("MainModule");
-
-            var typeBuilder = moduleBuilder.DefineType(
-                "DirectSubmissionLoggerProviderProxy",
-                TypeAttributes.Interface | TypeAttributes.Public | TypeAttributes.Abstract,
-                parent: null,
-                interfaces: new[] { iLoggerProviderType, iSupportExternalScopeType });
-
-            ProviderInterfaces = typeBuilder.CreateTypeInfo().AsType();
+                Log.Warning(ex, "Error loading logger factory types for " + typeof(TLoggerFactory));
+                ProviderInterfaces = null;
+            }
         }
 
-        internal static object AddDirectSubmissionLoggerProvider(TLoggerFactory loggerFactory)
+        internal static void AddDirectSubmissionLoggerProvider(TLoggerFactory loggerFactory)
         {
+            if (ProviderInterfaces is null)
+            {
+                // there was a problem loading the assembly for some reason
+                return;
+            }
+
             var provider = new DirectSubmissionLoggerProvider();
 
-            return AddDirectSubmissionLoggerProvider(loggerFactory, provider);
+            AddDirectSubmissionLoggerProvider(loggerFactory, provider);
         }
 
         // Internal for testing
