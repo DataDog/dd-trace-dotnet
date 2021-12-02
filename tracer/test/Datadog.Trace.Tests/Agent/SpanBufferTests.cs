e0af7b0058e5eca613f5b83773a78c89fbb66d0f
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-12-02 11:52:32 -0500

    replace 3 implementation of mock spans with a new MockSpan

--- tracer/test/Datadog.Trace.Tests/Agent/SpanBufferTests.cs
+++ tracer/test/Datadog.Trace.Tests/Agent/SpanBufferTests.cs
@@ -139,45 +139,5 @@ namespace Datadog.Trace.Tests.Agent
         {
             Assert.Throws<ArgumentException>(() => new SpanBuffer(4, SpanFormatterResolver.Instance));
         }
-
-        [MessagePackObject]
-        public struct FakeSpan
-        {
-            [Key("trace_id")]
-            public ulong TraceId { get; set; }
-
-            [Key("span_id")]
-            public ulong SpanId { get; set; }
-
-            [Key("name")]
-            public string Name { get; set; }
-
-            [Key("resource")]
-            public string Resource { get; set; }
-
-            [Key("service")]
-            public string Service { get; set; }
-
-            [Key("type")]
-            public string Type { get; set; }
-
-            [Key("start")]
-            public long Start { get; set; }
-
-            [Key("duration")]
-            public long Duration { get; set; }
-
-            [Key("parent_id")]
-            public ulong? ParentId { get; set; }
-
-            [Key("error")]
-            public byte Error { get; set; }
-
-            [Key("meta")]
-            public Dictionary<string, string> Tags { get; set; }
-
-            [Key("metrics")]
-            public Dictionary<string, double> Metrics { get; set; }
-        }
     }
 }
--- tracer/test/Datadog.Trace.Tests/Tagging/TagsListTests.cs
+++ tracer/test/Datadog.Trace.Tests/Tagging/TagsListTests.cs
@@ -221,45 +221,5 @@ namespace Datadog.Trace.Tests.Tagging
                                .BeTrue($"Property {propertyAndTag.property.Name} of type {type.Name} is not mapped");
             }
         }
-
-        [global::MessagePack.MessagePackObject]
-        public struct FakeSpan
-        {
-            [global::MessagePack.Key("trace_id")]
-            public ulong TraceId { get; set; }
-
-            [global::MessagePack.Key("span_id")]
-            public ulong SpanId { get; set; }
-
-            [global::MessagePack.Key("name")]
-            public string Name { get; set; }
-
-            [global::MessagePack.Key("resource")]
-            public string Resource { get; set; }
-
-            [global::MessagePack.Key("service")]
-            public string Service { get; set; }
-
-            [global::MessagePack.Key("type")]
-            public string Type { get; set; }
-
-            [global::MessagePack.Key("start")]
-            public long Start { get; set; }
-
-            [global::MessagePack.Key("duration")]
-            public long Duration { get; set; }
-
-            [global::MessagePack.Key("parent_id")]
-            public ulong? ParentId { get; set; }
-
-            [global::MessagePack.Key("error")]
-            public byte Error { get; set; }
-
-            [global::MessagePack.Key("meta")]
-            public Dictionary<string, string> Tags { get; set; }
-
-            [global::MessagePack.Key("metrics")]
-            public Dictionary<string, double> Metrics { get; set; }
-        }
     }
 }
