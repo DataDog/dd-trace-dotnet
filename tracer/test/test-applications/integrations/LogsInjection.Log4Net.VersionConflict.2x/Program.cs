1af8ba168d59d73265b1db7c1f19613f41793f33
Zach Montoya <zach.montoya@datadoghq.com>
2021-12-07 15:37:54 -0800

    Add a LogsInjection.Log4Net.VersionConflict.2x sample and test it in the integration tests

---
+++ tracer/test/test-applications/integrations/LogsInjection.Log4Net.VersionConflict.2x/Program.cs
@@ -0,0 +1,112 @@
+using System;
+using System.IO;
+using Datadog.Trace;
+using log4net;
+using log4net.Config;
+using Samples;
+
+namespace LogsInjection.Log4Net.VersionConflict_2x
+{
+    public class Program
+    {
+        /// <summary>
+        /// Prepend a string to log lines that should not be validated for logs injection.
+        /// In other words, they're not written within a Datadog scope
+        /// </summary>
+        private static readonly string ExcludeMessagePrefix = "[ExcludeMessage]";
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        public static int Main(string[] args)
+        {
+            var appDirectory = Directory.GetParent(typeof(Program).Assembly.Location).FullName;
+            var textFilePath = Path.Combine(appDirectory, "log-textFile.log");
+            var jsonFilePath = Path.Combine(appDirectory, "log-jsonFile.log");
+
+            File.Delete(textFilePath);
+            File.Delete(jsonFilePath);
+
+            // Initialize log4net
+            var logRepository = LogManager.GetRepository(typeof(Program).Assembly);
+            XmlConfigurator.Configure(logRepository, new FileInfo(Path.Combine(appDirectory, "log4net.205.config")));
+
+            try
+            {
+                RunLoggingProcedure(log);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return (int)ExitCode.UnknownError;
+            }
+
+#if NETCOREAPP2_1
+            // Add a delay to avoid a race condition on shutdown: https://github.com/dotnet/coreclr/pull/22712
+            // This would cause a segmentation fault on .net core 2.x
+            System.Threading.Thread.Sleep(5000);
+#endif
+
+            return (int)ExitCode.Success;
+        }
+
+        private static void RunLoggingProcedure(ILog log)
+        {
+            log.Info($"{ExcludeMessagePrefix}Starting manual1 Datadog scope.");
+            using (Tracer.Instance.StartActive("manual1"))
+            {
+                log.Info($"Trace: manual1");
+                using (TracerUtils.StartAutomaticTrace("automatic2"))
+                {
+                    log.Info($"Trace: manual1-automatic2");
+                    using (Tracer.Instance.StartActive("manual3"))
+                    {
+                        log.Info($"Trace: manual1-automatic2-manual3");
+                        using (Tracer.Instance.StartActive("manual4"))
+                        {
+                            log.Info($"Trace: manual1-automatic2-manual3-manual4");
+
+                            using (TracerUtils.StartAutomaticTrace("automatic5"))
+                            {
+                                log.Info($"Trace: manual1-automatic2-manual3-manual4-automatic5");
+                            }
+
+                            log.Info($"Trace: manual1-automatic2-manual3-manual4");
+                        }
+
+                        log.Info($"Trace: manual1-automatic2-manual3");
+
+                        using (TracerUtils.StartAutomaticTrace("automatic4"))
+                        {
+                            log.Info($"Trace: manual1-automatic2-manual3-automatic4");
+                            using (TracerUtils.StartAutomaticTrace("automatic5"))
+                            {
+                                log.Info($"Trace: manual1-automatic2-manual3-automatic4-automatic5");
+                                using (Tracer.Instance.StartActive("manual6"))
+                                {
+                                    log.Info($"Trace: manual1-automatic2-manual3-automatic4-automatic5-manual6");
+                                }
+
+                                log.Info($"Trace: manual1-automatic2-manual3-automatic4-automatic5");
+                            }
+
+                            log.Info($"Trace: manual1-automatic2-manual3-automatic4");
+                        }
+
+                        log.Info($"Trace: manual1-automatic2-manual3");
+                    }
+                    log.Info($"Trace: manual1-automatic2");
+                }
+
+                log.Info($"Trace: manual1");
+            }
+
+            log.Info($"{ExcludeMessagePrefix}Exited manual1 Datadog scope.");
+        }
+
+        enum ExitCode : int
+        {
+            Success = 0,
+            UnknownError = -10
+        }
+    }
+}
