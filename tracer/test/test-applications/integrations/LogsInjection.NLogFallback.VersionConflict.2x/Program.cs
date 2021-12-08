3782c5145803ec2d84efcad4e0e35693fdf605d5
Zach Montoya <zach.montoya@datadoghq.com>
2021-12-08 15:24:26 -0800

    Add LogsInjection.NLogFallback.VersionConflict.2x sample application. This fails right now, which I expected...must fix

---
+++ tracer/test/test-applications/integrations/LogsInjection.NLogFallback.VersionConflict.2x/Program.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using LogsInjectionHelper.VersionConflict;
+using NLog;
+using NLog.Config;
+
+namespace LogsInjection.NLogFallback.VersionConflict_2x
+{
+    public class Program
+    {
+        /// <summary>
+        /// Prepend a string to log lines that should not be validated for logs injection.
+        /// In other words, they're not written within a Datadog scope
+        /// </summary>
+        private static readonly string ExcludeMessagePrefix = "[ExcludeMessage]";
+
+        public static int Main(string[] args)
+        {
+            var appDirectory = Directory.GetParent(typeof(Program).Assembly.Location).FullName;
+            var textFilePath = Path.Combine(appDirectory, "log-textFile.log");
+            var jsonFilePath = Path.Combine(appDirectory, "log-jsonFile.log");
+
+            File.Delete(textFilePath);
+            File.Delete(jsonFilePath);
+
+            // Initialize NLog
+            LogManager.Configuration = new XmlLoggingConfiguration(Path.Combine(appDirectory, "NLog.40.config"));
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            return LoggingMethods.RunLoggingProcedure(logger.Info);
+        }
+    }
+}
