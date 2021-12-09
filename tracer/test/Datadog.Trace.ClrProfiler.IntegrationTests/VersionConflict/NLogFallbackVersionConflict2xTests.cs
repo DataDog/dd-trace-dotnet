40b1d61a0faef631b0c4f6cb30a08861aae6ae15
Zach Montoya <zach.montoya@datadoghq.com>
2021-12-08 21:03:42 -0800

    Only build the NLogFallback application on .NET Framework

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/VersionConflict/NLogFallbackVersionConflict2xTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/VersionConflict/NLogFallbackVersionConflict2xTests.cs
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#if !NETCOREAPP2_1
+#if NETFRAMEWORK
 using Datadog.Trace.TestHelpers;
 using Xunit;
 using Xunit.Abstractions;
