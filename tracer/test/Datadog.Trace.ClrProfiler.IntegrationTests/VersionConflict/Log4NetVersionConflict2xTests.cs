d3ec83a865ca74957548698d202ef081a13af202
Zach Montoya <zach.montoya@datadoghq.com>
2021-12-10 13:33:35 -0800

    Add a LogsInjection.Log4Net.VersionConflict.2x sample and test it in the integration tests

---
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/VersionConflict/Log4NetVersionConflict2xTests.cs
@@ -0,0 +1,58 @@
+// <copyright file="Log4NetVersionConflict2xTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if !NETCOREAPP2_1
+using Datadog.Trace.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests.VersionConflict
+{
+    public class Log4NetVersionConflict2xTests : LogsInjectionTestBase
+    {
+        private readonly LogFileTest[] _nlog205LogFileTests =
+            {
+                new LogFileTest()
+                {
+                    FileName = "log-textFile.log",
+                    RegexFormat = @"{0}: {1}",
+                    UnTracedLogTypes = UnTracedLogTypes.EmptyProperties,
+                    PropertiesUseSerilogNaming = false
+                },
+                new LogFileTest()
+                {
+                    FileName = "log-jsonFile.log",
+                    RegexFormat = @"""{0}"":{1}",
+                    UnTracedLogTypes = UnTracedLogTypes.EnvServiceTracingPropertiesOnly,
+                    PropertiesUseSerilogNaming = false
+                }
+            };
+
+        public Log4NetVersionConflict2xTests(ITestOutputHelper output)
+            : base(output, "LogsInjection.Log4Net.VersionConflict.2x")
+        {
+            SetServiceVersion("1.0.0");
+        }
+
+        [SkippableFact]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public void InjectsLogsWhenEnabled()
+        {
+            SetEnvironmentVariable("DD_LOGS_INJECTION", "true");
+
+            int agentPort = TcpPortProvider.GetOpenPort();
+            using (var agent = new MockTracerAgent(agentPort))
+            using (RunSampleAndWaitForExit(agent.Port))
+            {
+                var spans = agent.WaitForSpans(1, 2500);
+                Assert.True(spans.Count >= 1, $"Expecting at least 1 span, only received {spans.Count}");
+
+                ValidateLogCorrelation(spans, _nlog205LogFileTests);
+            }
+        }
+    }
+}
+#endif
