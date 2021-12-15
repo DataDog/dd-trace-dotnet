f58c5c793a12a2e5b028adf830000e5c94964f65
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-12-15 09:18:23 -0500

    Fix api snapshot

--- tracer/src/Datadog.Trace/Configuration/TracesTransportType.cs
+++ tracer/src/Datadog.Trace/Configuration/TracesTransportType.cs
@@ -1,4 +1,4 @@
-// <copyright file="TraceTransportType.cs" company="Datadog">
+// <copyright file="TracesTransportType.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
