b88bb6dcca31a2f01d1aab82a52aae26f9bf72e6
Zach Montoya <zach.montoya@datadoghq.com>
2021-12-15 12:49:17 -0800

    Fix path issue with NLog in NLog version conflict test

--- tracer/test/test-applications/integrations/LogsInjection.NLog.VersionConflict.2x/Program.cs
+++ tracer/test/test-applications/integrations/LogsInjection.NLog.VersionConflict.2x/Program.cs
@@ -1,7 +1,9 @@
 using System.IO;
+using System.Runtime.InteropServices;
 using LogsInjectionHelper.VersionConflict;
 using NLog;
 using NLog.Config;
+using NLog.Targets;
 
 namespace LogsInjection.NLog.VersionConflict_2x
 {
@@ -18,8 +20,26 @@ namespace LogsInjection.NLog.VersionConflict_2x
 
             // Initialize NLog
             LogManager.Configuration = new XmlLoggingConfiguration(Path.Combine(appDirectory, "NLog.46.config"));
-            Logger logger = LogManager.GetCurrentClassLogger();
+#if NETCOREAPP
+            // Hacks for the fact the NLog on Linux just can't do anything right
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var target = (FileTarget)LogManager.Configuration.FindTargetByName("textFile");
+                if (target is not null)
+                {
+                    target.FileName = Path.Combine(appDirectory, "log-textFile.log");
+                }
 
+                target = (FileTarget)LogManager.Configuration.FindTargetByName("jsonFile");
+                if (target is not null)
+                {
+                    target.FileName = Path.Combine(appDirectory, "log-jsonFile.log");
+                }
+                LogManager.ReconfigExistingLoggers();
+            }
+#endif
+
+            Logger logger = LogManager.GetCurrentClassLogger();
             return LoggingMethods.RunLoggingProcedure(logger.Info);
         }
     }
