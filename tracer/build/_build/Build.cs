0d073ab20fca9a89d9b701c02bc0e3ac3d430b0f
pierotibou <pierotibou@users.noreply.github.com>
2021-12-20 13:22:02 +0000

    [Version Bump] 2.0.1

--- tracer/build/_build/Build.cs
+++ tracer/build/_build/Build.cs
@@ -65,10 +65,10 @@ partial class Build : NukeBuild
     readonly bool IsAlpine = false;
 
     [Parameter("The build version. Default is latest")]
-    readonly string Version = "2.0.0";
+    readonly string Version = "2.0.1";
 
     [Parameter("Whether the build version is a prerelease(for packaging purposes). Default is latest")]
-    readonly bool IsPrerelease = true;
+    readonly bool IsPrerelease = false;
 
     [Parameter("Prints the available drive space before executing each target. Defaults to false")]
     readonly bool PrintDriveSpace = false;
