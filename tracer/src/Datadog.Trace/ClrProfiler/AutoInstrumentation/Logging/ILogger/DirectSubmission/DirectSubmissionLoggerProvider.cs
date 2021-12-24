1077ec2b737b2b9a2cdcc330582126bff0067952
Andrew Lock <andrew.lock@datadoghq.com>
2021-12-24 12:19:03 +0000

    Add ILogger Direct submission implementation
    This is slightly tricky because:
    * ILoggerFactory uses a different "root" constructor in .net 5+
    * We can't reverse duck type the ILoggerProvider directly, because we need to implement two interfaces, so have to `DefineType` an interface that implements both, and then duck type that.

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/ILogger/DirectSubmission/DirectSubmissionLoggerProvider.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="DirectSubmissionLoggerProvider.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using Datadog.Trace.DuckTyping;
+using Datadog.Trace.Logging.DirectSubmission;
+using Datadog.Trace.Logging.DirectSubmission.Formatting;
+using Datadog.Trace.Logging.DirectSubmission.Sink;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.ILogger.DirectSubmission
+{
+    /// <summary>
+    /// Duck type for ILoggerProvider
+    /// </summary>
+    internal class DirectSubmissionLoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, DirectSubmissionLogger> _loggers = new();
+        private readonly IDatadogSink _sink;
+        private readonly LogFormatter? _formatter;
+        private readonly DirectSubmissionLogLevel _minimumLogLevel;
+        private IExternalScopeProvider? _scopeProvider;
+
+        internal DirectSubmissionLoggerProvider(IDatadogSink sink, DirectSubmissionLogLevel minimumLogLevel)
+            : this(sink, formatter: null, minimumLogLevel)
+        {
+        }
+
+        // used for testing
+        internal DirectSubmissionLoggerProvider(
+            IDatadogSink sink,
+            LogFormatter? formatter,
+            DirectSubmissionLogLevel minimumLogLevel)
+        {
+            _sink = sink;
+            _formatter = formatter;
+            _minimumLogLevel = minimumLogLevel;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ILogger"/> instance.
+        /// </summary>
+        /// <param name="categoryName">The category name for messages produced by the logger.</param>
+        /// <returns>The instance of <see cref="ILogger"/> that was created.</returns>
+        [DuckReverseMethod]
+        public DirectSubmissionLogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, CreateLoggerImplementation);
+        }
+
+        private DirectSubmissionLogger CreateLoggerImplementation(string name)
+        {
+            return new DirectSubmissionLogger(name, _scopeProvider, _sink, _formatter, _minimumLogLevel);
+        }
+
+        /// <inheritdoc cref="IDisposable.Dispose"/>
+        [DuckReverseMethod]
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Method for ISupportExternalScope
+        /// </summary>
+        /// <param name="scopeProvider">The provider of scope data</param>
+        [DuckReverseMethod(ParameterTypeNames = new[] { "Microsoft.Extensions.Logging.IExternalScopeProvider, Microsoft.Extensions.Logging.Abstractions" })]
+        public void SetScopeProvider(IExternalScopeProvider scopeProvider)
+        {
+            _scopeProvider = scopeProvider;
+        }
+    }
+}
--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ILoggerTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ILoggerTests.cs
@@ -1,10 +1,16 @@
-// <copyright file="ILoggerTests.cs" company="Datadog">
+﻿// <copyright file="ILoggerTests.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
+using System.Collections.Generic;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.ExtensionMethods;
+using Datadog.Trace.Logging.DirectSubmission;
 using Datadog.Trace.TestHelpers;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -31,10 +37,12 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             SetEnvironmentVariable("DD_LOGS_INJECTION", "true");
         }
 
-        [SkippableFact]
+        [SkippableTheory]
+        [InlineData(false)]
+        [InlineData(true)]
         [Trait("Category", "EndToEnd")]
         [Trait("RunOnWindows", "True")]
-        public void InjectsLogs()
+        public void InjectsLogs(bool enableLogShipping)
         {
             // One of the traces starts by manual opening a span when the background service starts,
             // and then it sends a HTTP request to the server.
@@ -51,6 +59,12 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             var expectedCorrelatedSpanCount = 4;
 #endif
 
+            using var logsIntake = new MockLogsIntake();
+            if (enableLogShipping)
+            {
+                EnableDirectLogSubmission(logsIntake.Port, nameof(IntegrationId.ILogger), nameof(InjectsLogs));
+            }
+
             using (var agent = EnvironmentHelper.GetMockAgent())
             using (RunSampleAndWaitForExit(agent.Port, aspNetCorePort: 0))
             {
@@ -59,6 +73,37 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
                 ValidateLogCorrelation(spans, _logFiles, expectedCorrelatedTraceCount, expectedCorrelatedSpanCount);
             }
+        }
+
+        [SkippableFact]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public void DirectlyShipsLogs()
+        {
+            var hostName = "integration_ilogger_tests";
+            using var logsIntake = new MockLogsIntake();
+
+            EnableDirectLogSubmission(logsIntake.Port, nameof(IntegrationId.ILogger), hostName);
+
+            var agentPort = TcpPortProvider.GetOpenPort();
+            using var agent = new MockTracerAgent(agentPort);
+            using var processResult = RunSampleAndWaitForExit(agent.Port, aspNetCorePort: 0);
+
+            Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode} and exception: {processResult.StandardError}");
+
+            var logs = logsIntake.Logs;
+
+            using var scope = new AssertionScope();
+            logs.Should().NotBeNull();
+            logs.Should().HaveCountGreaterOrEqualTo(12); // have an unknown number of "Waiting for app started handling requests"
+            logs.Should()
+                .OnlyContain(x => x.Service == "LogsInjection.ILogger")
+                .And.OnlyContain(x => x.Host == hostName)
+                .And.OnlyContain(x => x.Source == "csharp")
+                .And.OnlyContain(x => x.Env == "integration_tests")
+                .And.OnlyContain(x => x.Version == "1.0.0")
+                .And.OnlyContain(x => x.Exception == null)
+                .And.OnlyContain(x => x.LogLevel == DirectSubmissionLogLevel.Information);
         }
     }
 }
