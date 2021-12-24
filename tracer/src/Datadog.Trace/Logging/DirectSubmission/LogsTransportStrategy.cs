94e7619151c2258a4a6adefce7a6a14b5f9eceec
Andrew Lock <andrew.lock@datadoghq.com>
2021-12-24 11:53:57 +0000

    Add sink implementation
    The sink is responsible for queueing logs and sending batches using the ILogsApi (analogous the Agent.IApi type.
    
    The batching code is single threaded, so we can reuse the same string builder throughout. Ideally this would all be serializing directly to utf8 bytes, which we _could_ consider ifdef-ing for .netcoreapp3.1.

---
+++ tracer/src/Datadog.Trace/Logging/DirectSubmission/LogsTransportStrategy.cs
@@ -0,0 +1,27 @@
+// <copyright file="LogsTransportStrategy.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using Datadog.Trace.Agent;
+using Datadog.Trace.Agent.Transports;
+
+namespace Datadog.Trace.Logging.DirectSubmission
+{
+    internal static class LogsTransportStrategy
+    {
+        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<Tracer>();
+
+        public static IApiRequestFactory Get(ImmutableDirectLogSubmissionSettings settings)
+        {
+#if NETCOREAPP
+            Log.Information("Using {FactoryType} for log submission transport.", nameof(HttpClientRequestFactory));
+            return new HttpClientRequestFactory();
+#else
+            Log.Information("Using {FactoryType} for log submission transport.", nameof(ApiWebRequestFactory));
+            return new ApiWebRequestFactory();
+#endif
+        }
+    }
+}
