6d13a79bf1b3ed2a0102c6deb8677bb438169f16
Andrew Lock <andrew.lock@datadoghq.com>
2021-12-24 10:58:20 +0000

    Add base batching API, based on Serilog.Sinks.PeriodicBatching
    This is a heavily modified version of Serilog.Sinks.PeriodicBatching. It removes various scenarios we don't need to support, and replaces Serilog's BatchedConnectionStatus with an alternative CircuitBreaker implementation. Retries etc will be handled inside derived implementations instead.

---
+++ tracer/src/Datadog.Trace/Logging/DirectSubmission/Sink/PeriodicBatching/BatchingSink.cs
@@ -0,0 +1,223 @@
+﻿// <copyright file="BatchingSink.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Datadog.Trace.Logging.DirectSubmission.Sink.PeriodicBatching
+{
+    internal abstract class BatchingSink : IDisposable
+    {
+        internal const int FailuresBeforeCircuitBreak = 10;
+
+        private readonly IDatadogLogger _log = DatadogLogging.GetLoggerFor<BatchingSink>();
+        private readonly int _batchSizeLimit;
+        private readonly TimeSpan _flushPeriod;
+        private readonly TimeSpan _circuitBreakPeriod;
+        private readonly BoundedConcurrentQueue<DatadogLogEvent> _queue;
+        private readonly Queue<DatadogLogEvent> _waitingBatch = new();
+        private readonly CircuitBreaker _circuitBreaker;
+        private readonly Task _flushTask;
+        private readonly Action? _disableSinkAction;
+        private readonly TaskCompletionSource<bool> _processExit = new();
+        private volatile bool _enqueueLogEnabled = true;
+
+        protected BatchingSink(BatchingSinkOptions sinkOptions, Action? disableSinkAction)
+        {
+            if (sinkOptions == null)
+            {
+                throw new ArgumentNullException(nameof(sinkOptions));
+            }
+
+            if (sinkOptions.BatchSizeLimit <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sinkOptions), "The batch size limit must be greater than zero.");
+            }
+
+            if (sinkOptions.Period <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sinkOptions), "The period must be greater than zero.");
+            }
+
+            _disableSinkAction = disableSinkAction;
+
+            _batchSizeLimit = sinkOptions.BatchSizeLimit;
+            _flushPeriod = sinkOptions.Period;
+            _circuitBreakPeriod = sinkOptions.CircuitBreakPeriod;
+
+            _queue = new BoundedConcurrentQueue<DatadogLogEvent>(sinkOptions.QueueLimit);
+            _circuitBreaker = new CircuitBreaker(FailuresBeforeCircuitBreak);
+
+            _flushTask = Task.Run(FlushBuffersTaskLoopAsync);
+            _flushTask.ContinueWith(
+                t =>
+                {
+                    _log.Error(t.Exception, "Error in flush task");
+                    _disableSinkAction?.Invoke();
+                },
+                TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink. If the sink is being disposed or
+        /// the app domain unloaded, then the event is ignored.
+        /// </summary>
+        /// <param name="logEvent">Log event to emit.</param>
+        /// <exception cref="ArgumentNullException">The event is null.</exception>
+        public void EnqueueLog(DatadogLogEvent logEvent)
+        {
+            if (logEvent == null)
+            {
+                throw new ArgumentNullException(nameof(logEvent));
+            }
+
+            if (_processExit.Task.IsCompleted || !_enqueueLogEnabled)
+            {
+                return;
+            }
+
+            _queue.TryEnqueue(logEvent);
+        }
+
+        public virtual void Dispose()
+        {
+            Dispose(finalFlush: true);
+        }
+
+        public void Dispose(bool finalFlush)
+        {
+            _processExit.TrySetResult(finalFlush);
+            _flushTask.GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Emit a batch of log events, running to completion synchronously.
+        /// </summary>
+        /// <param name="events">The events to emit.</param>
+        /// <returns><c>true</c> if the batch was emitted successfully. <c>false</c> if there was an error</returns>
+        protected abstract Task<bool> EmitBatch(Queue<DatadogLogEvent> events);
+
+        private async Task FlushBuffersTaskLoopAsync()
+        {
+            while (true)
+            {
+                if (_processExit.Task.IsCompleted)
+                {
+                    _log.Debug("Terminating Log submission loop");
+                    if (_processExit.Task.Result)
+                    {
+                        var maxShutDownDelay = Task.Delay(20_000);
+                        var finalFlushTask = FlushLogs();
+                        var completed = await Task.WhenAny(finalFlushTask, maxShutDownDelay).ConfigureAwait(false);
+
+                        if (completed != finalFlushTask)
+                        {
+                            _log.Warning("Could not finish flushing all logs before process end");
+                        }
+                    }
+
+                    return;
+                }
+
+                var circuitStatus = await FlushLogs().ConfigureAwait(false);
+
+                await HandleCircuitStatus(circuitStatus).ConfigureAwait(false);
+            }
+        }
+
+        private async Task<CircuitStatus> FlushLogs()
+        {
+            try
+            {
+                var status = CircuitStatus.Closed;
+                var haveMultipleBatchesToSend = false;
+                do
+                {
+                    while (_waitingBatch.Count < _batchSizeLimit &&
+                           _queue.TryDequeue(out var next))
+                    {
+                        _waitingBatch.Enqueue(next);
+                    }
+
+                    if (_waitingBatch.Count == 0)
+                    {
+                        // If the first batch was full, then use that status.
+                        // If this is the first batch in this loop, then there were no logs to send
+                        // So can't say anything about the status of the API.
+                        return haveMultipleBatchesToSend ? status : _circuitBreaker.MarkSkipped();
+                    }
+
+                    var success = await EmitBatch(_waitingBatch).ConfigureAwait(false);
+                    if (success)
+                    {
+                        status = _circuitBreaker.MarkSuccess();
+                        haveMultipleBatchesToSend = _waitingBatch.Count >= _batchSizeLimit;
+                        if (haveMultipleBatchesToSend)
+                        {
+                            _waitingBatch.Clear();
+                        }
+                    }
+                    else
+                    {
+                        return _circuitBreaker.MarkFailure();
+                    }
+                }
+                while (haveMultipleBatchesToSend);
+
+                return status;
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex, "Exception while emitting periodic batch");
+                return _circuitBreaker.MarkFailure();
+            }
+            finally
+            {
+                _waitingBatch.Clear();
+            }
+        }
+
+        private Task HandleCircuitStatus(CircuitStatus status)
+        {
+            var delayTillNextEmit = _flushPeriod;
+            switch (status)
+            {
+                case CircuitStatus.PermanentlyBroken:
+                    _processExit.TrySetResult(false);
+                    _enqueueLogEnabled = false;
+                    _disableSinkAction?.Invoke();
+
+                    // clear the queue
+                    while (_queue.TryDequeue(out _)) { }
+
+                    return _processExit.Task;
+
+                case CircuitStatus.Broken:
+                    // circuit breaker is broken, so stop queuing more logs
+                    // for now but don't disable log shipping entirely
+                    _enqueueLogEnabled = false;
+                    // Wait a while before trying again
+                    delayTillNextEmit = _circuitBreakPeriod;
+                    break;
+
+                case CircuitStatus.HalfBroken:
+                    // circuit breaker is tentatively open. Start queuing logs again
+                    _enqueueLogEnabled = true;
+                    break;
+
+                case CircuitStatus.Closed:
+                default:
+                    _enqueueLogEnabled = true;
+                    break;
+            }
+
+            return Task.WhenAny(
+                Task.Delay(delayTillNextEmit),
+                _processExit.Task);
+        }
+    }
+}
---
+++ tracer/src/Datadog.Trace/Logging/DirectSubmission/Sink/PeriodicBatching/BoundedConcurrentQueue.cs
@@ -0,0 +1,100 @@
+﻿// <copyright file="BoundedConcurrentQueue.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+// Based on https://github.com/serilog/serilog-sinks-periodicbatching/blob/66a74768196758200bff67077167cde3a7e346d5/src/Serilog.Sinks.PeriodicBatching/Sinks/PeriodicBatching/BoundedConcurrentQueue.cs
+// Copyright 2013-2020 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#nullable enable
+
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+namespace Datadog.Trace.Logging.DirectSubmission.Sink.PeriodicBatching
+{
+    internal class BoundedConcurrentQueue<T>
+    {
+        private const int Unbounded = -1;
+
+        private readonly ConcurrentQueue<T> _queue = new();
+        private readonly int _queueLimit;
+
+        private int _counter;
+
+        public BoundedConcurrentQueue(int? queueLimit = null)
+        {
+            if (queueLimit is <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(queueLimit), "Queue limit must be positive, or `null` to indicate unbounded.");
+            }
+
+            _queueLimit = queueLimit ?? Unbounded;
+        }
+
+        public int Count => _queue.Count;
+
+        public bool TryDequeue([NotNullWhen(returnValue: true)] out T? item)
+        {
+            if (_queueLimit == Unbounded)
+            {
+                return _queue.TryDequeue(out item!);
+            }
+
+            var result = false;
+            try
+            { }
+            finally
+            {
+                // prevent state corrupt while aborting
+                if (_queue.TryDequeue(out item!))
+                {
+                    Interlocked.Decrement(ref _counter);
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public bool TryEnqueue(T item)
+        {
+            if (_queueLimit == Unbounded)
+            {
+                _queue.Enqueue(item);
+                return true;
+            }
+
+            var result = true;
+            try
+            { }
+            finally
+            {
+                if (Interlocked.Increment(ref _counter) <= _queueLimit)
+                {
+                    _queue.Enqueue(item);
+                }
+                else
+                {
+                    Interlocked.Decrement(ref _counter);
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
