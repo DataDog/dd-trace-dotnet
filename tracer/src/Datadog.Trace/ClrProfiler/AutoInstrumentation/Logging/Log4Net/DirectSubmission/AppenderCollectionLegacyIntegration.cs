337330a6226e2c06a922f3ab12c578abbe44c5a1
Andrew Lock <andrew.lock@datadoghq.com>
2021-12-24 12:21:13 +0000

    Add log4net direct submission implementation
    Instruments the AppenderCollection.ToArray() method to sneak our own appender in every time a logger is requested

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Logging/Log4Net/DirectSubmission/AppenderCollectionLegacyIntegration.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="AppenderCollectionLegacyIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Logging;
+using Datadog.Trace.Logging.DirectSubmission;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Logging.Log4Net.DirectSubmission
+{
+    /// <summary>
+    /// AppenderCollection.ToArray() calltarget instrumentation
+    /// </summary>
+    [InstrumentMethod(
+        AssemblyName = "log4net",
+        TypeName = "log4net.Appender.AppenderCollection",
+        MethodName = "ToArray",
+        ReturnTypeName = "log4net.Appender.IAppender[]",
+        ParameterTypeNames = new string[0],
+        MinimumVersion = "1.0.0",
+        MaximumVersion = "1.*.*",
+        IntegrationName = nameof(IntegrationId.Log4Net))]
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class AppenderCollectionLegacyIntegration
+    {
+        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<AppenderCollectionLegacyIntegration>();
+        private static bool _logWritten = false;
+
+        /// <summary>
+        /// OnMethodEnd callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TResponse">The type of the response</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="response">The returned ILoggerWrapper </param>
+        /// <param name="exception">Exception instance in case the original code threw an exception.</param>
+        /// <param name="state">Calltarget state value</param>
+        /// <returns>A default CallTargetReturn to satisfy the CallTarget contract</returns>
+        public static CallTargetReturn<TResponse> OnMethodEnd<TTarget, TResponse>(TTarget instance, TResponse response, Exception exception, CallTargetState state)
+        {
+            if (!TracerManager.Instance.DirectLogSubmission.Settings.IsIntegrationEnabled(IntegrationId.Log4Net))
+            {
+                return new CallTargetReturn<TResponse>(response);
+            }
+
+            var updatedResponse = Log4NetCommon<TResponse>.AddAppenderToResponse(response, DirectSubmissionLog4NetLegacyAppender.Instance);
+            if (!_logWritten)
+            {
+                _logWritten = true;
+                Log.Information("Direct log submission via Log4Net enabled");
+            }
+
+            return new CallTargetReturn<TResponse>(updatedResponse);
+        }
+    }
+}
