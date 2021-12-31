fc95454a2e6c212582eef246425d5af7208a960c
Andrew Lock <andrew.lock@datadoghq.com>
2021-12-31 14:18:15 +0000

    Add workaround to support VS2022 with Nuke (#2252)
    * Add workaround to support VS2022 with Nuke
    
    Nuke supports VS2022 in main, but not currently in any released package. This patches in support for it until it does
    
    * Remove unnecessary glob in native loader build, which fails if the profiler isn't downloaded

--- tracer/build/_build/NukeExtensions/DotNetSettingsExtensions.cs
+++ tracer/build/_build/NukeExtensions/DotNetSettingsExtensions.cs
@@ -99,9 +99,26 @@ internal static partial class DotNetSettingsExtensions
     {
         var vsRoot = Environment.GetEnvironmentVariable("VSTUDIO_ROOT");
 
-        return settings
-            .When(!string.IsNullOrEmpty(vsRoot),
-                c => c.SetProcessToolPath(Path.Combine(vsRoot, "MSBuild", "Current", "Bin", "MSBuild.exe")));
+        // Workaround until Nuke supports VS 2022
+        string toolPath;
+        try
+        {
+            toolPath = string.IsNullOrEmpty(vsRoot)
+                           ? MSBuildToolPathResolver.Resolve()
+                           : Path.Combine(vsRoot, "MSBuild", "Current", "Bin", "MSBuild.exe");
+        }
+        catch
+        {
+
+            var editions = new[] { "Enterprise", "Professional", "Community", "BuildTools", "Preview" };
+            toolPath = editions
+                      .Select(edition => Path.Combine(
+                                  EnvironmentInfo.SpecialFolder(SpecialFolders.ProgramFiles)!,
+                                  $@"Microsoft Visual Studio\2022\{edition}\MSBuild\Current\Bin\msbuild.exe"))
+                      .First(File.Exists);
+        }
+
+        return settings.SetProcessToolPath(toolPath);
     }
 
     /// <summary>
