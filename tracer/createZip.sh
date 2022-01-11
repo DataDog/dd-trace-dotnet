d1b12dce93a3f8da18179692f8920608d8b8dc18
maxday <maxime.david@datadoghq.com>
2022-01-11 18:16:57 -0500

    clean

--- tracer/createZip.sh
+++
@@ -1,7 +0,0 @@
-cd bin/tracer-home
-rm /Users/maxime.david/dd/layerTest/datadog.zip
-rm -rf /tmp/dd-tracer
-mkdir -p /tmp/dd-tracer/datadog
-cp -R * /tmp/dd-tracer/datadog
-cd /tmp/dd-tracer
-zip -r -X /Users/maxime.david/dd/layerTest/datadog.zip datadog
