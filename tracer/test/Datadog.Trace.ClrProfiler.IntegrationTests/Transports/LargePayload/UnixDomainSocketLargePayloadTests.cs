ad1303a82bf7a4087fcd6bb3b8dbaf635cc4321d
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-01-12 16:47:38 -0500

    Missed a condition

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/Transports/LargePayload/UnixDomainSocketLargePayloadTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/Transports/LargePayload/UnixDomainSocketLargePayloadTests.cs
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#if NETCOREAPP
+#if NETCOREAPP3_1_OR_GREATER
 
 using Datadog.Trace.TestHelpers;
 using Xunit;
