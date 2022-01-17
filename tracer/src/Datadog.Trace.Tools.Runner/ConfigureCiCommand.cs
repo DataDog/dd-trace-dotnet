0484e11ea875ea81e3f28d83798c07a1b9471e14
Kevin Gosse <krix33@gmail.com>
2022-01-17 13:33:45 +0100

    Add new command line arguments (#2295)
    * Add new command line arguments

---
+++ tracer/src/Datadog.Trace.Tools.Runner/ConfigureCiCommand.cs
@@ -0,0 +1,77 @@
+// <copyright file="ConfigureCiCommand.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Datadog.Trace.Tools.Runner
+{
+    internal class ConfigureCiCommand : Command<ConfigureCiSettings>
+    {
+        private static readonly IReadOnlyDictionary<string, CIName> CiNamesMapping
+            = new Dictionary<string, CIName>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["azp"] = CIName.AzurePipelines
+        };
+
+        public ConfigureCiCommand(ApplicationContext applicationContext)
+        {
+            ApplicationContext = applicationContext;
+        }
+
+        protected ApplicationContext ApplicationContext { get; }
+
+        public override int Execute(CommandContext context, ConfigureCiSettings settings)
+        {
+            var profilerEnvironmentVariables = Utils.GetProfilerEnvironmentVariables(
+                ApplicationContext.RunnerFolder,
+                ApplicationContext.Platform,
+                settings);
+
+            if (profilerEnvironmentVariables == null)
+            {
+                return 1;
+            }
+
+            // Enable CI Visibility mode
+            profilerEnvironmentVariables[Configuration.ConfigurationKeys.CIVisibilityEnabled] = "1";
+
+            if (!TryExtractCiName(settings, out var ciName))
+            {
+                return 1;
+            }
+
+            AnsiConsole.WriteLine("Setting up the environment variables.");
+
+            if (!CIConfiguration.SetupCIEnvironmentVariables(profilerEnvironmentVariables, ciName))
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static bool TryExtractCiName(ConfigureCiSettings settings, out CIName? ciName)
+        {
+            ciName = null;
+
+            if (string.IsNullOrEmpty(settings.CiName))
+            {
+                return true;
+            }
+
+            if (CiNamesMapping.TryGetValue(settings.CiName, out var mappedCiName))
+            {
+                ciName = mappedCiName;
+                return true;
+            }
+
+            Utils.WriteError($"Unsupported CI name: {settings.CiName}. The supported values are: {string.Join(", ", CiNamesMapping.Keys)}.");
+            return false;
+        }
+    }
+}
---
+++ tracer/src/Datadog.Trace.Tools.Runner/RunCommand.cs
@@ -0,0 +1,112 @@
+// <copyright file="RunCommand.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Linq;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Datadog.Trace.Tools.Runner
+{
+    internal class RunCommand : Command<RunSettings>
+    {
+        public RunCommand(ApplicationContext applicationContext)
+        {
+            ApplicationContext = applicationContext;
+        }
+
+        protected ApplicationContext ApplicationContext { get; }
+
+        public override int Execute(CommandContext context, RunSettings settings)
+        {
+            return Execute(context, settings, enableCiMode: false);
+        }
+
+        public override ValidationResult Validate(CommandContext context, RunSettings settings)
+        {
+            var args = settings.Command ?? context.Remaining.Raw;
+
+            if (args.Count == 0)
+            {
+                return ValidationResult.Error("Missing command");
+            }
+
+            if (settings.AdditionalEnvironmentVariables != null)
+            {
+                foreach (var env in settings.AdditionalEnvironmentVariables)
+                {
+                    if (!env.Contains('='))
+                    {
+                        return ValidationResult.Error($"Badly formatted environment variable: {env}");
+                    }
+                }
+            }
+
+            return base.Validate(context, settings);
+        }
+
+        protected int Execute(CommandContext context, RunSettings settings, bool enableCiMode)
+        {
+            var args = settings.Command ?? context.Remaining.Raw;
+
+            var profilerEnvironmentVariables = Utils.GetProfilerEnvironmentVariables(
+                ApplicationContext.RunnerFolder,
+                ApplicationContext.Platform,
+                settings);
+
+            if (profilerEnvironmentVariables is null)
+            {
+                return 1;
+            }
+
+            if (enableCiMode)
+            {
+                profilerEnvironmentVariables[Configuration.ConfigurationKeys.CIVisibilityEnabled] = "1";
+            }
+
+            if (settings.AdditionalEnvironmentVariables != null)
+            {
+                foreach (var env in settings.AdditionalEnvironmentVariables)
+                {
+                    var (key, value) = ParseEnvironmentVariable(env);
+
+                    profilerEnvironmentVariables[key] = value;
+                }
+            }
+
+            // CI Visibility mode is enabled we check if we have connection to the agent before running the process.
+            if (enableCiMode && !Utils.CheckAgentConnectionAsync(settings.AgentUrl).GetAwaiter().GetResult())
+            {
+                return 1;
+            }
+
+            AnsiConsole.WriteLine("Running: " + string.Join(' ', args));
+
+            var arguments = args.Count > 1 ? string.Join(' ', args.Skip(1).ToArray()) : null;
+
+            if (Program.CallbackForTests != null)
+            {
+                Program.CallbackForTests(args[0], arguments, profilerEnvironmentVariables);
+                return 0;
+            }
+
+            var processInfo = Utils.GetProcessStartInfo(args[0], Environment.CurrentDirectory, profilerEnvironmentVariables);
+
+            if (args.Count > 1)
+            {
+                processInfo.Arguments = arguments;
+            }
+
+            return Utils.RunProcess(processInfo, ApplicationContext.TokenSource.Token);
+        }
+
+        private static (string Key, string Value) ParseEnvironmentVariable(string env)
+        {
+            var values = env.Split('=', 2);
+
+            return (values[0], values[1]);
+        }
+    }
+}
