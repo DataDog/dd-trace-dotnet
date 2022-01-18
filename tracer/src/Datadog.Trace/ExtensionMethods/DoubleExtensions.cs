1e89df9a09dd2350a80db0dc68ab7620651ef4b5
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-01-18 09:26:02 -0500

    add DoubleExtensions helper and tests

---
+++ tracer/src/Datadog.Trace/ExtensionMethods/DoubleExtensions.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="DoubleExtensions.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Datadog.Trace.ExtensionMethods;
+
+internal static class DoubleExtensions
+{
+    [return: NotNullIfNotNull("value")]
+    internal static double? RoundUp(this double? value, int digits)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        var pow = Math.Pow(10, digits);
+        return Math.Ceiling(value.Value * pow) / pow;
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/ExtensionMethods/DoubleExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="DoubleExtensionsTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Datadog.Trace.ExtensionMethods;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.ExtensionMethods;
+
+public class DoubleExtensionsTests
+{
+    [Theory]
+    [InlineData(0, 4, 0)]
+    [InlineData(1, 4, 1)]
+    [InlineData(0.98761, 1, 1.0)]
+    [InlineData(0.98761, 2, 0.99)]
+    [InlineData(0.98761, 3, 0.988)]
+    [InlineData(0.98761, 4, 0.9877)]
+    [InlineData(0.98769, 4, 0.9877)]
+    public void RoundUp(double? initialValue, int digits, double? expected)
+    {
+        initialValue.RoundUp(digits).Should().Be(expected);
+    }
+}
