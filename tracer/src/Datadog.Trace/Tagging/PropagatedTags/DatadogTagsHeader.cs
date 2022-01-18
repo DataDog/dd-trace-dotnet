573e6151d15d2efd8070ed6cf29c14c2448d578c
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-01-18 09:26:12 -0500

    add DatadogTagsHeader and tests

---
+++ tracer/src/Datadog.Trace/Tagging/PropagatedTags/DatadogTagsHeader.cs
@@ -0,0 +1,293 @@
+﻿// <copyright file="DatadogTagsHeader.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Datadog.Trace.Util;
+
+namespace Datadog.Trace.Tagging.PropagatedTags;
+
+internal static class DatadogTagsHeader
+{
+    /*
+        tagset = tag, { ",", tag };
+        tag = ( identifier - space ), "=", identifier;
+        identifier = allowed characters, { allowed characters };
+        allowed characters = ( ? ASCII characters 32-126 ? - equal or comma );
+        equal or comma = "=" | ",";
+        space = " ";
+     */
+
+    public const char TagPairSeparator = ',';
+    public const char KeyValueSeparator = '=';
+
+    public static string? GetTagValue(string? headers, string key)
+    {
+        if (headers is null or { Length: 0 })
+        {
+            return null;
+        }
+
+        if (FindTag(headers, key, out var valueIndex, out var valueLength))
+        {
+            return headers.Substring(valueIndex, valueLength);
+        }
+
+        return null;
+    }
+
+    private static bool FindTag(string headers, string key, out int valueIndex, out int valueLength)
+    {
+        valueIndex = -1;
+        valueLength = -1;
+
+        if (string.IsNullOrWhiteSpace(headers))
+        {
+            return false;
+        }
+
+        int searchStartIndex = 0;
+
+        while (searchStartIndex < headers!.Length)
+        {
+            var keyIndex = headers.IndexOf(key, searchStartIndex, StringComparison.Ordinal);
+
+            if (keyIndex < 0)
+            {
+                // key not found
+                return false;
+            }
+
+            int separatorIndex = keyIndex + key.Length;
+
+            if (separatorIndex < headers.Length - 1 && headers[separatorIndex] != KeyValueSeparator)
+            {
+                // found "key", but not "key=",
+                // this looks like a different key that starts the same ("keyfoo="),
+                // or we found the key inside a value ("foo=key"),
+                // skip ahead and keep looking
+                searchStartIndex = separatorIndex;
+                continue;
+            }
+
+            if (keyIndex > 0 && headers[keyIndex - 1] != TagPairSeparator)
+            {
+                // found "key", but not at the beginning of string and not preceded by tag separator ("...,key="),
+                // this looks like a different key that ends the same ("fookey="),
+                // skip ahead and keep looking
+                searchStartIndex = separatorIndex;
+                continue;
+            }
+
+            valueIndex = keyIndex + key.Length + 1;
+
+            // find the end of the tag's value
+            var valueEndIndex = headers.IndexOf(TagPairSeparator, valueIndex);
+
+            if (valueEndIndex < 0)
+            {
+                // tag separator not found, so the tag's value reaches the end of string
+                valueEndIndex = headers.Length;
+            }
+
+            valueLength = valueEndIndex - valueIndex;
+            return true;
+        }
+
+        // we should never reach this line
+        return false;
+    }
+
+    /// <summary>
+    /// Adds or replaces an <see cref="UpstreamService"/> tag on the specified list of headers.
+    /// If the tag does not already exists, it is appended at the end as a new tag. If the tag already exists,
+    /// the new value is appended to the existing tag using <see cref="UpstreamService.GroupSeparator"/>.
+    /// </summary>
+    /// <param name="headers">The list of tags, if any, to add a new tag to.</param>
+    /// <param name="tag">The tag to add.</param>
+    /// <returns>A new list of tags.</returns>
+    public static string SetTagValue(string headers, UpstreamService tag)
+    {
+        return SetTagValue(
+            headers,
+            UpstreamService.GroupSeparator,
+            key: Tags.Propagated.UpstreamServices,
+            value: tag.ToString());
+    }
+
+    /// <summary>
+    /// Adds or replaces an arbitrary tag (key/value pair) on the specified list of headers.
+    /// If the tag does not already exists, it is appended at the end as a new tag. If the tag already exists,
+    /// the new value is appended to the existing tag using <paramref name="tagValueSeparator"/>.
+    /// </summary>
+    /// <param name="headers">The list of tags, if any, to add a new tag to.</param>
+    /// <param name="tagValueSeparator">Separator used between multiple values of the same tag.</param>
+    /// <param name="key">The name of the tag to add or append to.</param>
+    /// <param name="value">The value of the tag to add or append.</param>
+    /// <returns>A new list of tags.</returns>
+    public static string SetTagValue(string headers, char tagValueSeparator, string key, string value)
+    {
+        StringBuilder sb;
+
+        if (FindTag(headers, key, out var valueIndex, out var valueLength))
+        {
+            // tag already exists
+            sb = StringBuilderCache.Acquire(headers.Length - valueLength + value.Length);
+
+            sb.Append(headers, startIndex: 0, count: valueLength)
+              .Append(value);
+
+            var valueEndIndex = valueIndex + valueLength;
+
+            if (valueEndIndex < headers.Length - 1)
+            {
+                sb.Append(headers, startIndex: valueEndIndex, count: headers.Length - valueEndIndex);
+            }
+
+            return StringBuilderCache.GetStringAndRelease(sb);
+        }
+
+        // key not found, append as new key/value pair
+        sb = StringBuilderCache.Acquire(headers.Length + key.Length + value.Length + 2);
+        sb.Append(headers);
+        AppendKeyValuePair(sb, key, value);
+        return StringBuilderCache.GetStringAndRelease(sb);
+    }
+
+    /*
+    /// <summary>
+    /// Adds or appends an <see cref="UpstreamService"/> tag to the specified list of tags.
+    /// </summary>
+    /// <param name="headers">The list of tags, if any, to add a new tag to.</param>
+    /// <param name="tag">The tag to add.</param>
+    /// <returns>A new list of tags.</returns>
+    public static string AppendTagValue(string? headers, UpstreamService tag)
+    {
+        return AppendTagValue(
+            headers,
+            UpstreamService.GroupSeparator,
+            key: Tags.Propagated.UpstreamServices,
+            value: tag.ToString());
+    }
+
+    /// <summary>
+    /// Adds an arbitrary tag (key/value pair) to <paramref name="headers"/>. If the tag does not already exists,
+    /// it is appended at the end of <paramref name="headers"/> as a new tag. If the tag already exists,
+    /// the new value is appended to the existing tag using <paramref name="tagValueSeparator"/>.
+    /// </summary>
+    /// <param name="headers">The list of tags, if any, to add a new tag to.</param>
+    /// <param name="tagValueSeparator">Separator used between multiple values of the same tag.</param>
+    /// <param name="key">The name of the tag to add or append to.</param>
+    /// <param name="value">The value of the tag to add or append.</param>
+    /// <returns>A new list of tags.</returns>
+    public static string AppendTagValue(string? headers, char tagValueSeparator, string key, string value)
+    {
+        if (string.IsNullOrEmpty(headers))
+        {
+            var sb = StringBuilderCache.Acquire(key.Length + value.Length + 1);
+            sb.Append(headers);
+            AppendKeyValuePair(sb, key, value);
+            return StringBuilderCache.GetStringAndRelease(sb);
+        }
+
+        return AppendTagValueWithSearch(headers ?? string.Empty, tagValueSeparator, key, value);
+    }
+
+    private static string AppendTagValueWithSearch(string headers, char tagValueSeparator, string key, string value)
+    {
+        if (FindTag(headers, key, out var valueIndex, out var valueLength))
+        {
+            // tag already exists, find the end of its current value
+            var sb = StringBuilderCache.Acquire(headers.Length + 1 + value.Length);
+            var valueEndIndex = valueIndex + valueLength;
+
+            sb.Append(headers, startIndex: 0, count: valueEndIndex)
+              .Append(tagValueSeparator)
+              .Append(value);
+
+            if (valueEndIndex < headers.Length - 1)
+            {
+                sb.Append(headers, startIndex: valueEndIndex, count: headers.Length - valueEndIndex);
+            }
+
+            return StringBuilderCache.GetStringAndRelease(sb);
+        }
+        else
+        {
+            // key not found, append as new key/value pair
+            var sb = StringBuilderCache.Acquire(headers.Length + key.Length + value.Length + 2);
+            sb.Append(headers);
+            AppendKeyValuePair(sb, key, value);
+            return StringBuilderCache.GetStringAndRelease(sb);
+        }
+    }
+    */
+
+    private static void AppendKeyValuePair(StringBuilder sb, string key, string value)
+    {
+        if (sb.Length > 0)
+        {
+            sb.Append(TagPairSeparator);
+        }
+
+        sb.Append(key)
+          .Append(KeyValueSeparator)
+          .Append(value);
+    }
+
+    public static string Serialize(KeyValuePair<string, string?>[]? tags)
+    {
+        if (tags == null || tags.Length == 0)
+        {
+            return string.Empty;
+        }
+
+        int totalLength = 0;
+
+        foreach (var tag in tags)
+        {
+            if (!string.IsNullOrEmpty(tag.Value))
+            {
+                // ",{key}={value}", we'll go over by one comma but that's fine
+                totalLength += tag.Key.Length + tag.Value!.Length + 2;
+            }
+        }
+
+        var sb = StringBuilderCache.Acquire(totalLength);
+
+        foreach (var tag in tags)
+        {
+            if (!string.IsNullOrEmpty(tag.Value))
+            {
+                AppendKeyValuePair(sb, tag.Key, tag.Value!);
+            }
+        }
+
+        return StringBuilderCache.GetStringAndRelease(sb);
+    }
+
+    public static List<KeyValuePair<string, string>> Parse(string datadogTags)
+    {
+        var pairs = datadogTags.Split(TagPairSeparator);
+        var list = new List<KeyValuePair<string, string>>(capacity: pairs.Length);
+
+        foreach (var pair in pairs)
+        {
+            var keyValueSeparatorIndex = pair.IndexOf(KeyValueSeparator);
+
+            if (keyValueSeparatorIndex > 0)
+            {
+                var key = pair.Substring(0, keyValueSeparatorIndex);
+                var value = pair.Substring(keyValueSeparatorIndex);
+                list.Add(new KeyValuePair<string, string>(key, value));
+            }
+        }
+
+        return list;
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/Tagging/PropagatedTags/DatadogTagsHeaderTests.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="DatadogTagsHeaderTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using Datadog.Trace.Tagging.PropagatedTags;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Tagging.PropagatedTags;
+
+public class DatadogTagsHeaderTests
+{
+    [Fact]
+    public void Serialize_0_Pairs()
+    {
+        var pairs = Array.Empty<KeyValuePair<string, string>>();
+        var header = DatadogTagsHeader.Serialize(pairs);
+        header.Should().Be(string.Empty);
+    }
+
+    [Fact]
+    public void Serialize_3_Pairs()
+    {
+        var pairs = new KeyValuePair<string, string>[]
+                    {
+                        new("key1", "value1"),
+                        new("key2", "value2"),
+                        new("key3", "value3"),
+                    };
+
+        var header = DatadogTagsHeader.Serialize(pairs);
+        header.Should().Be("key1=value1,key2=value2,key3=value3");
+    }
+
+    [Theory]
+    [InlineData(null, "key1", null)]                                      // null headers
+    [InlineData("", "key1", null)]                                        // empty headers
+    [InlineData("key1=value1,key2=value2,key3=value3", "key1", "value1")] // beginning
+    [InlineData("key1=value1,key2=value2,key3=value3", "key2", "value2")] // middle
+    [InlineData("key1=value1,key2=value2,key3=value3", "key3", "value3")] // end
+    [InlineData("key1=value1,ey1=value2", "ey1", "value2")]               // don't stop at "key1="
+    [InlineData("key1=key2,key2=value2", "key2", "value2")]               // don't stop at "=key2"
+    public void GetTagValue(string headers, string key, string expectedValue)
+    {
+        var value = DatadogTagsHeader.GetTagValue(headers, key);
+        value.Should().Be(expectedValue);
+    }
+
+    [Theory]
+    [InlineData(null, "key1", "newValue", "key1=newValue")]                                                                      // add new tag to null header
+    [InlineData("", "key1", "newValue", "key1=newValue")]                                                                        // add new tag to empty header
+    [InlineData("key1=value1,key2=value2,key3=value3", "key1", "newValue", "key1=value1|newValue,key2=value2,key3=value3")]      // append to first tag
+    [InlineData("key1=value1,key2=value2,key3=value3", "key2", "newValue", "key1=value1,key2=value2|newValue,key3=value3")]      // append to middle tag
+    [InlineData("key1=value1,key2=value2,key3=value3", "key3", "newValue", "key1=value1,key2=value2,key3=value3|newValue")]      // append to last tag
+    [InlineData("key1=value1,key2=value2,key3=value3", "key4", "newValue", "key1=value1,key2=value2,key3=value3,key4=newValue")] // add new tag
+    [InlineData("key1=value1,key2=value2,key3=value3", "ey3",  "newValue", "key1=value1,key2=value2,key3=value3,ey3=newValue")]  // add new tag, don't stop at "key3="
+    [InlineData("key1=key2,key2=value2,key3=value3",   "key2", "newValue", "key1=key2,key2=value2|newValue,key3=value3")]        // add new tag, don't stop at "=key2"
+    public void AppendTagValue_KeyValuePair(string existingHeader, string newKey, string newValue, string expectedHeader)
+    {
+        var newHeaderValue = DatadogTagsHeader.AppendTagValue(existingHeader, tagValueSeparator: '|', newKey, newValue);
+        newHeaderValue.Should().Be(expectedHeader);
+    }
+
+    [Fact]
+    public void AppendTagValue_UpstreamServices()
+    {
+        string header = string.Empty;
+
+        // add first value (create the tag)
+        var service1 = new UpstreamService("Service1", -1, 2, 0.95761);
+        var tagValue1 = service1.ToString();
+        header = DatadogTagsHeader.AppendTagValue(header, service1);
+        header.Should().Be($"_dd.p.upstream_services={tagValue1}");
+
+        // add second value (append to the existing tag)
+        var service2 = new UpstreamService("Service2", 1, 3, 0.90769);
+        var tagValue2 = service2.ToString();
+        header = DatadogTagsHeader.AppendTagValue(header, service2);
+        header.Should().Be($"_dd.p.upstream_services={tagValue1};{tagValue2}");
+    }
+}
