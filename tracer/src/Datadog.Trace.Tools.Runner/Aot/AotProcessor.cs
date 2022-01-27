d36c880be8eeecb1aa03de3d11998e8e036003cf
Tony Redondo <tony.redondo@datadoghq.com>
2022-01-27 18:35:56 +0100

    Start method rewriting.

--- tracer/src/Datadog.Trace.Tools.Runner/Aot/AotProcessor.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/Aot/AotProcessor.cs
@@ -8,10 +8,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.ClrProfiler;
+using Datadog.Trace.ClrProfiler.CallTarget;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 using Spectre.Console;
 
 namespace Datadog.Trace.Tools.Runner.Aot
@@ -20,11 +23,14 @@ namespace Datadog.Trace.Tools.Runner.Aot
     {
         private static readonly NativeCallTargetDefinition[] Definitions;
         private static readonly NativeCallTargetDefinition[] DerivedDefinitions;
+        private static readonly Assembly TracerAssembly;
+        private static readonly Type CallTargetInvokerType = typeof(Datadog.Trace.ClrProfiler.CallTarget.CallTargetInvoker);
 
         static AotProcessor()
         {
             Definitions = InstrumentationDefinitions.GetAllDefinitions().Definitions;
             DerivedDefinitions = InstrumentationDefinitions.GetDerivedDefinitions().Definitions;
+            TracerAssembly = typeof(Datadog.Trace.ClrProfiler.Instrumentation).Assembly;
         }
 
         public static void ProcessFolder(string inputFolder, string outputFolder)
@@ -58,9 +64,7 @@ namespace Datadog.Trace.Tools.Runner.Aot
                 ModuleContext modCtx = ModuleDef.CreateModuleContext();
                 using (ModuleDefMD module = ModuleDefMD.Load(inputPath, modCtx))
                 {
-                    var lstDefinitions = new List<NativeCallTargetDefinition>();
-                    var lstDerived = new List<NativeCallTargetDefinition>();
-
+                    var lstDefinitionsDefs = new List<DefinitionDef>();
                     var assemblyDef = module.Assembly;
 
                     // Extract direct definitions
@@ -87,9 +91,64 @@ namespace Datadog.Trace.Tools.Runner.Aot
                             }
                         }
 
-                        var typeDef = module.ExportedTypes.FirstOrDefault(type => type.FullName == definition.TargetType)?.Resolve();
+                        var typeDef = module.Find(definition.TargetType, true);
+                        if (typeDef is null)
+                        {
+                            typeDef = module.ExportedTypes.FirstOrDefault(eType => eType.FullName == definition.TargetType)?.Resolve();
+                        }
 
-                        lstDefinitions.Add(definition);
+                        if (typeDef is not null)
+                        {
+                            MethodDef methodDef = null;
+                            foreach (var mDef in typeDef.FindMethods(definition.TargetMethod))
+                            {
+                                var lstParameters = mDef.Parameters.ToList();
+                                if (lstParameters.Count != definition.TargetSignatureTypesLength)
+                                {
+                                    continue;
+                                }
+
+                                bool parameters = true;
+                                var ptr = definition.TargetSignatureTypes;
+                                for (var i = 0; i < definition.TargetSignatureTypesLength; i++)
+                                {
+                                    var localPtr = Marshal.ReadIntPtr(ptr);
+                                    var localString = Marshal.PtrToStringUni(localPtr);
+                                    ptr += Marshal.SizeOf(typeof(IntPtr));
+
+                                    if (localString == "_")
+                                    {
+                                        continue;
+                                    }
+
+                                    if (i == 0)
+                                    {
+                                        if (mDef.ReturnType.FullName != localString)
+                                        {
+                                            parameters = false;
+                                            break;
+                                        }
+                                    }
+                                    else if (lstParameters[i].Type.FullName != localString)
+                                    {
+                                        parameters = false;
+                                        break;
+                                    }
+                                }
+
+                                if (parameters)
+                                {
+                                    methodDef = mDef;
+                                    var integrationType = TracerAssembly.GetType(definition.IntegrationType, false);
+                                    if (integrationType is not null)
+                                    {
+                                        lstDefinitionsDefs.Add(new DefinitionDef(assemblyDef, typeDef, methodDef, integrationType, definition));
+                                    }
+
+                                    break;
+                                }
+                            }
+                        }
                     }
 
                     // Extract derived definitions
@@ -116,16 +175,20 @@ namespace Datadog.Trace.Tools.Runner.Aot
                                 continue;
                             }
 
-                            lstDerived.Add(definition);
+                            foreach (var typeDef in module.Types)
+                            {
+                                // ..
+                            }
                         }
                     }
 
-                    if (lstDefinitions.Count == 0 && lstDerived.Count == 0)
+                    if (lstDefinitionsDefs.Count == 0)
                     {
                         return false;
                     }
 
-                    AnsiConsole.WriteLine($"{assemblyDef.FullName} => {lstDefinitions.Count} - {lstDerived.Count}");
+                    AnsiConsole.WriteLine($"{assemblyDef.FullName} => {lstDefinitionsDefs.Count}");
+                    ProcessDefinitionDefs(module, lstDefinitionsDefs);
                 }
             }
             catch (BadImageFormatException)
@@ -139,6 +202,159 @@ namespace Datadog.Trace.Tools.Runner.Aot
             }
 
             return true;
+        }
+
+        private static void ProcessDefinitionDefs(ModuleDef moduleDef, List<DefinitionDef> definitionDefs)
+        {
+            var callTargetInvokerType = moduleDef.Import(CallTargetInvokerType);
+            var callTargetInvokerMethods = CallTargetInvokerType.GetMethods();
+            var exceptionTypeRef = moduleDef.Import(typeof(Exception));
+
+            foreach (var definitionDef in definitionDefs)
+            {
+                var methodDef = definitionDef.TargetMethodDef;
+                if (!methodDef.HasBody)
+                {
+                    continue;
+                }
+
+                var integrationTypeSig = moduleDef.ImportAsTypeSig(definitionDef.IntegrationType);
+                var targetTypeSig = definitionDef.TargetTypeDef.ToTypeSig();
+                var methodReturnTypeSig = methodDef.ReturnType;
+
+                var lstParameters = definitionDef.TargetMethodDef.Parameters.Where(p => p.IsNormalMethodParameter).ToList();
+
+                // CallTargetReturn
+                var callTargetReturnType = definitionDef.TargetMethodDef.HasReturnType ? typeof(CallTargetReturn<>) : typeof(CallTargetReturn);
+                var callTargetReturnTypeRef = moduleDef.Import(callTargetReturnType);
+                GenericInstSig callTargetReturnTypeGenInstSig = null;
+                if (definitionDef.TargetMethodDef.HasReturnType)
+                {
+                    callTargetReturnTypeGenInstSig = new GenericInstSig((ClassSig)callTargetReturnTypeRef.ToTypeSig(), methodReturnTypeSig);
+                }
+
+                // BeginMethod
+                var beginMethodMethodInfo = callTargetInvokerMethods.FirstOrDefault(m => m.Name == "BeginMethod" && m.GetParameters().Length == lstParameters.Count + 1);
+                var beginMethodInfoMethodRef = moduleDef.Import(beginMethodMethodInfo);
+                var lstBeginMethodGenericsTypeSigs = new List<TypeSig>();
+                lstBeginMethodGenericsTypeSigs.Add(integrationTypeSig);
+                lstBeginMethodGenericsTypeSigs.Add(targetTypeSig);
+                foreach (var mParam in lstParameters)
+                {
+                    lstBeginMethodGenericsTypeSigs.Add(mParam.Type);
+                }
+
+                var beginMethodInfoMethodSpec = new MethodSpecUser((MemberRefUser)beginMethodInfoMethodRef, new GenericInstMethodSig(lstBeginMethodGenericsTypeSigs));
+                var callTargetStateTypeSig = beginMethodInfoMethodRef.MethodSig.RetType;
+
+                // EndMethod
+                var endMethodMethodInfo = callTargetInvokerMethods.FirstOrDefault(
+                    m =>
+                    {
+                        if (m.Name != "EndMethod")
+                        {
+                            return false;
+                        }
+
+                        if (definitionDef.TargetMethodDef.HasReturnType && m.ReturnType == typeof(CallTargetReturn))
+                        {
+                            return false;
+                        }
+
+                        if (!m.GetParameters().Last().IsIn)
+                        {
+                            return false;
+                        }
+
+                        return true;
+                    });
+                var endMethodInfoMethodRef = moduleDef.Import(endMethodMethodInfo);
+                var lstEndMethodGenericsTypeSigs = new List<TypeSig>();
+                lstEndMethodGenericsTypeSigs.Add(integrationTypeSig);
+                lstEndMethodGenericsTypeSigs.Add(targetTypeSig);
+                if (definitionDef.TargetMethodDef.HasReturnType)
+                {
+                    lstEndMethodGenericsTypeSigs.Add(methodReturnTypeSig);
+                }
+
+                var endMethodInfoMethodSpec = new MethodSpecUser((MemberRefUser)endMethodInfoMethodRef, new GenericInstMethodSig(lstEndMethodGenericsTypeSigs));
+
+                // LogException
+                var logExceptionMethodInfo = callTargetInvokerMethods.FirstOrDefault(m => m.Name == "LogException");
+                var logExceptionMethodRef = moduleDef.Import(logExceptionMethodInfo);
+                var lstLogExceptionGenericsTypeSigs = new List<TypeSig>();
+                lstLogExceptionGenericsTypeSigs.Add(integrationTypeSig);
+                lstLogExceptionGenericsTypeSigs.Add(targetTypeSig);
+                var logExceptionMethodSpec = new MethodSpecUser((MemberRefUser)logExceptionMethodRef, new GenericInstMethodSig(lstLogExceptionGenericsTypeSigs));
+
+                // GetDefaultValue
+                var getDefaultValueMethodInfo = callTargetInvokerMethods.FirstOrDefault(m => m.Name == "GetDefaultValue");
+                var getDefaultValueMethodRef = moduleDef.Import(getDefaultValueMethodInfo);
+
+                var getDefaultValueReturnTypeMethodSpec = new MethodSpecUser((MemberRefUser)getDefaultValueMethodRef, new GenericInstMethodSig(methodReturnTypeSig));
+                var getDefaultValueCallTargetReturnTypeMethodSpec = new MethodSpecUser((MemberRefUser)getDefaultValueMethodRef, new GenericInstMethodSig(callTargetReturnTypeGenInstSig ?? callTargetReturnTypeRef.ToTypeSig()));
+
+                // Add new locals Add locals for TReturn (if non-void method), CallTargetState, CallTargetReturn/CallTargetReturn<TReturn>, Exception
+                var methodBody = methodDef.Body;
+                Local returnValueLocal = null;
+                Local callTargetReturnLocal = null;
+                if (definitionDef.TargetMethodDef.HasReturnType)
+                {
+                    returnValueLocal = methodBody.Variables.Add(new Local(methodReturnTypeSig, "cTargetReturnValue"));
+                    callTargetReturnLocal = methodBody.Variables.Add(new Local(callTargetReturnTypeGenInstSig, "cTargetReturn"));
+                }
+                else
+                {
+                    callTargetReturnLocal = methodBody.Variables.Add(new Local(callTargetReturnTypeRef.ToTypeSig(), "cTargetReturn"));
+                }
+
+                var callTargetStateLocal = methodBody.Variables.Add(new Local(callTargetStateTypeSig, "cTargetState"));
+                var exceptionLocal = methodBody.Variables.Add(new Local(exceptionTypeRef.ToTypeSig(), "cTargetException"));
+
+                var index = 0;
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Call, getDefaultValueReturnTypeMethodSpec));
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Stloc_S, returnValueLocal));
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Call, getDefaultValueCallTargetReturnTypeMethodSpec));
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Stloc_S, callTargetReturnLocal));
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Ldnull));
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Stloc_S, exceptionLocal));
+                // ..
+                if (methodDef.IsStatic)
+                {
+                    methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Ldnull));
+                }
+                else
+                {
+                    methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Ldarg_0));
+                }
+
+                foreach (var mParameter in lstParameters)
+                {
+                    methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Ldarga_S, mParameter));
+                }
+
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Call, beginMethodInfoMethodSpec));
+                methodBody.Instructions.Insert(index++, Instruction.Create(OpCodes.Stloc_S, callTargetStateLocal));
+            }
+        }
+    }
+
+#pragma warning disable SA1201
+    internal readonly struct DefinitionDef
+    {
+        public readonly AssemblyDef TargetAssemblyDef;
+        public readonly TypeDef TargetTypeDef;
+        public readonly MethodDef TargetMethodDef;
+        public readonly Type IntegrationType;
+        public readonly NativeCallTargetDefinition Definition;
+
+        public DefinitionDef(AssemblyDef targetAssemblyDef, TypeDef targetTypeDef, MethodDef targetMethodDef, Type integrationType, NativeCallTargetDefinition definition)
+        {
+            TargetAssemblyDef = targetAssemblyDef;
+            TargetTypeDef = targetTypeDef;
+            TargetMethodDef = targetMethodDef;
+            IntegrationType = integrationType;
+            Definition = definition;
         }
     }
 }
