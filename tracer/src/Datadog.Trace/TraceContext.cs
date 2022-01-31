13748db1ce84f6ca424c4f77edd8971715e93536
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-01-31 13:08:19 -0500

    remove unused `SetSamplingPriority(SamplingPriority?)` overload

--- tracer/src/Datadog.Trace/TraceContext.cs
+++ tracer/src/Datadog.Trace/TraceContext.cs
@@ -148,11 +148,6 @@ namespace Datadog.Trace
             }
         }
 
-        public void SetSamplingPriority(SamplingPriority? samplingPriority, bool notifyDistributedTracer = true)
-        {
-            SetSamplingPriority((int?)samplingPriority, notifyDistributedTracer);
-        }
-
         public void SetSamplingPriority(int? samplingPriority, bool notifyDistributedTracer = true)
         {
             _samplingPriority = samplingPriority;
