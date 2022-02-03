e57c249bc62ccfd1dfaaedfe5ade8f9d9b1fc6f5
Zach Montoya <zach.montoya@datadoghq.com>
2022-02-03 12:21:26 -0800

    Fix DuckAttributeBase.cs edit

--- tracer/src/Datadog.Trace/DuckTyping/DuckAttributeBase.cs
+++ tracer/src/Datadog.Trace/DuckTyping/DuckAttributeBase.cs
@@ -1,4 +1,4 @@
-// <copyright file="DuckAttributeBase.cs" company="Datadog">
+﻿// <copyright file="DuckAttributeBase.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
