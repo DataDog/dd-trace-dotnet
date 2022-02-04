00579aebf3d4814ae3a3ac34cac4cda260ce4b45
Kevin Gosse <krix33@gmail.com>
2022-02-04 16:23:22 +0100

    Check the registry in the CLI

--- tracer/src/Datadog.Trace.Tools.Runner/Checks/ProcessBasicCheck.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/Checks/ProcessBasicCheck.cs
@@ -5,7 +5,10 @@
 #nullable enable
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
 using Spectre.Console;
 
 using static Datadog.Trace.Tools.Runner.Checks.Resources;
@@ -81,7 +84,50 @@ namespace Datadog.Trace.Tools.Runner.Checks
                 foundIssue = true;
             }
 
+            if (!CheckRegistry())
+            {
+                foundIssue = true;
+            }
+
             return !foundIssue;
+        }
+
+        internal static bool CheckRegistry(IRegistryService? registry = null)
+        {
+            registry ??= new Windows.RegistryService();
+
+            try
+            {
+                var suspiciousNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "COR_ENABLE_PROFILING",
+                    "CORECLR_ENABLE_PROFILING",
+                    "COR_PROFILER",
+                    "CORECLR_PROFILER",
+                    "COR_PROFILER_PATH",
+                    "CORECLR_PROFILER_PATH",
+                    "COR_PROFILER_PATH_64",
+                    "CORECLR_PROFILER_PATH_64"
+                };
+
+                bool foundKey = false;
+
+                foreach (var name in registry.GetLocalMachineValueNames(@"SOFTWARE\Microsoft\.NETFramework"))
+                {
+                    if (suspiciousNames.Contains(name))
+                    {
+                        Utils.WriteWarning(SuspiciousRegistryKey(name));
+                        foundKey = true;
+                    }
+                }
+
+                return !foundKey;
+            }
+            catch (Exception ex)
+            {
+                Utils.WriteError(ErrorCheckingRegistry(ex.Message));
+                return true;
+            }
         }
 
         private static (string? Profiler, string? Tracer) FindTracerModules(ProcessInfo process)
