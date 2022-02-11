5c1cd6e614b7cbc151aa3bb865ad2b4484241637
Maximo Bautista <max.bautista97@gmail.com>
2022-02-11 16:37:07 -0500

    Removed duplicated Telemetry rebase line

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/AspNetMvcIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/AspNetMvcIntegration.cs
@@ -166,8 +166,6 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
                     {
                         Log.Error(ex, "Error extracting propagated HTTP headers.");
                     }
-
-                    tracer.TracerManager.Telemetry.IntegrationGeneratedSpan(IntegrationId);
                 }
 
                 var tags = new AspNetTags();
