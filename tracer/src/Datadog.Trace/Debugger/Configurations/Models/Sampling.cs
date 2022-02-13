683bb06e34af9909c554c7b79a97753afb206734
Alexander S <shurivich@gmail.com>
2022-02-13 14:17:21 +0200

    Probes Configuration Updater & Comparer
    Probes Configuration Updater & Comparer implementation

--- tracer/src/Datadog.Trace/Debugger/Configurations/Models/Sampling.cs
+++ tracer/src/Datadog.Trace/Debugger/Configurations/Models/Sampling.cs
@@ -3,9 +3,26 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
+
 namespace Datadog.Trace.Debugger.Configurations.Models;
 
-internal class Sampling
+internal struct Sampling : IEquatable<Sampling>
 {
     public double SnapshotsPerSecond { get; set; }
+
+    public bool Equals(Sampling other)
+    {
+        return SnapshotsPerSecond.Equals(other.SnapshotsPerSecond);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Sampling other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return SnapshotsPerSecond.GetHashCode();
+    }
 }
--- tracer/src/Datadog.Trace/Debugger/Configurations/Models/Tag.cs
+++ tracer/src/Datadog.Trace/Debugger/Configurations/Models/Tag.cs
@@ -3,11 +3,53 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
+
 namespace Datadog.Trace.Debugger.Configurations.Models;
 
-internal class Tag
+internal class Tag : IEquatable<Tag>
 {
     public string Key { get; set; }
 
     public string Value { get; set; }
+
+    public bool Equals(Tag other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Key == other.Key && Value == other.Value;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((Tag)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Key, Value);
+    }
 }
