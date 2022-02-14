ff370891504cbe7caa42811e47e83cd7fb3b424e
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-02-14 14:34:57 +0100

    Setup Github Actions for the profiler CI (#2415)
    Plug the profiler CI and add its jobs in the noop-pipeline when required
    
    * Github Actions jobs are recognized by their name (or key if the name is not present).
    When there is a matrix (example: x64,x86 / net45,net5.0), there will be as many jobs as number of combination of the matrix.
    The final name will be "name (XX, YY)" (ex: "build (x64, net45)").
    * Make run.sh executable

---
+++ .github/scripts/package_and_deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+ddprof_deploy_folder=$1
+commit_sha=$2
+commit_author=$3
+
+
+profiler_version=v1.1.1_$(date -u +%G%m%d%H%M%S)
+
+## Create master.index.txt file
+cat <<- EOF > master.index.txt
+master
+${commit_sha}
+datadog-dotnet-profiler-apm-${profiler_version}-*
+${commit_author}
+EOF
+
+## Create package
+package_name="datadog-dotnet-profiler-apm-${profiler_version}-full"
+compressed_package_name="${package_name}.zip"
+echo "Creating ${compressed_package_name}..."
+mv ${ddprof_deploy_folder} ${package_name}  || (echo "Failed to move ${ddprof_deploy_folder} to ${package_name}." && exit 1)
+zip -r ${compressed_package_name} ${package_name} || (echo "Failed to compress ${package_name}." && exit 1)
+
+## Deploy on AWS
+echo "Copying ${compressed_package_name} to S3"
+aws s3 cp ${compressed_package_name} s3://datadog-reliability-env/dotnet-profiler/${compressed_package_name} || (echo "Failed to copy ${compressed_package_name} to S3." && exit 1)
+
+echo "Copying master.index.txt to S3"
+aws s3 cp master.index.txt s3://datadog-reliability-env/dotnet-profiler/master.index.txt || (echo "Failed to copy master.index.txt to S3." && exit 1)
