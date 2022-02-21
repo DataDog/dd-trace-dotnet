08d734ed98bafc449bd0719dd7a1b04e16e4b3f1
Omer Raviv <omerraviv@gmail.com>
2022-02-21 23:41:44 +0200

    Add proper spdlog-based logging to NativeLoader (#2481)

--- tracer/src/Datadog.AutoInstrumentation.NativeLoader/dynamic_instance.cpp
+++ tracer/src/Datadog.AutoInstrumentation.NativeLoader/dynamic_instance.cpp
@@ -1,7 +1,7 @@
 #include "dynamic_instance.h"
 
 #include "guid.h"
-#include "logging.h"
+#include "log.h"
 #include "pal.h"
 
 namespace datadog::shared::nativeloader
@@ -76,7 +76,7 @@ namespace datadog::shared::nativeloader
         {
             if (!FreeDynamicLibrary(m_instance))
             {
-                Warn("DynamicInstanceImpl::~DynamicInstanceImpl: Error unloading: ", m_filepath, " dynamic library.");
+                Log::Warn("DynamicInstanceImpl::~DynamicInstanceImpl: Error unloading: ", m_filepath, " dynamic library.");
             }
             m_instance = nullptr;
         }
@@ -110,16 +110,16 @@ namespace datadog::shared::nativeloader
         }
         else
         {
-            Warn("DynamicInstanceImpl::LoadClassFactory: Error getting IClassFactory from: ", m_filepath);
+            Log::Warn("DynamicInstanceImpl::LoadClassFactory: Error getting IClassFactory from: ", m_filepath);
         }
 
-        Debug("DynamicInstanceImpl::LoadClassFactory: ", res);
+        Log::Debug("DynamicInstanceImpl::LoadClassFactory: ", res);
         return res;
     }
 
     HRESULT DynamicInstanceImpl::LoadInstance(IUnknown* pUnkOuter, REFIID riid)
     {
-        Debug("DynamicInstanceImpl::LoadInstance");
+        Log::Debug("DynamicInstanceImpl::LoadInstance");
 
         // Check if the class factory instance is loaded.
         if (m_classFactory == nullptr)
@@ -128,16 +128,16 @@ namespace datadog::shared::nativeloader
         }
 
         // Creates the profiler callback instance from the class factory
-        Debug("DynamicInstanceImpl::LoadInstance: m_classFactory: ", HexStr(m_classFactory, sizeof(IClassFactory*)));
+        Log::Debug("DynamicInstanceImpl::LoadInstance: m_classFactory: ", HexStr(m_classFactory, sizeof(IClassFactory*)));
         HRESULT res =
             m_classFactory->CreateInstance(nullptr, __uuidof(ICorProfilerCallback10), (void**) &m_corProfilerCallback);
         if (FAILED(res))
         {
             m_corProfilerCallback = nullptr;
-            Warn("DynamicInstanceImpl::LoadInstance: Error getting ICorProfilerCallback10 from: ", m_filepath);
+            Log::Warn("DynamicInstanceImpl::LoadInstance: Error getting ICorProfilerCallback10 from: ", m_filepath);
         }
 
-        Debug("DynamicInstanceImpl::LoadInstance: ", res);
+        Log::Debug("DynamicInstanceImpl::LoadInstance: ", res);
         return res;
     }
 
