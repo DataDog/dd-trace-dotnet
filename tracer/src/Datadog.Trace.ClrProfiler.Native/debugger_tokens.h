2172e845974df0b006e5e6f9fcdf2cd82d3d6c01
Matan Green <matan.green@datadoghq.com>
2022-02-21 14:38:34 +0200

    Simplified the DebuggerTokens according to the refactoring of the CallTargetTokens
    Simplified the DebuggerTokens according to the refactoring of the CallTargetTokens

--- tracer/src/Datadog.Trace.ClrProfiler.Native/debugger_tokens.h
+++ tracer/src/Datadog.Trace.ClrProfiler.Native/debugger_tokens.h
@@ -1,6 +1,8 @@
 #ifndef DD_CLR_PROFILER_DEBUGGER_TOKENS_H_
 #define DD_CLR_PROFILER_DEBUGGER_TOKENS_H_
 
+#include "calltarget_tokens.h"
+
 #include <corhlpr.h>
 
 #include <mutex>
@@ -22,76 +24,29 @@ namespace debugger
 /// Class to control all the token references of the module where the Debugger will be called.
 /// Also provides useful helpers for the rewriting process
 /// </summary>
-class DebuggerTokens
+class DebuggerTokens : public CallTargetTokens
 {
 private:
-    ModuleMetadata* module_metadata_ptr = nullptr;
-    const bool enable_by_ref_instrumentation = false;
-    const bool enable_debugger_state_by_ref = false;
-
-    // CorLib tokens
-    mdAssemblyRef corLibAssemblyRef = mdAssemblyRefNil;
-    mdTypeRef objectTypeRef = mdTypeRefNil;
-    mdTypeRef exTypeRef = mdTypeRefNil;
-    mdTypeRef typeRef = mdTypeRefNil;
-    mdTypeRef runtimeTypeHandleRef = mdTypeRefNil;
-    mdToken getTypeFromHandleToken = mdTokenNil;
-    mdTypeRef runtimeMethodHandleRef = mdTypeRefNil;
-
-    // CallTarget tokens
-    mdAssemblyRef profilerAssemblyRef = mdAssemblyRefNil;
-    mdTypeRef callTargetTypeRef = mdTypeRefNil;
-    mdTypeRef callTargetStateTypeRef = mdTypeRefNil;
-    mdTypeRef callTargetReturnVoidTypeRef = mdTypeRefNil;
-    mdTypeRef callTargetReturnTypeRef = mdTypeRefNil;
-
-    mdMemberRef beginArrayMemberRef = mdMemberRefNil;
     mdMemberRef beginMethodStartMarkerRef = mdMemberRefNil;
     mdMemberRef beginMethodEndMarkerRef = mdMemberRefNil;
     mdMemberRef endMethodEndMarkerRef = mdMemberRefNil;
-    mdMemberRef endVoidMemberRef = mdMemberRefNil;
-
-    mdMemberRef logExceptionRef = mdMemberRefNil;
-
     mdMemberRef logArgRef = mdMemberRefNil;
     mdMemberRef logLocalRef = mdMemberRefNil;
+    mdMemberRef endVoidMemberRef = mdMemberRefNil;
+    mdMemberRef logExceptionRef = mdMemberRefNil;
 
-    mdMemberRef callTargetStateTypeGetDefault = mdMemberRefNil;
-    mdMemberRef callTargetReturnVoidTypeGetDefault = mdMemberRefNil;
-    mdMemberRef getDefaultMemberRef = mdMemberRefNil;
+    HRESULT WriteLogArgOrLocal(void* rewriterWrapperPtr, const TypeSignature& argOrLocal, ILInstr** instruction,
+                               bool isArg);
 
-    ModuleMetadata* GetMetadata();
-    HRESULT EnsureCorLibTokens();
-    HRESULT EnsureBaseDebuggerTokens();
-    mdTypeRef GetTargetStateTypeRef();
-    mdTypeRef GetTargetVoidReturnTypeRef();
-    mdTypeSpec GetTargetReturnValueTypeRef(TypeSignature* returnArgument);
-    mdMemberRef GetCallTargetStateDefaultMemberRef();
-    mdMemberRef GetCallTargetReturnVoidDefaultMemberRef();
-    mdMemberRef GetCallTargetReturnValueDefaultMemberRef(mdTypeSpec callTargetReturnTypeSpec);
-    mdMethodSpec GetCallTargetDefaultValueMethodSpec(TypeSignature* methodArgument);
-    mdToken GetCurrentTypeRef(const TypeInfo* currentType, bool& isValueType);
-
-    HRESULT ModifyLocalSig(ILRewriter* reWriter, TypeSignature* methodReturnValue, ULONG* callTargetStateIndex,
-                           ULONG* exceptionIndex, ULONG* callTargetReturnIndex, ULONG* returnValueIndex,
-                           mdToken* callTargetStateToken, mdToken* exceptionToken, mdToken* callTargetReturnToken);
-
-    //HRESULT WriteBeginMethodWithArgumentsArray(void* rewriterWrapperPtr, mdTypeRef integrationTypeRef,
-    //                                           const TypeInfo* currentType, ILInstr** instruction);
-    HRESULT WriteLogArgOrLocal(void* rewriterWrapperPtr, const TypeSignature& argOrLocal, ILInstr** instruction, bool isArg);
+protected:
+    const WSTRING& GetCallTargetType() override;
+    const WSTRING& GetCallTargetStateType() override;
+    const WSTRING& GetCallTargetReturnType() override;
+    const WSTRING& GetCallTargetReturnGenericType() override;
 
 public:
-    DebuggerTokens(ModuleMetadata* module_metadata_ptr, const bool enableByRefInstrumentation, const bool enableCallTargetStateByRef);
-
-    mdTypeRef GetObjectTypeRef();
-    mdTypeRef GetExceptionTypeRef();
-    mdAssemblyRef GetCorLibAssemblyRef();
-
-    HRESULT ModifyLocalSigAndInitialize(void* rewriterWrapperPtr, FunctionInfo* functionInfo,
-                                        ULONG* callTargetStateIndex, ULONG* exceptionIndex,
-                                        ULONG* callTargetReturnIndex, ULONG* returnValueIndex,
-                                        mdToken* callTargetStateToken, mdToken* exceptionToken,
-                                        mdToken* callTargetReturnToken, ILInstr** firstInstruction);
+    DebuggerTokens(ModuleMetadata* module_metadata_ptr, const bool enableByRefInstrumentation,
+                   const bool enableCallTargetStateByRef);
 
     HRESULT WriteBeginMethod_StartMarker(void* rewriterWrapperPtr, const TypeInfo* currentType, ILInstr** instruction);
 
@@ -102,9 +57,6 @@ public:
 
     HRESULT WriteLogException(void* rewriterWrapperPtr, const TypeInfo* currentType, ILInstr** instruction);
 
-    HRESULT WriteCallTargetReturnGetReturnValue(void* rewriterWrapperPtr, mdTypeSpec callTargetReturnTypeSpec,
-                                                ILInstr** instruction);
-
     HRESULT WriteLogArg(void* rewriterWrapperPtr, const TypeSignature& argument, ILInstr** instruction);
 
     HRESULT WriteLogLocal(void* rewriterWrapperPtr, const TypeSignature& local, ILInstr** instruction);
@@ -114,6 +66,6 @@ public:
     HRESULT WriteEndMethod_EndMarker(void* rewriterWrapperPtr, ILInstr** instruction);
 };
 
-} // namespace trace
+} // namespace debugger
 
 #endif // DD_CLR_PROFILER_DEBUGGER_TOKENS_H_
