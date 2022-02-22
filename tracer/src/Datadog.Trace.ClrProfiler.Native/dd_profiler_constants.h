9be13e02cd204749be668874556795bce973efc3
andrewlock <andrewlock@users.noreply.github.com>
2022-02-22 13:15:20 +0000

    [Version Bump] 2.5.0

--- tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
+++ tracer/src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -80,7 +80,7 @@ const WSTRING system_private_corelib_assemblyName = WStr("System.Private.CoreLib
 const WSTRING datadog_trace_clrprofiler_managed_loader_assemblyName = WStr("Datadog.Trace.ClrProfiler.Managed.Loader");
 
 const WSTRING managed_profiler_full_assembly_version =
-    WStr("Datadog.Trace, Version=2.4.0.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb");
+    WStr("Datadog.Trace, Version=2.5.0.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb");
 
 const WSTRING managed_profiler_name = WStr("Datadog.Trace");
 
@@ -111,7 +111,7 @@ const AssemblyProperty managed_profiler_assembly_property = AssemblyProperty(
                   49,  105, 236, 40,  21,  176, 12, 238, 238, 204, 141, 90,  27,  244, 61,  182, 125, 41,  97,  163,
                   233, 190, 161, 57,  127, 4,   62, 192, 116, 145, 112, 150, 73,  37,  47,  85,  101, 183, 86,  197},
     160, 32772, 1)
-        .WithVersion(2, 4, 0, 0);
+        .WithVersion(2, 5, 0, 0);
 
 } // namespace trace
 
