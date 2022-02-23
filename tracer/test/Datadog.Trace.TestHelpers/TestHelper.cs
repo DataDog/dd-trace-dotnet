a21e96e06eb36b9316b9a2dbb5e2cc6a40f17137
Andrew Lock <andrew.lock@datadoghq.com>
2022-02-23 09:51:08 +0000

    Ignore common test failures (#2477)
    * We get occasional SegFaults in NativeProfilerChecks, but we're not doing anything about them at the moment, so stop failing the build
    
    * Ignore AbandonedMutexException thrown by Coverlet
    
    * Ignore AbandonedMutexException thrown by Coverlet in smoke tests

--- tracer/test/Datadog.Trace.TestHelpers/TestHelper.cs
+++ tracer/test/Datadog.Trace.TestHelpers/TestHelper.cs
@@ -169,6 +169,13 @@ namespace Datadog.Trace.TestHelpers
                 throw new SkipException("Segmentation fault on .NET Core 2.1");
             }
 #endif
+            if (exitCode == 134
+             && standardError?.Contains("System.Threading.AbandonedMutexException: The wait completed due to an abandoned mutex") == true
+             && standardError?.Contains("Coverlet.Core.Instrumentation.Tracker") == true)
+            {
+                // Coverlet occasionally throws AbandonedMutexException during clean up
+                throw new SkipException("Coverlet threw AbandonedMutexException during cleanup");
+            }
 
             Assert.True(exitCode >= 0, $"Process exited with code {exitCode}");
 
