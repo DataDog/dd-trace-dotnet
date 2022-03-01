4cb8983f644973d7007535697168d170cf93ee41
Tony Redondo <tony.redondo@datadoghq.com>
2022-03-01 13:02:29 +0100

    Use #nullable enable and remove some comments.

--- tracer/src/Datadog.Trace/Ci/Agent/MessagePack/CIEventMessagePackFormatter.cs
+++ tracer/src/Datadog.Trace/Ci/Agent/MessagePack/CIEventMessagePackFormatter.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using System;
 using Datadog.Trace.Ci.Agent.Payloads;
 using Datadog.Trace.Ci.Tags;
@@ -14,10 +16,9 @@ namespace Datadog.Trace.Ci.Agent.MessagePack
 {
     internal class CIEventMessagePackFormatter : EventMessagePackFormatter<EventsPayload>
     {
-        // .
         private readonly byte[] _metadataBytes = StringEncoding.UTF8.GetBytes("metadata");
         private readonly byte[] _containerIdBytes = StringEncoding.UTF8.GetBytes("container_id");
-        private readonly byte[] _containerIdValueBytes;
+        private readonly byte[]? _containerIdValueBytes;
         private readonly byte[] _runtimeIdBytes = StringEncoding.UTF8.GetBytes("runtime_id");
         private readonly byte[] _runtimeIdValueBytes = StringEncoding.UTF8.GetBytes(Tracer.RuntimeId);
         private readonly byte[] _languageNameBytes = StringEncoding.UTF8.GetBytes("language");
@@ -26,19 +27,19 @@ namespace Datadog.Trace.Ci.Agent.MessagePack
         private readonly byte[] _languageInterpreterValueBytes = StringEncoding.UTF8.GetBytes(FrameworkDescription.Instance.Name);
         private readonly byte[] _languageVersionBytes = StringEncoding.UTF8.GetBytes(CommonTags.RuntimeVersion);
         private readonly byte[] _languageVersionValueBytes = StringEncoding.UTF8.GetBytes(FrameworkDescription.Instance.ProductVersion);
-        // .
+
         private readonly byte[] _ciLibraryVersionBytes = StringEncoding.UTF8.GetBytes(CommonTags.LibraryVersion);
         private readonly byte[] _ciLibraryVersionValueBytes = StringEncoding.UTF8.GetBytes(TracerConstants.AssemblyVersion);
-        // .
+
         private readonly byte[] _environmentBytes = StringEncoding.UTF8.GetBytes("env");
-        private readonly byte[] _environmentValueBytes;
+        private readonly byte[]? _environmentValueBytes;
         private readonly byte[] _serviceBytes = StringEncoding.UTF8.GetBytes("service");
-        private readonly byte[] _serviceValueBytes;
+        private readonly byte[]? _serviceValueBytes;
         private readonly byte[] _appVersionBytes = StringEncoding.UTF8.GetBytes("app_version");
-        private readonly byte[] _appVersionValueBytes;
+        private readonly byte[]? _appVersionValueBytes;
         private readonly byte[] _hostnameBytes = StringEncoding.UTF8.GetBytes("hostname");
         private readonly byte[] _hostnameValueBytes = StringEncoding.UTF8.GetBytes(HostMetadata.Instance.Hostname);
-        // .
+
         private readonly byte[] _eventsBytes = StringEncoding.UTF8.GetBytes("events");
 
         private readonly ArraySegment<byte> _envelopBytes;
@@ -88,7 +89,7 @@ namespace Datadog.Trace.Ci.Agent.MessagePack
             _envelopBytes = GetEnvelopeArraySegment();
         }
 
-        public override int Serialize(ref byte[] bytes, int offset, EventsPayload value, IFormatterResolver formatterResolver)
+        public override int Serialize(ref byte[] bytes, int offset, EventsPayload? value, IFormatterResolver formatterResolver)
         {
             if (value is null)
             {
@@ -99,7 +100,7 @@ namespace Datadog.Trace.Ci.Agent.MessagePack
 
             // Write envelope
             MessagePackBinary.EnsureCapacity(ref bytes, offset, _envelopBytes.Count);
-            Buffer.BlockCopy(_envelopBytes.Array, _envelopBytes.Offset, bytes, offset, _envelopBytes.Count);
+            Buffer.BlockCopy(_envelopBytes.Array!, _envelopBytes.Offset, bytes, offset, _envelopBytes.Count);
             offset += _envelopBytes.Count;
 
             // Write events
@@ -107,7 +108,7 @@ namespace Datadog.Trace.Ci.Agent.MessagePack
             {
                 var data = value.Events.Data;
                 MessagePackBinary.EnsureCapacity(ref bytes, offset, data.Count);
-                Buffer.BlockCopy(data.Array, data.Offset, bytes, offset, data.Count);
+                Buffer.BlockCopy(data.Array!, data.Offset, bytes, offset, data.Count);
                 offset += data.Count;
             }
             else
