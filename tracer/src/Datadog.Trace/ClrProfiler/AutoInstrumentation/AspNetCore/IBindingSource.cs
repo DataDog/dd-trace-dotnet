175d4ad1be286bcf8cc6b167b87731e0d22b5632
Anna <anna.yafi@datadoghq.com>
2022-03-02 17:15:57 +0100

    Changing snapshot files to match new ressponse content length

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNetCore/IBindingSource.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNetCore/IBindingSource.cs
@@ -2,6 +2,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+#if !NETFRAMEWORK
 
 using System;
 using System.Collections.Generic;
@@ -22,3 +23,4 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNetCore
         string Id { get; }
     }
 }
+#endif
