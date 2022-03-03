4ab3d8780bec1c5828016f69bbb97e3466cad2c1
Andrew Lock <andrew.lock@datadoghq.com>
2022-03-03 17:00:33 +0000

    Run GeneratePackageVersions

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestMinorsAlpine.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestMinorsAlpine.g.cs
@@ -17,7 +17,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace Datadog.Trace.ClrProfiler.IntegrationTests
 {
     [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1516:Elements must be separated by blank line", Justification = "This is an auto-generated file.")]
-    public class PackageVersionsLatestMinors
+    public class PackageVersionsLatestMinorsAlpine
     {
       public static IEnumerable<object[]> AwsSqs =>
 
