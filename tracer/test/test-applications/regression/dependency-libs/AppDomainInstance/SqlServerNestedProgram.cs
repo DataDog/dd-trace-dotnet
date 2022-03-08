4a90ac90abe8b9779d2a91285284c296ff6ba047
Omer Raviv <omerraviv@gmail.com>
2022-03-09 00:01:54 +0200

    Fix ambiguity by renaming AppDomain.Instance to AppDomainInstance

--- tracer/test/test-applications/regression/dependency-libs/AppDomainInstance/SqlServerNestedProgram.cs
+++ tracer/test/test-applications/regression/dependency-libs/AppDomainInstance/SqlServerNestedProgram.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 
-namespace AppDomain.Instance
+namespace AppDomainInstance
 {
     public class SqlServerNestedProgram : NestedProgram
     {
