5050d2a74f5cd086786eb0fb3da2ee015faa0377
Omer Raviv <omer.raviv@datadoghq.com>
2022-03-23 11:46:00 -0700

    Add basic PDB reading capability (and vendor-in dnlib) (#2592)
    * Vendored in dnlib.
    
    * Fix ambiguity by renaming OSPlatform to OSPlatformName
    
    * Fix ambiguity by renaming AppDomain.Instance to AppDomainInstance
    
    * Update PublicApiTests.AssemblyReferencesHaveNotChanged verified snapshot
    
    * Add basic DatadogPdbReader and a very basic test.
    
    * Remove dependency on Microsoft.DiaSymReader.Native
    
    * Properly Dipose() the ModuleDefMD when we dispose DatadogPdbReader
    
    * Remove stream that was not getting properly Disposed

---
+++ tracer/src/Datadog.Trace/OSPlatformName.cs
@@ -0,0 +1,14 @@
+// <copyright file="OSPlatformName.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace
+{
+    internal static class OSPlatformName
+    {
+        public const string Windows = "Windows";
+        public const string Linux = "Linux";
+        public const string MacOS = "MacOS";
+    }
+}
