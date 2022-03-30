ab1b5fa808d5992f27aed0b5fe9540d6ac95a827
Zach Montoya <zach.montoya@datadoghq.com>
2022-03-30 10:16:56 -0700

    Create crash condition between manual instrumentation and automatic instrumentation when - There's an assembly mismatch between manual and automatic instrumentation so that the application loads a different version of Datadog.Trace.dll than the automatic instrumentation - An assembly references Datadog.Trace.dll (usually application code) and that version of Datadog.Trace.dll does not have the integration type specified by the automatic instrumentation

---
+++ tracer/test/test-applications/integrations/Samples.TraceAnnotations.VersionMismatch/Program.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Datadog.Trace;
+
+namespace Samples.TraceAnnotations.VersionMismatch
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await RunTestsAsync();
+        }
+
+        private static async Task RunTestsAsync()
+        {
+            using var scope = Tracer.Instance.StartActive("manual.request");
+            scope.Span.ResourceName = "manual.RunTestsAsync";
+
+            // Instrument some other library method that is not in mscorlib / System.Runtime
+            using var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+
+            var testType = new TestType();
+            testType.VoidMethod("Hello world", 42, Tuple.Create(1, 2));
+            testType.ReturnValueMethod("Hello world", 42, Tuple.Create(1, 2));
+            testType.ReturnReferenceMethod("Hello world", 42, Tuple.Create(1, 2));
+            testType.ReturnNullMethod("Hello world", 42, Tuple.Create(1, 2));
+            testType.ReturnGenericMethod<string, string, Tuple<int, int>>("Hello world", 42, Tuple.Create(1, 2));
+            testType.ReturnGenericMethod<int, int, Tuple<int, int>>(42, 99, Tuple.Create(1, 2));
+            await testType.ReturnTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testType.ReturnTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testType.ReturnValueTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testType.ReturnValueTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+
+            await Task.Delay(500);
+
+            var testTypeGenericString = new TestTypeGeneric<string>();
+            testTypeGenericString.VoidMethod("Hello World", 42, Tuple.Create(1, 2));
+            testTypeGenericString.ReturnValueMethod("Hello World", 42, Tuple.Create(1, 2));
+            testTypeGenericString.ReturnReferenceMethod("Hello World", 42, Tuple.Create(1, 2));
+            testTypeGenericString.ReturnNullMethod("Hello world", 42, Tuple.Create(1, 2));
+            testTypeGenericString.ReturnGenericMethod<string, Tuple<int, int>>("Hello World", 42, Tuple.Create(1, 2));
+            //
+            await testTypeGenericString.ReturnTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testTypeGenericString.ReturnTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testTypeGenericString.ReturnValueTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testTypeGenericString.ReturnValueTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+
+            await Task.Delay(500);
+
+            var testTypeStruct = new TestTypeStruct();
+            testTypeStruct.VoidMethod("Hello World", 42, Tuple.Create(1, 2));
+            testTypeStruct.ReturnValueMethod("Hello World", 42, Tuple.Create(1, 2));
+            testTypeStruct.ReturnReferenceMethod("Hello World", 42, Tuple.Create(1, 2));
+            testTypeStruct.ReturnNullMethod("Hello world", 42, Tuple.Create(1, 2));
+            testTypeStruct.ReturnGenericMethod<string, int, Tuple<int, int>>(42, 99, Tuple.Create(1, 2));
+            testTypeStruct.ReturnGenericMethod<int, string, Tuple<int, int>>("Hello World", 42, Tuple.Create(1, 2));
+            await testTypeStruct.ReturnTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testTypeStruct.ReturnTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testTypeStruct.ReturnValueTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await testTypeStruct.ReturnValueTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+
+            await Task.Delay(500);
+
+            TestTypeStatic.VoidMethod("Hello World", 42, Tuple.Create(1, 2));
+            TestTypeStatic.ReturnValueMethod("Hello World", 42, Tuple.Create(1, 2));
+            TestTypeStatic.ReturnReferenceMethod("Hello World", 42, Tuple.Create(1, 2));
+            TestTypeStatic.ReturnNullMethod("Hello world", 42, Tuple.Create(1, 2));
+            TestTypeStatic.ReturnGenericMethod<string, int, Tuple<int, int>>(42, 99, Tuple.Create(1, 2));
+            TestTypeStatic.ReturnGenericMethod<int, string, Tuple<int, int>>("Hello World", 42, Tuple.Create(1, 2));
+            await TestTypeStatic.ReturnTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await TestTypeStatic.ReturnTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+            await TestTypeStatic.ReturnValueTaskMethod("Hello world", 42, Tuple.Create(1, 2));
+            await TestTypeStatic.ReturnValueTaskTMethod("Hello world", 42, Tuple.Create(1, 2));
+        }
+    }
+}
