6ab11962929c02816529c6a3ffc7421104e433dc
Tony Redondo <tony.redondo@datadoghq.com>
2022-04-04 20:26:16 +0200

    [CIApp] - Implement specific CLI Run CI Settings and Descriptions (#2625)
    * Implement specific CLI Run CI Settings
    
    * Add command descriptions.
    
    * Remove unnecessary boolean argument.
    
    * Update tracer/src/Datadog.Trace.Tools.Runner/RunSettings.cs
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/src/Datadog.Trace.Tools.Runner/RunCiCommand.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/RunCiCommand.cs
@@ -3,20 +3,29 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Datadog.Trace.Tools.Runner
 {
-    internal class RunCiCommand : RunCommand
+    internal class RunCiCommand : Command<RunCiSettings>
     {
         public RunCiCommand(ApplicationContext applicationContext)
-            : base(applicationContext)
         {
+            ApplicationContext = applicationContext;
         }
 
-        public override int Execute(CommandContext context, RunSettings settings)
+        protected ApplicationContext ApplicationContext { get; }
+
+        public override int Execute(CommandContext context, RunCiSettings settings)
         {
-            return Execute(context, settings, enableCiMode: true);
+            return RunHelper.Execute(ApplicationContext, context, settings);
+        }
+
+        public override ValidationResult Validate(CommandContext context, RunCiSettings settings)
+        {
+            var runValidation = RunHelper.Validate(context, settings);
+            return !runValidation.Successful ? runValidation : base.Validate(context, settings);
         }
     }
 }
