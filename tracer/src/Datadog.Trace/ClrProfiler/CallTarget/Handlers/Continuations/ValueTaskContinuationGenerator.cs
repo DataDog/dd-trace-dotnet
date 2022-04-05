7b93e0c3743df9e3654d3cd965adba67299715a5
Andrew Lock <andrew.lock@datadoghq.com>
2022-04-05 15:06:48 +0100

    Simplify framework conditionals
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

--- tracer/src/Datadog.Trace/ClrProfiler/CallTarget/Handlers/Continuations/ValueTaskContinuationGenerator.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/CallTarget/Handlers/Continuations/ValueTaskContinuationGenerator.cs
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#if NETCOREAPP3_1 || NET5_0 || NETCOREAPP3_1_OR_GREATER
+#if NETCOREAPP3_1_OR_GREATER
 using System;
 using System.Threading.Tasks;
 
