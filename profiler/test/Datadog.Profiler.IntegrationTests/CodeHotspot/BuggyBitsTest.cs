2faa64f8acbd2a1a913ed34db79228332d6ead85
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-04-06 11:54:05 +0200

    Fix buggy bits test and ASAN test in CI

--- profiler/test/Datadog.Profiler.IntegrationTests/CodeHotspot/BuggyBitsTest.cs
+++ profiler/test/Datadog.Profiler.IntegrationTests/CodeHotspot/BuggyBitsTest.cs
@@ -3,13 +3,16 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
 // </copyright>
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using Datadog.Profiler.IntegrationTests.Helpers;
 using Datadog.Profiler.SmokeTests;
+using Datadog.Trace;
+using Datadog.Trace.TestHelpers;
+using MessagePack;
 using Perftools.Profiles.Tests;
 using Xunit;
 using Xunit.Abstractions;
@@ -18,6 +21,7 @@ namespace Datadog.Profiler.IntegrationTests.CodeHotspot
 {
     public class BuggyBitsTest
     {
+        private static readonly Regex RuntimeIdPattern = new("runtime-id:(?<runtimeId>[A-Z0-9-]+)", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase);
         private readonly ITestOutputHelper _output;
 
         public BuggyBitsTest(ITestOutputHelper output)
@@ -33,41 +37,31 @@ namespace Datadog.Profiler.IntegrationTests.CodeHotspot
 
             using var agent = new MockDatadogAgent(_output);
 
-            var profilerRuntimeId = string.Empty;
+            var profilerRuntimeIds = new List<string>();
             agent.ProfilerRequestReceived += (object sender, EventArgs<HttpListenerContext> ctx) =>
             {
-                var rid = ctx.Value.Request.Headers["runtime-id"];
-                Assert.NotNull(rid);
-
-                if (profilerRuntimeId != string.Empty)
-                {
-                    Assert.True(profilerRuntimeId == rid);
-                }
-                else
-                {
-                    profilerRuntimeId = rid;
-                }
+                profilerRuntimeIds.Add(ExtractRuntimeIdFromProfilerRequest(ctx.Value.Request));
             };
 
-            var tracerRuntimeId = string.Empty;
+            var tracerRuntimeIds = new List<string>();
             agent.TracerRequestReceived += (object sender, EventArgs<HttpListenerContext> ctx) =>
             {
-                var rid = ctx.Value.Request.Headers["runtime-id"];
-                Assert.NotNull(rid);
-
-                if (tracerRuntimeId != string.Empty)
-                {
-                    Assert.True(tracerRuntimeId == rid);
-                }
-                else
-                {
-                    tracerRuntimeId = rid;
-                }
+                tracerRuntimeIds.AddRange(ExtractRuntimeIdsFromTracerRequest(ctx.Value.Request));
             };
 
             runner.Run(agent);
 
             Assert.True(agent.NbCallsOnProfilingEndpoint > 0);
+
+            Assert.Single(profilerRuntimeIds.Distinct());
+            Assert.Single(tracerRuntimeIds.Distinct());
+
+            var profilerRuntimeId = profilerRuntimeIds.First();
+
+            Assert.NotNull(profilerRuntimeId);
+
+            var tracerRuntimeId = tracerRuntimeIds.First();
+            Assert.NotNull(tracerRuntimeId);
 
             Assert.Equal(profilerRuntimeId, tracerRuntimeId);
 
@@ -90,6 +84,38 @@ namespace Datadog.Profiler.IntegrationTests.CodeHotspot
 
             var tracingContexts = GetTracingContexts(runner.Environment.PprofDir);
             Assert.Empty(tracingContexts);
+        }
+
+        private static HashSet<string> ExtractRuntimeIdsFromTracerRequest(HttpListenerRequest request)
+        {
+            var traces = MessagePackSerializer.Deserialize<IList<IList<MockSpan>>>(request.InputStream);
+
+            var result = new HashSet<string>();
+            foreach (var trace in traces)
+            {
+                foreach (var span in trace)
+                {
+                    var currentRuntimeId = string.Empty;
+                    if (span.Tags?.TryGetValue(Tags.RuntimeId, out currentRuntimeId) ?? false)
+                    {
+                        result.Add(currentRuntimeId);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static string ExtractRuntimeIdFromProfilerRequest(HttpListenerRequest request)
+        {
+            string text;
+            using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            var match = RuntimeIdPattern.Match(text);
+            return match.Groups["runtimeId"].Value;
         }
 
         private static List<PprofHelper.Label> GetTracingContexts(string pprofDir)
