190d2f1b16cbf4e03b7ab6151aca79f33a9017dd
Andrew Lock <andrew.lock@datadoghq.com>
2022-04-13 12:52:22 +0100

    minor: remove unused parameters in CiIAgentlessWriter

--- tracer/src/Datadog.Trace/Ci/CITracerManagerFactory.cs
+++ tracer/src/Datadog.Trace/Ci/CITracerManagerFactory.cs
@@ -56,7 +56,7 @@ namespace Datadog.Trace.Ci
             {
                 if (!string.IsNullOrEmpty(_settings.ApiKey))
                 {
-                    return new CIAgentlessWriter(settings, sampler, new CIWriterHttpSender(GetRequestFactory(settings)));
+                    return new CIAgentlessWriter(new CIWriterHttpSender(GetRequestFactory(settings)));
                 }
                 else
                 {
