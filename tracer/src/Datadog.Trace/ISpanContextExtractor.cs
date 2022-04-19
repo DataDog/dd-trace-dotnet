ef2a8a49828b224d7f1109366266022fe25935ee
Pierre Bonet <pierre.bonet@datadoghq.com>
2022-04-19 14:13:40 +0200

    Rename and add kafka tests

--- tracer/src/Datadog.Trace/ISpanContextExtractor.cs
+++ tracer/src/Datadog.Trace/ISpanContextExtractor.cs
@@ -1,11 +1,10 @@
-// <copyright file="SpanContextGetter.cs" company="Datadog">
+// <copyright file="ISpanContextExtractor.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
 using System;
 using System.Collections.Generic;
-using Datadog.Trace.Propagators;
 
 #nullable enable
 
@@ -16,7 +15,7 @@ namespace Datadog.Trace
     /// This can happen for instance when libraries add an extra layer above the instrumented ones
     /// (eg consuming Kafka messages and enqueuing them prior to generate a span).
     /// </summary>
-    public class SpanContextGetter : ISpanContextGetter
+    public interface ISpanContextExtractor
     {
         /// <summary>
         /// Given a SpanContext carrier and a function to access the values, this method will extract SpanContext if any
@@ -25,7 +24,6 @@ namespace Datadog.Trace
         /// <param name="getter">Given a key name, returns values from the carrier</param>
         /// <typeparam name="TCarrier">Type of the carrier</typeparam>
         /// <returns>A potentially null Datadog SpanContext</returns>
-        public ISpanContext? GetContext<TCarrier>(TCarrier carrier, Func<TCarrier, string, IEnumerable<string?>> getter)
-            => SpanContextPropagator.Instance.Extract(carrier, getter);
+        public ISpanContext? ExtractContext<TCarrier>(TCarrier carrier, Func<TCarrier, string, IEnumerable<string?>> getter);
     }
 }
