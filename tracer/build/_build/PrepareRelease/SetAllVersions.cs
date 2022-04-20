4cbd768da4f3a80db1fdaa997b7389d01ee4201f
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-04-20 14:24:59 +0200

    Add the profiler deployment script to the bumping process

--- tracer/build/_build/PrepareRelease/SetAllVersions.cs
+++ tracer/build/_build/PrepareRelease/SetAllVersions.cs
@@ -177,6 +177,10 @@ namespace PrepareRelease
                 text => FullVersionReplace(text, "."));
 
             SynchronizeVersion(
+                "../.github/scripts/package_and_deploy.sh",
+                text => FullVersionReplace(text, ".", prefix: "current_profiler_version=\""));
+
+            SynchronizeVersion(
                 "../profiler/src/ProfilerEngine/ProductVersion.props",
                 PropsVersionReplace);
 
