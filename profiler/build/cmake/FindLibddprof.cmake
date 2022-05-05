09e6e585b1fef85275bdc00a23f08f53869c6757
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-05-06 00:47:36 +0200

    Cleanup FindLibddprof.cmake script

--- profiler/build/cmake/FindLibddprof.cmake
+++ profiler/build/cmake/FindLibddprof.cmake
@@ -4,10 +4,10 @@ set(LIBDDPROF_VERSION "v0.2.0" CACHE STRING "libddprof version")
 
 if (DEFINED ENV{IsAlpine} AND "$ENV{IsAlpine}" MATCHES "true")
    set(SHA256_LIBDDPROF "d519a6241d78260522624b8e79e98502510f11d5d9551f5f80fc1134e95fa146" CACHE STRING "libddprof sha256")
-   set(LIBDDPROF_BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/libddprof-${LIBDDPROF_VERSION}/src/libddprof-build/libddprof/libddprof-x86_64-alpine-linux-musl)
+   set(LIBDDPROF_BINARY_FOLDER libddprof-x86_64-alpine-linux-musl)
 else()
    set(SHA256_LIBDDPROF "cba0f24074d44781d7252b912faff50d330957e84a8f40a172a8138e81001f27" CACHE STRING "libddprof sha256")
-   set(LIBDDPROF_BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/libddprof-${LIBDDPROF_VERSION}/src/libddprof-build/libddprof/libddprof-x86_64-unknown-linux-gnu)
+   set(LIBDDPROF_BINARY_FOLDER libddprof-x86_64-unknown-linux-gnu)
 endif()
 
 ExternalProject_Add(libddprof
@@ -23,13 +23,11 @@ ExternalProject_Get_property(libddprof BINARY_DIR)
 set_property(DIRECTORY PROPERTY ADDITIONAL_MAKE_CLEAN_FILES
     ${BINARY_DIR})
 
-message (STATUS "Libddprof binary dir ${BINARY_DIR}")
-
-set(LIBDDPROF_REL_FFI_LIB ${LIBDDPROF_BASE_DIR}/lib/libddprof_ffi.a)
+set(LIBDDPROF_REL_FFI_LIB ${BINARY_DIR}/${LIBDDPROF_BINARY_FOLDER}/lib/libddprof_ffi.a)
 
 list(APPEND
     LIBDDPROF_INCLUDE_DIR
-    ${LIBDDPROF_BASE_DIR}/include)
+    ${BINARY_DIR}/${LIBDDPROF_BINARY_FOLDER}/include)
 
 add_library(libddprof-lib STATIC IMPORTED)
 set_property(TARGET libddprof-lib PROPERTY
