cbcf01739357fe85f4b6ee341dee052bfb39a981
Kevin Gosse <krix33@gmail.com>
2022-05-09 17:46:52 +0200

    Fix test

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/AssemblyLoadContextRedirectSmokeTest.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/AssemblyLoadContextRedirectSmokeTest.cs
@@ -1,14 +1,16 @@
-// <copyright file="AssemblyLoadContextRedirectTest.cs" company="Datadog">
+// <copyright file="AssemblyLoadContextRedirectSmokeTest.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+
+#if !NETFRAMEWORK
 
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
 {
-    public class AssemblyLoadContextRedirectTest : SmokeTestBase
+    public class AssemblyLoadContextRedirectSmokeTest : SmokeTestBase
     {
         public AssemblyLoadContextRedirectTest(ITestOutputHelper output)
             : base(output, "AssemblyLoadContextRedirect")
@@ -23,3 +25,5 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
         }
     }
 }
+
+#endif
--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/AssemblyLoadFileNotFoundExceptionSmokeTest.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/AssemblyLoadFileNotFoundExceptionSmokeTest.cs
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#if !NETFRAMEWORK
-
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,5 +23,3 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
         }
     }
 }
-
-#endif
