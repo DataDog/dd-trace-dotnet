b49a30aea582e597b7be842db909545ca8cd6eb8
Tony Redondo <tony.redondo@datadoghq.com>
2022-05-09 10:35:14 +0200

    [All] - Refactor ITags/TagsList tracer implementation (#2753)
    * TagsList refactoring.
    
    * Tags refactor.
    
    * Apply UTF8 pre-encoding
    
    * changes
    
    * Changes
    
    * Changes.
    
    * Remove linq.
    
    * Fix tests
    
    * changes
    
    * Update comments.
    
    * fix
    
    * Fix tests
    
    * revert change
    
    * Update tracer/src/Datadog.Trace/Tagging/TagsList.cs
    
    Co-authored-by: Pierre Bonet <pierre.bonet@datadoghq.com>
    
    * Change variable names
    
    * Remove unused class.
    
    * Reduce string allocation in the source generator.
    
    * Update tracer/src/Datadog.Trace.SourceGenerators/TagsListGenerator/Sources.cs
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
    
    * Fix tests and changes.
    
    Co-authored-by: Pierre Bonet <pierre.bonet@datadoghq.com>
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

--- tracer/src/Datadog.Trace/Generated/net6.0/Datadog.Trace.SourceGenerators/Datadog.Trace.SourceGenerators.TagsListGenerator.TagListGenerator/RedisTags.g.cs
+++ tracer/src/Datadog.Trace/Generated/net6.0/Datadog.Trace.SourceGenerators/Datadog.Trace.SourceGenerators.TagsListGenerator.TagListGenerator/RedisTags.g.cs
@@ -2,16 +2,22 @@
 #nullable enable
 
 using Datadog.Trace.Processors;
+using Datadog.Trace.Tagging;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Redis
 {
     partial class RedisTags
     {
-        private static readonly byte[] SpanKindBytes = Datadog.Trace.Vendors.MessagePack.StringEncoding.UTF8.GetBytes("span.kind");
-        private static readonly byte[] InstrumentationNameBytes = Datadog.Trace.Vendors.MessagePack.StringEncoding.UTF8.GetBytes("component");
-        private static readonly byte[] RawCommandBytes = Datadog.Trace.Vendors.MessagePack.StringEncoding.UTF8.GetBytes("redis.raw_command");
-        private static readonly byte[] HostBytes = Datadog.Trace.Vendors.MessagePack.StringEncoding.UTF8.GetBytes("out.host");
-        private static readonly byte[] PortBytes = Datadog.Trace.Vendors.MessagePack.StringEncoding.UTF8.GetBytes("out.port");
+        // SpanKindBytes = System.Text.Encoding.UTF8.GetBytes("span.kind");
+        private static readonly byte[] SpanKindBytes = new byte[] { 115, 112, 97, 110, 46, 107, 105, 110, 100 };
+        // InstrumentationNameBytes = System.Text.Encoding.UTF8.GetBytes("component");
+        private static readonly byte[] InstrumentationNameBytes = new byte[] { 99, 111, 109, 112, 111, 110, 101, 110, 116 };
+        // RawCommandBytes = System.Text.Encoding.UTF8.GetBytes("redis.raw_command");
+        private static readonly byte[] RawCommandBytes = new byte[] { 114, 101, 100, 105, 115, 46, 114, 97, 119, 95, 99, 111, 109, 109, 97, 110, 100 };
+        // HostBytes = System.Text.Encoding.UTF8.GetBytes("out.host");
+        private static readonly byte[] HostBytes = new byte[] { 111, 117, 116, 46, 104, 111, 115, 116 };
+        // PortBytes = System.Text.Encoding.UTF8.GetBytes("out.port");
+        private static readonly byte[] PortBytes = new byte[] { 111, 117, 116, 46, 112, 111, 114, 116 };
 
         public override string? GetTag(string key)
         {
@@ -45,73 +51,67 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Redis
             }
         }
 
-        protected override int WriteAdditionalTags(ref byte[] bytes, ref int offset, ITagProcessor[] tagProcessors)
+        public override void EnumerateTags<TProcessor>(ref TProcessor processor)
         {
-            var count = 0;
-            if (SpanKind != null)
+            if (SpanKind is not null)
             {
-                count++;
-                WriteTag(ref bytes, ref offset, SpanKindBytes, SpanKind, tagProcessors);
+                processor.Process(new TagItem<string>("span.kind", SpanKind, SpanKindBytes));
             }
 
-            if (InstrumentationName != null)
+            if (InstrumentationName is not null)
             {
-                count++;
-                WriteTag(ref bytes, ref offset, InstrumentationNameBytes, InstrumentationName, tagProcessors);
+                processor.Process(new TagItem<string>("component", InstrumentationName, InstrumentationNameBytes));
             }
 
-            if (RawCommand != null)
+            if (RawCommand is not null)
             {
-                count++;
-                WriteTag(ref bytes, ref offset, RawCommandBytes, RawCommand, tagProcessors);
+                processor.Process(new TagItem<string>("redis.raw_command", RawCommand, RawCommandBytes));
             }
 
-            if (Host != null)
+            if (Host is not null)
             {
-                count++;
-                WriteTag(ref bytes, ref offset, HostBytes, Host, tagProcessors);
+                processor.Process(new TagItem<string>("out.host", Host, HostBytes));
             }
 
-            if (Port != null)
+            if (Port is not null)
             {
-                count++;
-                WriteTag(ref bytes, ref offset, PortBytes, Port, tagProcessors);
+                processor.Process(new TagItem<string>("out.port", Port, PortBytes));
             }
 
-            return count + base.WriteAdditionalTags(ref bytes, ref offset, tagProcessors);
+            base.EnumerateTags(ref processor);
         }
 
         protected override void WriteAdditionalTags(System.Text.StringBuilder sb)
         {
-            if (SpanKind != null)
+            if (SpanKind is not null)
             {
                 sb.Append("span.kind (tag):")
                   .Append(SpanKind)
                   .Append(',');
             }
 
-            if (InstrumentationName != null)
+            if (InstrumentationName is not null)
             {
                 sb.Append("component (tag):")
                   .Append(InstrumentationName)
                   .Append(',');
             }
 
-            if (RawCommand != null)
+            if (RawCommand is not null)
             {
                 sb.Append("redis.raw_command (tag):")
                   .Append(RawCommand)
                   .Append(',');
             }
 
-            if (Host != null)
+            if (Host is not null)
             {
                 sb.Append("out.host (tag):")
                   .Append(Host)
                   .Append(',');
             }
 
-            if (Port != null)
+            if (Port is not null)
             {
                 sb.Append("out.port (tag):")
                   .Append(Port)
