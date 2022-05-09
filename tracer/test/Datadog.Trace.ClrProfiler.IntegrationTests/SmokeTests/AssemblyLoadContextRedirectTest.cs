6fa9214be880d1b7eab4873663f9b7f48c072f41
Kevin Gosse <krix33@gmail.com>
2022-05-09 15:45:23 +0200

    Add a smoke test for the ALC redirect issue

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/AssemblyLoadContextRedirectTest.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/AssemblyLoadContextRedirectTest.cs
@@ -1,19 +1,17 @@
-// <copyright file="AssemblyLoadFileNotFoundExceptionSmokeTest.cs" company="Datadog">
+// <copyright file="AssemblyLoadContextRedirectTest.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
-
-#if !NETFRAMEWORK
 
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
 {
-    public class AssemblyLoadFileNotFoundExceptionSmokeTest : SmokeTestBase
+    public class AssemblyLoadContextRedirectTest : SmokeTestBase
     {
-        public AssemblyLoadFileNotFoundExceptionSmokeTest(ITestOutputHelper output)
-            : base(output, "AssemblyLoad.FileNotFoundException")
+        public AssemblyLoadContextRedirectTest(ITestOutputHelper output)
+            : base(output, "AssemblyLoadContextRedirect")
         {
         }
 
@@ -21,9 +19,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
         [Trait("Category", "Smoke")]
         public void NoExceptions()
         {
-            CheckForSmoke(shouldDeserializeTraces: false);
+            CheckForSmoke();
         }
     }
 }
-
-#endif
