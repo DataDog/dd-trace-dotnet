1a71bd5fa1302c18b59b5f9893772f2e3aa25e27
Andrew Lock <andrew.lock@datadoghq.com>
2022-05-24 09:06:37 +0100

    Execution Benchmark and Throughput cleanup (#2790)
    * Update throughput tests
    
    Remove non-ngen scenario from tests
    Switch to agentless uploading for CIApp
    
    * Update execution benchmarks
    
    Don't test the non-NGEN scenarios any more
    Use dedicated benchmark agents - split across two agent to reduce overall execution time
    Switch to testing net6 instead of net5 - we have dedicated net6 targets, and net5 is EOL now

--- tracer/build/crank/run.sh
+++ tracer/build/crank/run.sh
@@ -7,6 +7,7 @@ echo "SYSTEM_PULLREQUEST_SOURCECOMMITID=$SYSTEM_PULLREQUEST_SOURCECOMMITID"
 echo "BUILD_SOURCEVERSION=$BUILD_SOURCEVERSION"
 echo "SYSTEM_PULLREQUEST_SOURCEREPOSITORYURI=$SYSTEM_PULLREQUEST_SOURCEREPOSITORYURI"
 echo "BUILD_REPOSITORY_URI=$BUILD_REPOSITORY_URI"
+echo "DD_CIVISIBILITY_AGENTLESS_ENABLED=$DD_CIVISIBILITY_AGENTLESS_ENABLED"
 
 repo="$SYSTEM_PULLREQUEST_SOURCEREPOSITORYURI"
 commit_sha="$SYSTEM_PULLREQUEST_SOURCECOMMITID"
@@ -31,10 +32,6 @@ if [ "$1" = "windows" ]; then
     dd-trace --crank-import="baseline_windows.json"
     rm baseline_windows.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget --profile windows --json calltarget_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
-    dd-trace --crank-import="calltarget_windows.json"
-    rm calltarget_windows.json
-
     crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget_ngen --profile windows --json calltarget_ngen_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget_ngen --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="calltarget_ngen_windows.json"
     rm calltarget_ngen_windows.json
@@ -45,10 +42,6 @@ elif [ "$1" = "linux" ]; then
     crank --config Samples.AspNetCoreSimpleController.yml --scenario baseline --profile linux --json baseline_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=baseline --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="baseline_linux.json"
     rm baseline_linux.json
-
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget --profile linux --json calltarget_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
-    dd-trace --crank-import="calltarget_linux.json"
-    rm calltarget_linux.json
 
     crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget_ngen --profile linux --json calltarget_ngen_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget_ngen --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="calltarget_ngen_linux.json"
@@ -61,9 +54,9 @@ elif [ "$1" = "linux_arm64" ]; then
     dd-trace --crank-import="baseline_linux_arm64.json"
     rm baseline_linux_arm64.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget --profile linux_arm64 --json calltarget_linux_arm64.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget --property profile=linux_arm64 --property arch=arm64 --variable commit_hash=$commit_sha
-    dd-trace --crank-import="calltarget_linux_arm64.json"
-    rm calltarget_linux_arm64.json
+    crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget_ngen --profile linux_arm64 --json calltarget_ngen_linux_arm64.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget_ngen --property profile=linux_arm64 --property arch=arm64 --variable commit_hash=$commit_sha
+    dd-trace --crank-import="calltarget_ngen_linux_arm64.json"
+    rm calltarget_ngen_linux_arm64.json
 
 else
     echo "Unknown argument $1"
