13ac8d2196d747c732d283cf8d0d31f21e5a0385
Andrew Lock <andrew.lock@datadoghq.com>
2022-05-27 14:00:53 +0100

    Add tests for when the mvc integration doesn't run
    In the tests, we're just disabling the MVC integration to simulate this, but in practice it will likely occur for other reasons
    
    Note that this highlighted a (known?) issue when running in Classic mode - the /BadRequestWithStatusCode/401 path _should_ mark the span as 500 (as that's what's returned by the server) but it remains as 200 in this snapshots.
    
    It also demonstrates the problem with resource names when the MVC integration is not executed - the resource name contains the virtual app path prefix

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetMvc5Tests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AspNet/AspNetMvc5Tests.cs
@@ -72,6 +72,42 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         }
     }
 
+    [Collection("IisTests")]
+    public class AspNetMvc5TestsModuleOnlyClassic : AspNetMvc5ModuleOnlyTests
+    {
+        public AspNetMvc5TestsModuleOnlyClassic(IisFixture iisFixture, ITestOutputHelper output)
+            : base(iisFixture, output, virtualApp: false, classicMode: true, enableRouteTemplateResourceNames: false)
+        {
+        }
+    }
+
+    [Collection("IisTests")]
+    public class AspNetMvc5TestsModuleOnlyIntegrated : AspNetMvc5ModuleOnlyTests
+    {
+        public AspNetMvc5TestsModuleOnlyIntegrated(IisFixture iisFixture, ITestOutputHelper output)
+            : base(iisFixture, output, virtualApp: false, classicMode: false, enableRouteTemplateResourceNames: false)
+        {
+        }
+    }
+
+    [Collection("IisTests")]
+    public class AspNetMvc5TestsModuleOnlyVirtualAppIntegrated : AspNetMvc5ModuleOnlyTests
+    {
+        public AspNetMvc5TestsModuleOnlyVirtualAppIntegrated(IisFixture iisFixture, ITestOutputHelper output)
+            : base(iisFixture, output, virtualApp: true, classicMode: false, enableRouteTemplateResourceNames: false)
+        {
+        }
+    }
+
+    [Collection("IisTests")]
+    public class AspNetMvc5TestsModuleOnlyVirtualAppIntegratedWithFeatureFlag : AspNetMvc5ModuleOnlyTests
+    {
+        public AspNetMvc5TestsModuleOnlyVirtualAppIntegratedWithFeatureFlag(IisFixture iisFixture, ITestOutputHelper output)
+            : base(iisFixture, output, virtualApp: true, classicMode: false, enableRouteTemplateResourceNames: false)
+        {
+        }
+    }
+
     [UsesVerify]
     public abstract class AspNetMvc5Tests : TestHelper, IClassFixture<IisFixture>
     {
@@ -148,6 +184,67 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             // Ensures that we get nice file nesting in Solution Explorer
             await Verifier.Verify(spans, settings)
                           .UseMethodName("_")
+                          .UseTypeName(_testName);
+        }
+    }
+
+    [UsesVerify]
+    public abstract class AspNetMvc5ModuleOnlyTests : TestHelper, IClassFixture<IisFixture>
+    {
+        private readonly IisFixture _iisFixture;
+        private readonly string _testName;
+
+        protected AspNetMvc5ModuleOnlyTests(IisFixture iisFixture, ITestOutputHelper output, bool classicMode, bool enableRouteTemplateResourceNames, bool virtualApp = false)
+            : base("AspNetMvc5", @"test\test-applications\aspnet", output)
+        {
+            SetServiceVersion("1.0.0");
+            SetEnvironmentVariable(ConfigurationKeys.FeatureFlags.RouteTemplateResourceNamesEnabled, enableRouteTemplateResourceNames.ToString());
+
+            // Disable the MVC part, so we can't back propagate any details to the tracing module
+            SetEnvironmentVariable(ConfigurationKeys.DisabledIntegrations, nameof(Configuration.IntegrationId.AspNetMvc));
+
+            _iisFixture = iisFixture;
+            _iisFixture.ShutdownPath = "/home/shutdown";
+            if (virtualApp)
+            {
+                _iisFixture.VirtualApplicationPath = "/my-app";
+            }
+
+            _iisFixture.TryStartIis(this, classicMode ? IisAppType.AspNetClassic : IisAppType.AspNetIntegrated);
+            _testName = nameof(AspNetMvc5Tests)
+                      + "ModuleOnly"
+                      + (virtualApp ? ".VirtualApp" : string.Empty)
+                      + (classicMode ? ".Classic" : ".Integrated");
+        }
+
+        public static TheoryData<string, int> Data() => new()
+        {
+            { "/Home/Index", 200 },
+            { "/Home/Get", 500 },
+            { "/badrequest", 500 },
+            { "/statuscode/503", 503 },
+            { "/BadRequestWithStatusCode/401", 500 }, // the exception thrown here overrides the status code set
+        };
+
+        [SkippableTheory]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        [Trait("LoadFromGAC", "True")]
+        [MemberData(nameof(Data))]
+        public async Task SubmitsTraces(string path, HttpStatusCode statusCode)
+        {
+            // Append virtual directory if there is one
+            var spans = await GetWebServerSpans(_iisFixture.VirtualApplicationPath + path, _iisFixture.Agent, _iisFixture.HttpPort, statusCode, expectedSpanCount: 1);
+
+            var sanitisedPath = VerifyHelper.SanitisePathsForVerify(path);
+
+            var settings = VerifyHelper.GetSpanVerifierSettings(sanitisedPath, (int)statusCode);
+
+            // Overriding the type name here as we have multiple test classes in the file
+            // Ensures that we get nice file nesting in Solution Explorer
+            await Verifier.Verify(spans, settings)
+                          .UseMethodName("_")
+                          .DisableRequireUniquePrefix()
                           .UseTypeName(_testName);
         }
     }
