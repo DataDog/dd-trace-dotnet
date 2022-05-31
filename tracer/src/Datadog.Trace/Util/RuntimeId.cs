55d3afed12ee4080deec2b9779eb654a49da4402
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-05-31 14:34:07 +0200

    Move native loader methods into its own class

--- tracer/src/Datadog.Trace/Util/RuntimeId.cs
+++ tracer/src/Datadog.Trace/Util/RuntimeId.cs
@@ -4,8 +4,6 @@
 // </copyright>
 
 using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Threading;
 using Datadog.Trace.Logging;
 
@@ -16,22 +14,13 @@ namespace Datadog.Trace.Util
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(RuntimeId));
         private static string _runtimeId;
 
-        private static bool IsNativeLoaderAvailable
-        {
-            get
-            {
-                var fd = FrameworkDescription.Instance;
-                return fd.ProcessArchitecture != ProcessArchitecture.Arm && fd.ProcessArchitecture != ProcessArchitecture.Arm64;
-            }
-        }
-
         public static string Get() => LazyInitializer.EnsureInitialized(ref _runtimeId, () => GetImpl());
 
         private static string GetImpl()
         {
-            if (IsNativeLoaderAvailable && TryGetRuntimeIdFromNative(out var runtimeId))
+            if (NativeLoader.TryGetRuntimeIdFromNative(out var runtimeId))
             {
-                Log.Information("Runtime id retrieved from native: " + runtimeId);
+                Log.Information("Runtime id retrieved from native loader: " + runtimeId);
                 return runtimeId;
             }
 
@@ -39,40 +28,6 @@ namespace Datadog.Trace.Util
             Log.Debug("Unable to get the runtime id from native. Fallback to Guid.NewGuid() : {NewGuid}", guid);
 
             return guid;
-        }
-
-        // Adding the attribute MethodImpl(MethodImplOptions.NoInlining) allows the caller to
-        // catch the SecurityException in case of we are running in a partial trust environment.
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static IntPtr GetRuntimeIdFromNative()
-        {
-            return NativeMethods.GetRuntimeIdFromNative();
-        }
-
-        private static bool TryGetRuntimeIdFromNative(out string runtimeId)
-        {
-            try
-            {
-                var runtimeIdPtr = GetRuntimeIdFromNative();
-                runtimeId = Marshal.PtrToStringAnsi(runtimeIdPtr);
-                return !string.IsNullOrWhiteSpace(runtimeId);
-            }
-            catch (Exception e)
-            {
-                // We failed to retrieve the runtime from native this can be because:
-                // - P/Invoke issue (unknown dll, unknown entrypoint...)
-                // - We are running in a partial trust environment
-                Log.Information("Failed to get the runtime-id from native: {Reason}", e.Message);
-            }
-
-            runtimeId = default;
-            return false;
-        }
-
-        private static class NativeMethods
-        {
-            [DllImport("Datadog.AutoInstrumentation.NativeLoader", CallingConvention = CallingConvention.StdCall, EntryPoint = "GetCurrentAppDomainRuntimeId")]
-            public static extern IntPtr GetRuntimeIdFromNative();
         }
     }
 }
