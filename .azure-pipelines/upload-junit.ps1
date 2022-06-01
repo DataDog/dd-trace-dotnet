a40ed8eda83b634782e7785998a25962a39ac578
Tony Redondo <tony.redondo@datadoghq.com>
2022-06-01 23:26:17 +0200

    changes

--- .azure-pipelines/upload-junit.ps1
+++ .azure-pipelines/upload-junit.ps1
@@ -4,13 +4,13 @@ $files = [System.IO.Directory]::GetFiles($resultFolder, "junit-result.xml", [Sys
 
 Write-Output "Downloading datadog-ci..."
 if ($IsLinux) {
-    if (![System.IO.File]::Exists("/usr/local/bin/datadog-ci")) {
-        Invoke-WebRequest -Uri "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_linux-x64" -OutFile "/usr/local/bin/datadog-ci"
+    if (![System.IO.File]::Exists("./datadog-ci")) {
+        Invoke-WebRequest -Uri "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_linux-x64" -OutFile "./datadog-ci"
         chmod +x /usr/local/bin/datadog-ci
     }
 } elseif ($IsMacOS) { 
-    if (![System.IO.File]::Exists("/usr/local/bin/datadog-ci")) {
-        Invoke-WebRequest -Uri "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_darwin-x64" -OutFile "/usr/local/bin/datadog-ci"
+    if (![System.IO.File]::Exists("./datadog-ci")) {
+        Invoke-WebRequest -Uri "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_darwin-x64" -OutFile "./datadog-ci"
         chmod +x /usr/local/bin/datadog-ci
     }
 } else {
@@ -50,9 +50,9 @@ foreach ($file in $files)
     Write-Output $env:DD_TAGS;
 
     if ($IsLinux) { 
-        datadog-ci junit upload --service $service $file
+        ./datadog-ci junit upload --service $service $file
     } elseif ($IsMacOS) { 
-        datadog-ci junit upload --service $service $file
+        ./datadog-ci junit upload --service $service $file
     } else {
         ./datadog-ci.exe junit upload --service $service $file
     }
