854f19e2eed6e2f7b067731525eadf96597d0ff0
Andrew Lock <andrew.lock@datadoghq.com>
2022-06-01 16:47:40 +0100

    Update ApiWebRequestFactoryTests.cs

--- tracer/test/Datadog.Trace.Tests/ApiWebRequestFactoryTests.cs
+++ tracer/test/Datadog.Trace.Tests/ApiWebRequestFactoryTests.cs
@@ -11,7 +11,7 @@ using Xunit;
 
 namespace Datadog.Trace.Tests
 {
-    [CollectionDefinition(nameof(WebRequestCollection), DisableParallelization = true)]
+    [Collection(nameof(WebRequestCollection), DisableParallelization = true)]
     public class ApiWebRequestFactoryTests
     {
         /// <summary>
