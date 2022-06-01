f20d2519d3a4c632b1de34357c8aa6efd2b109fd
Andrew Lock <andrew.lock@datadoghq.com>
2022-06-01 10:16:28 +0100

    Stop prefixing resource names with IIS app name when TracingHttpModule only (#2852)
    * Add overloads to GetCleanUriPath to allow removing a virtual path
    
    * Update ASP.NET integrations to remove the virtual path from the resource name when using the "raw" URL
    
    * Fixed snapshot tests
    
    Confirms that the application name prefix is not added to the resource name in the previously incorrect 2 cases:
    - when the MVC/WebApi2 integration doesn't run
    - when WebApi2 uses a message handler
    
    * Address PR feedback
    
    * Update snapshots for transferrequest tests
    
    * Add additional prefix test

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/RequestContextStruct.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/RequestContextStruct.cs
@@ -1,21 +1,20 @@
-// <copyright file="IHttpControllerContext.cs" company="Datadog">
+﻿// <copyright file="RequestContextStruct.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+
+using Datadog.Trace.DuckTyping;
 
 #if NETFRAMEWORK
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
 {
     /// <summary>
-    /// HttpControllerContext interface for ducktyping
+    /// Duck type for https://github.com/aspnet/AspNetWebStack/blob/main/src/System.Web.Http/Controllers/HttpRequestContext.cs
     /// </summary>
-    internal interface IHttpControllerContext
+    [DuckCopy]
+    internal struct RequestContextStruct
     {
-        IHttpRequestMessage Request { get; }
-
-        IHttpRouteData RouteData { get; }
-
-        RequestContextStruct RequestContext { get; }
+        public string VirtualPathRoot;
     }
 }
 #endif
