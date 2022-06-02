adc9753c3e046dec443462f556705fb37f0525cb
Anna <anna.yafi@datadoghq.com>
2022-06-02 15:58:06 +0200

    fix unit tests

--- tracer/test/Datadog.Trace.Security.Unit.Tests/IpExtractionTests.cs
+++ tracer/test/Datadog.Trace.Security.Unit.Tests/IpExtractionTests.cs
@@ -4,6 +4,7 @@
 // </copyright>
 
 using Datadog.Trace.AppSec.Transports.Http;
+using Datadog.Trace.Headers.Ip;
 using Xunit;
 
 namespace Datadog.Trace.Security.UnitTests
@@ -14,7 +15,7 @@ namespace Datadog.Trace.Security.UnitTests
         [InlineData("172.217.22.14", 80, new[] { "192.168.1.1", "172.16.32.41", "172.16.32.41", "172.217.22.14", "172.217.25.17:83" })]
         [InlineData("81.202.236.243", 5001, new[] { "192.168.0.2", "172.17.2.5:84", "10.11.12.14", "192.168.200.253:98", "81.202.236.243:5001" })]
         [InlineData("83.204.236.243", 443, new[] { "172.16.2.4", "172.31.255.255", "192.168.255.255", "10.145.255.255", "83.204.236.243:443" })]
-        [InlineData("172.16.32.43", 80, new[] { "192.168.1.1", "172.16.32.41", "172.16.32.43" })]
+        [InlineData("192.168.1.1", 80, new[] { "192.168.1.1", "172.16.32.41", "172.16.32.43" })]
         public void Ipv4PublicDetectedLocalIgnored(string expectedIp, int expectedPort, string[] ips)
         {
             var ip = IpExtractor.GetRealIpFromValues(ips, false);
@@ -25,7 +26,7 @@ namespace Datadog.Trace.Security.UnitTests
         [Theory]
         [InlineData("2001:db8:3333:4444:5555:6666:7777:8888", 80, new[] { "fe80::20e:cff:fe3b:883c", "2001:db8:3333:4444:5555:6666:7777:8888" })]
         [InlineData("2001:db8:3333:4444:5555:6666:7777:8888", 53, new[] { "fe80::20e:cff:fe3b:883c", "fe80::5525:2a3f:6fa6:cd4e%14", "[2001:db8:3333:4444:5555:6666:7777:8888]:53" })]
-        [InlineData("FE80::240:D0FF:FE48:4672", 80, new[] { "fe80::20e:cff:fe3b:883c", "fe80::5525:2a3f:6fa6:cd4e%14", "FE80::240:D0FF:FE48:4672" })]
+        [InlineData("fe80::20e:cff:fe3b:883c", 80, new[] { "fe80::20e:cff:fe3b:883c", "fe80::5525:2a3f:6fa6:cd4e%14", "FE80::240:D0FF:FE48:4672" })]
         public void Ipv6PublicDetectedPrivateIgnored(string expectedIp, int expectedPort, string[] ips)
         {
             var ip = IpExtractor.GetRealIpFromValues(ips, false);
