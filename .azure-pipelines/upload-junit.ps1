a932f18820dc8b648bb3b6f9369198346e88c1b0
Tony Redondo <tony.redondo@datadoghq.com>
2022-06-03 11:20:46 +0200

    Improvements

--- .azure-pipelines/upload-junit.ps1
+++ .azure-pipelines/upload-junit.ps1
@@ -14,17 +14,17 @@ if ($osArchitecture.Contains("arm"))
 Write-Output "Downloading datadog-ci..."
 $datadogCliReleases = "https://github.com/DataDog/datadog-ci/releases/latest/download"
 if ($IsLinux) {
-    if (![System.IO.File]::Exists("./datadog-ci")) {
+    if (!(Test-Path "./datadog-ci")) {
         Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_linux-x64" -OutFile "./datadog-ci"
         chmod +x ./datadog-ci
     }
 } elseif ($IsMacOS) { 
-    if (![System.IO.File]::Exists("./datadog-ci")) {
+    if (!(Test-Path "./datadog-ci")) {
         Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_darwin-x64" -OutFile "./datadog-ci"
         chmod +x ./datadog-ci
     }
 } else {
-    if (![System.IO.File]::Exists("datadog-ci.exe")) {
+    if (!(Test-Path "datadog-ci.exe")) {
         Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_win-x64.exe" -OutFile "datadog-ci.exe"
     }
 }
@@ -66,4 +66,12 @@ foreach ($file in $files)
     }
 
     Write-Output "";
+}
+
+Write-Output "Removing datadog-ci..."
+if (Test-Path "./datadog-ci") {
+  Remove-Item "./datadog-ci"
+}
+if (Test-Path "datadog-ci.exe") {
+    Remove-Item "datadog-ci.exe"
 }
