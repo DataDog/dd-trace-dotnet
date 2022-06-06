61e833d0e66a6a48c25a4c2aee01a4b78a48ad5a
Tony Redondo <tony.redondo@datadoghq.com>
2022-06-06 11:34:13 +0200

    Try NPM in ARM64

--- .azure-pipelines/upload-junit.ps1
+++ .azure-pipelines/upload-junit.ps1
@@ -4,28 +4,26 @@ $files = [System.IO.Directory]::GetFiles($resultFolder, "junit-result.xml", [Sys
 $osArchitecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture.ToString().ToLowerInvariant();
 $runtimeArchitecture = $env:ARCHITECTURE
 
-# ARM is not supported
-if ($osArchitecture.Contains("arm"))
-{
-    Write-Output "ARM architecture is not supported.";
-    Exit;
-}
-
-Write-Output "Downloading datadog-ci..."
-$datadogCliReleases = "https://github.com/DataDog/datadog-ci/releases/latest/download"
-if ($IsLinux) {
-    if (!(Test-Path "./datadog-ci")) {
-        Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_linux-x64" -OutFile "./datadog-ci"
-        chmod +x ./datadog-ci
-    }
-} elseif ($IsMacOS) { 
-    if (!(Test-Path "./datadog-ci")) {
-        Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_darwin-x64" -OutFile "./datadog-ci"
-        chmod +x ./datadog-ci
-    }
+if ($osArchitecture.Contains("arm")) {
+    Write-Output "Installing NPM datadog-ci";
+    npm install -g @datadog/datadog-ci
 } else {
-    if (!(Test-Path "datadog-ci.exe")) {
-        Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_win-x64.exe" -OutFile "datadog-ci.exe"
+    Write-Output "Downloading datadog-ci..."
+    $datadogCliReleases = "https://github.com/DataDog/datadog-ci/releases/latest/download"
+    if ($IsLinux) {
+        if (!(Test-Path "./datadog-ci")) {
+            Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_linux-x64" -OutFile "./datadog-ci"
+            chmod +x ./datadog-ci
+        }
+    } elseif ($IsMacOS) { 
+        if (!(Test-Path "./datadog-ci")) {
+            Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_darwin-x64" -OutFile "./datadog-ci"
+            chmod +x ./datadog-ci
+        }
+    } else {
+        if (!(Test-Path "datadog-ci.exe")) {
+            Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_win-x64.exe" -OutFile "datadog-ci.exe"
+        }
     }
 }
 
@@ -58,7 +56,11 @@ foreach ($file in $files)
     Write-Output $env:DD_TAGS;
 
     if ($IsLinux) { 
-        ./datadog-ci junit upload --service $service $file
+        if ($osArchitecture.Contains("arm")) {
+            datadog-ci junit upload --service $service $file
+        } else {
+            ./datadog-ci junit upload --service $service $file
+        }
     } elseif ($IsMacOS) { 
         ./datadog-ci junit upload --service $service $file
     } else {
