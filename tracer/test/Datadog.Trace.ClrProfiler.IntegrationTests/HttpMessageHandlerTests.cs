c19303ddb26ccff786c4e0d52afa104e81dff3a6
Zach Montoya <zach.montoya@datadoghq.com>
2022-06-06 14:38:09 -0700

    Refactor rules to compose in a more modular fashion, apply to the HttpMessageHandler rule first

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/HttpMessageHandlerTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/HttpMessageHandlerTests.cs
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Runtime.InteropServices;
 using Datadog.Trace.ClrProfiler.IntegrationTests.Helpers;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.TestHelpers;
+using Datadog.Trace.TestHelpers.FSharp;
 using FluentAssertions.Execution;
 using Xunit;
 using Xunit.Abstractions;
@@ -68,9 +70,10 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
                 foreach (var span in spans)
                 {
-                    Assert.Equal(expectedOperationName, span.Name);
+                    (bool result, string message) = SpanValidator.validateRule(TracingIntegrationRules.isHttpMessageHandler, span);
+                    Assert.True(result, message);
+
                     Assert.Equal(expectedServiceName, span.Service);
-                    Assert.Equal(SpanTypes.Http, span.Type);
                     Assert.Equal("HttpMessageHandler", span.Tags[Tags.InstrumentationName]);
                     Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
 
--- tracer/test/Datadog.Trace.TestHelpers.FSharp/SpanValidator.fs
+++ tracer/test/Datadog.Trace.TestHelpers.FSharp/SpanValidator.fs
@@ -9,14 +9,14 @@ module SpanValidator =
         | Success _ -> (true, "")
         | Failure x -> (false, x)
 
+    let validateRule rule span =
+        rule span
+        |> returnSpanResult
+
     let validateAspNetCoreSpan span =
         isAspNetCore span
         |> returnSpanResult
     
     let validateAspNetCoreMvcSpan span =
         isAspNetCoreMvc span
-        |> returnSpanResult
-
-    let validateHttpMessageHandlerSpan span =
-        isHttpMessageHandler span
         |> returnSpanResult
