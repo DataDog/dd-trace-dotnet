e5a07534facbee6c79c0750028fea7f4a1db1dc7
Tony Redondo <tony.redondo@datadoghq.com>
2022-06-06 14:52:02 +0200

    Clean up and enable logs

--- .azure-pipelines/upload-junit.ps1
+++ .azure-pipelines/upload-junit.ps1
@@ -7,29 +7,6 @@ $runtimeArchitecture = $env:ARCHITECTURE;
 Write-Output "Installing NPM datadog-ci";
 npm install -g @datadog/datadog-ci
 
-#if ($osArchitecture.Contains("arm")) {
-#    Write-Output "Installing NPM datadog-ci";
-#    npm install -g @datadog/datadog-ci
-#} else {
-#    Write-Output "Downloading datadog-ci..."
-#    $datadogCliReleases = "https://github.com/DataDog/datadog-ci/releases/latest/download"
-#    if ($IsLinux) {
-#        if (!(Test-Path "./datadog-ci")) {
-#            Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_linux-x64" -OutFile "./datadog-ci"
-#            chmod +x ./datadog-ci
-#        }
-#    } elseif ($IsMacOS) { 
-#        if (!(Test-Path "./datadog-ci")) {
-#            Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_darwin-x64" -OutFile "./datadog-ci"
-#            chmod +x ./datadog-ci
-#        }
-#    } else {
-#        if (!(Test-Path "datadog-ci.exe")) {
-#            Invoke-WebRequest -Uri "$datadogCliReleases/datadog-ci_win-x64.exe" -OutFile "datadog-ci.exe"
-#        }
-#    }
-#}
-
 Write-Output "Processing junit files...";
 foreach ($file in $files)
 {
@@ -58,38 +35,9 @@ foreach ($file in $files)
     Write-Output $file;
     Write-Output $env:DD_TAGS;
 
-    datadog-ci junit upload --service $service $file
-    #if ($IsLinux) { 
-    #    if ($osArchitecture.Contains("arm")) {
-    #        datadog-ci junit upload --service $service $file
-    #    } else {
-    #        ./datadog-ci junit upload --service $service $file
-    #    }
-    #} elseif ($IsMacOS) { 
-    #    ./datadog-ci junit upload --service $service $file
-    #} else {
-    #    ./datadog-ci.exe junit upload --service $service $file
-    #}
-
-    #Write-Output "Removing $file"
-    #Remove-Item $file -Force -ErrorAction Ignore
-    #if ($IsLinux -or $IsMacOS) {
-    #    sh -c "rm -f $file"
-    #} else {
-    #    cmd /c "erase /f /q $file"
-    #}
+    datadog-ci junit upload --logs --service $service $file
     Write-Output "";
 }
-
-#Write-Output "Removing datadog-ci..."
-#if (Test-Path "./datadog-ci") {
-#  Remove-Item "./datadog-ci" -Force -ErrorAction Ignore
-#  sh -c "rm -f ./datadog-ci"
-#}
-#if (Test-Path "datadog-ci.exe") {
-#  Remove-Item "datadog-ci.exe" -Force -ErrorAction Ignore
-#  cmd /c "erase /f /q datadog-ci.exe"
-#}
 
 Write-Output "Done.";
 Exit 0;
