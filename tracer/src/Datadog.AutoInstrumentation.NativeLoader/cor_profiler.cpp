015f76584dcd1fb9a2c69e45443a36f008d8ded9
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-06-07 17:05:32 +0200

    Fix crash in exection benchmark tests (#2873)
    Use the native loader to start the test
    Check if the CorProfiler is initialized before getting the runtime id for the app domain

--- tracer/src/Datadog.AutoInstrumentation.NativeLoader/cor_profiler.cpp
+++ tracer/src/Datadog.AutoInstrumentation.NativeLoader/cor_profiler.cpp
@@ -977,6 +977,12 @@ namespace datadog::shared::nativeloader
 
     AppDomainID CorProfiler::GetCurrentAppDomainId()
     {
+        if (m_this == nullptr)
+        {
+            Log::Warn("The native loader library is not properly initialized. We cannot get the current AppDomain Id.");
+            return (AppDomainID)0;
+        }
+
         ThreadID threadId;
         auto hr = m_this->m_info->GetCurrentThreadID(&threadId);
 
@@ -1002,6 +1008,12 @@ namespace datadog::shared::nativeloader
 
     const char* CorProfiler::GetRuntimeId(AppDomainID appDomain)
     {
+        if (m_this == nullptr)
+        {
+            Log::Warn("The native loader library is not properly initialized. We cannot get the runtime id for the AppDomain ID #", appDomain);
+            return nullptr;
+        }
+
         return m_this->m_runtimeIdStore.Get(appDomain).c_str();
     }
 
