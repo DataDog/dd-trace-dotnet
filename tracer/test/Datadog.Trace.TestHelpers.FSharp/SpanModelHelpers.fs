876274ad1db69345cde088c786e5b811785cc804
Zach Montoya <zach.montoya@datadoghq.com>
2022-06-13 15:19:00 -0400

    Add more helpers so printing failure messages is more clear

--- tracer/test/Datadog.Trace.TestHelpers.FSharp/SpanModelHelpers.fs
+++ tracer/test/Datadog.Trace.TestHelpers.FSharp/SpanModelHelpers.fs
@@ -25,51 +25,70 @@ module SpanModelHelpers =
         ( (nameof span.Type), span.Type )
 
     // Helper functions
-    let surroundStringWithQuotes (value: string) = $"\"{value}\""
+    let surroundStringWithBraces (value: string) = sprintf "[%s]" value
+
+    let surroundStringWithQuotes (value: string) = sprintf "\"%s\"" value
 
     let stringJoinWithComma (values: string[]) = System.String.Join(", ", values)
 
+    let sprintPresentFailure propertyKind propertyName = sprintf "%s \"%s\" was expected to be present" propertyKind propertyName
+
+    let sprintMatchesFailure propertyKind propertyName expectedValue actualValue = sprintf "%s \"%s\" was expected to have value %s, but the value is \"%s\"" propertyKind propertyName expectedValue actualValue
+
+    let sprintMatchesOneOfFailure propertyKind propertyName expectedValue actualValue = sprintf "%s \"%s\" was expected to have one of the following values %s, but the value is \"%s\"" propertyKind propertyName expectedValue actualValue
+
     // Comparison functions
-    let isOptional result propertyKind propertyName (span: MockSpan) =
+    let isOptional result (span: MockSpan) =
         match result with
         | _ -> Success span
 
-    let isPresent result propertyKind propertyName (span: MockSpan) =
+    let isPresent result failureString (span: MockSpan) =
         match result with
-        | null -> Failure $"{propertyKind} \"{propertyName}\" was expected to be present, but the {propertyKind} value is null"
+        | null -> Failure failureString
         | _ -> Success span
 
-    let matches result propertyKind propertyName expectedValue (span: MockSpan) =
+    let matches expectedValue result sprintFailureString (span: MockSpan) =
         match result with
-        | value when value <> expectedValue -> Failure $"{propertyKind} \"{propertyName}\" was expected to have value \"{expectedValue}\", but the {propertyKind} value is \"{value}\""
+        | actualValue when actualValue <> expectedValue -> Failure (sprintFailureString actualValue)
         | _ -> Success span
 
-    let matchesOneOf result propertyKind propertyName (expectedValueArray: string[]) (span: MockSpan) =
+    let matchesOneOf (expectedValueArray: string[]) result sprintFailureString (span: MockSpan) =
         match expectedValueArray |> Array.tryFind (fun elm -> elm = result) with
             | Some _ -> Success span
-            | None -> Failure ($"{propertyKind} \"{propertyName}\" was expected to have one of the following values [" + (expectedValueArray |> Array.map surroundStringWithQuotes |> stringJoinWithComma) + $"], but the {propertyKind} value is \"{result}\"")
+            | None -> Failure (sprintFailureString result)
     
     // DSL functions for easier parsing and documentation generation
     let propertyIsPresent extractProperty (span: MockSpan) =
         let (propertyName, result) = extractProperty span
-        isPresent result "property" propertyName span
+        let failureString = sprintPresentFailure "property" propertyName
+        isPresent result failureString span
 
     let propertyMatches extractProperty (expectedValue: string) (span:MockSpan) =
         let (propertyName, result) = extractProperty span
-        matches result "property" propertyName expectedValue span
+        let sprintFailureWithActualValue = sprintMatchesFailure "property" propertyName expectedValue
+        matches expectedValue result sprintFailureWithActualValue span
 
     let propertyMatchesOneOf extractProperty (expectedValueArray: string[]) (span:MockSpan) =
         let (propertyName, result) = extractProperty span
-        matchesOneOf result "property" propertyName expectedValueArray span
+        let expectedValueArrayString = expectedValueArray |> Array.map surroundStringWithQuotes |> stringJoinWithComma |> surroundStringWithBraces
+        let sprintFailureWithActualValue = sprintMatchesFailure "property" propertyName expectedValueArrayString
+        matchesOneOf expectedValueArray result sprintFailureWithActualValue span
 
     let tagIsOptional tagName (span: MockSpan) =
-        isOptional (span.GetTag tagName) "tag" tagName span
+        isOptional (span.GetTag tagName) span
 
     let tagIsPresent tagName (span: MockSpan) =
-        isPresent (span.GetTag tagName) "tag" tagName span
+        let result = (span.GetTag tagName)
+        let failureString = sprintPresentFailure "tag" tagName
+        isPresent result failureString span
 
     let tagMatches tagName (expectedValue: string) (span:MockSpan) =
-        matches (span.GetTag tagName) "tag" tagName expectedValue span
+        let result = span.GetTag tagName
+        let sprintFailureWithActualValue = sprintMatchesFailure "tag" tagName expectedValue
+        matches expectedValue result sprintFailureWithActualValue span
 
     let tagMatchesOneOf tagName (expectedValueArray: string[]) (span: MockSpan) =
-        matchesOneOf (span.GetTag tagName) "tag" tagName expectedValueArray span
+        let result = span.GetTag tagName
+        let expectedValueArrayString = expectedValueArray |> Array.map surroundStringWithQuotes |> stringJoinWithComma |> surroundStringWithBraces
+        let sprintFailureWithActualValue = sprintMatchesOneOfFailure "tag" tagName expectedValueArrayString
+        matchesOneOf expectedValueArray result sprintFailureWithActualValue span
