84eec3d481c91ae0e10ca696b1c550e0702dedb3
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2022-07-14 18:33:43 +0200

    Exclude unsupported HotChocolate test platforms

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/HotChocolateTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/HotChocolateTests.cs
@@ -2,6 +2,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+#if NET5_0_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 
 #pragma warning disable SA1402 // File may only contain a single type
 #pragma warning disable SA1649 // File name should match first type name
@@ -243,3 +244,5 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         }
     }
 }
+
+#endif
