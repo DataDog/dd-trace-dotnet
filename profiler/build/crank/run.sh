603beda6e68519a7a795a08b2ef4846c56f866f3
Kevin Gosse <krix33@gmail.com>
2022-07-20 14:51:18 +0200

    Enable verbose output

--- profiler/build/crank/run.sh
+++ profiler/build/crank/run.sh
@@ -30,46 +30,46 @@ commit="--application.source.branchOrCommit $branchOrCommit"
 if [ "$1" = "windows" ]; then
     echo "Running windows throughput tests"
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_baseline --profile windows --json profiler_baseline_windows.json $repository $commit --property name=AspNetCoreSimpleController --property scenario=profiler_baseline --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_baseline --profile windows --json profiler_baseline_windows.json $repository $commit --property name=AspNetCoreSimpleController --property scenario=profiler_baseline --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_baseline_windows.json"
     rm profiler_baseline_windows.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler --profile windows --json profiler_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler --profile windows --json profiler_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_windows.json"
     rm profiler_windows.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions_baseline --profile windows --json profiler_exceptions_baseline_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions_baseline --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions_baseline --profile windows --json profiler_exceptions_baseline_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions_baseline --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_exceptions_baseline_windows.json"
     rm profiler_exceptions_baseline_windows.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions --profile windows --json profiler_exceptions_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions --profile windows --json profiler_exceptions_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_exceptions_windows.json"
     rm profiler_exceptions_windows.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_cpu --profile windows --json profiler_cpu_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_cpu --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_cpu --profile windows --json profiler_cpu_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_cpu --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_cpu_windows.json"
     rm profiler_cpu_windows.json
 
 elif [ "$1" = "linux" ]; then
-    echo "Running Linux  x64 throughput tests"
+    echo "Running Linux x64 throughput tests"
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_baseline --profile linux --json profiler_baseline_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_baseline --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_baseline --profile linux --json profiler_baseline_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_baseline --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_baseline_linux.json"
     rm profiler_baseline_linux.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler --profile linux --json profiler_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler --profile linux --json profiler_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_linux.json"
     rm profiler_linux.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions_baseline --profile linux --json profiler_exceptions_baseline_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions_baseline --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions_baseline --profile linux --json profiler_exceptions_baseline_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions_baseline --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_exceptions_baseline_linux.json"
     rm profiler_exceptions_baseline_linux.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions --profile linux --json profiler_exceptions_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_exceptions --profile linux --json profiler_exceptions_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_exceptions --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_exceptions_linux.json"
     rm profiler_exceptions_linux.json
 
-    crank --config Samples.AspNetCoreSimpleController.yml --scenario profiler_cpu --profile linux --json profiler_cpu_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_cpu --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
+    crank -v --config Samples.AspNetCoreSimpleController.yml --scenario profiler_cpu --profile linux --json profiler_cpu_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=profiler_cpu --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="profiler_cpu_linux.json"
     rm profiler_cpu_linux.json
 
