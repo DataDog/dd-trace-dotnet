ede580ae9eb8df75aeaca361d56559bbfe339265
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2022-08-03 15:16:32 -0400

    Increase expectedExceptionCount to 7 for Custom WCF binding

--- tracer/test/test-applications/integrations/Samples.Wcf/Program.cs
+++ tracer/test/test-applications/integrations/Samples.Wcf/Program.cs
@@ -45,7 +45,7 @@ namespace Samples.Wcf
             {
                 binding = ConfigureCustomBinding();
                 baseAddress = new Uri($"http://localhost:{port}/{WcfNamespace}/");
-                expectedExceptionCount = 6;
+                expectedExceptionCount = 7;
             }
             else
             {
