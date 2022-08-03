43aeefdcf7944e6ccdf1cf74d91ae8c91b1127c4
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-08-03 13:44:50 +0200

    Fix compilation + add optimization flags

--- profiler/build/cmake/FindLibunwind.cmake
+++ profiler/build/cmake/FindLibunwind.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(libunwind
     INSTALL_COMMAND ""
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND <SOURCE_DIR>/autogen.sh && <SOURCE_DIR>/configure CXXFLAGS=-fPIC\ -D_GLIBCXX_USE_CXX11_ABI=0 CFLAGS=-fPIC --disable-minidebuginfo && make -j
+    BUILD_COMMAND autoreconf -i <SOURCE_DIR> && <SOURCE_DIR>/configure CXXFLAGS=-fPIC\ -D_GLIBCXX_USE_CXX11_ABI=0\ -O3 CFLAGS=-fPIC\ -O3 --disable-minidebuginfo && make -j
     BUILD_ALWAYS false
 )
 
