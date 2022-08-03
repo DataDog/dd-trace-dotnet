7b7c56f0d9a37177291ba6f51b0205e7364da613
Kevin Gosse <krix33@gmail.com>
2022-08-03 13:03:22 +0200

    Revert "[Profiler] Use uws_backtrace for stackwalking on Linux (#2867)"
    This reverts commit f44846fbf6661a9ab8688c672ff9a1cf49fcbf1c.

--- profiler/src/ProfilerEngine/Datadog.Profiler.Native/OsSpecificApi.h
+++ profiler/src/ProfilerEngine/Datadog.Profiler.Native/OsSpecificApi.h
@@ -12,6 +12,9 @@ namespace shared {
 struct LoaderResourceMonikerIDs;
 }
 
+class StackSnapshotResultReusableBuffer;
+class IManagedThreadList;
+
 // Those functions must be defined in the main projects (Linux and Windows)
 // Here are forward declarations to avoid hard coupling
 namespace OsSpecificApi {
