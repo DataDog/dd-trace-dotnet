fad509c3f8a15de7589ad183404eea3d65694562
Tony Redondo <tony.redondo@datadoghq.com>
2022-08-05 12:00:10 +0200

    Fix OSX native loader build issue (#3052)
    * Fix OSX build issues.
    
    * Fix
    
    * BuildNativeLoader when building macos
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- shared/src/native-src/string.h
+++ shared/src/native-src/string.h
@@ -10,15 +10,27 @@
 #define WStrLen(value) (size_t) wcslen(value)
 #else
 #define WStr(value) u##value
-#define WStrLen(value) (size_t) std::char_traits<char16_t>::length(value)
+#define WStrLen(value) (size_t) std::char_traits<WCHAR>::length(value)
 #endif
 
 namespace shared {
 
-    typedef std::basic_string<WCHAR> WSTRING;
+    typedef std::basic_string<WCHAR, std::char_traits<WCHAR>, std::allocator<WCHAR>> WSTRING;
+#ifndef MACOS
     typedef std::basic_stringstream<WCHAR, std::char_traits<WCHAR>, std::allocator<WCHAR>> WSTRINGSTREAM;
+#else
+    // MACOS only support:
+    //    std::basic_stringstream<char, std::char_traits<char>, std::allocator<char>>
+    //    std::basic_stringstream<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>>
+    // If we try to use char16_t (WCHAR on non windows) then there's compilation errors when using the stringstream instance.
+#endif
 
     static WSTRING EmptyWStr = WStr("");
+#if _WIN32
+    const static WSTRING EndLWStr = WStr("\r\n");
+#else
+    const static WSTRING EndLWStr = WStr("\n");
+#endif
 
     std::string ToString(const std::string& str);
     std::string ToString(const char* str);
