ad30c7f8d3f54186ba74ca8841dc9f698d2e1b7d
Andrew Lock <andrew.lock@datadoghq.com>
2022-08-17 14:59:29 +0100

    Add a ThreadSafe random implementation

---
+++ tracer/src/Datadog.Trace/Util/ThreadSafeRandom.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="ThreadSafeRandom.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.Threading;
+
+namespace Datadog.Trace.Util;
+
+internal static class ThreadSafeRandom
+{
+#if NET6_0_OR_GREATER
+    public static int Next(int maxValue) => Random.Shared.Next(maxValue);
+#else
+    private static readonly Random Global = new();
+
+    private static readonly ThreadLocal<Random> Local;
+
+    static ThreadSafeRandom()
+    {
+        Local = new ThreadLocal<Random>(
+            () =>
+            {
+                int seed;
+                lock (Global)
+                {
+                    seed = Global.Next();
+                }
+
+                return new Random(seed);
+            });
+    }
+
+    public static int Next(int maxValue)
+    {
+        return Local.Value!.Next(maxValue);
+    }
+#endif
+}
