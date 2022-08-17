5357d1ba4e679d18e7a02736d172cac35fe10a09
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-08-17 14:40:30 +0200

    [Profiler] Fix Sanitizer jobs (#3091)

--- profiler/src/ProfilerEngine/DeployResources/DDProf-SetEnv.sh
+++ profiler/src/ProfilerEngine/DeployResources/DDProf-SetEnv.sh
@@ -14,11 +14,11 @@ fi
 
 export CORECLR_ENABLE_PROFILING=1
 export CORECLR_PROFILER={BD1A650D-AC5D-4896-B64F-D6FA25D6B26A}
-export CORECLR_PROFILER_PATH_64=${DD_DEPLOY_DIRECTORY}/linux-x64/Datadog.Profiler.Native.so
+export CORECLR_PROFILER_PATH_64=${DD_DEPLOY_DIRECTORY}/Datadog.Profiler.Native.so
 
 export COMPlus_EnableDiagnostics=1
 export DD_PROFILING_ENABLED=1
 
-export LD_PRELOAD=${DD_DEPLOY_DIRECTORY}/linux-x64/Datadog.Linux.ApiWrapper.x64.so
+export LD_PRELOAD=${DD_DEPLOY_DIRECTORY}/Datadog.Linux.ApiWrapper.x64.so
 
 echo "DD_DEPLOY_DIRECTORY=${DD_DEPLOY_DIRECTORY}"
