aa61bb0bc5d60ac60b754c352adfbd798aca9034
Andrew Lock <andrew.lock@datadoghq.com>
2022-08-18 14:05:07 +0100

    Add PathwayContext object and encoding (which is propagated in headers)
    Encoding and decoding is binary, and uses the Var encoding
    Injected into binary headers (e.g. kafka) using the new IBinaryHeadersCollection interface (equivalent to IHeadersCollection)

---
+++ tracer/src/Datadog.Trace/DataStreamsMonitoring/PathwayContextEncoder.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="PathwayContextEncoder.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+using System;
+using Datadog.Trace.DataStreamsMonitoring.Hashes;
+using Datadog.Trace.DataStreamsMonitoring.Utils;
+using Datadog.Trace.Logging;
+
+namespace Datadog.Trace.DataStreamsMonitoring;
+
+internal static class PathwayContextEncoder
+{
+    private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(PathwayContextEncoder));
+
+    /// <summary>
+    /// Encodes a <see cref="PathwayContext"/> as a series of bytes
+    /// NOTE: the encoding is lossy, in that we convert <see cref="PathwayContext.PathwayStart"/>
+    /// and <see cref="PathwayContext.EdgeStart"/> from ns to ms
+    /// </summary>
+    /// <param name="pathway">The pathway to encode</param>
+    /// <returns>The encoded pathway</returns>
+    public static byte[] Encode(PathwayContext pathway)
+    {
+        var pathwayStartMs = pathway.PathwayStart / 1000;
+        var edgeStartMs = pathway.EdgeStart / 1000;
+
+        var pathwayBytes = VarEncodingHelper.VarLongZigZagLength(pathwayStartMs);
+        var edgeBytes = VarEncodingHelper.VarLongZigZagLength(edgeStartMs);
+
+        // maximum size = 8 + edge + pathway
+        var bytes = new byte[8 + pathwayBytes + edgeBytes];
+        BinaryPrimitivesHelper.WriteUInt64LittleEndian(bytes, pathway.Hash.Value);
+        VarEncodingHelper.WriteVarLongZigZag(bytes, offset: 8, pathwayStartMs);
+        VarEncodingHelper.WriteVarLongZigZag(bytes, offset: 8 + pathwayBytes, edgeStartMs);
+        return bytes;
+    }
+
+    /// <summary>
+    /// Tries to decode a <see cref="PathwayContext"/> from a <c>byte[]</c>.
+    /// NOTE: the encoding process is lossy, so the decoded <see cref="PathwayContext"/>
+    /// contains truncated values for <see cref="PathwayContext.PathwayStart"/>
+    /// and <see cref="PathwayContext.EdgeStart"/> (conversion from ns to ms)
+    /// </summary>
+    /// <param name="bytes">The pathway to decode</param>
+    /// <returns>The decoded <see cref="PathwayContext"/>, or <c>null</c> if decoding fails </returns>
+    public static PathwayContext? Decode(byte[] bytes)
+    {
+        if (bytes.Length < 10)
+        {
+            Log.Warning<string, int>("Error decoding Data Stream PathwayContext from bytes {Base64EncodedBytes}: insufficient bytes ({ByteCount})", Convert.ToBase64String(bytes), bytes.Length);
+            return null;
+        }
+
+        // first 8 bytes
+        var hash = BinaryPrimitivesHelper.ReadUInt64LittleEndian(bytes);
+
+        var pathwayStartMs = VarEncodingHelper.ReadVarLongZigZag(bytes, offset: 8, out var bytesRead);
+        if (pathwayStartMs is null)
+        {
+            Log.Warning("Error decoding Data Stream PathwayContext from bytes {Base64EncodedBytes}: invalid pathway start", Convert.ToBase64String(bytes));
+            return null;
+        }
+
+        var edgeBytesMs = VarEncodingHelper.ReadVarLongZigZag(bytes, offset: 8 + bytesRead, out _);
+        if (edgeBytesMs is null)
+        {
+            Log.Warning("Error decoding Data Stream PathwayContext from bytes {Base64EncodedBytes}: invalid edge start", Convert.ToBase64String(bytes));
+            return null;
+        }
+
+        // Pathway context values are in ns
+        return new PathwayContext(new PathwayHash(hash), pathwayStartMs.Value * 1000, edgeBytesMs.Value * 1000);
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/DataStreamsMonitoring/DataStreamsContextPropagatorTests.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="DataStreamsContextPropagatorTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using Datadog.Trace.DataStreamsMonitoring;
+using Datadog.Trace.DataStreamsMonitoring.Hashes;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.DataStreamsMonitoring;
+
+public class DataStreamsContextPropagatorTests
+{
+    [Fact]
+    public void CanRoundTripPathwayContext()
+    {
+        var headers = new TestHeadersCollection();
+        var context = new PathwayContext(
+            new PathwayHash(1234),
+            DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() * 1000, // leaky abstraction, the encoder truncates
+            DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() * 1000);
+
+        DataStreamsContextPropagator.Instance.Inject(context, headers);
+
+        var extracted = DataStreamsContextPropagator.Instance.Extract(headers);
+
+        extracted.Should().NotBeNull();
+        extracted.Value.Hash.Value.Should().Be(context.Hash.Value);
+        extracted.Value.PathwayStart.Should().Be(context.PathwayStart);
+        extracted.Value.EdgeStart.Should().Be(context.EdgeStart);
+    }
+}
