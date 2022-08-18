d279af9d60da9b5a64748affa1e76ef9672a1c1b
Christophe Nasarre <christophe.nasarre@datadoghq.com>
2022-08-18 16:45:19 +0200

    Add timeit configuration for exceptions

--- profiler/build/run.cmd
+++ profiler/build/run.cmd
@@ -1,2 +1,3 @@
 @echo off
 %GOPATH%\\bin\\timeit.exe PiComputation.windows.%1.json
+%GOPATH%\\bin\\timeit.exe Exceptions.windows.%1.json
--- profiler/build/run.sh
+++ profiler/build/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ${GOPATH}/bin/timeit PiComputation.linux.$1.json
+${GOPATH}/bin/timeit Exceptions.linux.$1.json
