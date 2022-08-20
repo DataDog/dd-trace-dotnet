563dfa70a9b78eafd6792f6c38bc0c4f9ed1f1ea
Tony Redondo <tony.redondo@datadoghq.com>
2022-08-20 08:24:54 +0200

    [CIVisibility] Fix CIAgentlessWriter flush algorithm (#3096)
    * Fix CIAgentlessWriter flush algorithm
    
    * Add CIAgentlessWriterTests.ConcurrencyFlushTest
    
    * fix
    
    * Changes from review.
    
    * Fix test

--- tracer/src/Datadog.Trace/Ci/Agent/MessagePack/SpanEventMessagePackFormatter.cs
+++ tracer/src/Datadog.Trace/Ci/Agent/MessagePack/SpanEventMessagePackFormatter.cs
@@ -14,6 +14,7 @@ namespace Datadog.Trace.Ci.Agent.MessagePack
         {
             if (value is null)
             {
+                Log.Warning("SpanEventMessagePackFormatter: SpanEvent is null");
                 return 0;
             }
 
