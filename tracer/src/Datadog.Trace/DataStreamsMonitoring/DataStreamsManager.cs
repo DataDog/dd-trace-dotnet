e13dbcada485694dfe975497e169c66f691da758
Andrew Lock <andrew.lock@datadoghq.com>
2022-08-22 15:15:29 +0100

    Initial Data Streams Monitoring propagation support
    Adds checkpointing and merging of contexts (and hence propagation), but doesn't yet _record_ or send the checkpointed values

--- tracer/src/Datadog.Trace/DataStreamsMonitoring/DataStreamsManager.cs
+++ tracer/src/Datadog.Trace/DataStreamsMonitoring/DataStreamsManager.cs
@@ -4,11 +4,15 @@
 // </copyright>
 
 #nullable enable
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.DataStreamsMonitoring.Hashes;
+using Datadog.Trace.ExtensionMethods;
+using Datadog.Trace.Headers;
 using Datadog.Trace.Logging;
+using Datadog.Trace.Vendors.Serilog.Events;
 
 namespace Datadog.Trace.DataStreamsMonitoring;
 
@@ -42,5 +46,90 @@ internal class DataStreamsManager
     {
         Volatile.Write(ref _isEnabled, false);
         return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Trys to extract a <see cref="PathwayContext"/>, from the provided <paramref name="headers"/>
+    /// If data streams is disabled, or no pathway is present, returns null.
+    /// </summary>
+    public PathwayContext? ExtractPathwayContext<TCarrier>(TCarrier headers)
+        where TCarrier : IBinaryHeadersCollection
+        => IsEnabled ? DataStreamsContextPropagator.Instance.Extract(headers) : null;
+
+    /// <summary>
+    /// Injects a <see cref="PathwayContext"/> into headers
+    /// </summary>
+    /// <param name="context">The pathway context to inject</param>
+    /// <param name="headers">The header collection to inject the headers into</param>
+    public void InjectPathwayContext<TCarrier>(PathwayContext? context, TCarrier headers)
+        where TCarrier : IBinaryHeadersCollection
+    {
+        if (!IsEnabled)
+        {
+            return;
+        }
+
+        if (context is not null)
+        {
+            DataStreamsContextPropagator.Instance.Inject(context.Value, headers);
+            return;
+        }
+
+        // This shouldn't happen normally, as you should call SetCheckpoint before calling InjectPathwayContext
+        // But if data streams was disabled, you call SetCheckpoint, and then data streams is enabled
+        // you will hit this code path
+        System.Diagnostics.Debug.Fail("Attempted to inject null pathway context");
+        Log.Debug("Attempted to inject null pathway context");
+    }
+
+    /// <summary>
+    /// Sets a checkpoint using the provided <see cref="PathwayContext"/>
+    /// NOTE: <paramref name="edgeTags"/> must be in correct sort order
+    /// </summary>
+    /// <param name="parentPathway">The current pathway</param>
+    /// <param name="edgeTags">Edge tags to set for the new pathway. MUST be sorted in alphabetical order</param>
+    /// <returns>If disabled, returns <c>null</c>. Otherwise returns a new <see cref="PathwayContext"/></returns>
+    public PathwayContext? SetCheckpoint(in PathwayContext? parentPathway, string[] edgeTags)
+    {
+        if (!IsEnabled)
+        {
+            return null;
+        }
+
+        try
+        {
+            var edgeStartNs = DateTimeOffset.UtcNow.ToUnixTimeNanoseconds();
+            var pathwayStartNs = parentPathway?.PathwayStart ?? edgeStartNs;
+
+            var nodeHash = HashHelper.CalculateNodeHash(_nodeHashBase, edgeTags);
+            var parentHash = parentPathway?.Hash ?? default;
+            var pathwayHash = HashHelper.CalculatePathwayHash(nodeHash, parentHash);
+
+            // TODO: send to aggregator
+            var pathway = new PathwayContext(
+                hash: pathwayHash,
+                pathwayStartNs: pathwayStartNs,
+                edgeStartNs: edgeStartNs);
+
+            if (Log.IsEnabled(LogEventLevel.Debug))
+            {
+                Log.Debug(
+                    "SetCheckpoint with {PathwayHash}, {PathwayStart}, {EdgeStart}",
+                    pathway.Hash,
+                    pathway.PathwayStart,
+                    pathway.EdgeStart);
+            }
+
+            return pathway;
+        }
+        catch (Exception ex)
+        {
+            Log.Error("Error setting a data streams checkpoint. Disabling data streams monitoring", ex);
+            // Set this to false out of an abundance of caution.
+            // We will look at being less conservative in the future
+            // if we see intermittent errors for some reason.
+            Volatile.Write(ref _isEnabled, false);
+            return null;
+        }
     }
 }
