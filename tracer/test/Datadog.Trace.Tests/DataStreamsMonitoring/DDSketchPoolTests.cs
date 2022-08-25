4269398450979ea243f1f040664b2643264cc063
Andrew Lock <andrew.lock@datadoghq.com>
2022-08-25 11:29:26 +0100

    Add a simple pooling mechanism for DDSketches
    We could end up creating a _lot_ of them, so keep a (bounded) pool

---
+++ tracer/test/Datadog.Trace.Tests/DataStreamsMonitoring/DDSketchPoolTests.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="DDSketchPoolTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Generic;
+using Datadog.Trace.DataStreamsMonitoring.Utils;
+using Datadog.Trace.Vendors.Datadog.Sketches;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.DataStreamsMonitoring;
+
+public class DDSketchPoolTests
+{
+    [Fact]
+    public void CanGetAndReleaseSketchRepeatedly()
+    {
+        var pool = new DDSketchPool();
+        var sketch = pool.Get();
+        pool.Release(sketch);
+
+        var sketch2 = pool.Get();
+        sketch2.Should().Be(sketch);
+        pool.Release(sketch2);
+
+        var sketch3 = pool.Get();
+        sketch2.Should().Be(sketch);
+        pool.Release(sketch3);
+    }
+
+    [Fact]
+    public void WhenPoolFull_GetsNewInstance()
+    {
+        const int maxPoolSize = 10;
+        var pool = new DDSketchPool(maxPoolSize);
+        var pools = new List<DDSketch>(maxPoolSize);
+
+        for (var i = 0; i < maxPoolSize; i++)
+        {
+            pools.Add(pool.Get());
+        }
+
+        pools.Should().OnlyHaveUniqueItems();
+
+        var sketch = pool.Get();
+        pools.Should().NotContain(sketch);
+    }
+
+    [Fact]
+    public void WhenPoolReleased_ItIsCleared()
+    {
+        var pool = new DDSketchPool();
+        var sketch = pool.Get();
+
+        sketch.Add(5);
+        sketch.IsEmpty().Should().BeFalse();
+
+        pool.Release(sketch);
+        sketch.IsEmpty().Should().BeTrue();
+    }
+}
