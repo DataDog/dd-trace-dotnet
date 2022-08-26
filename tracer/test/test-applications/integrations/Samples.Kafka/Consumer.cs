9594409bd55c1f4b71d26179b5b04b40c8d086a4
Andrew Lock <andrew.lock@datadoghq.com>
2022-08-26 09:54:34 +0100

    Add Data Streams Monitoring integration test and snapshot
    Reused a lot of the code from Samples.Kafka for creating consumers and producing messages, so extracted a base class to help with that.
    
    The snapshots are rough but the best I can do.

--- tracer/test/test-applications/integrations/Samples.Kafka/Consumer.cs
+++ tracer/test/test-applications/integrations/Samples.Kafka/Consumer.cs
@@ -9,142 +9,17 @@ using Newtonsoft.Json;
 
 namespace Samples.Kafka
 {
-    internal class Consumer: IDisposable
+    internal class Consumer: ConsumerBase
     {
-        private readonly string _consumerName;
-        private readonly IConsumer<string, string> _consumer;
-
-        public static int TotalAsyncMessages = 0;
-        public static int TotalSyncMessages = 0;
-        public static int TotalTombstones = 0;
-
         private Consumer(ConsumerConfig config, string topic, string consumerName)
+            : base(config, topic, consumerName)
         {
-            _consumerName = consumerName;
-            _consumer = new ConsumerBuilder<string, string>(config).Build();
-            _consumer.Subscribe(topic);
         }
 
-
-        public bool Consume(int retries, int timeoutMilliSeconds)
-        {
-            try
-            {
-                for (int i = 0; i < retries; i++)
-                {
-                    try
-                    {
-                        // will block until a message is available
-                        // on 1.5.3 this will throw if the topic doesn't exist
-                        var consumeResult = _consumer.Consume(timeoutMilliSeconds);
-                        if (consumeResult is null)
-                        {
-                            Console.WriteLine($"{_consumerName}: Null consume result");
-                            return true;
-                        }
-
-                        if (consumeResult.IsPartitionEOF)
-                        {
-                            Console.WriteLine($"{_consumerName}: Reached EOF");
-                            return true;
-                        }
-                        else
-                        {
-                            HandleMessage(consumeResult);
-                            return true;
-                        }
-                    }
-                    catch (ConsumeException ex)
-                    {
-                        Console.WriteLine($"Consume Exception in manual consume: {ex}");
-                    }
-
-                    Task.Delay(500);
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                Console.WriteLine($"{_consumerName}: Cancellation requested, exiting.");
-            }
-
-            return false;
-        }
-
-        public void Consume(CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    // will block until a message is available
-                    var consumeResult = _consumer.Consume(cancellationToken);
-
-                    if (consumeResult.IsPartitionEOF)
-                    {
-                        Console.WriteLine($"{_consumerName}: Reached EOF");
-                    }
-                    else
-                    {
-                        HandleMessage(consumeResult);
-                    }
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                Console.WriteLine($"{_consumerName}: Cancellation requested, exiting.");
-            }
-        }
-
-        public void ConsumeWithExplicitCommit(int commitEveryXMessages, CancellationToken cancellationToken = default)
-        {
-            ConsumeResult<string, string> consumeResult = null;
-            try
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    // will block until a message is available
-                    consumeResult = _consumer.Consume(cancellationToken);
-
-                    if (consumeResult.IsPartitionEOF)
-                    {
-                        Console.WriteLine($"{_consumerName}: Reached EOF");
-                    }
-                    else
-                    {
-                        HandleMessage(consumeResult);
-                    }
-
-                    if (consumeResult.Offset % commitEveryXMessages == 0)
-                    {
-                        try
-                        {
-                            Console.WriteLine($"{_consumerName}: committing...");
-                            _consumer.Commit(consumeResult);
-                        }
-                        catch (KafkaException e)
-                        {
-                            Console.WriteLine($"{_consumerName}: commit error: {e.Error.Reason}");
-                        }
-                    }
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                Console.WriteLine($"{_consumerName}: Cancellation requested, exiting.");
-            }
-
-            // As we're doing manual commit, make sure we force a commit now
-            if (consumeResult is not null)
-            {
-                Console.WriteLine($"{_consumerName}: committing...");
-                _consumer.Commit(consumeResult);
-            }
-        }
-
-        private void HandleMessage(ConsumeResult<string, string> consumeResult)
+        protected override void HandleMessage(ConsumeResult<string, string> consumeResult)
         {
             var kafkaMessage = consumeResult.Message;
-            Console.WriteLine($"{_consumerName}: Consuming {kafkaMessage.Key}, {consumeResult.TopicPartitionOffset}");
+            Console.WriteLine($"{ConsumerName}: Consuming {kafkaMessage.Key}, {consumeResult.TopicPartitionOffset}");
 
             var messageHeaders = kafkaMessage.Headers;
             SampleHelpers.ExtractScope(messageHeaders, GetValues, out var traceId, out var spanId);
@@ -196,13 +71,6 @@ namespace Samples.Kafka
                     Interlocked.Increment(ref TotalSyncMessages);
                 }
             }
-        }
-
-        public void Dispose()
-        {
-            Console.WriteLine($"{_consumerName}: Closing consumer");
-            _consumer?.Close();
-            _consumer?.Dispose();
         }
 
         public static Consumer Create(bool enableAutoCommit, string topic, string consumerName)
