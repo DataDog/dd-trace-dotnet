8a149966fd1b91399296672a9f371d3ea5908427
Andrew Lock <andrew.lock@datadoghq.com>
2022-08-30 09:38:21 +0100

    Replace the SpanIdGenerator with ThreadSafeRandom
    SpanIdGenerator was a thin wrapper around essentially the same implementation, so converted it to a simple static method that uses ThreadSafeRandom implicitly
    
    The downside to this is we can't create a `SpanIdGenerator` with a specific seed any more. But as far as I can tell we were never doing that anyway, even in tests, so I think this is simpler

--- tracer/src/Datadog.Trace/Util/SpanIdGenerator.cs
+++ tracer/src/Datadog.Trace/Util/SpanIdGenerator.cs
@@ -9,66 +9,13 @@ namespace Datadog.Trace.Util
 {
     internal class SpanIdGenerator
     {
-#if NETFRAMEWORK
-        private static readonly Random GlobalSeedGenerator = new Random();
-#endif
-
-        [ThreadStatic]
-        private static SpanIdGenerator _threadInstance;
-
-        private readonly Random _random;
-
-        public SpanIdGenerator()
-        {
-            _random = new Random();
-        }
-
-        public SpanIdGenerator(int seed)
-        {
-            _random = new Random(seed);
-        }
-
-        public static SpanIdGenerator ThreadInstance
-        {
-            get
-            {
-                SpanIdGenerator idGenerator = _threadInstance;
-
-                if (idGenerator == null)
-                {
-#if NETFRAMEWORK
-                    // On .NET Framework, the clock is used to seed the new random instances.
-                    // Resolution is too low, so if many threads are created at the same time,
-                    // some could end up using the same seed.
-                    // Instead, use a shared random instance to generate the seed.
-                    // The same approach was used for System.Random on .NET Core:
-                    // https://docs.microsoft.com/en-us/dotnet/api/system.random.-ctor?view=netcore-3.1
-                    int seed;
-
-                    lock (GlobalSeedGenerator)
-                    {
-                        seed = GlobalSeedGenerator.Next();
-                    }
-
-                    idGenerator = new SpanIdGenerator(seed);
-#else
-                    idGenerator = new SpanIdGenerator();
-#endif
-
-                    _threadInstance = idGenerator;
-                }
-
-                return idGenerator;
-            }
-        }
-
-        public ulong CreateNew()
+        public static ulong CreateNew()
         {
             ulong value;
             do
             {
-                long high = _random.Next(int.MinValue, int.MaxValue);
-                long low = _random.Next(int.MinValue, int.MaxValue);
+                long high = ThreadSafeRandom.Next(int.MinValue, int.MaxValue);
+                long low = ThreadSafeRandom.Next(int.MinValue, int.MaxValue);
 
                 // Concatenate both values, and truncate the 32 top bits from low
                 value = (ulong)(high << 32 | (low & 0xFFFFFFFF)) & 0x7FFFFFFFFFFFFFFF;
