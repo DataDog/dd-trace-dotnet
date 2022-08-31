98d410470c2906ce83fddf03a7bbd2bd06658208
Alexander S <shurivich@gmail.com>
2022-08-31 17:37:45 +0300

    Support UDS and Named Pipes for Live Debugger (#3125)
    * support uds

--- tracer/src/Datadog.Trace/RemoteConfigurationManagement/Transport/RemoteConfigurationApiFactory.cs
+++ tracer/src/Datadog.Trace/RemoteConfigurationManagement/Transport/RemoteConfigurationApiFactory.cs
@@ -2,16 +2,28 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+#nullable enable
 
+using System;
 using Datadog.Trace.Agent;
 using Datadog.Trace.Agent.DiscoveryService;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.HttpOverStreams;
 
 namespace Datadog.Trace.RemoteConfigurationManagement.Transport
 {
     internal class RemoteConfigurationApiFactory
     {
-        public static IRemoteConfigurationApi Create(RemoteConfigurationSettings remoteConfigurationSettings, IApiRequestFactory apiRequestFactory, IDiscoveryService discoveryService)
+        public static IRemoteConfigurationApi Create(ImmutableExporterSettings exporterSettings, RemoteConfigurationSettings remoteConfigurationSettings, IDiscoveryService discoveryService)
         {
+            var apiRequestFactory = AgentTransportStrategy.Get(
+                exporterSettings,
+                productName: "rcm",
+                tcpTimeout: TimeSpan.FromSeconds(15),
+                AgentHttpHeaderNames.MinimalHeaders,
+                () => new MinimalAgentHeaderHelper(),
+                uri => uri);
+
             return
                 string.IsNullOrWhiteSpace(remoteConfigurationSettings.FilePath)
                     ? RemoteConfigurationApi.Create(apiRequestFactory, discoveryService)
