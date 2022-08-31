612b4a752baf0d1c759ab03ff7ab13e7afc79f7a
Robert Pickering <robert.pickering@datadoghq.com>
2022-08-31 18:54:09 +0200

    Rough cut of RCTE2 ack might look like

---
+++ tracer/src/Datadog.Trace/RemoteConfigurationManagement/NamedTypedFile.cs
@@ -0,0 +1,26 @@
+// <copyright file="NamedTypedFile.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datadog.Trace.RemoteConfigurationManagement
+{
+    internal class NamedTypedFile<T>
+    {
+        public NamedTypedFile(string name, T value)
+        {
+            Name = name;
+            TypedFile = value;
+        }
+
+        public string Name { get; }
+
+        public T TypedFile { get; }
+    }
+}
--- tracer/src/Datadog.Trace/RemoteConfigurationManagement/ProductConfigChangedEventArgs.cs
+++ tracer/src/Datadog.Trace/RemoteConfigurationManagement/ProductConfigChangedEventArgs.cs
@@ -12,22 +12,59 @@ namespace Datadog.Trace.RemoteConfigurationManagement
 {
     internal class ProductConfigChangedEventArgs : EventArgs
     {
-        private readonly IEnumerable<byte[]> _configContents;
+        private readonly IEnumerable<NamedRawFile> _configContents;
 
-        public ProductConfigChangedEventArgs(IEnumerable<byte[]> configContents)
+        private Dictionary<string, ApplyDetails> applyStates = new();
+
+        public ProductConfigChangedEventArgs(IEnumerable<NamedRawFile> configContents)
         {
             _configContents = configContents;
         }
 
-        public IEnumerable<T> GetDeserializedConfigurations<T>()
+        public IEnumerable<NamedTypedFile<T>> GetDeserializedConfigurations<T>()
         {
             foreach (var configContent in _configContents)
             {
                 using var stream = new MemoryStream(configContent);
                 using var streamReader = new StreamReader(stream);
                 using var jsonReader = new JsonTextReader(streamReader);
-                yield return JsonSerializer.CreateDefault().Deserialize<T>(jsonReader);
+                yield return new NamedTypedFile<T>(c.Name, JsonSerializer.CreateDefault().Deserialize<T>(jsonReader));
             }
+        }
+
+        public void Acknowldge(string filename)
+        {
+            var applyDetails = GetOrCreateApplyDetails(filename);
+
+            // can only move from unack to ack
+            if (applyDetails.ApplyState == ApplyState.UNACKNOWLEDGED)
+            {
+                applyDetails.ApplyState = ApplyState.ACKNOWLEDGED;
+            }
+        }
+
+        public void Error(string filename, string error)
+        {
+            var applyDetails = GetOrCreateApplyDetails(filename);
+            applyDetails.ApplyState = ApplyState.ERROR;
+            applyDetails.Error = error;
+        }
+
+        public IEnumerable<ApplyDetails> GetResults()
+        {
+            return applyStates.Values;
+        }
+
+        private ApplyDetails GetOrCreateApplyDetails(string filename)
+        {
+            ApplyDetails applyDetails = null;
+            if (!applyStates.TryGetValue(filename, out applyDetails))
+            {
+                applyDetails = new ApplyDetails() { Filename = filename };
+                applyStates.Add(filename, applyDetails);
+            }
+
+            return applyDetails;
         }
     }
 }
