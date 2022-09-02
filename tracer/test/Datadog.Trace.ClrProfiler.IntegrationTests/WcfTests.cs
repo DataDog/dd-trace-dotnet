b6ee8b52b32a0690e5dc8c80d784f8ed748fd0dc
Kevin Gosse <kevin.gosse@datadoghq.com>
2022-09-02 18:45:06 +0200

    Update WcfTests.cs

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/WcfTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/WcfTests.cs
@@ -93,7 +93,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
                 var settings = VerifyHelper.GetSpanVerifierSettings(binding, enableNewWcfInstrumentation, enableWcfObfuscation);
 
-                await VerifyHelper.Verify(spans, settings)
+                await VerifyHelper.VerifySpans(spans, settings)
                               .UseMethodName("_");
 
                 foreach (var span in spans)
