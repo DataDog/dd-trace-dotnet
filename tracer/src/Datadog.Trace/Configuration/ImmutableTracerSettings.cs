48b9ecde0713bd2b083a9a476ce4a3444e980f45
Alexander S <alexander.seleznyov@datadoghq.com>
2022-09-04 20:56:20 +0300

    fix tests

--- tracer/src/Datadog.Trace/Configuration/ImmutableTracerSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/ImmutableTracerSettings.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using Datadog.Trace.Debugger;
 using Datadog.Trace.Logging.DirectSubmission;
 using Datadog.Trace.Util;
 
