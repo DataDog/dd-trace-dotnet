f6320905850070cc8fd40d5ec9c40b8251b51bc7
Christophe Nasarre <christophe.nasarre@datadoghq.com>
2022-09-05 15:49:02 +0200

    Handle header template 2017/2022 template difference

--- tracer/test/Datadog.Trace.TestHelpers/MockSpan.cs
+++ tracer/test/Datadog.Trace.TestHelpers/MockSpan.cs
@@ -1,6 +1,6 @@
 // <copyright file="MockSpan.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
-// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
 using System.Collections.Generic;
