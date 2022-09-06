49b000d66975f5d366d0475320b3072d5f5f5238
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-09-06 15:37:30 +0200

    Instrumentation of basic hashing types

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/InsecureHashing/InsecureHashingTags.cs
@@ -0,0 +1,15 @@
+// <copyright file="InsecureHashingTags.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Datadog.Trace.SourceGenerators;
+
+namespace Datadog.Trace.Tagging
+{
+    internal partial class InsecureHashingTags : InstrumentationTags
+    {
+        [Tag(Trace.Tags.SpanKind)]
+        public override string SpanKind => SpanKinds.Consumer;
+    }
+}
