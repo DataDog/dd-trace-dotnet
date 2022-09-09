af1caece8c9b13972884189cdf37b2eda66ec0d3
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-09-09 16:26:19 +0200

    Add IAST configuration

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/HashAlgorithm/IAST/Settings/IASTSettings.cs
@@ -0,0 +1,32 @@
+// <copyright file="IASTSettings.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Logging;
+using Datadog.Trace.Util.Http;
+
+namespace Datadog.Trace.IAST
+{
+    internal class IASTSettings
+    {
+        public IASTSettings(IConfigurationSource source)
+        {
+            Enabled = source?.GetBool(ConfigurationKeys.IAST.Enabled) ?? false;
+            InsecureHashingAlgorithmEnabled = source?.GetBool(ConfigurationKeys.IAST.WeakHashAlgorithmsEnabled) ?? true;
+        }
+
+        public bool Enabled { get; set; }
+
+        public bool InsecureHashingAlgorithmEnabled { get; set; }
+
+        public static IASTSettings FromDefaultSources()
+        {
+            return new IASTSettings(GlobalConfigurationSource.Instance);
+        }
+    }
+}
