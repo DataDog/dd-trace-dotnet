59d9a879f942beaf124d4356704815e341f79309
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-09-12 10:13:26 +0200

    Move files.

--- tracer/src/Datadog.Trace/IAST/Settings/IASTSettings.cs
+++ tracer/src/Datadog.Trace/IAST/Settings/IASTSettings.cs
@@ -15,14 +15,14 @@ internal class IastSettings
     public static readonly string WeakHashAlgorithmsDefault = "HMACMD5,MD5,HMACSHA1,SHA1";
 
     public IastSettings(IConfigurationSource source)
-    {
+        {
         WeakCipherAlgorithms = source?.GetString(ConfigurationKeys.Iast.WeakCipherAlgorithms) ?? WeakCipherAlgorithmsDefault;
         WeakCipherAlgorithmsArray = WeakCipherAlgorithms.Split(new char[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries);
         WeakHashAlgorithms = source?.GetString(ConfigurationKeys.Iast.WeakHashAlgorithms) ?? WeakHashAlgorithmsDefault;
         WeakHashAlgorithmsArray = WeakHashAlgorithms.Split(new char[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries);
         Enabled = (source?.GetBool(ConfigurationKeys.Iast.Enabled) ?? false) &&
             (WeakHashAlgorithmsArray.Length > 0 || WeakCipherAlgorithmsArray.Length > 0);
-    }
+        }
 
     public bool Enabled { get; set; }
 
@@ -35,7 +35,7 @@ internal class IastSettings
     public string WeakCipherAlgorithms { get; }
 
     public static IastSettings FromDefaultSources()
-    {
+        {
         return new IastSettings(GlobalConfigurationSource.Instance);
     }
 }
