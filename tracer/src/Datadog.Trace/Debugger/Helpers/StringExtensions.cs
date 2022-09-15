ed20e714656155fa081ce66b51058e226f2ea46e
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-09-15 16:49:30 +0200

    Avoid insecure hashing spans form DD code.

--- tracer/src/Datadog.Trace/Debugger/Helpers/StringExtensions.cs
+++ tracer/src/Datadog.Trace/Debugger/Helpers/StringExtensions.cs
@@ -15,16 +15,16 @@ namespace Datadog.Trace.Debugger.Helpers
         public static string ToUUID(this string input)
         {
             byte[] hash;
-            using (var md5 = MD5.Create())
+            using (var sha256 = SHA256.Create())
             {
-                hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+                hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
             }
 
             hash[6] &= 0x0f;
             hash[6] |= 0x30;
             hash[8] &= 0x3f;
             hash[8] |= 0x80;
-            var hex = BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+            var hex = BitConverter.ToString(hash).Replace("-", string.Empty).Substring(0, 32).ToLower();
             return hex.Insert(8, "-").Insert(13, "-").Insert(18, "-").Insert(23, "-");
         }
     }
