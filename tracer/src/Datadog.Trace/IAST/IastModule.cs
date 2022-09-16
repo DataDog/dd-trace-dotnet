eee079be1baa8459fca220789cec010c0f95405d
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-09-16 11:23:52 +0200

    Add full method name if filename is not available

--- tracer/src/Datadog.Trace/IAST/IastModule.cs
+++ tracer/src/Datadog.Trace/IAST/IastModule.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using Datadog.Trace.ClrProfiler.AutoInstrumentation.HashAlgorithm;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.Iast;
