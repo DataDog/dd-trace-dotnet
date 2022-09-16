9f55ec08ba1ad1904970841f2f92bea8f65218a5
Robert Pickering <robert@strangelights.com>
2022-09-16 18:43:06 +0200

    update error message

--- tracer/test/Datadog.Trace.Security.IntegrationTests/AspNetCore5AsmToggle.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/AspNetCore5AsmToggle.cs
@@ -101,7 +101,7 @@ namespace Datadog.Trace.Security.IntegrationTests
             agent.SetupRcm(Output, new[] { ((object)"haha, you weren't expect this!", "1") }, "ASM_FEATURES");
 
             var request = await agent.WaitRcmRequestAndReturnLast();
-            CheckAckState(request, ApplyStates.ERROR, "Error converting value \"haha, you weren't expect this!\" to type 'Datadog.Trace.Configuration.Features'. Path '', line 1, position 32.", "First RCM call");
+            CheckAckState(request, ApplyStates.ERROR, "Error converting value \"haha, you weren't expect this!\" to type 'Datadog.Trace.AppSec.AsmFeatures'. Path '', line 1, position 32.", "First RCM call");
 
             await VerifySpans(spans1.ToImmutableList(), settings, true);
         }
