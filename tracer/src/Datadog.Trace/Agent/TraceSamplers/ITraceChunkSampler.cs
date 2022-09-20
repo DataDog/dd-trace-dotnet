311904c2e6d0b4ca0e2437ad3c2cca25b2a65fc9
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2022-09-20 11:18:28 -0400

    Rename ITraceSampler to ITraceChunkSampler

--- tracer/src/Datadog.Trace/Agent/TraceSamplers/ITraceChunkSampler.cs
+++ tracer/src/Datadog.Trace/Agent/TraceSamplers/ITraceChunkSampler.cs
@@ -1,4 +1,4 @@
-// <copyright file="ITraceSampler.cs" company="Datadog">
+// <copyright file="ITraceChunkSampler.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -7,7 +7,7 @@ using System;
 
 namespace Datadog.Trace.Agent
 {
-    internal interface ITraceSampler
+    internal interface ITraceChunkSampler
     {
         /// <summary>
         /// Determines if a trace chunk should be sampled
