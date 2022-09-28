a3f957c59d634e4532c127971c2c39cc6888b4af
Pierre Bonet <pierre.bonet@datadoghq.com>
2022-09-28 16:38:33 +0200

    Rename a few other variables, and change the clean

--- tracer/build/_build/Projects.cs
+++ tracer/build/_build/Projects.cs
@@ -1,11 +1,11 @@
 public static class Projects
 {
-    public const string DatadogMonitoringDistribution = "Datadog.Trace.Bundle";
     public const string DatadogTrace = "Datadog.Trace";
     public const string DatadogTraceAnnotations = "Datadog.Trace.Annotations";
     public const string DatadogTraceAspNet = "Datadog.Trace.AspNet";
-    public const string DatadogTraceOpenTracing = "Datadog.Trace.OpenTracing";
+    public const string DatadogTraceBundle = "Datadog.Trace.Bundle";
     public const string DatadogTraceMsBuild = "Datadog.Trace.MSBuild";
+    public const string DatadogTraceOpenTracing = "Datadog.Trace.OpenTracing";
     public const string ClrProfilerNative = "Datadog.Tracer.Native";
     public const string NativeLoader = "Datadog.Trace.ClrProfiler.Native";
 
