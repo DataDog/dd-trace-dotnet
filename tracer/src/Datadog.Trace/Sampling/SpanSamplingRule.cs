46676b17c13f16dcf5fd6c09f2093e7254ccda50
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2022-09-30 09:00:17 -0400

    Add SpanSamplingRule implementation

---
+++ tracer/src/Datadog.Trace/Sampling/SpanSamplingRule.cs
@@ -0,0 +1,157 @@
+// <copyright file="SpanSamplingRule.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Datadog.Trace.Logging;
+using Datadog.Trace.Util;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
+
+namespace Datadog.Trace.Sampling
+{
+    /// <summary>
+    /// Represents a sampling rules for single span ingestion.
+    /// </summary>
+    internal class SpanSamplingRule : ISpanSamplingRule
+    {
+        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<SpanSamplingRule>();
+        private static readonly TimeSpan RegexTimeout = TimeSpan.FromSeconds(1);
+
+        // TODO consider moving toward this https://github.com/dotnet/runtime/blob/main/src/libraries/Common/src/System/Text/SimpleRegex.cs
+        private readonly Regex _serviceNameRegex;
+        private readonly Regex _operationNameRegex;
+
+        private readonly IRateLimiter _limiter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpanSamplingRule"/> class with default values.
+        /// </summary>
+        public SpanSamplingRule()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpanSamplingRule"/> class.
+        /// </summary>
+        /// <param name="serviceNameGlob">The glob pattern for the <see cref="Span.ServiceName"/>.</param>
+        /// <param name="operationNameGlob">The glob pattern for the <see cref="Span.OperationName"/>.</param>
+        /// <param name="samplingRate">The proportion of spans that are kept. <c>1.0</c> indicates keep all where <c>0.0</c> would be drop all.</param>
+        /// <param name="maxPerSecond">The maximum number of spans allowed to be kept per second - <see langword="null"/> indicates that there is no limit</param>
+        public SpanSamplingRule(string serviceNameGlob, string operationNameGlob, float samplingRate = 1.0f, float? maxPerSecond = null)
+        {
+            if (string.IsNullOrWhiteSpace(serviceNameGlob))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(serviceNameGlob));
+            }
+
+            if (string.IsNullOrWhiteSpace(operationNameGlob))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(operationNameGlob));
+            }
+
+            SamplingRate = samplingRate;
+            MaxPerSecond = maxPerSecond;
+
+            _serviceNameRegex = ConvertGlobToRegex(serviceNameGlob);
+            _operationNameRegex = ConvertGlobToRegex(operationNameGlob);
+
+            // null/absent for MaxPerSecond indicates unlimited, which is a negative value in the limiter
+            _limiter = MaxPerSecond is null ? new SpanRateLimiter(-1) : new SpanRateLimiter((int?)MaxPerSecond);
+        }
+
+        /// <inheritdoc/>
+        public float SamplingRate { get; } = 1.0f;
+
+        /// <inheritdoc/>
+        public float? MaxPerSecond { get; } = null;
+
+        /// <summary>
+        ///     Creates <see cref="SpanSamplingRule"/>s from the supplied JSON <paramref name="configuration"/>.
+        /// </summary>
+        /// <param name="configuration">The JSON-serialized configuration.</param>
+        /// <returns><see cref="IEnumerable{T}"/> of <see cref="SpanSamplingRule"/>.</returns>
+        public static IEnumerable<SpanSamplingRule> BuildFromConfigurationString(string configuration)
+        {
+            if (string.IsNullOrWhiteSpace(configuration))
+            {
+                return Enumerable.Empty<SpanSamplingRule>();
+            }
+
+            try
+            {
+                var rules = JsonConvert.DeserializeObject<List<SpanSamplingRuleConfig>>(configuration);
+                return rules?.Select(
+                                  rule => new SpanSamplingRule(
+                                      rule.ServiceNameGlob,
+                                      rule.OperationNameGlob,
+                                      rule.SampleRate,
+                                      rule.MaxPerSecond))
+                             .ToList() ?? Enumerable.Empty<SpanSamplingRule>();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Unable to parse the span sampling rule.");
+                return Enumerable.Empty<SpanSamplingRule>();
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool ShouldKeep(Span span)
+        {
+            // TODO should we log here?
+            if (!IsMatch(span))
+            {
+                return false;
+            }
+
+            var sampleKeep = SamplingHelpers.SampleByRate(span.SpanId, SamplingRate);
+
+            if (!sampleKeep)
+            {
+                return false;
+            }
+
+            var limitKeep = _limiter.Allowed(span);
+
+            return limitKeep;
+        }
+
+        internal bool IsMatch(Span span)
+        {
+            if (span is null)
+            {
+                return false;
+            }
+
+            return _serviceNameRegex.Match(span.ServiceName).Success && _operationNameRegex.Match(span.OperationName).Success;
+        }
+
+        private static Regex ConvertGlobToRegex(string glob)
+        {
+            // TODO default glob (maybe null/empty/whitespace) should be *
+            var regexPattern = "^" + Regex.Escape(glob).Replace("\\?", ".").Replace("\\*", ".*") + "$";
+            var regex = new Regex(regexPattern, RegexOptions.Compiled, RegexTimeout);
+            return regex;
+        }
+
+        [Serializable]
+        internal class SpanSamplingRuleConfig
+        {
+            [JsonProperty(PropertyName = "service")]
+            public string ServiceNameGlob { get; set; } = "*";
+
+            [JsonProperty(PropertyName = "name")]
+            public string OperationNameGlob { get; set; } = "*";
+
+            [JsonProperty(PropertyName = "sample_rate")]
+            public float SampleRate { get; set; } = 1.0f; // default to accept all
+
+            [JsonProperty(PropertyName = "max_per_second")]
+            public float? MaxPerSecond { get; set; } = null;
+        }
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/Sampling/SpanSamplingRuleTests.cs
@@ -0,0 +1,186 @@
+// <copyright file="SpanSamplingRuleTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Linq;
+using Datadog.Trace.Sampling;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Sampling
+{
+    [Collection(nameof(Datadog.Trace.Tests.Sampling))]
+    public class SpanSamplingRuleTests
+    {
+        // copied these from CustomSamplingRule - maybe should combine or share?
+        private static readonly ulong Id = 1;
+        private static readonly Span CartCheckoutSpan = new Span(new SpanContext(Id++, Id++, null, serviceName: "shopping-cart-service"), DateTimeOffset.Now) { OperationName = "checkout" };
+        private static readonly Span AddToCartSpan = new Span(new SpanContext(Id++, Id++, null, serviceName: "shopping-cart-service"), DateTimeOffset.Now) { OperationName = "cart-add" };
+        private static readonly Span ShippingAuthSpan = new Span(new SpanContext(Id++, Id++, null, serviceName: "shipping-auth-service"), DateTimeOffset.Now) { OperationName = "authorize" };
+        private static readonly Span ShippingRevertSpan = new Span(new SpanContext(Id++, Id++, null, serviceName: "shipping-auth-service"), DateTimeOffset.Now) { OperationName = "authorize-revert" };
+        private static readonly Span RequestShippingSpan = new Span(new SpanContext(Id++, Id++, null, serviceName: "request-shipping"), DateTimeOffset.Now) { OperationName = "submit" };
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")] // empty
+        [InlineData(" ")] // whitespace
+        public void Constructor_ShouldThrow_ArgumentException_WhenServiceNameGlob_EmptyOrNull(string serviceNameGlob)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new SpanSamplingRule(serviceNameGlob, "*", 1.0f, null));
+            Assert.Equal("serviceNameGlob", exception.ParamName);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")] // empty
+        [InlineData(" ")] // whitespace
+        public void Constructor_ShouldThrow_ArgumentException_WhenOperationNameGlob_EmptyOrNull(string operationNameGlob)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new SpanSamplingRule("*", operationNameGlob, 1.0f, null));
+            Assert.Equal("operationNameGlob", exception.ParamName);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void BuildFromConfigurationString_ShouldReturnEmpty_WhenNoConfigGiven(string config)
+        {
+            var result = SpanSamplingRule.BuildFromConfigurationString(config);
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData("[{\"service\":\"shopping-cart*\", \"name\":\"checkou?\", \"sample_rate\":0.5}]")]
+        [InlineData("[{\"service\":\"shopping-cart*\", \"name\":\"checkou?\", \"max_per_second\":1000.5}]")]
+        public void BuildFromConfigurationString_ShouldHandle_MissingOptionals(string config)
+        {
+            var result = SpanSamplingRule.BuildFromConfigurationString(config);
+
+            Assert.Single(result);
+        }
+
+        [Theory]
+        [InlineData("test")]
+        public void BuildFromConfigurationString_ShouldHandle_MalformedData(string config)
+        {
+            var result = SpanSamplingRule.BuildFromConfigurationString(config);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void BuildFromConfigurationString_Should_ReturnSingleRule()
+        {
+            var config = "[{\"service\":\"shopping-cart*\", \"name\":\"checkou?\", \"sample_rate\":0.5, \"max_per_second\":1000.5}]";
+
+            VerifySingleRule(config, CartCheckoutSpan, true);
+            VerifySingleRule(config, AddToCartSpan, false);
+            VerifySingleRule(config, ShippingAuthSpan, false);
+            VerifySingleRule(config, ShippingRevertSpan, false);
+            VerifySingleRule(config, RequestShippingSpan, false);
+        }
+
+        [Fact]
+        public void WildcardService_ShouldMatch_OnOperation()
+        {
+            var config = "[{\"service\":\"*\", \"name\":\"authorize\", \"sample_rate\":0.5, \"max_per_second\":1000.5}]";
+
+            VerifySingleRule(config, CartCheckoutSpan, false);
+            VerifySingleRule(config, AddToCartSpan, false);
+            VerifySingleRule(config, ShippingAuthSpan, true);
+            VerifySingleRule(config, ShippingRevertSpan, false);
+            VerifySingleRule(config, RequestShippingSpan, false);
+        }
+
+        [Fact]
+        public void WildcardOperation_ShouldMatch_OnService()
+        {
+            var config = "[{\"service\":\"shopping-cart-service\", \"name\":\"*\", \"sample_rate\":0.5, \"max_per_second\":1000.5}]";
+
+            VerifySingleRule(config, CartCheckoutSpan, true);
+            VerifySingleRule(config, AddToCartSpan, true);
+            VerifySingleRule(config, ShippingAuthSpan, false);
+            VerifySingleRule(config, ShippingRevertSpan, false);
+            VerifySingleRule(config, RequestShippingSpan, false);
+        }
+
+        [Theory]
+        [InlineData("*", "*", true)]
+        public void MatchAll_ShouldMatchAll(string serviceGlob, string operationGlob, bool shouldMatch)
+        {
+            var rule = new SpanSamplingRule(serviceGlob, operationGlob);
+            Assert.Equal(rule.IsMatch(CartCheckoutSpan), shouldMatch);
+            Assert.Equal(rule.IsMatch(AddToCartSpan), shouldMatch);
+            Assert.Equal(rule.IsMatch(ShippingAuthSpan), shouldMatch);
+            Assert.Equal(rule.IsMatch(ShippingRevertSpan), shouldMatch);
+            Assert.Equal(rule.IsMatch(RequestShippingSpan), shouldMatch);
+        }
+
+        [Fact]
+        public void ShouldKeep_ShouldReturnFalse_ForNullSpan()
+        {
+            var rule = new SpanSamplingRule("*", "*");
+
+            Assert.False(rule.ShouldKeep(null));
+        }
+
+        [Fact]
+        public void ShouldKeep_ShouldReturnFalse_WhenServiceAndOperationDontMatch()
+        {
+            var config = "[{\"service\":\"test\", \"name\":\"test\"}]";
+            var rule = SpanSamplingRule.BuildFromConfigurationString(config).Single();
+
+            Assert.False(rule.ShouldKeep(CartCheckoutSpan));
+        }
+
+        [Fact]
+        public void ShouldKeep_ShouldReturnFalse_WhenSamplerIsZero()
+        {
+            var config = "[{\"service\":\"*\", \"name\":\"*\", \"sample_rate\":0.0}]";
+            var rule = SpanSamplingRule.BuildFromConfigurationString(config).Single();
+
+            Assert.False(rule.ShouldKeep(CartCheckoutSpan));
+        }
+
+        [Fact]
+        public void ShouldKeep_ShouldReturnTrue_WhenEverythingMatches()
+        {
+            var config = "[{\"service\":\"*\", \"name\":\"*\"}]";
+            var rule = SpanSamplingRule.BuildFromConfigurationString(config).Single();
+
+            Assert.True(rule.ShouldKeep(CartCheckoutSpan));
+        }
+
+        [Fact]
+        public void MaxPerSecond_ShouldDefaultTo_NullWhenAbsent()
+        {
+            var config = "[{\"service\":\"*\", \"name\":\"*\"}]";
+            var rule = SpanSamplingRule.BuildFromConfigurationString(config).Single();
+
+            Assert.Null(rule.MaxPerSecond);
+        }
+
+        [Fact]
+        public void SampleRate_ShouldDefaultTo_OneWhenAbsent()
+        {
+            var config = "[{\"service\":\"*\", \"name\":\"*\"}]";
+            var rule = SpanSamplingRule.BuildFromConfigurationString(config).Single();
+
+            Assert.Equal(1.0f, rule.SamplingRate);
+        }
+
+        private void VerifySingleRule(string config, Span span, bool isMatch)
+        {
+            var rule = SpanSamplingRule.BuildFromConfigurationString(config).Single();
+            VerifySingleRule(rule, span, isMatch);
+        }
+
+        private void VerifySingleRule(SpanSamplingRule rule, Span span, bool isMatch)
+        {
+            Assert.Equal(rule.IsMatch(span), isMatch);
+        }
+
+        // TODO no tests yet for rate limit / sampling priority
+    }
+}
