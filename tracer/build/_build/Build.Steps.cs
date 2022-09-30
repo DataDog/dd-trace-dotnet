a6d7525313c525f15ea6733b9ad21ca1db08a416
Tony Redondo <tony.redondo@datadoghq.com>
2022-09-30 19:24:32 +0200

    Add Apple Silicon support on tracer builds (#3279)
    * Fix CPP builds
    
    * Modify Nuke to support osx-arm64
    
    * More changes
    
    * fix issue
    
    * fix
    
    * Trying to fix RuntimeIdentifier problem with osx-arm64 in the BuildStandaloneTool build target
    
    * Final fix (disabling the osx-arm64 in dd-trace standalone build)
    
    * Merging LinuxArchitectureIdentifier and OsxArchitectureIdentifier

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -91,7 +91,7 @@ partial class Build
             : new[] { MSBuildTargetPlatform.x86 };
 
     bool IsArm64 => RuntimeInformation.ProcessArchitecture == Architecture.Arm64;
-    string LinuxArchitectureIdentifier => IsArm64 ? "arm64" : TargetPlatform.ToString();
+    string UnixArchitectureIdentifier => IsArm64 ? "arm64" : TargetPlatform.ToString();
 
     IEnumerable<string> LinuxPackageTypes => IsAlpine ? new[] { "tar" } : new[] { "deb", "rpm", "tar" };
 
@@ -642,7 +642,7 @@ partial class Build
             CopyFileToDirectory(BuildDirectory / "artifacts" / FileNames.CreateLogPathScript, assetsDirectory);
             chmod.Invoke($"+x {assetsDirectory / FileNames.CreateLogPathScript}");
 
-            var workingDirectory = ArtifactsDirectory / $"linux-{LinuxArchitectureIdentifier}";
+            var workingDirectory = ArtifactsDirectory / $"linux-{UnixArchitectureIdentifier}";
             EnsureCleanDirectory(workingDirectory);
 
             const string packageName = "datadog-dotnet-apm";
@@ -1845,16 +1845,17 @@ partial class Build
     private (string Arch, string Ext) GetLibDdWafUnixArchitectureAndExtension() =>
         (IsOsx) switch
         {
-            (true) => ("osx-x64", "dylib"),
-            (false) => ($"linux-{LinuxArchitectureIdentifier}", "so"), // LibDdWaf doesn't
+            // (true) => ($"osx-{UnixArchitectureIdentifier}", "dylib"), //LibDdWaf doesn't support osx-arm64 yet.
+            (true) => ($"osx-x64", "dylib"),
+            (false) => ($"linux-{UnixArchitectureIdentifier}", "so"), // LibDdWaf doesn't
         };
 
     private (string Arch, string Ext) GetUnixArchitectureAndExtension() =>
         (IsOsx, IsAlpine) switch
         {
-            (true, _) => ("osx-x64", "dylib"),
-            (false, false) => ($"linux-{LinuxArchitectureIdentifier}", "so"),
-            (false, true) => ($"linux-musl-{LinuxArchitectureIdentifier}", "so"),
+            (true, _) => ($"osx-{UnixArchitectureIdentifier}", "dylib"),
+            (false, false) => ($"linux-{UnixArchitectureIdentifier}", "so"),
+            (false, true) => ($"linux-musl-{UnixArchitectureIdentifier}", "so"),
         };
 
     // the integration tests need their own copy of the profiler, this achieved through build.props on Windows, but doesn't seem to work under Linux
