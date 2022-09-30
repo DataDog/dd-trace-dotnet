8004d97904dc91af48e48b913303cda0ba5ed322
Andrew Lock <andrew.lock@datadoghq.com>
2022-09-30 14:24:35 +0100

    Try loading SSL eagerly

--- tracer/src/Datadog.Trace/ClrProfiler/Instrumentation.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/Instrumentation.cs
@@ -16,6 +16,7 @@ using Datadog.Trace.Ci;
 using Datadog.Trace.ClrProfiler.ServerlessInstrumentation;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.Debugger;
+using Datadog.Trace.Debugger.Helpers;
 using Datadog.Trace.DiagnosticListeners;
 using Datadog.Trace.Logging;
 using Datadog.Trace.RemoteConfigurationManagement;
@@ -192,6 +193,21 @@ namespace Datadog.Trace.ClrProfiler
                 }
             }
 
+#if NETSTANDARD2_0
+            try
+            {
+                // On .NET Core 2.0-3.0 we see an occasional hang caused by OpenSSL being loaded
+                // while the app is shutting down, which results in flaky tests due to the short-
+                // lived nature of our apps. This appears to be a bug in the runtime (although
+                // we haven't yet confirmed that). Calling the `ToUuid()` method uses an MD5
+                // hash which calls into the native library, triggering the load.
+                var result = string.Empty.ToUUID();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error triggering eager OpenSSL load");
+            }
+#endif
             LifetimeManager.Instance.AddShutdownTask(RunShutdown);
 
             Log.Debug("Initialization finished.");
