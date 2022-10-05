79f5f96ae3e6698aded18d0f48c8a93b06d6e8b1
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-10-05 14:00:47 -0400

    add comments

--- tracer/src/Datadog.Trace/Agent/MessagePack/SpanMessagePackFormatter.cs
+++ tracer/src/Datadog.Trace/Agent/MessagePack/SpanMessagePackFormatter.cs
@@ -18,7 +18,14 @@ namespace Datadog.Trace.Agent.MessagePack
     {
         public static readonly SpanMessagePackFormatter Instance = new();
 
-        // name of top-level span fields
+        // Cache the UTF-8 bytes for string constants (like tag names)
+        // and values that are constant within the lifetime of a service (like process id).
+        //
+        // Don't make these static to avoid the additional redirection when this
+        // assembly is loaded in the shared domain. We only create a single instance of
+        // this class so that's fine.
+
+        // names of top-level span fields
         private readonly byte[] _traceIdBytes = StringEncoding.UTF8.GetBytes("trace_id");
         private readonly byte[] _spanIdBytes = StringEncoding.UTF8.GetBytes("span_id");
         private readonly byte[] _nameBytes = StringEncoding.UTF8.GetBytes("name");
@@ -30,18 +37,16 @@ namespace Datadog.Trace.Agent.MessagePack
         private readonly byte[] _parentIdBytes = StringEncoding.UTF8.GetBytes("parent_id");
         private readonly byte[] _errorBytes = StringEncoding.UTF8.GetBytes("error");
 
-        // name of string tag dictionary
+        // string tags
         private readonly byte[] _metaBytes = StringEncoding.UTF8.GetBytes("meta");
-
         private readonly byte[] _languageNameBytes = StringEncoding.UTF8.GetBytes(Trace.Tags.Language);
         private readonly byte[] _languageValueBytes = StringEncoding.UTF8.GetBytes(TracerConstants.Language);
         private readonly byte[] _runtimeIdNameBytes = StringEncoding.UTF8.GetBytes(Trace.Tags.RuntimeId);
         private readonly byte[] _runtimeIdValueBytes = StringEncoding.UTF8.GetBytes(Tracer.RuntimeId);
         private readonly byte[] _originNameBytes = StringEncoding.UTF8.GetBytes(Trace.Tags.Origin);
 
-        // name of numeric tag dictionary
+        // numeric tags
         private readonly byte[] _metricsBytes = StringEncoding.UTF8.GetBytes("metrics");
-
         private readonly byte[] _samplingPriorityNameBytes = StringEncoding.UTF8.GetBytes(Trace.Metrics.SamplingPriority);
         private readonly byte[] _processIdNameBytes = StringEncoding.UTF8.GetBytes(Trace.Metrics.ProcessId);
         private readonly byte[] _processIdValueBytes;
@@ -285,7 +290,8 @@ namespace Datadog.Trace.Agent.MessagePack
                 offset += MessagePackBinary.WriteRaw(ref bytes, offset, _processIdValueBytes);
             }
 
-            // add "_sampling_priority_v1" tag to all "chunk orphans" (spans whose parents it not in the same chunk)
+            // add "_sampling_priority_v1" tag to all "chunk orphans"
+            // (spans whose parents are not found in the same chunk)
             if (model.IsChunkOrphan && model.TraceChunk?.SamplingPriority is { } samplingPriority)
             {
                 count++;
