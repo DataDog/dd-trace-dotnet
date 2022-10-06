6179356c1f1010d3b94116b5b443564436de7134
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-10-06 21:51:04 +0200

    Add top level CMakeLists.txt file

---
+++ tracer/build/cmake/FindCoreclr.cmake
@@ -0,0 +1,11 @@
+add_library(coreclr-lib INTERFACE)
+target_include_directories(coreclr-lib INTERFACE
+    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/native-lib/coreclr/src/pal/inc/rt
+    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/native-lib/coreclr/src/pal/prebuilt/inc
+    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/native-lib/coreclr/src/pal/inc
+    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/native-lib/coreclr/src/inc
+)
+
+target_sources(coreclr-lib INTERFACE
+    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/native-lib/coreclr/src/pal/prebuilt/idl/corprof_i.cpp
+)
