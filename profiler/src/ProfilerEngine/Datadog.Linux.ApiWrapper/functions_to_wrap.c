edc19fe54d065c6c6236fdbec0e0153da84ae050
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-10-06 16:32:53 +0200

    [Profiler] Fix deadlock on alpine on thread creation (#3288)
    * Address __pthread_create case
    * Apply only for Alpine

--- profiler/src/ProfilerEngine/Datadog.Linux.ApiWrapper/functions_to_wrap.c
+++ profiler/src/ProfilerEngine/Datadog.Linux.ApiWrapper/functions_to_wrap.c
@@ -1,10 +1,9 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
-#include <signal.h>
 #include <link.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <dlfcn.h>
 
 /* dl_iterate_phdr wrapper
 The .NET profiler on Linux uses a classic signal-based approach to collect thread callstack.
@@ -34,11 +33,10 @@ This is done by libunwind, just taking advantage of it.
 
 */
 
-
 /* Function pointers to hold the value of the glibc functions */
-static int (*__real_dl_iterate_phdr)(int (*callback) (struct dl_phdr_info* info, size_t size, void* data), void* data) = NULL;
+static int (*__real_dl_iterate_phdr)(int (*callback)(struct dl_phdr_info* info, size_t size, void* data), void* data) = NULL;
 
-int dl_iterate_phdr(int (*callback) (struct dl_phdr_info* info, size_t size, void* data), void* data)
+int dl_iterate_phdr(int (*callback)(struct dl_phdr_info* info, size_t size, void* data), void* data)
 {
     if (__real_dl_iterate_phdr == NULL)
     {
@@ -70,7 +68,7 @@ int dl_iterate_phdr(int (*callback) (struct dl_phdr_info* info, size_t size, voi
 /* Function pointers to hold the value of the glibc functions */
 static void* (*__real_dlopen)(const char* file, int mode) = NULL;
 
-void *dlopen(const char *file, int mode)
+void* dlopen(const char* file, int mode)
 {
     if (__real_dlopen == NULL)
     {
@@ -122,3 +120,146 @@ int dladdr(const void* addr_arg, Dl_info* info)
 
     return result;
 }
+
+__thread int _dd_in_pthread_create = 0;
+
+// this function is called in by the profiler
+int dd_IsInPthreadCreate()
+{
+    return _dd_in_pthread_create;
+}
+
+#ifdef DD_ALPINE
+
+/* Function pointers to hold the value of the glibc functions */
+static int (*__real___pthread_create)(pthread_t* restrict res, const pthread_attr_t* restrict attrp, void* (*entry)(void*), void* restrict arg) = NULL;
+
+int __pthread_create(pthread_t* restrict res, const pthread_attr_t* restrict attrp, void* (*entry)(void*), void* restrict arg)
+{
+    if (__real___pthread_create == NULL)
+    {
+        __real___pthread_create = dlsym(RTLD_NEXT, "__pthread_create");
+    }
+
+    _dd_in_pthread_create = 1;
+    // call the real __pthread_create (libc/musl-libc)
+    int result = __real___pthread_create(res, attrp, entry, arg);
+
+    _dd_in_pthread_create = 0;
+
+    return result;
+}
+
+/* Function pointers to hold the value of the glibc functions */
+static int (*__real_pthread_attr_init)(pthread_attr_t* a) = NULL;
+
+int pthread_attr_init(pthread_attr_t* a)
+{
+    if (__real_pthread_attr_init == NULL)
+    {
+        __real_pthread_attr_init = dlsym(RTLD_NEXT, "pthread_attr_init");
+    }
+
+    sigset_t oldOne;
+    sigset_t newOne;
+
+    // initialize the set to all signals
+    sigfillset(&newOne);
+
+    // prevent any signals from interrupting the execution of the real dladdr
+    pthread_sigmask(SIG_SETMASK, &newOne, &oldOne);
+
+    // call the real pthread_attr_init (libc/musl-libc)
+    int result = __real_pthread_attr_init(a);
+
+    // restore the previous state for signals
+    pthread_sigmask(SIG_SETMASK, &oldOne, NULL);
+
+    return result;
+}
+
+/* Function pointers to hold the value of the glibc functions */
+static int (*__real_pthread_getattr_default_np)(pthread_attr_t* attrp) = NULL;
+
+int pthread_getattr_default_np(pthread_attr_t* a)
+{
+    if (__real_pthread_getattr_default_np == NULL)
+    {
+        __real_pthread_getattr_default_np = dlsym(RTLD_NEXT, "pthread_getattr_default_np");
+    }
+
+    sigset_t oldOne;
+    sigset_t newOne;
+
+    // initialize the set to all signals
+    sigfillset(&newOne);
+
+    // prevent any signals from interrupting the execution of the real dladdr
+    pthread_sigmask(SIG_SETMASK, &newOne, &oldOne);
+
+    // call the real pthread_getattr_default_np (libc/musl-libc)
+    int result = __real_pthread_getattr_default_np(a);
+
+    // restore the previous state for signals
+    pthread_sigmask(SIG_SETMASK, &oldOne, NULL);
+
+    return result;
+}
+
+/* Function pointers to hold the value of the glibc functions */
+static int (*__real_pthread_setattr_default_np)(const pthread_attr_t* attrp) = NULL;
+
+int pthread_setattr_default_np(const pthread_attr_t* a)
+{
+    if (__real_pthread_setattr_default_np == NULL)
+    {
+        __real_pthread_setattr_default_np = dlsym(RTLD_NEXT, "pthread_setattr_default_np");
+    }
+
+    sigset_t oldOne;
+    sigset_t newOne;
+
+    // initialize the set to all signals
+    sigfillset(&newOne);
+
+    // prevent any signals from interrupting the execution of the real dladdr
+    pthread_sigmask(SIG_SETMASK, &newOne, &oldOne);
+
+    // call the real pthread_setattr_default_np (libc/musl-libc)
+    int result = __real_pthread_setattr_default_np(a);
+
+    // restore the previous state for signals
+    pthread_sigmask(SIG_SETMASK, &oldOne, NULL);
+
+    return result;
+}
+
+/* Function pointers to hold the value of the glibc functions */
+static int (*__real_fork)() = NULL;
+
+pid_t fork()
+{
+    if (__real_fork == NULL)
+    {
+        __real_fork = dlsym(RTLD_NEXT, "fork");
+    }
+
+    sigset_t oldOne;
+    sigset_t newOne;
+
+    // initialize the set to all signals
+    sigfillset(&newOne);
+
+    // prevent any signals from interrupting the execution of the real dladdr
+    pthread_sigmask(SIG_SETMASK, &newOne, &oldOne);
+
+    // call the real fork (libc/musl-libc)
+    pid_t result = __real_fork();
+
+    // restore the previous state for signals
+    pthread_sigmask(SIG_SETMASK, &oldOne, NULL);
+
+    return result;
+}
+
+#endif
