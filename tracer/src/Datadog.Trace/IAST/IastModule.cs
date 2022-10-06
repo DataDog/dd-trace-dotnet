882b2d9406d659ebf8edd113b4fae8ce9eb6dc7f
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2022-10-06 17:31:41 +0200

    Update tracer/src/Datadog.Trace/IAST/IastModule.cs
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>

--- tracer/src/Datadog.Trace/IAST/IastModule.cs
+++ tracer/src/Datadog.Trace/IAST/IastModule.cs
@@ -71,7 +71,16 @@ namespace Datadog.Trace.Iast
                 return null;
             }
 
-            return StringBuilderCache.GetStringAndRelease(StringBuilderCache.Acquire(0).Append(namespaceName).Append('.').Append(typeName).Append("::").Append(methodName));
+            var length = namespaceName.Length + typeName.Length + methodName.Length + 3;
+            var sb = StringBuilderCache.Acquire(length);
+            
+            sb.Append(namespaceName)
+              .Append('.')
+              .Append(typeName)
+              .Append("::")
+              .Append(methodName);
+            
+            return StringBuilderCache.GetStringAndRelease(sb);
         }
 
         private static bool InvalidHashAlgorithm(string algorithm, Iast iast)
