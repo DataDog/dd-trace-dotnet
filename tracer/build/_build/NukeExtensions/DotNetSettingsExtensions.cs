13aac923b37700cbe0ac80b388af42ce1f5ad17c
Tony Redondo <tony.redondo@datadoghq.com>
2022-10-13 18:21:27 +0200

    Initial commit.

--- tracer/build/_build/NukeExtensions/DotNetSettingsExtensions.cs
+++ tracer/build/_build/NukeExtensions/DotNetSettingsExtensions.cs
@@ -196,4 +196,12 @@ internal static partial class DotNetSettingsExtensions
                     .Add($"--blame-hang-timeout {timeoutInMinutes}m")
         );
     }
+
+    public static DotNetTestSettings WithDatadogLogger(this DotNetTestSettings settings)
+    {
+        return settings.SetProcessArgumentConfigurator(
+            args =>
+                args.Add("--logger:{value}", "datadog")
+        );
+    }
 }
