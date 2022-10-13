b08adc4ffd11d2045fab9c6de30ff7adabe0fd13
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2022-10-13 15:08:09 +0200

    Implementation of the basic classes that will be used in IAST and the insecure hashing vulnerability detection (#3225)
    * Instrumentation of basic hashing types
    
    * Test case. Exclude by black list approach.
    
    * Instrumentate async computehash call
    
    * Rename variables.
    
    * Rename interation folder
    
    * Add some basic IAST classes
    
    * Add IAST configuration
    
    * undo instrumentationcategory
    
    * Use new IAST category
    
    * Add a IAST module.
    
    * Move files.
    
    * Serialize vulnerability JSON
    
    * Move files
    
    * Move weak hasing sample
    
    * Include filtered filename and line in the span.
    
    * Improve InsecureHashingAlgorithms configuration
    
    * Add nullable enable to classes
    
    * Avoid insecure hashing spans form DD code.
    
    * Add new _dd.iast.enabled tag
    
    * add integration tests
    
    * Improve stack trace detection. Add some integration tests snapshots.
    
    * Merge the net50/net60 differentiation in the integration tests
    
    * Add full method name if filename is not available
    
    * Use IAST java classes to build the Json and get the right format
    
    * Fix
    
    * add #nullable for null exception protection
    
    * small refactor
    
    * AddIASTSpecificInstrumentations
    
    * IAST Configuration
    
    * Name change
    
    * rename
    
    * fix
    
    * Move integration test file
    
    * Add category to hash algorithm instrumentation
    
    * fix canGenerate tests
    
    * New test for insecure hash instrumentation
    
    * Rename hashing tags to iastTags
    
    * Fix hash tests
    
    * remove not needed file
    
    * remove not needed files
    
    * lowcase property names
    
    * Send the algorithm name without the namespace.
    
    * fix tests
    
    * Serialization error fix
    
    * remove location from test
    
    * remove comment
    
    * Try to avoid log collision in parallel tests
    
    * Increase timeout for tests running in CI
    
    * Avoid process start test warnings
    
    * Renam file names
    
    * Update tracer/src/Datadog.Trace/IAST/StackWalker.cs
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
    
    * Update tracer/src/Datadog.Trace/IAST/IastModule.cs
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
    
    * Update tracer/test/test-applications/integrations/Samples.WeakHashing/Program.cs
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>
    
    * Update tracer/src/Datadog.Trace/Telemetry/TelemetryController.cs
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>
    
    * Update tracer/test/test-applications/integrations/Samples.WeakHashing/Samples.WeakHashing.csproj
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>
    
    * Improve string performance
    
    * Improve performance / readonly structs
    
    * Add custom md5 test case
    
    * Update tracer/src/Datadog.Trace/IAST/IastModule.cs
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
    
    * remove service name
    
    * disable iast if there are no hash algorithms.
    
    * make vulnerability type a string
    
    * little refactor in hashalgorithmIntegration
    
    * Exclude stringextension md5 from span generation
    
    * Add argument to make method testable
    
    * variables renaming.
    
    Co-authored-by: Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

---
+++ tracer/src/Datadog.Trace/IAST/Range.cs
@@ -0,0 +1,25 @@
+// <copyright file="Range.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+namespace Datadog.Trace.Iast
+{
+    internal readonly struct Range
+    {
+        public Range(int start, int length, Source source)
+        {
+            this.Start = start;
+            this.Length = length;
+            this.Source = source;
+        }
+
+        public int Start { get; }
+
+        public int Length { get; }
+
+        public Source Source { get; }
+    }
+}
