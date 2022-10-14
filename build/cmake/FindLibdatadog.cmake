e5f076c5b5404d71d101a8e46cc8945ecf53d203
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2022-10-14 07:09:46 +0200

    Revisit CMake projects architecture (#3329)
    * Add top level CMakeLists.txt file
    * Create interface library for coreclr source files and headers
    * Add FMT and RE2 as module
    * Add spdlog header as library
    * Add managed loader as cmake library
    * Adjust FMT and RE2 packages download step
    * Adjust output dir
    * Fix nuke steps for native: build and publish
    * Cleanup: Add missing CMakeLists.txt file to version bump
    * Ensure corprof_i.cpp is compiled once
    corprof_i.cpp is compiled multiple times by the profiler, the tracer and
    the native loader.
    With this new type of library (OBJECT), we can compile it once and use
    the object file every time.

---
+++ build/cmake/FindLibdatadog.cmake
@@ -0,0 +1,43 @@
+include(FetchContent)
+
+set(LIBDATADOG_VERSION "v0.9.0" CACHE STRING "libdatadog version")
+
+if (CMAKE_SYSTEM_PROCESSOR STREQUAL aarch64)
+    if (DEFINED ENV{IsAlpine} AND "$ENV{IsAlpine}" MATCHES "true")
+        set(SHA256_LIBDATADOG "a094bae08153b521d467435e3ef5364d3099e4b8aac1291a16c70c51ccc2f171" CACHE STRING "libdatadog sha256")
+    else()
+        set(SHA256_LIBDATADOG "9e55e8917521edf57c28bc2dad363bcc0a68570380480244f898714e31f356fb" CACHE STRING "libdatadog sha256")
+    endif()
+else()
+    if (DEFINED ENV{IsAlpine} AND "$ENV{IsAlpine}" MATCHES "true")
+        set(SHA256_LIBDATADOG "15e6d94208b94ff5f0e757310c8de372da67099e982d6ebd5cc88fdf8d9c2756" CACHE STRING "libdatadog sha256")
+    else()
+        set(SHA256_LIBDATADOG "277d638d7e1cd9c6724ba3094fab5ce0e3a87a2c26b1cf0a89a86ae2a7f1ddc8" CACHE STRING "libdatadog sha256")
+    endif()
+endif()
+
+if (DEFINED ENV{IsAlpine} AND "$ENV{IsAlpine}" MATCHES "true")
+    set(FILE_TO_DOWNLOAD libdatadog-${CMAKE_SYSTEM_PROCESSOR}-alpine-linux-musl.tar.gz)
+else()
+    set(FILE_TO_DOWNLOAD libdatadog-${CMAKE_SYSTEM_PROCESSOR}-unknown-linux-gnu.tar.gz)
+endif()
+
+FetchContent_Declare(libdatadog-${LIBDATADOG_VERSION}
+    URL https://github.com/DataDog/libdatadog/releases/download/${LIBDATADOG_VERSION}/${FILE_TO_DOWNLOAD}
+    URL_HASH SHA256=${SHA256_LIBDATADOG}
+    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/libdatadog-${LIBDATADOG_VERSION}
+)
+if(NOT libdatadog-${LIBDATADOG_VERSION}_POPULATED)
+    FetchContent_Populate(libdatadog-${LIBDATADOG_VERSION})
+endif()
+
+set(LIBDATADOG_BASE_DIR ${libdatadog-${LIBDATADOG_VERSION}_SOURCE_DIR})
+
+add_library(libdatadog-lib STATIC IMPORTED)
+
+set_target_properties(libdatadog-lib PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${LIBDATADOG_BASE_DIR}/include
+    IMPORTED_LOCATION ${LIBDATADOG_BASE_DIR}/lib/libdatadog_profiling.a
+)
+
+add_dependencies(libdatadog-lib libdatadog-${LIBDATADOG_VERSION})
