aff3bede81680afc3dfa484cec2350b262bab4c4
Tony Redondo <tony.redondo@datadoghq.com>
2022-10-14 23:30:26 +0200

    [CI Visibility] - Add Intelligent Test Runner support in MSTest testing framework (#3341)
    * [CIVisibility] Add Intelligent Test Runner support on MSTest
    
    * fix tests
    
    * Changes from review.

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/MsTestV2/ITestMethodInfo.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/MsTestV2/ITestMethodInfo.cs
@@ -1,20 +1,17 @@
-// <copyright file="ITestMethodRunner.cs" company="Datadog">
+﻿// <copyright file="ITestMethodInfo.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using Datadog.Trace.DuckTyping;
-
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Testing.MsTestV2;
 
 /// <summary>
-/// TestMethodRunner ducktype interface
+/// TestMethodInfo ducktype interface
 /// </summary>
-internal interface ITestMethodRunner
+internal interface ITestMethodInfo : ITestMethod
 {
     /// <summary>
-    /// Gets the TestMethodInfo instance
+    /// Gets the test method options
     /// </summary>
-    [DuckField(Name = "testMethodInfo")]
-    ITestMethodInfo TestMethodInfo { get; }
+    ITestMethodOptions TestMethodOptions { get; }
 }
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/MsTestV2/TestMethodRunnerExecuteTestIntegration.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="TestMethodRunnerExecuteTestIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Testing.MsTestV2;
+
+/// <summary>
+/// Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Execution.TestMethodRunner.ExecuteTest calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter",
+    TypeName = "Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Execution.TestMethodRunner",
+    MethodName = "ExecuteTest",
+    ReturnTypeName = "Microsoft.VisualStudio.TestTools.UnitTesting.TestResult[]",
+    ParameterTypeNames = new[] { "Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Execution.TestMethodInfo" },
+    MinimumVersion = "14.0.0",
+    MaximumVersion = "14.*.*",
+    IntegrationName = MsTestIntegration.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class TestMethodRunnerExecuteTestIntegration
+{
+    private static SkipTestMethodExecutor _skipTestMethodExecutor;
+
+    /// <summary>
+    /// OnMethodBegin callback
+    /// </summary>
+    /// <typeparam name="TTarget">Type of the target</typeparam>
+    /// <typeparam name="TTestMethod">Type of the ITestMethod</typeparam>
+    /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+    /// <param name="testMethod">Test method instance</param>
+    /// <returns>Calltarget state value</returns>
+    internal static CallTargetState OnMethodBegin<TTarget, TTestMethod>(TTarget instance, TTestMethod testMethod)
+        where TTarget : ITestMethodRunner
+        where TTestMethod : ITestMethod
+    {
+        if (MsTestIntegration.IsEnabled)
+        {
+            // Check if the test should be skipped by ITR
+            if (MsTestIntegration.ShouldSkip(testMethod))
+            {
+                // In order to skip a test we change the Executor to one that returns a valid outcome without calling
+                // the MethodInfo of the test
+                var executor = instance.TestMethodInfo.TestMethodOptions.Executor;
+                _skipTestMethodExecutor ??= new SkipTestMethodExecutor(executor.GetType().Assembly);
+                instance.TestMethodInfo.TestMethodOptions.Executor = DuckType.CreateReverse(executor.GetType(), _skipTestMethodExecutor);
+            }
+        }
+
+        return CallTargetState.GetDefault();
+    }
+}
