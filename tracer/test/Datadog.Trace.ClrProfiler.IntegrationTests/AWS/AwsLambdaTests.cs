79a69b7cf3d0893a0c1885131c9fe6f68b9d0e8e
Andrew Lock <andrewlock.net@gmail.com>
2022-10-19 09:38:28 +0100

    Update Samples.AWS.Lambda and tests (#3359)
    * Refactor the way serverless tests work
    
    - Don't auto-throw on every span, call a real API instead
    - Add manual spans in each handler
    - Create a `MockLambdaExtension` (based on the `MockTracerAgent`) to give easier control (and testing) of context responses (replaces the "context" containers)
    - Add additional parameter types (currently unsupported)
    - Update the snapshots to include the "implicit" spans created by the extension endpoints
    
    * Disable telemetry (to avoid sending to a mock agent that's not ready for it)
    
    * Add documentation for how AwsLambda tests work

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/AwsLambdaTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/AwsLambdaTests.cs
@@ -8,9 +8,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Datadog.Trace.ClrProfiler.IntegrationTests.Helpers;
+using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.TestHelpers;
 using FluentAssertions;
+using FluentAssertions.Execution;
+using Newtonsoft.Json;
 using VerifyXunit;
 using Xunit;
 using Xunit.Abstractions;
@@ -19,7 +21,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AWS
 {
     [UsesVerify]
     [Trait("RequiresDockerDependency", "true")]
-    public class AwsLambdaTests : TestHelper
+    public class AwsLambdaTests : TestHelper, IDisposable
     {
         public AwsLambdaTests(ITestOutputHelper output)
             : base("AWS.Lambda", output)
@@ -31,21 +33,72 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AWS
         [Trait("Category", "Lambda")]
         public async Task SubmitsTraces()
         {
+            // See documentation at docs/development/Serverless.md for examples and diagrams
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("IsAlpine")))
             {
                 Output.WriteLine("Skipping");
                 return;
             }
 
+            using var extensionWithContext = new MockLambdaExtension(shouldSendContext: true, port: 9004, Output);
+            using var extensionNoContext = new MockLambdaExtension(shouldSendContext: false, port: 9003, Output);
             using var agent = EnvironmentHelper.GetMockAgent(fixedPort: 5002);
+            agent.Output = Output;
             using (RunSampleAndWaitForExit(agent))
             {
-                var spans = agent.WaitForSpans(12, 15_000).ToArray();
+                var requests = 9 // param tests
+                                  + 3 // param with context
+                                  + 5 // base instrumentation
+                                  + 4 // other parameter types
+                                  + 3 // throwing (manual only)
+                                  + 3; // throwing with context
+
+                var expectedSpans = requests * 2; // we manually instrument each request too
+
+                var spans = agent.WaitForSpans(expectedSpans, 15_000).ToArray();
+
+                // Verify all the "with context" end invocations have a corresponding start context
+                using var s = new AssertionScope();
+                var startWithContext = extensionWithContext.StartInvocations.ToList();
+                var endWithContext = extensionWithContext.EndInvocations.ToList();
+                startWithContext.Should().OnlyContain(x => x.TraceId.HasValue);
+                endWithContext.Should().OnlyContain(x => x.TraceId.HasValue);
+                var contextPairs = extensionWithContext.EndInvocations
+                                                       .Select(x => (start: startWithContext.SingleOrDefault(y => y.TraceId == x.TraceId), end: x))
+                                                       .ToList();
+                contextPairs.Should().OnlyContain(x => x.start != null);
+                var withContextSpans = contextPairs.Select(x => ToMockSpan(x.end, x.start.Created));
+
+                // We can't match no-context start invocations with the end invocations, so just use the end ones
+                var noContextSpans = extensionNoContext.EndInvocations.Select(x => ToMockSpan(x, startTime: null));
+
+                // Create the complete traces
+                var allSpans = withContextSpans
+                              .Concat(noContextSpans)
+                              .Concat(spans)
+                              .ToList();
 
                 var settings = VerifyHelper.GetSpanVerifierSettings();
-                await VerifyHelper.VerifySpans(spans, settings)
+                await VerifyHelper.VerifySpans(allSpans, settings)
                                   .UseFileName(nameof(AwsLambdaTests));
             }
+        }
+
+        private static MockSpan ToMockSpan(MockLambdaExtension.EndExtensionRequest endInvocation, DateTimeOffset? startTime)
+        {
+            var start = startTime ?? endInvocation.Created.AddMilliseconds(100);
+            return new MockSpan
+            {
+                Duration = endInvocation.Created.Subtract(start).ToNanoseconds(),
+                Name = "lambda.invocation",
+                Service = "LambdaExtension",
+                Resource = "/lambda/end-invocation",
+                TraceId = endInvocation.TraceId ?? 0,
+                SpanId = endInvocation.SpanId ?? 0,
+                Start = start.ToUnixTimeNanoseconds(),
+                Error = endInvocation.IsError ? (byte)1 : (byte)0,
+                Tags = new Dictionary<string, string> { { "_sampling_priority_v1", endInvocation.SamplingPriority?.ToString("N1") } }
+            };
         }
     }
 }
