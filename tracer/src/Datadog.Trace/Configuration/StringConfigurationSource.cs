a68540c7f76d38c78b7c87dc22533c76ed7599a5
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-10-26 16:26:02 -0400

    allow colons in tag value

--- tracer/src/Datadog.Trace/Configuration/StringConfigurationSource.cs
+++ tracer/src/Datadog.Trace/Configuration/StringConfigurationSource.cs
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
@@ -16,6 +17,8 @@ namespace Datadog.Trace.Configuration
     /// </summary>
     public abstract class StringConfigurationSource : IConfigurationSource
     {
+        private static readonly char[] DictionarySeparatorChars = { ',' };
+
         /// <summary>
         /// Returns a <see cref="IDictionary{TKey, TValue}"/> from parsing
         /// <paramref name="data"/>.
@@ -36,8 +39,6 @@ namespace Datadog.Trace.Configuration
         /// <returns><see cref="IDictionary{TKey, TValue}"/> of key value pairs.</returns>
         public static IDictionary<string, string> ParseCustomKeyValues(string data, bool allowOptionalMappings)
         {
-            var dictionary = new ConcurrentDictionary<string, string>();
-
             // A null return value means the key was not present,
             // and CompositeConfigurationSource depends on this behavior
             // (it returns the first non-null value it finds).
@@ -46,31 +47,44 @@ namespace Datadog.Trace.Configuration
                 return null;
             }
 
+            var dictionary = new ConcurrentDictionary<string, string>();
+
             if (string.IsNullOrWhiteSpace(data))
             {
+                // return empty collection
                 return dictionary;
             }
 
-            var entries = data.Split(',');
+            var entries = data.Split(DictionarySeparatorChars, StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (var e in entries)
+            foreach (var entry in entries)
             {
-                var kv = e.Split(':');
-                if (allowOptionalMappings && kv.Length == 1)
+                // we need TrimStart() before looking for ':' so we can skip entries with no key
+                // (that is, entries with a leading ':', like "<empty or whitespace>:value")
+                var trimmedEntry = entry.TrimStart();
+
+                if (trimmedEntry.Length > 0)
                 {
-                    var key = kv[0];
-                    var value = string.Empty;
-                    dictionary[key] = value;
-                }
-                else if (kv.Length != 2)
-                {
-                    continue;
-                }
-                else
-                {
-                    var key = kv[0];
-                    var value = kv[1];
-                    dictionary[key] = value;
+                    // colonIndex == 0 is a leading colon, not valid
+                    var colonIndex = trimmedEntry.IndexOf(':');
+
+                    if (colonIndex < 0 && allowOptionalMappings)
+                    {
+                        // entries with no colon are allowed (e.g. "key1, key2:value2, key3"),
+                        // it's a key with no value.
+                        // note we already did TrimStart(), so we only need TrimEnd().
+                        var key = trimmedEntry.TrimEnd();
+                        dictionary[key] = string.Empty;
+                    }
+                    else if (colonIndex > 0)
+                    {
+                        // split at the first colon only. any other colons are part of the value.
+                        // if a colon is present with no value, we take the value to be empty (e.g. "key1:, key2: ").
+                        // note we already did TrimStart() on the key, so it only needs TrimEnd().
+                        var key = trimmedEntry.Substring(0, colonIndex).TrimEnd();
+                        var value = trimmedEntry.Substring(colonIndex + 1).Trim();
+                        dictionary[key] = value;
+                    }
                 }
             }
 
