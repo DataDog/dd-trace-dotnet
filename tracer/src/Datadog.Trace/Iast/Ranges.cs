42b0537110c3d8be47c5e45ed7b686a848705a07
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-11-03 14:30:58 +0100

    First Version

---
+++ tracer/src/Datadog.Trace/Iast/Ranges.cs
@@ -0,0 +1,25 @@
+// <copyright file="Ranges.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Iast;
+internal class Ranges
+{
+    // public static Range[] EMPTY = new Range[0];
+
+    public static Range[] ForString(string obj, Source source)
+    {
+        return new Range[] { new Range(0, obj.Length, source) };
+    }
+
+    /*
+    public static void copyShift(Range[] src, Range[] dst, final dstPos, int shift)
+    {
+        for (int iSrc = 0, iDst = dstPos; iSrc < src.Length; iSrc++, iDst++)
+        {
+            dst[iDst] = src[iSrc].Shift(shift);
+        }
+    }
+    */
+}
