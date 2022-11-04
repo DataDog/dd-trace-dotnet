fcf77d66ec5e624c2b8d656f5d04ee4af7abc87d
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-11-04 16:19:40 +0100

    First implementation

---
+++ tracer/src/Datadog.Trace/Iast/IWeakAware.cs
@@ -0,0 +1,15 @@
+// <copyright file="IWeakAware.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+
+namespace Datadog.Trace.Iast;
+
+internal interface IWeakAware
+{
+    WeakReference Weak { get; }
+
+    bool IsAlive { get; }
+}
