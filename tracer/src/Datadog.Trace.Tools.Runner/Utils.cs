f8661016edd936efff97a8b9b59e18b72ba0543f
Tony Redondo <tony.redondo@datadoghq.com>
2022-11-10 20:20:54 +0100

    [CI Visibility] - Add a new helper method to the discovery service (#3441)
    * Add a new helper method to the discovery service.
    
    * changes
    
    * await for DiscoveryService.DisposeAsync()
    
    * Remove one .ctor

--- tracer/src/Datadog.Trace.Tools.Runner/Utils.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/Utils.cs
@@ -253,25 +253,16 @@ namespace Datadog.Trace.Tools.Runner
 
             var tracerSettings = new TracerSettings(configurationSource);
             var settings = tracerSettings.Build();
-            var discoveryService = new DiscoveryService(
-                AgentTransportStrategy.Get(
-                    settings.Exporter,
-                    productName: "discovery",
-                    tcpTimeout: TimeSpan.FromSeconds(5),
-                    AgentHttpHeaderNames.MinimalHeaders,
-                    () => new MinimalAgentHeaderHelper(),
-                    uri => uri),
-                10,
-                1000,
-                int.MaxValue);
+
+            var discoveryService = DiscoveryService.Create(
+                settings.Exporter,
+                tcpTimeout: TimeSpan.FromSeconds(5),
+                initialRetryDelayMs: 10,
+                maxRetryDelayMs: 1000,
+                recheckIntervalMs: int.MaxValue);
 
             var tcs = new TaskCompletionSource<AgentConfiguration>(TaskCreationOptions.RunContinuationsAsynchronously);
-            discoveryService.SubscribeToChanges(
-                aCfg =>
-                {
-                    tcs.TrySetResult(aCfg);
-                    discoveryService.DisposeAsync();
-                });
+            discoveryService.SubscribeToChanges(aCfg => tcs.TrySetResult(aCfg));
 
             var cts = new CancellationTokenSource();
             cts.CancelAfter(5000);
@@ -280,10 +271,11 @@ namespace Datadog.Trace.Tools.Runner
                        {
                            WriteError($"Error connecting to the Datadog Agent at {tracerSettings.Exporter.AgentUri}.");
                            tcs.TrySetResult(null);
-                           discoveryService.DisposeAsync();
                        }))
             {
-                return await tcs.Task.ConfigureAwait(false);
+                var configuration = await tcs.Task.ConfigureAwait(false);
+                await discoveryService.DisposeAsync().ConfigureAwait(false);
+                return configuration;
             }
         }
 
