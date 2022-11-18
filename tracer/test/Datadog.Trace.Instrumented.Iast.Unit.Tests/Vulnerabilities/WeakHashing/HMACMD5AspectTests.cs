bda8e660687a7d0abda4d110935a0a33dfaf572d
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-11-18 10:24:05 +0100

    More hash tests

---
+++ tracer/test/Datadog.Trace.Instrumented.Iast.Unit.Tests/Vulnerabilities/WeakHashing/HMACMD5AspectTests.cs
@@ -0,0 +1,53 @@
+// <copyright file="HMACMD5AspectTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using Moq;
+using Xunit;
+
+namespace Datadog.Trace.Instrumented.Iast.Unit.Tests.Vulnerabilities.WeakHashing;
+public class HMACMD5AspectTests : InstrumentationTestsBase
+{
+    [Fact]
+    public void GivenAHMACMD5_WhenComputeHash_VulnerabilityIsLogged()
+    {
+        new HMACMD5().ComputeHash(new Mock<Stream>().Object);
+        AssertVulnerable();
+    }
+
+    [Fact]
+    public void GivenAHMACMD5_WhenComputeHash_VulnerabilityIsLogged2()
+    {
+        new HMACMD5().ComputeHash(new byte[] { });
+        AssertVulnerable();
+    }
+
+    [Fact]
+    public void GivenAHMACMD5_WhenComputeHash_ExceptionIsThrown()
+    {
+        Assert.Throws<ArgumentException>(() => new HMACMD5().ComputeHash(new byte[] { }, 10, 10));
+    }
+
+    [Fact]
+    public void GivenAHMACMD5_WhenComputeHashNullStream_ExceptionIsThrown2()
+    {
+        Assert.Throws<NullReferenceException>(() => new HMACMD5().ComputeHash(((Stream)null)));
+    }
+
+    [Fact]
+    public void GivenAHMACMD5_WhenComputeHash_VulnerabilityIsLogged3()
+    {
+        new HMACMD5().ComputeHash(new byte[] { 5, 5, 5 }, 0, 2);
+        AssertVulnerable();
+    }
+
+    [Fact]
+    public void GivenANewHMACMD5_WhenComputeHash_ExceptionIsThrown()
+    {
+        Assert.Throws<ArgumentException>(() => new HMACMD5().ComputeHash(new byte[] { 5, 5, 5 }, 10, 2));
+    }
+}
--- tracer/test/Datadog.Trace.Instrumented.Iast.Unit.Tests/Vulnerabilities/WeakHashing/MD5Tests.cs
+++ tracer/test/Datadog.Trace.Instrumented.Iast.Unit.Tests/Vulnerabilities/WeakHashing/MD5Tests.cs
@@ -4,20 +4,14 @@
 // </copyright>
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using Datadog.Trace.Configuration;
-using Datadog.Trace.Iast;
-using Datadog.Trace.Iast.Settings;
-using FluentAssertions.Equivalency.Tracing;
 using Moq;
 using Xunit;
 
 namespace Datadog.Trace.Instrumented.Iast.Unit.Tests.Vulnerabilities.WeakHashing;
 
-public class MD5Tests
+public class MD5Tests : InstrumentationTestsBase
 {
     [Fact]
     public void GivenAMD5_WhenComputeHash_VulnerabilityIsLogged()
@@ -25,30 +19,5 @@ public class MD5Tests
         var rrr = Environment.GetEnvironmentVariable("RRR");
         MD5.Create().ComputeHash(new Mock<Stream>().Object);
         AssertVulnerable();
-    }
-
-    private void AssertVulnerable(int vulnerabilities = 1)
-    {
-        var spans = GetGeneratedSpans((Tracer.Instance.ActiveScope.Span as Span).Context.TraceContext);
-        Assert.Equal(vulnerabilities, GetIastSpansCount(spans));
-    }
-
-    private int GetIastSpansCount(List<Span> spans)
-    {
-        return spans.Where(x => x.GetTag(Tags.IastEnabled) != null).Count();
-    }
-
-    private List<Span> GetGeneratedSpans(TraceContext context)
-    {
-        var spans = new List<Span>();
-
-        var contextSpans = context.Spans.GetArray();
-
-        foreach (var span in contextSpans)
-        {
-            spans.Add(span);
-        }
-
-        return spans;
     }
 }
