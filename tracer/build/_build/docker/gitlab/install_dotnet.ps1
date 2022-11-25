a30379e518b44899ebc4a854deff59a6d431d070
Andrew Lock <andrew.lock@datadoghq.com>
2022-11-25 11:45:49 +0000

    Create custom image for building in Gitlab
    Previously, we were using the Windows agent runners, but due to differing dependencies, that's no longer possible.
    
    Instead, this defines a custom Docker image which we build externally and push to dockerhub.
    
    Only installs the minimal required dependencies. Many of the scripts are taken from the agent build repo (and tweaked)

---
+++ tracer/build/_build/docker/gitlab/install_dotnet.ps1
@@ -0,0 +1,31 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$Version,
+    [Parameter(Mandatory=$true)][string]$Sha512,
+    [Parameter(Mandatory=$true)][string]$Url
+)
+
+# Enabled TLS12
+$ErrorActionPreference = 'Stop'
+
+# Script directory is $PSScriptRoot
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$out = "$($PSScriptRoot)\dotnetcoresdk.exe"
+
+Write-Host -ForegroundColor Green Downloading $Url to $out
+(New-Object System.Net.WebClient).DownloadFile($Url, $out)
+if ((Get-FileHash -Algorithm SHA512 $out).Hash -ne "$Sha512") { Write-Host \"Wrong hashsum for ${out}: got '$((Get-FileHash -Algorithm SHA512 $out).Hash)', expected '$Sha512'.\"; exit 1 }
+
+# Skip extraction of XML docs - generally not useful within an image/container - helps performance
+setx NUGET_XMLDOC_MODE skip
+Write-Host -ForegroundColor Green "Installing dotnetcore"
+
+start-process -FilePath $out -ArgumentList "/install /quiet /norestart" -wait
+
+Remove-Item $out
+
+# Trigger first run experience by running arbitrary cmd
+dotnet help
+
+Write-Host -ForegroundColor Green Done with DotNet Core SDK $Version
