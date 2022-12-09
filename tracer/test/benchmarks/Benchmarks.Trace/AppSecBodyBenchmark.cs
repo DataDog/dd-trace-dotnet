d513409cc21d28f99b23d959c051b738c64154b6
Anna <anna.yafi@datadoghq.com>
2022-12-09 13:57:00 +0100

    fix variables

--- tracer/test/benchmarks/Benchmarks.Trace/AppSecBodyBenchmark.cs
+++ tracer/test/benchmarks/Benchmarks.Trace/AppSecBodyBenchmark.cs
@@ -75,13 +75,16 @@ namespace Benchmarks.Trace
         static AppSecBodyBenchmark()
         {
             var dir = Directory.GetCurrentDirectory();
-            while (!dir.EndsWith("tracer"))
+            while (!Directory.GetDirectories(dir).Any(s=>s.Contains("shared")))
             {
                 dir = Directory.GetParent(dir).FullName;
             }
+           
+            dir = Directory.GetDirectories(dir).First(s=>s.Contains("shared"));
             Environment.SetEnvironmentVariable("DD_APPSEC_ENABLED", "true");
-            Environment.SetEnvironmentVariable("DD_DOTNET_TRACER_HOME", Path.Combine(dir, "bin", "dd-tracer-home", RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows) ? $"win-{(Environment.Is64BitOperatingSystem ? "x64" : "x86")}" : string.Empty));
-            security = Security.Instance;
+            var path = Path.Combine(dir, "bin", "monitoring-home", RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"win-{(Environment.Is64BitOperatingSystem ? "x64" : "x86")}" : string.Empty);
+            Environment.SetEnvironmentVariable("DD_DOTNET_TRACER_HOME", path);
+                       security = Security.Instance;
 #if NETFRAMEWORK
             var ms = new MemoryStream();
             using var sw = new StreamWriter(ms);
