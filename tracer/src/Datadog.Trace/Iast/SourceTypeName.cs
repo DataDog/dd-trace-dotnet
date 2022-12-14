eb1bccb8f818f9fb9cb819c29c2c42073b5217ae
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2022-12-14 11:51:49 +0100

    [ASM] Taint request parameters (#3513)
    * basic class
    
    * First Version
    
    * First implementation
    
    * Tests.
    
    * start second map to compare
    
    * compare times
    
    * remove map<weak,tainted> option
    
    * Update tests
    
    * minos fixes
    
    * Add tests
    
    * refactor map
    
    * update entries count only in flat mode
    
    * basic tainting
    
    * fix
    
    * Add route parameters for netcore
    
    * add asp core parameters to tainted
    
    * aspnetmvc5 controller
    
    * iast classes for tainted parameters
    
    * unit tests for IastRequestContext
    
    * Add parameters capture point
    
    * Improve parameter processing
    
    * rebase from main fixes
    
    * cosmetic changes
    
    * more cosmetic changes
    
    * Cosmetic fixes
    
    * Remove comment
    
    * fix
    
    * Fix Iast integration tests
    
    * Parameter name/value set correctly
    
    * refactor source type.
    
    * Fix compilation error.
    
    * Add nullable enable and do not return in asp observer if iast enabled

--- tracer/src/Datadog.Trace/Iast/SourceTypeName.cs
+++ tracer/src/Datadog.Trace/Iast/SourceTypeName.cs
@@ -1,17 +1,17 @@
-// <copyright file = "ITaintedMap.cs" company = "Datadog" >
+// <copyright file="SourceTypeName.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System.Collections.Generic;
-
 namespace Datadog.Trace.Iast;
 
-internal interface ITaintedMap
+internal enum SourceTypeName
 {
-    public void Put(ITaintedObject tainted);
-
-    public ITaintedObject Get(object obj);
-
-    public List<ITaintedObject> GetListValues();
+    RequestBody,
+    RequestPath,
+    RequestParameterName,
+    RequestParameterValue,
+    RoutedParameterValue,
+    RequestHeader,
+    RequestQueryString
 }
--- tracer/test/test-applications/security/aspnet/Samples.Security.AspNetMvc5/Controllers/IastController.cs
+++ tracer/test/test-applications/security/aspnet/Samples.Security.AspNetMvc5/Controllers/IastController.cs
@@ -1,21 +1,18 @@
 using System.Security.Cryptography;
-using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
 
 namespace Samples.Security.AspNetCore5.Controllers
 {
     [Route("[controller]")]
-    [ApiController]
-    public class IastController : ControllerBase
+    public class IastController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return Content("Ok\n");
         }
 
-
-        [HttpGet("WeakHashing")]
         [Route("WeakHashing/{delay1}")]
-        public IActionResult WeakHashing(int delay1 = 0, int delay2 = 0)
+        public ActionResult WeakHashing(int delay1 = 0, int delay2 = 0)
         {
             System.Threading.Thread.Sleep(delay1 + delay2);
 #pragma warning disable SYSLIB0021 // Type or member is obsolete
