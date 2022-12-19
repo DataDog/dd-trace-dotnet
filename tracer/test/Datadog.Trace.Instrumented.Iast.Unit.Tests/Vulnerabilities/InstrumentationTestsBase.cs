e160dad0ce35c29d2effc6c9066ee4d3d7a92a0a
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-12-19 15:20:26 +0100

    minor changes for net461

--- tracer/test/Datadog.Trace.Instrumented.Iast.Unit.Tests/Vulnerabilities/InstrumentationTestsBase.cs
+++ tracer/test/Datadog.Trace.Instrumented.Iast.Unit.Tests/Vulnerabilities/InstrumentationTestsBase.cs
@@ -6,23 +6,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using FluentAssertions;
 using Xunit;
 
 namespace Datadog.Trace.Instrumented.Iast.Unit.Tests.Vulnerabilities;
 
 public class InstrumentationTestsBase
 {
+    public InstrumentationTestsBase()
+    {
+        AssertInstrumented();
+    }
+
+    protected void AssertInstrumented()
+    {
+        Tracer.Instance.ActiveScope.Should().NotBeNull("Test is not instrumented");
+    }
+
     protected void AssertSpanGenerated(string operationName, int spansGenerated = 1)
     {
         var spans = GetGeneratedSpans((Tracer.Instance.ActiveScope.Span as Span).Context.TraceContext);
         spans = spans.Where(x => x.OperationName == operationName).ToList();
-        Assert.Equal(spansGenerated, spans.Count);
+        spansGenerated.Should().Be(spans.Count);
     }
 
     protected void AssertVulnerable(int vulnerabilities = 1)
     {
+#if !NETFRAMEWORK
         var spans = GetGeneratedSpans((Tracer.Instance.ActiveScope.Span as Span).Context.TraceContext);
+        vulnerabilities.Should().Be(GetIastSpansCount(spans));
+#else
+        var i = Tracer.Instance;
+        var s = i.ActiveScope.Span as Span;
+        var span = Tracer.Instance.ActiveScope.Span.Context;
+        var property = span.GetType().GetProperty("TraceContext", BindingFlags.NonPublic | BindingFlags.Instance);
+        var context1 = property.GetValue(span);
+        var context = ((Span)span).Context;
+        var spans = GetGeneratedSpans(context.TraceContext);
         Assert.Equal(vulnerabilities, GetIastSpansCount(spans));
+#endif
     }
 
     protected void AssertNotVulnerable()
