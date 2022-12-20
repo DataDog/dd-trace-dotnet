16ebdc67a4c59d9203f216b7518fa25e0ebb549f
Andrew Lock <andrew.lock@datadoghq.com>
2022-12-20 12:56:08 +0000

    Don't immediately delete the output files after importing
    Delete them _before_ we do a run instead

--- tracer/build/crank/run.sh
+++ tracer/build/crank/run.sh
@@ -29,55 +29,58 @@ commit="--application.source.branchOrCommit #$commit_sha"
 if [ "$1" = "windows" ]; then
     echo "Running windows throughput tests"
 
+    rm -f baseline_windows.json
+    rm -f calltarget_ngen_windows.json
+    rm -f trace_stats_windows.json
+
     crank --config Samples.AspNetCoreSimpleController.yml --scenario baseline --profile windows --json baseline_windows.json $repository $commit --property name=AspNetCoreSimpleController --property scenario=baseline --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="baseline_windows.json"
-    rm baseline_windows.json
 
     crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget_ngen --profile windows --json calltarget_ngen_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget_ngen --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="calltarget_ngen_windows.json"
-    rm calltarget_ngen_windows.json
 
     if [ "$2" = "True" ]; then
       echo "Running throughput tests with stats enabled"
       crank --config Samples.AspNetCoreSimpleController.yml --scenario trace_stats --profile windows --json trace_stats_windows.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=trace_stats --property profile=windows --property arch=x64 --variable commit_hash=$commit_sha
       dd-trace --crank-import="trace_stats_windows.json"
-      rm trace_stats_windows.json
     fi
 
 elif [ "$1" = "linux" ]; then
     echo "Running Linux  x64 throughput tests"
 
+    rm -f baseline_linux.json
+    rm -f calltarget_ngen_linux.json
+    rm -f trace_stats_linux.json
+
     crank --config Samples.AspNetCoreSimpleController.yml --scenario baseline --profile linux --json baseline_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=baseline --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="baseline_linux.json"
-    rm baseline_linux.json
 
     crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget_ngen --profile linux --json calltarget_ngen_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget_ngen --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="calltarget_ngen_linux.json"
-    rm calltarget_ngen_linux.json
 
     if [ "$2" = "True" ]; then
       echo "Running throughput tests with stats enabled"
       crank --config Samples.AspNetCoreSimpleController.yml --scenario trace_stats --profile linux --json trace_stats_linux.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=trace_stats --property profile=linux --property arch=x64 --variable commit_hash=$commit_sha
       dd-trace --crank-import="trace_stats_linux.json"
-      rm trace_stats_linux.json
     fi
 
 elif [ "$1" = "linux_arm64" ]; then
     echo "Running Linux arm64 throughput tests"
 
+    rm -f baseline_linux_arm64.json
+    rm -f calltarget_ngen_linux_arm64.json
+    rm -f trace_stats_linux_arm64.json
+
     crank --config Samples.AspNetCoreSimpleController.yml --scenario baseline --profile linux_arm64 --json baseline_linux_arm64.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=baseline --property profile=linux_arm64 --property arch=arm64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="baseline_linux_arm64.json"
-    rm baseline_linux_arm64.json
 
     crank --config Samples.AspNetCoreSimpleController.yml --scenario calltarget_ngen --profile linux_arm64 --json calltarget_ngen_linux_arm64.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=calltarget_ngen --property profile=linux_arm64 --property arch=arm64 --variable commit_hash=$commit_sha
     dd-trace --crank-import="calltarget_ngen_linux_arm64.json"
-    rm calltarget_ngen_linux_arm64.json
 
     if [ "$2" = "True" ]; then
       echo "Running throughput tests with stats enabled"
       crank --config Samples.AspNetCoreSimpleController.yml --scenario trace_stats --profile linux_arm64 --json trace_stats_linux_arm64.json $repository $commit  --property name=AspNetCoreSimpleController --property scenario=trace_stats --property profile=linux_arm64 --property arch=arm64 --variable commit_hash=$commit_sha
       dd-trace --crank-import="trace_stats_linux_arm64.json"
-      rm trace_stats_linux_arm64.json
     fi
 
 else
