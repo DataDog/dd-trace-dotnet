cbba69c1ce8f8a6c1edb32359baf2505c5cbf6b7
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2022-12-22 18:02:50 +0100

    Add exception information when controller request fails.

--- tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/IastController.cs
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/IastController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Data.SQLite;
 using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Samples.Security.AspNetCore5.Controllers
@@ -32,25 +34,58 @@ namespace Samples.Security.AspNetCore5.Controllers
         [Route("SqlQuery")]
         public IActionResult SqlQuery(string username, string query)
         {
-            if (dbConnection is null)
+            try
             {
-                dbConnection = CreateDatabase();
-            }
+                if (dbConnection is null)
+                {
+                    dbConnection = CreateDatabase();
+                }
 
-            if (!string.IsNullOrEmpty(username))
-            {
-                var taintedQuery = "SELECT Surname from Persons where name = '" + username + "'";
-                var rname = new SQLiteCommand(taintedQuery, dbConnection).ExecuteScalar();
-                return Content($"Result: " + rname);
-            }
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var taintedQuery = "SELECT Surname from Persons where name = '" + username + "'";
+                    var rname = new SQLiteCommand(taintedQuery, dbConnection).ExecuteScalar();
+                    return Content($"Result: " + rname);
+                }
 
-            if (!string.IsNullOrEmpty(query))
+                if (!string.IsNullOrEmpty(query))
+                {
+                    var rname = new SQLiteCommand(query, dbConnection).ExecuteScalar();
+                    return Content($"Result: " + rname);
+                }
+            }
+            catch (Exception ex)
             {
-                var rname = new SQLiteCommand(query, dbConnection).ExecuteScalar();
-                return Content($"Result: " + rname);
+                return Content(ToFormattedString(ex));
             }
 
             return Content($"No query or username was provided");
+        }
+
+        public static string ToFormattedString(Exception ex)
+        {
+            var message = new StringBuilder();
+            if (ex != null)
+            {
+                message.AppendFormat("Name:       {0}", ex.GetType().FullName);
+                message.Append(Environment.NewLine);
+                message.AppendFormat("Message:    {0}", GetFullMessage(ex));
+                message.Append(Environment.NewLine);
+                message.AppendFormat("StackTrace: {0}", ex.StackTrace);
+                message.Append(Environment.NewLine);
+            }
+            return message.ToString();
+        }
+
+        public static string GetFullMessage(Exception err)
+        {
+            string res = "";
+            while (err != null)
+            {
+                res += err.Message + " ";
+                err = err.InnerException;
+            }
+            return res;
         }
 
         public static SQLiteConnection CreateDatabase()
