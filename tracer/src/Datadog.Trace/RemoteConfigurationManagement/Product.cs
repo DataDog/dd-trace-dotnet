83c45c5ac730b86d7b6886f8308980f433b35053
Omer Raviv <omer.raviv@datadoghq.com>
2022-12-29 13:59:16 +0200

    Change verbosity level to avoid CI failures

--- tracer/src/Datadog.Trace/RemoteConfigurationManagement/Product.cs
+++ tracer/src/Datadog.Trace/RemoteConfigurationManagement/Product.cs
@@ -56,7 +56,7 @@ namespace Datadog.Trace.RemoteConfigurationManagement
                 {
                     foreach (var item in filteredConfigs)
                     {
-                        Log.Error($"Failed to apply Remote Configuration record {item.Name} for product {Name}", ex);
+                        Log.Debug($"Failed to apply Remote Configuration record {item.Name} for product {Name}", ex);
                         e.Error(item.Name, ex.Message);
                     }
                 }
