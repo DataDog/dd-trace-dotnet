433f6422401d8063d5a8d1aaff2e762063559ce7
Matan Green <matangrn@gmail.com>
2023-01-03 16:03:59 +0200

    revived TracerAnnotation-related types for backward compatibility with older managed tracer versions

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/TraceAnnotations/TraceAnnotationsIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/TraceAnnotations/TraceAnnotationsIntegration.cs
@@ -24,6 +24,30 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.TraceAnnotations
     public class TraceAnnotationsIntegration
     {
         internal static readonly IntegrationId IntegrationId = IntegrationId.TraceAnnotations;
+        private static readonly ConcurrentDictionary<RuntimeHandleTuple, TraceAnnotationInfo> InstrumentedMethodCache = new ConcurrentDictionary<RuntimeHandleTuple, TraceAnnotationInfo>();
+
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="methodHandle">The RuntimeMethodHandle representing the instrumented method</param>
+        /// <param name="typeHandle">The RuntimeTypeHandle representing the instrumented method's owning type</param>
+        /// <returns>Calltarget state value</returns>
+        internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, RuntimeMethodHandle methodHandle, RuntimeTypeHandle typeHandle)
+        {
+            var info = InstrumentedMethodCache.GetOrAdd(
+                new RuntimeHandleTuple(methodHandle, typeHandle),
+                key => TraceAnnotationInfoFactory.Create(MethodBase.GetMethodFromHandle(key.MethodHandle, key.TypeHandle)));
+
+            var tags = new TraceAnnotationTags();
+            var scope = Tracer.Instance.StartActiveInternal(info.OperationName, tags: tags);
+            scope.Span.ResourceName = info.ResourceName;
+
+            Tracer.Instance.TracerManager.Telemetry.IntegrationGeneratedSpan(IntegrationId);
+
+            return new CallTargetState(scope);
+        }
 
         /// <summary>
         /// OnMethodBegin callback
