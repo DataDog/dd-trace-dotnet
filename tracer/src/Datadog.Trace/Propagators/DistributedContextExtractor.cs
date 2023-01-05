60842be556222ddfae112700675d1e309458919a
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2023-01-05 16:38:57 -0500

    fix SpanContex and DistributedContextExtractor

--- tracer/src/Datadog.Trace/Propagators/DistributedContextExtractor.cs
+++ tracer/src/Datadog.Trace/Propagators/DistributedContextExtractor.cs
@@ -24,6 +24,7 @@ namespace Datadog.Trace.Propagators
             }
 
             var traceId = ParseUtility.ParseUInt64(carrier, carrierGetter, SpanContext.Keys.TraceId);
+
             if (traceId is null or 0)
             {
                 // a valid traceId is required to use distributed tracing
@@ -36,10 +37,12 @@ namespace Datadog.Trace.Propagators
             var rawTraceId = ParseUtility.ParseString(carrier, carrierGetter, SpanContext.Keys.RawTraceId);
             var rawSpanId = ParseUtility.ParseString(carrier, carrierGetter, SpanContext.Keys.RawSpanId);
             var propagatedTraceTags = ParseUtility.ParseString(carrier, carrierGetter, SpanContext.Keys.PropagatedTags);
+            var w3CTraceState = ParseUtility.ParseString(carrier, carrierGetter, SpanContext.Keys.AdditionalW3CTraceState);
 
             spanContext = new SpanContext(traceId, parentId, samplingPriority, serviceName: null, origin, rawTraceId, rawSpanId)
                           {
-                              PropagatedTags = propagatedTraceTags
+                              PropagatedTags = propagatedTraceTags,
+                              AdditionalW3CTraceState = w3CTraceState,
                           };
 
             return true;
