cd0d80d271620c825d7916a4b2e84fb694d550af
Tony Redondo <tony.redondo@datadoghq.com>
2023-01-10 15:35:28 +0100

    cleanup

--- tracer/src/Datadog.Trace/Ci/Tags/TestTags.cs
+++ tracer/src/Datadog.Trace/Ci/Tags/TestTags.cs
@@ -2,7 +2,9 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+#nullable enable
 
+// ReSharper disable InconsistentNaming
 namespace Datadog.Trace.Ci.Tags
 {
     /// <summary>
