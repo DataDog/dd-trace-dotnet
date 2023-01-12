a935cd5516f9fa6059424a96379bc80123cd68c6
Andrew Lock <andrew.lock@datadoghq.com>
2023-01-12 19:55:34 +0000

    Fix execution time (startup time) benchmarks and save artifacts (#3654)
    * Add json export and upload of execution benchmarks
    
    * Update references from net461 -> net462

--- tracer/build/timeit/Samples.FakeDbCommand/run.cmd
+++ tracer/build/timeit/Samples.FakeDbCommand/run.cmd
@@ -1,8 +1,12 @@
 @echo off
+IF EXIST results_Samples.FakeDbCommand.windows.net462.json DEL /F results_Samples.FakeDbCommand.windows.net462.json
+IF EXIST results_Samples.FakeDbCommand.windows.netcoreapp31.json DEL /F results_Samples.FakeDbCommand.windows.netcoreapp31.json
+IF EXIST results_Samples.FakeDbCommand.windows.net60.json DEL /F results_Samples.FakeDbCommand.windows.net60.json
+
 echo *********************
 echo .NET Framework 4.6.1
 echo *********************
-%GOPATH%\\bin\\timeit.exe Samples.FakeDbCommand.windows.net461.json
+%GOPATH%\\bin\\timeit.exe Samples.FakeDbCommand.windows.net462.json
 
 echo *********************
 echo .NET Core 3.1
