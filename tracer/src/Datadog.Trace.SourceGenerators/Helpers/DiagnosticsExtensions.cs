5b45ec2f96e9cf1da41a1abc167bc274d7173ea7
Andrew Lock <andrew.lock@datadoghq.com>
2023-01-13 17:54:25 +0000

    Add helpers for ensuring proper equality checking

---
+++ tracer/src/Datadog.Trace.SourceGenerators/Helpers/DiagnosticsExtensions.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="DiagnosticsExtensions.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Microsoft.CodeAnalysis;
+
+namespace Datadog.Trace.SourceGenerators.Helpers;
+
+/// <summary>
+/// Extension methods for <see cref="GeneratorExecutionContext"/>, specifically for reporting diagnostics.
+/// </summary>
+internal static class DiagnosticsExtensions
+{
+    /// <summary>
+    /// Registers an output node into an <see cref="IncrementalGeneratorInitializationContext"/> to output diagnostics.
+    /// </summary>
+    /// <param name="context">The input <see cref="IncrementalGeneratorInitializationContext"/> instance.</param>
+    /// <param name="diagnostics">The input <see cref="IncrementalValuesProvider{TValues}"/> sequence of diagnostics.</param>
+    public static void ReportDiagnostics(
+        this IncrementalGeneratorInitializationContext context,
+        IncrementalValuesProvider<DiagnosticInfo> diagnostics)
+    {
+        context.RegisterSourceOutput(
+            diagnostics,
+            static (context, info) =>
+            {
+                context.ReportDiagnostic(Diagnostic.Create(info.Descriptor, info.Location));
+            });
+    }
+}
---
+++ tracer/src/Datadog.Trace.SourceGenerators/Helpers/Result.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Result.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+
+namespace Datadog.Trace.SourceGenerators.Helpers;
+
+internal sealed record Result<TValue>
+    where TValue : IEquatable<TValue>?
+{
+    public Result(TValue value, EquatableArray<DiagnosticInfo> errors)
+    {
+        Value = value;
+        Errors = errors;
+    }
+
+    public TValue Value { get; }
+
+    public EquatableArray<DiagnosticInfo> Errors { get; }
+}
