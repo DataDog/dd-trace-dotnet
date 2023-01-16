b6c2682afb128aa557b4fd77ee13e0d6152ec5c0
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2023-01-16 10:39:50 +0100

    Update tracer/src/Datadog.Trace/IAST/IastModule.cs
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/src/Datadog.Trace/IAST/IastModule.cs
+++ tracer/src/Datadog.Trace/IAST/IastModule.cs
@@ -68,9 +68,13 @@ internal class IastModule
         }
 
         TaintedObject? tainted = null;
-        if (taintedFromEvidenceRequired && ((tainted = traceContext?.IastRequestContext?.GetTainted(evidenceValue)) == null))
+        if (taintedFromEvidenceRequired)
         {
-            return null;
+            tainted = traceContext?.IastRequestContext?.GetTainted(evidenceValue);
+            if (tainted is null)
+            {
+                return null;
+            }
         }
 
         var isRequest = traceContext?.RootSpan?.Type == SpanTypes.Web;
