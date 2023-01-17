a7b6455f4dc79488cdee98314a1056b2c51e5799
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-01-17 17:03:45 +0100

    add MonitoringHomeDirectory

--- tracer/test/test-applications/integrations/Samples.Deduplication/Program.cs
+++ tracer/test/test-applications/integrations/Samples.Deduplication/Program.cs
@@ -19,8 +19,8 @@ internal static class Program
     private static string GetErrorMessage(Assembly[] assemblies)
     {
         var assemblyListString = string.Join(Environment.NewLine, assemblies.Where(x => x.GetName().Name.IndexOf("datadog", StringComparison.OrdinalIgnoreCase) >= 0).Select(x => x.GetName().Name));
-
-        return $"Info: {Environment.NewLine}{EnvironmentVariableMessage("CORECLR_ENABLE_PROFILING")}{EnvironmentVariableMessage("CORECLR_PROFILER")}{EnvironmentVariableMessage("CORECLR_PROFILER_PATH")}{EnvironmentVariableMessage("CORECLR_PROFILER_PATH_64")}{EnvironmentVariableMessage("CORECLR_PROFILER_PATH_32")}{EnvironmentVariableMessage("COR_ENABLE_PROFILING")}{EnvironmentVariableMessage("COR_PROFILER_PATH_32")}{EnvironmentVariableMessage("COR_PROFILER_PATH_64")}{EnvironmentVariableMessage("DD_DOTNET_TRACER_HOME")}{(string.IsNullOrEmpty(assemblyListString) ? string.Empty : (Environment.NewLine + assemblyListString))}";
+        
+        return $"Info: {Environment.NewLine}{EnvironmentVariableMessage("MonitoringHomeDirectory")}{EnvironmentVariableMessage("CORECLR_ENABLE_PROFILING")}{EnvironmentVariableMessage("CORECLR_PROFILER")}{EnvironmentVariableMessage("CORECLR_PROFILER_PATH")}{EnvironmentVariableMessage("CORECLR_PROFILER_PATH_64")}{EnvironmentVariableMessage("CORECLR_PROFILER_PATH_32")}{EnvironmentVariableMessage("COR_ENABLE_PROFILING")}{EnvironmentVariableMessage("COR_PROFILER_PATH_32")}{EnvironmentVariableMessage("COR_PROFILER_PATH_64")}{EnvironmentVariableMessage("DD_DOTNET_TRACER_HOME")}{(string.IsNullOrEmpty(assemblyListString) ? string.Empty : (Environment.NewLine + assemblyListString))}";
     }
 
     private static string EnvironmentVariableMessage(string variable)
