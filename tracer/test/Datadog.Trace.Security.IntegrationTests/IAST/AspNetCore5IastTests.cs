d3b1ac2da8a7440b819b6128f59f47e59e5d1bce
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-01-17 09:44:00 +0100

    Fix test

--- tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetCore5IastTests.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetCore5IastTests.cs
@@ -150,6 +150,7 @@ namespace Datadog.Trace.Security.IntegrationTests.Iast
         }
 
         [SkippableFact]
+        [Trait("Category", "ArmUnsupported")]
         [Trait("RunOnWindows", "True")]
         public async Task TestIastSqlInjectionRequest()
         {
