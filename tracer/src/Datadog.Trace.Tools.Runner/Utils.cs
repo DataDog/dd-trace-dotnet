a89671440b6282a5f3469968cd7bfd04895a33fd
Tony Redondo <tony.redondo@datadoghq.com>
2023-01-18 15:24:22 +0100

    [CI Visibility] - Fix dd-trace arguments handling (#3676)
    * Fix arguments handling when running CI visibility mode
    
    * fix defaults for DD_ENV when using CI visibility.

--- tracer/src/Datadog.Trace.Tools.Runner/Utils.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/Utils.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.Agent;
@@ -19,6 +20,7 @@ using Datadog.Trace.Ci.Agent;
 using Datadog.Trace.Ci.Sampling;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.HttpOverStreams;
+using Datadog.Trace.Util;
 using Spectre.Console;
 
 namespace Datadog.Trace.Tools.Runner
@@ -406,6 +408,108 @@ namespace Datadog.Trace.Tools.Runner
             }
 
             return envVars;
+        }
+
+        /// <summary>
+        /// Convert the arguments array to a string
+        /// </summary>
+        /// <remarks>
+        /// This code is taken from https://source.dot.net/#System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/PasteArguments.cs,624678ba1465e776
+        /// </remarks>
+        /// <param name="args">Arguments array</param>
+        /// <returns>String of arguments</returns>
+        public static string GetArgumentsAsString(string[] args)
+        {
+            const char Quote = '\"';
+            const char Backslash = '\\';
+            var stringBuilder = StringBuilderCache.Acquire(100);
+
+            foreach (var argument in args)
+            {
+                if (stringBuilder.Length != 0)
+                {
+                    stringBuilder.Append(' ');
+                }
+
+                // Parsing rules for non-argv[0] arguments:
+                //   - Backslash is a normal character except followed by a quote.
+                //   - 2N backslashes followed by a quote ==> N literal backslashes followed by unescaped quote
+                //   - 2N+1 backslashes followed by a quote ==> N literal backslashes followed by a literal quote
+                //   - Parsing stops at first whitespace outside of quoted region.
+                //   - (post 2008 rule): A closing quote followed by another quote ==> literal quote, and parsing remains in quoting mode.
+                if (argument.Length != 0 && ContainsNoWhitespaceOrQuotes(argument))
+                {
+                    // Simple case - no quoting or changes needed.
+                    stringBuilder.Append(argument);
+                }
+                else
+                {
+                    stringBuilder.Append(Quote);
+                    int idx = 0;
+                    while (idx < argument.Length)
+                    {
+                        char c = argument[idx++];
+                        if (c == Backslash)
+                        {
+                            int numBackSlash = 1;
+                            while (idx < argument.Length && argument[idx] == Backslash)
+                            {
+                                idx++;
+                                numBackSlash++;
+                            }
+
+                            if (idx == argument.Length)
+                            {
+                                // We'll emit an end quote after this so must double the number of backslashes.
+                                stringBuilder.Append(Backslash, numBackSlash * 2);
+                            }
+                            else if (argument[idx] == Quote)
+                            {
+                                // Backslashes will be followed by a quote. Must double the number of backslashes.
+                                stringBuilder.Append(Backslash, (numBackSlash * 2) + 1);
+                                stringBuilder.Append(Quote);
+                                idx++;
+                            }
+                            else
+                            {
+                                // Backslash will not be followed by a quote, so emit as normal characters.
+                                stringBuilder.Append(Backslash, numBackSlash);
+                            }
+
+                            continue;
+                        }
+
+                        if (c == Quote)
+                        {
+                            // Escape the quote so it appears as a literal. This also guarantees that we won't end up generating a closing quote followed
+                            // by another quote (which parses differently pre-2008 vs. post-2008.)
+                            stringBuilder.Append(Backslash);
+                            stringBuilder.Append(Quote);
+                            continue;
+                        }
+
+                        stringBuilder.Append(c);
+                    }
+
+                    stringBuilder.Append(Quote);
+                }
+            }
+
+            return StringBuilderCache.GetStringAndRelease(stringBuilder);
+
+            static bool ContainsNoWhitespaceOrQuotes(string s)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char c = s[i];
+                    if (char.IsWhiteSpace(c) || c == Quote)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
         }
     }
 }
