fc6a6f84e7ddbe831fee348424eecc39c4296343
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-01-25 16:58:47 +0100

    Instrumentation test

---
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests.cs
@@ -0,0 +1,46 @@
+// <copyright file="IastInstrumentationUnitTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if NETCOREAPP
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.TestHelpers;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.Security.IntegrationTests.Iast;
+
+public class IastInstrumentationUnitTests : TestHelper
+{
+    public IastInstrumentationUnitTests(ITestOutputHelper output)
+        : base("InstrumentedTests", output)
+    {
+    }
+
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    public void TestUnit()
+    {
+        using (var agent = EnvironmentHelper.GetMockAgent())
+        {
+            EnableIast(true);
+            ProcessResult processResult = RunDotnetTestSampleAndWaitForExit(agent, string.Empty);
+            var error = processResult.StandardError;
+            var output = processResult.StandardOutput;
+
+            error.Should().BeEmpty(error);
+        }
+    }
+}
+#endif
