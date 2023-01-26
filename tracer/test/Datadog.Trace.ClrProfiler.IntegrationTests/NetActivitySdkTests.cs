26dcf70839b64f79dea45bd7e4d84e837e6774e9
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-01-26 13:26:07 -0500

    Fix .NET activity tests

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/NetActivitySdkTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/NetActivitySdkTests.cs
@@ -28,22 +28,29 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         }
 
         public override Result ValidateIntegrationSpan(MockSpan span) =>
-            span.IsOpenTelemetry(excludeTags: new HashSet<string>
-            {
-                "attribute-string",
-                "attribute-int",
-                "attribute-bool",
-                "attribute-double",
-                "attribute-stringArray",
-                "attribute-stringArrayEmpty",
-                "attribute-intArray",
-                "attribute-intArrayEmpty",
-                "attribute-boolArray",
-                "attribute-boolArrayEmpty",
-                "attribute-doubleArray",
-                "attribute-doubleArrayEmpty",
-                "set-string"
-            });
+            span.IsOpenTelemetry(
+                resources: new HashSet<string>
+                {
+                    "service.instance.id",
+                    "service.name",
+                    "service.version"
+                },
+                excludeTags: new HashSet<string>
+                {
+                    "attribute-string",
+                    "attribute-int",
+                    "attribute-bool",
+                    "attribute-double",
+                    "attribute-stringArray",
+                    "attribute-stringArrayEmpty",
+                    "attribute-intArray",
+                    "attribute-intArrayEmpty",
+                    "attribute-boolArray",
+                    "attribute-boolArrayEmpty",
+                    "attribute-doubleArray",
+                    "attribute-doubleArrayEmpty",
+                    "set-string"
+                });
 
         [SkippableFact]
         [Trait("Category", "EndToEnd")]
