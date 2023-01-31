4c21470b32895b6fcdd2970e0c1445780e6358a8
Andrew Lock <andrew.lock@datadoghq.com>
2023-01-31 17:02:02 +0000

    Add helper source generators for telemetry metrics (#3701)
    * Add TelemetryMetricAttribute source generator
    
    * Add TelemetryMetricAttribute output
    
    * Add PublicApiGenerator source generator
    
    Used to generate "auto properties" with additional functionality from fields
    
    * Add GeneratePublicApiAttribute output

---
+++ tracer/src/Datadog.Trace.SourceGenerators/TelemetryMetric/Diagnostics/DuplicateMetricValueDiagnostic.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="DuplicateMetricValueDiagnostic.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Datadog.Trace.SourceGenerators.Helpers;
+using Microsoft.CodeAnalysis;
+
+namespace Datadog.Trace.SourceGenerators.TelemetryMetric.Diagnostics;
+
+internal static class DuplicateMetricValueDiagnostic
+{
+    internal const string Id = "TM4";
+    private const string Message = "The combination of metric value, namespace, and IsCommon must be unique";
+    private const string Title = "Duplicate values";
+
+    public static DiagnosticInfo CreateInfo(SyntaxNode? currentNode)
+        => new(
+            new DiagnosticDescriptor(
+                Id,
+                Title,
+                Message,
+                category: SourceGenerators.Constants.Usage,
+                defaultSeverity: DiagnosticSeverity.Warning,
+                isEnabledByDefault: true),
+            currentNode?.GetLocation());
+}
