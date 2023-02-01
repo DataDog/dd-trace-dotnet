e2a1b9562da7b97231d6e467599c06d079e02cea
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-02-01 13:28:29 +0100

    more tests

--- tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests2.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests2.cs
@@ -60,4 +60,51 @@ public class IastInstrumentationUnitTests2 : TestHelper
             }
         }
     }
+
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    public void TestInstrumentedUnitTests444()
+    {
+        SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Enabled, "1");
+        SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Logs, "1");
+
+        using (var agent = EnvironmentHelper.GetMockAgent())
+        {
+            using (ProcessResult processResult = RunDotnetTestSampleAndWaitForExit(agent))
+            {
+                processResult.StandardError.Should().BeEmpty("arguments: " + Environment.NewLine + processResult.StandardError + Environment.NewLine + processResult.StandardOutput);
+            }
+        }
+    }
+
+    [SkippableFact]
+    public void TestInstrumentedUnitTests555()
+    {
+        SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Enabled, "1");
+        SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Logs, "1");
+
+        using (var agent = EnvironmentHelper.GetMockAgent())
+        {
+            using (ProcessResult processResult = RunDotnetTestSampleAndWaitForExit(agent))
+            {
+                processResult.StandardError.Should().BeEmpty("arguments: " + Environment.NewLine + processResult.StandardError + Environment.NewLine + processResult.StandardOutput);
+            }
+        }
+    }
+
+    [Trait("Category", "TestIntegrations")]
+    [SkippableFact]
+    public void TestInstrumentedUnitTests6666()
+    {
+        SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Enabled, "1");
+        SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Logs, "1");
+
+        using (var agent = EnvironmentHelper.GetMockAgent())
+        {
+            using (ProcessResult processResult = RunDotnetTestSampleAndWaitForExit(agent))
+            {
+                processResult.StandardError.Should().BeEmpty("arguments: " + Environment.NewLine + processResult.StandardError + Environment.NewLine + processResult.StandardOutput);
+            }
+        }
+    }
 }
