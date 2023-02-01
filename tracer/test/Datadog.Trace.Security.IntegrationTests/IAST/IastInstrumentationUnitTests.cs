cc85b7eea9755aacd4598bf0dc9206b1fd4bfef0
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-02-01 17:34:41 +0100

    some more tests

--- tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests.cs
@@ -5,11 +5,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.TestHelpers;
 using FluentAssertions;
@@ -40,5 +42,98 @@ public class IastInstrumentationUnitTests : TestHelper
             ProcessResult processResult = RunDotnetTestSampleAndWaitForExit(agent, arguments: arguments);
             processResult.StandardError.Should().BeEmpty("arguments: " + arguments + Environment.NewLine + processResult.StandardError + Environment.NewLine + processResult.StandardOutput);
         }
+    }
+
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    public void TestInstrumentedUnitTests2()
+    {
+        using (var agent = EnvironmentHelper.GetMockAgent())
+        {
+            EnableIast(true);
+            string arguments = string.Empty;
+#if NET462
+            arguments = @" /Framework:"".NETFramework,Version=v4.6.2"" ";
+#endif
+            ProcessResult processResult = RunDotnetTestSampleAndWaitForExit2(agent, arguments: arguments);
+            processResult.StandardError.Should().BeEmpty("arguments: " + arguments + Environment.NewLine + processResult.StandardError + Environment.NewLine + processResult.StandardOutput);
+        }
+    }
+
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    public void TestInstrumentedUnitTests3()
+    {
+        using (var agent = EnvironmentHelper.GetMockAgent())
+        {
+            EnableIast(true);
+            string arguments = string.Empty;
+            string sampleAppPath = string.Empty;
+#if NET462
+            arguments = @" /Framework:"".NETFramework,Version=v4.6.2"" ";
+            sampleAppPath = EnvironmentHelper.GetSampleProjectDirectory() + "\\..\\..\\..\\Datadog.Trace.Security.Unit.Tests\\bin\\Release\\net462\\Datadog.Trace.Security.Unit.Tests.dll";
+#endif
+            ProcessResult processResult = RunDotnetTestSampleAndWaitForExit2(agent, arguments: arguments, dllPath: sampleAppPath);
+            processResult.StandardError.Should().BeEmpty("arguments: " + arguments + Environment.NewLine + processResult.StandardError + Environment.NewLine + processResult.StandardOutput);
+        }
+    }
+
+    public Process StartDotnetTestSample2(MockTracerAgent agent, string arguments, string packageVersion, int aspNetCorePort, string framework = "", string dllPath = "")
+    {
+        // get path to sample app that the profiler will attach to
+        string sampleAppPath = dllPath.IsNullOrEmpty() ? EnvironmentHelper.GetTestCommandForSampleApplicationPath(packageVersion) : dllPath;
+        if (!File.Exists(sampleAppPath))
+        {
+            throw new Exception($"application not found: {sampleAppPath}");
+        }
+
+        Output.WriteLine($"Starting Application: {sampleAppPath}");
+        string testCli = EnvironmentHelper.GetDotNetTest();
+        string exec = testCli;
+        string appPath = testCli.StartsWith("dotnet") ? $"test {sampleAppPath}" : sampleAppPath;
+        Output.WriteLine("Executable: " + exec);
+        Output.WriteLine("ApplicationPath: " + appPath);
+        var process = ProfilerHelper.StartProcessWithProfiler(
+            exec,
+            EnvironmentHelper,
+            agent,
+            $"{appPath} {arguments ?? string.Empty}",
+            aspNetCorePort: aspNetCorePort,
+            processToProfile: exec + ";testhost.exe");
+
+        Output.WriteLine($"ProcessId: {process.Id}");
+
+        return process;
+    }
+
+    public ProcessResult RunDotnetTestSampleAndWaitForExit2(MockTracerAgent agent, string arguments = null, string packageVersion = "", string framework = "", string dllPath = "")
+    {
+        var process = StartDotnetTestSample2(agent, arguments, packageVersion, aspNetCorePort: 5000, framework: framework, dllPath: dllPath);
+
+        using var helper = new ProcessHelper(process);
+
+        process.WaitForExit();
+        helper.Drain();
+        var exitCode = process.ExitCode;
+
+        Output.WriteLine($"Exit Code: " + exitCode);
+
+        var standardOutput = helper.StandardOutput;
+
+        if (!string.IsNullOrWhiteSpace(standardOutput))
+        {
+            Output.WriteLine($"StandardOutput:{Environment.NewLine}{standardOutput}");
+        }
+
+        var standardError = helper.ErrorOutput;
+
+        if (!string.IsNullOrWhiteSpace(standardError))
+        {
+            Output.WriteLine($"StandardError:{Environment.NewLine}{standardError}");
+        }
+
+        return new ProcessResult(process, standardOutput, standardError, exitCode);
     }
 }
