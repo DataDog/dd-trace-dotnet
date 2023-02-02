f54587e0f444efa311ad1c74a4dc31a286570bf2
Zach Montoya <zach.montoya@datadoghq.com>
2023-02-02 11:11:10 -0800

    Update Dockerfile to only copy required files into docker image (to save on space)

--- lib-injection/copy-lib.sh
+++ lib-injection/copy-lib.sh
@@ -1,22 +1,2 @@
 #!/bin/sh
-
-# This script is used by the admission controller to install the library from the
-# init container into the application container.
-
-# Based on the user architecture,
-archOutput="$(arch)"
-case "$archOutput" in
-    'x86_64')
-        if [ -d linux_amd64 ]; then
-            cp -R linux_amd64/* "$1"
-        fi
-        ;;
-    'aarch64')
-        if [ -d linux_arm64 ]; then
-            cp -R linux_arm64/* "$1"
-        fi
-        ;;
-    *)
-        echo >&2 "error: unsupported architecture '$archOutput'"
-        ;;
-esac
+cp -R monitoring-home/* "$1"
