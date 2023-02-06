29ca28eee45c39ce10e169f192d64bd780b08de8
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2023-02-06 12:40:40 -0500

    use `record struct` for TraceId

--- tracer/src/Datadog.Trace/TraceId.cs
+++ tracer/src/Datadog.Trace/TraceId.cs
@@ -4,7 +4,6 @@
 // </copyright>
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
 using Datadog.Trace.Util;
 
@@ -12,39 +11,17 @@ using Datadog.Trace.Util;
 
 namespace Datadog.Trace;
 
-internal readonly struct TraceId : IEquatable<TraceId>
+internal readonly record struct TraceId(ulong Upper, ulong Lower)
 {
-    public readonly ulong Lower;
-    public readonly ulong Upper;
-
-    /// <summary>Initializes a new instance of the <see cref="TraceId" /> struct.</summary>
-    /// <param name="upper">The upper 64-bits of the 128-bit value.</param>
-    /// <param name="lower">The lower 64-bits of the 128-bit value.</param>
-    public TraceId(ulong upper, ulong lower)
-    {
-        Upper = upper;
-        Lower = lower;
-    }
+    public readonly ulong Lower = Lower;
+    public readonly ulong Upper = Upper;
 
     public static implicit operator TraceId(ulong lower) => new(0, lower);
 
     public static implicit operator TraceId(int lower) => new(0, (ulong)lower);
 
-    public static bool operator ==(TraceId left, TraceId right)
-    {
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(TraceId left, TraceId right)
-    {
-        return !left.Equals(right);
-    }
-
     [Pure]
     public bool Equals(TraceId value) => Lower == value.Lower && Upper == value.Upper;
-
-    [Pure]
-    public override bool Equals([NotNullWhen(true)] object? value) => value is TraceId other && Equals(other);
 
     [Pure]
     public override int GetHashCode() => HashCode.Combine(Lower, Upper);
