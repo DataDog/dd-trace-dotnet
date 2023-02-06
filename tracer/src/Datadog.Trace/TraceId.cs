313cf083eedabe8c53fd6f5520cc826d75e4c24d
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2023-02-06 17:09:13 -0500

    implement IComparable in TraceId (for statistical tests)

--- tracer/src/Datadog.Trace/TraceId.cs
+++ tracer/src/Datadog.Trace/TraceId.cs
@@ -11,19 +11,69 @@ using Datadog.Trace.Util;
 
 namespace Datadog.Trace;
 
-internal readonly record struct TraceId(ulong Upper, ulong Lower)
+internal readonly record struct TraceId(ulong Upper, ulong Lower) : IComparable<TraceId>, IComparable
 {
     public const int Size = sizeof(ulong) * 2;
 
-    public readonly ulong Lower = Lower;
     public readonly ulong Upper = Upper;
+    public readonly ulong Lower = Lower;
 
     public static implicit operator TraceId(ulong lower) => new(0, lower);
 
     public static implicit operator TraceId(int lower) => new(0, (ulong)lower);
 
+    public static bool operator <(TraceId left, TraceId right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator >(TraceId left, TraceId right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    public static bool operator <=(TraceId left, TraceId right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >=(TraceId left, TraceId right)
+    {
+        return left.CompareTo(right) >= 0;
+    }
+
     [Pure]
     public bool Equals(TraceId value) => Lower == value.Lower && Upper == value.Upper;
+
+    [Pure]
+    public int CompareTo(TraceId other)
+    {
+        var upperComparison = Upper.CompareTo(other.Upper);
+
+        if (upperComparison != 0)
+        {
+            return upperComparison;
+        }
+
+        return Lower.CompareTo(other.Lower);
+    }
+
+    [Pure]
+    public int CompareTo(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return 1;
+        }
+
+        if (obj is TraceId other)
+        {
+            return CompareTo(other);
+        }
+
+        ThrowHelper.ThrowArgumentException($"Object must be of type {nameof(TraceId)}", nameof(obj));
+        return 0; // unreachable code
+    }
 
     [Pure]
     public override int GetHashCode() => HashCode.Combine(Lower, Upper);
