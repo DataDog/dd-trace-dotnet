51b723b8628ffe1295d4fe96adc33bb015cf648d
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2023-02-07 12:28:00 +0100

    Support functionality

--- tracer/src/Datadog.Trace/IAST/Propagation/StringModuleImpl.cs
+++ tracer/src/Datadog.Trace/IAST/Propagation/StringModuleImpl.cs
@@ -494,38 +494,38 @@ internal static class StringModuleImpl
         try
         {
             if (string.IsNullOrEmpty(left) || string.IsNullOrEmpty(right) || string.IsNullOrEmpty(result))
-            {
+        {
                 return result;
-            }
+        }
 
             var iastContext = IastModule.GetIastContext();
             if (iastContext == null)
-            {
+        {
                 return result;
-            }
+        }
 
             TaintedObjects taintedObjects = iastContext.GetTaintedObjects();
             TaintedObject? taintedLeft = filter != AspectFilter.StringLiteral_1 ? GetTainted(taintedObjects, left) : null;
             TaintedObject? taintedRight = filter != AspectFilter.StringLiteral_0 ? GetTainted(taintedObjects, right) : null;
-            if (taintedLeft == null && taintedRight == null)
-            {
+        if (taintedLeft == null && taintedRight == null)
+        {
                 return result;
-            }
+        }
 
-            Range[]? ranges;
-            if (taintedRight == null)
-            {
-                ranges = taintedLeft!.Ranges;
-            }
-            else if (taintedLeft == null)
-            {
-                ranges = new Range[taintedRight!.Ranges!.Length];
+        Range[]? ranges;
+        if (taintedRight == null)
+        {
+            ranges = taintedLeft!.Ranges;
+        }
+        else if (taintedLeft == null)
+        {
+            ranges = new Range[taintedRight!.Ranges!.Length];
                 Ranges.CopyShift(taintedRight!.Ranges, ranges, 0, left.Length);
-            }
-            else
-            {
-                ranges = Ranges.MergeRanges(left.Length, taintedLeft!.Ranges, taintedRight!.Ranges);
-            }
+        }
+        else
+        {
+            ranges = Ranges.MergeRanges(left.Length, taintedLeft!.Ranges, taintedRight!.Ranges);
+        }
 
             taintedObjects.Taint(result, ranges);
         }
@@ -664,9 +664,9 @@ internal static class StringModuleImpl
 
             if (ranges != null)
             {
-                taintedObjects.Taint(result, ranges);
-            }
-        }
+        taintedObjects.Taint(result, ranges);
+    }
+}
         catch (Exception err)
         {
             Log.Error(err, "StringModuleImpl.OnstringConcat(IEnumerable) exception");
