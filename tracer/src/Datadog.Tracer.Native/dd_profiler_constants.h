576b65b7bd6190afd0703026d728e7130f8f9157
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-02-09 16:04:15 +0100

    remove testhost from excluded assemblies

--- tracer/src/Datadog.Tracer.Native/dd_profiler_constants.h
+++ tracer/src/Datadog.Tracer.Native/dd_profiler_constants.h
@@ -50,7 +50,6 @@ const shared::WSTRING skip_assembly_prefixes[]{
     WStr("System.Text"),
     WStr("System.Threading"),
     WStr("System.Xml"),
-    WStr("testhost")
 };
 
 const shared::WSTRING include_assemblies[]{
