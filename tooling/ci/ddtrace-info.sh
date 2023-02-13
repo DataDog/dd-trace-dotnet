6160d25df633003dac0723bb5f5cdc53abbde98a
roberto montero <roberto.montero@datadoghq.com>
2023-02-13 08:31:33 +0100

    Fix version file name

--- tooling/ci/ddtrace-info.sh
+++ tooling/ci/ddtrace-info.sh
@@ -2,13 +2,10 @@
 set -eu
 cd /binaries
 mkdir -p /opt/datadog
-mkdir /app 
 tar xzf $(ls datadog-dotnet-apm.tar.gz) -C /opt/datadog
-
-ls /opt/datadog
 
 LD_LIBRARY_PATH=/opt/datadog dotnet fsi --langversion:preview /binaries/query-versions.fsx
 
-echo "dd-trace version: $(cat /binaries/TESTS_LIBRARY_VERSION)"
-echo "libddwaf version: $(cat /binaries/TESTS_LIBDDWAF_VERSION)"
+echo "dd-trace version: $(cat /binaries/LIBRARY_VERSION)"
+echo "libddwaf version: $(cat /binaries/LIBDDWAF_VERSION)"
 echo "appsec event rules version: $(cat /binaries/APPSEC_EVENT_RULES_VERSION)"
