98811b62d256e19cf27e10dfa6fc2be56f1524d6
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-02-15 13:08:26 +0100

    string tests passing

--- tracer/src/Datadog.Trace/IAST/Aspects/System/StringAspects.cs
+++ tracer/src/Datadog.Trace/IAST/Aspects/System/StringAspects.cs
@@ -175,7 +175,6 @@ public partial class StringAspects
         return StringModuleImpl.OnStringConcat(values, string.Concat(values));
     }
 
-/*
     /// <summary>
     /// String.Concat aspect
     /// </summary>
