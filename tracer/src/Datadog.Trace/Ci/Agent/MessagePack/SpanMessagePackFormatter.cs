534677a31b682e836cf55c9088c65e8a327356f2
Tony Redondo <tony.redondo@datadoghq.com>
2023-02-21 11:58:54 +0100

    remove unused using.

--- tracer/src/Datadog.Trace/Ci/Agent/MessagePack/SpanMessagePackFormatter.cs
+++ tracer/src/Datadog.Trace/Ci/Agent/MessagePack/SpanMessagePackFormatter.cs
@@ -7,7 +7,6 @@ using System;
 using System.Runtime.CompilerServices;
 using Datadog.Trace.Ci.Tagging;
 using Datadog.Trace.Ci.Tags;
-using Datadog.Trace.Configuration;
 using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.Processors;
 using Datadog.Trace.Tagging;
