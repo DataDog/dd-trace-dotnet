ed8c1d576bb17f676c92c0496323db4afa3990ae
Tony Redondo <tony.redondo@datadoghq.com>
2023-03-17 15:12:14 +0100

    Reverting some changes just to temporaly enable CI

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -102,7 +102,7 @@ partial class Build
         {
             if (TargetPlatform == MSBuildTargetPlatform.x64)
             {
-                return new[] { MSBuildTargetPlatform.x64, MSBuildTargetPlatform.x86, ARM64TargetPlatform };
+                return new[] { MSBuildTargetPlatform.x64, MSBuildTargetPlatform.x86 };
             }
             else if (TargetPlatform == MSBuildTargetPlatform.x86)
             {
