ff1776f8d6227d5fbb40e65373c9eb77508369cc
Flavien Darche <flavien.darche@datadoghq.com>
2023-03-21 18:22:53 +0100

    Update and create new tests

--- tracer/test/Datadog.Trace.Security.IntegrationTests/Rcm/AspNetCore5AsmAttributesWafTimeout.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/Rcm/AspNetCore5AsmAttributesWafTimeout.cs
@@ -15,52 +15,107 @@ using Datadog.Trace.TestHelpers;
 using Xunit;
 using Xunit.Abstractions;
 
+#pragma warning disable SA1402 // File may only contain a single type       | contains alls test types
+#pragma warning disable SA1649 // File name should match first type name    | contains alls test types
+
 namespace Datadog.Trace.Security.IntegrationTests.Rcm
 {
-    public class AspNetCore5AsmAttributesWafTimeout : RcmBase
+    public class TestWafTimeoutValueChanged : AspNetCore5AsmAttributesWafTimeout
     {
-        private const string AsmProduct = "ASM";
-
-        public AspNetCore5AsmAttributesWafTimeout(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
-            : base(fixture, outputHelper, enableSecurity: true, testName: nameof(AspNetCore5AsmAttributesWafTimeout))
+        public TestWafTimeoutValueChanged(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper, nameof(TestWafTimeoutValueChanged), "global", 1)
         {
-            SetEnvironmentVariable(ConfigurationKeys.DebugEnabled, "1");
         }
 
         [SkippableTheory]
         [InlineData("/params-endpoint/appscan_fingerprint", 200)]
         [Trait("RunOnWindows", "True")]
-        public async Task TestWafTimeoutValueChanged(string type, int statusCode)
+        public async Task RunTask(string type, int statusCode)
+        {
+            await RunTest(type, statusCode);
+        }
+    }
+
+    public class TestWafLargeValueChanged : AspNetCore5AsmAttributesWafTimeout
+    {
+        public TestWafLargeValueChanged(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper, nameof(TestWafLargeValueChanged), "global", 2000000000)
+        {
+        }
+
+        [SkippableTheory]
+        [InlineData("/params-endpoint/appscan_fingerprint", 200)]
+        [Trait("RunOnWindows", "True")]
+        public async Task RunTask(string type, int statusCode)
+        {
+            await RunTest(type, statusCode);
+        }
+    }
+
+    public class TestWafInvalidNegativeValueChanged : AspNetCore5AsmAttributesWafTimeout
+    {
+        public TestWafInvalidNegativeValueChanged(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper, nameof(TestWafInvalidNegativeValueChanged), "global", -1801)
+        {
+        }
+
+        [SkippableTheory]
+        [InlineData("/params-endpoint/appscan_fingerprint", 200)]
+        [Trait("RunOnWindows", "True")]
+        public async Task RunTask(string type, int statusCode)
+        {
+            await RunTest(type, statusCode);
+        }
+    }
+
+    public class TestWafInvalidZeroValueChanged : AspNetCore5AsmAttributesWafTimeout
+    {
+        public TestWafInvalidZeroValueChanged(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper, nameof(TestWafInvalidZeroValueChanged), "global", 0)
+        {
+        }
+
+        [SkippableTheory]
+        [InlineData("/params-endpoint/appscan_fingerprint", 200)]
+        [Trait("RunOnWindows", "True")]
+        public async Task RunTask(string type, int statusCode)
+        {
+            await RunTest(type, statusCode);
+        }
+    }
+
+    public class AspNetCore5AsmAttributesWafTimeout : RcmBase
+    {
+        private const string AsmProduct = "ASM";
+
+        private readonly string _id;
+        private readonly int _timeout;
+        private readonly string _testName;
+
+        public AspNetCore5AsmAttributesWafTimeout(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper, string testName, string id, int timeout)
+            : base(fixture, outputHelper, enableSecurity: true, testName: testName)
+        {
+            _id = id;
+            _timeout = timeout;
+            _testName = testName;
+
+            SetEnvironmentVariable(ConfigurationKeys.DebugEnabled, "1");
+        }
+
+        public async Task RunTest(string type, int statusCode)
         {
             EnableDebugMode();
 
             await TryStartApp();
             var agent = Fixture.Agent;
             var settings = VerifyHelper.GetSpanVerifierSettings(type, statusCode);
-            var acknowledgedId = nameof(TestWafTimeoutValueChanged) + Guid.NewGuid();
 
             var spans1 = await SendRequestsAsync(agent, type);
-            acknowledgedId = nameof(TestWafTimeoutValueChanged) + Guid.NewGuid();
+            var acknowledgedId = _testName + Guid.NewGuid();
 
-            var rcmWafData = new Payload
-            {
-                Data = new Data[]
-                {
-                    new()
-                    {
-                        Attributes = new Attributes
-                        {
-                            CustomAttributes = new Dictionary<string, object>
-                            {
-                                { "waf_timeout", 2 }, // Set a low timeout to make the waf timeout
-                            }
-                        },
-                        Id = "3dd-0uc-h1s"
-                    }
-                }
-            };
-
+            var rcmWafData = CreateWafTimeoutRcm(_timeout, _id);
             await agent.SetupRcmAndWait(Output, new[] { ((object)rcmWafData, acknowledgedId) }, AsmProduct, appliedServiceNames: new[] { acknowledgedId });
+
             var spans2 = await SendRequestsAsync(agent, type);
             var spans = new List<MockSpan>();
             spans.AddRange(spans1);
@@ -68,7 +123,10 @@ namespace Datadog.Trace.Security.IntegrationTests.Rcm
             await VerifySpans(spans.ToImmutableList(), settings);
         }
 
-        protected override string GetTestName() => Prefix + nameof(AspNetCore5AsmAttributesWafTimeout);
+        private Payload CreateWafTimeoutRcm(int timeoutValue, string id)
+        {
+            return new Payload { Data = new Data { Id = id, Type = "custom_attributes", Attributes = new Attributes { Values = new Dictionary<string, object> { { "waf_timeout", timeoutValue }, } } } };
+        }
     }
 }
 #endif
