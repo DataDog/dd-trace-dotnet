1eaf59a6c3e86b8c348bce0db3e71e0872ed1d5f
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-03-22 10:46:35 -0400

    Check if environment variables are set or not
    The tracer will already be initialized, so setting the environment variables won't do anything - instead just check to see if they are set or not. If not, throw.
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/test/test-applications/integrations/dependency-libs/LogsInjectionHelper/LoggingMethods.cs
+++ tracer/test/test-applications/integrations/dependency-libs/LogsInjectionHelper/LoggingMethods.cs
@@ -35,8 +35,13 @@ namespace PluginApplication
 
             // Do not explicitly set LogsInjectionEnabled = true, use DD_LOGS_INJECTION environment variable to enable
 
-            Environment.SetEnvironmentVariable("DD_ENV", "dev"); // Ensure that we have an env value. In CI, this will automatically be assigned. Later we can test that everything is fine when Environment=null
-            Environment.SetEnvironmentVariable("DD_VERSION", "1.0.0"); // Ensure that we have an env value. In CI, this will automatically be assigned. Later we can test that everything is fine when when ServiceVersion=null
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DD_ENV")
+                || string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DD_VERSION"))
+            {
+                // Ensure that we have an env value. In CI, this will automatically be assigned. Later we can test that everything is fine when Environment=null
+                // Ensure that we have a version value. In CI, this will automatically be assigned. Later we can test that everything is fine when when ServiceVersion=null
+                throw new Exception("You must set DD_ENV or DD_VERSION");
+            }
 
             try
             {
