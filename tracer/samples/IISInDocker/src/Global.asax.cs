15f48ad685bcfc1cbb8ca436bf579191419d9dc3
Zach Montoya <zach.montoya@datadoghq.com>
2023-03-28 16:24:15 -0700

    Remove unnecessary js/css files to minimize bloat, while still remaining functional and able to send traces

--- tracer/samples/IISInDocker/src/Global.asax.cs
+++ tracer/samples/IISInDocker/src/Global.asax.cs
@@ -13,9 +13,7 @@ namespace IISInDocker
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
