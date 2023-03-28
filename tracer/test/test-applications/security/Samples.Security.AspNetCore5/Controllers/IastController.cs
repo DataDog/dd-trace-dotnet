a3f7828d97d1e573b38bc96bd672b0f082c2d2ed
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-03-28 12:09:47 +0200

    modify iast controller

--- tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/IastController.cs
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/IastController.cs
@@ -3,90 +3,92 @@ using System.ComponentModel;
 using System.Data.SQLite;
 using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Samples.Security.Data;
 
-namespace Samples.Security.AspNetCore5.Controllers;
-
-[Route("[controller]")]
-[ApiController]
-public class IastController : ControllerBase
+namespace Samples.Security.AspNetCore5.Controllers
 {
-    static SQLiteConnection dbConnection = null;
-
-    public IActionResult Index()
+    [Route("[controller]")]
+    [ApiController]
+    public class IastController : ControllerBase
     {
-        return Content("Ok\n");
-    }
+        static SQLiteConnection dbConnection = null;
 
-    [HttpGet("WeakHashing")]
-    [Route("WeakHashing/{delay1}")]
-    public IActionResult WeakHashing(int delay1 = 0, int delay2 = 0)
-    {
-        System.Threading.Thread.Sleep(delay1 + delay2);
+        public IActionResult Index()
+        {
+            return Content("Ok\n");
+        }
+
+        [HttpGet("WeakHashing")]
+        [Route("WeakHashing/{delay1}")]
+        public IActionResult WeakHashing(int delay1 = 0, int delay2 = 0)
+        {
+            System.Threading.Thread.Sleep(delay1 + delay2);
 #pragma warning disable SYSLIB0021 // Type or member is obsolete
-        var byteArg = new byte[] { 3, 5, 6 };
-        MD5.Create().ComputeHash(byteArg);
-        SHA1.Create().ComputeHash(byteArg);
-        return Content($"Weak hashes launched with delays {delay1} and {delay2}.\n");
+            var byteArg = new byte[] { 3, 5, 6 };
+            MD5.Create().ComputeHash(byteArg);
+            SHA1.Create().ComputeHash(byteArg);
+            return Content($"Weak hashes launched with delays {delay1} and {delay2}.\n");
 #pragma warning restore SYSLIB0021 // Type or member is obsolete
-    }
-
-    [HttpGet("SqlQuery")]
-    [Route("SqlQuery")]
-    public IActionResult SqlQuery(string username, string query)
-    {
-        try
-        {
-            if (dbConnection is null)
-            {
-                dbConnection = IastControllerHelper.CreateDatabase();
-            }
-
-            if (!string.IsNullOrEmpty(username))
-            {
-                var taintedQuery = "SELECT Surname from Persons where name = '" + username + "'";
-                var rname = new SQLiteCommand(taintedQuery, dbConnection).ExecuteScalar();
-                return Content($"Result: " + rname);
-            }
-
-            if (!string.IsNullOrEmpty(query))
-            {
-                var rname = new SQLiteCommand(query, dbConnection).ExecuteScalar();
-                return Content($"Result: " + rname);
-            }
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, IastControllerHelper.ToFormattedString(ex));
         }
 
-        return BadRequest($"No query or username was provided");
-    }
+        [HttpGet("SqlQuery")]
+        [Route("SqlQuery")]
+        public IActionResult SqlQuery(string username, string query)
+        {
+            try
+            {
+                if (dbConnection is null)
+                {
+                    dbConnection = IastControllerHelper.CreateDatabase();
+                }
 
-    [HttpGet("ExecuteCommand")]
-    [Route("ExecuteCommand")]
-    public IActionResult ExecuteCommand(string file, string argumentLine)
-    {
-        try
-        {
-            if (!string.IsNullOrEmpty(file))
-            {
-                var result = Process.Start(file, argumentLine);
-                return Content($"Process launched: " + result.ProcessName);
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var taintedQuery = "SELECT Surname from Persons where name = '" + username + "'";
+                    var rname = new SQLiteCommand(taintedQuery, dbConnection).ExecuteScalar();
+                    return Content($"Result: " + rname);
+                }
+
+                if (!string.IsNullOrEmpty(query))
+                {
+                    var rname = new SQLiteCommand(query, dbConnection).ExecuteScalar();
+                    return Content($"Result: " + rname);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return BadRequest($"No file was provided");
+                return StatusCode(500, IastControllerHelper.ToFormattedString(ex));
             }
+
+            return BadRequest($"No query or username was provided");
         }
-        catch (Win32Exception ex)
+
+        [HttpGet("ExecuteCommand")]
+        [Route("ExecuteCommand")]
+        public IActionResult ExecuteCommand(string file, string argumentLine)
         {
-            return Content(IastControllerHelper.ToFormattedString(ex));
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, IastControllerHelper.ToFormattedString(ex));
+            try
+            {
+                if (!string.IsNullOrEmpty(file))
+                {
+                    var result = Process.Start(file, argumentLine);
+                    return Content($"Process launched: " + result.ProcessName);
+                }
+                else
+                {
+                    return BadRequest($"No file was provided");
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                return Content(IastControllerHelper.ToFormattedString(ex));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, IastControllerHelper.ToFormattedString(ex));
+            }
         }
     }
 }
