8f954a6a3abebdc43674e3739702edf8489fe6a8
Zach Montoya <zach.montoya@datadoghq.com>
2023-03-28 17:07:05 -0700

    Remove reference to IISInDocker for the GitHub release expected changes list

--- tracer/build/_build/Build.GitHub.cs
+++ tracer/build/_build/Build.GitHub.cs
@@ -428,7 +428,6 @@ partial class Build
                 "tracer/samples/ConsoleApp/Alpine3.10.dockerfile",
                 "tracer/samples/ConsoleApp/Alpine3.9.dockerfile",
                 "tracer/samples/ConsoleApp/Debian.dockerfile",
-                "tracer/samples/IISInDocker/Dockerfile",
                 "tracer/samples/WindowsContainer/Dockerfile",
                 "tracer/src/Datadog.Trace.Bundle/Datadog.Trace.Bundle.csproj",
                 "tracer/src/Datadog.Trace.AspNet/Datadog.Trace.AspNet.csproj",
