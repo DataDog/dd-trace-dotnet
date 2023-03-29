e418fca9c9965d71415d1aeb345f1d4ff2836d5f
Andrew Lock <andrew.lock@datadoghq.com>
2023-03-29 13:23:12 +0100

    Add a "block list" to profiled processes in native profilers (#3965)
    * Add a "block list" to profiled processes
    
    Automatically bail-out profiling processes in the block list, _unless_ we've explicitly opted in via the include list
    
    * Add sqlservr.exe to exclude list
    
    * Rename profiler -> ClrProfiler
    
    * Always check the default exclude list, even when we have a custom exclude list

--- tracer/src/Datadog.Tracer.Native/dd_profiler_constants.h
+++ tracer/src/Datadog.Tracer.Native/dd_profiler_constants.h
@@ -59,6 +59,14 @@ const shared::WSTRING include_assemblies[]{
     WStr("System.Diagnostics.Process"),
 };
 
+// Note that this list should be kept in sync with the values in shared/src/Datadog.Trace.ClrProfiler.Native/util.h
+const shared::WSTRING default_exclude_assemblies[]{
+    WStr("dd-trace"),
+    WStr("dd-trace.exe"),
+    WStr("aspnet_state.exe"),
+    WStr("sqlservr.exe"),
+};
+
 const shared::WSTRING skip_traceattribute_assembly_prefixes[]{
     WStr("System."), WStr("Microsoft."), WStr("Datadog.")};
 
