3115695a8dcd903e7b2cc78a62ea88bfcbaab196
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-03-30 13:07:14 +0200

    Force a crash to test

--- tracer/test/test-applications/integrations/Samples.TraceAnnotations.VersionMismatch.BeforeFeature/Program.cs
+++ tracer/test/test-applications/integrations/Samples.TraceAnnotations.VersionMismatch.BeforeFeature/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Datadog.Trace;
 
@@ -7,6 +8,8 @@ namespace Samples.TraceAnnotations.VersionMismatch.BeforeFeature
     {
         public static async Task Main(string[] args)
         {
+            Environment.FailFast("nope");
+
             // Access Tracer.Instance.ActiveScope so there's an AssemblyRef to Datadog.Trace
             var scope = Tracer.Instance.ActiveScope;
             await ProgramHelpers.RunTestsAsync();
