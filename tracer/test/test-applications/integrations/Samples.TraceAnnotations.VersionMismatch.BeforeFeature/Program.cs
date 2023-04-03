5613a7fa082e0fc11d47c1471e941ea3d18ba9d2
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-04-03 16:42:27 +0200

    Remove the test exception

--- tracer/test/test-applications/integrations/Samples.TraceAnnotations.VersionMismatch.BeforeFeature/Program.cs
+++ tracer/test/test-applications/integrations/Samples.TraceAnnotations.VersionMismatch.BeforeFeature/Program.cs
@@ -8,8 +8,6 @@ namespace Samples.TraceAnnotations.VersionMismatch.BeforeFeature
     {
         public static async Task Main(string[] args)
         {
-            throw new Exception("nope");
-
             // Access Tracer.Instance.ActiveScope so there's an AssemblyRef to Datadog.Trace
             var scope = Tracer.Instance.ActiveScope;
             await ProgramHelpers.RunTestsAsync();
