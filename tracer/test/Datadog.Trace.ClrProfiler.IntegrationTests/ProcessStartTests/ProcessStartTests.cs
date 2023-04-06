cc74af6d72afb9ad7214b6f1853cd480848f8c5e
Zach Montoya <zach.montoya@datadoghq.com>
2023-04-06 14:19:55 -0700

    Schema version testing: Process
    Notes:
    - The previous snapshot has been renamed with a SchemaV0 suffix
    - The new SchemaV1 snapshot file differs from the SchemaV0 snapshot in the following ways:
      - The client spans now have the $DD_SERVICE value
      - The client spans now have the "version" tag populated with the $DD_VERSION value

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ProcessStartTests/ProcessStartTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/ProcessStartTests/ProcessStartTests.cs
@@ -20,6 +20,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
     [UsesVerify]
     public class ProcessStartTests : TracingIntegrationTest
     {
+        private const string ServiceName = "Samples.ProcessStart";
         private static readonly Regex StackRegex = new(@"      error.stack:(\n|\r){1,2}.*(\n|\r){1,2}.*,(\r|\n){1,2}");
         private static readonly Regex ErrorMsgRegex = new(@"      error.msg:.*,(\r|\n){1,2}");
 
@@ -34,34 +35,12 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
         [SkippableFact]
         [Trait("Category", "EndToEnd")]
         [Trait("RunOnWindows", "True")]
-        public async Task SubmitsTraces()
-        {
-            const int expectedSpanCount = 5;
-            const string expectedOperationName = "command_execution";
+        public async Task SubmitsTracesV0() => await RunTest("v0");
 
-            using var telemetry = this.ConfigureTelemetry();
-            using var agent = EnvironmentHelper.GetMockAgent();
-            using var process = RunSampleAndWaitForExit(agent);
-            var spans = agent.WaitForSpans(expectedSpanCount, operationName: expectedOperationName);
-            ValidateIntegrationSpans(spans, expectedServiceName: "Samples.ProcessStart-command");
-
-            var settings = VerifyHelper.GetSpanVerifierSettings();
-            settings.AddRegexScrubber(StackRegex, string.Empty);
-            settings.AddRegexScrubber(ErrorMsgRegex, string.Empty);
-            var filename = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ?
-                "ProcessStartTests.SubmitsTracesLinux" :
-                RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ?
-                    "ProcessStartTests.SubmitsTracesOsx" :
-                    "ProcessStartTests.SubmitsTraces";
-
-            await VerifyHelper.VerifySpans(spans, settings)
-                              .UseFileName(filename)
-                              .DisableRequireUniquePrefix();
-
-            VerifyInstrumentation(process.Process);
-
-            telemetry.AssertIntegrationEnabled(IntegrationId.Process);
-        }
+        [SkippableFact]
+        [Trait("Category", "EndToEnd")]
+        [Trait("RunOnWindows", "True")]
+        public async Task SubmitsTracesV1() => await RunTest("v1");
 
         [SkippableFact]
         [Trait("Category", "EndToEnd")]
@@ -80,6 +59,39 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 
             Assert.Empty(spans.Where(s => s.Name.Equals(expectedOperationName)));
             telemetry.AssertIntegrationDisabled(IntegrationId.Process);
+        }
+
+        private async Task RunTest(string metadataSchemaVersion)
+        {
+            const int expectedSpanCount = 5;
+            const string expectedOperationName = "command_execution";
+
+            SetEnvironmentVariable("DD_TRACE_SPAN_ATTRIBUTE_SCHEMA", metadataSchemaVersion);
+            var isExternalSpan = metadataSchemaVersion == "v0";
+            var clientSpanServiceName = isExternalSpan ? $"{ServiceName}-command" : ServiceName;
+
+            using var telemetry = this.ConfigureTelemetry();
+            using var agent = EnvironmentHelper.GetMockAgent();
+            using var process = RunSampleAndWaitForExit(agent);
+            var spans = agent.WaitForSpans(expectedSpanCount, operationName: expectedOperationName);
+            ValidateIntegrationSpans(spans, expectedServiceName: clientSpanServiceName, isExternalSpan);
+
+            var settings = VerifyHelper.GetSpanVerifierSettings();
+            settings.AddRegexScrubber(StackRegex, string.Empty);
+            settings.AddRegexScrubber(ErrorMsgRegex, string.Empty);
+            var filename = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ?
+                "ProcessStartTests.SubmitsTracesLinux" :
+                RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ?
+                    "ProcessStartTests.SubmitsTracesOsx" :
+                    "ProcessStartTests.SubmitsTraces";
+
+            await VerifyHelper.VerifySpans(spans, settings)
+                              .UseFileName(filename + $".Schema{metadataSchemaVersion.ToUpper()}")
+                              .DisableRequireUniquePrefix();
+
+            VerifyInstrumentation(process.Process);
+
+            telemetry.AssertIntegrationEnabled(IntegrationId.Process);
         }
     }
 }
