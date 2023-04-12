af21e95c2a7b08f69ee10865ea55fce3c8832eb5
Andrew Lock <andrew.lock@datadoghq.com>
2023-04-12 14:34:36 +0100

    Add initial types for collection telemetry about configuration sources

---
+++ tracer/src/Datadog.Trace/Configuration/ConfigurationSources/Telemetry/IConfigurationTelemetry.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IConfigurationTelemetry.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Datadog.Trace.Configuration.Telemetry;
+
+internal interface IConfigurationTelemetry
+{
+    void Record(string key, string? value, bool recordValue, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null);
+
+    void Record(string key, bool value, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null);
+
+    void Record(string key, double value, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null);
+
+    void Record(string key, int value, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null);
+}
---
+++ tracer/src/Datadog.Trace/Configuration/ConfigurationSources/Telemetry/NullConfigurationTelemetry.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="NullConfigurationTelemetry.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+namespace Datadog.Trace.Configuration.Telemetry;
+
+internal class NullConfigurationTelemetry : IConfigurationTelemetry
+{
+    public static readonly NullConfigurationTelemetry Instance = new();
+
+    public void Record(string key, string? value, bool recordValue, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null)
+    {
+    }
+
+    public void Record(string key, bool value, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null)
+    {
+    }
+
+    public void Record(string key, double value, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null)
+    {
+    }
+
+    public void Record(string key, int value, ConfigurationOrigins origin, ConfigurationTelemetryErrorCode? error = null)
+    {
+    }
+}
