2facbb27bd95d7cf30bf4df03aa51985ea52fee3
Flavien Darche <flavien.darche@datadoghq.com>
2023-04-13 18:19:20 +0200

    Move nodes into AST dir + rename

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/GraphQL/HotChocolate/ASM/AST/ItemsNode.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/GraphQL/HotChocolate/ASM/AST/ItemsNode.cs
@@ -1,18 +1,19 @@
-// <copyright file="NamedNodeProxy.cs" company="Datadog">
+// <copyright file="ItemsNode.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System.Collections.Generic;
 using Datadog.Trace.DuckTyping;
 
-namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.GraphQL.HotChocolate.ASM
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.GraphQL.HotChocolate.ASM.AST
 {
     /// <summary>
-    /// Node with Name attribute for ducktyping
+    /// ItemsNodeProxy that contains Items list struct for ducktyping
     /// </summary>
     [DuckCopy]
-    internal struct NamedNodeProxy
+    internal struct ItemsNode
     {
-        public NameNodeProxy Name;
+        public IEnumerable<object> Items;
     }
 }
