ecdd09b68fdc8fd13ff12224d407663c11ae0980
Flavien Darche <flavien.darche@datadoghq.com>
2023-04-19 15:21:22 +0200

    Add 'createHumans' test function to GraphQL Sample

--- tracer/test/test-applications/integrations/Samples.GraphQL/StarWarsExtensions/StarWarsMutation.cs
+++ tracer/test/test-applications/integrations/Samples.GraphQL/StarWarsExtensions/StarWarsMutation.cs
@@ -1,21 +1,22 @@
 using System.Collections.Generic;
 using System.Linq;
-using GraphQL;
+using GraphQL.StarWars;
+using GraphQL.StarWars.Types;
 using GraphQL.Types;
-using Samples.GraphQL3.StarWars.Types;
 
-namespace Samples.GraphQL3.StarWars
+namespace Samples.GraphQL.StarWarsExtensions
 {
     public class StarWarsMutation : ObjectGraphType<object>
     {
         public StarWarsMutation(StarWarsData data)
         {
             Name = "Mutation";
+            
             var queryArgumentArray = new QueryArgument[1];
             var queryArgument = new QueryArgument<NonNullGraphType<HumanInputType>> { Name = "human" };
             queryArgumentArray[0] = queryArgument;
             Field<HumanType>("createHuman", null, new QueryArguments(queryArgumentArray), context => data.AddHuman(context.GetArgument<Human>("human")));
-            
+
             Field<ListGraphType<HumanType>>(
                 "createHumans",
                 arguments: new QueryArguments(
@@ -23,7 +24,7 @@ namespace Samples.GraphQL3.StarWars
                 ),
                 resolve: context =>
                 {
-                    List<string> names = context.GetArgument<List<string>>("names");
+                    var names = context.GetArgument<List<string>>("names");
                     return names.Select(name =>
                     {
                         var human = new Human();
