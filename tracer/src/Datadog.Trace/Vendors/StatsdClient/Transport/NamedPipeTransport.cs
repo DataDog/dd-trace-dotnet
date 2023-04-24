55a7de9208c86e821fc8b0fbf047313f61c85e9f
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-04-24 15:10:40 +0200

    Use Monitor in statsd

--- tracer/src/Datadog.Trace/Vendors/StatsdClient/Transport/NamedPipeTransport.cs
+++ tracer/src/Datadog.Trace/Vendors/StatsdClient/Transport/NamedPipeTransport.cs
@@ -14,14 +14,9 @@ namespace Datadog.Trace.Vendors.StatsdClient.Transport
     {
         private readonly NamedPipeClientStream _namedPipe;
         private readonly TimeSpan _timeout;
-        private byte[] _internalbuffer = new byte[0];
+        private readonly object _lock = new object();
 
-        // `SpinLock` is a struct. A struct marked as `readonly` is copied each time a mutating function is called.
-        // When calling `_lock.Enter` and `_lock.Exit()` the `SpinLock` instance is copied. Calling `_lock.Exit()` raises an
-        // error as the instance does not hold the lock (System.Threading.SynchronizationLockException : The calling
-        // thread does not hold the lock.)
-        // For this reason, `_lock` is not marked as `readonly`
-        private SpinLock _lock = new SpinLock(enableThreadOwnerTracking: true);
+        private byte[] _internalbuffer = Array.Empty<byte>();
 
         public NamedPipeTransport(string pipeName, TimeSpan? timeout = null)
         {
@@ -35,10 +30,8 @@ namespace Datadog.Trace.Vendors.StatsdClient.Transport
 
         public bool Send(byte[] buffer, int length)
         {
-            var gotLock = false;
-            try
+            lock (_lock)
             {
-                _lock.Enter(ref gotLock);
                 if (_internalbuffer.Length < length + 1)
                 {
                     _internalbuffer = new byte[length + 1];
@@ -49,13 +42,6 @@ namespace Datadog.Trace.Vendors.StatsdClient.Transport
                 _internalbuffer[length] = (byte)'\n';
 
                 return SendBuffer(_internalbuffer, length + 1, allowRetry: true);
-            }
-            finally
-            {
-                if (gotLock)
-                {
-                    _lock.Exit();
-                }
             }
         }
 
