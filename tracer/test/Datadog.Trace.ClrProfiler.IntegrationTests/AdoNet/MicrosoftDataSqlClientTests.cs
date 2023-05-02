a2e347c030e0e5642f21d1825ce054e4aaf6993a
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-05-02 17:14:09 -0400

    Enable OTEL on a few more database tests

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MicrosoftDataSqlClientTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MicrosoftDataSqlClientTests.cs
@@ -20,6 +20,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
             : base("Microsoft.Data.SqlClient", output)
         {
             SetServiceVersion("1.0.0");
+            SetEnvironmentVariable("DD_TRACE_OTEL_ENABLED", "true");
         }
 
         public static IEnumerable<object[]> GetEnabledConfig()
