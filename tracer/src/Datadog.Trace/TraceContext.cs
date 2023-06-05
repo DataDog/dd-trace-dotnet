9f7a440269ee9df83cf67cacb60a94a77b77b335
Ignacio Echevarria <ignacio.echevarria@datadoghq.com>
2023-06-05 16:55:48 +0200

    Remove unnecessary null check

--- tracer/src/Datadog.Trace/TraceContext.cs
+++ tracer/src/Datadog.Trace/TraceContext.cs
@@ -138,10 +138,7 @@ namespace Datadog.Trace
                     }
                     else
                     {
-                        if (_iastRequestContext is null)
-                        {
-                            IastRequestContext.AddIastDisabledFlagToSpan(span);
-                        }
+                        IastRequestContext.AddIastDisabledFlagToSpan(span);
                     }
                 }
             }
