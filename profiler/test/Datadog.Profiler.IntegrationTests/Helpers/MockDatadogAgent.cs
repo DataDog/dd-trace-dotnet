0c34022d8ae21a4c3f1e529fd23ec5aed83357db
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-06-06 18:39:32 +0200

    Use the Tracer MockHttpParser

--- profiler/test/Datadog.Profiler.IntegrationTests/Helpers/MockDatadogAgent.cs
+++ profiler/test/Datadog.Profiler.IntegrationTests/Helpers/MockDatadogAgent.cs
@@ -234,11 +234,12 @@ namespace Datadog.Profiler.IntegrationTests
             {
                 Interlocked.Increment(ref _nbTime);
 
-                var stream = new MemoryStream();
-                await ss.CopyToAsync(stream, cancellationToken);
-
-                await ss.WriteAsync(_responseBytes, cancellationToken);
-                NbCallsOnProfilingEndpoint++;
+                while (ss.IsConnected)
+                {
+                    _ = MockHttpParser.ReadRequest(ss);
+                    await ss.WriteAsync(_responseBytes, cancellationToken);
+                    NbCallsOnProfilingEndpoint++;
+                }
             }
         }
 
