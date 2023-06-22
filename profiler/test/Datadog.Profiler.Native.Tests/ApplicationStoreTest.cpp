f0ca29fd85934f46df81559c5b50a3fa41e47fbd
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2023-06-22 17:18:09 +0200

    Add more unit tests + fix

--- profiler/test/Datadog.Profiler.Native.Tests/ApplicationStoreTest.cpp
+++ profiler/test/Datadog.Profiler.Native.Tests/ApplicationStoreTest.cpp
@@ -1,11 +1,11 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
 
-#include "ApplicationInfo.h"
 #include "ApplicationStore.h"
+#include "ApplicationInfo.h"
 
-#include "gtest/gtest.h"
 #include "ProfilerMockedInterface.h"
+#include "gtest/gtest.h"
 
 using ::testing::ReturnRef;
 
@@ -36,9 +36,48 @@ TEST(ApplicationStoreTest, GetDefaultName)
     ASSERT_EQ(info.CommitSha, expectedGitCommitSha);
 }
 
-TEST(ApplicationStoreTest, SetName)
+TEST(ApplicationStoreTest, CheckGitMetadataIfSetGitMetadataIsNotCalled)
 {
     auto [configuration, mockConfiguration] = CreateConfiguration();
+    std::string expectedGitRepositoryUrl = "ExpectedGitRepositoryUrl";
+    std::string expectedGitCommitSha = "ExpectedGitCommitSha";
+    EXPECT_CALL(mockConfiguration, GetGitRepositoryUrl()).WillRepeatedly(ReturnRef(expectedGitRepositoryUrl));
+    EXPECT_CALL(mockConfiguration, GetGitCommitSha()).WillRepeatedly(ReturnRef(expectedGitCommitSha));
+
+    ApplicationStore applicationStore(configuration.get());
+
+    const auto runtimeId = "{82F18E9B-138D-4202-8D21-7BE1AF82EC8B}";
+
+    const auto expectedApplicationInfo = ApplicationInfo{
+        "ExpectedServiceName",
+        "ExpectedEnvironment",
+        "ExpectedVersion",
+        expectedGitRepositoryUrl,
+        expectedGitCommitSha};
+
+    applicationStore.SetApplicationInfo(
+        runtimeId,
+        expectedApplicationInfo.ServiceName,
+        expectedApplicationInfo.Environment,
+        expectedApplicationInfo.Version);
+
+    auto const& info = applicationStore.GetApplicationInfo(runtimeId);
+
+    ASSERT_EQ(info.Environment, expectedApplicationInfo.Environment);
+    ASSERT_EQ(info.ServiceName, expectedApplicationInfo.ServiceName);
+    ASSERT_EQ(info.Version, expectedApplicationInfo.Version);
+    ASSERT_EQ(info.RepositoryUrl, expectedApplicationInfo.RepositoryUrl);
+    ASSERT_EQ(info.CommitSha, expectedApplicationInfo.CommitSha);
+}
+
+TEST(ApplicationStoreTest, MakeSureCallToSetGitMetadataOverrideThePreviousValue)
+{
+    auto [configuration, mockConfiguration] = CreateConfiguration();
+
+    std::string randomRepoUrl = "RandomRepoUrl";
+    std::string randomCommitSha = "RandomCommitSha";
+    EXPECT_CALL(mockConfiguration, GetGitRepositoryUrl()).WillRepeatedly(ReturnRef(randomRepoUrl));
+    EXPECT_CALL(mockConfiguration, GetGitCommitSha()).WillRepeatedly(ReturnRef(randomCommitSha));
 
     ApplicationStore applicationStore(configuration.get());
 
@@ -58,6 +97,16 @@ TEST(ApplicationStoreTest, SetName)
         expectedApplicationInfo.ServiceName,
         expectedApplicationInfo.Environment,
         expectedApplicationInfo.Version);
+
+    {
+        auto const& info = applicationStore.GetApplicationInfo(runtimeId);
+
+        ASSERT_EQ(info.Environment, expectedApplicationInfo.Environment);
+        ASSERT_EQ(info.ServiceName, expectedApplicationInfo.ServiceName);
+        ASSERT_EQ(info.Version, expectedApplicationInfo.Version);
+        ASSERT_EQ(info.RepositoryUrl, randomRepoUrl);
+        ASSERT_EQ(info.CommitSha, randomCommitSha);
+    }
 
     applicationStore.SetGitMetadata(
         runtimeId,
