b52a04721f4d3152ff641efe6a86d330a9fc3fa4
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-06-22 11:58:01 +0200

    Always push telemetry on exit

--- tracer/src/Datadog.Trace/Telemetry/TelemetryControllerV2.cs
+++ tracer/src/Datadog.Trace/Telemetry/TelemetryControllerV2.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TelemetryControllerV2.cs" company="Datadog">
+// <copyright file="TelemetryControllerV2.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -186,10 +186,8 @@ internal class TelemetryControllerV2 : ITelemetryController
             {
                 Log.Debug("Process exit requested, ending telemetry loop");
                 var sendAppClosingTelemetry = _processExit.Task.Result;
-                if (sendAppClosingTelemetry)
-                {
-                    await PushTelemetry(isFinalPush: true).ConfigureAwait(false);
-                }
+
+                await PushTelemetry(isFinalPush: sendAppClosingTelemetry).ConfigureAwait(false);
 
                 return;
             }
