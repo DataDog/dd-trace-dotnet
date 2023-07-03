780e1648003067a1436c0ed47315311f95dba08c
Anna <anna.yafi@datadoghq.com>
2023-07-03 14:33:47 +0200

    Fix accidental indent change

--- tracer/src/Datadog.Trace/AppSec/Coordinator/SecurityCoordinatorHelpers.Core.cs
+++ tracer/src/Datadog.Trace/AppSec/Coordinator/SecurityCoordinatorHelpers.Core.cs
@@ -43,7 +43,11 @@ internal static class SecurityCoordinatorHelpers
                 if (!transport.IsBlocked)
                 {
                     var securityCoordinator = new SecurityCoordinator(security, httpContext, span, transport);
-                    var args = new Dictionary<string, object> { { AddressesConstants.ResponseHeaderNoCookies, SecurityCoordinator.ExtractHeadersFromRequest(headers) }, { AddressesConstants.ResponseStatus, httpContext.Response.StatusCode.ToString() }, };
+                    var args = new Dictionary<string, object>
+                    {
+                        { AddressesConstants.ResponseHeaderNoCookies, SecurityCoordinator.ExtractHeadersFromRequest(headers) },
+                        { AddressesConstants.ResponseStatus, httpContext.Response.StatusCode.ToString() },
+                    };
                     var result = securityCoordinator.RunWaf(args);
                     securityCoordinator.CheckAndBlock(result);
                 }
