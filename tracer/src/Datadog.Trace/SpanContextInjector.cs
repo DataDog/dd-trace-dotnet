81b138d172f18ac4808fef99801a7b8a400f74af
Pierre Bonet <pierre.bonet@datadoghq.com>
2023-07-12 14:08:11 +0200

    remove interface and fix snapshot test

--- tracer/src/Datadog.Trace/SpanContextInjector.cs
+++ tracer/src/Datadog.Trace/SpanContextInjector.cs
@@ -17,10 +17,21 @@ using Datadog.Trace.Telemetry.Metrics;
 
 namespace Datadog.Trace
 {
-    /// <inheritdoc />
-    public class SpanContextInjector : ISpanContextInjector
+    /// <summary>
+    /// The SpanContextInjector is responsible for injecting SpanContext in the rare cases where the Tracer couldn't propagate it itself.
+    /// This can happen for instance when we don't support a specific library
+    /// </summary>
+    public class SpanContextInjector
     {
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a SpanContext carrier and a function to set a value, this method will inject a SpanContext.
+        /// You should only call <see cref="Inject{TCarrier}"/> once on the message <paramref name="carrier"/>. Calling
+        /// multiple times may lead to incorrect behaviors.
+        /// </summary>
+        /// <param name="carrier">The carrier of the SpanContext. Often a header (http, kafka message header...)</param>
+        /// <param name="setter">Given a key name and value, sets the value in the carrier</param>
+        /// <param name="context">The context you want to inject</param>
+        /// <typeparam name="TCarrier">Type of the carrier</typeparam>
         [PublicApi]
         public void Inject<TCarrier>(TCarrier carrier, Action<TCarrier, string, string> setter, ISpanContext context)
         {
