56588abde1fe57cb884bdc839a7d1c31c789f148
Maximo Bautista <max.bautista97@gmail.com>
2023-07-12 10:21:30 -0400

    Updated parameters to be expected types

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MongoDb/BsonExtensions.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MongoDb/BsonExtensions.cs
@@ -11,7 +11,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
 {
     internal static class BsonExtensions
     {
-        public static string ToShortString(this object obj, object args = default)
+        public static string ToShortString(this object obj)
         {
             Type nominalType = obj.GetType();
 
@@ -20,47 +20,35 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
                 throw new ArgumentNullException("nominalType");
             }
 
-            PropertyInfo nominalTypeProperty = args?.GetType().GetProperty("NominalType");
-
-            nominalTypeProperty?.SetValue(args, nominalType);
+            var bsonSerializationArgsType = Type.GetType("MongoDB.Bson.Serialization.BsonSerializationArgs, MongoDB.Bson", false);
+            object args = Activator.CreateInstance(bsonSerializationArgsType);
+            bsonSerializationArgsType.GetProperty("NominalType")?.SetValue(args, nominalType);
 
             Type bsonSerializerType = Type.GetType("MongoDB.Bson.Serialization.BsonSerializer, MongoDB.Bson");
             MethodInfo lookupSerializerMethod = bsonSerializerType?.GetMethod("LookupSerializer", new Type[] { typeof(Type) });
 
-            Type bsonDocumentType = Type.GetType("MongoDB.Bson.BsonDocument, MongoDB.Bson");
-
-            object serializer = null;
-
-            if (serializer == null)
-            {
-                serializer = lookupSerializerMethod.Invoke(null, new[] { bsonDocumentType });
-            }
+            object serializer = lookupSerializerMethod.Invoke(null, new[] { nominalType });
 
             using (var stringWriter = new StringWriter())
             {
-                object writerSettings = null;
-
                 Type typeJsonWriterSettings = Type.GetType("MongoDB.Bson.IO.JsonWriterSettings, MongoDB.Bson", throwOnError: false);
-
                 Type[] types = { typeof(StringWriter), typeJsonWriterSettings };
 
                 Type typeJsonWriter = Type.GetType("MongoDB.Bson.IO.JsonWriter, MongoDB.Bson", throwOnError: false);
                 ConstructorInfo constructorJsonWriter = typeJsonWriter?.GetConstructor(types);
+                var defaultsValueJsonWriterSettings = new object[] { (TextWriter)stringWriter, typeJsonWriterSettings?.GetProperty("Defaults")?.GetValue(null) };
 
-                var defaultsValueJsonWriterSettings = new[] { (TextWriter)stringWriter, typeJsonWriterSettings?.GetProperty("Defaults")?.GetValue(writerSettings) };
                 var bsonWriter = constructorJsonWriter?.Invoke(defaultsValueJsonWriterSettings);
 
-                Type contextType = Type.GetType("MongoDB.Bson.BsonSerializationContext, MongoDB.Bson", throwOnError: false);
+                Type contextType = Type.GetType("MongoDB.Bson.Serialization.BsonSerializationContext, MongoDB.Bson", throwOnError: false);
                 MethodInfo createRootMethod = contextType?.GetMethod("CreateRoot", BindingFlags.Static | BindingFlags.Public);
 
-                object configurator = null;
-                var context = createRootMethod?.Invoke(null, new[] { bsonWriter, configurator });
+                var rootContext = createRootMethod?.Invoke(null, new[] { bsonWriter, null });
 
                 Type bsonSerializerInterfaceType = Type.GetType("MongoDB.Bson.Serialization.IBsonSerializer, MongoDB.Bson", throwOnError: false);
                 MethodInfo serializeMethod = bsonSerializerInterfaceType?.GetMethod("Serialize");
 
-                object castedObj = Convert.ChangeType(obj, bsonDocumentType);
-                serializeMethod?.Invoke(serializer, new[] { context, args, castedObj });
+                serializeMethod?.Invoke(serializer, new[] { rootContext, args, obj });
 
                 return stringWriter.ToString();
             }
