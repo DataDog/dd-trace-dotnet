134b0b1c434e9e128fb3c18b2ef664e480b1bdb4
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-07-14 11:45:13 -0400

    remove `PeerService` from `AwsSdkTags.cs`
    this is going to be applied per instrumentation

--- tracer/src/Datadog.Trace/Tagging/AwsSdkTags.cs
+++ tracer/src/Datadog.Trace/Tagging/AwsSdkTags.cs
@@ -35,8 +35,5 @@ namespace Datadog.Trace.Tagging
 
         [Tag(Trace.Tags.HttpStatusCode)]
         public string HttpStatusCode { get; set; }
-
-        [Tag(Trace.Tags.PeerService)]
-        public string PeerService { get; set; }
     }
 }
