d436104f9081135c110dd91fca664f9d9494925f
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2023-07-19 14:43:31 +0200

    Add allocations profiler timeit-based tests

---
+++ profiler/build/run-allocations.cmd
@@ -0,0 +1,14 @@
+@echo on
+
+dotnet tool update -g timeitsharp --version 0.0.9
+
+:: remove DOTNET_ROOT environment variable to ensure we can run
+:: the benchmark in x64 and x86
+set DOTNET_ROOT=
+
+:: Run x64
+dotnet timeit Allocations.windows.x64.json
+
+:: Run x86
+dotnet timeit Allocations.windows.x86.json
+
