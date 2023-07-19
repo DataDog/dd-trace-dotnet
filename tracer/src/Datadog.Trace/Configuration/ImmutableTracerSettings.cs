7a534afedf1d3b9c4b1d33f6b581da4114802124
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-07-19 17:15:45 -0400

    Revert "Remove PropagationStyle from ImmutableTracerSettings - not needed"
    This reverts commit e272a28255f180dc5007a87731d9e3f10b61226f.
    We are testing that ImmutableTracerSettings has the same properties as
    TracerSettings and I'm unsure whether to ignore this one or not, so
    adding it back in.

--- tracer/src/Datadog.Trace/Configuration/ImmutableTracerSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/ImmutableTracerSettings.cs
@@ -121,6 +121,7 @@ namespace Datadog.Trace.Configuration
             RouteTemplateResourceNamesEnabled = settings.RouteTemplateResourceNamesEnabled;
             DelayWcfInstrumentationEnabled = settings.DelayWcfInstrumentationEnabled;
             WcfObfuscationEnabled = settings.WcfObfuscationEnabled;
+            PropagationStyle = settings.PropagationStyle;
             PropagationStyleInject = settings.PropagationStyleInject;
             PropagationStyleExtract = settings.PropagationStyleExtract;
             TraceMethods = settings.TraceMethods;
@@ -441,6 +442,11 @@ namespace Datadog.Trace.Configuration
         /// into the <c>resourceName</c> of a span.
         /// </summary>
         internal bool WcfObfuscationEnabled { get; }
+
+        /// <summary>
+        /// Gets a value indicating the injection and extraction propagation styles.
+        /// </summary>
+        internal string[] PropagationStyle { get; }
 
         /// <summary>
         /// Gets a value indicating the injection propagation style.
