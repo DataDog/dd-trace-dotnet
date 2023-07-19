b3475a6ddd6631161cbfca62b6065d4b9b8df101
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-07-19 15:16:23 -0400

    Default to `Activity.OperationName` for `Span.OperationName` (#4409)
    * Default to Activity.OperationName for Span.OperationName
    
    * Update snapshots
    
    * Add feature flag for legacy operation name
    
    * Use is not instead of !=
    
    * Clarify OperationName feature flag requirements
    
    * Add missing snapshot

--- tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
+++ tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -604,6 +604,16 @@ namespace Datadog.Trace.Configuration
             public const string OpenTelemetryEnabled = "DD_TRACE_OTEL_ENABLED";
 
             /// <summary>
+            /// Enables the use of the <see cref="ISpan.OperationName"/> being set to the legacy value.
+            /// This flag defaults to <see langword="false"/> and is intended to allow beta users of OpenTelemetry support
+            /// to toggle on to give them time to upgrade to the new format. This additionally requires that
+            /// the <c>ActivitySource</c> has a <c>Name</c> property which was introduced in .NET 5 and/or v5 of
+            /// <c>System.Diagnostics</c> library.
+            /// Note: This feature flag may be dropped when our OpenTelemetry support becomes generally available.
+            /// </summary>
+            public const string OpenTelemetryLegacyOperationNameEnabled = "DD_TRACE_OTEL_LEGACY_OPERATION_NAME_ENABLED";
+
+            /// <summary>
             /// Enables generating 128-bit trace ids instead of 64-bit trace ids.
             /// Note that a 128-bit trace id may be received from an upstream service or from
             /// an Activity even if we are not generating them ourselves.
