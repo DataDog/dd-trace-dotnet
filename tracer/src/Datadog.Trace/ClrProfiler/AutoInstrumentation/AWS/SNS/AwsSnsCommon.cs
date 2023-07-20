d4bacf0eb2ce0ae9b630dd9d8de6dd3e192f9541
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-07-20 17:16:00 -0400

    update `AwsSnsCommons.cs` to use new methods to get right tags

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/SNS/AwsSnsCommon.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/SNS/AwsSnsCommon.cs
@@ -13,14 +13,15 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.SNS
     internal static class AwsSnsCommon
     {
         private const string DatadogAwsSnsServiceName = "aws-sns";
-        private const string SnsOperationName = "sns.request";
+        private const string SnsRequestOperationName = "sns.request";
         private const string SnsServiceName = "SNS";
+        private const string SnsOperationName = "aws.sqs";
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(AwsSnsCommon));
 
         internal const string IntegrationName = nameof(Configuration.IntegrationId.AwsSns);
         internal const IntegrationId IntegrationId = Configuration.IntegrationId.AwsSns;
 
-        public static Scope CreateScope(Tracer tracer, string operation, out AwsSnsTags tags, ISpanContext parentContext = null)
+        public static Scope CreateScope(Tracer tracer, string operation, string spanKind, out AwsSnsTags tags, ISpanContext parentContext = null)
         {
             tags = null;
 
@@ -34,9 +35,10 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.SNS
 
             try
             {
-                tags = new AwsSnsTags();
+                tags = tracer.CurrentTraceSettings.Schema.Messaging.CreateAwsSnsTags(spanKind);
                 string serviceName = tracer.CurrentTraceSettings.GetServiceName(tracer, DatadogAwsSnsServiceName);
-                scope = tracer.StartActiveInternal(SnsOperationName, parent: parentContext, tags: tags, serviceName: serviceName);
+                string operationName = GetOperationName(tracer, spanKind);
+                scope = tracer.StartActiveInternal(operationName, parent: parentContext, tags: tags, serviceName: serviceName);
                 var span = scope.Span;
 
                 span.Type = SpanTypes.Http;
@@ -55,6 +57,32 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.SNS
             // always returns the scope, even if it's null because we couldn't create it,
             // or we couldn't populate it completely (some tags is better than no tags)
             return scope;
+        }
+
+        public static string GetTopicName(string topicArn)
+        {
+            if (topicArn is null)
+            {
+                return topicArn;
+            }
+
+            var lastSeparationIndex = topicArn.LastIndexOf(':') + 1;
+            return topicArn.Substring(lastSeparationIndex);
+        }
+
+        internal static string GetOperationName(Tracer tracer, string spanKind)
+        {
+            if (tracer.CurrentTraceSettings.Schema.Version == SchemaVersion.V0)
+            {
+                return SnsRequestOperationName;
+            }
+
+            return spanKind switch
+            {
+                SpanKinds.Consumer => tracer.CurrentTraceSettings.Schema.Messaging.GetInboundOperationName(SnsOperationName),
+                SpanKinds.Producer => tracer.CurrentTraceSettings.Schema.Messaging.GetOutboundOperationName(SnsOperationName),
+                _ => $"{SnsOperationName}.request"
+            };
         }
     }
 }
