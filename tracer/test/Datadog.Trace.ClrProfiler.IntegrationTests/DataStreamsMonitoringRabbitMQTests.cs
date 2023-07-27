68e16c0cea866e778cec6b24a2603d5fe289de89
Hoki Tam <hoki.tam@datadoghq.com>
2023-07-27 16:29:35 -0400

    change the ordering

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DataStreamsMonitoringRabbitMQTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DataStreamsMonitoringRabbitMQTests.cs
@@ -93,7 +93,7 @@ public class DataStreamsMonitoringRabbitMQTests : TestHelper
             }
         }
 
-        points.OrderBy(s => s.TimestampType).ThenBy(s => s.Hash);
+        points.OrderBy(s => s.Hash).ThenBy(s => s.TimestampType);
         return points.ToList();
     }
 }
