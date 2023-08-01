599feb5df7f810fd3fdb68750ad531549ae9bb0b
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-07-31 21:56:18 -0400

    add tags to aws `sdk` integration

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/SDK/RuntimePipelineInvokeAsyncIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/SDK/RuntimePipelineInvokeAsyncIntegration.cs
@@ -48,7 +48,9 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.SDK
             var scope = Tracer.Instance.InternalActiveScope;
             if (scope?.Span.Tags is AwsSdkTags tags)
             {
-                tags.Region = executionContext.RequestContext.ClientConfig.RegionEndpoint?.SystemName;
+                var region = executionContext.RequestContext.ClientConfig.RegionEndpoint?.SystemName;
+                tags.Region = region;
+                tags.AwsRegion = region;
             }
 
             return new CallTargetState(scope, state: executionContext);
