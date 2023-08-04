921d7bdd836ab21469bcbe952f5fc39b6c4f7eb8
Tony Redondo <tony.redondo@datadoghq.com>
2023-08-04 12:58:50 +0200

    bump version.

--- tracer/build/timeit/Samples.FakeDbCommand/run.cmd
+++ tracer/build/timeit/Samples.FakeDbCommand/run.cmd
@@ -6,7 +6,7 @@ IF EXIST results_Samples.FakeDbCommand.windows.net60.json DEL /F results_Samples
 echo *********************
 echo Installing timeitsharp
 echo *********************
-dotnet tool update -g timeitsharp --version 0.0.14
+dotnet tool update -g timeitsharp --version 0.0.15
 
 echo *********************
 echo .NET Framework 4.6.1
