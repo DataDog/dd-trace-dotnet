cfb878186272c2a0ef9efc702277fdcc613c8002
Andrew Lock <andrew.lock@datadoghq.com>
2023-08-15 15:25:28 +0100

    Remove unused JSON extensions

--- tracer/test/Datadog.Trace.TestHelpers/FluentAssertionsExtensions/Json/StringAssertionsExtensions.cs
+++ tracer/test/Datadog.Trace.TestHelpers/FluentAssertionsExtensions/Json/StringAssertionsExtensions.cs
@@ -14,26 +14,6 @@ namespace Datadog.Trace.TestHelpers.FluentAssertionsExtensions.Json;
 
 internal static class StringAssertionsExtensions
 {
-    [CustomAssertionAttribute]
-    public static AndWhichConstraint<StringAssertions, JToken> BeValidJson(this StringAssertions stringAssertions, string because = "", params object[] becauseArgs)
-    {
-        JToken json = null;
-
-        try
-        {
-            json = JToken.Parse(stringAssertions.Subject);
-        }
-#pragma warning disable CA1031 // Ignore catching general exception
-        catch (Exception ex)
-#pragma warning restore CA1031 // Ignore catching general exception
-        {
-            Execute.Assertion.BecauseOf(because, becauseArgs)
-                .FailWith("Expected {context:string} to be valid JSON{reason}, but parsing failed with {0}.", ex.Message);
-        }
-
-        return new AndWhichConstraint<StringAssertions, JToken>(stringAssertions, json);
-    }
-
     public static AndWhichConstraint<StringAssertions, string> BeJsonEquivalentTo(this StringAssertions stringAssertions, string json, Func<string, string, bool> filterProperty = null, string because = "", params object[] becauseArgs)
     {
         try
