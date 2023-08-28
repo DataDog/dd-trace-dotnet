31678334a488ff2adb80ade0ae6f53086d13b050
Robert Pickering <robert@strangelights.com>
2023-08-28 15:23:32 +0200

    Remove some native interop functions that are no longer needed

--- tracer/src/Datadog.Trace/AppSec/Waf/NativeBindings/WafLibraryInvoker.cs
+++ tracer/src/Datadog.Trace/AppSec/Waf/NativeBindings/WafLibraryInvoker.cs
@@ -26,18 +26,7 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
         private readonly RunDelegate _runField;
         private readonly DestroyDelegate _destroyField;
         private readonly ContextDestroyDelegate _contextDestroyField;
-        private readonly ObjectInvalidDelegate _objectInvalidField;
-        private readonly ObjectStringLengthDelegate _objectStringLengthField;
-        private readonly ObjectBoolDelegate _objectBoolField;
-        private readonly ObjectArrayDelegate _objectArrayField;
-        private readonly ObjectMapDelegate _objectMapField;
-        private readonly ObjectArrayAddDelegate _objectArrayAddField;
-        private readonly ObjectArrayGetAtIndexDelegate _objectArrayGetIndex;
-        private readonly ObjectMapAddDelegateX64 _objectMapAddFieldX64;
-        private readonly ObjectMapAddDelegateX86 _objectMapAddFieldX86;
         private readonly FreeResultDelegate _freeResultField;
-        private readonly FreeObjectDelegate _freeObjectield;
-        private readonly IntPtr _freeObjectFuncField;
         private readonly FreeRulesetInfoDelegate _rulesetInfoFreeField;
         private readonly SetupLoggingDelegate _setupLogging;
         private readonly SetupLogCallbackDelegate _setupLogCallbackField;
@@ -53,18 +42,6 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
             _destroyField = GetDelegateForNativeFunction<DestroyDelegate>(libraryHandle, "ddwaf_destroy");
             _updateField = GetDelegateForNativeFunction<UpdateDelegate>(libraryHandle, "ddwaf_update");
             _contextDestroyField = GetDelegateForNativeFunction<ContextDestroyDelegate>(libraryHandle, "ddwaf_context_destroy");
-            _objectInvalidField = GetDelegateForNativeFunction<ObjectInvalidDelegate>(libraryHandle, "ddwaf_object_invalid");
-            _objectStringLengthField = GetDelegateForNativeFunction<ObjectStringLengthDelegate>(libraryHandle, "ddwaf_object_stringl");
-            _objectBoolField = GetDelegateForNativeFunction<ObjectBoolDelegate>(libraryHandle, "ddwaf_object_bool");
-            _objectArrayField = GetDelegateForNativeFunction<ObjectArrayDelegate>(libraryHandle, "ddwaf_object_array");
-            _objectMapField = GetDelegateForNativeFunction<ObjectMapDelegate>(libraryHandle, "ddwaf_object_map");
-            _objectArrayAddField = GetDelegateForNativeFunction<ObjectArrayAddDelegate>(libraryHandle, "ddwaf_object_array_add");
-            _objectArrayGetIndex = GetDelegateForNativeFunction<ObjectArrayGetAtIndexDelegate>(libraryHandle, "ddwaf_object_get_index");
-            _objectMapAddFieldX64 =
-                Environment.Is64BitProcess ? GetDelegateForNativeFunction<ObjectMapAddDelegateX64>(libraryHandle, "ddwaf_object_map_addl") : null;
-            _objectMapAddFieldX86 =
-                Environment.Is64BitProcess ? null : GetDelegateForNativeFunction<ObjectMapAddDelegateX86>(libraryHandle, "ddwaf_object_map_addl");
-            _freeObjectield = GetDelegateForNativeFunction<FreeObjectDelegate>(libraryHandle, "ddwaf_object_free", out _freeObjectFuncField);
             _freeResultField = GetDelegateForNativeFunction<FreeResultDelegate>(libraryHandle, "ddwaf_result_free");
             _rulesetInfoFreeField = GetDelegateForNativeFunction<FreeRulesetInfoDelegate>(libraryHandle, "ddwaf_ruleset_info_free");
             _getVersionField = GetDelegateForNativeFunction<GetVersionDelegate>(libraryHandle, "ddwaf_get_version");
@@ -94,26 +71,6 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
 
         private delegate void ContextDestroyDelegate(IntPtr context);
 
-        private delegate IntPtr ObjectInvalidDelegate(IntPtr emptyObjPtr);
-
-        private delegate IntPtr ObjectStringLengthDelegate(IntPtr emptyObjPtr, string s, ulong length);
-
-        private delegate IntPtr ObjectBoolDelegate(IntPtr emptyObjPtr, bool b);
-
-        private delegate IntPtr ObjectArrayDelegate(IntPtr emptyObjPtr);
-
-        private delegate IntPtr ObjectMapDelegate(IntPtr emptyObjPtr);
-
-        private delegate bool ObjectArrayAddDelegate(IntPtr array, IntPtr entry);
-
-        private delegate IntPtr ObjectArrayGetAtIndexDelegate(IntPtr array, long index);
-
-        private delegate bool ObjectMapAddDelegateX64(IntPtr map, string entryName, ulong entryNameLength, IntPtr entry);
-
-        private delegate bool ObjectMapAddDelegateX86(IntPtr map, string entryName, uint entryNameLength, IntPtr entry);
-
-        private delegate void FreeObjectDelegate(IntPtr input);
-
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void SetupLogCallbackDelegate(
             DDWAF_LOG_LEVEL level,
@@ -136,8 +93,6 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
         }
 
         internal bool ExportErrorHappened { get; private set; }
-
-        internal IntPtr ObjectFreeFuncPtr => _freeObjectFuncField;
 
         /// <summary>
         /// Initializes static members of the <see cref="WafLibraryInvoker"/> class.
@@ -212,50 +167,6 @@ namespace Datadog.Trace.AppSec.Waf.NativeBindings
         internal void Destroy(IntPtr wafHandle) => _destroyField(wafHandle);
 
         internal void ContextDestroy(IntPtr handle) => _contextDestroyField(handle);
-
-        internal IntPtr ObjectArrayGetIndex(IntPtr array, long index) => _objectArrayGetIndex(array, index);
-
-        internal IntPtr ObjectInvalid()
-        {
-            var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(DdwafObjectStruct)));
-            _objectInvalidField(ptr);
-            return ptr;
-        }
-
-        internal IntPtr ObjectStringLength(string s, ulong length)
-        {
-            var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(DdwafObjectStruct)));
-            _objectStringLengthField(ptr, s, length);
-            return ptr;
-        }
-
-        internal IntPtr ObjectBool(bool b)
-        {
-            var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(DdwafObjectStruct)));
-            _objectBoolField(ptr, b);
-            return ptr;
-        }
-
-        internal IntPtr ObjectArray()
-        {
-            var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(DdwafObjectStruct)));
-            _objectArrayField(ptr);
-            return ptr;
-        }
-
-        internal IntPtr ObjectMap()
-        {
-            var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(DdwafObjectStruct)));
-            _objectMapField(ptr);
-            return ptr;
-        }
-
-        internal bool ObjectArrayAdd(IntPtr array, IntPtr entry) => _objectArrayAddField(array, entry);
-
-        // Setting entryNameLength to 0 will result in the entryName length being re-computed with strlen
-        internal bool ObjectMapAdd(IntPtr map, string entryName, ulong entryNameLength, IntPtr entry) => Environment.Is64BitProcess ? _objectMapAddFieldX64!(map, entryName, entryNameLength, entry) : _objectMapAddFieldX86!(map, entryName, (uint)entryNameLength, entry);
-
-        internal void ObjectFreePtr(IntPtr input) => _freeObjectield(input);
 
         internal void ResultFree(ref DdwafResultStruct output) => _freeResultField(ref output);
 
