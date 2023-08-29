c81c929ed543042387424f282f26c970954bd9fa
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2023-08-29 10:13:45 +0200

    Add tests for Wrapper library + pthread_create for alpine

---
+++ profiler/test/Datadog.Linux.ApiWrapper.Tests/WrappedFunctions.cpp
@@ -0,0 +1,42 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
+
+#include "gtest/gtest.h"
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <link.h>
+#include <pthread.h>
+
+#include "shared/src/native-src/dd_filesystem.hpp"
+
+namespace WrappedFunctionsTest {
+class WrappedFunctionsParametersTests : public ::testing::TestWithParam<void*>
+{
+};
+
+TEST_P(WrappedFunctionsParametersTests, CheckIfFirstResolvedByDynamicLoader)
+{
+    auto* fn = GetParam();
+    Dl_info info;
+    ASSERT_NE(0, dladdr(fn, &info));
+    auto filePath = fs::path(info.dli_fname);
+    ASSERT_STREQ("Datadog.Linux.ApiWrapper.x64.so", filePath.filename().c_str());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    WrappedFunctionsTest,
+    WrappedFunctionsParametersTests,
+    ::testing::Values(
+#ifdef DD_ALPINE
+        (void*)::pthread_create,
+        (void*)::pthread_attr_init,
+        (void*)::pthread_getattr_default_np,
+        (void*)::pthread_setattr_default_np,
+        (void*)::fork,
+#endif
+        (void*)::dl_iterate_phdr,
+        (void*)::dlopen,
+        (void*)::dladdr));
+
+} // namespace WrappedFunctionsTest
