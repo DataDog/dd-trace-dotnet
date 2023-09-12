42c59155d64a1979b73e01e19607fc62a79aae0d
Andrew Lock <andrew.lock@datadoghq.com>
2023-09-12 13:22:04 +0100

    Extract base class from MetricsTelemetryCollector

--- tracer/src/Datadog.Trace.SourceGenerators/TelemetryMetric/TelemetryMetricGenerator.cs
+++ tracer/src/Datadog.Trace.SourceGenerators/TelemetryMetric/TelemetryMetricGenerator.cs
@@ -70,6 +70,13 @@ namespace Datadog.Trace.SourceGenerators.TelemetryMetric
                 "distribution" => Sources.CreateDistributionTelemetryCollectorPartial(sb, in enumDetails, enumDictionary, metricsToLocation, entryCounts),
                 _ => $"// Metric type: {enumDetails.MetricType} not currently supported",
             };
+            var baseSource = enumDetails.MetricType switch
+            {
+                "count" => Sources.CreateCountTelemetryCollectorBasePartial(sb, in enumDetails, enumDictionary, metricsToLocation, entryCounts),
+                "gauge" => Sources.CreateGaugeTelemetryCollectorBasePartial(sb, in enumDetails, enumDictionary, metricsToLocation, entryCounts),
+                "distribution" => Sources.CreateDistributionTelemetryCollectorBasePartial(sb, in enumDetails, enumDictionary, metricsToLocation, entryCounts),
+                _ => $"// Metric type: {enumDetails.MetricType} not currently supported",
+            };
             var interfaceSource = enumDetails.MetricType switch
             {
                 "count" => Sources.CreateCountITelemetryCollectorPartial(sb, in enumDetails, enumDictionary),
@@ -86,6 +93,7 @@ namespace Datadog.Trace.SourceGenerators.TelemetryMetric
             };
 
             spc.AddSource($"{enumDetails.ShortName}Extensions.g.cs", SourceText.From(enumSource, Encoding.UTF8));
+            spc.AddSource($"MetricsTelemetryCollectorBase_{enumDetails.MetricType}.g.cs", SourceText.From(baseSource, Encoding.UTF8));
             spc.AddSource($"MetricsTelemetryCollector_{enumDetails.MetricType}.g.cs", SourceText.From(collectorSource, Encoding.UTF8));
             spc.AddSource($"IMetricsTelemetryCollector_{enumDetails.MetricType}.g.cs", SourceText.From(interfaceSource, Encoding.UTF8));
             spc.AddSource($"NullMetricsTelemetryCollector_{enumDetails.MetricType}.g.cs", SourceText.From(nullSource, Encoding.UTF8));
