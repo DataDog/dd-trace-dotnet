93cb0794837e047d772686b87e654b5c5f7373d8
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-09-14 17:59:01 -0400

    add integration testing

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/AwsDynamoDbTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/AwsDynamoDbTests.cs
@@ -46,10 +46,10 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AWS
             using (RunSampleAndWaitForExit(agent, packageVersion: packageVersion))
             {
 #if NETFRAMEWORK
-                var expectedCount = 28;
+                var expectedCount = 48;
                 var frameworkName = "NetFramework";
 #else
-                var expectedCount = 14;
+                var expectedCount = 24;
                 var frameworkName = "NetCore";
 #endif
                 var spans = agent.WaitForSpans(expectedCount);
