7f8e33c0929c7517910832b399c0b4c16306badc
Tony Redondo <tony.redondo@datadoghq.com>
2023-09-20 15:07:45 +0200

    Changes

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/XUnitIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/XUnitIntegration.cs
@@ -123,10 +123,11 @@ internal static class XUnitIntegration
 
     internal static bool ShouldSkip(ref TestRunnerStruct runnerInstance, out bool isUnskippable, out bool isForcedRun, Dictionary<string, List<string>>? traits = null)
     {
+        isUnskippable = false;
+        isForcedRun = false;
+
         if (CIVisibility.Settings.IntelligentTestRunnerEnabled != true)
         {
-            isUnskippable = false;
-            isForcedRun = false;
             return false;
         }
 
