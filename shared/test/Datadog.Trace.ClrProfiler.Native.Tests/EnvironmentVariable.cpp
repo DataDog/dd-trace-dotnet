5bd7033594eb20d0640732ad2e7b205f860fcbba
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2023-09-27 16:36:02 +0200

    Add unit tests

---
+++ shared/test/Datadog.Trace.ClrProfiler.Native.Tests/EnvironmentVariable.cpp
@@ -0,0 +1,14 @@
+#include "EnvironmentVariable.h"
+#include "../../src/native-src/util.h"
+
+EnvironmentVariable::EnvironmentVariable(const ::shared::WSTRING& name, const ::shared::WSTRING& value) :
+    _name{name},
+    _value{value}
+{
+    ::shared::SetEnvironmentValue(_name, _value);
+}
+
+EnvironmentVariable::~EnvironmentVariable()
+{
+    ::shared::UnsetEnvironmentValue(_name);
+}
