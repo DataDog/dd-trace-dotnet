22e4577b8a41d3daf743e20c64757bb157df7f35
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-10-04 09:04:32 -0400

    Add basic ActivityOperationNameMapper and tests

---
+++ tracer/src/Datadog.Trace/Activity/ActivityOperationNameMapper.cs
@@ -0,0 +1,39 @@
+// <copyright file="ActivityOperationNameMapper.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using Datadog.Trace.Activity.DuckTypes;
+
+namespace Datadog.Trace.Activity
+{
+    /// <summary>
+    /// Helper class to map <see cref="SpanKinds"/> and various tags on an Activity to a <see cref="Span.OperationName"/>.
+    /// </summary>
+    internal static class ActivityOperationNameMapper
+    {
+        public static string MapToOperationName(IActivity5 activity)
+        {
+            var operationName = "unknown.operation";
+            switch (activity.Kind)
+            {
+                case ActivityKind.Internal:
+                    break;
+                case ActivityKind.Server:
+                    break;
+                case ActivityKind.Client:
+                    break;
+                case ActivityKind.Producer:
+                    break;
+                case ActivityKind.Consumer:
+                    break;
+                default:
+                    break;
+            }
+
+            return operationName;
+        }
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/OpenTelemetryOperationNameMapperTests.cs
@@ -0,0 +1,371 @@
+// <copyright file="OpenTelemetryOperationNameMapperTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Generic;
+using Datadog.Trace.Activity;
+using Datadog.Trace.Activity.DuckTypes;
+using Moq;
+using Xunit;
+
+namespace Datadog.Trace.Tests
+{
+    [Collection(nameof(OpenTelemetryOperationNameMapperTests))]
+    public class OpenTelemetryOperationNameMapperTests
+    {
+        [Fact]
+        public void OperationName_ShouldBe_HttpServerRequest()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "http.request.method", "GET" }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            Assert.Equal("http.server.request", name);
+        }
+
+        [Fact]
+        public void OperationName_ShouldBe_HttpClientRequest()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "http.request.method", "GET" }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            Assert.Equal("http.client.request", name);
+        }
+
+        [Theory]
+        [InlineData("mongodb", "delete")]
+        public void OperationName_ShouldBe_DbSystem_Dot_DbOperation(string dbSystem, string dbOperation)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "db.system", dbSystem },
+                { "db.operation", dbOperation },
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{dbSystem}.{dbOperation}";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("mongodb")]
+        public void OperationName_ShouldBe_DbSystem_Dot_Query(string dbSystem)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "db.system", dbSystem }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{dbSystem}.query";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("kafka", "receive", (int)ActivityKind.Client)]
+        [InlineData("kafka", "receive", (int)ActivityKind.Consumer)]
+        [InlineData("kafka", "publish", (int)ActivityKind.Producer)]
+        public void OperationName_ShouldBe_MessagingSystem_Dot_MessagingOperation(string messagingSystem, string messagingOperation, int kind)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns((ActivityKind)kind);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "messaging.system", messagingSystem },
+                { "messaging.operation", messagingOperation }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{messagingSystem}.{messagingOperation}";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("S3")]
+        [InlineData("SNS")]
+        public void OperationName_ShouldBe_Aws_Dot_RpcService(string rpcService)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "rpc.system", "aws-api" },
+                { "rpc.service", rpcService }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"aws.{rpcService.ToLower()}";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("grpc")]
+        [InlineData("dotnet_wcf")]
+        public void OperationName_ShouldBe_RpcSystem_Dot_Request(string rpcSystem)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "rpc.system", rpcSystem }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{rpcSystem}.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("grpc")]
+        [InlineData("dotnet_wcf")]
+        public void OperationName_ShouldBe_RpcSystem_Dot_Call(string rpcSystem)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "rpc.system", rpcSystem }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{rpcSystem}.call";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("query")]
+        [InlineData("mutation")]
+        public void OperationName_ShouldBe_GraphQl_Dot_OperationType(string operationType)
+        {
+            // https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/instrumentation/graphql/
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "graphql.operation.type", operationType }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"graphql.{operationType}";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("amqp")]
+        [InlineData("mqtt")]
+        public void OperationName_ShouldBe_NetworkProtocolName_Dot_Server_Dot_Request(string protocol)
+        {
+            // https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/span-general/#network-attributes
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "network.protocol.name", protocol }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{protocol}.server.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Fact]
+        public void OperationName_ShouldBe_Unknown_Dot_Server_Dot_Request()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"unknown.server.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("amqp")]
+        [InlineData("mqtt")]
+        public void OperationName_ShouldBe_NetworkProtocolName_Dot_Client_Dot_Request(string protocol)
+        {
+            // https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/span-general/#network-attributes
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "network.protocol.name", protocol }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{protocol}.client.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Fact]
+        public void OperationName_ShouldBe_Unknown_Dot_Client_Dot_Request()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"unknown.client.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Fact]
+        public void OperationName_ShouldBe_Unknown_Dot_Producer_Dot_Request()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Producer);
+            var tagObjects = new Dictionary<string, object>
+            {
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"unknown.producer.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Fact]
+        public void OperationName_ShouldBe_Unknown_Dot_Consumer_Dot_Request()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Consumer);
+            var tagObjects = new Dictionary<string, object>
+            {
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"unknown.consumer.request";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("datasource")]
+        [InlineData("http")]
+        [InlineData("pubsub")]
+        [InlineData("timer")]
+        [InlineData("other")]
+        public void OperationName_ShouldBe_FaasTrigger_Dot_Trigger(string trigger)
+        {
+            // https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/faas/
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Server);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "faas.trigger", trigger }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{trigger}.trigger";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("alibaba_cloud")]
+        [InlineData("aws")]
+        [InlineData("azure")]
+        [InlineData("gcp")]
+        [InlineData("tencent_cloud")]
+        public void OperationName_ShouldBe_FaasInvokedProvider_Dot_Invoke(string provider)
+        {
+            // https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/faas/
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Client);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "faas.invoked_provider", provider }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{provider}.invoke";
+            Assert.Equal(expected, name);
+        }
+
+        [Theory]
+        [InlineData("Datadog", "Foo()")]
+        [InlineData("Datadog.Tracer", "Bar()")]
+        public void OperationName_ShouldBe_CodeNamespace_Dot_CodeFunction(string codeNamespace, string functionName)
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Internal);
+            var tagObjects = new Dictionary<string, object>
+            {
+                { "code.namespace", codeNamespace },
+                { "code.function", functionName }
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"{codeNamespace}.{functionName}";
+            Assert.Equal(expected, name);
+        }
+
+        [Fact]
+        public void OperationName_ShouldBe_Unknown_Dot_Operation()
+        {
+            var activityMock = new Mock<IActivity5>();
+            activityMock.Setup(x => x.Kind).Returns(ActivityKind.Internal);
+            var tagObjects = new Dictionary<string, object>
+            {
+            };
+            activityMock.Setup(x => x.TagObjects).Returns(tagObjects);
+
+            var name = ActivityOperationNameMapper.MapToOperationName(activityMock.Object);
+
+            var expected = $"unknown.operation";
+            Assert.Equal(expected, name);
+        }
+    }
+}
