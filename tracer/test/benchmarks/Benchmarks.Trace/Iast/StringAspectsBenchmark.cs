d6776d62c3696d0c39544a96450f86d049ab869a
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2023-10-10 23:51:00 +0200

    StringAspects micro benchmark

--- tracer/test/benchmarks/Benchmarks.Trace/Iast/StringAspectsBenchmark.cs
+++ tracer/test/benchmarks/Benchmarks.Trace/Iast/StringAspectsBenchmark.cs
@@ -5,13 +5,14 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using BenchmarkDotNet.Attributes;
 using Datadog.Trace;
-using Datadog.Trace.AppSec;
-using Datadog.Trace.AppSec.Waf;
-using Datadog.Trace.AppSec.Waf.NativeBindings;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Configuration.Telemetry;
+using Datadog.Trace.Iast;
+using Datadog.Trace.Iast.Settings;
+using Datadog.Trace.Security.Unit.Tests.Iast;
+
 
 namespace Benchmarks.Trace.Iast;
 
@@ -25,12 +26,20 @@ public class StringAspectsBenchmark
     {
     }
 
-    public IEnumerable<int> TaintedDictionary()
+    public IEnumerable<int> IastEnabledContext()
     {
-        yield return MakeTaintedDictionary(10);
-        yield return MakeTaintedDictionary(20);
-        yield return MakeTaintedDictionary(100);
+        yield return InitTaintedContext(10);
+        //yield return InitTaintedContext(20);
+        //yield return InitTaintedContext(100);
     }
+
+    public IEnumerable<int> IastDisabledContext()
+    {
+        yield return 10;
+        //yield return InitTaintedContext(20);
+        //yield return InitTaintedContext(100);
+    }
+
 
     /// <summary>
     /// Generates dummy arguments for the waf
@@ -38,34 +47,57 @@ public class StringAspectsBenchmark
     /// <param name="nestingDepth">Encoder.cs respects WafConstants.cs limits to process arguments with a max depth of 20 so above depth 20, there shouldn't be much difference of performances.</param>
     /// <param name="withAttack">an attack present in arguments can slow down waf's run</param>
     /// <returns></returns>
-    private static int MakeTaintedDictionary(int size)
+    private static int InitTaintedContext(int size)
     {
+        System.Diagnostics.Debugger.Break();
+
+        var settings = new CustomSettingsForTests(new Dictionary<string, object>()
+        {
+            { ConfigurationKeys.Iast.RequestSampling, 100 },
+            { ConfigurationKeys.Iast.Enabled, true }
+        });
+        var iastSettings = new IastSettings(settings, NullConfigurationTelemetry.Instance);
+        Datadog.Trace.Iast.Iast.Instance = new Datadog.Trace.Iast.Iast(iastSettings);
+
+        var tracer = Tracer.Instance;
+        System.Diagnostics.Debug.Assert(tracer != null, "Tracer is NULL");
+        var iast = Datadog.Trace.Iast.Iast.Instance;
+        System.Diagnostics.Debug.Assert(iast != null, "IastInstance is NULL");
+        var context = IastModule.GetIastContext();
+        System.Diagnostics.Debug.Assert(context != null, "IastContext is NULL");
+        var taintedObjects = context.GetTaintedObjects();
+        System.Diagnostics.Debug.Assert(taintedObjects != null, "TaintedObjects is NULL");
+
+        taintedObjects.TaintInputString("param1", new Source(1, "kk", "kk"));
+
         return size;
     }
 
     [Benchmark]
-    [ArgumentsSource(nameof(MakeTaintedDictionary))]
+    [ArgumentsSource(nameof(IastDisabledContext))]
     public void RunStringBenchmark(int size)
-    { 
-    
+    {
+        var parameters = new string[] { "param1", "param2", "param3" };
+        for (int x = 0; x < 1000; x++)
+        {
+            var txt = "Select * from users where name in (" + string.Join(", ", parameters) + ")";
+        }
     }
 
     [Benchmark]
-    [ArgumentsSource(nameof(MakeTaintedDictionary))]
+    [ArgumentsSource(nameof(IastEnabledContext))]
     public void RunStringAspectBenchmark(int size) 
     {
-    
+        var parameters = new string[] { "param1", "param2", "param3" };
+        for (int x = 0; x < 1000; x++)
+        {
+            var txt = "Select * from users where name in (" + string.Join(", ", parameters) + ")";
+        }
     }
 
-    private void RunStringBenchmark(NestedMap args)
+    [IterationCleanup]
+    public void Cleanup()
     {
-        var context = Waf.CreateContext();
-        context!.Run(args.Map, TimeoutMicroSeconds);
-        context.Dispose();
-    }
-
-    public record NestedMap(Dictionary<string, object> Map, int NestingDepth, bool IsAttack = false)
-    {
-        public override string ToString() => IsAttack ? $"NestedMap ({NestingDepth}, attack)" : $"NestedMap ({NestingDepth})";
+        Datadog.Trace.Iast.Iast.Instance = null;
     }
 }
