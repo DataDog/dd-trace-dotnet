ecdcd7a0c129aee60fcb827d93cf03f06cd7def6
chrisnas <chrisnas@users.noreply.github.com>
2023-10-12 15:52:22 +0200

    [Profiler] Add Profiler_Version as exported symbol to allow version check in dumps (#4710)
    * Add Profiler_Version as exported symbol to allow version check in memory dump
    * Add profiler version to log

--- profiler/src/ProfilerEngine/Datadog.Profiler.Native/CorProfilerCallback.cpp
+++ profiler/src/ProfilerEngine/Datadog.Profiler.Native/CorProfilerCallback.cpp
@@ -58,13 +58,25 @@
 #include "shared/src/native-src/pal.h"
 #include "shared/src/native-src/string.h"
 
+#include "dd_profiler_version.h"
+
+
 IClrLifetime* CorProfilerCallback::GetClrLifetime() const
 {
     return _pClrLifetime.get();
 }
 
-// Initialization
+// This can be used to detect profiler version in a memory dump
+// in WinDbg:  dt Datadog_Profiler_Native!Profiler_Version
+// in gdb: p Profiler_Version
+#ifdef _WINDOWS
+extern "C" __declspec(dllexport) const char* Profiler_Version = PROFILER_VERSION;
+#else
+extern "C" __attribute__((visibility("default"))) const char* Profiler_Version = PROFILER_VERSION;
+#endif
 
+
+// Initialization
 CorProfilerCallback* CorProfilerCallback::_this = nullptr;
 
 CorProfilerCallback::CorProfilerCallback()
