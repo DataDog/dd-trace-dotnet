70ed127e7852d6e02fa81155c960af95d0c613d3
Andrew Lock <andrew.lock@datadoghq.com>
2023-10-13 14:39:56 +0100

    Add a workaround for the unit tests failing on MacOS
    Currently we are failing to "find" MSBuild on macos, which means the GetTargetFrameworks() call is failing.
    
    I hope this is a quirk in .NET 8 RC2 on the runners, but if not, may need to upgrade Nuke/add more workarounds

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -509,7 +509,13 @@ partial class Build
                 {
                     var project = Solution.GetProject(Projects.AppSecUnitTests);
                     var testDir = project.Directory;
-                    var frameworks = project.GetTargetFrameworks();
+                    
+                    // FIXME: This is a hack for the .NET 8 RC2 on macos, where it's 
+                    // failing to load MSBuild, so can't get the target frameworks here
+                    // so hardcoding to use the default test frameworks for now
+                    var frameworks = IsOsx
+                        ? TestingFrameworks.Select(x => (string) x).ToArray()
+                        : project.GetTargetFrameworks();
 
                     var testBinFolder = testDir / "bin" / BuildConfiguration;
 
