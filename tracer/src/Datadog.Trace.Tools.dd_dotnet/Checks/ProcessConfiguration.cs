00a6690d7384e320c4852e30b7a657e8328a2a68
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-10-19 16:36:14 +0200

    Fix LoadApplicationConfig refactoring

--- tracer/src/Datadog.Trace.Tools.dd_dotnet/Checks/ProcessConfiguration.cs
+++ tracer/src/Datadog.Trace.Tools.dd_dotnet/Checks/ProcessConfiguration.cs
@@ -5,72 +5,13 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Xml.Linq;
 using Datadog.Trace.Tools.Shared;
-using Datadog.Trace.Tools.Shared.Windows;
 
 namespace Datadog.Trace.Tools.dd_dotnet.Checks;
 
 internal static class ProcessConfiguration
 {
-    private static IReadOnlyDictionary<string, string>? LoadApplicationConfig(string? mainModule)
-    {
-        if (mainModule == null)
-        {
-            return null;
-        }
-
-        var folder = Path.GetDirectoryName(mainModule);
-
-        if (folder == null)
-        {
-            return null;
-        }
-
-        var configFileName = Path.GetFileName(mainModule) + ".config";
-        var configPath = Path.Combine(folder, configFileName);
-
-        if (!File.Exists(configPath))
-        {
-            return null;
-        }
-
-        try
-        {
-            var document = XDocument.Load(configPath);
-
-            var appSettings = document.Element("configuration")?.Element("appSettings");
-
-            if (appSettings == null)
-            {
-                return null;
-            }
-
-            var settings = new Dictionary<string, string>();
-
-            foreach (var setting in appSettings.Elements())
-            {
-                var key = setting.Attribute("key")?.Value;
-                var value = setting.Attribute("value")?.Value;
-
-                if (key != null)
-                {
-                    settings[key] = value ?? string.Empty;
-                }
-            }
-
-            return settings;
-        }
-        catch (Exception ex)
-        {
-            Utils.WriteWarning($"An error occured while parsing the configuration file {configPath}: {ex.Message}");
-            return null;
-        }
-    }
-
     internal static IConfigurationSource ExtractConfigurationSource(this ProcessInfo process,  string? baseDirectory, IReadOnlyDictionary<string, string>? appSettings)
     {
         baseDirectory ??= Path.GetDirectoryName(process.MainModule);
@@ -85,7 +26,16 @@ internal static class ProcessConfiguration
         }
         else if (process.DotnetRuntime.HasFlag(ProcessInfo.Runtime.NetFx))
         {
-            var appConfigSource = LoadApplicationConfig(process.MainModule);
+            IReadOnlyDictionary<string, string>? appConfigSource = null;
+
+            try
+            {
+                appConfigSource = process.LoadApplicationConfig();
+            }
+            catch (Exception ex)
+            {
+                Utils.WriteError("Error while trying to load application configuration: " + ex.Message);
+            }
 
             if (appConfigSource != null)
             {
