a3255d9f4a5924c4b3830cb498639e05dd131ff6
Andrew Lock <andrew.lock@datadoghq.com>
2023-10-23 12:55:44 +0100

    Add telemetry logs payload

---
+++ tracer/src/Datadog.Trace/Telemetry/DTOs/LogMessageData.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="LogMessageData.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using Datadog.Trace.Vendors.Newtonsoft.Json.Converters;
+
+namespace Datadog.Trace.Telemetry.DTOs;
+
+internal class LogMessageData
+{
+    public LogMessageData(string message, TelemetryLogLevel level, DateTimeOffset timestamp)
+    {
+        Message = message;
+        Level = level;
+        TracerTime = timestamp.ToUnixTimeSeconds();
+    }
+
+    public string Message { get; set; }
+
+    [Vendors.Newtonsoft.Json.JsonConverter(typeof(StringEnumConverter))]
+    public TelemetryLogLevel Level { get; set; }
+
+    public string? Tags { get; set; }
+
+    public string? StackTrace { get; set; }
+
+    /// <summary>
+    /// Gets or sets unix timestamp (in seconds) for the log
+    /// </summary>
+    public long TracerTime { get; set; }
+}
