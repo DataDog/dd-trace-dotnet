5f9de5bab72cb5567f22544a7f03393e8a575c53
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-10-27 11:04:05 +0200

    cleanup

--- tracer/src/Datadog.Trace.Tools.Runner/Program.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/Program.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Help;
-using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
 using System.IO;
 using System.Linq;
