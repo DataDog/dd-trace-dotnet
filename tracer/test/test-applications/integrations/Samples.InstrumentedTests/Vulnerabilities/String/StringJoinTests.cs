0dcc1e3257c30e40c112bd88c4dde737c4e276eb
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2023-11-01 10:23:35 +0100

    Fixed Join with char separator bug

--- tracer/test/test-applications/integrations/Samples.InstrumentedTests/Vulnerabilities/String/StringJoinTests.cs
+++ tracer/test/test-applications/integrations/Samples.InstrumentedTests/Vulnerabilities/String/StringJoinTests.cs
@@ -249,7 +249,37 @@ public class StringJoinTests : InstrumentationTestsBase
         AssertTaintedFormatWithOriginalCallCheck(":+-tainted-+:", String.Join(",", new ClassForStringTest(taintedValue)), () => String.Join(",", new ClassForStringTest(taintedValue)));
     }
 
+
 #if !NET462
+    [Fact]
+    public void GivenATaintedStringInNestedMethodObject_WhenCallingJoinWithChar_ResultIsTainted6()
+    {
+        void NestedMethod<T>(List<T> parameters)
+        {
+            AssertTaintedFormatWithOriginalCallCheck(":+-tainted-+:", String.Join(',', parameters), () => String.Join(',', parameters));
+        }
+
+        NestedMethod(new List<string> { taintedValue });
+    }
+
+    [Fact]
+    public void GivenATaintedStringInNestedMethodObject_WhenCallingJoinWithChar_ResultIsTainted7()
+    {
+        void NestedMethod<T>(List<T> parameters)
+        {
+            AssertTaintedFormatWithOriginalCallCheck(":+-tainted-+:,NonTainted", String.Join(',', parameters), () => String.Join(',', parameters));
+        }
+
+        NestedMethod(new List<string> { taintedValue, "NonTainted" });
+    }
+
+    [Fact]
+    public void GivenATaintedStringInList_WhenCallingJoinWithChar_ResultIsTainted8()
+    {
+        var parameters = new List<string> { taintedValue, "NonTainted" };
+        AssertTaintedFormatWithOriginalCallCheck(":+-tainted-+:,NonTainted", String.Join(',', parameters), () => String.Join(',', parameters));
+    }
+
     [Fact]
     public void GivenATaintedString_WhenCallingJoin_ResultIsTainted7()
     {
