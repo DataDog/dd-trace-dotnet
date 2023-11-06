1926cb000f7d5230adc6a241b9d77ab2c6dbf662
andrewlock <andrewlock@users.noreply.github.com>
2023-11-06 11:36:11 +0000

    [Version Bump] 2.42.0

--- tracer/build/_build/Build.cs
+++ tracer/build/_build/Build.cs
@@ -57,7 +57,7 @@ partial class Build : NukeBuild
     readonly bool IsAlpine = false;
 
     [Parameter("The current version of the source and build")]
-    readonly string Version = "2.41.0";
+    readonly string Version = "2.42.0";
 
     [Parameter("Whether the current build version is a prerelease(for packaging purposes)")]
     readonly bool IsPrerelease = false;
