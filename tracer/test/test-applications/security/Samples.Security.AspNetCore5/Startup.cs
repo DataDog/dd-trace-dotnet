d3ffcc3b68ad1addc995e3551f618b42dc342ad5
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2023-11-10 12:25:58 +0100

    fix tests

--- tracer/test/test-applications/security/Samples.Security.AspNetCore5/Startup.cs
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5/Startup.cs
@@ -116,23 +116,13 @@ namespace Samples.Security.AspNetCore5
                         });
                 });
 
-            app.Use(async (context, next) =>
-            {
-                context.Response.OnStarting(state =>
+            app.Use(
+                async (context, next) =>
                 {
-                    var httpContext = (HttpContext)state;
-                    if (!httpContext.Request.Path.Value.ToLowerInvariant().Contains("xcontenttypeheadermissing"))
-                    {
-                        if (!httpContext.Response.Headers.ContainsKey("X-Content-Type-Options"))
-                        {
-                            httpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                        }
-                    }
-                    return Task.CompletedTask;
-                }, context);
-
-                await next.Invoke(); // Pass control to the next middleware.
-            });
+                    // make sure if we go into this middleware after blocking has happened that it s not a second request issued by the developerhandlingpage middleware! if it s that no worries it s another request, not the attack one., its  a redirect.
+                    // await context.Response.WriteAsync("do smth before all");
+                    await next.Invoke();
+                });
 
 
             app.UseEndpoints(
