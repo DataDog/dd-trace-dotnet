f26a152be016ab792b29766616d1fb2888a4ed8f
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-11-10 11:32:32 -0500

    Attempt to assert the priority/order of operation name mapping

--- tracer/test/Datadog.Trace.Tests/OpenTelemetryOperationNameMapperTests.cs
+++ tracer/test/Datadog.Trace.Tests/OpenTelemetryOperationNameMapperTests.cs
@@ -66,5 +66,88 @@ namespace Datadog.Trace.Tests
 
             Assert.Equal(expectedOperationName, span.OperationName);
         }
+
+        [Theory]
+        [InlineData(SpanKinds.Server)]
+        [InlineData(SpanKinds.Client)]
+        [InlineData(SpanKinds.Consumer)]
+        [InlineData(SpanKinds.Producer)]
+        [InlineData(SpanKinds.Internal)]
+        public void OperationName_ShouldFollow_PriorityOrder(string expectedActivityKind)
+        {
+            var span = new Span(new SpanContext(1, 1), DateTimeOffset.UtcNow, new OpenTelemetryTags());
+            var order = new Queue<OperationNameData>();
+            span.Tags.SetTag("span.kind", expectedActivityKind);
+            foreach (var data in NameData)
+            {
+                var operationNameData = new OperationNameData(data);
+                if (operationNameData.ExpectedActivityKind != expectedActivityKind)
+                {
+                    continue;
+                }
+
+                foreach (var tag in operationNameData.Tags)
+                {
+                    span.Tags.SetTag(tag.Key, tag.Value);
+                }
+
+                order.Enqueue(operationNameData);
+            }
+
+            // map the name, assert it is the expected  operation name, then remove necessary tags, repeat
+            while (order.Count > 0)
+            {
+                var expected = order.Dequeue();
+                OperationNameMapper.MapToOperationName(span);
+
+                Assert.Equal(expected.ExpectedOperationName, span.OperationName);
+
+                // can't reach the RemoveTag with SetTag with a null value for types that define tags so need to recreate entirely
+                span.Tags = new OpenTelemetryTags();
+                foreach (var remainingData in order)
+                {
+                    foreach (var tag in remainingData.Tags)
+                    {
+                        // hacky, if we already have set the tag, skip
+                        // this will be skipping the lower priority tag
+                        if (span.Tags.GetTag(tag.Key) is not null)
+                        {
+                            continue;
+                        }
+
+                        span.Tags.SetTag(tag.Key, tag.Value);
+                    }
+                }
+
+                span.Tags.SetTag("span.kind", expectedActivityKind);
+
+                // must clear OperationName as this method will not override it if there is a value
+                // this is because prior to this we may see an `operation.name` tag and use that value
+                span.OperationName = null;
+            }
+        }
+
+        private class OperationNameData
+        {
+            public OperationNameData(object[] data)
+            {
+                if (data.Length != 3)
+                {
+                    throw new ArgumentException("Invalid data for operation name", nameof(data));
+                }
+
+                ExpectedOperationName = data[0].ToString();
+
+                ExpectedActivityKind = data[1]?.ToString() ?? string.Empty;
+
+                Tags = data[2] is null ? new Dictionary<string, string>() : (Dictionary<string, string>)data[2];
+            }
+
+            public string ExpectedOperationName { get; }
+
+            public string ExpectedActivityKind { get; }
+
+            public Dictionary<string, string> Tags { get; }
+        }
     }
 }
