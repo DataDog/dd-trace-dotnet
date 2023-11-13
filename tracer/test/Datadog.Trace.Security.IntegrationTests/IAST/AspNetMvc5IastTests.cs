f1eb9aa8c18e0311cef6a37f1ed13c6b5ebfa411
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2023-11-13 13:15:49 +0100

    update

--- tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetMvc5IastTests.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetMvc5IastTests.cs
@@ -444,7 +444,7 @@ public abstract class AspNetMvc5IastTests : AspNetBase, IClassFixture<IisFixture
             (string.IsNullOrEmpty(xContentTypeHeaderValue) ? string.Empty : "&xContentTypeHeaderValue=" + xContentTypeHeaderValue);
         var url = "/Iast/XContentTypeHeaderMissing" + queryParams;
         var sanitisedUrl = VerifyHelper.SanitisePathsForVerify(url);
-        var settings = VerifyHelper.GetSpanVerifierSettings(AddressesConstants.RequestQuery, sanitisedUrl);
+        var settings = VerifyHelper.GetSpanVerifierSettings(test, sanitisedUrl, null);
         var spans = await SendRequestsAsync(_iisFixture.Agent, new string[] { url });
         var spansFiltered = spans.Where(x => x.Type == SpanTypes.Web).ToList();
         settings.AddIastScrubbing(scrubHash: false);
