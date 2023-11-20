4ae7aba682a796c6313db176bfbb41383287d6a2
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-11-20 17:31:25 +0100

    Add DD_TAGS to dynamic config

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DynamicConfigurationTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DynamicConfigurationTests.cs
@@ -65,7 +65,8 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                         TraceSampleRate = .5,
                         // CustomSamplingRules = "[{\"sample_rate\":0.1}]",
                         // ServiceNameMapping = "[{\"from_key\":\"foo\", \"to_name\":\"bar\"}]",
-                        TraceHeaderTags = "[{ \"header\": \"User-Agent\", \"tag_name\": \"http.user_agent\" }]"
+                        TraceHeaderTags = "[{ \"header\": \"User-Agent\", \"tag_name\": \"http.user_agent\" }]",
+                        GlobalTags = "[\"foo1:bar1\",\"foo2:bar2\"]"
                     },
                     new Config
                     {
@@ -77,22 +78,14 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                         TraceSampleRate = .5,
                         // CustomSamplingRules = "[{\"sample_rate\":0.1}]",
                         // ServiceNameMapping = "foo:bar",
-                        TraceHeaderTags = "User-Agent:http.user_agent"
+                        TraceHeaderTags = "User-Agent:http.user_agent",
+                        GlobalTags = "[\"foo1:bar1\",\"foo2:bar2\"]"
                     });
 
                 await UpdateAndValidateConfig(
                     agent,
                     logEntryWatcher,
-                    new Config
-                    {
-                        // RuntimeMetricsEnabled = false,
-                        // DebugLogsEnabled = false,
-                        // DataStreamsEnabled = false,
-                        LogInjectionEnabled = false,
-                        // SpanSamplingRules = string.Empty,
-                        TraceSampleRate = null,
-                        // CustomSamplingRules = string.Empty,
-                    });
+                    new Config());
             }
             finally
             {
@@ -204,7 +197,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             // json["data_streams_enabled"]?.Value<bool>().Should().Be(expectedConfig.DataStreamsEnabled);
             FlattenJsonArray(json["header_tags"]).Should().Be(expectedConfig.TraceHeaderTags ?? string.Empty);
             // FlattenJsonArray(json["service_mapping"]).Should().Be(expectedConfig.ServiceNameMapping ?? string.Empty);
-
+            json["tags"]?.ToString(Formatting.None).Should().Be(expectedConfig.GlobalTags ?? "[]");
             WaitForTelemetry(agent);
 
             AssertConfigurationChanged(agent.Telemetry, config);
@@ -243,6 +236,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 // (ConfigurationKeys.DataStreamsMonitoring.Enabled, config.DataStreamsEnabled),
                 (ConfigurationKeys.HeaderTags, config.TraceHeaderTags == null ? string.Empty : JToken.Parse(config.TraceHeaderTags).ToString()),
                 // (ConfigurationKeys.ServiceNameMappings, config.ServiceNameMapping == null ? string.Empty : JToken.Parse(config.ServiceNameMapping).ToString())
+                (ConfigurationKeys.GlobalTags, config.GlobalTags == null ? string.Empty : JToken.Parse(config.GlobalTags).ToString())
             };
 
             var expectedCount = expectedKeys.Count(k => k.Value is not null);
@@ -345,6 +339,10 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             [JsonProperty("tracing_header_tags")]
             [JsonConverter(typeof(PlainJsonStringConverter))]
             public string TraceHeaderTags { get; init; }
+
+            [JsonProperty("tracing_tags")]
+            [JsonConverter(typeof(PlainJsonStringConverter))]
+            public string GlobalTags { get; init; }
 
             // [JsonProperty("tracing_service_mapping")]
             // [JsonConverter(typeof(PlainJsonStringConverter))]
