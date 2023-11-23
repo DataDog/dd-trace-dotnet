09c285012447b6770dc12aa8714b08fb952c87cd
Andrew Lock <andrew.lock@datadoghq.com>
2023-11-23 10:27:56 +0100

    Add support for Npgqsl 8.0.0 (#4910)
    * Add support for Npgsql 8.x.x
    
    * Add testing for Npgsql 8.0.0

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
@@ -510,6 +510,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 new object[] { "5.0.17" },
                 new object[] { "6.0.10" },
                 new object[] { "7.0.6" },
+                new object[] { "8.0.0" },
 #endif
 #if NETCOREAPP2_1
                 new object[] { "4.1.12" },
@@ -524,30 +525,35 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 new object[] { "5.0.17" },
                 new object[] { "6.0.10" },
                 new object[] { "7.0.6" },
+                new object[] { "8.0.0" },
 #endif
 #if NET5_0
                 new object[] { "4.1.12" },
                 new object[] { "5.0.17" },
                 new object[] { "6.0.10" },
                 new object[] { "7.0.6" },
+                new object[] { "8.0.0" },
 #endif
 #if NET6_0
                 new object[] { "4.1.12" },
                 new object[] { "5.0.17" },
                 new object[] { "6.0.10" },
                 new object[] { "7.0.6" },
+                new object[] { "8.0.0" },
 #endif
 #if NET7_0
                 new object[] { "4.1.12" },
                 new object[] { "5.0.17" },
                 new object[] { "6.0.10" },
                 new object[] { "7.0.6" },
+                new object[] { "8.0.0" },
 #endif
 #if NET8_0
                 new object[] { "4.1.12" },
                 new object[] { "5.0.17" },
                 new object[] { "6.0.10" },
                 new object[] { "7.0.6" },
+                new object[] { "8.0.0" },
 #endif
 #endif
             };
