cdb6ee06fb50576f1b33e59ca0f3fb0df002d1cb
Andrew Lock <andrew.lock@datadoghq.com>
2023-11-24 09:31:32 +0000

    Add nullability attributes to public interfaces

--- tracer/src/Datadog.Trace/ISpanContext.cs
+++ tracer/src/Datadog.Trace/ISpanContext.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 namespace Datadog.Trace
 {
     /// <summary>
@@ -23,6 +25,6 @@ namespace Datadog.Trace
         /// <summary>
         /// Gets the service name to propagate to child spans.
         /// </summary>
-        string ServiceName { get; }
+        string? ServiceName { get; }
     }
 }
