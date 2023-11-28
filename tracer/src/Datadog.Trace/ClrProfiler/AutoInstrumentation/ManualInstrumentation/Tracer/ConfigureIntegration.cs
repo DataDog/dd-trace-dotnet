3e4349d84f8f70885e2c34118dff5f73374eb587
Andrew Lock <andrew.lock@datadoghq.com>
2023-11-28 13:51:13 +0000

    Add initial auto-instrumentation of Datadog.Trace.Manual

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Tracer/ConfigureIntegration.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="ConfigureIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+using System.Collections.Generic;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Telemetry;
+using Datadog.Trace.Telemetry.Metrics;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Tracer;
+
+/// <summary>
+/// System.Void Datadog.Trace.Tracer::Configure(System.Collections.Generic.Dictionary`2[System.String,System.Object]) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Tracer",
+    MethodName = "Configure",
+    ReturnTypeName = ClrNames.Void,
+    ParameterTypeNames = new[] { "System.Collections.Generic.Dictionary`2[System.String, System.Object]" },
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class ConfigureIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget>(Dictionary<string, object> settings)
+    {
+        // Technically this isn't a public API, but it's only called by the public API, so this will do
+        TelemetryFactory.Metrics.Record(PublicApiUsage.Tracer_Configure);
+
+        // TODO: use the settings values as an input to TracerSettings/ImmutableTracerSettings
+        // and update the global instance
+        // Trace.Tracer.ConfigureInternal();
+
+        return CallTargetState.GetDefault();
+    }
+}
