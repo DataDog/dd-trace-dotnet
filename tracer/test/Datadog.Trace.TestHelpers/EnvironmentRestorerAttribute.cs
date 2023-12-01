73738860bf8c33b3585beae48a14a0e9c5a1133c
Andrew Lock <andrew.lock@datadoghq.com>
2023-12-01 18:13:57 +0100

    Fix bugs with DogStatsD when using named pipes or UDS (#4933)
    * Explicitly set the pipename when using named pipes with dogstatsd
    
    * Add EnvironmentRestorerAttribute to make it easier to test on environment variables
    
    * Add tests for creation of DogStatsD instance
    
    * Add workaround for dogstatsd when using UDS for traces but UDP for metrics
    
    * I forgot, macos doesn't like UDS
    
    * Forgot macos doesn't even like UDP

---
+++ tracer/test/Datadog.Trace.TestHelpers/EnvironmentRestorerAttribute.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="EnvironmentRestorerAttribute.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Datadog.Trace.TestHelpers;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class EnvironmentRestorerAttribute : BeforeAfterTestAttribute
+{
+    private readonly string[] _variables;
+    private readonly Dictionary<string, string> _originalVariables;
+
+    public EnvironmentRestorerAttribute(params string[] args)
+    {
+        _variables = args;
+        _originalVariables = new(args.Length);
+    }
+
+    public override void Before(MethodInfo methodUnderTest)
+    {
+        foreach (var variable in _variables)
+        {
+            _originalVariables[variable] = Environment.GetEnvironmentVariable(variable);
+            // clear variable
+            Environment.SetEnvironmentVariable(variable, null);
+        }
+
+        base.Before(methodUnderTest);
+    }
+
+    public override void After(MethodInfo methodUnderTest)
+    {
+        foreach (var variable in _originalVariables)
+        {
+            Environment.SetEnvironmentVariable(variable.Key, variable.Value);
+        }
+    }
+}
