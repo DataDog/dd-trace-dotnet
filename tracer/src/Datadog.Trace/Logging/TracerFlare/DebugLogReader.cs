02e0e412939befe1a899fdf4787bd8500a47e6e6
Andrew Lock <andrew.lock@datadoghq.com>
2023-12-13 09:56:59 +0000

    Try to create sentinel file

---
+++ tracer/src/Datadog.Trace/Logging/TracerFlare/DebugLogReader.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="DebugLogReader.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.IO;
+
+namespace Datadog.Trace.Logging.TracerFlare;
+
+internal static class DebugLogReader
+{
+    private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(DebugLogReader));
+
+    public static bool TryToCreateSentinelFile(string logDirectory, string flareRequestId)
+    {
+        // Assuming the log directory is accessible and writeable
+        // If it isn't this will return false anyway, but we do an extra check, just to be sure
+        try
+        {
+            // Using the dotnet-tracer-sentinel-*.log pattern means it gets automatically cleaned up by the log cleaning task
+            var filePath = Path.Combine(logDirectory, $"dotnet-tracer-sentinel-{flareRequestId}.log");
+            using var file = File.Open(filePath, FileMode.CreateNew, FileAccess.Write);
+
+            // if we got this far, the file was created
+            return true;
+        }
+        catch (IOException)
+        {
+            // expected, the file already exists
+            Log.Information("Sentinel file for config {FlareRequestID} found in {LogDirectory}", flareRequestId, logDirectory);
+            return false;
+        }
+        catch (Exception ex)
+        {
+            // check if the log directory exists, to see if this is an "expected" consequence
+            if (Directory.Exists(logDirectory))
+            {
+                // strange error
+                Log.Warning(ex, "Error creating sentinel file for config {FlareRequestID} in {LogDirectory}", flareRequestId, logDirectory);
+            }
+            else
+            {
+                Log.Warning(ex, "Error creating sentinel file for config {FlareRequestID} in {LogDirectory}: Directory does not exist", flareRequestId, logDirectory);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tracer/test/Datadog.Trace.Tests/Logging/TracerFlare/DebugLogReaderTests.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="DebugLogReaderTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System;
+using System.IO;
+using Datadog.Trace.Logging.TracerFlare;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Logging.TracerFlare;
+
+public class DebugLogReaderTests
+{
+    [Fact]
+    public void TryToCreateSentinelFile_CreateSentinelInExistingDirectory_Succeeds()
+    {
+        var directory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(directory);
+
+        var id = Guid.NewGuid().ToString();
+        var result = DebugLogReader.TryToCreateSentinelFile(directory, id);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TryToCreateSentinelFile_CreateSecondSentinelInExistingDirectory_Succeeds()
+    {
+        var directory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(directory);
+
+        var id1 = Guid.NewGuid().ToString();
+        var id2 = Guid.NewGuid().ToString();
+
+        var result1 = DebugLogReader.TryToCreateSentinelFile(directory, id1);
+        var result2 = DebugLogReader.TryToCreateSentinelFile(directory, id2);
+
+        result1.Should().BeTrue();
+        result2.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TryToCreateSentinelFile_CreateSameSentinel_Fails()
+    {
+        var directory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(directory);
+
+        var id = Guid.NewGuid().ToString();
+
+        var result1 = DebugLogReader.TryToCreateSentinelFile(directory, id);
+        var result2 = DebugLogReader.TryToCreateSentinelFile(directory, id);
+
+        result1.Should().BeTrue();
+        result2.Should().BeFalse();
+    }
+
+    [Fact]
+    public void TryToCreateSentinelFile_CreateSentinelInNonExistingDirectory_Fails()
+    {
+        var directory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        var id = Guid.NewGuid().ToString();
+
+        var result = DebugLogReader.TryToCreateSentinelFile(directory, id);
+
+        result.Should().BeFalse();
+    }
+}
