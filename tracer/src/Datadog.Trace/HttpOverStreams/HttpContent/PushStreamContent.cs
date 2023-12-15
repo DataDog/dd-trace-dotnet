aa3a94c694c1e948bcd622cb536ca7615dd45616
Andrew Lock <andrew.lock@datadoghq.com>
2023-12-15 11:45:48 +0000

    Don't require the caller to call close on the stream (as it seems weird)

--- tracer/src/Datadog.Trace/HttpOverStreams/HttpContent/PushStreamContent.cs
+++ tracer/src/Datadog.Trace/HttpOverStreams/HttpContent/PushStreamContent.cs
@@ -6,10 +6,7 @@
 #nullable enable
 
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Diagnostics.Contracts;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Datadog.Trace.HttpOverStreams.HttpContent;
@@ -36,15 +33,12 @@ internal class PushStreamContent : IHttpContent
 
     public long? Length => 0;
 
-    public async Task CopyToAsync(Stream destination)
+    public Task CopyToAsync(Stream destination)
     {
-        var serializeToStreamTask = new TaskCompletionSource<bool>();
-
-        var wrappedStream = new CompleteTaskOnCloseStream(destination, serializeToStreamTask);
-        await _onStreamAvailable(wrappedStream).ConfigureAwait(false);
-
-        // wait for wrappedStream.Close/Dispose to get called.
-        await serializeToStreamTask.Task.ConfigureAwait(false);
+        // Note the callee must not close or dispose the stream because they don't own it
+        // We _could_ use a wrapper stream to enforce that, but then it _requires_ the
+        // callee to call close/dispose which seems weird
+        return _onStreamAvailable(destination);
     }
 
     public Task CopyToAsync(byte[] buffer)
@@ -52,181 +46,5 @@ internal class PushStreamContent : IHttpContent
         // This CopyToAsync overload is only used to read responses
         // And we never use PushStreamContent for that
         throw new NotImplementedException();
-    }
-
-    private class CompleteTaskOnCloseStream : DelegatingStream
-    {
-        private readonly TaskCompletionSource<bool> _serializeToStreamTask;
-
-        public CompleteTaskOnCloseStream(Stream innerStream, TaskCompletionSource<bool> serializeToStreamTask)
-            : base(innerStream)
-        {
-            Contract.Assert(serializeToStreamTask != null);
-            _serializeToStreamTask = serializeToStreamTask!;
-        }
-
-        [SuppressMessage(
-            "Microsoft.Usage",
-            "CA2215:Dispose methods should call base class dispose",
-            Justification = "See comments, this is intentional.")]
-        protected override void Dispose(bool disposing)
-        {
-            // We don't dispose the underlying stream because we don't own it. Dispose in this case just signifies
-            // that the user's action is finished.
-            _serializeToStreamTask.TrySetResult(true);
-        }
-
-        public override void Close()
-        {
-            // We don't Close the underlying stream because we don't own it. Dispose in this case just signifies
-            // that the user's action is finished.
-            _serializeToStreamTask.TrySetResult(true);
-        }
-    }
-
-    /// <summary>
-    /// Stream that delegates to inner stream.
-    /// This is taken from System.Net.Http
-    /// </summary>
-    private abstract class DelegatingStream : Stream
-    {
-        private readonly Stream _innerStream;
-
-        protected DelegatingStream(Stream innerStream)
-        {
-            _innerStream = innerStream;
-        }
-
-        protected Stream InnerStream
-        {
-            get { return _innerStream; }
-        }
-
-        public override bool CanRead
-        {
-            get { return _innerStream.CanRead; }
-        }
-
-        public override bool CanSeek
-        {
-            get { return _innerStream.CanSeek; }
-        }
-
-        public override bool CanWrite
-        {
-            get { return _innerStream.CanWrite; }
-        }
-
-        public override long Length
-        {
-            get { return _innerStream.Length; }
-        }
-
-        public override long Position
-        {
-            get { return _innerStream.Position; }
-            set { _innerStream.Position = value; }
-        }
-
-        public override int ReadTimeout
-        {
-            get { return _innerStream.ReadTimeout; }
-            set { _innerStream.ReadTimeout = value; }
-        }
-
-        public override bool CanTimeout
-        {
-            get { return _innerStream.CanTimeout; }
-        }
-
-        public override int WriteTimeout
-        {
-            get { return _innerStream.WriteTimeout; }
-            set { _innerStream.WriteTimeout = value; }
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _innerStream.Dispose();
-            }
-
-            base.Dispose(disposing);
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            return _innerStream.Seek(offset, origin);
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            return _innerStream.Read(buffer, offset, count);
-        }
-
-        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            return _innerStream.ReadAsync(buffer, offset, count, cancellationToken);
-        }
-
-#if !NETSTANDARD1_3 // BeginX and EndX not supported on Streams in netstandard1.3
-        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
-        {
-            return _innerStream.BeginRead(buffer, offset, count, callback!, state);
-        }
-
-        public override int EndRead(IAsyncResult asyncResult)
-        {
-            return _innerStream.EndRead(asyncResult);
-        }
-#endif
-
-        public override int ReadByte()
-        {
-            return _innerStream.ReadByte();
-        }
-
-        public override void Flush()
-        {
-            _innerStream.Flush();
-        }
-
-        public override Task FlushAsync(CancellationToken cancellationToken)
-        {
-            return _innerStream.FlushAsync(cancellationToken);
-        }
-
-        public override void SetLength(long value)
-        {
-            _innerStream.SetLength(value);
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            _innerStream.Write(buffer, offset, count);
-        }
-
-        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            return _innerStream.WriteAsync(buffer, offset, count, cancellationToken);
-        }
-
-#if !NETSTANDARD1_3 // BeginX and EndX not supported on Streams in netstandard1.3
-        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
-        {
-            return _innerStream.BeginWrite(buffer, offset, count, callback!, state);
-        }
-
-        public override void EndWrite(IAsyncResult asyncResult)
-        {
-            _innerStream.EndWrite(asyncResult);
-        }
-#endif
-
-        public override void WriteByte(byte value)
-        {
-            _innerStream.WriteByte(value);
-        }
     }
 }
