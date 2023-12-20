a5734bf1e52c8be075898f128cee19a875ad86ff
Andrew Lock <andrew.lock@datadoghq.com>
2023-12-20 15:33:58 +0000

    Ensure we don't exceed the uploaded file size

--- tracer/src/Datadog.Trace/Logging/TracerFlare/DebugLogReader.cs
+++ tracer/src/Datadog.Trace/Logging/TracerFlare/DebugLogReader.cs
@@ -6,14 +6,18 @@
 #nullable enable
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Threading;
 using System.Threading.Tasks;
+using Datadog.Trace.Util.Streams;
 
 namespace Datadog.Trace.Logging.TracerFlare;
 
 internal static class DebugLogReader
 {
+    public const long MaximumCompressedSizeBytes = 29_000_000; // 31457280 would be 30MiB, we go to 29MB to be safe
     private const string SentinelFilePrefix = "dotnet-tracer-sentinel-";
     private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(DebugLogReader));
 
@@ -53,24 +57,31 @@ internal static class DebugLogReader
         }
     }
 
-    public static async Task WriteDebugLogArchiveToStream(Stream writeTo, string logDirectory)
+    public static Task WriteDebugLogArchiveToStream(Stream writeTo, string logDirectory)
+        => WriteDebugLogArchiveToStream(writeTo, logDirectory, StreamHasCapacity);
+
+    public static async Task WriteDebugLogArchiveToStream(Stream writeTo, string logDirectory, Func<FileInfo, long, bool> streamHasCapacityFunc)
     {
         try
         {
-            using var archive = new ZipArchive(writeTo, ZipArchiveMode.Create, true);
+            using var monitoringStream = new WriteCountingStream(writeTo);
+            using var archive = new ZipArchive(monitoringStream, ZipArchiveMode.Create, true);
 
             // Only sending .log files to avoid the risk of sending files we don't want.
             // Also not recurrsing, in-case they have a weird log setup
             // Grabbing all the filenames once, as we could be creating more in the background, and don't
             // want to run into any modified enumerable issues etc
-            var filesToUpload = Directory.GetFiles(logDirectory, "*.log", SearchOption.TopDirectoryOnly);
-            if (filesToUpload.Length == 0)
+            var allFiles = Directory.GetFiles(logDirectory, "*.log", SearchOption.TopDirectoryOnly);
+            if (allFiles.Length == 0)
             {
                 // no files to upload
                 return;
             }
 
-            foreach (var filePath in filesToUpload)
+            // we want to upload the most recent files, and leave the older files if we're oversized,
+            // so grab the FileInfo for each file, and sort by most recent
+            var filesToUpload = new List<FileInfo>();
+            foreach (var filePath in allFiles)
             {
                 try
                 {
@@ -81,21 +92,90 @@ internal static class DebugLogReader
                         continue;
                     }
 
-                    var entry = archive.CreateEntry(filename);
+                    var fileInfo = new FileInfo(filePath);
+                    filesToUpload.Add(fileInfo);
+                }
+                catch (Exception e)
+                {
+                    Log.Warning(e, "Error reading file {Filename} when preparing tracer flare zip", filePath);
+                }
+            }
+
+            // order by most recently modified (note swapped order in compareto)
+            filesToUpload.Sort((info1, info2) => info2.LastWriteTimeUtc.CompareTo(info1.LastWriteTimeUtc));
+
+            foreach (var fileDetails in filesToUpload)
+            {
+                var remainingSize = MaximumCompressedSizeBytes - monitoringStream.BytesWritten;
+                if (!streamHasCapacityFunc(fileDetails, remainingSize))
+                {
+                    Log.Warning(
+                        "File {FileName} with uncompressed length {Length} may cause compressed size to exceed {MaxFileSize}. Ignoring remaining files",
+                        fileDetails.FullName,
+                        fileDetails.Length,
+                        MaximumCompressedSizeBytes);
+                    break;
+                }
+
+                try
+                {
+                    // There's a SmallestSize for .NET 5+ but it doesn't give much benefit
+                    // and we would rather not add the extra memory pressure for little gain
+                    var entry = archive.CreateEntry(fileDetails.Name, CompressionLevel.Optimal);
                     using var entryStream = entry.Open();
                     // Have to allow FileShare.ReadWrite as the logger will already have it open for writing
-                    using var file = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    using var file = File.Open(fileDetails.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
                     await file.CopyToAsync(entryStream).ConfigureAwait(false);
+                    await entryStream.FlushAsync().ConfigureAwait(false);
                 }
                 catch (Exception ex)
                 {
-                    Log.Warning(ex, "Error recording file {Filename} in tracer flare zip", filePath);
+                    Log.Warning(ex, "Error recording file {Filename} in tracer flare zip", fileDetails.FullName);
                 }
             }
         }
         catch (Exception e)
         {
             Log.Warning(e, "Error creating sanitized debug log stream for tracer flare");
+        }
+    }
+
+    internal static bool StreamHasCapacity(FileInfo fileDetails, long remainingCapacity)
+    {
+        // we estimate that the file compresses _roughly_ 10x, so if we're going to exceed that
+        // we bail out. That means that we may miss some older smaller files, but at least we
+        // have a clear cut off.
+        var estimatedFileSize = fileDetails.Length / 10;
+        return remainingCapacity >= estimatedFileSize;
+    }
+
+    public class WriteCountingStream(Stream innerStream) : LeaveOpenDelegatingStream(innerStream)
+    {
+        public long BytesWritten { get; private set; }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            BytesWritten += count;
+            base.Write(buffer, offset, count);
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            BytesWritten += count;
+            return base.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+        {
+            BytesWritten += count;
+            return base.BeginWrite(buffer, offset, count, callback!, state);
+        }
+
+        public override void WriteByte(byte value)
+        {
+            BytesWritten++;
+            base.WriteByte(value);
         }
     }
 }
--- tracer/test/Datadog.Trace.Tests/Logging/TracerFlare/DebugLogReaderTests.cs
+++ tracer/test/Datadog.Trace.Tests/Logging/TracerFlare/DebugLogReaderTests.cs
@@ -4,9 +4,11 @@
 // </copyright>
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.Logging.TracerFlare;
@@ -216,7 +218,76 @@ public class DebugLogReaderTests(ITestOutputHelper output)
         }
     }
 
-    private static async Task CreateLogFileWithContent(string directory, string filename)
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public async Task WriteDebugLogArchiveToStream_OnlyIncludesMostRecentLogsWhenOverSizeLimit(int filesToWrite)
+    {
+        // create files
+        var directory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        output.WriteLine("Creating log directory " + directory);
+        Directory.CreateDirectory(directory);
+
+        // These files are very repetitive, so each one compresses down massively
+        const int totalFiles = 4;
+        var skippedFileCount = totalFiles - filesToWrite;
+        var allFiles = new List<string>();
+        for (var i = 0; i < totalFiles; i++)
+        {
+            var filename = $"dotnet-tracer-managed-dotnet-202312{i}.log";
+            await CreateLogFileWithContent(directory, filename);
+            allFiles.Add(filename);
+        }
+
+        // make sure we flag as being "oversized" when we have more than two of them
+        // We only write the _last_
+        var includedFiles = allFiles.Skip(skippedFileCount).ToArray();
+        var filesWritten = 0;
+        var extractTo = await CreateAndExtractZipFile(
+                            directory,
+                            includedFiles,
+                            streamHasCapacityFunc: (_, _) => Interlocked.Increment(ref filesWritten) <= filesToWrite);
+
+        // confirm we only have the files we expected and not the remaining
+        foreach (var filename in includedFiles)
+        {
+            var originalPath = Path.Combine(directory, filename);
+            var extractedPath = Path.Combine(extractTo, filename);
+
+            File.Exists(extractedPath).Should().BeTrue();
+            var originalContents = File.ReadAllText(originalPath);
+            var extractedContents = File.ReadAllText(extractedPath);
+
+            extractedContents.Should().Be(originalContents);
+        }
+
+        foreach (var filename in allFiles.Take(skippedFileCount))
+        {
+            var extractedPath = Path.Combine(extractTo, filename);
+            File.Exists(extractedPath).Should().BeFalse();
+        }
+    }
+
+    [Fact]
+    public async Task StreamHasCapacity_EstimatesCapacityCorrectly()
+    {
+        var directory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        output.WriteLine("Creating log directory " + directory);
+        Directory.CreateDirectory(directory);
+        var file = await CreateLogFileWithContent(directory, "somefile.log");
+
+        // Plenty of capacity
+        DebugLogReader.StreamHasCapacity(file, remainingCapacity: file.Length * 2).Should().BeTrue();
+        // just enough capacity
+        DebugLogReader.StreamHasCapacity(file, remainingCapacity: file.Length / 10).Should().BeTrue();
+        // just too little capacity
+        DebugLogReader.StreamHasCapacity(file, remainingCapacity: (file.Length / 10) - 1).Should().BeFalse();
+        // No capacity
+        DebugLogReader.StreamHasCapacity(file, remainingCapacity: 0).Should().BeFalse();
+    }
+
+    private static async Task<FileInfo> CreateLogFileWithContent(string directory, string filename)
     {
         // These lines don't include anything that needs scrubbing so can be round-tripped
         const string logLines =
@@ -226,22 +297,36 @@ public class DebugLogReaderTests(ITestOutputHelper output)
             2023-12-06 11:29:18.051 +00:00 [INF] DATADOG TRACER CONFIGURATION - {"date":"2023-12-06T11:29:18.047752+00:00","os_name":"Windows","os_version":"Microsoft Windows NT 10.0.19045.0","version":"2.43.0.0","native_tracer_version":"2.43.0","platform":"x64","lang":".NET","lang_version":"8.0.0","env":"andrew","enabled":true,"service":"BlazorWebApp","agent_url":"http://127.0.0.1:8126/","agent_transport":"Default","debug":true,"health_checks_enabled":false,"analytics_enabled":false,"sample_rate":null,"sampling_rules":null,"tags":[],"log_injection_enabled":false,"runtime_metrics_enabled":false,"disabled_integrations":["OpenTelemetry"],"routetemplate_resourcenames_enabled":true,"routetemplate_expansion_enabled":false,"querystring_reporting_enabled":true,"obfuscation_querystring_regex_timout":200.0,"obfuscation_querystring_size":5000,"partialflush_enabled":false,"partialflush_minspans":500,"runtime_id":"b3eef1ac-546a-408a-994c-fdc69456c36c","agent_reachable":true,"agent_error":"","appsec_enabled":false,"appsec_trace_rate_limit":100,"appsec_rules_file_path":"(default)","appsec_libddwaf_version":"(none)","iast_enabled":false,"iast_deduplication_enabled":true,"iast_weak_hash_algorithms":"HMACMD5,MD5,HMACSHA1,SHA1","iast_weak_cipher_algorithms":"DES,TRIPLEDES,RC2","direct_logs_submission_enabled_integrations":[],"direct_logs_submission_enabled":false,"direct_logs_submission_error":"","exporter_settings_warning":["No transport configuration found, using default values"],"dd_trace_methods":"","activity_listener_enabled":false,"profiler_enabled":false,"code_hotspots_enabled":false,"wcf_obfuscation_enabled":true,"data_streams_enabled":false,"span_sampling_rules":null,"stats_computation_enabled":false,"dbm_propagation_mode":"Disabled","header_tags":[],"service_mapping":[]}  { MachineName: ".", Process: "[35160 dotnet]", AppDomain: "[1 BlazorWebApp]", AssemblyLoadContext: "\"\" Datadog.Trace.ClrProfiler.Managed.Loader.ManagedProfilerAssemblyLoadContext #1", TracerVersion: "2.43.0.0" }
             """;
 
-        using var writer = File.CreateText(Path.Combine(directory, filename));
-        // Creates files ~10MB (max file size we expect)
-        const int repeats = 4000;
-        foreach (var line in Enumerable.Repeat(logLines, repeats))
+        var filepath = Path.Combine(directory, filename);
+        using var writer = File.CreateText(filepath);
         {
-            await writer.WriteLineAsync(line);
+            // Creates files ~10MB (max file size we expect)
+            const int repeats = 4000;
+            // write the name of the file in the top to ensure each file is unique
+            await writer.WriteLineAsync(filename);
+            foreach (var line in Enumerable.Repeat(logLines, repeats))
+            {
+                await writer.WriteLineAsync(line);
+            }
+
+            await writer.FlushAsync();
         }
 
-        await writer.FlushAsync();
+        return new FileInfo(filepath);
     }
 
-    private async Task<string> CreateAndExtractZipFile(string directory, string[] files)
+    private async Task<string> CreateAndExtractZipFile(string directory, string[] files, Func<FileInfo, long, bool> streamHasCapacityFunc = null)
     {
         using var ms = new MemoryStream();
         output.WriteLine("Reading log files");
-        await DebugLogReader.WriteDebugLogArchiveToStream(ms, directory);
+        if (streamHasCapacityFunc is not null)
+        {
+            await DebugLogReader.WriteDebugLogArchiveToStream(ms, directory, streamHasCapacityFunc);
+        }
+        else
+        {
+            await DebugLogReader.WriteDebugLogArchiveToStream(ms, directory);
+        }
 
         // write the zip file for external testing
         var zipFile = Path.Combine(directory, "debug_logs.zip");
