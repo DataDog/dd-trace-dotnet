73eafa1c923c02c4145b3ae2a193363f5503963c
Andrew Lock <andrew.lock@datadoghq.com>
2023-12-27 12:39:10 +0000

    Add support for writing logs to stdout

---
+++ tracer/src/Datadog.Trace/Logging/Internal/Configuration/ConsoleLoggingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ConsoleLoggingConfiguration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+namespace Datadog.Trace.Logging.Internal.Configuration;
+
+internal readonly struct ConsoleLoggingConfiguration
+{
+    public readonly string MessageTemplate;
+    public readonly int BufferSize;
+
+    public ConsoleLoggingConfiguration(string formatString, int bufferSize)
+    {
+        MessageTemplate = formatString;
+        BufferSize = bufferSize;
+    }
+}
