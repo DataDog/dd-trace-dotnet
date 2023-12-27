998c7a2efe82db45fec59ed717fcde31fa97f55b
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-12-27 11:48:04 +0100

    Ignore flare configs that are not targeted at the tracer

--- tracer/src/Datadog.Trace/Logging/TracerFlare/TracerFlareManager.cs
+++ tracer/src/Datadog.Trace/Logging/TracerFlare/TracerFlareManager.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TracerFlareManager.cs" company="Datadog">
+// <copyright file="TracerFlareManager.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -228,7 +228,7 @@ internal class TracerFlareManager : ITracerFlareManager
             {
                 // not the right sort of task, just acknowledge it
                 Log.Debug($"{RcmProducts.TracerFlareRequested} was not tracer flare - ignoring");
-                return ApplyDetails.FromOk(configPath);
+                return new ApplyDetails(configPath);
             }
 
             if (!jObject.TryGetValue("args", StringComparison.Ordinal, out var args)
