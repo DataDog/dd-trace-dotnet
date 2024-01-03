af385325b76f923c057027e4620cd5006876bde8
Raphaël Vandon <raphael.vandon@datadoghq.com>
2024-01-03 17:34:23 +0100

    putting some changes aside for an other PR

--- tracer/build/_build/Build.Utilities.cs
+++ tracer/build/_build/Build.Utilities.cs
@@ -335,35 +335,21 @@ partial class Build
               {
                   if (t.operation != Operation.EQUAL)
                   {
-                      var str = DiffToString(t);
-                      if (str.Contains(value: '\n'))
-                      {
-                          // if the diff is multiline, start with a newline so that all changes are aligned
-                          // otherwise it's easy to miss the first line of the diff
-                          str = "\n" + str;
-                      }
-
-                      Logger.Information(str);
+                      Logger.Information(DiffToString(t));
                   }
               }
           }
 
           string DiffToString(Diff diff)
           {
-              if (diff.operation == Operation.EQUAL)
+              var line = diff.operation switch
               {
-                  return string.Empty;
-              }
-
-              var symbol = diff.operation switch
-              {
-                  Operation.DELETE => '-',
-                  Operation.INSERT => '+',
+                  Operation.DELETE => $"- {diff.text}",
+                  Operation.INSERT => $"+ {diff.text}",
+                  Operation.EQUAL => string.Empty,
                   _ => throw new Exception("Unknown value of the Option enum.")
               };
-              // put the symbol at the beginning of each line to make diff clearer when whole blocks of text are missing
-              var lines = diff.text.TrimEnd(trimChar: '\n').Split(Environment.NewLine);
-              return string.Join(Environment.NewLine, lines.Select(l => symbol + l));
+              return line.Trim('\n');
           }
       });
 
