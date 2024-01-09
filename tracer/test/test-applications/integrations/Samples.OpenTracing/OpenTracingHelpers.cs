932e5b1fbc4bd311ac409a534a88c3c67c2eb75d
Andrew Lock <andrew.lock@datadoghq.com>
2024-01-09 15:25:52 +0000

    Add OpenTracing sample app for running integration tests

---
+++ tracer/test/test-applications/integrations/Samples.OpenTracing/OpenTracingHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using Datadog.Trace.OpenTracing;
+
+/// <summary>
+/// Helper methods for testing the OpenTracing library
+/// </summary>
+public static class OpenTracingHelpers
+{
+    private static Type OpenTracingSpanType = typeof(OpenTracingTracerFactory).Assembly.GetType("Datadog.Trace.OpenTracing.OpenTracingSpan");
+    private static MethodInfo GetSpanMethod = OpenTracingSpanType.GetProperty("Span", BindingFlags.Instance | BindingFlags.NonPublic).GetMethod;
+
+    private static Type OpenTracingSpanContextType = typeof(OpenTracingTracerFactory).Assembly.GetType("Datadog.Trace.OpenTracing.OpenTracingSpanContext");
+    private static MethodInfo GetSpanContextMethod = OpenTracingSpanContextType.GetProperty("Context", BindingFlags.Instance | BindingFlags.NonPublic).GetMethod;
+
+    public static Datadog.Trace.ISpan GetDdTraceSpan(this OpenTracing.ISpan span)
+        => (Datadog.Trace.ISpan)GetSpanMethod.Invoke(span, Array.Empty<object>());
+
+    public static Datadog.Trace.ISpanContext GetDdTraceSpanContext(this OpenTracing.ISpan span)
+        => (Datadog.Trace.ISpanContext)GetSpanContextMethod.Invoke(span.Context, Array.Empty<object>());
+}
---
+++ tracer/test/test-applications/integrations/Samples.OpenTracing/Program.cs
@@ -0,0 +1,34 @@
+﻿using Datadog.Trace.OpenTracing;
+using System;
+using ITracer = OpenTracing.ITracer;
+
+if (args.Length == 0)
+{
+    throw new InvalidOperationException("Must provide an argument to define the scenario");
+}
+
+var scenario = args[0];
+
+var scenarioToRun = scenario switch
+{
+    _ => throw new InvalidOperationException("Unknown scenario: " + scenario),
+};
+
+await scenarioToRun;
+
+return 0;
+
+ITracer GetWrappedTracer()
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    return OpenTracingTracerFactory.WrapTracer(Datadog.Trace.Tracer.Instance);
+#pragma warning restore CS0618
+}
+
+ITracer CreateTracer(string defaultServiceName)
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    return OpenTracingTracerFactory.CreateTracer(defaultServiceName: defaultServiceName);
+#pragma warning restore CS0618
+}
+
