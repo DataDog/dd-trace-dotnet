eb81704a06ab7e2ecd19d98915682505d1ca166c
Anna <anna.yafi@datadoghq.com>
2024-01-18 13:20:02 +0100

    answer to comment: fix agent number and constant

--- tracer/test/benchmarks/Benchmarks.Trace/Asm/AppSecBodyBenchmark.cs
+++ tracer/test/benchmarks/Benchmarks.Trace/Asm/AppSecBodyBenchmark.cs
@@ -23,7 +23,7 @@ using Microsoft.AspNetCore.Http.Features;
 namespace Benchmarks.Trace.Asm
 {
     [MemoryDiagnoser]
-    [BenchmarkAgent2]
+    [BenchmarkAgent7]
     [BenchmarkCategory(Constants.AppSecCategory)]
     public class AppSecBodyBenchmark
     {
