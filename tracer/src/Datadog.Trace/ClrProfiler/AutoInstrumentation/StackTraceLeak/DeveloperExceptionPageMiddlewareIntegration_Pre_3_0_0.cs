4f9e3b9c73dee0bc6a51413b3011c4bd4473278a
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-01-23 12:44:22 +0100

    nomenclature change.

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/StackTraceLeak/DeveloperExceptionPageMiddlewareIntegration_Pre_3_0_0.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/StackTraceLeak/DeveloperExceptionPageMiddlewareIntegration_Pre_3_0_0.cs
@@ -1,4 +1,4 @@
-// <copyright file="DeveloperExceptionPageMiddlewareIntegrationBis.cs" company="Datadog">
+// <copyright file="DeveloperExceptionPageMiddlewareIntegration_Pre_3_0_0.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -42,7 +42,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.StackTraceLeak;
 
 [Browsable(false)]
 [EditorBrowsable(EditorBrowsableState.Never)]
-public static class DeveloperExceptionPageMiddlewareIntegrationBis
+public static class DeveloperExceptionPageMiddlewareIntegration_Pre_3_0_0
 {
     internal interface IErrorContext
     {
