050ef98a8f92469a3054b7af7a1370dbaf897608
Tony Redondo <tony.redondo@datadoghq.com>
2024-01-26 23:20:05 +0100

    Changes missing on the SCI git metadata spec (#5082)
    * Changes missing on the SCI git metadata spec
    
    * fix test build
    
    * update snapshots
    
    * add missing snapshot changes
    
    * snapshots update 1
    
    * snapshots
    
    * update snapshots
    
    * fix
    
    * fix
    
    * Fix TraceAnnotationsVersionMismatchNewerNuGetTests
    
    * Change to only set values in the first span in the chunk
    
    * Changes
    
    * fix snapshots
    
    * Snapshot changes
    
    * update
    
    * change
    
    * changes
    
    * Ignore git metadata tags from snapshot testing
    
    * revert snapshots
    
    * Update snapshots
    
    * update windows snapshots
    
    * arm64 snapshots
    
    * Fix for windows
    
    * fix for arm64
    
    * fix
    
    * SourceCodeIntegrationGitMetadata tests

---
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SourceCodeIntegrationGitMetadataTests.cs
@@ -0,0 +1,75 @@
+// <copyright file="SourceCodeIntegrationGitMetadataTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Linq;
+using System.Threading.Tasks;
+using Datadog.Trace.TestHelpers;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests;
+
+public class SourceCodeIntegrationGitMetadataTests : TestHelper
+{
+    public SourceCodeIntegrationGitMetadataTests(ITestOutputHelper output)
+        : base("ManualInstrumentation", output)
+    {
+    }
+
+    [SkippableFact]
+    [Trait("RunOnWindows", "True")]
+    public async Task ManualAndAutomatic()
+    {
+        const int expectedSpans = 36;
+        using var telemetry = this.ConfigureTelemetry();
+        using var agent = EnvironmentHelper.GetMockAgent();
+        using var assert = new AssertionScope();
+        using var process = await RunSampleAndWaitForExit(agent);
+
+        var spans = agent.WaitForSpans(expectedSpans);
+        spans.Should().HaveCount(expectedSpans);
+
+        // Let's separate the traces
+        foreach (var trace in spans.GroupBy(s => s.TraceId))
+        {
+            // Only a single tagging per trace
+            trace.Should().ContainSingle(s => s.Tags.ContainsKey(Trace.Tags.GitCommitSha));
+            trace.Should().ContainSingle(s => s.Tags.ContainsKey(Trace.Tags.GitRepositoryUrl));
+
+            // Must be the first span of the trace
+            trace.First().Tags.Should().ContainKey(Trace.Tags.GitCommitSha);
+            trace.First().Tags.Should().ContainKey(Trace.Tags.GitRepositoryUrl);
+        }
+    }
+
+    [SkippableFact]
+    [Trait("RunOnWindows", "True")]
+    public async Task ManualOnly()
+    {
+        EnvironmentHelper.SetAutomaticInstrumentation(false);
+        const int expectedSpans = 29;
+        using var telemetry = this.ConfigureTelemetry();
+        using var agent = EnvironmentHelper.GetMockAgent();
+        using var assert = new AssertionScope();
+        using var process = await RunSampleAndWaitForExit(agent);
+
+        var spans = agent.WaitForSpans(expectedSpans);
+        spans.Should().HaveCount(expectedSpans);
+
+        // Let's separate the traces
+        foreach (var trace in spans.GroupBy(s => s.TraceId))
+        {
+            // Only a single tagging per trace
+            trace.Should().ContainSingle(s => s.Tags.ContainsKey(Trace.Tags.GitCommitSha));
+            trace.Should().ContainSingle(s => s.Tags.ContainsKey(Trace.Tags.GitRepositoryUrl));
+
+            // Must be the first span of the trace
+            trace.First().Tags.Should().ContainKey(Trace.Tags.GitCommitSha);
+            trace.First().Tags.Should().ContainKey(Trace.Tags.GitRepositoryUrl);
+        }
+    }
+}
