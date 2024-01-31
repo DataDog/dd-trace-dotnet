7e1311f35435621db0e167bd4f87a257f9c88e79
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2024-01-31 18:25:42 +0100

    [ASM] Fix flaky test (#5123)
    * Fix error. Update namespaces.
    
    * Use System.Text.RegularExpressions
    
    * Fix compilation error.
    
    * Update trim file

--- tracer/src/Datadog.Trace/Iast/SensitiveData/HeaderInjectionTokenizer.cs
+++ tracer/src/Datadog.Trace/Iast/SensitiveData/HeaderInjectionTokenizer.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Datadog.Trace.Configuration;
-using Datadog.Trace.Logging;
 
 #nullable enable
 
--- tracer/src/Datadog.Trace/Iast/SensitiveData/LdapTokenizer.cs
+++ tracer/src/Datadog.Trace/Iast/SensitiveData/LdapTokenizer.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Datadog.Trace.Configuration;
-using Datadog.Trace.Iast.Settings;
 
 #nullable enable
 
