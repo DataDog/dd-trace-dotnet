44caa0e80cf4cb099c8ed8867f7ff6a35224eb6f
dudik <dudi.keleti@gmail.com>
2024-02-02 18:14:30 +0100

    replace @exceptions with @exception in tests

--- tracer/test/test-applications/debugger/dependency-libs/Samples.Probes.TestRuns/ExpressionTests/TemplateNoExceptionValue.cs
+++ tracer/test/test-applications/debugger/dependency-libs/Samples.Probes.TestRuns/ExpressionTests/TemplateNoExceptionValue.cs
@@ -7,7 +7,7 @@ namespace Samples.Probes.TestRuns.ExpressionTests
     internal class TemplateNoException : IRun
     {
         private const string Json = @"{
-        ""ref"": ""@exceptions""
+        ""ref"": ""@exception""
 }";
 
         [MethodImpl(MethodImplOptions.NoInlining)]
