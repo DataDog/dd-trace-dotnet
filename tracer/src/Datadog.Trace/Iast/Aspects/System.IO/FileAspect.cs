b0674a039d008b8d94593bc84dc2c3b08c254ab3
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-02-06 16:38:33 +0100

    remove file rasp sink

--- tracer/src/Datadog.Trace/Iast/Aspects/System.IO/FileAspect.cs
+++ tracer/src/Datadog.Trace/Iast/Aspects/System.IO/FileAspect.cs
@@ -10,7 +10,7 @@ using Datadog.Trace.Iast.Dataflow;
 namespace Datadog.Trace.Iast.Aspects;
 
 /// <summary> File class aspects </summary>
-[AspectClass("mscorlib,System.IO.FileSystem,System.Runtime", AspectType.IastSink | AspectType.RaspSink, VulnerabilityType.PathTraversal)]
+[AspectClass("mscorlib,System.IO.FileSystem,System.Runtime", AspectType.IastSink, VulnerabilityType.PathTraversal)]
 [global::System.ComponentModel.Browsable(false)]
 [global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]
 public class FileAspect
