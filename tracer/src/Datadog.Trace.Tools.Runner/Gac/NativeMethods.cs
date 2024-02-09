35642fee2b3b4e9ebde71dbad8ffbfd7c6acb37b
Tony Redondo <tony.redondo@datadoghq.com>
2024-02-09 18:42:04 +0100

    Fix netcoreapp2.x test

--- tracer/src/Datadog.Trace.Tools.Runner/Gac/NativeMethods.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/Gac/NativeMethods.cs
@@ -20,7 +20,9 @@ internal sealed class NativeMethods
 {
     private const string NetFrameworkSubKey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
 
-    internal static unsafe AssemblyCacheContainer CreateAssemblyCache()
+    private delegate int CreateAssemblyCacheDelegate(out IAssemblyCache ppAsmCache, int reserved);
+    
+    internal static AssemblyCacheContainer CreateAssemblyCache()
     {
         string fusionFullPath;
         using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, Environment.Is64BitProcess ? RegistryView.Registry64 : RegistryView.Registry32).OpenSubKey(NetFrameworkSubKey))
@@ -41,8 +43,8 @@ internal sealed class NativeMethods
 
         var libPointer = NativeLibrary.Load(fusionFullPath);
         var createAssemblyCachePointer = NativeLibrary.GetExport(libPointer, nameof(CreateAssemblyCache));
-        var functionPointer = (delegate* unmanaged[Stdcall]<out IAssemblyCache, uint, int>)createAssemblyCachePointer;
-        var hr = functionPointer(out var ppAsmCache, 0);
+        var createAssemblyCache = Marshal.GetDelegateForFunctionPointer<CreateAssemblyCacheDelegate>(createAssemblyCachePointer);
+        var hr = createAssemblyCache(out var ppAsmCache, 0);
         if (hr != 0)
         {
             NativeLibrary.Free(libPointer);
