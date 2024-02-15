34f7fd16fae2be5a220e2f3fb6f338b39388803a
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-02-15 12:54:01 -0500

    [Tracing] special-case the "any" pattern in sampling rules (#5142)
    * special-case the "any" pattern
    
    * never match a null span
    
    * add RegexBuilderTests, fix bug in RegexBuilder
    
    * add more test scenarios
    
    * Update tracer/test/Datadog.Trace.Tests/Sampling/RegexBuilderTests.cs
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>
    
    ---------
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

---
+++ tracer/test/Datadog.Trace.Tests/Sampling/RegexBuilderTests.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="RegexBuilderTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Datadog.Trace.Sampling;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.Sampling;
+
+public class RegexBuilderTests
+{
+    [Theory]
+    // regex
+    [InlineData(null, "regex", null)]
+    [InlineData(".*", "regex", null)]
+    [InlineData("", "regex", "^$")]
+    [InlineData("test", "regex", "^test$")]
+    [InlineData("te.*st", "regex", "^te.*st$")]
+    [InlineData("^test", "regex", "^test$")]
+    [InlineData("test$", "regex", "^test$")]
+    [InlineData("^test$", "regex", "^test$")]
+    // glob
+    [InlineData(null, "glob", null)]
+    [InlineData("*", "glob", null)]
+    [InlineData("**", "glob", null)]
+    [InlineData("*****", "glob", null)]
+    [InlineData("", "glob", "^$")]
+    [InlineData("test", "glob", "^test$")]
+    [InlineData("te*st", "glob", "^te.*st$")]
+    [InlineData("te?st", "glob", "^te.st$")]
+    public void Build(string pattern, string format, string expected)
+    {
+        var regex = RegexBuilder.Build(pattern, format);
+
+        if (expected == null)
+        {
+            regex.Should().BeNull();
+        }
+        else
+        {
+            regex.Should().NotBeNull().And.Subject.ToString().Should().Be(expected);
+        }
+    }
+}
