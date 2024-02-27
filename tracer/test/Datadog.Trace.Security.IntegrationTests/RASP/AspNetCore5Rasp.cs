62ae7203795e9b190bc604df7fb879d5fbec6054
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-02-27 16:55:13 +0100

    netcore integration tests

--- tracer/test/Datadog.Trace.Security.IntegrationTests/RASP/AspNetCore5Rasp.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/RASP/AspNetCore5Rasp.cs
@@ -8,6 +8,7 @@
 #pragma warning disable SA1649 // File name must match first type name
 
 using System;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Datadog.Trace.Configuration;
@@ -81,11 +82,9 @@ public abstract class AspNetCore5Rasp : AspNetBase, IClassFixture<AspNetCoreTest
     [Trait("RunOnWindows", "True")]
     public async Task TestRaspRequest(string url, string exploit)
     {
-        SetEnvironmentVariable("DD_TRACE_DEBUG", "1");
-        SetEnvironmentVariable("DD_APPSEC_RULES", "C:\\CommonFolder\\shared\\repos\\dd-trace-5\\tracer\\test\\Datadog.Trace.Security.Unit.Tests\\rasp-rule-set.json");
-        var filePath = "/etc/password";
-        var filename = IastEnabled ? "Rasp.PathTraversal.AspNetCore5.IastEnabled" : "Rasp.PathTraversal.AspNetCore5.IastDisabled";
-        var url = $"/Iast/GetFileContent?file={filePath}";
+        var methodName = "PathTraversal";
+        var testName = IastEnabled ? "RaspIast.AspNetCore5" : "Rasp.AspNetCore5";
+        var url = $"/Iast/GetFileContent?file={file}";
         IncludeAllHttpSpans = true;
         await TryStartApp();
         var agent = Fixture.Agent;
