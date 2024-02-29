6ffa6ac345e542ea86ace9f6d482e93d2cd276fa
Christophe Nasarre <christophe.nasarre@datadoghq.com>
2024-02-29 14:49:16 +0100

    Add end to end test for SSI

---
+++ profiler/test/Datadog.Profiler.IntegrationTests/SingleStepInstrumentation/SingleStepInstrumentationTest.cs
@@ -0,0 +1,77 @@
+using System.IO;
+using System.Linq;
+using Datadog.Profiler.IntegrationTests.Helpers;
+using FluentAssertions;
+using Xunit.Abstractions;
+
+
+namespace Datadog.Profiler.IntegrationTests.SingleStepInstrumentation
+{
+    public class SingleStepInstrumentationTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SingleStepInstrumentationTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [TestAppFact("Samples.Computer01")]
+        public void CheckProcessStartWhenSsiDeployment(string appName, string framework, string appAssembly)
+        {
+            var runner = new TestApplicationRunner(appName, framework, appAssembly, _output, commandLine: "--scenario 1");
+
+            // deployed with SSI
+            runner.Environment.SetVariable(EnvironmentVariables.SsiDeployed, "tracer");
+
+            using var agent = MockDatadogAgent.CreateHttpAgent(_output);
+
+            runner.Run(agent);
+
+            var logFile = Directory.GetFiles(runner.Environment.LogDir)
+                .Single(f => Path.GetFileName(f).StartsWith("DD-DotNet-Profiler-Native-"));
+
+            var lines = File.ReadAllLines(logFile);
+
+            lines.Should().ContainMatch("*ProcessStart(ssi)*");
+        }
+
+        [TestAppFact("Samples.Computer01")]
+        public void CheckProcessStartWhenManualDeployment(string appName, string framework, string appAssembly)
+        {
+            var runner = new TestApplicationRunner(appName, framework, appAssembly, _output, commandLine: "--scenario 1");
+
+            // not deployed with SSI because the env var is not set
+
+            using var agent = MockDatadogAgent.CreateHttpAgent(_output);
+
+            runner.Run(agent);
+
+            var logFile = Directory.GetFiles(runner.Environment.LogDir)
+                .Single(f => Path.GetFileName(f).StartsWith("DD-DotNet-Profiler-Native-"));
+
+            var lines = File.ReadAllLines(logFile);
+
+            lines.Should().ContainMatch("*ProcessStart(manual)*");
+        }
+
+        [TestAppFact("Samples.Computer01")]
+        public void CheckSkipWhenNoSpan(string appName, string framework, string appAssembly)
+        {
+            var runner = new TestApplicationRunner(appName, framework, appAssembly, _output, commandLine: "--scenario 1");
+
+            // no span are created and should be short lived too
+
+            using var agent = MockDatadogAgent.CreateHttpAgent(_output);
+
+            runner.Run(agent);
+
+            var logFile = Directory.GetFiles(runner.Environment.LogDir)
+                .Single(f => Path.GetFileName(f).StartsWith("DD-DotNet-Profiler-Native-"));
+
+            var lines = File.ReadAllLines(logFile);
+
+            lines.Should().ContainMatch("*SkippedProfile(*");
+        }
+    }
+}
