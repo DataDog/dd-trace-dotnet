de3cd7309e1535c7a770dcb152b21740726fa250
Anna <anna.yafi@datadoghq.com>
2024-02-29 14:07:35 +0100

    Add hash algorithm for routes

--- tracer/src/Datadog.Trace/AppSec/ApiSecurity.cs
+++ tracer/src/Datadog.Trace/AppSec/ApiSecurity.cs
@@ -6,44 +6,67 @@
 #nullable enable
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Text;
-using Datadog.Trace.AppSec.Waf;
-using Datadog.Trace.Sampling;
-using Datadog.Trace.Vendors.Newtonsoft.Json;
-using Datadog.Trace.Vendors.Serilog;
+using System.Linq;
+using Datadog.Trace.Logging;
 
 namespace Datadog.Trace.AppSec;
 
 internal class ApiSecurity
 {
+    private const int MinTimeBetweenReprocess = 30;
+    private const int MaxRoutesSize = 4096;
+    private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<ApiSecurity>();
     private readonly bool _enabled;
-    private readonly OverheadController _overheadController;
+    private readonly TimeSpan _minTimeBetweenReprocessTimeSpan = TimeSpan.FromSeconds(MinTimeBetweenReprocess);
+    private readonly IDictionary<int, DateTime> _processedRoutes = new Dictionary<int, DateTime>();
+    private readonly Dictionary<string, bool> _apiSecurityAddress = new() { { "extract-schema", true } };
 
     public ApiSecurity(SecuritySettings securitySettings)
     {
-        _overheadController = new(securitySettings.ApiSecurityMaxConcurrentRequests, (int)(securitySettings.ApiSecuritySampling * 100));
         // todo: later, will be enabled by default, depending on if Security is enabled
         _enabled = securitySettings.ApiSecurityEnabled;
     }
 
-    public bool TryTellWafToAnalyzeSchema(IDictionary<string, object> args)
+    public bool ShouldAnalyzeSchema(bool lastWafCall, Span localRootSpan, IDictionary<string, object> args)
     {
-        if (_enabled && _overheadController.AcquireRequest())
+        if (_enabled && lastWafCall && localRootSpan.Context.SamplingPriority > (int?)SamplingPriority.AutoReject)
         {
-            args.Add(AddressesConstants.WafContextProcessor, new Dictionary<string, bool> { { "extract-schema", true } });
-            return true;
+            var httpRouteTag = localRootSpan.GetTag(Tags.HttpRoute);
+            var httpMethod = localRootSpan.GetTag(Tags.HttpMethod);
+            var statusCode = localRootSpan.GetTag(Tags.HttpStatusCode);
+            if (httpRouteTag == null || httpMethod == null || statusCode == null)
+            {
+                Log.Debug("Unsupported groupkey for api security {Route}, {Method}, {Status}", httpRouteTag, httpMethod, statusCode);
+                return false;
+            }
+
+            var hash = HashCode.Combine(httpRouteTag.GetHashCode(), httpMethod.GetHashCode(), statusCode.GetHashCode());
+            var now = DateTime.UtcNow;
+            if (_processedRoutes.TryGetValue(hash, out var lastProcessed))
+            {
+                if (lastProcessed - now > _minTimeBetweenReprocessTimeSpan)
+                {
+                    args.Add(AddressesConstants.WafContextProcessor, _apiSecurityAddress);
+                    _processedRoutes[hash] = now;
+                    return true;
+                }
+            }
+            else
+            {
+                // shouldn't happen so often (that's a lot of routes)
+                if (_processedRoutes.Count == MaxRoutesSize)
+                {
+                    var oldest = _processedRoutes.OrderBy(x => x.Value).First();
+                    _processedRoutes.Remove(oldest);
+                }
+
+                args.Add(AddressesConstants.WafContextProcessor, _apiSecurityAddress);
+                _processedRoutes.Add(hash, now);
+
+                return true;
+            }
         }
 
         return false;
-    }
-
-    public void ReleaseRequest()
-    {
-        if (_enabled)
-        {
-            _overheadController.ReleaseRequest();
-        }
     }
 }
