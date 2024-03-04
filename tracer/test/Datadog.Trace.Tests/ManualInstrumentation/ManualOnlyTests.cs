1e8c1d9985b46d1cdde48c3baf86fe67d18a8cfd
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-04 08:56:56 +0000

    [v3] Change manual instrumentation approach to not use reverse duck-typing (#5214)
    * Fix null ref in `GetManagedTracerVersion()`
    
    * Add benchmark for manual instrumentation
    
    * Try alternative ducktyping approach
    
    * Fix OpenTracing helpers
    
    * Revert the changes to benchmark stat types
    
    Seeing as we're not reverse duck-typing any more, we don't need to do this
    
    * Update manual Ci Visibility to not rely on reverse duck-typing
    
    This is made rather irritating by the fact we _can't_ ducktype Test as an ITest. That, coupled with the APIs that connect module + suite + test together means we need to duplicate the interfaces in their entirety.
    
    It seems to work though :tada:
    
    * Revert the snapshots to match the existing public API
    
    * Use instrumentation of DuckType.DuckCast<T> in Datadog.Trace.Manual.csproj
    
    Means we can avoid having to include the whole DuckTyping library inside Datadog.Trace.Manual
    The downside is that it's hard to test in unit tests. as everything requires auto instrumentation
    
    * Skip the duck typing tests that won't work and add some others
    
    * Simplify the implementation
    
    Instead of having wrapper ManualScope implementations etc, we can just use duck typing instead directly. This reduces one level of indirection and reduces the allocations
    
    * Redesign approach with CI Visibility
    
    - Use the standard duck typing approach instead of reverse duck typing
    - Remove the ManualTest etc
    - Move methods that we _can't_ duck type to be extension methods that we instrument
    
    * Update generated code
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>
    
    * PR feedback
    
    - Fix previous PR feedback suggestions ;)
    - Add [Instrumented] to Tracer.ctor
    - Fix merge conflict in IntegrationIdExtensions
    - Add missing telemetry integration
    - Move TracerSettingsPopulateDictionaryIntegration into correct folder, and rename to PopulateDictionaryIntegration
    - Add a unit test for SetTag
    
    * Add unit tests for the mapping between tracersettings and public API settings
    
    * PR Feedback - add helper class for serialization of integration settings
    
    * PR feedback - include initial value in OverrideValue<T> helper
    
    * PR feedback - too many braces
    
    * Fix bug introduced
    
    * Use defaults for null test suite
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>

---
+++ tracer/test/Datadog.Trace.Tests/ManualInstrumentation/ManualOnlyTests.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="ManualOnlyTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+extern alias DatadogTraceManual;
+
+using System;
+using DatadogTraceManual::Datadog.Trace.Ci;
+using FluentAssertions;
+using Xunit;
+using BenchmarkDiscreteStats = DatadogTraceManual::Datadog.Trace.Ci.BenchmarkDiscreteStats;
+using BenchmarkHostInfo = DatadogTraceManual::Datadog.Trace.Ci.BenchmarkHostInfo;
+using BenchmarkJobInfo = DatadogTraceManual::Datadog.Trace.Ci.BenchmarkJobInfo;
+using BenchmarkMeasureType = DatadogTraceManual::Datadog.Trace.Ci.BenchmarkMeasureType;
+using ManualIScope = DatadogTraceManual::Datadog.Trace.IScope;
+using ManualISpan = DatadogTraceManual::Datadog.Trace.ISpan;
+using ManualISpanContext = DatadogTraceManual::Datadog.Trace.ISpanContext;
+using ManualITest = DatadogTraceManual::Datadog.Trace.Ci.ITest;
+using ManualITestModule = DatadogTraceManual::Datadog.Trace.Ci.ITestModule;
+using ManualITestSession = DatadogTraceManual::Datadog.Trace.Ci.ITestSession;
+using ManualITestSuite = DatadogTraceManual::Datadog.Trace.Ci.ITestSuite;
+using TestParameters = DatadogTraceManual::Datadog.Trace.Ci.TestParameters;
+using TestStatus = DatadogTraceManual::Datadog.Trace.Ci.TestStatus;
+
+namespace Datadog.Trace.Tests.ManualInstrumentation;
+
+public class ManualOnlyTests
+{
+    [Fact]
+    public void CreatingAManualSpanDoesNotCrash()
+    {
+        using var scope = DatadogTraceManual::Datadog.Trace.Tracer.Instance.StartActive("manual");
+        scope.Should().BeAssignableTo<ManualIScope>().And.NotBeNull();
+
+        var span = scope.Span.Should().NotBeNull().And.BeAssignableTo<ManualISpan>().Subject;
+        span.SetException(new Exception());
+        span.SetTag("James", "Bond").Should().BeSameAs(span);
+        span.GetTag("James").Should().BeNull();
+        span.OperationName.Should().BeNullOrEmpty();
+        span.ResourceName.Should().BeNullOrEmpty();
+
+        var context = span.Context.Should().NotBeNull().And.BeAssignableTo<ManualISpanContext>().Subject;
+        context.Should().NotBeNull();
+        context.ServiceName.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public void CreatingAManualOnlyCiSessionDoesNotCrash()
+    {
+        var manualSession = TestSession.GetOrCreate("some sesssion");
+        manualSession.Should().BeAssignableTo<ManualITestSession>().And.NotBeNull();
+        manualSession.SetTag("session_key", "session_value");
+
+        var module = manualSession.CreateModule("somemodule");
+        module.Should().BeAssignableTo<ManualITestModule>().And.NotBeNull();
+        module.SetTag("module_key", "module_value");
+        module.SetErrorInfo(new Exception());
+
+        var suite = module.GetOrCreateSuite("mysuite");
+        suite.Should().BeAssignableTo<ManualITestSuite>().And.NotBeNull();
+        suite.SetTag("suite_key", "suite_value");
+
+        var test = suite.CreateTest("mytest");
+        test.Should().BeAssignableTo<ManualITest>().And.NotBeNull();
+        test.SetTag("key", "value");
+
+        test.SetParameters(new TestParameters { Arguments = new(), Metadata = new() });
+        test.SetBenchmarkMetadata(new BenchmarkHostInfo() { RuntimeVersion = "123" }, new BenchmarkJobInfo() { Description = "weeble" });
+        var stats = new BenchmarkDiscreteStats(100, 100, 100, 100, 100, 0, 0, 0, 0, 100, 100, 100);
+        test.AddBenchmarkData(BenchmarkMeasureType.ApplicationLaunch, info: "something", in stats);
+
+        test.Close(TestStatus.Pass);
+        suite.Close();
+        module.Close();
+        manualSession.Close(TestStatus.Pass);
+    }
+}
