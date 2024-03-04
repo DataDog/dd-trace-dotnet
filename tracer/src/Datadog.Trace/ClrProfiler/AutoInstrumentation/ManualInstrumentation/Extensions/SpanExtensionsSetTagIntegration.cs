a9b1097f16adb1884ff4b01d8a192f843e606f3c
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-04 08:48:33 +0000

    [v3] Implement manual tracing functionality in Datadog.Trace.Manual (#5072)
    * Add `[Instrumented]` and `[DuckTypeTarget]` attribute to make it easier to keep track of what we rely on instrumenting
    
    * Add custom instrumentation as an integration ID
    
    * Initial Datadog.Trace.Manual implementation
    
    * Add initial auto-instrumentation of Datadog.Trace.Manual
    
    * Add EventTrackingSdk
    
    * We don't expect any spans when using manual-only instrumentation
    
    * Make TracerSettings setters internal so we can set them from manual instrumentation
    
    * Add moving of settings to and from manual and automatic
    
    * Add moving of tracersettings back and forth between manual and automatic
    
    * Add support for transferring integrationsettings back and forthh between manual and automatic
    
    * Add support for exposing immutableintegrationsettings on manual side
    
    * Add additional telemetry instrumentation
    
    * Add support for CI-app's public API
    
    * Add unit tests
    
    * Update source generated code
    
    * Duplicate Datadog.Trace snapshots for Custom as a baseline
    
    * Update public API snapshots
    
    * Add Datadog.Trace.Manual to trimming XML and fix version generation
    
    * Update tracer/src/Datadog.Trace.Manual/Configuration/ImmutableTracerSettings.cs
    
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>
    
    ---------
    
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Extensions/SpanExtensionsSetTagIntegration.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="SpanExtensionsSetTagIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Proxies;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.DuckTyping;
+using Datadog.Trace.Telemetry;
+using Datadog.Trace.Telemetry.Metrics;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Extensions;
+
+/// <summary>
+/// Datadog.Trace.ISpan Datadog.Trace.SpanExtensions::SetTag(Datadog.Trace.ISpan,System.String,System.Nullable`1[System.Double]) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.SpanExtensions",
+    MethodName = "SetTag",
+    ReturnTypeName = "Datadog.Trace.ISpan",
+    ParameterTypeNames = new[] { "Datadog.Trace.ISpan", ClrNames.String, "System.Nullable`1[System.Double]" },
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class SpanExtensionsSetTagIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget, TSpan>(ref TSpan span, ref string key, ref double? value)
+    {
+        TelemetryFactory.Metrics.Record(PublicApiUsage.SpanExtensions_SetTag);
+
+        // Annoyingly, this takes an ISpan, so we have to do some duckTyping to make it work
+        if (span is IDuckType { Instance: ManualSpan { AutomaticSpan: { } duckTyped } })
+        {
+            // this is the "typical" scenario
+            duckTyped.SetTagInternal(key, value);
+        }
+        else if (span is Span autoSpan)
+        {
+            // Not likely, but technically possible for this to happen
+            autoSpan.SetTagInternal(key, value);
+        }
+        else
+        {
+            // This is a worst case, should basically never be necessary
+            // Only required if customers create a custom ISpan
+            span.DuckCast<ISpanSetTagProxy>()!.SetTag(key, value?.ToString());
+        }
+
+        // The default implementation returns the span
+        return CallTargetState.GetDefault();
+    }
+}
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Proxies/ISpanContextProxy.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="ISpanContextProxy.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Proxies;
+
+/// <summary>
+/// Duck type for custom ISpanContext implementations in Datadog.Trace.Manual
+/// </summary>
+[DuckCopy]
+// ReSharper disable once InconsistentNaming
+internal struct ISpanContextProxy
+{
+    public ulong TraceId;
+    public ulong SpanId;
+    public string? ServiceName;
+}
