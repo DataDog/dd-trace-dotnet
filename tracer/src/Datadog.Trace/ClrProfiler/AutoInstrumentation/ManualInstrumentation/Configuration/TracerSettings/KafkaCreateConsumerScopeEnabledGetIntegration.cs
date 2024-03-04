c0b24792fc4e0863ccba94d925b32c301b57cfa5
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-04 08:48:33 +0000

    [v3] Implement manual tracing functionality in Datadog.Trace.Manual (#5072)
    * Add `[Instrumented]` and `[DuckTypeTarget]` attribute to make it easier to keep track of what we rely on instrumenting
    
    * Add custom instrumentation as an integration ID
    
    * Initial Datadog.Trace.Manual implementation
    
    * Add initial auto-instrumentation of Datadog.Trace.Manual
    
    * Add EventTrackingSdk
    
    * We don't expect any spans when using manual-only instrumentation
    
    * Make TracerSettings setters internal so we can set them from manual instrumentation
    
    * Add moving of settings to and from manual and automatic
    
    * Add moving of tracersettings back and forth between manual and automatic
    
    * Add support for transferring integrationsettings back and forthh between manual and automatic
    
    * Add support for exposing immutableintegrationsettings on manual side
    
    * Add additional telemetry instrumentation
    
    * Add support for CI-app's public API
    
    * Add unit tests
    
    * Update source generated code
    
    * Duplicate Datadog.Trace snapshots for Custom as a baseline
    
    * Update public API snapshots
    
    * Add Datadog.Trace.Manual to trimming XML and fix version generation
    
    * Update tracer/src/Datadog.Trace.Manual/Configuration/ImmutableTracerSettings.cs
    
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>
    
    ---------
    
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Configuration/TracerSettings/KafkaCreateConsumerScopeEnabledGetIntegration.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="KafkaCreateConsumerScopeEnabledGetIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Telemetry;
+using Datadog.Trace.Telemetry.Metrics;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Configuration.TracerSettings;
+
+/// <summary>
+/// System.Boolean Datadog.Trace.Configuration.TracerSettings::get_KafkaCreateConsumerScopeEnabled() calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Configuration.TracerSettings",
+    MethodName = "get_KafkaCreateConsumerScopeEnabled",
+    ReturnTypeName = ClrNames.Bool,
+    ParameterTypeNames = [],
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class KafkaCreateConsumerScopeEnabledGetIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance)
+    {
+        TelemetryFactory.Metrics.Record(PublicApiUsage.TracerSettings_KafkaCreateConsumerScopeEnabled_Get);
+        return CallTargetState.GetDefault();
+    }
+}
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Tracer/GetAutomaticTracerInstanceIntegration.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="GetAutomaticTracerInstanceIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+using System;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Tracer;
+
+/// <summary>
+/// System.Object Datadog.Trace.Tracer::GetAutomaticTracerInstance() calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Tracer",
+    MethodName = "GetAutomaticTracerInstance",
+    ReturnTypeName = ClrNames.Object,
+    ParameterTypeNames = new string[0],
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class GetAutomaticTracerInstanceIntegration
+{
+    internal static CallTargetReturn<object> OnMethodEnd<TTarget>(object returnValue, Exception exception, in CallTargetState state)
+    {
+        // Used by Datadog.Trace.Manual.Tracer.Instance to create a new Datadog.Trace.Manual.Tracer instance
+        return new CallTargetReturn<object>(Datadog.Trace.Tracer.Instance);
+    }
+}
