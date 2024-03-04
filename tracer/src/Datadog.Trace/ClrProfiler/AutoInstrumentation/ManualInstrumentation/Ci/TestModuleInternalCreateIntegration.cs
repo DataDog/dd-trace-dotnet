9854237ad2bc059c7ed437da72a20824315f32d3
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-04 08:56:56 +0000

    [v3] Change manual instrumentation approach to not use reverse duck-typing (#5214)
    * Fix null ref in `GetManagedTracerVersion()`
    
    * Add benchmark for manual instrumentation
    
    * Try alternative ducktyping approach
    
    * Fix OpenTracing helpers
    
    * Revert the changes to benchmark stat types
    
    Seeing as we're not reverse duck-typing any more, we don't need to do this
    
    * Update manual Ci Visibility to not rely on reverse duck-typing
    
    This is made rather irritating by the fact we _can't_ ducktype Test as an ITest. That, coupled with the APIs that connect module + suite + test together means we need to duplicate the interfaces in their entirety.
    
    It seems to work though :tada:
    
    * Revert the snapshots to match the existing public API
    
    * Use instrumentation of DuckType.DuckCast<T> in Datadog.Trace.Manual.csproj
    
    Means we can avoid having to include the whole DuckTyping library inside Datadog.Trace.Manual
    The downside is that it's hard to test in unit tests. as everything requires auto instrumentation
    
    * Skip the duck typing tests that won't work and add some others
    
    * Simplify the implementation
    
    Instead of having wrapper ManualScope implementations etc, we can just use duck typing instead directly. This reduces one level of indirection and reduces the allocations
    
    * Redesign approach with CI Visibility
    
    - Use the standard duck typing approach instead of reverse duck typing
    - Remove the ManualTest etc
    - Move methods that we _can't_ duck type to be extension methods that we instrument
    
    * Update generated code
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>
    
    * PR feedback
    
    - Fix previous PR feedback suggestions ;)
    - Add [Instrumented] to Tracer.ctor
    - Fix merge conflict in IntegrationIdExtensions
    - Add missing telemetry integration
    - Move TracerSettingsPopulateDictionaryIntegration into correct folder, and rename to PopulateDictionaryIntegration
    - Add a unit test for SetTag
    
    * Add unit tests for the mapping between tracersettings and public API settings
    
    * PR Feedback - add helper class for serialization of integration settings
    
    * PR feedback - include initial value in OverrideValue<T> helper
    
    * PR feedback - too many braces
    
    * Fix bug introduced
    
    * Use defaults for null test suite
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Ci/TestModuleInternalCreateIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Ci/TestModuleInternalCreateIntegration.cs
@@ -9,6 +9,7 @@ using System.ComponentModel;
 using Datadog.Trace.Ci;
 using Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Ci.Proxies;
 using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.DuckTyping;
 using Datadog.Trace.Telemetry;
 using Datadog.Trace.Telemetry.Metrics;
 
@@ -39,8 +40,7 @@ public class TestModuleInternalCreateIntegration
 
     internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TReturn returnValue, Exception exception, in CallTargetState state)
     {
-        // reverse ducktype the TestModule as an ITestModule
-        var proxy = (TReturn)TestObjectsHelper<TReturn>.CreateTestModule((TestModule)state.State);
-        return new CallTargetReturn<TReturn>(proxy);
+        // Duck cast TestModule as an ITestModule
+        return new CallTargetReturn<TReturn>(state.State.DuckCast<TReturn>());
     }
 }
