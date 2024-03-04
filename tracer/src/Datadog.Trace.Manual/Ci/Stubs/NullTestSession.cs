403cc6be25d370f10b864c1de38b1130a405f104
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-04 08:56:56 +0000

    [v3] Change manual instrumentation approach to not use reverse duck-typing (#5214)
    * Fix null ref in `GetManagedTracerVersion()`
    
    * Add benchmark for manual instrumentation
    
    * Try alternative ducktyping approach
    
    * Fix OpenTracing helpers
    
    * Revert the changes to benchmark stat types
    
    Seeing as we're not reverse duck-typing any more, we don't need to do this
    
    * Update manual Ci Visibility to not rely on reverse duck-typing
    
    This is made rather irritating by the fact we _can't_ ducktype Test as an ITest. That, coupled with the APIs that connect module + suite + test together means we need to duplicate the interfaces in their entirety.
    
    It seems to work though :tada:
    
    * Revert the snapshots to match the existing public API
    
    * Use instrumentation of DuckType.DuckCast<T> in Datadog.Trace.Manual.csproj
    
    Means we can avoid having to include the whole DuckTyping library inside Datadog.Trace.Manual
    The downside is that it's hard to test in unit tests. as everything requires auto instrumentation
    
    * Skip the duck typing tests that won't work and add some others
    
    * Simplify the implementation
    
    Instead of having wrapper ManualScope implementations etc, we can just use duck typing instead directly. This reduces one level of indirection and reduces the allocations
    
    * Redesign approach with CI Visibility
    
    - Use the standard duck typing approach instead of reverse duck typing
    - Remove the ManualTest etc
    - Move methods that we _can't_ duck type to be extension methods that we instrument
    
    * Update generated code
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>
    
    * PR feedback
    
    - Fix previous PR feedback suggestions ;)
    - Add [Instrumented] to Tracer.ctor
    - Fix merge conflict in IntegrationIdExtensions
    - Add missing telemetry integration
    - Move TracerSettingsPopulateDictionaryIntegration into correct folder, and rename to PopulateDictionaryIntegration
    - Add a unit test for SetTag
    
    * Add unit tests for the mapping between tracersettings and public API settings
    
    * PR Feedback - add helper class for serialization of integration settings
    
    * PR feedback - include initial value in OverrideValue<T> helper
    
    * PR feedback - too many braces
    
    * Fix bug introduced
    
    * Use defaults for null test suite
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>

--- tracer/src/Datadog.Trace.Manual/Ci/Stubs/NullTestSession.cs
+++ tracer/src/Datadog.Trace.Manual/Ci/Stubs/NullTestSession.cs
@@ -3,19 +3,19 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#nullable enable
-
 namespace Datadog.Trace.Ci.Stubs;
 
-internal class NullTestSession(string? command, string? workingDirectory, string? framework, DateTimeOffset? startDate) : ITestSession
+internal class NullTestSession : ITestSession
 {
-    public string? Command { get; } = command;
+    public static readonly NullTestSession Instance = new();
 
-    public string? WorkingDirectory { get; } = workingDirectory;
+    public string? Command => null;
 
-    public DateTimeOffset StartTime { get; } = startDate ?? DateTimeOffset.UtcNow;
+    public string? WorkingDirectory => null;
 
-    public string? Framework { get; } = framework;
+    public DateTimeOffset StartTime => default;
+
+    public string? Framework => null;
 
     public void SetTag(string key, string? value)
     {
@@ -45,12 +45,12 @@ internal class NullTestSession(string? command, string? workingDirectory, string
 
     public Task CloseAsync(TestStatus status, TimeSpan? duration) => Task.CompletedTask;
 
-    public ITestModule CreateModule(string name)
-        => new NullTestModule(name, null, null);
+    ITestModule ITestSession.CreateModule(string name)
+        => NullTestModule.Instance;
 
-    public ITestModule CreateModule(string name, string framework, string frameworkVersion)
-        => new NullTestModule(name, framework, null);
+    ITestModule ITestSession.CreateModule(string name, string framework, string frameworkVersion)
+        => NullTestModule.Instance;
 
-    public ITestModule CreateModule(string name, string framework, string frameworkVersion, DateTimeOffset startDate)
-        => new NullTestModule(name, framework, startDate);
+    ITestModule ITestSession.CreateModule(string name, string framework, string frameworkVersion, DateTimeOffset startDate)
+        => NullTestModule.Instance;
 }
