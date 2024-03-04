9be42c45fbaa7a2e58ad363318d2a377202de65d
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-04 08:48:33 +0000

    [v3] Implement manual tracing functionality in Datadog.Trace.Manual (#5072)
    * Add `[Instrumented]` and `[DuckTypeTarget]` attribute to make it easier to keep track of what we rely on instrumenting
    
    * Add custom instrumentation as an integration ID
    
    * Initial Datadog.Trace.Manual implementation
    
    * Add initial auto-instrumentation of Datadog.Trace.Manual
    
    * Add EventTrackingSdk
    
    * We don't expect any spans when using manual-only instrumentation
    
    * Make TracerSettings setters internal so we can set them from manual instrumentation
    
    * Add moving of settings to and from manual and automatic
    
    * Add moving of tracersettings back and forth between manual and automatic
    
    * Add support for transferring integrationsettings back and forthh between manual and automatic
    
    * Add support for exposing immutableintegrationsettings on manual side
    
    * Add additional telemetry instrumentation
    
    * Add support for CI-app's public API
    
    * Add unit tests
    
    * Update source generated code
    
    * Duplicate Datadog.Trace snapshots for Custom as a baseline
    
    * Update public API snapshots
    
    * Add Datadog.Trace.Manual to trimming XML and fix version generation
    
    * Update tracer/src/Datadog.Trace.Manual/Configuration/ImmutableTracerSettings.cs
    
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>
    
    ---------
    
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/AppSec/EventTrackingSdkTrackCustomEventMetadataIntegration.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="EventTrackingSdkTrackCustomEventMetadataIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using System.ComponentModel;
+using Datadog.Trace.AppSec;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Telemetry;
+using Datadog.Trace.Telemetry.Metrics;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.AppSec;
+
+/// <summary>
+/// System.Void Datadog.Trace.AppSec.EventTrackingSdk::TrackCustomEvent(System.String,System.Collections.Generic.IDictionary`2[System.String,System.String]) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.AppSec.EventTrackingSdk",
+    MethodName = "TrackCustomEvent",
+    ReturnTypeName = ClrNames.Void,
+    ParameterTypeNames = new[] { ClrNames.String, "System.Collections.Generic.IDictionary`2[System.String,System.String]" },
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class EventTrackingSdkTrackCustomEventMetadataIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget>(string eventName, System.Collections.Generic.IDictionary<string, string> metadata)
+    {
+        TelemetryFactory.Metrics.Record(PublicApiUsage.EventTrackingSdk_TrackCustomEvent_Metadata);
+        EventTrackingSdk.TrackCustomEvent(eventName, metadata, Datadog.Trace.Tracer.Instance);
+        return CallTargetState.GetDefault();
+    }
+}
