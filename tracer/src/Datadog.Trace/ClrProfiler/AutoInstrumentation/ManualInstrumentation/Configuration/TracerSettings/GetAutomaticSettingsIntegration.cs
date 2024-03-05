cf3dbaaf81a0e0935ec60ee1093e6582b02c5cd6
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-05 10:20:13 +0000

    Update the automatic instrumentation to use ITracerSettings

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Configuration/TracerSettings/GetAutomaticSettingsIntegration.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="GetAutomaticSettingsIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+using System;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Configuration.Telemetry;
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Configuration.TracerSettings;
+
+/// <summary>
+/// Datadog.Trace.Configuration.ITracerSettings Datadog.Trace.Configuration.TracerSettings::GetAutomaticSettings(System.Boolean) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Configuration.TracerSettings",
+    MethodName = "GetAutomaticSettings",
+    ReturnTypeName = "Datadog.Trace.Configuration.ITracerSettings",
+    ParameterTypeNames = [ClrNames.Bool],
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class GetAutomaticSettingsIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget>(bool useDefaultSources)
+    {
+        var settings = useDefaultSources
+                           ? Trace.Configuration.TracerSettings.FromDefaultSourcesInternal()
+                           : new Trace.Configuration.TracerSettings(null, new ConfigurationTelemetry());
+        return new CallTargetState(scope: null, state: settings);
+    }
+
+    internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TReturn returnValue, Exception exception, in CallTargetState state)
+    {
+        // replace the null settings object with the duck typed TracerSettings
+        return new CallTargetReturn<TReturn>(state.State.DuckCast<TReturn>());
+    }
+}
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Tracer/GetAutomaticSettingsIntegration.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="GetAutomaticSettingsIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Tracer;
+
+/// <summary>
+/// Datadog.Trace.Configuration.ITracerSettings Datadog.Trace.Tracer::GetAutomaticSettings(System.Object) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Tracer",
+    MethodName = "GetAutomaticSettings",
+    ReturnTypeName = "Datadog.Trace.Configuration.ITracerSettings",
+    ParameterTypeNames = [ClrNames.Object],
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class GetAutomaticSettingsIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget>(object automaticTracer)
+    {
+        if (automaticTracer is Datadog.Trace.Tracer tracer)
+        {
+            return new CallTargetState(scope: null, state: automaticTracer);
+        }
+
+        return CallTargetState.GetDefault();
+    }
+
+    internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TReturn returnValue, Exception exception, in CallTargetState state)
+    {
+        return new CallTargetReturn<TReturn>(state.State is null ? returnValue : state.State.DuckCast<TReturn>());
+    }
+}
