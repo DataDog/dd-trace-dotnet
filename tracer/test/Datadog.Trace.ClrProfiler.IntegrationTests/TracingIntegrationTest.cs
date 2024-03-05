389818dba628ce154911f98978bd15d031d001f4
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-03-05 18:46:18 -0400

    add helpers RunSampleWithAllTracingDisabled() and RunSampleWithIntegrationDisabled()

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/TracingIntegrationTest.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/TracingIntegrationTest.cs
@@ -3,8 +3,12 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Datadog.Trace.Configuration;
 using Datadog.Trace.TestHelpers;
+using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -30,13 +34,61 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
             {
                 var result = ValidateIntegrationSpan(span, metadataSchemaVersion);
                 Assert.True(result.Success, result.ToString());
-
                 Assert.Equal(expectedServiceName, span.Service);
-                if (isExternalSpan == true)
+
+                if (isExternalSpan)
                 {
                     Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
                 }
             }
+        }
+
+        internal async Task RunSampleWithAllTracingDisabled(
+            int timeoutInMilliseconds = 500,
+            string packageVersion = "",
+            Func<MockSpan, bool> spanFilter = null)
+        {
+            SetEnvironmentVariable("DD_TRACE_ENABLED", "false");
+
+            var spans = await RunDisabled(packageVersion, timeoutInMilliseconds, spanFilter);
+
+            spans.Should().BeEmpty("no spans should be created when DD_TRACE_ENABLED=false");
+        }
+
+        internal async Task RunSampleWithIntegrationDisabled(
+            IntegrationId id,
+            int timeoutInMilliseconds = 500,
+            string packageVersion = "",
+            Func<MockSpan, bool> spanFilter = null)
+        {
+            var integrationName = id.ToStringFast();
+            SetEnvironmentVariable($"DD_TRACE_{integrationName}_ENABLED", "false");
+
+            var spans = await RunDisabled(packageVersion, timeoutInMilliseconds, spanFilter);
+
+            spans.Should().BeEmpty($"no {integrationName} spans should be created when DD_TRACE_{integrationName}_ENABLED=false");
+        }
+
+        private async Task<IEnumerable<MockSpan>> RunDisabled(
+            string packageVersion,
+            int timeoutInMilliseconds,
+            Func<MockSpan, bool> spanFilter)
+        {
+            using var agent = EnvironmentHelper.GetMockAgent();
+
+            if (spanFilter != null)
+            {
+                agent.SpanFilters.Add(spanFilter);
+            }
+
+            using var process = await RunSampleAndWaitForExit(agent, packageVersion: packageVersion);
+
+            // count: 1, to force the agent to wait until timeout. count: 0 would return immediately.
+            // assertExpectedCount: false, because we will never get any spans.
+            return agent.WaitForSpans(
+                count: 1,
+                timeoutInMilliseconds: timeoutInMilliseconds,
+                assertExpectedCount: false);
         }
     }
 }
