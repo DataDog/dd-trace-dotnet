e9292ef51346dc009f4b067507af9d7e30f7adcb
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-11 09:22:40 +0100

    Update tracer/src/Datadog.Trace/Sampling/TracerRateLimiter.cs
    Co-authored-by: Lucas Pimentel <lucas.pimentel@datadoghq.com>

--- tracer/src/Datadog.Trace/Sampling/TracerRateLimiter.cs
+++ tracer/src/Datadog.Trace/Sampling/TracerRateLimiter.cs
@@ -22,7 +22,7 @@ namespace Datadog.Trace.Sampling
         {
             if (!Volatile.Read(ref _warningWritten))
             {
-                Log.Warning<string, int, int>("Dropping trace id {TraceId} with count of {Count} for last {Interval}ms.", span.Context.RawTraceId, count, intervalMs);
+                Log.Warning<string, int, int>("Rate limiter dropped a trace ({TraceId}) after reaching {Count} traces in the last {Interval}ms.", span.Context.RawTraceId, count, intervalMs);
                 _warningWritten = true;
             }
         }
