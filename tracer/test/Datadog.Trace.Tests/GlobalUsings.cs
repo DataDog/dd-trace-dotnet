4fdcf26c98df294e7f681a8d125967069998ce03
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-03-12 12:28:21 +0100

    Simplify condition

--- tracer/test/Datadog.Trace.Tests/GlobalUsings.cs
+++ tracer/test/Datadog.Trace.Tests/GlobalUsings.cs
@@ -3,6 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#if NETFRAMEWORK || NETSTANDARD2_0 || (NETCOREAPP2_0_OR_GREATER && !NETCOREAPP3_1_OR_GREATER)
+#if !NETCOREAPP3_1_OR_GREATER
 global using Datadog.Trace.VendoredMicrosoftCode.System;
 #endif
