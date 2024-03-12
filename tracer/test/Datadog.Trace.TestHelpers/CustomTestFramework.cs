8e04c54d9ba4215219c268333dc0d28ee584e8c8
Maximo Bautista <max.bautista97@gmail.com>
2024-03-12 14:17:38 -0400

    Doing another run in parallalel

--- tracer/test/Datadog.Trace.TestHelpers/CustomTestFramework.cs
+++ tracer/test/Datadog.Trace.TestHelpers/CustomTestFramework.cs
@@ -152,12 +152,6 @@ namespace Datadog.Trace.TestHelpers
             private static bool IsParallelizationDisabled(ITestCollection collection)
             {
                 var attr = collection.CollectionDefinition?.GetCustomAttributes(typeof(CollectionDefinitionAttribute)).SingleOrDefault();
-                var isIntegrationTest = collection.DisplayName.Contains("Datadog.Trace.ClrProfiler.IntegrationTests");
-
-                if (isIntegrationTest)
-                {
-                    return true;
-                }
 
                 return attr?.GetNamedArgument<bool>(nameof(CollectionDefinitionAttribute.DisableParallelization)) is true;
             }
