f7c26c2986db214cd9567550c1593bb36e74c5b4
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-03-13 11:42:06 +0100

    Update GlobalUsings.cs

--- tracer/src/Datadog.Trace/GlobalUsings.cs
+++ tracer/src/Datadog.Trace/GlobalUsings.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 global using ThrowHelper = Datadog.Trace.Util.ThrowHelper;
 
 #if NETFRAMEWORK || NETSTANDARD2_0
