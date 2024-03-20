abf716fc557fcdc6bfa8683615a9dd29f3cac19c
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-03-20 11:03:13 +0100

    Clean dangling AgentWriter instances in unit tests (#5311)

--- tracer/test/Datadog.Trace.Tests/Sampling/TraceSamplerTests.cs
+++ tracer/test/Datadog.Trace.Tests/Sampling/TraceSamplerTests.cs
@@ -3,10 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.Sampling;
+using Datadog.Trace.TestHelpers;
 using FluentAssertions;
 using Xunit;
 
@@ -24,10 +25,10 @@ namespace Datadog.Trace.Tests.Sampling
         private static readonly Dictionary<string, float> MockAgentRates = new() { { $"service:{ServiceName},env:{Env}", FallbackRate } };
 
         [Fact]
-        public void RateLimiter_Never_Applied_For_DefaultRule()
+        public async Task RateLimiter_Never_Applied_For_DefaultRule()
         {
             var sampler = new TraceSampler(new DenyAll());
-            RunSamplerTest(
+            await RunSamplerTest(
                 sampler,
                 iterations: 500,
                 expectedAutoKeepRate: 1,
@@ -36,7 +37,7 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void RateLimiter_Denies_All_Traces()
+        public async Task RateLimiter_Denies_All_Traces()
         {
             var sampler = new TraceSampler(new DenyAll());
 
@@ -50,7 +51,7 @@ namespace Datadog.Trace.Tests.Sampling
                     resourceNamePattern: ".*",
                     tagPatterns: null));
 
-            RunSamplerTest(
+            await RunSamplerTest(
                 sampler,
                 iterations: 500,
                 expectedAutoKeepRate: 0,
@@ -59,7 +60,7 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void Keep_Everything_Rule()
+        public async Task Keep_Everything_Rule()
         {
             var sampler = new TraceSampler(new NoLimits());
 
@@ -73,7 +74,7 @@ namespace Datadog.Trace.Tests.Sampling
                     resourceNamePattern: ".*",
                     tagPatterns: null));
 
-            RunSamplerTest(
+            await RunSamplerTest(
                 sampler,
                 iterations: 500,
                 expectedAutoKeepRate: 0,
@@ -82,7 +83,7 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void Keep_Nothing_Rule()
+        public async Task Keep_Nothing_Rule()
         {
             var sampler = new TraceSampler(new NoLimits());
 
@@ -96,7 +97,7 @@ namespace Datadog.Trace.Tests.Sampling
                     resourceNamePattern: ".*",
                     tagPatterns: null));
 
-            RunSamplerTest(
+            await RunSamplerTest(
                 sampler,
                 iterations: 500,
                 expectedAutoKeepRate: 0,
@@ -105,7 +106,7 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void Keep_Half_Rule()
+        public async Task Keep_Half_Rule()
         {
             var sampler = new TraceSampler(new NoLimits());
 
@@ -119,7 +120,7 @@ namespace Datadog.Trace.Tests.Sampling
                     resourceNamePattern: ".*",
                     tagPatterns: null));
 
-            RunSamplerTest(
+            await RunSamplerTest(
                 sampler,
                 iterations: 50_000, // Higher number for lower variance
                 expectedAutoKeepRate: 0,
@@ -128,12 +129,12 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void No_Registered_Rules_Uses_Legacy_Rates()
+        public async Task No_Registered_Rules_Uses_Legacy_Rates()
         {
             var sampler = new TraceSampler(new NoLimits());
             sampler.SetDefaultSampleRates(MockAgentRates);
 
-            RunSamplerTest(
+            await RunSamplerTest(
                 sampler,
                 iterations: 50_000, // Higher number for lower variance
                 expectedAutoKeepRate: FallbackRate,
@@ -142,10 +143,10 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void Choose_Between_Sampling_Mechanisms()
+        public async Task Choose_Between_Sampling_Mechanisms()
         {
             var settings = new TracerSettings { ServiceName = ServiceName };
-            var tracer = new Tracer(settings, agentWriter: null, sampler: null, scopeManager: null, statsd: null);
+            await using var tracer = TracerHelper.CreateWithFakeAgent(settings);
 
             using var scope = (Scope)tracer.StartActive(OperationName);
             scope.Span.Context.TraceContext.Environment = Env;
@@ -165,7 +166,7 @@ namespace Datadog.Trace.Tests.Sampling
             mechanism2.Should().Be(SamplingMechanism.AgentRate);
         }
 
-        private void RunSamplerTest(
+        private async Task RunSamplerTest(
             ITraceSampler sampler,
             int iterations,
             float expectedAutoKeepRate,
@@ -177,7 +178,7 @@ namespace Datadog.Trace.Tests.Sampling
             var userKeeps = 0;
 
             var settings = new TracerSettings { ServiceName = ServiceName };
-            var tracer = new Tracer(settings, agentWriter: null, sampler: null, scopeManager: null, statsd: null);
+            await using var tracer = TracerHelper.CreateWithFakeAgent(settings);
 
             while (sampleSize-- > 0)
             {
