39c941656a5cc12c2bab5dd073734af28dd41415
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-03-20 15:33:02 +0100

    Update libdatadog

--- profiler/src/ProfilerEngine/Datadog.Profiler.Native/CrashReporting.cpp
+++ profiler/src/ProfilerEngine/Datadog.Profiler.Native/CrashReporting.cpp
@@ -71,12 +71,12 @@ void CrashReporting::ReportCrash(char** frames, int count, char* threadId)
 
     const std::string endpointUrl = "file:///tmp/crash.txt";
 
-    auto endpoint = ddog_endpoint_from_url(libdatadog::FfiHelper::StringToCharSlice(endpointUrl));
+    auto endpoint = ddog_prof_Endpoint_agent(libdatadog::FfiHelper::StringToCharSlice(endpointUrl));
 
     const std::string stdErr = "/tmp/crash_stderr.txt";
     const std::string stdOut = "/tmp/crash_stdout.txt";
 
-    result = ddog_crashinfo_upload_to_endpoint(crashInfo, *endpoint, 30);
+    result = ddog_crashinfo_upload_to_endpoint(crashInfo, endpoint, 30);
 
     if (result.tag == DDOG_PROF_CRASHTRACKER_RESULT_ERR)
     {
@@ -92,6 +92,5 @@ void CrashReporting::ReportCrash(char** frames, int count, char* threadId)
     delete[] stackFrames;
     delete[] stackFrameNames;
 
-    ddog_endpoint_drop(endpoint);
     ddog_crashinfo_drop(crashInfo);
 }
