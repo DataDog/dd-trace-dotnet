9345ecc1454c98a364d5c12cebaeddbecde13a45
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-03-25 17:57:19 -0400

    check for null TraceContext

--- tracer/src/Datadog.Trace/Sampling/CustomSamplingRule.cs
+++ tracer/src/Datadog.Trace/Sampling/CustomSamplingRule.cs
@@ -129,7 +129,9 @@ namespace Datadog.Trace.Sampling
 
         public float GetSamplingRate(Span span)
         {
-            var rootSpan = span.Context.TraceContext.RootSpan ?? span;
+            // span.Context.TraceContext can be null only in tests,
+            // where we sometimes create "detached" spans
+            var rootSpan = span.Context.TraceContext?.RootSpan ?? span;
             rootSpan.SetMetric(Metrics.SamplingRuleDecision, _samplingRate);
             return _samplingRate;
         }
