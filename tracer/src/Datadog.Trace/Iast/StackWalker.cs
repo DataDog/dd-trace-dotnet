7b89971c91df16c6013188537f4afcfb953d5e5b
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-26 15:45:36 +0000

    [v3] Change `netstandard2.0` target to `netcoreapp2.1` i.e. drop `netcoreapp2.0` (#5260)
    * Change target framework for instrumentation from netstandard2.0 -> netcoreapp2.1
    
    * Update native references to netstandard2.0
    
    * Fix problem with System.CodeDom lying about netstandard2.0 support
    
    * Move generated code to correct folder
    
    * Fix Nuke build to expect netcoreapp2.1
    
    * Fix areas that are actively broken by the switch
    
    Mostly #ifs that were using NETCOREAPP or where we were using ^1 or ..
    
    * Update MSI from netstandard2.0 -> netcoreapp2.1
    
    * Replace some code paths with NETCOREAPP
    
    As we can use them in all .NET Core FXs
    
    * Replace more netstandard2.0 cases
    
    * Update assembly references changed by changing the target reference
    
    * Explicitly add reference to System.Runtime.CompilerServices.Unsafe
    
    * Update _OR_GREATER checks
    
    * Don't use built-in Unsafe.As in < .NET Core 3.1
    
    This isn't built-in, it's part of the "platform extensions", so it's not available
    
    * Revert "Explicitly add reference to System.Runtime.CompilerServices.Unsafe"
    
    This reverts commit 1f64e2533b7fd9bc42be333ed1d04e4bc878eda2.
    
    * Update assembly references
    
    * Fix rebasing issues
    
    * Fix rebase problem
    
    * Fix incorrect directive in AWS SQS integration
    
    * Update tracer/src/Datadog.Trace/Propagators/W3CTraceContextPropagator.cs
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>
    
    * Extract string.Concat equivalent methods for .NET Core 2.1 to helper
    
    * Fix rebasing issue
    
    * Fix weird issue in unit tests
    
    * [v3] Ensure coverage collector targets netstandard2.0 (#5334)
    
    * Split CIVisibilitySettings in two so we can link into the coverage collector
    
    * Split DatadogLoggingFactory in two so we can link into the coverage collector
    
    * Remove inprocess collector that wouldn't work any more
    
    * Remove direct references to Datadog.Trace types
    
    * Revert collector to targetting netstandard2.0 again
    
    * Replace Datadog.Trace reference with linked files + stubs
    
    * Update DataCollectorLogger to use the shared logging components
    
    * Avoid issues with duplicate referenced code
    
    * Add unit tests for DatadogTraceConstants
    
    * Fix issues merging the new file coverage algorithm
    
    * Try fix more issues merging the code coverage changes
    
    ---------
    
    Co-authored-by: Kevin Gosse <kevin.gosse@datadoghq.com>

--- tracer/src/Datadog.Trace/Iast/StackWalker.cs
+++ tracer/src/Datadog.Trace/Iast/StackWalker.cs
@@ -91,7 +91,7 @@ internal static class StackWalker
     {
         foreach (var assemblyToSkip in AssemblyNamesToSkip)
         {
-#if NETCOREAPP3_1_OR_GREATER
+#if NETCOREAPP
             if (assemblyToSkip.EndsWith('.'))
 #else
             if (assemblyToSkip.EndsWith("."))
