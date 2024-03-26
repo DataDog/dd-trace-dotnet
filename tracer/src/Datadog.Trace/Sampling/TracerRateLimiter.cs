6299d0a5e9de375a37643d0c019a5a876d7266a8
Andrew Lock <andrew.lock@datadoghq.com>
2024-03-26 13:42:12 +0000

    Add rate limit for the log written by `TraceRateLimiter` (#5229)
    * Add rate limit for trace rate log
    
    * Update tracer/src/Datadog.Trace/Sampling/TracerRateLimiter.cs
    
    Co-authored-by: Lucas Pimentel <lucas.pimentel@datadoghq.com>
    
    ---------
    
    Co-authored-by: Lucas Pimentel <lucas.pimentel@datadoghq.com>

--- tracer/src/Datadog.Trace/Sampling/TracerRateLimiter.cs
+++ tracer/src/Datadog.Trace/Sampling/TracerRateLimiter.cs
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System.Threading;
 using Datadog.Trace.Logging;
 
 namespace Datadog.Trace.Sampling
@@ -10,6 +11,7 @@ namespace Datadog.Trace.Sampling
     internal class TracerRateLimiter : RateLimiter
     {
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<TracerRateLimiter>();
+        private bool _warningWritten;
 
         public TracerRateLimiter(int? maxTracesPerInterval)
             : base(maxTracesPerInterval)
@@ -18,7 +20,21 @@ namespace Datadog.Trace.Sampling
 
         public override void OnDisallowed(Span span, int count, int intervalMs, int maxTracesPerInterval)
         {
-            Log.Warning<string, int, int>("Dropping trace id {TraceId} with count of {Count} for last {Interval}ms.", span.Context.RawTraceId, count, intervalMs);
+            if (!Volatile.Read(ref _warningWritten))
+            {
+                Log.Warning<string, int, int>("Rate limiter dropped a trace ({TraceId}) after reaching {Count} traces in the last {Interval}ms.", span.Context.RawTraceId, count, intervalMs);
+                _warningWritten = true;
+            }
+        }
+
+        public override void OnRefresh(int intervalMs, int checksInLastInterval, int allowedInLastInterval)
+        {
+            if (Volatile.Read(ref _warningWritten))
+            {
+                Log.Warning<int, int, int>("Trace rate limit interval reset: Allowed {Allowed} traces out of {Checked} in last {Interval}ms.", allowedInLastInterval, checksInLastInterval, intervalMs);
+            }
+
+            _warningWritten = false;
         }
 
         public override void OnFinally(Span span)
