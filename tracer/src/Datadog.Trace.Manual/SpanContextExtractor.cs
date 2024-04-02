b3064b67fe63b1a399dab521dcc67861be809c79
Andrew Lock <andrew.lock@datadoghq.com>
2024-04-02 09:21:43 +0100

    Delete the ISpanContextExtractor (#5355)

--- tracer/src/Datadog.Trace.Manual/SpanContextExtractor.cs
+++ tracer/src/Datadog.Trace.Manual/SpanContextExtractor.cs
@@ -9,8 +9,13 @@ using Datadog.Trace.SourceGenerators;
 
 namespace Datadog.Trace
 {
-    /// <inheritdoc />
-    public sealed class SpanContextExtractor : ISpanContextExtractor
+    /// <summary>
+    /// The <see cref="SpanContextExtractor"/> is responsible for extracting <see cref="ISpanContext"/> in the rare cases
+    /// where the Tracer couldn't propagate it itself. This can happen for instance when libraries add an extra
+    /// layer above the instrumented ones (eg consuming Kafka messages and enqueuing them prior to generate a span).
+    /// When enabled (and present in the headers) also used to set data streams monitoring checkpoints.
+    /// </summary>
+    public sealed class SpanContextExtractor
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SpanContextExtractor"/> class
@@ -20,7 +25,13 @@ namespace Datadog.Trace
         {
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a SpanContext carrier and a function to access the values, this method will extract the <see cref="ISpanContext"/> if any.
+        /// </summary>
+        /// <param name="carrier">The carrier of the SpanContext. Often a header (http, kafka message header...)</param>
+        /// <param name="getter">Given a key name, returns values from the carrier</param>
+        /// <typeparam name="TCarrier">Type of the carrier</typeparam>
+        /// <returns>A potentially null Datadog <see cref="ISpanContext"/></returns>
         [Instrumented]
         public ISpanContext? Extract<TCarrier>(TCarrier carrier, Func<TCarrier, string, IEnumerable<string?>> getter)
             => null;
