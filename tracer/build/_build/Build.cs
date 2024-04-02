0d826256a98769a21e809d27505e59a5456a1fae
Andrew Lock <andrew.lock@datadoghq.com>
2024-04-02 14:57:38 +0100

    Stop building samples in integration tests

--- tracer/build/_build/Build.cs
+++ tracer/build/_build/Build.cs
@@ -223,9 +223,9 @@ partial class Build : NukeBuild
         .Unlisted()
         .Requires(() => IsWin)
         .Description("Builds the integration tests for Windows")
-        .DependsOn(CompileDependencyLibs)
+        // .DependsOn(CompileDependencyLibs)
         .DependsOn(CompileManagedTestHelpers)
-        .DependsOn(CompileSamplesWindows)
+        // .DependsOn(CompileSamplesWindows)
         .DependsOn(CompileIntegrationTests)
         .DependsOn(BuildRunnerTool);
 
@@ -233,7 +233,7 @@ partial class Build : NukeBuild
         .Unlisted()
         .Requires(() => IsWin)
         .Description("Builds the ASP.NET integration tests for Windows")
-        .DependsOn(CompileDependencyLibs)
+        // .DependsOn(CompileDependencyLibs)
         .DependsOn(CompileManagedTestHelpers)
         .DependsOn(PublishIisSamples)
         .DependsOn(CompileIntegrationTests);
@@ -272,11 +272,11 @@ partial class Build : NukeBuild
     Target BuildLinuxIntegrationTests => _ => _
         .Requires(() => !IsWin)
         .Description("Builds the linux integration tests")
-        .DependsOn(CompileDependencyLibs)
-        .DependsOn(CompileRegressionDependencyLibs)
+        // .DependsOn(CompileDependencyLibs)
+        // .DependsOn(CompileRegressionDependencyLibs)
         .DependsOn(CompileManagedTestHelpers)
-        .DependsOn(CompileSamplesLinuxOrOsx)
-        .DependsOn(CompileMultiApiPackageVersionSamples)
+        // .DependsOn(CompileSamplesLinuxOrOsx)
+        // .DependsOn(CompileMultiApiPackageVersionSamples)
         .DependsOn(CompileLinuxOrOsxIntegrationTests)
         .DependsOn(CompileLinuxDdDotnetIntegrationTests)
         .DependsOn(BuildRunnerTool)
@@ -292,11 +292,11 @@ partial class Build : NukeBuild
     Target BuildOsxIntegrationTests => _ => _
         .Requires(() => IsOsx)
         .Description("Builds the osx integration tests")
-        .DependsOn(CompileDependencyLibs)
-        .DependsOn(CompileRegressionDependencyLibs)
+        // .DependsOn(CompileDependencyLibs)
+        // .DependsOn(CompileRegressionDependencyLibs)
         .DependsOn(CompileManagedTestHelpers)
-        .DependsOn(CompileSamplesLinuxOrOsx)
-        .DependsOn(CompileMultiApiPackageVersionSamples)
+        // .DependsOn(CompileSamplesLinuxOrOsx)
+        // .DependsOn(CompileMultiApiPackageVersionSamples)
         .DependsOn(CompileLinuxOrOsxIntegrationTests)
         .DependsOn(BuildRunnerTool)
         .DependsOn(CopyServerlessArtifacts);
