5fbe3b7ad84c9dde997ac859dcac9dfe620e8cc6
Tony Redondo <tony.redondo@datadoghq.com>
2024-04-03 16:05:59 +0200

    [Auto instrumentation generator] Add support for nested types (#5382)
    * [Auto instrumentation generator] Add support for nested types
    
    * Change Generic Type names to something more meaningful
    
    * Remove comments
    
    * Add nullable to parameters and returnvalues

--- tracer/src/Datadog.AutoInstrumentation.Generator/Resources/Data/OnMethodBeginStatic.cs
+++ tracer/src/Datadog.AutoInstrumentation.Generator/Resources/Data/OnMethodBeginStatic.cs
@@ -1,8 +1,3 @@
-    /// <summary>
-    /// OnMethodBegin callback
-    /// </summary>
-    /// <typeparam name="TTarget">Type of the target</typeparam>$(TArgsTypesTypeParamDocumentation)$(TArgsTypesParamDocumentation)
-    /// <returns>Calltarget state value</returns>
     internal static CallTargetState OnMethodBegin<TTarget$(TArgsTypes)>($(TArgsParameters))$(TArgsConstraint)
     {
         return CallTargetState.GetDefault();
