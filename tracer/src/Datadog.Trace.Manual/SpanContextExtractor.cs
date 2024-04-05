8253ff5261c47ba7fa757ee7eedf7f48ba430b10
Raphaël Vandon <raphael.vandon@datadog.com>
2024-04-05 13:48:55 +0200

    [v3] Add DSM checkpoints to the public inject/extract API (#5128)
    * add DSM checkpointing to context injection & extraction API
    
    also add tracer.manual to osx slnf
    
    * improved exception throwing on null args

--- tracer/src/Datadog.Trace.Manual/SpanContextExtractor.cs
+++ tracer/src/Datadog.Trace.Manual/SpanContextExtractor.cs
@@ -10,10 +10,10 @@ using Datadog.Trace.SourceGenerators;
 namespace Datadog.Trace
 {
     /// <summary>
-    /// The <see cref="SpanContextExtractor"/> is responsible for extracting <see cref="ISpanContext"/> in the rare cases
-    /// where the Tracer couldn't propagate it itself. This can happen for instance when libraries add an extra
-    /// layer above the instrumented ones (eg consuming Kafka messages and enqueuing them prior to generate a span).
-    /// When enabled (and present in the headers) also used to set data streams monitoring checkpoints.
+    /// The <see cref="SpanContextExtractor"/> is responsible for extracting <see cref="ISpanContext"/> in the rare cases where the Tracer couldn't propagate it itself.
+    /// This can happen for instance when libraries add an extra layer above the instrumented ones
+    /// (eg consuming Kafka messages and enqueuing them prior to generate a span).
+    /// When messageType and target are specified, also used to set data streams monitoring checkpoints (if enabled).
     /// </summary>
     public sealed class SpanContextExtractor
     {
@@ -26,14 +26,30 @@ namespace Datadog.Trace
         }
 
         /// <summary>
-        /// Given a SpanContext carrier and a function to access the values, this method will extract the <see cref="ISpanContext"/> if any.
+        /// Given a SpanContext carrier and a function to access the values, this method will extract the <see cref="ISpanContext"/> if any
         /// </summary>
         /// <param name="carrier">The carrier of the SpanContext. Often a header (http, kafka message header...)</param>
-        /// <param name="getter">Given a key name, returns values from the carrier</param>
+        /// <param name="getter">Given a key name, returns values from the carrier. Should return an empty collection if the requested key is not present.</param>
         /// <typeparam name="TCarrier">Type of the carrier</typeparam>
         /// <returns>A potentially null Datadog <see cref="ISpanContext"/></returns>
         [Instrumented]
         public ISpanContext? Extract<TCarrier>(TCarrier carrier, Func<TCarrier, string, IEnumerable<string?>> getter)
+            => null;
+
+        /// <summary>
+        /// Given a SpanContext carrier and a function to access the values, this method will extract the SpanContext
+        /// and the PathwayContext, and will set a DataStreams Monitoring checkpoint if enabled.
+        /// You should only call <see cref="ExtractIncludingDsm{TCarrier}"/> once on the message <paramref name="carrier"/>. Calling
+        /// multiple times may lead to incorrect stats when using Data Streams Monitoring.
+        /// </summary>
+        /// <param name="carrier">The carrier of the SpanContext. Often a header (http, kafka message header...)</param>
+        /// <param name="getter">Given a key name, returns values from the carrier. Should return an empty collection if the requested key is not present.</param>
+        /// <param name="messageType">For Data Streams Monitoring: The type of messaging system where the message is coming from.</param>
+        /// <param name="source">For Data Streams Monitoring: The queue or topic where the message is coming from.</param>
+        /// <typeparam name="TCarrier">Type of the carrier</typeparam>
+        /// <returns>A potentially null Datadog SpanContext</returns>
+        [Instrumented]
+        public ISpanContext? ExtractIncludingDsm<TCarrier>(TCarrier carrier, Func<TCarrier, string, IEnumerable<string?>> getter, string messageType, string source)
             => null;
     }
 }
