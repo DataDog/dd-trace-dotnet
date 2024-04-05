8606df5481472407a3c405ec5322932fb50eccc3
Anna <anna.yafi@datadoghq.com>
2024-04-05 11:10:36 +0200

    [ASM] Fix nullreference exception escalation on response body instrumentation for NET Fx (#5365)
    * add null checks
    
    * nullable method
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>
    
    * Answer to comments
    
    * last fixes and nullability file
    
    ---------
    
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/src/Datadog.Trace/AspNet/SharedItems.cs
+++ tracer/src/Datadog.Trace/AspNet/SharedItems.cs
@@ -2,7 +2,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
-
+#nullable enable
 #if NETFRAMEWORK
 
 using System;
@@ -19,7 +19,7 @@ namespace Datadog.Trace.AspNet
 
         internal static void PushScope(HttpContext context, string key, Scope item)
         {
-            if (context == null)
+            if (context is null)
             {
                 return;
             }
@@ -43,11 +43,11 @@ namespace Datadog.Trace.AspNet
             }
         }
 
-        internal static Scope TryPopScope(HttpContext context, string key) => ExtractScope(context, key, Pop);
+        internal static Scope? TryPopScope(HttpContext context, string key) => ExtractScope(context, key, Pop);
 
-        internal static Scope TryPeekScope(HttpContext context, string key) => ExtractScope(context, key, Peek);
+        internal static Scope? TryPeekScope(HttpContext context, string key) => ExtractScope(context, key, Peek);
 
-        private static Scope ExtractScope(HttpContext context, string key, Func<Stack<Scope>, Scope> getter)
+        private static Scope? ExtractScope(HttpContext context, string key, Func<Stack<Scope>, Scope> getter)
         {
             var item = context?.Items[key];
             if (item is Scope storedScope)
--- tracer/test/test-applications/security/aspnet/Samples.Security.WebApi/Controllers/HomeController.cs
+++ tracer/test/test-applications/security/aspnet/Samples.Security.WebApi/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Results;
 using Samples.AspNetMvc5.Models;
 using Samples.Security.AspNetMvc5.Models;
 
@@ -28,6 +30,20 @@ namespace Samples.Security.WebApi.Controllers
         public IHttpActionResult EmptyModel([FromBody] ApiSecurityModel model)
         {
             return Json<ApiSecurityModel>(null);
+        }
+        
+        [AcceptVerbs("GET")]
+        [Route("api/home/null-action/{pathparam}/{pathparam2}")]
+        public object NullAction(string pathparam, string pathparam2)
+        {
+            return null;
+        }
+        
+        [AcceptVerbs("GET")]
+        [Route("api/home/null-action-async/{pathparam}/{pathparam2}")]
+        public Task<object> NullActionAsync(string pathparam, string pathparam2)
+        {
+            return Task.FromResult<object>(null);
         }
     }
 }
