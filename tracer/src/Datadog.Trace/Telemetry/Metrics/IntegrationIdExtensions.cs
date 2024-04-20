5059d4f0fc762a09ede6069dd33e5716ec42e056
Tony Redondo <tony.redondo@datadoghq.com>
2024-04-21 00:06:02 +0200

    fix tests

--- tracer/src/Datadog.Trace/Telemetry/Metrics/IntegrationIdExtensions.cs
+++ tracer/src/Datadog.Trace/Telemetry/Metrics/IntegrationIdExtensions.cs
@@ -78,6 +78,7 @@ internal static class IntegrationIdExtensions
             IntegrationId.ReflectionInjection => MetricTags.IntegrationName.ReflectionInjection,
             IntegrationId.Xss => MetricTags.IntegrationName.Xss,
             IntegrationId.NHibernate => MetricTags.IntegrationName.NHibernate,
+            IntegrationId.DotnetTest => MetricTags.IntegrationName.DotnetTest,
             _ => throw new InvalidOperationException($"Unknown IntegrationID {integrationId}"), // dangerous, but we test it will never be called
         };
 }
