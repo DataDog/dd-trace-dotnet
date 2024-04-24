45271eba92d01003cc5a75a7282a18c12e091f4b
Tony Redondo <tony.redondo@datadoghq.com>
2024-04-24 14:45:23 +0200

    CodeCoverageAdapterInjectorTests.cs

--- tracer/test/Datadog.Trace.Tools.Runner.Tests/CodeCoverageAdapterInjectorTests.cs
+++ tracer/test/Datadog.Trace.Tools.Runner.Tests/CodeCoverageAdapterInjectorTests.cs
@@ -38,11 +38,11 @@ public class CodeCoverageAdapterInjectorTests
             "-target:VSTest",
             "-nodereuse:false",
             "-nologo",
-            "-property:VSTestTestAdapterPath=\"TempPath\"",
-            "-property:VSTestCollect=\"DatadogCoverage\"",
             "-property:TargetFramework=net8.0",
             "-property:VSTestArtifactsProcessingMode=collect",
-            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e"
+            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e",
+            "-property:VSTestCollect=\"DatadogCoverage\"",
+            "-property:VSTestTestAdapterPath=\"TempPath\"",
         ];
         yield return [example1, example1Expected];
 
@@ -61,12 +61,12 @@ public class CodeCoverageAdapterInjectorTests
             "-target:VSTest",
             "-nodereuse:false",
             "-nologo",
-            "-property:VSTestTestAdapterPath=\"TempPath\"",
-            "-property:VSTestCollect=\"DatadogCoverage\"",
             "-property:TargetFramework=net8.0",
             "-property:VSTestCLIRunSettings=\"my;prop\"",
             "-property:VSTestArtifactsProcessingMode=collect",
-            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e"
+            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e",
+            "-property:VSTestCollect=\"DatadogCoverage\"",
+            "-property:VSTestTestAdapterPath=\"TempPath\""
         ];
         yield return [example2, example2Expected];
 
@@ -85,11 +85,11 @@ public class CodeCoverageAdapterInjectorTests
             "-target:VSTest",
             "-nodereuse:false",
             "-nologo",
-            "-property:VSTestTestAdapterPath=\"TempPath\"",
             "-property:VSTestCollect=\"MyCustomCollector;DatadogCoverage\"",
             "-property:TargetFramework=net8.0",
             "-property:VSTestArtifactsProcessingMode=collect",
-            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e"
+            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e",
+            "-property:VSTestTestAdapterPath=\"TempPath\"",
         ];
         yield return [example3, example3Expected];
 
@@ -146,12 +146,12 @@ public class CodeCoverageAdapterInjectorTests
             "-target:VSTest",
             "-nodereuse:false",
             "-nologo",
-            "-property:VSTestTestAdapterPath=\"TempPath\"",
             "-property:VSTestCollect=\"MyCustomCollector;DatadogCoverage\"",
             "-property:TargetFramework=net8.0",
             "-property:VSTestCLIRunSettings=\"MyProp=true;NewProp=false\"",
             "-property:VSTestArtifactsProcessingMode=collect",
-            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e"
+            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e",
+            "-property:VSTestTestAdapterPath=\"TempPath\"",
         ];
         yield return [example6, example6Expected];
 
@@ -171,12 +171,40 @@ public class CodeCoverageAdapterInjectorTests
             "-nodereuse:false",
             "-nologo",
             "-property:VSTestTestAdapterPath=\"MyCustomCollectorPath;TempPath\"",
-            "-property:VSTestCollect=\"DatadogCoverage\"",
             "-property:TargetFramework=net8.0",
             "-property:VSTestArtifactsProcessingMode=collect",
-            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e"
+            "-property:VSTestSessionCorrelationId=44899_366ec916-8e2d-4410-af60-36466bee387e",
+            "-property:VSTestCollect=\"DatadogCoverage\"",
         ];
         yield return [example7, example7Expected];
+    }
+
+    public static IEnumerable<object[]> VsConsoleTestData()
+    {
+        string[] emptyStringArray = [string.Empty];
+        yield return [emptyStringArray, emptyStringArray.Concat(["/TestAdapterPath:\"TempPath\"", "/Collect:DatadogCoverage"])];
+
+        string[] spaceStringArray = [" "];
+        yield return [spaceStringArray, spaceStringArray.Concat(["/TestAdapterPath:\"TempPath\"", "/Collect:DatadogCoverage"])];
+
+        string[] example1 = ["Samples.dll"];
+        yield return [example1, example1.Concat(["/TestAdapterPath:\"TempPath\"", "/Collect:DatadogCoverage"])];
+
+        string[] example2 = ["Samples.dll", "--", "DataCollectorSettings.Enabled=true"];
+        string[] example2Expected = ["Samples.dll", "/TestAdapterPath:\"TempPath\"", "/Collect:DatadogCoverage", "--", "DataCollectorSettings.Enabled=true"];
+        yield return [example2, example2Expected];
+
+        string[] example3 = ["Samples.dll", "/Collect:MyCollector"];
+        string[] example3Expected = ["Samples.dll", "/Collect:MyCollector", "/Collect:DatadogCoverage", "/TestAdapterPath:\"TempPath\""];
+        yield return [example3, example3Expected];
+
+        string[] example4 = ["Samples.dll", "/Collect:MyCollector", "/TestAdapterPath:c:\\temp"];
+        string[] example4Expected = ["Samples.dll", "/Collect:MyCollector", "/Collect:DatadogCoverage", "/TestAdapterPath:c:\\temp", "/TestAdapterPath:\"TempPath\""];
+        yield return [example4, example4Expected];
+
+        string[] example5 = ["Samples.dll", "/Collect:MyCollector", "/TestAdapterPath:c:\\temp", "--", "DataCollectorSettings.Enabled=true"];
+        string[] example5Expected = ["Samples.dll", "/Collect:MyCollector", "/Collect:DatadogCoverage", "/TestAdapterPath:c:\\temp", "/TestAdapterPath:\"TempPath\"", "--", "DataCollectorSettings.Enabled=true"];
+        yield return [example5, example5Expected];
     }
 
     [Theory]
@@ -190,7 +218,23 @@ public class CodeCoverageAdapterInjectorTests
 
         var modifiedArgs = (IEnumerable<string>)new List<string>(args);
         DotnetCommon.InjectCodeCoverageCollectorToDotnetTest(ref modifiedArgs);
-        modifiedArgs.Should().BeEquivalentTo(expectedArgs);
+        modifiedArgs.Should().Equal(expectedArgs);
+
+        EnvironmentHelpers.SetEnvironmentVariable(ConfigurationKeys.CIVisibility.CodeCoverageCollectorPath, originalValue);
+    }
+
+    [Theory]
+    [MemberData(nameof(VsConsoleTestData))]
+    public void InjectCodeCoverageCollectorToVsConsoleTest(string[] args, string[] expectedArgs)
+    {
+        var originalValue = EnvironmentHelpers.GetEnvironmentVariable(ConfigurationKeys.CIVisibility.CodeCoverageCollectorPath);
+        EnvironmentHelpers.SetEnvironmentVariable(ConfigurationKeys.CIVisibility.CodeCoverageCollectorPath, "TempPath");
+
+        using var scope = new AssertionScope();
+
+        var modifiedArgs = new List<string>(args).ToArray();
+        DotnetCommon.InjectCodeCoverageCollectorToVsConsoleTest(ref modifiedArgs);
+        modifiedArgs.Should().Equal(expectedArgs);
 
         EnvironmentHelpers.SetEnvironmentVariable(ConfigurationKeys.CIVisibility.CodeCoverageCollectorPath, originalValue);
     }
