64b3278bdcdef07345e10231f02b53890620a2c8
Andrew Lock <andrew.lock@datadoghq.com>
2024-05-01 11:30:10 +0100

    Rebasing fixes

--- tracer/src/Datadog.Trace/Generated/net461/Datadog.Trace.SourceGenerators/TelemetryMetricGenerator/MetricsTelemetryCollector_CountShared.g.cs
+++ tracer/src/Datadog.Trace/Generated/net461/Datadog.Trace.SourceGenerators/TelemetryMetricGenerator/MetricsTelemetryCollector_CountShared.g.cs
@@ -11,7 +11,7 @@ using System.Threading;
 namespace Datadog.Trace.Telemetry;
 internal partial class MetricsTelemetryCollector
 {
-    private const int CountSharedLength = 216;
+    private const int CountSharedLength = 225;
 
     /// <summary>
     /// Creates the buffer for the <see cref="Datadog.Trace.Telemetry.Metrics.CountShared" /> values.
@@ -233,6 +233,15 @@ internal partial class MetricsTelemetryCollector
             new(new[] { "integration_name:dotnettest", "error_type:duck_typing" }),
             new(new[] { "integration_name:dotnettest", "error_type:invoker" }),
             new(new[] { "integration_name:dotnettest", "error_type:execution" }),
+            new(new[] { "integration_name:selenium", "error_type:duck_typing" }),
+            new(new[] { "integration_name:selenium", "error_type:invoker" }),
+            new(new[] { "integration_name:selenium", "error_type:execution" }),
+            new(new[] { "integration_name:directorylistingleak", "error_type:duck_typing" }),
+            new(new[] { "integration_name:directorylistingleak", "error_type:invoker" }),
+            new(new[] { "integration_name:directorylistingleak", "error_type:execution" }),
+            new(new[] { "integration_name:sessiontimeout", "error_type:duck_typing" }),
+            new(new[] { "integration_name:sessiontimeout", "error_type:invoker" }),
+            new(new[] { "integration_name:sessiontimeout", "error_type:execution" }),
             new(new[] { "integration_name:datadogtracemanual", "error_type:duck_typing" }),
             new(new[] { "integration_name:datadogtracemanual", "error_type:invoker" }),
             new(new[] { "integration_name:datadogtracemanual", "error_type:execution" }),
@@ -244,7 +253,7 @@ internal partial class MetricsTelemetryCollector
     /// It is equal to the cardinality of the tag combinations (or 1 if there are no tags)
     /// </summary>
     private static int[] CountSharedEntryCounts { get; }
-        = new int[]{ 216, };
+        = new int[]{ 225, };
 
     public void RecordCountSharedIntegrationsError(Datadog.Trace.Telemetry.Metrics.MetricTags.IntegrationName tag1, Datadog.Trace.Telemetry.Metrics.MetricTags.InstrumentationError tag2, int increment = 1)
     {
