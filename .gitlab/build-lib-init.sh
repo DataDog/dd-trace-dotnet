f1778e697b888a7f83fea751cbd414e858c36c95
Andrew Lock <andrew.lock@datadoghq.com>
2024-05-12 16:37:23 +0100

    Fix and consolidate lib-init tag generation into dynamic pipeline

---
+++ .gitlab/build-lib-init.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+set -e
+
+# Safety checks to make sure we have required values
+if [ -z "$CI_COMMIT_TAG" ]; then
+  echo "Error: CI_COMMIT_TAG was not provided"
+  exit 1
+fi
+
+if [ -z "$CI_COMMIT_SHA" ]; then
+  echo "Error: CI_COMMIT_SHA was not provided"
+  exit 1
+fi
+
+if [ -z "$IMG_SOURCES_BASE" ]; then
+  echo "Error: IMG_SOURCES_BASE. This should be set to the full source docker image, excluding the tag name, e.g. ghcr.io/datadog/dd-trace-dotnet/dd-lib-dotnet-init"
+  exit 1
+fi
+
+if [ -z "$IMG_DESTINATION_BASE" ]; then
+  echo "Error: IMG_DESTINATION_BASE. This should be set to the destination docker image, excluding the tag name, e.g. dd-lib-dotnet-init"
+  exit 1
+fi
+
+if [ -z "$PUBLIC_IMAGES_PIPELINE_PROJECT" ]; then
+  echo "Error: PUBLIC_IMAGES_PIPELINE_PROJECT. This should be set to the public-images repo, DataDog/public-images, unless you're testing"
+  exit 1
+fi
+
+if [ -z "$INCLUDE_MUSL" ]; then
+  INCLUDE_MUSL=0
+else
+  if [ "$INCLUDE_MUSL" -ne 0 ] && [ "$INCLUDE_MUSL" -ne 1 ]; then
+    echo "Error: INCLUDE_MUSL should be set to 0 or 1"
+    exit 1
+  fi
+fi
+
+# If this is a pre-release release, we skip all the additional checks 
+if echo "$CI_COMMIT_TAG" | grep -q "-" > /dev/null; then
+  echo "This is a pre-release version: $CI_COMMIT_TAG"
+  IS_PRERELEASE=1
+  MAJOR_MINOR_VERSION=""
+  MAJOR_VERSION=""
+  IS_LATEST_TAG=0
+  IS_LATEST_MAJOR_TAG=0
+else
+  IS_PRERELEASE=0
+
+  # Calculate the tags we use for floating major and minor versions
+  MAJOR_MINOR_VERSION="$(sed -nE 's/^(v[0-9]+\.[0-9]+)\.[0-9]+$/\1/p' <<< ${CI_COMMIT_TAG})"
+  MAJOR_VERSION="$(sed -nE 's/^(v[0-9]+)\.[0-9]+\.[0-9]+$/\1/p' <<< ${CI_COMMIT_TAG})"
+  
+  # Make sure we have all the tags
+  git fetch --tags
+
+  # We need to determine whether this is is the latest tag and whether it's the latest major or not
+  # So we fetch all tags and sort them to find both the latest, and the latest in this major.
+  # 'sort' technically gets prerelease versions in the wrong order here, but we explicitly
+  # exclude them anyway, as they're ignored for the purposes of determining the 'latest' tags.
+  LATEST_TAG="$(git tag | grep -v '-' | sort -V -r | head -n 1)"
+  LATEST_MAJOR_TAG="$(git tag -l "$MAJOR_VERSION.*" | grep -v '-' | sort -V -r | head -n 1)"
+  echo "This tag: $CI_COMMIT_TAG"
+  echo "Latest repository tag: $LATEST_TAG"
+  echo "Latest repository tag for this major: $LATEST_MAJOR_TAG"
+  echo "---------"
+  
+  # GNU sort -C (silent) reports via exit code whether the data is already in sorted order
+  # We use this to check whether the current tag is greater than (or equal to) the latest tag 
+  if printf '%s\n' "$LATEST_TAG" "$CI_COMMIT_TAG" | sort -C -V; then
+    # The current tag is the latest in the repository
+    IS_LATEST_TAG=1
+  else
+    IS_LATEST_TAG=0
+  fi
+  
+  if printf '%s\n' "$LATEST_MAJOR_TAG" "$CI_COMMIT_TAG" | sort -C -V; then
+    # The current tag is the latest for this major version in the repository
+    IS_LATEST_MAJOR_TAG=1
+  else
+    IS_LATEST_MAJOR_TAG=0
+  fi
+fi
+
+# print everything for debugging purposes
+echo "Calculated values:"
+echo "MAJOR_MINOR_VERSION=${MAJOR_MINOR_VERSION}"
+echo "MAJOR_VERSION=${MAJOR_VERSION}"
+echo "IS_LATEST_TAG=${IS_LATEST_TAG}"
+echo "IS_LATEST_MAJOR_TAG=${IS_LATEST_MAJOR_TAG}"
+echo "IS_PRERELEASE=${IS_PRERELEASE}"
+echo "---------"
+
+# Final check that everything is ok
+# if this is non-prerelease, we should have a major_minor version
+if [ "$IS_PRERELEASE" -eq 0 ] && [ -z "$MAJOR_MINOR_VERSION" ]; then
+  echo "Error: Could not determine major_minor version for stable release, this should not happen"
+  exit 1
+fi
+
+# if this is a latest major tag, we should have a major version
+if [ "$IS_LATEST_MAJOR_TAG" -eq 1 ] && [ -z "$MAJOR_VERSION" ]; then
+  echo "Error: Could not determine major version for latest major release, this should not happen"
+  exit 1
+fi
+
+# Helper functions for building the script
+add_stage() {
+  STAGE_NAME="$1"
+  DEST_TAG="$2"
+  TAG_SUFFIX="$3"
+  
+  cat << EOF >> generated-config.yml
+deploy_${STAGE_NAME}_docker:
+  stage: trigger-public-images
+  trigger:
+    project: $PUBLIC_IMAGES_PIPELINE_PROJECT
+    branch: main
+    strategy: depend
+  variables:
+    IMG_SOURCES: $IMG_SOURCES_BASE:$CI_COMMIT_SHA$TAG_SUFFIX
+    IMG_DESTINATIONS: $IMG_DESTINATION_BASE:$DEST_TAG$TAG_SUFFIX
+    IMG_SIGNING: "false"
+    
+EOF
+}
+
+add_all_stages() {
+  SUFFIX="$1"
+  STAGE_PREFIX="${SUFFIX:+${SUFFIX}_}"
+  TAG_SUFFIX="${SUFFIX:+-$SUFFIX}"
+  
+  # We always add this tag, regardless of the version 
+  add_stage "${STAGE_PREFIX}major_minor_patch" $CI_COMMIT_TAG $TAG_SUFFIX
+  
+  # If this is a pre-release version, we never add any other stages
+  if [ "$IS_PRERELEASE" -eq 1 ]; then
+    return
+  fi
+
+  # All non-prerelease stages get the major_minor tag
+  add_stage "${STAGE_PREFIX}major_minor" $MAJOR_MINOR_VERSION $TAG_SUFFIX
+
+  # Only latest-major releases get the major tag
+  if [ "$IS_LATEST_MAJOR_TAG" -eq 1 ]; then
+    add_stage "${STAGE_PREFIX}major" $MAJOR_VERSION $TAG_SUFFIX
+  fi
+  
+  # Only latest releases get the latest tag
+  if [ "$IS_LATEST_TAG" -eq 1 ]; then
+    add_stage "${STAGE_PREFIX}latest" "latest" $TAG_SUFFIX
+  fi
+}
+
+# Generate the pipeline for triggering child jobs
+cat << EOF > generated-config.yml
+stages:
+  - trigger-public-images
+
+EOF
+
+# Append the non-suffixed stages
+add_all_stages
+
+if [ "$INCLUDE_MUSL" -eq 1 ]; then
+  # Append the musl stages if requested
+  add_all_stages "musl"
+fi
+
+# All finished - print the generated yaml for debugging purposes
+echo "Generated pipeline:"
+cat generated-config.yml
