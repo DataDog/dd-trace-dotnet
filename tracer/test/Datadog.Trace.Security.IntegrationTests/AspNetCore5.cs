808057e727940a942051882afb2d218be1c13b94
Anna <anna.yafi@datadoghq.com>
2024-05-23 19:10:58 +0200

    Add null result action test

--- tracer/test/Datadog.Trace.Security.IntegrationTests/AspNetCore5.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/AspNetCore5.cs
@@ -62,6 +62,20 @@ namespace Datadog.Trace.Security.IntegrationTests
 #endif
             await TestAppSecRequestWithVerifyAsync(agent, url, null, 5, 1, settings);
         }
+
+        [SkippableFact]
+        [Trait("RunOnWindows", "True")]
+        public async Task TestNullAction()
+        {
+            await TryStartApp();
+            var agent = Fixture.Agent;
+            var url = "/null-action/test/test";
+            var dateTime = DateTime.UtcNow;
+            await SubmitRequest(url, null, null);
+            var spans = agent.WaitForSpans(1, minDateTime: dateTime);
+            var settings = VerifyHelper.GetSpanVerifierSettings();
+            await VerifyHelper.VerifySpans(spans, settings).UseFileName($"{GetTestName()}.test-null-action");
+        }
     }
 
     public class AspNetCore5TestsSecurityDisabledWithDefaultExternalRulesFile : AspNetCoreSecurityDisabledWithExternalRulesFile
