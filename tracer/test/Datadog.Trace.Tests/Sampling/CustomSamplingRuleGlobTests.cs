c83b3ac9a78c97a260df8715cb57873b71516dc6
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-05-30 19:24:16 -0400

    add test for remote config json

--- tracer/test/Datadog.Trace.Tests/Sampling/CustomSamplingRuleGlobTests.cs
+++ tracer/test/Datadog.Trace.Tests/Sampling/CustomSamplingRuleGlobTests.cs
@@ -53,7 +53,7 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void Constructs_With_ResourceName()
+        public void Constructs_With_ResourceName_Local()
         {
             const string config = """[{ "sample_rate":0.3, resource: "/api/v1/*" }]""";
             var rule = CustomSamplingRule.BuildFromLocalConfigurationString(config, SamplingRulesFormat.Glob, Timeout).Single();
@@ -67,10 +67,57 @@ namespace Datadog.Trace.Tests.Sampling
         }
 
         [Fact]
-        public void Constructs_With_Tags()
+        public void Constructs_With_ResourceName_Remote()
+        {
+            const string config = """[{ "sample_rate":0.3, resource: "/api/v1/*" }]""";
+            var rule = CustomSamplingRule.BuildFromRemoteConfigurationString(config, Timeout).Single();
+
+            var matchingSpan = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now) { ResourceName = "/api/v1/user/123" };
+            var nonMatchingSpan = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now) { ResourceName = "/api/v2/user/123" };
+
+            VerifyRate(rule, 0.3f);
+            VerifySingleRule(rule, matchingSpan, isMatch: true);
+            VerifySingleRule(rule, nonMatchingSpan, isMatch: false);
+        }
+
+        [Fact]
+        public void Constructs_With_Tags_Local()
         {
             const string config = """[{ "sample_rate":0.3, tags: { "http.method": "GE?", "http.status_code": "200", "balance": "*" } }]""";
             var rule = CustomSamplingRule.BuildFromLocalConfigurationString(config, SamplingRulesFormat.Glob, Timeout).Single();
+
+            var matchingSpan1 = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now);
+            matchingSpan1.SetTag("http.method", "GET");
+            matchingSpan1.SetMetric("http.status_code", 200); // matches as string or int
+            matchingSpan1.SetMetric("balance", 12);           // "*" matches ints or floats
+
+            var matchingSpan2 = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now);
+            matchingSpan2.SetTag("http.method", "GEX");      // "GE?" matches any single character
+            matchingSpan2.SetTag("http.status_code", "200"); // matches as string or int
+            matchingSpan2.SetMetric("balance", 12.34);       // "*" matches ints or floats
+
+            // missing "http.status_code" tag
+            var nonMatchingSpan1 = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now);
+            nonMatchingSpan1.SetTag("http.method", "GET");
+            nonMatchingSpan1.SetMetric("balance", 12.34);
+
+            var nonMatchingSpan2 = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now);
+            nonMatchingSpan2.SetTag("http.method", "POST"); // no match
+            nonMatchingSpan2.SetMetric("http.status_code", 200);
+            nonMatchingSpan2.SetMetric("balance", 12.34);
+
+            VerifyRate(rule, 0.3f);
+            VerifySingleRule(rule, matchingSpan1, isMatch: true);
+            VerifySingleRule(rule, matchingSpan2, isMatch: true);
+            VerifySingleRule(rule, nonMatchingSpan1, isMatch: false);
+            VerifySingleRule(rule, nonMatchingSpan2, isMatch: false);
+        }
+
+        [Fact]
+        public void Constructs_With_Tags_Remote()
+        {
+            const string config = """[{ "sample_rate":0.3, "tags": [{ "key":"http.method", "value_glob":"GE?" }, { "key":"http.status_code", "value_glob":"200"}, { "key":"balance", "value_glob":"*" }] }]""";
+            var rule = CustomSamplingRule.BuildFromRemoteConfigurationString(config, Timeout).Single();
 
             var matchingSpan1 = new Span(new SpanContext(1, 1, serviceName: "foo"), DateTimeOffset.Now);
             matchingSpan1.SetTag("http.method", "GET");
