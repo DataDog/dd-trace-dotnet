daaaec53adb6afbe4157f559108b534b73b624db
Tony Redondo <tony.redondo@datadoghq.com>
2024-05-31 18:10:42 +0200

    fix

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/SeleniumTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/SeleniumTests.cs
@@ -2,7 +2,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
-
+#if NETCOREAPP3_1_OR_GREATER
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -187,3 +187,5 @@ public class SeleniumTests : TestingFrameworkEvpTest
         _gacFixture.RemoveAssembliesFromGac();
     }
 }
+
+#endif
