0aea9291f599188366a947f963d35c34328b18ab
Tony Redondo <tony.redondo@datadoghq.com>
2024-06-03 15:26:08 +0200

    changes

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/SeleniumTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/SeleniumTests.cs
@@ -156,7 +156,7 @@ public class SeleniumTests : TestingFrameworkEvpTest
         using var processResult = await RunDotnetTestSampleAndWaitForExit(
                                       agent,
                                       packageVersion: packageVersion,
-                                      arguments: $"--settings:\"{Path.Combine(sampleFolder, "ci.runsettings")}\" --diag:\"diag.diagtxt\"");
+                                      arguments: $"--settings:\"{Path.Combine(sampleFolder, "ci.runsettings")}\" --diag:\"{Path.Combine(sampleFolder, "diag.diagtxt")}\"");
 
         // Check if we have the data
         using var s = new AssertionScope();
