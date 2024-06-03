7fac4a16970c9ef3e7c1910aa64cb0c64f4f108e
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-06-03 16:42:54 -0400

    rename test class

--- tracer/test/Datadog.Trace.Tests/Sampling/RemoteCustomSamplingRuleTests.cs
+++ tracer/test/Datadog.Trace.Tests/Sampling/RemoteCustomSamplingRuleTests.cs
@@ -1,11 +1,9 @@
-// <copyright file="CustomSamplingRuleTests.cs" company="Datadog">
+// <copyright file="RemoteCustomSamplingRuleTests.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Datadog.Trace.Sampling;
 using FluentAssertions;
 using Xunit;
@@ -13,7 +11,7 @@ using Xunit;
 namespace Datadog.Trace.Tests.Sampling;
 
 [Collection(nameof(Datadog.Trace.Tests.Sampling))]
-public class CustomSamplingRuleTests
+public class RemoteCustomSamplingRuleTests
 {
     [Fact]
     public void ConvertToLocalTags()
