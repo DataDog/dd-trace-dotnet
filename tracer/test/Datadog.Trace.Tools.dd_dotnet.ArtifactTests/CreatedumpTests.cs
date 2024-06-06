23e23f438ed1134a6f55aaae27084db7967acfb0
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-06-06 13:55:55 +0200

    _sigh_

--- tracer/test/Datadog.Trace.Tools.dd_dotnet.ArtifactTests/CreatedumpTests.cs
+++ tracer/test/Datadog.Trace.Tools.dd_dotnet.ArtifactTests/CreatedumpTests.cs
@@ -381,8 +381,16 @@ public class CreatedumpTests : ConsoleTestHelper
         using var assertionScope = new AssertionScope();
         assertionScope.AddReportable("Report", report.ToString());
 
-        ValidateStacktrace(report["stacktrace"]);
-        ValidateStacktrace(report["additional_stacktraces"][expectedPid.Value.ToString()]);
+        try
+        {
+            ValidateStacktrace(report["stacktrace"]);
+            ValidateStacktrace(report["additional_stacktraces"][expectedPid.Value.ToString()]);
+        }
+        catch (ArgumentNullException)
+        {
+            Output.WriteLine(report.ToString());
+            throw;
+        }
 
         void ValidateStacktrace(JToken callstack)
         {
