9bf401d21ec801932bd124c8965e238b83b251e5
Andrew Lock <andrew.lock@datadoghq.com>
2024-06-13 10:55:14 +0100

    [SSI] Fix some of the single step injection forwarder code (#5678)
    * `library_entrypoint.abort.runtime` should not have additional tags
    
    * minor: use static variable properly
    
    * Send incompatible_runtime as well as eol_runtime
    
    * nit: record the telemetry we send too
    
    * Fix trailing pid

--- shared/src/Datadog.Trace.ClrProfiler.Native/single_step_guard_rails.h
+++ shared/src/Datadog.Trace.ClrProfiler.Native/single_step_guard_rails.h
@@ -11,16 +11,11 @@ private:
     bool m_isRunningInSingleStep;
     bool m_isForcedExecution = false;
 
-    inline static const std::string MinNetFrameworkVersion = "4.6.1";
-    inline static const std::string MaxNetFrameworkVersion = "4.8.1";
-    inline static const std::string MinNetCoreVersion = "3.1.0";
-    inline static const std::string MaxNetCoreVersion = "8.0.0";
-
     bool ShouldForceInstrumentationOverride(const std::string& eolDescription);
-    HRESULT HandleUnsupportedNetCoreVersion(const std::string& unsupportedDescription, const std::string& runtimeVersion);
-    HRESULT HandleUnsupportedNetFrameworkVersion(const std::string& unsupportedDescription, const std::string& runtimeVersion);
+    HRESULT HandleUnsupportedNetCoreVersion(const std::string& unsupportedDescription, const std::string& runtimeVersion, const bool isEol);
+    HRESULT HandleUnsupportedNetFrameworkVersion(const std::string& unsupportedDescription, const std::string& runtimeVersion, const bool isEol);
 
-    void SendAbortTelemetry(const std::string& runtimeName, const std::string& runtimeVersion, const std::string& minVersion, const std::string& maxVersion) const;
+    void SendAbortTelemetry(const std::string& runtimeName, const std::string& runtimeVersion, const bool isEol) const;
     void SendTelemetry(const std::string& runtimeName, const std::string& runtimeVersion, const std::string& telemetryPoints) const;
 public:
     inline static const std::string NetFrameworkRuntime = ".NET Framework";
