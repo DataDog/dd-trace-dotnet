a62cfff0fbed98e5fb397630f68367ef73360a4b
Omer Raviv <omer.raviv@datadoghq.com>
2024-06-15 22:24:21 -0400

    Re-add missing code that was missing due to bad backmerge

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNetCore/AspNetCoreBlockMiddlewareIntegrationEnd.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNetCore/AspNetCoreBlockMiddlewareIntegrationEnd.cs
@@ -5,12 +5,18 @@
 
 #if !NETFRAMEWORK
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Datadog.Trace.AppSec;
 using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.Configuration;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Options;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNetCore
 {
@@ -53,10 +59,101 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNetCore
         public static CallTargetState OnMethodBegin<TTarget>(TTarget instance)
             where TTarget : IApplicationBuilder
         {
+            
+            var methodProbes = GetMethodProbeLocations(instance);
+
+            if (methodProbes.Any())
+            {
+                methodProbes.ForEach(
+                    tuple =>
+                    {
+                        FakeProbeCreator.CreateAndInstallMethodProbe("SpanEntry", tuple.Item2);
+                        // TimeTravelInitiator.InitiateTimeTravel(tuple.Item2);
+                    });
+            }
+
             instance.Components.Insert(0, rd => new BlockingMiddleware(rd).Invoke);
             instance.Components.Add(rd => new BlockingMiddleware(rd, endPipeline: true).Invoke);
 
             return default;
+        }
+        
+           private static List<Tuple<string, MethodInfo>> GetMethodProbeLocations<TTarget>(TTarget instance) where TTarget : IApplicationBuilder
+        {
+            var service = (IEnumerable<IConfigureOptions<RouteOptions>>)instance.ApplicationServices.GetService(typeof(IEnumerable<IConfigureOptions<RouteOptions>>));
+
+            var methodProbes = new List<Tuple<string, MethodInfo>>();
+
+            foreach (IConfigureOptions<RouteOptions> configureOptions in service)
+            {
+                var field = configureOptions.GetType().GetField("_dataSources", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (field == null)
+                {
+                    continue;
+                }
+
+                var endpointDataSources = field.GetValue(configureOptions);
+
+                if (endpointDataSources == null)
+                {
+                    continue;
+                }
+
+                Type enumerableType = endpointDataSources.GetType();
+                MethodInfo getEnumeratorMethod = enumerableType.GetMethod("GetEnumerator");
+                var endpointDataSourcesEnumerator = (IEnumerator)getEnumeratorMethod.Invoke(endpointDataSources, null);
+                while (endpointDataSourcesEnumerator.MoveNext())
+                {
+                    var endpointDataSource = endpointDataSourcesEnumerator.Current;
+                    var endpoints = GetProperty(endpointDataSource, "Endpoints").GetValue(endpointDataSource);
+
+                    if (endpoints == null)
+                    {
+                        continue;
+                    }
+
+                    enumerableType = endpoints.GetType();
+                    getEnumeratorMethod = enumerableType.GetMethod("GetEnumerator");
+                    var endpointsEnumerator = (IEnumerator)getEnumeratorMethod.Invoke(endpoints, null);
+                    while (endpointsEnumerator.MoveNext())
+                    {
+                        var endpoint = endpointsEnumerator.Current;
+                        var metadata = GetProperty(endpoint, "Metadata").GetValue(endpoint);
+                        Type controllerActionDescriptorType = Type.GetType("Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor, Microsoft.AspNetCore.Mvc.Core");
+
+                        if (controllerActionDescriptorType == null)
+                        {
+                            continue;
+                        }
+
+                        var getMetadata = metadata.GetType().GetMethod("GetMetadata").MakeGenericMethod(controllerActionDescriptorType);
+
+                        if (getMetadata == null)
+                        {
+                            continue;
+                        }
+
+                        var controllerActionDescriptor = getMetadata.Invoke(metadata, null);
+                        if (controllerActionDescriptor == null)
+                        {
+                            continue;
+                        }
+
+                        MethodInfo method = (MethodInfo)GetProperty(controllerActionDescriptor, "MethodInfo").GetValue(controllerActionDescriptor);
+
+                        string displayName = (string)GetProperty(controllerActionDescriptor, "DisplayName").GetValue(controllerActionDescriptor);
+
+                        if (method == null || string.IsNullOrEmpty(displayName))
+                        {
+                            continue;
+                        }
+
+                        methodProbes.Add(Tuple.Create(displayName, method));
+                    }
+                }
+            }
+
+            return methodProbes;
         }
     }
 }
