f59d39b94a44c8bcbccb38199b83b86320713233
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2024-06-20 22:33:57 +0200

    clean up

--- build/cmake/FindGlibcCompat.cmake
+++ build/cmake/FindGlibcCompat.cmake
@@ -1,5 +1,5 @@
 add_library(glibc-compat OBJECT
-    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/glibc-compat/math.c
+    ${DOTNET_TRACER_REPO_ROOT_PATH}/shared/src/glibc-compat/glibc-compat.c
 )
 
 set_target_properties(glibc-compat PROPERTIES POSITION_INDEPENDENT_CODE 1)
