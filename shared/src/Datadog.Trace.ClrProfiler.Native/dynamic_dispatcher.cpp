abc724e47fe4db8389648af288fa85bac72435ab
Andrew Lock <andrew.lock@datadoghq.com>
2024-06-21 14:49:26 +0100

    [v3] Update native loader to explicitly handle alpine (#5593)
    * Update native loader to explicitly handle alpine
    
    - Moved GetAllOsArch and GetCurrentOsArch to helper methods
    - Added IsRunningOnAlpine() helper method
    - Added linux-musl-x64 entry to the loader.conf
    
    * Fix generated loader.conf in profiler integration tests

--- shared/src/Datadog.Trace.ClrProfiler.Native/dynamic_dispatcher.cpp
+++ shared/src/Datadog.Trace.ClrProfiler.Native/dynamic_dispatcher.cpp
@@ -4,62 +4,11 @@
 #include <unordered_map>
 
 #include "log.h"
+#include "util.h"
 #include "../../../shared/src/native-src/dd_filesystem.hpp"
 // namespace fs is an alias defined in "dd_filesystem.hpp"
 #include "../../../shared/src/native-src/pal.h"
 #include "../../../shared/src/native-src/util.h"
-
-#if AMD64
-
-#if _WINDOWS
-const std::string currentOsArch = "win-x64";
-#elif LINUX
-const std::string currentOsArch = "linux-x64";
-#elif MACOS
-const std::string currentOsArch = "osx-x64";
-#else
-#error "currentOsArch not defined."
-#endif
-
-#elif X86
-
-#if _WINDOWS
-const std::string currentOsArch = "win-x86";
-#elif LINUX
-const std::string currentOsArch = "linux-x86";
-#elif MACOS
-const std::string currentOsArch = "osx-x86";
-#else
-#error "currentOsArch not defined."
-#endif
-
-#elif ARM64
-
-#if _WINDOWS
-const std::string currentOsArch = "win-arm64";
-#elif LINUX
-const std::string currentOsArch = "linux-arm64";
-#elif MACOS
-const std::string currentOsArch = "osx-arm64";
-#else
-#error "currentOsArch not defined."
-#endif
-
-#elif ARM
-
-#if _WINDOWS
-const std::string currentOsArch = "win-arm";
-#elif LINUX
-const std::string currentOsArch = "linux-arm";
-#elif MACOS
-const std::string currentOsArch = "osx-arm";
-#else
-#error "currentOsArch not defined."
-#endif
-
-#else
-#error "currentOsArch not defined."
-#endif
 
 namespace datadog::shared::nativeloader
 {
@@ -105,11 +54,14 @@ namespace datadog::shared::nativeloader
         // Set the current path to the configuration folder (to allow relative paths)
         fs::current_path(configFolder);
 
-        const std::string allOsArch[12] = {
-            "win-x64", "linux-x64", "osx-x64",
-            "win-x86", "linux-x86", "osx-x86",
-            "win-arm64", "linux-arm64", "osx-arm64",
-            "win-arm", "linux-arm", "osx-arm",
+        const auto isRunningOnAlpine = IsRunningOnAlpine();
+        const auto currentOsArch = GetCurrentOsArch(isRunningOnAlpine);
+
+        const std::string allOsArch[16] = {
+            "win-x64", "linux-x64", "linux-musl-x64", "osx-x64",
+            "win-x86", "linux-x86", "linux-musl-x86", "osx-x86",
+            "win-arm64", "linux-arm64", "linux-musl-arm64", "osx-arm64",
+            "win-arm", "linux-arm", "linux-musl-arm", "osx-arm",
         };
 
         while (t)
@@ -144,7 +96,7 @@ namespace datadog::shared::nativeloader
                         // Convert possible relative paths to absolute paths using the configuration file folder as base
                         // (current_path)
                         std::string absoluteFilepathValue = fs::absolute(filepathValue).string();
-                        Log::Debug("DynamicDispatcherImpl::LoadConfiguration: [", type, "] Loading: ", filepathValue, " [AbsolutePath=", absoluteFilepathValue,"]");
+                        Log::Debug("DynamicDispatcherImpl::LoadConfiguration: [", type, "] Loading: ", filepathValue, " [AbsolutePath=", absoluteFilepathValue,"] (", currentOsArch, ")" );
                         if (fs::exists(absoluteFilepathValue))
                         {
                             Log::Debug("[", type, "] Creating a new DynamicInstance object");
