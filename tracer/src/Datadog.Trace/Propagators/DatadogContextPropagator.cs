c444a734c252e686f5e7e7f756b471c3a90489e2
Flavien Darche <flavien.darche@datadoghq.com>
2024-07-17 16:36:38 +0200

    Move to SpanContextPropagator

--- tracer/src/Datadog.Trace/Propagators/DatadogContextPropagator.cs
+++ tracer/src/Datadog.Trace/Propagators/DatadogContextPropagator.cs
@@ -24,12 +24,6 @@ namespace Datadog.Trace.Propagators
         public void Inject<TCarrier, TCarrierSetter>(SpanContext context, TCarrier carrier, TCarrierSetter carrierSetter)
             where TCarrierSetter : struct, ICarrierSetter<TCarrier>
         {
-            // If appsec standalone is enabled and appsec propagation is disabled (no ASM events) -> stop propagation
-            if (context.TraceContext?.Tracer.Settings?.AppsecStandaloneEnabledInternal == true && context.TraceContext.Tags.GetTag(Tags.Propagated.AppSec) != "1")
-            {
-                return;
-            }
-
             TelemetryFactory.Metrics.RecordCountContextHeaderStyleInjected(MetricTags.ContextHeaderStyle.Datadog);
             var invariantCulture = CultureInfo.InvariantCulture;
 
