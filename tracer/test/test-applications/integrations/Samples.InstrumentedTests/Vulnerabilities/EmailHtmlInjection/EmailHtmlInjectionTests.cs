030e8316c037f03d9d021a431ace664a95cd486b
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-08-12 11:30:24 +0200

    Disable email Injection tests

--- tracer/test/test-applications/integrations/Samples.InstrumentedTests/Vulnerabilities/EmailHtmlInjection/EmailHtmlInjectionTests.cs
+++ tracer/test/test-applications/integrations/Samples.InstrumentedTests/Vulnerabilities/EmailHtmlInjection/EmailHtmlInjectionTests.cs
@@ -11,6 +11,7 @@ using System.Web;
 using System.Threading.Tasks;
 
 namespace Samples.InstrumentedTests.Iast.Vulnerabilities;
+
 public class EmailHtmlInjectionTests : InstrumentationTestsBase
 { 
     private static string taintedName = "Alice<h1>Hi</h1>";
@@ -32,7 +33,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
 
     // Tests for method Send(MailMessage message);
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailMessageTaintedVaulesHtml_ThenIsVulnerable()
     {
         var mailMessage = BuildMailMessage(true, taintedName, taintedLastName);
@@ -40,7 +41,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertVulnerable(emailHtmlInjectionType, "Hi :+-Alice<h1>Hi</h1>-+: :+-Stevens-+:!");
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailMessageTaintedVaulesHtmlEscaped_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, WebUtility.HtmlEncode(taintedName), WebUtility.HtmlEncode(taintedLastName));
@@ -48,7 +49,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailMessageTaintedVaulesHtmlEscaped_ThenIsNotVulnerable2()
     {
         var mailMessage = BuildMailMessage(true, HttpUtility.HtmlEncode(taintedName), HttpUtility.HtmlEncode(taintedLastName));
@@ -56,7 +57,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailMessageNotTaintedVaulesHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, untaintedName, untaintedLastName);
@@ -64,7 +65,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailMessageTaintedVaulesNoHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(false, taintedName, taintedLastName);
@@ -72,7 +73,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailMessageNull_ThenIsNotVulnerable()
     {
         try
@@ -94,7 +95,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
 
     // Tests for method SendMailAsync(MailMessage message);
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageTaintedVaulesHtml_ThenIsVulnerable()
     {
         var mailMessage = BuildMailMessage(true, taintedName, taintedLastName);
@@ -102,7 +103,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertVulnerable(emailHtmlInjectionType, "Hi :+-Alice<h1>Hi</h1>-+: :+-Stevens-+:!");
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageTaintedVaulesHtmlEscaped_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, WebUtility.HtmlEncode(taintedName), WebUtility.HtmlEncode(taintedLastName));
@@ -110,7 +111,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageTaintedVaulesHtmlEscaped_ThenIsNotVulnerable2()
     {
         var mailMessage = BuildMailMessage(true, HttpUtility.HtmlEncode(taintedName), HttpUtility.HtmlEncode(taintedLastName));
@@ -118,7 +119,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenMailSendAsyncMailMessageNotTaintedVaulesHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, untaintedName, untaintedLastName);
@@ -126,7 +127,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenMailSendAsyncMailMessageTaintedVaulesNoHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(false, taintedName, taintedLastName);
@@ -144,7 +145,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
 
     // Test SendAsync(MailMessage message, object userToken);
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendAsyncMailMessageTaintedVaulesHtml_ThenIsVulnerable()
     {
         var mailMessage = BuildMailMessage(true, taintedName, taintedLastName);
@@ -152,7 +153,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertVulnerable(emailHtmlInjectionType, "Hi :+-Alice<h1>Hi</h1>-+: :+-Stevens-+:!");
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendAsyncMailMessageTaintedVaulesHtmlEscaped_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, WebUtility.HtmlEncode(taintedName), WebUtility.HtmlEncode(taintedLastName));
@@ -160,7 +161,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendAsyncMailMessageTaintedVaulesHtmlEscaped_ThenIsNotVulnerable2()
     {
         var mailMessage = BuildMailMessage(true, HttpUtility.HtmlEncode(taintedName), HttpUtility.HtmlEncode(taintedLastName));
@@ -168,7 +169,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendAsyncMailMessageNotTaintedVaulesHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, untaintedName, untaintedLastName);
@@ -176,7 +177,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendAsyncMailMessageTaintedVaulesNoHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(false, taintedName, taintedLastName);
@@ -196,7 +197,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
 
 #if NET5_0_OR_GREATER
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageCancellationTaintedVaulesHtml_ThenIsVulnerable()
     {
         var mailMessage = BuildMailMessage(true, taintedName, taintedLastName);
@@ -204,7 +205,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertVulnerable(emailHtmlInjectionType, "Hi :+-Alice<h1>Hi</h1>-+: :+-Stevens-+:!");
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageCancellationTaintedVaulesHtmlEscaped_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, WebUtility.HtmlEncode(taintedName), WebUtility.HtmlEncode(taintedLastName));
@@ -212,7 +213,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageCancellationTaintedVaulesHtmlEscaped_ThenIsNotVulnerable2()
     {
         var mailMessage = BuildMailMessage(true, HttpUtility.HtmlEncode(taintedName), HttpUtility.HtmlEncode(taintedLastName));
@@ -220,7 +221,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageCancellationNotTaintedVaulesHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(true, untaintedName, untaintedLastName);
@@ -228,7 +229,7 @@ public class EmailHtmlInjectionTests : InstrumentationTestsBase
         AssertNotVulnerable();
     }
 
-    [Fact]
+    [Fact(Skip = "Some tests are hanging the CI in netcore <=3.1")]
     public void GivenAnEmail_WhenSendMailAsyncMailMessageCancellationTaintedVaulesNoHtml_ThenIsNotVulnerable()
     {
         var mailMessage = BuildMailMessage(false, taintedName, taintedLastName);
