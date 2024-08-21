9daac0174fe7a07dda27efbf73462ee9efdcbc91
Nathaniel (Nati) Tsechanski <nati.tsechanski@datadoghq.com>
2024-08-21 16:04:27 -0400

    Testing version scheme fix and added validation for the version.

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -7,10 +7,18 @@ PACKAGE_VERSION=$(< ../artifacts/version.txt)
 if [ -n "$CI_COMMIT_TAG" ] || [ -n "$DOTNET_PACKAGE_VERSION" ]; then
   VERSION=$PACKAGE_VERSION
 else
-  VERSION=$PACKAGE_VERSION$CI_VERSION_SUFFIX
+  VERSION=$PACKAGE_VERSION-${CI_VERSION_SUFFIX:1}
 fi
 
-echo "VERSION=$VERSION"
+# https://github.com/har7an/bash-semver-regex
+SEMVER_REGEX="^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$"
+
+if ! [[ $VERSION =~ $SEMVER_REGEX ]]; then
+  echo "Invalid version format: '$VERSION' (check https://semver.org/ for more information)"
+  exit 1
+fi
+
+echo "'VERSION=$VERSION'"
 
 if [ -z "$ARCH" ]; then
   ARCH=amd64
