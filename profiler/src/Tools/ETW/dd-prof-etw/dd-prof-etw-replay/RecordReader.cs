5fee35d7895d2fae4a15f8214b475450f968a22b
Christophe Nasarre <christophe.nasarre@datadoghq.com>
2024-08-27 16:54:32 +0200

    Remove unneeded uses

--- profiler/src/Tools/ETW/dd-prof-etw/dd-prof-etw-replay/RecordReader.cs
+++ profiler/src/Tools/ETW/dd-prof-etw/dd-prof-etw-replay/RecordReader.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace dd_prof_etw_replay
 {
