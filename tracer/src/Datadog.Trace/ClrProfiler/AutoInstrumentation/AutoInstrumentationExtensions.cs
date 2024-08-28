529877a6994f78bb92f969d66a2f7687a40b8338
Tony Redondo <tony.redondo@datadoghq.com>
2024-08-28 15:49:05 +0200

    Missing file

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AutoInstrumentationExtensions.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AutoInstrumentationExtensions.cs
@@ -2,8 +2,10 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
+#nullable enable
 
 using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation
@@ -11,7 +13,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation
     internal static class AutoInstrumentationExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static void DisposeWithException(this Scope scope, Exception exception)
+        internal static void DisposeWithException(this Scope? scope, Exception? exception)
         {
             if (scope != null)
             {
@@ -27,6 +29,29 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation
                     scope.Dispose();
                 }
             }
+        }
+
+        public static bool TryGetAssemblyFileVersionFromType(Type type, out Version? version)
+        {
+            if (type is null)
+            {
+                version = null;
+                return false;
+            }
+
+            // Get the assembly containing this type
+            var assembly = type.Assembly;
+
+            // Get the AssemblyFileVersion attribute from the assembly
+            // Parse the file version string to a Version object and return it
+            if (assembly.GetCustomAttribute<AssemblyFileVersionAttribute>() is { } attribute &&
+                Version.TryParse(attribute.Version, out version))
+            {
+                return true;
+            }
+
+            version = null;
+            return false;
         }
     }
 }
