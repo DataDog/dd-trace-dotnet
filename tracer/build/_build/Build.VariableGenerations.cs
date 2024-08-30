DataDog/dd-trace-dotnet

commit 5602968b8ac840764083f8d186074d22943c755b

Add support for alpine on arm64  (#5933)
## Summary of changes

Adds support for the tracer on alpine on ARM64

## Reason for change

We currently support x64 on glibc/alpine but only glibc on arm64. This
closes that matrix which makes it easier to understand

## Implementation details

There's several layers, but at a high level
- Anywhere where we detect platform, make sure to include
`linux-musl-arm64` as well as `linux-arm64`
- Build the tracer + profiler on alpine arm64

There were several difficulties
- The ruby-based fpm bundler we use doesn't work on alpine arm64, hence
#5905
- I couldn't get our existing alpine.build.dockerfile to compile on
arm64 (where we currently compile llvm/clang16 on alpine14).
- To work around it, created a separate alpine.build.arm64.dockerfile
image which uses alpine:18 (the first version that includes clang16) as
the base.
- Obviously this gives us different requirements in arm64, but I think
that's fine because a) this is a new feature b) we already have
differences on the glibc requirements for arm64 c) We only support .NET
5+ anyway on arm64 (.NET 6 really given .NET 5 is mostly gone) and they
ship alpine:18 images for .NET 6+
- I created a multi arch base image from the two base images ([as some
bloke with a blog describes
here](https://andrewlock.net/combining-multiple-docker-images-into-a-multi-arch-image/))
- I had difficulty compiling the profiler on arm64 initially (we require
it for some crash tracking functionality), as we were getting `undefined
reference to `_Uaarch64_get_accessors_int'` when trying to compile
libunwind. @gleocadie tracked it down to [this
issue](https://github.com/libunwind/libunwind/issues/788), and fixed it
by just not building the libunwind tests 🙇‍♂️
- The sqlite IAST instrumentation tests on alpine arm64 were failing in
.NET 5. I _think_ that's because the sqlite package we're using there
doesn't support it (which makes sense) so just disabled the tests for

.azure-pipelines/ultimate-pipeline.yml
@@ -620,6 +620,14 @@ stages:
   - job: build
     timeoutInMinutes: 60 #default value
     dependsOn: []
+    strategy:
+      matrix:
+        arm64:
+          baseImage: debian
+          artifactSuffix: linux-arm64
+        alpine:
+          baseImage: alpine
+          artifactSuffix: linux-musl-arm64
     pool:
       name: aws-arm64-auto-scaling
     workspace:
@@ -633,21 +641,21 @@ stages:
       parameters:
         build: true
         target: builder
-        baseImage: debian
+        baseImage: $(baseImage)
         command: "Clean CompileManagedLoader BuildNativeTracerHome BuildManagedTracerHome ExtractDebugInfoLinux"
         retryCountForRunCommand: 1
 
     - publish: $(monitoringHome)
       displayName: Uploading linux tracer home artifact
-      artifact: linux-tracer-home-linux-arm64
+      artifact: linux-tracer-home-$(artifactSuffix)
 
     - publish: $(System.DefaultWorkingDirectory)
       displayName: Upload working directory after the build
-      artifact: build-linux-arm64-working-directory
+      artifact: build-$(artifactSuffix)-working-directory
 
     - publish: $(symbols)
       displayName: Upload linux tracer symbols
-      artifact: linux-tracer-symbols-linux-arm64
+      artifact: linux-tracer-symbols-$(artifactSuffix)
 
 - stage: build_arm64_profiler
   dependsOn: [merge_commit_id]
@@ -662,6 +670,14 @@ stages:
   - job: build
     timeoutInMinutes: 60 #default value
     dependsOn: []
+    strategy:
+      matrix:
+        arm64:
+          baseImage: debian
+          artifactSuffix: linux-arm64
+        alpine:
+          baseImage: alpine
+          artifactSuffix: linux-musl-arm64
     pool:
       name: aws-arm64-auto-scaling
     workspace:
@@ -675,21 +691,21 @@ stages:
       parameters:
         build: true
         target: builder
-        baseImage: debian
+        baseImage: $(baseImage)
         command: "Clean BuildProfilerHome ExtractDebugInfoLinux"
         retryCountForRunCommand: 1
 
     - publish: $(System.DefaultWorkingDirectory)/profiler/_build
       displayName: Uploading linux profiler output build folder
-      artifact: linux-profiler-binaries-linux-arm64
+      artifact: linux-profiler-binaries-$(artifactSuffix)
 
     - publish: $(monitoringHome)
       displayName: Uploading linux profiler home artifact
-      artifact: linux-profiler-home-linux-arm64
+      artifact: linux-profiler-home-$(artifactSuffix)
 
     - publish: $(symbols)
       displayName: Upload linux profiler symbols
-      artifact: linux-profiler-symbols-linux-arm64
+      artifact: linux-profiler-symbols-$(artifactSuffix)
 
 - stage: build_arm64_universal
   dependsOn: [merge_commit_id]
@@ -744,6 +760,14 @@ stages:
   - job: package
     dependsOn: []
     timeoutInMinutes: 60 #default value
+    strategy:
+      matrix:
+        arm64:
+          baseImage: debian
+          artifactSuffix: linux-arm64
+        alpine:
+          baseImage: alpine
+          artifactSuffix: linux-musl-arm64
     pool:
       name: aws-arm64-auto-scaling
 
@@ -752,50 +776,75 @@ stages:
       parameters:
         targetShaId: $(targetShaId)
         targetBranch: $(targetBranch)
-
+      
+      # Download _both_ musl and glibc if we're building the universal package
+      # but only the musl package if we're building alpine package
     - task: DownloadPipelineArtifact@2
       displayName: Download tracer arm64 native binary
+      condition: eq(variables['artifactSuffix'], 'linux-arm64')
       inputs:
         artifact: linux-tracer-home-linux-arm64
         path: $(monitoringHome)
 
     - task: DownloadPipelineArtifact@2
       displayName: Download profiler arm64 native binary
+      condition: eq(variables['artifactSuffix'], 'linux-arm64')
       inputs:
         artifact: linux-profiler-home-linux-arm64
         path: $(monitoringHome)
 
     - task: DownloadPipelineArtifact@2
       displayName: Download dd-dotnet linux-arm64
+      condition: eq(variables['artifactSuffix'], 'linux-arm64')
       inputs:
         artifact: dd-dotnet-linux-arm64
         path: $(monitoringHome)/linux-arm64
 
+    - task: DownloadPipelineArtifact@2
+      displayName: Download tracer linux-musl-arm64 native binary
+      inputs:
+        artifact: linux-tracer-home-linux-musl-arm64
+        path: $(monitoringHome)
+
+    - task: DownloadPipelineArtifact@2
+      displayName: Download profiler linux-musl-arm64 native binary
+      inputs:
+        artifact: linux-profiler-home-linux-musl-arm64
+        path: $(monitoringHome)
+
+    - task: DownloadPipelineArtifact@2
+      displayName: Download dd-dotnet linux-musl-arm64
+      inputs:
+        artifact: dd-dotnet-linux-musl-arm64
+        path: $(monitoringHome)/linux-musl-arm64
+
     - script: |
-        chmod +x $(monitoringHome)/linux-arm64/dd-dotnet
+        chmod +x $(monitoringHome)/linux-musl-arm64/dd-dotnet
+        # don't panic if the file doesn't exist
+        chmod +x $(monitoringHome)/linux-arm64/dd-dotnet | :
       displayName: chmod dd-dotnet
 
     - task: DownloadPipelineArtifact@2
       displayName: Download universal native binaries
       inputs:
         artifact: linux-universal-home-linux-arm64
-        path: $(monitoringHome)/linux-arm64
+        path: $(monitoringHome)/$(artifactSuffix)
 
     - template: steps/run-in-docker.yml
       parameters:
         build: true
         target: builder
-        baseImage: debian
+        baseImage: $(baseImage)
         command: "ZipMonitoringHome"
         retryCountForRunCommand: 1
 
     - publish: $(artifacts)/linux-arm64
       displayName: Upload arm64 packages
-      artifact: linux-packages-linux-arm64
+      artifact: linux-packages-$(artifactSuffix)
 
     - publish: $(monitoringHome)
       displayName: Upload arm64 monitoring home
-      artifact: linux-monitoring-home-linux-arm64
+      artifact: linux-monitoring-home-$(artifactSuffix)
 
 - stage: build_macos
   condition: and(succeeded(), eq(variables['isBenchmarksOnlyBuild'], 'False'))
@@ -1018,8 +1067,6 @@ stages:
   variables:
     targetShaId: $[ stageDependencies.merge_commit_id.fetch.outputs['set_sha.sha']]
     targetBranch: $[ stageDependencies.merge_commit_id.fetch.outputs['set_sha.branch']]
-    baseImage: debian
-    artifactSuffix: linux-arm64
     targetFramework: net8.0
   jobs:
   - template: steps/update-github-status-jobs.yml
@@ -1028,6 +1075,14 @@ stages:
 
   - job: linux
     timeoutInMinutes: 60 #default value
+    strategy:
+      matrix:
+        x64:
+          baseImage: debian
+          artifactSuffix: linux-arm64
+        alpine:
+          baseImage: alpine
+          artifactSuffix: linux-musl-arm64
 
     pool:
       name: aws-arm64-auto-scaling
@@ -1322,6 +1377,14 @@ stages:
 
     - job: test
       timeoutInMinutes: 60 #default value
+      strategy:
+        matrix:
+          arm64:
+            baseImage: debian
+            artifactSuffix: linux-arm64
+          alpine:
+            baseImage: alpine
+            artifactSuffix: linux-musl-arm64
       pool:
         name: aws-arm64-auto-scaling
       workspace:
@@ -1333,12 +1396,12 @@ stages:
             targetBranch: $(targetBranch)
         - template: steps/restore-working-directory.yml
           parameters:
-            artifact: build-linux-arm64-working-directory
+            artifact: build-$(artifactSuffix)-working-directory
 
         - template: steps/run-in-docker.yml
           parameters:
             build: true
-            baseImage: debian
+            baseImage: $(baseImage)
             command: "BuildAndRunManagedUnitTests"
             apiKey: $(DD_LOGGER_DD_API_KEY)
 
@@ -2702,6 +2765,7 @@ stages:
 
     - job: Test
       timeoutInMinutes: 60 #default value
+      # TODO: move this to GenerateVariables stage
       strategy:
         matrix: $[stageDependencies.generate_variables.generate_variables_job.outputs['generate_variables_step.integration_tests_linux_arm64_matrix']]
       workspace:
@@ -3382,6 +3446,15 @@ stages:
         path: $(monitoringHome)
 
     - task: DownloadPipelineArtifact@2
+      displayName: Download alpine arm64 tracer home
+      inputs:
+        artifact: linux-tracer-home-linux-musl-arm64
+        patterns:  |
+          **/*.so
+          **/loader.conf
+        path: $(monitoringHome)
+
+    - task: DownloadPipelineArtifact@2
       displayName: Download osx tracer home
       inputs:
         artifact: macos-tracer-home
@@ -3425,6 +3498,15 @@ stages:
         path: $(monitoringHome)
 
     - task: DownloadPipelineArtifact@2
+      displayName: Download alpine arm64 profiler home
+      inputs:
+        artifact: linux-profiler-home-linux-musl-arm64
+        patterns:  |
+          **/*.so
+          **/loader.conf
+        path: $(monitoringHome)
+
+    - task: DownloadPipelineArtifact@2
       displayName: Download dd-dotnet win-x64
       inputs:
         artifact: dd-dotnet-win-x64
@@ -3449,6 +3531,12 @@ stages:
         path: $(monitoringHome)/linux-arm64
 
     - task: DownloadPipelineArtifact@2
+      displayName: Download dd-dotnet linux-musl-arm64
+      inputs:
+        artifact: dd-dotnet-linux-musl-arm64
+        path: $(monitoringHome)/linux-musl-arm64
+
+    - task: DownloadPipelineArtifact@2
       displayName: Download linux universal artifacts 
       inputs:
         artifact: linux-universal-home-linux-x64
@@ -3465,6 +3553,12 @@ stages:
       inputs:
         artifact: linux-universal-home-linux-arm64
         path: $(monitoringHome)/linux-arm64
+
+    - task: DownloadPipelineArtifact@2
+      displayName: Download linux universal artifacts
+      inputs:
+        artifact: linux-universal-home-linux-arm64
+        path: $(monitoringHome)/linux-musl-arm64
 
     - script: tracer\build.cmd CreateBundleHome BuildBundleNuget BuildRunnerTool PackRunnerToolNuget BuildStandaloneTool BuildBenchmarkNuget
       displayName: Build Bundle, Benchmark NuGet and tools
@@ -3502,6 +3596,10 @@ stages:
     - publish: $(artifacts)/dd-trace-linux-arm64.tar.gz
       displayName: Uploading runner standalone linux-arm64 artifact
       artifact: runner-standalone-linux-arm64
+
+    - publish: $(artifacts)/dd-trace-linux-musl-arm64.tar.gz
+      displayName: Uploading runner standalone linux-musl-arm64 artifact
+      artifact: runner-standalone-linux-musl-arm64
 
     - publish: $(artifacts)/dd-trace-osx-x64.tar.gz
       displayName: Uploading runner standalone osx-x64 artifact
@@ -3625,7 +3723,7 @@ stages:
           artifactSuffix: linux-x64
           poolName: azure-linux-scale-set-2
           targetArch: x64
-        alpine:
+        alpine_x64:
           baseImage: alpine
           artifactSuffix: linux-musl-x64
           poolName: azure-linux-scale-set-2
@@ -3633,6 +3731,11 @@ stages:
         arm64:
           baseImage: debian
           artifactSuffix: linux-arm64
+          poolname: aws-arm64-auto-scaling
+          targetArch: arm64
+        alpine_arm64:
+          baseImage: alpine
+          artifactSuffix: linux-musl-arm64
           poolname: aws-arm64-auto-scaling
           targetArch: arm64
 
@@ -5367,7 +5470,7 @@ stages:
         - template: steps/run-in-docker.yml
           parameters:
             build: true
-            baseImage: debian
+            baseImage: alpine
             command: "CheckSmokeTestsForErrors"
 
 - stage: installer_chiseled_smoke_tests_arm64
@@ -5531,7 +5634,7 @@ stages:
     - template: steps/run-in-docker.yml
       parameters:
         build: true
-        baseImage: debian
+        baseImage: alpine
         command: "CheckSmokeTestsForErrors"
 
 - stage: dotnet_tool_smoke_tests_arm64
@@ -5594,81 +5697,8 @@ stages:
     - template: steps/run-in-docker.yml
       parameters:
         build: true
-        baseImage: debian
+        baseImage: alpine
         command: "CheckSmokeTestsForErrors"
-
-- stage: installer_noop_smoke_tests_arm64
-  condition: and(succeeded(), eq(variables['isBenchmarksOnlyBuild'], 'False'), or(eq(variables.isMainOrReleaseBranch, 'true'), eq(variables.run_all_installer_tests, 'true')))
-  dependsOn: [package_arm64, generate_variables, merge_commit_id]
-  variables:
-    targetShaId: $[ stageDependencies.merge_commit_id.fetch.outputs['set_sha.sha']]
-    targetBranch: $[ stageDependencies.merge_commit_id.fetch.outputs['set_sha.branch']]
-  jobs:
-    - template: steps/update-github-status-jobs.yml
-      parameters:
-        jobs: [linux]
-
-    - job: linux
-      timeoutInMinutes: 45 # should take ~15 mins
-      strategy:
-        matrix: $[ stageDependencies.generate_variables.generate_variables_job.outputs['generate_variables_step.installer_noop_smoke_tests_arm64_matrix'] ]
-      variables:
-        smokeTestAppDir: "$(System.DefaultWorkingDirectory)/tracer/test/test-applications/regression/AspNetCoreSmokeTest"
-      pool:
-        name: aws-arm64-auto-scaling
-
-      steps:
-        - template: steps/clone-repo.yml
-          parameters:
-            targetShaId: $(targetShaId)
-            targetBranch: $(targetBranch)
-
-        - task: DownloadPipelineArtifact@2
-          displayName: Download artifacts to smoke test directory
-          inputs:
-            artifact: $(linuxArtifacts)
-            path: $(smokeTestAppDir)/artifacts
-
-        # Run the "normal" noop installer smoke tests 
-        - script: |
-            docker-compose -p $(DockerComposeProjectName) build \
-              --build-arg DOTNETSDK_VERSION=$(dotnetCoreSdkLatestVersionShort) \
-              --build-arg RUNTIME_IMAGE=$(runtimeImage) \
-              --build-arg PUBLISH_FRAMEWORK=$(publishFramework) \
-              smoke-tests
-          env:
-            dockerTag: $(dockerTag)
-            DD_LOGGER_DD_API_KEY: $(ddApiKey)
-          displayName: docker-compose build smoke-tests
-          retryCountOnTaskFailure: 3
-
-        - template: steps/run-snapshot-test.yml
-          parameters:
-            target: 'smoke-tests'
-            snapshotPrefix: "smoke_test"
-            isNoop: true
-        
-        # Run the "dd-dotnet" "unsupported tfm" installer smoke tests
-        - script: |
-            echo "Expected installer: $(expectedInstaller)"
-
-            cd tracer/build/artifacts
-            result=$(docker run --rm -v ${PWD}:/app --entrypoint /app/dd-dotnet.sh $(runtimeImage))
-            if [[ "$result" != *"$(expectedInstaller)"* ]]; then
-              echo "Incorrect message. Expected '$(expectedInstaller)', but found:"
-              echo $result
-              exit 1
-            fi
-
-            echo "Received expected response:"
-            echo $result
-
-          displayName: Run artifact test (expecting failure)
-
-        - publish: artifacts/build_data
-          artifact: _$(System.StageName)_$(Agent.JobName)_logs_$(System.JobAttempt)
-          condition: succeededOrFailed()
-          continueOnError: true
 
 - stage: nuget_installer_smoke_tests_windows
   condition: and(succeeded(), eq(variables['isBenchmarksOnlyBuild'], 'False'), or(eq(variables.isMainOrReleaseBranch, 'true'), eq(variables.run_all_installer_tests, 'true')))

tracer/build/_build/Build.VariableGenerations.cs
@@ -260,6 +260,7 @@ partial class Build : NukeBuild
                 var baseImages = new []
                 {
                     (baseImage: "debian", artifactSuffix: "linux-arm64"),
+                    (baseImage: "alpine", artifactSuffix: "linux-musl-arm64"),
                 };
 
                 var targetFrameworks = GetTestingFrameworks(isArm64: true).Except(new[] { TargetFramework.NET461, TargetFramework.NET462, TargetFramework.NETSTANDARD2_0 });
@@ -400,9 +401,6 @@ partial class Build : NukeBuild
                 GenerateLinuxSmokeTestsArm64Matrix();
                 GenerateLinuxChiseledInstallerSmokeTestsMatrix();
                 GenerateLinuxChiseledInstallerArm64SmokeTestsMatrix();
-
-                // noop smoke tests
-                GenerateLinuxInstallerArm64NoopSmokeTestsMatrix();
 
                 // nuget smoke tests
                 GenerateLinuxNuGetSmokeTestsMatrix();
@@ -668,22 +666,24 @@ partial class Build : NukeBuild
                         dockerName: "andrewlock/dotnet-fedora-arm64"
                     );
 
-                    // We don't support alpine on arm64 yet, so just use debian for the tar test
+                    // Alpine tests with the default package
                     AddToLinuxSmokeTestsMatrix(
                         matrix,
-                        "debian_tar",
+                        "alpine",
                         new SmokeTestImage[]
                         {
-                            new (publishFramework: TargetFramework.NET8_0, "8.0-bookworm-slim"),
-                            new (publishFramework: TargetFramework.NET7_0, "7.0-bullseye-slim"),
-                            new (publishFramework: TargetFramework.NET6_0, "6.0-bullseye-slim"),
-                            // https://github.com/dotnet/runtime/issues/66707
-                            new (publishFramework: TargetFramework.NET5_0, "5.0-buster-slim", runCrashTest: false),
+                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.20"),
+                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.19-composite"),
+                            new (publishFramework: TargetFramework.NET7_0, "7.0-alpine3.18"),
+                            new (publishFramework: TargetFramework.NET6_0, "6.0-alpine3.18"),
+                            // runtimes on earlier alpine versions aren't provided by MS
                         },
-                        installer: "datadog-dotnet-apm_*_arm64.deb", // we advise customers to install the .deb in this case
-                        installCmd: "tar -C /opt/datadog -xzf ./datadog-dotnet-apm*.arm64.tar.gz",
-                        linuxArtifacts: "linux-packages-linux-arm64",
-                        runtimeId: "linux-arm64",
+                        // currently we direct customers to the musl-specific package in the command line.
+                        // Should we update this to point to the default artifact instead?
+                        installer: "datadog-dotnet-apm*.arm64.tar.gz", // used by the dd-dotnet checks to direct customers to the right place
+                        installCmd: "tar -C /opt/datadog -xzf ./datadog-dotnet-apm*.tar.gz",
+                        linuxArtifacts: "linux-packages-linux-arm64", // these are what we download
+                        runtimeId: "linux-musl-arm64", // used by the dd-dotnet checks to direct customers to the right place
                         dockerName: "mcr.microsoft.com/dotnet/aspnet"
                     );
 
@@ -865,6 +865,22 @@ partial class Build : NukeBuild
                         dockerName: "mcr.microsoft.com/dotnet/aspnet"
                     );
 
+                    AddToNuGetSmokeTestsMatrix(
+                        matrix,
+                        "alpine",
+                        new SmokeTestImage[]
+                        {
+                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.20"),
+                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.19-composite"),
+                            new (publishFramework: TargetFramework.NET7_0, "7.0-alpine3.18"),
+                            new (publishFramework: TargetFramework.NET6_0, "6.0-alpine3.18"),
+                        },
+                        relativeProfilerPath: "datadog/linux-musl-arm64/Datadog.Trace.ClrProfiler.Native.so",
+                        relativeApiWrapperPath: "datadog/linux-musl-arm64/Datadog.Linux.ApiWrapper.x64.so",
+                        dockerName: "mcr.microsoft.com/dotnet/aspnet"
+                    );
+
+
                     Logger.Information($"Installer smoke tests nuget matrix ARM64");
                     Logger.Information(JsonConvert.SerializeObject(matrix, Formatting.Indented));
                     AzurePipelines.Instance.SetOutputVariable("nuget_installer_linux_smoke_tests_arm64_matrix", JsonConvert.SerializeObject(matrix, Formatting.None));
@@ -1006,6 +1022,20 @@ partial class Build : NukeBuild
                             new (publishFramework: TargetFramework.NET5_0, "5.0-focal", runCrashTest: false),
                         },
                         platformSuffix: "linux-arm64",
+                        dockerName: "mcr.microsoft.com/dotnet/aspnet"
+                    );
+
+                    AddToDotNetToolSmokeTestsMatrix(
+                        matrix,
+                        "alpine",
+                        new SmokeTestImage[]
+                        {
+                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.20"),
+                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.19-composite"),
+                            new (publishFramework: TargetFramework.NET7_0, "7.0-alpine3.18"),
+                            new (publishFramework: TargetFramework.NET6_0, "6.0-alpine3.18"),
+                        },
+                        platformSuffix: "linux-musl-arm64",
                         dockerName: "mcr.microsoft.com/dotnet/aspnet"
                     );
 
@@ -1239,36 +1269,6 @@ partial class Build : NukeBuild
                     AzurePipelines.Instance.SetOutputVariable("dotnet_tool_nuget_installer_macos_smoke_tests_matrix", JsonConvert.SerializeObject(matrix, Formatting.None));
                 }
 
-                void GenerateLinuxInstallerArm64NoopSmokeTestsMatrix()
-                {
-                    var matrix = new Dictionary<string, object>();
-
-                    // Alpine tests with the default package (alpine isn't supported on arm64 currently, so this is a noop)
-                    AddToLinuxSmokeTestsMatrix(
-                        matrix,
-                        "alpine",
-                        new SmokeTestImage[]
-                        {
-                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.18"),
-                            new (publishFramework: TargetFramework.NET8_0, "8.0-alpine3.18-composite"),
-                            new (publishFramework: TargetFramework.NET7_0, "7.0-alpine3.16"),
-                            new (publishFramework: TargetFramework.NET6_0, "6.0-alpine3.14"),
-                            new (publishFramework: TargetFramework.NET5_0, "5.0-alpine3.13"),
-                        },
-                        // currently we direct customers to the musl-specific package in the command line.
-                        // Should we update this to point to the default artifact instead?
-                        installer: "Alpine ARM64 is not supported", // used by the dd-dotnet checks to direct customers to the right place
-                        installCmd: "tar -C /opt/datadog -xzf ./datadog-dotnet-apm*.tar.gz",
-                        linuxArtifacts: "linux-packages-linux-arm64", // these are what we download
-                        runtimeId: "linux-musl-arm64", // used by the dd-dotnet checks to direct customers to the right place
-                        dockerName: "mcr.microsoft.com/dotnet/aspnet"
-                    );
-
-                    Logger.Information($"Installer noop smoke tests arm64 matrix");
-                    Logger.Information(JsonConvert.SerializeObject(matrix, Formatting.Indented));
-                    AzurePipelines.Instance.SetOutputVariable("installer_noop_smoke_tests_arm64_matrix", JsonConvert.SerializeObject(matrix, Formatting.None));
-                }
-
                 static string GetInstallerChannel(string publishFramework) =>
                     publishFramework.Replace("netcoreapp", string.Empty)
                                     .Replace("net", string.Empty);
@@ -1277,7 +1277,7 @@ partial class Build : NukeBuild
             void GenerateIntegrationTestsDebuggerArm64Matrices()
             {
                 var targetFrameworks = TestingFrameworksDebugger.Except(new[] { TargetFramework.NET462, TargetFramework.NETCOREAPP2_1, TargetFramework.NETCOREAPP3_1,  });
-                var baseImages = new[] { "debian" };
+                var baseImages = new[] { "debian", "alpine" };
                 var optimizations = new[] { "true", "false" };
 
                 var matrix = new Dictionary<string, object>();

Datadog.Trace.Minimal.slnf
@@ -2,7 +2,6 @@
   "solution": {
     "path": "Datadog.Trace.sln",
     "projects": [
-      "tracer\\build\\_build\\_build.csproj",
       "tracer\\src\\Datadog.Trace.ClrProfiler.Managed.Loader\\Datadog.Trace.ClrProfiler.Managed.Loader.csproj",
       "tracer\\src\\Datadog.Tracer.Native\\Datadog.Tracer.Native.DLL.vcxproj",
       "tracer\\src\\Datadog.Tracer.Native\\Datadog.Tracer.Native.vcxproj",

commit 1973973728cbcfbdb1e02d4622869a5a4bda8452

Fix debugger arm64 alpine tests (#5965)
## Summary of changes

Fixes the debugger alpine tests that were broken by #5933 

## Reason for change

The debugger tests were broken, but because that PR didn't run the
debugger tests by default, we didn't notice.

## Implementation details

Download the correct artifacts in arm64 alpine

.azure-pipelines/ultimate-pipeline.yml
@@ -2960,7 +2960,7 @@ stages:
 
         - template: steps/restore-working-directory.yml
           parameters:
-            artifact: build-linux-arm64-working-directory
+            artifact: build-$(artifactSuffix)-working-directory
 
         - template: steps/run-in-docker.yml
           parameters:
@@ -2970,9 +2970,9 @@ stages:
             apiKey: $(DD_LOGGER_DD_API_KEY)
 
         - task: DownloadPipelineArtifact@2
-          displayName: Download arm64 monitoring home
+          displayName: Download $(artifactSuffix) monitoring home
           inputs:
-            artifact: linux-monitoring-home-linux-arm64
+            artifact: linux-monitoring-home-$(artifactSuffix)
             path: $(monitoringHome)
 
         - script: |

tracer/build/_build/Build.VariableGenerations.cs
@@ -1277,13 +1277,17 @@ partial class Build : NukeBuild
             void GenerateIntegrationTestsDebuggerArm64Matrices()
             {
                 var targetFrameworks = TestingFrameworksDebugger.Except(new[] { TargetFramework.NET462, TargetFramework.NETCOREAPP2_1, TargetFramework.NETCOREAPP3_1,  });
-                var baseImages = new[] { "debian", "alpine" };
+                var baseImages = new []
+                {
+                    (baseImage: "debian", artifactSuffix: "linux-arm64"),
+                    (baseImage: "alpine", artifactSuffix: "linux-musl-arm64"),
+                };
                 var optimizations = new[] { "true", "false" };
 
                 var matrix = new Dictionary<string, object>();
                 foreach (var framework in targetFrameworks)
                 {
-                    foreach (var baseImage in baseImages)
+                    foreach (var (baseImage, artifactSuffix) in baseImages)
                     {
                         foreach (var optimize in optimizations)
                         {
@@ -1293,6 +1297,7 @@ partial class Build : NukeBuild
                                            publishTargetFramework = framework,
                                            baseImage = baseImage,
                                            optimize = optimize,
+                                           artifactSuffix = artifactSuffix,
                                        });
                         }
                     }