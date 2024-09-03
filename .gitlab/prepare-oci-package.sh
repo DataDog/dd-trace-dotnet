a63fc754089a28da2cde1c62ba02fd85b245f334
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-09-03 10:49:23 -0400

    Create musl+arm placeholder image + other fixes for 2.x

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -23,22 +23,30 @@ if [ "$ARCH" == "amd64" ]; then
     SUFFIX=""
   fi
 elif [ "$ARCH" == "arm64" ]; then
-  SUFFIX=".arm64"
+  if [ "$LIBC" == "musl" ]; then
+    SUFFIX="placeholder"
+  else
+    SUFFIX=".arm64"
+  fi
 else
   echo "Unsupported architecture: $ARCH"
   exit 1
 fi
 
-SRC_TAR="../artifacts/datadog-dotnet-apm-$PACKAGE_VERSION${SUFFIX}.tar.gz"
-
-if [ ! -f $SRC_TAR ]; then
-   echo "$SRC_TAR was not found!"
-   exit 1
-fi
-
 mkdir -p sources
 
-# extract the tarball, making sure to preserve the owner and permissions
-tar --same-owner -pxvzf $SRC_TAR -C sources
+if [ "$SUFFIX" != "placeholder" ]; then
+  SRC_TAR="../artifacts/datadog-dotnet-apm-$PACKAGE_VERSION${SUFFIX}.tar.gz"
+
+  if [ ! -f $SRC_TAR ]; then
+     echo "$SRC_TAR was not found!"
+     exit 1
+  fi
+
+  # extract the tarball, making sure to preserve the owner and permissions
+  tar --same-owner -pxvzf $SRC_TAR -C sources
+else
+  touch sources/placeholder.txt
+fi
 
 echo -n $VERSION > sources/version
