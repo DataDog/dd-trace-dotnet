f90a6ddd2159347575025a36abfcd8196c884968
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2024-09-04 17:23:40 +0200

    Fix test on Windows

--- profiler/test/Datadog.Profiler.Native.Tests/ConfigurationTest.cpp
+++ profiler/test/Datadog.Profiler.Native.Tests/ConfigurationTest.cpp
@@ -1122,11 +1122,15 @@ TEST_F(ConfigurationTest, CheckCallstackCachingIsDisabledByDefault)
     ASSERT_FALSE(configuration.IsCallstackCachingEnabled());
 }
 
-TEST_F(ConfigurationTest, CheckCAllstackCachingIsEnabledIfEnvVarIsTrue)
+TEST_F(ConfigurationTest, CheckCallstackCachingIsEnabledIfEnvVarIsTrue)
 {
     EnvironmentHelper::EnvironmentVariable ar(EnvironmentVariables::CacheCallstack, WStr("1"));
     auto configuration = Configuration{};
+#ifdef _WINDOWS
+    ASSERT_FALSE(configuration.IsCallstackCachingEnabled());
+#else
     ASSERT_TRUE(configuration.IsCallstackCachingEnabled());
+#endif
 }
 
 TEST_F(ConfigurationTest, CheckCAllstackCachingIsEnabledIfEnvVarIsFalse)
