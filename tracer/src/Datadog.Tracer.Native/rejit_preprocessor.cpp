ff4bb6df0cc23bbc61ad7f7bbe34c4feaa7484c6
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-09-06 15:20:57 +0200

    Add proper error checking around GetModuleMetadata (#5985 => v2) (#5992)
    ## Summary of changes
    
    Explicitly check for `S_OK` when calling `GetModuleMetadata`.
    
    ## Reason for change
    
    `GetModuleMetadata` can return `S_FALSE` (one known case is if the
    module is a resource, but there might be other). We've been using the
    `SUCCEEDED`/`FAILED` macros to check the error code, but `S_FALSE` is
    not considered as an error so we would mistakenly assume that the call
    succeeded.
    
    ## Implementation details
    
    Note that I only updated the error checks. There are a couple of places
    where we call `GetModuleMetadata` without checking the error code, I
    haven't touched those.
    
    For `GetModuleInterfaces` in IAST, I also moved the call to `Release` to
    only do it if the call succeeded.

--- tracer/src/Datadog.Tracer.Native/rejit_preprocessor.cpp
+++ tracer/src/Datadog.Tracer.Native/rejit_preprocessor.cpp
@@ -576,7 +576,7 @@ ULONG RejitPreprocessor<RejitRequestDefinition>::PreprocessRejitRequests(
                     Logger::Debug("  Loading Assembly Metadata...");
                     auto hr = corProfilerInfo->GetModuleMetaData(moduleInfo.id, ofRead | ofWrite, IID_IMetaDataImport2,
                                                                  metadataInterfaces.GetAddressOf());
-                    if (FAILED(hr))
+                    if (hr != S_OK)
                     {
                         Logger::Warn("CallTarget_RequestRejitForModule failed to get metadata interface for ",
                                      moduleInfo.id, " ", moduleInfo.assembly.name);
@@ -799,7 +799,7 @@ ULONG RejitPreprocessor<RejitRequestDefinition>::PreprocessRejitRequests(
                     Logger::Debug("  Loading Assembly Metadata...");
                     auto hr = corProfilerInfo->GetModuleMetaData(moduleInfo.id, ofRead | ofWrite, IID_IMetaDataImport2,
                                                                  metadataInterfaces.GetAddressOf());
-                    if (FAILED(hr))
+                    if (hr != S_OK)
                     {
                         Logger::Warn("CallTarget_RequestRejitForModule failed to get metadata interface for ",
                                      moduleInfo.id, " ", moduleInfo.assembly.name);
