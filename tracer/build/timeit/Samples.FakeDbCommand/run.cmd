8ddbbc8571ea6ca2328ce72f40011478ef6ea696
Tony Redondo <tony.redondo@datadoghq.com>
2024-09-20 10:49:29 +0200

    changes

--- tracer/build/timeit/Samples.FakeDbCommand/run.cmd
+++ tracer/build/timeit/Samples.FakeDbCommand/run.cmd
@@ -23,8 +23,3 @@ echo *********************
 echo .NET Core 6.0
 echo *********************
 dotnet timeit Samples.FakeDbCommand.windows.net60.json
-
-echo *********************
-echo .NET Core 8.0
-echo *********************
-dotnet timeit Samples.FakeDbCommand.windows.net80.json
