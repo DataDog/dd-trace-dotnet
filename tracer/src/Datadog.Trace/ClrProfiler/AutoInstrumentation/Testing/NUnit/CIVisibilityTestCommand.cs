89d34f75765e01f90dfb3276bb7b37da526ea73a
Tony Redondo <tony.redondo@datadoghq.com>
2024-09-23 16:16:18 +0200

    Flaky retry for NUnit testing framework

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/NUnit/CIVisibilityTestCommand.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/NUnit/CIVisibilityTestCommand.cs
@@ -5,14 +5,15 @@
 #nullable enable
 
 using System;
+using System.Threading;
 using Datadog.Trace.Ci;
-using Datadog.Trace.Ci.Tags;
 using Datadog.Trace.DuckTyping;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Testing.NUnit;
 
 internal class CIVisibilityTestCommand
 {
+    private static int _totalRetries = -1;
     private readonly ITestCommand _innerCommand;
 
     public CIVisibilityTestCommand(ITestCommand innerCommand)
@@ -23,71 +24,122 @@ internal class CIVisibilityTestCommand
     [DuckReverseMethod]
     public object? Execute(object contextObject)
     {
+        Interlocked.CompareExchange(ref _totalRetries, CIVisibility.Settings.TotalFlakyRetryCount, -1);
         var context = contextObject.TryDuckCast<ITestExecutionContextWithRepeatCount>(out var contextWithRepeatCount) ?
                           contextWithRepeatCount :
                           contextObject.DuckCast<ITestExecutionContext>();
         var executionNumber = 0;
         var result = ExecuteTest(context, executionNumber++, out var isTestNew, out var duration);
-        if (result.ResultState.Status != TestStatus.Skipped &&
-            result.ResultState.Status != TestStatus.Inconclusive &&
-            isTestNew)
+        var resultStatus = result.ResultState.Status;
+
+        if (resultStatus != TestStatus.Skipped &&
+            resultStatus != TestStatus.Inconclusive)
         {
-            // Get retries number
-            var remainingRetries = Common.GetNumberOfExecutionsForDuration(duration) - 1;
-
-            // Retries
-            var retryNumber = 0;
-            var totalRetries = remainingRetries;
-            while (remainingRetries-- > 0)
+            if (isTestNew)
             {
-                retryNumber++;
-                Common.Log.Debug<int, int>("EFD: [Retry {Num}] Running retry of {TotalRetries}.", retryNumber, totalRetries);
-                ClearResultForRetry(context);
-                var retryResult = ExecuteTest(context, executionNumber++, out _, out _);
-                Common.Log.Debug<int>("EFD: [Retry {Num}] Aggregating results.", retryNumber);
-                result.Duration += retryResult.Duration;
-                result.StartTime = result.StartTime < retryResult.StartTime ? result.StartTime : retryResult.StartTime;
-                result.EndTime = result.EndTime > retryResult.EndTime ? result.EndTime : retryResult.EndTime;
+                // **************************************************************
+                // Early flake detection mode
+                // **************************************************************
 
-                var message = result.Message;
-                if (!string.IsNullOrEmpty(retryResult.Message))
+                // Get retries number
+                var remainingRetries = Common.GetNumberOfExecutionsForDuration(duration) - 1;
+
+                // Retries
+                var retryNumber = 0;
+                var totalRetries = remainingRetries;
+                while (remainingRetries-- > 0)
                 {
-                    message += Environment.NewLine + retryResult.Message;
+                    retryNumber++;
+                    Common.Log.Debug<int, int>("EFD: [Retry {Num}] Running retry of {TotalRetries}.", retryNumber, totalRetries);
+                    ClearResultForRetry(context);
+                    var retryResult = ExecuteTest(context, executionNumber++, out _, out _);
+                    Common.Log.Debug<int>("EFD: [Retry {Num}] Aggregating results.", retryNumber);
+                    AgregateResults(result, retryResult);
                 }
 
-                var stackTrace = result.StackTrace;
-                if (!string.IsNullOrEmpty(retryResult.StackTrace))
+                if (retryNumber > 0)
                 {
-                    stackTrace += Environment.NewLine + retryResult.StackTrace;
-                }
-
-                result.SetResult(
-                    result.ResultState.Status != TestStatus.Passed ? retryResult.ResultState : result.ResultState,
-                    message,
-                    stackTrace);
-
-                if (retryResult.Output is { } testResultOutput && testResultOutput != "\r\n" && testResultOutput != "\n")
-                {
-                    result.OutWriter.WriteLine(testResultOutput);
-                }
-
-                if (retryResult.AssertionResults.Count > 0)
-                {
-                    foreach (var assertionResult in retryResult.AssertionResults)
-                    {
-                        result.AssertionResults.Add(assertionResult);
-                    }
+                    Common.Log.Debug("EFD: All retries were executed.");
                 }
             }
-
-            if (retryNumber > 0)
+            else if (resultStatus == TestStatus.Failed && CIVisibility.Settings.FlakyRetryEnabled == true)
             {
-                Common.Log.Debug("EFD: All retries were executed.");
+                // **************************************************************
+                // Flaky retry mode
+                // **************************************************************
+
+                // Get retries number
+                var remainingRetries = CIVisibility.Settings.FlakyRetryCount;
+
+                // Retries
+                var retryNumber = 0;
+                while (remainingRetries-- > 0)
+                {
+                    if (Interlocked.Decrement(ref _totalRetries) <= 0)
+                    {
+                        Common.Log.Debug<int>("FlakyRetry: Exceeded de number of total retries. [{Number}]", CIVisibility.Settings.TotalFlakyRetryCount);
+                        break;
+                    }
+
+                    retryNumber++;
+                    Common.Log.Debug<int>("FlakyRetry: [Retry {Num}] Running retry...", retryNumber);
+                    ClearResultForRetry(context);
+                    var retryResult = ExecuteTest(context, executionNumber++, out _, out _);
+                    Common.Log.Debug<int>("FlakyRetry: [Retry {Num}] Aggregating results.", retryNumber);
+                    AgregateResults(result, retryResult);
+                    if (retryResult.ResultState.Status != TestStatus.Failed)
+                    {
+                        Common.Log.Debug<int>("FlakyRetry: [Retry {Num}] Test passed in retry.", retryNumber);
+                        break;
+                    }
+                }
+
+                if (remainingRetries <= 0)
+                {
+                    Common.Log.Debug("FlakyRetry: All retries were executed.");
+                }
             }
         }
 
         context.CurrentResult = result;
         return result.Instance!;
+    }
+
+    private static void AgregateResults(ITestResult result, ITestResult retryResult)
+    {
+        result.Duration += retryResult.Duration;
+        result.StartTime = result.StartTime < retryResult.StartTime ? result.StartTime : retryResult.StartTime;
+        result.EndTime = result.EndTime > retryResult.EndTime ? result.EndTime : retryResult.EndTime;
+
+        var message = result.Message;
+        if (!string.IsNullOrEmpty(retryResult.Message))
+        {
+            message += Environment.NewLine + retryResult.Message;
+        }
+
+        var stackTrace = result.StackTrace;
+        if (!string.IsNullOrEmpty(retryResult.StackTrace))
+        {
+            stackTrace += Environment.NewLine + retryResult.StackTrace;
+        }
+
+        result.SetResult(
+            result.ResultState.Status != TestStatus.Passed ? retryResult.ResultState : result.ResultState,
+            message,
+            stackTrace);
+
+        if (retryResult.Output is { } testResultOutput && testResultOutput != "\r\n" && testResultOutput != "\n")
+        {
+            result.OutWriter.WriteLine(testResultOutput);
+        }
+
+        if (retryResult.AssertionResults.Count > 0)
+        {
+            foreach (var assertionResult in retryResult.AssertionResults)
+            {
+                result.AssertionResults.Add(assertionResult);
+            }
+        }
     }
 
     private void ClearResultForRetry(ITestExecutionContext context)
