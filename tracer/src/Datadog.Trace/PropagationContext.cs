0a4afa386327be24fb4c54a4208b792b31c6fa17
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-09-30 18:27:46 -0400

    add PropagationContext, Baggage, and BaggageTests

---
+++ tracer/src/Datadog.Trace/PropagationContext.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="PropagationContext.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+namespace Datadog.Trace;
+
+internal readonly struct PropagationContext
+{
+    public PropagationContext(SpanContext? spanContext, Baggage? baggage)
+    {
+        SpanContext = spanContext;
+        Baggage = baggage;
+    }
+
+    public SpanContext? SpanContext { get; init; }
+
+    public Baggage? Baggage { get; init; }
+}
