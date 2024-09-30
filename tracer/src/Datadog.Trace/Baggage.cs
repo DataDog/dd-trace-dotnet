2f6b2690023d4ee32acb7ae746c070c4dfaee2d1
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-09-30 18:27:46 -0400

    add new types

---
+++ tracer/src/Datadog.Trace/Baggage.cs
@@ -0,0 +1,155 @@
+﻿// <copyright file="Baggage.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Threading;
+using Datadog.Trace.Util;
+
+#nullable enable
+
+namespace Datadog.Trace;
+
+/// <summary>
+/// Baggage is a collection of name-value pairs that are propagated to downstream services.
+/// </summary>
+internal class Baggage
+{
+    private static readonly AsyncLocal<Baggage> AsyncStorage = new();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Baggage"/> class.
+    /// </summary>
+    public Baggage()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Baggage"/> class using the specified items.
+    /// </summary>
+    /// <param name="items">The baggage items.</param>
+    public Baggage(IEnumerable<KeyValuePair<string, string>> items)
+    {
+        if (items != null!)
+        {
+            Items = new OrderedKeyValuePairList<string, string>(items);
+        }
+    }
+
+    private OrderedKeyValuePairList<string, string>? Items { get; set; }
+
+    /// <summary>
+    /// Gets or sets the baggage collection for the current execution context.
+    /// </summary>
+    public static Baggage Current
+    {
+        get => AsyncStorage.Value ??= new Baggage();
+        set => AsyncStorage.Value = value;
+    }
+
+    /// <summary>
+    /// Gets the count of items in this baggage instance.
+    /// </summary>
+    public int Count => Items?.Count ?? 0;
+
+    /// <summary>
+    /// Gets the baggage value associated with the specified name.
+    /// </summary>
+    /// <param name="name">The baggage item name.</param>
+    /// <returns>Returns the baggage item value, or <c>null</c> if not found.</returns>
+    public string? Get(string name)
+    {
+        if (name == null!)
+        {
+            ThrowHelper.ThrowArgumentNullException(nameof(name));
+        }
+
+        if (Items is null)
+        {
+            return null;
+        }
+
+        Items.TryGetValue(name, out var value);
+        return value;
+    }
+
+    /// <summary>
+    /// Gets all baggage values.
+    /// </summary>
+    /// <returns>All baggage values.</returns>
+    public KeyValuePair<string, string>[] GetAll()
+    {
+        return Items?.ToArray() ?? [];
+    }
+
+    /// <summary>
+    /// Sets the baggage value associated with the specified name.
+    /// </summary>
+    /// <param name="name">The baggage item name.</param>
+    /// <param name="value">The baggage item value.</param>
+    public void Set(string name, string? value)
+    {
+        if (name == null!)
+        {
+            ThrowHelper.ThrowArgumentNullException(nameof(name));
+        }
+
+        if (value is null)
+        {
+            Remove(name);
+        }
+        else
+        {
+            Items ??= new OrderedKeyValuePairList<string, string>();
+            Items[name] = value;
+        }
+    }
+
+    /// <summary>
+    /// Removes the baggage value associated with the specified name.
+    /// </summary>
+    /// <param name="name">The baggage item name.</param>
+    /// <returns><c>true</c> if the object was removed successfully; otherwise, <c>false</c>.</returns>
+    public bool Remove(string name)
+    {
+        if (name == null!)
+        {
+            ThrowHelper.ThrowArgumentNullException(nameof(name));
+        }
+
+        return Items?.Remove(name, out _) ?? false;
+    }
+
+    /// <summary>
+    /// Removes all baggage values.
+    /// </summary>
+    public void Clear()
+    {
+        Items?.Clear();
+    }
+
+    /// <summary>
+    /// Adds the baggage items from <paramref name="baggage"/> to this baggage.
+    /// </summary>
+    public void Merge(Baggage? baggage)
+    {
+        if (baggage?.Items is null)
+        {
+            // nothing to add
+            return;
+        }
+
+        if (Items is null)
+        {
+            // initialize with the items from the other baggage
+            Items = new OrderedKeyValuePairList<string, string>(baggage.Items);
+            return;
+        }
+
+        foreach (var item in baggage.Items)
+        {
+            Items[item.Key] = item.Value;
+        }
+    }
+}
---
+++ tracer/src/Datadog.Trace/Util/OrderedKeyValuePairList.cs
@@ -0,0 +1,219 @@
+﻿// <copyright file="OrderedKeyValuePairList.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+namespace Datadog.Trace.Util;
+
+internal class OrderedKeyValuePairList<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+{
+    private static readonly EqualityComparer<TKey> EqualityComparer = EqualityComparer<TKey>.Default;
+
+    private static readonly List<KeyValuePair<TKey, TValue>> EmptyList = [];
+
+    private readonly ReaderWriterLockSlim _lock = new();
+
+    private List<KeyValuePair<TKey, TValue>>? _list;
+
+    public OrderedKeyValuePairList()
+    {
+    }
+
+    public OrderedKeyValuePairList(IEnumerable<KeyValuePair<TKey, TValue>> items)
+    {
+        _list = [..items];
+    }
+
+    public int Count => _list?.Count ?? 0;
+
+    public TValue this[TKey key]
+    {
+        get
+        {
+            _lock.EnterReadLock();
+
+            try
+            {
+                if (TryGetValue(key, out var value))
+                {
+                    return value;
+                }
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+
+            ThrowHelper.ThrowKeyNotFoundException($"The key was not found: {key}");
+            return default!; // unreachable
+        }
+
+        set
+        {
+            _lock.EnterWriteLock();
+
+            try
+            {
+                Set(key, value);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+
+    private List<KeyValuePair<TKey, TValue>> EnsureListInitialized()
+    {
+        if (_list == null)
+        {
+            Interlocked.CompareExchange(ref _list, [], null);
+        }
+
+        return _list;
+    }
+
+    public void Set(TKey key, TValue value)
+    {
+        _lock.EnterWriteLock();
+
+        try
+        {
+            var list = EnsureListInitialized();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var item = list[i];
+
+                if (EqualityComparer.Equals(item.Key, key))
+                {
+                    list[i] = new KeyValuePair<TKey, TValue>(key, value);
+                    return;
+                }
+            }
+
+            // key is found, add new entry
+            list.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    public bool TryGetValue(TKey key, [NotNullWhen(true)] out TValue? value)
+    {
+        if (_list is { } list)
+        {
+            _lock.EnterReadLock();
+
+            try
+            {
+                foreach (var pair in list)
+                {
+                    if (EqualityComparer.Equals(pair.Key, key))
+                    {
+                        value = pair.Value!;
+                        return true;
+                    }
+                }
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        value = default;
+        return false;
+    }
+
+    public bool Remove(TKey key, [NotNullWhen(true)] out TValue? value)
+    {
+        if (_list is { } list)
+        {
+            _lock.EnterUpgradeableReadLock();
+
+            try
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (EqualityComparer.Equals(list[i].Key, key))
+                    {
+                        // found the specified key,
+                        // upgrade read lock to write lock
+                        _lock.EnterWriteLock();
+
+                        try
+                        {
+                            value = list[i].Value!;
+                            list.RemoveAt(i);
+                            return true;
+                        }
+                        finally
+                        {
+                            _lock.ExitWriteLock();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                _lock.ExitUpgradeableReadLock();
+            }
+        }
+
+        value = default;
+        return false;
+    }
+
+    public void Clear()
+    {
+        _lock.EnterWriteLock();
+
+        try
+        {
+            _list?.Clear();
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    public KeyValuePair<TKey, TValue>[] ToArray()
+    {
+        _lock.EnterReadLock();
+
+        try
+        {
+            return _list?.ToArray() ?? [];
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
+
+    public List<KeyValuePair<TKey, TValue>>.Enumerator GetEnumerator()
+    {
+        return _list?.GetEnumerator() ?? EmptyList.GetEnumerator();
+    }
+
+    IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
