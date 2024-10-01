c6da13c485e6382e9fb466122d6e157bb8d842e9
Igor Kravchenko <21974069+kr-igor@users.noreply.github.com>
2024-10-01 14:30:12 -0400

    Use index of

--- tracer/build/_build/Build.Profiler.Steps.cs
+++ tracer/build/_build/Build.Profiler.Steps.cs
@@ -180,9 +180,10 @@ partial class Build
                 .Select(x => x.Trim())
                 .Where(x => x.StartsWith("U "))
                 .Select(x => x.TrimStart("U "))
-                .Select(x => x.TrimEnd("@"))
+                .Select(x => x.Contains("@") ? x.Substring(0, x.IndexOf("@")) : x)
                 .OrderBy(x => x)
                 .ToList();
+            
 
 
             var received = string.Join(Environment.NewLine, symbols);
