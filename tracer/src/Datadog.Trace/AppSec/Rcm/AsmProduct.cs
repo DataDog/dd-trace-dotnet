255e5677fb00271c90236578ac9b4dc940eef7bf
Anna <anna.yafi@datadoghq.com>
2024-10-02 10:07:24 +0200

    [ASM][RCM] Read waf actions per file and remove per file (#6072)
    ## Summary of changes
    [Read waf actions](https://datadoghq.atlassian.net/browse/APPSEC-54989)
    per file and remove per file
    
    
    ## Reason for change
    
    ## Implementation details
    
    ## Test coverage
    
    ## Other details
    <!-- Fixes #{issue} -->
    
    <!-- ⚠️ Note: where possible, please obtain 2 approvals prior to
    merging. Unless CODEOWNERS specifies otherwise, for external teams it is
    typically best to have one review from a team member, and one review
    from apm-dotnet. Trivial changes do not require 2 reviews. -->

--- tracer/src/Datadog.Trace/AppSec/Rcm/AsmProduct.cs
+++ tracer/src/Datadog.Trace/AppSec/Rcm/AsmProduct.cs
@@ -46,20 +46,8 @@ internal class AsmProduct : IAsmConfigUpdater
 
             if (asmConfig.Actions != null)
             {
-                foreach (var action in asmConfig.Actions)
-                {
-                    if (action.Id is not null)
-                    {
-                        configurationStatus.Actions[action.Id] = action;
-                    }
-                }
-
+                configurationStatus.ActionsByFile[asmConfigName] = asmConfig.Actions;
                 configurationStatus.IncomingUpdateState.WafKeysToApply.Add(ConfigurationStatus.WafActionsKey);
-
-                if (asmConfig.Actions.Length == 0)
-                {
-                    configurationStatus.Actions.Clear();
-                }
             }
         }
     }
@@ -69,11 +57,13 @@ internal class AsmProduct : IAsmConfigUpdater
         var removedRulesOverride = false;
         var removedExclusions = false;
         var removedCustomRules = false;
+        var removedActions = false;
         foreach (var configurationPath in removedConfigsForThisProduct)
         {
             removedRulesOverride |= configurationStatus.RulesOverridesByFile.Remove(configurationPath.Path);
             removedExclusions |= configurationStatus.ExclusionsByFile.Remove(configurationPath.Path);
             removedCustomRules |= configurationStatus.CustomRulesByFile.Remove(configurationPath.Path);
+            removedActions |= configurationStatus.ActionsByFile.Remove(configurationPath.Path);
         }
 
         if (removedRulesOverride)
@@ -89,6 +79,11 @@ internal class AsmProduct : IAsmConfigUpdater
         if (removedCustomRules)
         {
             configurationStatus.IncomingUpdateState.WafKeysToApply.Add(ConfigurationStatus.WafCustomRulesKey);
+        }
+
+        if (removedActions)
+        {
+            configurationStatus.IncomingUpdateState.WafKeysToApply.Add(ConfigurationStatus.WafActionsKey);
         }
     }
 }
