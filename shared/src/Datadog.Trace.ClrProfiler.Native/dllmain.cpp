b7079da5eb569cea25c4eb6f1d8d25955a0900c3
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-10-03 12:23:05 +0200

    [Crashtracking] Implement support for Windows (#6088)
    ## Summary of changes
    
    Add crashtracking support for Windows.
    
    At the moment, only x64 is supported. Supporting x86 would require to
    include a 32-bit version of `dd-dotnet` in the package.
    
    ## Reason for change
    
    Until now, crashtracking only supported Linux. There are many crashes
    (due to either netfx or IIS) that only occur on Windows.
    
    ## Implementation details
    
    Crashtracking on Windows relies on
    [WER](https://learn.microsoft.com/en-us/windows/win32/wer/windows-error-reporting).
    
    When the native loader is loaded, we use
    `WerRegisterRuntimeExceptionModule` to register it as a crash handler.
    Note that .NET also registers a crash handler, and they're invoked by
    order of registration. To be invoked first, we unregister the .NET crash
    handler, register our own, then re-register the .NET crash handler.
    
    The .NET crash handler is in the DAC (mscordawks.dll for .NET Framework,
    mscordaccore.dll for .NET Core). The context argument is set to the
    address of `clr.dll`/`coreclr.dll` in memory (this is important because
    `WerUnregisterRuntimeExceptionModule` fails if the context value is
    different).
    
    When a crash occurs, Windows freezes the process and launches
    `WerFault.exe`. In WerFault, the crash handler is loaded and given a
    chance to inspect the crash (using the exported
    `OutOfProcessExceptionEventCallback` function). We invoke `dd-dotnet` to
    inspect the process and generate the crash report.
    
    The environment variables of the crashing process are not propagated to
    `WerFault.exe`. Because we need them (for instance, to know the location
    of the agent), we make a copy of all the `DD_*` environment variables
    and give the address of that copy as the "context" argument of
    `WerRegisterRuntimeExceptionModule`. When
    `OutOfProcessExceptionEventCallback` is invoked in `WerFault.exe`, we
    use `ReadProcessMemory` to read the saved environment variables from the
    crashing process, and set them for `dd-dotnet`.
    
    ## Test coverage
    
    Reusing the existing crashtracking tests.
    
    ## Other details
    
    More information on WER: https://minidump.net/windows-error-reporting/

--- shared/src/Datadog.Trace.ClrProfiler.Native/dllmain.cpp
+++ shared/src/Datadog.Trace.ClrProfiler.Native/dllmain.cpp
@@ -1,9 +1,21 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
+
+#if _WIN32 && AMD64
+#define CRASHTRACKING 1
+#endif
+
 #include "cor_profiler_class_factory.h"
 
 #include "log.h"
 #include "dynamic_dispatcher.h"
 #include "util.h"
+
+#if CRASHTRACKING
+#include "crashhandler.h"
+#endif 
+
+#include "util.h"
+#include <map>
 
 #ifndef _WIN32
 #undef EXTERN_C
@@ -14,67 +26,90 @@ using namespace datadog::shared::nativeloader;
 
 IDynamicDispatcher* dispatcher;
 
+#if CRASHTRACKING
+std::unique_ptr<CrashHandler> crashHandler;
+#endif
+
 EXTERN_C BOOL STDMETHODCALLTYPE DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     // Perform actions based on the reason for calling.
     switch (ul_reason_for_call)
     {
-        case DLL_PROCESS_ATTACH:
+    case DLL_PROCESS_ATTACH:
+    {
+        // Initialize once for each new process.
+        // Return FALSE to fail DLL load.
+
+        constexpr const bool IsLogDebugEnabledDefault = false;
+        bool isLogDebugEnabled;
+
+        shared::WSTRING isLogDebugEnabledStr = shared::GetEnvironmentValue(EnvironmentVariables::DebugLogEnabled);
+
+        // no environment variable set
+        if (isLogDebugEnabledStr.empty())
         {
-            // Initialize once for each new process.
-            // Return FALSE to fail DLL load.
+            Log::Info("No \"", EnvironmentVariables::DebugLogEnabled, "\" environment variable has been found.",
+                " Enable debug log = ", IsLogDebugEnabledDefault, " (default).");
 
-            constexpr const bool IsLogDebugEnabledDefault = false;
-            bool isLogDebugEnabled;
-
-            shared::WSTRING isLogDebugEnabledStr = shared::GetEnvironmentValue(EnvironmentVariables::DebugLogEnabled);
-
-            // no environment variable set
-            if (isLogDebugEnabledStr.empty())
+            isLogDebugEnabled = IsLogDebugEnabledDefault;
+        }
+        else
+        {
+            if (!shared::TryParseBooleanEnvironmentValue(isLogDebugEnabledStr, isLogDebugEnabled))
             {
-                Log::Info("No \"", EnvironmentVariables::DebugLogEnabled, "\" environment variable has been found.",
-                          " Enable debug log = ", IsLogDebugEnabledDefault, " (default).");
+                // invalid value for environment variable
+                Log::Info("Non boolean value \"", isLogDebugEnabledStr, "\" for \"",
+                    EnvironmentVariables::DebugLogEnabled, "\" environment variable.",
+                    " Enable debug log = ", IsLogDebugEnabledDefault, " (default).");
 
                 isLogDebugEnabled = IsLogDebugEnabledDefault;
             }
             else
             {
-                if (!shared::TryParseBooleanEnvironmentValue(isLogDebugEnabledStr, isLogDebugEnabled))
-                {
-                    // invalid value for environment variable
-                    Log::Info("Non boolean value \"", isLogDebugEnabledStr, "\" for \"",
-                              EnvironmentVariables::DebugLogEnabled, "\" environment variable.",
-                              " Enable debug log = ", IsLogDebugEnabledDefault, " (default).");
-
-                    isLogDebugEnabled = IsLogDebugEnabledDefault;
-                }
-                else
-                {
-                    // take environment variable into account
-                    Log::Info("Enable debug log = ", isLogDebugEnabled, " from (", EnvironmentVariables::DebugLogEnabled, " environment variable)");
-                }
+                // take environment variable into account
+                Log::Info("Enable debug log = ", isLogDebugEnabled, " from (", EnvironmentVariables::DebugLogEnabled, " environment variable)");
             }
-
-            if (isLogDebugEnabled)
-            {
-                Log::EnableDebug(true);
-            }
-
-            Log::Debug("DllMain: DLL_PROCESS_ATTACH");
-            Log::Debug("DllMain: Pointer size: ", 8 * sizeof(void*), " bits.");
-
-            dispatcher = new DynamicDispatcherImpl();
-            dispatcher->LoadConfiguration(GetConfigurationFilePath());
-
-            // *****************************************************************************************************************
-            break;
         }
 
-        case DLL_PROCESS_DETACH:
-            // Perform any necessary cleanup.
-            Log::Debug("DllMain: DLL_PROCESS_DETACH");
+        if (isLogDebugEnabled)
+        {
+            Log::EnableDebug(true);
+        }
 
-            break;
+#if CRASHTRACKING
+        bool telemetry_enabled = true;
+        shared::TryParseBooleanEnvironmentValue(shared::GetEnvironmentValue(L"DD_INSTRUMENTATION_TELEMETRY_ENABLED"), telemetry_enabled);
+
+        bool crashtracking_enabled = true;
+        shared::TryParseBooleanEnvironmentValue(shared::GetEnvironmentValue(L"DD_CRASHTRACKING_ENABLED"), crashtracking_enabled);
+
+        if (telemetry_enabled && crashtracking_enabled)
+        {
+            Log::Info("Crashtracking - Registering unhandled exception filter.");
+            crashHandler = CrashHandler::Create();
+        }
+        else
+        {
+            Log::Info("Crashtracking - Disabled by configuration.");
+        }
+#endif
+
+        dispatcher = new DynamicDispatcherImpl();
+        dispatcher->LoadConfiguration(GetConfigurationFilePath());
+
+        // *****************************************************************************************************************
+        break;
+    }
+
+    case DLL_PROCESS_DETACH:
+        // Perform any necessary cleanup.
+        Log::Debug("DllMain: DLL_PROCESS_DETACH");
+
+#if CRASHTRACKING
+        crashHandler = nullptr;
+#endif
+
+        break;
     }
     return TRUE; // Successful DLL_PROCESS_ATTACH.
 }
@@ -84,7 +119,7 @@ EXTERN_C HRESULT STDMETHODCALLTYPE DllGetClassObject(REFCLSID rclsid, REFIID rii
     Log::Debug("DllGetClassObject");
 
     // {846F5F1C-F9AE-4B07-969E-05C26BC060D8}
-    const GUID CLSID_CorProfiler = {0x846f5f1c, 0xf9ae, 0x4b07, {0x96, 0x9e, 0x5, 0xc2, 0x6b, 0xc0, 0x60, 0xd8}};
+    const GUID CLSID_CorProfiler = { 0x846f5f1c, 0xf9ae, 0x4b07, {0x96, 0x9e, 0x5, 0xc2, 0x6b, 0xc0, 0x60, 0xd8} };
 
     if (ppv == nullptr || rclsid != CLSID_CorProfiler)
     {
