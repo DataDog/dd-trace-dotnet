5e09b8961675be094b19e0323d961693e2d95e6c
Dylan Yang <dylan.yang@datadoghq.com>
2024-10-15 14:47:50 -0400

    add step functions trace injection

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/StepFunctions/AwsStepFunctionsCommon.cs
@@ -0,0 +1,72 @@
+// <copyright file="AwsStepFunctionsCommon.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Logging;
+using Datadog.Trace.Tagging;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.StepFunctions
+{
+    internal static class AwsStepFunctionsCommon
+    {
+        internal const string IntegrationName = nameof(Configuration.IntegrationId.AwsStepFunctions);
+        internal const IntegrationId IntegrationId = Configuration.IntegrationId.AwsStepFunctions;
+
+        private const string DatadogAwsStepFunctionsServiceName = "aws-stepfunctions";
+        private const string StepFunctionsServiceName = "StepFunctions";
+        private const string StepFunctionsOperationName = "aws.stepfunctions";
+        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(AwsStepFunctionsCommon));
+
+        public static Scope? CreateScope(Tracer tracer, string operation, string spanKind, out AwsSdkTags? tags, ISpanContext? parentContext = null)
+        {
+            tags = null;
+            if (!tracer.Settings.IsIntegrationEnabled(IntegrationId) || !tracer.Settings.IsIntegrationEnabled(AwsConstants.IntegrationId))
+            {
+                // integration disabled, don't create a scope, skip this trace
+                return null;
+            }
+
+            Scope? scope = null;
+
+            try
+            {
+                var serviceName = tracer.CurrentTraceSettings.GetServiceName(tracer, DatadogAwsStepFunctionsServiceName);
+                var operationName = GetOperationName(tracer, spanKind);
+                scope = tracer.StartActiveInternal(operationName, parent: parentContext, tags: tags, serviceName: serviceName);
+                var span = scope.Span;
+                tags = span.Tags as AwsSdkTags;
+
+                span.Type = SpanTypes.Http;
+                span.ResourceName = $"{StepFunctionsServiceName}.{operation}";
+
+                if (tags != null)
+                {
+                    Console.WriteLine("============ Setting tags for StepFunctions");
+                    tags.Service = StepFunctionsServiceName;
+                    tags.Operation = operation;
+                    tags.SetAnalyticsSampleRate(IntegrationId, tracer.Settings, enabledWithGlobalSetting: false);
+                }
+
+                tracer.TracerManager.Telemetry.IntegrationGeneratedSpan(IntegrationId);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error creating or populating scope.");
+            }
+
+            // always returns the scope, even if it's null because we couldn't create it,
+            // or we couldn't populate it completely (some tags is better than no tags)
+            return scope;
+        }
+
+        internal static string GetOperationName(Tracer tracer, string spanKind)
+        {
+            return StepFunctionsOperationName;
+        }
+    }
+}
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/StepFunctions/StartSyncExecutionAsyncIntegration.cs
@@ -0,0 +1,66 @@
+// <copyright file="StartSyncExecutionAsyncIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using System.Threading;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.StepFunctions
+{
+    /// <summary>
+    /// AWSSDK.StepFunctions StartSyncExecutionAsync CallTarget instrumentation
+    /// </summary>
+    [InstrumentMethod(
+        AssemblyName = "AWSSDK.StepFunctions",
+        TypeName = "Amazon.StepFunctions.AmazonStepFunctionsClient",
+        MethodName = "StartSyncExecutionAsync",
+        ReturnTypeName = "Amazon.StepFunctions.Model.StartSyncExecutionResponse",
+        ParameterTypeNames = new[] { "Amazon.StepFunctions.Model.StartSyncExecutionRequest", ClrNames.CancellationToken },
+        MinimumVersion = "3.0.0",
+        MaximumVersion = "3.*.*",
+        IntegrationName = AwsStepFunctionsCommon.IntegrationName)]
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class StartSyncExecutionAsyncIntegration
+    {
+        private const string Operation = "StartSyncExecutionAsync";
+
+        /// <summary>
+        /// OnMethodBegin callback
+        /// </summary>
+        /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TStartSyncExecutionRequest">Type of the request object</typeparam>
+        /// <param name="instance">Instance value, aka `this` of the instrumented method</param>
+        /// <param name="request">The request for the Step Functions operation</param>
+        /// <param name="cancellationToken">CancellationToken value</param>
+        /// <returns>CallTarget state value</returns>
+        internal static CallTargetState OnMethodBegin<TTarget, TStartSyncExecutionRequest>(TTarget instance, TStartSyncExecutionRequest request, CancellationToken cancellationToken)
+            where TStartSyncExecutionRequest : IStartSyncExecutionRequest, IDuckType
+        {
+            if (request.Instance is null)
+            {
+                return CallTargetState.GetDefault();
+            }
+
+            var scope = AwsStepFunctionsCommon.CreateScope(Tracer.Instance, Operation, SpanKinds.Producer, out var tags);
+            if (request.Input is not null && scope?.Span.Context is { } context)
+            {
+                ContextPropagation.InjectContextIntoInput<TTarget, TStartSyncExecutionRequest>(request, context);
+            }
+
+            return new CallTargetState(scope, state: request);
+        }
+
+        internal static TResponse OnAsyncMethodEnd<TTarget, TResponse>(TTarget instance, TResponse response, Exception exception, in CallTargetState state)
+        {
+            state.Scope.DisposeWithException(exception);
+            return response;
+        }
+    }
+}
