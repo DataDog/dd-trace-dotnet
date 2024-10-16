d096da06f53d8fd429cc924786863dc3f7c1e3c8
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-10-16 18:32:44 -0400

    add PropagatorType property to IContextInjector and IContextExtractor

--- tracer/src/Datadog.Trace/Propagators/DistributedContextExtractor.cs
+++ tracer/src/Datadog.Trace/Propagators/DistributedContextExtractor.cs
@@ -19,6 +19,8 @@ namespace Datadog.Trace.Propagators
         {
         }
 
+        public PropagatorType PropagatorType => PropagatorType.TraceContext;
+
         public bool TryExtract<TCarrier, TCarrierGetter>(TCarrier carrier, TCarrierGetter carrierGetter, out SpanContext? spanContext)
             where TCarrierGetter : struct, ICarrierGetter<TCarrier>
         {
