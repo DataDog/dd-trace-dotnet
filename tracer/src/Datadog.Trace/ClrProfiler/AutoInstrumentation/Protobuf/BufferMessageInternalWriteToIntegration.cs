1dd221603ef0099623f03a8d74012cb2162ca508
Raphaël Vandon <raphael.vandon@datadoghq.com>
2024-10-17 12:25:15 +0200

    add instrumentation for BufferMessage

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Protobuf/BufferMessageInternalWriteToIntegration.cs
@@ -0,0 +1,35 @@
+// <copyright file="BufferMessageInternalWriteToIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Protobuf;
+
+/// <summary>
+/// System.Void Google.Protobuf.IBufferMessage::InternalWriteTo(Google.Protobuf.WriteContext&) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Google.Protobuf",
+    TypeName = "Google.Protobuf.IBufferMessage",
+    MethodName = "InternalWriteTo",
+    ReturnTypeName = ClrNames.Void,
+    ParameterTypeNames = ["Google.Protobuf.WriteContext&"],
+    MinimumVersion = "3.0.0",
+    MaximumVersion = "3.*.*",
+    IntegrationName = "IBufferMessage")]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class BufferMessageInternalWriteToIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget, TCtx>(TTarget instance, ref TCtx ctx)
+        where TTarget : IMessageProxy
+    {
+        SchemaExtractor.EnrichActiveSpanWith(instance.Descriptor, "deserialization");
+        return CallTargetState.GetDefault();
+    }
+}
