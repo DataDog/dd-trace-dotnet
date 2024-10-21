85bb8c0a753f389df46ce7373635f0e22b3bdcc9
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2024-10-21 15:17:49 -0400

    Fix .NET Framework sample code

--- tracer/test/test-applications/integrations/Samples.AWS.StepFunctions/SyncHelpers.cs
+++ tracer/test/test-applications/integrations/Samples.AWS.StepFunctions/SyncHelpers.cs
@@ -13,49 +13,48 @@ namespace Samples.AWS.StepFunctions
         private const string StepFunctionName = "StepFunction";
         private const string Input = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
 
-
-        public static async Task StartStepFunctionsTasks(AmazonStepFunctionsClient stepFunctionsClient)
+        public static void StartStepFunctionsTasks(AmazonStepFunctionsClient stepFunctionsClient)
         {
             Console.WriteLine("Beginning Sync methods");
             using (var scope = SampleHelpers.CreateScope("sync-methods"))
             {
-                var stepFunctionArn = await CreateStateMachineSync(stepFunctionsClient, StepFunctionName);
+                var stepFunctionArn = CreateStateMachineSync(stepFunctionsClient, StepFunctionName);
 
                 // Needed in order to allow resource to be in
                 // Ready status.
                 Thread.Sleep(1000);
-                
-                var executionRequest = new StartSyncExecutionRequest
+
+                var executionRequest = new StartExecutionRequest
                 {
                     Input = Input,
                     StateMachineArn = stepFunctionArn
                 };
 
-                var response = await stepFunctionsClient.StartSyncExecution(executionRequest);
+                var response = stepFunctionsClient.StartExecution(executionRequest);
                 Console.WriteLine($"StartSyncExecution(StartSyncExecution) HTTP status code: {response.HttpStatusCode}");
 
-                await DeleteStateMachineSync(stepFunctionsClient, stepFunctionArn);
+                DeleteStateMachineSync(stepFunctionsClient, stepFunctionArn);
 
                 // Needed in order to allow resource to be deleted
                 Thread.Sleep(1000);
             }
         }
 
-        private static async Task DeleteStateMachineSync(AmazonStepFunctionsClient stepFunctionsClient, string stepFunctionArn)
+        private static void DeleteStateMachineSync(AmazonStepFunctionsClient stepFunctionsClient, string stepFunctionArn)
         {
             var deleteStateMachineRequest = new DeleteStateMachineRequest { StateMachineArn = stepFunctionArn };
 
-            var response = await stepFunctionsClient.DeleteStateMachine(deleteStateMachineRequest);
+            var response = stepFunctionsClient.DeleteStateMachine(deleteStateMachineRequest);
 
             Console.WriteLine($"DeleteStateMachine(DeleteStateMachineRequest) HTTP status code: {response.HttpStatusCode}");
         }
 
-        private static async Task<string> CreateStateMachineSync(AmazonStepFunctionsClient stepFunctionsClient, string stepFunctionName)
+        private static string CreateStateMachineSync(AmazonStepFunctionsClient stepFunctionsClient, string stepFunctionName)
         {
             var def = "{\"Comment\":\"Hello.\",\"StartAt\":\"FirstState\",\"States\":{\"FirstState\":{\"Type\":\"Task\",\"Resource\":\"arn:aws:lambda:us-east-1:123456789012:function:FUNCTION_NAME\",\"Next\":\"ChoiceState\"},\"ChoiceState\":{\"Type\":\"Choice\",\"Choices\":[{\"Variable\":\"$.foo\",\"NumericEquals\":1,\"Next\":\"FirstMatchState\"},{\"Variable\":\"$.foo\",\"NumericEquals\":2,\"Next\":\"SecondMatchState\"}],\"Default\":\"DefaultState\"},\"FirstMatchState\":{\"Type\":\"Task\",\"Resource\":\"arn:aws:lambda:us-east-1:123456789012:function:OnFirstMatch\",\"Next\":\"NextState\"},\"SecondMatchState\":{\"Type\":\"Task\",\"Resource\":\"arn:aws:lambda:us-east-1:123456789012:function:OnSecondMatch\",\"Next\":\"NextState\"},\"DefaultState\":{\"Type\":\"Fail\",\"Error\":\"DefaultStateError\",\"Cause\":\"No Matches!\"},\"NextState\":{\"Type\":\"Task\",\"Resource\":\"arn:aws:lambda:us-east-1:123456789012:function:FUNCTION_NAME\",\"End\":true}}}";
             var createStateMachineRequest = new CreateStateMachineRequest { Name = stepFunctionName, Definition = def };
 
-            var response = await stepFunctionsClient.CreateStateMachine(createStateMachineRequest);
+            var response = stepFunctionsClient.CreateStateMachine(createStateMachineRequest);
 
             Console.WriteLine($"CreateStateMachine(CreateStateMachineRequest) HTTP status code: {response.HttpStatusCode}");
 
