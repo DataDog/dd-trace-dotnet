91e20527ad99c0979fc1a31274fa8734e3c984ae
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-10-29 18:05:07 -0400

    add get/set integrations for Baggage.Current

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Baggage/GetCurrentIntegration.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="GetCurrentIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Baggage;
+
+/// <summary>
+/// System.Collections.Generic.IDictionary`2[System.String,System.String] Datadog.Trace.Baggage::get_Current() calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Baggage",
+    MethodName = "get_Current",
+    ReturnTypeName = "System.Collections.Generic.IDictionary`2[System.String,System.String]",
+    ParameterTypeNames = [],
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class GetCurrentIntegration
+{
+    internal static CallTargetReturn<IDictionary<string, string>?> OnMethodEnd<TTarget>(
+        IDictionary<string, string>? returnValue,
+        Exception? exception,
+        in CallTargetState state)
+    {
+        return new CallTargetReturn<IDictionary<string, string>?>(Trace.Baggage.Current);
+    }
+}
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/ManualInstrumentation/Baggage/SetCurrentIntegration.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="SetCurrentIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Configuration;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.ManualInstrumentation.Baggage;
+
+/// <summary>
+/// System.Void Datadog.Trace.Baggage::set_Current(System.Collections.Generic.IDictionary`2[System.String,System.String]) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Datadog.Trace.Manual",
+    TypeName = "Datadog.Trace.Baggage",
+    MethodName = "set_Current",
+    ReturnTypeName = ClrNames.Void,
+    ParameterTypeNames = ["System.Collections.Generic.IDictionary`2[System.String,System.String]"],
+    MinimumVersion = ManualInstrumentationConstants.MinVersion,
+    MaximumVersion = ManualInstrumentationConstants.MaxVersion,
+    IntegrationName = ManualInstrumentationConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class SetCurrentIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget>(ref IDictionary<string, string>? value)
+    {
+        Trace.Baggage.Current = new Trace.Baggage(value);
+        return CallTargetState.GetDefault();
+    }
+
+    internal static CallTargetReturn OnMethodEnd<TTarget>(Exception? exception, in CallTargetState state)
+    {
+        return CallTargetReturn.GetDefault();
+    }
+}
