8142a8d2685fb0ed64349daef10317e851fd15de
Raphaël Vandon <raphael.vandon@datadoghq.com>
2024-11-07 14:36:42 +0100

    add integration test for SNS DSM

---
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AWS/DataStreamsMonitoringAwsSnsTests.cs
@@ -0,0 +1,74 @@
+// <copyright file="DataStreamsMonitoringAwsSnsTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.TestHelpers;
+using Datadog.Trace.TestHelpers.DataStreamsMonitoring;
+using FluentAssertions;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datadog.Trace.ClrProfiler.IntegrationTests.AWS;
+
+[Trait("RequiresDockerDependency", "true")]
+[UsesVerify]
+public class DataStreamsMonitoringAwsSnsTests : TestHelper
+{
+    public DataStreamsMonitoringAwsSnsTests(ITestOutputHelper output)
+        : base("AWS.SimpleNotificationService", output)
+    {
+    }
+
+    public static IEnumerable<object[]> GetEnabledConfig()
+    {
+        return from packageVersionArray in PackageVersions.AwsSns
+               select new[] { packageVersionArray[0] };
+    }
+
+    [SkippableTheory]
+    [MemberData(nameof(GetEnabledConfig))]
+    [Trait("Category", "EndToEnd")]
+    public async Task SubmitsDsmMetrics(string packageVersion)
+    {
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
+
+        using var telemetry = this.ConfigureTelemetry();
+        using var agent = EnvironmentHelper.GetMockAgent();
+        using (await RunSampleAndWaitForExit(agent, packageVersion: packageVersion))
+        {
+#if NETFRAMEWORK
+            // there is no snapshot for NetFramework so this test would fail if run
+            // but it is compiled, so it still needs to look legit for the CI
+            var expectedCount = 0;
+#else
+            var expectedCount = 5;
+#endif
+            var spans = agent.WaitForSpans(expectedCount);
+            var sqsSpans = spans.Where(
+                span => span.Tags.TryGetValue("component", out var component) && component == "aws-sdk");
+
+            sqsSpans.Should().NotBeEmpty();
+
+            var taggedSpans = spans.Where(s => s.Tags.ContainsKey("pathway.hash"));
+            taggedSpans.Should().HaveCount(expected: 2);
+
+            var dsPoints = agent.WaitForDataStreamsPoints(statsCount: 2);
+
+            var settings = VerifyHelper.GetSpanVerifierSettings();
+            settings.UseParameters(packageVersion);
+            settings.AddDataStreamsScrubber();
+            var fileName = $"{nameof(DataStreamsMonitoringAwsSnsTests)}.{nameof(SubmitsDsmMetrics)}";
+            await Verifier.Verify(MockDataStreamsPayload.Normalize(dsPoints), settings)
+                          .UseFileName(fileName)
+                          .DisableRequireUniquePrefix();
+
+            telemetry.AssertIntegrationEnabled(IntegrationId.AwsSns);
+        }
+    }
+}
