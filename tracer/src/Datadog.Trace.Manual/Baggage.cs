a71867ff7fc52ce33c537d0632d99595d792b6ac
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-11-08 13:02:31 -0500

    clean up manual Baggage

--- tracer/src/Datadog.Trace.Manual/Baggage.cs
+++ tracer/src/Datadog.Trace.Manual/Baggage.cs
@@ -5,7 +5,6 @@
 
 using System.Collections;
 using Datadog.Trace.SourceGenerators;
-using Datadog.Trace.Util;
 
 namespace Datadog.Trace;
 
@@ -20,63 +19,70 @@ public static class Baggage
     /// Gets or sets the baggage collection for the current execution context.
     /// </summary>
     [Instrumented]
-    public static IDictionary<string, string> Current
+    public static IDictionary<string, string?> Current
     {
-        get => NoopDictionaryInstance;
-        set => _ = value; // discard
+        get
+        {
+            // auto-instrumentation will return Trace.Baggage.Current instead
+            return NoopDictionaryInstance;
+        }
+
+        set
+        {
+            // auto-instrumentation will set Trace.Baggage.Current instead
+            _ = value;
+        }
     }
 
-    private sealed class NoopDictionary : IDictionary<string, string>
+    private sealed class NoopDictionary : IDictionary<string, string?>
     {
-        private static readonly KeyValuePair<string, string>[] Empty = [];
+        int ICollection<KeyValuePair<string, string?>>.Count => 0;
 
-        int ICollection<KeyValuePair<string, string>>.Count => 0;
+        bool ICollection<KeyValuePair<string, string?>>.IsReadOnly => true;
 
-        bool ICollection<KeyValuePair<string, string>>.IsReadOnly => true;
+        ICollection<string> IDictionary<string, string?>.Keys { get; } = [];
 
-        ICollection<string> IDictionary<string, string>.Keys => [];
+        ICollection<string?> IDictionary<string, string?>.Values { get; } = [];
 
-        ICollection<string> IDictionary<string, string>.Values => [];
-
-        string IDictionary<string, string>.this[string key]
+        string? IDictionary<string, string?>.this[string key]
         {
             get => null;
             set { }
         }
 
-        IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator()
-            => ((IEnumerable<KeyValuePair<string, string>>)Empty).GetEnumerator();
+        IEnumerator<KeyValuePair<string, string?>> IEnumerable<KeyValuePair<string, string?>>.GetEnumerator()
+            => Enumerable.Empty<KeyValuePair<string, string?>>().GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator()
-            => Empty.GetEnumerator();
+            => Enumerable.Empty<KeyValuePair<string, string?>>().GetEnumerator();
 
-        void ICollection<KeyValuePair<string, string>>.Add(KeyValuePair<string, string> item)
+        void ICollection<KeyValuePair<string, string?>>.Add(KeyValuePair<string, string?> item)
         {
         }
 
-        void ICollection<KeyValuePair<string, string>>.Clear()
+        void ICollection<KeyValuePair<string, string?>>.Clear()
         {
         }
 
-        bool ICollection<KeyValuePair<string, string>>.Contains(KeyValuePair<string, string> item) => false;
+        bool ICollection<KeyValuePair<string, string?>>.Contains(KeyValuePair<string, string?> item) => false;
 
-        void ICollection<KeyValuePair<string, string>>.CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        void ICollection<KeyValuePair<string, string?>>.CopyTo(KeyValuePair<string, string?>[] array, int arrayIndex)
         {
         }
 
-        bool ICollection<KeyValuePair<string, string>>.Remove(KeyValuePair<string, string> item) => false;
+        bool ICollection<KeyValuePair<string, string?>>.Remove(KeyValuePair<string, string?> item) => false;
 
-        bool IDictionary<string, string>.ContainsKey(string key) => false;
+        bool IDictionary<string, string?>.ContainsKey(string key) => false;
 
-        void IDictionary<string, string>.Add(string key, string value)
+        void IDictionary<string, string?>.Add(string key, string? value)
         {
         }
 
-        bool IDictionary<string, string>.Remove(string key) => false;
+        bool IDictionary<string, string?>.Remove(string key) => false;
 
-        bool IDictionary<string, string>.TryGetValue(string key, out string value)
+        bool IDictionary<string, string?>.TryGetValue(string key, out string? value)
         {
-            value = null!;
+            value = null;
             return false;
         }
     }
