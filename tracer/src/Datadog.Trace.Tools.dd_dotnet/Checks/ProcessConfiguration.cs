42fde7ba422f5d7779336665caa16d57e6b5ec15
Mohammad Islam <mohammad.islam@datadoghq.com>
2024-11-12 16:37:14 -0500

    dynamic info log based on if json exists

--- tracer/src/Datadog.Trace.Tools.dd_dotnet/Checks/ProcessConfiguration.cs
+++ tracer/src/Datadog.Trace.Tools.dd_dotnet/Checks/ProcessConfiguration.cs
@@ -44,10 +44,14 @@ internal static class ProcessConfiguration
         }
 
         var jsonConfigurationSource = JsonConfigurationSource.TryLoadJsonConfigurationFile(configurationSource, baseDirectory, out string jsonDirectory);
-        Utils.WriteInfo("Trying to load JSON configuration file from: " + jsonDirectory);
         if (jsonConfigurationSource != null)
         {
+            Utils.WriteInfo("Loaded JSON configuration file from: " + jsonDirectory);
             configurationSource.Add(jsonConfigurationSource);
+        }
+        else
+        {
+                Utils.WriteInfo("Did not find JSON configuration file to load from: " + jsonDirectory);
         }
 
         return configurationSource;
