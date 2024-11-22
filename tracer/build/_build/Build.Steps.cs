f6a9db8b010cac83fc36339fc7330c17acdfacd1
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2024-11-22 19:46:06 +0100

    Debug crash in libdatadog

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -1986,6 +1986,7 @@ partial class Build
                        .EnableNoBuild()
                        .SetIsDebugRun(isDebugRun)
                        .SetProcessEnvironmentVariable("MonitoringHomeDirectory", MonitoringHomeDirectory)
+                       .SetProcessEnvironmentVariable("RUST_BACKTRACE", "1")
                        .SetLogsDirectory(TestLogsDirectory)
                        .EnableTrxLogOutput(GetResultsDirectory(project))
                        .WithDatadogLogger());
