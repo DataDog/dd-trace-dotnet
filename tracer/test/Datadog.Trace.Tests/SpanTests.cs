57db1435e9be61370b3c460838e2c8a4282981c6
Zach Montoya <zach.montoya@datadoghq.com>
2024-12-03 13:55:48 -0800

    Re-organize unit tests somewhat

--- tracer/test/Datadog.Trace.Tests/SpanTests.cs
+++ tracer/test/Datadog.Trace.Tests/SpanTests.cs
@@ -70,6 +70,38 @@ namespace Datadog.Trace.Tests
         }
 
         [Fact]
+        public void AddLink_BeforeSpanFinished_IsSuccessful()
+        {
+            var parentScope = (Scope)_tracer.StartActive("Parent");
+            var childScope = (Scope)_tracer.StartActive("Child");
+
+            var parentSpan = parentScope.Span;
+            var childSpan = childScope.Span;
+            var spanLink = new SpanLink(parentSpan.Context);
+
+            childSpan.AddLink(spanLink);
+            childSpan.Finish();
+
+            childSpan.SpanLinks.Should().ContainSingle();
+        }
+
+        [Fact]
+        public void AddLink_AfterSpanFinished_IsNoOp()
+        {
+            var parentScope = (Scope)_tracer.StartActive("Parent");
+            var childScope = (Scope)_tracer.StartActive("Child");
+
+            var parentSpan = parentScope.Span;
+            var childSpan = childScope.Span;
+            var spanLink = new SpanLink(parentSpan.Context);
+
+            childSpan.Finish();
+            childSpan.AddLink(spanLink);
+
+            childSpan.SpanLinks.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
         public void Finish_StartTimeInThePastWithNoEndTime_DurationProperlyComputed()
         {
             // The 100 additional milliseconds account for the clock precision
