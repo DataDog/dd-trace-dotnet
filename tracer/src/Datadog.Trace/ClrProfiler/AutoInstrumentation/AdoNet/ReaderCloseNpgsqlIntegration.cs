a8d80d198a15bbea0538b9f9d0f43e8f4d84073b
Robert Pickering <robert.pickering@datadoghq.com>
2024-12-03 14:44:23 +0100

    fix flake in npgsql tests

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AdoNet/ReaderCloseNpgsqlIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AdoNet/ReaderCloseNpgsqlIntegration.cs
@@ -1,4 +1,4 @@
-// <copyright file="ReaderCloseIntegration.cs" company="Datadog">
+// <copyright file="ReaderCloseNpgsqlIntegration.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -18,9 +18,19 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet
     /// CallTarget instrumentation for:
     /// [*]DataReader [Command].ExecuteReader()
     /// </summary>
+    // special case for npgsql which expose a close with three params
+    [InstrumentMethod(
+        AssemblyName = "Npgsql",
+        TypeName = "Npgsql.NpgsqlDataReader",
+        MethodName = "Close",
+        ReturnTypeName = ClrNames.Task,
+        ParameterTypeNames = new[] { ClrNames.Bool, ClrNames.Bool, ClrNames.Bool, },
+        MinimumVersion = "4.0.0",
+        MaximumVersion = "8.*.*",
+        IntegrationName = nameof(IntegrationId.Npgsql))]
     [Browsable(false)]
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public class ReaderCloseIntegration
+    public class ReaderCloseNpgsqlIntegration
     {
         private static bool errorLogged = false;
 
@@ -28,11 +38,14 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet
         /// OnMethodEnd callback
         /// </summary>
         /// <typeparam name="TTarget">Type of the target</typeparam>
+        /// <typeparam name="TReturn">Type of the return</typeparam>
         /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
+        /// <param name="returnValue">Instance of the return value</param>
         /// <param name="exception">Exception instance in case the original code threw an exception.</param>
         /// <param name="state">Calltarget state value</param>
         /// <returns>A response value, in an async scenario will be T of Task of T</returns>
-        internal static CallTargetReturn OnMethodEnd<TTarget>(TTarget instance, Exception? exception, in CallTargetState state)
+        // internal static CallTargetReturn OnMethodEnd<TTarget>(TTarget instance, Exception? exception, in CallTargetState state)
+        internal static TReturn OnAsyncMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception exception, in CallTargetState state)
         {
             try
             {
@@ -50,7 +63,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet
                 }
             }
 
-            return CallTargetReturn.GetDefault();
+            return returnValue;
         }
     }
 }
