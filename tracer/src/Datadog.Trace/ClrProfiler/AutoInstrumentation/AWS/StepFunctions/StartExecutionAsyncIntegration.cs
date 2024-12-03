3ab02adb30eea04cc1c25779b7178ec26c7362a7
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2024-12-03 13:27:17 -0500

    Use file from autoinstrumentation generator

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/StepFunctions/StartExecutionAsyncIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/StepFunctions/StartExecutionAsyncIntegration.cs
@@ -2,72 +2,41 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
-
 #nullable enable
 
 using System;
 using System.ComponentModel;
 using System.Threading;
 using Datadog.Trace.ClrProfiler.CallTarget;
-using Datadog.Trace.DuckTyping;
-using Datadog.Trace.Propagators;
+using Datadog.Trace.Configuration;
 
-namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.StepFunctions
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.StepFunctions;
+
+/// <summary>
+/// AWSSDK.StepFunctions StartExecutionAsync CallTarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "AWSSDK.StepFunctions",
+    TypeName = "Amazon.StepFunctions.AmazonStepFunctionsClient",
+    MethodName = "StartExecutionAsync",
+    ReturnTypeName = "System.Threading.Tasks.Task`1[Amazon.StepFunctions.Model.StartExecutionResponse]",
+    ParameterTypeNames = ["Amazon.StepFunctions.Model.StartExecutionRequest", ClrNames.CancellationToken],
+    MinimumVersion = "3.3.0",
+    MaximumVersion = "3.*.*",
+    IntegrationName = nameof(IntegrationId.AwsStepFunctions))]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class StartExecutionAsyncIntegration
 {
-    /// <summary>
-    /// AWSSDK.StepFunctions StartExecutionAsync CallTarget instrumentation
-    /// </summary>
-    [InstrumentMethod(
-        AssemblyName = "AWSSDK.StepFunctions",
-        TypeName = "Amazon.StepFunctions.AmazonStepFunctionsClient",
-        MethodName = "StartExecutionAsync",
-        ReturnTypeName = "Amazon.StepFunctions.Model.StartExecutionResponse",
-        ParameterTypeNames = new[] { "Amazon.StepFunctions.Model.StartExecutionRequest", ClrNames.CancellationToken },
-        MinimumVersion = "3.0.0",
-        MaximumVersion = "3.*.*",
-        IntegrationName = AwsStepFunctionsCommon.IntegrationName)]
-    [Browsable(false)]
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public class StartExecutionAsyncIntegration
+    internal static CallTargetState OnMethodBegin<TTarget, TRequest>(TTarget instance, ref TRequest? request, ref CancellationToken cancellationToken)
     {
-        private const string Operation = "StartExecutionAsync";
+        Console.WriteLine("[TEST] StartExecutionAsyncIntegration.OnMethodBegin");
+        return CallTargetState.GetDefault();
+    }
 
-        /// <summary>
-        /// OnMethodBegin callback
-        /// </summary>
-        /// <typeparam name="TTarget">Type of the target</typeparam>
-        /// <typeparam name="TStartExecutionRequest">Type of the request object</typeparam>
-        /// <param name="instance">Instance value, aka `this` of the instrumented method</param>
-        /// <param name="request">The request for the Step Functions operation</param>
-        /// <param name="cancellationToken">CancellationToken value</param>
-        /// <returns>CallTarget state value</returns>
-        internal static CallTargetState OnMethodBegin<TTarget, TStartExecutionRequest>(TTarget instance, TStartExecutionRequest request, CancellationToken cancellationToken)
-            where TStartExecutionRequest : IStartExecutionRequest, IDuckType
-        {
-            if (request.Instance is null)
-            {
-                return CallTargetState.GetDefault();
-            }
-
-            var scope = AwsStepFunctionsCommon.CreateScope(Tracer.Instance, Operation, SpanKinds.Producer, out var tags);
-            if (tags is not null && request.StateMachineArn is not null)
-            {
-                tags.StateMachineName = AwsStepFunctionsCommon.GetStateMachineName(request.StateMachineArn);
-            }
-
-            if (request.Input is not null && scope?.Span.Context is { } spanContext)
-            {
-                var context = new PropagationContext(spanContext, Baggage.Current);
-                ContextPropagation.InjectContextIntoInput<TTarget, TStartExecutionRequest>(request, context);
-            }
-
-            return new CallTargetState(scope, state: request);
-        }
-
-        internal static TResponse OnAsyncMethodEnd<TTarget, TResponse>(TTarget instance, TResponse response, Exception exception, in CallTargetState state)
-        {
-            state.Scope.DisposeWithException(exception);
-            return response;
-        }
+    internal static TReturn? OnAsyncMethodEnd<TTarget, TReturn>(TTarget instance, TReturn? returnValue, Exception exception, in CallTargetState state)
+    {
+        Console.WriteLine("[TEST] StartExecutionAsyncIntegration.OnAsyncMethodEnd");
+        return returnValue;
     }
 }
