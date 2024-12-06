c1670653bdf61d2bb77d6e82613c49d8e6678b8c
Robert Pickering <robert.pickering@datadoghq.com>
2024-12-06 09:01:44 +0100

    do connection string properlly

--- tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/IastController.cs
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/IastController.cs
@@ -31,11 +31,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Primitives;
 using MongoDB.Bson;
 using MongoDB.Driver;
-using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Npgsql;
-using Oracle.ManagedDataAccess.Client;
 using Samples.Security.Data;
 
 #pragma warning disable ASP0019 // warning ASP0019: Use IHeaderDictionary.Append or the indexer to append or set headers. IDictionary.Add will throw an ArgumentException when attempting to add a duplicate key
@@ -78,9 +75,6 @@ namespace Samples.Security.AspNetCore5.Controllers
         private static SQLiteConnection _dbConnectionSystemData = null;
         private static SqliteConnection _dbConnectionSystemDataMicrosoftData = null;
         private static SqlConnection _dbConnectionSystemDataSqlClient = null;
-        private static NpgsqlConnection _dbConnectionNpgsql = null;
-        private static MySqlConnection _dbConnectionMySql = null;
-        private static OracleConnection _dbConnectionOracle = null;
         private static IMongoDatabase _mongoDb = null;
 
         public IActionResult Index()
@@ -96,26 +90,6 @@ namespace Samples.Security.AspNetCore5.Controllers
         private static SqliteConnection DbConnectionSystemDataMicrosoftData
         {
             get { return _dbConnectionSystemDataMicrosoftData ??= IastControllerHelper.CreateMicrosoftDataDatabase(); }
-        }
-
-        private static NpgsqlConnection DbConnectionNpgsql
-        {
-            get { return _dbConnectionNpgsql ??= IastControllerHelper.CreatePostgresDatabase(); }
-        }
-
-        private static SqlConnection DbConnectionSystemDataSqlClient
-        {
-            get { return _dbConnectionSystemDataSqlClient ??= IastControllerHelper.CreateSqlServerDatabase(); }
-        }
-
-        private static MySqlConnection DbConnectionMySql
-        {
-            get { return _dbConnectionMySql ??= IastControllerHelper.CreateMySqlDatabase(); }
-        }
-
-        private static OracleConnection DbConnectionOracle
-        {
-            get { return _dbConnectionOracle ??= IastControllerHelper.CreateOracleDatabase(); }
         }
 
         [HttpGet("HardcodedSecrets")]
@@ -1022,100 +996,6 @@ namespace Samples.Security.AspNetCore5.Controllers
             return View("Xss");
         }
 
-        [HttpGet("StoredXss")]
-        [Route("StoredXss")]
-        public IActionResult StoredXss(string database = null)
-        {
-            var db = GetDbConnectionFromName(database);
-
-            var param = GetDbValue(db);
-            ViewData["XSS"] = param + "<b>More Text</b>";
-            return View("Xss");
-        }
-
-        [HttpGet("StoredXssEscaped")]
-        [Route("StoredXssEscaped")]
-        public IActionResult StoredXssEscaped(string database = null)
-        {
-            var db = GetDbConnectionFromName(database);
-
-            var param = GetDbValue(db);
-            var escapedText = System.Net.WebUtility.HtmlEncode($"System.Net.WebUtility.HtmlEncode({param})") + Environment.NewLine
-                            + System.Web.HttpUtility.HtmlEncode($"System.Web.HttpUtility.HtmlEncode({param})") + Environment.NewLine;
-            ViewData["XSS"] = escapedText;
-            return View("Xss");
-        }
-
-
-        [HttpGet("StoredSqli")]
-        [Route("StoredSqli")]
-        public IActionResult StoredSqli(string database = null)
-        {
-            try
-            {
-
-                var db = GetDbConnectionFromName(database);
-
-                var details = GetDbValue(db, "Michael");
-                var taintedQuery = "SELECT name from Persons where Details = '" + details + "'";
-
-                var name = db switch
-                {
-                    SQLiteConnection connection => new SQLiteCommand(taintedQuery, connection).ExecuteScalar(),
-                    SqliteConnection sqliteConnection => new SqliteCommand(taintedQuery, sqliteConnection).ExecuteScalar(),
-                    SqlConnection connection => new SqlCommand(taintedQuery, connection).ExecuteScalar(),
-                    NpgsqlConnection connection => new NpgsqlCommand(taintedQuery, connection).ExecuteScalar(),
-                    MySqlConnection connection => new MySqlCommand(taintedQuery, connection).ExecuteScalar(),
-                    OracleConnection connection => new OracleCommand(taintedQuery, connection).ExecuteScalar(),
-                    _ => null
-                };
-
-                return Content($"Result: " + name);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return StatusCode(500);
-            }
-        }
-
-        private static IDbConnection GetDbConnectionFromName(string database)
-        {
-            IDbConnection db =
-                database switch
-                {
-                    "System.Data.SQLite" => DbConnectionSystemDataMicrosoftData,
-                    "System.Data.SqlClient" => DbConnectionSystemDataSqlClient,
-                    "Microsoft.Data.Sqlite" => DbConnectionSystemData,
-                    "Npgsql" => DbConnectionNpgsql,
-                    "MySql.Data" => DbConnectionMySql,
-                    "Oracle" => DbConnectionOracle,
-                    null => DbConnectionSystemData,
-                    _ => throw new Exception($"unknown db type: {database}")
-                };
-            return db;
-        }
-
-        private static string GetDbValue(IDbConnection db, string name = "Name1")
-        {
-            var taintedQuery = $"SELECT Details from Persons where name = '{name}'";
-
-            using IDataReader reader = db switch
-            {
-                SQLiteConnection connection => new SQLiteCommand(taintedQuery, connection).ExecuteReader(),
-                SqliteConnection connection => new SqliteCommand(taintedQuery, connection).ExecuteReader(),
-                SqlConnection connection => new SqlCommand(taintedQuery, connection).ExecuteReader(),
-                NpgsqlConnection connection => new NpgsqlCommand(taintedQuery, connection).ExecuteReader(),
-                MySqlConnection connection => new MySqlCommand(taintedQuery, connection).ExecuteReader(),
-                OracleConnection connection => new OracleCommand(taintedQuery, connection).ExecuteReader(),
-                _ => throw new ArgumentException("Invalid db connection")
-            };
-
-            reader.Read();
-            var res = reader.GetString(0);
-            return res;
-        }
-        
         #if NET6_0_OR_GREATER
         [HttpGet("InterpolatedSqlString")]
         [Route("InterpolatedSqlString")]
@@ -1281,6 +1161,23 @@ namespace Samples.Security.AspNetCore5.Controllers
 
             return Content(result, "text/html");
         }
+
+        private static string GetDbValue(IDbConnection db, string name = "Name1")
+        {
+            var taintedQuery = $"SELECT Details from Persons where name = '{name}'";
+
+            using IDataReader reader = db switch
+            {
+                SQLiteConnection connection => new SQLiteCommand(taintedQuery, connection).ExecuteReader(),
+                SqliteConnection connection => new SqliteCommand(taintedQuery, connection).ExecuteReader(),
+                _ => throw new ArgumentException("Invalid db connection")
+            };
+
+            reader.Read();
+            var res = reader.GetString(0);
+            return res;
+        }
+
 
         [HttpGet("Print")]
         public ActionResult PrintReport(
--- tracer/test/test-applications/security/Samples.Security.AspNetCore5/Data/IastControllerHelper.cs
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5/Data/IastControllerHelper.cs
@@ -13,32 +13,6 @@ namespace Samples.Security.Data
 {
     public static class IastControllerHelper
     {
-        private static readonly string[] SqlServerTables =
-            [
-                "CREATE TABLE Persons (Id uniqueidentifier NOT NULL,Name varchar(50) NOT NULL,Surname varchar(50) NULL,Gender char(10) NULL,Email varchar(50) NULL,FlattedAddress varchar(500) NULL,Mobile varchar(50) NULL,NIF varchar(50) NULL,Phone varchar(50) NULL,PostalCode varchar(50) NULL,Details varchar(5000) NULL,CityIniqueId varchar(50) NULL,CountryUniqueId varchar(50) NULL,ImagePath varchar(500) NULL)",
-                @"CREATE TABLE Books(Id uniqueidentifier NOT NULL,Title nvarchar(500) NOT NULL,Price numeric(18, 2) NULL,ISBN nvarchar(50) NULL,	Pages int NOT NULL,    Author nvarchar(500) NOT NULL,    Editorial nvarchar(50) NULL,	Prologue nvarchar(4000) NULL,	Format int NULL)",
-            ];
-
-        private static readonly string[] PostgresTables =
-            [
-                "DROP TABLE IF EXISTS Persons, Books;",
-                "CREATE TABLE Persons (Id uuid NOT NULL,Name varchar(50) NOT NULL,Surname varchar(50) NULL,Gender char(10) NULL,Email varchar(50) NULL,FlattedAddress varchar(500) NULL,Mobile varchar(50) NULL,NIF varchar(50) NULL,Phone varchar(50) NULL,PostalCode varchar(50) NULL,Details varchar(5000) NULL,CityIniqueId varchar(50) NULL,CountryUniqueId varchar(50) NULL,ImagePath varchar(500) NULL)",
-                @"CREATE TABLE Books(Id uuid NOT NULL,Title varchar(500) NOT NULL,Price numeric(18, 2) NULL,ISBN varchar(50) NULL,	Pages int NOT NULL,    Author varchar(500) NOT NULL,    Editorial varchar(50) NULL,	Prologue varchar(4000) NULL,	Format int NULL)",
-            ];
-
-        private static readonly string[] MySqlTables =
-        [
-            "DROP TABLE IF EXISTS Persons, Books;",
-            "CREATE TABLE Persons (Id char(36) NOT NULL,Name varchar(50) NOT NULL,Surname varchar(50) NULL,Gender char(10) NULL,Email varchar(50) NULL,FlattedAddress varchar(500) NULL,Mobile varchar(50) NULL,NIF varchar(50) NULL,Phone varchar(50) NULL,PostalCode varchar(50) NULL,Details varchar(5000) NULL,CityIniqueId varchar(50) NULL,CountryUniqueId varchar(50) NULL,ImagePath varchar(500) NULL)",
-            @"CREATE TABLE Books(Id char(36) NOT NULL,Title varchar(500) NOT NULL,Price numeric(18, 2) NULL,ISBN varchar(50) NULL,	Pages int NOT NULL,    Author varchar(500) NOT NULL,    Editorial varchar(50) NULL,	Prologue varchar(4000) NULL,	Format int NULL)",
-        ];
-
-        private static readonly string[] OracleTables =
-        [
-            "DROP TABLE IF EXISTS Persons",
-            "CREATE TABLE Persons (Id char(40) NOT NULL,Name varchar(50) NOT NULL,Surname varchar(50) NULL,Gender char(10) NULL,Email varchar(50) NULL,FlattedAddress varchar(500) NULL,Mobile varchar(50) NULL,NIF varchar(50) NULL,Phone varchar(50) NULL,PostalCode varchar(50) NULL,Details varchar(2000) NULL,CityIniqueId varchar(50) NULL,CountryUniqueId varchar(50) NULL,ImagePath varchar(500) NULL)",
-        ];
-
         private static readonly string[] SqliteTables =
         [
             "CREATE TABLE Persons (Id NOT NULL,Name varchar(50) NOT NULL,Surname varchar(50) NULL,Gender char(10) NULL,Email varchar(50) NULL,FlattedAddress varchar(500) NULL,Mobile varchar(50) NULL,NIF varchar(50) NULL,Phone varchar(50) NULL,PostalCode varchar(50) NULL,Details varchar(5000) NULL,CityIniqueId varchar(50) NULL,CountryUniqueId varchar(50) NULL,ImagePath varchar(500) NULL)",
@@ -54,98 +28,6 @@ namespace Samples.Security.Data
             "INSERT INTO Persons(Id, Name, Surname, Gender, Email, FlattedAddress, Mobile, NIF, Phone, PostalCode, Details, CityIniqueId, CountryUniqueId, ImagePath) VALUES('D305C1EB-B72E-4340-B5BA-A19D0105A6C1', 'Name1', 'Surname1',   'Female    ', 'name1.surname1@gmail.com', 'Address1', 'Mob1', 'N1', 'Phone1', 'Zip1', '<script language=''javascript'' type=''text/javascript''>alert(''Stored XSS attack'');</script>', NULL, NULL, NULL)",
             "INSERT INTO Books (Id, Title, Price, ISBN, Pages, Author, Editorial, Prologue, Format) VALUES ('026decfd-bba3-4aa8-85d4-1c71ffcfe8e9', 'CLR via C#', CAST(50.00 AS Decimal(18, 2)), '0735669954', 894, ' Jeffrey Richter', 'Microsoft Press', 'bla bla bla', 0)"
         ];
-
-        public static OracleConnection CreateOracleDatabase()
-        {
-            var host = "localhost";
-            var cstringBuilder = new OracleConnectionStringBuilder();
-            cstringBuilder.DataSource = $"{host}:1521/FREE";
-            cstringBuilder.UserID = "system";
-            cstringBuilder.Password = "testpassword";
-
-            var connection = new OracleConnection(cstringBuilder.ConnectionString);
-            connection.Open();
-
-            foreach (var command in OracleTables)
-            {
-                new OracleCommand(command, connection).ExecuteNonQuery();
-            }
-            foreach (var command in PopulateCommands.Take(5))
-            {
-                new OracleCommand(command, connection).ExecuteNonQuery();
-            }
-
-            return connection;
-        }
-
-        public static MySqlConnection CreateMySqlDatabase()
-        {
-            var connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
-
-            if (connectionString == null)
-            {
-                var host = Environment.GetEnvironmentVariable("MYSQL_HOST") ?? "localhost";
-                var port = Environment.GetEnvironmentVariable("MYSQL_PORT") ?? "3307";
-                connectionString = $"server={host};user=mysqldb;password=mysqldb;port={port};database=world";
-            }
-
-            var connection = new MySqlConnection(connectionString);
-            connection.Open();
-
-            foreach (var command in MySqlTables)
-            {
-                new MySqlCommand(command, connection).ExecuteNonQuery();
-            }
-            foreach (var command in PopulateCommands)
-            {
-                new MySqlCommand(command, connection).ExecuteNonQuery();
-            }
-
-            return connection;
-        }
-
-        public static NpgsqlConnection CreatePostgresDatabase()
-        {
-            // assumes the postgres docker image is running...
-            var connection = new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=postgres;");
-            connection.Open();
-
-            foreach (var command in PostgresTables)
-            {
-                new NpgsqlCommand(command, connection).ExecuteNonQuery();
-            }
-            foreach (var command in PopulateCommands)
-            {
-                new NpgsqlCommand(command, connection).ExecuteNonQuery();
-            }
-
-            return connection;
-        }
-
-        public static SqlConnection CreateSqlServerDatabase()
-        {
-            // assumes the sqlserver docker image is running...
-            using (var masterConnection = new SqlConnection("Server=localhost;Database=master;User Id=sa;Password=Strong!Passw0rd;"))
-            {
-                masterConnection.Open();
-                new SqlCommand("IF DB_ID (N'books') IS NOT NULL\nDROP DATABASE books;", masterConnection).ExecuteNonQuery();
-                new SqlCommand("CREATE DATABASE books;", masterConnection).ExecuteNonQuery();
-            }
-
-            var connection = new SqlConnection("Server=localhost;Database=books;User Id=sa;Password=Strong!Passw0rd;");
-            connection.Open();
-
-            foreach (var command in SqlServerTables)
-            {
-                new SqlCommand(command, connection).ExecuteNonQuery();
-            }
-            foreach (var command in PopulateCommands)
-            {
-                new SqlCommand(command, connection).ExecuteNonQuery();
-            }
-
-            return connection;
-        }
 
         public static SQLiteConnection CreateSystemDataDatabase()
         {
