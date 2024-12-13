0e5062291213266810ad7e659277e8dc9096a2c7
Robert Pickering <robert.pickering@datadoghq.com>
2024-12-13 15:52:07 +0100

    [IAST] fix tainting values stored in the db (#6389)
    ## Summary of changes
    
    Add tests and corrections to support for various database platforms.
    
    ## Reason for change
    
    Some bugs were reported relating to database tainting not working
    correctly for some dbs.
    
    ## Test coverage
    
    Adds new integration tests dependent on docker db instances.

--- tracer/test/test-applications/security/Samples.Security.AspNetCore5.DatabaseIntegration/Controllers/HomeController.cs
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5.DatabaseIntegration/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Samples.Security.AspNetCore5.Models;
 using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Mime;
 
 #pragma warning disable ASP0019 // warning ASP0019: Use IHeaderDictionary.Append or the indexer to append or set headers. IDictionary.Add will throw an ArgumentException when attempting to add a duplicate key
-namespace Samples.Security.AspNetCore5.Controllers
+namespace Samples.Security.AspNetCore5.Controllers.DatabaseIntegration
 {
     public class HomeController : Controller
     {
@@ -30,24 +29,5 @@ namespace Samples.Security.AspNetCore5.Controllers
         }
 
         public IActionResult Privacy() => View();
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult LangHeader()
-        {
-            Response.Headers.Add("content-language", "krypton");
-            return Content("Setting content-language");
-        }
-        
-        [AcceptVerbs("GET")]
-        [Route("/null-action/{pathparam}/{pathparam2}")]
-        public object NullAction(string pathparam, string pathparam2)
-        {
-            return null;
-        }
     }
 }
