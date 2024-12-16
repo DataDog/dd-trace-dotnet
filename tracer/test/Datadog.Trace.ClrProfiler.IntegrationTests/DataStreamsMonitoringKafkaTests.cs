1aaa9254be130abfdf0f571ad2693f4d6f29011c
veer <veer.bhatia@datadoghq.com>
2024-12-16 16:17:46 -0500

    revert kafka tests

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DataStreamsMonitoringKafkaTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DataStreamsMonitoringKafkaTests.cs
@@ -33,21 +33,39 @@ public class DataStreamsMonitoringKafkaTests : TestHelper
     ///  - service -> topic 1 -> Consumer 1 -> topic 2 -> Consumer 2 -> topic 3 -> consumer 3
     ///  - service -> topic 2 -> Consumer 2 -> topic 3 -> consumer 3
     /// Each node (apart from 'service') in the pipelines above have a unique hash
+    ///
+    /// In mermaid (view at https://mermaid.live/), this looks like:
+    /// sequenceDiagram
+    ///    participant A as Root Service (12926600137239154356)
+    ///    participant T1 as Topic 1 (2704081292861755358)
+    ///    participant C1 as Consumer 1 (5289074475783863123)
+    ///    participant T2a as Topic 2 (2821413369272395429)
+    ///    participant C2a as Consumer 2 (9753735904472423641)
+    ///    participant T3a as Topic 3 (5363062531028060751)
+    ///    participant T2 as Topic 2 (246622801349204431)
+    ///    participant C2 as Consumer 2 (3398817358352474903)
+    ///    participant T3 as Topic 3 (16689539899325095461 )
+    ///
+    ///    A->>+T1: Produce
+    ///    T1-->>-C1: Consume
+    ///    C1->>+T2a: Produce
+    ///    T2a-->>-C2a: Consume
+    ///    C2a->>+T3a: Produce
+    ///
+    ///    A->>+T2: Produce
+    ///    T2-->>-C2: Consume
+    ///    C2->>+T3: Produce
     /// </summary>
     /// <param name="enableConsumerScopeCreation">Is the scope created manually or using built-in support</param>
-    /// <param name="enableLegacyHeaders">Should legacy headers be enabled or not</param>
     [SkippableTheory]
-    [InlineData(true, true)]
-    [InlineData(true, false)]
-    [InlineData(false, true)]
-    [InlineData(false, false)]
+    [InlineData(true)]
+    [InlineData(false)]
     [Trait("Category", "EndToEnd")]
     [Trait("Category", "ArmUnsupported")]
-    public async Task SubmitsDataStreams(bool enableConsumerScopeCreation, bool enableLegacyHeaders)
+    public async Task SubmitsDataStreams(bool enableConsumerScopeCreation)
     {
         SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
         SetEnvironmentVariable(ConfigurationKeys.KafkaCreateConsumerScopeEnabled, enableConsumerScopeCreation ? "1" : "0");
-        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.LegacyHeadersEnabled, enableLegacyHeaders ? "1" : "0");
 
         using var agent = EnvironmentHelper.GetMockAgent(useTelemetry: true);
 
@@ -62,5 +80,73 @@ public class DataStreamsMonitoringKafkaTests : TestHelper
         await Verifier.Verify(payload, settings)
                       .UseFileName($"{nameof(DataStreamsMonitoringKafkaTests)}.{nameof(SubmitsDataStreams)}")
                       .DisableRequireUniquePrefix();
+    }
+
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    /// <summary>
+    /// This sample tests a fan in + out scenario:
+    ///  - service -> topic 1 -|                  | -> topic 2 -> Consumer 2
+    ///  - service -> topic 1 -|---> Consumer 1  -| -> topic 2 -> Consumer 2
+    ///  - service -> topic 1 -|                  | -> topic 2 -> Consumer 2
+    ///
+    /// It aims to replicate what happens when a bunch of messages are read at once,
+    /// then processed in a loop before new outputs are produced.
+    /// </summary>
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    [Trait("Category", "ArmUnsupported")]
+    public async Task HandlesBatchProcessing()
+    {
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
+        // set variable to create short spans on receive instead of spans that last until the next consume
+        SetEnvironmentVariable(ConfigurationKeys.KafkaCreateConsumerScopeEnabled, "0");
+
+        using var agent = EnvironmentHelper.GetMockAgent(useTelemetry: true);
+
+        using var processResult = await RunSampleAndWaitForExit(agent, "--batch-processing");
+
+        using var assertionScope = new AssertionScope();
+
+        var payload = MockDataStreamsPayload.Normalize(agent.DataStreams);
+
+        // using span verifier to add all the default scrubbers
+        var settings = VerifyHelper.GetSpanVerifierSettings();
+        settings.AddSimpleScrubber(TracerConstants.AssemblyVersion, "2.x.x.x");
+        settings.AddDataStreamsScrubber();
+        await Verifier.Verify(payload, settings)
+                      .UseFileName($"{nameof(DataStreamsMonitoringKafkaTests)}.{nameof(HandlesBatchProcessing)}")
+                      .DisableRequireUniquePrefix();
+    }
+
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    [Trait("Category", "ArmUnsupported")]
+    public async Task WhenDisabled_DoesNotSubmitDataStreams()
+    {
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "0");
+
+        using var agent = EnvironmentHelper.GetMockAgent();
+        using var processResult = await RunSampleAndWaitForExit(agent);
+
+        using var assertionScope = new AssertionScope();
+        // We don't expect any streams here, so no point waiting for ages
+        var dataStreams = agent.WaitForDataStreams(2, timeoutInMilliseconds: 2_000);
+        dataStreams.Should().BeEmpty();
+    }
+
+    [SkippableFact]
+    [Trait("Category", "EndToEnd")]
+    [Trait("Category", "ArmUnsupported")]
+    public async Task WhenNotSupported_DoesNotSubmitDataStreams()
+    {
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
+
+        using var agent = EnvironmentHelper.GetMockAgent();
+        agent.Configuration = new MockTracerAgent.AgentConfiguration { Endpoints = Array.Empty<string>() };
+        using var processResult = await RunSampleAndWaitForExit(agent);
+
+        using var assertionScope = new AssertionScope();
+        var dataStreams = agent.DataStreams;
+        dataStreams.Should().BeEmpty();
     }
 }
