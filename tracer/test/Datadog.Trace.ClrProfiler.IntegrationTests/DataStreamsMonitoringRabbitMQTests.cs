95a8651d7e16720efcf56089203310748ae2299e
veer <veer.bhatia@datadoghq.com>
2024-12-16 16:11:19 -0500

    fix rabbitmq tests

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DataStreamsMonitoringRabbitMQTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/DataStreamsMonitoringRabbitMQTests.cs
@@ -12,89 +12,80 @@ using Datadog.Trace.TestHelpers;
 using Datadog.Trace.TestHelpers.DataStreamsMonitoring;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using ICSharpCode.Decompiler.Util;
 using VerifyXunit;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Datadog.Trace.ClrProfiler.IntegrationTests
+namespace Datadog.Trace.ClrProfiler.IntegrationTests;
+
+[UsesVerify]
+[Trait("RequiresDockerDependency", "true")]
+public class DataStreamsMonitoringRabbitMQTests : TestHelper
 {
-    [UsesVerify]
-    [Trait("RequiresDockerDependency", "true")]
-    public class DataStreamsMonitoringRabbitMQTests : TestHelper
+    public DataStreamsMonitoringRabbitMQTests(ITestOutputHelper output)
+        : base("DataStreams.RabbitMQ", output)
     {
-        public DataStreamsMonitoringRabbitMQTests(ITestOutputHelper output)
-            : base("DataStreams.RabbitMQ", output)
+        SetServiceVersion("1.0.0");
+    }
+
+    [SkippableTheory]
+    [MemberData(nameof(PackageVersions.RabbitMQ), MemberType = typeof(PackageVersions))]
+    [Trait("Category", "EndToEnd")]
+    public async Task HandleProduceAndConsume(string packageVersion)
+    {
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.LegacyHeadersEnabled, "1");
+
+        using var assertionScope = new AssertionScope();
+        using var agent = EnvironmentHelper.GetMockAgent();
+        using (await RunSampleAndWaitForExit(agent, arguments: $"{TestPrefix}", packageVersion: packageVersion))
         {
-            SetServiceVersion("1.0.0");
+            var spans = agent.WaitForSpans(31);
+            spans.Should().HaveCount(31);
+
+            var settings = VerifyHelper.GetSpanVerifierSettings();
+            settings.UseParameters(packageVersion);
+            settings.AddDataStreamsScrubber();
+            await Verifier.Verify(PayloadsToPoints(agent.DataStreams), settings)
+                          .UseFileName($"{nameof(DataStreamsMonitoringRabbitMQTests)}.{nameof(HandleProduceAndConsume)}")
+                          .DisableRequireUniquePrefix();
         }
+    }
 
-        public static IEnumerable<object[]> GetRabbitMQTestData()
+    [SkippableTheory]
+    [MemberData(nameof(PackageVersions.RabbitMQ), MemberType = typeof(PackageVersions))]
+    [Trait("Category", "EndToEnd")]
+    public async Task ValidateSpanTags(string packageVersion)
+    {
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
+        SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.LegacyHeadersEnabled, "1");
+
+        using var assertionScope = new AssertionScope();
+        using var agent = EnvironmentHelper.GetMockAgent();
+        using (await RunSampleAndWaitForExit(agent, arguments: $"{TestPrefix}", packageVersion: packageVersion))
         {
-            foreach (var version in PackageVersions.RabbitMQ)
-            {
-                yield return new object[] { version[0], true };
-                yield return new object[] { version[0], false };
-            }
+            var spans = agent.WaitForSpans(31);
+            spans.Should().HaveCount(31);
+            var taggedSpans = spans.Where(s => s.Tags.ContainsKey("pathway.hash"));
+            taggedSpans.Should().HaveCount(13);
         }
+    }
 
-        [SkippableTheory]
-        [MemberData(nameof(GetRabbitMQTestData))]
-        [Trait("Category", "EndToEnd")]
-        public async Task HandleProduceAndConsume(string packageVersion, bool enableLegacyHeaders)
+    private static IList<MockDataStreamsStatsPoint> PayloadsToPoints(IImmutableList<MockDataStreamsPayload> payloads)
+    {
+        var points = new List<MockDataStreamsStatsPoint>();
+        foreach (var payload in payloads)
         {
-            SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
-            SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.LegacyHeadersEnabled, enableLegacyHeaders ? "1" : "0");
-
-            using var assertionScope = new AssertionScope();
-            using var agent = EnvironmentHelper.GetMockAgent();
-            using (await RunSampleAndWaitForExit(agent, arguments: $"{TestPrefix}", packageVersion: packageVersion))
+            foreach (var bucket in payload.Stats)
             {
-                var spans = agent.WaitForSpans(31);
-                spans.Should().HaveCount(31);
-
-                var settings = VerifyHelper.GetSpanVerifierSettings();
-                settings.UseParameters(packageVersion, enableLegacyHeaders);
-                settings.AddDataStreamsScrubber();
-                await Verifier.Verify(PayloadsToPoints(agent.DataStreams), settings)
-                              .UseFileName($"{nameof(DataStreamsMonitoringRabbitMQTests)}.{nameof(HandleProduceAndConsume)}")
-                              .DisableRequireUniquePrefix();
-            }
-        }
-
-        [SkippableTheory]
-        [MemberData(nameof(GetRabbitMQTestData))]
-        [Trait("Category", "EndToEnd")]
-        public async Task ValidateSpanTags(string packageVersion, bool enableLegacyHeaders)
-        {
-            SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.Enabled, "1");
-            SetEnvironmentVariable(ConfigurationKeys.DataStreamsMonitoring.LegacyHeadersEnabled, enableLegacyHeaders ? "1" : "0");
-
-            using var assertionScope = new AssertionScope();
-            using var agent = EnvironmentHelper.GetMockAgent();
-            using (await RunSampleAndWaitForExit(agent, arguments: $"{TestPrefix}", packageVersion: packageVersion))
-            {
-                var spans = agent.WaitForSpans(31);
-                spans.Should().HaveCount(31);
-                var taggedSpans = spans.Where(s => s.Tags.ContainsKey("pathway.hash"));
-                taggedSpans.Should().HaveCount(13);
-            }
-        }
-
-        private static IList<MockDataStreamsStatsPoint> PayloadsToPoints(IImmutableList<MockDataStreamsPayload> payloads)
-        {
-            var points = new List<MockDataStreamsStatsPoint>();
-            foreach (var payload in payloads)
-            {
-                foreach (var bucket in payload.Stats)
+                if (bucket.Stats != null)
                 {
-                    if (bucket.Stats != null)
-                    {
-                        points.AddRange(bucket.Stats);
-                    }
+                    points.AddRange(bucket.Stats);
                 }
             }
-
-            return points.OrderBy(s => s.Hash).ThenBy(s => s.TimestampType).ToList();
         }
+
+        return points.OrderBy(s => s.Hash).ThenBy(s => s.TimestampType).ToList();
     }
 }
