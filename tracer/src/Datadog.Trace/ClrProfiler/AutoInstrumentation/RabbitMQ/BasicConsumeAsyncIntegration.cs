c3d4e5be06b0bd10382b0ca27d0dc21119b35e0d
Andrew Lock <andrew.lock@datadoghq.com>
2024-12-18 15:42:48 +0000

    Add support for RabbitMQ 7.x

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/BasicConsumeAsyncIntegration.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="BasicConsumeAsyncIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using Datadog.Trace.ClrProfiler.CallTarget;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ;
+
+/// <summary>
+/// RabbitMQ.Client BasicConsumeAsync calltarget instrumentation for v7+
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "RabbitMQ.Client",
+    TypeName = "RabbitMQ.Client.Impl.Channel",
+    MethodName = "BasicConsumeAsync",
+    ReturnTypeName = ClrNames.StringTask,
+    ParameterTypeNames = [ClrNames.String, ClrNames.Bool, ClrNames.String, ClrNames.Bool, ClrNames.Bool, RabbitMQConstants.IDictionaryArgumentsTypeName, "RabbitMQ.Client.IAsyncBasicConsumer", ClrNames.CancellationToken],
+    MinimumVersion = "7.0.0",
+    MaximumVersion = "7.*.*",
+    IntegrationName = RabbitMQConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class BasicConsumeAsyncIntegration
+{
+    private const string Command = "basic.consume";
+
+    internal static CallTargetState OnMethodBegin<TTarget, TConsumer>(TTarget instance, string? queue, bool autoAck, string? consumerTag, bool noLocal, bool exclusive, IDictionary<string, object>? arguments, TConsumer consumer, in CancellationToken cancellationToken)
+    {
+        return BasicConsumeIntegration.OnMethodBegin(instance, queue, autoAck, consumerTag, noLocal, exclusive, arguments, consumer);
+    }
+
+    internal static string? OnAsyncMethodEnd<TTarget>(TTarget instance, string? returnValue, Exception? exception, in CallTargetState state)
+    {
+        state.Scope.DisposeWithException(exception);
+        return returnValue;
+    }
+}
---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/BasicPublishAsyncCachedStringsIntegration.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="BasicPublishAsyncCachedStringsIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using System.Threading;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.DuckTyping;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ;
+
+/// <summary>
+/// System.Threading.Tasks.ValueTask RabbitMQ.Client.Impl.Channel::BasicPublishAsync[TProperties](System.String,System.String,System.Boolean,TProperties,System.ReadOnlyMemory`1[System.Byte],System.Threading.CancellationToken) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "RabbitMQ.Client",
+    TypeName = "RabbitMQ.Client.Impl.Channel",
+    MethodName = "BasicPublishAsync",
+    ReturnTypeName = "System.Threading.Tasks.ValueTask",
+    ParameterTypeNames = ["RabbitMQ.Client.CachedString", "RabbitMQ.Client.CachedString", ClrNames.Bool, "!!0", "System.ReadOnlyMemory`1[System.Byte]", ClrNames.CancellationToken],
+    MinimumVersion = "7.0.0",
+    MaximumVersion = "7.*.*",
+    IntegrationName = RabbitMQConstants.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class BasicPublishAsyncCachedStringsIntegration
+{
+    internal static CallTargetState OnMethodBegin<TTarget, TExchange, TRoutingKey, TBasicProperties, TBody>(TTarget instance, TExchange exchange, TRoutingKey routingKey, bool mandatory, TBasicProperties basicProperties, TBody body, in CancellationToken cancellationToken)
+        where TBasicProperties : IReadOnlyBasicProperties, IDuckType
+        where TBody : IBody, IDuckType
+        where TExchange : ICachedStringProxy
+        where TRoutingKey : ICachedStringProxy
+        where TTarget : IModelBase
+    {
+        var exchangeString = exchange.Instance is not null ? exchange.Value : null;
+        var routingKeyString = routingKey.Instance is not null ? routingKey.Value : null;
+        return BasicPublishAsyncIntegration.OnMethodBegin(instance, exchangeString, routingKeyString, mandatory, basicProperties, body, default);
+    }
+
+    internal static TReturn? OnAsyncMethodEnd<TTarget, TReturn>(TTarget instance, TReturn? returnValue, Exception? exception, in CallTargetState state)
+    {
+        state.Scope.DisposeWithException(exception);
+        return returnValue;
+    }
+}
+
+/// <summary>
+/// DuckTyping interface for RabbitMQ.Client.CachedString
+/// </summary>
+#pragma warning disable SA1201 // An interface should not follow a class
+internal interface ICachedStringProxy : IDuckType
+{
+    /// <summary>
+    /// Gets or sets a value of System.String
+    /// </summary>
+    [DuckField(Name = "Value")]
+    string Value { get; set; }
+}
