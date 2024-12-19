17992fc7620489b855bdfdbf65c6378da0da5c7d
Raphaël Vandon <raphael.vandon@datadoghq.com>
2024-12-19 17:30:57 +0100

    oops

--- tracer/src/Datadog.Trace/Configuration/IntegrationId.cs
+++ tracer/src/Datadog.Trace/Configuration/IntegrationId.cs
@@ -83,7 +83,6 @@ namespace Datadog.Trace.Configuration
         DirectoryListingLeak,
         SessionTimeout,
         DatadogTraceManual,
-        EmailHtmlInjection,
-        Protobuf
+        EmailHtmlInjection
     }
 }
