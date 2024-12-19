9fc1ca12fb8917f6615a0ed3aa51f2cbc5e4f444
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-12-19 13:01:23 +0100

    Add only error count metric when there are not only unknown operator errors.

--- tracer/src/Datadog.Trace/AppSec/Coordinator/SecurityCoordinator.Reporter.cs
+++ tracer/src/Datadog.Trace/AppSec/Coordinator/SecurityCoordinator.Reporter.cs
@@ -92,9 +92,9 @@ internal readonly partial struct SecurityCoordinator
             security.WafInitResult.Reported = true;
             span.Context.TraceContext?.SetSamplingPriority(SamplingPriorityValues.UserKeep, SamplingMechanism.Asm);
             span.SetMetric(Metrics.AppSecWafInitRulesLoaded, security.WafInitResult.LoadedRules);
-            span.SetMetric(Metrics.AppSecWafInitRulesErrorCount, security.WafInitResult.FailedToLoadRules);
             if (security.WafInitResult.HasErrors && !Security.HasOnlyUnknownMatcherErrors(security.WafInitResult.Errors))
             {
+                span.SetMetric(Metrics.AppSecWafInitRulesErrorCount, security.WafInitResult.FailedToLoadRules);
                 span.SetTag(Tags.AppSecWafInitRuleErrors, security.WafInitResult.ErrorMessage);
             }
 
