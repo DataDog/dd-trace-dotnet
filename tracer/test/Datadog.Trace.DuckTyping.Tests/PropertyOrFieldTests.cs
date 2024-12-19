ea2b11a9f3433569e12b971ab14b58aab9b7f8ee
Andrew Lock <andrew.lock@datadoghq.com>
2024-12-19 14:59:23 +0000

    Add support for `[DuckPropertyOrField]` (#6463)
    ## Summary of changes
    
    Allows you to specify that a duck type member can match a property _or_
    a field - whatever's available.
    
    ## Reason for change
    
    In the recent version of RabbitMQ, they changed some properties into
    identically-named fields. Without this change, we'd have to duplicate
    the whole duck chain hierarchy.
    
    ## Implementation details
    
    If the `DuckKind` is `PropertyOrField`, and we fail to find a property,
    then try to look for a matching field instead.
    
    ## Test coverage
    
    Added some simple tests, that it works as expected, but I don't think we
    need to go as far as duplicating the extensive suite we have for
    Property or Field matching.
    
    ## Other details
    
    Required for RabbitMQ v7 support

---
+++ tracer/test/Datadog.Trace.DuckTyping.Tests/PropertyOrFieldTests.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="PropertyOrFieldTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.DuckTyping.Tests;
+
+public class PropertyOrFieldTests
+{
+    [Fact]
+    public void PropertyOrFieldTestUsesPropertyIfAvailable()
+    {
+        var target = new TargetWithProperty();
+        var proxy = target.DuckCast<Proxy>();
+
+        proxy.Value.Should().Be(target.Value);
+    }
+
+    [Fact]
+    public void PropertyOrFieldTestUsesFieldIfAvailable()
+    {
+        var target = new TargetWithField();
+        var proxy = target.DuckCast<Proxy>();
+
+        proxy.Value.Should().Be(target.Value);
+    }
+
+    [Fact]
+    public void PropertyOrFieldTestUsesNameFromDuckAttribute()
+    {
+        var target = new TargetWithFieldAndProperty();
+        var proxy = target.DuckCast<ProxyWithName>();
+
+        proxy.Value.Should().Be(target.GetValue());
+    }
+
+    [DuckCopy]
+    public struct Proxy
+    {
+        [DuckPropertyOrField]
+        public int Value;
+    }
+
+    [DuckCopy]
+    public struct ProxyWithName
+    {
+        [DuckPropertyOrField(Name = "value")]
+        public int Value;
+    }
+
+    public class TargetWithProperty
+    {
+        public int Value { get; } = 1;
+    }
+
+    public class TargetWithField
+    {
+#pragma warning disable SA1401 // should be private
+        public int Value = 1;
+#pragma warning restore SA1401
+    }
+
+    public class TargetWithFieldAndProperty
+    {
+        private int value = 3;
+
+        public int Value { get; } = 4;
+
+        public int GetValue() => value;
+    }
+}
