27b7257d9e581c778b7ebab7134b82844bbb9a92
Mohammad Islam <mohammad.islam@datadoghq.com>
2024-12-20 15:44:50 -0500

    remove white space

--- tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
@@ -584,7 +584,6 @@ namespace Datadog.Trace.Configuration
 
             this.JsonConfigurationSource = source.ToString() ?? "N/A because Tracer Settings";
 
-
             // we "enrich" with these values which aren't _strictly_ configuration, but which we want to track as we tracked them in v1
             telemetry.Record(ConfigTelemetryData.NativeTracerVersion, Instrumentation.GetNativeTracerVersion(), recordValue: true, ConfigurationOrigins.Default);
             telemetry.Record(ConfigTelemetryData.FullTrustAppDomain, value: AppDomain.CurrentDomain.IsFullyTrusted, ConfigurationOrigins.Default);
