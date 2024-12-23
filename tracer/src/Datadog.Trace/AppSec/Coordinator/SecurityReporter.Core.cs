cd029428df6354798a1a4e566c927d5a8469c81b
Anna <anna.yafi@datadoghq.com>
2024-12-23 17:32:25 +0100

    Add security reporter so that sometimes we can instantiate the reporter alone without needing the security object

---
+++ tracer/src/Datadog.Trace/AppSec/Coordinator/SecurityReporter.Core.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="SecurityReporter.Core.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+#if !NETFRAMEWORK
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+using Datadog.Trace.AppSec.AttackerFingerprint;
+using Datadog.Trace.AppSec.Waf;
+using Datadog.Trace.Headers;
+using Datadog.Trace.Propagators;
+using Datadog.Trace.Sampling;
+using Datadog.Trace.Vendors.Newtonsoft.Json;
+using Datadog.Trace.Vendors.Serilog;
+using Datadog.Trace.Vendors.Serilog.Events;
+
+namespace Datadog.Trace.AppSec.Coordinator;
+
+internal partial class SecurityReporter
+{
+    private bool CanAccessHeaders => true;
+
+    /// <summary>
+    /// Outside of a web context this can't work and there are no web assemblies to load so without the no inlining, this would cause a load assembly exception
+    /// </summary>
+    /// <param name="span">the span to report on</param>
+    /// <param name="searchRootSpan">should we fetch the root span for you</param>
+    /// <returns>security reporter</returns>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static SecurityReporter? SafeCollectHeaders(Span span, bool searchRootSpan = true)
+    {
+        var securityReporter = AspNetCoreAvailabilityChecker.IsAspNetCoreAvailable() ? GetSecurityReporter(searchRootSpan) : null;
+        securityReporter?.CollectHeaders();
+        return securityReporter;
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        SecurityReporter? GetSecurityReporter(bool searchRootSpanImpl)
+        {
+            var context = CoreHttpContextStore.Instance.Get();
+            return context != null ? new SecurityReporter(span, new SecurityCoordinator.HttpTransport(context), searchRootSpanImpl) : null;
+        }
+    }
+
+    internal void CollectHeaders()
+    {
+        var headers = new HeadersCollectionAdapter(_httpTransport.Context.Request.Headers);
+        AddRequestHeaders(headers);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal void CollectHeadersSafeOutsideWeb()
+    {
+        if (AspNetCoreAvailabilityChecker.IsAspNetCoreAvailable())
+        {
+            CollectHeadersImpl();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        void CollectHeadersImpl()
+        {
+            var context = CoreHttpContextStore.Instance.Get();
+            if (context is not null)
+            {
+                var headers = new HeadersCollectionAdapter(context.Request.Headers);
+                AddRequestHeaders(headers);
+            }
+        }
+    }
+}
+#endif
---
+++ tracer/src/Datadog.Trace/AppSec/Coordinator/SecurityReporter.Framework.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="SecurityReporter.Framework.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+#if NETFRAMEWORK
+using System;
+using System.Runtime.CompilerServices;
+using System.Web;
+using Datadog.Trace.Headers;
+using Datadog.Trace.Vendors.Serilog;
+
+namespace Datadog.Trace.AppSec.Coordinator;
+
+internal partial class SecurityReporter
+{
+    private static readonly bool? UsingIntegratedPipeline;
+
+    static SecurityReporter()
+    {
+        if (UsingIntegratedPipeline == null)
+        {
+            try
+            {
+                UsingIntegratedPipeline = TryGetUsingIntegratedPipelineBool();
+            }
+            catch (Exception ex)
+            {
+                UsingIntegratedPipeline = false;
+                Log.Error(ex, "Unable to query the IIS pipeline. Request and response information may be limited.");
+            }
+        }
+    }
+
+    internal bool CanAccessHeaders => UsingIntegratedPipeline is true or null;
+
+    /// <summary>
+    /// ! This method should be called from within a try-catch block !
+    /// If the application is running in partial trust, then trying to call this method will result in
+    /// a SecurityException to be thrown at the method CALLSITE, not inside the <c>TryGetUsingIntegratedPipelineBool(..)</c> method itself.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool TryGetUsingIntegratedPipelineBool() => HttpRuntime.UsingIntegratedPipeline;
+
+    internal static void SafeCollectHeaders(Span internalSpan, bool isRoot)
+    {
+        var context = HttpContext.Current;
+        var securityReporter = new SecurityReporter(internalSpan, new SecurityCoordinator.HttpTransport(context), isRoot);
+        if (context != null)
+        {
+            var headers = new NameValueHeadersCollection(context.Request.Headers);
+            securityReporter.AddRequestHeaders(headers);
+        }
+    }
+}
+#endif
