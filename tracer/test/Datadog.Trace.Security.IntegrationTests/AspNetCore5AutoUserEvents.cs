5a3e874ddda7fd8295182cd82f2776f5067c1722
Anna <anna.yafi@datadoghq.com>
2025-01-13 17:35:21 +0100

    Add and adapt existing integration tests

--- tracer/test/Datadog.Trace.Security.IntegrationTests/AspNetCore5AutoUserEvents.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/AspNetCore5AutoUserEvents.cs
@@ -7,9 +7,12 @@
 #pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1649 // File name must match first type name
 
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Datadog.Trace.Configuration;
 using Datadog.Trace.TestHelpers;
+using FluentAssertions;
+using VerifyTests;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -40,11 +43,6 @@ namespace Datadog.Trace.Security.IntegrationTests
             }
         }
 
-        public virtual string GetTestFileName(string testName)
-        {
-            return $"{_testName}-{testName}";
-        }
-
         public override void Dispose()
         {
             base.Dispose();
@@ -53,7 +51,7 @@ namespace Datadog.Trace.Security.IntegrationTests
 
         protected async Task TryStartApp()
         {
-            await _fixture.TryStartApp(this, _enableSecurity);
+            await _fixture.TryStartApp(this, _enableSecurity, externalRulesFile: ("ruleset.blocked.users.json"));
             SetHttpPort(_fixture.HttpPort);
         }
 
@@ -66,54 +64,77 @@ namespace Datadog.Trace.Security.IntegrationTests
         {
             await TryStartApp();
             var agent = _fixture.Agent;
-            var url = "/Account/Index";
             var settings = VerifyHelper.GetSpanVerifierSettings(eventName, bodyString);
-            await TestAppSecRequestWithVerifyAsync(agent, url, bodyString, 1, 1, settings, contentType: "application/x-www-form-urlencoded", methodNameOverride: nameof(TestUserLoginEvent), fileNameOverride: GetTestFileName(eventName));
+            VerifyScrubber.ScrubAuthenticatedTags(settings);
+            await TestAppSecRequestWithVerifyAsync(
+                agent,
+                "/Account/Index",
+                bodyString,
+                1,
+                1,
+                settings,
+                contentType: "application/x-www-form-urlencoded",
+                methodNameOverride: nameof(TestUserLoginEvent),
+                fileNameOverride: GetTestFileName(eventName));
+
             // reset memory database (useless for net7 as it runs with EF7 on app.db
             await SendRequestsAsync(_fixture.Agent, "/account/reset-memory-db");
             await SendRequestsAsync(_fixture.Agent, "/account/logout");
         }
+
+        [SkippableFact]
+        [Trait("RunOnWindows", "True")]
+        protected async Task TestAuthenticatedRequest()
+        {
+            await TryStartApp();
+            var settings = VerifyHelper.GetSpanVerifierSettings();
+            var request = await SubmitRequest("/Account/Index", "Input.UserName=TestUser&Input.Password=test", contentType: "application/x-www-form-urlencoded");
+            request.StatusCode.Should().Be(HttpStatusCode.OK);
+            await TestAppSecRequestWithVerifyAsync(_fixture.Agent, "/Account/SomeAuthenticatedAction", null, 1, 1, settings, fileNameOverride: GetTestFileName(nameof(TestAuthenticatedRequest)));
+            // reset memory database (useless for net7 as it runs with EF7 on app.db
+            await SendRequestsAsync(_fixture.Agent, "/account/reset-memory-db");
+            await SendRequestsAsync(_fixture.Agent, "/account/logout");
+        }
+
+        [SkippableTheory]
+        [Trait("RunOnWindows", "True")]
+        [InlineData("blocked-user")]
+        [InlineData("not-blocked-user")]
+        protected async Task TestLoginWithSdk(string userIdSdk)
+        {
+            await TryStartApp();
+            var agent = _fixture.Agent;
+            var settings = VerifyHelper.GetSpanVerifierSettings(nameof(TestLoginWithSdk), userIdSdk);
+            VerifyScrubber.ScrubAuthenticatedTags(settings);
+            await TestAppSecRequestWithVerifyAsync(
+                agent,
+                $"/Account/Index?userIdSdk={userIdSdk}",
+                "Input.UserName=TestUser&Input.Password=test",
+                1,
+                1,
+                settings,
+                contentType: "application/x-www-form-urlencoded",
+                methodNameOverride: nameof(TestUserLoginEvent),
+                fileNameOverride: GetTestFileName($"{nameof(TestLoginWithSdk)}.{userIdSdk}"));
+            // reset memory database (useless for net7 as it runs with EF7 on app.db
+            await SendRequestsAsync(_fixture.Agent, "/account/reset-memory-db");
+            await SendRequestsAsync(_fixture.Agent, "/account/logout");
+        }
+
+        private string GetTestFileName(string testName) => $"{_testName}-{testName}";
     }
 
-    public class AspNetCore5AutoUserEventsDefaultModeSecurityEnabled : AspNetCore5AutoUserEvents
-    {
-        public AspNetCore5AutoUserEventsDefaultModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
-            : base(fixture, outputHelper, true)
-        {
-        }
-    }
+    public class AspNetCore5AutoUserEventsDefaultModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+        : AspNetCore5AutoUserEvents(fixture, outputHelper, true);
 
-    public class AspNetCore5AutoUserEventsDefaultModeSecurityDisabled : AspNetCore5AutoUserEvents
-    {
-        public AspNetCore5AutoUserEventsDefaultModeSecurityDisabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
-            : base(fixture, outputHelper, false)
-        {
-        }
-    }
+    public class AspNetCore5AutoUserEventsExtendedModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+        : AspNetCore5AutoUserEvents(fixture, outputHelper, true, "extended");
 
-    public class AspNetCore5AutoUserEventsExtendedModeSecurityEnabled : AspNetCore5AutoUserEvents
-    {
-        public AspNetCore5AutoUserEventsExtendedModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
-            : base(fixture, outputHelper, true, "extended")
-        {
-        }
-    }
+    public class AspNetCore5AutoUserEventsIndentModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+        : AspNetCore5AutoUserEvents(fixture, outputHelper, true, "ident");
 
-    public class AspNetCore5AutoUserEventsIndentModeSecurityEnabled : AspNetCore5AutoUserEvents
-    {
-        public AspNetCore5AutoUserEventsIndentModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
-            : base(fixture, outputHelper, true, "ident")
-        {
-        }
-    }
-
-    public class AspNetCore5AutoUserEventsAnonModeSecurityEnabled : AspNetCore5AutoUserEvents
-    {
-        public AspNetCore5AutoUserEventsAnonModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
-            : base(fixture, outputHelper, true, "anon")
-        {
-        }
-    }
+    public class AspNetCore5AutoUserEventsAnonModeSecurityEnabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
+        : AspNetCore5AutoUserEvents(fixture, outputHelper, true, "anon");
 
     public class AspNetCore5AutoUserEventsExtendedModeSecurityDisabled(AspNetCoreTestFixture fixture, ITestOutputHelper outputHelper)
         : AspNetCore5AutoUserEvents(fixture, outputHelper, false, "extended")
---
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/VerifyScrubber.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="VerifyScrubber.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+using System.Text.RegularExpressions;
+using Datadog.Trace.TestHelpers;
+using VerifyTests;
+
+namespace Datadog.Trace.Security.IntegrationTests;
+
+internal class VerifyScrubber
+{
+    private static readonly Regex AppSecFingerPrintSession = new(@"_dd.appsec.fp.session: ssn.[\s\-a-z0-9]*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+    private static readonly Regex AuthenticationCollectionMode = new(@"_dd.appsec.user.collection_mode: .*,", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+    public static void ScrubAuthenticatedTags(VerifySettings settings)
+    {
+        // these tags are added by HttpContext.SetUser. After a login event it's not always called by all framework versions
+        // we dont want to test authenticated tags here anyway, as they're tested by TestAuthenticatedRequest
+        settings.AddRegexScrubber(AuthenticationCollectionMode, string.Empty);
+        settings.AddRegexScrubber(AppSecFingerPrintSession, "_dd.appsec.fp.session: <SessionFp>");
+    }
+}
