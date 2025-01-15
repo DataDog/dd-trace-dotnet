44ab80ffe76de07421d962f6af14601bda11597c
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2025-01-15 10:53:17 -0500

    Add some more configuration options to sample

--- tracer/test/test-applications/azure-functions/Samples.AzureFunctions.V4Isolated.AspNetCore/Program.cs
+++ tracer/test/test-applications/azure-functions/Samples.AzureFunctions.V4Isolated.AspNetCore/Program.cs
@@ -4,10 +4,7 @@ using Microsoft.Extensions.Hosting;
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+builder.UseFunctionExecutionMiddleware();
+builder.UseOutputBindingsMiddleware();
 
 builder.Build().Run();
