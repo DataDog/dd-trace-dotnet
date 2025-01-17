96c8b4d06619d3130a7b25b67b2007049882bca1
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2025-01-17 08:42:19 -0500

    Simplify the Program.cs for v2

--- tracer/test/test-applications/azure-functions/Samples.AzureFunctions.V4Isolated.AspNetCore/Program.cs
+++ tracer/test/test-applications/azure-functions/Samples.AzureFunctions.V4Isolated.AspNetCore/Program.cs
@@ -3,8 +3,11 @@ using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
+// this uses the ASP.NET Core integration
+// this will cause the one function app (func.exe) to proxy
+// the HTTP trigger functions HTTP request to the ASP NET Core app
+// instead of sending it (primarily) as a gRPC message
+// Note: this is similar to the V1 Program.cs, just a different way to configure the builder
 builder.ConfigureFunctionsWebApplication();
-builder.UseFunctionExecutionMiddleware();
-builder.UseOutputBindingsMiddleware();
 
 builder.Build().Run();
