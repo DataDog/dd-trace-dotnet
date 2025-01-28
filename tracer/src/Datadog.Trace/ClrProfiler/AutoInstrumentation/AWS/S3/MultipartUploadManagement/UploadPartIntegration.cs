bf0a5e1ae655ec776f7e084ef4b6ccab98da115b
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-01-28 17:25:10 -0500

    create UploadPart integration

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/S3/MultipartUploadManagement/UploadPartIntegration.cs
@@ -0,0 +1,51 @@
+// <copyright file="UploadPartIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using Datadog.Trace.ClrProfiler.CallTarget;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.S3.MultipartUploadManagement;
+
+/// <summary>
+/// AWSSDK.S3 UploadPart CallTarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "AWSSDK.S3",
+    TypeName = "Amazon.S3.AmazonS3Client",
+    MethodName = "UploadPart",
+    ReturnTypeName = "Amazon.S3.Model.UploadPartResponse",
+    ParameterTypeNames = ["Amazon.S3.Model.UploadPartRequest"],
+    MinimumVersion = "3.3.0",
+    MaximumVersion = "3.*.*",
+    IntegrationName = AwsS3Common.IntegrationName)]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class UploadPartIntegration
+{
+    private const string Operation = "UploadPart";
+    private const string SpanKind = SpanKinds.Producer;
+
+    internal static CallTargetState OnMethodBegin<TTarget, TRequest>(TTarget instance, TRequest request)
+        where TRequest : IUploadPartRequest
+    {
+        if (request.Instance is null)
+        {
+            return CallTargetState.GetDefault();
+        }
+
+        var scope = AwsS3Common.CreateScope(Tracer.Instance, Operation, SpanKind, out var tags);
+        AwsS3Common.SetTags(tags, request.BucketName, request.ObjectKey);
+
+        return new CallTargetState(scope);
+    }
+
+    internal static CallTargetReturn<TReturn?> OnMethodEnd<TTarget, TReturn>(TTarget instance, TReturn? returnValue, Exception? exception, in CallTargetState state)
+    {
+        state.Scope.DisposeWithException(exception);
+        return new CallTargetReturn<TReturn?>(returnValue);
+    }
+}
