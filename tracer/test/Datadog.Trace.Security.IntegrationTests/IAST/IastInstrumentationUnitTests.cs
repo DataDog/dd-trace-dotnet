3b9ca3a040be4f35a1dfdd11377f5f42a4cd83d7
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2025-01-30 09:59:12 +0100

    comment

--- tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/IastInstrumentationUnitTests.cs
@@ -353,7 +353,7 @@ public class IastInstrumentationUnitTests : TestHelper
         using (var agent = EnvironmentHelper.GetMockAgent())
         {
             EnableIast(true);
-            EnableDebugMode(); // debug enablkkfged
+            EnableDebugMode(); // debug enabl7kkfged
             Environment.SetEnvironmentVariable("DD_TRACE_DEBUG", "1");
             var logDirectory = Path.Combine(EnvironmentHelper.LogDirectory, "InstrumentedTests");
             SetDumpInfo(logDirectory);
