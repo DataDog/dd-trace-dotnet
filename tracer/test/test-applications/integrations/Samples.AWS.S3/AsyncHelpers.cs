b2b2f2da794f151aad7ae4c907c8295b56b7efb8
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-01-31 16:42:02 -0500

    add test coverage for every instrumented operation

--- tracer/test/test-applications/integrations/Samples.AWS.S3/AsyncHelpers.cs
+++ tracer/test/test-applications/integrations/Samples.AWS.S3/AsyncHelpers.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.S3;
@@ -11,7 +13,10 @@ namespace Samples.AWS.S3
     {
         private const string BucketName = "my-bucket";
         private const string ObjectKey = "sample.txt";
+        private const string CopiedObjectKey = "copy.txt";
+        private const string MultipartObjectKey = "multipart.txt";
         private const string ObjectContent = "Hello World!";
+        private const int PartSize = 5 * 1024 * 1024; // 5MB for each part
 
         public static async Task StartS3Tasks(AmazonS3Client s3Client)
         {
@@ -23,8 +28,28 @@ namespace Samples.AWS.S3
                 // Allow time for the bucket to be ready
                 Thread.Sleep(1000);
 
+                // Object management
                 await PutObjectAsync(s3Client, BucketName, ObjectKey);
+                await CopyObjectAsync(s3Client, BucketName, ObjectKey, CopiedObjectKey);
+                await ListObjectsAsync(s3Client, BucketName);
+                Thread.Sleep(1000);
+                await DeleteObjectAsync(s3Client, BucketName, CopiedObjectKey);
 
+                // Multipart uploads
+                var uploadId = await InitiateMultipartUpload(s3Client, BucketName, MultipartObjectKey);
+                var part1Content = new string('a', PartSize);
+                var uploadPart1Response = await UploadPart(s3Client, BucketName, MultipartObjectKey, uploadId, 1, part1Content);
+
+                var part2Content = new string('b', PartSize);
+                var uploadPart2Response = await UploadPart(s3Client, BucketName, MultipartObjectKey, uploadId, 2, part2Content);
+                List<PartETag> etagParts = [
+                    new(1, uploadPart1Response.ETag),
+                    new(2, uploadPart2Response.ETag)
+                ];
+                await CompleteMultipartUpload(s3Client, BucketName, MultipartObjectKey, uploadId, etagParts);
+
+                // Bucket management
+                await ListBucketsAsync(s3Client);
                 await DeleteBucketAsync(s3Client, BucketName);
 
                 // Allow time for the bucket to be deleted
@@ -32,6 +57,7 @@ namespace Samples.AWS.S3
             }
         }
 
+        // OBJECT MANAGEMENT
         private static async Task PutObjectAsync(AmazonS3Client s3Client, string bucketName, string objectKey)
         {
             var request = new PutObjectRequest
@@ -44,6 +70,117 @@ namespace Samples.AWS.S3
             var response = await s3Client.PutObjectAsync(request);
 
             Console.WriteLine($"PutObjectAsync(PutObjectRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+        
+        private static async Task CopyObjectAsync(AmazonS3Client s3Client, string bucketName, string sourceKey, string destKey)
+        {
+            var request = new CopyObjectRequest
+            {
+                SourceBucket = bucketName,
+                DestinationBucket = bucketName,
+                SourceKey = sourceKey,
+                DestinationKey = destKey
+            };
+
+            var response = await s3Client.CopyObjectAsync(request);
+            Console.WriteLine($"CopyObjectAsync(CopyObjectRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+        
+        private static async Task ListObjectsAsync(AmazonS3Client s3Client, string bucketName)
+        {
+            var request = new ListObjectsV2Request
+            {
+                BucketName = bucketName,
+            };
+
+            var response = await s3Client.ListObjectsV2Async(request);
+            Console.WriteLine($"ListObjectsV2Async(ListObjectsV2Request) HTTP status code: {response.HttpStatusCode}");
+        }
+        
+        private static async Task DeleteObjectAsync(AmazonS3Client s3Client, string bucketName, string objectKey)
+        {
+            var request = new DeleteObjectRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey
+            };
+
+            var response = await s3Client.DeleteObjectAsync(request);
+            Console.WriteLine($"DeleteObjectAsync(DeleteObjectRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        // MULTIPART UPLOADS
+        private static async Task<string> InitiateMultipartUpload(AmazonS3Client s3Client, string bucketName, string objectKey)
+        {
+            var initRequest = new InitiateMultipartUploadRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey
+            };
+
+            var initResponse = await s3Client.InitiateMultipartUploadAsync(initRequest);
+            Console.WriteLine($"InitiateMultipartUploadAsync(InitiateMultipartUploadRequest) HTTP status code: {initResponse.HttpStatusCode}");
+            return initResponse.UploadId;
+        }
+
+        private static async Task<UploadPartResponse> UploadPart(
+            AmazonS3Client s3Client,
+            string bucketName,
+            string objectKey,
+            string uploadId,
+            int partNumber,
+            string content
+        ) {
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+            var uploadRequest = new UploadPartRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey,
+                UploadId = uploadId,
+                PartNumber = partNumber,
+                InputStream = memoryStream
+            };
+
+            var response = await s3Client.UploadPartAsync(uploadRequest);
+            Console.WriteLine($"UploadPartAsync(UploadPartRequest) HTTP status code: {response.HttpStatusCode}");
+            return response;
+        }
+
+        private static async Task CompleteMultipartUpload(
+            AmazonS3Client s3Client, 
+            string bucketName, 
+            string objectKey, 
+            string uploadId,
+            List<PartETag> partEtags
+        ){
+            var completionRequest = new CompleteMultipartUploadRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey,
+                UploadId = uploadId,
+                PartETags = partEtags
+            };
+            
+            var completeUploadResponse = await s3Client.CompleteMultipartUploadAsync(completionRequest);
+            Console.WriteLine($"CompleteMultipartUploadAsync(CompleteMultipartUploadRequest) HTTP status code: {completeUploadResponse.HttpStatusCode}");
+        }
+
+        // BUCKET MANAGEMENT
+        private static async Task CreateBucketAsync(AmazonS3Client s3Client, string bucketName)
+        {
+            var createBucketRequest = new PutBucketRequest
+            {
+                BucketName = bucketName
+            };
+
+            var response = await s3Client.PutBucketAsync(createBucketRequest);
+            Console.WriteLine($"CreateBucketAsync(PutBucketRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        private static async Task ListBucketsAsync(AmazonS3Client s3Client)
+        {
+            var response = await s3Client.ListBucketsAsync();
+            Console.WriteLine($"ListBucketsAsync(ListBucketsRequest) HTTP status code: {response.HttpStatusCode}");
         }
 
         private static async Task DeleteBucketAsync(AmazonS3Client s3Client, string bucketName)
@@ -77,18 +214,6 @@ namespace Samples.AWS.S3
                 Console.WriteLine($"Error in DeleteBucketAsync: {ex.Message}");
                 throw;
             }
-        }
-
-        private static async Task CreateBucketAsync(AmazonS3Client s3Client, string bucketName)
-        {
-            var createBucketRequest = new PutBucketRequest
-            {
-                BucketName = bucketName
-            };
-
-            var response = await s3Client.PutBucketAsync(createBucketRequest);
-
-            Console.WriteLine($"CreateBucketAsync(PutBucketRequest) HTTP status code: {response.HttpStatusCode}");
         }
     }
 }
--- tracer/test/test-applications/integrations/Samples.AWS.S3/SyncHelpers.cs
+++ tracer/test/test-applications/integrations/Samples.AWS.S3/SyncHelpers.cs
@@ -1,7 +1,9 @@
 #if NETFRAMEWORK
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading;
 using Amazon.S3;
 using Amazon.S3.Model;
@@ -12,7 +14,10 @@ namespace Samples.AWS.S3
     {
         private const string BucketName = "my-bucket";
         private const string ObjectKey = "sample.txt";
+        private const string CopiedObjectKey = "copy.txt";
+        private const string MultipartObjectKey = "multipart.txt";
         private const string ObjectContent = "Hello World!";
+        private const int PartSize = 5 * 1024 * 1024; // 5MB for each part
 
         public static void StartS3Tasks(AmazonS3Client s3Client)
         {
@@ -24,8 +29,31 @@ namespace Samples.AWS.S3
                 // Allow time for the bucket to be ready
                 Thread.Sleep(1000);
 
+                // Object management
                 PutObject(s3Client, BucketName, ObjectKey);
+                CopyObject(s3Client, BucketName, ObjectKey, CopiedObjectKey);
+                ListObjects(s3Client, BucketName);
+                Thread.Sleep(1000);
+                DeleteObject(s3Client, BucketName, CopiedObjectKey);
 
+                // Multipart uploads
+                var uploadId = InitiateMultipartUpload(s3Client, BucketName, MultipartObjectKey);
+                var part1Content = new string('a', PartSize);
+                var uploadPart1Response = UploadPart(s3Client, BucketName, MultipartObjectKey, uploadId, 1, part1Content);
+                
+                var part2Content = new string('b', PartSize);
+                var uploadPart2Response = UploadPart(s3Client, BucketName, MultipartObjectKey, uploadId, 2, part2Content);
+
+                var etagParts = new List<PartETag>
+                {
+                    new PartETag(1, uploadPart1Response.ETag),
+                    new PartETag(2, uploadPart2Response.ETag)
+                };
+
+                CompleteMultipartUpload(s3Client, BucketName, MultipartObjectKey, uploadId, etagParts);
+
+                // Bucket management
+                ListBuckets(s3Client);
                 DeleteBucket(s3Client, BucketName);
 
                 // Allow time for the bucket to be deleted
@@ -33,6 +61,7 @@ namespace Samples.AWS.S3
             }
         }
 
+        // OBJECT MANAGEMENT
         private static void PutObject(AmazonS3Client s3Client, string bucketName, string objectKey)
         {
             var request = new PutObjectRequest
@@ -43,8 +72,118 @@ namespace Samples.AWS.S3
             };
 
             var response = s3Client.PutObject(request);
-
             Console.WriteLine($"PutObject(PutObjectRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        private static void CopyObject(AmazonS3Client s3Client, string bucketName, string sourceKey, string destKey)
+        {
+            var request = new CopyObjectRequest
+            {
+                SourceBucket = bucketName,
+                SourceKey = sourceKey,
+                DestinationBucket = bucketName,
+                DestinationKey = destKey
+            };
+
+            var response = s3Client.CopyObject(request);
+            Console.WriteLine($"CopyObject(CopyObjectRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        private static void ListObjects(AmazonS3Client s3Client, string bucketName)
+        {
+            var request = new ListObjectsV2Request
+            {
+                BucketName = bucketName,
+            };
+
+            var response = s3Client.ListObjectsV2(request);
+            Console.WriteLine($"ListObjectsV2(ListObjectsV2Request) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        private static void DeleteObject(AmazonS3Client s3Client, string bucketName, string objectKey)
+        {
+            var request = new DeleteObjectRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey
+            };
+
+            var response = s3Client.DeleteObject(request);
+            Console.WriteLine($"DeleteObject(DeleteObjectRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        // MULTIPART UPLOADS
+        private static string InitiateMultipartUpload(AmazonS3Client s3Client, string bucketName, string objectKey)
+        {
+            var request = new InitiateMultipartUploadRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey
+            };
+
+            var response = s3Client.InitiateMultipartUpload(request);
+            Console.WriteLine($"InitiateMultipartUpload(InitiateMultipartUploadRequest) HTTP status code: {response.HttpStatusCode}");
+            return response.UploadId;
+        }
+
+        private static UploadPartResponse UploadPart(
+            AmazonS3Client s3Client,
+            string bucketName,
+            string objectKey,
+            string uploadId,
+            int partNumber,
+            string content
+        ) {
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+            var request = new UploadPartRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey,
+                UploadId = uploadId,
+                PartNumber = partNumber,
+                InputStream = memoryStream
+            };
+
+            var response = s3Client.UploadPart(request);
+            Console.WriteLine($"UploadPart(UploadPartRequest) HTTP status code: {response.HttpStatusCode}");
+            return response;
+        }
+
+        private static void CompleteMultipartUpload(
+            AmazonS3Client s3Client, 
+            string bucketName, 
+            string objectKey, 
+            string uploadId,
+            List<PartETag> partEtags
+        ){
+            var request = new CompleteMultipartUploadRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey,
+                UploadId = uploadId,
+                PartETags = partEtags
+            };
+
+            var response = s3Client.CompleteMultipartUpload(request);
+            Console.WriteLine($"CompleteMultipartUpload(CompleteMultipartUploadRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        // BUCKET MANAGEMENT
+        private static void CreateBucket(AmazonS3Client s3Client, string bucketName)
+        {
+            var createBucketRequest = new PutBucketRequest
+            {
+                BucketName = bucketName
+            };
+
+            var response = s3Client.PutBucket(createBucketRequest);
+            Console.WriteLine($"CreateBucket(PutBucketRequest) HTTP status code: {response.HttpStatusCode}");
+        }
+
+        private static void ListBuckets(AmazonS3Client s3Client)
+        {
+            var response = s3Client.ListBuckets();
+            Console.WriteLine($"ListBuckets() HTTP status code: {response.HttpStatusCode}");
         }
 
         private static void DeleteBucket(AmazonS3Client s3Client, string bucketName)
@@ -56,7 +195,7 @@ namespace Samples.AWS.S3
                 {
                     BucketName = bucketName
                 };
-                
+
                 var listResponse = s3Client.ListObjectsV2(listRequest);
                 foreach (var obj in listResponse.S3Objects)
                 {
@@ -78,18 +217,6 @@ namespace Samples.AWS.S3
                 Console.WriteLine($"Error in DeleteBucket: {ex.Message}");
                 throw;
             }
-        }
-
-        private static void CreateBucket(AmazonS3Client s3Client, string bucketName)
-        {
-            var createBucketRequest = new PutBucketRequest
-            {
-                BucketName = bucketName
-            };
-
-            var response = s3Client.PutBucket(createBucketRequest);
-
-            Console.WriteLine($"CreateBucket(PutBucketRequest) HTTP status code: {response.HttpStatusCode}");
         }
     }
 }
