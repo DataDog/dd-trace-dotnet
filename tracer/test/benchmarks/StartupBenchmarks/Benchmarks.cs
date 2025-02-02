b5c809135ed6f60246fb71e9a04ea5b4b9a099f9
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-02 00:19:13 -0500

    adding startup benchmarks

---
+++ tracer/test/benchmarks/StartupBenchmarks/Benchmarks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace StartupBenchmarks;
+
+public class Benchmarks
+{
+    [Benchmark]
+    public void NoTracer()
+    {
+        var process = new Process();
+        process.StartInfo.FileName = "dotnet";
+        process.StartInfo.Arguments = "run";
+
+        SetEnvironmentVariables(process.StartInfo.Environment);
+
+        process.Start();
+        process.WaitForExit();
+    }
+
+    private void SetEnvironmentVariables(IDictionary<string, string> startInfo)
+    {
+        startInfo["CORECLR_PROFILER"] = "none";
+    }
+}
