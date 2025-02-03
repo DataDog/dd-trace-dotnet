45fff37f356e15ec9aafade36acf2ebae1d4903e
Anna <anna.yafi@datadoghq.com>
2025-02-03 12:09:54 +0100

    Additive context in AppsecRequest Context

--- tracer/src/Datadog.Trace/AppSec/Coordinator/HttpTransportBase.cs
+++ tracer/src/Datadog.Trace/AppSec/Coordinator/HttpTransportBase.cs
@@ -18,7 +18,7 @@ namespace Datadog.Trace.AppSec.Coordinator;
 
 internal abstract class HttpTransportBase
 {
-    private bool _isAdditiveContextDisposed;
+    internal bool IsHttpContextDisposed { get; set; }
 
     internal abstract bool IsBlocked { get; }
 
@@ -30,24 +30,7 @@ internal abstract class HttpTransportBase
 
     public abstract HttpContext Context { get; }
 
-    internal abstract IContext? GetAdditiveContext();
-
-    /// <summary>
-    /// Disposes the WAF's context stored in HttpContext.Items[]. If it doesn't exist, nothing happens, no crash
-    /// </summary>
-    internal void DisposeAdditiveContext()
-    {
-        GetAdditiveContext()?.Dispose();
-        _isAdditiveContextDisposed = true;
-    }
-
-    internal bool IsAdditiveContextDisposed() => _isAdditiveContextDisposed;
-
-    protected void SetAdditiveContextDisposed(bool value) => _isAdditiveContextDisposed = value;
-
-    internal abstract void SetAdditiveContext(IContext additiveContext);
-
-    internal abstract IHeadersCollection GetRequestHeaders();
+    internal abstract IHeadersCollection? GetRequestHeaders();
 
     internal abstract IHeadersCollection GetResponseHeaders();
 
