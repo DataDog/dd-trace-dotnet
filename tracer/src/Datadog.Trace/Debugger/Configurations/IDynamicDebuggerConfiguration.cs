3211fb34a7032ab60af7040449becff085d09200
dudik <dudi.keleti@gmail.com>
2025-02-05 11:40:42 +0100

    WIP: Refactor debugger features to support configuration updates

--- tracer/src/Datadog.Trace/Debugger/Configurations/IDynamicDebuggerConfiguration.cs
+++ tracer/src/Datadog.Trace/Debugger/Configurations/IDynamicDebuggerConfiguration.cs
@@ -1,15 +1,12 @@
-// <copyright file="ProbeLocationType.cs" company="Datadog">
+// <copyright file="IDynamicDebuggerConfiguration.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
-#nullable enable
 
-namespace Datadog.Trace.Debugger
+namespace Datadog.Trace.Debugger.Configurations
 {
-    internal enum ProbeLocationType
+    internal interface IDynamicDebuggerConfiguration
     {
-        Line,
-        Method,
-        Unrecognized
+        void UpdateConfiguration(DebuggerSettings settings);
     }
 }
