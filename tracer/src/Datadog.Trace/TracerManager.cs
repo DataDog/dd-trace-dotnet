8b248b59216e37e2c4cbc0eb00ee1fd54213b3cb
dudik <dudi.keleti@gmail.com>
2025-02-05 11:40:42 +0100

    WIP: Refactor debugger features to support configuration updates

--- tracer/src/Datadog.Trace/TracerManager.cs
+++ tracer/src/Datadog.Trace/TracerManager.cs
@@ -19,6 +19,7 @@ using Datadog.Trace.Configuration.ConfigurationSources.Telemetry;
 using Datadog.Trace.Configuration.Schema;
 using Datadog.Trace.ContinuousProfiler;
 using Datadog.Trace.DataStreamsMonitoring;
+using Datadog.Trace.Debugger;
 using Datadog.Trace.DogStatsd;
 using Datadog.Trace.Logging;
 using Datadog.Trace.Logging.DirectSubmission;
@@ -678,6 +679,8 @@ namespace Datadog.Trace
 
             // start the heartbeat loop
             _heartbeatTimer = new Timer(HeartbeatCallback, state: null, dueTime: TimeSpan.Zero, period: TimeSpan.FromMinutes(1));
+
+            _ = DebuggerManager.Instance;
         }
 
         private static Task RunShutdownTasksAsync(Exception ex) => RunShutdownTasksAsync(_instance, _heartbeatTimer);
