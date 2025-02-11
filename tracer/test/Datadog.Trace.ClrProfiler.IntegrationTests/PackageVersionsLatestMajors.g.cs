9d35ac468698978bb5dc44ff9e29fdafd382669f
Dylan Yang <dylan.yang@datadoghq.com>
2025-02-11 12:55:58 -0500

    fix

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestMajors.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestMajors.g.cs
@@ -709,7 +709,62 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 #if DEFAULT_SAMPLES
                 new object[] { string.Empty },
 #else
+#if NET462
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+#endif
+#if NETCOREAPP2_1
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
+#if NETCOREAPP3_0
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
+#if NETCOREAPP3_1
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
+#if NET5_0
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
+#if NET6_0
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
+#if NET7_0
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
 #if NET8_0
+                new object[] { "3.6.9" },
+                new object[] { "4.1.3" },
+                new object[] { "5.2.0" },
+                new object[] { "6.8.1" },
+                new object[] { "7.0.0" },
+#endif
+#if NET9_0
                 new object[] { "3.6.9" },
                 new object[] { "4.1.3" },
                 new object[] { "5.2.0" },
