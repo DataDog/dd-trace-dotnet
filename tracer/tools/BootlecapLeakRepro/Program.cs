ddaf66c798b0174e57437005f6edfd178ba21836
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-12 11:22:33 -0500

    write iteration number to console

--- tracer/tools/BootlecapLeakRepro/Program.cs
+++ tracer/tools/BootlecapLeakRepro/Program.cs
@@ -6,10 +6,14 @@ internal static class Program
 {
     public static async Task Main()
     {
+        int i = 0;
+
         while (true)
         {
             try
             {
+                Console.WriteLine($"Iteration {i++}");
+
                 var requestBuilder = new LambdaRequestBuilder();
                 var scope = LambdaCommon.SendStartInvocation(requestBuilder, data: string.Empty, context: null);
                 await LambdaCommon.EndInvocationAsync(string.Empty, exception: null, scope, requestBuilder);
