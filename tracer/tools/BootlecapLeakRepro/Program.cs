2ec9768bb81d97780665ed6b27de88ef6d6c51bc
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-12 10:58:58 -0500

    add small repro console app

---
+++ tracer/tools/BootlecapLeakRepro/Program.cs
@@ -0,0 +1,13 @@
+﻿using Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.Lambda;
+
+namespace BootlecapLeakRepro;
+
+internal static class Program
+{
+    public static async Task Main()
+    {
+        var requestBuilder = new LambdaRequestBuilder();
+        var scope = LambdaCommon.SendStartInvocation(requestBuilder, data: string.Empty, context: null);
+        await LambdaCommon.EndInvocationAsync(string.Empty, exception: null, scope, requestBuilder);
+    }
+}
