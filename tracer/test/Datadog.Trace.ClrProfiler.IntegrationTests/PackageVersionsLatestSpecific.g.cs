035e21bddc9dae70d0cecb4a5dbd2300bb2ce44f
Dylan Yang <dylan.yang@datadoghq.com>
2025-02-12 08:58:48 -0500

    [SVLS-5559] Inject trace context into AWS Step Functions input attribute (#6171)
    inject trace context to step functions input

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
@@ -331,6 +331,61 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 #endif
             };
 
+      public static IEnumerable<object[]> AwsStepFunctions =>
+
+            new List<object[]>
+            {
+#if DEFAULT_SAMPLES
+                new object[] { string.Empty },
+#else
+#if NET462
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NETCOREAPP2_1
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NETCOREAPP3_0
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NETCOREAPP3_1
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NET5_0
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NET6_0
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NET7_0
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NET8_0
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#if NET9_0
+                new object[] { "3.3.104.87" },
+                new object[] { "3.5.3.37" },
+                new object[] { "3.7.403.32" },
+#endif
+#endif
+            };
+
       public static IEnumerable<object[]> MongoDB =>
 
             new List<object[]>
