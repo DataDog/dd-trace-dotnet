51903fe262911c07e2adffd4294a61659779d1b7
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-11 22:40:05 -0500

    add api stubs

---
+++ tracer/src/Datadog.Trace/MinimalBuildStubs/Debugger/LiveDebugger.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="LiveDebugger.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Threading.Tasks;
+
+namespace Datadog.Trace.Debugger;
+
+internal class LiveDebugger
+{
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ tracer/src/Datadog.Trace/MinimalBuildStubs/Iast/Iast.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Iast.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+namespace Datadog.Trace.Iast;
+
+internal class Iast
+{
+    public static Iast Instance { get; } = new();
+
+    public IastSettings Settings { get; } = new();
+
+    public OverheadControllerStub OverheadController { get; } = new();
+
+    public void InitAnalyzers()
+    {
+    }
+
+    internal class OverheadControllerStub
+    {
+        public void ReleaseRequest()
+        {
+        }
+
+        public bool AcquireRequest() => false;
+    }
+}
