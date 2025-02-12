fc3bbf9dab7090ea556b44a8ce53b855b60fa43c
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-11 22:40:05 -0500

    add api stubs

---
+++ tracer/src/Datadog.Trace/MinimalBuildStubs/Iast/IastRequestContext.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="IastRequestContext.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Datadog.Trace.Iast;
+
+internal class IastRequestContext
+{
+    public void AddIastVulnerabilitiesToSpan(Span span)
+    {
+    }
+
+    internal static void AddIastDisabledFlagToSpan(Span span)
+    {
+    }
+
+    public void AddRequestData(HttpRequest httpContextRequest, RouteValueDictionary routeValues)
+    {
+    }
+
+    public void AddRequestBody(object resultModel, object bodyExtracted)
+    {
+    }
+}
