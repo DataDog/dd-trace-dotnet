f28d63423386593a1e4e7b5c3e8c8f8ed980ce89
Matan Green <matangrn@gmail.com>
2025-02-17 17:23:02 +0200

    Properly disposing Exception Cases as part of Exception Replay for the remote enablement

--- tracer/src/Datadog.Trace/Debugger/ExceptionAutoInstrumentation/ExceptionTrackManager.cs
+++ tracer/src/Datadog.Trace/Debugger/ExceptionAutoInstrumentation/ExceptionTrackManager.cs
@@ -636,6 +636,21 @@ namespace Datadog.Trace.Debugger.ExceptionAutoInstrumentation
             this._cts.Cancel();
             this._reportingCircuitBreaker.Dispose();
 
+            foreach (var trackedExceptionCase in _trackedExceptionCases.Values)
+            {
+                try
+                {
+                    if (!trackedExceptionCase.Revert(0))
+                    {
+                        ExceptionCaseInstrumentationManager.Revert(trackedExceptionCase.ExceptionCase); // Force revert
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "ExceptionTrackManager: An exception was thrown while calling Revert on given exception case {ExceptionIdentifier}.", trackedExceptionCase.ExceptionIdentifier);
+                }
+            }
+
             try
             {
                 _exceptionProcessorTask?.Wait();
@@ -652,6 +667,7 @@ namespace Datadog.Trace.Debugger.ExceptionAutoInstrumentation
             {
                 this._workAvailable.Dispose();
                 this._cts.Dispose();
+                _trackedExceptionCases.Clear();
             }
         }
 
