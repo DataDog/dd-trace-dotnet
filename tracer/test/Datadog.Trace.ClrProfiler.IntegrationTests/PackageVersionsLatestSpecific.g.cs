ac7d84be7fd4e316b171743f8a52b4e8c54357aa
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2025-02-19 09:47:13 -0500

    Only test versions of Cosmos where we can disable SSL

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
@@ -1844,39 +1844,39 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
                 new object[] { string.Empty },
 #else
 #if NET462
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #if NETCOREAPP2_1
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.28.0" },
 #endif
 #if NETCOREAPP3_0
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.28.0" },
 #endif
 #if NETCOREAPP3_1
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #if NET5_0
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #if NET6_0
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #if NET7_0
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #if NET8_0
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #if NET9_0
-                new object[] { "3.6.0" },
+                new object[] { "3.13.0" },
                 new object[] { "3.47.0" },
 #endif
 #endif
