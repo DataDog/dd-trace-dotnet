ad8e3c651d8857e08b365c59a0cf14fda91028ef
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2025-02-19 14:41:14 +0100

    update nupkg to 56257499.0.0

--- build/nupkg_local/libdatadog/build.sh
+++ build/nupkg_local/libdatadog/build.sh
@@ -2,195 +2,94 @@
 
 set -e
 
-# Get the latest release version from GitHub
-version=$(curl -s https://api.github.com/repos/DataDog/libdatadog/releases/latest | grep -o '"tag_name": "*\([^"]*\)"' | cut -d '"' -f 4)
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --profile) profile="$2"; shift ;;
+        --pipeline-id) pipeline_id="$2"; shift ;;
+        --object-prefix) object_prefix="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
-# Define the URLs
-urls=(
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-aarch64-alpine-linux-musl.tar.gz"
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-aarch64-apple-darwin.tar.gz"
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-aarch64-unknown-linux-gnu.tar.gz"
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-x64-windows.zip"
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-x86-windows.zip"
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-x86_64-alpine-linux-musl.tar.gz"
-    # "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-x86_64-apple-darwin.tar.gz"
-    "https://github.com/DataDog/libdatadog/releases/download/$version/libdatadog-x86_64-unknown-linux-gnu.tar.gz"
-)
+# Check if profile argument is passed
+if [ -z "$profile" ]; then
+    echo "Usage: $0 --profile <aws-profile> --pipeline-id <pipeline-id> --object-prefix <object-prefix>"
+    exit 1
+fi
 
-# Create the sources/runtime directory if it doesn't exist
-# mkdir -p sources/runtimes
-# Download release files
-# for url in "${urls[@]}"; do
-#     filename=$(basename "$url")
-
-#     echo "Downloading $url... at release/$filename"
-#     curl -L -o "release/$filename" "$url"
-# done
+# Get the latest libdatadog build
+files=$(aws s3 ls s3://$object_prefix/libdatadog_$pipeline_id --profile $profile | awk '{print $4}')
+for file in $files; do
+    # check if we have the file already
+    if [ -f "release/$file" ]; then
+        echo "Skipping download of $file"
+        continue
+    fi
+    aws s3 cp s3://$object_prefix/$file release/ --profile $profile
+done
 
 # # extract the files
-# mkdir -p release/unzipped
-# for file in release/*; do
-#     if [[ $file == *.tar.gz ]]; then
-#         echo "Extracting $file..."
-#         tar -xzf "$file" -C release/unzipped
-#     elif [[ $file == *.zip ]]; then
-#         echo "Extracting $file..."
-#         # unzip with overwrite
-#         unzip -q "$file" -d release/unzipped -o
-#     fi
-# done
-
-# artifacts.zip                                              libdatadog_55986604_24558711_i686-alpine-linux-musl.tar    libdatadog_55986604_24558711_x86_64-unknown-linux-gnu.tar
-# libdatadog_55986604_24558711_aarch64-alpine-linux-musl.tar libdatadog_55986604_24558711_i686-unknown-linux-gnu.tar
-# libdatadog_55986604_24558711_aarch64-unknown-linux-gnu.tar libdatadog_55986604_24558711_x86_64-alpine-linux-musl.tar
-
-# extract pipeline artifacts
 mkdir -p release/unzipped
-# for file in release/*; do
-#     if [[ $file == *.tar ]]; then
-#         echo "Extracting $file..."
-#         tar -xf "$file" -C release/unzipped
-#     elif [[ $file == *.zip ]]; then
-#         echo "Extracting $file..."
-#         # unzip with overwrite
-#         unzip -q "$file" -d release/unzipped
-#         unzip -q release/unzipped/artifacts/libdatadog-x64-windows.zip -d release/unzipped
-#         unzip -q release/unzipped/artifacts/libdatadog-x86-windows.zip -d release/unzipped
-#         rm -rf release/unzipped/artifacts
-#     fi
-# done
+files=$(ls release/*.tar.gz)
+for file in $files; do
+    tar -xzf "$file" -C release/unzipped
+done
 
 # copy required binaries to their rid format inside sources/runtimes
-# aarch64-alpine-linux-musl i686-alpine-linux-musl    libdatadog-x64-windows    version.txt               x86_64-unknown-linux-gnu
-# aarch64-unknown-linux-gnu i686-unknown-linux-gnu    libdatadog-x86-windows    x86_64-alpine-linux-musl
-for dir in release/unzipped/*; do
-    # if dir is any linux distro
-    # they follow same structure
-    # check if the dir contains linux and then follow {arch}-{os+distro+libc}
-    if [[ $dir == *-linux-* ]]; then
-        # get the arch
-        arch=$(basename "$dir" | cut -d'-' -f1)
-        os=$(basename "$dir" | cut -d'-' -f2-)
+dirs=$(ls -d release/unzipped/*/)
+for dir in $dirs; do
+    echo "Processing $dir"
+    base_dir=$(basename $dir)
+    if [ $base_dir == "aarch64-alpine-linux-musl" ]; then
+        rid="linux-musl-arm64"
+    elif [ $base_dir == "aarch64-apple-darwin" ]; then
+        rid="osx-arm64"
+    elif [ $base_dir == "aarch64-unknown-linux-gnu" ]; then
+        rid="linux-arm64"
+    elif [ $base_dir == "i686-alpine-linux-musl" ]; then
+        echo "Skipping $base_dir"
+        continue
+    elif [ $base_dir == "i686-unknown-linux-gnu" ]; then
+        echo "Skipping $base_dir"
+        continue
+    elif [ $base_dir == "libdatadog-x64-windows" ]; then
+        rid="win-x64"
+    elif [ $base_dir == "libdatadog-x86-windows" ]; then
+        rid="win-x86"
+    elif [ $base_dir == "x86_64-alpine-linux-musl" ]; then
+        rid="linux-musl-x64"
+    elif [ $base_dir == "x86_64-apple-darwin" ]; then
+        echo "Skipping $base_dir"
+        continue
+    elif [ $base_dir == "x86_64-unknown-linux-gnu" ]; then
+        rid="linux-x64"
+    else
+        echo "Unknown rid $base_dir"
+        continue
+    fi
 
-        container_dir=$(basename "$dir")
 
-        # create rids
-        if [[ $container_dir == "aarch64-alpine-linux-musl" ]]; then
-            rid="linux-musl-arm64"
-        elif [[ $container_dir == "aarch64-unknown-linux-gnu" ]]; then
-            rid="linux-arm64"
-        elif [[ $container_dir == "x86_64-alpine-linux-musl" ]]; then
-            rid="linux-musl-x64"
-        elif [[ $container_dir == "x86_64-unknown-linux-gnu" ]]; then
-            rid="linux-x64"
-        else
-            echo "Unknown linux distro $container_dir"
-            continue
-        fi
-
-        echo "Copying $dir to sources/runtimes/$rid"
-        mkdir -p "sources/runtimes/$rid/native"
+    mkdir -p "sources/runtimes/$rid/native"
+    # if linux
+    if [[ $rid == *"linux"* ]]; then
+        # copy the libdatadog.so to the correct runtime directory
         cp -r "$dir/lib/libdatadog_profiling.so" "sources/runtimes/$rid/native/datadog_profiling_ffi.so"
-    fi
-
-    # if dir is windows
-    # they follow same structure libdatadog-{arch}-{os}
-    # check if the dir contains windows and then follow libdatadog-{arch}-{os}
-    if [[ $dir == *windows ]]; then
-        # get the arch
-        arch=$(basename "$dir" | cut -d'-' -f2)
-        os=$(basename "$dir" | cut -d'-' -f3)
-
-        # create rids
-        if [[ $os == "windows" ]]; then
-            os="win"
-        fi
-
-        if [[ $arch == "x86" ]]; then
-            arch="x86"
-        elif [[ $arch == "x64" ]]; then
-            arch="x64"
-        fi
-
-        rid="$os-$arch"
-
-        echo "Copying $dir to sources/runtimes/$rid"
-        mkdir -p "sources/runtimes/$rid/native"
+    elif [[ $rid == *"win"* ]]; then
+        # copy the libdatadog.dll to the correct runtime directory
         cp -r "$dir/release/dynamic/datadog_profiling_ffi.dll" "sources/runtimes/$rid/native/datadog_profiling_ffi.dll"
-    fi
-
-    # if dir is apple
-    # they follow same structure {arch}-{distro}
-    # check if the dir contains apple and then follow {arch}-{distro}
-    if [[ $dir == *apple* ]]; then
-        container_dir=$(basename "$dir")
-
-        # create rids
-        if [[ $container_dir == "aarch64-apple-darwin" ]]; then
-            rid="osx-arm64"
-        elif [[ $container_dir == "x86_64-apple-darwin" ]]; then
-            rid="osx-x64"
-        else
-            echo "Unknown apple distro $container_dir"
-            continue
-        fi
-
-        echo "Copying $dir to sources/runtimes/$rid"
-        mkdir -p "sources/runtimes/$rid/native"
+    elif [[ $rid == *"osx"* ]]; then
+        # copy the libdatadog.dylib to the correct runtime directory
         cp -r "$dir/lib/libdatadog_profiling.dylib" "sources/runtimes/$rid/native/datadog_profiling_ffi.dylib"
     fi
 done
 
-# copy required binaries to their rid format inside sources/runtimes
-# {os]-{distro}-{arch}
-# os: linux, osx, win
-# for dir in release/unzipped/*; do
-#     if [[ -d $dir ]]; then
-#         # libdatadog-aarch64-alpine-linux-musl
-#         # libdatadog-{arch}-{os+distro+libc}
-
-#         # get the arch
-#         arch=$(basename "$dir" | cut -d'-' -f2)
-#         os=$(basename "$dir" | cut -d'-' -f3-)
-
-#         # create rids
-#         if [[ $os == "alpine-linux-musl" ]]; then
-#             os="linux-musl"
-#         elif [[ $os == "apple-darwin" ]]; then
-#             os="osx"
-#         elif [[ $os == "unknown-linux-gnu" ]]; then
-#             os="linux"
-#         elif [[ $os == "windows" ]]; then
-#             os="win"
-#         fi
-
-#         if [[ $arch == "x86_64" ]]; then
-#             arch="x64"
-#         elif [[ $arch == "aarch64" ]]; then
-#             arch="arm64"
-#         elif [[ $arch == "x86" ]]; then
-#             arch="x86"
-#         elif [[ $arch == "x64" ]]; then
-#             arch="x64"
-#         fi
-
-#         rid="$os-$arch"
-
-#         echo "Copying $dir to sources/runtimes/$rid"
-#         mkdir -p "sources/runtimes/$rid/native"
-#         if [[ $os == "win" ]]; then
-#             cp -r "$dir/release/dynamic/datadog_profiling_ffi.dll" "sources/runtimes/$rid/native/datadog_profiling_ffi.dll"
-#         elif [[ $os == "osx" ]]; then
-#             cp -r "$dir/lib/libdatadog_profiling.dylib" "sources/runtimes/$rid/native/datadog_profiling_ffi.dylib"
-#         else
-#             cp -r "$dir/lib/libdatadog_profiling.so" "sources/runtimes/$rid/native/datadog_profiling_ffi.so"
-#         fi
-#     fi
-# done
-
 # trim any aplha characters from the version
-version=55986604.0.0
+version=$pipeline_id.0.0
+rm -f ../../../packages/libdatadog.*.nupkg
 nuget pack sources/libdatadog.nuspec -OutputDirectory ../../../packages -Version $version
+git add ../../../packages/libdatadog.$version.nupkg -f
 
 # update the version in the csproj file
 # ../../../tracer/src/Datadog.Trace/Datadog.Trace.csproj
@@ -198,3 +97,4 @@ nuget pack sources/libdatadog.nuspec -OutputDirectory ../../../packages -Version
 echo "Adding libdatadog package version $version to projects"
 dotnet add ../../../tracer/src/Datadog.Trace/Datadog.Trace.csproj package libdatadog --version $version
 dotnet add ../../../build/nupkg_local/libdatadog/test/Console/Console.csproj package libdatadog --version $version
+dotnet run --project ../../../build/nupkg_local/libdatadog/test/Console/Console.csproj
