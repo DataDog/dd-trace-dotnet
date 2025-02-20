144154049ab566c6f70a1b67339b8587c52cae17
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-02-20 14:09:23 +0100

    First commit to create Datadog Symbols Server

---
+++ shared/src/DatadogSymbolsServer/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace DatadogSymbolsServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddHostedService<DotnetApmSymbolsCache>(s => s.GetService<ISymbolsCache>() as DotnetApmSymbolsCache);
+            services.AddSingleton<ISymbolsCache, DotnetApmSymbolsCache>();
+            services.AddControllers();
+            services.AddHttpClient();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors();
+            app.UseRouting();
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(app2 =>
+            {
+                app2.MapControllers();
+            });
+        }
+    }
+}
