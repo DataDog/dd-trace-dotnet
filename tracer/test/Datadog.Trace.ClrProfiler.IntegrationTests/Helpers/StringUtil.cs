be6fc179194aced65b36a8320c15995a8527a7d8
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-21 12:45:35 -0500

    add StringUtil.GetAllHeaders()

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/StringUtil.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/Helpers/StringUtil.cs
@@ -3,26 +3,47 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Datadog.Trace.ClrProfiler.IntegrationTests.Helpers
 {
     internal static class StringUtil
     {
+        private static readonly Regex HeaderRegex = new(
+            @"^\[HttpListener\] request header: (?<name>.*?)=(?<value>.*?)\r?$",
+            RegexOptions.Multiline);
+
         /// <summary>
         /// Retrieves a header using a regular expression match.
         /// </summary>
         /// <param name="source">Source string to search.</param>
         /// <param name="name">Name of header to search for.</param>
         /// <returns>Matched string or null if no match</returns>
-        internal static string GetHeader(string source, string name)
+        internal static string? GetHeader(string source, string name)
         {
             var pattern = $@"^\[HttpListener\] request header: {name}=(.*?)\r?$";
             var match = Regex.Match(source, pattern, RegexOptions.Multiline);
 
-            return match.Success
-                       ? match.Groups[1].Value
-                       : null;
+            return match.Success ? match.Groups[1].Value : null;
+        }
+
+        /// <summary>
+        /// Retrieves all headers using a regular expression match.
+        /// </summary>
+        /// <param name="source">Source string to search.</param>
+        /// <returns>Matched headers.</returns>
+        internal static Dictionary<string, string> GetAllHeaders(string source)
+        {
+            return HeaderRegex.Matches(source)
+                              .Cast<Match>() // required in older runtimes where MatchCollection doesn't implement IEnumerable<Match>
+                              .Where(match => match.Success)
+                              .ToDictionary(
+                                  match => match.Groups["name"].Value,
+                                  match => match.Groups["value"].Value);
         }
     }
 }
