c48719f3ed217f6f1ca7b4156c7b83a02f724f14
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-02-24 17:22:31 +0100

    Cleanup and adjustements to make it work

--- shared/src/DatadogSymbolsServer/Program.cs
+++ shared/src/DatadogSymbolsServer/Program.cs
@@ -5,12 +5,41 @@ namespace DatadogSymbolsServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Run();
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args)
+        private static WebApplication CreateHostBuilder(string[] args)
         {
-            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(builder => { builder.UseStartup<Startup>(); });
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseSystemd();
+            builder.Host.UseContentRoot(Directory.GetCurrentDirectory());
+
+            var services = builder.Services;
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddHostedService<DotnetApmSymbolsCache>(s => s.GetService<ISymbolsCache>() as DotnetApmSymbolsCache);
+            services.AddSingleton<ISymbolsCache, DotnetApmSymbolsCache>();
+            services.AddControllers();
+            services.AddHttpClient();
+
+            var app = builder.Build();
+
+
+            app.UseCors();
+            app.UseRouting();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(app2 => app2.MapControllers());
+
+            return app;
+
         }
     }
 }
