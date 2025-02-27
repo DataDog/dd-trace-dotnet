f67c01344e8311f769d8486a5240bfd26f432bee
dudik <dudi.keleti@gmail.com>
2025-02-27 11:38:26 +0100

    add namespace and remove 'this' keyword

--- tracer/src/Datadog.Trace/Debugger/DynamicInstrumentation.cs
+++ tracer/src/Datadog.Trace/Debugger/DynamicInstrumentation.cs
@@ -185,7 +185,7 @@ namespace Datadog.Trace.Debugger
                                 switch (status)
                                 {
                                     case LiveProbeResolveStatus.Bound:
-                                        lineProbes.Add(new NativeLineProbeDefinition(location!.ProbeDefinition.Id, location.MVID, location.MethodToken, (int)location.BytecodeOffset, location.LineNumber, location.ProbeDefinition.Where.SourceFile));
+                                        lineProbes.Add(new NativeLineProbeDefinition(location!.ProbeDefinition.Id, location.Mvid, location.MethodToken, (int)location.BytecodeOffset, location.LineNumber, location.ProbeDefinition.Where.SourceFile));
                                         fetchProbeStatus.Add(new FetchProbeStatus(probe.Id, probe.Version ?? 0));
                                         ProbeExpressionsProcessor.Instance.AddProbeProcessor(probe);
                                         SetRateLimit(probe);
@@ -343,7 +343,7 @@ namespace Datadog.Trace.Debugger
                         noLongerUnboundProbes ??= new List<ProbeDefinition>();
 
                         noLongerUnboundProbes.Add(unboundProbe);
-                        lineProbes.Add(new NativeLineProbeDefinition(location!.ProbeDefinition.Id, location.MVID, location.MethodToken, (int)location.BytecodeOffset, location.LineNumber, location.ProbeDefinition.Where.SourceFile));
+                        lineProbes.Add(new NativeLineProbeDefinition(location!.ProbeDefinition.Id, location.Mvid, location.MethodToken, (int)location.BytecodeOffset, location.LineNumber, location.ProbeDefinition.Where.SourceFile));
                     }
                 }
 
