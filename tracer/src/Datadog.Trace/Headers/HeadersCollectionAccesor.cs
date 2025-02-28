254acfe0ec84de74cf31786e34265ee3afc6e85e
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-28 12:40:30 -0500

    rename to HeadersCollectionAccesor

--- tracer/src/Datadog.Trace/Headers/HeadersCollectionAccesor.cs
+++ tracer/src/Datadog.Trace/Headers/HeadersCollectionAccesor.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="HeadersCollectionGetterAndSetter.cs" company="Datadog">
+﻿// <copyright file="HeadersCollectionAccesor.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -10,7 +10,7 @@ using Datadog.Trace.Propagators;
 
 namespace Datadog.Trace.Headers;
 
-internal readonly struct HeadersCollectionGetterAndSetter<TCarrier> : ICarrierGetter<TCarrier>, ICarrierSetter<TCarrier>
+internal readonly struct HeadersCollectionAccesor<TCarrier> : ICarrierGetter<TCarrier>, ICarrierSetter<TCarrier>
     where TCarrier : IHeadersCollection
 {
     public IEnumerable<string?> Get(TCarrier carrier, string key)
