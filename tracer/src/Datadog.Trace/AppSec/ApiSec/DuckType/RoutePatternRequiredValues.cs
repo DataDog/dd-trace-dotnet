9cf36f8aa93a051b8cff67a933a9afebaa53fe8d
Flavien Darche <flavien.darche@datadoghq.com>
2025-03-03 17:09:03 +0000

    fix tests

--- tracer/src/Datadog.Trace/AppSec/ApiSec/DuckType/RoutePatternRequiredValues.cs
+++ tracer/src/Datadog.Trace/AppSec/ApiSec/DuckType/RoutePatternRequiredValues.cs
@@ -1,29 +1,27 @@
-// <copyright file="RoutePattern.cs" company="Datadog">
+// <copyright file="RoutePatternRequiredValues.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
 #nullable enable
 
-using System.Collections;
+#if NETCOREAPP3_0_OR_GREATER
+
 using Datadog.Trace.DuckTyping;
 
-namespace Datadog.Trace.DiagnosticListeners
+namespace Datadog.Trace.AppSec.ApiSec.DuckType
 {
     /// <summary>
     /// RoutePattern for duck typing
     /// </summary>
     [DuckCopy]
-    internal struct RoutePattern
+    public struct RoutePatternRequiredValues
     {
         /// <summary>
-        /// Gets the list of IReadOnlyList&lt;RoutePatternPathSegment&gt;
+        /// Gets the RoutePattern.RequiredValues
         /// </summary>
-        public IEnumerable PathSegments;
-
-        /// <summary>
-        /// Gets the RoutePattern.RawText
-        /// </summary>
-        public string? RawText;
+        public System.Collections.Generic.IReadOnlyDictionary<string, object?> RequiredValues;
     }
 }
+
+#endif
