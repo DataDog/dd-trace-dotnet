c05c0b55024e5a1d97931bb5b6dd9aa74b6dd0ad
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-03-06 13:19:01 -0500

    add missing null checks in TraceContext and ExtraServicesProvider

--- tracer/src/Datadog.Trace/Configuration/ExtraServicesProvider.cs
+++ tracer/src/Datadog.Trace/Configuration/ExtraServicesProvider.cs
@@ -27,7 +27,8 @@ internal class ExtraServicesProvider
     {
         // Several threads entering simultaneously can cause MaxExtraService to be exceeded.
         // As long as the list doesn't grow much beyond MaxExtraService we don't care.
-        if (_serviceCount >= MaxExtraServices ||
+        if (serviceName == null! ||
+            _serviceCount >= MaxExtraServices ||
             _extraServices.ContainsKey(serviceName))
         {
             return;
