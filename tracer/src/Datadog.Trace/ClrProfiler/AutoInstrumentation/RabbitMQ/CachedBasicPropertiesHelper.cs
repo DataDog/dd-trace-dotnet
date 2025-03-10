ec9af0fff58a25a0636cd6a2cc8f0ecce14f9819
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-03-10 19:49:50 -0400

    wrap static initialization in try/catch

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/CachedBasicPropertiesHelper.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/CachedBasicPropertiesHelper.cs
@@ -7,39 +7,45 @@
 using System;
 using System.Reflection.Emit;
 using Datadog.Trace.DuckTyping;
+using Datadog.Trace.Logging;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ;
 
 internal static class CachedBasicPropertiesHelper<TBasicProperties>
 {
-    private static readonly Func<object, TBasicProperties> Activator;
+    private static readonly Func<object, TBasicProperties>? Activator;
 
     static CachedBasicPropertiesHelper()
     {
-        var targetType = typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.BasicProperties")!;
-        var parameterType = typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.IReadOnlyBasicProperties")!;
+        try
+        {
+            var targetType = typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.BasicProperties")!;
+            var parameterType = typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.IReadOnlyBasicProperties")!;
 
-        var constructor = targetType.GetConstructor([parameterType])!;
+            var constructor = targetType.GetConstructor([parameterType])!;
 
-        var createBasicPropertiesMethod = new DynamicMethod(
-            $"TypeActivator_{targetType.Name}_{parameterType.Name}",
-            targetType,
-            [typeof(object)],
-            typeof(DuckType).Module,
-            skipVisibility: true);
+            var createBasicPropertiesMethod = new DynamicMethod(
+                $"TypeActivator_{targetType.Name}_{parameterType.Name}",
+                targetType,
+                [typeof(object)],
+                typeof(DuckType).Module,
+                skipVisibility: true);
 
-        var il = createBasicPropertiesMethod.GetILGenerator();
-        il.Emit(OpCodes.Ldarg_0);                  // Load first argument (as object).
-        il.Emit(OpCodes.Castclass, parameterType); // Cast to IReadOnlyBasicProperties
-        il.Emit(OpCodes.Newobj, constructor);      // Call constructor
-        il.Emit(OpCodes.Ret);                      // Return new instance
+            var il = createBasicPropertiesMethod.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);                  // Load first argument (as object).
+            il.Emit(OpCodes.Castclass, parameterType); // Cast to IReadOnlyBasicProperties
+            il.Emit(OpCodes.Newobj, constructor);      // Call constructor
+            il.Emit(OpCodes.Ret);                      // Return new instance
 
-        // typeof(Func<object, BasicProperties>)
-        var funcType = typeof(Func<,>).MakeGenericType(typeof(object), targetType);
-
-        Activator = (Func<object, TBasicProperties>)createBasicPropertiesMethod.CreateDelegate(funcType);
+            Activator = (Func<object, TBasicProperties>)createBasicPropertiesMethod.CreateDelegate(typeof(Func<object, TBasicProperties>));
+        }
+        catch (Exception ex)
+        {
+            var log = DatadogLogging.GetLoggerFor(typeof(CachedBasicPropertiesHelper<TBasicProperties>));
+            log.Error(ex, "Failed to create activator for {TBasicProperties}", typeof(TBasicProperties).FullName);
+        }
     }
 
-    public static TBasicProperties CreateHeaders(object readonlyBasicProperties)
-        => Activator(readonlyBasicProperties);
+    public static TBasicProperties? CreateHeaders(object readonlyBasicProperties)
+        => Activator is null ? default : Activator(readonlyBasicProperties);
 }
