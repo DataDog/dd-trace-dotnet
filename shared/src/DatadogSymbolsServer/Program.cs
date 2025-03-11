8e0662a81f66b836bb95940dab3495c4f12e797a
Andrew Lock <andrew.lock@datadoghq.com>
2025-03-11 16:48:31 +0000

    Convert to minimal APIs and make some minor modifications

--- shared/src/DatadogSymbolsServer/Program.cs
+++ shared/src/DatadogSymbolsServer/Program.cs
@@ -1,4 +1,6 @@
+using System.Text.RegularExpressions;
 using DatadogSymbolsServer;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,8 @@ var services = builder.Services;
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 services.AddSingleton<ISymbolsCache, DotnetApmSymbolsCache>();
-services.AddControllers();
+services.AddAuthorization();
+services.AddCors();
 // Add named httpclient, with resiliency policy
 services.AddHttpClient("github", client =>
     {
@@ -35,7 +38,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 // Configure endpoints
-app.MapControllers();
+app.MapGet("/debuginfod/buildid/{guid}/{kind}", Endpoints.GetByGuid);
+app.MapGet("/ms/{file}/{guid}/{ignored}", Endpoints.GetByFile);
+app.MapGet("/ingest", Endpoints.IngestNewVersion);
 
 // prep the cache
 using (var scope = app.Services.CreateScope())
@@ -45,3 +50,56 @@ using (var scope = app.Services.CreateScope())
 }
 
 await app.RunAsync();
+
+
+public partial class Endpoints
+{
+    private static readonly Regex _fileRegex = GetFileRegex();
+
+    [GeneratedRegex(@"elf-buildid-(sym-)?(?<guid>[0-9a-f]+)", RegexOptions.IgnoreCase)]
+    private static partial Regex GetFileRegex();
+
+    // kind can be symbol file or binary file.
+    // Did not find yet how it's used by gdb. For now just use it as the download filename
+    public static Results<FileStreamHttpResult, NotFound> GetByGuid(
+        string guid, string kind, ILogger<Endpoints> logger, ISymbolsCache symbolsCache)
+    {
+        logger.LogInformation("Getting file {Guid}", guid);
+        var symbolFromCache = symbolsCache.Get(guid, SymbolKind.Linux);
+        if (symbolFromCache != null)
+        {
+            logger.LogInformation($"Found {guid} in cache.");
+            return TypedResults.File(symbolFromCache, "application/octet-stream", fileDownloadName: kind);
+        }
+
+        return TypedResults.NotFound();
+    }
+
+    public static Results<FileStreamHttpResult, NotFound> GetByFile(
+        string file, string guid, string ignored, ILogger<Endpoints> logger, ISymbolsCache symbolsCache)
+    {
+        _ = ignored; // to stop it complaining about unused;
+
+        var match = _fileRegex.Match(guid);
+        var (guidd, kind) = match.Success switch
+        {
+            true => (match.Groups["guid"].Value, SymbolKind.Linux),
+            _ => (guid, SymbolKind.Windows)
+        };
+
+        logger.LogInformation("Getting file {File}", file);
+        var symbolsFile = symbolsCache.Get(guidd, kind);
+        if (symbolsFile != null)
+        {
+            logger.LogInformation("Found {File} in cache.", file);
+            return TypedResults.File(symbolsFile, "application/octet-stream", fileDownloadName: file);
+        }
+
+        return TypedResults.NotFound();
+    }
+
+    public static async Task IngestNewVersion(string version, ISymbolsCache symbolsCache, CancellationToken cancellationToken)
+    {
+        await symbolsCache.Ingest(version, cancellationToken);
+    }
+}
