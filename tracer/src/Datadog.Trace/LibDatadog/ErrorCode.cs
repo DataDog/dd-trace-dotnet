35965c86fe347837e4cf655b273ec08e6e000622
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2025-03-13 13:37:20 +0100

    doc update

--- tracer/src/Datadog.Trace/LibDatadog/ErrorCode.cs
+++ tracer/src/Datadog.Trace/LibDatadog/ErrorCode.cs
@@ -12,23 +12,98 @@ namespace Datadog.Trace.LibDatadog;
 /// </summary>
 internal enum ErrorCode
 {
+    /// <summary>
+    /// Address already in use
+    /// </summary>
     AddressInUse = 0,
+
+    /// <summary>
+    /// Connection aborted
+    /// </summary>
     ConnectionAborted = 1,
+
+    /// <summary>
+    /// Connection refused
+    /// </summary>
     ConnectionRefused = 2,
+
+    /// <summary>
+    /// Connection reset by peer
+    /// </summary>
     ConnectionReset = 3,
+
+    /// <summary>
+    /// Error parsing HTTP body
+    /// </summary>
     HttpBodyFormat = 4,
+
+    /// <summary>
+    /// HTTP body too long
+    /// </summary>
     HttpBodyTooLong = 5,
+
+    /// <summary>
+    /// HTTP error originated by client
+    /// </summary>
     HttpClient = 6,
+
+    /// <summary>
+    /// Error while parsing HTTP message
+    /// </summary>
     HttpParse = 7,
+
+    /// <summary>
+    /// HTTP error originated by server
+    /// </summary>
     HttpServer = 8,
+
+    /// <summary>
+    /// HTTP unknown error
+    /// </summary>
     HttpUnknown = 9,
+
+    /// <summary>
+    /// HTTP wrong status number
+    /// </summary>
     HttpWrongStatus = 10,
+
+    /// <summary>
+    /// Invalid argument provided
+    /// </summary>
     InvalidArgument = 11,
+
+    /// <summary>
+    /// Invalid data payload
+    /// </summary>
     InvalidData = 12,
+
+    /// <summary>
+    /// Invalid input
+    /// </summary>
     InvalidInput = 13,
+
+    /// <summary>
+    /// Invalid URL
+    /// </summary>
     InvalidUrl = 14,
+
+    /// <summary>
+    /// Input/Output error
+    /// </summary>
     IoError = 15,
+
+    /// <summary>
+    /// Unknown network error
+    /// </summary>
     NetworkUnknown = 16,
+
+    /// <summary>
+    /// Serialization/Deserialization error
+    /// </summary>
     Serde = 17,
+
+    /// <summary>
+    /// Operation timed out
+    /// </summary>
     TimedOut = 18,
 }
