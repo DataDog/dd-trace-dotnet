01ca5af031696a4301e382acf8f865110440c8e8
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-03-19 15:51:07 +0100

    Fix linux build

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -560,7 +560,7 @@ partial class Build
                     if (IsLinux || IsOsx)
                     {
                         CMake.Value(
-                            arguments: $"-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -B {NativeBuildDirectory} -S {RootDirectory} -DCMAKE_BUILD_TYPE={BuildConfiguration}");
+                            arguments: $"-DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -B {NativeBuildDirectory} -S {RootDirectory}");
                     }
                     else if (IsWin)
                     {
