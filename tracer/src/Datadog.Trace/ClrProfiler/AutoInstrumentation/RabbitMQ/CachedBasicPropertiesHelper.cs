9e9b0f1562cddc730ad80ac97227a951cefca540
Andrew Lock <andrew.lock@datadoghq.com>
2025-03-19 11:28:48 +0000

    Fix: Rabbitmq7 header injection overwrites user supplied basic properties

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/CachedBasicPropertiesHelper.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/RabbitMQ/CachedBasicPropertiesHelper.cs
@@ -1,9 +1,13 @@
-﻿// <copyright file="CachedBasicPropertiesHelper.cs" company="Datadog">
+// <copyright file="CachedBasicPropertiesHelper.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
 #nullable enable
+using System;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using Datadog.Trace.DuckTyping;
 using Datadog.Trace.Util;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ;
@@ -11,13 +15,29 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.RabbitMQ;
 internal static class CachedBasicPropertiesHelper<TBasicProperties>
 {
     // ReSharper disable once StaticMemberInGenericType
-    private static readonly ActivatorHelper Activator;
+    private static readonly Func<object, object> Activator;
 
     static CachedBasicPropertiesHelper()
     {
-        Activator = new ActivatorHelper(typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.BasicProperties")!);
+        var targetType = typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.BasicProperties")!;
+        var parameterType = typeof(TBasicProperties).Assembly.GetType("RabbitMQ.Client.IReadOnlyBasicProperties")!;
+
+        var constructor = targetType.GetConstructor([parameterType])!;
+
+        var createBasicPropertiesMethod = new DynamicMethod(
+            $"TypeActivator_{targetType.Name}_{parameterType.Name}",
+            targetType,
+            [parameterType],
+            typeof(DuckType).Module,
+            true);
+
+        var il = createBasicPropertiesMethod.GetILGenerator();
+        il.Emit(OpCodes.Ldarg_0);
+        il.Emit(OpCodes.Newobj, constructor);
+        il.Emit(OpCodes.Ret);
+        Activator = (Func<object, object>)createBasicPropertiesMethod.CreateDelegate(typeof(Func<object, object>), targetType);
     }
 
-    public static TBasicProperties CreateHeaders()
-        => (TBasicProperties)Activator.CreateInstance();
+    public static TBasicProperties CreateHeaders(object readonlyBasicProperties)
+        => (TBasicProperties)Activator(readonlyBasicProperties);
 }
