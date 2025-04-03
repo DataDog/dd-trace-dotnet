5cddad1c36b7a18093b023dec08ba7eab2bfad31
Andrew Lock <andrew.lock@datadoghq.com>
2025-04-03 11:03:17 +0100

    [Version Bump] 3.14.2

--- profiler/src/ProfilerEngine/Datadog.Profiler.Native/dd_profiler_version.h
+++ profiler/src/ProfilerEngine/Datadog.Profiler.Native/dd_profiler_version.h
@@ -3,4 +3,4 @@
 
 #pragma once
 
-constexpr auto PROFILER_VERSION = "3.14.1";
+constexpr auto PROFILER_VERSION = "3.14.2";
--- shared/src/native-src/version.h
+++ shared/src/native-src/version.h
@@ -1,3 +1,3 @@
 #pragma once
 
-constexpr auto PROFILER_VERSION = "3.14.1";
+constexpr auto PROFILER_VERSION = "3.14.2";
