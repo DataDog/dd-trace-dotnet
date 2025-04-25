e03b06c96399feb1aae44aa2523cef7b7b4380cf
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2025-04-25 17:32:56 +0200

    Undo unintended change

--- tracer/src/Datadog.Trace/Ci/CodeOwners.cs
+++ tracer/src/Datadog.Trace/Ci/CodeOwners.cs
@@ -8,87 +8,197 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
+using Datadog.Trace.Util;
 
-namespace Datadog.Trace.Ci;
-
-internal class CodeOwners
+namespace Datadog.Trace.Ci
 {
-    private List<Entry> _entriesList = new List<Entry>();
-
-    public CodeOwners(string filePath)
+    internal class CodeOwners
     {
-        if (string.IsNullOrEmpty(filePath))
+        private readonly IGrouping<string?, Entry>[] _sections;
+
+        public CodeOwners(string filePath)
         {
-            throw new ArgumentException("The CODEOWNERS file path is invalid.", nameof(filePath));
-        }
-
-        _entriesList = File.ReadLines(filePath)
-            .Where(line => !string.IsNullOrWhiteSpace(line) && line[0] != '#')
-            .Select(ParseLine)
-            .Reverse()
-            .ToList();
-    }
-
-    private static Entry ParseLine(string line)
-    {
-        var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-        var pattern = parts.First();
-        var owners = parts.Skip(1).Where(p => p.Contains('@')).ToArray();
-        return new Entry(pattern, owners);
-    }
-
-    public static bool IsFileIncluded(string fileName, string pattern)
-    {
-        string regexPattern = pattern.Replace("*", @"[^/\r\n\\]*");
-
-        if (!pattern.StartsWith("/"))
-        {
-            regexPattern = ".*" + regexPattern;
-        }
-
-        if (!pattern.EndsWith("/"))
-        {
-            regexPattern += "$";
-        }
-
-        var regex = new Regex(regexPattern, RegexOptions.IgnoreCase);
-        var match = regex.Match(fileName);
-        return match.Success && match.Index == 0;
-    }
-
-    public Entry? Match(string fileName)
-    {
-        foreach (var entry in _entriesList)
-        {
-            if (IsFileIncluded(fileName, entry.Pattern))
+            if (string.IsNullOrEmpty(filePath))
             {
-                return entry; // the last matching pattern (first in the entryList) takes the most precedence.
-            }
-        }
-
-        return null;
-    }
-
-    internal readonly struct Entry
-    {
-        public readonly string Pattern;
-        public readonly IEnumerable<string> Owners;
-
-        public Entry(string pattern, IEnumerable<string> owners)
-        {
-            Pattern = pattern;
-            Owners = owners;
-        }
-
-        public string? GetOwnersString()
-        {
-            if (Owners is null || !Owners.Any())
-            {
-                return null;
+                ThrowHelper.ThrowArgumentNullException(nameof(filePath));
             }
 
-            return "[\"" + string.Join("\",\"", Owners) + "\"]";
+            var entriesList = new List<Entry>();
+            var sectionsList = new List<string>();
+            string? currentSectionName = null;
+            var atSpan = "@".AsSpan();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                if (string.IsNullOrEmpty(line) || line[0] == '#')
+                {
+                    continue;
+                }
+
+                if (line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    currentSectionName = line.Substring(1, line.Length - 2);
+                    var foundSectionName = sectionsList.FirstOrDefault(s => string.Equals(s, currentSectionName, StringComparison.OrdinalIgnoreCase));
+                    if (foundSectionName is null)
+                    {
+                        sectionsList.Add(currentSectionName);
+                    }
+
+                    currentSectionName = foundSectionName ?? currentSectionName;
+                    continue;
+                }
+
+                var finalLine = line;
+                var ownersList = new List<string>();
+                foreach (var currentTermSplitValue in line.SplitIntoSpans(' '))
+                {
+                    var currentTerm = currentTermSplitValue.AsSpan();
+                    if (currentTerm.Length == 0)
+                    {
+                        continue;
+                    }
+
+                    // Teams and users handles starts with @
+                    // Emails contains @
+                    if (currentTerm[0] == '@' || currentTerm.Contains(atSpan, StringComparison.Ordinal))
+                    {
+                        ownersList.Add(currentTerm.ToString());
+                        var pos = finalLine.AsSpan().IndexOf(currentTerm, StringComparison.Ordinal);
+                        if (pos > 0)
+                        {
+                            finalLine = finalLine.Remove(pos, currentTerm.Length);
+                        }
+                    }
+                }
+
+                finalLine = finalLine.Trim();
+                if (finalLine.Length == 0)
+                {
+                    continue;
+                }
+
+                entriesList.Add(new Entry(finalLine, ownersList.ToArray(), currentSectionName));
+            }
+
+            entriesList.Reverse();
+            _sections = entriesList.GroupBy(i => i.Section).ToArray();
+        }
+
+        public Entry? Match(string value)
+        {
+            var lstEntries = new List<Entry>();
+            foreach (var section in _sections)
+            {
+                foreach (var entry in section)
+                {
+                    var pattern = entry.Pattern;
+                    var finalPattern = pattern;
+
+                    bool includeAnythingBefore;
+                    bool includeAnythingAfter;
+
+                    if (pattern.StartsWith("/"))
+                    {
+                        includeAnythingBefore = false;
+                    }
+                    else
+                    {
+                        if (finalPattern.StartsWith("*"))
+                        {
+                            finalPattern = finalPattern.Substring(1);
+                        }
+
+                        includeAnythingBefore = true;
+                    }
+
+                    if (pattern.EndsWith("/"))
+                    {
+                        includeAnythingAfter = true;
+                    }
+                    else if (pattern.EndsWith("/*"))
+                    {
+                        includeAnythingAfter = true;
+                        finalPattern = finalPattern.Substring(0, finalPattern.Length - 1);
+                    }
+                    else
+                    {
+                        includeAnythingAfter = false;
+                    }
+
+                    if (includeAnythingAfter)
+                    {
+                        var found = includeAnythingBefore ? value.Contains(finalPattern) : value.StartsWith(finalPattern);
+                        if (!found)
+                        {
+                            continue;
+                        }
+
+                        if (!pattern.EndsWith("/*"))
+                        {
+                            lstEntries.Add(entry);
+                            break;
+                        }
+
+                        var patternEnd = value.IndexOf(finalPattern, StringComparison.Ordinal);
+                        if (patternEnd != -1)
+                        {
+                            patternEnd += finalPattern.Length;
+                            var remainingString = value.Substring(patternEnd);
+                            if (remainingString.IndexOf("/", StringComparison.Ordinal) == -1)
+                            {
+                                lstEntries.Add(entry);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (includeAnythingBefore)
+                        {
+                            if (value.EndsWith(finalPattern))
+                            {
+                                lstEntries.Add(entry);
+                                break;
+                            }
+                        }
+                        else if (value == finalPattern)
+                        {
+                            lstEntries.Add(entry);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return lstEntries.Count switch
+            {
+                0 => null,
+                1 => lstEntries[0],
+                _ => lstEntries.Aggregate((a, b) => new Entry($"{a.Pattern} | {b.Pattern}", a.Owners.Concat(b.Owners).ToArray(), $"{a.Section} | {b.Section}"))
+            };
+        }
+
+        internal readonly struct Entry
+        {
+            public readonly string Pattern;
+            public readonly IEnumerable<string> Owners;
+            public readonly string? Section;
+
+            public Entry(string pattern, IEnumerable<string> owners, string? section)
+            {
+                Pattern = pattern;
+                Owners = owners;
+                Section = section;
+            }
+
+            public string? GetOwnersString()
+            {
+                if (Owners is null || !Owners.Any())
+                {
+                    return null;
+                }
+
+                return "[\"" + string.Join("\",\"", Owners) + "\"]";
+            }
         }
     }
 }
