f8a40447131c9eef72b7513256974a2090c015f0
Mohammad Islam <mohammad.islam@datadoghq.com>
2025-04-28 15:37:36 -0400

    create hangfire sample app

---
+++ test/test-applications/integrations/Samples.Hangfire/Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenTelemetry.Resources;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using Hangfire;
+using System;
+using Hangfire.MemoryStorage;
+
+namespace Samples.Hangfire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var tracer = Sdk
+    .CreateTracerProviderBuilder()
+        .AddHangfireInstrumentation()
+    .AddConsoleExporter()
+    .Build();
+
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseMemoryStorage();
+
+            BackgroundJob.Enqueue(() => Console.WriteLine("Hello, world!"));
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
