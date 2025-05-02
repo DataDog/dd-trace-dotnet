4ad7e0d4f4a7a1a43609607fb5dc215ff6c2b7fc
Andrew Lock <andrew.lock@datadoghq.com>
2025-05-02 12:20:44 +0100

    Try fix agent writer tests

--- tracer/test/Datadog.Trace.Tests/Agent/AgentWriterTests.cs
+++ tracer/test/Datadog.Trace.Tests/Agent/AgentWriterTests.cs
@@ -4,7 +4,6 @@
 // </copyright>
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -19,16 +18,19 @@ using Datadog.Trace.Vendors.StatsdClient;
 using FluentAssertions;
 using Moq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Datadog.Trace.Tests.Agent
 {
     public class AgentWriterTests
     {
+        private readonly ITestOutputHelper _output;
         private readonly AgentWriter _agentWriter;
         private readonly Mock<IApi> _api;
 
-        public AgentWriterTests()
+        public AgentWriterTests(ITestOutputHelper output)
         {
+            _output = output;
             _api = new Mock<IApi>();
             _agentWriter = new AgentWriter(_api.Object, statsAggregator: null, statsd: null);
         }
@@ -132,13 +134,13 @@ namespace Datadog.Trace.Tests.Agent
         [Fact]
         public async Task SpanSampling_ShouldSend_MultipleMatchedSpans_WhenStatsDropsOne()
         {
-            var api = new StubApi();
+            var api = new Mock<IApi>();
             var statsAggregator = new Mock<IStatsAggregator>();
             statsAggregator.Setup(x => x.CanComputeStats).Returns(true);
             statsAggregator.Setup(x => x.ProcessTrace(It.IsAny<ArraySegment<Span>>())).Returns<ArraySegment<Span>>(x => x);
             statsAggregator.Setup(x => x.ShouldKeepTrace(It.IsAny<ArraySegment<Span>>())).Returns(false);
             var settings = SpanSamplingRule("*", "operation");
-            var agent = new AgentWriter(api, statsAggregator.Object, statsd: null, automaticFlush: false);
+            var agent = new AgentWriter(api.Object, statsAggregator.Object, statsd: null, automaticFlush: false);
             var tracer = new Tracer(settings, agent, sampler: null, scopeManager: null, statsd: null);
 
             var traceContext = new TraceContext(tracer);
@@ -168,22 +170,18 @@ namespace Datadog.Trace.Tests.Agent
 
             var traceChunk = new ArraySegment<Span>(spans, 5, 4);
             var expectedChunk = new ArraySegment<Span>(new[] { rootSpan, keptChildSpan });
-            var expectedData1 = Vendors.MessagePack.MessagePackSerializer.Serialize(new TraceChunkModel(expectedChunk, SamplingPriorityValues.UserKeep), SpanFormatterResolver.Instance);
+            var tempBuffer = new byte[8192];
+            var spanBuffer = new SpanBuffer(64 * 1024, SpanFormatterResolver.Instance);
+            spanBuffer.TryWrite(expectedChunk, ref tempBuffer, samplingPriority: SamplingPriorityValues.UserKeep).Should().Be(SpanBuffer.WriteStatus.Success);
+            spanBuffer.Lock();
+            var expectedData1 = spanBuffer.Data;
 
             agent.WriteTrace(traceChunk);
             await agent.FlushTracesAsync(); // Force a flush to make sure the trace is written to the API
 
             var expectedDroppedP0Traces = 1;
             var expectedDroppedP0Spans = 2;
-
-            api.Invocations.Should()
-               .HaveCount(1)
-               .And
-               .ContainSingle(x => x.NumberOfDroppedP0Spans == expectedDroppedP0Spans
-                                && x.ApmTracingEnabled == true
-                                && x.NumberOfTraces == 1
-                                && x.NumberOfDroppedP0Traces == expectedDroppedP0Traces
-                                && Equals(x.Traces, expectedChunk.Array));
+            api.Verify(x => x.SendTracesAsync(It.Is<ArraySegment<byte>>(y => EqualSegments(y, expectedData1)), It.Is<int>(i => i == 1), It.IsAny<bool>(), It.Is<long>(i => i == expectedDroppedP0Traces), It.Is<long>(i => i == expectedDroppedP0Spans), It.IsAny<bool>()), Times.Once);
 
             await _agentWriter.FlushAndCloseAsync();
         }
@@ -568,20 +566,24 @@ namespace Datadog.Trace.Tests.Agent
             return TracerSettings.Create(new() { { ConfigurationKeys.SpanSamplingRules, JsonConvert.SerializeObject(rules) } });
         }
 
-        private class StubApi : IApi
+        private bool EqualSegments(ArraySegment<byte> expected, ArraySegment<byte> actual)
         {
-            public ConcurrentStack<(ArraySegment<byte> Traces, int NumberOfTraces, bool StatsComputationEnabled, long NumberOfDroppedP0Traces, long NumberOfDroppedP0Spans, bool ApmTracingEnabled)> Invocations { get; } = new();
-
-            public Task<bool> SendTracesAsync(ArraySegment<byte> traces, int numberOfTraces, bool statsComputationEnabled, long numberOfDroppedP0Traces, long numberOfDroppedP0Spans, bool apmTracingEnabled = true)
+            if (expected.Count != actual.Count)
             {
-                Invocations.Push((traces, numberOfTraces, statsComputationEnabled, numberOfDroppedP0Traces, numberOfDroppedP0Spans, apmTracingEnabled));
-                return Task.FromResult(true);
+                _output.WriteLine($"Segment count mismatch, Expected {expected.Count}, Actual {actual.Count}");
+                return false;
             }
 
-            public Task<bool> SendStatsAsync(StatsBuffer stats, long bucketDuration)
+            for (var ele = 0; ele < expected.Count; ele++)
             {
-                return Task.FromResult(true);
+                if (expected.Array![expected.Offset + ele] != actual.Array![actual.Offset + ele])
+                {
+                    _output.WriteLine($"Element mismatch at entry {ele}, Expected {expected.Count}, Actual {actual.Count}");
+                    return false;
+                }
             }
+
+            return true;
         }
     }
 }
