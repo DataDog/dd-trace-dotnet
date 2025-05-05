35672d7096f8b56d7cabf73bb583e32118e13316
Zach Montoya <zach.montoya@datadoghq.com>
2025-05-05 12:34:28 -0700

    Add two new benchmark projects to measure: 1. OTEL Tracing API with the OTEL SDK (Benchmarks.OpenTelemetry.Api) 2. OTEL Tracing API with the Datadog SDK (Benchmarks.OpenTelemetry.InstrumentedApi)
    So far there are two benchmarks whose code is shared in each project (since they only reference the OTEL Tracing API):
    - TracerBenchmark for operations on the OTEL Tracer class
    - TelemetrySpanBenchmark for operations on the OTEL Span class

---
+++ tracer/test/benchmarks/Benchmarks.OpenTelemetry.InstrumentedApi/Setup/TracerBenchmarkSetup.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+
+namespace Benchmarks.OpenTelemetry.Api.Setup
+{
+    internal class TracerBenchmarkSetup
+    {
+        static void PrintSetup()
+        {
+            Console.WriteLine($"Info: COR_PROFILER={Environment.GetEnvironmentVariable("COR_PROFILER")}");
+            Console.WriteLine($"Info: COR_ENABLE_PROFILING={Environment.GetEnvironmentVariable("COR_ENABLE_PROFILING")}");
+            Console.WriteLine($"Info: COR_PROFILER_PATH={Environment.GetEnvironmentVariable("COR_PROFILER_PATH")}");
+
+            Console.WriteLine($"Info: CORECLR_PROFILER={Environment.GetEnvironmentVariable("CORECLR_PROFILER")}");
+            Console.WriteLine($"Info: CORECLR_ENABLE_PROFILING={Environment.GetEnvironmentVariable("CORECLR_ENABLE_PROFILING")}");
+            Console.WriteLine($"Info: CORECLR_PROFILER_PATH={Environment.GetEnvironmentVariable("CORECLR_ENABLE_PROFILING")}");
+
+            Console.WriteLine($"Info: DD_DOTNET_TRACER_HOME={Environment.GetEnvironmentVariable("DD_DOTNET_TRACER_HOME")}");
+            Console.WriteLine($"Info: DD_TRACE_OTEL_ENABLED={Environment.GetEnvironmentVariable("DD_TRACE_OTEL_ENABLED")}");
+
+            Console.WriteLine($"Datadog.Trace loaded = {AppDomain.CurrentDomain.GetAssemblies().Any(s => s.GetName().Name == "Datadog.Trace")}");
+        }
+
+        internal void GlobalSetup()
+        {
+            PrintSetup();
+        }
+
+        internal void GlobalCleanup()
+        {
+        }
+    }
+}
