e5890c666b202330c9e8805015f48d23f40f6758
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2025-05-09 12:13:43 +0200

    Service Bus tests. Return 13 code.

--- tracer/test/test-applications/regression/ServiceBus.Minimal.NServiceBus/Program.cs
+++ tracer/test/test-applications/regression/ServiceBus.Minimal.NServiceBus/Program.cs
@@ -17,7 +17,7 @@ namespace ServiceBus.Minimal.NServiceBus
         internal static readonly CountdownEvent Countdown = new CountdownEvent(NumMessagesToSend);
         private static readonly string EndpointName = "ServiceBus.Minimal.NServiceBus";
 
-        static async Task Main()
+        static async Task<int> Main()
         {
             var connectionString = GetSqlServerConnectionString("NsbSamplesSqlPersistence");
             var endpointConfiguration = new EndpointConfiguration(EndpointName);
@@ -30,7 +30,11 @@ namespace ServiceBus.Minimal.NServiceBus
                     return new SqlConnection(connectionString);
                 });
 
-            EnsureDatabaseExists(connectionString);
+            if (!EnsureDatabaseExists(connectionString))
+            {
+                Console.WriteLine("No SQL connection could be established. Exiting with skip code (13)");
+                return 13;
+            }
 
             var storageDirectory = Path.Combine(Path.GetTempPath(), "learningtransport");
 
@@ -47,6 +51,8 @@ namespace ServiceBus.Minimal.NServiceBus
             await endpointInstance.Stop()
                 .ConfigureAwait(false);
             Console.WriteLine("App completed successfully");
+
+            return 0;
         }
 
         static async Task SendMessagesAsync(string storageDirectory)
@@ -100,7 +106,7 @@ namespace ServiceBus.Minimal.NServiceBus
             return builder.ConnectionString;
         }
 
-        static void EnsureDatabaseExists(string connectionString)
+        static bool EnsureDatabaseExists(string connectionString)
         {
             var builder = new SqlConnectionStringBuilder(connectionString);
             var database = builder.InitialCatalog;
@@ -121,6 +127,8 @@ namespace ServiceBus.Minimal.NServiceBus
     ";
                         command.ExecuteNonQuery();
                     }
+
+                    return true;
                 }
             }
             catch (SqlException ex)
@@ -128,6 +136,8 @@ namespace ServiceBus.Minimal.NServiceBus
                 Console.WriteLine(ex);
                 Console.WriteLine($"Unable to open connection to connection string {connectionString}");
             }
+
+            return false;
         }
     }
 }
