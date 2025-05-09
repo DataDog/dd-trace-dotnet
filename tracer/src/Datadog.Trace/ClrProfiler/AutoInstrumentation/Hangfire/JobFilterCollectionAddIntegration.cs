389940bfb754ba078fc117d30bb1ce02e473ab3c
Mohammad Islam <mohammad.islam@datadoghq.com>
2025-05-09 17:08:19 -0400

    trying to inject into global filter

---
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Hangfire/JobFilterCollectionAddIntegration.cs
@@ -0,0 +1,50 @@
+// <copyright file="JobFilterCollectionAddIntegration.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using Datadog.Trace.ClrProfiler.AutoInstrumentation.Hangfire;
+using Datadog.Trace.ClrProfiler.CallTarget;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.DuckTyping;
+using Datadog.Trace.Logging;
+
+namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Hangfire;
+
+/// <summary>
+/// System.Void Hangfire.Common.JobFilterCollection::Add(System.Object) calltarget instrumentation
+/// </summary>
+[InstrumentMethod(
+    AssemblyName = "Hangfire.Core",
+    TypeName = "Hangfire.Common.JobFilterCollection",
+    MethodName = "Add",
+    ReturnTypeName = ClrNames.Void,
+    ParameterTypeNames = [ClrNames.Object],
+    MinimumVersion = "1.8.18",
+    MaximumVersion = "1.*.*",
+    IntegrationName = nameof(IntegrationId.Hangfire))]
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class JobFilterCollectionAddIntegration
+{
+    private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(JobFilterCollectionAddIntegration));
+
+    internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, ref object? filter)
+        where TTarget : IJobFilterCollectionProxy
+    {
+        Log.Debug("Did we get in? JobFilterCollectionAddIntegration.OnMethodBegin");
+        return CallTargetState.GetDefault();
+    }
+
+    internal static CallTargetReturn OnMethodEnd<TTarget>(TTarget instance, Exception? exception, in CallTargetState state)
+        where TTarget : IJobFilterCollectionProxy
+    {
+        Log.Debug("Did we get in? JobFilterCollectionAddIntegration.OnMethodEnd");
+        return CallTargetReturn.GetDefault();
+    }
+}
