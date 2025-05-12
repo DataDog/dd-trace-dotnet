2656f92198f54194b7273725ec5d1d3a7b120760
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2025-05-12 12:15:44 +0200

    Disabled sampling in appsec schema extractor in Standalone mode

--- tracer/src/Datadog.Trace/Agent/MessagePack/TraceChunkModel.cs
+++ tracer/src/Datadog.Trace/Agent/MessagePack/TraceChunkModel.cs
@@ -96,7 +96,7 @@ internal readonly struct TraceChunkModel
                 {
                     IsRunningInAzureAppService = settings.IsRunningInAzureAppService;
                     AzureAppServiceSettings = settings.AzureAppServiceMetadata ?? null;
-                    IsApmEnabled = settings.ApmTracingEnabledInternal;
+                    IsApmEnabled = settings.ApmTracingEnabled;
                 }
 
                 if (tracer.GitMetadataTagsProvider?.TryExtractGitMetadata(out var gitMetadata) == true &&
