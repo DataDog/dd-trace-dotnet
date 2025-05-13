d4606c8194f3e7da1ee4d9945d2a123db4c38f48
Andrew Lock <andrew.lock@datadoghq.com>
2025-05-13 16:19:22 +0100

    Print drive space

--- tracer/build/_build/Build.cs
+++ tracer/build/_build/Build.cs
@@ -75,7 +75,7 @@ partial class Build : NukeBuild
     readonly bool? NewIsPrerelease;
 
     [Parameter("Prints the available drive space before executing each target. Defaults to false")]
-    readonly bool PrintDriveSpace = false;
+    readonly bool PrintDriveSpace = true;
 
     [Parameter("Override the default test filters for integration tests. (Optional)")]
     readonly string Filter;
