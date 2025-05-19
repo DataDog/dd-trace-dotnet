7b0ebb64cb05eb2133965b3a2c86fac9572a7755
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2025-05-19 10:59:49 +0200

    Fix.

--- tracer/build/_build/Build.VariableGenerations.cs
+++ tracer/build/_build/Build.VariableGenerations.cs
@@ -179,19 +179,12 @@ partial class Build : NukeBuild
                             // fails on CI with error "apphost.exe" not found.
                             continue;
                         }
-
-                        foreach (var debugType in debugTypes)
-                        {
-                            foreach (var optimize in optimizations)
-                            {
-                                matrix.Add($"{targetPlatform}_{framework}",
-                                           new
-                                           {
-                                               framework = framework,
-                                               targetPlatform = targetPlatform,
-                                           });
-                            }
-                        }
+                        matrix.Add($"{targetPlatform}_{framework}",
+                                    new
+                                    {
+                                        framework = framework,
+                                        targetPlatform = targetPlatform,
+                                    });
                     }
                 }
 
