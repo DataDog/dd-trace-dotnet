340871d26487e0841e67286839c7caddfe0229a4
dudik <dudi.keleti@gmail.com>
2025-06-03 14:03:16 +0200

    Fix build

--- tracer/test/Datadog.Trace.Debugger.IntegrationTests/DynamicInstrumentationTests.cs
+++ tracer/test/Datadog.Trace.Debugger.IntegrationTests/DynamicInstrumentationTests.cs
@@ -69,7 +69,7 @@ public class DynamicInstrumentationTests : TestHelper
 
         using var agent = EnvironmentHelper.GetMockAgent();
         string processName = EnvironmentHelper.IsCoreClr() ? "dotnet" : "Samples.Probes";
-        var logPath = Path.Combine(DatadogLoggingFactory.GetLogDirectory(NullConfigurationTelemetry.Instance), nameof(LiveDebuggerTests) + "Logs");
+        var logPath = Path.Combine(DatadogLoggingFactory.GetLogDirectory(NullConfigurationTelemetry.Instance), nameof(DynamicInstrumentationTests) + "Logs");
         using var logEntryWatcher = new LogEntryWatcher($"{LogFileNamePrefix}{processName}*", logPath);
         using var sample = await StartSample(agent, $"--test-name {testType.TestType}", string.Empty, aspNetCorePort: 5000);
         await logEntryWatcher.WaitForLogEntry(DynamicInstrumentationDisabledLogEntry);
