40a4f89c6cd9b2c5852d7f54398990e181a3ec1a
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2025-06-04 13:05:44 +0200

    chore: disable data pipeline by default

--- tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
@@ -416,7 +416,7 @@ namespace Datadog.Trace.Configuration
 
             DataPipelineEnabled = config
                                   .WithKeys(ConfigurationKeys.TraceDataPipelineEnabled)
-                                  .AsBool(defaultValue: true);
+                                  .AsBool(defaultValue: false);
 
             if (DataPipelineEnabled)
             {
