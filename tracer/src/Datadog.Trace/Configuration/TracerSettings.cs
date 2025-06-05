203070ce2c50f8adbe19ff1bb9ff791502d08e0e
Munir Abdinur <munir_abdinur@hotmail.com>
2025-06-05 13:53:47 -0400

    lint

--- tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
@@ -751,22 +751,19 @@ namespace Datadog.Trace.Configuration
             telemetry.Record(ConfigTelemetryData.SsiInjectionEnabled, value: EnvironmentHelpers.GetEnvironmentVariable("DD_INJECTION_ENABLED"), recordValue: true, ConfigurationOrigins.EnvVars);
             telemetry.Record(ConfigTelemetryData.SsiAllowUnsupportedRuntimesEnabled, value: EnvironmentHelpers.GetEnvironmentVariable("DD_INJECT_FORCE"), recordValue: true, ConfigurationOrigins.EnvVars);
 
-             var installType = EnvironmentHelpers.GetEnvironmentVariable("DD_INSTRUMENTATION_INSTALL_TYPE");
-             
-             var instrumentationSource = installType switch
-             {
-                 // not sure what values you actually want for these, 
-                 // e.g. do you want to be able to distinguish between them?
-                 "dd_dotnet_launcher" => "cmd_line",
-                 "dd_trace_tool" => "cmd_line",
-                 "dotnet_msi" => "env_var",
-                 "windows_fleet_installer" => "ssi", // windows SSI on IIS
-                 _ when !string.IsNullOrEmpty(EnvironmentHelpers.GetEnvironmentVariable("DD_INJECTION_ENABLED")) => "ssi", // "normal" ssi
-                 _ => "unknown" // everything else
-             };
+            var installType = EnvironmentHelpers.GetEnvironmentVariable("DD_INSTRUMENTATION_INSTALL_TYPE");
+
+            var instrumentationSource = installType switch
+            {
+                "dd_dotnet_launcher" => "cmd_line",
+                "dd_trace_tool" => "cmd_line",
+                "dotnet_msi" => "env_var",
+                "windows_fleet_installer" => "ssi", // windows SSI on IIS
+                _ when !string.IsNullOrEmpty(EnvironmentHelpers.GetEnvironmentVariable("DD_INJECTION_ENABLED")) => "ssi", // "normal" ssi
+                _ => "unknown" // everything else
+            };
 
             telemetry.Record(ConfigTelemetryData.InstrumentationSource, instrumentationSource, recordValue: true, ConfigurationOrigins.Calculated);
-
 
             if (AzureAppServiceMetadata is not null)
             {
