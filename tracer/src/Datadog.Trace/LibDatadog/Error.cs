1e7443066312ee200957f19d69c297d08bfa6849
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2025-06-06 13:22:23 +0200

    feat(libdatadog-logger): integrate libdatadog logging

--- tracer/src/Datadog.Trace/LibDatadog/Error.cs
+++ tracer/src/Datadog.Trace/LibDatadog/Error.cs
@@ -4,6 +4,7 @@
 // </copyright>
 
 #nullable enable
+
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -16,19 +17,14 @@ namespace Datadog.Trace.LibDatadog;
 /// Libdatadog interop mapping of https://github.com/DataDog/libdatadog/blob/60583218a8de6768f67d04fcd5bc6443f67f516b/ddcommon-ffi/src/error.rs#L14
 /// </summary>
 [StructLayout(LayoutKind.Sequential)]
-internal struct Error
+internal readonly struct Error
 {
-    public VecU8 ErrorMessage;
+    internal readonly FFIVec Message;
 
-    internal static string Read(ref Error resultErr)
+    public Exception ToException()
     {
-        var message = resultErr.ErrorMessage;
-        if (message.Length == 0)
-        {
-            return string.Empty;
-        }
-
-        var errorMessage = NativeStringHelpers.ReadUtf8NativeString(resultErr.ErrorMessage);
-        return errorMessage;
+        var messageBytes = Message.ToByteArray();
+        var message = Encoding.UTF8.GetString(messageBytes);
+        return new Exception(message);
     }
 }
