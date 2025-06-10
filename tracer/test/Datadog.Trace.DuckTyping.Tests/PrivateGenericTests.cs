1775152788e2e03f3919d09b8526ae01660011d6
Andrew Lock <andrew.lock@datadoghq.com>
2025-06-10 14:19:06 +0100

    Repro test

---
+++ tracer/test/Datadog.Trace.DuckTyping.Tests/PrivateGenericTests.cs
@@ -0,0 +1,47 @@
+// <copyright file="PrivateGenericTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Generic;
+using Datadog.Trace.Util;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.DuckTyping.Tests;
+
+public class PrivateGenericTests
+{
+    public interface IDuckType
+    {
+        IDuckTypeInner Method { get; }
+    }
+
+    public interface IDuckTypeInner
+    {
+        string Value { get; }
+    }
+
+    [Fact]
+    public void CanDuckTypeInstanceReferencingPrivateTypesFromOtherAssembliesInDeepGeneric()
+    {
+        // Setting up a scenario similar to this:
+        // PublicTypeFromAssembly1<PublicTypeFromAssembly2<PrivateTypeFromAssembly3>>>
+        // Where each type is defined in a different assembly, and the inner most one is a type that should not be accessible
+
+        var instance = new TargetObject<IEnumerable<Span>>();
+        var duckType = instance.DuckCast<IDuckType>();
+        duckType.Method.Should().NotBeNull();
+        duckType.Method.Value.Should().NotBeNullOrWhiteSpace();
+    }
+
+    public class TargetObject<T>
+    {
+        private PrivateType<T> Method { get; } = new();
+
+        private class PrivateType<TInner>
+        {
+            public string Value => typeof(TInner).ToString();
+        }
+    }
+}
