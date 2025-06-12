73e35df8a51186898951c07780e4797efc3e7a92
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2025-06-12 14:05:07 +0200

    Update AspNetCore5IastTests.cs

--- tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetCore5IastTests.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetCore5IastTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="AspNetCore5IastTests.cs" company="Datadog">
+w// <copyright file="AspNetCore5IastTests.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -218,7 +218,6 @@ public class AspNetCore5IastTestsFullSamplingIastEnabled : AspNetCore5IastTestsF
         await VerifyHelper.VerifySpans(spansFiltered, settings)
                           .UseFileName(filename)
                           .DisableRequireUniquePrefix();
-        Assert.False(true);
     }
 
     [Theory]
