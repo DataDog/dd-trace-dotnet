e0bbf1f0cf5e24a331a7852614f0614f3882a5ac
Maximo Bautista <max.bautista97@gmail.com>
2025-06-12 17:47:42 -0400

    Fixing All HotChocolate snapshots but Pre_13_1_0 ones

--- tracer/test/test-applications/integrations/Samples.HotChocolate/Query.cs
+++ tracer/test/test-applications/integrations/Samples.HotChocolate/Query.cs
@@ -23,41 +23,7 @@ namespace Samples.HotChocolate
 
         public string ThrowException()
         {
-            try
-            {
-                throw new System.Exception("Something went wrong");
-            }
-            catch (Exception ex)
-            {
-                var error = ErrorBuilder
-                           .New()
-                           .SetMessage(ex.Message)
-                           .SetCode("ERROR_CODE")
-                           .SetException(ex)
-                           .Build();
-
-               var extensions = new Dictionary<string, object>
-                {
-                    { "int", 1 },
-                    { "float", 1.1f },
-                    { "str", "1" },
-                    { "bool", true },
-                    { "other", new object[] { 1, "foo" } },
-                    { "sbyte", (sbyte)-42 },
-                    { "byte", (byte)42 },
-                    { "short", (short)-1000 },
-                    { "ushort", (ushort)1000 },
-                    { "uint", (uint)4294967295 },
-                    { "long", (long)-9223372036854775808 },
-                    { "ulong", (ulong)18446744073709551615 },
-                    { "decimal", (decimal)3.1415926535897932384626433833 },
-                    { "double", 3.1415926535897932384626433833 },
-                    { "char", 'A' },
-                    { "not_captured", "This should not be captured" }
-                };
-
-                throw new GraphQLException(error.WithExtensions(extensions));
-            }
+            throw new System.Exception("Something went wrong");
         }
     }
 }
