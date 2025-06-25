652a7a69f35498d452ae8436b7dd265023f99946
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-06-25 15:44:10 -0400

    refactor ConsoleSink to use BlockingCollection<T>

--- tracer/src/Datadog.Trace/Logging/Internal/Sinks/ConsoleSink.cs
+++ tracer/src/Datadog.Trace/Logging/Internal/Sinks/ConsoleSink.cs
@@ -6,133 +6,117 @@
 #nullable enable
 
 using System;
+using System.Collections.Concurrent;
+using System.Globalization;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Datadog.Trace.Util;
 using Datadog.Trace.Vendors.Serilog.Core;
 using Datadog.Trace.Vendors.Serilog.Events;
 using Datadog.Trace.Vendors.Serilog.Formatting.Display;
 
 namespace Datadog.Trace.Logging.Internal.Sinks;
 
-/// <summary>
-/// Loosely based on https://github.com/manigandham/serilog-sinks-fastconsole
-/// </summary>
-internal class ConsoleSink : ILogEventSink, IDisposable
+internal sealed class ConsoleSink : ILogEventSink, IDisposable
 {
-    private readonly StringWriter _bufferWriter = new();
-    private readonly TextWriter _consoleWriter;
-    private readonly BoundedConcurrentQueue<LogEvent?> _writeQueue;
-    private readonly Task _writeTask;
+    // these are only used from the background thread
+    private readonly StringBuilder _bufferBuilder;
+    private readonly StringWriter _bufferWriter;
 
     private readonly MessageTemplateTextFormatter _textFormatter;
-    private readonly ManualResetEventSlim _serializationMutex = new(initialState: false);
-    private readonly TaskCompletionSource<bool> _processExit = new();
+    private readonly BlockingCollection<LogEvent> _writeQueue;
+    private readonly TextWriter _consoleWriter;
+    private readonly Task _writeTask;
 
     public ConsoleSink(string messageTemplate, int queueLimit, TextWriter? consoleWriter = null)
     {
-        _consoleWriter = consoleWriter ?? new StreamWriter(Console.OpenStandardOutput(), Console.OutputEncoding) { AutoFlush = true };
-        _textFormatter = new MessageTemplateTextFormatter(messageTemplate);
-        _writeQueue = new BoundedConcurrentQueue<LogEvent?>(queueLimit);
-        _writeTask = Task.Run(WriteToConsoleStream);
+        // these are only used from the background thread
+        _bufferBuilder = new StringBuilder(capacity: 512);
+        _bufferWriter = new StringWriter(_bufferBuilder);
+
+        _textFormatter = new MessageTemplateTextFormatter(messageTemplate, CultureInfo.InvariantCulture);
+        _writeQueue = new BlockingCollection<LogEvent>(queueLimit);
+
+        // do not use the locking textwriter from console.out used by console.writeline
+        _consoleWriter = consoleWriter ??
+                         new StreamWriter(Console.OpenStandardOutput(), Console.OutputEncoding) { AutoFlush = false };
+
+        _writeTask = Task.Factory.StartNew(
+            WriteToConsoleStream,
+            CancellationToken.None,
+            TaskCreationOptions.LongRunning | TaskCreationOptions.DenyChildAttach,
+            TaskScheduler.Default);
     }
 
-    // logs are immediately queued to channel
     public void Emit(LogEvent? logEvent)
     {
         // TODO: metrics about dropped logs?
-        if (_processExit.Task.IsCompleted)
+
+        if (logEvent is null || _writeQueue.IsAddingCompleted || _writeTask.IsCompleted)
         {
+            // if log event is null, or
+            // if we are no longer writing to the console,
+            // so don't accept any more log events
             return;
         }
 
-        if (_writeQueue.TryEnqueue(logEvent))
+        try
         {
-            if (!_serializationMutex.IsSet)
-            {
-                _serializationMutex.Set();
-            }
+            // try to add the log event to the queue
+            _writeQueue.TryAdd(logEvent);
+        }
+        catch (InvalidOperationException)
+        {
+            // queue is marked as complete, drop log event
         }
     }
 
-    private async Task WriteToConsoleStream()
+    private void WriteToConsoleStream()
     {
-        // cache reference to stringbuilder inside writer
-        var sb = _bufferWriter.GetStringBuilder();
-        var isFinalFlush = false;
-
-        while (true)
+        try
         {
-            try
+            foreach (var logEvent in _writeQueue.GetConsumingEnumerable())
             {
-                while (_writeQueue.TryDequeue(out var logEvent))
-                {
-                    // console output is an IO stream
-                    // format and write event to in-memory buffer and then flush to console async
-                    // do not use the locking textwriter from console.out used by console.writeline
-                    _textFormatter.Format(logEvent, _bufferWriter);
+                _bufferBuilder.Clear();
+
+                // format event into in-memory buffer and then flush to console
+                _textFormatter.Format(logEvent, _bufferWriter);
 
 #if NET5_0_OR_GREATER
-                    // use StringBuilder internal buffers directly without allocating a new string
-                    foreach (var chunk in sb.GetChunks())
-                    {
-                        await _consoleWriter.WriteAsync(chunk).ConfigureAwait(false);
-                    }
+                // use StringBuilder internal buffers directly without allocating a new string
+                foreach (var chunk in _bufferBuilder.GetChunks())
+                {
+                    _consoleWriter.Write(chunk.Span);
+                }
 #else
-                    // fallback to creating string output
-                    await _consoleWriter.WriteAsync(sb.ToString()).ConfigureAwait(false);
+                // fallback to creating string output
+                _consoleWriter.Write(_bufferBuilder.ToString());
 #endif
 
-                    sb.Clear();
-                }
+                _consoleWriter.Flush();
             }
-            catch (Exception)
-            {
-                // Nowhere to log safely so just swallow!
-            }
-
-            await _consoleWriter.FlushAsync().ConfigureAwait(false);
-
-            if (_processExit.Task.IsCompleted)
-            {
-                if (isFinalFlush)
-                {
-                    return;
-                }
-
-                // do one more loop to make sure everything is flushed
-                if (!_serializationMutex.IsSet)
-                {
-                    _serializationMutex.Set();
-                }
-
-                isFinalFlush = true;
-                continue;
-            }
-
-            _serializationMutex.Wait();
-            _serializationMutex.Reset();
+        }
+        catch (Exception)
+        {
+            // Nowhere to log safely so just swallow!
+        }
+        finally
+        {
+            _consoleWriter.Flush();
         }
     }
 
-    public void Dispose() => Dispose(true);
-
-    protected virtual void Dispose(bool disposing)
+    public async Task FlushAsync()
     {
-        if (!_processExit.TrySetResult(true))
-        {
-            return;
-        }
+        _writeQueue.CompleteAdding();
+        await _writeTask.ConfigureAwait(false);
+    }
 
-        if (disposing)
-        {
-            // Close write queue and wait until items are drained
-            // then wait for all console output to be flushed.
-            _writeTask.GetAwaiter().GetResult();
-
-            _bufferWriter.Dispose();
-            _consoleWriter.Dispose();
-        }
+    public void Dispose()
+    {
+        FlushAsync().GetAwaiter().GetResult();
+        _bufferWriter.Dispose();
+        _consoleWriter.Dispose();
     }
 }
