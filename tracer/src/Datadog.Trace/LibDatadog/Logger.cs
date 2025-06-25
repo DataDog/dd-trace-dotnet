c01b6f36f777ddb46dfce19e855c6f8b83aa11b9
Andrew Lock <andrew.lock@datadoghq.com>
2025-06-25 15:18:59 +0200

    Updates to libdatadog logging integration (#7084)
    ## Summary of changes
    
    Address my comments in
    https://github.com/DataDog/dd-trace-dotnet/pull/7075
    
    ## Reason for change
    
    ## Implementation details
    
    ## Test coverage
    
    ## Other details
    <!-- Fixes #{issue} -->
    
    <!-- ⚠️ Note: where possible, please obtain 2 approvals prior to
    merging. Unless CODEOWNERS specifies otherwise, for external teams it is
    typically best to have one review from a team member, and one review
    from apm-dotnet. Trivial changes do not require 2 reviews. -->

--- tracer/src/Datadog.Trace/LibDatadog/Logger.cs
+++ tracer/src/Datadog.Trace/LibDatadog/Logger.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Globalization;
 using System.IO;
-using Datadog.Trace.Configuration;
 using Datadog.Trace.Logging;
 using Datadog.Trace.Logging.Internal.Configuration;
 using Datadog.Trace.Util;
@@ -18,47 +17,35 @@ namespace Datadog.Trace.LibDatadog;
 internal sealed class Logger
 {
     private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<Logger>();
-    private static readonly Lazy<Logger> _instance = new(() => new Logger());
 
-    private readonly TracerSettings _tracerSettings;
+    private bool _loggingEnabled = false;
 
-    private Logger()
+    public static Logger Instance { get; } = new();
+
+    public void Enable(FileLoggingConfiguration fileConfig, DomainMetadata domainMetadata)
     {
-        _tracerSettings = TracerSettings.FromDefaultSourcesInternal();
-    }
-
-    public static Logger Instance => _instance.Value;
-
-    public void Enable(DatadogLoggingConfiguration config, DomainMetadata domainMetadata)
-    {
-        if (!_tracerSettings.DataPipelineEnabled)
-        {
-            return;
-        }
-
-        if (config.File is not { } fileConfig)
-        {
-            return;
-        }
+        // Rebuild the logger even if it has already been enabled, in case any configuration has changed
 
         var filePath = Path.Combine(
             fileConfig.LogDirectory,
-            $"dotnet-libdatadog-{domainMetadata.ProcessName}-{domainMetadata.ProcessId.ToString(CultureInfo.InvariantCulture)}.log");
+            $"dotnet-tracer-libdatadog-{domainMetadata.ProcessName}-{domainMetadata.ProcessId.ToString(CultureInfo.InvariantCulture)}.log");
 
         using var path = new CharSlice(filePath);
         var cfg = new FileConfig
         {
             Path = path,
-            MaxFiles = 1,
+            MaxFiles = 31, // Matches existing Serilog configuration
             MaxSizeBytes = (ulong)fileConfig.MaxLogFileSizeBytes,
         };
 
         try
         {
+            Log.Debug("Enabling libdatadog logger");
             using var error = NativeInterop.Logger.ConfigureFile(cfg);
             error.ThrowIfError();
+            _loggingEnabled = true;
         }
-        catch (Exception ex) when (ex is not LibDatadogException)
+        catch (Exception ex)
         {
             Log.Error(ex, "Failed to configure libdatadog logger");
         }
@@ -66,26 +53,37 @@ internal sealed class Logger
 
     public void Disable()
     {
-        if (!_tracerSettings.DataPipelineEnabled)
+        if (!_loggingEnabled)
         {
+            Log.Debug("Libdatadog logging is not enabled, skipping disable operation.");
             return;
         }
 
         try
         {
+            Log.Debug("Disabling libdatadog logger");
             using var error = NativeInterop.Logger.DisableFile();
             error.ThrowIfError();
+            _loggingEnabled = false;
         }
-        catch (Exception ex) when (ex is not LibDatadogException)
+        catch (Exception ex)
         {
             Log.Error(ex, "Failed to disable libdatadog logger");
         }
     }
 
-    public void SetLogLevel(Vendors.Serilog.Events.LogEventLevel logLevel)
+    public void SetLogLevel(bool debugEnabled)
     {
-        if (!_tracerSettings.DataPipelineEnabled)
+        var logLevel = debugEnabled ? Vendors.Serilog.Events.LogEventLevel.Debug : DatadogLogging.DefaultLogLevel;
+        SetLogLevel(logLevel);
+    }
+
+    private void SetLogLevel(Vendors.Serilog.Events.LogEventLevel logLevel)
+    {
+        if (!_loggingEnabled)
         {
+            // This could happen if, for example, datapipeline is enabled, and we trigger a tracer flare to change the log level.
+            Log.Information("Attempted to set libdatadog log level to {LogLevel} without enabling logging first", logLevel);
             return;
         }
 
@@ -95,7 +93,7 @@ internal sealed class Logger
             using var error = NativeInterop.Logger.SetLogLevel(level);
             error.ThrowIfError();
         }
-        catch (Exception ex) when (ex is not LibDatadogException)
+        catch (Exception ex)
         {
             Log.Error(ex, "Failed to set libdatadog log level");
         }
