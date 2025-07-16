203ffbc16d0a2d2739bea1f7d872f0facdce36f8
Mohammad Islam <mohammad.islam@datadoghq.com>
2025-07-16 16:18:24 -0400

    attempt on adding logic based on frameworks
    mac can't netcoreapp 2.1

--- tracer/test/test-applications/integrations/Samples.Hangfire/Program.cs
+++ tracer/test/test-applications/integrations/Samples.Hangfire/Program.cs
@@ -3,11 +3,15 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Hangfire;
 using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+
+#if NET5_0_OR_GREATER
+using Microsoft.Extensions.Hosting; // generic host
+#endif
 
 namespace Samples.Hangfire
 {
@@ -15,7 +19,11 @@ namespace Samples.Hangfire
     {
         public static async Task Main(string[] args)
         {
-            using var host = CreateHostBuilder(args).Build();
+#if NET5_0_OR_GREATER
+            using var host = CreateGenericHost(args).Build();
+#elif NETCOREAPP
+            using var host = CreateWebHost(args);
+#endif
             await host.StartAsync();
 
             await Should_Create_Distributed_Trace();
@@ -26,76 +34,117 @@ namespace Samples.Hangfire
             Console.ReadLine();
         }
 
-        public static void ExecuteTracedJob(string additionText)
-        {
-            Console.WriteLine("Hello from the Hangfire job! " + additionText);
-        }
+        // --------------------------------------------------
+        // Demo jobs & helpers
+        // --------------------------------------------------
+
+        public static void ExecuteTracedJob(string msg) =>
+            Console.WriteLine($"Hello from the Hangfire job! {msg}");
 
         public static async Task Should_Create_Span()
         {
             BackgroundJob.Enqueue<TestJob>(x => x.Execute());
-            await WaitJobProcessedAsync(1);
+            await Task.Delay(1000);
         }
 
         public static async Task Should_Create_Span_With_Status_Error_When_Job_Failed()
         {
             BackgroundJob.Enqueue<TestJob>(x => x.ThrowException());
-            await WaitJobProcessedAsync(1);
-        }
-
-        private static async Task WaitJobProcessedAsync(int maxSeconds)
-        {
-            await Task.Delay(1000 * maxSeconds);
+            await Task.Delay(1000);
         }
 
         public static async Task Should_Create_Distributed_Trace()
         {
             using var client = new HttpClient();
-            var response = await client.GetAsync("http://localhost:5000/trace");
-            Console.WriteLine($"Response from MVC controller: {await response.Content.ReadAsStringAsync()}");
-            await WaitJobProcessedAsync(1);
+            var r = await client.GetAsync("http://localhost:5000/trace");
+            Console.WriteLine(await r.Content.ReadAsStringAsync());
+            await Task.Delay(1000);
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        // --------------------------------------------------
+        // Host builders
+        // --------------------------------------------------
+
+#if NET5_0_OR_GREATER
+        private static IHostBuilder CreateGenericHost(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureWebHostDefaults(wb =>
                 {
-                    webBuilder.UseUrls("http://localhost:5000");
-                    webBuilder.ConfigureServices(services =>
-                    {
-                        services.AddHangfire(config =>
-                        {
-                            config.UseMemoryStorage();
-                            config.UseFilter(new AutomaticRetryAttribute
-                            {
-                                Attempts = 0,
-                                OnAttemptsExceeded = AttemptsExceededAction.Fail
-                            });
-                        });
-
-                        services.AddHangfireServer();
-                        services.AddControllers();
-                    });
-
-                    webBuilder.Configure(app =>
-                    {
-                        app.UseRouting();
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapControllers();
-                        });
-                    });
+                    wb.UseUrls("http://localhost:5000");
+                    ConfigureCommon(wb);
                 });
+#endif
+
+#if NETCOREAPP   // netcoreapp2.1 branch
+        private static IWebHost CreateWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args)
+                                 .UseUrls("http://localhost:5000");
+            ConfigureCommon(builder);
+            return builder.Build();
+        }
+#endif
+
+        // --------------------------------------------------
+        // Shared service & middleware setup
+        // --------------------------------------------------
+
+        private static void ConfigureCommon(IWebHostBuilder wb)
+        {
+            wb.ConfigureServices(services =>
+            {
+                services.AddHangfire(cfg =>
+                {
+                    cfg.UseMemoryStorage();
+                    cfg.UseFilter(new AutomaticRetryAttribute { Attempts = 0 });
+                });
+                services.AddHangfireServer();
+
+#if NET5_0_OR_GREATER
+                services.AddControllers();
+#else
+                services.AddMvc();
+#endif
+            });
+
+            wb.Configure(app =>
+            {
+#if NET5_0_OR_GREATER
+                app.UseRouting();
+                app.UseEndpoints(e => e.MapControllers());
+#else
+                app.UseMvc();
+#endif
+            });
+        }
     }
 
+    // --------------------------------------------------
+    // Controllers (per-TFM)
+    // --------------------------------------------------
+
+#if NET5_0_OR_GREATER
     [ApiController]
+    [Route("/")]
     public class TraceController : ControllerBase
     {
-        [HttpGet("/trace")]
+        [HttpGet("trace")]
         public IActionResult Trace()
         {
             BackgroundJob.Enqueue(() => Program.ExecuteTracedJob("from distributed trace"));
             return Content("Job enqueued");
         }
     }
+#else   // netcoreapp2.1
+    [Route("")]
+    public class TraceController : Controller
+    {
+        [HttpGet("trace")]
+        public IActionResult Trace()
+        {
+            BackgroundJob.Enqueue(() => Program.ExecuteTracedJob("from distributed trace"));
+            return Content("Job enqueued");
+        }
+    }
+#endif
 }
