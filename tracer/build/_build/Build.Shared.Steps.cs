bd520d500f6312d39bef99f4ae53de0890f324ba
Andrew Lock <andrew.lock@datadoghq.com>
2025-07-16 16:12:34 +0100

    Add snapshot tests to document the symbols we depend on in alpine and native loader (#7224)
    ## Summary of changes
    
    Adds more "native symbol" snapshot tests for the symbols we depend on in
    our native libraries
    
    ## Background
    
    As [part of the initial work on "universal
    binaries"](https://github.com/DataDog/dd-trace-dotnet/pull/6048) we had
    to [fix an
    issue](https://github.com/DataDog/dd-trace-dotnet/issues/6045) where we
    accidentally started depending on a new symbol, which was not always
    guaranteed to be present and could break customers. As a consequence, we
    subsequently set-up snapshot tests that [use
    `nm`](https://en.wikipedia.org/wiki/Nm_(Unix)) to dump the symbols
    required by our "native wrapper library" AKA
    `Datadog.Linux.ApiWrapper.x64.so`. We also added verification checks
    that we were building against the correct glibc version so we didn't
    accidentally change our supported distros.
    
    ## Reason for change
    
    .NET 10 [has bumped](https://github.com/dotnet/runtime/issues/109939)
    the minimum supported version of Alpine from 3.13 ([.NET
    9<](https://github.com/dotnet/core/blob/main/release-notes/9.0/supported-os.md#linux-compatibility))
    to 3.17 ([.NET
    10+](https://github.com/dotnet/core/blob/main/release-notes/10.0/supported-os.md#linux-compatibility)).
    My _previous_ understanding was that musl is essentially unversioned, so
    they're essentially also forward compatible.
    
    Unfortunately, that is apparently not true. Attempting to run .NET 10 on
    Alpine 3.13 does not work, and it specifically fails with a `symbol not
    found` error:
    
    ```
    Failed to load /usr/share/dotnet/host/fxr/10.0.0-preview.5.25277.114/libhostfxr.so, error: 
    Error relocating /usr/share/dotnet/host/fxr/10.0.0-preview.5.25277.114/libhostfxr.so: 
    _ZSt28__throw_bad_array_new_lengthv: symbol not found
    ```
    
    So this leaves us with a dilemma; we need to update to alpine 3.17 so we
    can build and test with .NET 10, but doing so _might_ end up introducing
    breaking dependencies on new symbols.
    
    This PR is about _documenting_ more fully what we actually depend on.
    Currently we have
    
    - Symbol tests for `Datadog.Linux.ApiWrapper.x64.so` on `linux-x64` and
    `linux-arm64`
    - Maximum GLibc usages verification for `linux-x64` and `linux-arm64`
    for
      - `Datadog.Tracer.Native.so`
      - `Datadog.Profiler.Native.so`
      - `libdatadog_profiling.so`
    
    This PR adds additional tests for the missing libraries and targets
    
    - Symbol tests for `Datadog.Trace.ClrProfiler.Native.so` on `linux-x64`
    and `linux-arm64`
    - Symbol tests  for `linux-musl-x64` and `linux-musl-arm64` for
      - `Datadog.Tracer.Native.so`
      - `Datadog.Profiler.Native.so`
      - `libdatadog_profiling.so`
    
    > Note that `Datadog.Linux.ApiWrapper.x64.so` and
    `Datadog.Trace.ClrProfiler.Native.so` are "universal" binaries, so the
    same binary is used for both Glibc and musl.
    
    ## Implementation details
    
    - Extracted the existing symbol test to a reusable function (1st commit,
    feel free to skip).
    - Call the reusable function in the missing scenarios.
    - Run the tests and save the snapshots.
    
    ## Test coverage
    
    More now! 
    
    Note that I'm not really sure how much value there is in documenting the
    `libdatadog_profiling.so` symbols. It's not something we control in this
    repository and it means needing to update the snapshot when something
    changes. I've included it now to be safe, but we can always address
    later.
    
    ## Other details
    
    Investigating the alpine:3.17 changes is out of scope for this PR, I'll
    investigate that separately.
    
    The one notably not-documented native library here is ddwaf. Should we
    document the symbols in that one too?

--- tracer/build/_build/Build.Shared.Steps.cs
+++ tracer/build/_build/Build.Shared.Steps.cs
@@ -4,8 +4,10 @@ using Nuke.Common;
 using Nuke.Common.IO;
 using System.Linq;
 using System.IO;
+using DiffMatchPatch;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Utilities;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
@@ -136,6 +138,19 @@ partial class Build
 
             var testExe = ToolResolver.GetLocalTool(exePath);
             testExe($"--gtest_output=xml:{testsResultFile}", workingDirectory: workingDirectory);
+        });
+
+    Target ValidateNativeLoaderSnapshotTestsLinux => _ => _
+        .Unlisted()
+        .After(CompileNativeLoaderLinux)
+        .Before(ExtractDebugInfoLinux)
+        .OnlyWhenStatic(() => IsLinux)
+        .Executes(() =>
+        {
+            // Compare the symbols in the native loader with the snapshot
+            var libraryPath = NativeLoaderProject.Directory / "bin" / $"{NativeLoaderProject.Name}.so";
+            var snapshotName = $"native-loader-symbols-{UnixArchitectureIdentifier}";
+            CompareNativeSymbolsSnapshot(libraryPath, snapshotName);
         });
 
     Target CompileNativeLoaderOsx => _ => _
@@ -289,6 +304,11 @@ partial class Build
                 : new Version(2, 17);
 
             ValidateNativeLibraryGlibcCompatibility(dest, expectedGlibcVersion);
+
+            if (IsAlpine)
+            {
+                CompareNativeSymbolsSnapshot(dest, $"native-tracer-symbols-alpine-{UnixArchitectureIdentifier}");
+            }
         });
 
     void ValidateNativeLibraryGlibcCompatibility(AbsolutePath libraryPath, Version expectedGlibcVersion, IEnumerable<string> allowedSymbols = null)
@@ -336,5 +356,81 @@ partial class Build
               .Where(x => x.Contains("@GLIBC_") && allowedSymbols?.Any(y => x.Contains(y)) != true)
               .Select(x => System.Version.Parse(x.Substring(x.IndexOf("@GLIBC_") + 7)))
               .Max();
+    }
+
+    void CompareNativeSymbolsSnapshot(AbsolutePath libraryPath, string snapshotNamePrefix)
+    {
+        var output = Nm.Value($"-D {libraryPath}").Select(x => x.Text).ToList();
+
+        // Gives output similar to this:
+        // 0000000000006bc8 D DdDotnetFolder
+        // 0000000000006bd0 D DdDotnetMuslFolder
+        //                  w _ITM_deregisterTMCloneTable
+        //                  w _ITM_registerTMCloneTable
+        //                  w __cxa_finalize
+        //                  w __deregister_frame_info
+        //                  U __errno_location
+        //                  U __tls_get_addr
+        // 0000000000002d1b T _fini
+        // 0000000000002d18 T _init
+        // 0000000000003d70 T accept
+        // 0000000000003e30 T accept4
+        //                  U access
+        //
+        // The types of symbols are:
+        // D: Data section symbol. These symbols are initialized global variables.
+        // w: Weak symbol. These symbols are weakly referenced and can be overridden by other symbols.
+        // U: Undefined symbol. These symbols are referenced in the file but defined elsewhere.
+        // T: Text section symbol. These symbols are functions or executable code.
+        // B: BSS (Block Started by Symbol) section symbol. These symbols are uninitialized global variables.
+        //
+        // We only care about the Undefined symbols - we don't want to accidentally add more of them
+
+        Logger.Debug("NM output: {Output}", string.Join(Environment.NewLine, output));
+
+        var symbols = output
+                     .Select(x => x.Trim())
+                     .Where(x => x.StartsWith("U "))
+                     .Select(x => x.TrimStart("U "))
+                     .OrderBy(x => x)
+                     .ToList();
+
+
+        var received = string.Join(Environment.NewLine, symbols);
+        var verifiedPath = TestsDirectory / "snapshots" / $"{snapshotNamePrefix}.verified.txt";
+        var verified = File.Exists(verifiedPath)
+                           ? File.ReadAllText(verifiedPath)
+                           : string.Empty;
+
+        var libraryName = Path.GetFileNameWithoutExtension(libraryPath);
+        Logger.Information("Comparing snapshot of Undefined symbols in the {LibraryName} using {Path}...", libraryName, verifiedPath);
+
+        var dmp = new diff_match_patch();
+        var diff = dmp.diff_main(verified, received);
+        dmp.diff_cleanupSemantic(diff);
+
+        var changedSymbols = diff
+                            .Where(x => x.operation != Operation.EQUAL)
+                            .Select(x => x.text.Trim())
+                            .ToList();
+
+        if (changedSymbols.Count == 0)
+        {
+            Logger.Information("No changes found in Undefined symbols in {LibraryName}", libraryName);
+            return;
+        }
+
+        // Print the expected values, so it's easier to copy-paste them into the snapshot file as required
+        Logger.Information("Received snapshot for {LibraryName}{Break}{Symbols}", libraryName, Environment.NewLine, received);
+
+        Logger.Information("Changed symbols for {LibraryName}:", libraryName);
+
+        PrintDiff(diff);
+
+        throw new Exception($"Found differences in undefined symbols in {libraryName}. {Environment.NewLine} These are shown above as both a diff and the" +
+                            "full expected snapshot. Verify that these changes are expected, and will not cause problems. " +
+                            "Removing symbols is generally a safe operation, but adding them could cause crashes. " +
+                            $"If the new symbols are safe to add, update the snapshot file at {verifiedPath} with the " +
+                            "new values");
     }
 }
