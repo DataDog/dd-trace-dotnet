eac46da4e429e68e15b7172c56c57139b40fd225
Andrew Lock <andrew.lock@datadoghq.com>
2025-07-21 17:52:08 +0100

    Fix execution benchmarks (#7248)
    ## Summary of changes
    
    - Ensure we fail the job if the execution benchmark scenario is marked
    as a failure
    - Update the thresholds
    - Typo fixes etc 
    
    ## Reason for change
    
    #7221 bumped the timeit version to 0.4.1 which added a feature for
    checking the overhead threshold. Unfortunately, one of the scenarios has
    been failing ever since, but we didn't notice until checking the weekly
    report, because it was marking the test as a failure, but _wasn't_
    failing the job.
    
    ## Implementation details
    
    - Track failures for a job (look for non-zero exit codes)
    - Update the thresholds based on 
    ```
    ((max instrumented time in last 3 months/min baseline time in last 3 months) -1) * 100
    ```
    
    i.e. the threshold is set to the current theoretical worst overhead for
    each scenario
    - Added missing .NET 8 scenario
    
    ## Test coverage
    
    This is the test (but I confirmed that the job fails with low thresholds
    [in a dedicated
    run](https://dev.azure.com/datadoghq/dd-trace-dotnet/_build/results?buildId=183061&view=logs&j=2f1275c9-cccc-5708-e01b-b4f9c775a244&t=b5d8615e-110e-5c2e-5786-e9af31add95b))
    
    ## Other details
    
    Also did a bunch of fixes [in the benchmark
    dashboard](https://app.datadoghq.com/dashboard/gf2-auj-wve?fromUser=true&p=1&refresh_mode=sliding&from_ts=1752492314226&to_ts=1753097114226&live=true),
    including
    - adding support for .NET 8
    - Fixing the display of .NET FX
    - Using "corrected" duration instead of raw duration for .NET core
    (subtracts the overhead of the timeit startup hook)

--- tracer/build/timeit/Samples.FakeDbCommand/run.cmd
+++ tracer/build/timeit/Samples.FakeDbCommand/run.cmd
@@ -1,25 +1,47 @@
 @echo off
+setlocal enabledelayedexpansion
+
 IF EXIST results_Samples.FakeDbCommand.windows.net48.json DEL /F results_Samples.FakeDbCommand.windows.net48.json
 IF EXIST results_Samples.FakeDbCommand.windows.netcoreapp31.json DEL /F results_Samples.FakeDbCommand.windows.netcoreapp31.json
 IF EXIST results_Samples.FakeDbCommand.windows.net60.json DEL /F results_Samples.FakeDbCommand.windows.net60.json
 IF EXIST results_Samples.FakeDbCommand.windows.net80.json DEL /F results_Samples.FakeDbCommand.windows.net80.json
 
+set FAILED=0
+
 echo *********************
 echo Installing timeitsharp
 echo *********************
 dotnet tool update -g timeitsharp --version 0.4.3
+IF ERRORLEVEL 1 set FAILED=1
 
 echo *********************
-echo .NET Framework 4.6.1
+echo .NET Framework 4.8
 echo *********************
 dotnet timeit Samples.FakeDbCommand.windows.net48.json
+IF ERRORLEVEL 1 set FAILED=1
 
 echo *********************
 echo .NET Core 3.1
 echo *********************
 dotnet timeit Samples.FakeDbCommand.windows.netcoreapp31.json
+IF ERRORLEVEL 1 set FAILED=1
 
 echo *********************
 echo .NET Core 6.0
 echo *********************
 dotnet timeit Samples.FakeDbCommand.windows.net60.json
+IF ERRORLEVEL 1 set FAILED=1
+
+echo *********************
+echo .NET Core .0
+echo *********************
+dotnet timeit Samples.FakeDbCommand.windows.net80.json
+IF ERRORLEVEL 1 set FAILED=1
+
+IF "!FAILED!"=="1" (
+    echo One or more benchmarks failed.
+    exit /b 1
+)
+
+echo All benchmarks completed successfully.
+exit /b 0
