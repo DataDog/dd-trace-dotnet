54f877754bf84152c703f64cf8305b17a6c473b8
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-07-24 19:07:14 -0400

    revert change (bad merge?)

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AzureFunctionsTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AzureFunctionsTests.cs
@@ -317,7 +317,7 @@ public abstract class AzureFunctionsTests : TestHelper
 
                 // There are _additional_ spans created for these compared to the non-AspNetCore version
                 // These are http-client-handler-type: System.Net.Http.SocketsHttpHandler that come in around
-                // the same time as some of the `azure-functions.invoke` spans
+                // the same time as some of the `azure_functions.invoke` spans
                 // because of this they cause a lot of flake in the snapshots where they shift places
                 // opting to just scrub them from the snapshots - we also don't think that the spans provide much
                 // value so they may be removed from being traced.
