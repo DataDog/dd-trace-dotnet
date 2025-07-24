a908aafe4c52148137d80e28a66e439db4e5f0e5
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2025-07-24 15:45:27 +0200

    fix

--- tracer/test/Datadog.Trace.Tools.Runner.Tests/CoverageRewriteTests.cs
+++ tracer/test/Datadog.Trace.Tools.Runner.Tests/CoverageRewriteTests.cs
@@ -2,16 +2,3 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Xml;
-using Datadog.Trace.Coverage.Collector;
-using ICSharpCode.Decompiler;
-using ICSharpCode.Decompiler.CSharp;
-using VerifyTests;
-using VerifyXunit;
-using Xunit;
