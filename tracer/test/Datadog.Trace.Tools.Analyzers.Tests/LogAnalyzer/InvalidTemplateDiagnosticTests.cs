c9ab95b737b22bc1e61a4de7f19a089d5fb701a1
Andrew Lock <andrew.lock@datadoghq.com>
2025-07-24 11:51:03 +0100

    Update Datadog.Trace.Tools.Analyzers.Tests to use the newer library-agnostic test libraries

--- tracer/test/Datadog.Trace.Tools.Analyzers.Tests/LogAnalyzer/InvalidTemplateDiagnosticTests.cs
+++ tracer/test/Datadog.Trace.Tools.Analyzers.Tests/LogAnalyzer/InvalidTemplateDiagnosticTests.cs
@@ -9,8 +9,9 @@ using Datadog.Trace.Tools.Analyzers.LogAnalyzer;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Testing;
 using Xunit;
-using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
-    Datadog.Trace.Tools.Analyzers.LogAnalyzer.LogAnalyzer>;
+using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<
+    Datadog.Trace.Tools.Analyzers.LogAnalyzer.LogAnalyzer,
+    Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
 
 namespace Datadog.Trace.Tools.Analyzers.Tests.LogAnalyzer;
 
