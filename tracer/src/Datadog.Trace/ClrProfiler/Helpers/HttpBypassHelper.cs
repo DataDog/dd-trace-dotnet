b794363f2534e35208a7a1a93b9dc69064474b87
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-07-24 22:14:36 -0400

    remove null checks, inline past path

--- tracer/src/Datadog.Trace/ClrProfiler/Helpers/HttpBypassHelper.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/Helpers/HttpBypassHelper.cs
@@ -6,19 +6,22 @@
 #nullable enable
 
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Datadog.Trace.ClrProfiler.Helpers
 {
     internal static class HttpBypassHelper
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool UriContainsAnyOf(Uri requestUri, string[] substrings)
         {
-            if (requestUri == null! || substrings == null! || substrings.Length == 0)
-            {
-                return false;
-            }
+            return substrings.Length > 0 && UriContainsAnyOfSlow(requestUri, substrings);
+        }
 
+        private static bool UriContainsAnyOfSlow(Uri requestUri, string[] substrings)
+        {
             var uriString = requestUri.ToString().ToUpperInvariant();
+
             for (var index = 0; index < substrings.Length; index++)
             {
                 if (uriString.Contains(substrings[index]))
