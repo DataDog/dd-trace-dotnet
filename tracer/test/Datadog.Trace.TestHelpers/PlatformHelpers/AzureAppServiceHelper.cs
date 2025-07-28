d0d795bd1aa817e41d9ce555a2747c1eb3d5f642
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-07-28 19:31:01 -0400

    [Azure Functions] Fixes, refactoring, and clean-up (#7256)
    ## Summary of changes
    
    Fix issues that affect Azure Functions when not using the AAS Site
    extension. Refactor and clean up code used to detect serverless
    environments.
    
    ## Reason for change
    
    Improve support for Azure Functions instrumented with the
    `Datadog.AzureFunctions` nuget package instead of the AAS Site
    Extension.
    
    ## Implementation details
    
    - Distinguish between
      - running in Azure App Services
      - running in Azure Functions
      - using the AAS Site Extension
    - Move Azure Functions configuration keys from
    `ConfigurationKeys.AzureAppService.cs` to new
    `ConfigurationKeys.AzureFunctions.cs` and add a few more
    - In Azure Functions that _don't_ use the AAS Site extension:
      - Fix the default service name (use the Functions App name)
      - Fix the logging directory
    - Fix missing `component` tag (old bug)
    - More refactoring and clean-up and adding comments to make things
    easier for my future self 😅
    
    ## Test coverage
    
    - Fixed tests that relied on env vars added by the AAS Site Extension to
    determine if running in Azure App Services.
    - Add new tests.
    
    ## Other details
    Fixes APMSVLS-118
    
    (AAS = Azure App Services)
    
    <!-- ⚠️ Note: where possible, please obtain 2 approvals prior to
    merging. Unless CODEOWNERS specifies otherwise, for external teams it is
    typically best to have one review from a team member, and one review
    from apm-dotnet. Trivial changes do not require 2 reviews. -->

--- tracer/test/Datadog.Trace.TestHelpers/PlatformHelpers/AzureAppServiceHelper.cs
+++ tracer/test/Datadog.Trace.TestHelpers/PlatformHelpers/AzureAppServiceHelper.cs
@@ -5,13 +5,37 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Runtime.InteropServices;
 using Datadog.Trace.Configuration;
 
-namespace Datadog.Trace.Tests.PlatformHelpers;
+namespace Datadog.Trace.TestHelpers.PlatformHelpers;
 
 public static class AzureAppServiceHelper
 {
+    public static IConfigurationSource CreateMinimalAzureAppServiceConfiguration(string siteName)
+    {
+        var dict = new Dictionary<string, string>
+        {
+            { "WEBSITE_SITE_NAME", siteName }
+        };
+
+        return new DictionaryConfigurationSource(dict);
+    }
+
+    public static IConfigurationSource CreateMinimalAzureFunctionsConfiguration(string siteName, string functionsWorkerRuntime, string functionsExtensionVersion)
+    {
+        var dict = new Dictionary<string, string>
+        {
+            { "WEBSITE_SITE_NAME", siteName },
+            { "FUNCTIONS_WORKER_RUNTIME", "dotnet-isolated" },
+            { "FUNCTIONS_EXTENSION_VERSION", "dotnet-isolated" }
+        };
+
+        return new DictionaryConfigurationSource(dict);
+    }
+
     public static IConfigurationSource GetRequiredAasConfigurationValues(
         string subscriptionId,
         string deploymentId,
@@ -46,24 +70,25 @@ public static class AzureAppServiceHelper
         if (addContextKey)
         {
             vars.Add(ConfigurationKeys.AzureAppService.AzureAppServicesContextKey, "1");
+            vars.Add(ConfigurationKeys.AzureAppService.SiteExtensionVersionKey, "3.0.0");
         }
 
         vars.Add(ConfigurationKeys.AzureAppService.WebsiteOwnerNameKey, $"{subscriptionId}+{planResourceGroup}-EastUSwebspace");
         vars.Add(ConfigurationKeys.AzureAppService.ResourceGroupKey, siteResourceGroup);
         vars.Add(ConfigurationKeys.AzureAppService.SiteNameKey, deploymentId);
-        vars.Add(ConfigurationKeys.AzureAppService.OperatingSystemKey, "windows");
+        vars.Add(ConfigurationKeys.AzureAppService.OperatingSystemKey, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "windows" : "linux");
         vars.Add(ConfigurationKeys.AzureAppService.InstanceIdKey, "instance_id");
         vars.Add(ConfigurationKeys.AzureAppService.InstanceNameKey, "instance_name");
         vars.Add(ConfigurationKeys.DebugEnabled, ddTraceDebug);
 
         if (functionsVersion != null)
         {
-            vars.Add(ConfigurationKeys.AzureAppService.FunctionsExtensionVersionKey, functionsVersion);
+            vars.Add(ConfigurationKeys.AzureFunctions.FunctionsExtensionVersion, functionsVersion);
         }
 
         if (functionsRuntime != null)
         {
-            vars.Add(ConfigurationKeys.AzureAppService.FunctionsWorkerRuntimeKey, functionsRuntime);
+            vars.Add(ConfigurationKeys.AzureFunctions.FunctionsWorkerRuntime, functionsRuntime);
         }
 
         vars.Add(ConfigurationKeys.AzureAppService.AasEnableCustomTracing, enableCustomTracing ?? "false");
