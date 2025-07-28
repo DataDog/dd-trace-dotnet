83d945f27d58d4b4efaa76fcaec2cb09a19a6411
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2025-07-28 18:21:40 +0200

    Fix compilation error

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/InstrumentationTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/InstrumentationTests.cs
@@ -500,7 +500,7 @@ namespace Foo
                                "tags": ["injection_forced:true"]
                              }]
                              """;
-            AssertHasExpectedTelemetry(logFileName, processResult, pointsJson);
+            AssertHasExpectedTelemetry(logFileName, processResult, pointsJson, "success", ".NET 10 or higher", "success_forced");
         }
 
         [SkippableFact]
@@ -533,7 +533,7 @@ namespace Foo
                                "name": "library_entrypoint.abort.runtime"
                              }]
                              """;
-            AssertHasExpectedTelemetry(logFileName, processResult, pointsJson);
+            AssertHasExpectedTelemetry(logFileName, processResult, pointsJson, "abort", ".NET 10 or higher", "incompatible_runtime");
         }
 
         [SkippableFact]
