f49d3f63d1ae50e23f721a727b1ed8f662b1e76e
Tony Redondo <tony.redondo@datadoghq.com>
2025-07-29 16:38:05 +0200

    changes

--- profiler/build/run-cpu-walltime.cmd
+++ profiler/build/run-cpu-walltime.cmd
@@ -2,12 +2,6 @@
 
 call install_timeit.cmd
 
-:: remove DOTNET_ROOT environment variable to ensure we can run
-:: the benchmark in x64 and x86
-::dotnet --info
-::echo %DOTNET_ROOT%
-::set DOTNET_ROOT=
-
 :: Run x64
 dotnet timeit CpuWallTime.windows.json --variable arch=x64
 
