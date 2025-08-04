35b5889aec25fe3d78823ec31f403026dfe7feeb
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2025-08-04 11:30:46 +0200

    Add latest supported waf capabilities to RC

--- tracer/src/Datadog.Trace/RemoteConfigurationManagement/RcmCapabilitiesIndices.cs
+++ tracer/src/Datadog.Trace/RemoteConfigurationManagement/RcmCapabilitiesIndices.cs
@@ -85,6 +85,10 @@ namespace Datadog.Trace.RemoteConfigurationManagement
 
         public static readonly BigInteger AsmRaspCmd = Create(37);
 
+        public static readonly BigInteger AsmDdMulticonfig = Create(42);
+
+        public static readonly BigInteger AsmTraceTaggingRules = Create(43);
+
         private static BigInteger Create(int index) => new(1UL << index);
     }
 }
