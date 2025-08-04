cd329cc112947c796508d85c89cafc9d8edda415
Tony Redondo <tony.redondo@datadoghq.com>
2025-08-04 10:01:00 +0200

    changes

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/TestCaseMetadata.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Testing/XUnit/TestCaseMetadata.cs
@@ -42,8 +42,8 @@ internal class TestCaseMetadata
     public bool AllAttemptsPassed { get; set; } = true;
 
     public bool AllRetriesFailed { get; set; } = true;
-    
-    public bool Skipped { get; set; } = false;
+
+    public bool Skipped { get; set; }
 
     public string UniqueID { get; }
 }
