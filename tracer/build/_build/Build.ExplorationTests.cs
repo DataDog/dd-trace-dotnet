1bb9a2c6805d404a7d14edffe875ab1fac0112fc
Rachel Yang <rachel.yang@datadoghq.com>
2025-08-07 13:03:20 -0400

    exploration tests env var

--- tracer/build/_build/Build.ExplorationTests.cs
+++ tracer/build/_build/Build.ExplorationTests.cs
@@ -167,6 +167,8 @@ partial class Build
             ["DD_TRACE_LOG_DIRECTORY"] = TestLogsDirectory,
             ["DD_SERVICE"] = "exploration_tests",
             ["DD_VERSION"] = Version
+            // Disable logs injection for exploration tests to avoid interfering with third-party test expectations
+            ["DD_LOGS_INJECTION"] = "false"
         };
 
         switch (ExplorationTestUseCase)
