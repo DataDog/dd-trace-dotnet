2d9c6c6f3f4de81fe24581725d7ecede08a4d7b8
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2025-08-19 12:28:26 +0200

    [IAST] Fix NRE in Native IAST ModuleInfo->GetFullName() (#7404)
    ## Summary of changes
    Avoid calling ModuleInfo::GetFullName functions in Debug traces without
    a IsDebugEnabled guard
    
    ## Reason for change
    
    ## Implementation details
    
    ## Test coverage
    
    ## Other details
    <!-- Fixes #{issue} -->
    
    
    <!--  ⚠️ Note:
    
    Where possible, please obtain 2 approvals prior to merging. Unless
    CODEOWNERS specifies otherwise, for external teams it is typically best
    to have one review from a team member, and one review from apm-dotnet.
    Trivial changes do not require 2 reviews.
    
    MergeQueue is NOT enabled in this repository. If you have write access
    to the repo, the PR has 1-2 approvals (see above), and all of the
    required checks have passed, you can use the Squash and Merge button to
    merge the PR. If you don't have write access, or you need help, reach
    out in the #apm-dotnet channel in Slack.
    -->

--- tracer/src/Datadog.Tracer.Native/iast/module_info.cpp
+++ tracer/src/Datadog.Tracer.Native/iast/module_info.cpp
@@ -71,7 +71,7 @@ bool ModuleInfo::IsCoreLib()
 WSTRING ModuleInfo::GetModuleFullName()
 {
     std::stringstream res;
-    res << "(" << Hex((ULONG)_id) << ") " << shared::ToString(_name) << " [" << shared::ToString(_appDomain.Name) << "]  on "
+    res << "(" << Hex((ULONG)_id) << ") " << shared::ToString(_name) << " [" << shared::ToString(_appDomain.Name) << "]  on " 
         << shared::ToString(_path);
     return ToWSTRING(res.str());
 }
@@ -821,7 +821,7 @@ mdToken ModuleInfo::DefineMemberRef(const WSTRING& moduleName, const WSTRING& ty
         moduleInfo = GetModuleInfoByName(moduleName);
         if (!moduleInfo)
         {
-            trace::Logger::Info("Module ", moduleName, " NOT FOUND for ", GetModuleFullName());
+            trace::Logger::Info("Module ", moduleName, " NOT FOUND");
             return 0;
         }
         mdAssemblyRef assemblyRef;
@@ -864,8 +864,11 @@ mdToken ModuleInfo::DefineMemberRef(const WSTRING& moduleName, const WSTRING& ty
     {
         _mMemberImports[memberKey] = methodRef;
         auto memberRefInfo = GetMemberRefInfo(methodRef);
-        trace::Logger::Debug("DefineMemberRef : ", Hex(methodRef), " for ", memberKey,
-                             " MethodName: ", memberRefInfo->GetFullName(), " Module: ", GetModuleFullName());
+        if (trace::Logger::IsDebugEnabled())
+        {
+            trace::Logger::Debug("DefineMemberRef : ", Hex(methodRef), " for ", memberKey,
+                                 " MethodName: ", memberRefInfo->GetFullName(), " Module: ", GetModuleFullName());
+        }
         return methodRef;
     }
     else
