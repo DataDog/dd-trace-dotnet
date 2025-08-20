50d19a4f20f4e34af256c147155a8b06cb49c105
Andrew Lock <andrew.lock@datadoghq.com>
2025-08-20 17:44:55 +0100

    Fix NaN

--- tracer/build/_build/LogParsing/NativeFunctionMetrics.cs
+++ tracer/build/_build/LogParsing/NativeFunctionMetrics.cs
@@ -5,6 +5,6 @@ namespace LogParsing;
 public record NativeFunctionMetrics(DateTimeOffset Timestamp, string Name, double TimeMs, int? Count)
 {
     public const string TotalInitializationTime = "__total_initialization_time__";
-    public double MeanDurationMs => Count.HasValue ? TimeMs / Count.Value : 0;
+    public double MeanDurationMs => Count is > 0 ? TimeMs / Count.Value : 0;
     public static bool IsTotalName(string name) => name == TotalInitializationTime;
 }
