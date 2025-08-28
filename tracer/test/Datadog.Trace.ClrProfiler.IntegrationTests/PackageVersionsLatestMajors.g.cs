76ddf9f9b48c0e0ccd8f06dfa0073060eab2be0e
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2025-08-28 16:44:00 +0200

    Service Bus integration tests
    # Conflicts:
    #	tracer/dependabot/Datadog.Dependabot.Integrations.csproj

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestMajors.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestMajors.g.cs
@@ -2451,6 +2451,43 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 #endif
             };
 
+      public static IEnumerable<object[]> AzureServiceBusAPM =>
+
+            new List<object[]>
+            {
+#if DEFAULT_SAMPLES
+                new object[] { string.Empty },
+#else
+#if NETCOREAPP2_1
+                new object[] { "7.20.1" },
+#endif
+#if NETCOREAPP3_0
+                new object[] { "7.20.1" },
+#endif
+#if NETCOREAPP3_1
+                new object[] { "7.20.1" },
+#endif
+#if NET5_0
+                new object[] { "7.20.1" },
+#endif
+#if NET6_0
+                new object[] { "7.20.1" },
+#endif
+#if NET7_0
+                new object[] { "7.20.1" },
+#endif
+#if NET8_0
+                new object[] { "7.20.1" },
+#endif
+#if NET9_0
+                new object[] { "7.20.1" },
+#endif
+#if NET10_0
+                new object[] { "7.20.1" },
+#endif
+#endif
+            };
+
       public static IEnumerable<object[]> Selenium =>
 
             new List<object[]>
