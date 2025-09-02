42561e4f0081556471f2e7d49dc09d086171f1ee
Raphaël Vandon <raphael.vandon@datadoghq.com>
2025-09-02 14:30:12 +0200

    remove meaningless comments

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AdoNet/BatchExecuteAsyncIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AdoNet/BatchExecuteAsyncIntegration.cs
@@ -21,28 +21,11 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet;
 public class BatchExecuteAsyncIntegration
 {
 #if NET6_0_OR_GREATER
-    /// <summary>
-    /// OnMethodBegin callback
-    /// </summary>
-    /// <typeparam name="TTarget">Type of the target</typeparam>
-    /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
-    /// <param name="cancellationToken">CancellationToken value</param>
-    /// <returns>Calltarget state value</returns>
     internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, CancellationToken cancellationToken)
     {
         return new CallTargetState(DbScopeFactory.Cache<TTarget>.CreateDbBatchScope(Tracer.Instance, (DbBatch)(object)instance!));
     }
 
-    /// <summary>
-    /// OnAsyncMethodEnd callback
-    /// </summary>
-    /// <typeparam name="TTarget">Type of the target</typeparam>
-    /// <typeparam name="TReturn">Type of the return value, in an async scenario will be T of Task of T</typeparam>
-    /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
-    /// <param name="returnValue">Return value instance</param>
-    /// <param name="exception">Exception instance in case the original code threw an exception.</param>
-    /// <param name="state">Calltarget state value</param>
-    /// <returns>A response value, in an async scenario will be T of Task of T</returns>
     internal static TReturn OnAsyncMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception exception, in CallTargetState state)
     {
         state.Scope.DisposeWithException(exception);
--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AdoNet/BatchExecuteIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AdoNet/BatchExecuteIntegration.cs
@@ -20,27 +20,11 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AdoNet;
 public class BatchExecuteIntegration
 {
 #if NET6_0_OR_GREATER
-    /// <summary>
-    /// OnMethodBegin callback
-    /// </summary>
-    /// <typeparam name="TTarget">Type of the target</typeparam>
-    /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
-    /// <returns>Calltarget state value</returns>
     internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance)
     {
         return new CallTargetState(DbScopeFactory.Cache<TTarget>.CreateDbBatchScope(Tracer.Instance, (DbBatch)(object)instance!));
     }
 
-    /// <summary>
-    /// OnMethodEnd callback
-    /// </summary>
-    /// <typeparam name="TTarget">Type of the target</typeparam>
-    /// <typeparam name="TReturn">Type of the return value</typeparam>
-    /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
-    /// <param name="returnValue">Task of HttpResponse message instance</param>
-    /// <param name="exception">Exception instance in case the original code threw an exception.</param>
-    /// <param name="state">Calltarget state value</param>
-    /// <returns>A response value, in an async scenario will be T of Task of T</returns>
     internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception exception, in CallTargetState state)
     {
         state.Scope.DisposeWithException(exception);
