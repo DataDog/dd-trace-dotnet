6baf47efb56362d5e8e4fd1cbb712e977d80dc1d
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2025-09-04 10:26:00 +0200

    [Native] Convert Rejitters vector to a fixed array (#7460)
    ## Summary of changes
    After converting the creation of Dataflow to dynamic, when adding it to
    the rejiters vector, this could cause a race condition when it grows.
    So, in order to avoid this, the rejitters vector has been converted to a
    fixed array.
    
    As there are 3 known rejitters, ATM, the size of the array has been set
    to 4 to have a minimal buffer.
    
    ## Reason for change
    Previous vector reserve implementation could be unsafe if a new rejitter
    was added without modifying the reserved size. This way if a new one is
    added without modifying the max count, an exception will be thrown
    
    ## Implementation details
    Replace the previous rejitters vector with a fixed array.
    
    ## Test coverage
    Unit tests have been added for the array insert function
    
    ## Other details
    <!-- Fixes #{issue} -->
    
    
    <!--  ⚠️ Note:
    
    Where possible, please obtain 2 approvals prior to merging. Unless
    CODEOWNERS specifies otherwise, for external teams it is typically best
    to have one review from a team member, and one review from apm-dotnet.
    Trivial changes do not require 2 reviews.
    
    MergeQueue is NOT enabled in this repository. If you have write access
    to the repo, the PR has 1-2 approvals (see above), and all of the
    required checks have passed, you can use the Squash and Merge button to
    merge the PR. If you don't have write access, or you need help, reach
    out in the #apm-dotnet channel in Slack.
    -->

--- tracer/src/Datadog.Tracer.Native/iast/dataflow.cpp
+++ tracer/src/Datadog.Tracer.Native/iast/dataflow.cpp
@@ -160,7 +160,7 @@ AspectFilter* ModuleAspects::GetFilter(DataflowAspectFilterValue filterValue)
 
 Dataflow::Dataflow(ICorProfilerInfo* profiler, std::shared_ptr<RejitHandler> rejitHandler,
                    const RuntimeInformation& runtimeInfo) :
-    Rejitter(rejitHandler, RejitterPriority::Low)
+    Rejitter(rejitHandler, RejitterPriority::Low, false)
 {
     m_runtimeType = runtimeInfo.runtime_type;
     m_runtimeVersion = VersionInfo{runtimeInfo.major_version, runtimeInfo.minor_version, runtimeInfo.build_version, 0};
@@ -180,6 +180,7 @@ Dataflow::Dataflow(ICorProfilerInfo* profiler, std::shared_ptr<RejitHandler> rej
     }
 
     _initialized = false;
+    rejitHandler->RegisterRejitter(this);
 }
 
 Dataflow::~Dataflow()
@@ -198,8 +199,8 @@ void Dataflow::LoadAspects(WCHAR** aspects, int aspectsLength, UINT32 enabledCat
 
     if (aspectsLength > 10)
     {
-        _aspectClasses.reserve(aspectsLength / 10);
-        _aspects.reserve(aspectsLength);
+        _aspects.reserve(aspectsLength); // We know the max number of aspects we are going to have, so reserve the space to avoid vector resizes
+        _aspectClasses.reserve(aspectsLength / 10); // We don't know exactly the number of aspects which are class aspects, but 1/10 is a fine approach
     }
 
     DataflowAspectClass* aspectClass = nullptr;
