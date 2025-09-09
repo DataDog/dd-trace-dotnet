600168270b16e78b5a204cbcdaa14fc19dd5812e
Andrew Lock <andrew.lock@datadoghq.com>
2025-09-09 16:51:21 +0100

    Remove existing native-lib folder

--- shared/src/native-lib/dd-policy-engine/c/src/test/test_main.c
+++
@@ -1,3 +0,0 @@
-#include "utest/utest.h"
-
-UTEST_MAIN()
--- shared/src/native-lib/dd-policy-engine/go/examples/example_json_to_hardcoded_injector_policies/converter/converter.go
+++
@@ -1,7 +0,0 @@
-package converter
-
-import flatbuffers "github.com/google/flatbuffers/go"
-
-type Converter interface {
-	ConvertToWLS(builder *flatbuffers.Builder) (flatbuffers.UOffsetT, error)
-}
