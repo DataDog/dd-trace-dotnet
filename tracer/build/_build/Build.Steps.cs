a6079cd4ad58b8758e9adef4498b74636db7ca7c
NachoEchevarria <53266532+NachoEchevarria@users.noreply.github.com>
2025-09-11 10:54:14 +0200

    Decrease log level when uncompressing files in Nuke (#7496)
    ## Summary of changes
    
    We are getting our logs flooded by thousand of lines when uncompressing
    files in our Nuke tasks. We display a line for every directory created
    when uncompressing. This creates thousand of useless lines in the task
    DownloadLibDatadog, which floods the output, so we stop capturing logs
    in some gitlab tasks and missing some information. Since we don't need
    these lines, getting ride of them would help us to get the log
    information that we need.
    
    The problem is that this is done by the Nuke library when calling the
    method CompressionTasks.UncompressZip. In order to fix that, we have
    created our own version of that method, which is a copy of the Nuke one
    without all the unnecessary logging.
    
    ## Reason for change
    
    ## Implementation details
    
    ## Test coverage
    
    ## Other details
    <!-- Fixes #{issue} -->
    
    
    <!--  ⚠️ Note:
    
    Where possible, please obtain 2 approvals prior to merging. Unless
    CODEOWNERS specifies otherwise, for external teams it is typically best
    to have one review from a team member, and one review from apm-dotnet.
    Trivial changes do not require 2 reviews.
    
    MergeQueue is NOT enabled in this repository. If you have write access
    to the repo, the PR has 1-2 approvals (see above), and all of the
    required checks have passed, you can use the Squash and Merge button to
    merge the PR. If you don't have write access, or you need help, reach
    out in the #apm-dotnet channel in Slack.
    -->
    
    ---------
    
    Co-authored-by: Lucas Pimentel <lucas.pimentel@datadoghq.com>

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -10,6 +10,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using CodeGenerators;
+using ICSharpCode.SharpZipLib.Zip;
 using LogParsing;
 using Mono.Cecil;
 using Nuke.Common;
@@ -546,7 +547,7 @@ partial class Build
         uncompressFolderTarget ??= LibDdwafDirectory(libddwafVersion);
         Console.WriteLine($"{libDdwafZip} downloaded. Extracting to {uncompressFolderTarget}...");
 
-        UncompressZip(libDdwafZip, uncompressFolderTarget);
+        UncompressZipQuiet(libDdwafZip, uncompressFolderTarget);
     }
 
     Target CopyLibDdwaf => _ => _
@@ -2777,9 +2778,34 @@ partial class Build
         EnsureExistingParentDirectory(destinationFolder);
         var parentFolder = destinationFolder.Parent;
 
-        CompressionTasks.UncompressZip(vcpkgZip, parentFolder);
+        UncompressZipQuiet(vcpkgZip, parentFolder);
 
         RenameDirectory(parentFolder / $"vcpkg-{vcpkgVersion}", destinationFolder.Name);
+    }
+
+    // Quiet version of CompressionTasks:UncompressZip, which displays a log line for every created directory when uncompressing
+    private static void UncompressZipQuiet(string archiveFile, string directory)
+    {
+        Logger.Information("Uncompressing {File} to {Directory} ...", Path.GetFileName(archiveFile), directory);
+
+        using var fileStream = File.OpenRead(archiveFile);
+        using var zipFile = new ZipFile(fileStream);
+
+        var entries = zipFile.Cast<ZipEntry>().Where(x => !x.IsDirectory);
+        foreach (var entry in entries)
+        {
+            var file = PathConstruction.Combine(directory, entry.Name);
+            var path = Path.GetDirectoryName(file);
+
+            if (!Directory.Exists(path) && !string.IsNullOrEmpty(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+            using var entryStream = zipFile.GetInputStream(entry);
+            using var outputStream = File.Open(file, FileMode.Create);
+            entryStream.CopyTo(outputStream);
+        }
     }
 
     public static class LibdatadogLogParser
