4953afbd9f4d24e1438ad1ed57a0186d1e34754c
Andrew Lock <andrew.lock@datadoghq.com>
2025-09-16 10:40:23 +0100

    Fix trying to remove the timeit logs

--- tracer/build/timeit/remove_logs.cmd
+++ tracer/build/timeit/remove_logs.cmd
@@ -1,1 +1,11 @@
-del /S/q "%ProgramData%\Datadog .NET Tracer\logs\*.*"
+@echo off
+REM Recursively walk through all files under the Datadog .NET Tracer logs folder
+for /R "%ProgramData%\Datadog .NET Tracer\logs" %%F in (*.*) do (
+
+    REM Check if the filename (%%~nxF = name + extension) contains "timeit"
+    REM The `find /I` command searches case-insensitively
+    echo %%~nxF | find /I "timeit" >nul
+
+    REM If "timeit" is NOT found (errorlevel=1), delete the file
+    if errorlevel 1 del /Q "%%F"
+)
