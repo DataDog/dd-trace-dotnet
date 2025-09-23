9dd4764ec344138ebeb2e3edec52af1e6cdc960f
Damien MEHALA <damien.mehala@datadoghq.com>
2025-09-23 15:48:49 +0200

    [skip ci] fix: update process discovery FFI

---
+++ tracer/src/Datadog.Trace/LibDatadog/ServiceDiscovery/MetadataKind.cs
@@ -0,0 +1,22 @@
+// <copyright file="MetadataKind.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+using System;
+
+namespace Datadog.Trace.LibDatadog.ServiceDiscovery;
+
+internal enum MetadataKind : int
+{
+    RuntimeId = 0,
+    TracerLanguage = 1,
+    TracerVersion = 2,
+    Hostname = 3,
+    ServiceName = 4,
+    ServiceEnvironment = 5,
+    ServiceVersion = 6,
+    ProcessTags = 7,
+    ContainerId = 8,
+}
