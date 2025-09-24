f40d1fb56b62c10a74d96846142cdfb3d0806cc8
Anna <anna.yafi@datadoghq.com>
2025-09-24 18:40:02 +0200

    Adapt platform keys usage in tracer

--- tracer/src/Datadog.Trace/Configuration/ImmutableGCPFunctionSettings.cs
+++ tracer/src/Datadog.Trace/Configuration/ImmutableGCPFunctionSettings.cs
@@ -22,12 +22,12 @@ namespace Datadog.Trace.Configuration
             source ??= NullConfigurationSource.Instance;
             var config = new ConfigurationBuilder(source, telemetry);
 
-            var deprecatedFunctionKey = config.WithKeys(ConfigurationKeys.GCPFunction.DeprecatedFunctionNameKey).AsString();
-            var deprecatedProjectKey = config.WithKeys(ConfigurationKeys.GCPFunction.DeprecatedProjectKey).AsString();
+            var deprecatedFunctionKey = config.WithKeys(PlatformKeys.GCPFunction.DeprecatedFunctionNameKey).AsString();
+            var deprecatedProjectKey = config.WithKeys(PlatformKeys.GCPFunction.DeprecatedProjectKey).AsString();
             IsDeprecatedFunction = deprecatedFunctionKey != null && deprecatedProjectKey != null;
 
-            var functionNameKey = config.WithKeys(ConfigurationKeys.GCPFunction.FunctionNameKey).AsString();
-            var functionTargetKey = config.WithKeys(ConfigurationKeys.GCPFunction.FunctionTargetKey).AsString();
+            var functionNameKey = config.WithKeys(PlatformKeys.GCPFunction.FunctionNameKey).AsString();
+            var functionTargetKey = config.WithKeys(PlatformKeys.GCPFunction.FunctionTargetKey).AsString();
             IsNewerFunction = functionNameKey != null && functionTargetKey != null;
 
             IsGCPFunction = IsDeprecatedFunction || IsNewerFunction;
