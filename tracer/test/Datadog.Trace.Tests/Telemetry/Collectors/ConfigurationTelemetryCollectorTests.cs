d72acce51610b0ba031e2a7f7c56649f17d383b0
Anna <anna.yafi@datadoghq.com>
2025-09-24 18:40:13 +0200

    Adapt platform key usage in tests

--- tracer/test/Datadog.Trace.Tests/Telemetry/Collectors/ConfigurationTelemetryCollectorTests.cs
+++ tracer/test/Datadog.Trace.Tests/Telemetry/Collectors/ConfigurationTelemetryCollectorTests.cs
@@ -133,9 +133,9 @@ public class ConfigurationTelemetryCollectorTests
         var config = new NameValueCollection
         {
             { ConfigurationKeys.AzureAppService.SiteExtensionVersionKey, "1.5.0" },
-            { ConfigurationKeys.AzureAppService.SiteNameKey, "site-name" },
-            { ConfigurationKeys.AzureFunctions.FunctionsExtensionVersion, "~4" },
-            { ConfigurationKeys.AzureFunctions.FunctionsWorkerRuntime, "dotnet-isolated" },
+            { PlatformKeys.AzureAppService.SiteNameKey, "site-name" },
+            { PlatformKeys.AzureFunctions.FunctionsExtensionVersion, "~4" },
+            { PlatformKeys.AzureFunctions.FunctionsWorkerRuntime, "dotnet-isolated" },
             { ConfigurationKeys.ServiceName, serviceName },
             { ConfigurationKeys.Environment, env },
             { ConfigurationKeys.ServiceVersion, serviceVersion },
@@ -152,9 +152,9 @@ public class ConfigurationTelemetryCollectorTests
 
         using var scope = new AssertionScope();
         GetLatestValueFromConfig(data, ConfigurationKeys.AzureAppService.SiteExtensionVersionKey).Should().Be("1.5.0");
-        GetLatestValueFromConfig(data, ConfigurationKeys.AzureAppService.SiteNameKey).Should().Be("site-name");
-        GetLatestValueFromConfig(data, ConfigurationKeys.AzureFunctions.FunctionsExtensionVersion).Should().Be("~4");
-        GetLatestValueFromConfig(data, ConfigurationKeys.AzureFunctions.FunctionsWorkerRuntime).Should().Be("dotnet-isolated");
+        GetLatestValueFromConfig(data, PlatformKeys.AzureAppService.SiteNameKey).Should().Be("site-name");
+        GetLatestValueFromConfig(data, PlatformKeys.AzureFunctions.FunctionsExtensionVersion).Should().Be("~4");
+        GetLatestValueFromConfig(data, PlatformKeys.AzureFunctions.FunctionsWorkerRuntime).Should().Be("dotnet-isolated");
         GetLatestValueFromConfig(data, ConfigTelemetryData.AasConfigurationError).Should().BeOfType<bool>().Subject.Should().Be(!isSafeToTrace);
         GetLatestValueFromConfig(data, ConfigTelemetryData.CloudHosting).Should().Be("Azure");
         GetLatestValueFromConfig(data, ConfigTelemetryData.AasAppType).Should().Be("function");
