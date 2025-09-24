1f2e1857d28e22373ac04463b4df68f7b536bb57
dudik <dudi.keleti@gmail.com>
2025-09-24 19:51:48 +0200

    make the sorting more understandable and add early exit

--- tracer/src/Datadog.Trace/Configuration/ApmTracingConfigMerger.cs
+++ tracer/src/Datadog.Trace/Configuration/ApmTracingConfigMerger.cs
@@ -7,7 +7,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Datadog.Trace.Logging;
 using Datadog.Trace.RemoteConfigurationManagement;
@@ -67,16 +66,92 @@ namespace Datadog.Trace.Configuration
                 return "{\"lib_config\":{}}";
             }
 
-            applicableConfigs.Sort((a, b) =>
-            {
-                var priorityComparison = b.Priority.CompareTo(a.Priority); // Descending
-                return priorityComparison != 0 ? priorityComparison : string.Compare(a.ConfigId, b.ConfigId, StringComparison.Ordinal); // Ascending
-            });
+            // Sort configs by priority (highest first), then by config ID (alphabetically) for deterministic ordering
+            applicableConfigs.Sort(CompareConfigsByPriority);
 
-            var mergedConfig = applicableConfigs.Aggregate((current, next) => current.MergeWith(next));
+            // Merge all configs in priority order, with higher priority configs overriding lower priority ones
+            var mergedLibConfig = MergeConfigsInPriorityOrder(applicableConfigs);
 
-            var result = new { lib_config = mergedConfig.LibConfig };
+            var result = new { lib_config = mergedLibConfig };
             return JsonConvert.SerializeObject(result);
+        }
+
+        /// <summary>
+        /// Compares two configs by priority (highest first), then by config ID (alphabetically)
+        /// </summary>
+        private static int CompareConfigsByPriority(ApmTracingConfig a, ApmTracingConfig b)
+        {
+            var priorityComparison = b.Priority.CompareTo(a.Priority); // Descending
+            return priorityComparison != 0 ? priorityComparison : string.Compare(a.ConfigId, b.ConfigId, StringComparison.Ordinal); // Ascending
+        }
+
+        /// <summary>
+        /// Merges configs in priority order, leveraging the fact that they're already sorted.
+        /// Stops early when all LibConfig properties are populated (no further merging needed).
+        /// </summary>
+        private static LibConfig MergeConfigsInPriorityOrder(List<ApmTracingConfig> sortedConfigs)
+        {
+            if (sortedConfigs.Count == 1)
+            {
+                return sortedConfigs[0].LibConfig;
+            }
+
+            // Since configs are already sorted, start with the highest priority
+            var result = sortedConfigs[0].LibConfig;
+            for (int i = 1; i < sortedConfigs.Count; i++)
+            {
+                result = MergeLibConfigs(result, sortedConfigs[i].LibConfig);
+
+                // Early exit if all LibConfig properties are populated
+                if (IsLibConfigComplete(result))
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks if all LibConfig properties are populated (non-null)
+        /// </summary>
+        private static bool IsLibConfigComplete(LibConfig libConfig)
+        {
+            return libConfig is
+            {
+                TracingEnabled: not null,
+                LogInjectionEnabled: not null,
+                TracingSamplingRate: not null,
+                TracingSamplingRules: not null,
+                TracingHeaderTags: not null,
+                TracingTags: not null,
+                DebugEnabled: not null,
+                RuntimeMetricsEnabled: not null,
+                ServiceMapping: not null,
+                DataStreamsEnabled: not null,
+                SpanSamplingRules: not null
+            };
+        }
+
+        /// <summary>
+        /// Merges LibConfig objects, with higher priority config taking precedence
+        /// </summary>
+        private static LibConfig MergeLibConfigs(LibConfig higher, LibConfig lower)
+        {
+            return new LibConfig
+            {
+                TracingEnabled = higher.TracingEnabled ?? lower.TracingEnabled,
+                LogInjectionEnabled = higher.LogInjectionEnabled ?? lower.LogInjectionEnabled,
+                TracingSamplingRate = higher.TracingSamplingRate ?? lower.TracingSamplingRate,
+                TracingSamplingRules = higher.TracingSamplingRules ?? lower.TracingSamplingRules,
+                TracingHeaderTags = higher.TracingHeaderTags ?? lower.TracingHeaderTags,
+                TracingTags = higher.TracingTags ?? lower.TracingTags,
+                DebugEnabled = higher.DebugEnabled ?? lower.DebugEnabled,
+                RuntimeMetricsEnabled = higher.RuntimeMetricsEnabled ?? lower.RuntimeMetricsEnabled,
+                ServiceMapping = higher.ServiceMapping ?? lower.ServiceMapping,
+                DataStreamsEnabled = higher.DataStreamsEnabled ?? lower.DataStreamsEnabled,
+                SpanSamplingRules = higher.SpanSamplingRules ?? lower.SpanSamplingRules,
+            };
         }
 
         /// <summary>
