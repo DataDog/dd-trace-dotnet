DataDog/dd-trace-dotnet

commit ed3fd248dfb5cc856d027c11504a721b9db7d440

Refactor the profiling configuration reading (#7303)
## Summary of changes

Refactors how we determine the profiler's current configuration state

## Reason for change

Today, the profiler determines if it's enabled or disabled independently
of the tracer, in the native code. We then try to mirror that state in
the managed code, for several reasons

- To know whether we need to send app-domain information to the profiler
(e.g. service name, env)
- To know whether we need to send thread-context-change events to the
profiler (for CodeHotspots/EndpointProfiling)
- For telemetry reasons
- For recording the state in DSM payloads

As part of the hands-off config work, we will calculate whether to
enable the profiler based on the _full_ managed config, instead of just
using environment variables. However, when managed activation is
disabled (for back-compat investigation purposes), we _do_ need to just
use env vars, to "mirror" the state that will be calculated by the
native continuous profiler, essentially working in the existing "legacy"
state.

The refactoring in this PR is intended to lay the groundwork for the
future hands-off config work in the profiler, as well as to solve some
existing issues with inconsistent/missing telemetry

## Implementation details

- Add a `ProfilerState` `enum` to track the profiler as a 3-state
enablement which represents its real state, instead of trying to force
it as a boolean
- Use `EnvironmentConfigurationSource` to record variables read from env
in telemetry correctly.
- Add support for the `DD_PROFILER_MANAGED_ACTIVATION_ENABLED` which
will be used shortly

## Test coverage

Added unit tests for the new `ProfilerSettings` which encapsulates most
of the logic

## Other details

This PR technically requires support for
`DD_PROFILER_MANAGED_ACTIVATION_ENABLED` has been merged in order to
report the correct values. This isn't a big deal, as it will likely be
merged soon, but if it isn't, we should tweak the logic in

tracer/src/Datadog.Trace/ContinuousProfiler/ProfilerSettings.cs
@@ -1,0 +1,106 @@
+// <copyright file="ProfilerSettings.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Configuration.ConfigurationSources.Telemetry;
+using Datadog.Trace.Configuration.Telemetry;
+
+namespace Datadog.Trace.ContinuousProfiler;
+
+internal class ProfilerSettings
+{
+    public ProfilerSettings(IConfigurationSource config, IConfigurationTelemetry telemetry)
+        : this(config, new EnvironmentConfigurationSource(), telemetry)
+    {
+    }
+
+    // Internal for testing only
+    internal ProfilerSettings(IConfigurationSource config, IConfigurationSource envConfig, IConfigurationTelemetry telemetry)
+    {
+        if (!IsProfilingSupported)
+        {
+            ProfilerState = ProfilerState.Disabled;
+            telemetry.Record(ConfigurationKeys.ProfilingEnabled, false, ConfigurationOrigins.Calculated);
+            return;
+        }
+
+        // If managed activation is enabled, we need to _just_ read from the environment variables,
+        // as that's all that applies
+        var envConfigBuilder = new ConfigurationBuilder(envConfig, telemetry);
+        var managedActivationEnabled = envConfigBuilder
+                                      .WithKeys(ConfigurationKeys.ProfilerManagedActivationEnabled)
+                                      .AsBool(true);
+
+        // If we're using managed activation, we use the "full" config source set.
+        // Otherwise we only read from the environment variables, to "match" the behavior of the profiler
+        var profilingConfig = managedActivationEnabled
+                                  ? new ConfigurationBuilder(config, telemetry)
+                                  : envConfigBuilder;
+
+        // With SSI, beyond ContinuousProfiler.ConfigurationKeys.ProfilingEnabled (true or auto vs false),
+        // the profiler could be enabled via ContinuousProfiler.ConfigurationKeys.SsiDeployed. If it is non-empty, then the
+        // profiler is "active", though won't begin profiling until 30 seconds have passed + at least 1 span has been generated.
+        var profilingEnabled = profilingConfig
+                              .WithKeys(ConfigurationKeys.ProfilingEnabled)
+                               // We stick with strings here instead of using the `GetAs` method,
+                               // so that telemetry continues to store true/false/auto, instead of the enum values.
+                              .AsString(
+                                   converter: x => x switch
+                                   {
+                                       "auto" => "auto",
+                                       _ when x.ToBoolean() is { } boolean => boolean ? "true" : "false",
+                                       _ => ParsingResult<string>.Failure(),
+                                   },
+                                   getDefaultValue: () =>
+                                   {
+                                       // If there's no explicit `DD_PROFILING_ENABLED` key,
+                                       // we set the state based on the SSI value, only checking env vars (not the full stack)
+                                       var isSsiDeployment = envConfigBuilder
+                                                            .WithKeys(ConfigurationKeys.SsiDeployed)
+                                                            .AsString();
+
+                                       return isSsiDeployment switch
+                                       {
+                                           { Length: > 0 } => "auto",
+                                           _ => "false",
+                                       };
+                                   },
+                                   validator: null);
+
+        ProfilerState = profilingEnabled switch
+        {
+            "auto" => ProfilerState.Auto,
+            "true" => ProfilerState.Enabled,
+            _ => ProfilerState.Disabled,
+        };
+    }
+
+    // Internal for testing only
+    internal ProfilerSettings(ProfilerState state)
+    {
+        ProfilerState = state;
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the profiler is supported on this platform at all.
+    /// If it's not supported, we should not try to P/Invoke to it or do any context tracking.
+    /// </summary>
+    public static bool IsProfilingSupported
+    {
+        get
+        {
+            var fd = FrameworkDescription.Instance;
+            return
+                (fd.OSPlatform == OSPlatformName.Windows && fd.ProcessArchitecture is ProcessArchitecture.X64 or ProcessArchitecture.X86) ||
+                (fd.OSPlatform == OSPlatformName.Linux && fd.ProcessArchitecture is ProcessArchitecture.X64);
+        }
+    }
+
+    public ProfilerState ProfilerState { get; }
+
+    public bool IsProfilerEnabled => ProfilerState != ProfilerState.Disabled;
+}

tracer/test/Datadog.Trace.Tests/ContinuousProfiler/ProfilerSettingsTests.cs
@@ -1,0 +1,155 @@
+// <copyright file="ProfilerSettingsTests.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Configuration.Telemetry;
+using Datadog.Trace.ContinuousProfiler;
+using Datadog.Trace.TestHelpers;
+using FluentAssertions;
+using Xunit;
+
+namespace Datadog.Trace.Tests.ContinuousProfiler;
+
+public class ProfilerSettingsTests : SettingsTestsBase
+{
+    // profiling takes precedence over SSI
+    // "auto" is a special profiling value that enables profiling when deployed via SSI
+    // the profiler will also be enabled when "profiler" will be added to the DD_INJECTION_ENABLED environment variable
+    [Theory]
+    [InlineData("1", null, (int)ProfilerState.Enabled)]
+    [InlineData("0", null, (int)ProfilerState.Disabled)]
+    [InlineData("true", null, (int)ProfilerState.Enabled)]
+    [InlineData("false", null, (int)ProfilerState.Disabled)]
+    [InlineData("auto", null, (int)ProfilerState.Auto)]
+    [InlineData("1", "not used", (int)ProfilerState.Enabled)]
+    [InlineData("0", "not used", (int)ProfilerState.Disabled)]
+    [InlineData("true", "not used", (int)ProfilerState.Enabled)]
+    [InlineData("false", "not used", (int)ProfilerState.Disabled)]
+    [InlineData("auto", "not used", (int)ProfilerState.Auto)]
+    [InlineData("invalid", "foo, profiler, bar", (int)ProfilerState.Auto)]
+    [InlineData("invalid", "profiler", (int)ProfilerState.Auto)]
+    [InlineData("invalid", "anything else", (int)ProfilerState.Auto)]
+    [InlineData("invalid", "", (int)ProfilerState.Disabled)]
+    [InlineData("invalid", null, (int)ProfilerState.Disabled)]
+    [InlineData("", "foo, profiler, bar", (int)ProfilerState.Auto)]
+    [InlineData("", "profiler", (int)ProfilerState.Auto)]
+    [InlineData("", "anything else", (int)ProfilerState.Auto)]
+    [InlineData("", "", (int)ProfilerState.Disabled)]
+    [InlineData("", null, (int)ProfilerState.Disabled)]
+    [InlineData(null, "foo, profiler, bar", (int)ProfilerState.Auto)]
+    [InlineData(null, "profiler", (int)ProfilerState.Auto)]
+    [InlineData(null, "anything else", (int)ProfilerState.Auto)]
+    [InlineData(null, null, (int)ProfilerState.Disabled)]
+    [InlineData(null, "", (int)ProfilerState.Disabled)]
+    public void ProfilerState_WhenPassedViaEnvironment(string profilingValue, string ssiValue, int expected)
+    {
+        var values = new List<(string, string)>();
+        if (profilingValue is not null)
+        {
+            values.Add((Datadog.Trace.ContinuousProfiler.ConfigurationKeys.ProfilingEnabled, profilingValue));
+        }
+
+        if (ssiValue is not null)
+        {
+            values.Add((Datadog.Trace.ContinuousProfiler.ConfigurationKeys.SsiDeployed, ssiValue));
+        }
+
+        var source = CreateConfigurationSource(values.ToArray());
+        var settings = new ProfilerSettings(source, source, NullConfigurationTelemetry.Instance);
+
+        // Whether the profiler is supported at all overrides the expected profiler state
+        var expectedState = ProfilerSettings.IsProfilingSupported ? (ProfilerState)expected : ProfilerState.Disabled;
+        settings.ProfilerState.Should().Be(expectedState);
+    }
+
+    [Theory]
+    [InlineData("1", null, (int)ProfilerState.Enabled)]
+    [InlineData("0", null, (int)ProfilerState.Disabled)]
+    [InlineData("auto", null, (int)ProfilerState.Auto)]
+    [InlineData(null, "1", (int)ProfilerState.Disabled)]
+    [InlineData(null, "0", (int)ProfilerState.Disabled)]
+    [InlineData(null, "auto", (int)ProfilerState.Disabled)]
+    public void ProfilerState_WhenManagedActivationIsMissingOrOnByDefault_DontReadFromOnlyEnvVars(string configProfilingEnabled, string envProfilingEnabled, int expected)
+    {
+        var envValues = new List<(string, string)>();
+        if (envProfilingEnabled is not null)
+        {
+            envValues.Add((Datadog.Trace.ContinuousProfiler.ConfigurationKeys.ProfilingEnabled, envProfilingEnabled));
+        }
+
+        var otherValues = new List<(string, string)>();
+        if (configProfilingEnabled is not null)
+        {
+            otherValues.Add((Datadog.Trace.ContinuousProfiler.ConfigurationKeys.ProfilingEnabled, configProfilingEnabled));
+        }
+
+        var envConfig = CreateConfigurationSource(envValues.ToArray());
+        var otherConfig = CreateConfigurationSource(otherValues.ToArray());
+
+        // When profiler managed activation is enabled, the profiler state should be read from all config
+        var settings = new ProfilerSettings(otherConfig, envConfig, NullConfigurationTelemetry.Instance);
+
+        // Whether the profiler is supported at all overrides the expected profiler state
+        var expectedState = ProfilerSettings.IsProfilingSupported ? (ProfilerState)expected : ProfilerState.Disabled;
+        settings.ProfilerState.Should().Be(expectedState);
+    }
+
+    [Theory]
+    [InlineData("1", null, (int)ProfilerState.Disabled)]
+    [InlineData("0", null, (int)ProfilerState.Disabled)]
+    [InlineData("auto", null, (int)ProfilerState.Disabled)]
+    [InlineData(null, "1", (int)ProfilerState.Enabled)]
+    [InlineData(null, "0", (int)ProfilerState.Disabled)]
+    [InlineData(null, "auto", (int)ProfilerState.Auto)]
+    public void ProfilerState_WhenManagedActivationIsDisabled_OnlyReadsFromEnvVars(string configProfilingEnabled, string envProfilingEnabled, int expected)
+    {
+        var envValues = new List<(string, string)>
+        {
+            (Datadog.Trace.ContinuousProfiler.ConfigurationKeys.ProfilerManagedActivationEnabled, "0")
+        };
+
+        if (envProfilingEnabled is not null)
+        {
+            envValues.Add((Datadog.Trace.ContinuousProfiler.ConfigurationKeys.ProfilingEnabled, envProfilingEnabled));
+        }
+
+        var otherValues = new List<(string, string)>();
+        if (configProfilingEnabled is not null)
+        {
+            otherValues.Add((Datadog.Trace.ContinuousProfiler.ConfigurationKeys.ProfilingEnabled, configProfilingEnabled));
+        }
+
+        var envConfig = CreateConfigurationSource(envValues.ToArray());
+        var otherConfig = CreateConfigurationSource(otherValues.ToArray());
+
+        // When profiler managed activation is disabled, the profiler state should only be read from the environment variables
+        var settings = new ProfilerSettings(otherConfig, envConfig, NullConfigurationTelemetry.Instance);
+
+        // Whether the profiler is supported at all overrides the expected profiler state
+        var expectedState = ProfilerSettings.IsProfilingSupported ? (ProfilerState)expected : ProfilerState.Disabled;
+        settings.ProfilerState.Should().Be(expectedState);
+    }
+
+    [Fact]
+    public void ProfilerState_IsProfilingSupported_OnlySupportedOnExpectedPlatforms()
+    {
+        // get the current runtime and platform
+        var arch = RuntimeInformation.OSArchitecture;
+        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        var isSupported = (arch, isWindows, isLinux) switch
+        {
+            (Architecture.X64, true, _) => true, // Windows x64
+            (Architecture.X86, true, _) => true, // Windows x86
+            (Architecture.X64, _, true) => true, // Linux x64
+            _ => false // Unsupported platforms
+        };
+
+        ProfilerSettings.IsProfilingSupported.Should().Be(isSupported);
+    }
+}

tracer/src/Datadog.Trace/Configuration/TracerSettings.cs
@@ -20,6 +20,7 @@ using Datadog.Trace.ClrProfiler;
 using Datadog.Trace.ClrProfiler.ServerlessInstrumentation;
 using Datadog.Trace.Configuration.ConfigurationSources.Telemetry;
 using Datadog.Trace.Configuration.Telemetry;
+using Datadog.Trace.ContinuousProfiler;
 using Datadog.Trace.Logging;
 using Datadog.Trace.Logging.DirectSubmission;
 using Datadog.Trace.Processors;
@@ -138,28 +139,6 @@ namespace Datadog.Trace.Configuration
             {
                 AzureAppServiceMetadata = new ImmutableAzureAppServiceSettings(source, _telemetry);
             }
-
-            // With SSI, beyond ContinuousProfiler.ConfigurationKeys.ProfilingEnabled (true or auto vs false),
-            // the profiler could be enabled via ContinuousProfiler.ConfigurationKeys.SsiDeployed:
-            //  - if it contains "profiler", the profiler is enabled after 30 seconds + at least 1 span
-            //  - if not, the profiler needed to be loaded by the CLR but no profiling will be done, only telemetry metrics will be sent
-            // So, for the Tracer, the profiler should be seen as enabled if ContinuousProfiler.ConfigurationKeys.SsiDeployed has a value
-            // (even without "profiler") so that spans will be sent to the profiler.
-            ProfilingEnabledInternal = config
-                         .WithKeys(ContinuousProfiler.ConfigurationKeys.ProfilingEnabled)
-                         .GetAs(
-                            converter: x => x switch
-                            {
-                                "auto" => true,
-                                _ when x.ToBoolean() is { } boolean => boolean,
-                                _ => ParsingResult<bool>.Failure(),
-                            },
-                            getDefaultValue: () =>
-                            {
-                                var profilingSsiDeployed = config.WithKeys(ContinuousProfiler.ConfigurationKeys.SsiDeployed).AsString();
-                                return (profilingSsiDeployed != null);
-                            },
-                            validator: null);
 
             var otelTags = config
                           .WithKeys(ConfigurationKeys.OpenTelemetry.ResourceAttributes)
@@ -889,13 +868,6 @@ namespace Datadog.Trace.Configuration
         /// </summary>
         /// <seealso cref="ConfigurationKeys.ApmTracingEnabled"/>
         internal bool ApmTracingEnabled => DynamicSettings.ApmTracingEnabled ?? _apmTracingEnabled;
-
-        /// <summary>
-        /// Gets a value indicating whether profiling is enabled.
-        /// Default is <c>false</c>.
-        /// </summary>
-        /// <seealso cref="ContinuousProfiler.ConfigurationKeys.ProfilingEnabled"/>
-        internal bool ProfilingEnabledInternal { get; }
 
         /// <summary>
         /// Gets the names of disabled integrations.

tracer/src/Datadog.Trace/ContinuousProfiler/ConfigurationKeys.cs
@@ -11,5 +11,6 @@ namespace Datadog.Trace.ContinuousProfiler
         public const string CodeHotspotsEnabled = "DD_PROFILING_CODEHOTSPOTS_ENABLED";
         public const string EndpointProfilingEnabled = "DD_PROFILING_ENDPOINT_COLLECTION_ENABLED";
         public const string SsiDeployed = "DD_INJECTION_ENABLED";
+        public const string ProfilerManagedActivationEnabled = "DD_PROFILER_MANAGED_ACTIVATION_ENABLED";
     }
 }

tracer/src/Datadog.Trace/ContinuousProfiler/Profiler.cs
@@ -4,6 +4,8 @@
 // </copyright>
 
 using System.Threading;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Telemetry;
 
 namespace Datadog.Trace.ContinuousProfiler
 {
@@ -11,16 +13,19 @@ namespace Datadog.Trace.ContinuousProfiler
     {
         private static Profiler _instance;
 
-        internal Profiler(IContextTracker contextTracker, IProfilerStatus status)
+        internal Profiler(IContextTracker contextTracker, IProfilerStatus status, ProfilerSettings settings)
         {
             ContextTracker = contextTracker;
             Status = status;
+            Settings = settings;
         }
 
         public static Profiler Instance
         {
             get { return LazyInitializer.EnsureInitialized(ref _instance, () => Create()); }
         }
+
+        public ProfilerSettings Settings { get; }
 
         public IProfilerStatus Status { get; }
 
@@ -33,9 +38,10 @@ namespace Datadog.Trace.ContinuousProfiler
 
         private static Profiler Create()
         {
-            var status = new ProfilerStatus();
+            var settings = new ProfilerSettings(GlobalConfigurationSource.Instance, TelemetryFactory.Config);
+            var status = new ProfilerStatus(settings);
             var contextTracker = new ContextTracker(status);
-            return new Profiler(contextTracker, status);
+            return new Profiler(contextTracker, status, settings);
         }
     }
 }

tracer/src/Datadog.Trace/ContinuousProfiler/ProfilerState.cs
@@ -1,0 +1,26 @@
+// <copyright file="ProfilerState.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+namespace Datadog.Trace.ContinuousProfiler;
+
+internal enum ProfilerState
+{
+    /// <summary>
+    /// The profiler is explicitly disabled via configuration
+    /// </summary>
+    Disabled = 0,
+
+    /// <summary>
+    /// The profiler is explicitly enabled via configuration
+    /// </summary>
+    Enabled = 1,
+
+    /// <summary>
+    /// The profiler is in "auto" mode; i.e. will start after a delay and if traces are created
+    /// </summary>
+    Auto = 2,
+}

tracer/src/Datadog.Trace/ContinuousProfiler/ProfilerStatus.cs
@@ -5,9 +5,7 @@
 
 using System;
 using System.Runtime.InteropServices;
-using Datadog.Trace.ExtensionMethods;
 using Datadog.Trace.Logging;
-using Datadog.Trace.Util;
 
 namespace Datadog.Trace.ContinuousProfiler
 {
@@ -15,38 +13,22 @@ namespace Datadog.Trace.ContinuousProfiler
     {
         private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(ProfilerStatus));
 
-        private readonly bool _isProfilingEnabled;
+        private readonly ProfilerState _profilerState;
         private readonly object _lockObj;
         private bool _isInitialized;
         private IntPtr _engineStatusPtr;
 
-        public ProfilerStatus()
+        public ProfilerStatus(ProfilerSettings settings)
         {
-            var fd = FrameworkDescription.Instance;
-            var isSupported =
-                (fd.OSPlatform == OSPlatformName.Windows && (fd.ProcessArchitecture == ProcessArchitecture.X64 || fd.ProcessArchitecture == ProcessArchitecture.X86)) ||
-                (fd.OSPlatform == OSPlatformName.Linux && fd.ProcessArchitecture == ProcessArchitecture.X64);
-
-            _isProfilingEnabled = false;
-
-            if (isSupported)
+            _profilerState = settings.ProfilerState;
+            var state = _profilerState switch
             {
-                var manualDeployement = EnvironmentHelpers.GetEnvironmentVariable(ConfigurationKeys.ProfilingEnabled);
-                if (manualDeployement != null)
-                {
-                    // it is possible that SSI installation script is setting the environment variable to "auto" to enable the profiler
-                    // instead of "true" to avoid starting the profiler immediately after the installation
-                    _isProfilingEnabled = manualDeployement.ToBoolean() ?? (manualDeployement == "auto");
-                }
-                else
-                {
-                    // the profiler is declared "enabled" just if the SSI environment variable exists to be sure that telemetry metrics
-                    // will contain the right status (i.e. we need the tracer to send the spans even if the profiler is not started yet)
-                    _isProfilingEnabled = (EnvironmentHelpers.GetEnvironmentVariable(ConfigurationKeys.SsiDeployed) != null);
-                }
-            }
+                ProfilerState.Enabled => "enabled",
+                ProfilerState.Auto => "auto",
+                _ => "disabled"
+            };
 
-            Log.Information("Continuous Profiler is {IsEnabled}.", _isProfilingEnabled ? "enabled" : "disabled");
+            Log.Information("Continuous Profiler mode = {ProfilerState}", state);
             _lockObj = new();
             _isInitialized = false;
         }
@@ -55,7 +37,7 @@ namespace Datadog.Trace.ContinuousProfiler
         {
             get
             {
-                if (!_isProfilingEnabled)
+                if (_profilerState == ProfilerState.Disabled)
                 {
                     return false;
                 }

commit 14282510387253102e13b03d80aef8855c45e6ea

Update the `ProfilerSettings` enablement to ignore SSI (#7557)
## Summary of changes

Updates the rules around `ProfilerSettings` enablement

## Reason for change

After #7287, we decide whether the profiler is enabled in managed code,
instead of on the native side. However, we got some of the rules wrong.
Specifically, we believed that the profiler should behave different when
`DD_INJECTION_ENABLED` was set, but that's not the case. The profiler
should behave the same in all cases.

## Implementation details

Fixed the implementation to ignore the SSI variable, and updated the
corresponding unit tests

## Test coverage

Updated the tests that were affected

## Other details

The incorrect behaviour was actually introduced in #7303, but it didn't
have a visible impact until #7287

tracer/src/Datadog.Trace/ContinuousProfiler/ProfilerSettings.cs
@@ -55,20 +55,7 @@ internal class ProfilerSettings
                                        _ when x.ToBoolean() is { } boolean => boolean ? "true" : "false",
                                        _ => ParsingResult<string>.Failure(),
                                    },
-                                   getDefaultValue: () =>
-                                   {
-                                       // If there's no explicit `DD_PROFILING_ENABLED` key,
-                                       // we set the state based on the SSI value, only checking env vars (not the full stack)
-                                       var isSsiDeployment = envConfigBuilder
-                                                            .WithKeys(ConfigurationKeys.SsiDeployed)
-                                                            .AsString();
-
-                                       return isSsiDeployment switch
-                                       {
-                                           { Length: > 0 } => "auto",
-                                           _ => "false",
-                                       };
-                                   },
+                                   getDefaultValue: () => "false",
                                    validator: null);
 
         ProfilerState = profilingEnabled switch

tracer/test/Datadog.Trace.Tests/ContinuousProfiler/ProfilerSettingsTests.cs
@@ -16,9 +16,9 @@ namespace Datadog.Trace.Tests.ContinuousProfiler;
 
 public class ProfilerSettingsTests : SettingsTestsBase
 {
-    // profiling takes precedence over SSI
-    // "auto" is a special profiling value that enables profiling when deployed via SSI
-    // the profiler will also be enabled when "profiler" will be added to the DD_INJECTION_ENABLED environment variable
+    // "auto" is a special profiling value that enables profiling based on heuristics
+    // historically, we set a different value for profiling when we are in SSI. That is no longer
+    // the case, but we keep the test here to ensure that remains the case!
     [Theory]
     [InlineData("1", null, (int)ProfilerState.Enabled)]
     [InlineData("0", null, (int)ProfilerState.Disabled)]
@@ -30,19 +30,19 @@ public class ProfilerSettingsTests : SettingsTestsBase
     [InlineData("true", "not used", (int)ProfilerState.Enabled)]
     [InlineData("false", "not used", (int)ProfilerState.Disabled)]
     [InlineData("auto", "not used", (int)ProfilerState.Auto)]
-    [InlineData("invalid", "foo, profiler, bar", (int)ProfilerState.Auto)]
-    [InlineData("invalid", "profiler", (int)ProfilerState.Auto)]
-    [InlineData("invalid", "anything else", (int)ProfilerState.Auto)]
+    [InlineData("invalid", "foo, profiler, bar", (int)ProfilerState.Disabled)]
+    [InlineData("invalid", "profiler", (int)ProfilerState.Disabled)]
+    [InlineData("invalid", "anything else", (int)ProfilerState.Disabled)]
     [InlineData("invalid", "", (int)ProfilerState.Disabled)]
     [InlineData("invalid", null, (int)ProfilerState.Disabled)]
-    [InlineData("", "foo, profiler, bar", (int)ProfilerState.Auto)]
-    [InlineData("", "profiler", (int)ProfilerState.Auto)]
-    [InlineData("", "anything else", (int)ProfilerState.Auto)]
+    [InlineData("", "foo, profiler, bar", (int)ProfilerState.Disabled)]
+    [InlineData("", "profiler", (int)ProfilerState.Disabled)]
+    [InlineData("", "anything else", (int)ProfilerState.Disabled)]
     [InlineData("", "", (int)ProfilerState.Disabled)]
     [InlineData("", null, (int)ProfilerState.Disabled)]
-    [InlineData(null, "foo, profiler, bar", (int)ProfilerState.Auto)]
-    [InlineData(null, "profiler", (int)ProfilerState.Auto)]
-    [InlineData(null, "anything else", (int)ProfilerState.Auto)]
+    [InlineData(null, "foo, profiler, bar", (int)ProfilerState.Disabled)]
+    [InlineData(null, "profiler", (int)ProfilerState.Disabled)]
+    [InlineData(null, "anything else", (int)ProfilerState.Disabled)]
     [InlineData(null, null, (int)ProfilerState.Disabled)]
     [InlineData(null, "", (int)ProfilerState.Disabled)]
     public void ProfilerState_WhenPassedViaEnvironment(string profilingValue, string ssiValue, int expected)