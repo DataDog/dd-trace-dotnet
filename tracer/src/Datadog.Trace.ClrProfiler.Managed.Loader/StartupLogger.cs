797437a3124452c9c900c0960f3f7852f5ba8c8a
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-10-01 14:31:14 -0400

    more refactoring in StartupLogger

--- tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/StartupLogger.cs
+++ tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/StartupLogger.cs
@@ -13,8 +13,6 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
 {
     internal static class StartupLogger
     {
-        private const string NixDefaultDirectory = "/var/log/datadog/dotnet";
-
         private static readonly bool DebugEnabled;
         private static readonly string? StartupLogFilePath;
         private static readonly object PadLock = new();
@@ -22,11 +20,24 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
         static StartupLogger()
         {
             var envVars = new EnvironmentVariableProvider(false);
-
             DebugEnabled = envVars.GetBooleanEnvironmentVariable("DD_TRACE_DEBUG", false);
 
-            var logDirectory = GetLogDirectory(envVars);
-            StartupLogFilePath = SetStartupLogFilePath(logDirectory);
+            try
+            {
+                // check DD_TRACE_LOG_DIRECTORY first, then DD_TRACE_LOG_PATH,
+                // otherwise fallback to the default log directory for the current platform
+                var logDirectory = GetLogDirectoryFromEnvVars(envVars) ?? GetDefaultLogDirectory(envVars);
+                Directory.CreateDirectory(logDirectory);
+                StartupLogFilePath = ComputeStartupLogFilePath(logDirectory);
+            }
+            catch
+            {
+                // The try block may throw a SecurityException if not granted the System.Security.Permissions.FileIOPermission
+                // because of the following API calls
+                //   - Directory.Exists
+                //   - Environment.GetFolderPath
+                //   - Path.GetTempPath
+            }
         }
 
         public static void Log(string message, params object?[] args)
@@ -72,102 +83,68 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
             }
         }
 
-        internal static string? GetLogDirectory(IEnvironmentVariableProvider envVars)
+        internal static string? GetLogDirectoryFromEnvVars(IEnvironmentVariableProvider envVars)
         {
-            string? logDirectory;
-
-            try
+            if (envVars.GetEnvironmentVariable("DD_TRACE_LOG_DIRECTORY") is { } logDirectory)
             {
-                logDirectory = envVars.GetEnvironmentVariable("DD_TRACE_LOG_DIRECTORY");
+                return logDirectory;
+            }
 
-                if (logDirectory == null)
+            if (envVars.GetEnvironmentVariable("DD_TRACE_LOG_PATH") is { } logFilename && !string.IsNullOrEmpty(logFilename))
+            {
+                return Path.GetDirectoryName(logFilename);
+            }
+
+            return null;
+        }
+
+        internal static string GetDefaultLogDirectory(IEnvironmentVariableProvider envVars)
+        {
+            var isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
+            var isAas = !string.IsNullOrEmpty(envVars.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+
+            if (isAas)
+            {
+                // Azure App Services and Azure Functions
+                return isWindows ? @"C:\home\LogFiles\datadog" : "/home/LogFiles/datadog";
+            }
+
+            if (isWindows)
+            {
+                // On Nano Server, this returns "", so we fallback to reading from the env var set in the base image instead
+                // - https://github.com/dotnet/runtime/issues/22690
+                // - https://github.com/dotnet/runtime/issues/21430
+                // - https://github.com/dotnet/runtime/pull/109673
+                // If _that_ fails, we just hard code it to "C:\ProgramData", which is what the native components do anyway
+                var programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                if (string.IsNullOrEmpty(programData))
                 {
-                    var nativeLogFile = envVars.GetEnvironmentVariable("DD_TRACE_LOG_PATH");
-
-                    if (!string.IsNullOrEmpty(nativeLogFile))
+                    programData = envVars.GetEnvironmentVariable("ProgramData");
+                    if (string.IsNullOrEmpty(programData))
                     {
-                        logDirectory = Path.GetDirectoryName(nativeLogFile);
+                        programData = @"C:\ProgramData";
                     }
                 }
 
-                if (logDirectory == null)
-                {
-                    if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-                    {
-                        // On Nano Server, this returns "", so we fallback to reading from the env var set in the base image instead
-                        // - https://github.com/dotnet/runtime/issues/22690
-                        // - https://github.com/dotnet/runtime/issues/21430
-                        // - https://github.com/dotnet/runtime/pull/109673
-                        // If _that_ fails, we just hard code it to "C:\ProgramData", which is what the native components do anyway
-                        var programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-                        if (string.IsNullOrEmpty(programData))
-                        {
-                            programData = Environment.GetEnvironmentVariable("ProgramData");
-                            if (string.IsNullOrEmpty(programData))
-                            {
-                                programData = @"C:\ProgramData";
-                            }
-                        }
-
-                        var windowsDefaultDirectory = Path.Combine(programData, "Datadog .NET Tracer", "logs");
-                        logDirectory = windowsDefaultDirectory;
-                    }
-                    else
-                    {
-                        // Linux
-                        logDirectory = NixDefaultDirectory;
-                    }
-                }
-
-                logDirectory = CreateDirectoryIfMissing(logDirectory) ?? Path.GetTempPath();
-            }
-            catch
-            {
-                // The try block may throw a SecurityException if not granted the System.Security.Permissions.FileIOPermission
-                // because of the following API calls
-                //   - Directory.Exists
-                //   - Environment.GetFolderPath
-                //   - Path.GetTempPath
-
-                // Unsafe to log
-                logDirectory = null;
+                return Path.Combine(programData, "Datadog .NET Tracer", "logs");
             }
 
-            return logDirectory;
+            // not Windows
+            return "/var/log/datadog/dotnet";
         }
 
-        private static string? CreateDirectoryIfMissing(string pathToCreate)
+        private static string ComputeStartupLogFilePath(string logDirectory)
         {
             try
             {
-                Directory.CreateDirectory(pathToCreate);
-                return pathToCreate;
-            }
-            catch
-            {
-                // Unable to create the directory meaning that the user will have to create it on their own.
-                // It is unsafe to log here, so return null to defer deciding what the path is
-                return null;
-            }
-        }
-
-        private static string? SetStartupLogFilePath(string? logDirectory)
-        {
-            if (logDirectory == null)
-            {
-                return null;
-            }
-
-            try
-            {
+                // Do our best to not block other processes on write by using the process name and id
                 using var process = Process.GetCurrentProcess();
-                // Do our best to not block other processes on write
                 return Path.Combine(logDirectory, $"dotnet-tracer-loader-{process.ProcessName}-{process.Id}.log");
             }
             catch
             {
-                // We can't get the process info
-                return Path.Combine(logDirectory, "dotnet-tracer-loader.log");
+                // We can't get the process info, use a random guid instead
+                return Path.Combine(logDirectory, $"dotnet-tracer-loader-{Guid.NewGuid()}.log");
             }
         }
     }
