a2bf39f1ac4bc2492bdc1fd013c41caa133110c5
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2025-10-14 12:30:55 +0000

    Revert timeout

--- tracer/test/Datadog.Trace.IntegrationTests/LibDatadog/TraceExporterTests.cs
+++ tracer/test/Datadog.Trace.IntegrationTests/LibDatadog/TraceExporterTests.cs
@@ -136,7 +136,7 @@ public class TraceExporterTests
         sampleRateResponses.Should().ContainSingle();
 
         // Assert telemetry payloads were sent
-        await agent.WaitForLatestTelemetryAsync(t => t != null, 60000);
+        await agent.WaitForLatestTelemetryAsync(t => t != null);
         agent.Telemetry.Should().NotBeNull();
 
         Dictionary<string, object> GetSettings()
