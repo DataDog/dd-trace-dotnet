91daa16afbe12ea1d5908fdd5ffff108833a9625
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-10-20 17:05:35 -0400

    remove win-arm64

--- tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/Startup.cs
+++ tracer/src/Datadog.Trace.ClrProfiler.Managed.Loader/Startup.cs
@@ -26,7 +26,6 @@ namespace Datadog.Trace.ClrProfiler.Managed.Loader
         {
             "win-x64",
             "win-x86",
-            "win-arm64",
             "linux-x64",
             "linux-arm64",
             "linux-musl-x64",
