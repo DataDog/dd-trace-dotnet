2f6763b41ed00aa5f03a97acad20c6b94257ca94
Anna <anna.yafi@datadoghq.com>
2025-10-22 17:22:31 +0200

    Replacing ConfigurationKeys by generated ones!

--- tracer/src/Datadog.Trace/Configuration/DeprecationMessages.cs
+++ tracer/src/Datadog.Trace/Configuration/DeprecationMessages.cs
@@ -3,11 +3,16 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
+
 namespace Datadog.Trace.Configuration
 {
     internal static class DeprecationMessages
     {
         public const string AppAnalytics = "App Analytics has been replaced by Tracing without Limits. For more information see https://docs.datadoghq.com/tracing/legacy_app_analytics/";
-        public const string LogPath = "DD_TRACE_LOG_PATH is deprecated. Use DD_TRACE_LOG_DIRECTORY instead";
+        public const string ProfilerLogPathObsoleteMessage = "DD_TRACE_LOG_PATH is deprecated. Use DD_TRACE_LOG_DIRECTORY instead";
+
+        [Obsolete(ProfilerLogPathObsoleteMessage)]
+        public const string LogPath = "DD_TRACE_LOG_PATH";
     }
 }
