6eb9684652e4d5fee858adbe2042abf25a63d6f6
Anna <anna.yafi@datadoghq.com>
2025-10-23 14:01:06 +0200

    Small adaptations

--- tracer/src/Datadog.Trace/Configuration/DeprecationMessages.cs
+++ tracer/src/Datadog.Trace/Configuration/DeprecationMessages.cs
@@ -3,16 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System;
-
 namespace Datadog.Trace.Configuration
 {
     internal static class DeprecationMessages
     {
         public const string AppAnalytics = "App Analytics has been replaced by Tracing without Limits. For more information see https://docs.datadoghq.com/tracing/legacy_app_analytics/";
-        public const string ProfilerLogPathObsoleteMessage = "DD_TRACE_LOG_PATH is deprecated. Use DD_TRACE_LOG_DIRECTORY instead";
-
-        [Obsolete(ProfilerLogPathObsoleteMessage)]
-        public const string LogPath = "DD_TRACE_LOG_PATH";
+        public const string LogPath = "DD_TRACE_LOG_PATH is deprecated. Use DD_TRACE_LOG_DIRECTORY instead";
     }
 }
