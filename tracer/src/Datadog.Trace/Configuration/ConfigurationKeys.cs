2dfb175b4002b33f1243f69dc7dc6385aebcef1c
Anna <anna.yafi@datadoghq.com>
2025-10-23 12:46:57 +0200

    Commit temporary generated ConfigurationKeys2.cs (will eventually be renamed to ConfigurationKeys.cs)

--- tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
+++ tracer/src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -14,6 +14,9 @@ namespace Datadog.Trace.Configuration
     /// </summary>
     internal static partial class ConfigurationKeys
     {
+        // temporary so that ConfigurationKeys2 can resolve reference, all this is removed later
+        public const string TraceLogPath = "DD_TRACE_LOG_PATH";
+
         /// <summary>
         /// Configuration key to enable experimental features.
         /// </summary>
@@ -994,7 +997,7 @@ namespace Datadog.Trace.Configuration
             /// Configuration key for enabling legacy binary headers in Data Streams Monitoring.
             /// false by default if DSM is in default state, true otherwise
             /// </summary>
-             /// <see cref="TracerSettings.IsDataStreamsLegacyHeadersEnabled"/>
+            /// <see cref="TracerSettings.IsDataStreamsLegacyHeadersEnabled"/>
             public const string LegacyHeadersEnabled = "DD_DATA_STREAMS_LEGACY_HEADERS";
         }
     }
