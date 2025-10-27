0e8bd4c6cb80b93e25ac91941eab91252fe5efc4
Andrew Lock <andrew.lock@datadoghq.com>
2025-10-27 10:30:04 +0000

    Increase verbosity

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -276,6 +276,7 @@ partial class Build
             {
                 NuGetTasks.NuGetRestore(s => s
                     .SetTargetPath(Solution)
+                    .SetVerbosity(NuGetVerbosity.Detailed)
                     .SetProcessLogOutput(!IsServerBuild)
                     .When(!string.IsNullOrEmpty(NugetPackageDirectory), o =>
                         o.SetPackagesDirectory(NugetPackageDirectory)));
