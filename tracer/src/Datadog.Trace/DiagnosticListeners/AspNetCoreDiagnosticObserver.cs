3bf408c3855616da9dcea1faa273ffc1a11df756
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2025-10-28 09:52:06 -0400

    Use DuckCast instead of TryDuckCast
    Co-authored-by: Andrew Lock <andrew.lock@datadoghq.com>

--- tracer/src/Datadog.Trace/DiagnosticListeners/AspNetCoreDiagnosticObserver.cs
+++ tracer/src/Datadog.Trace/DiagnosticListeners/AspNetCoreDiagnosticObserver.cs
@@ -754,8 +754,7 @@ namespace Datadog.Trace.DiagnosticListeners
                 return;
             }
 
-            if (arg.TryDuckCast<HttpRequestInStopStruct>(out var stopStruct)
-             && stopStruct.HttpContext is { } httpContext
+            if (arg.DuckCast<HttpRequestInStopStruct>()?.HttpContext is { } httpContext
              && httpContext.Features.Get<AspNetCoreHttpRequestHandler.RequestTrackingFeature>() is { RootScope: { } rootScope })
             {
                 AspNetCoreRequestHandler.StopAspNetCorePipelineScope(tracer, CurrentSecurity, rootScope, httpContext);
