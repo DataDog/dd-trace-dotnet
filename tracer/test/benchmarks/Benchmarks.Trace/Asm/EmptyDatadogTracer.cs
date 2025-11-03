DataDog/dd-trace-dotnet

commit a2cc41a0d98127423ac265b2e0b186eacf060a46

Fix ad-hoc use of `TracerSettings` to use `MutableSettings` where appropriate (#7543)
## Summary of changes

Fix usages of `Tracer.Instance.Settings` to use
`Tracer.Instance.CurrentTracerSettings.Settings` where appropriate

## Reason for change

This PR "fixes" the places that were previously grabbing the
environment/service etc from `TracerSettings` to use the
`MutableSettings` exposed via `CurrentTracerSettings` instead, as the
location where these settings will ultimately exist.

This is effectively still just a refactoring, but prepares for the point
where these settings aren't exposed on `TracerSettings` at all. The
updates in this PR are for cases where you _don't_ have long-lived
services, and rather need to do ad-hoc `Tracer.Instance` grabbing of the
setting values in a global context. Note too that many of these places
_could_ be updated in the future to subscribe to changes if that
provides performance benefits. Also note that I elected not to change
most calls to `IsIntegrationEnabled()` etc in this PR as there are
hundreds of locations. The follow up PR handles that

Also found a few cases that were incorrectly assuming that these values
cannot change. Marked them with 'TODO: Subscribe to changes in settings'

## Implementation details

- Mostly find and replace to use `CurrentTracerSettings.Settings`
- Occasional extraction of a variable where it makes sense to avoid
repeated access
- Functionally identical currently (where `MutableSettings` is replaced
on `TracerSettings`) but will be a required change once we stop
replacing `TracerManager`.

## Test coverage

Covered by existing tests

## Other details

https://datadoghq.atlassian.net/browse/LANGPLAT-819

Part of a config stack

- https://github.com/DataDog/dd-trace-dotnet/pull/7522
- https://github.com/DataDog/dd-trace-dotnet/pull/7525
- https://github.com/DataDog/dd-trace-dotnet/pull/7530
- https://github.com/DataDog/dd-trace-dotnet/pull/7532

tracer/test/Datadog.Trace.Security.Unit.Tests/EmptyDatadogTracer.cs
@@ -5,6 +5,10 @@
 
 using System;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.Configuration.Schema;
+using Datadog.Trace.Sampling;
+using Moq;
+
 // <copyright file="EmptyDatadogTracer.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
@@ -12,15 +16,23 @@ using Datadog.Trace.Configuration;
 
 namespace Datadog.Trace.Security.Unit.Tests
 {
-    public class EmptyDatadogTracer : IDatadogTracer
+    internal class EmptyDatadogTracer : IDatadogTracer
     {
-        public string DefaultServiceName => "My Service Name";
+        public EmptyDatadogTracer()
+        {
+            DefaultServiceName = "My Service Name";
+            Settings = new TracerSettings(NullConfigurationSource.Instance);
+            var namingSchema = new NamingSchema(SchemaVersion.V0, false, false, DefaultServiceName, null, null);
+            PerTraceSettings = new PerTraceSettings(null, null, namingSchema, MutableSettings.CreateWithoutDefaultSources(Settings));
+        }
 
-        public TracerSettings Settings => new(new NullConfigurationSource());
+        public string DefaultServiceName { get; }
+
+        public TracerSettings Settings { get; }
 
         IGitMetadataTagsProvider IDatadogTracer.GitMetadataTagsProvider => new NullGitMetadataProvider();
 
-        PerTraceSettings IDatadogTracer.PerTraceSettings => null;
+        public PerTraceSettings PerTraceSettings { get; }
 
         void IDatadogTracer.Write(ArraySegment<Span> span)
         {

commit eb9acab26fa7dfa3e85e860c7c8826595886c828

Fix Asm benchmark and stop-reinitializing tracer (#7750)
## Summary of changes

- Fix ASM benchmarks by not returning `null` from
`EmptyDatadogTracer.PerTraceSettings`
- Stop re-initializing `TracerSettings` for every execution of the ASM
benchmark

## Reason for change

The ASM benchmarks have been broken since
https://github.com/DataDog/dd-trace-dotnet/pull/7543 (I think). This is
because the `EmptyDatadogTracer` stub used in the benchmark returns
`null` from `PerTraceSettings` (which can't happen in practice).

Additionally, noticed that the benchmark is repeatedly creating a new
`TracerSettings` object with every execution, which will add noise and
be much more expensive than real life.

## Implementation details

- Ensure `EmptyDatadogTracer.PerTraceSettings` returns a "real" value
- Stop rebuilding `TracerSettings` with every execution

## Test coverage

This is the test

tracer/test/benchmarks/Benchmarks.Trace/Asm/AppSecBodyBenchmark.cs
@@ -73,7 +73,7 @@ namespace Benchmarks.Trace.Asm
 
         private void ExecuteCycle(object body)
         {
-            var traceContext = new Datadog.Trace.TraceContext(new EmptyDatadogTracer(), null);
+            var traceContext = new Datadog.Trace.TraceContext(EmptyDatadogTracer.Instance, null);
             var spanContext = new SpanContext(parent: null, traceContext, serviceName: "My Service Name", traceId: (TraceId)100, spanId: 200);
             var span = new Span(spanContext, DateTimeOffset.Now);
 

tracer/test/benchmarks/Benchmarks.Trace/Asm/EmptyDatadogTracer.cs
@@ -17,13 +17,25 @@ namespace Benchmarks.Trace.Asm
 {
     public class EmptyDatadogTracer : IDatadogTracer
     {
+        public static readonly EmptyDatadogTracer Instance = new();
+
+        private readonly IGitMetadataTagsProvider _gitMetadata;
+        private readonly PerTraceSettings _perTraceSettings;
+
+        public EmptyDatadogTracer()
+        {
+            Settings = new(new NullConfigurationSource());
+            _gitMetadata = new NullGitMetadataProvider();
+            _perTraceSettings = new PerTraceSettings(null, null, null, Settings.InitialMutableSettings);
+        }
+
         public string DefaultServiceName => "My Service Name";
 
-        public TracerSettings Settings => new(new NullConfigurationSource());
+        public TracerSettings Settings { get; }
 
-        IGitMetadataTagsProvider IDatadogTracer.GitMetadataTagsProvider => new NullGitMetadataProvider();
+        IGitMetadataTagsProvider IDatadogTracer.GitMetadataTagsProvider => _gitMetadata;
 
-        PerTraceSettings IDatadogTracer.PerTraceSettings => null;
+        PerTraceSettings IDatadogTracer.PerTraceSettings => _perTraceSettings;
 
         void IDatadogTracer.Write(ArraySegment<Span> span)
         {