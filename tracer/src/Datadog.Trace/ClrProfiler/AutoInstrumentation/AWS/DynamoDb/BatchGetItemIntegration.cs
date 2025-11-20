dcf51efd3db2bd1d41929e86415dc58b1d637b9d
Andrew Lock <andrew.lock@datadoghq.com>
2025-11-20 11:57:11 +0000

    Add #nullable enable to many files in DynamoDb

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/DynamoDb/BatchGetItemIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AWS/DynamoDb/BatchGetItemIntegration.cs
@@ -3,11 +3,12 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using System;
 using System.ComponentModel;
 using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.DuckTyping;
-using Datadog.Trace.Tagging;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.DynamoDb
 {
@@ -19,7 +20,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.DynamoDb
         TypeName = "Amazon.DynamoDBv2.AmazonDynamoDBClient",
         MethodName = "BatchGetItem",
         ReturnTypeName = "Amazon.DynamoDBv2.Model.BatchGetItemResponse",
-        ParameterTypeNames = new[] { "Amazon.DynamoDBv2.Model.BatchGetItemRequest" },
+        ParameterTypeNames = ["Amazon.DynamoDBv2.Model.BatchGetItemRequest"],
         MinimumVersion = "3.0.0",
         MaximumVersion = "4.*.*",
         IntegrationName = AwsDynamoDbCommon.IntegrationName)]
@@ -29,14 +30,6 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.DynamoDb
     {
         private const string Operation = "BatchGetItem";
 
-        /// <summary>
-        /// OnMethodBegin callback
-        /// </summary>
-        /// <typeparam name="TTarget">Type of the target</typeparam>
-        /// <typeparam name="TBatchGetItemRequest">Type of the request object</typeparam>
-        /// <param name="instance">Instance value, aka `this` of the instrumented method</param>
-        /// <param name="request">The request for the DynamoDB operation</param>
-        /// <returns>CallTarget state value</returns>
         internal static CallTargetState OnMethodBegin<TTarget, TBatchGetItemRequest>(TTarget instance, TBatchGetItemRequest request)
             where TBatchGetItemRequest : IBatchRequest, IDuckType
         {
@@ -45,23 +38,13 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AWS.DynamoDb
                 return CallTargetState.GetDefault();
             }
 
-            var scope = AwsDynamoDbCommon.CreateScope(Tracer.Instance, Operation, out AwsDynamoDbTags tags);
+            var scope = AwsDynamoDbCommon.CreateScope(Tracer.Instance, Operation, out var tags);
             AwsDynamoDbCommon.TagBatchRequest(request, tags, scope);
 
             return new CallTargetState(scope);
         }
 
-        /// <summary>
-        /// OnMethodEnd callback
-        /// </summary>
-        /// <typeparam name="TTarget">Type of the target</typeparam>
-        /// <typeparam name="TReturn">Type of the return value</typeparam>
-        /// <param name="instance">Instance value, aka `this` of the instrumented method.</param>
-        /// <param name="returnValue">Task of HttpResponse message instance</param>
-        /// <param name="exception">Exception instance in case the original code threw an exception.</param>
-        /// <param name="state">CallTarget state value</param>
-        /// <returns>A response value, in an async scenario will be T of Task of T</returns>
-        internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception exception, in CallTargetState state)
+        internal static CallTargetReturn<TReturn> OnMethodEnd<TTarget, TReturn>(TTarget instance, TReturn returnValue, Exception? exception, in CallTargetState state)
         {
             state.Scope.DisposeWithException(exception);
             return new CallTargetReturn<TReturn>(returnValue);
--- tracer/src/Datadog.Trace/Tagging/AwsDynamoDbTags.cs
+++ tracer/src/Datadog.Trace/Tagging/AwsDynamoDbTags.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using System;
 using Datadog.Trace.SourceGenerators;
 
@@ -12,7 +14,7 @@ namespace Datadog.Trace.Tagging
     internal partial class AwsDynamoDbTags : AwsSdkTags
     {
         [Tag(Trace.Tags.TableName)]
-        public string TableName { get; set; }
+        public string? TableName { get; set; }
 
         [Tag(Trace.Tags.SpanKind)]
         public override string SpanKind => SpanKinds.Client;
