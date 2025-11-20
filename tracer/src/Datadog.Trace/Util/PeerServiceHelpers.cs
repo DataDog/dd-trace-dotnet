432dde8440a4a4d7b149ec2e5d0baae9e904e72c
Rithika Narayan <rithika.narayan@datadoghq.com>
2025-11-20 16:04:51 -0500

    Add DerivePeerService and call it in RuntimePipeline integrations

---
+++ tracer/src/Datadog.Trace/Util/PeerServiceHelpers.cs
@@ -0,0 +1,101 @@
+// <copyright file="PeerServiceHelpers.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using Datadog.Trace.Tagging;
+
+namespace Datadog.Trace.Util
+{
+    /// <summary>
+    /// Helpers to access environment variables
+    /// </summary>
+    internal static class PeerServiceHelpers
+    {
+        /// <summary>
+        /// Sets peer.service tag for AwsSdk spans based on the service, environment, and region
+        /// </summary>
+        /// <param name="tags">AwsSdkTags for the current span</param>
+        public static AwsSdkTags DerivePeerService(AwsSdkTags tags)
+        {
+            var service = tags.AwsService;
+            var region = tags.Region;
+            var isServerless = EnvironmentHelpers.IsAwsLambda();
+            if (isServerless && tags.AwsRegion != null)
+            {
+                switch (service)
+                {
+                    case "DynamoDB":
+                        tags.PeerService = "dynamodb." + region + ".amazonaws.com";
+                        break;
+                    case "EventBridge":
+                        tags.PeerService = "events." + region + ".amazonaws.com";
+                        break;
+                    case "Kinesis":
+                        tags.PeerService = "kinesis." + region + ".amazonaws.com";
+                        break;
+                    case "S3":
+                        var s3Tags = (AwsS3Tags)tags;
+                        if (s3Tags.BucketName != null)
+                        {
+                            tags.PeerService = s3Tags.BucketName + ".s3." + region + ".amazonaws.com";
+                        }
+                        else
+                        {
+                            tags.PeerService = "s3." + region + ".amazonaws.com";
+                        }
+
+                        break;
+                    case "SNS":
+                        tags.PeerService = "sns." + region + ".amazonaws.com";
+                        break;
+                    case "SQS":
+                        tags.PeerService = "sqs." + region + ".amazonaws.com";
+                        break;
+                }
+
+                tags.PeerServiceSource = "peer.service";
+            }
+            else
+            {
+                switch (service)
+                {
+                    case "DynamoDB":
+                        var dbTags = (AwsDynamoDbTags)tags;
+                        tags.PeerService = dbTags.TableName;
+                        tags.PeerServiceSource = Trace.Tags.TableName;
+                        break;
+                    case "EventBridge":
+                        var eventTags = (AwsEventBridgeTags)tags;
+                        tags.PeerService = eventTags.RuleName;
+                        tags.PeerServiceSource = Trace.Tags.RuleName;
+                        break;
+                    case "Kinesis":
+                        var kinesisTags = (AwsKinesisTags)tags;
+                        tags.PeerService = kinesisTags.StreamName;
+                        tags.PeerServiceSource = Trace.Tags.StreamName;
+                        break;
+                    case "S3":
+                        var s3Tags = (AwsS3Tags)tags;
+                        tags.PeerService = s3Tags.BucketName;
+                        tags.PeerServiceSource = Trace.Tags.BucketName;
+                        break;
+                    case "SNS":
+                        var snsTags = (AwsSnsTags)tags;
+                        tags.PeerService = snsTags.TopicName;
+                        tags.PeerServiceSource = Trace.Tags.TopicName;
+                        break;
+                    case "SQS":
+                        var sqsTags = (AwsSqsTags)tags;
+                        tags.PeerService = sqsTags.QueueName;
+                        tags.PeerServiceSource = Trace.Tags.QueueName;
+                        break;
+                }
+            }
+
+            return tags;
+        }
+    }
+}
