5b8194a3a47cc0ebd9352dc5bb2f7348264cb2ab
Andrew Lock <andrew.lock@datadoghq.com>
2025-11-21 12:27:59 +0000

    Add testing for latest Microsoft.Extensions.Telemetry version (#7817)
    ## Summary of changes
    
    Adds testing for 10.x.x of Microsoft.Extensions.Telemetry
    
    ## Reason for change
    
    We don't test it currently, though we do instrument it
    
    ## Implementation details
    
    Update the generated versions to make sure we test it
    
    ## Test coverage
    
    More now

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
@@ -2493,6 +2493,7 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 #if NET48
                 new object[] { "8.10.0" },
                 new object[] { "9.10.0" },
+                new object[] { "10.0.0" },
 #endif
 #if NET6_0
                 new object[] { "8.10.0" },
@@ -2503,14 +2504,17 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 #if NET8_0
                 new object[] { "8.10.0" },
                 new object[] { "9.10.0" },
+                new object[] { "10.0.0" },
 #endif
 #if NET9_0
                 new object[] { "8.10.0" },
                 new object[] { "9.10.0" },
+                new object[] { "10.0.0" },
 #endif
 #if NET10_0
                 new object[] { "8.10.0" },
                 new object[] { "9.10.0" },
+                new object[] { "10.0.0" },
 #endif
 #endif
             };
