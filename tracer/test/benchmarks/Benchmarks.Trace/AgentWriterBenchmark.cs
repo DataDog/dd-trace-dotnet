DataDog/dd-trace-dotnet

commit 8c4a8c4f2ae19bafca442160160dccb710330b09

Handle changes to `MutableSettings` and `ExporterSettings` without rebuilding (#7724)
## Summary of changes

- This is the big one
- Update services to dynamically update when mutable settings or
exporter settings change
- Stop rebuilding everything when there's manual/remote configuration

## Reason for change

This is the "endpoint" that we've been heading for - services only being
disposed/rebuilt at the end of the app, and otherwise only rebuilding
the _necessary_ parts. For example - we don't need to tear down all the
API factories when a customer changes a global tag via remote config;
they only need to change if the `ExporterSettings` change.

The hope is that overall this reduces the overhead of using
configuration in code and/or remote configuration, while also reducing
the number of issues due to managing disposal of services.

## Implementation details

Overall, this PR is kind of a pain. Moving from the "rebuild everything"
to "reconfigure each service" couldn't be done piecemeal, so this is the
one-shot PR. What's more, different services need different patterns
(though we can probably consolidate some of them, this has taken a _lot_
of work and I likely changed patterns unnecessarily in some places).

In general, there's a couple of patterns:

- CI Vis doesn't let you change settings at runtime, so it _never_ needs
to respond to changes. It always just uses the "initial" settings
- Debugger _today_ doesn't respond to changes at runtime (except its own
dynamic config), so for now we ignore Debugger too as it's not really a
regression. I hope we can fix this soon though.
- I've introduced the concept of `Managed*` versions of some services
- These services generally "wrap" the existing type, delegating access
to the underlying service, and handling settings changes
- Many services only care about a sub-set of mutable settings, so they
only update if they need to
- Somewhat annoyingly, setting updates occur on a background thread, so

tracer/test/benchmarks/Benchmarks.Trace/AgentWriterBenchmark.cs
@@ -8,6 +8,7 @@ using Datadog.Trace;
 using Datadog.Trace.Agent;
 using Datadog.Trace.Agent.Transports;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.DogStatsd;
 using Datadog.Trace.Util;
 
 namespace Benchmarks.Trace
@@ -47,7 +48,12 @@ namespace Benchmarks.Trace
             var sources = new CompositeConfigurationSource(new[] { overrides, GlobalConfigurationSource.Instance });
             var settings = new TracerSettings(sources);
 
-            var api = new Api(new FakeApiRequestFactory(settings.Exporter.AgentUri), statsd: null, updateSampleRates: null, partialFlushEnabled: false);
+            var api = new Api(
+                new FakeApiRequestFactory(settings.Manager.InitialExporterSettings.AgentUri),
+                statsd: new StatsdManager(settings, (_, _) => null!),
+                updateSampleRates: null,
+                partialFlushEnabled: false,
+                healthMetricsEnabled: false);
 
             _agentWriter = new AgentWriter(api, statsAggregator: null, statsd: null, automaticFlush: false);
 

commit 181939ca5598fdfec55adbbcca804117c369d491

Fix (and extend) `AgentWriter` benchmark (#7882)
## Summary of changes

- Fix `null` reference exception in `AgentWriter` benchmark (introduced
in #7724)
- Add a (local only) benchmark that tests serialization without flushing
- use a custom tag object in the benchmarks

## Reason for change

The `AgentWriter` benchmark has been broken since #7724 was merged, as
it's passing a null ref to a non-null field. This fixes that issue.

Also, this adds a benchmark (which _doesn't_ run in CI currently) which
tests the serialization of spans _without_ flushing, so we can
distinguish the source of allocation more easily.

Finally, I made some changes to the spans used in the existing
`AgentWriter` benchmark. This is due to discrepancies I noticed in the
results from the benchmark vs other testing.

## Implementation details

- Pass a `StatsdManager` into the `AgentWriter` instead of `null`
- Create a "noop" `Api` which doesn't do anything, and use that in a
different `AgentWriter` instance, this effectively isolates the overhead
in the benchmark to being the serialization _only_, instead of including
"flush" related overhead (which _is_ included in the existing
benchmark). To avoid bloating our benchmarking runs, don't bother to run
this one in CI (it's just useful for inestigation).
- Pass a `SqlTags` object into the spans, with a couple of fields set.
- Set a tag _other_ than `env` on the spans. `env` is a "special"
trace-level tag, so it doesn't _really_ do what you think it does here
and IMO is less revealing.

## Test coverage

Tested locally - running the benchmarks _without_ the sql tags change:

| Method | Runtime | Mean | Error | StdDev | Median | Ratio | RatioSD |
Allocated | Alloc Ratio |
|---------------------------- |---------------------
|---------:|--------:|---------:|---------:|------:|--------:|----------:|------------:|
| WriteEnrichedTraces | .NET 6.0 | 391.0 us | 7.54 us | 9.54 us | 389.2
us | 0.79 | 0.02 | 105 B | 0.50 |

tracer/test/benchmarks/Benchmarks.Trace/AgentWriterBenchmark.cs
@@ -9,17 +9,18 @@ using Datadog.Trace.Agent;
 using Datadog.Trace.Agent.Transports;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.DogStatsd;
+using Datadog.Trace.Tagging;
 using Datadog.Trace.Util;
 
 namespace Benchmarks.Trace
 {
     [MemoryDiagnoser]
-    [BenchmarkCategory(Constants.TracerCategory, Constants.RunOnPrs, Constants.RunOnMaster)]
     public class AgentWriterBenchmark
     {
         private const int SpanCount = 1000;
 
         private IAgentWriter _agentWriter;
+        private IAgentWriter _agentWriterNoOpFlush;
         private ArraySegment<Span> _enrichedSpans;
 
         [GlobalSetup]
@@ -32,19 +33,23 @@ namespace Benchmarks.Trace
 
             for (int i = 0; i < SpanCount; i++)
             {
-                enrichedSpans[i] = new Span(new SpanContext((TraceId)i, (ulong)i, SamplingPriorityValues.UserReject, serviceName: "Benchmark", origin: null), now);
-                enrichedSpans[i].SetTag(Tags.Env, "Benchmark");
+                var tags = new SqlTags()
+                {
+                    DbType = "sql-server",
+                    InstrumentationName = nameof(IntegrationId.SqlClient),
+                };
+                enrichedSpans[i] = new Span(new SpanContext((TraceId)i, (ulong)i, SamplingPriorityValues.UserReject, serviceName: "Benchmark", origin: null), now, tags);
+                enrichedSpans[i].SetTag("somekey", "Benchmark");
                 enrichedSpans[i].SetMetric(Metrics.SamplingRuleDecision, 1.0);
             }
 
             _enrichedSpans = new ArraySegment<Span>(enrichedSpans);
 
-            var overrides = new NameValueConfigurationSource(new()
+            var sources = new NameValueConfigurationSource(new()
             {
                 { ConfigurationKeys.StartupDiagnosticLogEnabled, false.ToString() },
                 { ConfigurationKeys.TraceEnabled, false.ToString() },
             });
-            var sources = new CompositeConfigurationSource(new[] { overrides, GlobalConfigurationSource.Instance });
             var settings = new TracerSettings(sources);
 
             var api = new Api(
@@ -54,16 +59,31 @@ namespace Benchmarks.Trace
                 partialFlushEnabled: false,
                 healthMetricsEnabled: false);
 
-            _agentWriter = new AgentWriter(api, statsAggregator: null, statsd: null, automaticFlush: false);
+            var noOpStatsd = new StatsdManager(settings, (_, _) => null);
+            var noopApi = new NullApi();
+            _agentWriter = new AgentWriter(api, statsAggregator: null, statsd: noOpStatsd, automaticFlush: false);
+            _agentWriterNoOpFlush = new AgentWriter(noopApi, statsAggregator: null, statsd: noOpStatsd, automaticFlush: false);
 
             // Warmup to reduce noise
             WriteAndFlushEnrichedTraces().GetAwaiter().GetResult();
         }
 
         /// <summary>
+        /// Write realistic traces, but don't flush, to isolate overhead from serialization only
+        /// </summary>
+        [Benchmark]
+        public Task WriteEnrichedTraces()
+        {
+            _agentWriterNoOpFlush.WriteTrace(_enrichedSpans);
+            // Flush os that we clear the buffer
+            return _agentWriterNoOpFlush.FlushTracesAsync();
+        }
+
+        /// <summary>
         /// Same as WriteAndFlushTraces but with more realistic traces (with tags and metrics)
         /// </summary>
         [Benchmark]
+        [BenchmarkCategory(Constants.TracerCategory, Constants.RunOnPrs, Constants.RunOnMaster)]
         public Task WriteAndFlushEnrichedTraces()
         {
             _agentWriter.WriteTrace(_enrichedSpans);
@@ -71,7 +91,7 @@ namespace Benchmarks.Trace
         }
 
         /// <summary>
-        /// Try to mimick as much as possible the overhead of the ApiWebRequestFactory,
+        /// Try to mimic as much as possible the overhead of the ApiWebRequestFactory,
         /// without actually sending the requests
         /// </summary>
         private class FakeApiRequestFactory : IApiRequestFactory
@@ -182,5 +202,18 @@ namespace Benchmarks.Trace
             {
             }
         }
+
+            private class NullApi : IApi
+            {
+                public Task<bool> SendTracesAsync(ArraySegment<byte> traces, int numberOfTraces, bool statsComputationEnabled, long numberOfDroppedP0Traces, long numberOfDroppedP0Spans, bool apmTracingEnabled = true)
+                {
+                    return Task.FromResult(true);
+                }
+
+                public Task<bool> SendStatsAsync(StatsBuffer stats, long bucketDuration)
+                {
+                    return Task.FromResult(true);
+                }
+            }
     }
 }