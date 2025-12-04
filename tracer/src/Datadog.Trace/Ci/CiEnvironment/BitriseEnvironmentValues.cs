ce147db956c9484edece461b254d12d13ed28f0a
Anna <anna.yafi@datadoghq.com>
2025-12-04 17:19:44 +0100

    Adapt ci visibility value provider

--- tracer/src/Datadog.Trace/Ci/CiEnvironment/BitriseEnvironmentValues.cs
+++ tracer/src/Datadog.Trace/Ci/CiEnvironment/BitriseEnvironmentValues.cs
@@ -1,9 +1,10 @@
-﻿// <copyright file="BitriseEnvironmentValues.cs" company="Datadog">
+// <copyright file="BitriseEnvironmentValues.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 #nullable enable
 
+using Datadog.Trace.Configuration;
 using Datadog.Trace.Telemetry.Metrics;
 
 namespace Datadog.Trace.Ci.CiEnvironment;
@@ -18,33 +19,33 @@ internal sealed class BitriseEnvironmentValues<TValueProvider>(TValueProvider va
         IsCI = true;
         Provider = "bitrise";
         MetricTag = MetricTags.CIVisibilityTestSessionProvider.Bitrise;
-        Repository = ValueProvider.GetValue(Constants.BitriseGitRepositoryUrl);
+        Repository = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitRepositoryUrl);
 
-        var prCommit = ValueProvider.GetValue(Constants.BitriseGitCommit);
-        Commit = !string.IsNullOrWhiteSpace(prCommit) ? prCommit : ValueProvider.GetValue(Constants.BitriseGitCloneCommitHash);
+        var prCommit = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitCommit);
+        Commit = !string.IsNullOrWhiteSpace(prCommit) ? prCommit : ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitCloneCommitHash);
 
-        var prBranch = ValueProvider.GetValue(Constants.BitrisePullRequestHeadBranch);
-        Branch = !string.IsNullOrWhiteSpace(prBranch) ? prBranch : ValueProvider.GetValue(Constants.BitriseGitBranch);
+        var prBranch = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.PullRequestHeadBranch);
+        Branch = !string.IsNullOrWhiteSpace(prBranch) ? prBranch : ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitBranch);
 
-        Tag = ValueProvider.GetValue(Constants.BitriseGitTag);
-        SourceRoot = ValueProvider.GetValue(Constants.BitriseSourceDir);
-        WorkspacePath = ValueProvider.GetValue(Constants.BitriseSourceDir);
-        PipelineId = ValueProvider.GetValue(Constants.BitriseBuildSlug);
-        PipelineNumber = ValueProvider.GetValue(Constants.BitriseBuildNumber);
-        PipelineName = ValueProvider.GetValue(Constants.BitriseTriggeredWorkflowId);
-        PipelineUrl = ValueProvider.GetValue(Constants.BitriseBuildUrl);
+        Tag = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitTag);
+        SourceRoot = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.SourceDir);
+        WorkspacePath = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.SourceDir);
+        PipelineId = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.BuildSlug);
+        PipelineNumber = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.BuildNumber);
+        PipelineName = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.TriggeredWorkflowId);
+        PipelineUrl = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.BuildUrl);
 
-        Message = ValueProvider.GetValue(Constants.BitriseGitMessage);
-        AuthorName = ValueProvider.GetValue(Constants.BitriseCloneCommitAuthorName);
-        AuthorEmail = ValueProvider.GetValue(Constants.BitriseCloneCommitAuthorEmail);
-        CommitterName = ValueProvider.GetValue(Constants.BitriseCloneCommitCommiterName);
-        CommitterEmail = ValueProvider.GetValue(Constants.BitriseCloneCommitCommiterEmail);
+        Message = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitMessage);
+        AuthorName = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.CloneCommitAuthorName);
+        AuthorEmail = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.CloneCommitAuthorEmail);
+        CommitterName = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.CloneCommitCommiterName);
+        CommitterEmail = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.CloneCommitCommiterEmail);
         if (string.IsNullOrWhiteSpace(CommitterEmail))
         {
             CommitterEmail = CommitterName;
         }
 
-        PrBaseBranch = ValueProvider.GetValue(Constants.BitriseGitBranchDest);
-        PrNumber = ValueProvider.GetValue(Constants.BitrisePullRequestNumber);
+        PrBaseBranch = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.GitBranchDest);
+        PrNumber = ValueProvider.GetValue(PlatformKeys.Ci.Bitrise.PullRequestNumber);
     }
 }
--- tracer/src/Datadog.Trace/Ci/CiEnvironment/GitlabEnvironmentValues.cs
+++ tracer/src/Datadog.Trace/Ci/CiEnvironment/GitlabEnvironmentValues.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="GitlabEnvironmentValues.cs" company="Datadog">
+// <copyright file="GitlabEnvironmentValues.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using Datadog.Trace.Configuration;
 using Datadog.Trace.Telemetry.Metrics;
 
 namespace Datadog.Trace.Ci.CiEnvironment;
@@ -20,44 +21,44 @@ internal sealed class GitlabEnvironmentValues<TValueProvider>(TValueProvider val
         IsCI = true;
         Provider = "gitlab";
         MetricTag = MetricTags.CIVisibilityTestSessionProvider.Gitlab;
-        Repository = ValueProvider.GetValue(Constants.GitlabRepositoryUrl);
-        Commit = ValueProvider.GetValue(Constants.GitlabCommitSha);
-        Branch = ValueProvider.GetValue(Constants.GitlabCommitBranch);
-        Tag = ValueProvider.GetValue(Constants.GitlabCommitTag);
+        Repository = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.RepositoryUrl);
+        Commit = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.CommitSha);
+        Branch = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.CommitBranch);
+        Tag = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.CommitTag);
         if (string.IsNullOrWhiteSpace(Branch))
         {
-            Branch = ValueProvider.GetValue(Constants.GitlabCommitRefName);
+            Branch = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.CommitRefName);
         }
 
-        SourceRoot = ValueProvider.GetValue(Constants.GitlabProjectDir);
-        WorkspacePath = ValueProvider.GetValue(Constants.GitlabProjectDir);
+        SourceRoot = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.ProjectDir);
+        WorkspacePath = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.ProjectDir);
 
-        PipelineId = ValueProvider.GetValue(Constants.GitlabPipelineId);
-        PipelineName = ValueProvider.GetValue(Constants.GitlabProjectPath);
-        PipelineNumber = ValueProvider.GetValue(Constants.GitlabPipelineIId);
-        PipelineUrl = ValueProvider.GetValue(Constants.GitlabPipelineUrl);
+        PipelineId = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.PipelineId);
+        PipelineName = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.ProjectPath);
+        PipelineNumber = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.PipelineIId);
+        PipelineUrl = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.PipelineUrl);
 
-        JobUrl = ValueProvider.GetValue(Constants.GitlabJobUrl);
-        JobId = ValueProvider.GetValue(Constants.GitlabJobId);
-        JobName = ValueProvider.GetValue(Constants.GitlabJobName);
-        StageName = ValueProvider.GetValue(Constants.GitlabJobStage);
+        JobUrl = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.JobUrl);
+        JobId = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.JobId);
+        JobName = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.JobName);
+        StageName = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.JobStage);
 
-        Message = ValueProvider.GetValue(Constants.GitlabCommitMessage);
+        Message = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.CommitMessage);
 
-        var author = ValueProvider.GetValue(Constants.GitlabCommitAuthor);
+        var author = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.CommitAuthor);
         var authorArray = author?.Split('<', '>');
         AuthorName = authorArray?[0].Trim();
         AuthorEmail = authorArray?[1].Trim();
 
-        var authorDate = GetDateTimeOffsetVariableIfIsNotEmpty(Constants.GitlabCommitTimestamp, null);
+        var authorDate = GetDateTimeOffsetVariableIfIsNotEmpty(PlatformKeys.Ci.GitLab.CommitTimestamp, null);
         if (authorDate is not null)
         {
             AuthorDate = authorDate;
         }
 
         // Node
-        NodeName = ValueProvider.GetValue(Constants.GitlabRunnerId);
-        if (ValueProvider.GetValue(Constants.GitlabRunnerTags) is { } runnerTags)
+        NodeName = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.RunnerId);
+        if (ValueProvider.GetValue(PlatformKeys.Ci.GitLab.RunnerTags) is { } runnerTags)
         {
             try
             {
@@ -65,21 +66,21 @@ internal sealed class GitlabEnvironmentValues<TValueProvider>(TValueProvider val
             }
             catch (Exception ex)
             {
-                Log.Warning(ex, "Error deserializing '{GitlabRunnerTags}' environment variable.", Constants.GitlabRunnerTags);
+                Log.Warning(ex, "Error deserializing '{GitlabRunnerTags}' environment variable.", PlatformKeys.Ci.GitLab.RunnerTags);
             }
         }
 
         VariablesToBypass = new Dictionary<string, string?>();
         SetVariablesIfNotEmpty(
             VariablesToBypass,
-            Constants.GitlabProjectUrl,
-            Constants.GitlabPipelineId,
-            Constants.GitlabJobId);
+            PlatformKeys.Ci.GitLab.ProjectUrl,
+            PlatformKeys.Ci.GitLab.PipelineId,
+            PlatformKeys.Ci.GitLab.JobId);
 
-        HeadCommit = ValueProvider.GetValue(Constants.GitlabMergeRequestSourceBranchSha);
-        PrBaseHeadCommit = ValueProvider.GetValue(Constants.GitlabMergeRequestTargetBranchSha);
-        PrBaseCommit = ValueProvider.GetValue(Constants.GitlabMergeRequestDiffBaseSha);
-        PrBaseBranch = ValueProvider.GetValue(Constants.GitlabMergeRequestTargetBranchName);
-        PrNumber = ValueProvider.GetValue(Constants.GitlabMergeRequestId);
+        HeadCommit = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.MergeRequestSourceBranchSha);
+        PrBaseHeadCommit = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.MergeRequestTargetBranchSha);
+        PrBaseCommit = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.MergeRequestDiffBaseSha);
+        PrBaseBranch = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.MergeRequestTargetBranchName);
+        PrNumber = ValueProvider.GetValue(PlatformKeys.Ci.GitLab.MergeRequestId);
     }
 }
