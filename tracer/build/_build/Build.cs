7b883801cbe2c0d6000c4e39ab83e62cc3ea209a
andrewlock <andrewlock@users.noreply.github.com>
2025-12-09 12:03:29 +0000

    [Version Bump] 3.32.1

--- tracer/build/_build/Build.cs
+++ tracer/build/_build/Build.cs
@@ -65,7 +65,7 @@ partial class Build : NukeBuild
     const int LatestMajorVersion = 3;
 
     [Parameter("The current version of the source and build")]
-    readonly string Version = "3.32.0";
+    readonly string Version = "3.32.1";
 
     [Parameter("Whether the current build version is a prerelease(for packaging purposes)")]
     readonly bool IsPrerelease = false;
--- tracer/build/artifacts/dd-dotnet.sh
+++ tracer/build/artifacts/dd-dotnet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TRACER_VERSION="3.32.0"
+TRACER_VERSION="3.32.1"
 
 # Get the directory of the script
 DIR=$(dirname "$(readlink -f "$0")")
