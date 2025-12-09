5f61e7630d2be08853010f11646104dcac5dd253
Andrew Lock <andrew.lock@datadoghq.com>
2025-12-09 11:39:35 +0000

    Add additional overload to WaitOrDump

--- tracer/test/Datadog.Trace.Tests/Util/MutexHelper.cs
+++ tracer/test/Datadog.Trace.Tests/Util/MutexHelper.cs
@@ -19,8 +19,24 @@ internal static class MutexHelper
     /// If the mutex is not set, take a memory dump, and return the result
     /// </summary>
     /// <param name="mutex">The mutex to wait for</param>
-    /// <param name="timeout">How long to wait for the mutext to be set</param>
+    /// <param name="timeout">How long to wait for the mutex to be set, in ms</param>
+    /// <param name="output">Optional output parameter</param>
+    /// <param name="caller">The caller, populated automatically</param>
+    public static bool WaitOrDump(this ManualResetEventSlim mutex, int timeout, ITestOutputHelper output = null, [CallerMemberName] string caller = null)
+        => DoWaitOrDump(mutex, TimeSpan.FromMilliseconds(timeout), output, caller);
+
+    /// <summary>
+    /// Wait for the <paramref name="mutex"/> to be set for the specified <paramref name="timeout"/>.
+    /// If the mutex is not set, take a memory dump, and return the result
+    /// </summary>
+    /// <param name="mutex">The mutex to wait for</param>
+    /// <param name="timeout">How long to wait for the mutex to be set, in ms</param>
+    /// <param name="output">Optional output parameter</param>
+    /// <param name="caller">The caller, populated automatically</param>
     public static bool WaitOrDump(this ManualResetEventSlim mutex, TimeSpan timeout, ITestOutputHelper output = null, [CallerMemberName] string caller = null)
+        => DoWaitOrDump(mutex, timeout, output, caller);
+
+    private static bool DoWaitOrDump(ManualResetEventSlim mutex, TimeSpan timeout, ITestOutputHelper output, string caller)
     {
         var result = mutex.Wait(timeout);
         if (!result)
@@ -28,8 +44,8 @@ internal static class MutexHelper
             output?.WriteLine($"Mutex timed out in {caller}. Capturing memory dump");
             var process = Process.GetCurrentProcess();
             var log = MemoryDumpHelper.CaptureMemoryDump(process)
-                             ? "Successfully captured memory dump"
-                             : "Failed to capture memory dump";
+                          ? "Successfully captured memory dump"
+                          : "Failed to capture memory dump";
             output?.WriteLine(log);
         }
 
