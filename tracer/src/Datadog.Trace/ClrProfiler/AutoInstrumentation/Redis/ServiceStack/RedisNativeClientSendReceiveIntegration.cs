0d03dcae69ef88fe0d57d7a7f0204dcbdbe99b95
Andrew Lock <andrew.lock@datadoghq.com>
2025-12-09 09:02:39 +0000

    Revert incorrect change

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Redis/ServiceStack/RedisNativeClientSendReceiveIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Redis/ServiceStack/RedisNativeClientSendReceiveIntegration.cs
@@ -22,7 +22,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Redis.ServiceStack
         ReturnTypeName = "!!0",
         ParameterTypeNames = new[] { "System.Byte[][]", "System.Func`1[!!0]", "System.Action`1[System.Func`1[!!0]]", ClrNames.Bool },
         MinimumVersion = "4.0.0",
-        MaximumVersion = "10.*.*",
+        MaximumVersion = "6.*.*",
         IntegrationName = IntegrationName)]
     [Browsable(false)]
     [EditorBrowsable(EditorBrowsableState.Never)]
