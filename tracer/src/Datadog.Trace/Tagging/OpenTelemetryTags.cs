b36d9d6fbb5583ea3f34d01f8767493a8490884e
Andrew Lock <andrew.lock@datadoghq.com>
2026-01-08 11:39:22 +0000

    Optimize the handling of OTel tags in OtlpHelpers.AgentConvertSpan to reduce allocations

--- tracer/src/Datadog.Trace/Tagging/OpenTelemetryTags.cs
+++ tracer/src/Datadog.Trace/Tagging/OpenTelemetryTags.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using Datadog.Trace.SourceGenerators;
 
 namespace Datadog.Trace.Tagging
@@ -10,39 +12,18 @@ namespace Datadog.Trace.Tagging
     internal partial class OpenTelemetryTags : TagsList
     {
         [Tag(Tags.SpanKind)]
-        public virtual string SpanKind { get; set; }
+        public virtual string? SpanKind { get; set; }
 
-        [Tag("http.request.method")]
-        public string HttpRequestMethod { get; set; }
+        [Tag("otel.trace_id")]
+        public virtual string? OtelTraceId { get; set; }
 
-        [Tag("db.system")]
-        public string DbSystem { get; set; }
+        [Tag("otel.library.name")]
+        public virtual string? OtelLibraryName { get; set; }
 
-        [Tag("messaging.system")]
-        public string MessagingSystem { get; set; }
+        [Tag("otel.library.version")]
+        public virtual string? OtelLibraryVersion { get; set; }
 
-        [Tag(Trace.Tags.MessagingOperation)]
-        public string MessagingOperation { get; set; }
-
-        [Tag("rpc.system")]
-        public string RpcSystem { get; set; }
-
-        [Tag("rpc.service")]
-        public string RpcService { get; set; }
-
-        [Tag("faas.invoked_provider")]
-        public string FaasInvokedProvider { get; set; }
-
-        [Tag("faas.invoked_name")]
-        public string FaasInvokedName { get; set; }
-
-        [Tag("faas.trigger")]
-        public string FaasTrigger { get; set; }
-
-        [Tag("graphql.operation.type")]
-        public string GraphQlOperationType { get; set; }
-
-        [Tag("network.protocol.name")]
-        public string NetworkProtocolName { get; set; }
+        [Tag("otel.status_code")]
+        public virtual string? OtelStatusCode { get; set; }
     }
 }
