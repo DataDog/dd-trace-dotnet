419a9f702a026542465a0a872ca19837eebd70e5
Mohammad Islam <mohammad.islam@datadoghq.com>
2026-01-14 14:53:37 -0500

    add #nullable enable

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MassTransit/MassTransitConstants.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MassTransit/MassTransitConstants.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using Datadog.Trace.Configuration;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MassTransit
--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MassTransit/MassTransitTags.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MassTransit/MassTransitTags.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using Datadog.Trace.SourceGenerators;
 using Datadog.Trace.Tagging;
 
@@ -22,66 +24,66 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MassTransit
         public string InstrumentationName => MassTransitConstants.IntegrationName;
 
         [Tag("messaging.operation")]
-        public string MessagingOperation { get; set; }
+        public string? MessagingOperation { get; set; }
 
         [Tag("messaging.system")]
-        public string MessagingSystem { get; set; }
+        public string? MessagingSystem { get; set; }
 
         [Tag("messaging.destination.name")]
-        public string DestinationName { get; set; }
+        public string? DestinationName { get; set; }
 
         [Tag("messaging.masstransit.message_id")]
-        public string MessageId { get; set; }
+        public string? MessageId { get; set; }
 
         [Tag("messaging.message.conversation_id")]
-        public string ConversationId { get; set; }
+        public string? ConversationId { get; set; }
 
         [Tag("messaging.masstransit.source_address")]
-        public string SourceAddress { get; set; }
+        public string? SourceAddress { get; set; }
 
         [Tag("messaging.masstransit.destination_address")]
-        public string DestinationAddress { get; set; }
+        public string? DestinationAddress { get; set; }
 
         // MT8 OTEL has input_address on receive spans
         [Tag("messaging.masstransit.input_address")]
-        public string InputAddress { get; set; }
+        public string? InputAddress { get; set; }
 
         [Tag("messaging.masstransit.message_types")]
-        public string MessageTypes { get; set; }
+        public string? MessageTypes { get; set; }
 
         [Tag("messaging.message.body.size")]
-        public string MessageSize { get; set; }
+        public string? MessageSize { get; set; }
 
         [Tag("messaging.masstransit.initiator_id")]
-        public string InitiatorId { get; set; }
+        public string? InitiatorId { get; set; }
 
         [Tag("messaging.masstransit.request_id")]
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         [Tag("messaging.masstransit.response_address")]
-        public string ResponseAddress { get; set; }
+        public string? ResponseAddress { get; set; }
 
         [Tag("messaging.masstransit.fault_address")]
-        public string FaultAddress { get; set; }
+        public string? FaultAddress { get; set; }
 
         // Saga state machine tags (matching MT8 OTEL instrumentation)
         [Tag("messaging.masstransit.begin_state")]
-        public string BeginState { get; set; }
+        public string? BeginState { get; set; }
 
         [Tag("messaging.masstransit.end_state")]
-        public string EndState { get; set; }
+        public string? EndState { get; set; }
 
         [Tag("messaging.masstransit.correlation_id")]
-        public string CorrelationId { get; set; }
+        public string? CorrelationId { get; set; }
 
         [Tag("messaging.masstransit.saga_id")]
-        public string SagaId { get; set; }
+        public string? SagaId { get; set; }
 
         // Additional MT8 OTEL tags
         [Tag("peer.address")]
-        public string PeerAddress { get; set; }
+        public string? PeerAddress { get; set; }
 
         [Tag("messaging.masstransit.consumer_type")]
-        public string ConsumerType { get; set; }
+        public string? ConsumerType { get; set; }
     }
 }
