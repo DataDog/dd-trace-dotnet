DataDog/dd-trace-dotnet

commit 1472f1b2b77d0d0741152007c20816f8296a7e3c

Don't do `DynamicMethod` based allocation-free enumeration on .NET 10 (#8058)
## Summary of changes

Don't use the `DynamicMethod` approach in .NET 10

## Reason for change

In #8041 we added allocation-free enumeration of tags objects by
building a `DynamicMethod` that calls the `struct` method to avoid
boxing. I benchmarked it on a bunch of TFMs, but missed .NET 10.

However, .NET 10 enumeration is _already_ allocation free, so the
`DynamicMethod` actually hurts performance (presumably primarily because
it messes with other inlining and stack allocation improvements the JIT
can do), so we _shouldn't_ use the `DynamicMethod` approach in .NET 10 😅

## Implementation details

- Just do a "normal" enumeration of the tags if it's .NET 10
- Add .NET 10 to the benchmark project TFMs

## Test coverage

Did a quick benchmark comparing naive enumeration of the tag objects of
a duck typed activity with and without the dynamic method approach. In
general, the results are better for `DynamicMethod` in all these TFMs
_except_ .NET 10 (I tested .NET 8/9 previously and confirmed
`DynamicMEthod` is better there too)


| Method | Runtime | Mean | Error | StdDev | Median | Allocated |
| -------------------------- | ------------------ | -------: | -------:
| -------: | -------: | --------: |
| EnumerateTags | .NET 10.0 | 189.9 ns | 6.94 ns | 20.46 ns | 182.6 ns |
592 B |
| EnumerateTagsDynamicMethod | .NET 10.0 | 205.6 ns | 4.14 ns | 8.83 ns
| 205.1 ns | 592 B |
| | | | | | | |
| EnumerateTags | .NET 6.0 | 366.1 ns | 9.49 ns | 27.54 ns | 361.0 ns |
624 B |
| EnumerateTagsDynamicMethod | .NET 6.0 | 307.9 ns | 6.37 ns | 18.29 ns
| 299.4 ns | 592 B |
| | | | | | | |
| EnumerateTags | .NET Core 3.1 | 501.0 ns | 12.15 ns | 35.64 ns | 494.2
ns | 672 B |
| EnumerateTagsDynamicMethod | .NET Core 3.1 | 441.9 ns | 8.83 ns |
25.19 ns | 436.7 ns | 640 B |
| | | | | | | |
| EnumerateTags | .NET Framework 4.8 | 536.9 ns | 10.52 ns | 14.05 ns |
534.7 ns | 746 B |

tracer/src/Datadog.Trace/Activity/Helpers/ActivityEnumerationHelper.cs
@@ -35,6 +35,17 @@ internal static class ActivityEnumerationHelper
     public static void EnumerateTagObjects<T>(T activity5, ref OtelTagsEnumerationState state, TagObjectsEnumerator.ForEachDelegate funcToRun)
         where T : IActivity5
     {
+        if (Environment.Version.Major >= 10)
+        {
+            // .NET 10 doesn't allocate _anyway_, so we should just do naive enumeration
+            foreach (var value in activity5.TagObjects)
+            {
+                funcToRun(ref state, value);
+            }
+
+            return;
+        }
+
         var tagObjects = activity5.TagObjects;
         var runtimeType = tagObjects.GetType();
 
@@ -72,6 +83,17 @@ internal static class ActivityEnumerationHelper
     public static void EnumerateTags<T>(T activity, ref OtelTagsEnumerationState state, TagsEnumerator.ForEachDelegate funcToRun)
         where T : IActivity
     {
+        if (Environment.Version.Major >= 10)
+        {
+            // .NET 10 doesn't allocate _anyway_, so we should just do naive enumeration
+            foreach (var value in activity.Tags)
+            {
+                funcToRun(ref state, value);
+            }
+
+            return;
+        }
+
         var tags = activity.Tags;
         var runtimeType = tags.GetType();
 

tracer/test/benchmarks/Benchmarks.Trace/Benchmarks.Trace.csproj
@@ -2,7 +2,7 @@
 
   <PropertyGroup>
     <OutputType>Exe</OutputType>
-    <TargetFrameworks>net9.0;net8.0;net7.0;net6.0;netcoreapp3.1;net472</TargetFrameworks>
+    <TargetFrameworks>net10.0;net9.0;net8.0;net7.0;net6.0;netcoreapp3.1;net472</TargetFrameworks>
     <GenerateDocumentationFile>false</GenerateDocumentationFile>
 
     <!-- benchmarkdotnet only support numberic values, not "latest"-->
@@ -20,6 +20,7 @@
     <NoDefaultLaunchSettingsFile>true</NoDefaultLaunchSettingsFile>
     <CheckEolTargetFramework>false</CheckEolTargetFramework>
     <SuppressTfmSupportBuildWarnings>true</SuppressTfmSupportBuildWarnings>
+    <LangVersion>latest</LangVersion>
   </PropertyGroup>
 
   <ItemGroup>

commit bbf81d633a027a4c2a80a2563f9af03a48a30b2e

Fix `Environment.Version` causing huge allocation (#8068)
## Summary of changes

- Don't call `Environment.Version` except in `FrameworkDescription`
- Fix `ActivityEnumerationHelper` to use `FrameworkDescription` instead
of `Environment.Version`

## Reason for change

`Environment.Version` allocates every time you access it. So my "improve
performance by checking if we're in .NET 10" PR caused a [massive
allocation
regression](https://app.datadoghq.com/dashboard/9eu-mfe-5ad?fromUser=true&refresh_mode=sliding&tpl_var_branch%5B0%5D=%28master%20OR%20benchmarks%5C%2F%2A%29&tpl_var_runtime%5B0%5D=%2A&from_ts=1767877815370&to_ts=1768482615370&live=true)
💀 😂

<img width="477" height="57" alt="image"
src="https://github.com/user-attachments/assets/62600d31-d089-48b7-9ca6-6bcdd46d0a5a"
/>

So I think it makes sense to cache `Environment.Version` in our
`FrameworkDescription` type, and access it from there.

## Implementation details

- Read (or calculate, where it can't be trusted) the
`Environment.Version` for the app in `FrameworkDescription`
- Expose it as `FrameworkDescription.RuntimeVersion`
- Use the exposed version where possible
- Add `#nullable enable` seeing as I need to think about all that
properly anyway

## Test coverage

This fixes the benchmarks again now 🙄 Everything else should be covered
by existing, but I'll likely follow up with some more tests in a follow
up PR (just don't want to delay this getting in!)

## Other details

The benchmarks _did_ show the regression in the PR
https://github.com/DataDog/dd-trace-dotnet/pull/8058... but I wasn't
looking, because those numbers are so flaky 😬

tracer/src/Datadog.Trace/Activity/Helpers/ActivityEnumerationHelper.cs
@@ -24,6 +24,14 @@ namespace Datadog.Trace.Activity.Helpers;
 internal static class ActivityEnumerationHelper
 {
     private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(ActivityEnumerationHelper));
+
+    private static readonly bool IsDotNet10
+#if NET10_0_OR_GREATER
+        = true;
+#else
+        = FrameworkDescription.Instance.RuntimeVersion.Major >= 10;
+#endif
+
     private static TagObjectsEnumerator.AllocationFreeForEachDelegate? _tagObjectsEnumerator;
     private static TagsEnumerator.AllocationFreeForEachDelegate? _tagsEnumerator;
     private static Type? _tagObjectsType;
@@ -35,7 +43,7 @@ internal static class ActivityEnumerationHelper
     public static void EnumerateTagObjects<T>(T activity5, ref OtelTagsEnumerationState state, TagObjectsEnumerator.ForEachDelegate funcToRun)
         where T : IActivity5
     {
-        if (Environment.Version.Major >= 10)
+        if (IsDotNet10)
         {
             // .NET 10 doesn't allocate _anyway_, so we should just do naive enumeration
             foreach (var value in activity5.TagObjects)
@@ -83,7 +91,7 @@ internal static class ActivityEnumerationHelper
     public static void EnumerateTags<T>(T activity, ref OtelTagsEnumerationState state, TagsEnumerator.ForEachDelegate funcToRun)
         where T : IActivity
     {
-        if (Environment.Version.Major >= 10)
+        if (IsDotNet10)
         {
             // .NET 10 doesn't allocate _anyway_, so we should just do naive enumeration
             foreach (var value in activity.Tags)

tracer/src/Datadog.Trace/FrameworkDescription.NetCore.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 #if !NETFRAMEWORK
 using System;
 using System.IO;
@@ -13,21 +15,20 @@ namespace Datadog.Trace
 {
     internal partial class FrameworkDescription
     {
-        private static FrameworkDescription _instance = null;
+        private const string Unknown = "unknown";
+        private static readonly Lazy<FrameworkDescription> _instance = new(Create);
 
-        public static FrameworkDescription Instance
-        {
-            get { return _instance ?? (_instance = Create()); }
-        }
+        public static FrameworkDescription Instance => _instance.Value;
 
         public static FrameworkDescription Create()
         {
-            var frameworkName = "unknown";
-            var frameworkVersion = "unknown";
-            var osPlatform = "unknown";
-            var osArchitecture = "unknown";
-            var processArchitecture = "unknown";
-            var osDescription = "unknown";
+            string frameworkName = Unknown;
+            string frameworkVersion = Unknown;
+            string osPlatform = Unknown;
+            string osArchitecture = Unknown;
+            string processArchitecture = Unknown;
+            string osDescription = Unknown;
+            Version? runtimeVersion = null;
 
             try
             {
@@ -59,11 +60,11 @@ namespace Datadog.Trace
 
                 osArchitecture = RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant();
                 processArchitecture = RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
-                frameworkVersion = GetNetCoreOrNetFrameworkVersion();
+                GetNetCoreOrNetFrameworkVersion(out runtimeVersion, out frameworkVersion);
 #if NET8_0_OR_GREATER
                 osDescription = RuntimeInformation.OSDescription;
 #else
-                osDescription = GetOsDescription();
+                osDescription = GetOsDescription() ?? Unknown;
 #endif
             }
             catch (Exception ex)
@@ -77,7 +78,8 @@ namespace Datadog.Trace
                 osPlatform: osPlatform,
                 osArchitecture: osArchitecture,
                 processArchitecture: processArchitecture,
-                osDescription: osDescription);
+                osDescription: osDescription,
+                runtimeVersion ?? Environment.Version);
         }
 
         public bool IsCoreClr()
@@ -85,61 +87,61 @@ namespace Datadog.Trace
             return Name.ToLowerInvariant().Contains("core") || IsNet5();
         }
 
-        private static string GetNetCoreOrNetFrameworkVersion()
+        private static void GetNetCoreOrNetFrameworkVersion(out Version version, out string productVersion)
         {
-            string productVersion = null;
-
-            if (Environment.Version.Major == 3 || Environment.Version.Major >= 5)
+            version = Environment.Version;
+            if (version.Major == 3 || version.Major >= 5)
             {
                 // Environment.Version returns "4.x" in .NET Core 2.x,
                 // but it is correct since .NET Core 3.0.0
-                productVersion = Environment.Version.ToString();
+                productVersion = version.ToString();
+                return;
             }
 
-            if (productVersion == null)
+            string? foundVersion = null;
+            try
             {
-                try
-                {
-                    // try to get product version from assembly path
+                // try to get product version from assembly path
 #if NET5_0_OR_GREATER
-                    // Can't use RootAssembly.CodeBase in .NET 5+
-                    var location = RootAssembly.Location;
+                // Can't use RootAssembly.CodeBase in .NET 5+
+                var location = RootAssembly.Location;
 #else
-                    var location = RootAssembly.CodeBase;
+                var location = RootAssembly.CodeBase;
 #endif
-                    Match match = Regex.Match(
-                        location,
-                        @"[\\/][^\\/]*microsoft\.netcore\.app[\\/](\d+\.\d+\.\d+[^/]*)[\\/]",
-                        RegexOptions.IgnoreCase);
+                Match match = Regex.Match(
+                    location,
+                    @"[\\/][^\\/]*microsoft\.netcore\.app[\\/](\d+\.\d+\.\d+[^/]*)[\\/]",
+                    RegexOptions.IgnoreCase);
 
-                    if (match.Success && match.Groups.Count > 0 && match.Groups[1].Success)
-                    {
-                        productVersion = match.Groups[1].Value;
-                    }
-                }
-                catch (Exception e)
+                if (match.Success && match.Groups.Count > 0 && match.Groups[1].Success)
                 {
-                    Log.Error(e, "Error getting .NET Core version from assembly path");
+                    foundVersion = match.Groups[1].Value;
                 }
             }
-
-            if (productVersion == null)
+            catch (Exception e)
             {
-                // if we fail to extract version from assembly path,
-                // fall back to the [AssemblyInformationalVersion] or [AssemblyFileVersion]
-                productVersion = GetVersionFromAssemblyAttributes();
+                Log.Error(e, "Error getting .NET Core version from assembly path");
             }
 
-            if (productVersion == null)
+            // if we fail to extract version from assembly path,
+            // fall back to the [AssemblyInformationalVersion] or [AssemblyFileVersion]
+            foundVersion ??= GetVersionFromAssemblyAttributes();
+            if (foundVersion != null)
             {
-                // at this point, everything else has failed (this is probably the same as [AssemblyFileVersion] above)
-                productVersion = Environment.Version.ToString();
+                productVersion = foundVersion;
+                if (Version.TryParse(foundVersion, out var parsedVersion))
+                {
+                    version = parsedVersion;
+                }
+
+                return;
             }
 
-            return productVersion;
+            // at this point, everything else has failed (this is probably the same as [AssemblyFileVersion] above)
+            productVersion = version.ToString();
         }
 
-        private static string GetOsDescription()
+        private static string? GetOsDescription()
         {
             if (RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
             {
@@ -194,7 +196,7 @@ namespace Datadog.Trace
 #else
                 // Parse the NAME, PRETTY_NAME, and VERSION fields.
                 // These fields are suitable for presentation to the user.
-                string prettyName = default, name = default, version = default;
+                string? prettyName = default, name = default, version = default;
                 foreach (string line in lines)
                 {
                     _ = TryGetFieldValue(line, "PRETTY_NAME=", ref prettyName) ||
@@ -246,7 +248,7 @@ namespace Datadog.Trace
                 return true;
             }
 #else
-            static bool TryGetFieldValue(string line, string prefix, ref string value)
+            static bool TryGetFieldValue(string line, string prefix, ref string? value)
             {
                 if (!line.StartsWith(prefix))
                 {

tracer/src/Datadog.Trace/FrameworkDescription.NetFramework.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 #if NETFRAMEWORK
 using System;
 using System.Linq;
@@ -12,26 +14,25 @@ namespace Datadog.Trace
 {
     internal partial class FrameworkDescription
     {
-        private static FrameworkDescription _instance = null;
+        private const string Unknown = "unknown";
+        private static readonly Lazy<FrameworkDescription> _instance = new(Create);
 
-        public static FrameworkDescription Instance
-        {
-            get { return _instance ?? (_instance = Create()); }
-        }
+        public static FrameworkDescription Instance => _instance.Value;
 
         public static FrameworkDescription Create()
         {
-            var osArchitecture = "unknown";
-            var processArchitecture = "unknown";
-            var frameworkVersion = "unknown";
-            var osDescription = "unknown";
+            var osArchitecture = Unknown;
+            var processArchitecture = Unknown;
+            var frameworkVersion = Unknown;
+            var osDescription = Unknown;
+            Version? runtimeVersion = null;
 
             try
             {
                 osDescription = Environment.OSVersion.VersionString;
                 osArchitecture = Environment.Is64BitOperatingSystem ? "x64" : "x86";
                 processArchitecture = Environment.Is64BitProcess ? "x64" : "x86";
-                frameworkVersion = GetNetFrameworkVersion();
+                GetNetFrameworkVersion(out frameworkVersion, out runtimeVersion);
             }
             catch (Exception ex)
             {
@@ -44,7 +45,8 @@ namespace Datadog.Trace
                 osPlatform: "Windows",
                 osArchitecture: osArchitecture,
                 processArchitecture: processArchitecture,
-                osDescription: osDescription);
+                osDescription: osDescription,
+                runtimeVersion ?? Environment.Version);
         }
 
         public bool IsCoreClr()
@@ -52,13 +54,11 @@ namespace Datadog.Trace
             return false;
         }
 
-        private static string GetNetFrameworkVersion()
+        private static void GetNetFrameworkVersion(out string frameworkVersion, out Version version)
         {
-            string productVersion = null;
-
             try
             {
-                object registryValue;
+                object? registryValue;
 
                 using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default))
                 using (var subKey = baseKey.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"))
@@ -70,7 +70,12 @@ namespace Datadog.Trace
                 {
                     // find the known version on the list with the largest release number
                     // that is lower than or equal to the release number in the Windows Registry
-                    productVersion = DotNetFrameworkVersionMapping.FirstOrDefault(t => release >= t.Item1)?.Item2;
+                    if (GetDotNetFrameworkProductMapping(release, out var productVersion, out var runtime))
+                    {
+                        frameworkVersion = productVersion;
+                        version = runtime;
+                        return;
+                    }
                 }
             }
             catch (Exception e)
@@ -78,20 +83,26 @@ namespace Datadog.Trace
                 Log.Error(e, "Error getting .NET Framework version from Windows Registry");
             }
 
-            if (productVersion == null)
+            // if we fail to extract version from assembly path,
+            // fall back to the [AssemblyInformationalVersion] or [AssemblyFileVersion]
+            if (GetVersionFromAssemblyAttributes() is { } foundVersion)
             {
-                // if we fail to extract version from assembly path,
-                // fall back to the [AssemblyInformationalVersion] or [AssemblyFileVersion]
-                productVersion = GetVersionFromAssemblyAttributes();
+                frameworkVersion = foundVersion;
+                if (Version.TryParse(foundVersion, out var parsedVersion))
+                {
+                    version = parsedVersion;
+                }
+                else
+                {
+                    version = Environment.Version;
+                }
+
+                return;
             }
 
-            if (productVersion == null)
-            {
-                // at this point, everything else has failed (this is probably the same as [AssemblyFileVersion] above)
-                productVersion = Environment.Version.ToString();
-            }
-
-            return productVersion;
+            // at this point, everything else has failed (this is probably the same as [AssemblyFileVersion] above)
+            version = Environment.Version;
+            frameworkVersion = version.ToString();
         }
     }
 }

tracer/src/Datadog.Trace/FrameworkDescription.cs
@@ -3,7 +3,10 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#nullable enable
+
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Datadog.Trace.Logging;
 
@@ -15,29 +18,14 @@ namespace Datadog.Trace
 
         private static readonly Assembly RootAssembly = typeof(object).Assembly;
 
-        private static readonly Tuple<int, string>[] DotNetFrameworkVersionMapping =
-        {
-            // known min value for each framework version
-            Tuple.Create(533325, "4.8.1"),
-            Tuple.Create(528040, "4.8"),
-            Tuple.Create(461808, "4.7.2"),
-            Tuple.Create(461308, "4.7.1"),
-            Tuple.Create(460798, "4.7"),
-            Tuple.Create(394802, "4.6.2"),
-            Tuple.Create(394254, "4.6.1"),
-            Tuple.Create(393295, "4.6"),
-            Tuple.Create(379893, "4.5.2"),
-            Tuple.Create(378675, "4.5.1"),
-            Tuple.Create(378389, "4.5"),
-        };
-
         private FrameworkDescription(
             string name,
             string productVersion,
             string osPlatform,
             string osArchitecture,
             string processArchitecture,
-            string osDescription)
+            string osDescription,
+            Version runtimeVersion)
         {
             Name = name;
             ProductVersion = productVersion;
@@ -45,6 +33,7 @@ namespace Datadog.Trace
             OSArchitecture = osArchitecture;
             OSDescription = osDescription;
             ProcessArchitecture = processArchitecture;
+            RuntimeVersion = runtimeVersion;
         }
 
         public string Name { get; }
@@ -58,6 +47,8 @@ namespace Datadog.Trace
         public string ProcessArchitecture { get; }
 
         public string OSDescription { get; }
+
+        public Version RuntimeVersion { get; }
 
         public static bool IsNet5()
         {
@@ -77,14 +68,14 @@ namespace Datadog.Trace
             return $"{Name} {ProductVersion} {ProcessArchitecture} on {OSPlatform} {OSArchitecture}";
         }
 
-        private static string GetVersionFromAssemblyAttributes()
+        private static string? GetVersionFromAssemblyAttributes()
         {
-            string productVersion = null;
+            string? productVersion = null;
 
             try
             {
                 // if we fail to extract version from assembly path, fall back to the [AssemblyInformationalVersion],
-                var informationalVersionAttribute = (AssemblyInformationalVersionAttribute)RootAssembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute));
+                var informationalVersionAttribute = (AssemblyInformationalVersionAttribute?)RootAssembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute));
 
                 // split remove the commit hash from pre-release versions
                 productVersion = informationalVersionAttribute?.InformationalVersion?.Split('+')[0];
@@ -99,7 +90,7 @@ namespace Datadog.Trace
                 try
                 {
                     // and if that fails, try [AssemblyFileVersion]
-                    var fileVersionAttribute = (AssemblyFileVersionAttribute)RootAssembly.GetCustomAttribute(typeof(AssemblyFileVersionAttribute));
+                    var fileVersionAttribute = (AssemblyFileVersionAttribute?)RootAssembly.GetCustomAttribute(typeof(AssemblyFileVersionAttribute));
                     productVersion = fileVersionAttribute?.Version;
                 }
                 catch (Exception e)
@@ -109,6 +100,65 @@ namespace Datadog.Trace
             }
 
             return productVersion;
+        }
+
+        private static bool GetDotNetFrameworkProductMapping(
+            int release,
+            [NotNullWhen(true)] out string? productVersion,
+            [NotNullWhen(true)] out Version? runtimeVersion)
+        {
+            switch (release)
+            {
+                // known min value for each framework version
+                case >= 533325:
+                    productVersion = "4.8.1";
+                    runtimeVersion = new Version(4, 8, 1);
+                    return true;
+                case >= 528040:
+                    productVersion = "4.8";
+                    runtimeVersion = new Version(4, 8);
+                    return true;
+                case >= 461808:
+                    productVersion = "4.7.2";
+                    runtimeVersion = new Version(4, 7, 2);
+                    return true;
+                case >= 461308:
+                    productVersion = "4.7.1";
+                    runtimeVersion = new Version(4, 7, 1);
+                    return true;
+                case >= 460798:
+                    productVersion = "4.7";
+                    runtimeVersion = new Version(4, 7);
+                    return true;
+                case >= 394802:
+                    productVersion = "4.6.2";
+                    runtimeVersion = new Version(4, 6, 2);
+                    return true;
+                case >= 394254:
+                    productVersion = "4.6.1";
+                    runtimeVersion = new Version(4, 6, 1);
+                    return true;
+                case >= 393295:
+                    productVersion = "4.6";
+                    runtimeVersion = new Version(4, 6);
+                    return true;
+                case >= 379893:
+                    productVersion = "4.5.2";
+                    runtimeVersion = new Version(4, 5, 2);
+                    return true;
+                case >= 378675:
+                    productVersion = "4.5.1";
+                    runtimeVersion = new Version(4, 5, 1);
+                    return true;
+                case >= 378389:
+                    productVersion = "4.5";
+                    runtimeVersion = new Version(4, 5);
+                    return true;
+            }
+
+            productVersion = null;
+            runtimeVersion = null;
+            return false;
         }
     }
 }