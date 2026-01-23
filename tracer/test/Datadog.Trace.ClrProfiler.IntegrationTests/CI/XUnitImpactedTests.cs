882dbe00eed69e1b1a611336bd80e03c7b09871f
Tony Redondo <tony.redondo@datadoghq.com>
2026-01-23 17:09:58 +0100

    test: use PlatformKeys for Azure env vars

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/XUnitImpactedTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/XUnitImpactedTests.cs
@@ -103,9 +103,9 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.CI
                 SetEnvironmentVariable(ConfigurationKeys.CIVisibility.ImpactedTestsDetectionEnabled, "True");
                 SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Enabled, "1");
                 SetEnvironmentVariable(ConfigurationKeys.CIVisibility.Logs, "1");
-                SetEnvironmentVariable(CIEnvironmentValues.Constants.AzureSystemPullRequestSourceBranch, testBranchName);
-                SetEnvironmentVariable(CIEnvironmentValues.Constants.AzureBuildSourceBranch, testBranchName);
-                SetEnvironmentVariable(CIEnvironmentValues.Constants.AzureBuildSourceBranchName, testBranchName);
+                SetEnvironmentVariable(PlatformKeys.Ci.Azure.SystemPullRequestSourceBranch, testBranchName);
+                SetEnvironmentVariable(PlatformKeys.Ci.Azure.BuildSourceBranch, testBranchName);
+                SetEnvironmentVariable(PlatformKeys.Ci.Azure.BuildSourceBranchName, testBranchName);
 
                 // Run the test submission without GitHub Actions injection
                 await SubmitTestsWithGitBranch(packageVersion, 2, TestIsModified);
