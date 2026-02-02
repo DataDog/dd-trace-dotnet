548d1ac47c5996a56aa4730ba9ec21d492a7ada6
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2026-02-02 14:19:43 -0500

    keep System.Runtime.CompilerServices.Unsafe for Unsafe.IsNullRef()

--- tracer/src/Datadog.Trace/GlobalUsings.cs
+++ tracer/src/Datadog.Trace/GlobalUsings.cs
@@ -8,12 +8,18 @@
 global using Datadog.Trace.ExtensionMethods;
 global using ThrowHelper = Datadog.Trace.Util.ThrowHelper;
 
+#if NET5_0_OR_GREATER
+global using Unsafe = System.Runtime.CompilerServices.Unsafe;
+#else
+// we use some methods from Unsafe that were added in .NET 5
+global using Unsafe = Datadog.Trace.VendoredMicrosoftCode.System.Runtime.CompilerServices.Unsafe.Unsafe;
+#endif
+
 #if NETCOREAPP3_1_OR_GREATER
 global using System.Buffers;
 global using System.Buffers.Binary;
 global using System.Buffers.Text;
 global using System.Collections.Immutable;
-global using Unsafe = System.Runtime.CompilerServices.Unsafe;
 #else
 global using Datadog.Trace.VendoredMicrosoftCode.System;
 global using Datadog.Trace.VendoredMicrosoftCode.System.Buffers;
@@ -22,5 +28,4 @@ global using Datadog.Trace.VendoredMicrosoftCode.System.Buffers.Text;
 global using Datadog.Trace.VendoredMicrosoftCode.System.Collections.Immutable;
 global using Datadog.Trace.VendoredMicrosoftCode.System.Runtime.CompilerServices.Unsafe;
 global using Datadog.Trace.VendoredMicrosoftCode.System.Runtime.InteropServices;
-global using Unsafe = Datadog.Trace.VendoredMicrosoftCode.System.Runtime.CompilerServices.Unsafe.Unsafe;
 #endif
