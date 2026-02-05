e7034b03bbc8f955692c4605c4ba01475cbc8ce3
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2026-02-05 18:04:54 -0500

    clarify comment

--- tracer/src/Datadog.Trace/ClrProfiler/Instrumentation.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/Instrumentation.cs
@@ -515,7 +515,7 @@ namespace Datadog.Trace.ClrProfiler
 
             if (!EnvironmentHelpers.IsAzureFunctions())
             {
-                // we only need to skip in some Azure Functions
+                // we only ever skip AspNetCoreDiagnosticObserver in Azure Functions
                 return false;
             }
 
@@ -526,6 +526,7 @@ namespace Datadog.Trace.ClrProfiler
                 return true;
             }
 
+            // FUNCTIONS_WORKER_RUNTIME == "dotnet-isolated"
             if (!EnvironmentHelpers.IsAzureFunctionsIsolated())
             {
                 // Skip AspNetCoreDiagnosticObserver in in-process Azure Functions
