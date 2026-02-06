a428b31f5186ad2734dc7ba9a81c09e6a88ef969
Zach Montoya <zach.montoya@datadoghq.com>
2026-02-06 09:24:01 -0800

    Add a new IApi.Ping method to separate the initial ping operation from actually sending traces

--- tracer/src/Datadog.Trace/Agent/AgentWriter.cs
+++ tracer/src/Datadog.Trace/Agent/AgentWriter.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="AgentWriter.cs" company="Datadog">
+// <copyright file="AgentWriter.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
--- tracer/src/Datadog.Trace/Agent/IApi.cs
+++ tracer/src/Datadog.Trace/Agent/IApi.cs
@@ -10,6 +10,8 @@ namespace Datadog.Trace.Agent
 {
     internal interface IApi
     {
+        Task<bool> Ping();
+
         Task<bool> SendTracesAsync(ArraySegment<byte> traces, int numberOfTraces, bool statsComputationEnabled, long numberOfDroppedP0Traces, long numberOfDroppedP0Spans, bool apmTracingEnabled = true);
 
         Task<bool> SendStatsAsync(StatsBuffer stats, long bucketDuration);
