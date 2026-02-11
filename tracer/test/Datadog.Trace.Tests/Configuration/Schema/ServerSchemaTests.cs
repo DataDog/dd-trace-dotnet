bdb35c9f15eff5c171b0da23b67ef2d287e1f8da
Andrew Lock <andrew.lock@datadoghq.com>
2026-02-11 14:11:52 +0000

    Update ServerSchema implementation to avoid allocation
    Could be simpler, but keeping the same pattern for usability

--- tracer/test/Datadog.Trace.Tests/Configuration/Schema/ServerSchemaTests.cs
+++ tracer/test/Datadog.Trace.Tests/Configuration/Schema/ServerSchemaTests.cs
@@ -3,7 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.Configuration.Schema;
 using FluentAssertions;
@@ -15,16 +17,16 @@ namespace Datadog.Trace.Tests.Configuration.Schema
     {
         private const string DefaultServiceName = "MyApplication";
 
-        public static IEnumerable<(int SchemaVersion, string Protocol, string ExpectedOperationName)> GetProtocolOperationNameData()
+        public static IEnumerable<(int SchemaVersion, int Protocol, string ExpectedOperationName)> GetProtocolOperationNameData()
         {
-            yield return (0, "grpc", "grpc.request");
-            yield return (1, "grpc", "grpc.server.request");
+            yield return (0, (int)ServerSchema.Protocol.Grpc, "grpc.request");
+            yield return (1, (int)ServerSchema.Protocol.Grpc, "grpc.server.request");
         }
 
-        public static IEnumerable<(int SchemaVersion, string Component, string ExpectedOperationName)> GetComponentOperationNameData()
+        public static IEnumerable<(int SchemaVersion, int Component, string ExpectedOperationName)> GetComponentOperationNameData()
         {
-            yield return (0, "wcf", "wcf.request");
-            yield return (1, "wcf", "http.server.request");
+            yield return (0, (int)ServerSchema.Component.Wcf, "wcf.request");
+            yield return (1, (int)ServerSchema.Component.Wcf, "http.server.request");
         }
 
         public static IEnumerable<(int SchemaVersion, string ExpectedSuffix)> GetOperationNameSuffixForRequestData()
@@ -36,25 +38,25 @@ namespace Datadog.Trace.Tests.Configuration.Schema
         [Theory]
         [CombinatorialData]
         public void GetOperationNameForProtocolIsCorrect(
-            [CombinatorialMemberData(nameof(GetProtocolOperationNameData))] (int SchemaVersion, string Protocol, string ExpectedOperationName) values,
+            [CombinatorialMemberData(nameof(GetProtocolOperationNameData))] (int SchemaVersion, int Protocol, string ExpectedOperationName) values,
             bool peerServiceTagsEnabled,
             bool removeClientServiceNamesEnabled)
         {
             var schemaVersion = (SchemaVersion)values.SchemaVersion;
             var namingSchema = new NamingSchema(schemaVersion, peerServiceTagsEnabled, removeClientServiceNamesEnabled, DefaultServiceName, new Dictionary<string, string>(), new Dictionary<string, string>());
-            namingSchema.Server.GetOperationNameForProtocol(values.Protocol).Should().Be(values.ExpectedOperationName);
+            namingSchema.Server.GetOperationNameForProtocol((ServerSchema.Protocol)values.Protocol).Should().Be(values.ExpectedOperationName);
         }
 
         [Theory]
         [CombinatorialData]
         public void GetOperationNameForComponentIsCorrect(
-            [CombinatorialMemberData(nameof(GetComponentOperationNameData))] (int SchemaVersion, string Component, string ExpectedOperationName) values,
+            [CombinatorialMemberData(nameof(GetComponentOperationNameData))] (int SchemaVersion, int Component, string ExpectedOperationName) values,
             bool peerServiceTagsEnabled,
             bool removeClientServiceNamesEnabled)
         {
             var schemaVersion = (SchemaVersion)values.SchemaVersion;
             var namingSchema = new NamingSchema(schemaVersion, peerServiceTagsEnabled, removeClientServiceNamesEnabled, DefaultServiceName, new Dictionary<string, string>(), new Dictionary<string, string>());
-            namingSchema.Server.GetOperationNameForComponent(values.Component).Should().Be(values.ExpectedOperationName);
+            namingSchema.Server.GetOperationNameForComponent((ServerSchema.Component)values.Component).Should().Be(values.ExpectedOperationName);
         }
 
         [Theory]
@@ -67,6 +69,28 @@ namespace Datadog.Trace.Tests.Configuration.Schema
             var schemaVersion = (SchemaVersion)values.SchemaVersion;
             var namingSchema = new NamingSchema(schemaVersion, peerServiceTagsEnabled, removeClientServiceNamesEnabled, DefaultServiceName, new Dictionary<string, string>(), new Dictionary<string, string>());
             namingSchema.Server.GetOperationNameSuffixForRequest().Should().Be(values.ExpectedSuffix);
+        }
+
+        [Theory]
+        [CombinatorialData]
+        public void GetOperationNameForProtocol_SupportsAllEnumValues([CombinatorialValues(0, 1)]int schemaVersion, bool peerServiceTagsEnabled, bool removeClientServiceNamesEnabled)
+        {
+            var namingSchema = new NamingSchema((SchemaVersion)schemaVersion, peerServiceTagsEnabled, removeClientServiceNamesEnabled, DefaultServiceName, new Dictionary<string, string>(), new Dictionary<string, string>());
+            foreach (var value in Enum.GetValues(typeof(ServerSchema.Protocol)).Cast<ServerSchema.Protocol>())
+            {
+                namingSchema.Server.GetOperationNameForProtocol(value).Should().NotBeNull();
+            }
+        }
+
+        [Theory]
+        [CombinatorialData]
+        public void GetOperationNameForComponent_SupportsAllEnumValues([CombinatorialValues(0, 1)]int schemaVersion, bool peerServiceTagsEnabled, bool removeClientServiceNamesEnabled)
+        {
+            var namingSchema = new NamingSchema((SchemaVersion)schemaVersion, peerServiceTagsEnabled, removeClientServiceNamesEnabled, DefaultServiceName, new Dictionary<string, string>(), new Dictionary<string, string>());
+            foreach (var value in Enum.GetValues(typeof(ServerSchema.Component)).Cast<ServerSchema.Component>())
+            {
+                namingSchema.Server.GetOperationNameForComponent(value).Should().NotBeNull();
+            }
         }
     }
 }
