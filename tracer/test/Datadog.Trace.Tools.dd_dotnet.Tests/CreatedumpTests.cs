2a4e3d473af64ca26704c8d164a4665cac13c8d7
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2026-02-11 10:04:40 +0100

    Fixing everything

--- tracer/test/Datadog.Trace.Tools.dd_dotnet.Tests/CreatedumpTests.cs
+++ tracer/test/Datadog.Trace.Tools.dd_dotnet.Tests/CreatedumpTests.cs
@@ -13,10 +13,10 @@ public class CreatedumpTests
 {
     [SkippableTheory]
     [InlineData("invalid", false, 0, null, null, null, null)]
-    [InlineData("--crashthread 5 --blabla 100 --signal 3 --aaaaaa", true, 100, 3, 4, 5, 0xC0000005)]
-    [InlineData("--crashthread 5 --blabla 99999 --signal 3 --aaaaaa 99998", false, 0, 3, 4, 5, 0xC0000005)] // Two potential PIDs, that probably don't exist
+    [InlineData("--crashthread 5 --blabla 100 --signal 3 --aaaaaa --code 4 --dd-native-exception-code 3221225477", true, 100, 3, 4, 5, 0xC0000005)]
+    [InlineData("--crashthread 5 --blabla 99999 --signal 3 --aaaaaa 99998 --code 4 --dd-native-exception-code 3221225477", false, 0, 3, 4, 5, 0xC0000005)] // Two potential PIDs, that probably don't exist
     [InlineData("10", true, 10, null, null, null, null)]
-    public void ParseCommandLine(string commandLine, bool expectedResult, int expectedPid, int? expectedSignal, int? expectedSignalCode, int? expectedCrashThread, int? expectedExceptionCode)
+    public void ParseCommandLine(string commandLine, bool expectedResult, int expectedPid, int? expectedSignal, int? expectedSignalCode, int? expectedCrashThread, uint? expectedExceptionCode)
     {
         var result = CreatedumpCommand.ParseArguments(commandLine.Split(' '), out var pid, out var signal, out var signalCode, out var crashThread, out var exceptionCode);
 
