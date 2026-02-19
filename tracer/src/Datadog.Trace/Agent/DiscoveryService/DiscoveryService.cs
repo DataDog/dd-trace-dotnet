89c3d8f7e7223a7931cad3fc1b5d0349f8a5dbfa
Raphaël Vandon <raphael.vandon@datadoghq.com>
2026-02-19 14:26:23 +0100

    use prop value to avoid overriding with null

--- tracer/src/Datadog.Trace/Agent/DiscoveryService/DiscoveryService.cs
+++ tracer/src/Datadog.Trace/Agent/DiscoveryService/DiscoveryService.cs
@@ -429,7 +429,7 @@ namespace Datadog.Trace.Agent.DiscoveryService
                 eventPlatformProxyEndpoint: eventPlatformProxyEndpoint,
                 telemetryProxyEndpoint: telemetryProxyEndpoint,
                 tracerFlareEndpoint: tracerFlareEndpoint,
-                containerTagsHash: containerTagsHash,
+                _containerMetadata.ContainerTagsHash, // either the value just received, or the one we stored before (prevents overriding with null)
                 clientDropP0: clientDropP0,
                 spanMetaStructs: spanMetaStructs,
                 spanEvents: spanEvents);
