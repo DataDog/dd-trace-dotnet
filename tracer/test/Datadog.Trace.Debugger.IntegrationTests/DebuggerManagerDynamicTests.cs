2dcb6f95884060379406a3554d15490cd082ff05
dudik <8845578+dudikeleti@users.noreply.github.com>
2026-03-04 17:26:17 +0100

    Make code origin enable by default

--- tracer/test/Datadog.Trace.Debugger.IntegrationTests/DebuggerManagerDynamicTests.cs
+++ tracer/test/Datadog.Trace.Debugger.IntegrationTests/DebuggerManagerDynamicTests.cs
@@ -54,12 +54,10 @@ public class DebuggerManagerDynamicTests : TestHelper
     [Trait("Category", "LinuxUnsupported")]
     public async Task DebuggerManager_DynamicInstrumentation_StartDisabled_EnabledViaRemoteConfig()
     {
-        // Set it true so we won't go through path of no debugger products at all
-        SetEnvironmentVariable(ConfigurationKeys.Debugger.CodeOriginForSpansEnabled, "true");
         SetEnvironmentVariable(ConfigurationKeys.Rcm.RemoteConfigurationEnabled, "true");
 
         await RunDynamicConfigurationTest(
-            false,
+            true,
             initialMemoryAssertions: memoryAssertions =>
             {
                 // Initially, no DI objects should exist
@@ -67,6 +65,8 @@ public class DebuggerManagerDynamicTests : TestHelper
                 memoryAssertions.NoObjectsExist<LineProbeResolver>();
                 // SymDB uploader waits for SymDB remote config before it starts.
                 memoryAssertions.NoObjectsExist<Symbols.SymbolsUploader>();
+                // Code Origin for spans is enabled by default
+                memoryAssertions.ObjectsExist<SpanCodeOrigin.SpanCodeOrigin>();
             },
             remoteConfig: new { dynamic_instrumentation_enabled = true },
             DynamicInstrumentationEnabledLogEntry,
@@ -99,34 +99,15 @@ public class DebuggerManagerDynamicTests : TestHelper
         SetEnvironmentVariable(ConfigurationKeys.Rcm.RemoteConfigurationEnabled, "true");
 
         await RunDynamicConfigurationTest(
-            false,
+            true,
             initialMemoryAssertions: memoryAssertions =>
             {
                 // Initially, no Exception Replay objects should exist
                 memoryAssertions.NoObjectsExist<ExceptionAutoInstrumentation.ExceptionReplay>();
+                memoryAssertions.ObjectsExist<SpanCodeOrigin.SpanCodeOrigin>();
             },
             remoteConfig: new { exception_replay_enabled = true },
             ExceptionReplayEnabledLogEntry);
-    }
-
-    [SkippableFact]
-    [Trait("Category", "EndToEnd")]
-    [Trait("Category", "ArmUnsupported")]
-    [Trait("RunOnWindows", "True")]
-    [Trait("Category", "LinuxUnsupported")]
-    public async Task DebuggerManager_CodeOrigin_StartDisabled_EnabledViaRemoteConfig()
-    {
-        SetEnvironmentVariable(ConfigurationKeys.Rcm.RemoteConfigurationEnabled, "true");
-
-        await RunDynamicConfigurationTest(
-            false,
-            initialMemoryAssertions: memoryAssertions =>
-            {
-                // Initially, no Code Origin object should exist
-                memoryAssertions.NoObjectsExist<SpanCodeOrigin.SpanCodeOrigin>();
-            },
-            remoteConfig: new { code_origin_enabled = true },
-            CodeOriginForSpansEnabledLogEntry);
     }
 
     [SkippableFact]
@@ -140,12 +121,13 @@ public class DebuggerManagerDynamicTests : TestHelper
         SetEnvironmentVariable(ConfigurationKeys.Debugger.CodeOriginForSpansEnabled, string.Empty);
 
         await RunDynamicConfigurationTest(
-            false,
+            true,
             initialMemoryAssertions: memoryAssertions =>
             {
                 // Initially, no debugger objects should exist
                 memoryAssertions.NoObjectsExist<DynamicInstrumentation>();
                 memoryAssertions.NoObjectsExist<ExceptionAutoInstrumentation.ExceptionReplay>();
+                memoryAssertions.ObjectsExist<SpanCodeOrigin.SpanCodeOrigin>();
                 memoryAssertions.NoObjectsExist<SnapshotSink>();
                 memoryAssertions.NoObjectsExist<LineProbeResolver>();
                 // SymDB uploader waits for SymDB remote config before it starts.
