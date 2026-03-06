097633afd4b573eafb25b36ea1e517abedb6073c
Daniel Romano <daniel.romanolorente@datadoghq.com>
2026-03-06 16:08:34 +0100

    Fix non json body parsing

--- tracer/test/Datadog.Trace.Security.IntegrationTests/RASP/AspNetCore5RaspDownstream.cs
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/RASP/AspNetCore5RaspDownstream.cs
@@ -3,9 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-#if NETCOREAPP3_0_OR_GREATER
-#pragma warning disable SA1402 // File may only contain a single class
-#pragma warning disable SA1649 // File name must match first type name
+#if NET5_0_OR_GREATER
 
 using System.Collections.Immutable;
 using System.Linq;
