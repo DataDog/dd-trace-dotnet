6ed5beb9c2eeb9e757f8806cefdcca0ea7449676
Andrew Lock <andrew.lock@datadoghq.com>
2026-03-10 08:57:32 +0000

    Add integration test for trimming

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AppTrimmingTests.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/AppTrimmingTests.cs
@@ -5,6 +5,8 @@
 
 using System.Linq;
 using System.Threading.Tasks;
+using Datadog.Trace.Telemetry;
+using Datadog.Trace.Telemetry.DTOs;
 using Datadog.Trace.TestHelpers;
 using FluentAssertions;
 using Xunit;
@@ -25,9 +27,6 @@ public class AppTrimmingTests : TestHelper
     [SkippableFact]
     public async Task TrimmerTest()
     {
-        var httpPort = TcpPortProvider.GetOpenPort();
-        Output.WriteLine($"Assigning port {httpPort} for the httpPort.");
-
         using var agent = EnvironmentHelper.GetMockAgent();
         var aspnetPort = TcpPortProvider.GetOpenPort();
         using var processResult = await RunSampleAndWaitForExit(agent, aspNetCorePort: aspnetPort, usePublishWithRID: true);
@@ -37,6 +36,38 @@ public class AppTrimmingTests : TestHelper
         spans.Where(s => s.Name == "http.request").Should().HaveCount(10);
         spans.Where(s => s.Name == "aspnet_core.request").Should().HaveCount(10);
         spans.Where(s => s.Name == "aspnet_core_mvc.request").Should().HaveCount(10);
+    }
+
+    [SkippableFact]
+    public async Task RedactedErrorLogsTest()
+    {
+        SetEnvironmentVariable("SEND_ERROR_LOG", "1");
+
+        using var agent = EnvironmentHelper.GetMockAgent(useTelemetry: true);
+        var aspnetPort = TcpPortProvider.GetOpenPort();
+        using var processResult = await RunSampleAndWaitForExit(agent, aspNetCorePort: aspnetPort, usePublishWithRID: true);
+        var spans = await agent.WaitForSpansAsync(30);
+
+        // Target app does 10 request, so it generates 30 spans (Http Request + AspNetCore + AspNetCore.Mvc)
+        spans.Where(s => s.Name == "http.request").Should().HaveCount(10);
+        spans.Where(s => s.Name == "aspnet_core.request").Should().HaveCount(10);
+        spans.Where(s => s.Name == "aspnet_core_mvc.request").Should().HaveCount(10);
+
+        await agent.WaitForLatestTelemetryAsync(x => ((TelemetryData)x).IsRequestType(TelemetryRequestTypes.AppClosing));
+        var allLogs = agent.Telemetry
+                           .Cast<TelemetryData>()
+                           .OrderBy(x => x.SeqId)
+                           .Select(x => x.TryGetPayload<LogsPayload>(TelemetryRequestTypes.RedactedErrorLogs))
+                           .Where(x => x is not null)
+                           .SelectMany(x => x.Logs)
+                           .ToList();
+
+        allLogs.Should()
+               .NotBeNullOrEmpty()
+               .And.AllSatisfy(log => log.Tags.Should().Contain("trim:0"))
+               .And.ContainSingle()
+               .Which.Message.Should()
+               .Be("Sending an error log using hacky reflection");
     }
 }
 
