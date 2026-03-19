7695b2378308de701cd35dc33af5d0dddba9692a
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2026-03-19 12:21:25 +0000

    Fix path construction when running the integration tests

--- profiler/test/Datadog.Profiler.IntegrationTests/Helpers/EnvironmentHelper.cs
+++ profiler/test/Datadog.Profiler.IntegrationTests/Helpers/EnvironmentHelper.cs
@@ -81,7 +81,12 @@ namespace Datadog.Profiler.IntegrationTests.Helpers
 
         public static string GetPlatform()
         {
-            return Environment.Is64BitProcess ? "x64" : "x86";
+            return RuntimeInformation.ProcessArchitecture switch
+            {
+                Architecture.Arm64 => "ARM64",
+                Architecture.X86 => "x86",
+                _ => "x64",
+            };
         }
 
         public static bool IsRunningInCi() =>
@@ -352,8 +357,8 @@ namespace Datadog.Profiler.IntegrationTests.Helpers
                 ("win", "x86", _) => "win-x86",
                 ("linux", "x64", false) => "linux-x64",
                 ("linux", "x64", true) => "linux-musl-x64",
-                ("linux", "Arm64", false) => "linux-arm64",
-                ("linux", "Arm64", true) => "linux-musl-arm64",
+                ("linux", "ARM64", false) => "linux-arm64",
+                ("linux", "ARM64", true) => "linux-musl-arm64",
                 ("osx", _, _) => "osx-x64",
                 _ => throw new PlatformNotSupportedException()
             };
