fcef4706fdb235a7ed1a02fd5b2f2e8130094d6f
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2026-03-23 15:43:37 -0400

    We don't use docker on the Windows agent for SQLServer

--- tracer/test/Datadog.Trace.TestHelpers.AutoInstrumentation/Containers/SqlServerFixture.cs
+++ tracer/test/Datadog.Trace.TestHelpers.AutoInstrumentation/Containers/SqlServerFixture.cs
@@ -16,22 +16,37 @@ namespace Datadog.Trace.TestHelpers.AutoInstrumentation.Containers;
 
 public class SqlServerFixture : ContainerFixture
 {
-    protected MsSqlContainer Container => GetResource<MsSqlContainer>("container");
+    private MsSqlContainer? _container;
 
     public override IEnumerable<KeyValuePair<string, string>> GetEnvironmentVariables()
     {
+        if (_container is null)
+        {
+            // No container was started; SQLSERVER_CONNECTION_STRING was already
+            // set externally (e.g., by the Nuke build) or the sample will fall
+            // back to LocalDB.
+            yield break;
+        }
+
         // Build the connection string manually to match the old docker-compose format
         // (Server + User + Password only). The default GetConnectionString() includes
         // Database=master and User Id=sa which adds db.name/db.user tags and changes
         // the peer.service source from out.host to db.name.
-        var host = Container.Hostname;
-        var port = Container.GetMappedPublicPort(MsSqlBuilder.MsSqlPort);
+        var host = _container.Hostname;
+        var port = _container.GetMappedPublicPort(MsSqlBuilder.MsSqlPort);
         var connectionString = $"Server={host},{port};User=sa;Password=Strong!Passw0rd";
         yield return new("SQLSERVER_CONNECTION_STRING", connectionString);
     }
 
     protected override async Task InitializeResources(Action<string, object> registerResource)
     {
+        // If the connection string is already provided (e.g., by the Nuke build
+        // pointing at a pre-installed SQL Server), skip container creation.
+        if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING")))
+        {
+            return;
+        }
+
         // mssql/server has no native arm64 image; use Azure SQL Edge on arm64 instead
         var image = RuntimeInformation.ProcessArchitecture == Architecture.Arm64
             ? new DockerImage("mcr.microsoft.com/azure-sql-edge:latest")
@@ -43,6 +58,7 @@ public class SqlServerFixture : ContainerFixture
 
         await container.StartAsync();
 
+        _container = container;
         registerResource("container", container);
     }
 }
