572870cd4d6602dd1df3bb548d349380ff9c14de
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2026-03-30 15:56:04 -0400

    Update KafkaHelper to use the new TaskStatus

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Kafka/KafkaHelper.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/Kafka/KafkaHelper.cs
@@ -10,6 +10,7 @@ using System.Collections.Concurrent;
 using System.Reflection;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Datadog.Trace.Configuration.Schema;
 using Datadog.Trace.DataStreamsMonitoring;
 using Datadog.Trace.DataStreamsMonitoring.TransactionTracking;
@@ -521,6 +522,11 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.Kafka
             options.DuckCast<IDescribeClusterOptions>().RequestTimeout = TimeSpan.FromSeconds(2);
 
             var duckTask = adminClient.DescribeClusterAsync(options);
+
+            if (duckTask.Status == TaskStatus.RanToCompletion)
+            {
+                return duckTask.Result?.ClusterId;
+            }
 
             var originalContext = SynchronizationContext.Current;
             try
