6be3208e73f5a75128cfff4dfc55ac6dfcf8b73b
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2026-04-03 15:58:51 -0400

    Remove old generation infrastructure and honeypot projects
    Delete the old PackageVersionGenerator pipeline, JSON config files,
    NuGet version cache, supported_versions.json, and all 75 dependabot
    honeypot .csproj files. Simplify DependabotFileManager to only handle
    vendored dependencies. Remove dependabot integration directory monitoring
    and pull_request_target auto-bump trigger.

--- tracer/build/_build/Honeypot/DependabotFileManager.cs
+++ tracer/build/_build/Honeypot/DependabotFileManager.cs
@@ -1,17 +1,11 @@
-// <copyright file="UpdateHoneypot.cs" company="Datadog">
+// <copyright file="DependabotFileManager.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GeneratePackageVersions;
 using Nuke.Common.IO;
-using PrepareRelease;
 using UpdateVendors;
 
 namespace Honeypot
@@ -32,88 +26,6 @@ namespace Honeypot
             honeypotProjTemplate = honeypotProjTemplate.Replace("##PACKAGE_REFS##", fakeRefs);
 
             File.WriteAllText(honeypotProject, honeypotProjTemplate);
-        }
-
-        public static async Task UpdateIntegrations(AbsolutePath honeypotDirectory, List<IntegrationMap> distinctIntegrations)
-        {
-            FileSystemTasks.EnsureCleanDirectory(honeypotDirectory);
-
-            // have to group by packages so we don't have duplicate package references
-            // so reverse the dependencies here
-            var integrationsByPackageName = distinctIntegrations
-                .SelectMany(integration => integration.Packages.Select(package => (integration, package)))
-                .GroupBy(x => x.package)
-                .OrderBy(x => x.Key.NugetName);
-
-            var sb = new StringBuilder();
-            foreach (var packageNameGroup in integrationsByPackageName)
-            {
-                var filename = honeypotDirectory / $"Datadog.Dependabot.{packageNameGroup.Key.NugetName}.csproj";
-                sb.Clear();
-
-                var package = packageNameGroup.Key;
-                foreach (var (integration, _) in packageNameGroup.OrderBy(x => x.integration.Name))
-                {
-                    sb.AppendLine($"    <!-- Integration: {integration.Name} -->");
-                    sb.AppendLine($"    <!--    Assembly: {integration.AssemblyName} -->");
-                }
-
-                sb.AppendLine($"    <!-- Latest package https://www.nuget.org/packages/{package.NugetName}/{package.LatestVersion} -->");
-                sb.AppendLine($"""    <PackageReference Include="{package.NugetName}" Version="{package.LatestTestedVersion ?? package.LatestSupportedVersion}" />""");
-                var honeypotProjTemplate = GetHoneyPotProjTemplate();
-
-                honeypotProjTemplate = honeypotProjTemplate.Replace("##PACKAGE_REFS##", sb.ToString());
-
-                await File.WriteAllTextAsync(filename, honeypotProjTemplate);
-            }
-        }
-
-        public static async Task<List<IntegrationMap>> BuildDistinctIntegrationMaps(
-            List<InstrumentedAssembly> targets,
-            List<PackageVersionGenerator.TestedPackage> testedVersions,
-            Func<string, bool> shouldQueryNuGet,
-            Dictionary<(string AssemblyName, string PackageName), GeneratePackageVersions.GenerateSupportMatrix.SupportedNuGetPackage> previousSupportedVersions)
-        {
-            var distinctIntegrations = new List<IntegrationMap>();
-
-            foreach (var tg in targets.GroupBy(t => t.TargetAssembly))
-            {
-                var maxVersionTarget =
-                    tg
-                        .OrderByDescending(a => a.TargetMaximumMajor)
-                        .ThenByDescending(a => a.TargetMaximumMinor)
-                        .ThenByDescending(a => a.TargetMaximumPatch)
-                        .First();
-                var minVersionTarget =
-                    tg
-                        .OrderBy(a => a.TargetMinimumMajor)
-                        .ThenBy(a => a.TargetMinimumMinor)
-                        .ThenBy(a => a.TargetMinimumPatch)
-                        .First();
-                var minSupportedVersion =
-                    new Version(
-                        minVersionTarget.TargetMinimumMajor,
-                        minVersionTarget.TargetMinimumMinor,
-                        minVersionTarget.TargetMinimumPatch);
-                var maxSupportedVersion =
-                    new Version(
-                        maxVersionTarget.TargetMaximumMajor,
-                        maxVersionTarget.TargetMaximumMinor,
-                        maxVersionTarget.TargetMaximumPatch);
-
-                distinctIntegrations.Add(
-                    await IntegrationMap.Create(
-                        name: tg.Key,
-                        integrationId: maxVersionTarget.IntegrationName,
-                        assemblyName: maxVersionTarget.TargetAssembly,
-                        minSupportedVersion,
-                        maxSupportedVersion,
-                        testedVersions,
-                        shouldQueryNuGet,
-                        previousSupportedVersions));
-            }
-
-            return distinctIntegrations;
         }
 
         private static string GetHoneyPotProjTemplate()
