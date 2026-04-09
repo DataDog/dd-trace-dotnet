9430dcc8cb6880f68bd342655ffff2c2725648d6
Andrew Lock <andrew.lock@datadoghq.com>
2026-04-09 10:48:33 +0100

    Fix misisng using

--- tracer/src/Datadog.Trace/Agent/Api.cs
+++ tracer/src/Datadog.Trace/Agent/Api.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Net.Sockets;
 using System.Threading;
