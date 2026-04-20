92376215184b5e8bf5313500a32e5ab082ed1d5c
Flavien Darche <flavien.darche@datadoghq.com>
2026-04-20 13:58:35 +0200

    add tests

---
+++ tracer/test/Datadog.Trace.Security.IntegrationTests/IAST/AspNetWebApiIastTests.cs
@@ -0,0 +1,319 @@
+#if NETFRAMEWORK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.Iast.Telemetry;
+using Datadog.Trace.TestHelpers;
+using Datadog.Trace.Vendors.Newtonsoft.Json.Linq;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+#pragma warning disable SA1402 // File may only contain a single class
+#pragma warning disable SA1649 // File name must match first type name
+
+namespace Datadog.Trace.Security.IntegrationTests.IAST;
+
+[Collection("IisTests")]
+public class AspNetWebApiIntegratedWithIast : AspNetWebApiIastTests
+{
+    public AspNetWebApiIntegratedWithIast(IisFixture iisFixture, ITestOutputHelper output)
+        : base(iisFixture, output, classicMode: false)
+    {
+    }
+}
+
+[Collection("IisTests")]
+public class AspNetWebApiClassicWithIast : AspNetWebApiIastTests
+{
+    public AspNetWebApiClassicWithIast(IisFixture iisFixture, ITestOutputHelper output)
+        : base(iisFixture, output, classicMode: true)
+    {
+    }
+}
+
+public abstract class AspNetWebApiIastTests : AspNetBase, IClassFixture<IisFixture>, IAsyncLifetime
+{
+    private readonly IisFixture _iisFixture;
+    private readonly bool _classicMode;
+
+    protected AspNetWebApiIastTests(IisFixture iisFixture, ITestOutputHelper output, bool classicMode)
+        : base("WebApi", output, "/api/home/shutdown", @"test\test-applications\security\aspnet", allowAutoRedirect: false)
+    {
+        EnableIast(true);
+        EnableEvidenceRedaction(false);
+        EnableIastTelemetry((int)IastMetricsVerbosityLevel.Off);
+        SetEnvironmentVariable("DD_IAST_DEDUPLICATION_ENABLED", "false");
+        SetEnvironmentVariable("DD_IAST_REQUEST_SAMPLING", "100");
+        SetEnvironmentVariable("DD_IAST_MAX_CONCURRENT_REQUESTS", "100");
+        SetEnvironmentVariable("DD_IAST_VULNERABILITIES_PER_REQUEST", "100");
+        DisableObfuscationQueryString();
+        SetEnvironmentVariable(ConfigurationKeys.AppSec.Rules, DefaultRuleFile);
+        SetEnvironmentVariable(ConfigurationKeys.AppSec.StackTraceEnabled, "false");
+
+        _iisFixture = iisFixture;
+        _classicMode = classicMode;
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsSqlInjectionFromQuery()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync("/Iast/SqlQuery?username=Vicent");
+
+        var vulnerability = GetSingleVulnerability(iastJson, "SQL_INJECTION");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "SqlQuery");
+        AssertSource(iastJson, "http.request.parameter", "username", "Vicent");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsPathTraversalFromBody()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync(
+            "/Iast/PathTraversal",
+            body: "{\"Id\":\"nonexisting.txt\"}",
+            contentType: "application/json");
+
+        var vulnerability = GetSingleVulnerability(iastJson, "PATH_TRAVERSAL");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "PathTraversal");
+        AssertSource(iastJson, "http.request.body", value: "nonexisting.txt");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsCommandInjectionFromQuery()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync("/Iast/ExecuteCommand?file=nonexisting.exe&argumentLine=arg1");
+
+        var vulnerability = GetSingleVulnerability(iastJson, "COMMAND_INJECTION");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "ExecuteCommandInternal");
+        AssertSource(iastJson, "http.request.parameter", "file", "nonexisting.exe");
+        AssertSource(iastJson, "http.request.parameter", "argumentLine", "arg1");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsCommandInjectionFromHeaders()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync(
+            "/Iast/ExecuteCommandFromHeader",
+            headers: new Dictionary<string, string>
+            {
+                { "file", "nonexisting.exe" },
+                { "argumentLine", "arg1" },
+            });
+
+        var vulnerability = GetSingleVulnerability(iastJson, "COMMAND_INJECTION");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "ExecuteCommandInternal");
+        AssertSource(iastJson, "http.request.header", "file", "nonexisting.exe");
+        AssertSource(iastJson, "http.request.header", "argumentLine", "arg1");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsCommandInjectionFromCookies()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync(
+            "/Iast/ExecuteCommandFromCookie",
+            cookies: new Dictionary<string, string>
+            {
+                { "file", "nonexisting.exe" },
+                { "argumentLine", "arg1" },
+            });
+
+        var vulnerability = GetSingleVulnerability(iastJson, "COMMAND_INJECTION");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "ExecuteCommandInternal");
+        AssertSource(iastJson, "http.request.cookie.value", "file", "nonexisting.exe");
+        AssertSource(iastJson, "http.request.cookie.value", "argumentLine", "arg1");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsLdapInjectionFromQuery()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync("/Iast/Ldap?userName=Vicent&skipQueryExecution=true");
+
+        var vulnerability = GetSingleVulnerability(iastJson, "LDAP_INJECTION");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "Ldap");
+        AssertSource(iastJson, "http.request.parameter", "userName", "Vicent");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsHeaderInjectionFromHeaders()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync(
+            "/Iast/HeaderInjection?useValueFromOriginHeader=false",
+            headers: new Dictionary<string, string>
+            {
+                { "name", "Name" },
+                { "value", "value" },
+            });
+
+        GetSingleVulnerability(iastJson, "HEADER_INJECTION");
+        AssertSource(iastJson, "http.request.header", "name", "Name");
+        AssertSource(iastJson, "http.request.header", "value", "value");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsCookieSecurityVulnerabilities()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync("/Iast/AllVulnerabilitiesCookie");
+
+        var vulnerabilities = GetVulnerabilities(iastJson).ToList();
+        var vulnerabilityTypes = vulnerabilities.Select(v => v["type"]?.Value<string>()).ToList();
+        vulnerabilityTypes.Should().Contain("NO_SAMESITE_COOKIE");
+        vulnerabilityTypes.Should().Contain("NO_HTTPONLY_COOKIE");
+        vulnerabilityTypes.Should().Contain("INSECURE_COOKIE");
+        AssertCookieEvidence(vulnerabilities, "NO_SAMESITE_COOKIE");
+        AssertCookieEvidence(vulnerabilities, "NO_HTTPONLY_COOKIE");
+        AssertCookieEvidence(vulnerabilities, "INSECURE_COOKIE");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsUnvalidatedRedirect()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync("/Iast/UnvalidatedRedirect?param=value");
+
+        var vulnerability = GetSingleVulnerability(iastJson, "UNVALIDATED_REDIRECT");
+        AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "UnvalidatedRedirect");
+        AssertSource(iastJson, "http.request.parameter", "param", "value");
+    }
+
+    [Trait("Category", "EndToEnd")]
+    [Trait("RunOnWindows", "True")]
+    [Trait("LoadFromGAC", "True")]
+    [SkippableFact]
+    public async Task DetectsWeakHashing()
+    {
+        var (_, _, iastJson) = await SendIastRequestAsync("/Iast/WeakHashing");
+
+        var vulnerabilities = GetVulnerabilities(iastJson, "WEAK_HASH").ToList();
+        vulnerabilities.Should().HaveCount(2);
+        vulnerabilities.Select(v => v["evidence"]?["value"]?.Value<string>())
+                       .Should()
+                       .BeEquivalentTo(new[] { "MD5", "SHA1" });
+
+        foreach (var vulnerability in vulnerabilities)
+        {
+            AssertLocation(vulnerability, "Samples.Security.WebApi.Controllers.IastController", "WeakHashing");
+        }
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _iisFixture.TryStartIis(this, _classicMode ? IisAppType.AspNetClassic : IisAppType.AspNetIntegrated);
+        SetHttpPort(_iisFixture.HttpPort);
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    private async Task<(MockSpan RootSpan, MockSpan WebApiSpan, JObject IastJson)> SendIastRequestAsync(
+        string url,
+        string body = null,
+        string contentType = null,
+        IDictionary<string, string> headers = null,
+        IDictionary<string, string> cookies = null)
+    {
+        if (headers != null)
+        {
+            AddHeaders(new Dictionary<string, string>(headers));
+        }
+
+        if (cookies != null)
+        {
+            AddCookies(new Dictionary<string, string>(cookies));
+        }
+
+        var spans = body == null
+                        ? await SendRequestsAsync(_iisFixture.Agent, new[] { url })
+                        : await SendRequestsAsync(_iisFixture.Agent, url, body, 1, 1, string.Empty, contentType ?? "application/json");
+
+        var rootSpan = spans.Single(span => span.Name == "aspnet.request" && span.ParentId == null);
+        var webApiSpan = spans.Single(span => span.Name == "aspnet-webapi.request" && span.ParentId == rootSpan.SpanId);
+
+        webApiSpan.ParentId.Should().Be(rootSpan.SpanId);
+        rootSpan.GetTag(Tags.IastEnabled).Should().Be("1");
+
+        IastVerifyScrubberExtensions.IastMetaStructScrubbing(rootSpan);
+        var iastJsonText = rootSpan.GetTag(Tags.IastJson);
+        iastJsonText.Should().NotBeNullOrEmpty();
+
+        return (rootSpan, webApiSpan, JObject.Parse(iastJsonText));
+    }
+
+    private static IEnumerable<JObject> GetVulnerabilities(JObject iastJson)
+    {
+        var vulnerabilities = iastJson["vulnerabilities"] as JArray;
+        vulnerabilities.Should().NotBeNull();
+        return vulnerabilities!.OfType<JObject>();
+    }
+
+    private static IEnumerable<JObject> GetVulnerabilities(JObject iastJson, string vulnerabilityType)
+    {
+        return GetVulnerabilities(iastJson).Where(vulnerability =>
+            string.Equals(vulnerability["type"]?.Value<string>(), vulnerabilityType, StringComparison.Ordinal));
+    }
+
+    private static JObject GetSingleVulnerability(JObject iastJson, string vulnerabilityType)
+    {
+        var vulnerabilities = GetVulnerabilities(iastJson, vulnerabilityType).ToList();
+        vulnerabilities.Should().ContainSingle();
+        return vulnerabilities[0];
+    }
+
+    private static void AssertLocation(JObject vulnerability, string expectedPathContains, string expectedMethodContains)
+    {
+        var location = vulnerability["location"] as JObject;
+        location.Should().NotBeNull();
+        location!["path"]?.Value<string>().Should().Contain(expectedPathContains);
+        location["method"]?.Value<string>().Should().Contain(expectedMethodContains);
+    }
+
+    private static void AssertCookieEvidence(IEnumerable<JObject> vulnerabilities, string vulnerabilityType)
+    {
+        vulnerabilities.Where(v => string.Equals(v["type"]?.Value<string>(), vulnerabilityType, StringComparison.Ordinal))
+                       .Select(v => v["evidence"]?["value"]?.Value<string>())
+                       .Should()
+                       .Contain("AllVulnerabilitiesCookieKey");
+    }
+
+    private static void AssertSource(JObject iastJson, string origin, string name = null, string value = null)
+    {
+        var sources = iastJson["sources"] as JArray;
+        sources.Should().NotBeNull();
+
+        var source = sources!
+                    .OfType<JObject>()
+                    .FirstOrDefault(candidate =>
+                         string.Equals(candidate["origin"]?.Value<string>(), origin, StringComparison.Ordinal)
+                      && (name == null || string.Equals(candidate["name"]?.Value<string>(), name, StringComparison.Ordinal))
+                      && (value == null || string.Equals(candidate["value"]?.Value<string>(), value, StringComparison.Ordinal)));
+
+        source.Should().NotBeNull();
+    }
+}
+#endif
--- tracer/test/test-applications/security/aspnet/Samples.Security.WebApi/Controllers/IastController.cs
+++ tracer/test/test-applications/security/aspnet/Samples.Security.WebApi/Controllers/IastController.cs
@@ -1,28 +1,271 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
 using System.Web.Http;
 using Samples.AspNetMvc5.Models;
+using Samples.Security.Data;
 
 namespace Samples.Security.WebApi.Controllers
 {
     public class IastController : ApiController
     {
-        [AcceptVerbs("GET")]
+        private static SQLiteConnection _dbConnection;
+
+        private static SQLiteConnection DbConnection
+        {
+            get
+            {
+                if (_dbConnection == null)
+                {
+                    _dbConnection = IastControllerHelper.CreateSystemDataDatabase();
+                }
+
+                return _dbConnection;
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("Iast/PathTraversal")]
         public string PathTraversal([FromBody] MiscModel miscModel)
         {
             try
             {
-                if (!string.IsNullOrEmpty(miscModel.Id))
+                if (miscModel != null && !string.IsNullOrEmpty(miscModel.Id))
                 {
                     var result = System.IO.File.ReadAllText(miscModel.Id);
-                    return ($"file content: " + result);
+                    return "file content: " + result;
                 }
-                else
-                {
-                    return "No file was provided";
-                }
+
+                return "No file was provided";
             }
             catch
             {
-                return ("The provided file " + miscModel.Id + " could not be opened");
+                return "The provided file " + miscModel?.Id + " could not be opened";
+            }
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/SqlQuery")]
+        public string SqlQuery(string username, string query)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var taintedQuery = "SELECT Surname from Persons where name = '" + username + "'";
+                    var result = new SQLiteCommand(taintedQuery, DbConnection).ExecuteScalar();
+                    return "Result: " + result;
+                }
+
+                if (!string.IsNullOrEmpty(query))
+                {
+                    var result = new SQLiteCommand(query, DbConnection).ExecuteScalar();
+                    return "Result: " + result;
+                }
+
+                return "No query or username was provided";
+            }
+            catch (SQLiteException ex)
+            {
+                return IastControllerHelper.ToFormattedString(ex);
+            }
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/ExecuteCommand")]
+        public string ExecuteCommand(string file, string argumentLine, bool fromShell = false)
+        {
+            return ExecuteCommandInternal(file, argumentLine, fromShell);
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/WeakHashing")]
+        public string WeakHashing()
+        {
+#pragma warning disable SYSLIB0021 // Type or member is obsolete
+            var byteArg = new byte[] { 3, 5, 6 };
+            MD5.Create().ComputeHash(byteArg);
+            SHA1.Create().ComputeHash(byteArg);
+#pragma warning restore SYSLIB0021 // Type or member is obsolete
+            return "Weak hashes launched.\n";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/Ldap")]
+        public string Ldap(string path, string userName, bool skipQueryExecution = false)
+        {
+            var resultString = string.Empty;
+            DirectoryEntry entry;
+
+            try
+            {
+                var directoryEntryPath = !string.IsNullOrEmpty(path) ? path : "LDAP://fakeorg";
+                entry = new DirectoryEntry(directoryEntryPath, string.Empty, string.Empty, AuthenticationTypes.None);
+            }
+            catch
+            {
+                entry = new DirectoryEntry();
+            }
+
+            var search = new DirectorySearcher(entry);
+            if (!string.IsNullOrEmpty(userName))
+            {
+                search.Filter = "(uid=" + userName + ")";
+            }
+
+            if (skipQueryExecution)
+            {
+                return "LDAP filter prepared";
+            }
+
+            try
+            {
+                var result = search.FindAll();
+                for (var i = 0; i < result.Count; i++)
+                {
+                    resultString += result[i].Path + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                return IastControllerHelper.ToFormattedString(ex);
+            }
+
+            return "Result: " + resultString;
+        }
+
+        private static string Combine(string first, string second, string defaultValue)
+        {
+            var firstIsEmpty = string.IsNullOrWhiteSpace(first);
+            var secondIsEmpty = string.IsNullOrWhiteSpace(second);
+
+            if (firstIsEmpty && secondIsEmpty)
+            {
+                return defaultValue;
+            }
+
+            if (!firstIsEmpty && !secondIsEmpty)
+            {
+                return first + second;
+            }
+
+            return firstIsEmpty ? second : first;
+        }
+
+        private string GetHeaderValue(string name)
+        {
+            IEnumerable<string> values;
+            return Request.Headers.TryGetValues(name, out values) ? values.FirstOrDefault() : null;
+        }
+
+        private static HttpCookie GetDefaultCookie(string key, string value)
+        {
+            var cookie = new HttpCookie(key, value);
+            cookie.Values["SameSite"] = "Strict";
+            cookie.HttpOnly = true;
+            cookie.Secure = true;
+            return cookie;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/AllVulnerabilitiesCookie")]
+        public string AllVulnerabilitiesCookie()
+        {
+            var cookie = new HttpCookie("AllVulnerabilitiesCookieKey", "AllVulnerabilitiesCookieValue");
+            cookie.Values["SameSite"] = "None";
+            cookie.HttpOnly = false;
+            cookie.Secure = false;
+            HttpContext.Current.Response.Cookies.Add(cookie);
+            return "Sending AllVulnerabilitiesCookie";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/UnvalidatedRedirect")]
+        public IHttpActionResult UnvalidatedRedirect(string param)
+        {
+            var location = $"Redirected?param={param}";
+            return Redirect(location);
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/Redirected")]
+        public string Redirected(string param)
+        {
+            return $"Redirected param:{param}\n";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/HeaderInjection")]
+        public string HeaderInjection(bool useValueFromOriginHeader = false)
+        {
+            const string defaultHeaderName = "defaultName";
+            const string defaultHeaderValue = "defaultValue";
+
+            var headerName = GetHeaderValue("name");
+            var headerValue = GetHeaderValue("value");
+            var originValue = GetHeaderValue("origin");
+            var propagationHeader = GetHeaderValue("propagation");
+            var cookieName = HttpContext.Current.Request.Cookies["name"]?.Value;
+            var cookieValue = HttpContext.Current.Request.Cookies["value"]?.Value;
+
+            if (!string.IsNullOrEmpty(propagationHeader))
+            {
+                HttpContext.Current.Response.AddHeader("propagation", propagationHeader);
+                return "returned propagation header";
+            }
+
+            var returnedName = Combine(headerName, cookieName, defaultHeaderName);
+            var returnedValue = useValueFromOriginHeader ? originValue : Combine(headerValue, cookieValue, defaultHeaderValue);
+
+            HttpContext.Current.Response.AddHeader(returnedName, returnedValue);
+            HttpContext.Current.Response.AddHeader("extraName", "extraValue");
+            return $"returned header {returnedName},{returnedValue}";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/ExecuteCommandFromHeader")]
+        public string ExecuteCommandFromHeader()
+        {
+            return ExecuteCommandInternal(GetHeaderValue("file"), GetHeaderValue("argumentLine"));
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("Iast/ExecuteCommandFromCookie")]
+        public string ExecuteCommandFromCookie()
+        {
+            var file = HttpContext.Current.Request.Cookies["file"]?.Value;
+            var argumentLine = HttpContext.Current.Request.Cookies["argumentLine"]?.Value;
+            return ExecuteCommandInternal(file, argumentLine);
+        }
+
+        private string ExecuteCommandInternal(string file, string argumentLine, bool fromShell = false)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(file))
+                {
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName = file,
+                        Arguments = argumentLine,
+                        UseShellExecute = fromShell
+                    };
+
+                    var result = Process.Start(startInfo);
+                    return "Process launched: " + result.ProcessName;
+                }
+
+                return "No file was provided";
+            }
+            catch (Win32Exception ex)
+            {
+                return IastControllerHelper.ToFormattedString(ex);
             }
         }
     }
