12d75ebdcdc0029f1ebc5e6f7493ce36ad820d3a
Flavien Darche <flavien.darche@datadoghq.com>
2026-05-05 15:34:34 +0200

    remove useless import

--- tracer/src/Datadog.Trace/Iast/Aspects/System.Text/StringBuilderAspects.cs
+++ tracer/src/Datadog.Trace/Iast/Aspects/System.Text/StringBuilderAspects.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Datadog.Trace.Iast.Dataflow;
 using Datadog.Trace.Iast.Helpers;
