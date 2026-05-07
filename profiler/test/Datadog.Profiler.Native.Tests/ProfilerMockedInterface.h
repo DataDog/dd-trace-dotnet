6d9882ff62446d89782ba98142929fc1e8c32bd1
chrisnas <chrisnas@users.noreply.github.com>
2026-05-07 06:49:30 -0700

    [Profiler] Ensure valid json format for information json (#8578)
    ## Summary of changes
    Fix possible invalid Json generation
    
    ## Reason for change
    Invalid Json would fail the profile sending
    
    ## Implementation details
    Handle empty cases
    
    ## Test coverage
    New tests have been added
    
    ## Other details
    <!-- Fixes #{issue} -->
    
    
    <!--  ⚠️ Note:
    
    Where possible, please obtain 2 approvals prior to merging. Unless
    CODEOWNERS specifies otherwise, for external teams it is typically best
    to have one review from a team member, and one review from apm-dotnet.
    Trivial changes do not require 2 reviews.
    
    MergeQueue is NOT enabled in this repository. If you have write access
    to the repo, the PR has 1-2 approvals (see above), and all of the
    required checks have passed, you can use the Squash and Merge button to
    merge the PR. If you don't have write access, or you need help, reach
    out in the #apm-dotnet channel in Slack.
    -->

--- profiler/test/Datadog.Profiler.Native.Tests/ProfilerMockedInterface.h
+++ profiler/test/Datadog.Profiler.Native.Tests/ProfilerMockedInterface.h
@@ -14,6 +14,7 @@
 #include "IContentionListener.h"
 #include "IExporter.h"
 #include "IGcSettingsProvider.h"
+#include "IMetadataProvider.h"
 #include "IMetricsSender.h"
 #include "IRuntimeIdStore.h"
 #include "ISamplesCollector.h"
@@ -144,6 +145,20 @@ public:
     MOCK_METHOD(void, ProcessEnd, (), (override));
     MOCK_METHOD(SkipProfileHeuristicType, GetSkipProfileHeuristic, (), (const override));
     MOCK_METHOD(DeploymentMode, GetDeploymentMode, (), (const override));
+};
+
+class MockMetadataProvider : public IMetadataProvider
+{
+public:
+    MOCK_METHOD(void, Initialize, (), (override));
+    MOCK_METHOD(void, Add, (std::string const&, std::string const&, std::string const&), (override));
+    MOCK_METHOD(IMetadataProvider::metadata_t const&, Get, (), (override));
+};
+
+class MockGcSettingsProvider : public IGcSettingsProvider
+{
+public:
+    MOCK_METHOD(GCMode, GetMode, (), (override));
 };
 
 class MockMetricsSender : public IMetricsSender
@@ -277,6 +292,7 @@ std::tuple<std::shared_ptr<ISamplesProvider>, MockSampleProvider&> CreateSamples
 std::tuple<std::unique_ptr<IExporter>, MockExporter&> CreateExporter();
 std::tuple<std::unique_ptr<ISamplesCollector>, MockSamplesCollector&> CreateSamplesCollector();
 std::tuple<std::unique_ptr<ISsiManager>, MockSsiManager&> CreateSsiManager();
+std::tuple<std::unique_ptr<IMetadataProvider>, MockMetadataProvider&> CreateMetadataProvider();
 
 std::shared_ptr<Sample> CreateSample(std::string_view runtimeId, const std::vector<std::pair<std::string, std::string>>& callstack, const std::vector<std::pair<std::string, std::string>>& labels, std::int64_t value);
 
