629b45b6a789970ce852bd992e6057b0e1e5b914
Zach Montoya <zach.montoya@datadoghq.com>
2026-05-12 15:35:52 -0700

    When submitting OTLP traces, use an OTLP specific logic for constructing an IApiRequestFactory

---
+++ tracer/src/Datadog.Trace/HttpOverStreams/OtlpHeaderHelper.cs
@@ -0,0 +1,37 @@
+// <copyright file="OtlpHeaderHelper.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using Datadog.Trace.Util;
+
+namespace Datadog.Trace.HttpOverStreams;
+
+internal sealed class OtlpHeaderHelper : HttpHeaderHelperBase
+{
+    private readonly string _serializedHeaders;
+
+    public OtlpHeaderHelper(KeyValuePair<string, string>[] signalHeaders)
+    {
+        DefaultHeaders = signalHeaders;
+
+        var sb = StringBuilderCache.Acquire();
+        foreach (var kvp in signalHeaders)
+        {
+            sb.Append(kvp.Key);
+            sb.Append(": ");
+            sb.Append(kvp.Value);
+            sb.Append(DatadogHttpValues.CrLf);
+        }
+
+        _serializedHeaders = StringBuilderCache.GetStringAndRelease(sb);
+    }
+
+    public override KeyValuePair<string, string>[] DefaultHeaders { get; }
+
+    protected override string HttpSerializedDefaultHeaders => _serializedHeaders;
+}
---
+++ tracer/src/Datadog.Trace/OtlpTransportStrategy.cs
@@ -0,0 +1,63 @@
+// <copyright file="OtlpTransportStrategy.cs" company="Datadog">
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
+// </copyright>
+
+#if NET6_0_OR_GREATER
+
+#nullable enable
+using System;
+using System.Collections.Generic;
+using Datadog.Trace.Agent.StreamFactories;
+using Datadog.Trace.Agent.Transports;
+using Datadog.Trace.Configuration;
+using Datadog.Trace.HttpOverStreams;
+using Datadog.Trace.Logging;
+
+namespace Datadog.Trace.Agent;
+
+internal static class OtlpTransportStrategy
+{
+    private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor(typeof(OtlpTransportStrategy));
+    private static readonly Uri Localhost = new Uri("http://localhost");
+
+    public static IApiRequestFactory GetTraces(
+        ExporterSettings settings)
+    {
+        return Get(settings.TracesTransport, settings.OtlpTracesEndpoint, settings.OtlpTracesHeaders ?? [], settings.OtlpTracesTimeoutMs, "traces");
+    }
+
+    private static IApiRequestFactory Get(
+        AgentTransportType strategy,
+        Uri signalEndpoint,
+        KeyValuePair<string, string>[] signalHeaders,
+        int timeoutMs,
+        string productName)
+    {
+        switch (strategy)
+        {
+            case AgentTransportType.WindowsNamedPipe:
+                Log.Information<string, string?, int>("Using " + nameof(NamedPipeClientStreamFactory) + " for {ProductName} transport, with pipe name {TracesPipeName} and timeout {TracesPipeTimeoutMs}ms.", productName, signalEndpoint.ToString(), timeoutMs);
+                return new HttpStreamRequestFactory(
+                    new NamedPipeClientStreamFactory(signalEndpoint.ToString(), timeoutMs),
+                    new DatadogHttpClient(new OtlpHeaderHelper(signalHeaders)),
+                    Localhost);
+
+            case AgentTransportType.UnixDomainSocket:
+                Log.Information("Using " + nameof(SocketHandlerRequestFactory) + " for {ProductName} transport, with UDS path {Path}.", productName, signalEndpoint.ToString());
+                // use http://localhost as base endpoint
+                return new SocketHandlerRequestFactory(
+                    new UnixDomainSocketStreamFactory(signalEndpoint.ToString()),
+                    signalHeaders,
+                    Localhost);
+            case AgentTransportType.Default:
+            default:
+                Log.Information("Using " + nameof(HttpClientRequestFactory) + " for {ProductName} transport.", productName);
+                return new HttpClientRequestFactory(
+                    signalEndpoint,
+                    signalHeaders,
+                    timeout: TimeSpan.FromMilliseconds(timeoutMs));
+        }
+    }
+}
+#endif
