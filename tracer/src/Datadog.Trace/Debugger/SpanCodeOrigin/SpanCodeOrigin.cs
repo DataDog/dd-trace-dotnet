08aeaa4680ca431c88001d73939db6fc026ec04a
dudik <8845578+dudikeleti@users.noreply.github.com>
2026-05-14 11:13:15 +0200

    Reduce span code origin PDB failure log severity

--- tracer/src/Datadog.Trace/Debugger/SpanCodeOrigin/SpanCodeOrigin.cs
+++ tracer/src/Datadog.Trace/Debugger/SpanCodeOrigin/SpanCodeOrigin.cs
@@ -240,7 +240,7 @@ namespace Datadog.Trace.Debugger.SpanCodeOrigin
                 }
                 catch (Exception ex)
                 {
-                    Log.Error(ex, "Error while getting endpoints for {AssemblyName} in location: {AssemblyLocation}", assembly.FullName, assembly.Location);
+                    Log.Warning(ex, "Error while getting endpoints for {AssemblyName} in location: {AssemblyLocation}", assembly.FullName, assembly.Location);
                     sequencePoints = null;
                 }
 
