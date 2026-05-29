8ebef9942dfa3b326cb2bd30192998ca6e66525a
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2026-05-29 12:34:33 +0200

    Fix header

--- tracer/test/Datadog.Trace.Tests/Ci/Coverage/FileBitmapTests.cs
+++ tracer/test/Datadog.Trace.Tests/Ci/Coverage/FileBitmapTests.cs
@@ -1,5 +1,5 @@
 // <copyright file="FileBitmapTests.cs" company="Datadog">
-// Unless otherwise specified all files in this repository are licensed under the Apache 2 License.
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
