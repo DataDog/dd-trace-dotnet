21d9668aeecee3a1f94c243a72a1a213fe3a0972
Daniel Romano <daniel.romanolorente@datadoghq.com>
2026-06-09 16:39:31 +0200

    Segregate integrations into different files

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/DataContractJsonSerializer_WriteObject_XmlDictionaryWriter_Integration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/AspNet/DataContractJsonSerializer_WriteObject_XmlDictionaryWriter_Integration.cs
@@ -1,4 +1,4 @@
-// <copyright file="DataContractJsonSerializer_WriteObject_Integration.cs" company="Datadog">
+// <copyright file="DataContractJsonSerializer_WriteObject_XmlDictionaryWriter_Integration.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
@@ -8,37 +8,37 @@
 
 using System;
 using System.ComponentModel;
-using System.IO;
+using System.Xml;
 using Datadog.Trace.ClrProfiler.CallTarget;
 using Datadog.Trace.Configuration;
 
 namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.AspNet
 {
     /// <summary>
-    /// System.Runtime.Serialization.XmlObjectSerializer.WriteObject calltarget instrumentation
+    /// System.Runtime.Serialization.Json.DataContractJsonSerializer.WriteObject calltarget instrumentation
     /// </summary>
     [InstrumentMethod(
         AssemblyName = "System.Runtime.Serialization",
-        TypeName = "System.Runtime.Serialization.XmlObjectSerializer",
+        TypeName = "System.Runtime.Serialization.Json.DataContractJsonSerializer",
         MethodName = "WriteObject",
         ReturnTypeName = ClrNames.Void,
-        ParameterTypeNames = new[] { ClrNames.Stream, ClrNames.Object },
+        ParameterTypeNames = new[] { "System.Xml.XmlDictionaryWriter", ClrNames.Object },
         MinimumVersion = "4",
         MaximumVersion = "4",
         IntegrationName = nameof(IntegrationId.AspNetMvc),
         InstrumentationCategory = InstrumentationCategory.AppSec,
-        CallTargetIntegrationKind = CallTargetKind.Derived)]
+        CallTargetIntegrationKind = CallTargetKind.Default)]
     // ReSharper disable once InconsistentNaming
     [Browsable(false)]
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public sealed class DataContractJsonSerializer_WriteObject_Integration
+    public sealed class DataContractJsonSerializer_WriteObject_XmlDictionaryWriter_Integration
     {
-        internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, Stream stream, object? graph)
+        internal static CallTargetState OnMethodBegin<TTarget>(TTarget instance, XmlDictionaryWriter writer, object? graph)
         {
             try
             {
                 if (!DataContractJsonSerializerWriteObjectCommon.TryGetCaptureContext(instance, graph, out var httpContext, out var response, out var scope, out var useSimpleDictionaryFormat)
-                 || !DataContractJsonSerializerWriteObjectCommon.IsResponseOutputStream(stream, response))
+                 || !DataContractJsonSerializerWriteObjectCommon.IsResponseOutputWriter(writer, response))
                 {
                     return CallTargetState.GetDefault();
                 }
