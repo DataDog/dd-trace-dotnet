6f4c6a6706d37488415238c0b13f6713df55184a
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2026-06-10 13:18:13 +0000

    Implement Replace std::function Deleters

--- profiler/src/ProfilerEngine/Datadog.Profiler.Native.Linux/DlPhdrInfoWrapper.cpp
+++ profiler/src/ProfilerEngine/Datadog.Profiler.Native.Linux/DlPhdrInfoWrapper.cpp
@@ -12,7 +12,7 @@
 // dlclose is happening when libunwind is calling our custom dl_iterate_phdr.
 // The pointers will be invalidated and a crash can happen.
 DlPhdrInfoWrapper::DlPhdrInfoWrapper(struct dl_phdr_info const* info, std::size_t size, shared::pmr::memory_resource* allocator) :
-    _info{0}, _phdr{nullptr}, _name{nullptr}, _size(size), _allocator{allocator}
+    _info{0}, _phdr{nullptr, {}}, _name{nullptr, {}}, _size(size), _allocator{allocator}
 {
     DeepCopy(_info, info);
 }
@@ -22,25 +22,25 @@ std::pair<struct dl_phdr_info*, std::size_t> DlPhdrInfoWrapper::Get()
     return {&_info, _size};
 }
 
-using custom_deleter = std::function<void(void*)>;
 template <class T, typename _Naked_T = std::remove_cv_t<T>>
-std::unique_ptr<_Naked_T, custom_deleter> Duplicate(shared::pmr::memory_resource* allocator, T* src, std::size_t count)
+std::unique_ptr<_Naked_T[], DlPhdrInfoWrapper::PmrDeleter> DuplicateWithPmr(
+    shared::pmr::memory_resource* allocator, T* src, std::size_t count)
 {
     // Static assertions to that T does not need special treatment (ex:. calling ctor or dtor)
     static_assert(std::is_trivially_copyable_v<T>, 
         "T must be trivially copyable");
-    static_assert(std::is_trivially_destructible_v<T>, 
+    static_assert(std::is_trivially_destructible_v<T>,
         "T must be trivially destructible");
 
     if (src == nullptr)
     {
-        return {nullptr};
+        return {nullptr, {}};
     }
 
     const std::size_t size = sizeof(T) * count;
     auto* newPtr = static_cast<_Naked_T*>(allocator->allocate(size, alignof(T)));
     memcpy(newPtr, src, size);
-    return {static_cast<_Naked_T*>(newPtr), [allocator, size](void* ptr) { allocator->deallocate(ptr, size, alignof(T)); }};
+    return {newPtr, {allocator, size, alignof(T)}};
 }
 
 void DlPhdrInfoWrapper::DeepCopy(struct dl_phdr_info& destination, struct dl_phdr_info const* source)
@@ -48,11 +48,11 @@ void DlPhdrInfoWrapper::DeepCopy(struct dl_phdr_info& destination, struct dl_phd
     // first copy all fields
     destination = *source;
     // then update the pointer-ones
-    _name = Duplicate(_allocator, source->dlpi_name, strlen(source->dlpi_name) + 1);
+    _name = DuplicateWithPmr(_allocator, source->dlpi_name, strlen(source->dlpi_name) + 1);
     destination.dlpi_name = _name.get();
 
     // copy header
-    _phdr = Duplicate(_allocator, source->dlpi_phdr, source->dlpi_phnum);
+    _phdr = DuplicateWithPmr(_allocator, source->dlpi_phdr, source->dlpi_phnum);
     destination.dlpi_phdr = _phdr.get();
 
     // Those fields appeared in glibc 2.4 (with two others).
