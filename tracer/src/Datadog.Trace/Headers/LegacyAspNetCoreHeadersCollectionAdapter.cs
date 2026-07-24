0a924c60b15dbf2836deda05be11515fe1f02c22
Andrew Lock <andrew.lock@datadoghq.com>
2026-07-24 16:31:42 +0100

    File based namespaces

--- tracer/src/Datadog.Trace/Headers/LegacyAspNetCoreHeadersCollectionAdapter.cs
+++ tracer/src/Datadog.Trace/Headers/LegacyAspNetCoreHeadersCollectionAdapter.cs
@@ -10,31 +10,30 @@
 using System;
 using System.Collections.Generic;
 
-namespace Datadog.Trace.Headers
+namespace Datadog.Trace.Headers;
+
+/// <summary>
+/// Adapts an ASP.NET Core 2.x IHeaderDictionary without referencing Microsoft.Extensions.Primitives.StringValues.
+/// The duck-typing runtime boxes StringValues and converts it to the BCL IEnumerable implementation shared by
+/// ASP.NET Core 2.x versions.
+/// </summary>
+internal readonly struct LegacyAspNetCoreHeadersCollectionAdapter : IHeadersCollection
 {
-    /// <summary>
-    /// Adapts an ASP.NET Core 2.x IHeaderDictionary without referencing Microsoft.Extensions.Primitives.StringValues.
-    /// The duck-typing runtime boxes StringValues and converts it to the BCL IEnumerable implementation shared by
-    /// ASP.NET Core 2.x versions.
-    /// </summary>
-    internal readonly struct LegacyAspNetCoreHeadersCollectionAdapter : IHeadersCollection
+    private readonly ILegacyAspNetCoreHeaders _headers;
+
+    public LegacyAspNetCoreHeadersCollectionAdapter(ILegacyAspNetCoreHeaders headers)
     {
-        private readonly ILegacyAspNetCoreHeaders _headers;
-
-        public LegacyAspNetCoreHeadersCollectionAdapter(ILegacyAspNetCoreHeaders headers)
-        {
-            _headers = headers;
-        }
-
-        public IEnumerable<string> GetValues(string name)
-            => _headers[name] ?? [];
-
-        public void Set(string name, string value) => throw new NotSupportedException();
-
-        public void Add(string name, string value) => throw new NotSupportedException();
-
-        public void Remove(string name) => throw new NotSupportedException();
+        _headers = headers;
     }
+
+    public IEnumerable<string> GetValues(string name)
+        => _headers[name] ?? [];
+
+    public void Set(string name, string value) => throw new NotSupportedException();
+
+    public void Add(string name, string value) => throw new NotSupportedException();
+
+    public void Remove(string name) => throw new NotSupportedException();
 }
 
 #endif
