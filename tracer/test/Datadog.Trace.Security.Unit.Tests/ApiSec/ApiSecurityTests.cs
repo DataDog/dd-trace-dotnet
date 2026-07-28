1c287059d6787735c9b8bfdac1ff88b2a245aada
Zach Montoya <zach.montoya@datadoghq.com>
2026-07-28 12:30:28 -0700

    Fix additional src and test callsites to use int status codes correctly

--- tracer/test/Datadog.Trace.Security.Unit.Tests/ApiSec/ApiSecurityTests.cs
+++ tracer/test/Datadog.Trace.Security.Unit.Tests/ApiSec/ApiSecurityTests.cs
@@ -86,7 +86,7 @@ public class ApiSecurityTests
 
             var route = $"route{i}";
             var method = $"GET{i}";
-            var statusCode = i.ToString();
+            var statusCode = i;
             var resHash = ApiSecurity.CombineHashes(route, method, statusCode);
             queue.Enqueue(resHash);
             var dic = new Dictionary<string, object>();
@@ -95,7 +95,7 @@ public class ApiSecurityTests
 
             var span = new Span(new SpanContext(SpanContext.None, tc, "Test"), dt);
             span.SetTag(Tags.HttpRoute, route);
-            span.SetTag(Tags.HttpStatusCode, statusCode);
+            span.SetTag(Tags.HttpStatusCode, statusCode.ToString());
             span.SetTag(Tags.HttpMethod, method);
             var res = apiSec.ShouldAnalyzeSchema(true, span, dic, statusCode, new Dictionary<string, object>());
             res.Should().BeTrue();
