ae27004cb82967c0d71f81f09634de9fb8bbc7fe
Christophe Nasarre <christophe.nasarre@datadoghq.com>
2026-07-31 14:01:33 +0200

    Fix issues raised by AI

--- profiler/src/Demos/Samples.Computer01/GlobalStruct.cs
+++ profiler/src/Demos/Samples.Computer01/GlobalStruct.cs
@@ -3,6 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
 // </copyright>
 
+#pragma warning disable SA1401 // Fields should be private
+
+
 // This value type is deliberately declared outside of any namespace: when it is given as the type
 // argument of a generic method, the |fg: part of the frame must not be prefixed by a '.' standing
 // for the empty namespace.
@@ -10,3 +13,5 @@ internal struct GlobalStruct
 {
     public int Member;
 }
+
+#pragma warning restore SA1401 // Fields should be private
