7af06f6ca595ffff568fdd2e11f7b07752583ae7
Milan Garnier <milan.garnier@datadoghq.com>
2026-08-12 14:14:27 +0200

    fix(propagators): normalize ot tracestate values

--- tracer/test/Datadog.Trace.Tests/Propagators/OtelTraceStateHelpersTests.cs
+++ tracer/test/Datadog.Trace.Tests/Propagators/OtelTraceStateHelpersTests.cs
@@ -20,7 +20,7 @@ namespace Datadog.Trace.Tests.Propagators
         [InlineData("rv:ef284ace7a91e1", 0xef284ace7a91e1UL)]
         [InlineData("rv:ef284ace7a91e1;th:e6666666666668", 0xef284ace7a91e1UL)]
         [InlineData("th:e6666666666668;rv:ef284ace7a91e1", 0xef284ace7a91e1UL)]
-        [InlineData("foo:bar;rv:1;baz:qux", 0x1UL)]
+        [InlineData("foo:bar;rv:1;baz:qux", null)]
         [InlineData("rv:zzzzzz", null)] // not hex -> malformed -> null
         [InlineData("rv:123456789abcdef1", null)] // 15 hex digits -> too long -> malformed -> null
         [InlineData("rv:", null)] // empty value -> malformed -> null
@@ -40,7 +40,7 @@ namespace Datadog.Trace.Tests.Propagators
         // both rv and th, rv first
         [InlineData(null, 0xef284ace7a91e1UL, 0xe6666666666668UL, "rv:ef284ace7a91e1;th:e6666666666668")]
         // th with trailing zero nibbles trimmed
-        [InlineData(null, null, 0x100UL, "th:1")]
+        [InlineData(null, null, 0x100UL, "th:000000000001")]
         // existing rv/th replaced, unrelated sub-key preserved in original order
         [InlineData("foo:bar;rv:1;th:2", 0xef284ace7a91e1UL, 0xe6666666666668UL, "rv:ef284ace7a91e1;th:e6666666666668;foo:bar")]
         // malformed existing rv/th still stripped even though we're not re-deriving them
