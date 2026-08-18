2d66f09184eee0d3efe334644c7f950ee1516c7f
Andrew Lock <andrew.lock@datadoghq.com>
2026-08-18 11:48:09 +0100

    Bump testing of Microsoft.Data.SQLite (#9063)
    ## Summary of changes
    
    "Fixes" the Microsoft.Data.Sqlite tests for the latest versions
    
    ## Reason for change
    
    `Microsoft.Data.Sqlite` 8.0.30/9.0.19/10.0.11 (bumped in the latest test
    PR, #9043, raised their minimum `SQLitePCLRaw.bundle_e_sqlite3`
    dependency (2.1.6 -> 2.1.12), which ships a native `libe_sqlite3.so`
    (via `SQLitePCLRaw.lib.e_sqlite3` `3.53.3`) built against a _newer_
    glibc (GLIBC_2.33) than we use in our docker image (2.28). Consequently,
    we can't run these tests in our linux environment currently.
    
    ## Implementation details
    
    Currently, this _skips_ those tests when running on glibc that is too
    low. It doesn't skip on other platforms, so there's still _some_
    coverage, but obviously this is less than idea.
    
    An alternative approach is to run our _testing_ on a different base
    image than our _build_. That seems reasonable, and we should probably do
    it, but it's a fair amount of work, and as we're evaluating gitlab
    anyway, it may be easier to postpone this until later.
    
    If/When we start running on a newer glibc, these tests should light up
    again...
    
    ## Test coverage
    
    It skips the tests of the latest patch versions when on glibc, and glibc
    is too low.

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/PackageVersionsLatestSpecific.g.cs
@@ -1688,65 +1688,65 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests
 #if NET48
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NETCOREAPP3_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NETCOREAPP3_1
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NET5_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NET6_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NET7_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NET8_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NET9_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #if NET10_0
                 new object[] { "2.3.0" },
                 new object[] { "5.0.17" },
-                new object[] { "8.0.29" },
-                new object[] { "9.0.18" },
-                new object[] { "10.0.10" },
+                new object[] { "8.0.30" },
+                new object[] { "9.0.19" },
+                new object[] { "10.0.11" },
 #endif
 #endif
             };
