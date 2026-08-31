90b41809dfb95307fdcd3330edcfb4a06a035c2c
pablo.martinezbernardo <pablo.martinezbernardo@datadoghq.com>
2026-08-31 17:24:01 +0200

    [DuckTyping] Use compiled fixtures for cross-ALC repro

--- tracer/test/Datadog.Trace.DuckTyping.Tests/AssemblyLoadContextTests.cs
+++ tracer/test/Datadog.Trace.DuckTyping.Tests/AssemblyLoadContextTests.cs
@@ -10,7 +10,6 @@
 using System;
 using System.IO;
 using System.Reflection;
-using System.Reflection.Emit;
 using System.Runtime.Loader;
 using FluentAssertions;
 using Xunit;
@@ -19,36 +18,36 @@ namespace Datadog.Trace.DuckTyping.Tests;
 
 public class AssemblyLoadContextTests
 {
+    private const string SharedAssemblyName = "Datadog.Trace.DuckTyping.Tests.Fixtures.Shared";
+    private const string TargetAssemblyName = "Datadog.Trace.DuckTyping.Tests.Fixtures.Target";
+    private const string TargetTypeName = "Datadog.Trace.DuckTyping.Tests.Fixtures.Target.DuckTypingTarget";
+
     [Fact]
     public void DuckFieldThrowsMissingFieldExceptionAcrossAssemblyLoadContexts()
     {
         var testAssemblyPath = Assembly.GetExecutingAssembly().Location;
+        var assemblyDirectory = Path.GetDirectoryName(testAssemblyPath)!;
+        var sharedAssemblyPath = Path.Combine(assemblyDirectory, SharedAssemblyName + ".dll");
+        var targetAssemblyPath = Path.Combine(assemblyDirectory, TargetAssemblyName + ".dll");
         var targetContext = new AssemblyLoadContext("DuckTypingTarget");
         var proxyContext = new ProxyLoadContext(testAssemblyPath);
 
-        var target = CreateGeneratedTarget(targetContext, testAssemblyPath);
+        // The target field and the generated proxy resolve the same dependency identity in different contexts.
+        var targetSharedAssembly = targetContext.LoadFromAssemblyPath(sharedAssemblyPath);
+        var proxySharedAssembly = proxyContext.LoadFromAssemblyPath(sharedAssemblyPath);
+        targetSharedAssembly.FullName.Should().Be(proxySharedAssembly.FullName);
+        targetSharedAssembly.Should().NotBeSameAs(proxySharedAssembly);
+
+        var targetAssembly = targetContext.LoadFromAssemblyPath(targetAssemblyPath);
+        var targetType = targetAssembly.GetType(TargetTypeName, throwOnError: true)!;
+        var target = Activator.CreateInstance(targetType)!;
+
         var proxyAssembly = proxyContext.LoadFromAssemblyPath(testAssemblyPath);
         var proxyRunner = proxyAssembly.GetType(typeof(ProxyRunner).FullName!, throwOnError: true)!;
 
         var exception = Assert.Throws<TargetInvocationException>(
             () => proxyRunner.GetMethod(nameof(ProxyRunner.AccessField))!.Invoke(null, [target]));
         exception.InnerException.Should().BeOfType<MissingFieldException>();
-    }
-
-    private static object CreateGeneratedTarget(AssemblyLoadContext targetContext, string testAssemblyPath)
-    {
-        var targetAssembly = targetContext.LoadFromAssemblyPath(testAssemblyPath);
-        var fieldType = targetAssembly.GetType(typeof(FieldValue).FullName!, throwOnError: true)!;
-
-        // An ordinary type also present in the proxy assembly does not reproduce the mismatch.
-        // Contextual reflection makes this generated type belong only to targetContext.
-        using (AssemblyLoadContext.EnterContextualReflection(targetAssembly))
-        {
-            var assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("DuckTypingTargetAssembly"), AssemblyBuilderAccess.Run);
-            var type = assembly.DefineDynamicModule("MainModule").DefineType("DuckTypingTarget", TypeAttributes.Public);
-            type.DefineField("_field", fieldType, FieldAttributes.Private);
-            return Activator.CreateInstance(type.CreateType()!)!;
-        }
     }
 
     public static class ProxyRunner
@@ -67,10 +66,6 @@ public class AssemblyLoadContextTests
         {
             _ = target.DuckCast<ITargetProxy>().Field;
         }
-    }
-
-    public sealed class FieldValue
-    {
     }
 
     private sealed class ProxyLoadContext : AssemblyLoadContext
