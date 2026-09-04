88b36ed9f19bdfd48cb27db4ceb03e6b530d662a
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2026-09-04 11:16:10 +0200

    Use ExceptionHashProvider

--- tracer/src/Datadog.Trace/Debugger/ExceptionAutoInstrumentation/ExceptionHashProvider.cs
+++ tracer/src/Datadog.Trace/Debugger/ExceptionAutoInstrumentation/ExceptionHashProvider.cs
@@ -1,16 +1,15 @@
-// <copyright file="MD5HashProvider.cs" company="Datadog">
+// <copyright file="ExceptionHashProvider.cs" company="Datadog">
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2 License.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System.Collections.Generic;
 using System.Linq;
 using Datadog.Trace.Debugger.Helpers;
 
 #nullable enable
 namespace Datadog.Trace.Debugger.ExceptionAutoInstrumentation
 {
-    internal static class MD5HashProvider
+    internal static class ExceptionHashProvider
     {
         internal static string GetHash(ExceptionIdentifier exceptionId)
         {
