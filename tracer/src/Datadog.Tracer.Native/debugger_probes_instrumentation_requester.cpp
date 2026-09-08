910635513ad092366f31d349a599070cd759b669
Dmitry Tsarevich <dzmitry.tsarevich@snoonu.com>
2026-09-08 17:59:47 +0300

    Drop unused std::uniform_int_distribution<> instance

--- tracer/src/Datadog.Tracer.Native/debugger_probes_instrumentation_requester.cpp
+++ tracer/src/Datadog.Tracer.Native/debugger_probes_instrumentation_requester.cpp
@@ -58,7 +58,6 @@ WSTRING DebuggerProbesInstrumentationRequester::GenerateRandomProbeId()
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 15);
-    std::uniform_int_distribution<> dis8(8, 11);
 
     std::wstringstream ss;
     int i;
