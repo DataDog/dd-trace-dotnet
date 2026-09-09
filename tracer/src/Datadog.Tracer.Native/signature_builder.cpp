ea1b8298e3b78d4281b744a6af7b9f637c9d7035
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2026-09-09 15:26:09 +0200

    [tracer] Fix heap corruption (#9199)

--- tracer/src/Datadog.Tracer.Native/signature_builder.cpp
+++ tracer/src/Datadog.Tracer.Native/signature_builder.cpp
@@ -1,5 +1,7 @@
 #include "signature_builder.h"
 
+#include <algorithm>
+
 SignatureBuilder::SignatureBuilder()
 {
     _buffer = _stackSignatureBuffer;
@@ -24,10 +26,11 @@ void SignatureBuilder::EnsureBufferSpace(size_t size)
 {
     if (_offset + size >= _length)
     {
-        auto newSignatureBuffer = std::make_unique<COR_SIGNATURE[]>(_length * 2);
-        memcpy(newSignatureBuffer.get(), _buffer, _length);
+        auto newLength = (std::max)(_length * 2, _offset + size);
+        auto newSignatureBuffer = std::make_unique<COR_SIGNATURE[]>(newLength);
+        memcpy(newSignatureBuffer.get(), _buffer, _offset);
         _heapSignatureBuffer = std::move(newSignatureBuffer);
         _buffer = _heapSignatureBuffer.get();
-        _length *= 2;
+        _length = newLength;
     }
 }
