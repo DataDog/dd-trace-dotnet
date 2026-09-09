a66ddc070f190b72aab1d2dbcac9d061d92b9ef3
Andrew Lock <andrew.lock@datadoghq.com>
2026-09-09 13:29:26 +0100

    Raise the auto-instrumentation support ceiling to .NET 11
    Bumps SupportedVersions.LatestDotNet to 11.*.* and regenerates the
    derived call-target artifacts. Also allow-lists the expected SSI
    guard-rail bail-out log line for .NET 11 preview runtimes, mirroring
    the existing .NET 10 entry.

--- tracer/build/_build/Build.Steps.cs
+++ tracer/build/_build/Build.Steps.cs
@@ -2720,6 +2720,12 @@ partial class Build
                knownPatterns.Add(new(@".*SingleStepGuardRails::ShouldForceInstrumentationOverride: Found incompatible runtime .NET 10 or higher.*", RegexOptions.Compiled));
            }
 
+           // Make sure we _only_ add this while .NET 11 is in preview (to make sure we don't forget in the final release)
+           if (RuntimeInformation.FrameworkDescription.StartsWith(".NET 11.0.0-"))
+           {
+               knownPatterns.Add(new(@".*SingleStepGuardRails::ShouldForceInstrumentationOverride: Found incompatible runtime .NET 11 or higher.*", RegexOptions.Compiled));
+           }
+
            // CI Visibility known errors
            knownPatterns.Add(new(@".*The Git repository couldn't be automatically extracted.*", RegexOptions.Compiled));
            knownPatterns.Add(new(@".*DD_GIT_REPOSITORY_URL is set with.*", RegexOptions.Compiled));
