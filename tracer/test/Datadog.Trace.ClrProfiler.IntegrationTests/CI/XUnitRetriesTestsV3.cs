d9e5b2719710c0cfeea73c7c3e92de04da0b6187
Tony Redondo <tony.redondo@datadoghq.com>
2026-09-11 11:47:34 +0200

    [CI Visibility] Preserve quarantine with automatic retries across test frameworks

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/XUnitRetriesTestsV3.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/CI/XUnitRetriesTestsV3.cs
@@ -32,6 +32,17 @@ public class XUnitRetriesTestsV3 : TestingFrameworkRetriesTests
 
     protected override bool UseDotnetExec => true;
 
+    public static IEnumerable<object[]> GetQuarantineRetryData() => GetQuarantineRetryData(PackageVersions.XUnitRetriesV3);
+
+    [SkippableTheory]
+    [MemberData(nameof(GetQuarantineRetryData))]
+    [Trait("Category", "EndToEnd")]
+    [Trait("Category", "TestIntegrations")]
+    [Trait("Category", "QuarantinedTests")]
+    [Trait("Category", "FlakyRetries")]
+    public override Task QuarantineWithAutomaticRetries(string packageVersion, bool quarantined, bool retriesEnabled, bool quarantineAlwaysFails)
+        => base.QuarantineWithAutomaticRetries(packageVersion, quarantined, retriesEnabled, quarantineAlwaysFails);
+
     [SkippableTheory]
     [MemberData(nameof(PackageVersions.XUnitRetriesV3), MemberType = typeof(PackageVersions))]
     [Trait("Category", "EndToEnd")]
