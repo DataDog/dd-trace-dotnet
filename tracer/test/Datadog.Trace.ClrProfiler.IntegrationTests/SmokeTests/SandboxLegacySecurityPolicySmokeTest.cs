b32eb9d7df4723b590dc7b7c3bc70a6e4f05d0a4
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2026-09-14 14:26:01 +0200

    Update SandboxLegacySecurityPolicySmokeTest.cs

--- tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/SandboxLegacySecurityPolicySmokeTest.cs
+++ tracer/test/Datadog.Trace.ClrProfiler.IntegrationTests/SmokeTests/SandboxLegacySecurityPolicySmokeTest.cs
@@ -4,9 +4,7 @@
 // </copyright>
 
 #if NETFRAMEWORK
-using System.IO;
 using System.Threading.Tasks;
-using Datadog.Trace.Configuration;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -22,9 +20,10 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.SmokeTests
         public SandboxLegacySecurityPolicySmokeTest(ITestOutputHelper output)
             : base(output, "Sandbox.LegacySecurityPolicy")
         {
-            // This test expects instrumentation to fail gracefully, which writes an error log.
-            // Keep that expected error out of the directory scanned by CheckBuildLogsForErrors.
-            SetEnvironmentVariable(ConfigurationKeys.LogDirectory, Path.GetTempPath());
+            // The managed tracer is intentionally not loaded in this test, so the Continuous Profiler
+            // cannot receive its managed activation configuration.
+            SetEnvironmentVariable("DD_PROFILING_ENABLED", "0");
+            SetEnvironmentVariable("DD_PROFILING_MANAGED_ACTIVATION_ENABLED", "0");
         }
 
         [SkippableFact]
