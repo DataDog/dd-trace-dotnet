f1259f2b018e30e3ec30a6ec2aecd116fc43dd4f
Mohammad Islam <mohammad.islam@datadoghq.com>
2026-09-14 15:21:48 -0400

    Honor activity integration setting changes

--- tracer/src/Datadog.Trace/Activity/Handlers/AzureServiceBusActivityHandler.cs
+++ tracer/src/Datadog.Trace/Activity/Handlers/AzureServiceBusActivityHandler.cs
@@ -14,14 +14,16 @@ namespace Datadog.Trace.Activity.Handlers
     internal sealed class AzureServiceBusActivityHandler : IActivityHandler
     {
         public bool ShouldListenTo(string sourceName, string? version)
-            => sourceName.StartsWith("Azure.Messaging.ServiceBus")
-            && Tracer.Instance.CurrentTraceSettings.Settings.IsIntegrationEnabled(IntegrationId.AzureServiceBus);
+            => sourceName.StartsWith("Azure.Messaging.ServiceBus");
 
         public void ActivityStarted<T>(string sourceName, T activity)
             where T : IActivity
         {
+            var integrationId = Tracer.Instance.CurrentTraceSettings.Settings.IsIntegrationEnabled(IntegrationId.AzureServiceBus)
+                                    ? IntegrationId.AzureServiceBus
+                                    : IntegrationId.OpenTelemetry;
             var tags = Tracer.Instance.CurrentTraceSettings.Schema.Client.CreateAzureServiceBusTags();
-            ActivityHandlerCommon.ActivityStarted(IntegrationId.AzureServiceBus, sourceName, activity, tags: tags, out var activityMapping);
+            ActivityHandlerCommon.ActivityStarted(integrationId, sourceName, activity, tags: tags, out _);
         }
 
         public void ActivityStopped<T>(string sourceName, T activity)
--- tracer/src/Datadog.Trace/Activity/Handlers/QuartzActivityHandler.cs
+++ tracer/src/Datadog.Trace/Activity/Handlers/QuartzActivityHandler.cs
@@ -32,14 +32,16 @@ namespace Datadog.Trace.Activity.Handlers
         public bool ShouldListenTo(string sourceName, string? version)
         {
             // Listen to Quartz diagnostic source
-            return sourceName.StartsWith("Quartz")
-                && Tracer.Instance.CurrentTraceSettings.Settings.IsIntegrationEnabled(IntegrationId.Quartz);
+            return sourceName.StartsWith("Quartz");
         }
 
         public void ActivityStarted<T>(string sourceName, T activity)
             where T : IActivity
         {
-            ActivityHandlerCommon.ActivityStarted(IntegrationId.Quartz, sourceName, activity, tags: new OpenTelemetryTags(), out var activityMapping);
+            var integrationId = Tracer.Instance.CurrentTraceSettings.Settings.IsIntegrationEnabled(IntegrationId.Quartz)
+                                    ? IntegrationId.Quartz
+                                    : IntegrationId.OpenTelemetry;
+            ActivityHandlerCommon.ActivityStarted(integrationId, sourceName, activity, tags: new OpenTelemetryTags(), out _);
         }
 
         public void ActivityStopped<T>(string sourceName, T activity)
