6abc4c36e91e9aa5c03b4ab39dee75a26cd15464
Andrew Lock <andrew.lock@datadoghq.com>
2026-09-18 14:17:18 +0100

    Fix suspended process start for .NET 11
    The internals have changed, and this is now a natively provided API (and our shim is broken on .NET 11)

--- tracer/test/Datadog.Trace.TestHelpers/NativeProcess/SuspendedProcess.cs
+++ tracer/test/Datadog.Trace.TestHelpers/NativeProcess/SuspendedProcess.cs
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
+#if !NET11_0_OR_GREATER
+
 #nullable enable
 
 using System;
@@ -97,3 +99,4 @@ internal class SuspendedProcess(ProcessStartInfo startInfo, int pid, SafeProcess
         }
     }
 }
+#endif
